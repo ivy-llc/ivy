4d98d3d0946f6c3bb9f30aa2196dfef029f0b83a
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-13 11:14:24 +0100

    finished improving unit tests +  modifying methods for logic.py.
    Former-commit-id: d10bde6e0430f36affb5e4a04697daa564045186

--- ivy_tests/test_core/test_logic.py
+++ ivy_tests/test_core/test_logic.py
@@ -3,44 +3,85 @@ Collection of tests for templated logic functions
 """
 
 # global
+import pytest
 import numpy as np
 
 # local
 import ivy
+import ivy.numpy
 import ivy_tests.helpers as helpers
 
 
-def test_logical_and(dev_str, call):
-    assert np.array_equal(call(ivy.logical_and, ivy.array([True, True]),
-                               ivy.array([False, True])),
-                          np.logical_and(np.array([True, True]), np.array([False, True])))
-    assert np.array_equal(call(ivy.logical_and, ivy.array([[0.]]),
-                               ivy.array([[1.]])),
-                          np.logical_and(np.array([[0.]]), np.array([[1.]])))
+# logical_and
+@pytest.mark.parametrize(
+    "x1_n_x2", [([True, True], [False, True]), ([[0.]], [[1.]])])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_logical_and(x1_n_x2, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    x1, x2 = x1_n_x2
+    x1 = tensor_fn(x1, dtype_str, dev_str)
+    x2 = tensor_fn(x2, dtype_str, dev_str)
+    ret = ivy.logical_and(x1, x2)
+    # type test
+    assert isinstance(ret, ivy.Array)
+    # cardinality test
+    assert ret.shape == x1.shape
+    # value test
+    assert np.allclose(call(ivy.logical_and, x1, x2), ivy.numpy.logical_and(ivy.to_numpy(x1), ivy.to_numpy(x2)))
+    # compilation test
     if call in [helpers.torch_call]:
         # pytorch scripting does not support .type() method
         return
     helpers.assert_compilable(ivy.logical_and)
 
 
-def test_logical_or(dev_str, call):
-    assert np.array_equal(call(ivy.logical_or, ivy.array([True, True]),
-                               ivy.array([False, True])),
-                          np.logical_or(np.array([True, True]), np.array([False, True])))
-    assert np.array_equal(call(ivy.logical_or, ivy.array([[0.]]),
-                               ivy.array([[1.]])),
-                          np.logical_or(np.array([[0.]]), np.array([[1.]])))
+# logical_or
+@pytest.mark.parametrize(
+    "x1_n_x2", [([True, True], [False, True]), ([[0.]], [[1.]])])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_logical_or(x1_n_x2, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    x1, x2 = x1_n_x2
+    x1 = tensor_fn(x1, dtype_str, dev_str)
+    x2 = tensor_fn(x2, dtype_str, dev_str)
+    ret = ivy.logical_or(x1, x2)
+    # type test
+    assert isinstance(ret, ivy.Array)
+    # cardinality test
+    assert ret.shape == x1.shape
+    # value test
+    assert np.allclose(call(ivy.logical_or, x1, x2), ivy.numpy.logical_or(ivy.to_numpy(x1), ivy.to_numpy(x2)))
+    # compilation test
     if call in [helpers.torch_call]:
         # pytorch scripting does not support .type() method
         return
     helpers.assert_compilable(ivy.logical_or)
 
 
-def test_logical_not(dev_str, call):
-    assert np.array_equal(call(ivy.logical_not, ivy.array([True, True])),
-                          np.logical_not(np.array([True, True])))
-    assert np.array_equal(call(ivy.logical_not, ivy.array([[0.]])),
-                          np.logical_not(np.array([[0.]])))
+# logical_not
+@pytest.mark.parametrize(
+    "x", [[True, True], [[0.]]])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_logical_not(x, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    x = tensor_fn(x, dtype_str, dev_str)
+    ret = ivy.logical_not(x)
+    # type test
+    assert isinstance(ret, ivy.Array)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.logical_not, x), ivy.numpy.logical_not(ivy.to_numpy(x)))
+    # compilation test
     if call in [helpers.torch_call]:
         # pytorch scripting does not support .type() method
         return
