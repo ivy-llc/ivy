1f4721c236e095baea2d8e7ed12986f2d83f7300
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-13 12:21:51 +0100

    finished improving unit tests +  modifying methods for random.py.

--- ivy/tensorflow/core/random.py
+++ ivy/tensorflow/core/random.py
@@ -5,7 +5,8 @@ Collection of TensorFlow random functions, wrapped to fit Ivy syntax and signatu
 # global
 import tensorflow as _tf
 
-random_uniform = lambda low=0., high=1., shape=None, dev=None: _tf.random.uniform(shape, low, high)
-randint = lambda low, high, shape, dev=None: _tf.random.uniform(shape=shape, minval=low, maxval=high, dtype=_tf.int32)
+random_uniform = lambda low=0., high=1., shape=None, dev_str=None:\
+    _tf.random.uniform(shape if shape else (), low, high)
+randint = lambda low, high, shape, dev_str=None: _tf.random.uniform(shape=shape, minval=low, maxval=high, dtype=_tf.int32)
 seed = lambda seed_value=0: _tf.random.set_seed(seed_value)
 shuffle = lambda x: _tf.random.shuffle(x)
