2946b28a34b2ad3970acc9fa0ee389d15dcc6d9f
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-15 17:04:58 +0100

    modified reductions to always at least return 1-dim array. 0-dim arrays cannot propagate gradients in mxnet.
    Former-commit-id: 6b0d2cacecb1247b86e95fc6c14aa81991c95061

--- ivy/mxnd/core/reductions.py
+++ ivy/mxnd/core/reductions.py
@@ -6,9 +6,6 @@ Collection of MXNet reduction functions, wrapped to fit Ivy syntax and signature
 import mxnet as _mx
 from numbers import Number
 
-# local
-from ivy.mxnd.core.general import _1_dim_array_to_flat_array
-
 
 def reduce_sum(x, axis=None, keepdims=False):
     axis_specified = True
@@ -20,9 +17,7 @@ def reduce_sum(x, axis=None, keepdims=False):
         axis = (axis,)
     elif isinstance(axis, list):
         axis = tuple(axis)
-    if keepdims or (axis_specified and len(axis) != len(x.shape)):
-        return _mx.nd.sum(x, axis=axis, keepdims=keepdims)
-    return _1_dim_array_to_flat_array(_mx.nd.sum(x, axis=axis, keepdims=keepdims))
+    return _mx.nd.sum(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_prod(x, axis=None, keepdims=False):
@@ -35,9 +30,7 @@ def reduce_prod(x, axis=None, keepdims=False):
         axis = (axis,)
     elif isinstance(axis, list):
         axis = tuple(axis)
-    if keepdims or (axis_specified and len(axis) != len(x.shape)):
-        return _mx.nd.prod(x, axis=axis, keepdims=keepdims)
-    return _1_dim_array_to_flat_array(_mx.nd.prod(x, axis=axis, keepdims=keepdims))
+    return _mx.nd.prod(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_mean(x, axis=None, keepdims=False):
@@ -50,9 +43,7 @@ def reduce_mean(x, axis=None, keepdims=False):
         axis = (axis,)
     elif isinstance(axis, list):
         axis = tuple(axis)
-    if keepdims or (axis_specified and len(axis) != len(x.shape)):
-        return _mx.nd.mean(x, axis=axis, keepdims=keepdims)
-    return _1_dim_array_to_flat_array(_mx.nd.mean(x, axis=axis, keepdims=keepdims))
+    return _mx.nd.mean(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_min(x, axis=None, keepdims=False):
@@ -65,9 +56,7 @@ def reduce_min(x, axis=None, keepdims=False):
         axis = (axis,)
     elif isinstance(axis, list):
         axis = tuple(axis)
-    if keepdims or (axis_specified and len(axis) != len(x.shape)):
-        return _mx.nd.min(x, axis=axis, keepdims=keepdims)
-    return _1_dim_array_to_flat_array(_mx.nd.min(x, axis=axis, keepdims=keepdims))
+    return _mx.nd.min(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_max(x, axis=None, keepdims=False):
@@ -80,6 +69,4 @@ def reduce_max(x, axis=None, keepdims=False):
         axis = (axis,)
     elif isinstance(axis, list):
         axis = tuple(axis)
-    if keepdims or (axis_specified and len(axis) != len(x.shape)):
-        return _mx.nd.max(x, axis=axis, keepdims=keepdims)
-    return _1_dim_array_to_flat_array(_mx.nd.max(x, axis=axis, keepdims=keepdims))
+    return _mx.nd.max(x, axis=axis, keepdims=keepdims)
