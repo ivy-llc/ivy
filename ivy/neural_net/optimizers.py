0fd844ea71c4d8dd54e4b710d7d5e20f3e1710f0
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-24 21:54:10 +0100

    added Adam optimizer class.
    Former-commit-id: 80a3f442b719bb7b139c957fa9210abe576be432

--- ivy/neural_net/optimizers.py
+++ ivy/neural_net/optimizers.py
@@ -80,3 +80,51 @@ class SGD(Optimizer):
         :return: The new updated variables container, following gradient descent step.
         """
         return ivy.gradient_descent_update(v, grads, self._lr)
+
+
+class Adam(Optimizer):
+
+    def __init__(self, lr=1e-4, beta1=0.9, beta2=0.999, epsilon=1e-07, compile_step=False):
+        """
+        Construct an ADAM optimizer.
+
+        :param lr: Learning rate, default is 1e-4.
+        :type lr: float, optional
+        :param beta1: gradient forgetting factor, default is 0.9
+        :type beta1: float, optional
+        :param beta2: second moment of gradient forgetting factor, default is 0.999
+        :type beta2: float, optional
+        :param epsilon: divisor during adam update, preventing division by zero, default is 1e-07
+        :type epsilon: float, optional
+        :param compile_step: Whether to compile the optimizer step, default is False.
+        :type compile_step: bool, option
+        """
+        Optimizer.__init__(self, lr, compile_step)
+        self._beta1 = beta1
+        self._beta2 = beta2
+        self._epsilon = epsilon
+        self._mw = None
+        self._vw = None
+        self._first_pass = True
+        self._step = ivy.array([0])
+
+    # Custom Step
+
+    def _step(self, v, grads):
+        """
+        Update nested variables container v by Adam update step, using nested grads container.
+
+        :param v: Nested variables to update.
+        :type v: Ivy container of variables
+        :param grads: Nested gradients to update.
+        :type grads: sequence of arrays
+        :return: The updated variables, following Adam update step.
+        """
+        if self._first_pass:
+            self._mw = grads
+            self._vw = grads.map(lambda x, _: x ** 2)
+            self._first_pass = False
+        new_v, self._mw, self._vw = ivy.adam_update(
+            v, grads, self._lr, self._mw, self._vw, self._step, self._beta1, self._beta2, self._epsilon)
+        self._step += 1
+        return new_v
