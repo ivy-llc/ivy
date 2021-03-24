8cea4fa1dd025b8d6d25ea2362fc1a64b041cb65
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-24 21:49:30 +0100

    added new module for stateful optimizers, starting with SGD class.

---
+++ ivy_tests/test_nn/test_optimizers.py
@@ -0,0 +1,76 @@
+"""
+Collection of tests for Ivy optimizers
+"""
+
+# global
+import pytest
+import numpy as np
+
+# local
+import ivy
+import ivy_tests.helpers as helpers
+from ivy.core.container import Container
+
+
+# sgd
+@pytest.mark.parametrize(
+    "bs_ic_oc_target", [
+        ([1, 2], 4, 5, [[0.30230279, 0.65123089, 0.30132881, -0.90954636, 1.08810135]]),
+    ])
+@pytest.mark.parametrize(
+    "with_v", [True, False])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_sgd_optimizer(bs_ic_oc_target, with_v, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    if call is helpers.np_call:
+        # NumPy does not support gradients
+        pytest.skip()
+    batch_shape, input_channels, output_channels, target = bs_ic_oc_target
+    x = ivy.cast(ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), input_channels), 'float32')
+    if with_v:
+        np.random.seed(0)
+        wlim = (6 / (output_channels + input_channels)) ** 0.5
+        w = ivy.variable(ivy.array(np.random.uniform(-wlim, wlim, (output_channels, input_channels)), 'float32'))
+        b = ivy.variable(ivy.zeros([output_channels]))
+        v = Container({'w': w, 'b': b})
+    else:
+        v = None
+    linear_layer = ivy.Linear(input_channels, output_channels, v)
+
+    def loss_fn(v_):
+        out = linear_layer(x, v=v_)
+        return ivy.reduce_mean(out)[0]
+
+    # optimizer
+    optimizer = ivy.SGD()
+
+    # train
+    loss_tm1 = 1e12
+    loss = None
+    grads = None
+    for i in range(10):
+        loss, grads = ivy.execute_with_gradients(loss_fn, linear_layer.v)
+        linear_layer.v = optimizer.step(linear_layer.v, grads)
+        assert loss < loss_tm1
+        loss_tm1 = loss
+
+    # type test
+    assert isinstance(loss, ivy.Array)
+    assert isinstance(grads, ivy.Container)
+    # cardinality test
+    if call is helpers.mx_call:
+        # mxnet slicing cannot reduce dimension to zero
+        assert loss.shape == (1,)
+    else:
+        assert loss.shape == ()
+    # value test
+    assert ivy.reduce_max(ivy.abs(grads.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.w)) > 0
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not **kwargs
+        return
+    helpers.assert_compilable(loss_fn)
