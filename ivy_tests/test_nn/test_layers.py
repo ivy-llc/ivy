84eefb8f92c26de81ba40c19c32e0f75ffc9a26d
djl11 <daniel.lenton11@imperial.ac.uk>
2021-03-24 18:17:23 +0100

    replaced layer class with more general module class, with internal variable tracking, and a wrapper function for handling variables in functional form, allowing use of execute_with_grads() method. Also added tests for training modules and layers over multiple time steps.
    Former-commit-id: ecb476b3d3c578ec06fbbf95d5f6c405825e2f30

--- ivy_tests/test_nn/test_layers.py
+++ ivy_tests/test_nn/test_layers.py
@@ -55,6 +55,67 @@ def test_linear_layer(bs_ic_oc_target, with_v, dtype_str, tensor_fn, dev_str, ca
     helpers.assert_compilable(linear_layer)
 
 
+# linear training
+@pytest.mark.parametrize(
+    "bs_ic_oc_target", [
+        ([1, 2], 4, 5, [[0.30230279, 0.65123089, 0.30132881, -0.90954636, 1.08810135]]),
+    ])
+@pytest.mark.parametrize(
+    "with_v", [True, False])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_linear_layer_training(bs_ic_oc_target, with_v, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    if call is helpers.np_call:
+        # NumPy does not support gradients
+        pytest.skip()
+    batch_shape, input_channels, output_channels, target = bs_ic_oc_target
+    x = ivy.cast(ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), input_channels), 'float32')
+    if with_v:
+        np.random.seed(0)
+        wlim = (6 / (output_channels + input_channels)) ** 0.5
+        w = ivy.variable(ivy.array(np.random.uniform(-wlim, wlim, (output_channels, input_channels)), 'float32'))
+        b = ivy.variable(ivy.zeros([output_channels]))
+        v = Container({'w': w, 'b': b})
+    else:
+        v = None
+    linear_layer = ivy.Linear(input_channels, output_channels, v)
+
+    def loss_fn(v_):
+        out = linear_layer(x, v=v_)
+        return ivy.reduce_mean(out)[0]
+
+    # train
+    loss_tm1 = 1e12
+    loss = None
+    grads = None
+    for i in range(10):
+        loss, grads = ivy.execute_with_gradients(loss_fn, linear_layer.v)
+        linear_layer.v = ivy.gradient_descent_update(linear_layer.v, grads, 1e-3)
+        assert loss < loss_tm1
+        loss_tm1 = loss
+
+    # type test
+    assert isinstance(loss, ivy.Array)
+    assert isinstance(grads, ivy.Container)
+    # cardinality test
+    if call is helpers.mx_call:
+        # mxnet slicing cannot reduce dimension to zero
+        assert loss.shape == (1,)
+    else:
+        assert loss.shape == ()
+    # value test
+    assert ivy.reduce_max(ivy.abs(grads.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.w)) > 0
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not **kwargs
+        return
+    helpers.assert_compilable(loss_fn)
+
+
 # LSTM #
 # -----#
 
@@ -82,8 +143,8 @@ def test_lstm_layer(b_t_ic_hc_otf_sctv, with_v, with_initial_state, dtype_str, t
     else:
         initial_state = None
     if with_v:
-        kernel = ivy.variable(ivy.ones([input_channels, 4*hidden_channels]))*0.5
-        recurrent_kernel = ivy.variable(ivy.ones([hidden_channels, 4*hidden_channels]))*0.5
+        kernel = ivy.variable(ivy.ones([input_channels, 4*hidden_channels])*0.5)
+        recurrent_kernel = ivy.variable(ivy.ones([hidden_channels, 4*hidden_channels])*0.5)
         v = Container({'input': {'layer_0': {'w': kernel}},
                        'recurrent': {'layer_0': {'w': recurrent_kernel}}})
     else:
@@ -111,3 +172,64 @@ def test_lstm_layer(b_t_ic_hc_otf_sctv, with_v, with_initial_state, dtype_str, t
         # this is not a backend implemented function
         pytest.skip()
     helpers.assert_compilable(ivy.lstm_update)
+
+
+# lstm training
+@pytest.mark.parametrize(
+    "b_t_ic_hc_otf_sctv", [
+        (2, 3, 4, 5, [0.93137765, 0.9587628, 0.96644664, 0.93137765, 0.9587628, 0.96644664], 3.708991),
+    ])
+@pytest.mark.parametrize(
+    "with_v", [True, False])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_lstm_layer_training(b_t_ic_hc_otf_sctv, with_v, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    if call is helpers.np_call:
+        # NumPy does not support gradients
+        pytest.skip()
+    # smoke test
+    b, t, input_channels, hidden_channels, output_true_flat, state_c_true_val = b_t_ic_hc_otf_sctv
+    x = ivy.cast(ivy.linspace(ivy.zeros([b, t]), ivy.ones([b, t]), input_channels), 'float32')
+    if with_v:
+        kernel = ivy.variable(ivy.ones([input_channels, 4*hidden_channels])*0.5)
+        recurrent_kernel = ivy.variable(ivy.ones([hidden_channels, 4*hidden_channels])*0.5)
+        v = Container({'input': {'layer_0': {'w': kernel}},
+                       'recurrent': {'layer_0': {'w': recurrent_kernel}}})
+    else:
+        v = None
+    lstm_layer = ivy.LSTM(input_channels, hidden_channels, v=v)
+
+    def loss_fn(v_):
+        out, (state_h, state_c) = lstm_layer(x, v=v_)
+        return ivy.reduce_mean(out)[0]
+
+    # train
+    loss_tm1 = 1e12
+    loss = None
+    grads = None
+    for i in range(10):
+        loss, grads = ivy.execute_with_gradients(loss_fn, lstm_layer.v)
+        lstm_layer.v = ivy.gradient_descent_update(lstm_layer.v, grads, 1e-3)
+        assert loss < loss_tm1
+        loss_tm1 = loss
+
+    # type test
+    assert isinstance(loss, ivy.Array)
+    assert isinstance(grads, ivy.Container)
+    # cardinality test
+    if call is helpers.mx_call:
+        # mxnet slicing cannot reduce dimension to zero
+        assert loss.shape == (1,)
+    else:
+        assert loss.shape == ()
+    # value test
+    for key, val in grads.to_iterator():
+        assert ivy.reduce_max(ivy.abs(val)) > 0
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not **kwargs
+        return
+    helpers.assert_compilable(loss_fn)
