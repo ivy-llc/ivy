9473599dc11309c07bc9bd0cdd8e070926841a97
djl11 <daniel.lenton11@imperial.ac.uk>
2021-06-08 14:50:24 +0200

    added conv1DTranspose stateful layer.

--- ivy_tests/test_nn/test_layers_training.py
+++ ivy_tests/test_nn/test_layers_training.py
@@ -80,6 +80,7 @@ def test_linear_layer_training(bs_ic_oc_target, with_v, dtype_str, tensor_fn, de
 # -------------#
 
 
+# conv1d
 @pytest.mark.parametrize(
     "x_n_fs_n_pad_n_res", [
         ([[[0.], [3.], [0.]]],
@@ -141,6 +142,91 @@ def test_conv1d_layer_training(x_n_fs_n_pad_n_res, with_v, dtype_str, tensor_fn,
     for i in range(10):
         loss, grads = ivy.execute_with_gradients(loss_fn, conv1d_layer.v)
         conv1d_layer.v = ivy.gradient_descent_update(conv1d_layer.v, grads, 1e-3)
+        assert loss < loss_tm1
+        loss_tm1 = loss
+
+    # type test
+    assert ivy.is_array(loss)
+    assert isinstance(grads, ivy.Container)
+    # cardinality test
+    if call is helpers.mx_call:
+        # mxnet slicing cannot reduce dimension to zero
+        assert loss.shape == (1,)
+    else:
+        assert loss.shape == ()
+    # value test
+    assert ivy.reduce_max(ivy.abs(grads.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.w)) > 0
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not **kwargs
+        return
+    helpers.assert_compilable(loss_fn)
+
+
+# conv1d transpose
+@pytest.mark.parametrize(
+    "x_n_fs_n_pad_n_outshp_n_res", [
+        ([[[0.], [3.], [0.]]],
+         3,
+         "SAME",
+         (1, 3, 1),
+         [[[0.5072848], [2.2363136], [1.0679483]]]),
+
+        ([[[0.], [3.], [0.]] for _ in range(5)],
+         3,
+         "SAME",
+         (5, 3, 1),
+         [[[0.5072848], [2.2363136], [1.0679483]] for _ in range(5)]),
+
+        ([[[0.], [3.], [0.]]],
+         3,
+         "VALID",
+         (1, 5, 1),
+         [[[0.], [0.5072848], [2.2363136], [1.0679483], [0.]]])])
+@pytest.mark.parametrize(
+    "with_v", [True, False])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_conv1d_transpose_layer_training(x_n_fs_n_pad_n_outshp_n_res, with_v, dtype_str, tensor_fn, dev_str, call):
+    if call in [helpers.tf_call, helpers.tf_graph_call] and 'cpu' in dev_str:
+        # tf conv1d does not work when CUDA is installed, but array is on CPU
+        pytest.skip()
+    if call in [helpers.np_call, helpers.jnp_call]:
+        # numpy and jax do not yet support conv1d
+        pytest.skip()
+    # smoke test
+    x, filter_size, padding, out_shape, target = x_n_fs_n_pad_n_outshp_n_res
+    x = tensor_fn(x, dtype_str, dev_str)
+    target = np.asarray(target)
+    input_channels = x.shape[-1]
+    output_channels = target.shape[-1]
+    batch_size = x.shape[0]
+    width = x.shape[1]
+    if with_v:
+        np.random.seed(0)
+        wlim = (6 / (output_channels + input_channels)) ** 0.5
+        w = ivy.variable(ivy.array(np.random.uniform(
+            -wlim, wlim, (filter_size, output_channels, input_channels)), 'float32'))
+        b = ivy.variable(ivy.zeros([1, 1, output_channels]))
+        v = Container({'w': w, 'b': b})
+    else:
+        v = None
+    conv1d_trans_layer = ivy.Conv1DTranspose(input_channels, output_channels, filter_size, 1, padding, out_shape, v=v)
+
+    def loss_fn(v_):
+        out = conv1d_trans_layer(x, v=v_)
+        return ivy.reduce_mean(out)[0]
+
+    # train
+    loss_tm1 = 1e12
+    loss = None
+    grads = None
+    for i in range(10):
+        loss, grads = ivy.execute_with_gradients(loss_fn, conv1d_trans_layer.v)
+        conv1d_trans_layer.v = ivy.gradient_descent_update(conv1d_trans_layer.v, grads, 1e-3)
         assert loss < loss_tm1
         loss_tm1 = loss
 
