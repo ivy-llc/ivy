acb73383a6cd7ce9c260f99c89dd76398b2a8e16
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-03 14:00:18 +0200

    added seperate FirstLayerSiren initializer class.
    Former-commit-id: f8f24e5d43974c1e2872f4dceb716f1fb5c7fcc4

--- ivy/neural_net_stateful/initializers.py
+++ ivy/neural_net_stateful/initializers.py
@@ -35,6 +35,26 @@ class Xavier:
         return ivy.variable(ivy.random_uniform(-wlim, wlim, var_shape, dev_str=dev_str))
 
 
+class FirstLayerSiren:
+
+    def __init__(self, var_shape=None, input_channels=None, output_channels=None):
+        self._var_shape = var_shape
+        if input_channels is None:
+            raise Exception('input_channels must be specified for Xavier initializer.')
+        self._input_channels = input_channels
+        if output_channels is None:
+            raise Exception('output_channels must be specified for Xavier initializer.')
+        self._output_channels = output_channels
+
+    def create_variables(self, dev_str, var_shape=None):
+        """
+        Create internal variables for the layer
+        """
+        wlim = 1 / self._input_channels
+        var_shape = var_shape if var_shape is not None else self._var_shape
+        return ivy.variable(ivy.random_uniform(-wlim, wlim, var_shape, dev_str=dev_str))
+
+
 class Siren:
 
     def __init__(self, var_shape=None, input_channels=None, output_channels=None):
@@ -50,6 +70,6 @@ class Siren:
         """
         Create internal variables for the layer
         """
-        wlim = (6 / self._input_channels) ** 0.5
+        wlim = ((6 / self._input_channels) ** 0.5) / 30
         var_shape = var_shape if var_shape is not None else self._var_shape
         return ivy.variable(ivy.random_uniform(-wlim, wlim, var_shape, dev_str=dev_str))
