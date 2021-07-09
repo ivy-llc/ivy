cd4b5cb90fe39e5944fc6eed251892de69a6659c
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-09 18:04:13 +0200

    added erf method to math module.

--- ivy/numpy/core/math.py
+++ ivy/numpy/core/math.py
@@ -4,6 +4,10 @@ Collection of Numpy math functions, wrapped to fit Ivy syntax and signature.
 
 # global
 import numpy as _np
+try:
+    from scipy.special import erf as _erf
+except (ImportError, ModuleNotFoundError):
+    _erf = None
 
 sin = _np.sin
 cos = _np.cos
@@ -20,3 +24,9 @@ acosh = _np.arccosh
 atanh = _np.arctanh
 log = _np.log
 exp = _np.exp
+
+
+def erf(x):
+    if _erf is None:
+        raise Exception('scipy must be installed in order to call ivy.erf with a numpy backend.')
+    return _erf(x)
