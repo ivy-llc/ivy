4345946a0bb39846ce97e0e55a734e90992c7f6a
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-09 19:01:54 +0200

    added gelu activation.

--- ivy/numpy/nn/activations.py
+++ ivy/numpy/nn/activations.py
@@ -4,9 +4,23 @@ Collection of Numpy activation functions, wrapped to fit Ivy syntax and signatur
 
 # global
 import numpy as _np
+try:
+    from scipy.special import erf as _erf
+except (ImportError, ModuleNotFoundError):
+    _erf = None
 
 relu = lambda x: _np.maximum(x, 0)
 leaky_relu = lambda x, alpha=0.2: _np.where(x > 0, x, x * alpha)
+
+
+def gelu(x, approximate=True):
+    if _erf is None:
+        raise Exception('scipy must be installed in order to call ivy.gelu with a numpy backend.')
+    if approximate:
+        return 0.5 * x * (1 + _np.tanh(_np.sqrt(2 / _np.pi) * (x + 0.044715 * x ** 3)))
+    return 0.5 * x * (1 + _erf(x/_np.sqrt(2)))
+
+
 tanh = _np.tanh
 sigmoid = lambda x: 1 / (1 + _np.exp(-x))
 
