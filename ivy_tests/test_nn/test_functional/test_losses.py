4fd520c95237e2552142cb7df1f546b345a1f519
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-10 01:02:22 +0200

    added cross_entropy and sparse_cross_entropy loss functions.

--- ivy_tests/test_nn/test_functional/test_losses.py
+++ ivy_tests/test_nn/test_functional/test_losses.py
@@ -9,28 +9,82 @@ import ivy.numpy
 import ivy_tests.helpers as helpers
 
 
-# binary_cross_entropy
+# cross_entropy
 @pytest.mark.parametrize(
-    "x_n_y_n_res", [([[0.3, 0.7, 0.5]], [[0., 1., 0.]], [[0.35667494, 0.35667494, 0.69314718]])])
+    "t_n_p_n_res", [([[0., 1., 0.]], [[0.3, 0.2, 0.5]], [1.609438])])
 @pytest.mark.parametrize(
     "dtype_str", ['float32'])
 @pytest.mark.parametrize(
     "tensor_fn", [ivy.array, helpers.var_fn])
-def test_binary_cross_entropy(x_n_y_n_res, dtype_str, tensor_fn, dev_str, call):
+def test_cross_entropy(t_n_p_n_res, dtype_str, tensor_fn, dev_str, call):
     # smoke test
-    x, y, true_target = x_n_y_n_res
-    x = tensor_fn(x, dtype_str, dev_str)
-    y = tensor_fn(y, dtype_str, dev_str)
-    ret = ivy.binary_cross_entropy(x, y)
+    true, pred, true_target = t_n_p_n_res
+    pred = tensor_fn(pred, dtype_str, dev_str)
+    true = tensor_fn(true, dtype_str, dev_str)
+    ret = ivy.cross_entropy(true, pred)
     # type test
     assert ivy.is_array(ret)
     # cardinality test
-    assert ret.shape == x.shape
+    assert list(ret.shape) == [1]
     # value test
-    assert np.allclose(call(ivy.binary_cross_entropy, x, y), np.asarray(true_target))
+    assert np.allclose(call(ivy.cross_entropy, true, pred), np.asarray(true_target))
+    # compilation test
+    if call in [helpers.torch_call]:
+        # cross_entropy does not have backend implementation,
+        # pytorch scripting requires direct bindings to work, which bypass get_framework()
+        return
+    helpers.assert_compilable(ivy.cross_entropy)
+
+
+# binary_cross_entropy
+@pytest.mark.parametrize(
+    "t_n_p_n_res", [([[0., 1., 0.]], [[0.3, 0.7, 0.5]], [[0.35667494, 0.35667494, 0.69314718]])])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_binary_cross_entropy(t_n_p_n_res, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    true, pred, true_target = t_n_p_n_res
+    pred = tensor_fn(pred, dtype_str, dev_str)
+    true = tensor_fn(true, dtype_str, dev_str)
+    ret = ivy.binary_cross_entropy(true, pred)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == pred.shape
+    # value test
+    assert np.allclose(call(ivy.binary_cross_entropy, true, pred), np.asarray(true_target))
     # compilation test
     if call in [helpers.torch_call]:
         # binary_cross_entropy does not have backend implementation,
         # pytorch scripting requires direct bindings to work, which bypass get_framework()
         return
     helpers.assert_compilable(ivy.binary_cross_entropy)
+
+
+# sparse_cross_entropy
+@pytest.mark.parametrize(
+    "t_n_p_n_res", [([1], [[0.3, 0.2, 0.5]], [1.609438])])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_sparse_cross_entropy(t_n_p_n_res, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    true, pred, true_target = t_n_p_n_res
+    pred = tensor_fn(pred, dtype_str, dev_str)
+    true = ivy.array(true, 'int32', dev_str)
+    ret = ivy.sparse_cross_entropy(true, pred)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert list(ret.shape) == [1]
+    # value test
+    assert np.allclose(call(ivy.sparse_cross_entropy, true, pred), np.asarray(true_target))
+    # compilation test
+    if call in [helpers.torch_call]:
+        # sparse_cross_entropy does not have backend implementation,
+        # pytorch scripting requires direct bindings to work, which bypass get_framework()
+        return
+    helpers.assert_compilable(ivy.sparse_cross_entropy)
