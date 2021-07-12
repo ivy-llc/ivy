24ce5c06753c38f2411700ff7a28e39ddf915a9b
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-12 17:26:28 +0200

    added container method set_at_key_chain.
    Former-commit-id: 2f9d18948183b638c47163db127ed18b0169a852

--- ivy_tests/test_core/test_container.py
+++ ivy_tests/test_core/test_container.py
@@ -45,6 +45,23 @@ def test_container_at_key_chain(dev_str, call):
     assert (sub_container == ivy.array([2]))[0]
 
 
+def test_container_set_at_key_chain(dev_str, call):
+    dict_in = {'a': ivy.array([1]),
+               'b': {'c': ivy.array([2]), 'd': ivy.array([3])}}
+    container = Container(dict_in)
+    new_container = container.set_at_key_chain('b/e', ivy.array([4]))
+    assert (new_container['a'] == ivy.array([1]))[0]
+    assert (new_container['b']['c'] == ivy.array([2]))[0]
+    assert (new_container['b']['d'] == ivy.array([3]))[0]
+    assert (new_container['b']['e'] == ivy.array([4]))[0]
+    new_container = container.set_at_key_chain('f', ivy.array([5]))
+    assert (new_container['a'] == ivy.array([1]))[0]
+    assert (new_container['b']['c'] == ivy.array([2]))[0]
+    assert (new_container['b']['d'] == ivy.array([3]))[0]
+    assert (new_container['b']['e'] == ivy.array([4]))[0]
+    assert (new_container['f'] == ivy.array([5]))[0]
+
+
 def test_container_prune_key_chain(dev_str, call):
     dict_in = {'a': ivy.array([1]),
                'b': {'c': ivy.array([2]), 'd': ivy.array([3])}}
