6f9fe06a5c71cef23716c8dea470eb7095fcf80c
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-16 16:03:56 +0200

    added test for FOMAML with different outer loss function, and added docstring to the method.

--- ivy_tests/test_core/test_meta.py
+++ ivy_tests/test_core/test_meta.py
@@ -12,12 +12,13 @@ import ivy.numpy
 import ivy_tests.helpers as helpers
 
 
-# maml_step
+# fomaml_step
 @pytest.mark.parametrize(
-    "igs_og", [(1, -0.01), (2, -0.02), (3, -0.03)])
-def test_maml_step(dev_str, call, igs_og):
+    "igs_og_wocf", [(1, -0.01, False), (2, -0.02, False), (3, -0.03, False),
+                    (1, 0.01, True), (2, 0.02, True), (3, 0.03, True)])
+def test_fomaml_step(dev_str, call, igs_og_wocf):
 
-    inner_grad_steps, true_outer_grad = igs_og
+    inner_grad_steps, true_outer_grad, with_outer_cost_fn = igs_og_wocf
 
     if call in [helpers.np_call, helpers.jnp_call]:
         # Numpy does not support gradients, and jax does not support gradients on custom nested classes
@@ -32,15 +33,18 @@ def test_maml_step(dev_str, call, igs_og):
     latent = ivy.Container({'latent': ivy.variable(ivy.array([0.]))})
 
     # batch
-    batch = ivy.Container({'x': ivy.array([[1.], [2.], [3.]])})
+    batch = ivy.Container({'x': ivy.array([[0.]])})
 
     # inner cost function
     def inner_cost_fn(sub_batch, inner_v, outer_v):
-        network_pred = inner_v['latent'] * outer_v['weight']
-        return -network_pred[0]
+        return -(inner_v['latent'] * outer_v['weight'])[0]
+
+    # outer cost function
+    def outer_cost_fn(sub_batch, inner_v, outer_v):
+        return (inner_v['latent'] * outer_v['weight'])[0]
 
     # meta update
     outer_cost, outer_grads = ivy.fomaml_step(
-        batch, inner_cost_fn, None, latent, weight, batch_size, inner_grad_steps, inner_learning_rate)
+        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
+        inner_grad_steps, inner_learning_rate)
     assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
-    print('framework: {}, value: {}'.format(call, ivy.to_numpy(outer_grads.weight[0]).item()))
