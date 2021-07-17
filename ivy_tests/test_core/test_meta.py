75091d59ac7457824132ccddb8529dfda907fd81
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-17 14:10:20 +0200

    added maml_step method, but currently the 2nd order terms are only correct for jax. Next to investigate this and fix for the other backend frameworks.

--- ivy_tests/test_core/test_meta.py
+++ ivy_tests/test_core/test_meta.py
@@ -12,6 +12,9 @@ import ivy.numpy
 import ivy_tests.helpers as helpers
 
 
+# First Order #
+# ------------#
+
 # fomaml_step
 @pytest.mark.parametrize(
     "igs_og_wocf", [(1, -0.01, False), (2, -0.02, False), (3, -0.03, False),
@@ -84,4 +87,50 @@ def test_reptile_step(dev_str, call, igs_og_wocf):
     outer_cost, outer_grads = ivy.reptile_step(
         batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
         inner_grad_steps, inner_learning_rate)
+    assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
+
+
+# Second Order #
+# -------------#
+
+# maml_step
+@pytest.mark.parametrize(
+    "igs_og_wocf", [(1, -0.02, False)])
+def test_maml_step(dev_str, call, igs_og_wocf):
+
+    inner_grad_steps, true_outer_grad, with_outer_cost_fn = igs_og_wocf
+
+    if call in [helpers.np_call]:
+        # Numpy does not support gradients
+        pytest.skip()
+
+    # ToDo: investigate why jax is the only framework where the second order terms are correct, and fix.
+    if call is not helpers.jnp_call:
+        # Currently only jax treats the inner loop optimization as unrolled graph
+        pytest.skip()
+
+    # config
+    batch_size = 1
+    inner_learning_rate = 1e-2
+
+    # create variables
+    weight = ivy.Container({'weight': ivy.variable(ivy.array([1.]))})
+    latent = ivy.Container({'latent': ivy.variable(ivy.array([0.]))})
+
+    # batch
+    batch = ivy.Container({'x': ivy.array([[0.]])})
+
+    # inner cost function
+    def inner_cost_fn(sub_batch, inner_v, outer_v):
+        return -(inner_v['latent'] * outer_v['weight'])[0]
+
+    # outer cost function
+    def outer_cost_fn(sub_batch, inner_v, outer_v):
+        return (inner_v['latent'] * outer_v['weight'])[0]
+
+    # meta update
+    outer_cost, outer_grads = ivy.maml_step(
+        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
+        inner_grad_steps, inner_learning_rate)
+    print('w {}'.format(outer_grads.weight[0]))
     assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
