241c51a23dfa643f7c45aee223ab57d420044ef4
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-17 15:40:23 +0200

    reduced redundant arguments to reptile method, and other small fixes.

--- ivy_tests/test_core/test_meta.py
+++ ivy_tests/test_core/test_meta.py
@@ -28,7 +28,7 @@ def test_fomaml_step(dev_str, call, igs_og_wocf):
         pytest.skip()
 
     # config
-    batch_size = 1
+    num_tasks = 1
     inner_learning_rate = 1e-2
 
     # create variables
@@ -48,46 +48,40 @@ def test_fomaml_step(dev_str, call, igs_og_wocf):
 
     # meta update
     outer_cost, outer_grads = ivy.fomaml_step(
-        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
+        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, num_tasks,
         inner_grad_steps, inner_learning_rate)
     assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
 
 
 # reptile_step
 @pytest.mark.parametrize(
-    "igs_og_wocf", [(1, -0.005, False), (2, -0.01, False), (3, -0.015, False)])
-def test_reptile_step(dev_str, call, igs_og_wocf):
+    "igs_og", [(1, -1.51), (2, -1.35), (3, -1.2726)])
+def test_reptile_step(dev_str, call, igs_og):
 
-    inner_grad_steps, true_outer_grad, with_outer_cost_fn = igs_og_wocf
+    inner_grad_steps, true_outer_grad = igs_og
 
     if call in [helpers.np_call, helpers.jnp_call]:
         # Numpy does not support gradients, and jax does not support gradients on custom nested classes
         pytest.skip()
 
     # config
-    batch_size = 1
+    num_tasks = 1
     inner_learning_rate = 1e-2
 
     # create variables
-    weight = ivy.Container({'weight': ivy.variable(ivy.array([1.]))})
-    latent = ivy.Container({'latent': ivy.variable(ivy.array([0.]))})
+    latent = ivy.Container({'latent': ivy.variable(ivy.array([1.]))})
 
     # batch
     batch = ivy.Container({'x': ivy.array([[0.]])})
 
-    # inner cost function
-    def inner_cost_fn(sub_batch, inner_v, outer_v):
-        return -(inner_v['latent'] * outer_v['weight'])[0]
-
-    # outer cost function
-    def outer_cost_fn(sub_batch, inner_v, outer_v):
-        return (inner_v['latent'] * outer_v['weight'])[0]
+    # cost function
+    def cost_fn(sub_batch, inner_v, outer_v):
+        return -(inner_v['latent'] * outer_v['latent'])[0]
 
     # meta update
     outer_cost, outer_grads = ivy.reptile_step(
-        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
-        inner_grad_steps, inner_learning_rate)
-    assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
+        batch, cost_fn, latent, num_tasks, inner_grad_steps, inner_learning_rate)
+    assert np.allclose(ivy.to_numpy(outer_grads.latent[0]), np.array(true_outer_grad), atol=1e-4)
 
 
 # Second Order #
@@ -100,8 +94,8 @@ def test_maml_step(dev_str, call, igs_og_wocf):
 
     inner_grad_steps, true_outer_grad, with_outer_cost_fn = igs_og_wocf
 
-    if call in [helpers.np_call]:
-        # Numpy does not support gradients
+    if call in [helpers.np_call, helpers.mx_call]:
+        # Numpy does not support gradients, mxnet.autograd.grad() does not support allow_unused like PyTorch
         pytest.skip()
 
     # ToDo: investigate why jax and pytorch are the only frameworks where the second order terms are correct, and fix.
@@ -110,7 +104,7 @@ def test_maml_step(dev_str, call, igs_og_wocf):
         pytest.skip()
 
     # config
-    batch_size = 1
+    num_tasks = 1
     inner_learning_rate = 1e-2
 
     # create variables
@@ -130,6 +124,6 @@ def test_maml_step(dev_str, call, igs_og_wocf):
 
     # meta update
     outer_cost, outer_grads = ivy.maml_step(
-        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, batch_size,
+        batch, inner_cost_fn, outer_cost_fn if with_outer_cost_fn else None, latent, weight, num_tasks,
         inner_grad_steps, inner_learning_rate)
     assert np.allclose(ivy.to_numpy(outer_grads.weight[0]), np.array(true_outer_grad))
