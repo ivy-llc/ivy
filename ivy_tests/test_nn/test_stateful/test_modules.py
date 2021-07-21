0d41389da3a4a03d075afd2510f03b52a65592ec
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-21 14:05:34 +0200

    made some changes to how ivy.Module instances hierarchically store their internal variables, and added support for storing variables in a dict structure in the module constructor.

--- ivy_tests/test_nn/test_stateful/test_modules.py
+++ ivy_tests/test_nn/test_stateful/test_modules.py
@@ -126,12 +126,12 @@ def test_module_w_list_training(bs_ic_oc, dev_str, call):
     else:
         assert loss.shape == ()
     # value test
-    assert ivy.reduce_max(ivy.abs(grads.layers0.b)) > 0
-    assert ivy.reduce_max(ivy.abs(grads.layers0.w)) > 0
-    assert ivy.reduce_max(ivy.abs(grads.layers1.b)) > 0
-    assert ivy.reduce_max(ivy.abs(grads.layers1.w)) > 0
-    assert ivy.reduce_max(ivy.abs(grads.layers2.b)) > 0
-    assert ivy.reduce_max(ivy.abs(grads.layers2.w)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v0.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v0.w)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v1.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v1.w)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v2.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.v2.w)) > 0
     # compilation test
     if call is helpers.torch_call:
         # pytest scripting does not support **kwargs
@@ -226,6 +226,71 @@ def test_module_training_with_duplicate(bs_c, same_layer, dev_str, call):
     assert ivy.reduce_max(ivy.abs(grads.linear0.w)) > 0
     if not same_layer:
         assert ivy.reduce_max(ivy.abs(grads.linear1.b)) > 0
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not support **kwargs
+        return
+    helpers.assert_compilable(loss_fn)
+
+
+class TrainableModuleWithDict(ivy.Module):
+
+    def __init__(self, in_size, out_size, dev_str='cpu', hidden_size=64):
+        linear0 = ivy.Linear(in_size, hidden_size)
+        linear1 = ivy.Linear(hidden_size, hidden_size)
+        linear2 = ivy.Linear(hidden_size, out_size)
+        self._layers = {'linear0': linear0, 'linear1': linear1, 'linear2': linear2}
+        ivy.Module.__init__(self, dev_str)
+
+    def _forward(self, x):
+        x = ivy.expand_dims(x, 0)
+        x = ivy.tanh(self._layers['linear0'](x))
+        x = ivy.tanh(self._layers['linear1'](x))
+        return ivy.tanh(self._layers['linear2'](x))[0]
+
+
+# module with dict training
+@pytest.mark.parametrize(
+    "bs_ic_oc", [([1, 2], 4, 5)])
+def test_module_w_dict_training(bs_ic_oc, dev_str, call):
+    # smoke test
+    if call is helpers.np_call:
+        # NumPy does not support gradients
+        pytest.skip()
+    batch_shape, input_channels, output_channels = bs_ic_oc
+    x = ivy.cast(ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), input_channels), 'float32')
+    module = TrainableModuleWithDict(input_channels, output_channels)
+
+    def loss_fn(v_):
+        out = module(x, v=v_)
+        return ivy.reduce_mean(out)[0]
+
+    # train
+    loss_tm1 = 1e12
+    loss = None
+    grads = None
+    for i in range(10):
+        loss, grads = ivy.execute_with_gradients(loss_fn, module.v)
+        module.v = ivy.gradient_descent_update(module.v, grads, 1e-3)
+        assert loss < loss_tm1
+        loss_tm1 = loss
+
+    # type test
+    assert ivy.is_array(loss)
+    assert isinstance(grads, ivy.Container)
+    # cardinality test
+    if call is helpers.mx_call:
+        # mxnet slicing cannot reduce dimension to zero
+        assert loss.shape == (1,)
+    else:
+        assert loss.shape == ()
+    # value test
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear0.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear0.w)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear1.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear1.w)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear2.b)) > 0
+    assert ivy.reduce_max(ivy.abs(grads.layers.linear2.w)) > 0
     # compilation test
     if call is helpers.torch_call:
         # pytest scripting does not support **kwargs
