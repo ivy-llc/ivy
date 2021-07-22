b7175971598460171a8625df7089b5784a965fa3
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-22 22:48:11 +0200

    added optional replace argument to multinomial method.
    Former-commit-id: 75d8f049ef9517c837b4f8d422a139ca3ff5de7f

--- ivy/tensorflow/core/random.py
+++ ivy/tensorflow/core/random.py
@@ -7,8 +7,16 @@ import tensorflow as _tf
 
 random_uniform = lambda low=0., high=1., shape=None, dev_str='cpu':\
     _tf.random.uniform(shape if shape else (), low, high)
-multinomial = lambda probs, num_samples:\
-    _tf.random.categorical(_tf.math.log(probs), num_samples)
+
+
+def multinomial(population_size, num_samples, probs=None, replace=True):
+    if not replace:
+        raise Exception('TensorFlow does not support multinomial without replacement')
+    if probs is None:
+        probs = _tf.ones((1, population_size,)) / population_size
+    return _tf.random.categorical(_tf.math.log(probs), num_samples)
+
+
 randint = lambda low, high, shape, dev_str='cpu':\
     _tf.random.uniform(shape=shape, minval=low, maxval=high, dtype=_tf.int32)
 seed = lambda seed_value=0: _tf.random.set_seed(seed_value)
