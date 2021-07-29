c1e7e407fb7500e166cf27ea1a28fae2c71d3fd2
djl11 <daniel.lenton11@imperial.ac.uk>
2021-07-29 15:53:42 +0200

    added new ivy.Container method reshape, which reshapes the constituent arrays, and can handle partial shape specifications for cases where the container array shapes vary.

--- ivy_tests/test_core/test_container.py
+++ ivy_tests/test_core/test_container.py
@@ -285,6 +285,49 @@ def test_container_swapaxes(dev_str, call):
     assert 'b/d' not in container_swapped
 
 
+def test_container_reshape(dev_str, call):
+    dict_in = {'a': ivy.array([[0., 1., 2., 3.]]),
+               'b': {'c': ivy.array([[5., 10., 15., 20.]]), 'd': ivy.array([[10., 9., 8., 7.]])}}
+    container = Container(dict_in)
+
+    # pre_shape only
+    container_reshaped = container.reshape((1, 2, 2))
+    assert np.allclose(ivy.to_numpy(container_reshaped['a']), np.array([[0., 1.], [2., 3.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.a), np.array([[0., 1.], [2., 3.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['c']), np.array([[5., 10.], [15., 20.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.c), np.array([[5., 10.], [15., 20.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['d']), np.array([[10., 9.], [8., 7.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.d), np.array([[10., 9.], [8., 7.]]))
+
+    # pre_shape and slice
+    dict_in = {'a': ivy.array([[[0., 1., 2., 3.], [0., 1., 2., 3.]]]),
+               'b': {'c': ivy.array([[[5., 10., 15.], [20., 25., 30.]]]), 'd': ivy.array([[[10.], [9.]]])}}
+    container = Container(dict_in)
+    container_reshaped = container.reshape((-1,), slice(2, None))
+    assert np.allclose(ivy.to_numpy(container_reshaped['a']), np.array([[0., 1., 2., 3.], [0., 1., 2., 3.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.a), np.array([[0., 1., 2., 3.], [0., 1., 2., 3.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['c']), np.array([[5., 10., 15.], [20., 25., 30.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.c), np.array([[5., 10., 15.], [20., 25., 30.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['d']), np.array([[10.], [9.]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.d), np.array([[10.], [9.]]))
+
+    # pre_shape, slice and post_shape
+    dict_in = {'a': ivy.array([[[0., 1., 2., 3.], [0., 1., 2., 3.]]]),
+               'b': {'c': ivy.array([[[5., 10., 15.], [20., 25., 30.]]]), 'd': ivy.array([[[10.], [9.]]])}}
+    container = Container(dict_in)
+    container_reshaped = container.reshape((-1,), slice(2, None), (1,))
+    assert np.allclose(ivy.to_numpy(container_reshaped['a']), np.array([[[0.], [1.], [2.], [3.]],
+                                                                        [[0.], [1.], [2.], [3.]]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.a), np.array([[[0.], [1.], [2.], [3.]],
+                                                                     [[0.], [1.], [2.], [3.]]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['c']), np.array([[[5.], [10.], [15.]],
+                                                                             [[20.], [25.], [30.]]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.c), np.array([[[5.], [10.], [15.]],
+                                                                       [[20.], [25.], [30.]]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped['b']['d']), np.array([[[10.]], [[9.]]]))
+    assert np.allclose(ivy.to_numpy(container_reshaped.b.d), np.array([[[10.]], [[9.]]]))
+
+
 def test_container_stop_gradients(dev_str, call):
     dict_in = {'a': ivy.variable(ivy.array([[[1., 2.], [3., 4.]], [[5., 6.], [7., 8.]]])),
                'b': {'c': ivy.variable(ivy.array([[[8., 7.], [6., 5.]], [[4., 3.], [2., 1.]]])),
@@ -738,12 +781,12 @@ def test_container_with_entries_as_lists(dev_str, call):
         assert value == expected_value
 
 
-def test_container_reshape(dev_str, call):
+def test_container_reshape_like(dev_str, call):
     container = Container({'a': ivy.array([[1.]]),
                            'b': {'c': ivy.array([[3.], [4.]]), 'd': ivy.array([[5.], [6.], [7.]])}})
     new_shapes = Container({'a': (1,),
                            'b': {'c': (1, 2, 1), 'd': (3, 1, 1)}})
-    container_reshaped = container.reshape(new_shapes)
+    container_reshaped = container.reshape_like(new_shapes)
     assert list(container_reshaped['a'].shape) == [1]
     assert list(container_reshaped.a.shape) == [1]
     assert list(container_reshaped['b']['c'].shape) == [1, 2, 1]
