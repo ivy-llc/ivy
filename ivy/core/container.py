bb4fa2f6d32f03b1e399e1233f89a698eaf6ffc8
djl11 <daniel.lenton11@imperial.ac.uk>
2021-08-13 17:56:55 +0200

    added ivy.Container method einops_reduce.

--- ivy/core/container.py
+++ ivy/core/container.py
@@ -886,6 +886,28 @@ class Container(dict):
         return self.map(lambda x, kc: einops.rearrange(x, pattern, **axes_lengths) if _ivy.is_array(x) else x,
                         key_chains, to_apply, prune_unapplied)
 
+    def einops_reduce(self, pattern,  reduction, key_chains=None, to_apply=True, prune_unapplied=False, **axes_lengths):
+        """
+        Perform einops reduce operation on each sub array in the container.
+
+        :param pattern: Reduction pattern.
+        :type pattern: str
+        :param reduction: One of available reductions ('min', 'max', 'sum', 'mean', 'prod'), or callable.
+        :type reduction: str or callable
+        :param axes_lengths: Any additional specifications for dimensions.
+        :type axes_lengths: keyword parameter args
+        :param key_chains: The key-chains to apply or not apply the method to. Default is None.
+        :type key_chains: list or dict of strs, optional
+        :param to_apply: If True, the method will be applied to key_chains, otherwise key_chains will be skipped.
+                         Default is True.
+        :type to_apply: bool, optional
+        :param prune_unapplied: Whether to prune key_chains for which the function was not applied. Default is False.
+        :type prune_unapplied: bool, optional
+        :return: ivy.Container with each chosen array having the axes swapped.
+        """
+        return self.map(lambda x, kc: einops.reduce(x, pattern, reduction, **axes_lengths) if _ivy.is_array(x) else x,
+                        key_chains, to_apply, prune_unapplied)
+
     def to_dev(self, dev_str, key_chains=None, to_apply=True, prune_unapplied=False):
         """
         Move the container arrays to the desired device, specified by device string.
