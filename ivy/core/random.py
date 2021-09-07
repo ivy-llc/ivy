c4063227b7460c658b1cab8606fb8455ea0acc51
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-08 01:43:49 +0200

    added method random_normal to random module.
    Former-commit-id: 99a5269c34dea13c78b85d95f3eb344595b891ad

--- ivy/core/random.py
+++ ivy/core/random.py
@@ -29,6 +29,25 @@ def random_uniform(low=0.0, high=1.0, shape=None, dev_str='cpu', f=None):
     return _cur_framework(f=f).random_uniform(low, high, shape, dev_str)
 
 
+def random_normal(mean=0.0, std=1.0, shape=None, dev_str='cpu', f=None):
+    """
+    Draws samples from a normal distribution.
+
+    :param mean: The mean of the normal distribution to sample from. Default is 0.
+    :type mean: float
+    :param std: The standard deviation of the normal distribution to sample from. Default is 1.
+    :type std: float
+    :param shape: Output shape. If the given shape is, e.g., (m, n, k), then m * n * k samples are drawn.
+                    If size is None (default), a single value is returned.
+    :param dev_str: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
+    :type dev_str: str
+    :param f: Machine learning framework. Inferred from inputs if None.
+    :type f: ml_framework, optional
+    :return: Drawn samples from the parameterized uniform distribution.
+    """
+    return _cur_framework(f=f).random_normal(mean, std, shape, dev_str)
+
+
 def multinomial(population_size, num_samples, batch_size, probs=None, replace=True, dev_str='cpu', f=None):
     """
     Draws samples from a multinomial distribution. Specifcally, returns a tensor where each row contains num_samples
