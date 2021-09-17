ceb0fea1c6ebee982f1b9d2f0b2b26a53915ab67
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-17 12:46:44 +0200

    bug fixes for ivy.Container key_chain parsing with '.' delimeter, and associated fixes to pytorch module converter method.

--- ivy/torch/nn/converters.py
+++ ivy/torch/nn/converters.py
@@ -15,7 +15,8 @@ def to_ivy_module(native_module):
 
         def __init__(self):
             self._native_module = native_module
-            self._native_params = OrderedDict(sorted(dict(native_module.named_parameters()).items()))
+            self._native_params = OrderedDict(
+                sorted([(k.replace('.', '_'), v) for k, v in dict(native_module.named_parameters()).items()]))
             ivy.Module.__init__(
                 self, dev_str=ivy.dev_to_str(next(native_module.parameters()).device), v=self._native_params)
 
