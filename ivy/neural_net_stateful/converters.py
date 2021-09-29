4c26d2c721e8f7604490ca0be3570eba7c33ecba
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-29 23:36:26 +0200

    added conveter function from Haiku modules to ivy modules.

--- ivy/neural_net_stateful/converters.py
+++ ivy/neural_net_stateful/converters.py
@@ -6,14 +6,21 @@ Converters from Native Modules to Ivy Modules
 from ivy.framework_handler import current_framework as _cur_framework
 
 
-def to_ivy_module(native_module, f=None):
+def to_ivy_module(native_module=None, native_module_class=None, args=None, kwargs=None, f=None):
     """
     Convert an instance of a trainable module from a native framework into a trainable ivy.Module instance.
 
-    :param native_module: The module in the native framework to convert.
-    :type native_module: native module instance
+    :param native_module: The module in the native framework to convert, required if native_module_class is not given.
+                          Default is None.
+    :type native_module: native module instance, optional
+    :param native_module_class: The class of the native module, required if native_module is not given. Default is None.
+    :type native_module_class: class, optional
+    :param args: Positional arguments to pass to the native module class. Default is None.
+    :type args: list of any
+    :param kwargs: Key-word arguments to pass to the native module class. Default is None.
+    :type kwargs: dict of any
     :param f: Machine learning library. Inferred from Inputs if None.
     :type f: ml_framework, optional
     :return: The new trainable ivy.Module instance.
     """
-    return _cur_framework(f=f).to_ivy_module(native_module)
+    return _cur_framework(f=f).to_ivy_module(native_module, native_module_class, args, kwargs)
