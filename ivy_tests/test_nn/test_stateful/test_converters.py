734f4c69909125fbc3cc1a33c4143d402652c249
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-29 23:36:26 +0200

    added conveter function from Haiku modules to ivy modules.

--- ivy_tests/test_nn/test_stateful/test_converters.py
+++ ivy_tests/test_nn/test_stateful/test_converters.py
@@ -5,6 +5,8 @@ Collection of tests for module converters
 # global
 import pytest
 import torch.nn
+import haiku as hk
+import jax.numpy as jnp
 
 # local
 import ivy
@@ -25,7 +27,22 @@ class TorchModule(torch.nn.Module):
         return torch.tanh(self._linear2(x))[0]
 
 
-NATIVE_MODULES = {'torch': TorchModule}
+class HaikuModule(hk.Module):
+    def __init__(self, in_size, out_size, dev_str='cpu', hidden_size=64):
+        super(HaikuModule, self).__init__()
+        self._linear0 = hk.Linear(hidden_size)
+        self._linear1 = hk.Linear(hidden_size)
+        self._linear2 = hk.Linear(out_size)
+
+    def __call__(self, x):
+        x = jnp.expand_dims(x, 0)
+        x = jnp.tanh(self._linear0(x))
+        x = jnp.tanh(self._linear1(x))
+        return jnp.tanh(self._linear2(x))[0]
+
+
+NATIVE_MODULES = {'torch': TorchModule,
+                  'jax': HaikuModule}
 
 
 # to_ivy_module
@@ -33,15 +50,19 @@ NATIVE_MODULES = {'torch': TorchModule}
     "bs_ic_oc", [([1, 2], 4, 5)])
 def test_to_ivy_module(bs_ic_oc, dev_str, call):
     # smoke test
-    if call is not helpers.torch_call:
+    if call not in [helpers.torch_call, helpers.jnp_call]:
         # Currently only implemented for PyTorch
         pytest.skip()
     batch_shape, input_channels, output_channels = bs_ic_oc
     x = ivy.cast(ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), input_channels), 'float32')
-    native_module = NATIVE_MODULES[ivy.current_framework_str()](input_channels, output_channels)
-    ivy_module = ivy.to_ivy_module(native_module)
+    natvie_module_class = NATIVE_MODULES[ivy.current_framework_str()]
+    if call is helpers.jnp_call:
+        ivy_module = ivy.to_ivy_module(native_module_class=natvie_module_class, args=[input_channels, output_channels])
+    else:
+        native_module = natvie_module_class(input_channels, output_channels)
+        ivy_module = ivy.to_ivy_module(native_module)
 
-    def loss_fn(v_):
+    def loss_fn(v_=None):
         out = ivy_module(x, v=v_)
         return ivy.reduce_mean(out)[0]
 
@@ -49,6 +70,7 @@ def test_to_ivy_module(bs_ic_oc, dev_str, call):
     loss_tm1 = 1e12
     loss = None
     grads = None
+    loss_fn()  # for on-call mode
     for i in range(10):
         loss, grads = ivy.execute_with_gradients(loss_fn, ivy_module.v)
         ivy_module.v = ivy.gradient_descent_update(ivy_module.v, grads, 1e-3)
@@ -65,5 +87,4 @@ def test_to_ivy_module(bs_ic_oc, dev_str, call):
     else:
         assert loss.shape == ()
     # value test
-    for grad in grads.values():
-        assert ivy.reduce_max(ivy.abs(grad)) > 0
+    assert (abs(grads).reduce_max() > 0).all_true()
