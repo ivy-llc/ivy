05a5794b7c1ae7e838f8e73934e5d7c503b785ce
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-30 00:07:31 +0200

    made further improvements to both torch and jax module conversions functions, and further improved the flexibility of ivy.default method.

--- ivy/torch/nn/converters.py
+++ ivy/torch/nn/converters.py
@@ -9,27 +9,40 @@ from collections import OrderedDict
 import ivy
 
 
-def to_ivy_module(native_module):
+def to_ivy_module(native_module=None, native_module_class=None, args=None, kwargs=None):
+
+    args = ivy.default(args, [])
+    kwargs = ivy.default(kwargs, {})
+
+    if not ivy.exists(native_module):
+        if not ivy.exists(native_module_class):
+            raise Exception('native_module_class must be specified if native_module is not given')
 
     class IvyModule(ivy.Module):
 
         def __init__(self):
-            self._native_module = native_module
+            ivy.Module.__init__(self)
+
+        def _create_variables(self, dev_str):
+            return self._native_params
+
+        def _build(self):
+            self._native_module = ivy.default(
+                lambda: native_module, lambda: native_module_class(*args, **kwargs), with_callable=True)
             self._native_params = OrderedDict(
-                sorted([(k.replace('.', '_'), v) for k, v in dict(native_module.named_parameters()).items()]))
-            ivy.Module.__init__(
-                self, dev_str=ivy.dev_to_str(next(native_module.parameters()).device), v=self._native_params)
+                sorted([(k.replace('.', '_'), v) for k, v in dict(self._native_module.named_parameters()).items()]))
+            self._dev_str = ivy.dev_to_str(next(self._native_module.parameters()).device)
 
         @staticmethod
         def _inplace_update(p, v):
             p.data = v
 
-        def _forward(self, *args, **kwargs):
+        def _forward(self, *a, **kw):
             wrapped_mode = ivy.wrapped_mode()
             if wrapped_mode:
-                args, kwargs = ivy.args_to_native(*args, **kwargs)
+                a, kw = ivy.args_to_native(*a, **kw)
             [self._inplace_update(p, v) for p, v in zip(self._native_params.values(), self.v.values())]
-            ret = self._native_module(*args, **kwargs)
+            ret = self._native_module(*a, **kw)
             if wrapped_mode:
                 if isinstance(ret, tuple):
                     return ivy.args_to_native(*ret)
