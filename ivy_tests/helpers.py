ca030bf67e2307a2b0678d95e6338d3c2cb46b20
djl11 <daniel.lenton11@imperial.ac.uk>
2021-09-30 14:10:31 +0200

    modified unit tests so only the backend frameworks being tested are imported.

--- ivy_tests/helpers.py
+++ ivy_tests/helpers.py
@@ -6,16 +6,12 @@ Collection of helpers for ivy unit tests
 import ivy
 try:
     import numpy as _np
-    from ivy import numpy as _ivy_np
 except ImportError:
     _np = None
-    _ivy_np = None
 try:
     import jax.numpy as _jnp
-    from ivy import jax as _ivy_jnp
 except ImportError:
     _jnp = None
-    _ivy_jnp = None
 try:
     import tensorflow as _tf
     _tf_version = float('.'.join(_tf.__version__.split('.')[0:2]))
@@ -29,31 +25,66 @@ try:
     physical_devices = _tf.config.list_physical_devices('GPU')
     for device in physical_devices:
         _tf.config.experimental.set_memory_growth(device, True)
-    from ivy import tensorflow as _ivy_tf
 except ImportError:
     _tf = None
-    _ivy_tf = None
 try:
     import torch as _torch
-    from ivy import torch as _ivy_torch
 except ImportError:
     _torch = None
-    _ivy_torch = None
 try:
     import mxnet as _mx
     import mxnet.ndarray as _mx_nd
-    from ivy import mxnd as _ivy_mxnd
 except ImportError:
     _mx = None
     _mx_nd = None
-    _ivy_mxnd = None
 
-_ivy_fws_dict = {'numpy': _ivy_np,
-                 'jax': _ivy_jnp,
-                 'tensorflow': _ivy_tf,
-                 'tensorflow_graph': _ivy_tf,
-                 'torch': _ivy_torch,
-                 'mxnd': _ivy_mxnd}
+
+def get_ivy_numpy():
+    try:
+        import ivy.numpy
+    except ImportError:
+        return None
+    return ivy.numpy
+
+
+def get_ivy_jax():
+    try:
+        import ivy.jax
+    except ImportError:
+        return None
+    return ivy.jax
+
+
+def get_ivy_tensorflow():
+    try:
+        import ivy.tensorflow
+    except ImportError:
+        return None
+    return ivy.tensorflow
+
+
+def get_ivy_torch():
+    try:
+        import ivy.torch
+    except ImportError:
+        return None
+    return ivy.torch
+
+
+def get_ivy_mxnd():
+    try:
+        import ivy.mxnd
+    except ImportError:
+        return None
+    return ivy.mxnd
+
+
+_ivy_fws_dict = {'numpy': lambda: get_ivy_numpy(),
+                 'jax': lambda: get_ivy_jax(),
+                 'tensorflow': lambda: get_ivy_tensorflow(),
+                 'tensorflow_graph': lambda: get_ivy_tensorflow(),
+                 'torch': lambda: get_ivy_torch(),
+                 'mxnd': lambda: get_ivy_mxnd()}
 
 _iterable_types = [list, tuple, dict]
 _excluded = []
@@ -171,15 +202,15 @@ def exclude(exclusion_list):
 
 
 def frameworks():
-    return list(set([ivy_fw for fw_str, ivy_fw in _ivy_fws_dict.items()
-                     if ivy_fw is not None and fw_str not in _excluded]))
+    return list(set([ivy_fw() for fw_str, ivy_fw in _ivy_fws_dict.items()
+                     if ivy_fw() is not None and fw_str not in _excluded]))
 
 
 def calls():
     return [call for (fw_str, ivy_fw), call in zip(_ivy_fws_dict.items(), _calls)
-            if ivy_fw is not None and fw_str not in _excluded]
+            if ivy_fw() is not None and fw_str not in _excluded]
 
 
 def f_n_calls():
-    return [(ivy_fw, call) for (fw_str, ivy_fw), call in zip(_ivy_fws_dict.items(), _calls)
-            if ivy_fw is not None and fw_str not in _excluded]
+    return [(ivy_fw(), call) for (fw_str, ivy_fw), call in zip(_ivy_fws_dict.items(), _calls)
+            if ivy_fw() is not None and fw_str not in _excluded]
