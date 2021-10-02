275265743a290ab01196f40c6b4b925fab1fbb20
djl11 <daniel.lenton11@imperial.ac.uk>
2021-10-02 13:50:48 +0200

    removed device split functions, added ivy.map, and other small fixes for distributed training.

--- ivy/neural_net_stateful/distributed.py
+++ ivy/neural_net_stateful/distributed.py
@@ -34,13 +34,16 @@ class DistributedModule:
         :type module_kwargs: dict of any, optional
         :type build_mode: str, optional
         """
-        self._distributed_modules = list()
         self._dev_strs = dev_strs
-        if 'v' in inspect.getfullargspec(module_class.__init__).args:
-            module_kwargs['v'] = ivy.default(lambda: v.to_dev(dev_str), None, True)
-        for dev_str in dev_strs:
-            self._distributed_modules.append(module_class(
-                dev_str=dev_str, **module_kwargs))
+        self._distributed_modules = list()
+        with_v = True if 'v' in inspect.getfullargspec(module_class.__init__).args and ivy.exists(v) else False
+        distributed_v = isinstance(v, ivy.Distributed)
+        for i, dev_str in enumerate(dev_strs):
+            if with_v:
+                module = module_class(dev_str=dev_str, v=v[i] if distributed_v else v.to_dev(dev_str), **module_kwargs)
+            else:
+                module = module_class(dev_str=dev_str, **module_kwargs)
+            self._distributed_modules.append(module)
 
     # Public #
     # -------#
@@ -49,11 +52,11 @@ class DistributedModule:
         """
         the forward pass of the layer, treating layer instance as callable function.
         """
-        args_dist, kwargs_dist = ivy.distribute_args(self._dev_strs, *args, **kwargs)
+        args_dist, kwargs_dist = ivy.dev_dist_args(self._dev_strs, *args, **kwargs)
         rets = list()
         for module, args_d, kwargs_d in zip(self._distributed_modules, args_dist, kwargs_dist):
             rets.append(module(*args_d, **kwargs_d))
-        return rets
+        return ivy.Distributed(rets)
 
     def save_weights(self, weights_path):
         """
@@ -85,62 +88,6 @@ class DistributedModule:
     def built(self):
         return min([module.built for module in self._distributed_modules])
 
-
-# Optimizer #
-# ----------#
-
-# noinspection PyCallingNonCallable
-class DistributedOptimizer:
-
-    def __init__(self, optimizer_class, dev_strs, lr, compile_step=False, inplace=True, stop_gradients=True,
-                 **optimizer_kwargs):
-        """
-        Construct an general Optimizer. This is an abstract class, and must be derived.
-
-        :param optimizer_class: The customized ivy.Optimizer class you wish to distribute across devices.
-        :type optimizer_class: Customized ivy.Optimizer class
-        :param dev_strs: devices on which to distribute the module.
-        :type dev_strs: str
-        :param lr: Learning rate.
-        :type lr: function or float.
-        :param compile_step: Whether to compile the optimizer step, default is False.
-        :type compile_step: bool, optional
-        :param inplace: Whether to update the variables in-place, or to create new variable handles.
-                        This is only relevant for frameworks with stateful variables such as PyTorch. Default is True.
-        :type inplace: bool, optional
-        :param stop_gradients: Whether to stop the gradients of the variables after each gradient step. Default is True.
-        :type stop_gradients: bool, optional
-        :param optimizer_kwargs: Dict of keywords arguments to pass the optimizer constructor. Default is None.
-        :type optimizer_kwargs: dict of any, optional
-        """
-        self._distributed_optimizers = list()
-        self._dev_strs = dev_strs
-        for dev_str in dev_strs:
-            self._distributed_optimizers.append(optimizer_class(
-                lr=lr, compile_step=compile_step, inplace=inplace, stop_gradients=stop_gradients, dev_str=dev_str,
-                **optimizer_kwargs))
-
-    def set_state(self, state):
-        """
-        Set state of the optimizer.
-
-        :param state: Nested state to update.
-        :type state: Ivy container of state tensors
-        """
-        [optim.set_state(s_sub) for optim, s_sub in zip(self._distributed_optimizers, state)]
-
-    def step(self, v, grads, ignore_missing=False):
-        """
-        Update nested variables container v from possibly compiled overriden private self._step_fn
-
-        :param v: Nested variables to update.
-        :type v: Ivy container of variables
-        :param grads: Nested gradients to update.
-        :type grads: sequence of arrays
-        :param ignore_missing: Whether to ignore keys missing from the gradients which exist in the variables.
-                               Default is False.
-        :type ignore_missing: bool, optional
-        :return: The updated variables, following update step.
-        """
-        [optim.step(v_sub, g_sub, ignore_missing)
-         for optim, v_sub, g_sub in zip(self._distributed_optimizers, v, grads)]
+    @property
+    def v(self):
+        return ivy.Distributed([module.v for module in self._distributed_modules])
