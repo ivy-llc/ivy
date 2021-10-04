8bab81727b7ba99697b9be48299e698670596a7d
djl11 <daniel.lenton11@imperial.ac.uk>
2021-10-04 18:57:20 +0200

    re-added split_func_call method, and associated unit tests.
    Former-commit-id: 0bdc92ce1bbfdff526407f6b7d5984e3c8ce7abb

--- ivy_tests/test_core/test_device.py
+++ ivy_tests/test_core/test_device.py
@@ -5,6 +5,7 @@ Collection of tests for templated device functions
 # global
 import math
 import pytest
+import numpy as np
 from numbers import Number
 
 # local
@@ -15,6 +16,8 @@ import ivy_tests.helpers as helpers
 
 # Tests #
 # ------#
+
+# Device Queries #
 
 # dev
 @pytest.mark.parametrize(
@@ -35,37 +38,6 @@ def test_dev(x, dtype_str, tensor_fn, dev_str, call):
     # compilation test
     if not ivy.wrapped_mode():
         helpers.assert_compilable(ivy.dev)
-
-
-# to_dev
-@pytest.mark.parametrize(
-    "x", [1, [], [1], [[0.0, 1.0], [2.0, 3.0]]])
-@pytest.mark.parametrize(
-    "dtype_str", ['float32'])
-@pytest.mark.parametrize(
-    "tensor_fn", [ivy.array, helpers.var_fn])
-def test_to_dev(x, dtype_str, tensor_fn, dev_str, call):
-    # smoke test
-    if (isinstance(x, Number) or len(x) == 0) and tensor_fn == helpers.var_fn and call is helpers.mx_call:
-        # mxnet does not support 0-dimensional variables
-        pytest.skip()
-    x = tensor_fn(x, dtype_str, dev_str)
-    dev = ivy.dev(x)
-    x_on_dev = ivy.to_dev(x, dev_str)
-    dev_from_new_x = ivy.dev(x)
-    # value test
-    if call in [helpers.tf_call, helpers.tf_graph_call]:
-        assert '/' + ':'.join(dev_from_new_x[1:].split(':')[-2:]) == '/' + ':'.join(dev[1:].split(':')[-2:])
-    elif call is helpers.torch_call:
-        assert dev_from_new_x.type == dev.type
-    else:
-        assert dev_from_new_x == dev
-    # compilation test
-    if call is helpers.torch_call:
-        # pytorch scripting does not handle converting string to device
-        return
-    if not ivy.wrapped_mode():
-        helpers.assert_compilable(ivy.to_dev)
 
 
 # dev_to_str
@@ -163,6 +135,117 @@ def test_memory_on_dev(dev_str_to_check, dev_str, call):
         pytest.skip()
     if not ivy.wrapped_mode():
         helpers.assert_compilable(ivy.memory_on_dev)
+
+
+# Device Allocation #
+
+# to_dev
+@pytest.mark.parametrize(
+    "x", [1, [], [1], [[0.0, 1.0], [2.0, 3.0]]])
+@pytest.mark.parametrize(
+    "dtype_str", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_to_dev(x, dtype_str, tensor_fn, dev_str, call):
+    # smoke test
+    if (isinstance(x, Number) or len(x) == 0) and tensor_fn == helpers.var_fn and call is helpers.mx_call:
+        # mxnet does not support 0-dimensional variables
+        pytest.skip()
+    x = tensor_fn(x, dtype_str, dev_str)
+    dev = ivy.dev(x)
+    x_on_dev = ivy.to_dev(x, dev_str)
+    dev_from_new_x = ivy.dev(x)
+    # value test
+    if call in [helpers.tf_call, helpers.tf_graph_call]:
+        assert '/' + ':'.join(dev_from_new_x[1:].split(':')[-2:]) == '/' + ':'.join(dev[1:].split(':')[-2:])
+    elif call is helpers.torch_call:
+        assert dev_from_new_x.type == dev.type
+    else:
+        assert dev_from_new_x == dev
+    # compilation test
+    if call is helpers.torch_call:
+        # pytorch scripting does not handle converting string to device
+        return
+    if not ivy.wrapped_mode():
+        helpers.assert_compilable(ivy.to_dev)
+
+
+# Function Splitting #
+
+@pytest.mark.parametrize(
+    "x0", [[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+           [[9, 8, 7], [6, 5, 4], [3, 2, 1]]])
+@pytest.mark.parametrize(
+    "x1", [[[2, 4, 6], [8, 10, 12], [14, 16, 18]],
+           [[18, 16, 14], [12, 10, 8], [6, 4, 2]]])
+@pytest.mark.parametrize(
+    "chunk_size", [1, 3])
+@pytest.mark.parametrize(
+    "axis", [0, 1])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_split_func_call(x0, x1, chunk_size, axis, tensor_fn, dev_str, call):
+
+    if call is helpers.mx_call:
+        # MXNet does not support splitting based on section sizes, only integer number of sections input is supported.
+        pytest.skip()
+
+    # inputs
+    in0 = tensor_fn(x0, 'float32', dev_str)
+    in1 = tensor_fn(x1, 'float32', dev_str)
+
+    # function
+    def func(t0, t1):
+        return t0 * t1, t0 - t1, t1 - t0
+
+    # predictions
+    a, b, c = ivy.split_func_call(func, [in0, in1], chunk_size, axis)
+
+    # true
+    a_true, b_true, c_true = func(in0, in1)
+
+    # value test
+    assert np.allclose(ivy.to_numpy(a), ivy.to_numpy(a_true))
+    assert np.allclose(ivy.to_numpy(b), ivy.to_numpy(b_true))
+    assert np.allclose(ivy.to_numpy(c), ivy.to_numpy(c_true))
+
+
+@pytest.mark.parametrize(
+    "x0", [[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
+           [[9, 8, 7], [6, 5, 4], [3, 2, 1]]])
+@pytest.mark.parametrize(
+    "x1", [[[2, 4, 6], [8, 10, 12], [14, 16, 18]],
+           [[18, 16, 14], [12, 10, 8], [6, 4, 2]]])
+@pytest.mark.parametrize(
+    "chunk_size", [1, 3])
+@pytest.mark.parametrize(
+    "axis", [0, 1])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_split_func_call_with_cont_input(x0, x1, chunk_size, axis, tensor_fn, dev_str, call):
+
+    if call is helpers.mx_call:
+        # MXNet does not support splitting based on section sizes, only integer number of sections input is supported.
+        pytest.skip()
+
+    # inputs
+    in0 = ivy.Container(cont_key=tensor_fn(x0, 'float32', dev_str))
+    in1 = ivy.Container(cont_key=tensor_fn(x1, 'float32', dev_str))
+
+    # function
+    def func(t0, t1):
+        return t0 * t1, t0 - t1, t1 - t0
+
+    # predictions
+    a, b, c = ivy.split_func_call(func, [in0, in1], chunk_size, axis)
+
+    # true
+    a_true, b_true, c_true = func(in0, in1)
+
+    # value test
+    assert np.allclose(ivy.to_numpy(a.cont_key), ivy.to_numpy(a_true.cont_key))
+    assert np.allclose(ivy.to_numpy(b.cont_key), ivy.to_numpy(b_true.cont_key))
+    assert np.allclose(ivy.to_numpy(c.cont_key), ivy.to_numpy(c_true.cont_key))
 
 
 @pytest.mark.parametrize(
