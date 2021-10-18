ae77a6d810b2f9f50ec84966414b6beec2d3264e
djl11 <daniel.lenton11@imperial.ac.uk>
2021-10-18 14:17:28 +0200

    updated ivy compiler so run ops that don't contribute to the function output are not compiled as part of the graph, and added unit test to verify this.

--- ivy_tests/test_core/test_compilation.py
+++ ivy_tests/test_core/test_compilation.py
@@ -19,23 +19,29 @@ import ivy_tests.helpers as helpers
 
 
 # functions to compile
-def _fn_1(x):
+def _fn_1(x, with_non_compiled: bool = False):
     for _ in range(100000):
         pass
+    if with_non_compiled:
+        (x + 3) * 4  # ops not to be compiled into the graph
     return x**2
 
 
-def _fn_2(x):
+def _fn_2(x, with_non_compiled: bool = False):
     for _ in range(100000):
         pass
+    if with_non_compiled:
+        (x + 3) * 4  # ops not to be compiled into the graph
     return (x + 10)**0.5 - 5
 
 
-def _fn_3(x):
+def _fn_3(x, with_non_compiled: bool = False):
     time.sleep(0.05)
+    if with_non_compiled:
+        (x + 3) * 4  # ops not to be compiled into the graph
     return ivy.reduce_mean(ivy.reduce_sum(x, keepdims=True), keepdims=True)
 
-def _fn_4(x):
+def _fn_4(x, with_non_compiled: bool = False):
     y = ivy.reduce_mean(x)
     z = ivy.reduce_sum(x)
     f = ivy.reduce_var(y)
@@ -43,6 +49,8 @@ def _fn_4(x):
     k = ivy.cos(z)
     m = ivy.sin(f)
     o = ivy.tan(y)
+    if with_non_compiled:
+        (x + 3) * 4  # ops not to be compiled into the graph
     return ivy.concatenate([k, m, o], -1)
 
 
@@ -89,7 +97,9 @@ def test_compile_native(x, dtype_str, tensor_fn, dev_str, call):
     "dtype_str", ['float32'])
 @pytest.mark.parametrize(
     "tensor_fn", [ivy.array])
-def test_compile_ivy_inplace(x_raw, dtype_str, tensor_fn, dev_str, call):
+@pytest.mark.parametrize(
+    "with_non_compiled", [True, False])
+def test_compile_ivy_inplace(x_raw, dtype_str, tensor_fn, with_non_compiled, dev_str, call):
     if ivy.wrapped_mode():
         # Wrapped mode does not yet support function compilation
         pytest.skip()
@@ -103,34 +113,42 @@ def test_compile_ivy_inplace(x_raw, dtype_str, tensor_fn, dev_str, call):
 
     # function 1
     x = tensor_fn(x_raw, dtype_str, dev_str)
-    comp_fn = ivy.compile_ivy(_fn_1, x)
+    comp_fn = ivy.compile_ivy(_fn_1, x, with_non_compiled)
     # type test
     assert callable(comp_fn)
     # value test
     start_time = time.perf_counter()
-    non_compiled_return = _fn_1(x)
+    non_compiled_return = _fn_1(x, with_non_compiled)
     non_comp_time_taken = time.perf_counter() - start_time
     start_time = time.perf_counter()
+    assert len(comp_fn.__self__._param_dict) == 2
     assert comp_fn.__self__.params_all_empty()
-    compiled_return = comp_fn(x)
+    assert len(list(comp_fn.__self__._functions)) == 1
+    compiled_return = comp_fn(x, with_non_compiled)
+    assert len(comp_fn.__self__._param_dict) == 2
     assert comp_fn.__self__.params_all_empty()
+    assert len(list(comp_fn.__self__._functions)) == 1
     comp_time_taken = time.perf_counter() - start_time
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
     assert comp_time_taken < non_comp_time_taken
 
     # function 2
     x = tensor_fn(x_raw, dtype_str, dev_str)
-    comp_fn = ivy.compile_ivy(_fn_2, x)
+    comp_fn = ivy.compile_ivy(_fn_2, x, with_non_compiled)
     # type test
     assert callable(comp_fn)
     # value test
     start_time = time.perf_counter()
-    non_compiled_return = _fn_2(x)
+    non_compiled_return = _fn_2(x, with_non_compiled)
     non_comp_time_taken = time.perf_counter() - start_time
     start_time = time.perf_counter()
+    assert len(comp_fn.__self__._param_dict) == 4
     assert comp_fn.__self__.params_all_empty()
-    compiled_return = comp_fn(x)
+    assert len(list(comp_fn.__self__._functions)) == 3
+    compiled_return = comp_fn(x, with_non_compiled)
+    assert len(comp_fn.__self__._param_dict) == 4
     assert comp_fn.__self__.params_all_empty()
+    assert len(list(comp_fn.__self__._functions)) == 3
     comp_time_taken = time.perf_counter() - start_time
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
     assert comp_time_taken < non_comp_time_taken
@@ -143,7 +161,9 @@ def test_compile_ivy_inplace(x_raw, dtype_str, tensor_fn, dev_str, call):
     "dtype_str", ['float32'])
 @pytest.mark.parametrize(
     "tensor_fn", [ivy.array])
-def test_compile_ivy(x_raw, dtype_str, tensor_fn, dev_str, call):
+@pytest.mark.parametrize(
+    "with_non_compiled", [True, False])
+def test_compile_ivy(x_raw, dtype_str, tensor_fn, with_non_compiled, dev_str, call):
     if ivy.wrapped_mode():
         # Wrapped mode does not yet support function compilation
         pytest.skip()
@@ -157,34 +177,42 @@ def test_compile_ivy(x_raw, dtype_str, tensor_fn, dev_str, call):
 
     # function 3
     x = tensor_fn(x_raw, dtype_str, dev_str)
-    comp_fn = ivy.compile_ivy(_fn_3, x)
+    comp_fn = ivy.compile_ivy(_fn_3, x, with_non_compiled)
     # type test
     assert callable(comp_fn)
     # value test
     start_time = time.perf_counter()
-    non_compiled_return = _fn_3(x)
+    non_compiled_return = _fn_3(x, with_non_compiled)
     non_comp_time_taken = time.perf_counter() - start_time
     start_time = time.perf_counter()
+    assert len(comp_fn.__self__._param_dict) == 3
     assert comp_fn.__self__.params_all_empty()
-    compiled_return = comp_fn(x)
+    assert len(list(comp_fn.__self__._functions)) == 2
+    compiled_return = comp_fn(x, with_non_compiled)
+    assert len(comp_fn.__self__._param_dict) == 3
     assert comp_fn.__self__.params_all_empty()
+    assert len(list(comp_fn.__self__._functions)) == 2
     comp_time_taken = time.perf_counter() - start_time
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
     assert comp_time_taken < non_comp_time_taken
 
     # function 4
     x = tensor_fn(x_raw, dtype_str, dev_str)
-    comp_fn = ivy.compile_ivy(_fn_4, x)
+    comp_fn = ivy.compile_ivy(_fn_4, x, with_non_compiled)
     # type test
     assert callable(comp_fn)
     # value test
     start_time = time.perf_counter()
-    non_compiled_return = _fn_4(x)
+    non_compiled_return = _fn_4(x, with_non_compiled)
     non_comp_time_taken = time.perf_counter() - start_time
     start_time = time.perf_counter()
+    assert len(comp_fn.__self__._param_dict) == 11
     assert comp_fn.__self__.params_all_empty()
-    compiled_return = comp_fn(x)
+    assert len(list(comp_fn.__self__._functions)) == 10
+    compiled_return = comp_fn(x, with_non_compiled)
+    assert len(comp_fn.__self__._param_dict) == 11
     assert comp_fn.__self__.params_all_empty()
+    assert len(list(comp_fn.__self__._functions)) == 10
     comp_time_taken = time.perf_counter() - start_time
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
     assert comp_time_taken < non_comp_time_taken
