a385bc47cd4521dc530cbf08734629fa7d88ecce
djl11 <daniel.lenton11@imperial.ac.uk>
2021-10-27 23:17:36 +0200

    added ivy.Module method show_graph.

--- ivy/neural_net_stateful/module.py
+++ ivy/neural_net_stateful/module.py
@@ -215,6 +215,14 @@ class Module(abc.ABC):
         self._compiled_fn = ivy.compile_graph(self._call, *args, **kwargs)
         self._compiled = True
 
+    def show_graph(self, *args, v=None, with_grads=True, save_to_disk=False, **kwargs):
+        self(*args, v=v, with_grads=with_grads, **kwargs)  # for on call build modes
+        if not self._built:
+            self.build(*args, from_call=False, **kwargs)  # for explicit build modes
+        kwargs['v'] = ivy.default(v, self.v)
+        kwargs['with_grads'] = with_grads
+        ivy.show_graph(self._call, *args, **kwargs, save_to_disk=save_to_disk)
+
     def __call__(self, *args, v=None, with_grads=True, **kwargs):
         if self._compiled:
             return self._compiled_fn(*args, v=ivy.default(v, self.v), with_grads=with_grads, **kwargs)
