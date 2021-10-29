cf8026920267e9759b11a2f9bfc54a4cd9230750
djl11 <daniel.lenton11@imperial.ac.uk>
2021-10-29 16:22:54 +0200

    refactored large ivy graph compiler module into useful sub-modules.
    Former-commit-id: 27f5341815e49622119a6467501d09c3ed03eb16

---
+++ ivy/compiler/param.py
@@ -0,0 +1,44 @@
+# global
+import ivy
+
+
+class Param:
+
+    def __init__(self, ptype, tree_depth, shape=None):
+        self._count = 0
+        self._ptype = ptype
+        self._tree_depth = tree_depth
+        self._param_stack = list()
+        self._shape = tuple(shape) if ivy.exists(shape) else None
+
+    def set(self, val):
+        self._param_stack = [val]*self._count
+
+    def set_count(self, count):
+        self._count = count
+
+    def get(self):
+        return self._param_stack.pop()
+
+    def __repr__(self):
+        return '<Param, type={}, depth={}, count={}, current={}>'.format(
+            self._ptype, self._tree_depth, self._count, len(self._param_stack))
+
+    def __len__(self):
+        return len(self._param_stack)
+
+    @property
+    def count(self):
+        return self._count
+
+    @property
+    def depth(self):
+        return self._tree_depth
+
+    @property
+    def ptype(self):
+        return self._ptype
+
+    @property
+    def shape(self):
+        return self._shape
