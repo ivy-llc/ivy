d11e89c0b1dfd656e925310877ac6724e21649f9
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-01 23:07:24 +0100

    added framework-specific global GENERATOR_METHODS dict for graph compiler, which stores methods which should be added to graph even if un-connected to the input or stateful objects, such as torch.rand, which should give different results during each forward pass through the graph.

--- ivy/compiler/op_logging.py
+++ ivy/compiler/op_logging.py
@@ -151,7 +151,8 @@ def _wrap_method_for_op_logging(fn, graph, limit_attributes=True, stateful_class
 
         new_fn.signature = _get_fn_signature(backend_fn)
         new_fn.terminal = True
-        new_fn.is_constant = len(arg_param_ids + kwarg_param_ids) == 0
+        new_fn.is_constant = len(arg_param_ids + kwarg_param_ids) == 0 and \
+                             fn.__name__ not in glob.GENERATOR_METHODS[ivy.current_framework_str()]
 
         fns_in = [graph._pid_to_functions_dict[pid]
                   for pid in arg_param_ids + kwarg_param_ids if pid in graph._pid_to_functions_dict]
