3fbdb2055eee13bf468b69cb908c068a92572485
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-01 09:41:33 +0100

    small formatting fixes for graph visualization.

--- ivy/neural_net_stateful/module.py
+++ ivy/neural_net_stateful/module.py
@@ -216,14 +216,14 @@ class Module(abc.ABC):
         self._compiled = True
 
     def show_graph(self, *args, v=None, with_grads=True, save_to_disk=False, with_edge_labels=True,
-                   with_arg_labels=True, output_connected_only=True, **kwargs):
+                   with_arg_labels=True, output_connected_only=True, fname=None, **kwargs):
         self(*args, v=v, with_grads=with_grads, **kwargs)  # for on call build modes
         if not self._built:
             self.build(*args, from_call=False, **kwargs)  # for explicit build modes
         kwargs['v'] = ivy.default(v, self.v)
         kwargs['with_grads'] = with_grads
         ivy.show_graph(self._call, *args, **kwargs, save_to_disk=save_to_disk, with_edge_labels=with_edge_labels,
-                       with_arg_labels=with_arg_labels, output_connected_only=output_connected_only)
+                       with_arg_labels=with_arg_labels, output_connected_only=output_connected_only, fname=fname)
 
     def __call__(self, *args, v=None, with_grads=True, **kwargs):
         if self._compiled:
