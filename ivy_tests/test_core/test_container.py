db371c08bf4c01bd23505c6544e4e267f5afe2cf
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-15 20:04:57 +0100

    extended ivy.Container __contains__ method to also reason about sub-containers, if the key is another container, and extended unit test for this.
    Former-commit-id: 2118bcef0c755a7530f4ee62401bcb1fbf498e60

--- ivy_tests/test_core/test_container.py
+++ ivy_tests/test_core/test_container.py
@@ -262,6 +262,7 @@ def test_container_find_sub_container(dev_str, call):
                'b': {'c': ivy.array([2], dev_str=dev_str), 'd': ivy.array([3], dev_str=dev_str)}}
     top_cont = Container(dict_in)
     sub_cont = Container(dict_in['b'])
+    assert sub_cont in top_cont
     found_kc = top_cont.find_sub_container(sub_cont)
     assert found_kc == 'b'
 
