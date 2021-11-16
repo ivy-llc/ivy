a9c14c11e91974188403ffef7f8ab73774ce4b40
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-16 19:11:19 +0100

    updated ivy.Container method key_chains_containing to support include_empty arg.

--- ivy/core/container.py
+++ ivy/core/container.py
@@ -2112,11 +2112,11 @@ class Container(dict):
             raise Exception('Invalid type for input key_chains, must either be a list, tuple, dict, or ivy.Container,'
                             'but found type {}'.format(type(key_chains)))
 
-    def key_chains_containing(self, sub_str):
+    def key_chains_containing(self, sub_str, include_empty=False):
         """
         Return a list of all key-chains containing a given sub-string.
         """
-        return [kc for kc, v in self.to_iterator() if sub_str in kc]
+        return [kc for kc, v in self.to_iterator(include_empty=include_empty) if sub_str in kc]
 
     def set_at_keys(self, target_dict):
         """
