7ea7f37c228f66536bb5edd716fe89615bec6fa9
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-17 17:05:55 +0100

    renamed normalization variables gamma and beta with more intuitive names scale and offset.

--- ivy_tests/test_nn/test_functional/test_norms.py
+++ ivy_tests/test_nn/test_functional/test_norms.py
@@ -13,7 +13,7 @@ import ivy_tests.helpers as helpers
 
 # layer norm
 @pytest.mark.parametrize(
-    "x_n_ni_n_g_n_b_n_res", [
+    "x_n_ni_n_s_n_o_n_res", [
 
         ([[1., 2., 3.], [4., 5., 6.]],
          -1,
@@ -26,20 +26,20 @@ import ivy_tests.helpers as helpers
     "dtype_str", ['float32'])
 @pytest.mark.parametrize(
     "tensor_fn", [ivy.array, helpers.var_fn])
-def test_layer_norm(x_n_ni_n_g_n_b_n_res, dtype_str, tensor_fn, dev_str, call):
+def test_layer_norm(x_n_ni_n_s_n_o_n_res, dtype_str, tensor_fn, dev_str, call):
     # smoke test
-    x, norm_idxs, gamma, beta, true_res = x_n_ni_n_g_n_b_n_res
+    x, norm_idxs, scale, offset, true_res = x_n_ni_n_s_n_o_n_res
     x = tensor_fn(x, dtype_str, dev_str)
-    gamma = tensor_fn(gamma, dtype_str, dev_str)
-    beta = tensor_fn(beta, dtype_str, dev_str)
+    scale = tensor_fn(scale, dtype_str, dev_str)
+    offset = tensor_fn(offset, dtype_str, dev_str)
     true_res = tensor_fn(true_res, dtype_str, dev_str)
-    ret = ivy.layer_norm(x, norm_idxs, gamma=gamma, beta=beta)
+    ret = ivy.layer_norm(x, norm_idxs, scale=scale, offset=offset)
     # type test
     assert ivy.is_array(ret)
     # cardinality test
     assert ret.shape == true_res.shape
     # value test
-    assert np.allclose(call(ivy.layer_norm, x, norm_idxs, gamma=gamma, beta=beta), ivy.to_numpy(true_res))
+    assert np.allclose(call(ivy.layer_norm, x, norm_idxs, scale=scale, offset=offset), ivy.to_numpy(true_res))
     # compilation test
     if call in [helpers.torch_call]:
         # this is not a backend implemented function
