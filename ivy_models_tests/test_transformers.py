8ccff8d64b53fc110a6603708480f01b9bb78669
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-23 20:25:20 +0100

    got Perceiver outputs matching to official jax output after 8th and final iteration of layer PreNorm_13.

--- ivy_models_tests/test_transformers.py
+++ ivy_models_tests/test_transformers.py
@@ -92,9 +92,9 @@ def test_perceiver_io_img_classification(dev_str, f, call, batch_shape, img_dims
 
         # expected submodule returns
         expected_submod_rets = ivy.Container()
-        for dct in [{'val': 'PreNorm_97', 'atol': 1e-3}]:
+        for dct in [{'val': 'PreNorm_13', 'atol': 1e-1}]:
             key = dct['val']
-            dct['val'] = np.load(os.path.join(this_dir, key + '.npy'))
+            dct['val'] = [None]*(network_depth-1) + [np.load(os.path.join(this_dir, key + '.npy'))]
             expected_submod_rets[key] = dct
 
         # check submod returns
