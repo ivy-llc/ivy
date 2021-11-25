8ffe7cecd02fade8e35e1d643bf8cd88f4bae848
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-25 13:00:57 +0100

    updated imported jax and torch namespaces to be private, to prevent conflict with ivy namespace during nested calls to framework selection via use. Added unit test for this.
    Former-commit-id: 05e303b5ca3378a059a4207765a6f870551d4daa

--- ivy/torch/core/linalg.py
+++ ivy/torch/core/linalg.py
@@ -3,14 +3,14 @@ Collection of PyTorch linear algebra functions, wrapped to fit Ivy syntax and si
 """
 
 # global
-import torch
-from typing import Optional, List, Union
+import torch as _torch
+from typing import List
 
 
 # noinspection PyPep8Naming
-def svd(x) -> List[torch.Tensor]:
-    U, D, V = torch.svd(x)
-    VT = torch.transpose(V, -2, -1)
+def svd(x) -> List[_torch.Tensor]:
+    U, D, V = _torch.svd(x)
+    VT = _torch.transpose(V, -2, -1)
     return U, D, VT
 
 
@@ -19,33 +19,33 @@ def matrix_norm(x, p=2, axes=None, keepdims=False):
     if isinstance(axes, int):
         raise Exception('if specified, axes must be a length-2 sequence of ints,'
                         'but found {} of type {}'.format(axes, type(axes)))
-    ret = torch.linalg.matrix_norm(x, ord=p, dim=axes, keepdim=keepdims)
+    ret = _torch.linalg.matrix_norm(x, ord=p, dim=axes, keepdim=keepdims)
     if ret.shape == ():
-        return torch.unsqueeze(ret, 0)
+        return _torch.unsqueeze(ret, 0)
     return ret
 
 
 def inv(x):
-    return torch.inverse(x)
+    return _torch.inverse(x)
 
 
 def pinv(x):
-    return torch.pinverse(x)
+    return _torch.pinverse(x)
 
 
 def vector_to_skew_symmetric_matrix(vector):
     batch_shape = list(vector.shape[:-1])
     # BS x 3 x 1
-    vector_expanded = torch.unsqueeze(vector, -1)
+    vector_expanded = _torch.unsqueeze(vector, -1)
     # BS x 1 x 1
     a1s = vector_expanded[..., 0:1, :]
     a2s = vector_expanded[..., 1:2, :]
     a3s = vector_expanded[..., 2:3, :]
     # BS x 1 x 1
-    zs = torch.zeros(batch_shape + [1, 1], device=vector.device)
+    zs = _torch.zeros(batch_shape + [1, 1], device=vector.device)
     # BS x 1 x 3
-    row1 = torch.cat((zs, -a3s, a2s), -1)
-    row2 = torch.cat((a3s, zs, -a1s), -1)
-    row3 = torch.cat((-a2s, a1s, zs), -1)
+    row1 = _torch.cat((zs, -a3s, a2s), -1)
+    row2 = _torch.cat((a3s, zs, -a1s), -1)
+    row3 = _torch.cat((-a2s, a1s, zs), -1)
     # BS x 3 x 3
-    return torch.cat((row1, row2, row3), -2)
+    return _torch.cat((row1, row2, row3), -2)
