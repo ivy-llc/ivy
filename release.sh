6bc3cd1816060c0eac01facfae7b6852d9fed619
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-28 00:18:06 +0100

    version 1.1.6

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "You need to provide an old version number"
+    exit
+fi
+if [ -z "$2" ]; then
+    echo "You need to provide a release version number"
+    exit
+fi
+
+sed -i "s/$1/$2/g" setup.py
+git add -A
+git commit -m "version $2"
+#git push
+#git tag -a "v$2" -m "version $2"
+#git push origin "v$2"
