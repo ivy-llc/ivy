fdc6a27e8608fb506efca0ce10e4090174eaea8b
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-29 22:35:53 +0100

    version 1.1.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='ivy-models',
-      version='1.1.5',
+      version='1.1.6',
       author='Ivy Team',
       author_email='ivydl.team@gmail.com',
       description='Collection of pre-trained models, compatible with any backend framework',
