7355a6f4dcf0f7b81984a03a144317eda1503aa1
djl11 <daniel.lenton11@imperial.ac.uk>
2021-12-04 17:37:32 +0100

    updated mxnet math module to also support float inputs, not only tensors.

--- ivy/mxnet/core/math.py
+++ ivy/mxnet/core/math.py
@@ -4,20 +4,21 @@ Collection of MXNet math functions, wrapped to fit Ivy syntax and signature.
 
 # global
 import mxnet as _mx
+import math as _math
 
-sin = _mx.nd.sin
-cos = _mx.nd.cos
-tan = _mx.nd.tan
-asin = _mx.nd.arcsin
-acos = _mx.nd.arccos
-atan = _mx.nd.arctan
-atan2 = lambda x1, x2: _mx.np.arctan2(x1.as_np_ndarray(), x2.as_np_ndarray()).as_nd_ndarray()
-sinh = _mx.nd.sinh
-cosh = _mx.nd.cosh
-tanh = _mx.nd.tanh
-asinh = _mx.nd.arcsinh
-acosh = _mx.nd.arccosh
-atanh = _mx.nd.arctanh
-log = _mx.nd.log
-exp = _mx.nd.exp
-erf = _mx.nd.erf
+sin = lambda x: _math.sin(x) if isinstance(x, float) else _mx.nd.sin(x)
+cos = lambda x: _math.cos(x) if isinstance(x, float) else _mx.nd.cos(x)
+tan = lambda x: _math.tan(x) if isinstance(x, float) else _mx.nd.tan(x)
+asin = lambda x: _math.asin(x) if isinstance(x, float) else _mx.nd.arcsin(x)
+acos = lambda x: _math.acos(x) if isinstance(x, float) else _mx.nd.arccos(x)
+atan = lambda x: _math.atan(x) if isinstance(x, float) else _mx.nd.arctan(x)
+atan2 = lambda x, y: _math.atan2(x, y) if isinstance(x, float) else _mx.np.arctan2(x.as_np_ndarray(), y.as_np_ndarray()).as_nd_ndarray()
+sinh = lambda x: _math.sinh(x) if isinstance(x, float) else _mx.nd.sinh(x)
+cosh = lambda x: _math.cosh(x) if isinstance(x, float) else _mx.nd.cosh(x)
+tanh = lambda x: _math.tanh(x) if isinstance(x, float) else _mx.nd.tanh(x)
+asinh = lambda x: _math.asinh(x) if isinstance(x, float) else _mx.nd.arcsinh(x)
+acosh = lambda x: _math.acosh(x) if isinstance(x, float) else _mx.nd.arccosh(x)
+atanh = lambda x: _math.atanh(x) if isinstance(x, float) else _mx.nd.arctanh(x)
+log = lambda x: _math.log(x) if isinstance(x, float) else _mx.nd.log(x)
+exp = lambda x: _math.exp(x) if isinstance(x, float) else _mx.nd.exp(x)
+erf = lambda x: _math.erf(x) if isinstance(x, float) else _mx.nd.erf(x)
