f4f485f406e4b4e6676ebd4a864b9e4d580c2944
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-24 19:44:26 +0100

    added method abs to pytorch frontend.

--- ivy/frontends/torch/core/pointwise_ops.py
+++ ivy/frontends/torch/core/pointwise_ops.py
@@ -1,3 +1,12 @@
 """
 PyTorch Pointwise Ops Frontend
 """
+
+# local
+import ivy
+
+
+# noinspection PyShadowingBuiltins
+def abs(input, *, out=None):
+    # ToDo: support inplace operations once ivy.inplace_update is implemented
+    return ivy.abs(input)
