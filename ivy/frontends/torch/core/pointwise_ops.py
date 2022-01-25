a4d0aec1f75ad742278d412fbd7eeb7400db9fcd
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-25 12:10:48 +0100

    updated PyTorch frontend abs method to support inplace operation as a proof of concept. Much of this needs refactoring.

--- ivy/frontends/torch/core/pointwise_ops.py
+++ ivy/frontends/torch/core/pointwise_ops.py
@@ -8,5 +8,15 @@ import ivy
 
 # noinspection PyShadowingBuiltins
 def abs(input, *, out=None):
-    # ToDo: support inplace operations once ivy.inplace_update is implemented
-    return ivy.abs(input)
+    ret = ivy.abs(input)
+    if ivy.exists(out):
+        if ivy.is_variable(out):
+            if not ivy.inplace_variables_supported():
+                raise Exception(
+                    'Inplace variables are not supported for {} backend'.format(ivy.current_framework_str()))
+        elif ivy.is_array(out):
+            if not ivy.inplace_arrays_supported():
+                raise Exception(
+                    'Inplace arrays are not supported for {} backend'.format(ivy.current_framework_str()))
+        return ivy.inplace_update(out, ret)
+    return ret
