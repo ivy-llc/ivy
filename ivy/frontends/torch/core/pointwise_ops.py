15581d64f7ce6b8fdd572789c9875d075caafe27
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-25 12:21:39 +0100

    added methods supports_inplace and assert_supports_inplace.

--- ivy/frontends/torch/core/pointwise_ops.py
+++ ivy/frontends/torch/core/pointwise_ops.py
@@ -10,13 +10,6 @@ import ivy
 def abs(input, *, out=None):
     ret = ivy.abs(input)
     if ivy.exists(out):
-        if ivy.is_variable(out):
-            if not ivy.inplace_variables_supported():
-                raise Exception(
-                    'Inplace variables are not supported for {} backend'.format(ivy.current_framework_str()))
-        elif ivy.is_array(out):
-            if not ivy.inplace_arrays_supported():
-                raise Exception(
-                    'Inplace arrays are not supported for {} backend'.format(ivy.current_framework_str()))
+        ivy.assert_supports_inplace(out)
         return ivy.inplace_update(out, ret)
     return ret
