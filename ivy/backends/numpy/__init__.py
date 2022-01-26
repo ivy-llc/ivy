b82690813063f31ffbcf31e9163b151bbde0b109
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-26 19:39:03 +0100

    added dtype attributes for each backend module.

--- ivy/backends/numpy/__init__.py
+++ ivy/backends/numpy/__init__.py
@@ -14,4 +14,18 @@ NativeVariable = np.ndarray
 Device = str
 Dtype = np.dtype
 
+# data types
+int8 = np.int8
+int16 = np.int16
+int32 = np.int32
+int64 = np.int64
+uint8 = np.uint8
+uint16 = np.uint16
+uint32 = np.uint32
+uint64 = np.uint64
+float32 = np.float32
+float64 = np.float64
+# noinspection PyShadowingBuiltins
+bool = np.bool_
+
 backend = 'numpy'
