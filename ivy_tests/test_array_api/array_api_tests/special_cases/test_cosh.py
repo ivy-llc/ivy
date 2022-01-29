80e37eb26b69bee092f2e2cd689816e56b2222aa
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-29 13:25:26 +0100

    added pycharm configs for array API unit tests, and uncommented all of these tests.

--- ivy_tests/test_array_api/array_api_tests/special_cases/test_cosh.py
+++ ivy_tests/test_array_api/array_api_tests/special_cases/test_cosh.py
@@ -13,67 +13,67 @@ from .._array_module import cosh
 
 from hypothesis import given
 
-#
-# @given(numeric_arrays)
-# def test_cosh_special_cases_one_arg_equal_1(arg1):
-#     """
-#     Special case test for `cosh(x, /)`:
-#
-#         -   If `x_i` is `NaN`, the result is `NaN`.
-#
-#     """
-#     res = cosh(arg1)
-#     mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_cosh_special_cases_one_arg_equal_2(arg1):
-#     """
-#     Special case test for `cosh(x, /)`:
-#
-#         -   If `x_i` is `+0`, the result is `1`.
-#
-#     """
-#     res = cosh(arg1)
-#     mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_cosh_special_cases_one_arg_equal_3(arg1):
-#     """
-#     Special case test for `cosh(x, /)`:
-#
-#         -   If `x_i` is `-0`, the result is `1`.
-#
-#     """
-#     res = cosh(arg1)
-#     mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_cosh_special_cases_one_arg_equal_4(arg1):
-#     """
-#     Special case test for `cosh(x, /)`:
-#
-#         -   If `x_i` is `+infinity`, the result is `+infinity`.
-#
-#     """
-#     res = cosh(arg1)
-#     mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_cosh_special_cases_one_arg_equal_5(arg1):
-#     """
-#     Special case test for `cosh(x, /)`:
-#
-#         -   If `x_i` is `-infinity`, the result is `+infinity`.
-#
-#     """
-#     res = cosh(arg1)
-#     mask = exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
+
+@given(numeric_arrays)
+def test_cosh_special_cases_one_arg_equal_1(arg1):
+    """
+    Special case test for `cosh(x, /)`:
+
+        -   If `x_i` is `NaN`, the result is `NaN`.
+
+    """
+    res = cosh(arg1)
+    mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_cosh_special_cases_one_arg_equal_2(arg1):
+    """
+    Special case test for `cosh(x, /)`:
+
+        -   If `x_i` is `+0`, the result is `1`.
+
+    """
+    res = cosh(arg1)
+    mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_cosh_special_cases_one_arg_equal_3(arg1):
+    """
+    Special case test for `cosh(x, /)`:
+
+        -   If `x_i` is `-0`, the result is `1`.
+
+    """
+    res = cosh(arg1)
+    mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_cosh_special_cases_one_arg_equal_4(arg1):
+    """
+    Special case test for `cosh(x, /)`:
+
+        -   If `x_i` is `+infinity`, the result is `+infinity`.
+
+    """
+    res = cosh(arg1)
+    mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_cosh_special_cases_one_arg_equal_5(arg1):
+    """
+    Special case test for `cosh(x, /)`:
+
+        -   If `x_i` is `-infinity`, the result is `+infinity`.
+
+    """
+    res = cosh(arg1)
+    mask = exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
