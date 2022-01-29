539e1871c8fb139df338143a80859e652059b976
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-29 13:25:26 +0100

    added pycharm configs for array API unit tests, and uncommented all of these tests.
    Former-commit-id: 80e37eb26b69bee092f2e2cd689816e56b2222aa

--- ivy_tests/test_array_api/array_api_tests/special_cases/test_log.py
+++ ivy_tests/test_array_api/array_api_tests/special_cases/test_log.py
@@ -14,67 +14,67 @@ from .._array_module import log
 
 from hypothesis import given
 
-#
-# @given(numeric_arrays)
-# def test_log_special_cases_one_arg_equal_1(arg1):
-#     """
-#     Special case test for `log(x, /)`:
-#
-#         -   If `x_i` is `NaN`, the result is `NaN`.
-#
-#     """
-#     res = log(arg1)
-#     mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_log_special_cases_one_arg_equal_2(arg1):
-#     """
-#     Special case test for `log(x, /)`:
-#
-#         -   If `x_i` is `1`, the result is `+0`.
-#
-#     """
-#     res = log(arg1)
-#     mask = exactly_equal(arg1, one(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_log_special_cases_one_arg_equal_3(arg1):
-#     """
-#     Special case test for `log(x, /)`:
-#
-#         -   If `x_i` is `+infinity`, the result is `+infinity`.
-#
-#     """
-#     res = log(arg1)
-#     mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_log_special_cases_one_arg_less(arg1):
-#     """
-#     Special case test for `log(x, /)`:
-#
-#         -   If `x_i` is less than `0`, the result is `NaN`.
-#
-#     """
-#     res = log(arg1)
-#     mask = less(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_log_special_cases_one_arg_either(arg1):
-#     """
-#     Special case test for `log(x, /)`:
-#
-#         -   If `x_i` is either `+0` or `-0`, the result is `-infinity`.
-#
-#     """
-#     res = log(arg1)
-#     mask = logical_or(exactly_equal(arg1, zero(arg1.shape, arg1.dtype)), exactly_equal(arg1, -zero(arg1.shape, arg1.dtype)))
-#     assert_exactly_equal(res[mask], (-infinity(arg1.shape, arg1.dtype))[mask])
+
+@given(numeric_arrays)
+def test_log_special_cases_one_arg_equal_1(arg1):
+    """
+    Special case test for `log(x, /)`:
+
+        -   If `x_i` is `NaN`, the result is `NaN`.
+
+    """
+    res = log(arg1)
+    mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_log_special_cases_one_arg_equal_2(arg1):
+    """
+    Special case test for `log(x, /)`:
+
+        -   If `x_i` is `1`, the result is `+0`.
+
+    """
+    res = log(arg1)
+    mask = exactly_equal(arg1, one(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_log_special_cases_one_arg_equal_3(arg1):
+    """
+    Special case test for `log(x, /)`:
+
+        -   If `x_i` is `+infinity`, the result is `+infinity`.
+
+    """
+    res = log(arg1)
+    mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_log_special_cases_one_arg_less(arg1):
+    """
+    Special case test for `log(x, /)`:
+
+        -   If `x_i` is less than `0`, the result is `NaN`.
+
+    """
+    res = log(arg1)
+    mask = less(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_log_special_cases_one_arg_either(arg1):
+    """
+    Special case test for `log(x, /)`:
+
+        -   If `x_i` is either `+0` or `-0`, the result is `-infinity`.
+
+    """
+    res = log(arg1)
+    mask = logical_or(exactly_equal(arg1, zero(arg1.shape, arg1.dtype)), exactly_equal(arg1, -zero(arg1.shape, arg1.dtype)))
+    assert_exactly_equal(res[mask], (-infinity(arg1.shape, arg1.dtype))[mask])
--- ivy_tests/test_array_api/array_api_tests/test_constants.py
+++ ivy_tests/test_array_api/array_api_tests/test_constants.py
@@ -19,35 +19,35 @@ def assert_0d_float(name: str, x: Array):
         x.dtype
     ), f"xp.asarray(xp.{name})={x!r}, but should have float dtype"
 
-#
-# @pytest.mark.parametrize("name, n", [("e", math.e), ("pi", math.pi)])
-# def test_irrational_numbers(name, n):
-#     assert hasattr(xp, name)
-#     c = getattr(xp, name)
-#     assert_scalar_float(name, c)
-#     floor = math.floor(n)
-#     assert c > floor, f"xp.{name}={c!r} <= {floor}"
-#     ceil = math.ceil(n)
-#     assert c < ceil, f"xp.{name}={c!r} >= {ceil}"
-#     x = xp.asarray(c)
-#     assert_0d_float("name", x)
-#
-#
-# def test_inf():
-#     assert hasattr(xp, "inf")
-#     assert_scalar_float("inf", xp.inf)
-#     assert math.isinf(xp.inf)
-#     assert xp.inf > 0, "xp.inf not greater than 0"
-#     x = xp.asarray(xp.inf)
-#     assert_0d_float("inf", x)
-#     assert xp.isinf(x), "xp.isinf(xp.asarray(xp.inf))=False"
-#
-#
-# def test_nan():
-#     assert hasattr(xp, "nan")
-#     assert_scalar_float("nan", xp.nan)
-#     assert math.isnan(xp.nan)
-#     assert xp.nan != xp.nan, "xp.nan should not have equality with itself"
-#     x = xp.asarray(xp.nan)
-#     assert_0d_float("nan", x)
-#     assert xp.isnan(x), "xp.isnan(xp.asarray(xp.nan))=False"
+
+@pytest.mark.parametrize("name, n", [("e", math.e), ("pi", math.pi)])
+def test_irrational_numbers(name, n):
+    assert hasattr(xp, name)
+    c = getattr(xp, name)
+    assert_scalar_float(name, c)
+    floor = math.floor(n)
+    assert c > floor, f"xp.{name}={c!r} <= {floor}"
+    ceil = math.ceil(n)
+    assert c < ceil, f"xp.{name}={c!r} >= {ceil}"
+    x = xp.asarray(c)
+    assert_0d_float("name", x)
+
+
+def test_inf():
+    assert hasattr(xp, "inf")
+    assert_scalar_float("inf", xp.inf)
+    assert math.isinf(xp.inf)
+    assert xp.inf > 0, "xp.inf not greater than 0"
+    x = xp.asarray(xp.inf)
+    assert_0d_float("inf", x)
+    assert xp.isinf(x), "xp.isinf(xp.asarray(xp.inf))=False"
+
+
+def test_nan():
+    assert hasattr(xp, "nan")
+    assert_scalar_float("nan", xp.nan)
+    assert math.isnan(xp.nan)
+    assert xp.nan != xp.nan, "xp.nan should not have equality with itself"
+    x = xp.asarray(xp.nan)
+    assert_0d_float("nan", x)
+    assert xp.isnan(x), "xp.isnan(xp.asarray(xp.nan))=False"
