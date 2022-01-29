23c68cd5ac6fd42da1355ac58e14c31a5fee68b5
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-29 13:25:26 +0100

    added pycharm configs for array API unit tests, and uncommented all of these tests.

--- ivy_tests/test_array_api/array_api_tests/special_cases/test_sign.py
+++ ivy_tests/test_array_api/array_api_tests/special_cases/test_sign.py
@@ -14,41 +14,41 @@ from .._array_module import sign
 
 from hypothesis import given
 
-#
-# @given(numeric_arrays)
-# def test_sign_special_cases_one_arg_less(arg1):
-#     """
-#     Special case test for `sign(x, /)`:
-#
-#         -   If `x_i` is less than `0`, the result is `-1`.
-#
-#     """
-#     res = sign(arg1)
-#     mask = less(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (-one(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_sign_special_cases_one_arg_either(arg1):
-#     """
-#     Special case test for `sign(x, /)`:
-#
-#         -   If `x_i` is either `-0` or `+0`, the result is `0`.
-#
-#     """
-#     res = sign(arg1)
-#     mask = logical_or(exactly_equal(arg1, -zero(arg1.shape, arg1.dtype)), exactly_equal(arg1, zero(arg1.shape, arg1.dtype)))
-#     assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_sign_special_cases_one_arg_greater(arg1):
-#     """
-#     Special case test for `sign(x, /)`:
-#
-#         -   If `x_i` is greater than `0`, the result is `+1`.
-#
-#     """
-#     res = sign(arg1)
-#     mask = greater(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
+
+@given(numeric_arrays)
+def test_sign_special_cases_one_arg_less(arg1):
+    """
+    Special case test for `sign(x, /)`:
+
+        -   If `x_i` is less than `0`, the result is `-1`.
+
+    """
+    res = sign(arg1)
+    mask = less(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (-one(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_sign_special_cases_one_arg_either(arg1):
+    """
+    Special case test for `sign(x, /)`:
+
+        -   If `x_i` is either `-0` or `+0`, the result is `0`.
+
+    """
+    res = sign(arg1)
+    mask = logical_or(exactly_equal(arg1, -zero(arg1.shape, arg1.dtype)), exactly_equal(arg1, zero(arg1.shape, arg1.dtype)))
+    assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_sign_special_cases_one_arg_greater(arg1):
+    """
+    Special case test for `sign(x, /)`:
+
+        -   If `x_i` is greater than `0`, the result is `+1`.
+
+    """
+    res = sign(arg1)
+    mask = greater(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (one(arg1.shape, arg1.dtype))[mask])
--- ivy_tests/test_array_api/array_api_tests/special_cases/test_trunc.py
+++ ivy_tests/test_array_api/array_api_tests/special_cases/test_trunc.py
@@ -13,80 +13,80 @@ from .._array_module import trunc
 
 from hypothesis import given
 
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_1(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is already integer-valued, the result is `x_i`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = isintegral(arg1)
-#     assert_exactly_equal(res[mask], (arg1)[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_2(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is `+infinity`, the result is `+infinity`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_3(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is `-infinity`, the result is `-infinity`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (-infinity(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_4(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is `+0`, the result is `+0`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_5(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is `-0`, the result is `-0`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (-zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_trunc_special_cases_one_arg_equal_6(arg1):
-#     """
-#     Special case test for `trunc(x, /)`:
-#
-#         -   If `x_i` is `NaN`, the result is `NaN`.
-#
-#     """
-#     res = trunc(arg1)
-#     mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_1(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is already integer-valued, the result is `x_i`.
+
+    """
+    res = trunc(arg1)
+    mask = isintegral(arg1)
+    assert_exactly_equal(res[mask], (arg1)[mask])
+
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_2(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is `+infinity`, the result is `+infinity`.
+
+    """
+    res = trunc(arg1)
+    mask = exactly_equal(arg1, infinity(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (infinity(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_3(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is `-infinity`, the result is `-infinity`.
+
+    """
+    res = trunc(arg1)
+    mask = exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (-infinity(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_4(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is `+0`, the result is `+0`.
+
+    """
+    res = trunc(arg1)
+    mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_5(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is `-0`, the result is `-0`.
+
+    """
+    res = trunc(arg1)
+    mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (-zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_trunc_special_cases_one_arg_equal_6(arg1):
+    """
+    Special case test for `trunc(x, /)`:
+
+        -   If `x_i` is `NaN`, the result is `NaN`.
+
+    """
+    res = trunc(arg1)
+    mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
