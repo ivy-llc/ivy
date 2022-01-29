96ee2939738b78a3f93d026e12aa57696c31883f
djl11 <daniel.lenton11@imperial.ac.uk>
2022-01-29 13:25:26 +0100

    added pycharm configs for array API unit tests, and uncommented all of these tests.

--- ivy_tests/test_array_api/array_api_tests/special_cases/test_sin.py
+++ ivy_tests/test_array_api/array_api_tests/special_cases/test_sin.py
@@ -13,54 +13,54 @@ from .._array_module import sin
 
 from hypothesis import given
 
-#
-# @given(numeric_arrays)
-# def test_sin_special_cases_one_arg_equal_1(arg1):
-#     """
-#     Special case test for `sin(x, /)`:
-#
-#         -   If `x_i` is `NaN`, the result is `NaN`.
-#
-#     """
-#     res = sin(arg1)
-#     mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_sin_special_cases_one_arg_equal_2(arg1):
-#     """
-#     Special case test for `sin(x, /)`:
-#
-#         -   If `x_i` is `+0`, the result is `+0`.
-#
-#     """
-#     res = sin(arg1)
-#     mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_sin_special_cases_one_arg_equal_3(arg1):
-#     """
-#     Special case test for `sin(x, /)`:
-#
-#         -   If `x_i` is `-0`, the result is `-0`.
-#
-#     """
-#     res = sin(arg1)
-#     mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
-#     assert_exactly_equal(res[mask], (-zero(arg1.shape, arg1.dtype))[mask])
-#
-#
-# @given(numeric_arrays)
-# def test_sin_special_cases_one_arg_either(arg1):
-#     """
-#     Special case test for `sin(x, /)`:
-#
-#         -   If `x_i` is either `+infinity` or `-infinity`, the result is `NaN`.
-#
-#     """
-#     res = sin(arg1)
-#     mask = logical_or(exactly_equal(arg1, infinity(arg1.shape, arg1.dtype)), exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype)))
-#     assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+@given(numeric_arrays)
+def test_sin_special_cases_one_arg_equal_1(arg1):
+    """
+    Special case test for `sin(x, /)`:
+
+        -   If `x_i` is `NaN`, the result is `NaN`.
+
+    """
+    res = sin(arg1)
+    mask = exactly_equal(arg1, NaN(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_sin_special_cases_one_arg_equal_2(arg1):
+    """
+    Special case test for `sin(x, /)`:
+
+        -   If `x_i` is `+0`, the result is `+0`.
+
+    """
+    res = sin(arg1)
+    mask = exactly_equal(arg1, zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_sin_special_cases_one_arg_equal_3(arg1):
+    """
+    Special case test for `sin(x, /)`:
+
+        -   If `x_i` is `-0`, the result is `-0`.
+
+    """
+    res = sin(arg1)
+    mask = exactly_equal(arg1, -zero(arg1.shape, arg1.dtype))
+    assert_exactly_equal(res[mask], (-zero(arg1.shape, arg1.dtype))[mask])
+
+
+@given(numeric_arrays)
+def test_sin_special_cases_one_arg_either(arg1):
+    """
+    Special case test for `sin(x, /)`:
+
+        -   If `x_i` is either `+infinity` or `-infinity`, the result is `NaN`.
+
+    """
+    res = sin(arg1)
+    mask = logical_or(exactly_equal(arg1, infinity(arg1.shape, arg1.dtype)), exactly_equal(arg1, -infinity(arg1.shape, arg1.dtype)))
+    assert_exactly_equal(res[mask], (NaN(arg1.shape, arg1.dtype))[mask])
