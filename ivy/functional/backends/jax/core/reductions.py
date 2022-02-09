3ae48911ff2aa4ace8aed92f3e3c27c661b348a5
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-09 19:30:41 +0100

    added reduction method all.

--- ivy/functional/backends/jax/core/reductions.py
+++ ivy/functional/backends/jax/core/reductions.py
@@ -83,3 +83,10 @@ def einsum(equation, *operands):
     if ret.shape == ():
         return _jnp.reshape(ret, (1,))
     return ret
+
+
+def all(x, axis=None, keepdims=False):
+    ret = _jnp.all(x, axis, keepdims=keepdims)
+    if ret.shape == ():
+        return _jnp.reshape(ret, (1,))
+    return ret
