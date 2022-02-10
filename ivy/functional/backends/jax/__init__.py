646ef85d8f9c89470d8733219085aecef188abd3
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-10 21:08:30 +0100

    added attributes all_dtypes, valid_dtypes, invalid_dtypes for all backend frameworks.

--- ivy/functional/backends/jax/__init__.py
+++ ivy/functional/backends/jax/__init__.py
@@ -48,6 +48,12 @@ float64 = jnp.dtype('float64')
 # noinspection PyShadowingBuiltins
 bool = jnp.dtype('bool')
 
+all_dtypes = (int8, int16, int32, int64,
+              uint8, uint16, uint32, uint64,
+              bfloat16, float16, float32, float64)
+valid_dtypes = all_dtypes
+invalid_dtypes = ()
+
 iinfo = jnp.iinfo
 
 class Finfo:
