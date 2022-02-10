0e52503684ff855450ab8a3996bcf63f32a1491e
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-10 21:43:41 +0100

    added attributes all_dtype_strs, valid_dtype_strs, invalid_dtype_strs for all backend frameworks, and also added methods invalid_dtype and valid_dtype.

--- ivy/functional/backends/numpy/__init__.py
+++ ivy/functional/backends/numpy/__init__.py
@@ -36,6 +36,12 @@ all_dtypes = (int8, int16, int32, int64,
 valid_dtypes = all_dtypes
 invalid_dtypes = (bfloat16,)
 
+all_dtype_strs = ('int8', 'int16', 'int32', 'int64',
+                  'uint8', 'uint16', 'uint32', 'uint64',
+                  'float16', 'float32', 'float64')
+valid_dtype_strs = all_dtypes
+invalid_dtype_strs = ('bfloat16',)
+
 iinfo = np.iinfo
 
 class Finfo:
