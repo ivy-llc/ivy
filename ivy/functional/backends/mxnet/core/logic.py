9d38ba120dafa895f1c5f6e2e79a7d69a9c0d4af
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-11 19:07:16 +0100

    small mxnet fixes.

--- ivy/functional/backends/mxnet/core/logic.py
+++ ivy/functional/backends/mxnet/core/logic.py
@@ -8,6 +8,7 @@ import mxnet as _mx
 # local
 from ivy.functional.backends.mxnet.core.general import _handle_flat_arrays_in_out
 
+
 @_handle_flat_arrays_in_out
 def logical_and(x1, x2):
     return _mx.nd.logical_and(x1, x2)
