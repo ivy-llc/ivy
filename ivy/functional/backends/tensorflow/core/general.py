5cda91ff828d65ebcbef6b892ce60b12def28e6b
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-11 18:31:37 +0100

    added decorator methods _handle_flat_arrays_in and _handle_flat_arrays_in_out for handling flat arrays in the inputs and outputs of any function.

--- ivy/functional/backends/tensorflow/core/general.py
+++ ivy/functional/backends/tensorflow/core/general.py
@@ -231,11 +231,22 @@ transpose = _tf.transpose
 expand_dims = _tf.expand_dims
 where = lambda condition, x1, x2: _tf.where(_tf.cast(condition, _tf.bool), x1, x2)
 indices_where = _tf.where
-isnan = _tf.math.is_nan
-isinf = _tf.math.is_inf
+
+def isnan(x):
+    if ivy.is_int_dtype(x):
+        return _tf.zeros_like(x, _tf.bool)
+    return _tf.math.is_nan(x)
+
+
+def isinf(x):
+    if ivy.is_int_dtype(x):
+        return _tf.zeros_like(x, _tf.bool)
+    return _tf.math.is_inf(x)
 
 
 def isfinite(x):
+    if ivy.is_int_dtype(x):
+        return _tf.ones_like(x, _tf.bool)
     return _tf.math.is_finite(x)
 
 
