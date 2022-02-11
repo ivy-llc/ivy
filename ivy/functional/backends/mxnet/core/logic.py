7d3debf3260e74597ff7c2ac5273785527d18515
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-11 18:37:42 +0100

    added flat array handling to more mxnet methods.
    Former-commit-id: 66f1cc9ae7003541d400ee40c2b01e5ca8707b1f

--- ivy/functional/backends/mxnet/core/logic.py
+++ ivy/functional/backends/mxnet/core/logic.py
@@ -5,6 +5,19 @@ Collection of MXNet logic functions, wrapped to fit Ivy syntax and signature.
 # global
 import mxnet as _mx
 
-logical_and = lambda x1, x2: _mx.nd.logical_and(x1, x2)
-logical_or = lambda x1, x2: _mx.nd.logical_or(x1, x2)
-logical_not = _mx.nd.logical_not
+# local
+from ivy.functional.backends.mxnet.core.general import _handle_flat_arrays_in_out
+
+@_handle_flat_arrays_in_out
+def logical_and(x1, x2):
+    return _mx.nd.logical_and(x1, x2)
+
+
+@_handle_flat_arrays_in_out
+def logical_or(x1, x2):
+    return _mx.nd.logical_or(x1, x2)
+
+
+@_handle_flat_arrays_in_out
+def logical_not(x):
+    return _mx.nd.logical_not(x)
