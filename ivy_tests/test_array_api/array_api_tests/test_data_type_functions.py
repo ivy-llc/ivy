c315fdec4775d0447b7e6e0b64a6553d43f32c58
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-11 10:47:25 +0100

    added array API finfo unit test.
    Former-commit-id: 61da718cb5e56ea120e9776a902cf117e1723c5b

--- ivy_tests/test_array_api/array_api_tests/test_data_type_functions.py
+++ ivy_tests/test_array_api/array_api_tests/test_data_type_functions.py
@@ -1,24 +1,24 @@
-# import struct
-# from typing import Union
-#
-# import pytest
-# from hypothesis import given
-# from hypothesis import strategies as st
-#
-# import ivy
-# from . import _array_module as xp
-# from . import dtype_helpers as dh
-# from . import hypothesis_helpers as hh
-# from . import pytest_helpers as ph
-# from . import xps
-# from .algos import broadcast_shapes
-# from .typing import DataType
-#
-# pytestmark = pytest.mark.ci
-#
-#
-# def float32(n: Union[int, float]) -> float:
-#     return struct.unpack("!f", struct.pack("!f", float(n)))[0]
+import struct
+from typing import Union
+
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+import ivy
+from . import _array_module as xp
+from . import dtype_helpers as dh
+from . import hypothesis_helpers as hh
+from . import pytest_helpers as ph
+from . import xps
+from .algos import broadcast_shapes
+from .typing import DataType
+
+pytestmark = pytest.mark.ci
+
+
+def float32(n: Union[int, float]) -> float:
+    return struct.unpack("!f", struct.pack("!f", float(n)))[0]
 #
 #
 # @given(
@@ -128,29 +128,29 @@
 #     assert out == expected, f"{out=}, but should be {expected} {f_func}"
 #
 #
-# def make_dtype_id(dtype: DataType) -> str:
-#     return dh.dtype_to_name[dtype]
-#
-#
-# @pytest.mark.parametrize("dtype", dh.float_dtypes, ids=make_dtype_id)
-# def test_finfo(dtype):
-#     np_dtype = dtype
-#     dtype = ivy.convert_dtype(np_dtype, backend='numpy')
-#     out = ivy.finfo(dtype)
-#     f_func = f"[finfo({dh.dtype_to_name[np_dtype]})]"
-#     for attr, stype in [
-#         ("bits", int),
-#         ("eps", float),
-#         ("max", float),
-#         ("min", float),
-#         ("smallest_normal", float),
-#     ]:
-#         assert hasattr(out, attr), f"out has no attribute '{attr}' {f_func}"
-#         value = getattr(out, attr)
-#         assert isinstance(
-#             value, stype
-#         ), f"type(out.{attr})={type(value)!r}, but should be {stype.__name__} {f_func}"
-#     # TODO: test values
+def make_dtype_id(dtype: DataType) -> str:
+    return dh.dtype_to_name[dtype]
+
+
+@pytest.mark.parametrize("dtype", dh.float_dtypes, ids=make_dtype_id)
+def test_finfo(dtype):
+    np_dtype = dtype
+    dtype = ivy.convert_dtype(np_dtype, backend='numpy')
+    out = ivy.finfo(dtype)
+    f_func = f"[finfo({dh.dtype_to_name[np_dtype]})]"
+    for attr, stype in [
+        ("bits", int),
+        ("eps", float),
+        ("max", float),
+        ("min", float),
+        ("smallest_normal", float),
+    ]:
+        assert hasattr(out, attr), f"out has no attribute '{attr}' {f_func}"
+        value = getattr(out, attr)
+        assert isinstance(
+            value, stype
+        ), f"type(out.{attr})={type(value)!r}, but should be {stype.__name__} {f_func}"
+    # TODO: test values
 #
 #
 # @pytest.mark.parametrize("dtype", dh.all_int_dtypes, ids=make_dtype_id)
