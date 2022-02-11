6d5608199458512a134cc2b12c59e1e7e152d62c
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-11 14:29:02 +0100

    further improvements to dtype submodule and available dtypes for each backend.
    Former-commit-id: 98520829a732734a1d5a1912a43b3162705c8de4

--- ivy/functional/backends/mxnet/__init__.py
+++ ivy/functional/backends/mxnet/__init__.py
@@ -16,13 +16,13 @@ Dtype = type
 
 # data types
 int8 = np.dtype('int8')
-int16 = np.dtype('int16')
+int16 = 'int16'
 int32 = np.dtype('int32')
 int64 = np.dtype('int64')
 uint8 = np.dtype('uint8')
-uint16 = np.dtype('uint16')
-uint32 = np.dtype('uint32')
-uint64 = np.dtype('uint64')
+uint16 = 'uint16'
+uint32 = 'uint32'
+uint64 = 'uint64'
 bfloat16 = 'bfloat16'
 float16 = np.dtype('float16')
 float32 = np.dtype('float32')
@@ -30,17 +30,17 @@ float64 = np.dtype('float64')
 # noinspection PyShadowingBuiltins
 bool = np.dtype('bool')
 
-all_dtypes = (int8, int16, int32, int64,
-              uint8, uint16, uint32, uint64,
+all_dtypes = (int8, int32, int64,
+              uint8,
               float16, float32, float64)
 valid_dtypes = all_dtypes
-invalid_dtypes = (bfloat16,)
+invalid_dtypes = (int16, uint16, uint32, uint64, bfloat16)
 
-all_dtype_strs = ('int8', 'int16', 'int32', 'int64',
-                  'uint8', 'uint16', 'uint32', 'uint64',
+all_dtype_strs = ('int8', 'int32', 'int64',
+                  'uint8',
                   'float16', 'float32', 'float64')
 valid_dtype_strs = all_dtypes
-invalid_dtype_strs = ('bfloat16',)
+invalid_dtype_strs = ('int16', 'uint16', 'uint32', 'uint64', 'bfloat16')
 
 iinfo = np.iinfo
 
