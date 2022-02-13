0e9d1ffd648e77853f57d7450e665871ba85d861
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-02-13 21:18:22 +0100

    Update linalg.py

--- ivy/functional/backends/tensorflow/core/linear_algebra.py
+++ ivy/functional/backends/tensorflow/core/linear_algebra.py
@@ -35,6 +35,7 @@ def matrix_norm(x, p=2, axes=None, keepdims=False):
 inv = _tf.linalg.inv
 pinv = _tf.linalg.pinv
 cholesky = _tf.linalg.cholesky
+cross = _tf.linalg.cross
 
 def vector_to_skew_symmetric_matrix(vector):
     batch_shape = list(vector.shape[:-1])
