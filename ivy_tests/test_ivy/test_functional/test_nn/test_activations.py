fa6f6bb64b10cd54b593f65d7af1613d259bceba
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-16 18:18:36 +0100

    moved array API tests and ivy tests into separate sub-folders, each with their own conftest.py files.

---
+++ ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
@@ -0,0 +1,168 @@
+"""
+Collection of tests for unified neural network activation functions
+"""
+
+# global
+import pytest
+import numpy as np
+
+# local
+import ivy
+import ivy.functional.backends.numpy
+import ivy_tests.test_ivy.helpers as helpers
+
+
+# relu
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_relu(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.relu(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.relu, x), ivy.functional.backends.numpy.relu(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.relu)
+
+
+# leaky_relu
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_leaky_relu(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.leaky_relu(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.leaky_relu, x), ivy.functional.backends.numpy.leaky_relu(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.leaky_relu)
+
+
+# gelu
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "approx", [True, False])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_gelu(x, approx, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.gelu(x, approx)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.gelu, x, approx), ivy.functional.backends.numpy.gelu(ivy.to_numpy(x), approx))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.gelu)
+
+
+# tanh
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_tanh(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.tanh(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.tanh, x), ivy.functional.backends.numpy.tanh(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.tanh)
+
+
+# sigmoid
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_sigmoid(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.sigmoid(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.sigmoid, x), ivy.functional.backends.numpy.sigmoid(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.sigmoid)
+
+
+# softmax
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_softmax(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.softmax(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.softmax, x), ivy.functional.backends.numpy.softmax(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.softmax)
+
+
+# softplus
+@pytest.mark.parametrize(
+    "x", [[[-1., 1., 2.]]])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_softplus(x, dtype, tensor_fn, dev, call):
+    # smoke test
+    x = tensor_fn(x, dtype, dev)
+    ret = ivy.softplus(x)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == x.shape
+    # value test
+    assert np.allclose(call(ivy.softplus, x), ivy.functional.backends.numpy.softplus(ivy.to_numpy(x)))
+    # compilation test
+    if not ivy.array_mode():
+        helpers.assert_compilable(ivy.softplus)
