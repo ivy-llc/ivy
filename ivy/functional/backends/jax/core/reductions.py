aa1e5054a9bb428d7cb81551962d6f9c5f7ae88e
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-18 18:51:50 +0100

    updated all reductions to return 0-dimensional arrays in the case of full reduction (i.e. no or all axes specified).

--- ivy/functional/backends/jax/core/reductions.py
+++ ivy/functional/backends/jax/core/reductions.py
@@ -12,10 +12,7 @@ def reduce_sum(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.sum(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.sum(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_prod(x, axis=None, keepdims=False):
@@ -24,10 +21,7 @@ def reduce_prod(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.prod(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.prod(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_mean(x, axis=None, keepdims=False):
@@ -36,10 +30,7 @@ def reduce_mean(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.mean(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.mean(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_var(x, axis=None, keepdims=False):
@@ -48,10 +39,7 @@ def reduce_var(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.var(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.var(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_min(x, axis=None, keepdims=False):
@@ -60,10 +48,7 @@ def reduce_min(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.min(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.min(x, axis=axis, keepdims=keepdims)
 
 
 def reduce_max(x, axis=None, keepdims=False):
@@ -72,17 +57,11 @@ def reduce_max(x, axis=None, keepdims=False):
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = _jnp.max(x, axis=axis, keepdims=keepdims)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.max(x, axis=axis, keepdims=keepdims)
 
 
 def einsum(equation, *operands):
-    ret = _jnp.einsum(equation, *operands)
-    if ret.shape == ():
-        return _jnp.reshape(ret, (1,))
-    return ret
+    return _jnp.einsum(equation, *operands)
 
 
 def all(x, axis=None, keepdims=False):
