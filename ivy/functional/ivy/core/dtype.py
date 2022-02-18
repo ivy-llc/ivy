86892b5d0b96de80120455c5928439a6fec76d83
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-18 10:03:42 +0100

    added method dtype_bits for determining number of bits in a given dtype, and got unit tests passing for Array API method equal.

--- ivy/functional/ivy/core/dtype.py
+++ ivy/functional/ivy/core/dtype.py
@@ -85,6 +85,9 @@ def cast(x: Union[ivy.Array, ivy.NativeArray], dtype: ivy.Dtype, f: ivy.Framewor
     return _cur_framework(x, f=f).cast(x, dtype)
 
 
+astype = cast
+
+
 # Queries #
 # --------#
 
@@ -102,6 +105,17 @@ def dtype(x: Union[ivy.Array, ivy.NativeArray], as_str: bool = False, f: ivy.Fra
     :return: Data type of the array
     """
     return _cur_framework(x, f=f).dtype(x, as_str)
+
+
+def dtype_bits(dtype_in: Union[ivy.Dtype, str], f: ivy.Framework = None) -> int:
+    """
+    Get the number of bits used for representing the input data type.
+
+    :param dtype_in: The data tpye to determine the number of bits for.
+    :param f: Machine learning framework. Inferred from inputs if None.
+    :return: The number of bits used to represent the data type.
+    """
+    return _cur_framework(dtype_in, f=f).dtype_bits(dtype_in)
 
 
 def is_int_dtype(dtype_in: Union[ivy.Dtype, str, ivy.Array, ivy.NativeArray, Number]):
