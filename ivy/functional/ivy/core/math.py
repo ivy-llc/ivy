fb258109befbb48d0f53670688fb8711ddd949f1
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-19 15:10:44 +0100

    added method divide, and made small fixes to tensorflow ones method.

--- ivy/functional/ivy/core/math.py
+++ ivy/functional/ivy/core/math.py
@@ -180,3 +180,16 @@ def erf(x):
     :return: The Gauss error function of x.
     """
     return _cur_framework(x).erf(x)
+
+
+def divide(x1, x2):
+    """
+    Calculates the division for each element x1_i of the input array x1 with the respective element x2_i of the
+    input array x2.
+
+    :param x1: dividend input array. Should have a numeric data type.
+    :param x2: divisor input array. Must be compatible with x1 (see Broadcasting). Should have a numeric data type.
+    :return: an array containing the element-wise results. The returned array must have a floating-point data type
+             determined by Type Promotion Rules.
+    """
+    return x1 / x2
