d2550dc895156118f9c34187637a7a4a325f0d7b
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-19 11:53:11 +0100

    updated ivy builtin methods to all use builtin_ prefix to avoid name conflicts with existing methods, dtypes and python builtins.

--- ivy/functional/ivy/core/builtins.py
+++ ivy/functional/ivy/core/builtins.py
@@ -4,190 +4,190 @@ Collection of builtin Ivy functions.
 
 
 # noinspection PyShadowingBuiltins
-def dir(x):
+def builtin_dir(x):
     return x.__dir__()
 
 
 # noinspection PyShadowingBuiltins
-def getattr(x, item):
+def builtin_getattr(x, item):
     return x.__getattr__(item)
 
 
 # noinspection PyShadowingBuiltins
-def getattribute(x, item):
+def builtin_getattribute(x, item):
     return x.__getattribute__(item)
 
 
 # noinspection PyShadowingBuiltins
-def getitem(x, query):
+def builtin_getitem(x, query):
     return x.__getitem__(query)
 
 
 # noinspection PyShadowingBuiltins
-def setitem(x, query, val):
+def builtin_setitem(x, query, val):
     return x.__setitem__(query, val)
 
 
 # noinspection PyShadowingBuiltins
-def contains(x, key):
+def builtin_contains(x, key):
     return x.__contains__(key)
 
 
 # noinspection PyShadowingBuiltins
-def pos(x):
+def builtin_pos(x):
     return x.__pos__()
 
 
 # noinspection PyShadowingBuiltins
-def neg(x):
+def builtin_neg(x):
     return x.__neg__()
 
 
 # noinspection PyShadowingBuiltins
-def pow(x, power):
+def builtin_pow(x, power):
     return x.__pow__(power)
 
 
 # noinspection PyShadowingBuiltins
-def rpow(x, power):
+def builtin_rpow(x, power):
     return x.__rpow__(power)
 
 
 # noinspection PyShadowingBuiltins
-def add(x, other):
+def builtin_add(x, other):
     return x.__add__(other)
 
 
 # noinspection PyShadowingBuiltins
-def radd(x, other):
+def builtin_radd(x, other):
     return x.__radd__(other)
 
 
 # noinspection PyShadowingBuiltins
-def sub(x, other):
+def builtin_sub(x, other):
     return x.__sub__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rsub(x, other):
+def builtin_rsub(x, other):
     return x.__rsub__(other)
 
 
 # noinspection PyShadowingBuiltins
-def mul(x, other):
+def builtin_mul(x, other):
     return x.__mul__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rmul(x, other):
+def builtin_rmul(x, other):
     return x.__rmul__(other)
 
 
 # noinspection PyShadowingBuiltins
-def truediv(x, other):
+def builtin_truediv(x, other):
     return x.__truediv__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rtruediv(x, other):
+def builtin_rtruediv(x, other):
     return x.__rtruediv__(other)
 
 
 # noinspection PyShadowingBuiltins
-def floordiv(x, other):
+def builtin_floordiv(x, other):
     return x.__floordiv__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rfloordiv(x, other):
+def builtin_rfloordiv(x, other):
     return x.__rfloordiv__(other)
 
 
 # noinspection PyShadowingBuiltins
-def abs(x):
+def builtin_abs(x):
     return x.__abs__()
 
 
 # noinspection PyShadowingBuiltins
-def float(x):
+def builtin_float(x):
     return x.__float__()
 
 
 # noinspection PyShadowingBuiltins
-def int(x):
+def builtin_int(x):
     return x.__int__()
 
 
 # noinspection PyShadowingBuiltins
-def bool(x):
+def builtin_bool(x):
     return x.__bool__()
 
 
 # noinspection PyShadowingBuiltins
-def lt(x, other):
+def builtin_lt(x, other):
     return x.__lt__(other)
 
 
 # noinspection PyShadowingBuiltins
-def le(x, other):
+def builtin_le(x, other):
     return x.__le__(other)
 
 
 # noinspection PyShadowingBuiltins
-def eq(x, other):
+def builtin_eq(x, other):
     return x.__eq__(other)
 
 
 # noinspection PyShadowingBuiltins
-def ne(x, other):
+def builtin_ne(x, other):
     return x.__ne__(other)
 
 
 # noinspection PyShadowingBuiltins
-def gt(x, other):
+def builtin_gt(x, other):
     return x.__gt__(other)
 
 
 # noinspection PyShadowingBuiltins
-def ge(x, other):
+def builtin_ge(x, other):
     return x.__ge__(other)
 
 
 # noinspection PyShadowingBuiltins
-def and_(x, other):
+def builtin_and(x, other):
     return x.__and__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rand(x, other):
+def builtin_rand(x, other):
     return x.__rand__(other)
 
 
 # noinspection PyShadowingBuiltins
-def or_(x, other):
+def builtin_or(x, other):
     return x.__or__(other)
 
 
 # noinspection PyShadowingBuiltins
-def ror(x, other):
+def builtin_ror(x, other):
     return x.__ror__(other)
 
 
 # noinspection PyShadowingBuiltins
-def invert(x, other):
+def builtin_invert(x, other):
     return x.__invert__(other)
 
 
 # noinspection PyShadowingBuiltins
-def xor(x, other):
+def builtin_xor(x, other):
     return x.__xor__(other)
 
 
 # noinspection PyShadowingBuiltins
-def rxor(x, other):
+def builtin_rxor(x, other):
     return x.__rxor__(other)
 
 
 # noinspection PyShadowingBuiltins
-def deepcopy(x):
+def builtin_deepcopy(x):
     return x.__deepcopy__()
