667dba69eef24efa8ff6c169d05422ab0c9760d3
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-19 10:02:07 +0100

    removed option for explicit per-function framework setting. with framework.use context should instead be used for changing frameworks for specific blocks of code or functions.

--- ivy/functional/ivy/core/math.py
+++ ivy/functional/ivy/core/math.py
@@ -6,88 +6,76 @@ Collection of math Ivy functions.
 from ivy.framework_handler import current_framework as _cur_framework
 
 
-def sin(x, f=None):
+def sin(x):
     """
     Computes trigonometric sine element-wise.
 
     :param x: Input array, in radians (2*pi radian equals 360 degrees).
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The sine of x element-wise.
     """
-    return _cur_framework(x, f=f).sin(x)
+    return _cur_framework(x).sin(x)
 
 
-def cos(x, f=None):
+def cos(x):
     """
     Computes trigonometric cosine element-wise.
 
     :param x: Input array, in radians (2*pi radian equals 360 degrees).
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The cosine of x element-wise.
     """
-    return _cur_framework(x, f=f).cos(x)
+    return _cur_framework(x).cos(x)
 
 
-def tan(x, f=None):
+def tan(x):
     """
     Computes tangent element-wise.
     Equivalent to f.sin(x)/f.cos(x) element-wise.
 
     :param x: Input array, in radians (2*pi radian equals 360 degrees).
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The tangent of x element-wise.
     """
-    return _cur_framework(x, f=f).tan(x)
+    return _cur_framework(x).tan(x)
 
 
-def asin(x, f=None):
+def asin(x):
     """
     Computes inverse sine element-wise.
 
     :param x: y-coordinate on the unit circle.
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The inverse sine of each element in x, in radians and in the closed interval [-pi/2, pi/2].
     """
-    return _cur_framework(x, f=f).asin(x)
+    return _cur_framework(x).asin(x)
 
 
-def acos(x, f=None):
+def acos(x):
     """
     Computes trigonometric inverse cosine element-wise.
     The inverse of cos so that, if y = cos(x), then x = arccos(y).
 
     :param x: x-coordinate on the unit circle. For real arguments, the domain is [-1, 1].
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The angle of the ray intersecting the unit circle at the given x-coordinate in radians [0, pi].
     """
-    return _cur_framework(x, f=f).acos(x)
+    return _cur_framework(x).acos(x)
 
 
-def atan(x, f=None):
+def atan(x):
     """
     Computes trigonometric inverse tangent, element-wise.
     The inverse of tan, so that if y = tan(x) then x = arctan(y).
 
     :param x: Input array.
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: Out has the same shape as x. Its real part is in [-pi/2, pi/2] (arctan(+/-inf) returns +/-pi/2).
     """
-    return _cur_framework(x, f=f).atan(x)
+    return _cur_framework(x).atan(x)
 
 
-def atan2(x1, x2, f=None):
+def atan2(x1, x2):
     """
     Computes element-wise arc tangent of x1/x2 choosing the quadrant correctly.
 
@@ -96,119 +84,99 @@ def atan2(x1, x2, f=None):
     :param x2: x-coordinates. If x1.shape != x2.shape, they must be broadcastable to a common shape
                     (which becomes the shape of the output).
     :type x2: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: Array of angles in radians, in the range [-pi, pi].
     """
-    return _cur_framework(x1, f=f).atan2(x1, x2)
+    return _cur_framework(x1).atan2(x1, x2)
 
 
-def sinh(x, f=None):
+def sinh(x):
     """
     Returns a new array with the hyperbolic sine of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the hyperbolic sine of the elements of x.
     """
-    return _cur_framework(x, f=f).sinh(x)
+    return _cur_framework(x).sinh(x)
 
 
-def cosh(x, f=None):
+def cosh(x):
     """
     Returns a new array with the hyperbolic cosine of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the hyperbolic cosine of the elements of x.
     """
-    return _cur_framework(x, f=f).cosh(x)
+    return _cur_framework(x).cosh(x)
 
 
-def tanh(x, f=None):
+def tanh(x):
     """
     Returns a new array with the hyperbolic tangent of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the hyperbolic tangent of the elements of x.
     """
-    return _cur_framework(x, f=f).tanh(x)
+    return _cur_framework(x).tanh(x)
 
 
-def asinh(x, f=None):
+def asinh(x):
     """
     Returns a new array with the inverse hyperbolic sine of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the inverse hyperbolic sine of the elements of x.
     """
-    return _cur_framework(x, f=f).asinh(x)
+    return _cur_framework(x).asinh(x)
 
 
-def acosh(x, f=None):
+def acosh(x):
     """
     Returns a new array with the inverse hyperbolic cosine of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the inverse hyperbolic cosine of the elements of x.
     """
-    return _cur_framework(x, f=f).acosh(x)
+    return _cur_framework(x).acosh(x)
 
 
-def atanh(x, f=None):
+def atanh(x):
     """
     Returns a new array with the inverse hyperbolic tangent of the elements of x.
 
     :param x: Input array.
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: A new array with the inverse hyperbolic tangent of the elements of x.
     """
-    return _cur_framework(x, f=f).atanh(x)
+    return _cur_framework(x).atanh(x)
 
 
-def log(x, f=None):
+def log(x):
     """
     Computes natural logarithm of x element-wise.
 
     :param x: Value to compute log for.
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The natural logarithm of each element of x.
     """
-    return _cur_framework(x, f=f).log(x)
+    return _cur_framework(x).log(x)
 
 
-def exp(x, f=None):
+def exp(x):
     """
     Computes exponential of x element-wise.
 
     :param x: Value to compute exponential for.
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The exponential of each element of x.
     """
-    return _cur_framework(x, f=f).exp(x)
+    return _cur_framework(x).exp(x)
 
 
-def erf(x, f=None):
+def erf(x):
     """
     Computes the Gauss error function of x element-wise.
 
     :param x: Value to compute exponential for.
     :type x: array
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The Gauss error function of x.
     """
-    return _cur_framework(x, f=f).erf(x)
+    return _cur_framework(x).erf(x)
