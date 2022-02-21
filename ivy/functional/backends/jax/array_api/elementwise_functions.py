4cedead9d7385e960be987e7ee478dac2c2669bd
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-21 14:45:36 +0100

    moved isfinite method to the Array API submodule, with the correct formatting.

---
+++ ivy/functional/backends/jax/array_api/elementwise_functions.py
@@ -0,0 +1,11 @@
+# global
+import jax
+import jaxlib
+import jax.numpy as jnp
+from typing import Union
+from jaxlib.xla_extension import Buffer
+
+JaxArray = Union[jax.interpreters.xla._DeviceArray, jaxlib.xla_extension.DeviceArray, Buffer]
+
+def isfinite(x: JaxArray) -> JaxArray:
+    return jnp.isfinite(x)
---
+++ ivy/functional/backends/numpy/array_api/elementwise_functions.py
@@ -0,0 +1,6 @@
+# global
+import numpy as np
+
+
+def isfinite(x: np.ndarray) -> np.ndarray:
+    return np.isfinite(x)
