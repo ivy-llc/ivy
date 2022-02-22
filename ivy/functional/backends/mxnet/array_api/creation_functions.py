ddd82a1d26ca21dfa16e367036aa08a728e51db0
Ali Eren Ak <akali@sabanciuniv.edu>
2022-02-22 02:19:33 +0200

    mxnet small fix.

--- ivy/functional/backends/mxnet/array_api/creation_functions.py
+++ ivy/functional/backends/mxnet/array_api/creation_functions.py
@@ -1,6 +1,8 @@
 # global
 import mxnet as mx
+from collections.abc import Iterable
 from typing import Union, Optional, Tuple
+
 
 # local
 from ivy.functional.ivy.core import default_device
@@ -12,6 +14,7 @@ def ones(shape: Union[int, Tuple[int, ...]],
          device: Optional[str] = None) \
         -> mx.ndarray.ndarray.NDArray:
     cont = _mxnet_init_context(default_device(device))
-    if shape == 0 or len(shape) == 0 or 0 in shape:
+    shape = [shape] if shape is not isinstance(shape, Iterable) else shape
+    if len(shape) == 0 or 0 in shape:
         return _1_dim_array_to_flat_array(mx.nd.ones((1,), ctx=cont).astype(dtype))
     return mx.nd.ones(shape, ctx=cont).astype(dtype)
