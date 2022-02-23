93cc26d1db78be3bc1d781686d65e2f040b6af94
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2022-02-23 18:59:36 +0100

    Update linear_algebra_extension.py

--- ivy/functional/backends/tensorflow/array_api/linear_algebra_extension.py
+++ ivy/functional/backends/tensorflow/array_api/linear_algebra_extension.py
@@ -4,27 +4,28 @@ from tensorflow.python.types.core import Tensor
 from typing  import Union, Optional, Tuple, Literal
 
 # local
-inf = float("inf")
+from ivy import ivy
+
 
 def vector_norm(x: Tensor, 
-                p:Union[int, float, Literal[inf, - inf]] = 2, 
-                axis: Optional[Union[int, Tuple[int, ...]]] = None, 
+                p: Union[int, float, Literal[inf, - inf]] = 2, 
+                axis: Optional[Union[int, Tuple[int]]] = None, 
                 keepdims: bool = False)\
                  -> Tensor:
 
-    tn_normalized_vector_ = None
+    tn_normalized_vector = None
 
     if p == -float('inf'):
-        tn_normalized_vector_ = tf.reduce_min(tf.abs(x), axis, keepdims)
+        tn_normalized_vector = tf.reduce_min(tf.abs(x), axis, keepdims)
     elif p == -1:
-        tn_normalized_vector_ = tf.reduce_sum(tf.abs(x)**p, axis, keepdims)**(1./p)
+        tn_normalized_vector = tf.reduce_sum(tf.abs(x)**p, axis, keepdims)**(1./p)
 
     elif p == 0:
-        tn_normalized_vector_ = tf.reduce_sum(tf.cast(x != 0, 'float32'), axis, keepdims).numpy()
+        tn_normalized_vector = tf.reduce_sum(tf.cast(x != 0, 'float32'), axis, keepdims).numpy()
 
     else:
-        tn_normalized_vector_ = tf.linalg.norm(x,p,axis,keepdims)
+        tn_normalized_vector = tf.linalg.norm(x,p,axis,keepdims)
 
-    if tn_normalized_vector_.shape  == tuple():
-        return  tf.expand_dims(tn_normalized_vector_, 0)
-    return tn_normalized_vector_
+    if tn_normalized_vector.shape  == tuple():
+        return  tf.expand_dims(tn_normalized_vector, 0)
+    return tn_normalized_vector
