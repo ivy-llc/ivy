f0ae23addeced86b216fb9c3acf543588ab0a374
Daniel Lenton <daniel.lenton11@imperial.ac.uk>
2022-02-23 18:56:39 +0100

    Update linear_algebra_extension.py

--- ivy/functional/backends/numpy/array_api/linear_algebra_extension.py
+++ ivy/functional/backends/numpy/array_api/linear_algebra_extension.py
@@ -3,24 +3,23 @@ import numpy as np
 from typing  import Union, Optional, Tuple, Literal
 
 # local
-inf = float("inf")
+from ivy import inf
 
 
 def vector_norm(x: np.ndarray, 
-                p:Union[int, float, Literal[inf, - inf]] = 2, 
-                axis: Optional[Union[int, Tuple[int, ...]]] = None, 
+                p: Union[int, float, Literal[inf, - inf]] = 2, 
+                axis: Optional[Union[int, Tuple[int]]] = None, 
                 keepdims: bool = False)\
-                 -> np.ndarray :
+                 -> np.ndarray:
 
-    np_normalized_vector_ = None
+    np_normalized_vector = None
 
     if axis == None:
-        np_normalized_vector_ = np.linalg.norm(x.flatten(),p, axis,keepdims)
+        np_normalized_vector = np.linalg.norm(x.flatten(),p, axis,keepdims)
 
     else:
-        np_normalized_vector_ = np.linalg.norm(x,p, axis,keepdims)
+        np_normalized_vector = np.linalg.norm(x,p, axis,keepdims)
 
-
-    if np_normalized_vector_.shape == tuple():
-        return np.expand_dims(np_normalized_vector_, 0)
-    return np_normalized_vector_
+    if np_normalized_vector.shape == tuple():
+        return np.expand_dims(np_normalized_vector, 0)
+    return np_normalized_vector
