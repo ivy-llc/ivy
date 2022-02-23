16415976cd4c6ead11601cc7e707a7381415cd85
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2022-02-23 16:51:20 +0000

    formatting update for sinh elemet-wise function

--- ivy/functional/ivy/array_api/elementwise_functions.py
+++ ivy/functional/ivy/array_api/elementwise_functions.py
@@ -1,14 +1,15 @@
+# local
 import ivy
 from ivy.framework_handler import current_framework as _cur_framework
 
-def sinh(x: ivy.Array, /) -> ivy.Array:
+def sinh(x: ivy.Array) -> ivy.Array:
     """
-        Calculates an implementation-dependent approximation to the hyperbolic sine, having domain
-        [-infinity, +infinity] and codomain [-infinity, +infinity], for each element x_i
-        of the input array x.
+    Calculates an implementation-dependent approximation to the hyperbolic sine, having domain [-infinity, +infinity]
+    and codomain [-infinity, +infinity], for each element x_i of the input array x.
 
-        :param x: input array.
-        :return: an array containing the hyperbolic sine of each element in x.
+    :param x: input array whose elements each represent a hyperbolic angle. Should have a floating-point data type.
+    :return: an array containing the hyperbolic sine of each element in x. The returned array must have a
+             floating-point data type determined by Type Promotion Rules.
     """
     if x.__contains__(float('NaN')):
         return float('NaN')
