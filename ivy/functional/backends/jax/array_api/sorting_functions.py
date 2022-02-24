7b1abc78bf2082fdd7d1a20465979fb2ffe267be
Dusan Pantelic <pantelic.dusan@protonmail.com>
2022-02-24 15:10:35 +0100

    argsort and any implementation (#253)

---
+++ ivy/functional/backends/jax/array_api/sorting_functions.py
@@ -0,0 +1,17 @@
+# global
+import jax.numpy as jnp
+from typing import Union, Tuple, Optional, List
+
+# local
+from ivy.functional.backends.jax import JaxArray
+
+
+def argsort(x: JaxArray,
+            axis: int = -1,
+            descending: bool = False,
+            stable: bool = True)\
+            -> JaxArray:
+    if descending:
+        return jnp.asarray(jnp.argsort(-1 * jnp.searchsorted(jnp.unique(x), x), axis, kind='stable'))
+    else:
+        return jnp.asarray(jnp.argsort(x, axis, kind='stable'))
