246264a890959887a88cc653763a458903ac4401
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-02-25 18:47:59 +0100

    Update linear_algebra_functions.py

--- ivy/functional/backends/jax/array_api/linear_algebra_functions.py
+++ ivy/functional/backends/jax/array_api/linear_algebra_functions.py
@@ -1,11 +1,1 @@
-# global
-import jax.numpy as jnp
 
-
-# local
-from ivy.functional.backends.jax import JaxArray
-
-
-# noinspection PyShadowingBuiltins
-def cross(x1: JaxArray, x2: JaxArray, /, *, axis: int = -1) -> JaxArray:
-    return jnp.cross(a=x1, b=x2, axis=axis)
