e3f01afcc23c9474571f6ba09d501249e0259f78
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-02-25 18:51:08 +0100

    Update linear_algebra_functions.py

--- ivy/functional/backends/mxnet/array_api/linear_algebra_functions.py
+++ ivy/functional/backends/mxnet/array_api/linear_algebra_functions.py
@@ -1,8 +1,1 @@
-# global
-import mxnet as mx
-from typing import Union, Tuple, Optional
 
-
-def cross(x1: mx.ndarray.ndarray.NDArray, x2: mx.ndarray.ndarray.NDArray, /, *,
-          axis: int = -1) -> mx.ndarray.ndarray.NDArray:
-    return mx.linalg.cross(x1, x2, axis=axis)
