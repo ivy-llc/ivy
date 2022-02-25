5c6073d0365e17d4e798a577db4be7f719b043ea
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 17:08:05 +0100

    formatting fixes for Array API submodule in JAX backend.

--- ivy/functional/backends/jax/array_api/linear_algebra_extension.py
+++ ivy/functional/backends/jax/array_api/linear_algebra_extension.py
@@ -1,33 +1,23 @@
 # global
-from typing  import Union, Optional, Tuple, Literal
-import jax as _jax
-# noinspection PyPackageRequirements
-import jaxlib
 import jax.numpy as jnp
-from jaxlib.xla_extension import Buffer
-
+from typing import Union, Optional, Tuple, Literal
 
 # local
 from ivy import inf
-
-JaxArray = (_jax.interpreters.xla._DeviceArray, jaxlib.xla_extension.DeviceArray, Buffer)
+from ivy.functional.backends.jax import JaxArray
 
 
 def vector_norm(x: JaxArray, 
-                p: Union[int, float, Literal[inf, - inf]] = 2, 
+                p: Union[int, float, Literal[inf, -inf]] = 2,
                 axis: Optional[Union[int, Tuple[int]]] = None, 
                 keepdims: bool = False)\
                     -> JaxArray:
-                
 
-    jnp_normalized_vector = None
-
-    if axis == None:
-       jnp_normalized_vector = jnp.linalg.norm(jnp.ravel(x), p, axis, keepdims)
-
+    if axis is None:
+        jnp_normalized_vector = jnp.linalg.norm(jnp.ravel(x), p, axis, keepdims)
     else:
         jnp_normalized_vector = jnp.linalg.norm(x, p, axis, keepdims)
 
-    if jnp_normalized_vector.shape  == tuple():
-        return  jnp.expand_dims(jnp_normalized_vector, 0)
+    if jnp_normalized_vector.shape == ():
+        return jnp.expand_dims(jnp_normalized_vector, 0)
     return jnp_normalized_vector
