a6658185bbb6ba711558e63f58a69828a5bde128
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 19:11:52 +0100

    formatting fixes for Array API submodule in backend APIs.

--- ivy/functional/backends/tensorflow/array_api/linear_algebra_extension.py
+++ ivy/functional/backends/tensorflow/array_api/linear_algebra_extension.py
@@ -7,23 +7,24 @@ from typing import Union, Optional, Tuple, Literal
 from ivy import inf
 
 
-def vector_norm(x: Tensor, 
-                p: Union[int, float, Literal[inf, - inf]] = 2, 
+# noinspection PyUnusedLocal,PyShadowingBuiltins
+def vector_norm(x: Tensor,
                 axis: Optional[Union[int, Tuple[int]]] = None, 
-                keepdims: bool = False)\
+                keepdims: bool = False,
+                ord: Union[int, float, Literal[inf, - inf]] = 2)\
                  -> Tensor:
 
-    if p == -float('inf'):
+    if ord == -float('inf'):
         tn_normalized_vector = tf.reduce_min(tf.abs(x), axis, keepdims)
-    elif p == -1:
-        tn_normalized_vector = tf.reduce_sum(tf.abs(x)**p, axis, keepdims)**(1./p)
+    elif ord == -1:
+        tn_normalized_vector = tf.reduce_sum(tf.abs(x)**ord, axis, keepdims)**(1./ord)
 
-    elif p == 0:
+    elif ord == 0:
         tn_normalized_vector = tf.reduce_sum(tf.cast(x != 0, 'float32'), axis, keepdims).numpy()
 
     else:
-        tn_normalized_vector = tf.linalg.norm(x,p,axis,keepdims)
+        tn_normalized_vector = tf.linalg.norm(x, ord, axis, keepdims)
 
-    if tn_normalized_vector.shape  == tuple():
+    if tn_normalized_vector.shape == tuple():
         return tf.expand_dims(tn_normalized_vector, 0)
     return tn_normalized_vector
