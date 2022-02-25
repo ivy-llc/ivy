9d6e9d9132c05332415dd20d462d55a3da76f56e
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 19:04:46 +0100

    formatting fixes for Array API submodule in Ivy API.

--- ivy/functional/ivy/array_api/elementwise_functions.py
+++ ivy/functional/ivy/array_api/elementwise_functions.py
@@ -33,7 +33,8 @@ def isfinite(x: Union[ivy.Array, ivy.NativeArray])\
     return _cur_framework(x).isfinite(x)
 
 
-def sqrt(x: ivy.Array) -> ivy.Array:
+def sqrt(x: Union[ivy.Array, ivy.NativeArray])\
+        -> ivy.Array:
 
     """
     Calculates the square root, having domain [0, +infinity] and codomain [0, +infinity], for each element x_i of the
@@ -59,7 +60,7 @@ def cosh(x: Union[ivy.Array, ivy.NativeArray])\
 
 
 def isnan(x: Union[ivy.Array, ivy.NativeArray])\
-        -> Union[ivy.Array, ivy.NativeArray]:
+        -> ivy.Array:
     """
     Returns boolean map at locations where the input is not a number (nan).
 
@@ -70,7 +71,8 @@ def isnan(x: Union[ivy.Array, ivy.NativeArray])\
     return _cur_framework(x).isnan(x)
 
 
-def less(x1: Union[ivy.Array, ivy.NativeArray],x2: Union[ivy.Array, ivy.NativeArray], f: ivy.Framework = None)\
+def less(x1: Union[ivy.Array, ivy.NativeArray],
+         x2: Union[ivy.Array, ivy.NativeArray])\
         -> ivy.Array:
     """
     Computes the truth value of x1_i < x2_i for each element x1_i of the input array x1 with the respective 
