1e12fe4e201ff43bfb8f87398bd5135cae05024c
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 19:04:46 +0100

    formatting fixes for Array API submodule in Ivy API.
    Former-commit-id: 720ec1f832ddf34faa6f8a2a0a96876aa11aa42d

--- ivy/functional/ivy/array_api/linear_algebra_extension.py
+++ ivy/functional/ivy/array_api/linear_algebra_extension.py
@@ -6,33 +6,67 @@ import ivy
 inf = float('inf')
 
 
-def vector_norm(x: Union[ivy.Array, ivy.NativeArray], 
-                p: Union[int, float, Literal[inf, -inf]] = 2,
+# noinspection PyShadowingBuiltins
+def vector_norm(x: Union[ivy.Array, ivy.NativeArray],
                 axis: Optional[Union[int, Tuple[int]]] = None, 
-                keepdims: bool = False)\
-                    -> ivy.Array:
+                keepdims: bool = False,
+                ord: Union[int, float, Literal[inf, -inf]] = 2)\
+        -> ivy.Array:
 
     """
-    Compute the vector p-norm.
+    Computes the vector norm of a vector (or batch of vectors) x.
 
-    :param x: Input array.
-    :type x: array
-    :param p: Order of the norm. Default is 2.
-    :type p: int or str, optional
-    :param axis: If axis is an integer, it specifies the axis of x along which to compute the vector norms.
-                 Default is None, in which case the flattened array is considered.
-    :type axis: int or sequence of ints, optional
-    :param keepdims: If this is set to True, the axes which are normed over are left in the result as dimensions with
-                     size one. With this option the result will broadcast correctly against the original x.
-                     Default is False.
-    :type keepdims: bool, optional
-    :return: Vector norm of the array at specified axes.
+    :param x: input array. Should have a floating-point data type.
+    :param axis: If an integer, ``axis`` specifies the axis (dimension) along which to compute vector norms. If an
+                 n-tuple, ``axis`` specifies the axes (dimensions) along which to compute batched vector norms. If
+                 ``None``, the vector norm must be computed over all array values (i.e., equivalent to computing the
+                 vector norm of a flattened array). Negative indices must be supported. Default: ``None``.
+    :param keepdims: If ``True``, the axes (dimensions) specified by ``axis`` must be included in the result as
+                     singleton dimensions, and, accordingly, the result must be compatible with the input array.
+                     Otherwise, if ``False``, the axes (dimensions) specified by ``axis`` must not be included in the
+                     result. Default: ``False``.
+    :param keepdims: If True, the axes (dimensions) specified by axis must be included in the result as singleton
+                     dimensions, and, accordingly, the result must be compatible with the input array (see
+                     Broadcasting). Otherwise, if False, the axes (dimensions) specified by axis must not be included
+                     in the result. Default: False.
+    :param ord: order of the norm. The following mathematical norms must be supported:
+        +------------------+----------------------------+
+        | ord              | description                |
+        +==================+============================+
+        | 1                | L1-norm (Manhattan)        |
+        +------------------+----------------------------+
+        | 2                | L2-norm (Euclidean)        |
+        +------------------+----------------------------+
+        | inf              | infinity norm              |
+        +------------------+----------------------------+
+        | (int,float >= 1) | p-norm                     |
+        +------------------+----------------------------+
+        The following non-mathematical "norms" must be supported:
+        +------------------+--------------------------------+
+        | ord              | description                    |
+        +==================+================================+
+        | 0                | sum(a != 0)                    |
+        +------------------+--------------------------------+
+        | -1               | 1./sum(1./abs(a))              |
+        +------------------+--------------------------------+
+        | -2               | 1./sqrt(sum(1./abs(a)\*\*2))   |
+        +------------------+--------------------------------+
+        | -inf             | min(abs(a))                    |
+        +------------------+--------------------------------+
+        | (int,float < 1)  | sum(abs(a)\*\*ord)\*\*(1./ord) |
+        +------------------+--------------------------------+
+        Default: ``2``.
+    :return: an array containing the vector norms. If ``axis`` is ``None``, the returned array must be a
+             zero-dimensional array containing a vector norm. If ``axis`` is a scalar value (``int`` or ``float``), the
+             returned array must have a rank which is one less than the rank of ``x``. If ``axis`` is a ``n``-tuple, the
+             returned array must have a rank which is ``n`` less than the rank of ``x``. The returned array must have a
+             floating-point data type determined by type-promotion.
     """
-    if p == -float('inf'):
+    if ord == -float('inf'):
         return ivy.reduce_min(ivy.abs(x), axis, keepdims)
-    elif p == float('inf'):
+    elif ord == float('inf'):
         return ivy.reduce_max(ivy.abs(x), axis, keepdims)
-    elif p == 0:
+    elif ord == 0:
         return ivy.reduce_sum(ivy.cast(x != 0, 'float32'), axis, keepdims)
-    x_raised = x ** p
-    return ivy.reduce_sum(x_raised, axis, keepdims) ** (1/p)
+    x_raised = x ** ord
+    return ivy.reduce_sum(x_raised, axis, keepdims) ** (1/ord)
--- ivy/functional/ivy/array_api/sorting_functions.py
+++ ivy/functional/ivy/array_api/sorting_functions.py
@@ -9,22 +9,15 @@ from ivy.framework_handler import current_framework as _cur_framework
 def argsort(x: Union[ivy.Array, ivy.NativeArray],
             axis: int = -1,
             descending: bool = False,
-            stable: bool = True,
-            f: ivy.Framework = None)\
-            -> Union[ivy.Array, ivy.NativeArray]:
+            stable: bool = True)\
+            -> ivy.Array:
     """
     Returns the indices of a tensor that give its sorted order along an axis.
 
     :param x: Input array containing elements to argsort.
-    :type x: array
     :param axis: Axis to perform the argsort, default is -1.
-    :type axis: int, optional
     :param descending: Sort order, default is False.
-    :type descending: bool, optional
     :param stable: Sort stability, default is True.
-    :type stable: bool, optional
-    :param f: Machine learning framework. Inferred from inputs if None.
-    :type f: ml_framework, optional
     :return: The indices that would sort each slice of the given values along the given axis.
     """
-    return _cur_framework(x, f=f).argsort(x, axis, descending, stable)
+    return _cur_framework(x).argsort(x, axis, descending, stable)
