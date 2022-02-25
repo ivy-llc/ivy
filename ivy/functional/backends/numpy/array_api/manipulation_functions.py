193742e08dfe5a31f712bf3d6cf7125892efb328
Dusan Pantelic <pantelic.dusan@protonmail.com>
2022-02-25 18:21:06 +0100

    flip adhering to Array API Standard (#315)

---
+++ ivy/functional/backends/numpy/array_api/manipulation_functions.py
@@ -0,0 +1,17 @@
+# global
+import numpy as np
+from typing import Union, Tuple, Optional, List
+
+
+def flip(x: np.ndarray,
+         axis: Optional[Union[int, Tuple[int], List[int]]] = None)\
+         -> np.ndarray:
+    num_dims = len(x.shape)
+    if not num_dims:
+        return x
+    if axis is None:
+        axis = list(range(num_dims))
+    if type(axis) is int:
+        axis = [axis]
+    axis = [item + num_dims if item < 0 else item for item in axis]
+    return np.flip(x, axis)
