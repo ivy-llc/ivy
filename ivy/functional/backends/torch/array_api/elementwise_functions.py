b28f6f0fa346d860f0c541f308e8445afec158a9
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-02-25 13:50:52 +0000

    logical_not adhering to the Array API Standard (#274)

--- ivy/functional/backends/torch/array_api/elementwise_functions.py
+++ ivy/functional/backends/torch/array_api/elementwise_functions.py
@@ -1,6 +1,11 @@
 # global
 import torch
 
+
 def isfinite(x: torch.Tensor)\
         -> torch.Tensor:
     return torch.isfinite(x)
+
+
+def logical_not(x: torch.Tensor) -> torch.Tensor:
+    return torch.logical_not(x.type(torch.bool))
