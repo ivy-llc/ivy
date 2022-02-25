3c86419a1f7f07da8c735e78500e07704985731b
7wikd <satwik.dalvi@gmail.com>
2022-02-26 01:14:32 +0530

    Attempt to fix TF error

--- ivy/functional/backends/tensorflow/array_api/searching_functions.py
+++ ivy/functional/backends/tensorflow/array_api/searching_functions.py
@@ -2,10 +2,11 @@
 import tensorflow as tf
 from typing import Optional
 from tensorflow.python.types.core import Tensor
+from typing import Optional
 
 def argmax(
     x: Tensor,
     axis: Optional[int] = None,
-    out: Optional[Tensor] = None
+    out: Optional[int] = None
 ) -> Tensor:
     return tf.argmax(x, axis=axis, output_type=out)
