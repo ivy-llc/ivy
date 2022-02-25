d6d4f223d52ffbe77284349237621ff39be496b9
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 17:11:06 +0100

    formatting fixes for Array API submodule in NumPy backend.
    Former-commit-id: a5636e98bd1fa0ba5dbfca08cb8058eaa07687a7

--- ivy/functional/backends/numpy/array_api/creation_functions.py
+++ ivy/functional/backends/numpy/array_api/creation_functions.py
@@ -1,21 +1,21 @@
 # global
 import numpy as np
-from typing import Union, Tuple, Optional
+from typing import Union, Tuple, Optional, List
 
 # local
 from ivy import dtype_from_str, default_dtype
+# noinspection PyProtectedMember
 from ivy.functional.backends.numpy.core.general import _to_dev
 
 
-# noinspection PyShadowingNames
-def zeros(shape: Union[int, Tuple[int, ...]],
+def zeros(shape: Union[int, Tuple[int, ...], List[int, ...]],
           dtype: Optional[np.dtype] = None,
           device: Optional[str] = None) \
         -> np.ndarray:
     return _to_dev(np.zeros(shape, dtype_from_str(default_dtype(dtype))), device)
 
 
-def ones(shape: Union[int, Tuple[int, ...]],
+def ones(shape: Union[int, Tuple[int, ...], List[int, ...]],
          dtype: Optional[np.dtype] = None,
          device: Optional[str] = None) \
         -> np.ndarray:
