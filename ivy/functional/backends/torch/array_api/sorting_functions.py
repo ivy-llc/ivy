2937f7242469fd04b6d8b79a693ebe1070baaeed
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-25 17:15:25 +0100

    formatting fixes for Array API submodule in Torch backend.
    Former-commit-id: 197b9a24cedddab2287256b5081052ea8a2a9758

--- ivy/functional/backends/torch/array_api/sorting_functions.py
+++ ivy/functional/backends/torch/array_api/sorting_functions.py
@@ -1,11 +1,10 @@
 # global
 import torch
-from typing import Union, Optional, Tuple, List
 
 
 def argsort(x: torch.Tensor,
             axis: int = -1,
             descending: bool = False,
             stable: bool = True)\
-            -> torch.Tensor:
+        -> torch.Tensor:
     return torch.argsort(x, dim=axis, descending=descending)
