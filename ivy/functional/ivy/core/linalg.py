a62509e45cde3692c37bfd0c6dbbfad437429fb8
Kene <33866614+kene111@users.noreply.github.com>
2022-02-25 15:09:35 +0100

    vector_norm adhering to Array API Standard (#283)
    Former-commit-id: 52303ad56ca00736a4f9c33913dfda81b11ecc83

--- ivy/functional/ivy/core/linalg.py
+++ ivy/functional/ivy/core/linalg.py
@@ -31,31 +31,6 @@ def svd(x):
     return _cur_framework(x).svd(x)
 
 
-def vector_norm(x, p=2, axis=None, keepdims=False):
-    """
-    Compute the vector p-norm.
-
-    :param x: Input array.
-    :type x: array
-    :param p: Order of the norm. Default is 2.
-    :type p: int or str, optional
-    :param axis: If axis is an integer, it specifies the axis of x along which to compute the vector norms.
-                 Default is None, in which case the flattened array is considered.
-    :type axis: int or sequence of ints, optional
-    :param keepdims: If this is set to True, the axes which are normed over are left in the result as dimensions with
-                     size one. With this option the result will broadcast correctly against the original x.
-                     Default is False.
-    :type keepdims: bool, optional
-    :return: Vector norm of the array at specified axes.
-    """
-    if p == -float('inf'):
-        return ivy.reduce_min(ivy.abs(x), axis, keepdims)
-    elif p == float('inf'):
-        return ivy.reduce_max(ivy.abs(x), axis, keepdims)
-    elif p == 0:
-        return ivy.reduce_sum(ivy.cast(x != 0, 'float32'), axis, keepdims)
-    x_raised = x ** p
-    return ivy.reduce_sum(x_raised, axis, keepdims) ** (1/p)
 
 
 def matrix_norm(x, p=2, axes=None, keepdims=False):
