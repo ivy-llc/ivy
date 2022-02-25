863760f7b6a975bc3f0a1114e955c84011e3d82c
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-02-25 14:01:47 +0000

    cos adhering to the Array API Standard (#276)

--- ivy/functional/backends/torch/array_api/elementwise_functions.py
+++ ivy/functional/backends/torch/array_api/elementwise_functions.py
@@ -1,10 +1,19 @@
 # global
 import torch
+import math
+
 
 
 def isfinite(x: torch.Tensor)\
         -> torch.Tensor:
     return torch.isfinite(x)
+
+
+def cos(x: torch.Tensor)\
+        -> torch.Tensor:
+    if isinstance(x, float):
+        return math.cos(x)
+    return torch.cos(x)
 
 
 def logical_not(x: torch.Tensor) -> torch.Tensor:
