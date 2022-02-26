0fba21246f0a81b3bd0ef7b194c70b24345e2775
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-26 14:01:28 +0100

    updated test_array_api.sh so that all untested methods are skipped explicitly.

--- test_array_api.sh
+++ test_array_api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-python3 ivy_tests/set_array_api_methods_to_test.py
+python3 ivy_tests/write_array_api_tests_k_flag.py
 # shellcheck disable=SC2155
-export ARRAY_API_METHODS_TO_TEST=$(cat ivy_tests/.array_api_methods_to_test)
+export ARRAY_API_TESTS_K_FLAG=$(cat ivy_tests/.array_api_tests_k_flag)
 # shellcheck disable=SC2046
-docker run --rm --env IVY_BACKEND="$1" --env ARRAY_API_TESTS_MODULE="ivy" -v $(pwd):/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_array_api --ci -k "$ARRAY_API_METHODS_TO_TEST"
+docker run --rm --env IVY_BACKEND="$1" --env ARRAY_API_TESTS_MODULE="ivy" -v $(pwd):/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_array_api --ci -k "$ARRAY_API_TESTS_K_FLAG"
