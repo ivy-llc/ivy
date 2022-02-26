c6966f79ea86da05dcb1975dd13e67e2bf2d6721
Dhruva Bhardwaj <34804673+DhruvaBhardwaj404@users.noreply.github.com>
2022-02-26 14:39:17 +0530

    Update linear_algebra_extension.py

--- ivy/functional/backends/torch/array_api/linear_algebra_extension.py
+++ ivy/functional/backends/torch/array_api/linear_algebra_extension.py
@@ -4,5 +4,6 @@ import torch
 #local
 from typing import Optional
 
-def det(A:torch.Tensor,out:Optional[torch.Tensor]=None) -> torch.Tensor:
+def det(A:torch.Tensor,out:Optional[torch.Tensor]=None) \
+    -> torch.Tensor:
     return torch.linalg.det(A,out)
