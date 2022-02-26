d0a57831e1f3693642f5a7202ed702e93d796fdc
djl11 <daniel.lenton11@imperial.ac.uk>
2022-02-26 14:26:42 +0100

    added logic to test_array_api.sh for handling torch unsupported data types, and changed test to run in doubly verbose mode.

--- test_array_api.sh
+++ test_array_api.sh
@@ -2,5 +2,8 @@
 python3 ivy_tests/write_array_api_tests_k_flag.py
 # shellcheck disable=SC2155
 export ARRAY_API_TESTS_K_FLAG=$(cat ivy_tests/.array_api_tests_k_flag)
+if [ "$1" = "torch" ]; then
+  ARRAY_API_TESTS_K_FLAG="${ARRAY_API_TESTS_K_FLAG} and not (uint16 or uint32 or uint64)"
+fi
 # shellcheck disable=SC2046
-docker run --rm --env IVY_BACKEND="$1" --env ARRAY_API_TESTS_MODULE="ivy" -v $(pwd):/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_array_api --ci -k "$ARRAY_API_TESTS_K_FLAG"
+docker run --rm --env IVY_BACKEND="$1" --env ARRAY_API_TESTS_MODULE="ivy" -v $(pwd):/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_array_api --ci -k "$ARRAY_API_TESTS_K_FLAG" -vv
