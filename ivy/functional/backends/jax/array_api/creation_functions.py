5917a22f3c15f6cd1cd53dc424ce48b1497b6130
Dusan Pantelic <pantelic.dusan@protonmail.com>
2022-02-27 12:21:49 +0100

    numpy and jax issue

--- ivy/functional/backends/jax/array_api/creation_functions.py
+++ ivy/functional/backends/jax/array_api/creation_functions.py
@@ -22,3 +22,16 @@ def zeros(shape: Union[int, Tuple[int], List[int]],
           device: Optional[jaxlib.xla_extension.Device] = None) \
         -> JaxArray:
     return to_dev(jnp.zeros(shape, dtype_from_str(default_dtype(dtype))), default_device(device))
+
+def linspace(start: Union[int, float],
+             stop: Union[int, float],
+             num: int,
+             dtype: Optional[jnp.dtype] = None,
+             device: Optional[jaxlib.xla_extension.Device] = None,
+             endpoint: bool = True) \
+             -> JaxArray:
+    if dtype is None:
+        dtype = jnp.float32
+    return to_dev(jnp.linspace(jnp.double(start), jnp.double(stop), num, dtype=dtype, endpoint=endpoint),
+                  default_device(device))
+
--- ivy/functional/backends/numpy/array_api/creation_functions.py
+++ ivy/functional/backends/numpy/array_api/creation_functions.py
@@ -21,3 +21,17 @@ def ones(shape: Union[int, Tuple[int], List[int]],
         -> np.ndarray:
     dtype = dtype_from_str(default_dtype(dtype))
     return _to_dev(np.ones(shape, dtype), device)
+
+
+def linspace(start: Union[int, float],
+             stop: Union[int, float],
+             num: int,
+             dtype: Optional[np.dtype] = None,
+             device: Optional[str] = None,
+             endpoint: bool = True) \
+             -> np.ndarray:
+
+
+    if dtype is None:
+        dtype = np.float32
+    return _to_dev(np.linspace(np.longdouble(start), np.longdouble(stop), num, dtype=dtype, endpoint=endpoint), device)
