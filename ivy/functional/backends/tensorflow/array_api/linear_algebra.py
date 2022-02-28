74b08b1a9f080b274ba5ca1fb985ea287df5f355
Osuolale Emmanuel <59930570+dmarinere@users.noreply.github.com>
2022-02-28 10:51:50 +0100

    Update linear_algebra.py

--- ivy/functional/backends/tensorflow/array_api/linear_algebra.py
+++ ivy/functional/backends/tensorflow/array_api/linear_algebra.py
@@ -27,7 +27,6 @@ def vector_norm(x: Tensor,
 
     if tn_normalized_vector.shape == tuple():
         return tf.expand_dims(tn_normalized_vector, 0)
-<<<<<<< HEAD
     return tn_normalized_vector
 
 
@@ -36,6 +35,3 @@ def diagonal(x: tf.Tensor,
              axis1: int = -2,
              axis2: int = -1) -> tf.Tensor:
     return tf.experimental.numpy.diagonal(x, offset, axis1=axis1, axis2=axis2)
-=======
-    return tn_normalized_vector
->>>>>>> 73aa9d374 (updated)
