c03f0ada994737042c0ddf7934d49a04a2412271
shraavanchess2000 <avinash@uwm.edu>
2022-03-01 01:50:00 -0600

    Checking for each data type separately

--- ivy/functional/backends/torch/array_api/elementwise_functions.py
+++ ivy/functional/backends/torch/array_api/elementwise_functions.py
@@ -43,15 +43,19 @@ def isnan(x: torch.Tensor)\
 def equal(x1: torch.Tensor, x2: torch.Tensor)\
         -> torch.Tensor:
     x1_bits = ivy.dtype_bits(x1.dtype)
-    if isinstance(x2, (int, float, bool)):
-        return x1 == x2
+    if isinstance(x2, (int)):
+        return x1 == x2.int()
+    if isinstance(x2, (float)):
+        return x1 == x2.float()
+    if isinstance(x2, (bool)):
+        return x1 == x2.bool()
     x2_bits = ivy.dtype_bits(x2.dtype)
     if x1_bits > x2_bits:
         x2 = x2.type(x1.dtype)
     elif x2_bits > x1_bits:
         x1 = x1.type(x2.dtype)
     return x1 == x2
-    
+
 
 def less(x1: torch.Tensor, x2: torch.Tensor):
     if hasattr(x1, 'dtype') and hasattr(x2, 'dtype'):
