482209225841f3b0e1803ea5235888623ea98151
shraavanchess2000 <avinash@uwm.edu>
2022-03-01 00:29:03 -0600

    ivy.dtype_bits

--- ivy/functional/backends/torch/array_api/elementwise_functions.py
+++ ivy/functional/backends/torch/array_api/elementwise_functions.py
@@ -1,6 +1,8 @@
 # global
 import torch
 
+# local
+import ivy
 
 def bitwise_and(x1: torch.Tensor,
                 x2: torch.Tensor)\
@@ -38,13 +40,18 @@ def isnan(x: torch.Tensor)\
     return torch.isnan(x)
 
 
-def equal(x1: torch.Tensor, x2: torch.Tensor):
-    if hasattr(x1, 'dtype') and hasattr(x2, 'dtype'):
-        promoted_type = torch.promote_types(x1.dtype, x2.dtype)
-        x1 = x1.to(promoted_type)
-        x2 = x2.to(promoted_type)
-    return torch.equal(x1, x2)
-
+def equal(x1: torch.Tensor, x2: torch.Tensor)\
+        -> torch.Tensor:
+    x1_bits = ivy.dtype_bits(x1.dtype)
+    if isinstance(x2, (int, float, bool)):
+        return x1 == x2
+    x2_bits = ivy.dtype_bits(x2.dtype)
+    if x1_bits > x2_bits:
+        x2 = x2.type(x1.dtype)
+    elif x2_bits > x1_bits:
+        x1 = x1.type(x2.dtype)
+    return x1 == x2
+    
 
 def less(x1: torch.Tensor, x2: torch.Tensor):
     if hasattr(x1, 'dtype') and hasattr(x2, 'dtype'):
