27429cbea84eb5053e966a3ec8c1a6f770679d54
Mr. Oliver Xue <olivercromwell0214@gmail.com>
2022-03-03 18:50:42 +0000

    create var method for mxnet

---
+++ ivy/functional/backends/mxnet/array_api/statistical_functions.py
@@ -0,0 +1,11 @@
+import mxnet as mx
+from typing import Union, Tuple, Optional, List
+
+from ivy.functional.backends.mxnet import reduce_prod, _flat_array_to_1_dim_array, _1_dim_array_to_flat_array
+
+
+def var(x: mx.ndarray.ndarray.NDArray,
+        axis: Optional[Union[int, Tuple[int], List[int]]] = None,
+        correction: Union[int, float] = 0.0,
+        keepdims: bool = False) -> mx.ndarray.ndarray.NDArray:
+    return mx.nd.array(mx.nd.array(x).asnumpy().var(axis=axis, keepdims=keepdims))
