0a986d8fc731cd4a42a8bb55aaeb6ae28e4ebf07
rishishounak <65448224+rishishounak@users.noreply.github.com>
2022-03-07 19:30:14 +0530

    Update comparison_operators.py

--- ivy/array/array_api/comparison_operators.py
+++ ivy/array/array_api/comparison_operators.py
@@ -2,14 +2,7 @@
 import abc
 
 # ToDo: implement all Array API comparison operators here
-#local
-# import ivy
-# from typing import Union
-# from ivy.framework_handler import current_framework as _cur_framework
 
 
 class ArrayWithArrayAPIComparisonOperators(abc.ABC):
-    # def __eq__(self: ivy.array, y: ivy.array, /) -> ivy.array:
-    #     return _cur_framework(self).__eq__(self, y)
-
     pass
