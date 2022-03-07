86eed67e077d51cafb0c3dcd07fb0045ba8508b8
andy1122 <nihaltahariya95@gmail.com>
2022-03-07 21:29:14 +0530

    Add pyshadowingnames in jax file

--- ivy/functional/backends/jax/array_api/creation_functions.py
+++ ivy/functional/backends/jax/array_api/creation_functions.py
@@ -23,7 +23,7 @@ def zeros(shape: Union[int, Tuple[int], List[int]],
         -> JaxArray:
     return to_dev(jnp.zeros(shape, dtype_from_str(default_dtype(dtype))), default_device(device))
 
-
+# noinspection PyShadowingNames
 def zeros_like(x: JaxArray,
                dtype: Optional[jnp.dtype]=None,
                device: Optional[jaxlib.xla_extension.Device] =None) \
