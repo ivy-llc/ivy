60b4ed0d532dac1be491e11d0cfccb734d44b820
Arvind thorat <98656424+arv0804@users.noreply.github.com>
2022-03-09 00:18:50 +0530

    update linalg.py #115
    general descriptive method of vetor.dot product in linear algebra

--- ivy/array/linalg.py
+++ ivy/array/linalg.py
@@ -1,1 +1,25 @@
+#global 
+import abc 
+import numpy as ivy 
+#creating arry from array
+#function 
 
+#ToDo implement all methods of linear algebra 
+
+Class ArrayWithLinalg(abc.ABC):
+  abc.ABC = ABC
+  
+  def getABC(abc):
+    retun abc.ABC
+    
+    #vecdot 
+    #values are taken for code purpose
+    vector_A = np.array([[2+3j]])
+    vector_B = np.array([[3+4j]])
+    vector_C = np.array([[4+5j]])
+    
+    abc = ivy.dot(vector_A, vector_B, vector_C)
+    print("Dot Product", abc)
+    
+    # for n-dimentional array or tensores it is sum of product over the last axis 
+    # like ivy.dot(a,b,c)[i,j,k,l,m,n] = sum(a[i,j, :] * b[k,l, :] * c[m,n, :]
