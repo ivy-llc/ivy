717cade65e4b776473bbcff102128176e0128767
jameskeane <jameskeane508@gmail.com>
2022-03-08 21:20:12 +0000

    Removed ones_like from ivy/functional/ivy/core/general

--- ivy/functional/ivy/core/general.py
+++ ivy/functional/ivy/core/general.py
@@ -879,26 +879,6 @@ def ones(shape: Iterable[int], dtype: Union[ivy.Dtype, str] = 'float32', dev: iv
     return _cur_framework().ones(shape, dtype, dev)
 
 
-def ones_like(x: Union[ivy.Array, ivy.NativeArray],
-              dtype: Optional[Union[ivy.Dtype, str]] = None,
-              dev: Optional[Union[ivy.Device,str]] = None,)\
-        -> Union[ivy.Array, ivy.NativeArray]:
-    """
-    Returns a new array filled with ones and having the same shape as an input array x.
-
-    :param x: Input array from which to derive the output array shape.
-    :type x: array
-    :param dtype: Output array data type. If dtype is None, the output array data type must be inferred from x.
-    Default: None.
-    :type dtype: data-type string, optional
-    :param dev: device on which to place the created array. If device is None, the output array device must be inferred from x.
-    Default: None.
-    :type dev: ivy.Device, optional
-    :return: An array having the same shape as x and filled with ones.
-    """
-    return _cur_framework(x).ones_like(x, dtype, dev)
-
-
 # noinspection PyShadowingNames
 def full(shape: Union[int, Tuple[int]], fill_value: Union[int, float], dtype: Optional[ivy.Dtype] = None,
          device: Optional[ivy.Device] = None):
