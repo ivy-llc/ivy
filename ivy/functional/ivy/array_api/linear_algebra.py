e483aef1f332ad781aa3791127f732abe4fb9615
Dhruva Bhardwaj <34804673+DhruvaBhardwaj404@users.noreply.github.com>
2022-03-09 16:44:06 +0530

    Update linear_algebra.py

--- ivy/functional/ivy/array_api/linear_algebra.py
+++ ivy/functional/ivy/array_api/linear_algebra.py
@@ -6,10 +6,11 @@ from ivy.framework_handler import current_framework as _cur_framework
 def det(x: ivy.array) \
     -> ivy.array:
     """
-    Computes determinant of array
+    Returns the determinant of a square matrix (or a stack of square matrices) x.
 
-    :param x: Input array.
-    :return : if x is a two-dimensional array, a zero-dimensional array containing the determinant
-              otherwise, a non-zero dimensional array containing the determinant for each square matrix.
+    :param x:  input array having shape (..., M, M) and whose innermost two dimensions form square matrices. Should
+               have a floating-point data type.
+    :return :  if x is a two-dimensional array, a zero-dimensional array containing the determinant; otherwise, a non-zero
+               dimensional array containing the determinant for each square matrix. The returned array must have the same data type as x.
     """
     return _cur_framework(x).det(x)
