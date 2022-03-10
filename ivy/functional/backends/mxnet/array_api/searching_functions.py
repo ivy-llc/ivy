45de12d15df1a66334c5997795640556cc425006
shantam-8 <64306405+shantam-8@users.noreply.github.com>
2022-03-10 12:42:29 +0000

    Update mxnet for argmin
    Cannot test this file currently.

---
+++ ivy/functional/backends/mxnet/array_api/searching_functions.py
@@ -0,0 +1,13 @@
+#global
+import mxnet as mx
+from typing import Optional, Union
+
+
+def argmin(
+    x:mx.ndarray.ndarray.NDArray,
+    axis:Optional[int] = None, 
+    out : Optional[mx.ndarray.ndarray.NDArray] = None,
+    keepdims: bool = False
+    ) -> mx.ndarray.ndarray.NDArray:
+
+    return mx.nd.argmin(x,axis=axis,out=out, keepdims=keepdims) 
