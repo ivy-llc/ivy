b53f1d8914823bffc2475699a4d248cb6f2314a8
AnimeshGurjar <47267354+AnimeshGurjar@users.noreply.github.com>
2022-03-10 11:59:11 +0530

    Updated the function to the standard notation.

--- ivy/functional/ivy/array_api/linear_algebra.py
+++ ivy/functional/ivy/array_api/linear_algebra.py
@@ -100,14 +100,26 @@ def diagonal(x: ivy.Array,
     """
     return _cur_framework(x).diagonal(x, offset, axis1=axis1, axis2=axis2)
 
-def slodget(x):
+def slodget(x: Union[ivy.Array, ivy.NativeArray],) \
+            -> ivy.Array:
     """
-    Compute the sign and natural logarithm of the determinant of an array.
+    Computes the sign and natural logarithm of the determinant of an array.
 
-    :param x: This is a 2D array, and it has to be square
-    :type x: Array
-    :return: slogdet() function returns two values - sign and the natural log of
-     the absolute value of the determinant of matrix x
+    Parameters
+    ----------
+    x:
+        This is a 2D array, and it has to be square
+
+    Return
+    ----------
+    Out:
+
+        This function returns two values -
+            sign:
+            A number representing the sign of the determinant.
+
+            logdet:
+            The natural log of the absolute value of the determinant.
+
     """
-
     return _cur_framework(x).slodget(x)
