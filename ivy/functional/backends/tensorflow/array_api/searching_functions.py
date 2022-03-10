5ff273b6ca7b09793300ad397cf6b55a9a8424bf
shantam-8 <64306405+shantam-8@users.noreply.github.com>
2022-03-10 13:05:04 +0000

    Update searching_functions.py

--- ivy/functional/backends/tensorflow/array_api/searching_functions.py
+++ ivy/functional/backends/tensorflow/array_api/searching_functions.py
@@ -1,18 +1,12 @@
-# global
 import tensorflow as tf
 from typing import Optional
 from tensorflow.python.types.core import Tensor
 
 def argmin(
     x: Tensor,
-    axis: Optional[int] = None,
-    keepdims: bool = False,
-    out: Optional[int] = tf.dtypes.int64,
+    axis: Tensor = None,
+    output_type: Optional[int] = tf.dtypes.int64,
 ) -> Tensor:
 
-    ret = tf.constant(x).numpy().argmin(axis=axis, keepdims=keepdims)
-
-    ret_dtype = ret.dtype
-    ret = tf.convert_to_tensor(ret,dtype=ret_dtype)
-
+    ret = tf.math.argmin(x, axis=axis, output_type=output_type)
     return ret
