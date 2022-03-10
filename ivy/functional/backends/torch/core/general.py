cd52c227bf604f928025b1f339687e81468a593b
Georg Wölflein <georgw7777@gmail.com>
2022-03-10 13:04:41 +0000

    less_equal adhering to Array API Standard (#291)

--- ivy/functional/backends/torch/core/general.py
+++ ivy/functional/backends/torch/core/general.py
@@ -10,7 +10,7 @@ import math as _math
 import torch as _torch
 from operator import mul
 from torch.types import Number
-from functools import reduce as _reduce
+from functools import partial as _partial, reduce as _reduce
 from typing import List, Dict, Optional, Union
 
 # local
@@ -60,18 +60,6 @@ def dtype_bits(dtype_in):
         return 1
     return int(dtype_str.replace('torch.', '').replace('uint', '').replace('int', '').replace('bfloat', '').replace(
         'float', ''))
-
-
-def equal(x1, x2):
-    x1_bits = dtype_bits(x1.dtype)
-    if isinstance(x2, (int, float, bool)):
-        return x1 == x2
-    x2_bits = dtype_bits(x2.dtype)
-    if x1_bits > x2_bits:
-        x2 = x2.type(x1.dtype)
-    elif x2_bits > x1_bits:
-        x1 = x1.type(x2.dtype)
-    return x1 == x2
 
 
 def to_numpy(x) -> np.ndarray:
