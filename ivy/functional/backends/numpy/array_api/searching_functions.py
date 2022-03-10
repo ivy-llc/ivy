327487d689057cbba1a6efa76ce70e7fa3ea9727
shantam-8 <64306405+shantam-8@users.noreply.github.com>
2022-03-10 12:49:16 +0000

    Update searching_functions.py

--- ivy/functional/backends/numpy/array_api/searching_functions.py
+++ ivy/functional/backends/numpy/array_api/searching_functions.py
@@ -2,11 +2,11 @@
 import numpy as np
 from typing import Optional
 
-def argmin(x : np.ndarray,
-            axis: Optional[int] = None, 
-            out: Optional[np.ndarray] = None, 
-            keepdims: bool = False
-            ) -> np.ndarray:
+def argmin(x : np.ndarray, 
+    axis: Optional[int] = None, 
+    out: Optional[np.ndarray] = None, 
+    keepdims: bool = False
+    ) -> np.ndarray:
             
-            ret = np.argmin(x,axis=axis,out=out,keepdims=keepdims)
-            return ret
+    ret = np.argmin(x,axis=axis,out=out,keepdims=keepdims)
+    return ret
