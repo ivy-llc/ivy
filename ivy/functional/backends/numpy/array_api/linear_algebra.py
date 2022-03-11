9403aa2638b1908aa8f655483b721e6e663b4bc2
AnimeshGurjar <47267354+AnimeshGurjar@users.noreply.github.com>
2022-03-11 12:03:20 +0530

    Update linear_algebra.py

--- ivy/functional/backends/numpy/array_api/linear_algebra.py
+++ ivy/functional/backends/numpy/array_api/linear_algebra.py
@@ -45,7 +45,3 @@ def slogdet(x:Union[_ivy.Array,_ivy.NativeArray],full_matrices: bool = True) -> 
     sign, logabsdet = np.linalg.slogdet(x)
     res = results(sign, logabsdet)
     return res
-
-def det(x:Union[_ivy.Array,_ivy.NativeArray],full_matrices: bool = True) -> Union[_ivy.Array, Tuple[_ivy.Array,...]]:
-    d = np.linalg.det(x)
-    return d
