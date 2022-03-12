b43f3c6426ae119a7aef9c3f649bc8f5db6e62e9
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-03-12 14:43:06 +0300

    Update elementwise_functions.py

--- ivy/functional/ivy/array_api/elementwise_functions.py
+++ ivy/functional/ivy/array_api/elementwise_functions.py
@@ -430,7 +430,8 @@ def tanh(x: Union[ivy.Array, ivy.NativeArray]) -> ivy.Array:
     """
     return _cur_framework(x).tanh(x)
 
-  def sinh(x: Union[ivy.Array, ivy.NativeArray]) -> ivy.Array:
+  
+def sinh(x: Union[ivy.Array, ivy.NativeArray]) -> ivy.Array:
     """
     Calculates an implementation-dependent approximation to the hyperbolic sine, having domain ``[-infinity, +infinity]`` and codomain ``[-infinity, +infinity]``, for each element ``x_i`` of the input array ``x``.
     **Special cases**
