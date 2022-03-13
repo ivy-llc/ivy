73bdec44c6b4d1b229107a14bcc75646408ae455
Eng-Abdelrahman-M <abdo.rozelle@gmail.com>
2022-03-13 02:06:11 +0200

    fix dtype prod tensorflow

--- ivy/functional/backends/tensorflow/array_api/statistical_functions.py
+++ ivy/functional/backends/tensorflow/array_api/statistical_functions.py
@@ -14,14 +14,16 @@ def prod(x: _tf.Tensor,
          dtype: Optional[_tf.DType] = None,
          keepdims: bool = False)\
         -> _tf.Tensor:
-
-    if x.dtype in [ _tf.int8 , _tf.int16,_tf.int32]:
-        dtype = _tf.int32
-    elif x.dtype in [_tf.uint8, _tf.uint16,_tf.experimental.numpy.uint32]:
-        dtype = _tf.experimental.numpy.uint32
-    elif x.dtype == _tf.int64: 
-        dtype = _tf.int64
-    elif x.dtype == _tf.uint64 :
-        dtype = _tf.uint64
+    if dtype == None:
+        if x.dtype in [ _tf.int8 , _tf.int16,_tf.int32]:
+            dtype = _tf.int32
+        elif x.dtype == _tf.uint8:
+            dtype = _tf.uint8
+        elif x.dtype in [ _tf.uint16,_tf.experimental.numpy.uint32]:
+            dtype = _tf.experimental.numpy.uint32
+        elif x.dtype == _tf.int64: 
+            dtype = _tf.int64
+        elif x.dtype == _tf.uint64 :
+            dtype = _tf.uint64
         
     return _tf.experimental.numpy.prod(x,axis,dtype,keepdims)
