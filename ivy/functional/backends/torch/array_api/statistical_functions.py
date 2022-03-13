d7a925ea9133c3913bcd4c030a89521b5e66b867
Eng-Abdelrahman-M <abdo.rozelle@gmail.com>
2022-03-13 15:02:35 +0200

    fix prod torch

--- ivy/functional/backends/torch/array_api/statistical_functions.py
+++ ivy/functional/backends/torch/array_api/statistical_functions.py
@@ -2,6 +2,7 @@
 torch_scatter = None
 import torch as _torch
 from typing import Tuple, Union, Optional
+from ivy import dtype_from_str, default_dtype, dev_from_str, default_device
 
 def min(x: _torch.Tensor,
         axis: Union[int, Tuple[int]] = None,
@@ -22,7 +23,7 @@ def prod(x: _torch.Tensor,
         if x.dtype == _torch.int8:
             dtype = _torch.int32
         elif x.dtype == _torch.uint8:
-            dtype = _torch.uint8
+            dtype = dtype_from_str('uint32')
         elif x.dtype in [_torch.int64,_torch.int32]: 
             dtype = _torch.int64
 
@@ -32,6 +33,6 @@ def prod(x: _torch.Tensor,
         if len(axis) ==0:
             axis = x.dim()
         else:
-            return _torch.prod(_torch.Tensor([_torch.prod(input=x,dim=i,dtype=dtype,keepdim=keepdims) for i in axis]),dtype=dtype)
+            return _torch.prod(_torch.Tensor([_torch.prod(input=x,dim=i,dtype=dtype_from_str(default_dtype(dtype)),keepdim=keepdims) for i in axis]),dtype=dtype)
 
-    return _torch.prod(input=x,dim=axis,dtype=dtype,keepdim=keepdims)
+    return _torch.prod(input=x,dim=axis,dtype=dtype_from_str(default_dtype(dtype)),keepdim=keepdims)
