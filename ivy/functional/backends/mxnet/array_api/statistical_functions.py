25186301d60894d04507a867e2c3a33a3cd3eb2e
Kevin Xue <olivercromwell0214@gmail.com>
2022-03-13 01:01:10 +0000

    delete List[int]

--- ivy/functional/backends/mxnet/array_api/statistical_functions.py
+++ ivy/functional/backends/mxnet/array_api/statistical_functions.py
@@ -7,5 +7,6 @@ from ivy.functional.backends.mxnet import reduce_prod, _flat_array_to_1_dim_arra
 def var(x: mx.ndarray.ndarray.NDArray,
         axis: Optional[Union[int, Tuple[int]]] = None,
         correction: Union[int, float] = 0.0,
-        keepdims: bool = False) -> mx.ndarray.ndarray.NDArray:
+        keepdims: bool = False) \
+        -> mx.ndarray.ndarray.NDArray:
     return mx.nd.array(mx.nd.array(x).asnumpy().var(axis=axis, keepdims=keepdims))
