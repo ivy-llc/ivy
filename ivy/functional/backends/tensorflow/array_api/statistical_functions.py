efbb268fae6a9a6d1e34729678475b04fdc1d35c
Kevin Xue <olivercromwell0214@gmail.com>
2022-03-13 00:58:17 +0000

    delete List[int]

--- ivy/functional/backends/tensorflow/array_api/statistical_functions.py
+++ ivy/functional/backends/tensorflow/array_api/statistical_functions.py
@@ -5,15 +5,9 @@ from tensorflow.python.types.core import Tensor
 
 
 def var(x: Tensor,
-        axis: Optional[Union[int, Tuple[int], List[int]]] = None,
+        axis: Optional[Union[int, Tuple[int]]] = None,
         correction: Union[int, float] = 0.0,
         keepdims: bool = False) -> Tensor:
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = tuple(range(num_dims))
-    elif isinstance(axis, list):
-        axis = tuple(axis)
-
     m = tf.reduce_mean(x, axis=axis, keepdims=True)
     return tf.reduce_mean(tf.square(x - m), axis=axis, keepdims=keepdims)
 
