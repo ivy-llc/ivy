de94e1504d41efb19f44c24fdd051a6ab1227d21
Kevin Xue <olivercromwell0214@gmail.com>
2022-03-13 01:04:17 +0000

    delete List[int]

--- ivy/functional/backends/torch/array_api/statistical_functions.py
+++ ivy/functional/backends/torch/array_api/statistical_functions.py
@@ -3,18 +3,17 @@ from typing import Union, Optional, Tuple, List
 
 
 def var(x: torch.Tensor,
-        axis: Optional[Union[int, Tuple[int], List[int]]] = None,
+        axis: Optional[Union[int, Tuple[int]]] = None,
         correction: Union[int, float] = 0.0,
-        keepdims: bool = False) -> torch.Tensor:
-
+        keepdims: bool = False) \
+        -> torch.Tensor:
     if axis is None:
         num_dims = len(x.shape)
-        axis = list(range(num_dims))
+        axis = tuple(range(num_dims))
     if isinstance(axis, int):
         return torch.var(x, dim=axis, keepdim=keepdims)
     dims = len(x.shape)
-    axis = [i % dims for i in axis]
-    axis.sort()
+    axis = tuple([i % dims for i in axis])
     for i, a in enumerate(axis):
         x = torch.var(x, dim=a if keepdims else a - i, keepdim=keepdims)
     return x
