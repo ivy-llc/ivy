b0926d2a39f550a9e963346cc711609971d2ad45
Kevin Xue <olivercromwell0214@gmail.com>
2022-03-13 01:01:45 +0000

    delete List[int]

--- ivy/functional/backends/numpy/array_api/statistical_functions.py
+++ ivy/functional/backends/numpy/array_api/statistical_functions.py
@@ -6,7 +6,8 @@ from typing import Union, Tuple, Optional, List
 def var(x: np.ndarray,
         axis: Optional[Union[int, Tuple[int]]] = None,
         correction: Union[int, float] = 0.0,
-        keepdims: bool = False) -> np.ndarray:
+        keepdims: bool = False) \
+        -> np.ndarray:
     return np.var(npa.asarray(x), axis=axis, keepdims=keepdims)
 
 
