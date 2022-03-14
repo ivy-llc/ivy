70d315700591d600b17450cf1b8981e4fed2d011
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-03-14 17:22:36 +0800

    add stack adhere to array API

--- ivy/functional/ivy/array_api/manipulation_functions.py
+++ ivy/functional/ivy/array_api/manipulation_functions.py
@@ -26,3 +26,26 @@ def flip(x: Union[ivy.Array, ivy.NativeArray],
     """
 
     return _cur_framework(x).flip(x, axis)
+
+def stack(arrays: Union[ivy.Array, ivy.NativeArray, Tuple[ivy.Array], Tuple[ivy.NativeArray], List[ivy.Array], List[ivy.NativeArray]],
+          axis: Optional[int] = None) \
+          -> ivy.Array:
+    """
+    Joins a sequence of arrays along a new axis.
+
+    Parameters
+    ----------
+    arrays
+        input arrays to join. Each array must have the same shape.
+    axis:
+        axis along which the arrays will be joined. Providing an ``axis`` specifies the index of the new axis in the dimensions of the result. For example, if ``axis`` is ``0``, the new axis will be the first dimension and the output array will have shape ``(N, A, B, C)``; if ``axis`` is ``1``, the new axis will be the second dimension and the output array will have shape ``(A, N, B, C)``; and, if ``axis`` is ``-1``, the new axis will be the last dimension and the output array will have shape ``(A, B, C, N)``. A valid ``axis`` must be on the interval ``[-N, N)``, where ``N`` is the rank (number of dimensions) of ``x``. If provided an ``axis`` outside of the required interval, the function must raise an exception. Default: ``0``.
+
+    Returns
+    --------
+    out: array
+        an output array having rank ``N+1``, where ``N`` is the rank (number of dimensions) of ``x``. If the input arrays have different data types, normal :ref:`type-promotion` must apply. If the input arrays have the same data type, the output array must have the same data type as the input arrays.
+        .. note::
+           This specification leaves type promotion between data type families (i.e., ``intxx`` and ``floatxx``) unspecified.
+    """
+
+    return _cur_framework(arrays).stack(arrays, axis)
