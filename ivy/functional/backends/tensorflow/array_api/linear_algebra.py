f02ec0c8ed90c441d863e8fc967d7e396a3293b7
sherry30 <65318415+sherry30@users.noreply.github.com>
2022-03-14 18:09:18 +0500

    matmul adhering to Array API Standard (#495)

--- ivy/functional/backends/tensorflow/array_api/linear_algebra.py
+++ ivy/functional/backends/tensorflow/array_api/linear_algebra.py
@@ -51,8 +51,66 @@ def diagonal(x: tf.Tensor,
              axis2: int = -1) -> tf.Tensor:
     return tf.experimental.numpy.diagonal(x, offset, axis1=axis1, axis2=axis2)
 
+
+def matmul(x1: tf.Tensor,
+           x2: tf.Tensor) -> tf.Tensor:
+    dtype_from = tf.experimental.numpy.promote_types(x1.dtype.as_numpy_dtype, x2.dtype.as_numpy_dtype)
+    dtype_from = tf.as_dtype(dtype_from)
+    if dtype_from.is_unsigned or dtype_from==tf.int8 or dtype_from==tf.int16:
+        x1 = tf.cast(x1, tf.int64)
+        x2 = tf.cast(x2, tf.int64)
+    if x1.dtype != x2.dtype:
+        x1 = tf.cast(x1, dtype_from)
+        x2 = tf.cast(x2, dtype_from)
+
+    if (x1.shape == () or x2.shape == ()
+            or (len(x1.shape) == len(x2.shape) == 1 and x1.shape != x2.shape)
+            or (len(x1.shape) == len(x2.shape) == 1 and x1.shape != x2.shape)
+            or (len(x1.shape) == 1 and len(x2.shape) >= 2 and x1.shape[0] != x2.shape[-2])
+            or (len(x2.shape) == 1 and len(x1.shape) >= 2 and x2.shape[0] != x1.shape[-1])
+            or (len(x1.shape) >= 2 and len(x2.shape) >= 2 and x1.shape[-1] != x2.shape[-2])):
+        raise Exception('Error,shapes not compatible')
+
+    if len(x1.shape) == len(x2.shape) == 1:
+        if x1.shape == 0:
+            ret = tf.constant(0)
+        else:
+
+            ret = tf.math.multiply(x1, x2)[0]
+        ret = tf.cast(ret, dtype=dtype_from)
+        return ret
+
+    x1_padded = False
+    x1_padded_2 = False
+    x2_padded = False
+
+    if len(x1.shape) == 1:
+        if len(x2.shape) == 2:
+            x1_padded_2 = True
+        elif len(x2.shape) > 2:
+            x1_padded = True
+        x1 = tf.expand_dims(x1, axis=0)
+
+    elif len(x2.shape) == 1 and len(x1.shape) >= 2:
+        x2 = tf.expand_dims(x2, axis=1)
+        x2_padded = True
+
+    ret = tf.matmul(x1, x2)
+
+    ret = tf.cast(ret, dtype=dtype_from)
+    if x1_padded_2:
+        return ret[0]
+    elif x1_padded:
+        return tf.squeeze(ret, axis=-2)
+    elif x2_padded:
+        return tf.squeeze(ret, axis=-1)
+
+    return ret
+
+  
 def slogdet(x:Union[_ivy.Array,_ivy.NativeArray],full_matrices: bool = True) -> Union[_ivy.Array, Tuple[_ivy.Array,...]]:
     results = namedtuple("slogdet", "sign logabsdet")
     sign, logabsdet = tf.linalg.slogdet(x)
     res = results(sign, logabsdet)
     return res
+
