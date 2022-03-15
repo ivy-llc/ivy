4b6b0aebdde3e1a202bc41156ebe4aa37d9f7e1e
djl11 <daniel.lenton11@imperial.ac.uk>
2022-03-15 15:03:30 +0100

    refactored sub-module design for functional API, with removal of dedicated array_api submodule, with this now being the default. The old submodule has been marked as such until all of these methods are also brought into the new files which match the Array API groupings.

---
+++ ivy/functional/backends/jax/creation.py
@@ -0,0 +1,50 @@
+# global
+import jaxlib
+import jax.numpy as jnp
+from typing import Union, Optional, Tuple, List
+
+# local
+from ivy import dtype_from_str
+from ivy.functional.backends.jax import JaxArray
+from ivy.functional.backends.jax.old.device import to_dev
+from ivy.functional.ivy.old import default_device, default_dtype
+from jaxlib.xla_extension import Device, DeviceArray
+
+
+def ones(shape: Union[int, Tuple[int], List[int]],
+         dtype: Optional[jnp.dtype] = None,
+         device: Optional[jaxlib.xla_extension.Device] = None) \
+        -> JaxArray:
+    return to_dev(jnp.ones(shape, dtype_from_str(default_dtype(dtype))), default_device(device))
+
+
+def zeros(shape: Union[int, Tuple[int], List[int]],
+          dtype: Optional[jnp.dtype] = None,
+          device: Optional[jaxlib.xla_extension.Device] = None) \
+        -> JaxArray:
+    return to_dev(jnp.zeros(shape, dtype_from_str(default_dtype(dtype))), default_device(device))
+
+
+def ones_like(x : JaxArray,
+              dtype: Optional[Union[jnp.dtype, str]]=None,
+              dev: Optional[Union[Device, str]] = None)\
+        -> DeviceArray:
+
+    if dtype and str:
+        dtype = jnp.dtype(dtype)
+    else:
+        dtype = x.dtype
+    return to_dev(jnp.ones_like(x, dtype=dtype), default_device(dev))
+
+
+def tril(x: JaxArray,
+         k: int = 0) \
+         -> JaxArray:
+    return jnp.tril(x, k)
+
+
+def empty(shape: Union[int, Tuple[int], List[int]],
+          dtype: Optional[jnp.dtype] = None,
+          device: Optional[jaxlib.xla_extension.Device] = None) \
+        -> JaxArray:
+    return to_dev(jnp.empty(shape, dtype_from_str(default_dtype(dtype))), default_device(device))
---
+++ ivy/functional/backends/mxnet/old/logic.py
@@ -0,0 +1,19 @@
+"""
+Collection of MXNet logic functions, wrapped to fit Ivy syntax and signature.
+"""
+
+# global
+import mxnet as _mx
+
+# local
+from ivy.functional.backends.mxnet.old.general import _handle_flat_arrays_in_out
+
+
+@_handle_flat_arrays_in_out
+def logical_and(x1, x2):
+    return _mx.nd.logical_and(x1, x2)
+
+
+@_handle_flat_arrays_in_out
+def logical_or(x1, x2):
+    return _mx.nd.logical_or(x1, x2)
