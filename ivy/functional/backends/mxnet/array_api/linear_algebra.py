34cbc200a4bd23dac3d1e5b213c7deecaec199a3
JudeDavis1 <51873868+JudeDavis1@users.noreply.github.com>
2022-03-15 08:37:31 +0000

    trace adhering to Array API Standard (#503)

--- ivy/functional/backends/mxnet/array_api/linear_algebra.py
+++ ivy/functional/backends/mxnet/array_api/linear_algebra.py
@@ -1,8 +1,10 @@
 # global
 import mxnet as mx
 import numpy as _np
-from typing  import Union, Optional, Tuple, Literal
+
 from collections import namedtuple
+from mxnet.ndarray.ndarray import NDArray
+from typing import Union, Optional, Tuple, Literal
 
 
 
@@ -10,16 +12,17 @@ from collections import namedtuple
 from ivy import inf
 import ivy as _ivy
 
-def vector_norm(x: mx.ndarray.ndarray.NDArray,
+
+
+def vector_norm(x: NDArray,
                 p: Union[int, float, Literal[inf, - inf]] = 2,
                 axis: Optional[Union[int, Tuple[int]]] = None,
-                keepdims: bool = False)\
-                    -> mx.ndarray.ndarray.NDArray:
+                keepdims: bool = False) -> NDArray:
                 
     return mx.np.linalg.norm(x,p,axis,keepdims)
 
 # noinspection PyPep8Naming
-def svd(x:mx.ndarray.ndarray.NDArray,full_matrices: bool = True) -> Union[mx.ndarray.ndarray.NDArray, Tuple[mx.ndarray.ndarray.NDArray,...]]:
+def svd(x: NDArray, full_matrices: bool = True) -> Union[NDArray, Tuple[NDArray,...]]:
     results=namedtuple("svd", "U S Vh")
     U, D, VT=_np.linalg.svd(x, full_matrices=full_matrices)
     res=results(U, D, VT)
@@ -28,14 +31,21 @@ def svd(x:mx.ndarray.ndarray.NDArray,full_matrices: bool = True) -> Union[mx.nda
     return mx.np.linalg.norm(x, p, axis, keepdims)
 
 
-def diagonal(x: mx.nd.NDArray,
+def diagonal(x: NDArray,
              offset: int = 0,
              axis1: int = -2,
-             axis2: int = -1) -> mx.nd.NDArray:
-    return mx.nd.diag(x, k=offset, axis1=axis1, axis2=axis2)
+             axis2: int = -1) -> NDArray:
+    return diag(x, k=offset, axis1=axis1, axis2=axis2)
 
-def slogdet(x:Union[_ivy.Array,_ivy.NativeArray],full_matrices: bool = True) -> Union[_ivy.Array, Tuple[_ivy.Array,...]]:
+def slogdet(x: Union[_ivy.Array,_ivy.NativeArray],
+            full_matrices: bool = True) -> Union[_ivy.Array, Tuple[_ivy.Array,...]]:
     results = namedtuple("slogdet", "sign logabsdet")
     sign, logabsdet = mx.linalg.slogdet(x)
     res = results(sign, logabsdet)
+    
     return res
+
+def trace(x: NDArray,
+          offset: int = 0)\
+              -> mx.np.ndarray:
+    return mx.np.trace(x, offset=offset)
