5ddca2af18d398ef7da68ac7d2725a8fd1902047
Arvind thorat <98656424+arv0804@users.noreply.github.com>
2022-03-15 17:43:09 +0530

    __neg__
    #509

--- ivy/array/array_api/arithmetic_operators.py
+++ ivy/array/array_api/arithmetic_operators.py
@@ -5,17 +5,26 @@ import abc
 
 
 class ArrayWithArrayAPIArithmeticOperators(abc.ABC):
-    def __neg__(self):
+    def __neg__(x: Union[ivy.Array, ivy.NativeArray],
+            axes: Union[int, Tuple[int], List[int]],
+            dtype: Optional[Union[ivy.Dtype, str]] = None,
+            dev: Optional[Union[ivy.Dev, str]] = None) \
+        -> ivy.Array:
         
-          ''' Return element wise negative '''
-       
-    def call(*args, **kargs):
+    
+    '''returns elementwise negative'''
+    
+         def call(*args, **kargs):
             
-       '''we can use __call__ to create a new instance
-         of this class, with the help of any constructor'''
-           
-        arr = operator.neg(__values + from_object(self)
-                               retuns -self(arr, *args, **kargs, copy=false)
+        
+     '''we can use __call__ to create a new instance
+        of this class, with the help of any constructor'''
+
+    
+            returns _cur_framework(x).__neg__(operator.neg,*args, **kargs)
+        
+
+    
                                
            
             
