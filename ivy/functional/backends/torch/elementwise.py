532edb69edc5f3a46391d9b1a136b40893858105
djl11 <daniel.lenton11@imperial.ac.uk>
2022-03-15 15:11:59 +0100

    small import bug fix for pytorch.

--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -15,10 +15,10 @@ def isfinite(x: Tensor)\
 
 def _cast_for_binary_op(x1: Tensor, x2: Tensor)\
         -> typing.Tuple[typing.Union[Tensor, int, float, bool], typing.Union[Tensor, int, float, bool]]:
-    x1_bits = ivy.functional.backends.torch.core.general.dtype_bits(x1.dtype)
+    x1_bits = ivy.functional.backends.torch.old.general.dtype_bits(x1.dtype)
     if isinstance(x2, (int, float, bool)):
         return x1, x2
-    x2_bits = ivy.functional.backends.torch.core.general.dtype_bits(x2.dtype)
+    x2_bits = ivy.functional.backends.torch.old.general.dtype_bits(x2.dtype)
     if x1_bits > x2_bits:
         x2 = x2.type(x1.dtype)
     elif x2_bits > x1_bits:
