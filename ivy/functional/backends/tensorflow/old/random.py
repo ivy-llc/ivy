7570c66e310c2eb95c489c17c5dca3365cd9b130
djl11 <daniel.lenton11@imperial.ac.uk>
2022-03-15 15:03:30 +0100

    refactored sub-module design for functional API, with removal of dedicated array_api submodule, with this now being the default. The old submodule has been marked as such until all of these methods are also brought into the new files which match the Array API groupings.

---
+++ ivy/functional/backends/tensorflow/old/random.py
@@ -0,0 +1,40 @@
+"""
+Collection of TensorFlow random functions, wrapped to fit Ivy syntax and signature.
+"""
+
+# global
+import tensorflow as _tf
+
+# local
+from ivy.functional.ivy.old import default_device
+
+
+def random_uniform(low=0., high=1., shape=None, dev=None):
+    with _tf.device(default_device(dev)):
+        return _tf.random.uniform(shape if shape else (), low, high)
+
+
+def random_normal(mean=0., std=1., shape=None, dev=None):
+    dev = default_device(dev)
+    with _tf.device('/' + dev.upper()):
+        return _tf.random.normal(shape if shape else (), mean, std)
+
+
+def multinomial(population_size, num_samples, batch_size, probs=None, replace=True, dev=None):
+    if not replace:
+        raise Exception('TensorFlow does not support multinomial without replacement')
+    dev = default_device(dev)
+    with _tf.device('/' + dev.upper()):
+        if probs is None:
+            probs = _tf.ones((batch_size, population_size,)) / population_size
+        return _tf.random.categorical(_tf.math.log(probs), num_samples)
+
+
+def randint(low, high, shape, dev=None):
+    dev = default_device(dev)
+    with _tf.device('/' + dev.upper()):
+        return _tf.random.uniform(shape=shape, minval=low, maxval=high, dtype=_tf.int32)
+
+
+seed = lambda seed_value=0: _tf.random.set_seed(seed_value)
+shuffle = lambda x: _tf.random.shuffle(x)
---
+++ ivy/functional/backends/torch/creation.py
@@ -0,0 +1,62 @@
+# global
+import torch
+from torch import Tensor
+from typing import Union, Tuple, Optional, Dict
+
+# local
+from ivy import dtype_from_str, default_dtype, dev_from_str, default_device
+from ivy.functional.backends.torch.old.device import _callable_dev
+
+
+def zeros(shape: Union[int, Tuple[int]],
+          dtype: Optional[torch.dtype] = None,
+          device: Optional[torch.device] = None) \
+        -> Tensor:
+    return torch.zeros(shape, dtype=dtype_from_str(default_dtype(dtype)), device=dev_from_str(default_device(device)))
+
+
+def ones(shape: Union[int, Tuple[int]],
+         dtype: Optional[torch.dtype] = None,
+         device: Optional[Union[torch.device, str]] = None) \
+        -> torch.Tensor:
+    dtype_val: torch.dtype = dtype_from_str(dtype)
+    dev = default_device(device)
+    return torch.ones(shape, dtype=dtype_val, device=dev_from_str(dev))
+
+
+def ones_like(x : torch.Tensor,
+              dtype: Optional[Union[torch.dtype, str]] = None,
+              dev: Optional[Union[torch.device, str]] = None) \
+        -> torch.Tensor:
+    if dev is None:
+        dev = _callable_dev(x)
+    if dtype is not None and dtype is str:
+        type_dict: Dict[str, torch.dtype] = {'int8': torch.int8,
+            'int16': torch.int16,
+            'int32': torch.int32,
+            'int64': torch.int64,
+            'uint8': torch.uint8,
+            'bfloat16': torch.bfloat16,
+            'float16': torch.float16,
+            'float32': torch.float32,
+            'float64': torch.float64,
+            'bool': torch.bool}
+        return torch.ones_like(x, dtype=type_dict[dtype], device=dev_from_str(dev))
+    else:
+        return torch.ones_like(x, dtype= dtype, device=dev_from_str(dev))
+
+    return torch.ones_like(x, device=dev_from_str(dev))
+
+  
+def tril(x: torch.Tensor,
+         k: int = 0) \
+         -> torch.Tensor:
+    return torch.tril(x, diagonal=k)
+
+
+def empty(shape: Union[int, Tuple[int]],
+          dtype: Optional[torch.dtype] = None,
+          device: Optional[torch.device] = None) \
+        -> Tensor:
+    return torch.empty(shape, dtype=dtype_from_str(default_dtype(dtype)), device=dev_from_str(default_device(device)))
+
