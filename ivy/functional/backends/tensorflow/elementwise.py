90ab5882a453d48382c70fdf7e1875b3c97832ee
djl11 <daniel.lenton11@imperial.ac.uk>
2022-03-15 15:14:21 +0100

    small import bug fix for tensorflow.

--- ivy/functional/backends/tensorflow/elementwise.py
+++ ivy/functional/backends/tensorflow/elementwise.py
@@ -36,10 +36,10 @@ def _tf_cast(x: Tensor, dtype: tf.dtypes.DType) -> Tensor:
 
 def _cast_for_binary_op(x1: Tensor, x2: Tensor)\
         -> typing.Tuple[typing.Union[Tensor, int, float, bool], typing.Union[Tensor, int, float, bool]]:
-    x1_bits = ivy.functional.backends.tensorflow.core.general.dtype_bits(x1.dtype)
+    x1_bits = ivy.functional.backends.tensorflow.old.general.dtype_bits(x1.dtype)
     if isinstance(x2, (int, float, bool)):
         return x1, x2
-    x2_bits = ivy.functional.backends.tensorflow.core.general.dtype_bits(x2.dtype)
+    x2_bits = ivy.functional.backends.tensorflow.old.general.dtype_bits(x2.dtype)
     if x1_bits > x2_bits:
         x2 = _tf_cast(x2, x1.dtype)
     elif x2_bits > x1_bits:
