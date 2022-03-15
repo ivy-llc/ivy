133a0ad89aed5d8e4589dab9b8f698bb1bb7a4a8
djl11 <daniel.lenton11@imperial.ac.uk>
2022-03-15 15:03:30 +0100

    refactored sub-module design for functional API, with removal of dedicated array_api submodule, with this now being the default. The old submodule has been marked as such until all of these methods are also brought into the new files which match the Array API groupings.

--- ivy/functional/backends/jax/nn/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import activations
-from .activations import *
-from . import converters
-from .converters import *
-from . import layers
-from .layers import *
--- ivy/functional/backends/numpy/__init__.py
+++ ivy/functional/backends/numpy/__init__.py
@@ -53,8 +53,32 @@ backend = 'numpy'
 
 
 # local sub-modules
-from . import array_api
-from .array_api import *
-from .core import *
-from . import nn
-from .nn import *
+from . import activations
+from .activations import *
+from . import constants
+from .constants import *
+from . import creation
+from .creation import *
+from . import data_type
+from .data_type import *
+from . import elementwise
+from .elementwise import *
+from . import layers
+from .layers import *
+from . import linear_algebra
+from .linear_algebra import *
+from . import manipulation
+from .manipulation import *
+from . import searching
+from .searching import *
+from . import set
+from .set import *
+from . import sorting
+from .sorting import *
+from . import statistical
+from .statistical import *
+from . import utility
+from .utility import *
+
+from . import old
+from .old import *
