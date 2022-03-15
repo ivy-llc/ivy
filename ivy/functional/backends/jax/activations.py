5a5d43a8226cbb030abfb53f8d54826beea0564b
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-15 21:39:55 +0000

    Consistency of imports in the backend.
    made all import public

--- ivy/functional/backends/jax/activations.py
+++ ivy/functional/backends/jax/activations.py
@@ -3,19 +3,19 @@ Collection of Jax activation functions, wrapped to fit Ivy syntax and signature.
 """
 
 # global
-import jax as _jax
-import jax.numpy as _jnp
+import jax
+import jax.numpy as jnp
 
-relu = lambda x: _jnp.maximum(x, 0)
-leaky_relu = lambda x, alpha=0.2: _jnp.where(x > 0, x, x * alpha)
-gelu = _jax.nn.gelu
-tanh = _jnp.tanh
-sigmoid = lambda x: 1 / (1 + _jnp.exp(-x))
+relu = lambda x: jnp.maximum(x, 0)
+leaky_relu = lambda x, alpha=0.2: jnp.where(x > 0, x, x * alpha)
+gelu = jax.nn.gelu
+tanh = jnp.tanh
+sigmoid = lambda x: 1 / (1 + jnp.exp(-x))
 
 
 def softmax(x, axis=-1):
-    exp_x = _jnp.exp(x)
-    return exp_x / _jnp.sum(exp_x, axis, keepdims=True)
+    exp_x = jnp.exp(x)
+    return exp_x / jnp.sum(exp_x, axis, keepdims=True)
 
 
-softplus = lambda x: _jnp.log(_jnp.exp(x) + 1)
+softplus = lambda x: jnp.log(jnp.exp(x) + 1)
--- ivy/functional/backends/numpy/layers.py
+++ ivy/functional/backends/numpy/layers.py
@@ -3,7 +3,7 @@ Collection of Numpy network layers, wrapped to fit Ivy syntax and signature.
 """
 
 # global
-import numpy as _np
+import numpy as np
 
 
 def conv1d(*_):
@@ -18,9 +18,9 @@ def conv2d(x, filters, strides, padding, data_format='NHWC', dilations=1):
     filter_shape = filters.shape[0:2]
     filter_shape = list(filter_shape)
     if data_format == 'NCHW':
-        x = _np.transpose(x, (0, 2, 3, 1))
+        x = np.transpose(x, (0, 2, 3, 1))
     if padding == 'SAME':
-        x = _np.pad(x, [[0, 0], [filter_shape[0]//2]*2, [filter_shape[1]//2]*2, [0, 0]])
+        x = np.pad(x, [[0, 0], [filter_shape[0]//2]*2, [filter_shape[1]//2]*2, [0, 0]])
     x_shape = x.shape
     input_dim = filters.shape[-2]
     output_dim = filters.shape[-1]
@@ -30,15 +30,15 @@ def conv2d(x, filters, strides, padding, data_format='NHWC', dilations=1):
     # ToDo: add non-unit stride support
     new_strides = x.strides[0:1] + x.strides[1:3] + x.strides[1:3] + x.strides[-1:]
     # B x OH x OW x KH x KW x I
-    sub_matrices = _np.lib.stride_tricks.as_strided(x, new_shape, new_strides, writeable=False)
+    sub_matrices = np.lib.stride_tricks.as_strided(x, new_shape, new_strides, writeable=False)
     # B x OH x OW x KH x KW x I x O
-    sub_matrices_w_output_dim = _np.tile(_np.expand_dims(sub_matrices, -1), [1]*6 + [output_dim])
+    sub_matrices_w_output_dim = np.tile(np.expand_dims(sub_matrices, -1), [1]*6 + [output_dim])
     # B x OH x OW x KH x KW x I x O
     mult = sub_matrices_w_output_dim * filters.reshape([1]*3 + filter_shape + [input_dim, output_dim])
     # B x OH x OW x O
-    res = _np.sum(mult, (3, 4, 5))
+    res = np.sum(mult, (3, 4, 5))
     if data_format == 'NCHW':
-        return _np.transpose(res, (0, 3, 1, 2))
+        return np.transpose(res, (0, 3, 1, 2))
     return res
 
 
