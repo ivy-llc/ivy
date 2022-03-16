56074cd3f14939b2a006bea26bff9ad841c1a9e6
Christoforos00 <christrakas7@gmail.com>
2022-03-16 09:48:24 +0200

    not_equal function adhering to Array API Standard

--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -132,6 +132,11 @@ def sin(x: torch.Tensor)\
 def negative(x: torch.Tensor) -> torch.Tensor:
     return torch.neg(x)
 
+def not_equal(x1: Tensor, x2: Tensor)\
+        -> Tensor:
+    x1, x2 = _cast_for_binary_op(x1, x2)
+    return torch.not_equal(x1, x2)
+
 
 def tanh(x: torch.Tensor) -> torch.Tensor:
     return torch.tanh(x)
