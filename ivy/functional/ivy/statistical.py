bf7a523fe35d4006800cdb067bcc393810ae9fa2
Kevin Xue <zekuixue@gmail.com>
2022-03-16 21:58:02 +0000

    var

--- ivy/functional/ivy/statistical.py
+++ ivy/functional/ivy/statistical.py
@@ -56,5 +56,20 @@ def max(x: Union[ivy.Array, ivy.NativeArray],
     return _cur_framework.max(x, axis, keepdims)
 
 
+def var(x: Union[ivy.Array, ivy.NativeArray],
+        axis: Optional[Union[int, Tuple[int]]] = None,
+        correction: Union[int, float] = 0.0,
+        keepdims: bool = False) \
+        -> ivy.Array:
+    """
+    Calculates the variance of the input array x.
+    :param x: input array
+    :param axis: axis or axes along which variances must be computed. By default, the variance must be computed over the entire array
+    :param correction: degrees of freedom adjustment
+    :param keepdims: Default: False
+    :return: The returned array must have the same data type as x.
+    """
+    return _cur_framework(x).var(x, axis, correction, keepdims)
+
 # Extra #
 # ------#
