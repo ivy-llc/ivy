d8bf5bd8fa6dfb1c9afbf70680bc2c988d1acd37
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-16 22:16:26 +0200

    Refactoring Reductions
    reduce_prod, reduce_mean, reduce_var

--- ivy/functional/backends/torch/old/reductions.py
+++ ivy/functional/backends/torch/old/reductions.py
@@ -14,33 +14,5 @@ def reduce_sum(x, axis: Optional[List[int]] = None, keepdims: bool = False):
     return _torch.sum(x, dim=axis, keepdim=keepdims)
 
 
-def reduce_prod(x, axis: Optional[List[int]] = None, keepdims: bool = False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = list(range(num_dims))
-    if isinstance(axis, int):
-        return _torch.prod(x, dim=axis, keepdim=keepdims)
-    dims = len(x.shape)
-    axis = [i%dims for i in axis]
-    axis.sort()
-    for i, a in enumerate(axis):
-        x = _torch.prod(x, dim=a if keepdims else a - i, keepdim=keepdims)
-    return x
-
-
-def reduce_mean(x, axis: Optional[List[int]] = None, keepdims: bool = False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = list(range(num_dims))
-    return _torch.mean(x, dim=axis, keepdim=keepdims)
-
-
-def reduce_var(x, axis: Optional[List[int]] = None, keepdims: bool = False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = list(range(num_dims))
-    return _torch.var(x, dim=axis, unbiased=False, keepdim=keepdims)
-
-
 def einsum(equation, *operands):
     return _torch.einsum(equation, *operands)
--- ivy/functional/ivy/old/reductions.py
+++ ivy/functional/ivy/old/reductions.py
@@ -26,64 +26,6 @@ def reduce_sum(x, axis=None, keepdims=False):
     return _cur_framework(x).reduce_sum(x, axis, keepdims)
 
 
-def reduce_prod(x, axis=None, keepdims=False):
-    """
-    Multiplies array elements along a given axis.
-
-    :param x: Elements to multiply.
-    :type x: array
-    :param axis: Axis or axes along which a multiplication is performed. The default, axis=None, will multiply all of
-                 the elements of the input array. If axis is negative it counts from the last to the first axis.
-                 If axis is a tuple of ints, a multiplication is performed on all of the axes specified in the tuple
-                 instead of a single axis or all the axes as before.
-    :type axis: int or sequence of ints
-    :param keepdims: If this is set to True, the axes which are reduced are left in the result as dimensions with size
-                        one. With this option, the result will broadcast correctly against the input array.
-    :type keepdims: bool, optional
-    :return: The array with multiplications computed.
-    """
-    return _cur_framework(x).reduce_prod(x, axis, keepdims)
-
-
-def reduce_mean(x, axis=None, keepdims=False):
-    """
-    Computes the arithmetic mean along a given axis.
-    Returns the average of the array elements. The average is taken over the flattened array by default, otherwise over
-    the specified axis.
-
-    :param x: Array containing numbers whose mean is desired.
-    :type x: array
-    :param axis: Axis or axes along which the means are computed. The default is to compute the mean of the flattened
-                    array. If this is a tuple of ints, a mean is performed over multiple axes, instead of a single axis
-                    or all the axes as before.
-    :type axis: int or sequence of ints
-    :param keepdims: If this is set to True, the axes which are reduced are left in the result as dimensions with size
-                        one. With this option, the result will broadcast correctly against the input array.
-    :type keepdims: bool, optional
-    :return: The array with means computed.
-    """
-    return _cur_framework(x).reduce_mean(x, axis, keepdims)
-
-
-def reduce_var(x, axis=None, keepdims=False):
-    """
-    Computes the arithmetic variance along a given axis. The variance is taken over the flattened array by default,
-    otherwise over the specified axis.
-
-    :param x: Array containing numbers whose variance is desired.
-    :type x: array
-    :param axis: Axis or axes along which the means are computed. The default is to compute the mean of the flattened
-                    array. If this is a tuple of ints, a mean is performed over multiple axes, instead of a single axis
-                    or all the axes as before.
-    :type axis: int or sequence of ints
-    :param keepdims: If this is set to True, the axes which are reduced are left in the result as dimensions with size
-                        one. With this option, the result will broadcast correctly against the input array.
-    :type keepdims: bool, optional
-    :return: The array with variances computed.
-    """
-    return _cur_framework(x).reduce_var(x, axis, keepdims)
-
-
 def reduce_std(x, axis=None, keepdims=False):
     """
     Computes the arithmetic standard deviation along a given axis. The standard deviation is taken over
