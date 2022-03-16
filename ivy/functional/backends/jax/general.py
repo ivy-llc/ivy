a27a50c2777cb0c5ce7d41943d83f24f9afd4433
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-16 17:45:43 +0000

    fixing some imports etc.
    some leftover from commit ef445addd501948b5cc50dbbc3e7a6ede20c80fd [formerly 90cd9b95229acc61bfbf6789142f46bee25d9a2b]
    
    
    Former-commit-id: e6a5cb234a6500becb413b7be6e4e2efc4fc0f7b

--- ivy/functional/backends/jax/general.py
+++ ivy/functional/backends/jax/general.py
@@ -1,6 +1,25 @@
+"""
+Collection of Jax general functions, wrapped to fit Ivy syntax and signature.
+"""
+
+# global
 import jax as _jax
+import math as _math
+import numpy as _onp
 import jax.numpy as _jnp
 import jaxlib as _jaxlib
+from numbers import Number
+from operator import mul as _mul
+from functools import reduce as _reduce
+from jaxlib.xla_extension import Buffer
+import multiprocessing as _multiprocessing
+from haiku._src.data_structures import FlatMapping
+
+# local
+import ivy
+from ivy.functional.ivy.device import default_device
+from ivy.functional.ivy.old import default_dtype
+from ivy.functional.backends.jax.device import to_dev, dev as callable_dev
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 def is_array(x, exclusive=False):
