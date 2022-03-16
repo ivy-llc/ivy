a6cc8f225824cc9cfb7b77588c70b69771fe3e94
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-16 17:24:11 +0000

    refactoring backends
    copy_array, is_array in general.py and array in creations.py + import in __init__

---
+++ ivy/functional/backends/torch/general.py
@@ -0,0 +1,32 @@
+"""
+Collection of PyTorch general functions, wrapped to fit Ivy syntax and signature.
+"""
+
+# global
+import ivy
+import numpy as np
+torch_scatter = None
+import math as _math
+import torch as _torch
+from operator import mul
+from torch.types import Number
+from functools import reduce as _reduce
+from typing import List, Dict, Optional, Union
+
+
+# local
+from ivy.functional.ivy.old import default_dtype
+from ivy.functional.ivy.device import default_device
+from ivy.functional.backends.torch.device import dev_from_str, _callable_dev
+
+
+def is_array(x, exclusive=False):
+    if isinstance(x, _torch.Tensor):
+        if exclusive and x.requires_grad:
+            return False
+        return True
+    return False
+
+
+def copy_array(x):
+    return x.clone()
