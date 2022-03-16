8a47ede01f022afd72852dddc8fd089fc36835a2
Kevin Xue <zekuixue@gmail.com>
2022-03-16 22:01:19 +0000

    var for numpy

--- ivy/functional/backends/numpy/statistical.py
+++ ivy/functional/backends/numpy/statistical.py
@@ -1,6 +1,6 @@
 # global
 import numpy as _np
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 
 
 def min(x: _np.ndarray,
@@ -15,3 +15,11 @@ def max(x: _np.ndarray,
         keepdims: bool = False) \
         -> _np.ndarray:
     return _np.amax(a=x, axis=axis, keepdims=keepdims)
+
+
+def var(x: np.ndarray,
+        axis: Optional[Union[int, Tuple[int]]] = None,
+        correction: Union[int, float] = 0.0,
+        keepdims: bool = False) \
+        -> np.ndarray:
+    return np.var(npa.asarray(x), axis=axis, keepdims=keepdims)
