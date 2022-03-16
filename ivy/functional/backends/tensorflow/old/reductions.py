5f5a588a3e441edd3379a73f49606a53f2fd6dad
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-16 19:52:57 +0200

    Refactoring reductions
    reduce_min, reduce_max

--- ivy/functional/backends/tensorflow/old/reductions.py
+++ ivy/functional/backends/tensorflow/old/reductions.py
@@ -42,23 +42,5 @@ def reduce_var(x, axis=None, keepdims=False):
     return _tf.math.reduce_variance(x, axis=axis, keepdims=keepdims)
 
 
-def reduce_min(x, axis=None, keepdims=False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = tuple(range(num_dims))
-    elif isinstance(axis, list):
-        axis = tuple(axis)
-    return _tf.reduce_min(x, axis=axis, keepdims=keepdims)
-
-
-def reduce_max(x, axis=None, keepdims=False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = tuple(range(num_dims))
-    elif isinstance(axis, list):
-        axis = tuple(axis)
-    return _tf.reduce_max(x, axis=axis, keepdims=keepdims)
-
-
 def einsum(equation, *operands):
     return _tf.einsum(equation, *operands)
