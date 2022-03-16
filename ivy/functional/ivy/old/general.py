b6fb9e416fc1d7327f2f21c44bf6dc29df86138f
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-03-16 10:37:10 +0000

    Update general.py
    remove empty_like from old file

--- ivy/functional/ivy/old/general.py
+++ ivy/functional/ivy/old/general.py
@@ -1505,19 +1505,3 @@ def inplace_increment(x, val, f=None):
     :return: The variable following the in-place increment.
     """
     return _cur_framework(x).inplace_increment(x, val)
-
-
-def empty_like(x: Union[ivy.Array, ivy.NativeArray], dtype: ivy.Dtype = None, dev: ivy.Device = None,
-               ) -> Union[ivy.Array, ivy.NativeArray]:
-    """
-    Returns an uninitialized array with the same shape as an input array x.
-
-    :param x:  input array from which to derive the output array shape.
-    :type x: array
-    :param dtype: output array data type. If dtype is None, the output array data type must be inferred from x. Default: None.
-    :type dtype: data-type string, optional
-    :param dev: device on which to place the created array. If device is None, the output array device must be inferred from x. Default: None.
-    :type dev: ivy.Device, optional
-    :return: an array having the same shape as x and containing uninitialized data.
-    """
-    return _cur_framework(x).empty_like(x, dtype, dev)
