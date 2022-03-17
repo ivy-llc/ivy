5deb7704b7daaecf29fddabbbad7be927cd0f651
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-17 22:54:19 +0200

    Fixing data_types

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -145,10 +145,12 @@ def tanh(x: np.ndarray)\
 
 def bitwise_or(x1: np.ndarray , x2: np.ndarray) \
         -> np.ndarray:
-    x1, x2 = _cast_for_binary_op(x1, x2)
     if not isinstance(x2, np.ndarray):
         x2 = np.asarray(x2, dtype=x1.dtype)
-
+    else:
+        dtype = np.promote_types(x1.dtype, x2.dtype)
+        x1 = x1.astype(dtype)
+        x2 = x2.astype(dtype)
     return np.bitwise_or(x1, x2)
     
   
