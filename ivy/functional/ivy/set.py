b7812e6fd87715504d271ffd925d0c07609a044a
Mara Pislar <pislarmara589@gmail.com>
2022-03-17 16:24:08 +0000

    replace ivy.Tuple with typing.Tuple

--- ivy/functional/ivy/set.py
+++ ivy/functional/ivy/set.py
@@ -1,5 +1,5 @@
 # global
-from typing import Union
+from typing import Union, Tuple
 
 # local
 import ivy
@@ -7,8 +7,12 @@ from ivy.framework_handler import current_framework as _cur_framework
 
 
 def unique_inverse(x: Union[ivy.Array, ivy.NativeArray]) \
-        -> ivy.Tuple[ivy.Array, ivy.Array]:
+        -> Tuple[ivy.Array, ivy.Array]:
     """
+    Returns a tuple of two arrays, one being the unique elements of an input array x and the other one the indices from
+    the set of uniques elements that reconstruct x.
 
+    :param x: input array.
+    :return: tuple of two arrays (values, inverse_indices)
     """
     return _cur_framework.unique_inverse(x)
