9cb063c71807f724c9ccdb48ca7331439f738abe
Mara Pislar <pislarmara589@gmail.com>
2022-03-17 18:24:30 +0000

    modify unique_inverse for numpy framework

--- ivy/functional/backends/numpy/set.py
+++ ivy/functional/backends/numpy/set.py
@@ -6,4 +6,5 @@ import numpy as np
 def unique_inverse(x: np.ndarray) \
         -> Tuple[np.ndarray, np.ndarray]:
     values, inverse_indices = np.unique(x, return_inverse=True)
-    return values, inverse_indices
+    t = Tuple[values, inverse_indices]
+    return t
