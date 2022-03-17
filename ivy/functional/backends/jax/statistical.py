980c8b1933fefe86a338fc3d327b755b77fd21bb
Abdelrahman Othman <abdelrahman.m.a.osman@gmail.com>
2022-03-17 10:02:38 +0200

    prod adhering to Array API Standard (#532)
    Former-commit-id: f4b78d354c7eef469600ad3f9bb69e4400a86aa6

--- ivy/functional/backends/jax/statistical.py
+++ ivy/functional/backends/jax/statistical.py
@@ -1,6 +1,6 @@
 # global
 import jax.numpy as jnp
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional , List
 
 
 # Array API Standard #
@@ -11,6 +11,26 @@ def min(x: jnp.ndarray,
         keepdims = False, device = None) \
         -> jnp.ndarray:
     return jnp.min(a = jnp.asarray(x), axis = axis, keepdims = keepdims)
+
+
+def prod(x: jnp.ndarray,
+         axis: Optional[Union[int, Tuple[int]]] = None,
+         dtype: Optional[jnp.dtype] = None,
+         keepdims: bool = False)\
+        -> jnp.ndarray:
+
+    if dtype == None and jnp.issubdtype(x.dtype,jnp.integer):
+        if jnp.issubdtype(x.dtype,jnp.signedinteger) and x.dtype in [jnp.int8,jnp.int16,jnp.int32]:
+            dtype = jnp.int32
+        elif jnp.issubdtype(x.dtype,jnp.unsignedinteger) and x.dtype in [jnp.uint8,jnp.uint16,jnp.uint32]:
+            dtype = jnp.uint32
+        elif x.dtype == jnp.int64: 
+            dtype = jnp.int64
+        else:
+            dtype = jnp.uint64
+
+    return jnp.prod(a=x,axis=axis,dtype=dtype,keepdims=keepdims)
+
 
 def max(x: jnp.ndarray,
         axis: Union[int, Tuple[int]] = None,
