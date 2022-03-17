88871efba5ab8c8e472d32ad2def2292a21ec4de
Arvind thorat <98656424+arv0804@users.noreply.github.com>
2022-03-17 13:19:07 +0530

    __neg__
    #509

--- ivy/array/array_api/arithmetic_operators.py
+++ ivy/array/array_api/arithmetic_operators.py
@@ -5,4 +5,21 @@ import abc
 
 
 class ArrayWithArrayAPIArithmeticOperators(abc.ABC):
-    pass
+    def __neg__(x: torch.tensor)\
+       --> torch.tensor:
+        return[torch.__neg__(x)]
+    
+    def __neg__(x:tensor)\
+    --> Tensor:
+        return tf.numpy.__neg__(x)
+    
+    def __neg__(x: jaxArray)/
+    --> jaxArray:
+        return jax.numpy.__neg__(x)
+    
+    def __neg__(x: np.ndarray)/
+      ---> np.ndarray:
+            return -np.ndarray(x)
+    
+    
+    
