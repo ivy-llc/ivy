85f8367711bf21a17ab0813414f65970d321f535
Abdelrahman Othman <abdelrahman.m.a.osman@gmail.com>
2022-03-17 10:02:38 +0200

    prod adhering to Array API Standard (#532)

--- ivy/functional/backends/numpy/statistical.py
+++ ivy/functional/backends/numpy/statistical.py
@@ -1,6 +1,6 @@
 # global
 import numpy as np
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 
 
 # Array API Standard #
@@ -18,6 +18,25 @@ def max(x: np.ndarray,
         keepdims: bool = False) \
         -> np.ndarray:
     return np.amax(a=x, axis=axis, keepdims=keepdims)
+
+
+def prod(x: np.ndarray,
+         axis: Optional[Union[int, Tuple[int]]] = None,
+         dtype: Optional[np.dtype] = None,
+         keepdims: bool = False)\
+        -> np.ndarray:
+
+    if dtype == None and np.issubdtype(x.dtype,np.integer):
+        if np.issubdtype(x.dtype,np.signedinteger) and x.dtype in [np.int8,np.int16,np.int32]:
+            dtype = np.int32
+        elif np.issubdtype(x.dtype,np.unsignedinteger) and x.dtype in [np.uint8,np.uint16,np.uint32]:
+            dtype = np.uint32
+        elif x.dtype == np.int64: 
+            dtype = np.int64
+        else:
+            dtype = np.uint64
+
+    return np.prod(a=x,axis=axis,dtype=dtype,keepdims=keepdims)
 
 
 # Extra #
--- ivy/functional/backends/tensorflow/statistical.py
+++ ivy/functional/backends/tensorflow/statistical.py
@@ -1,7 +1,7 @@
 # global
 _round = round
 import tensorflow as tf
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
 
 
 # Array API Standard #
@@ -12,6 +12,25 @@ def min(x: tf.Tensor,
         keepdims: bool = False) \
         -> tf.Tensor:
     return tf.math.reduce_min(x, axis = axis, keepdims = keepdims)
+
+
+def prod(x: tf.Tensor,
+         axis: Optional[Union[int, Tuple[int]]] = None,
+         dtype: Optional[tf.DType] = None,
+         keepdims: bool = False)\
+        -> tf.Tensor:
+    if dtype == None:
+        if x.dtype in [ tf.int8 , tf.int16,tf.int32]:
+            dtype = tf.int32
+        elif x.dtype in [ tf.uint8,tf.uint16,tf.experimental.numpy.uint32]:
+            dtype = tf.experimental.numpy.uint32
+        elif x.dtype == tf.int64: 
+            dtype = tf.int64
+        elif x.dtype == tf.uint64 :
+            dtype = tf.uint64
+        
+    return tf.experimental.numpy.prod(x,axis,dtype,keepdims)
+
 
 def max(x: tf.Tensor,
         axis: Union[int, Tuple[int]] = None,
