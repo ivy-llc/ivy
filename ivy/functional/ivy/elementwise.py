410a4c0167ce4d608fbdc5a903e7a235f2ff4e51
Brian Ebiyau <38182764+ebyau@users.noreply.github.com>
2022-03-17 15:20:41 +0300

    bitwise_invert adhering to Array API Standard (#557)

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -9,6 +9,24 @@ from ivy.framework_handler import current_framework as _cur_framework
 # Array API Standard #
 # -------------------#
 
+def bitwise_invert(x: Union[ivy.Array, ivy.NativeArray]) \
+        -> ivy.Array:
+    """
+    Inverts (flips) each bit for each element x_i of the input array x.
+
+    Parameters
+    ----------
+    x:
+        input array. Should have an integer or boolean data type.
+
+    Returns
+    -------
+    out:
+        an array containing the element-wise results. The returned array must have the same data type as x.
+    """
+    return _cur_framework(x).bitwise_invert(x)
+
+
 def bitwise_and(x1: Union[ivy.Array, ivy.NativeArray],
                 x2: Union[ivy.Array, ivy.NativeArray])\
         -> ivy.Array:
