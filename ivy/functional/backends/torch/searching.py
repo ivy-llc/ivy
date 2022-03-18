93bbd7ca85ab4a41e486d302f488bd65559113bd
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-18 11:57:24 +0000

    fixing args for argmin in torch

--- ivy/functional/backends/torch/searching.py
+++ ivy/functional/backends/torch/searching.py
@@ -14,11 +14,11 @@ def argmax(
 
 def argmin(
     x: torch.Tensor,
-    dim: Optional[int] = None,
+    axis: Optional[int] = None,
     keepdims: Optional[bool] = False)\
     -> torch.Tensor:
 
     x = torch.tensor(x)
-    ret = torch.argmin(x,dim=dim, keepdim=keepdims)
+    ret = torch.argmin(x,axis=axis, keepdim=keepdims)
 
     return ret
