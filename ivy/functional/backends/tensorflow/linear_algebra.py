7c8fe813b4746acf514946ff6f4f09a11f1d6213
Matt <matthew-barrett@hotmail.co.uk>
2022-03-18 14:56:50 +0000

    moved vector_to_skew_symmetric_matrix

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -10,6 +10,9 @@ import ivy
 from collections import namedtuple
 
 
+# Array API Standard #
+# -------------------#
+
 inv = tf.linalg.inv
 pinv = tf.linalg.pinv
 cholesky = tf.linalg.cholesky
@@ -163,3 +166,25 @@ def trace(x: tf.Tensor,
           offset: int = 0)\
               -> tf.Tensor:
     return tf.trace(x, offset)
+
+
+# Extra #
+# ------#
+
+def vector_to_skew_symmetric_matrix(vector: Tensor)\
+        -> Tensor:
+    batch_shape = list(vector.shape[:-1])
+    # BS x 3 x 1
+    vector_expanded = tf.expand_dims(vector, -1)
+    # BS x 1 x 1
+    a1s = vector_expanded[..., 0:1, :]
+    a2s = vector_expanded[..., 1:2, :]
+    a3s = vector_expanded[..., 2:3, :]
+    # BS x 1 x 1
+    zs = tf.zeros(batch_shape + [1, 1])
+    # BS x 1 x 3
+    row1 = tf.concat((zs, -a3s, a2s), -1)
+    row2 = tf.concat((a3s, zs, -a1s), -1)
+    row3 = tf.concat((-a2s, a1s, zs), -1)
+    # BS x 3 x 3
+    return tf.concat((row1, row2, row3), -2)
