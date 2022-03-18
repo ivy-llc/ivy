f93e4f3da4f1b89c2fdfda4b9074aeb3928e4ca9
RickSanchezStoic <57310695+RickSanchezStoic@users.noreply.github.com>
2022-03-18 16:09:43 +0530

    empty_like adhering to Array API standard (#482)

--- ivy/functional/backends/torch/creation.py
+++ ivy/functional/backends/torch/creation.py
@@ -90,6 +90,31 @@ def empty(shape: Union[int, Tuple[int]],
         -> Tensor:
     return torch.empty(shape, dtype=dtype_from_str(default_dtype(dtype)), device=dev_from_str(default_device(device)))
 
+
+def empty_like(x: torch.Tensor,
+              dtype: Optional[Union[torch.dtype, str]] = None,
+              dev: Optional[Union[torch.device, str]] = None) \
+        -> torch.Tensor:
+    if dev is None:
+        dev = _callable_dev(x)
+    if dtype is not None and dtype is str:
+        type_dict: Dict[str, torch.dtype] = {'int8': torch.int8,
+            'int16': torch.int16,
+            'int32': torch.int32,
+            'int64': torch.int64,
+            'uint8': torch.uint8,
+            'bfloat16': torch.bfloat16,
+            'float16': torch.float16,
+            'float32': torch.float32,
+            'float64': torch.float64,
+            'bool': torch.bool}
+        return torch.empty_like(x, dtype=type_dict[dtype], device=dev_from_str(dev))
+    else:
+        return torch.empty_like(x, dtype= dtype, device=dev_from_str(dev))
+
+    return torch.empty_like(x, device=dev_from_str(dev))
+
+  
 def _differentiable_linspace(start, stop, num, device):
     if num == 1:
         return torch.unsqueeze(start, 0)
