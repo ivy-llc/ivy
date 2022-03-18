e9dea0f3707904a030d109cb2610ac73672559ae
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-18 16:59:33 +0000

    moving logspace to correcti loc
    plus moved asarray out of #extra# heading

--- ivy/functional/ivy/old/general.py
+++ ivy/functional/ivy/old/general.py
@@ -255,30 +255,7 @@ def linspace(start: Union[ivy.Array, ivy.NativeArray, Number], stop: Union[ivy.A
     return _cur_framework(start).linspace(start, stop, num, axis, dev)
 
 
-# noinspection PyShadowingNames
-def logspace(start: Union[ivy.Array, ivy.NativeArray, Number], stop: Union[ivy.Array, ivy.NativeArray, Number],
-             num: int, base: float = 10., axis: int = None, dev: ivy.Device = None)\
-        -> Union[ivy.Array, ivy.NativeArray]:
-    """
-    Generates a certain number of evenly-spaced values in log space, in an interval along a given axis.
 
-    See :math:`arange` that allows to specify the step size of evenly spaced values in an interval.
-
-    :param start: First entry in the range.
-    :type start: array
-    :param stop: Final entry in the range.
-    :type stop: array
-    :param num: Number of values to generate.
-    :type num: int
-    :param base: The base of the log space. Default is 10.0
-    :type base: float, optional
-    :param axis: Axis along which the operation is performed.
-    :type axis: int
-    :param dev: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
-    :type dev: ivy.Device
-    :return: Tensor of evenly-spaced values.
-    """
-    return _cur_framework(start).logspace(start, stop, num, base, axis, dev)
 
 
 def concatenate(xs: Iterable[Union[ivy.Array, ivy.NativeArray]], axis: int = -1)\
