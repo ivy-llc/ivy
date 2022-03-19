03a32a08f642787ab0aa6cf6fa3bf85dde953e9c
HotzingTone <adam.he.future@gmail.com>
2022-03-19 15:28:02 +0000

    Eye adhering to Array API Standard (#323)

--- ivy/functional/backends/tensorflow/creation.py
+++ ivy/functional/backends/tensorflow/creation.py
@@ -117,13 +117,37 @@ def empty_like(x: Tensor,
     with tf.device(dev_from_str(dev)):
         return tf.experimental.numpy.empty_like(x, dtype=dtype)
 
-      
+
 def linspace(start, stop, num, axis=None, dev=None):
     if axis is None:
         axis = -1
     dev = default_device(dev)
     with tf.device(ivy.dev_from_str(dev)):
         return tf.linspace(start, stop, num, axis=axis)
+
+
+def eye(n_rows: int,
+        n_cols: Optional[int] = None,
+        k: Optional[int] = 0,
+        dtype: Optional[Dtype] = None,
+        device: Optional[str] = None) \
+        -> tf.Tensor:
+    dtype = dtype_from_str(default_dtype(dtype))
+    device = dev_from_str(default_device(device))
+    with tf.device(device):
+        if n_cols is None:
+            n_cols = n_rows
+        i = tf.eye(n_rows, n_cols, dtype=dtype)
+        if k == 0:
+            return i
+        elif -n_rows < k < 0:
+            return tf.concat([tf.zeros([-k, n_cols], dtype=dtype),
+                              i[:n_rows+k]], 0)
+        elif 0 < k < n_cols:
+            return tf.concat([tf.zeros([n_rows, k], dtype=dtype),
+                              i[:, :n_cols-k]], 1)
+        else:
+            return tf.zeros([n_rows, n_cols], dtype=dtype)
 
 
 # Extra #
