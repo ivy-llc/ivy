323a82a01a198fc59a8b9536efb808e160c1beaf
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-19 15:41:25 +0200

    Refactoring Reductions
    reduce_sum
    
    
    Former-commit-id: 80c80dd84d1409b403f921da02e88ada47ae69a3

--- ivy/functional/backends/jax/old/reductions.py
+++ ivy/functional/backends/jax/old/reductions.py
@@ -6,15 +6,6 @@ Collection of Jax reduction functions, wrapped to fit Ivy syntax and signature.
 import jax.numpy as _jnp
 
 
-def sum(x, axis=None, keepdims=False):
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = tuple(range(num_dims))
-    elif isinstance(axis, list):
-        axis = tuple(axis)
-    return _jnp.sum(x, axis=axis, keepdims=keepdims)
-
-
 def reduce_prod(x, axis=None, keepdims=False):
     if axis is None:
         num_dims = len(x.shape)
