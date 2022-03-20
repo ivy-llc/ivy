7538bf88d53a28552d0afb39373dc7a6eb4c9bdc
sounritesh <sounritesh@gmail.com>
2022-03-21 00:19:47 +0530

    abs adhering to Array API Standard (#586)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -174,6 +174,11 @@ def bitwise_or(x1: np.ndarray , x2: np.ndarray) \
     return np.bitwise_or(x1, x2)
 
 
+def abs(x: np.ndarray)\
+        -> np.ndarray:
+    return np.absolute(x)
+
+  
 def logaddexp(x1: np.ndarray, x2: np.ndarray) -> np.ndarray:
     if not isinstance(x2, np.ndarray):
         x2 = np.asarray(x2, dtype=x1.dtype)
