a0f87c974d081f535de1357ddd812e87f45ccd40
Ved <vedpat3@gmail.com>
2022-03-20 21:34:08 +0530

    Implemented __irshift__ and __rrshift__

--- ivy/array/__init__.py
+++ ivy/array/__init__.py
@@ -326,6 +326,18 @@ class Array(ArrayWithArrayAPI, ArrayWithDevice, ArrayWithGeneral, ArrayWithGradi
     @_native_wrapper
     def __rshift__(self, other):
         return ivy.bitwise_right_shift(self._data, other)
+    
+    @_native_wrapper
+    def __irshift__(self, other):
+        self._data = ivy.bitwise_right_shift(self._data, other)
+    
+    @_native_wrapper
+    def __rrshift__(self, other):
+        other = to_native(other)
+        res = self._data.__rrshift__(other)
+        if res is NotImplemented:
+            return res
+        return to_ivy(res)
         
     @_native_wrapper
     def __ror__(self, other):
