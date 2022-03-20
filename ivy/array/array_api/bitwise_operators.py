90fb1fa496930bab55360e2ab814396cd4bdf361
Arvind thorat <98656424+arv0804@users.noreply.github.com>
2022-03-20 15:15:34 +0530

    Update bitwise_operators.py
    #497

--- ivy/array/array_api/bitwise_operators.py
+++ ivy/array/array_api/bitwise_operators.py
@@ -1,8 +1,16 @@
 # global
 import abc
+import ivy 
+from ivy.framework_handler import current_framework as cur_framework 
 
 # ToDo: implement all Array API attributes here
 
 
 class ArrayWithArrayAPIBitwiseOperators(abc.ABC):
-    pass
+    def __or__(X: np.ndarray)/
+      -->np.ndarray:
+            
+            return np.bitwise_or(x, a, b)
+        
+            
+      
