f9cbc7bd70485ef0172d6866e1d6b57d5fe60b56
Rashul Chutani <41239718+RashulChutani@users.noreply.github.com>
2022-03-20 15:30:54 +0530

    result_type adhering to Array API Standard  (#600)
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>

--- ivy/functional/backends/jax/data_type.py
+++ ivy/functional/backends/jax/data_type.py
@@ -46,6 +46,15 @@ def finfo(type: Union[jnp.dtype, str, JaxArray]) \
     return Finfo(jnp.finfo(ivy.dtype_from_str(type)))
 
 
+def result_type(*arrays_and_dtypes: Union[JaxArray, jnp.dtype]) -> jnp.dtype:
+    if len(arrays_and_dtypes) <= 1:
+        return jnp.result_type(arrays_and_dtypes)
+
+    result = jnp.result_type(arrays_and_dtypes[0], arrays_and_dtypes[1])
+    for i in range(2, len(arrays_and_dtypes)):
+        result = jnp.result_type(result, arrays_and_dtypes[i])
+    return result
+
+  
 def broadcast_to(x: JaxArray, shape: Tuple[int, ...]) -> JaxArray:
     return jnp.broadcast_to(x, shape)
-
