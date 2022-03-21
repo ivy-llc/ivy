887fc5e7243d2bf5cc7f39746ba34f62a71f1919
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-21 10:27:30 +0200

    Refactoring reductions
    reduce_std

--- ivy/functional/backends/mxnet/statistical.py
+++ ivy/functional/backends/mxnet/statistical.py
@@ -1,7 +1,7 @@
 from numbers import Number
 from ivy.functional.backends.mxnet.old.reductions import _handle_output
 import mxnet as mx
-from ivy.functional.backends.mxnet import _flat_array_to_1_dim_array
+from ivy.functional.backends.mxnet import _flat_array_to_1_dim_array, _1_dim_array_to_flat_array
 
 def sum(x, axis=None, keepdims=False):
     if axis is None:
@@ -56,3 +56,43 @@ def var(x, axis=None, keepdims=False):
     if is_flat:
         return _1_dim_array_to_flat_array(ret)
     return ret
+
+
+def std(x, axis=None, keepdims=False):
+    red_var = var(x, axis, keepdims)
+    is_flat = red_var.shape == ()
+    if is_flat:
+        red_var = _flat_array_to_1_dim_array(red_var)
+    red_std = red_var ** 0.5
+    if is_flat:
+        return _1_dim_array_to_flat_array(red_std)
+    return red_std
+
+
+def min(x, axis=None, keepdims=False):
+    if axis is None:
+        num_dims = len(x.shape)
+        axis = tuple(range(num_dims))
+    elif isinstance(axis, Number):
+        axis = (axis,)
+    elif isinstance(axis, list):
+        axis = tuple(axis)
+    if x.shape == ():
+        x = _flat_array_to_1_dim_array(x)
+    ret = _mx.nd.min(x, axis=axis, keepdims=keepdims)
+    return _handle_output(x, axis, keepdims, ret)
+
+
+def max(x, axis=None, keepdims=False):
+    if axis is None:
+        num_dims = len(x.shape)
+        axis = tuple(range(num_dims))
+    elif isinstance(axis, Number):
+        axis = (axis,)
+    elif isinstance(axis, list):
+        axis = tuple(axis)
+    if x.shape == ():
+        x = _flat_array_to_1_dim_array(x)
+    ret = _mx.nd.max(x, axis=axis, keepdims=keepdims)
+    return _handle_output(x, axis, keepdims, ret)
+
