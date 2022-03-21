971efe631a504d6577701156890cbc1e6924f963
Saeed Ashraf <saeedashrraf@gmail.com>
2022-03-21 11:15:32 +0200

    Refactoring Reductions
    einsum

--- ivy/functional/backends/mxnet/old/reductions.py
+++ ivy/functional/backends/mxnet/old/reductions.py
@@ -14,7 +14,3 @@ def _handle_output(x, axis, keepdims, ret):
     if not keepdims and (axis is None or len((axis,) if isinstance(axis, int) else axis) == len(x.shape)):
         return _1_dim_array_to_flat_array(ret)
     return ret
-
-
-def einsum(equation, *operands):
-    return _mx.np.einsum(equation, *[op.as_np_ndarray() for op in operands]).as_nd_ndarray()
