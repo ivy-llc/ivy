2958aafda509d6b697f9bea130696f695a069990
HotzingTone <adam.he.future@gmail.com>
2022-03-21 14:53:41 +0000

    remove all 'import logic' from old '__init__.py' files
    Former-commit-id: 797ae80557c3d38884e1ba76c362e29fd3a69fab

--- ivy/functional/ivy/old/__init__.py
+++ ivy/functional/ivy/old/__init__.py
@@ -3,8 +3,6 @@ from .dtype import *
 from . import general
 from .general import *
 from .linalg import *
-from . import logic
-from .logic import *
 from . import math
 from .math import *
 from .. import random
