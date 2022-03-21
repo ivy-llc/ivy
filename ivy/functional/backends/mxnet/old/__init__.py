5b948bc54483cb89015abb391173fa2f3455fdfa
HotzingTone <adam.he.future@gmail.com>
2022-03-21 16:09:57 +0000

    Refactor math module for all backends
    Former-commit-id: 023993c071e19546a748b1bc9659b3508a8c70e7

--- ivy/functional/backends/mxnet/old/__init__.py
+++ ivy/functional/backends/mxnet/old/__init__.py
@@ -2,7 +2,5 @@ from . import general
 from .general import *
 from . import linear_algebra
 from .linear_algebra import *
-from . import math
-from .math import *
 from . import reductions
 from .reductions import *
