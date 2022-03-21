ee348ba5fe5c81dd191055a76a9acf56b3eff0a0
HotzingTone <adam.he.future@gmail.com>
2022-03-21 16:55:45 +0000

    bypass not-yet implemented 'divide' func
    Former-commit-id: 84550e547cd8716a9bc1ce32c20984fde7189ca4

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -855,7 +855,7 @@ def divide(x1: Union[ivy.Array, ivy.NativeArray], x2: Union[ivy.Array, ivy.Nativ
     :return: an array containing the element-wise results. The returned array must have a floating-point data type
              determined by Type Promotion Rules.
     """
-    return _cur_framework(x1).divide(x1, x2)
+    pass
 
 
 # Extra #
