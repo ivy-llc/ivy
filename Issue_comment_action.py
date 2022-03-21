aac2f2719784fa2676e5d3d5ba4816b87e6eedd8
1Doomdie1 <horia.todoran@yahoo.com>
2022-03-21 02:32:57 +0000

    Fixing misspelling :D

--- Issue_comment_action.py
+++ Issue_comment_action.py
@@ -10,8 +10,8 @@ def command(cmd, save_output=True):
         os.system(cmd)
 
 
-def carve_main_issue_body_functions(main_issue_body, alocated=True):
-    if alocated:
+def carve_main_issue_body_functions(main_issue_body, allocated=True):
+    if allocated:
         return [int(i[7:]) for i in main_issue_body.split('\r\n') if '- [ ] #' in i]
     return [i[6:] for i in main_issue_body.split('\r\n') if '#' not in i]
 
@@ -47,25 +47,25 @@ main_issue_ids = main_issue_numbers(command('gh issue list --label "Array API","
 
 if issue_number in main_issue_ids:
     main_issue = command(f'gh issue view {issue_number} --json number,title,body')
-    alocate_functions = carve_main_issue_body_functions(main_issue['body'])
-    non_alocate_functions = carve_main_issue_body_functions(main_issue['body'], False)
+    allocate_functions = carve_main_issue_body_functions(main_issue['body'])
+    non_allocate_functions = carve_main_issue_body_functions(main_issue['body'], False)
     print(f"New comment detected on: '{main_issue['title']}'")
     print(f"Comment body: '{comment_body}'")
 
     if issue_in_comment(comment_body):
         comment_issue_id = int(comment_body[7:])
         comment_issue_title = comment_title(comment_body)
-        if comment_issue_title in non_alocate_functions:
+        if comment_issue_title in non_allocate_functions:
             print('Function Free')
             # ToDo: Add Labels "Array API" "Single Function"
             main_issue_body = main_issue['body'].replace(f'- [ ] {comment_issue_title}', comment_body)
             command(f'gh issue edit {comment_issue_id} --add-label "Array API","Single Function" --add-assignee "{comment_author}"', save_output=False)
             command(f'gh issue edit {main_issue["number"]} --body "{main_issue_body}"', save_output=False)
-        elif (comment_issue_title not in non_alocate_functions) and (comment_issue_id not in alocate_functions):
-            print('Function alreaddy alocated, closing issue.')
+        elif (comment_issue_title not in non_allocate_functions) and (comment_issue_id not in allocate_functions):
+            print('Function alreaddy allocated, closing issue.')
             command(f'gh issue comment {comment_issue_id} --body "This issue is being closed because the function has already been taken, plase choose another function and recomment on the main issue."', save_output=False)
             command(f'gh issue close {comment_issue_id}', save_output=False)
-        elif comment_issue_id in alocate_functions:
+        elif comment_issue_id in allocate_functions:
             print('Issue ID already in use...')
     else:
         # ToDo: Delete comment
