b354b1a4f9d75c6c8d5ab95e14b347becf328922
tayoshittu <tayoshittu@gmail.com>
2022-03-21 16:20:30 +0000

    expm1 Adhering to Array API Standards

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -1,7 +1,9 @@
 # global
 import numpy as np
 import numpy.array_api as npa
-
+def expm1(x: np.ndarray)\
+        -> np.ndarray:
+    return np.expm1(x)
 
 def bitwise_invert(x: np.ndarray)\
         -> np.ndarray:
--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -6,6 +6,9 @@ import typing
 
 # local
 import ivy
+def expm1(x: Tensor)\
+        -> Tensor:
+    return torch.expm1(x)
 
 def bitwise_invert(x: torch.Tensor) \
         -> torch.Tensor:
