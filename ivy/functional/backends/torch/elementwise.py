bbd920c053d0dabd4cde767ae54947f4d659cd31
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-03-22 18:56:00 +0530

    Logical_xor torch

--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -141,7 +141,12 @@ def acos(x: torch.Tensor)\
         -> torch.Tensor:
     return torch.acos(x)
 
-  
+
+def logical_xor(x1: torch.Tensor, x2: torch.Tensor) \
+        -> torch.Tensor:
+    return torch.logical_xor(x1.type(torch.bool), x2.type(torch.bool))
+
+
 def logical_or(x1: torch.Tensor, x2: torch.Tensor)\
         -> torch.Tensor:
     return torch.logical_or(x1.type(torch.bool), x2.type(torch.bool))
