feb0665f305923e633c42444c5a80b315695b562
Bethel Hall <bhall@M1-Mac-Air.local>
2022-03-22 11:38:09 +0300

    sort method implementation added

--- ivy/functional/backends/tensorflow/array_api/sorting_functions.py
+++ ivy/functional/backends/tensorflow/array_api/sorting_functions.py
@@ -6,8 +6,8 @@ from tensorflow.python.types.core import Tensor
 def argsort(x: Tensor,
             axis: int = -1,
             descending: bool = False,
-            stable: bool = True)\
-            -> Tensor:
+            stable: bool = True) \
+        -> Tensor:
     if tf.convert_to_tensor(x).dtype.is_bool:
         if descending:
             return tf.argsort(tf.cast(x, dtype=tf.int32), axis=axis, direction='DESCENDING', stable=stable)
@@ -18,3 +18,20 @@ def argsort(x: Tensor,
             return tf.argsort(tf.convert_to_tensor(x), axis=axis, direction='DESCENDING', stable=stable)
         else:
             return tf.argsort(tf.convert_to_tensor(x), axis=axis, direction='ASCENDING', stable=stable)
+
+
+def sort(x: Tensor,
+         axis: int = -1,
+         descending: bool = False,
+         stable: bool = True) -> Tensor:
+
+    if tf.convert_to_tensor(x).dtype.is_bool:
+        if descending:
+            return tf.sort(tf.cast(x, dtype=tf.int32), axis=axis, direction='DESCENDING', stable=stable)
+        else:
+            return tf.sort(tf.cast(x, dtype=tf.int32), axis=axis, direction='ASCENDING', stable=stable)
+    else:
+        if descending:
+            return tf.sort(tf.convert_to_tensor(x), axis=axis, direction='DESCENDING', stable=stable)
+        else:
+            return tf.sort(tf.convert_to_tensor(x), axis=axis, direction='ASCENDING', stable=stable)
