095a298f61fd0598e3e301b147ab086d01afb75e
bethelhall <bethelroger33@gmail.com>
2022-03-23 20:48:44 +0300

    TF sort method updated - v7

--- ivy/functional/backends/tensorflow/sorting.py
+++ ivy/functional/backends/tensorflow/sorting.py
@@ -23,17 +23,10 @@ def argsort(x: Tensor,
 def sort(x: tf.Tensor,
          axis: int = -1,
          descending: bool = False,
-         stable: bool = True)\
+         stable: bool = True) \
         -> tf.Tensor:
-    direction = 'ASCENDING' if stable else 'DESCENDING'
 
-    if tf.convert_to_tensor(x).dtype.is_bool:
-        if descending:
-            return tf.sort(tf.cast(x, dtype=tf.int32), axis=axis, direction=direction).numpy()
-        else:
-            return tf.sort(tf.cast(x, dtype=tf.int32), axis=axis, direction=direction).numpy()
+    if descending:
+        return tf.sort(x, axis=axis, direction='DESCENDING')
     else:
-        if descending:
-            return tf.sort(tf.convert_to_tensor(x), axis=axis, direction=direction).numpy()
-        else:
-            return tf.sort(tf.convert_to_tensor(x), axis=axis, direction=direction).numpy()
+        return tf.sort(x, axis=axis, direction='ASCENDING')
