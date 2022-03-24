8cf631db63e8fd1605471d9be7c0e7bea6236379
Arvind thorat <98656424+arv0804@users.noreply.github.com>
2022-03-24 16:45:39 +0530

    Update linear_algebra.py

--- ivy/functional/backends/mxnet/linear_algebra.py
+++ ivy/functional/backends/mxnet/linear_algebra.py
@@ -16,6 +16,11 @@ DET_THRESHOLD = 1e-12
 # Array API Standard #
 # -------------------#
 
+def eig(x: mx.ndarray)\
+  ->mx.ndarray:
+    return mx.np.linalg.eig(x)
+
+
 inv = mx.nd.linalg_inverse
 cholesky = lambda x: mx.np.linalg.cholesky(x.as_np_ndarray()).as_nd_ndarray()
 
@@ -39,9 +44,6 @@ def pinv(x):
             else:
                 return xT
             
-def eig(x: mx.ndarray)\
-  ->mx.ndarray:
-    return mx.np.linalg.eig(x)
 
 
 def vector_norm(x: NDArray,
