e894e07d568a9a829568cec61fa0315861e0a987
Mara Pislar <37437735+maraPislar@users.noreply.github.com>
2022-03-25 16:27:00 +0000

    unique_inverse adhering to Αrray API standard (#551)
    Co-authored-by: Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>

--- ivy/functional/ivy/set.py
+++ ivy/functional/ivy/set.py
@@ -1,12 +1,25 @@
 # global
-from typing import Union, Optional
+from typing import Union, Tuple, Optional
 
 # local
 import ivy
 from ivy.framework_handler import current_framework as _cur_framework
 
+
 # Array API Standard #
 # -------------------#
+
+def unique_inverse(x: Union[ivy.Array, ivy.NativeArray]) \
+        -> Tuple[ivy.Array, ivy.Array]:
+    """
+    Returns a tuple of two arrays, one being the unique elements of an input array x and the other one the indices from
+    the set of uniques elements that reconstruct x.
+
+    :param x: input array.
+    :return: tuple of two arrays (values, inverse_indices)
+    """
+    return _cur_framework.unique_inverse(x)
+
 
 def unique_values(x: Union[ivy.Array, ivy.NativeArray])\
         -> ivy.Array:
