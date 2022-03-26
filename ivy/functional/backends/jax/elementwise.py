c7889cfaddc512e5a4751930f33249d7087b3d4c
Georgy Trush <zhora@phygitalism.com>
2022-03-26 23:44:47 +0300

    Fix array type casting for jax test_equal

--- ivy/functional/backends/jax/elementwise.py
+++ ivy/functional/backends/jax/elementwise.py
@@ -51,9 +51,22 @@ def isinf(x: JaxArray)\
     return jnp.isinf(x)
 
 
-def equal(x1: JaxArray, x2: JaxArray)\
-        -> JaxArray:
-    return jnp.array_equal(x1, x2)
+def _cast_for_binary_op(x1: JaxArray, x2: JaxArray)\
+        -> typing.Tuple[typing.Union[JaxArray, int, float, bool], typing.Union[JaxArray, int, float, bool]]:
+    x1_bits = ivy.functional.backends.jax.old.general.dtype_bits(x1.dtype)
+    if isinstance(x2, (int, float, bool)):
+        return x1, x2
+    x2_bits = ivy.functional.backends.jax.old.general.dtype_bits(x2.dtype)
+    if x1_bits > x2_bits:
+        x2 = x2.astype(x1.dtype)
+    elif x2_bits > x1_bits:
+        x1 = x1.astype(x2.dtype)
+    return x1, x2
+
+
+def equal(x1: JaxArray, x2: JaxArray) -> JaxArray:
+    x1, x2 = _cast_for_binary_op(x1, x2)
+    return x1 == x2
 
 
 def greater_equal(x1: JaxArray, x2: JaxArray)\
