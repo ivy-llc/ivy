059bd1718f999fb5397ee621b9c339e53402263e
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-03-28 20:26:17 +0530

    bitwise_right_shift adhering to Array API standard (#696)
    Co-authored-by: Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
    Co-authored-by: Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -242,6 +242,17 @@ def logaddexp(x1: np.ndarray, x2: np.ndarray) -> np.ndarray:
     return np.logaddexp(x1, x2)
 
 
+def bitwise_right_shift(x1: np.ndarray, x2: np.ndarray)\
+        -> np.ndarray:
+    if not isinstance(x2, np.ndarray):
+        x2 = np.asarray(x2, dtype=x1.dtype)
+    else:
+        dtype = np.promote_types(x1.dtype, x2.dtype)
+        x1 = x1.astype(dtype)
+        x2 = x2.astype(dtype)
+    return np.right_shift(x1, x2)
+
+
 tan = np.tan
 
 
