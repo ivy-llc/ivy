4679f6d53db40f93f9fd51b74bc101a479da24de
Onyekachukwu Okonji <40724187+onyekaokonji@users.noreply.github.com>
2022-03-28 14:31:44 +0100

    sum adhering to Array API Standard (#686)
    Co-Authored-By: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/tensorflow/statistical.py
+++ ivy/functional/backends/tensorflow/statistical.py
@@ -15,15 +15,21 @@ def min(x: tf.Tensor,
 
 
 def sum(x: tf.Tensor,
-               axis: Optional[Union[int,Tuple[int]]] = None,
-               keepdims: bool = False)\
-                   -> tf.Tensor:
-    if axis is None:
-        num_dims = len(x.shape)
-        axis = tuple(range(num_dims))
-    elif isinstance(axis, list):
-        axis = tuple(axis)
-    return tf.reduce_sum(x, axis=axis, keepdims=keepdims)
+        axis: Optional[Union[int,Tuple[int]]] = None,
+        dtype: Optional[tf.DType] = None,
+        keepdims: bool = False) -> tf.Tensor:
+
+    if dtype == None:
+        if x.dtype in [tf.int8, tf.int16, tf.int32]:
+            dtype = tf.int32
+        elif x.dtype in [tf.uint8, tf.uint16, tf.experimental.numpy.uint32]:
+            dtype = tf.experimental.numpy.uint32
+        elif x.dtype == tf.int64:
+            dtype = tf.int64
+        elif x.dtype == tf.uint64:
+            dtype = tf.uint64
+
+    return tf.experimental.numpy.sum(x, axis, dtype, keepdims)
 
 
 def prod(x: tf.Tensor,
