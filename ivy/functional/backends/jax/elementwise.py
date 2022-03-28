933a8d8722e29ad2f93bb11f5f419578cd197248
Georgy Trush <tensorush@gmail.com>
2022-03-28 16:05:44 +0300

    atan2 adhering to Array API Standard (#693)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/jax/elementwise.py
+++ ivy/functional/backends/jax/elementwise.py
@@ -229,7 +229,14 @@ def atan(x: JaxArray)\
     return jnp.arctan(x)
 
 
-atan2 = jnp.arctan2
+def atan2(x1: JaxArray, x2: JaxArray) -> JaxArray:
+    if hasattr(x1, 'dtype') and hasattr(x2, 'dtype'):
+        promoted_type = jnp.promote_types(x1.dtype, x2.dtype)
+        x1 = x1.astype(promoted_type)
+        x2 = x2.astype(promoted_type)
+    return jnp.arctan2(x1, x2)
+
+
 cosh = jnp.cosh
 atanh = jnp.arctanh
 log = jnp.log
