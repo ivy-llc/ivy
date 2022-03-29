17a22b1d5365297dec15001d44642ad0d3686a5c
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-29 09:34:32 +0100

    refac: general,  concatenate and where

--- ivy/functional/backends/tensorflow/old/general.py
+++ ivy/functional/backends/tensorflow/old/general.py
@@ -7,7 +7,7 @@ import ivy
 _round = round
 import numpy as _np
 import math as _math
-import tensorflow as _tf
+import tensorflow as tf
 from numbers import Number
 from collections import Iterable
 
@@ -19,33 +19,33 @@ from ivy.functional.ivy import default_dtype
 from ivy.functional.ivy.device import default_device
 from ivy.functional.backends.tensorflow.device import _dev_callable, dev_from_str
 
-DTYPE_TO_STR = {_tf.int8: 'int8',
-                _tf.int16: 'int16',
-                _tf.int32: 'int32',
-                _tf.int64: 'int64',
-                _tf.uint8: 'uint8',
-                _tf.uint16: 'uint16',
-                _tf.uint32: 'uint32',
-                _tf.uint64: 'uint64',
-                _tf.bfloat16: 'bfloat16',
-                _tf.float16: 'float16',
-                _tf.float32: 'float32',
-                _tf.float64: 'float64',
-                _tf.bool: 'bool'}
+DTYPE_TO_STR = {tf.int8: 'int8',
+                tf.int16: 'int16',
+                tf.int32: 'int32',
+                tf.int64: 'int64',
+                tf.uint8: 'uint8',
+                tf.uint16: 'uint16',
+                tf.uint32: 'uint32',
+                tf.uint64: 'uint64',
+                tf.bfloat16: 'bfloat16',
+                tf.float16: 'float16',
+                tf.float32: 'float32',
+                tf.float64: 'float64',
+                tf.bool: 'bool'}
 
-DTYPE_FROM_STR = {'int8': _tf.int8,
-                'int16': _tf.int16,
-                'int32': _tf.int32,
-                'int64': _tf.int64,
-                'uint8': _tf.uint8,
-                'uint16': _tf.uint16,
-                'uint32': _tf.uint32,
-                'uint64': _tf.uint64,
-                'bfloat16': _tf.bfloat16,
-                'float16': _tf.float16,
-                'float32': _tf.float32,
-                'float64': _tf.float64,
-                'bool': _tf.bool}
+DTYPE_FROM_STR = {'int8': tf.int8,
+                'int16': tf.int16,
+                'int32': tf.int32,
+                'int64': tf.int64,
+                'uint8': tf.uint8,
+                'uint16': tf.uint16,
+                'uint32': tf.uint32,
+                'uint64': tf.uint64,
+                'bfloat16': tf.bfloat16,
+                'float16': tf.float16,
+                'float32': tf.float32,
+                'float64': tf.float64,
+                'bool': tf.bool}
 
 
 # API #
@@ -68,14 +68,14 @@ def dtype_bits(dtype_in):
 
 
 
-minimum = _tf.minimum
-maximum = _tf.maximum
+minimum = tf.minimum
+maximum = tf.maximum
 
 
 
 
 def cast(x, dtype):
-    return _tf.cast(x, dtype_from_str(dtype))
+    return tf.cast(x, dtype_from_str(dtype))
 
 
 astype = cast
@@ -87,10 +87,6 @@ astype = cast
 
 
 
-def concatenate(xs, axis=-1):
-    if xs[0].shape == ():
-        return _tf.concat([_tf.expand_dims(x, 0) for x in xs], axis)
-    return _tf.concat(xs, axis)
 
 
 
@@ -99,7 +95,6 @@ def concatenate(xs, axis=-1):
 
 
 
-where = lambda condition, x1, x2: _tf.where(_tf.cast(condition, _tf.bool), x1, x2)
 
 
 
@@ -107,39 +102,39 @@ where = lambda condition, x1, x2: _tf.where(_tf.cast(condition, _tf.bool), x1, x
 
 # noinspection PyShadowingNames
 def zeros_like(x, dtype=None, dev=None):
-    dtype = _tf.__dict__[dtype] if dtype else dtype
+    dtype = tf.__dict__[dtype] if dtype else dtype
     dev = default_device(dev)
-    with _tf.device(dev_from_str(dev)):
-        return _tf.zeros_like(x, dtype=dtype)
+    with tf.device(dev_from_str(dev)):
+        return tf.zeros_like(x, dtype=dtype)
 
 
 def full(shape, fill_value, dtype=None, device=None):
-    with _tf.device(dev_from_str(default_device(device))):
-        return _tf.fill(shape, _tf.constant(fill_value, dtype=dtype_from_str(default_dtype(dtype, fill_value))))
+    with tf.device(dev_from_str(default_device(device))):
+        return tf.fill(shape, tf.constant(fill_value, dtype=dtype_from_str(default_dtype(dtype, fill_value))))
 
 
 def one_hot(indices, depth, dev=None):
     dev = default_device(dev)
     if dev is not None:
-        with _tf.device(dev_from_str(dev)):
-            return _tf.one_hot(indices, depth)
-    return _tf.one_hot(indices, depth)
+        with tf.device(dev_from_str(dev)):
+            return tf.one_hot(indices, depth)
+    return tf.one_hot(indices, depth)
 
 
-cross = _tf.linalg.cross
+cross = tf.linalg.cross
 
 
 
 
 # noinspection PyShadowingNames
 def identity(n, dtype='float32', batch_shape=None, dev=None):
-    dtype = _tf.__dict__[dtype]
+    dtype = tf.__dict__[dtype]
     dev = default_device(dev)
-    with _tf.device(dev_from_str(dev)):
-        return _tf.eye(n, n, batch_shape=batch_shape, dtype=dtype)
+    with tf.device(dev_from_str(dev)):
+        return tf.eye(n, n, batch_shape=batch_shape, dtype=dtype)
 
 
-meshgrid = lambda *xs, indexing='ij': _tf.meshgrid(*xs, indexing=indexing)
+meshgrid = lambda *xs, indexing='ij': tf.meshgrid(*xs, indexing=indexing)
 
 
 
@@ -166,7 +161,7 @@ def dtype_from_str(dtype_in):
     return DTYPE_FROM_STR[dtype_in]
 
 
-compile = lambda fn, dynamic=True, example_inputs=None, static_argnums=None, static_argnames=None: _tf.function(fn)
+compile = lambda fn, dynamic=True, example_inputs=None, static_argnums=None, static_argnames=None: tf.function(fn)
 current_framework_str = lambda: 'tensorflow'
 current_framework_str.__name__ = 'current_framework_str'
 
