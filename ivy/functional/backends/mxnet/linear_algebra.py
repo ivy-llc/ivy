8c1bd8c8b790db73c90dbb65c32656b7376697e2
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-29 10:39:06 +0100

    refac: general. cross to linear_algebra.

--- ivy/functional/backends/mxnet/linear_algebra.py
+++ ivy/functional/backends/mxnet/linear_algebra.py
@@ -149,6 +149,21 @@ def matmul(x1, x2):
         return mx.nd.reshape(res, list(x1_shape[:-1]) + [res.shape[-1]])
     return res
 
+
+def cross(x1, x2):
+    a1 = x1[..., 0:1]
+    a2 = x1[..., 1:2]
+    a3 = x1[..., 2:3]
+    b1 = x2[..., 0:1]
+    b2 = x2[..., 1:2]
+    b3 = x2[..., 2:3]
+    res1 = a2*b3 - a3*b2
+    res2 = a3*b1 - a1*b3
+    res3 = a1*b2 - a2*b1
+    res = mx.nd.concat(res1, res2, res3, dim=-1)
+    return res
+
+
 # Extra #
 # ------#
 
--- ivy/functional/backends/mxnet/old/general.py
+++ ivy/functional/backends/mxnet/old/general.py
@@ -128,18 +128,6 @@ astype = cast
 
 
 
-def cross(x1, x2):
-    a1 = x1[..., 0:1]
-    a2 = x1[..., 1:2]
-    a3 = x1[..., 2:3]
-    b1 = x2[..., 0:1]
-    b2 = x2[..., 1:2]
-    b3 = x2[..., 2:3]
-    res1 = a2*b3 - a3*b2
-    res2 = a3*b1 - a1*b3
-    res3 = a1*b2 - a2*b1
-    res = _mx.nd.concat(res1, res2, res3, dim=-1)
-    return res
 
 
 
