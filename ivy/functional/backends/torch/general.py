e93861cb150345dd0c3877b2c07890c69f28a507
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-03-29 14:46:38 +0100

    refac general: cleanup several funcs. dtype, dtype from/to str, compile, dtype_bits, abs, one-hot

--- ivy/functional/backends/torch/general.py
+++ ivy/functional/backends/torch/general.py
@@ -303,3 +303,58 @@ def shape(x, as_tensor=False) -> Union[torch.Tensor, List[int]]:
 
 def get_num_dims(x, as_tensor=False) -> Union[torch.Tensor, int]:
     return torch.tensor(len(x.shape)) if as_tensor else len(x.shape)
+
+
+def dtype_bits(dtype_in):
+    dtype_str = dtype_to_str(dtype_in)
+    if 'bool' in dtype_str:
+        return 1
+    return int(dtype_str.replace('torch.', '').replace('uint', '').replace('int', '').replace('bfloat', '').replace(
+        'float', ''))
+
+
+def dtype(x, as_str=False):
+    dt = x.dtype
+    if as_str:
+        return dtype_to_str(dt)
+    return dt
+
+
+def dtype_to_str(dtype_in):
+    if isinstance(dtype_in, str):
+        return dtype_in
+    return {torch.int8: 'int8',
+            torch.int16: 'int16',
+            torch.int32: 'int32',
+            torch.int64: 'int64',
+            torch.uint8: 'uint8',
+            torch.bfloat16: 'bfloat16',
+            torch.float16: 'float16',
+            torch.float32: 'float32',
+            torch.float64: 'float64',
+            torch.bool: 'bool'}[dtype_in]
+
+
+def dtype_from_str(dtype_in: str) -> torch.dtype:
+    if not isinstance(dtype_in, str):
+        return dtype_in
+    return {'int8': torch.int8,
+            'int16': torch.int16,
+            'int32': torch.int32,
+            'int64': torch.int64,
+            'uint8': torch.uint8,
+            'bfloat16': torch.bfloat16,
+            'float16': torch.float16,
+            'float32': torch.float32,
+            'float64': torch.float64,
+            'bool': torch.bool}[dtype_in]
+
+
+def compile(fn, dynamic=True, example_inputs=None, static_argnums=None, static_argnames=None):
+    if dynamic:
+        return torch.jit.script(fn)
+    return torch.jit.trace(fn, example_inputs)
+
+
+def current_framework_str():
+    return 'torch'
