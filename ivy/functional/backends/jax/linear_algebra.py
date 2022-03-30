99436ad79aab3b92227aeee4c32ddace20568d60
Abdelrahman Othman <abdelrahman.m.a.osman@gmail.com>
2022-03-30 21:14:51 +0200

    cross adhering to Array API Standard (#703)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -137,8 +137,11 @@ def cholesky(x: JaxArray,
 def eigvalsh(x: JaxArray) -> JaxArray:
     return jnp.linalg.eigvalsh(x)
 
-cross = jnp.cross
 
+def cross (x1: JaxArray,
+           x2: JaxArray,
+           axis:int = -1) -> JaxArray:
+    return jnp.cross(a= x1, b = x2, axis= axis)
 
 # Extra #
 # ------#
