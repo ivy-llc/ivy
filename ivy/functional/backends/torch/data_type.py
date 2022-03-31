522ee5995e0363fa3b304f941a1dd8912c3bb923
Aarsh Chaube <65212523+Aarsh2001@users.noreply.github.com>
2022-04-01 00:36:36 +0530

    astype adhering to Array API standard (#735)
    Co-authored-by: Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
    Former-commit-id: ff4134e13e3995a98bad64664ee107a366f9a0ae

--- ivy/functional/backends/torch/data_type.py
+++ ivy/functional/backends/torch/data_type.py
@@ -60,10 +60,20 @@ def result_type(*arrays_and_dtypes: Union[torch.tensor, torch.dtype]) -> torch.d
 def broadcast_to(x: torch.Tensor, shape: Tuple[int,...]) -> torch.Tensor:
     return torch.broadcast_to(x,shape)
 
-
 def cast(x, dtype_in: str):
-    dtype_val = ivy.dtype_from_str(dtype_in)
-    return x.type(dtype_val)
+     dtype_val = ivy.dtype_from_str(dtype_in)
+     return x.type(dtype_val)
 
-
-astype = cast
+def astype(x: torch.Tensor, dtype: torch.dtype, copy: bool = True)\
+     -> torch.Tensor:
+     if copy:
+         if x.dtype == dtype:
+             new_tensor = x.clone().detach()
+             return new_tensor
+     else:
+         if x.dtype == dtype:
+             return x
+         else:
+            new_tensor = x.clone().detach()
+            return new_tensor.to(dtype)
+     return x.to(dtype)
