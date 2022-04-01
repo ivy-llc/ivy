e1ab582095a7f236a23de2a9cb1c87319fc30314
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-04-01 16:23:59 +0100

    Update linear_algebra.py

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -23,7 +23,7 @@ cholesky = tf.linalg.cholesky
 
 
 def inv(x: Tensor) -> Tensor:
-    return tf.linalg.inv(tf.cast(x, tf.float32))
+    return tf.linalg.solve(x, tf.eye(x.shape[-1], batch_shape=x.shape[:-2]))
 
 
 def tensordot(x1: Tensor, x2: Tensor,
