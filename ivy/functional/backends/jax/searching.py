eab737687ca03c4db3f2e4ad811abfb236821cdd
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-04-02 10:25:32 +0100

    where adhering to Array API standard (#771)

--- ivy/functional/backends/jax/searching.py
+++ ivy/functional/backends/jax/searching.py
@@ -3,21 +3,26 @@ import jax.numpy as jnp
 from ivy.functional.backends.jax import JaxArray
 from typing import Optional
 
+
 def argmax(
-    x:JaxArray, 
-    axis:Optional[int]=None, 
-    out: Optional[JaxArray] = None, 
-    keepdims:bool= False
-) -> JaxArray: 
-    return jnp.argmax(x,axis=axis,out=out,keepdims=keepdims)
-
-def argmin(x : JaxArray,
+    x: JaxArray,
     axis: Optional[int] = None,
-    out: Optional[JaxArray] = None,
+    out: Optional[JaxArray] = None, 
     keepdims: bool = False
-    ) -> JaxArray:
+) -> JaxArray: 
+    return jnp.argmax(x, axis=axis, out=out, keepdims=keepdims)
 
-    ret = jnp.argmin(x,axis=axis,out=out,keepdims=keepdims)
-    return (ret)
 
-where = lambda condition, x1, x2: jnp.where(condition, x1, x2)
+def argmin(x: JaxArray,
+           axis: Optional[int] = None,
+           out: Optional[JaxArray] = None,
+           keepdims: bool = False)\
+        -> JaxArray:
+    return jnp.argmin(x, axis=axis, out=out, keepdims=keepdims)
+
+
+def where(condition: JaxArray,
+          x1: JaxArray,
+          x2: JaxArray)\
+        -> JaxArray:
+    return jnp.where(condition, x1, x2)
