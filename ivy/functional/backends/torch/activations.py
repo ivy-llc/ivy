76662c8c37557df683ca86e5b24e7ae81d0e710d
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-04-02 11:18:49 +0100

    redundant and private import cleanup for torch backend

--- ivy/functional/backends/torch/activations.py
+++ ivy/functional/backends/torch/activations.py
@@ -3,7 +3,7 @@ Collection of PyTorch activation functions, wrapped to fit Ivy syntax and signat
 """
 
 # global
-import numpy as _np
+import numpy as np
 import torch
 
 
@@ -18,7 +18,7 @@ def leaky_relu(x, alpha: float = 0.2):
 
 def gelu(x, approximate: bool = True):
     if approximate:
-        return 0.5 * x * (1 + torch.tanh(((2 / _np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
+        return 0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
     return torch.nn.functional.gelu(x)
 
 
