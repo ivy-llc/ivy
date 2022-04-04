dd65299a0def3a532f6397ccaca34da83b757aeb
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-04-04 11:42:25 +0100

    lars_update uncommented and passing
    Former-commit-id: c74a7b53adbae90e14a06fd67c1cdf36eac4143e

--- ivy_tests/test_ivy/test_functional/test_core/test_gradients.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_gradients.py
@@ -252,34 +252,34 @@ def test_layerwise_gradient_descent_update(ws_n_grads_n_lr_n_wsnew, dtype, tenso
         return
 
 
-# lars_update
-# @pytest.mark.parametrize(
-#     "ws_n_grads_n_lr_n_wsnew", [(Container({'a': [3.], 'b': [3.]}), Container({'a': [6.], 'b': [6.]}),
-#                                  Container({'a': [0.1], 'b': [0.2]}), Container({'a': [2.7], 'b': [2.4]}))])
-# @pytest.mark.parametrize(
-#     "dtype", ['float32'])
-# @pytest.mark.parametrize(
-#     "tensor_fn", [ivy.array, helpers.var_fn])
-# def test_lars_update(ws_n_grads_n_lr_n_wsnew, dtype, tensor_fn, dev, call):
-#     # smoke test
-#     ws_raw, dcdws_raw, lr_raw, ws_raw_new = ws_n_grads_n_lr_n_wsnew
-#     ws = ws_raw.map(lambda x, _: ivy.variable(ivy.array(x)))
-#     dcdws = dcdws_raw.map(lambda x, _: ivy.array(x))
-#     lr = lr_raw.map(lambda x, _: ivy.array(x))
-#     ws_true_new = ws_raw_new.map(lambda x, _: ivy.variable(ivy.array(x)))
-#     ws_new = ivy.lars_update(ws, dcdws, lr)
-#     # type test
-#     assert isinstance(ws_new, dict)
-#     # cardinality test
-#     for (w_new, w_true_new) in zip(ws_new.values(), ws_true_new.values()):
-#         assert w_new.shape == w_true_new.shape
-#     # value test
-#     for (w_new, w_true_new) in zip(ws_new.values(), ws_true_new.values()):
-#         assert np.allclose(ivy.to_numpy(w_new), ivy.to_numpy(w_true_new))
-#     # compilation test
-#     if call in [helpers.torch_call]:
-#         # pytorch scripting does not support internal function definitions
-#         return
+#lars_update
+@pytest.mark.parametrize(
+    "ws_n_grads_n_lr_n_wsnew", [(Container({'a': [3.], 'b': [3.]}), Container({'a': [6.], 'b': [6.]}),
+                                 Container({'a': [0.1], 'b': [0.2]}), Container({'a': [2.7], 'b': [2.4]}))])
+@pytest.mark.parametrize(
+    "dtype", ['float32'])
+@pytest.mark.parametrize(
+    "tensor_fn", [ivy.array, helpers.var_fn])
+def test_lars_update(ws_n_grads_n_lr_n_wsnew, dtype, tensor_fn, dev, call):
+    # smoke test
+    ws_raw, dcdws_raw, lr_raw, ws_raw_new = ws_n_grads_n_lr_n_wsnew
+    ws = ws_raw.map(lambda x, _: ivy.variable(ivy.array(x)))
+    dcdws = dcdws_raw.map(lambda x, _: ivy.array(x))
+    lr = lr_raw.map(lambda x, _: ivy.array(x))
+    ws_true_new = ws_raw_new.map(lambda x, _: ivy.variable(ivy.array(x)))
+    ws_new = ivy.lars_update(ws, dcdws, lr)
+    # type test
+    assert isinstance(ws_new, dict)
+    # cardinality test
+    for (w_new, w_true_new) in zip(ws_new.values(), ws_true_new.values()):
+        assert w_new.shape == w_true_new.shape
+    # value test
+    for (w_new, w_true_new) in zip(ws_new.values(), ws_true_new.values()):
+        assert np.allclose(ivy.to_numpy(w_new), ivy.to_numpy(w_true_new))
+    # compilation test
+    if call in [helpers.torch_call]:
+        # pytorch scripting does not support internal function definitions
+        return
 
 
 # adam_update
