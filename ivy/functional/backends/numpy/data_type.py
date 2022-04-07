3406b2e72d29fd4d0d154dabc6e618fbf8e6c58b
Matt <matthew-barrett@hotmail.co.uk>
2022-04-07 16:06:11 +0100

    move numpy dtype functions to data_type.py

--- ivy/functional/backends/numpy/data_type.py
+++ ivy/functional/backends/numpy/data_type.py
@@ -6,6 +6,48 @@ from typing import Union, Tuple
 import ivy
 
 
+DTYPE_TO_STR = {np.dtype('int8'): 'int8',
+                np.dtype('int16'): 'int16',
+                np.dtype('int32'): 'int32',
+                np.dtype('int64'): 'int64',
+                np.dtype('uint8'): 'uint8',
+                np.dtype('uint16'): 'uint16',
+                np.dtype('uint32'): 'uint32',
+                np.dtype('uint64'): 'uint64',
+                'bfloat16': 'bfloat16',
+                np.dtype('float16'): 'float16',
+                np.dtype('float32'): 'float32',
+                np.dtype('float64'): 'float64',
+                np.dtype('bool'): 'bool',
+
+                np.int8: 'int8',
+                np.int16: 'int16',
+                np.int32: 'int32',
+                np.int64: 'int64',
+                np.uint8: 'uint8',
+                np.uint16: 'uint16',
+                np.uint32: 'uint32',
+                np.uint64: 'uint64',
+                np.float16: 'float16',
+                np.float32: 'float32',
+                np.float64: 'float64',
+                np.bool_: 'bool'}
+
+DTYPE_FROM_STR = {'int8': np.dtype('int8'),
+                'int16': np.dtype('int16'),
+                'int32': np.dtype('int32'),
+                'int64': np.dtype('int64'),
+                'uint8': np.dtype('uint8'),
+                'uint16': np.dtype('uint16'),
+                'uint32': np.dtype('uint32'),
+                'uint64': np.dtype('uint64'),
+                'bfloat16': 'bfloat16',
+                'float16': np.dtype('float16'),
+                'float32': np.dtype('float32'),
+                'float64': np.dtype('float64'),
+                'bool': np.dtype('bool')}
+
+
 # noinspection PyShadowingBuiltins
 def iinfo(type: Union[np.dtype, str, np.ndarray])\
         -> np.iinfo:
@@ -71,3 +113,29 @@ def astype(x: np.ndarray, dtype: np.dtype, copy: bool = True)\
             new_tensor = np.copy(x)
             return new_tensor.astype(dtype)
     return x.astype(dtype)
+
+
+def dtype_bits(dtype_in):
+    dtype_str = dtype_to_str(dtype_in)
+    if 'bool' in dtype_str:
+        return 1
+    return int(dtype_str.replace('uint', '').replace('int', '').replace('bfloat', '').replace('float', ''))
+
+
+def dtype(x, as_str=False):
+    dt = x.dtype
+    if as_str:
+        return dtype_to_str(dt)
+    return dt
+
+
+def dtype_to_str(dtype_in):
+    if isinstance(dtype_in, str):
+        return dtype_in
+    return DTYPE_TO_STR[dtype_in]
+
+
+def dtype_from_str(dtype_in):
+    if not isinstance(dtype_in, str):
+        return dtype_in
+    return DTYPE_FROM_STR[dtype_in]
