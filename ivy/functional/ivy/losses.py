c58349508d8210518b7c2f5a1eb2c314454d8226
Jatin Prakash <47978882+bicycleman15@users.noreply.github.com>
2022-04-10 14:56:27 +0000

    add `out` to cross_entropy

--- ivy/functional/ivy/losses.py
+++ ivy/functional/ivy/losses.py
@@ -4,29 +4,54 @@ Collection of Ivy loss functions.
 
 # local
 import ivy
+from ivy import NativeArray, Array
+
+from typing import Union, Optional
 
 
 # Extra #
 # ------#
 
-def cross_entropy(true, pred, axis=-1, epsilon=1e-7):
+def cross_entropy(true : Union[Array, NativeArray], pred : Union[Array, NativeArray], axis : Optional[int] = -1, epsilon : Optional[float] = 1e-7, \
+    out: Optional[Union[Array, NativeArray]] = None) \
+    -> Union[Array, NativeArray]:
     """
     Computes cross entropy between predicted and true discrete distrubtions.
+    Note that ``true`` and ``pred`` must have the same shape.
 
-    :param true: True labels
-    :type true: array
-    :param pred: predicted labels.
-    :type pred: array
-    :param axis: The class dimension, default is -1.
-    :type axis: int, optional
-    :param epsilon: small constant to add to log functions, default is 1e-7
-    :type epsilon: float, optional
-    :return: The cross entropy loss
+    Parameters
+    ----------
+    true: array
+        True labels
+    pred: array
+        predicted labels
+    axis: int, optional
+        The class dimension, default is -1
+    epsilon: float, optional
+        small constant to add to log functions, default is 1e-7
+    out:
+        optional output array, for writing the result to. It must have a shape that the inputs broadcast to.
+
+    Returns
+    -------
+        The cross entropy loss
+
+    Examples
+    -------
+    >>> true = ivy.array([[1, 0, 0], [0, 1, 0]]) # shape (2, 3)
+    >>> pred = ivy.array([[0.8, 0.1, 0.1], [0.2, 0.7, 0.1]]) # shape (2, 3)
+    >>> loss = ivy.cross_entropy(true, pred)
     """
     pred = ivy.clip(pred, epsilon, 1 - epsilon)
     log_pred = ivy.log(pred)
     # noinspection PyUnresolvedReferences
-    return -ivy.sum(log_pred * true, axis)
+    ret = -ivy.sum(log_pred * true, axis)
+
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+
+    return ret
+
 
 
 # noinspection PyUnresolvedReferences
