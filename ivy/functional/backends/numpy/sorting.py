97899ae4f56705ba06467c2002b9b63c77214693
Matt <matthew-barrett@hotmail.co.uk>
2022-04-11 15:21:42 +0100

    added out for np: sort, argsort

--- ivy/functional/backends/numpy/sorting.py
+++ ivy/functional/backends/numpy/sorting.py
@@ -1,26 +1,37 @@
 # global
 import numpy as np
+from typing import Optional
+
+# local
+import ivy
 
 
 def argsort(x: np.ndarray,
             axis: int = -1,
             descending: bool = False,
-            stable: bool = True) \
+            stable: bool = True,
+            out: Optional[np.ndarray] = None) \
         -> np.ndarray:
     if descending:
-        return np.asarray(np.argsort(-1 * np.searchsorted(np.unique(x), x), axis, kind='stable'))
+        ret = np.asarray(np.argsort(-1 * np.searchsorted(np.unique(x), x), axis, kind='stable'))
     else:
-        return np.asarray(np.argsort(x, axis, kind='stable'))
+        ret = np.asarray(np.argsort(x, axis, kind='stable'))
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
 def sort(x: np.ndarray,
          axis: int = -1,
          descending: bool = False,
-         stable: bool = True) -> np.ndarray:
+         stable: bool = True,
+         out: Optional[np.ndarray] = None) -> np.ndarray:
     kind = "stable" if stable else "quicksort"
-    res = np.asarray(np.sort(x, axis=axis, kind=kind))
+    ret = np.asarray(np.sort(x, axis=axis, kind=kind))
     if descending:
-        return np.asarray((np.flip(res, axis)))
-    return res
+        ret = np.asarray((np.flip(ret, axis)))
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
