173f840b7ba10f41604e3250bb5200f50bdbaf29
sherry30 <65318415+sherry30@users.noreply.github.com>
2022-04-11 16:40:14 +0500

    'out' argument added for 'any' method

--- ivy/functional/ivy/utility.py
+++ ivy/functional/ivy/utility.py
@@ -32,6 +32,8 @@ def all(x: Union[ivy.Array, ivy.NativeArray],
         axis or axes along which to perform a logical AND reduction. By default, a logical AND reduction must be performed over the entire array. If a tuple of integers, logical AND reductions must be performed over multiple axes. A valid ``axis`` must be an integer on the interval ``[-N, N)``, where ``N`` is the rank (number of dimensions) of ``x``. If an ``axis`` is specified as a negative integer, the function must determine the axis along which to perform a reduction by counting backward from the last dimension (where ``-1`` refers to the last dimension). If provided an invalid ``axis``, the function must raise an exception. Default: ``None``.
     keepdims:
         If ``True``, the reduced axes (dimensions) must be included in the result as singleton dimensions, and, accordingly, the result must be compatible with the input array (see :ref:`broadcasting`). Otherwise, if ``False``, the reduced axes (dimensions) must not be included in the result. Default: ``False``.
+    out:
+        optional output array, for writing the result to. It must have a shape that the inputs broadcast to.
 
     Returns
     -------
@@ -44,7 +46,9 @@ def all(x: Union[ivy.Array, ivy.NativeArray],
 # noinspection PyShadowingBuiltins
 def any(x: Union[ivy.Array, ivy.NativeArray],
         axis: Optional[Union[int, Tuple[int], List[int]]] = None,
-        keepdims: bool = False)\
+        keepdims: bool = False,
+        out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        )\
         -> ivy.Array:
     """
     Tests whether any input array element evaluates to ``True`` along a specified axis.
@@ -63,13 +67,15 @@ def any(x: Union[ivy.Array, ivy.NativeArray],
         axis or axes along which to perform a logical OR reduction. By default, a logical OR reduction must be performed over the entire array. If a tuple of integers, logical OR reductions must be performed over multiple axes. A valid ``axis`` must be an integer on the interval ``[-N, N)``, where ``N`` is the rank (number of dimensions) of ``x``. If an ``axis`` is specified as a negative integer, the function must determine the axis along which to perform a reduction by counting backward from the last dimension (where ``-1`` refers to the last dimension). If provided an invalid ``axis``, the function must raise an exception. Default: ``None``.
     keepdims:
         If ``True``, the reduced axes (dimensions) must be included in the result as singleton dimensions, and, accordingly, the result must be compatible with the input array (see :ref:`broadcasting`). Otherwise, if ``False``, the reduced axes (dimensions) must not be included in the result. Default: ``False``.
+    out:
+        optional output array, for writing the result to. It must have a shape that the inputs broadcast to.
 
     Returns
     -------
     out:
         if a logical OR reduction was performed over the entire array, the returned array must be a zero-dimensional array containing the test result; otherwise, the returned array must be a non-zero-dimensional array containing the test results. The returned array must have a data type of ``bool``.
     """
-    return _cur_framework(x).any(x, axis, keepdims)
+    return _cur_framework(x).any(x, axis, keepdims, out=out)
 
 
 # Extra #
