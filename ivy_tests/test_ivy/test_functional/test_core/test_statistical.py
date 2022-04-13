7c333ed9a88e3b7e29c0c5472ce13071986fdfd0
Saeed Ashraf <saeedashrraf@gmail.com>
2022-04-13 02:37:47 +0200

    create out test for max

--- ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
@@ -6,6 +6,8 @@ import pytest
 
 # local
 import ivy
+
+
 # min
 @pytest.mark.parametrize(
     "dtype", ivy.all_dtype_strs)
@@ -34,6 +36,43 @@ def test_min(dtype, as_variable, with_out, native_array):
         ret = ivy.min(x, out=out)
     else:
         ret = ivy.min(x)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
+
+
+# max
+@pytest.mark.parametrize(
+    "dtype", ivy.all_dtype_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_max(dtype, as_variable, with_out, native_array):
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x = ivy.array([2, 3, 4], dtype=dtype)
+    out = ivy.array(4, dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x = ivy.variable(x)
+        out = ivy.variable(out)
+    if native_array:
+        x = x.data
+        out = out.data
+    if with_out:
+        ret = ivy.max(x, out=out)
+    else:
+        ret = ivy.max(x)
     if with_out:
         if not native_array:
             assert ret is out
