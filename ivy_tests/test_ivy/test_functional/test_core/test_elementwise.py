307225e62b6c49254e0d99113ecd0beb6d16a48d
Matt <matthew-barrett@hotmail.co.uk>
2022-04-13 11:05:31 +0100

    testing out for bitwise_right_shift

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -505,3 +505,43 @@ def test_bitwise_or(dtype, as_variable, with_out, native_array):
             # these frameworks do not support native inplace updates
             return
         assert ret.data is (out if native_array else out.data)
+
+
+# bitwise_right_shift
+@pytest.mark.parametrize(
+    "dtype", ivy.int_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_bitwise_right_shift(dtype, as_variable, with_out, native_array):
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x1 = ivy.array([0, 1, 1], dtype=dtype)
+    x2 = ivy.array([0, 1, 1], dtype=dtype)
+    out = ivy.array([2, 3, 4], dtype=dtype)
+    if as_variable:
+        # float inputs aren't used
+        pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x1 = ivy.variable(x1)
+        x2 = ivy.variable(x2)
+        out = ivy.variable(out)
+    if native_array:
+        x1 = x1.data
+        x2 = x2.data
+        out = out.data
+    if with_out:
+        ret = ivy.bitwise_right_shift(x1, x2, out=out)
+    else:
+        ret = ivy.bitwise_right_shift(x1, x2)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
