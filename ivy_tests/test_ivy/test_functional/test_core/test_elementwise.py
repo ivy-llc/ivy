f53f278197ae1a18f5fbe2dc56a952cb1ac01513
Matt <matthew-barrett@hotmail.co.uk>
2022-04-13 17:32:07 +0100

    testing out for round

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1674,3 +1674,44 @@ def test_remainder(dtype, as_variable, with_out, native_array):
             # these frameworks do not support native inplace updates
             return
         assert ret.data is (out if native_array else out.data)
+
+
+# round
+@pytest.mark.parametrize(
+    "dtype", ivy.all_dtype_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_round(dtype, as_variable, with_out, native_array):
+    if ivy.current_framework_str() == 'torch' and dtype == 'float16':
+        pytest.skip("torch round doesnt allow float16")
+    if ivy.current_framework_str() == 'tensorflow' and dtype == 'bfloat16':
+        pytest.skip("tf round doesnt allow bfloat16")
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x = ivy.array([2, 3, 4], dtype=dtype)
+    out = ivy.array([2, 3, 4], dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x = ivy.variable(x)
+        out = ivy.variable(out)
+    if native_array:
+        x = x.data
+        out = out.data
+    if with_out:
+        ret = ivy.round(x, out=out)
+    else:
+        ret = ivy.round(x)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
