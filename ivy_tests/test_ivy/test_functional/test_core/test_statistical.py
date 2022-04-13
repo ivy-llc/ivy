ab33ee982ca90987deb14687e7e9109dadc61e41
Saeed Ashraf <saeedashrraf@gmail.com>
2022-04-13 03:17:03 +0200

    create out test for var
    Former-commit-id: 7593999b2f9226a40cef155e1905ba3542c6ad56

--- ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
@@ -80,3 +80,40 @@ def test_max(dtype, as_variable, with_out, native_array):
             # these frameworks do not support native inplace updates
             return
         assert ret.data is (out if native_array else out.data)
+
+
+# var
+@pytest.mark.parametrize(
+    "dtype", ivy.float_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_var(dtype, as_variable, with_out, native_array):
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x = ivy.array([1, 10, 25], dtype=dtype)
+    out = ivy.array(98, dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x = ivy.variable(x)
+        out = ivy.variable(out)
+    if native_array:
+        x = x.data
+        out = out.data
+    if with_out:
+        ret = ivy.var(x, out=out)
+    else:
+        ret = ivy.var(x)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
