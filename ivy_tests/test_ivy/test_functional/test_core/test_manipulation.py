9b9b3f17ecbe2fa48d9238a125b1acf74dfdb50a
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-04-14 16:28:57 +0100

    testing out for tile

--- ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
@@ -352,3 +352,42 @@ def test_repeat(dtype, as_variable, with_out, native_array):
             # these frameworks do not support native inplace updates
             return
         assert ret.data is (out if native_array else out.data)
+
+
+# tile
+@pytest.mark.parametrize(
+    "dtype", ivy.all_dtype_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_tile(dtype, as_variable, with_out, native_array):
+    if ivy.current_framework_str() == 'tensorflow' and dtype == 'uint16':
+        pytest.skip("tf tile doesnt allow uint16")
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x = ivy.array([1, 2, 3], dtype=dtype)
+    out = ivy.array([2, 3, 4, 5, 6, 7], dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x = ivy.variable(x)
+        out = ivy.variable(out)
+    if native_array:
+        x = x.data
+        out = out.data
+    if with_out:
+        ret = ivy.tile(x, 2, out=out)
+    else:
+        ret = ivy.tile(x, 2)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
