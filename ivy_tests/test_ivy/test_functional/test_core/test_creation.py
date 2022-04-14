f7fcfb3d14a613a698b4d7f78ff054af896d669f
saeedashrraf <48128381+saeedashrraf@users.noreply.github.com>
2022-04-14 02:11:34 +0200

    Updating the docstring test

--- ivy_tests/test_ivy/test_functional/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_creation.py
@@ -7,14 +7,6 @@ import ivy
 from ivy_tests.test_ivy import helpers
 
 
-@pytest.mark.parametrize(
-    "shape", [(), (1, 2, 3), tuple([1]*10)])
-@pytest.mark.parametrize(
-    "dtype", ['float32'])
-@pytest.mark.parametrize(
-    "tensor_fn", [ivy.array, helpers.var_fn])
-def test_zeros(shape, dtype, tensor_fn, dev, call):
-    # smoke test
-    ret = ivy.zeros(shape, dtype, dev)
+def test_zeros():
     # docstring test
     helpers.assert_docstring_examples_run(ivy.zeros)
