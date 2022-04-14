837a4dd81bb0e821604f487f3992fff308d9942e
ਰਾਹੁਲ ਭੇਲੇ <rahulbhalley@icloud.com>
2022-04-15 05:04:35 +0530

    Reformat `softplus`, add docs, and docstring test (#852)

--- ivy/functional/backends/numpy/activations.py
+++ ivy/functional/backends/numpy/activations.py
@@ -42,4 +42,6 @@ def softmax(x, axis=-1):
     return exp_x / np.sum(exp_x, axis, keepdims=True)
 
 
-softplus = lambda x: np.log(np.exp(x) + 1)
+def softplus(x: np.ndarray)\
+        -> np.ndarray:
+    return np.log(np.exp(x) + 1)
