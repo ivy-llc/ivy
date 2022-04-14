28bd1b84ca6acc61879cf9113e2ca84f00ed4fcc
Allaye <kolade199@gmail.com>
2022-04-14 15:07:36 +0000

    removed docstring from numpy backend function

--- ivy/functional/backends/numpy/image.py
+++ ivy/functional/backends/numpy/image.py
@@ -13,22 +13,6 @@ from ivy.functional.backends import numpy as _ivy
 
 
 def stack_images(images: List[np.ndarray], desired_aspect_ratio: Tuple[int, int] = (1, 1)) -> np.ndarray:
-    """
-      Stacks a group of images in matrix's into a combined windowed image, fitting the desired aspect ratio as closely as possible.
-
-       Parameters
-       ----------
-       images:
-           Sequence of image arrays to be stacked *[batch_shape,height,width,dims]*
-
-       desired_aspect_ratio:
-          desired aspect ratio of the stacked image
-
-       Returns
-       -------
-      out:
-         an ndarray containing the stacked images in the specified dimensions
-    """
     num_images = len(images)
     if num_images == 0:
         raise Exception('At least 1 image must be provided')
