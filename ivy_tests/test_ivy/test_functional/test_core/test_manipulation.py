8a37bb0925069e6bf1268a31f3b35d83405c1b6b
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-04-14 16:08:03 +0100

    testing out for squeeze

--- ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
@@ -232,3 +232,40 @@ def test_roll(dtype, as_variable, with_out, native_array):
             # these frameworks do not support native inplace updates
             return
         assert ret.data is (out if native_array else out.data)
+
+
+# squeeze
+@pytest.mark.parametrize(
+    "dtype", ivy.all_dtype_strs)
+@pytest.mark.parametrize(
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_squeeze(dtype, as_variable, with_out, native_array):
+    if dtype in ivy.invalid_dtype_strs:
+        pytest.skip("invalid dtype")
+    x = ivy.array([[[1, 2], [3, 4]]], dtype=dtype)
+    out = ivy.array([[2, 3], [4, 5]], dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            pytest.skip("only floating point variables are supported")
+        if with_out:
+            pytest.skip("variables do not support out argument")
+        x = ivy.variable(x)
+        out = ivy.variable(out)
+    if native_array:
+        x = x.data
+        out = out.data
+    if with_out:
+        ret = ivy.squeeze(x, 0, out=out)
+    else:
+        ret = ivy.squeeze(x, 0)
+    if with_out:
+        if not native_array:
+            assert ret is out
+        if ivy.current_framework_str() in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
