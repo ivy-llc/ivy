da95743f2ac35f3aecd6f2f1771ac1732e15c112
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-15 16:27:25 +0100

    added tuples for valid and invalid ints and floats for each backend.

--- ivy/functional/backends/tensorflow/__init__.py
+++ ivy/functional/backends/tensorflow/__init__.py
@@ -33,12 +33,22 @@ bool = tf.bool
 
 valid_dtypes = (int8, int16, int32, int64,
                 uint8, uint16, uint32, uint64,
-                bfloat16, float16, float32, float64)
+                bfloat16, float16, float32, float64,
+                bool)
 
+# valid
 valid_dtype_strs = ('int8', 'int16', 'int32', 'int64',
                     'uint8', 'uint16', 'uint32', 'uint64',
-                    'bfloat16', 'float16', 'float32', 'float64')
+                    'bfloat16', 'float16', 'float32', 'float64',
+                    'bool')
+valid_int_dtype_strs = ('int8', 'int16', 'int32', 'int64',
+                        'uint8', 'uint16', 'uint32', 'uint64')
+valid_float_dtype_strs = ('bfloat16', 'float16', 'float32', 'float64')
+
+# invalid
 invalid_dtype_strs = ()
+invalid_int_dtype_strs = ()
+invalid_float_dtype_strs = ()
 
 
 def closest_valid_dtype(type):
