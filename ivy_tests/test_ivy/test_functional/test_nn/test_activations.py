cb25dce0320cf6028fd25fed9b4fe8b4775de165
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-16 20:12:24 +0100

    added out argument to relu method.

--- ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
@@ -5,6 +5,7 @@ Collection of tests for unified neural network activation functions
 # global
 import pytest
 import numpy as np
+from hypothesis import given, strategies as st
 
 # local
 import ivy
@@ -16,21 +17,41 @@ import ivy_tests.test_ivy.helpers as helpers
 @pytest.mark.parametrize(
     "x", [[[-1., 1., 2.]]])
 @pytest.mark.parametrize(
-    "dtype", ['float32'])
+    "dtype", ivy.all_numeric_dtype_strs)
 @pytest.mark.parametrize(
-    "tensor_fn", [ivy.array, helpers.var_fn])
-def test_relu(x, dtype, tensor_fn, dev, call):
-    # smoke test
-    x = tensor_fn(x, dtype, dev)
+    "as_variable", [True, False])
+@pytest.mark.parametrize(
+    "with_out", [True, False])
+@pytest.mark.parametrize(
+    "native_array", [True, False])
+def test_relu(x, dtype, as_variable, with_out, native_array, fw):
+    if dtype in ivy.invalid_dtype_strs:
+        return  # invalid dtype
+    if dtype == 'float16' and fw == 'torch':
+        return  # torch does not support float16 for relu
+    x = ivy.array(x, dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            return  # only floating point variables are supported
+        if with_out:
+            return  # variables do not support out argument
+        x = ivy.variable(x)
+    if native_array:
+        x = x.data
     ret = ivy.relu(x)
-    # type test
-    assert ivy.is_native_array(ret)
-    # cardinality test
-    assert ret.shape == x.shape
-    # value test
-    assert np.allclose(call(ivy.relu, x), ivy.functional.backends.numpy.relu(ivy.to_numpy(x)))
-    # docstring test
-    helpers.assert_docstring_examples_run(ivy.relu)
+    out = ret
+    if with_out:
+        if as_variable:
+            out = ivy.variable(out)
+        if native_array:
+            out = out.data
+        ret = ivy.relu(x, out=out)
+        if not native_array:
+            assert ret is out
+        if fw in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
 
 
 # leaky_relu
