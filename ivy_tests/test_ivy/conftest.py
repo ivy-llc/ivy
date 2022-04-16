944df62322934853bff57cfd6799ca21d1c6ac25
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-16 15:05:44 +0100

    increased hypothesis max test examples from 10 to 100.
    Former-commit-id: ad5d73b65421519876bf76587abc685c245501d0

--- ivy_tests/test_ivy/conftest.py
+++ ivy_tests/test_ivy/conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from typing import Dict
 from hypothesis import settings
-settings.register_profile("default", max_examples=10)
+settings.register_profile("default", max_examples=100)
 settings.load_profile("default")
 
 # local
