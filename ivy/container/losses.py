d5e6809d5bc30857332c92b05ce078d62a9b5f78
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-17 19:22:00 +0100

    made a start on refactoring ivy.Container methods into seperate files.

--- ivy/container/losses.py
+++ ivy/container/losses.py
@@ -4,8 +4,9 @@ from ivy.container.base import ContainerBase
 # ToDo: implement all methods here as public instance methods
 
 
+# noinspection PyMissingConstructor
 class ContainerWithLosses(ContainerBase):
 
     def __init__(self):
         import ivy.functional.ivy.losses as losses
-        ContainerBase.__init__(self, losses)
+        ContainerBase.add_instance_methods(self, losses)
--- ivy/container/manipulation.py
+++ ivy/container/manipulation.py
@@ -4,8 +4,9 @@ from ivy.container.base import ContainerBase
 # ToDo: implement all methods here as public instance methods
 
 
+# noinspection PyMissingConstructor
 class ContainerWithManipulation(ContainerBase):
 
     def __init__(self):
         import ivy.functional.ivy.manipulation as manipulation
-        ContainerBase.__init__(self, manipulation, to_ignore=['expand_dims', 'split', 'repeat', 'swapaxes', 'reshape'])
+        ContainerBase.add_instance_methods(self, manipulation, to_ignore=['expand_dims', 'split', 'repeat', 'swapaxes', 'reshape'])
