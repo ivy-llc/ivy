ac5f92fdaa931059cb86084f7eeb3ea392a7214a
pawasRituja <ritujapawas.djr@gmail.com>
2022-04-17 18:34:14 +0530

    Reformat ones and ones_like in creation_functions (#907)
    Co-authored-by: sherry30 <sherrytst30@gmail.com>

--- ivy_tests/test_ivy/test_functional/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_creation.py
@@ -10,3 +10,13 @@ from ivy_tests.test_ivy import helpers
 def test_zeros():
     # docstring test
     helpers.assert_docstring_examples_run(ivy.zeros)
+
+
+def test_ones():
+    # docstring test
+    helpers.assert_docstring_examples_run(ivy.ones)
+
+
+def test_ones_like():
+    # docstring test
+    helpers.assert_docstring_examples_run(ivy.ones_like)
