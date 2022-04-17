e9242f2edcd31227c69587f8627a09f8f8c641d1
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-17 22:11:09 +0100

    modified ivy.Array so the instance methods are all programmatically added at import time, rather than in the constructor.

--- ivy/array/manipulation.py
+++ ivy/array/manipulation.py
@@ -1,11 +1,8 @@
-# local
-from ivy.array.base import ArrayBase
+# global
+import abc
 
 # ToDo: implement all methods here as public instance methods
 
 
-class ArrayWithManipulation(ArrayBase):
-
-    def __init__(self):
-        import ivy.functional.ivy.manipulation as manipulation
-        ArrayBase.__init__(self, manipulation)
+class ArrayWithManipulation(abc.ABC):
+    pass
--- ivy/array/norms.py
+++ ivy/array/norms.py
@@ -1,11 +1,8 @@
-# local
-from ivy.array.base import ArrayBase
+# global
+import abc
 
 # ToDo: implement all methods here as public instance methods
 
 
-class ArrayWithNorms(ArrayBase):
-
-    def __init__(self):
-        import ivy.functional.ivy.norms as norms
-        ArrayBase.__init__(self, norms)
+class ArrayWithNorms(abc.ABC):
+    pass
