04908d9d53e8cce9a16aedbd085e8c60cac3c53b
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-04-19 18:26:08 +0530

    docstring updated to numpydoc style

--- ivy/functional/ivy/activations.py
+++ ivy/functional/ivy/activations.py
@@ -66,64 +66,76 @@ def leaky_relu(x: Union[ivy.Array, ivy.NativeArray], alpha: Optional[float] = 0.
 
 
 def gelu(x, approximate=True):
-    """
-    Applies the Gaussian error linear unit (GELU) activation function.
+    """Applies the Gaussian error linear unit (GELU) activation function.
 
-    :param x: Input array.
-    :type x: array
-    :param approximate: Whether to approximate, default is True.
-    :type approximate: bool, optional
-    :return: The input array with leaky relu applied element-wise.
+    Parameters
+    ----------
+    x : array
+        Input array.
+    approximate : bool, optional
+        Whether to approximate, default is True.
+
+    Returns
+    -------
+    type
+        The input array with leaky relu applied element-wise.
+
     """
     return _cur_framework(x).gelu(x, approximate)
 
 
 def tanh(x: Union[ivy.Array, ivy.NativeArray])\
         -> ivy.Array:
-    """
-    Applies the Hyperbolic tangent activation function element-wise.
+    """Applies the Hyperbolic tangent activation function element-wise.
 
     Parameters
-     ----------
-    x:
-         input array
+    ----------
+    x :        
 
     Returns
     -------
-    ivy.Array
-        The input array with Hyperbolic tangent activation applied element-wise.
 
-    Examples:
-
+    
     >>> x = ivy.array([0.55 , -0.55])
     >>> y = ivy.tanh(x)
     >>> print(y)
     [0.50, -0.50]
-
     """
     return _cur_framework(x).tanh(x)
 
 
 def sigmoid(x):
-    """
-    Applies the sigmoid function element-wise.
+    """Applies the sigmoid function element-wise.
 
-    :param x: Input array.
-    :type x: array
-    :return: The input array with sigmoid applied element-wise.
+    Parameters
+    ----------
+    x : array
+        Input array.
+
+    Returns
+    -------
+    type
+        The input array with sigmoid applied element-wise.
+
     """
     return _cur_framework(x).sigmoid(x)
 
 
 def softmax(x, axis=-1):
-    """
-    Applies the softmax function element-wise.
+    """Applies the softmax function element-wise.
 
-    :param x: Input array.
-    :type x: array
-    :param axis: The dimension softmax would be performed on. The default is -1 which indicates the last dimension.
-    :type axis: int, optional
-    :return: The input array with softmax applied element-wise.
+    Parameters
+    ----------
+    x : array
+        Input array.
+    axis : int, optional
+        The dimension softmax would be performed on. The default is -1 which indicates the last dimension.
+
+    Returns
+    -------
+    type
+        The input array with softmax applied element-wise.
+
     """
     return _cur_framework(x).softmax(x, axis)
 
