85b85a6e6dee396a4062c710ffb03f6c5bb83675
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-04-21 16:22:13 +0100

    unique_all adhering to Array API standard (#966)
    Co-authored-by: Arkhymadhe <emoekabuoshomah@gmail.com>

--- ivy/functional/backends/torch/set.py
+++ ivy/functional/backends/torch/set.py
@@ -5,6 +5,35 @@ from collections import namedtuple
 
 import ivy
 
+
+def unique_all(x: torch.Tensor, /) \
+        -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    Results = namedtuple(typename='unique_all', field_names=['values', 'indices', 'inverse_indices', 'counts'])
+
+    outputs, inverse_indices, counts = torch.unique(x, sorted=True, return_inverse=True,
+                                                    return_counts=True, dim=None)
+
+    flat_tensor = x.flatten()
+    unique_nan = torch.isnan(outputs)
+    idx_dtype = inverse_indices.dtype
+
+    if torch.any(unique_nan):
+        nan_index = torch.where(torch.isnan(flat_tensor))
+        non_nan_index = [flat_tensor.tolist().index(val) for val in outputs if not torch.isnan(val)]
+
+        indices = outputs.clone().to(idx_dtype)
+
+        indices[unique_nan] = nan_index[0]
+        inverse_indices[torch.isnan(x)] = torch.where(unique_nan)[0][0]
+        counts[unique_nan] = 1
+        indices[~unique_nan] = torch.tensor(non_nan_index, dtype=idx_dtype)
+
+    else:
+        indices = torch.tensor([torch.where(flat_tensor == val)[0][0] for val in outputs], dtype=idx_dtype)
+
+    return Results(outputs.to(x.dtype), indices.view(outputs.shape), inverse_indices.reshape(x.shape), counts)
+
+
 def unique_inverse(x: torch.Tensor) \
         -> Tuple[torch.Tensor, torch.Tensor]:
     out = namedtuple('unique_inverse', ['values', 'inverse_indices'])
