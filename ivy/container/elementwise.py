13b733b0afb14a496beb3a37223401f5f906b07b
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-21 01:57:01 +0100

    added abs as an instance method for ivy.Container explicitly.

--- ivy/container/elementwise.py
+++ ivy/container/elementwise.py
@@ -1,9 +1,21 @@
+# global
+from typing import Optional, Union, List, Dict
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
 
 
-# noinspection PyMissingConstructor
+# noinspection PyMissingConstructor,PyMethodParameters
 class ContainerWithElementwise(ContainerBase):
-    pass
+
+    def abs(x: ContainerBase,
+            key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+            to_apply: bool = True,
+            prune_unapplied: bool = False,
+            out: Optional[ContainerBase] = None) \
+            -> ContainerBase:
+        return x.handle_inplace(
+            x.map(lambda x_, _: ivy.abs(x_) if ivy.is_array(x_) else x_, key_chains, to_apply, prune_unapplied), out)
