5cb9499f6f079349a89e045a465ed9832a6ba586
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-21 01:04:33 +0100

    small fix to method add_ivy_array_instance_methods.

--- ivy/array/wrapping.py
+++ ivy/array/wrapping.py
@@ -22,7 +22,7 @@ def add_ivy_array_instance_methods(cls, modules, to_ignore=()):
     for module in modules:
         for key, val in module.__dict__.items():
             if key.startswith('_') or key[0].isupper() or not callable(val) or \
-                    key in cls.__dict__ or key in to_ignore or key not in ivy.__dict__:
+                    key in cls.__dict__ or hasattr(cls, key) or key in to_ignore or key not in ivy.__dict__:
                 continue
             try:
                 setattr(cls, key, _wrap_fn(key))
