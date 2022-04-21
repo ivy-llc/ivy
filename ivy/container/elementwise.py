e063af735895171edfc687659dbf4f29d2e21d4e
djl11 <daniel.lenton11@imperial.ac.uk>
2022-04-21 11:11:48 +0100

    updated ivy.Container abs method to use self for first argument, rather than x.

--- ivy/container/elementwise.py
+++ ivy/container/elementwise.py
@@ -8,14 +8,14 @@ from ivy.container.base import ContainerBase
 # ToDo: implement all methods here as public instance methods
 
 
-# noinspection PyMissingConstructor,PyMethodParameters
+# noinspection PyMissingConstructor
 class ContainerWithElementwise(ContainerBase):
 
-    def abs(x: ivy.Container,
+    def abs(self: ivy.Container,
             key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
             to_apply: bool = True,
             prune_unapplied: bool = False,
             out: Optional[ivy.Container] = None) \
             -> ivy.Container:
-        return x.handle_inplace(
-            x.map(lambda x_, _: ivy.abs(x_) if ivy.is_array(x_) else x_, key_chains, to_apply, prune_unapplied), out)
+        return self.handle_inplace(
+            self.map(lambda x_, _: ivy.abs(x_) if ivy.is_array(x_) else x_, key_chains, to_apply, prune_unapplied), out)
