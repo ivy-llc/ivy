c482827e05856ba5a94be605efd60972ef3b3b0b
Albert Jialeng Guo <sub.jl@icloud.com>
2022-04-22 15:44:51 +0100

    Reformatted array_equal (#871)
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>

--- ivy/functional/backends/tensorflow/general.py
+++ ivy/functional/backends/tensorflow/general.py
@@ -27,8 +27,13 @@ def is_native_array(x, exclusive=False):
 
 
 copy_array = tf.identity
-array_equal = tf.experimental.numpy.array_equal
 
+
+def array_equal (x0: Tensor, x1: Tensor) \
+        -> bool:
+    return tf.experimental.numpy.array_equal(x0, x1)
+
+  
 def to_numpy(x: Tensor) \
         -> _np.ndarray:
     return _np.asarray(tf.convert_to_tensor(x))
@@ -38,6 +43,7 @@ def to_scalar(x: Tensor) \
         -> Number:
     return to_numpy(x).item()
 
+  
 def to_list(x: Tensor) \
         ->list:
     return x.numpy().tolist()
