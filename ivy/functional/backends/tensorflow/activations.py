92422970a9fb085bffd41ec94b15e45f332b10a4
Ereej <47918936+Erij97@users.noreply.github.com>
2022-04-23 04:21:32 +0100

    Reformatted sigmoid activation (#851)
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>

--- ivy/functional/backends/tensorflow/activations.py
+++ ivy/functional/backends/tensorflow/activations.py
@@ -12,6 +12,7 @@ from tensorflow.python.types.core import Tensor
 import ivy
 
 
+
 def relu(x: Tensor,
          out: Optional[Tensor] = None)\
         -> Tensor:
@@ -21,21 +22,23 @@ def relu(x: Tensor,
     return ret
 
 
-def leaky_relu(x: Tensor, alpha: Optional[float] = 0.2)\
+def leaky_relu(x: Tensor, alpha: Optional[float] = 0.2) \
         -> Tensor:
     return tf.nn.leaky_relu(x, alpha)
 
 
 gelu = lambda x, approximate=True: tf.nn.gelu(x, approximate)
 
+
+def sigmoid(x):
+    return tf.nn.sigmoid(x)
+
+
 def tanh(x: Tensor)\
         -> Tensor:
     return tf.nn.tanh(x)
 
-
-sigmoid = tf.nn.sigmoid
-
-
+  
 def softmax(x: Tensor, axis: Optional[int] = -1)\
     -> Tensor:
     return tf.nn.softmax(x, axis)
