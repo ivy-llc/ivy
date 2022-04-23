a21bcb95b6f05ff06bc5005d676e173cc71fac85
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-04-23 09:06:49 +0100

    Update test_linalg.py

--- ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
@@ -53,6 +53,3 @@ def test_matmul(dtype, as_variable, with_out, num_positional_args, native_array,
         rtol=1e-02, atol=1e-02,
         x1 = np.random.uniform(size=(a, b)).astype(dtype[0]),
         x2 = np.random.uniform(size=(b, c)).astype(dtype[1]))
-    
-def test_matrix_rank() :
-    assert helpers.docstring_examples_run(ivy.matrix_rank) == True
