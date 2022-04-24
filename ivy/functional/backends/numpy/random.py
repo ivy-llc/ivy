2f01efd581d0d5b81c1c8c770b4c81d5d2003c01
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-04-24 14:51:27 +0100

    reformatted random_uniform backends
    Former-commit-id: 1de98cb4234b59620c850c6494de549e8c380089

--- ivy/functional/backends/numpy/random.py
+++ ivy/functional/backends/numpy/random.py
@@ -3,32 +3,41 @@ Collection of Numpy random functions, wrapped to fit Ivy syntax and signature.
 """
 
 # global
-import numpy as _np
+import numpy as np
+from typing import Optional, Union, Tuple
 
+# local
+import ivy
 
 # Extra #
 # ------#
 
-random_uniform = lambda low=0., high=1., shape=None, dev=None: _np.asarray(_np.random.uniform(low, high, shape))
-random_normal = lambda mean=0., std=1., shape=None, dev=None: _np.asarray(_np.random.normal(mean, std, shape))
+def random_uniform(low: float = 0.0,
+                   high: float = 1.0,
+                   shape: Optional[Union[int, Tuple[int, ...]]] = None,
+                   dev: Optional[ivy.Device] = None) -> np.ndarray:
+    return np.asarray(np.random.uniform(low, high, shape))
+
+
+random_normal = lambda mean=0., std=1., shape=None, dev=None: np.asarray(np.random.normal(mean, std, shape))
 
 
 def multinomial(population_size, num_samples, batch_size, probs=None, replace=True, dev=None):
     if probs is None:
-        probs = _np.ones((batch_size, population_size,)) / population_size
+        probs = np.ones((batch_size, population_size,)) / population_size
     orig_probs_shape = list(probs.shape)
     num_classes = orig_probs_shape[-1]
-    probs_flat = _np.reshape(probs, (-1, orig_probs_shape[-1]))
-    probs_flat = probs_flat / _np.sum(probs_flat, -1, keepdims=True)
-    probs_stack = _np.split(probs_flat, probs_flat.shape[0])
-    samples_stack = [_np.random.choice(num_classes, num_samples, replace, p=prob[0]) for prob in probs_stack]
-    samples_flat = _np.stack(samples_stack)
-    return _np.asarray(_np.reshape(samples_flat, orig_probs_shape[:-1] + [num_samples]))
+    probs_flat = np.reshape(probs, (-1, orig_probs_shape[-1]))
+    probs_flat = probs_flat / np.sum(probs_flat, -1, keepdims=True)
+    probs_stack = np.split(probs_flat, probs_flat.shape[0])
+    samples_stack = [np.random.choice(num_classes, num_samples, replace, p=prob[0]) for prob in probs_stack]
+    samples_flat = np.stack(samples_stack)
+    return np.asarray(np.reshape(samples_flat, orig_probs_shape[:-1] + [num_samples]))
 
 
-randint = lambda low, high, shape, dev=None: _np.random.randint(low, high, shape)
-seed = lambda seed_value=0: _np.random.seed(seed_value)
+randint = lambda low, high, shape, dev=None: np.random.randint(low, high, shape)
+seed = lambda seed_value=0: np.random.seed(seed_value)
 
 
 def shuffle(x):
-    return _np.random.permutation(x)
+    return np.random.permutation(x)
