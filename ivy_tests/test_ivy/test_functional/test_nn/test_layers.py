1a20459acc75dfda14fac6a373cabfc4f0490f2e
Aarsh2001 <ac5973@srmist.edu.in>
2022-04-24 11:21:50 +0530

    test for nightly

--- ivy_tests/test_ivy/test_functional/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_layers.py
@@ -259,9 +259,6 @@ def test_conv1d_transpose(x_n_filters_n_pad_n_outshp_n_res, dtype, tensor_fn, de
 @pytest.mark.parametrize(
     "tensor_fn", [ivy.array, helpers.var_fn])
 def test_conv2d(x_n_filters_n_pad_n_res, dtype, tensor_fn, dev, call):
-    if call in [helpers.tf_call, helpers.tf_graph_call] and 'cpu' in dev:
-        # tf conv2d does not work when CUDA is installed, but array is on CPU
-        pytest.skip()
     # smoke test
     x, filters, padding, true_res = x_n_filters_n_pad_n_res
     x = tensor_fn(x, dtype, dev)
