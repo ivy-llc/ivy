810a398fb94ab8311cf933f079b92ca9309ece60
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-04-25 12:59:43 +0100

    reformatted files in ivy, container & array folders to numpy style (#965)

--- ivy/functional/ivy/random.py
+++ ivy/functional/ivy/random.py
@@ -52,78 +52,106 @@ def random_uniform(low: float = 0.0,
 
 
 def random_normal(mean=0.0, std=1.0, shape=None, dev=None):
-    """
-    Draws samples from a normal distribution.
+    """Draws samples from a normal distribution.
 
-    :param mean: The mean of the normal distribution to sample from. Default is 0.
-    :type mean: float
-    :param std: The standard deviation of the normal distribution to sample from. Default is 1.
-    :type std: float
-    :param shape: Output shape. If the given shape is, e.g., (m, n, k), then m * n * k samples are drawn.
-                    If size is None (default), a single value is returned.
-    :param dev: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
-    :type dev: ivy.Device
-    :return: Drawn samples from the parameterized uniform distribution.
+    Parameters
+    ----------
+    mean
+        The mean of the normal distribution to sample from. Default is 0.
+    std
+        The standard deviation of the normal distribution to sample from. Default is 1.
+    shape
+        Output shape. If the given shape is, e.g., (m, n, k), then m * n * k samples are drawn.
+        If size is None (default), a single value is returned.
+    dev
+        device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc. (Default value = None)
+
+    Returns
+    -------
+     ret
+        Drawn samples from the parameterized uniform distribution.
+
     """
     return _cur_framework().random_normal(mean, std, shape, dev)
 
 
 def multinomial(population_size, num_samples, batch_size, probs=None, replace=True, dev=None):
-    """
-    Draws samples from a multinomial distribution. Specifcally, returns a tensor where each row contains num_samples
+    """Draws samples from a multinomial distribution. Specifcally, returns a tensor where each row contains num_samples
     indices sampled from the multinomial probability distribution located in the corresponding row of tensor input.
 
-    :param population_size: The size of the population from which to draw samples.
-    :type population_size: int
-    :param num_samples: Number of independent samples to draw from the population.
-    :type num_samples: int
-    :param batch_size: Number of times to draw a new set of samples from the population.
-    :type num_samples: int
-    :param probs: The unnormalized probabilities for all elemtns in population,
-                        default is uniform *[batch_shape, num_classes]*
-    :type probs: array, optional
-    :param replace: Whether to replace samples once they've been drawn. Default is True.
-    :type replace: bool, optional
-    :param dev: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
-    :type dev: ivy.Device
-    :return: Drawn samples indices from the multinomial distribution.
+    Parameters
+    ----------
+    population_size
+        The size of the population from which to draw samples.
+    num_samples
+        Number of independent samples to draw from the population.
+    batch_size
+        Number of times to draw a new set of samples from the population.
+    probs
+        The unnormalized probabilities for all elemtns in population,
+        default is uniform *[batch_shape, num_classes]*
+    replace
+        Whether to replace samples once they've been drawn. Default is True.
+    dev
+        device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc. (Default value = None)
+
+    Returns
+    -------
+     ret
+        Drawn samples indices from the multinomial distribution.
+
     """
     return _cur_framework().multinomial(population_size, num_samples, batch_size, probs, replace, dev)
 
 
 def randint(low, high, shape, dev=None):
-    """
-    Returns a tensor filled with random integers generated uniformly between low (inclusive) and high (exclusive).
+    """Returns a tensor filled with random integers generated uniformly between low (inclusive) and high (exclusive).
 
-    :param low: Lowest integer to be drawn from the distribution.
-    :type low: int
-    :param high: One above the highest integer to be drawn from the distribution.
-    :type high: int
-    :param shape: a tuple defining the shape of the output tensor.
-    :type shape: sequence of ints
-    :param dev: device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
-    :type dev: ivy.Device
-    :return:
+    Parameters
+    ----------
+    low
+        Lowest integer to be drawn from the distribution.
+    high
+        One above the highest integer to be drawn from the distribution.
+    shape
+        a tuple defining the shape of the output tensor.
+    dev
+        device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc. (Default value = None)
+
+    Returns
+    -------
+
     """
     return _cur_framework().randint(low, high, shape, dev)
 
 
 def seed(seed_value=0):
-    """
-    Sets the seed for random number generation.
+    """Sets the seed for random number generation.
 
-    :param seed_value: Seed for random number generation, must be a positive integer.
-    :type seed_value: int
+    Parameters
+    ----------
+    seed_value
+        Seed for random number generation, must be a positive integer. (Default value = 0)
+
+    Returns
+    -------
+
     """
     return _cur_framework().seed(seed_value)
 
 
 def shuffle(x):
-    """
-    Shuffles the given array along axis 0.
+    """Shuffles the given array along axis 0.
 
-    :param x: An array object, in the specific Machine learning framework.
-    :type x: array
-    :return: An array object, shuffled along the first dimension.
+    Parameters
+    ----------
+    x
+        An array object, in the specific Machine learning framework.
+
+    Returns
+    -------
+     ret
+        An array object, shuffled along the first dimension.
+
     """
     return _cur_framework(x).shuffle(x)
