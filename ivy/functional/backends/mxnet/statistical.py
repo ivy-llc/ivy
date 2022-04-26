eb026cb12f3f6466928067d1fe698cc7f8d04f13
HotzingTone <adam.he.future@gmail.com>
2022-04-26 16:03:21 +0100

    Revert "Fix errors in merged PR "Reformat sum (#923)""
    This reverts commit ab175f3504e1ac1acfc631325506eb1ff967ab5e.

--- ivy/functional/backends/mxnet/statistical.py
+++ ivy/functional/backends/mxnet/statistical.py
@@ -11,9 +11,10 @@ from ivy.functional.backends.mxnet import _flat_array_to_1_dim_array, _1_dim_arr
 # -------------------#
 
 def sum(x: mx.ndarray.ndarray.NDArray,
-        axis: Optional[Union[int, Tuple[int, ...]]] = None,
-        keepdims: bool = False)\
-        -> mx.ndarray.ndarray.NDArray:
+         axis: Optional[Union[int, Tuple[int, ...]]] = None,
+         keepdims: bool = False,
+         out: Optional[mx.ndarray.ndarray.NDArray] = None)\
+            -> mx.ndarray.ndarray.NDArray:
     if axis is None:
         num_dims = len(x.shape)
         axis = tuple(range(num_dims))
@@ -24,7 +25,10 @@ def sum(x: mx.ndarray.ndarray.NDArray,
     if x.shape == ():
         x = _flat_array_to_1_dim_array(x)
     ret = mx.nd.sum(x, axis=axis, keepdims=keepdims)
-    return _handle_output(x, axis, keepdims, ret)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, _handle_output(x, axis, keepdims, ret))
+    else:
+        return _handle_output(x, axis, keepdims, ret)
 
 
 def prod(x: mx.ndarray.ndarray.NDArray,
