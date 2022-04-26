d735d3eea3601cf841d3a90b8f4cbc6f5bceedb5
Dhruva Bhardwaj <34804673+DhruvaBhardwaj404@users.noreply.github.com>
2022-04-26 23:02:29 +0530

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/test_statistical.py::test_einsum
+docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests
