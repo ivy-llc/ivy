407b9e0e3f9eaa6ee337439e8e301baa1aa193c0
HotzingTone <adam.he.future@gmail.com>
2022-04-26 15:40:59 +0100

    Fix errors in merged PR "Reformat sum (#923)"

--- ivy/functional/backends/jax/statistical.py
+++ ivy/functional/backends/jax/statistical.py
@@ -24,11 +24,10 @@ def min(x: JaxArray,
 def sum(x: JaxArray,
         axis: Optional[Union[int, Tuple[int, ...]]] = None,
         dtype: Optional[jnp.dtype] = None,
-        keepdims: bool = False,
-        out: Optional[JaxArray] = None)\
+        keepdims: bool = False)\
         -> JaxArray:
 
-    if dtype == None and jnp.issubdtype(x.dtype, jnp.integer):
+    if dtype is None and jnp.issubdtype(x.dtype, jnp.integer):
         if jnp.issubdtype(x.dtype, jnp.signedinteger) and x.dtype in [jnp.int8, jnp.int16, jnp.int32]:
             dtype = jnp.int32
         elif jnp.issubdtype(x.dtype, jnp.unsignedinteger) and x.dtype in [jnp.uint8, jnp.uint16, jnp.uint32]:
@@ -37,9 +36,6 @@ def sum(x: JaxArray,
             dtype = jnp.int64
         else:
             dtype = jnp.uint64
-    if ivy.exists(out):
-        return ivy.inplace_update(out, jnp.sum(x, axis=axis, dtype=dtype, keepdims=keepdims))
-    else:
         return jnp.sum(a=x, axis=axis, dtype=dtype, keepdims=keepdims)
 
 
