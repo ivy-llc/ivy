d4f4f8b012a511bdd675d51932046e726cac8770
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-04-29 19:30:43 +0100

    hypothesis working for tests with 2 input arrays

--- ivy_tests/test_ivy/helpers.py
+++ ivy_tests/test_ivy/helpers.py
@@ -33,7 +33,7 @@ try:
 except ImportError:
     _mx = None
     _mx_nd = None
-from hypothesis import strategies as st
+from hypothesis import assume, strategies as st
 
 # local
 import ivy
@@ -419,6 +419,7 @@ def integers(draw, min_value=None, max_value=None):
 @st.composite
 def dtype_and_values(draw, available_dtypes, n_arrays=1):
     dtype = draw(list_of_length(st.sampled_from(available_dtypes), n_arrays))
+    assume((dtype[0], dtype[1]) in ivy.promotion_table)
     size = draw(st.integers(0, 10))
     values = []
     for i in range(n_arrays):
