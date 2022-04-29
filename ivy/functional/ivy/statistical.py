c9bacdd51eb5259aca9e563979b907907e025e1c
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-04-29 04:14:34 +0100

    passing docstring test - fixing einsum docstring
    Former-commit-id: f123dde818960b7a5ba421d88bcc7e6b16d8b0a4

--- ivy/functional/ivy/statistical.py
+++ ivy/functional/ivy/statistical.py
@@ -369,9 +369,7 @@ def einsum(equation: str,
     --------
     The following gives us the sum of the diagonal elements:
     
-    >>> x = ivy.array([[0, 1, 2], 
-                       [3, 4, 5], 
-                       [6, 7, 8]])
+    >>> x = ivy.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
     >>> y = ivy.einsum('ii', x)
     >>> print(y)
     ivy.array(12)
