05eaea9667bfd1ff61939309d7bcc05d24965aeb
Krzysztof Olejniczak <98279532+kolejnyy@users.noreply.github.com>
2022-04-29 15:49:26 +0100

    Reformatted cross_entropy (#939)
    Co-authored-by: Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>

--- ivy/functional/ivy/losses.py
+++ ivy/functional/ivy/losses.py
@@ -4,30 +4,47 @@ Collection of Ivy loss functions.
 
 # local
 import ivy
-
+from typing import Optional, Union
 
 # Extra #
 # ------#
 
-def cross_entropy(true, pred, axis=-1, epsilon=1e-7):
-    """Computes cross entropy between predicted and true discrete distrubtions.
+def cross_entropy(true: Union[ivy.Array, ivy.NativeArray],
+                  pred: Union[ivy.Array, ivy.NativeArray],
+                  axis: Optional[int] = -1,
+                  epsilon: Optional[float] = 1e-7)\
+        -> ivy.Array:
+    """
+    Computes cross-entropy between predicted and true discrete distributions.
 
     Parameters
     ----------
     true
-        True labels
+        input array containing true labels.
     pred
-        predicted labels.
+        input array containing the predicted labels.
     axis
-        The class dimension, default is -1.
+        the axis along which to compute the cross-entropy. If axis is ``-1``, the cross-entropy will be computed along the last dimension. Default: ``-1``.
     epsilon
-        small constant to add to log functions, default is 1e-7
+        a float in [0.0, 1.0] specifying the amount of smoothing when calculating the loss. If epsilon is ``0``, no smoothing will be applied.
+        Default: ``1e-7``.
 
     Returns
     -------
-     ret
-        The cross entropy loss
+    ret
+        The cross-entropy loss between the given distributions
 
+    Examples
+    --------
+    >>> x = ivy.array([0, 0, 1, 0])
+    >>> y = ivy.array([0.25, 0.25, 0.25, 0.25])
+    >>> print(ivy.cross_entropy(x, y))
+    ivy.array(1.38629436)
+    
+    >>> z = ivy.array([0.1, 0.1, 0.7, 0.1])
+    >>> print(ivy.cross_entropy(x, z))
+    ivy.array(0.35667497)
+    
     """
     pred = ivy.clip(pred, epsilon, 1 - epsilon)
     log_pred = ivy.log(pred)
