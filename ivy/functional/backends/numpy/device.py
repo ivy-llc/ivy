c6355ea8390637ceb89e1fcd1b57017a683e6922
MihaiAnca13 <mihai.anca13@gmail.com>
2022-04-29 04:17:08 +0100

    reformatted num_gpu (#848)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/numpy/device.py
+++ ivy/functional/backends/numpy/device.py
@@ -19,10 +19,13 @@ _dev_callable = dev
 dev_to_str = lambda dev: 'cpu'
 dev_from_str = lambda dev: 'cpu'
 clear_mem_on_dev = lambda dev: None
-num_gpus = lambda: 0
 
 def tpu_is_available() -> bool:
     return False
+
+
+def num_gpus() -> int:
+    return 0
 
 
 def gpu_is_available() -> bool:
