17f454a031f542b693a43cefc15334e6fda7284a
djl11 <daniel.lenton11@imperial.ac.uk>
2022-05-02 22:18:54 +0200

    updated make_docs.sh to use latest dockerfile, where --root_dir flag is already provided in the entrypoint.

--- docs/make_docs.sh
+++ docs/make_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 IFS='/' read -r -a working_directory <<< "$PWD"
 user="${working_directory[2]}"
-docker run --rm -v "$(pwd)"/..:/home/"$user"/project unifyai/doc-builder:latest --root_dir "../ivy"
+docker run --rm -v "$(pwd)"/..:/home/"$user"/project unifyai/doc-builder:latest "../ivy"
