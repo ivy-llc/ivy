a271014059d4013ffbff241198f46ec1f2ab49e1
HotzingTone <adam.he.future@gmail.com>
2022-05-03 14:55:21 +0100

    integrate Hypothesis for test_sparse_cross_entropy

--- ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
@@ -2,10 +2,11 @@
 # global
 import pytest
 import numpy as np
+from hypothesis import given, strategies as st
 
 # local
 import ivy
-import ivy.functional.backends.numpy
+import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.helpers as helpers
 
 
@@ -52,14 +53,10 @@ def test_binary_cross_entropy(t_n_p_n_res, dtype, tensor_fn, device, call):
 
 
 # sparse_cross_entropy
-@pytest.mark.parametrize(
-    "t_n_p_n_res", [([1], [[0.3, 0.2, 0.5]], [1.609438])])
-@pytest.mark.parametrize(
-    "dtype", ['float32'])
-@pytest.mark.parametrize(
-    "tensor_fn", [ivy.array, helpers.var_fn])
+@given(t_n_p_n_res=st.sampled_from([([1], [[0.3, 0.2, 0.5]], [1.609438])]),
+       dtype=st.sampled_from(['float32']),
+       tensor_fn=st.sampled_from([ivy.array, helpers.var_fn]))
 def test_sparse_cross_entropy(t_n_p_n_res, dtype, tensor_fn, device, call):
-    # smoke test
     true, pred, true_target = t_n_p_n_res
     pred = tensor_fn(pred, dtype, device)
     true = ivy.array(true, 'int32', device)
