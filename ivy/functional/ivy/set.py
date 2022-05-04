57e70636535b9c3a14bc916f04ee449a9e6ea5a6
Arkhymadhe <emoekabuoshomah@gmail.com>
2022-05-04 10:08:20 +0100

    Updated docstrings in set, random, and manipulation submodules

--- ivy/functional/ivy/set.py
+++ ivy/functional/ivy/set.py
@@ -11,7 +11,7 @@ from ivy.framework_handler import current_framework as _cur_framework
 
 
 def unique_all(x: Union[ivy.Array, ivy.NativeArray]) \
-        -> Tuple[ivy.Array, ivy.Array, ivy.Array, ivy.Array]:
+               -> Tuple[ivy.Array, ivy.Array, ivy.Array, ivy.Array]:
     """
     Returns the unique elements of an input array ``x``, the first occurring indices for each unique element in ``x``, the indices from the set of unique elements that reconstruct ``x``, and the corresponding counts for each unique element in ``x``.
     .. admonition:: Data-dependent output shape
@@ -23,13 +23,15 @@ def unique_all(x: Union[ivy.Array, ivy.NativeArray]) \
        -   As ``-0`` and ``+0`` compare as ``True``, signed zeros should not be considered distinct, and the corresponding unique element will be implementation-dependent (e.g., an implementation could choose to return ``-0`` if ``-0`` occurs before ``+0``).
        As signed zeros are not distinct, using ``inverse_indices`` to reconstruct the input array is not guaranteed to return an array having the exact same values.
        Each ``nan`` value should have a count of one, while the counts for signed zeros should be aggregated as a single count.
+    
     Parameters
     ----------
-    x: array
+    x:
         input array. If ``x`` has more than one dimension, the function must flatten ``x`` and return the unique elements of the flattened array.
+    
     Returns
     -------
-    out: Tuple[array, array, array, array]
+    out:
         a namedtuple ``(values, indices, inverse_indices, counts)`` whose
         - first element must have the field name ``values`` and must be an array containing the unique elements of ``x``. The array must have the same data type as ``x``.
         - second element must have the field name ``indices`` and must be an array containing the indices (first occurrences) of ``x`` that result in ``values``. The array must have the same shape as ``values`` and must have the default array index data type.
@@ -37,7 +39,35 @@ def unique_all(x: Union[ivy.Array, ivy.NativeArray]) \
         - fourth element must have the field name ``counts`` and must be an array containing the number of times each unique element occurs in ``x``. The returned array must have same shape as ``values`` and must have the default array index data type.
         .. note::
            The order of unique elements is not specified and may vary between implementations.
-    """
+   
+   Examples
+   --------
+   >>> x = ivy.random_normal(mean=0.0, std=1.0, shape=(4, 4))
+   >>> print(x)
+   ivy.array([[ 2.1141,  0.8101,  0.9298,  0.8460],
+              [-1.2119, -0.3519, -0.6252,  0.4033],
+              [ 0.7443,  0.2577, -0.3707, -0.0545],
+              [-0.3238,  0.5944,  0.0775, -0.4327]])
+   >>> x[range(4), range(4)] = ivy.constants.nan #Introduce NaN values
+   >>> print(x)
+   ivy.array([[    nan,  0.8101,  0.9298,  0.8460],
+              [-1.2119,     nan, -0.6252,  0.4033],
+              [ 0.7443,  0.2577,     nan, -0.0545],
+              [-0.3238,  0.5944,  0.0775,     nan]])
+   >>> values, indices, inverse_indices, counts = unique_all(x)
+   >>> print(values)
+   ivy.array([-1.2119, -0.6252,  0.4033,     nan,     nan,     nan,     nan, -0.3238,
+              -0.0545,  0.0775,  0.2577,  0.5944,  0.7443,  0.8101,  0.8460,  0.9298])
+   >>> print(indices)
+   ivy.array([ 4,  6,  7,  0,  5, 10, 15, 12, 11, 14,  9, 13,  8,  1,  3,  2])
+   >>> print(inverse_indices)
+   ivy.array([[ 3, 13, 15, 14],
+              [ 0,  3,  1,  2],
+              [12, 10,  3,  8],
+              [ 7, 11,  9,  3]])
+   >>> print(counts)
+   ivy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+   """
     return _cur_framework(x).unique_all(x)
 
 
@@ -52,7 +82,7 @@ def unique_inverse(x: Union[ivy.Array, ivy.NativeArray]) \
         input array.
 
     Returns
-     -------
+    -------
     ret
         tuple of two arrays (values, inverse_indices)
 
@@ -76,7 +106,7 @@ def unique_values(x: Union[ivy.Array, ivy.NativeArray], out: Optional[Union[ivy.
     x
         input array. If ``x`` has more than one dimension, the function must flatten ``x`` and return the unique elements of the flattened array.
     Returns
-     -------
+    -------
     ret
         an array containing the set of unique elements in ``x``. The returned array must have the same data type as ``x``.
         .. note::
@@ -95,11 +125,31 @@ def unique_counts(x: Union[ivy.Array, ivy.NativeArray])\
         input array. If x has more than one dimension, the function must flatten x and return the unique elements of the flattened array.
 
     Returns
-     -------
+    -------
     ret
         a namedtuple (values, counts) whose
         -first element must have the field name values and must be an array containing the unique elements of x. The array must have the same data type as x.
         -second element must have the field name counts and must be an array containing the number of times each unique element occurs in x. The returned array must have same shape as values and must have the default array index data type.
-
-    """
+    
+   Examples
+   --------
+   >>> x = ivy.random_normal(mean=0.0, std=1.0, shape=(4, 4))
+   >>> print(x)
+   ivy.array([[ 2.1141,  0.8101,  0.9298,  0.8460],
+              [-1.2119, -0.3519, -0.6252,  0.4033],
+              [ 0.7443,  0.2577, -0.3707, -0.0545],
+              [-0.3238,  0.5944,  0.0775, -0.4327]])
+   >>> x[range(4), range(4)] = ivy.constants.nan #Introduce NaN values
+   >>> print(x)
+   ivy.array([[    nan,  0.8101,  0.9298,  0.8460],
+              [-1.2119,     nan, -0.6252,  0.4033],
+              [ 0.7443,  0.2577,     nan, -0.0545],
+              [-0.3238,  0.5944,  0.0775,     nan]])
+   >>> values, counts = unique_counts(x)
+   >>> print(values)
+   ivy.array([-1.2119, -0.6252,  0.4033,     nan,     nan,     nan,     nan, -0.3238,
+              -0.0545,  0.0775,  0.2577,  0.5944,  0.7443,  0.8101,  0.8460,  0.9298])
+   >>> print(counts)
+   ivy.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+   """
     return _cur_framework(x).unique_counts(x)
