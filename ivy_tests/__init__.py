f81636e8e5d02ae11c6fa19c798de471d843b18c
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-05-05 20:36:34 +0100

    Update __init__.py

--- ivy_tests/__init__.py
+++ ivy_tests/__init__.py
@@ -1,7 +1,2 @@
 from jax.config import config
 config.update("jax_enable_x64", True)
-
-from hypothesis import settings, HealthCheck
-
-settings.register_profile("ci", suppress_health_check=(HealthCheck(3),))
-settings.load_profile("ci")
