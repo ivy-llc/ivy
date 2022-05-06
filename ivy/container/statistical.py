33e904753af200c4184294a20d2f30741814b4f3
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-06 11:12:16 +0100

    container, array support for min, max, mean

--- ivy/container/statistical.py
+++ ivy/container/statistical.py
@@ -1,4 +1,8 @@
+# global
+from typing import Optional, Union, List, Dict, Tuple
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
@@ -6,4 +10,59 @@ from ivy.container.base import ContainerBase
 
 # noinspection PyMissingConstructor
 class ContainerWithStatistical(ContainerBase):
-    pass
+    def min(
+        self: ivy.Container,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.min(x_, axis, keepdims) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def max(
+        self: ivy.Container,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.max(x_, axis, keepdims) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def mean(
+        self: ivy.Container,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.mean(x_, axis, keepdims) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
