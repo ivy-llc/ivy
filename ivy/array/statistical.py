6c9848fe295a55fcf026650c695f3af21afcba01
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-06 11:12:16 +0100

    container, array support for min, max, mean

--- ivy/array/statistical.py
+++ ivy/array/statistical.py
@@ -1,8 +1,34 @@
 # global
+from typing import Optional, Union, Tuple
 import abc
+
+# local
+import ivy
 
 # ToDo: implement all methods here as public instance methods
 
 
 class ArrayWithStatistical(abc.ABC):
-    pass
+    def min(
+        self: ivy.Array,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        out: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+    ) -> ivy.Array:
+        return ivy.min(self, axis, keepdims, out=out)
+
+    def max(
+        self: ivy.Array,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        out: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+    ) -> ivy.Array:
+        return ivy.max(self, axis, keepdims, out=out)
+
+    def mean(
+        self: ivy.Array,
+        axis: Union[int, Tuple[int]] = None,
+        keepdims: bool = False,
+        out: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+    ) -> ivy.Array:
+        return ivy.mean(self, axis, keepdims, out=out)
