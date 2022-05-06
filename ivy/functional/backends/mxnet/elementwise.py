7148be009928bd26ae82813a0c9e6af5ba8d461f
jashdalvi <55826870+jashdalvi@users.noreply.github.com>
2022-05-06 16:40:52 +0530

    Reformate exp (#1014)
    Co-authored-by: HotzingTone <adam.he.future@gmail.com>

--- ivy/functional/backends/mxnet/elementwise.py
+++ ivy/functional/backends/mxnet/elementwise.py
@@ -357,6 +357,17 @@ def cos(x: mx.ndarray.ndarray.NDArray,
         return ivy.inplace_update(out, ret)
     return ret
 
+def exp(x: mx.ndarray.ndarray.NDArray,
+          out: Optional[mx.ndarray.ndarray.NDArray] = None)\
+      -> mx.ndarray.ndarray.NDArray:
+    if isinstance(x, float):
+        ret = math.exp(x)
+    else:
+        ret = mx.nd.exp(x)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
+
 tan = lambda x: math.tan(x) if isinstance(x, float) else mx.nd.tan(x)
 asin = lambda x: math.asin(x) if isinstance(x, float) else mx.nd.arcsin(x)
 atan = lambda x: math.atan(x) if isinstance(x, float) else mx.nd.arctan(x)
@@ -369,7 +380,6 @@ cosh = lambda x: math.cosh(x) if isinstance(x, float) else mx.nd.cosh(x)
 asinh = lambda x: math.asinh(x) if isinstance(x, float) else mx.nd.arcsinh(x)
 atanh = lambda x: math.atanh(x) if isinstance(x, float) else mx.nd.arctanh(x)
 log = lambda x: math.log(x) if isinstance(x, float) else mx.nd.log(x)
-exp = lambda x: math.exp(x) if isinstance(x, float) else mx.nd.exp(x)
 equal = lambda x1, x2: x1 == x2
 equal.__name__ = "equal"
 
