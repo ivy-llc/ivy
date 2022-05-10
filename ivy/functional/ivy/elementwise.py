797e85a0f5aca970d9d96614d3fbecee24a45117
saeedashrraf <48128381+saeedashrraf@users.noreply.github.com>
2022-05-10 18:30:20 +0200

    Update elementwise.py

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -2180,7 +2180,7 @@ def bitwise_right_shift(
     ret
         out (array) – an array containing the element-wise results. The returned array must have a data type determined by :ref:`Type Promotion Rules`.
         
-    Exemples :
+    Examples :
     >>> lhs = ivy.array([5, 2, 3, 1], dtype=ivy.int64)
     >>> rhs = ivy.array([[1, 3, 2, 1], dtype=ivy.int64)
     >>> print(ivy.bitwise_right_shift(lhs, rhs))
