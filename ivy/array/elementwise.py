caec3a4758c4ea8dd667e4218d7490e4da17494f
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-11 17:28:45 +0100

    add container and instance elementwise methods

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -14,3 +14,25 @@ class ArrayWithElementwise(abc.ABC):
         self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
     ) -> ivy.Array:
         return ivy.abs(self, out=out)
+
+    def acosh(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.acosh(self, out=out)
+
+    def acos(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.acos(self, out=out)
+
+    def add(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.add(self, x2, out=out)
+
+    def asin(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.asin(self, out=out)
