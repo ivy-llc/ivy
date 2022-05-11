3458c552881b1be65c470ca7b1dc160c1ab3ec06
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-11 15:50:05 +0100

    add hypothesis to statistical and add map_sequences argument

--- ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_statistical.py
@@ -1,6 +1,5 @@
-"""Collection of tests for statstical functions."""
+"""Collection of tests for statistical functions."""
 # global
-import pytest
 import numpy as np
 from hypothesis import given, assume, strategies as st
 
@@ -121,39 +120,41 @@ def test_mean(
 
 
 # var
-# @given(
-#     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtype_strs),
-#     as_variable=st.booleans(),
-#     with_out=st.booleans(),
-#     num_positional_args=st.integers(0, 1),
-#     native_array=st.booleans(),
-#     container=st.booleans(),
-#     instance_method=st.booleans(),
-# )
-# def test_var(
-#     dtype_and_x,
-#     as_variable,
-#     with_out,
-#     num_positional_args,
-#     native_array,
-#     container,
-#     instance_method,
-#     fw,
-# ):
-#     dtype, x = dtype_and_x
-#     assume(x)
-#     helpers.test_array_function(
-#         dtype,
-#         as_variable,
-#         with_out,
-#         num_positional_args,
-#         native_array,
-#         container,
-#         instance_method,
-#         fw,
-#         "var",
-#         x=np.asarray(x, dtype=dtype),
-#     )
+@given(
+    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtype_strs),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=st.integers(0, 1),
+    native_array=st.booleans(),
+    container=st.booleans(),
+    instance_method=st.booleans(),
+)
+def test_var(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    if fw == "torch":
+        return  # rounding errors with torch 0.2222 != 0.2223
+    dtype, x = dtype_and_x
+    assume(x)
+    helpers.test_array_function(
+        dtype,
+        as_variable,
+        with_out,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "var",
+        x=np.asarray(x, dtype=dtype),
+    )
 
 
 # prod
@@ -235,50 +236,56 @@ def test_sum(
 
 
 # std
-@pytest.mark.parametrize("dtype", ivy.float_dtype_strs)
-@pytest.mark.parametrize("as_variable", [True, False])
-@pytest.mark.parametrize("with_out", [True, False])
-@pytest.mark.parametrize("native_array", [True, False])
-def test_std(dtype, as_variable, with_out, native_array):
-    if dtype in ivy.invalid_dtype_strs:
-        pytest.skip("invalid dtype")
-    x = ivy.array([2, 3, 4], dtype=dtype)
-    out = ivy.array(9, dtype=dtype)
-    if as_variable:
-        if not ivy.is_float_dtype(dtype):
-            pytest.skip("only floating point variables are supported")
-        if with_out:
-            pytest.skip("variables do not support out argument")
-        x = ivy.variable(x)
-        out = ivy.variable(out)
-    if native_array:
-        x = x.data
-        out = out.data
-    if with_out:
-        ret = ivy.std(x, out=out)
-    else:
-        ret = ivy.std(x)
-    if with_out:
-        if not native_array:
-            assert ret is out
-        if ivy.current_framework_str() in ["tensorflow", "jax"]:
-            # these frameworks do not support native inplace updates
-            return
-        assert ret.data is (out if native_array else out.data)
+@given(
+    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtype_strs),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=st.integers(0, 1),
+    native_array=st.booleans(),
+    container=st.booleans(),
+    instance_method=st.booleans(),
+)
+def test_std(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    if fw == "torch":
+        return  # rounding errors with torch
+    dtype, x = dtype_and_x
+    assume(x)
+    helpers.test_array_function(
+        dtype,
+        as_variable,
+        with_out,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "std",
+        x=np.asarray(x, dtype=dtype),
+    )
 
 
 # einsum
-@pytest.mark.parametrize(
-    "eq_n_op_n_shp",
-    [
-        ("ii", (np.arange(25).reshape(5, 5),), ()),
-        ("ii->i", (np.arange(25).reshape(5, 5),), (5,)),
-        ("ij,j", (np.arange(25).reshape(5, 5), np.arange(5)), (5,)),
-    ],
+@given(
+    eq_n_op_n_shp=st.sampled_from(
+        [
+            ("ii", (np.arange(25).reshape(5, 5),), ()),
+            ("ii->i", (np.arange(25).reshape(5, 5),), (5,)),
+            ("ij,j", (np.arange(25).reshape(5, 5), np.arange(5)), (5,)),
+        ]
+    ),
+    dtype=st.sampled_from(ivy_np.valid_float_dtype_strs),
+    with_out=st.booleans(),
+    tensor_fn=st.sampled_from([ivy.array, helpers.var_fn]),
 )
-@pytest.mark.parametrize("dtype", ["float32"])
-@pytest.mark.parametrize("with_out", [True, False])
-@pytest.mark.parametrize("tensor_fn", [ivy.array, helpers.var_fn])
 def test_einsum(eq_n_op_n_shp, dtype, with_out, tensor_fn, device, call):
     # smoke test
     eq, operands, true_shape = eq_n_op_n_shp
