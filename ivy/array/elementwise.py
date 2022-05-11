2c4b58f7f59867aeed1b3684dbccab4fffb20fa9
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-11 19:20:45 +0100

    add container and instance elementwise methods

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -36,3 +36,65 @@ class ArrayWithElementwise(abc.ABC):
         self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
     ) -> ivy.Array:
         return ivy.asin(self, out=out)
+
+    def asinh(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.asinh(self, out=out)
+
+    def atan(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.atan(self, out=out)
+
+    def atan2(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.atan2(self, x2, out=out)
+
+    def atanh(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.atanh(self, out=out)
+
+    def bitwise_and(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.bitwise_and(self, x2, out=out)
+
+    def bitwise_left_shift(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.bitwise_left_shift(self, x2, out=out)
+
+    def bitwise_invert(
+        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+    ) -> ivy.Array:
+        return ivy.bitwise_invert(self, out=out)
+
+    def bitwise_or(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.bitwise_or(self, x2, out=out)
+
+    def bitwise_right_shift(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.bitwise_right_shift(self, x2, out=out)
+
+    def bitwise_xor(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.bitwise_xor(self, x2, out=out)
