44125f5bac90bed1cfedaf8bc8841c2d8701877c
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-12 09:57:17 +0100

    add container/instance elementwise methods

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -11,90 +11,170 @@ import ivy
 # noinspection PyUnresolvedReferences
 class ArrayWithElementwise(abc.ABC):
     def abs(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.abs(self, out=out)
 
     def acosh(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.acosh(self, out=out)
 
     def acos(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.acos(self, out=out)
 
     def add(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.add(self, x2, out=out)
 
     def asin(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.asin(self, out=out)
 
     def asinh(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.asinh(self, out=out)
 
     def atan(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.atan(self, out=out)
 
     def atan2(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.atan2(self, x2, out=out)
 
     def atanh(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.atanh(self, out=out)
 
     def bitwise_and(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.bitwise_and(self, x2, out=out)
 
     def bitwise_left_shift(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.bitwise_left_shift(self, x2, out=out)
 
     def bitwise_invert(
-        self: ivy.Array, out: Optional[Union[ivy.Array, ivy.NativeArray]] = None
+        self: ivy.Array, out: Optional[ivy.Array] = None
     ) -> ivy.Array:
         return ivy.bitwise_invert(self, out=out)
 
     def bitwise_or(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.bitwise_or(self, x2, out=out)
 
     def bitwise_right_shift(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.bitwise_right_shift(self, x2, out=out)
 
     def bitwise_xor(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
-        out: Optional[Union[ivy.Array, ivy.NativeArray, ivy.Container]] = None,
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
     ) -> Union[ivy.Array, ivy.Container]:
         return ivy.bitwise_xor(self, x2, out=out)
+
+    def ceil(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.ceil(self, out=out)
+
+    def cos(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.cos(self, out=out)
+
+    def cosh(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.cosh(self, out=out)
+
+    def divide(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.divide(self, x2, out=out)
+
+    def equal(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.equal(self, x2, out=out)
+
+    def exp(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.exp(self, out=out)
+
+    def expm1(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.expm1(self, out=out)
+
+    def floor(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.floor(self, out=out)
+
+    def floor_divide(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.floor_divide(self, x2, out=out)
+
+    def greater(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.greater(self, x2, out=out)
+
+    def greater_equal(
+        self: ivy.Array,
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[Union[ivy.Array, ivy.Container]] = None,
+    ) -> Union[ivy.Array, ivy.Container]:
+        return ivy.greater_equal(self, x2, out=out)
+
+    def isfinite(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.isfinite(self, out=out)
+
+    def isinf(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.isinf(self, out=out)
+
+    def isnan(
+        self: ivy.Array, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.isinf(self, out=out)
--- ivy/container/elementwise.py
+++ ivy/container/elementwise.py
@@ -361,3 +361,314 @@ class ContainerWithElementwise(ContainerBase):
             ),
             out,
         )
+
+    def ceil(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.ceil(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def cos(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.cos(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def cosh(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.cosh(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def divide(
+        self: ivy.Container,
+        x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"x1": self}
+        if ivy.is_array(x2):
+            kw["x2"] = x2
+        else:
+            conts["x2"] = x2
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.divide(**dict(zip(conts.keys(), xs)), **kw)
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def equal(
+        self: ivy.Container,
+        x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"x1": self}
+        if ivy.is_array(x2):
+            kw["x2"] = x2
+        else:
+            conts["x2"] = x2
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.equal(**dict(zip(conts.keys(), xs)), **kw)
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def exp(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.exp(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def expm1(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.expm1(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def floor(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.floor(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def floor_divide(
+        self: ivy.Container,
+        x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"x1": self}
+        if ivy.is_array(x2):
+            kw["x2"] = x2
+        else:
+            conts["x2"] = x2
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.floor_divide(**dict(zip(conts.keys(), xs)), **kw)
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def greater(
+        self: ivy.Container,
+        x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"x1": self}
+        if ivy.is_array(x2):
+            kw["x2"] = x2
+        else:
+            conts["x2"] = x2
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.greater(**dict(zip(conts.keys(), xs)), **kw)
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def greater_equal(
+        self: ivy.Container,
+        x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"x1": self}
+        if ivy.is_array(x2):
+            kw["x2"] = x2
+        else:
+            conts["x2"] = x2
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.greater_equal(**dict(zip(conts.keys(), xs)), **kw)
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
+    def isfinite(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.isfinite(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def isinf(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.isinf(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
+
+    def isnan(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.handle_inplace(
+            self.map(
+                lambda x_, _: ivy.isnan(x_) if ivy.is_array(x_) else x_,
+                key_chains,
+                to_apply,
+                prune_unapplied,
+                map_sequences,
+            ),
+            out,
+        )
