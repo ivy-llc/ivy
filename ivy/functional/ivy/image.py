39cb97856c8d8f5e112d025edf5c2172476e6396
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-12 17:22:51 +0100

    fix format failures

--- ivy/functional/ivy/image.py
+++ ivy/functional/ivy/image.py
@@ -84,7 +84,7 @@ def gradient_image(x):
     -------
     ret
         Gradient images dy *[batch_shape,h,w,d]* and dx *[batch_shape,h,w,d]* .
-        
+
     Examples
     --------
     >>> batch_size = 1
@@ -106,7 +106,7 @@ def gradient_image(x):
      ivy.array([[1., 1., 0.],
                [1., 1., 0.],
                [1., 1., 0.]])
- 
+
     """
     return _cur_framework(x).gradient_image(x)
 
