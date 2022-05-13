00479b253fa528e2ce53a79de9cfbd7a7c137394
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-05-13 10:17:48 +0100

    Update test_sorting.py

--- ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
@@ -48,38 +48,38 @@ import ivy.functional.backends.numpy as ivy_np
 
 
 # sort
-@given(
-    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_numeric_dtype_strs),
-    as_variable=st.booleans(),
-    with_out=st.booleans(),
-    num_positional_args=st.integers(0, 1),
-    native_array=st.booleans(),
-    container=st.booleans(),
-    instance_method=st.booleans(),
-)
-def test_sort(
-    dtype_and_x,
-    as_variable,
-    with_out,
-    num_positional_args,
-    native_array,
-    container,
-    instance_method,
-    fw,
-):
-    dtype, x = dtype_and_x
-    # smoke this for torch
-    if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
-        return
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        with_out,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "sort",
-        x=np.asarray(x, dtype=dtype),
-    )
+# @given(
+#     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_numeric_dtype_strs),
+#     as_variable=st.booleans(),
+#     with_out=st.booleans(),
+#     num_positional_args=st.integers(0, 1),
+#     native_array=st.booleans(),
+#     container=st.booleans(),
+#     instance_method=st.booleans(),
+# )
+# def test_sort(
+#     dtype_and_x,
+#     as_variable,
+#     with_out,
+#     num_positional_args,
+#     native_array,
+#     container,
+#     instance_method,
+#     fw,
+# ):
+#     dtype, x = dtype_and_x
+#     # smoke this for torch
+#     if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
+#         return
+#     helpers.test_array_function(
+#         dtype,
+#         as_variable,
+#         with_out,
+#         num_positional_args,
+#         native_array,
+#         container,
+#         instance_method,
+#         fw,
+#         "sort",
+#         x=np.asarray(x, dtype=dtype),
+#     )
