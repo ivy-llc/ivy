a234f4b4d7febf6446fcb0f06b3467d6f6ace1e7
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-16 14:36:16 +0100

    fix squeeze test and invalid example

--- ivy/functional/ivy/manipulation.py
+++ ivy/functional/ivy/manipulation.py
@@ -152,8 +152,6 @@ def squeeze(
     (1, 2, 2)
     >>> print(ivy.squeeze(x, axis=0).shape)
     (2, 2)
-    >>> print(ivy.squeeze(x, axis=1).shape)
-    ValueError: cannot select an axis to squeeze out which has size not equal to one
 
     """
     return _cur_framework(x).squeeze(x, axis, out)
