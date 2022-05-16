5724382f38acec707304123db772d1a82bacaaa6
Marouf Shaikh <maruf98@gmail.com>
2022-05-16 15:05:04 +0000

    removed error causing example from docstring
    Former-commit-id: 7656875e9ae4d11c0a76ad4cf606cffd2d734355

--- ivy/functional/ivy/manipulation.py
+++ ivy/functional/ivy/manipulation.py
@@ -150,10 +150,9 @@ def squeeze(
     >>> x = ivy.array([[[0, 1], [2, 3]]])
     >>> print(x.shape)
     (1, 2, 2)
+
     >>> print(ivy.squeeze(x, axis=0).shape)
     (2, 2)
-    >>> print(ivy.squeeze(x, axis=1).shape)
-    ValueError: cannot select an axis to squeeze out which has size not equal to one
 
     """
     return _cur_framework(x).squeeze(x, axis, out)
