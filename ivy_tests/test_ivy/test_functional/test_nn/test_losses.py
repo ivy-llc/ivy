462fe459eb3adea050f4d75ad8eb0c2bc79ddbdf
HotzingTone <adam.he.future@gmail.com>
2022-05-16 09:59:58 +0100

    Hypothesis integration for test_binary_cross_entropy

--- ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
@@ -47,43 +47,46 @@ def test_cross_entropy(
         true=np.asarray(x[0], dtype=dtype[0]),
         pred=np.asarray(x[1], dtype=dtype[1]),
     )
-#
-#
-# # binary_cross_entropy
-# @given(
-#     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_numeric_dtype_strs, 2),
-#     as_variable=helpers.list_of_length(st.booleans(), 2),
-#     num_positional_args=st.integers(0, 2),
-#     native_array=helpers.list_of_length(st.booleans(), 2),
-#     container=helpers.list_of_length(st.booleans(), 2),
-#     instance_method=st.booleans(),
-# )
-# def test_binary_cross_entropy(
-#     dtype_and_x,
-#     as_variable,
-#     num_positional_args,
-#     native_array,
-#     container,
-#     instance_method,
-#     fw,
-# ):
-#     dtype, x = dtype_and_x
-#     assume(not any(d in ivy.invalid_dtype_strs for d in dtype))
-#     if fw == "torch" and dtype == "float16":
-#         return
-#     helpers.test_array_function(
-#         dtype,
-#         as_variable,
-#         False,
-#         num_positional_args,
-#         native_array,
-#         container,
-#         instance_method,
-#         fw,
-#         "binary_cross_entropy",
-#         true=np.asarray(x[0], dtype=dtype[0]),
-#         pred=np.asarray(x[1], dtype=dtype[1]),
-#     )
+
+
+# binary_cross_entropy
+@given(
+    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_numeric_dtype_strs, 2),
+    as_variable=helpers.list_of_length(st.booleans(), 2),
+    num_positional_args=st.integers(0, 2),
+    native_array=helpers.list_of_length(st.booleans(), 2),
+    container=helpers.list_of_length(st.booleans(), 2),
+    instance_method=st.booleans(),
+)
+def test_binary_cross_entropy(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    dtype, x = dtype_and_x
+    if (d not in ivy.float_dtype_strs for d in dtype):
+        return
+    if (v == [] for v in x):
+        return
+    if fw == "torch" and dtype == "float16":
+        return
+    helpers.test_array_function(
+        dtype,
+        as_variable,
+        False,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "binary_cross_entropy",
+        true=np.asarray(x[0], dtype=dtype[0]),
+        pred=np.asarray(x[1], dtype=dtype[1]),
+    )
 #
 #
 # # sparse_cross_entropy
@@ -147,25 +150,25 @@ def test_cross_entropy(
 #     assert np.allclose(call(ivy.cross_entropy, true, pred), np.asarray(true_target))
 
 
-# binary_cross_entropy
-@pytest.mark.parametrize(
-    "t_n_p_n_res", [([[0., 1., 0.]], [[0.3, 0.7, 0.5]], [[0.35667494, 0.35667494, 0.69314718]])])
-@pytest.mark.parametrize(
-    "dtype", ['float32'])
-@pytest.mark.parametrize(
-    "tensor_fn", [ivy.array, helpers.var_fn])
-def test_binary_cross_entropy(t_n_p_n_res, dtype, tensor_fn, device, call):
-    # smoke test
-    true, pred, true_target = t_n_p_n_res
-    pred = tensor_fn(pred, dtype, device)
-    true = tensor_fn(true, dtype, device)
-    ret = ivy.binary_cross_entropy(true, pred)
-    # type test
-    assert ivy.is_ivy_array(ret)
-    # cardinality test
-    assert ret.shape == pred.shape
-    # value test
-    assert np.allclose(call(ivy.binary_cross_entropy, true, pred), np.asarray(true_target))
+# # binary_cross_entropy
+# @pytest.mark.parametrize(
+#     "t_n_p_n_res", [([[0., 1., 0.]], [[0.3, 0.7, 0.5]], [[0.35667494, 0.35667494, 0.69314718]])])
+# @pytest.mark.parametrize(
+#     "dtype", ['float32'])
+# @pytest.mark.parametrize(
+#     "tensor_fn", [ivy.array, helpers.var_fn])
+# def test_binary_cross_entropy(t_n_p_n_res, dtype, tensor_fn, device, call):
+#     # smoke test
+#     true, pred, true_target = t_n_p_n_res
+#     pred = tensor_fn(pred, dtype, device)
+#     true = tensor_fn(true, dtype, device)
+#     ret = ivy.binary_cross_entropy(true, pred)
+#     # type test
+#     assert ivy.is_ivy_array(ret)
+#     # cardinality test
+#     assert ret.shape == pred.shape
+#     # value test
+#     assert np.allclose(call(ivy.binary_cross_entropy, true, pred), np.asarray(true_target))
 
 
 # sparse_cross_entropy
