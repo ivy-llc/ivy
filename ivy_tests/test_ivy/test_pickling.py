c44fb5f402096111fef369ca9805e9b585134d1e
Jatin Prakash <47978882+bicycleman15@users.noreply.github.com>
2022-05-21 12:35:53 +0000

    add hypothesis for test_pickling

--- ivy_tests/test_ivy/test_pickling.py
+++ ivy_tests/test_ivy/test_pickling.py
@@ -4,19 +4,27 @@ import pickle
 import numpy as np
 import os
 
+from hypothesis import given, strategies as st
+
 # local
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
 
 
 # Tests #
 # ------#
 
 # pickling array test to str
-@pytest.mark.parametrize("x", [1, [], [1], [1, 2, 3], [[0.0, 1.0], [2.0, 3.0]]])
-@pytest.mark.parametrize("dtype", ["float32", "int32"])
-@pytest.mark.parametrize("tensor_fn", [ivy.array])
-def test_pickle_to_string(x, dtype, tensor_fn, device, call):
+@given(
+    array_shape=helpers.lists(
+        st.integers(0, 5), min_size="num_dims", max_size="num_dims", size_bounds=[0, 5]
+    ),
+    dtype=st.sampled_from(ivy_np.valid_dtype_strs),
+    data=st.data(),
+    tensor_fn=st.sampled_from([ivy.array])
+)
+def test_pickle_to_string(array_shape, dtype, data, tensor_fn, device, call):
     # smoke test
     if (
         (isinstance(x, Number) or len(x) == 0)
@@ -24,9 +32,11 @@ def test_pickle_to_string(x, dtype, tensor_fn, device, call):
         and call is helpers.mx_call
     ):
         # mxnet does not support 0-dimensional variables
-        pytest.skip()
+        return
 
+    x = data.draw(helpers.nph.arrays(shape=array_shape, dtype=dtype))
     x = tensor_fn(x, dtype, device)
+
     pickled_arr = pickle.dumps(x)
     unpickled_arr = pickle.loads(pickled_arr)
 
