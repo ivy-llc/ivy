63f4ac5be712b5d2872fd6e1b3467f4c72ea5140
Jatin Prakash <47978882+bicycleman15@users.noreply.github.com>
2022-05-22 16:43:41 +0000

    formatting

--- ivy_tests/test_ivy/test_pickling.py
+++ ivy_tests/test_ivy/test_pickling.py
@@ -1,5 +1,3 @@
-import pytest
-from numbers import Number
 import pickle
 import numpy as np
 import os
@@ -21,13 +19,13 @@ import ivy.functional.backends.numpy as ivy_np
         st.integers(0, 5), min_size="num_dims", max_size="num_dims", size_bounds=[0, 5]
     ),
     dtype=st.sampled_from(list(ivy_np.valid_dtypes)),
-    data=st.data()
+    data=st.data(),
 )
 def test_pickle_to_string(array_shape, dtype, data, device, fw):
     # smoke this for torch
     if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
         return
-    
+
     x = data.draw(helpers.nph.arrays(shape=array_shape, dtype=dtype))
     x = ivy.array(x, dtype=dtype, device=device)
 
@@ -44,13 +42,13 @@ def test_pickle_to_string(array_shape, dtype, data, device, fw):
         st.integers(0, 5), min_size="num_dims", max_size="num_dims", size_bounds=[0, 5]
     ),
     dtype=st.sampled_from(list(ivy_np.valid_dtypes)),
-    data=st.data()
+    data=st.data(),
 )
 def test_pickle_to_and_from_disk(array_shape, dtype, data, device, fw):
     # smoke this for torch
     if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
         return
-    
+
     x = data.draw(helpers.nph.arrays(shape=array_shape, dtype=dtype))
     x = ivy.array(x, dtype=dtype, device=device)
 
