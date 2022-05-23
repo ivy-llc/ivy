e57b7b9ce45e97c57eb5307fccfc67be3b0cf970
djl11 <daniel.lenton11@imperial.ac.uk>
2022-05-23 16:50:31 +0200

    added ivy.Container methods random_uniform and static_random_uniform.

--- ivy/container/random.py
+++ ivy/container/random.py
@@ -1,4 +1,8 @@
+# global
+from typing import Optional, Union, List, Dict, Tuple
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
@@ -6,4 +10,48 @@ from ivy.container.base import ContainerBase
 
 # noinspection PyMissingConstructor
 class ContainerWithRandom(ContainerBase):
-    pass
+    @staticmethod
+    def static_random_uniform(
+        low: Union[float, ivy.Container] = 0.0,
+        high: Union[float, ivy.Container] = 1.0,
+        shape: Optional[Union[int, Tuple[int, ...], ivy.Container]] = None,
+        device: Optional[Union[ivy.Device, ivy.NativeDevice, ivy.Container]] = None,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.call_static_multi_map_method(
+            {"low": low, "high": high, "shape": shape, "device": device},
+            ivy.random_uniform,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out,
+        )
+
+    def random_uniform(
+        self: ivy.Container,
+        low: Union[float, ivy.Container] = 0.0,
+        high: Union[float, ivy.Container] = 1.0,
+        device: Optional[Union[ivy.Device, ivy.NativeDevice, ivy.Container]] = None,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.static_random_uniform(
+            low,
+            high,
+            self,
+            device,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
