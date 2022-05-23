ae0618e34050ad183ddb99240ae7c6348336bd7e
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-05-23 11:16:20 +0100

    test_as_ivy_dev integrated with hypothesis
    Former-commit-id: 4d15adafc41ecb8923c75e00292f6b9fe75fb6c0

--- ivy_tests/test_ivy/test_functional/test_core/test_device.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_device.py
@@ -8,7 +8,7 @@ import time
 import numpy as np
 from numbers import Number
 from hypothesis import strategies as st, given, extra
-import tensorflow as tf
+
 # local
 import ivy
 import ivy.functional.backends.numpy
@@ -44,19 +44,25 @@ def test_dev(array_shape, dtype, as_variable, fw, device):
 
 
 # as_ivy_dev
-@pytest.mark.parametrize("x", [1, [], [1], [[0.0, 1.0], [2.0, 3.0]]])
-@pytest.mark.parametrize("dtype", ["float32"])
-@pytest.mark.parametrize("tensor_fn", [ivy.array, helpers.var_fn])
-def test_as_ivy_dev(x, dtype, tensor_fn, device, call):
-    # smoke test
-    if (
-        (isinstance(x, Number) or len(x) == 0)
-        and tensor_fn == helpers.var_fn
-        and call is helpers.mx_call
-    ):
+@given(array_shape=helpers.lists(st.integers(2, 3), min_size="num_dims", max_size="num_dims", size_bounds=[1, 3]),
+       dtype = st.sampled_from(ivy_np.valid_numeric_dtypes),
+       as_variable = st.booleans())
+def test_as_ivy_dev(array_shape, dtype, as_variable, fw, device):
+
+    if fw == "torch" and "int" in dtype:
+        return
+
+    x = np.random.uniform(size=tuple(array_shape)).astype(dtype)
+    x = ivy.asarray(x)
+    if as_variable:
+        x = ivy.variable(x)
+
+    if ((isinstance(x, Number) or x.size == 0)
+        and as_variable
+        and fw == "mxnet"):
         # mxnet does not support 0-dimensional variables
-        pytest.skip()
-    x = tensor_fn(x, dtype, device)
+        return
+
     device = ivy.dev(x)
     ret = ivy.as_ivy_dev(device)
     # type test
