c884f05d385824fccdbb38ddbffea2f2bcd435dc
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-05-24 12:17:51 +0100

    comment _wrap_function

--- ivy/array/wrapping.py
+++ ivy/array/wrapping.py
@@ -2,15 +2,38 @@
 import ivy
 
 
-def _wrap_fn(fn_name):
-    def new_fn(self, *args, **kwargs):
-        """
-        Add the data of the current array from which the instance function is invoked
+def _wrap_function(function_name):
+    """Wraps the function called `function_name`.
+
+    Parameters
+    ----------
+    function_name
+        the name of the function e.g. "abs", "mean" etc.
+
+    Returns
+    -------
+    new_function
+        the wrapped function.
+
+    Examples
+    --------
+    >>> ivy.set_framework('torch')
+    >>> from ivy.array.wrapping import _wrap_function
+    >>> absolute = _wrap_function("abs")
+    >>> x = ivy.array([-1])
+    >>> print(absolute(x))
+    ivy.array([1])
+
+    """
+
+    def new_function(self, *args, **kwargs):
+        """Add the data of the current array from which the instance function is invoked
         as the first arg parameter or kwarg parameter. Return the new function with
-        the name fn_name and the new args variable or kwargs as the new inputs.
+        the name function_name and the new args variable or kwargs as the new inputs.
         """
-        fn = ivy.__dict__[fn_name]
-        data_idx = fn.array_spec[0]
+        function = ivy.__dict__[function_name]
+        # gives us the position and name of the array argument
+        data_idx = function.array_spec[0]
         if len(args) > data_idx[0][0]:
             args = ivy.copy_nest(args, to_mutable=True)
             data_idx = [data_idx[0][0]] + [
@@ -23,9 +46,9 @@ def _wrap_fn(fn_name):
                 0 if idx is int else idx for idx in data_idx[1:]
             ]
             ivy.insert_into_nest_at_index(kwargs, data_idx, self._data)
-        return fn(*args, **kwargs)
+        return function(*args, **kwargs)
 
-    return new_fn
+    return new_function
 
 
 def add_ivy_array_instance_methods(cls, modules, to_ignore=()):
@@ -46,6 +69,6 @@ def add_ivy_array_instance_methods(cls, modules, to_ignore=()):
             ):
                 continue
             try:
-                setattr(cls, key, _wrap_fn(key))
+                setattr(cls, key, _wrap_function(key))
             except AttributeError:
                 pass
