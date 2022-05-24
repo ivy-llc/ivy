50232143587d1badec423d8cf382f3297baebf3d
djl11 <daniel.lenton11@imperial.ac.uk>
2022-05-24 12:02:58 +0200

    updated all mxnet NDArray type hints to mx.nd.NDArray.

--- ivy/functional/backends/mxnet/set.py
+++ ivy/functional/backends/mxnet/set.py
@@ -5,16 +5,16 @@ from collections import namedtuple
 
 
 def unique_inverse(
-    x: mx.ndarray.ndarray.NDArray,
-) -> Tuple[mx.ndarray.ndarray.NDArray, mx.ndarray.ndarray.NDArray]:
+    x: mx.nd.NDArray,
+) -> Tuple[mx.nd.NDArray, mx.nd.NDArray]:
     out = namedtuple("unique_inverse", ["values", "inverse_indices"])
     values, inverse_indices = mx.np.unique(x, return_inverse=True)
     return out(values, inverse_indices)
 
 
 def unique_counts(
-    x: mx.ndarray.ndarray.NDArray,
-) -> Tuple[mx.ndarray.ndarray.NDArray, mx.ndarray.ndarray.NDArray]:
+    x: mx.nd.NDArray,
+) -> Tuple[mx.nd.NDArray, mx.nd.NDArray]:
     uc = namedtuple("uc", ["values", "counts"])
     v, c = mx.unique(x, return_counts=True)
     return uc(v, c)
