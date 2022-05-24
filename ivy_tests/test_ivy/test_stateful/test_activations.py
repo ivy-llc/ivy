663dc93db492f28d7b58dcd1b1f5a7c309679ebc
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-05-24 10:42:41 +0100

    test_activation gelu hypo integration

--- ivy_tests/test_ivy/test_stateful/test_activations.py
+++ ivy_tests/test_ivy/test_stateful/test_activations.py
@@ -14,66 +14,46 @@ import ivy.functional.backends.numpy as ivy_np
 
 # GELU
 @given(
-    bs_oc_target=st.sampled_from(
-    [
-        (
-            [1, 2],
-            10,
-            [
-                [
-                    [
-                        0.0,
-                        0.0604706,
-                        0.13065024,
-                        0.21018247,
-                        0.2984952,
-                        0.39483193,
-                        0.49829122,
-                        0.6078729,
-                        0.7225253,
-                        0.841192,
-                    ],
-                    [
-                        0.0,
-                        0.0604706,
-                        0.13065024,
-                        0.21018247,
-                        0.2984952,
-                        0.39483193,
-                        0.49829122,
-                        0.6078729,
-                        0.7225253,
-                        0.841192,
-                    ],
-                ]
-            ],
-        )
-    ]),
+    x=st.lists(st.floats()),
     dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    native_array=st.booleans(),
 )
-def test_gelu(bs_oc_target, dtype, device, compile_graph, call):
-    # smoke test
-    batch_shape, output_channels, target = bs_oc_target
-    x = ivy.asarray(
-        ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), output_channels),
-        "float32",
-    )
-    gelu_layer = ivy.GELU()
-
-    # return
-    ret = gelu_layer(x)
-
-    # type test
-    assert ivy.is_ivy_array(ret)
-    # cardinality test
-    assert ret.shape == tuple(batch_shape + [output_channels])
+def test_gelu(x, dtype ,with_out, as_variable, native_array, fw,):
+    if dtype in ivy.invalid_dtypes:
+        return  # invalid dtype
+    if dtype == "float16" and fw == "torch":
+        return  # torch does not support float16 for gelu
+    x = ivy.array(x, dtype=dtype)
+    if as_variable:
+        if not ivy.is_float_dtype(dtype):
+            return  # only floating point variables are supported
+        if with_out:
+            return  # variables do not support out argument
+        x = ivy.variable(x)
+    if native_array:
+        x = x.data
+    ret = ivy.gelu(x)
+    out = ret
+    if with_out:
+        if as_variable:
+            out = ivy.variable(out)
+        if native_array:
+            out = out.data
+        ret = ivy.gelu(x, out=out)
+        if not native_array:
+            assert ret is out
+        if fw in ["tensorflow", "jax"]:
+            # these frameworks do not support native inplace updates
+            return
+        assert ret.data is (out if native_array else out.data)
     # value test
-    assert np.allclose(call(gelu_layer, x), np.array(target))
-    # compilation test
-    if call is helpers.torch_call:
-        # pytest scripting does not **kwargs
-        return
-
+    if dtype == "bfloat16":
+        return  # bfloat16 is not supported by numpy
+    assert np.allclose(
+        np.nan_to_num(ivy.to_numpy(ret)), np.nan_to_num(ivy_np.gelu(ivy.to_numpy(x)))
+    )
 
 # GEGLU
 @given(
