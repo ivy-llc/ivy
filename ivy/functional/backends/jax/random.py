bbcc5df76dbb19a585526b880d8aec45b0dc3ae3
HotzingTone <adam.he.future@gmail.com>
2022-05-25 11:36:59 +0100

    Fix device issue for random_normal of JAX backend.

--- ivy/functional/backends/jax/random.py
+++ ivy/functional/backends/jax/random.py
@@ -43,7 +43,7 @@ def random_normal(
     return (
         to_dev(
             _jax.random.normal(rng_input, shape if shape else ()),
-            default_device(device),
+            device=default_device(device),
         )
         * std
         + mean
