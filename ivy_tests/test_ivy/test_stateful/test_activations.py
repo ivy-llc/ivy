119cf98124a78814765961b9b90279bd35e7adec
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-05-25 14:59:24 +0530

    fix

--- ivy_tests/test_ivy/test_stateful/test_activations.py
+++ ivy_tests/test_ivy/test_stateful/test_activations.py
@@ -3,7 +3,7 @@
 # global
 from hypothesis import given, strategies as st
 import numpy as np
-
+import pytest
 # local
 from ivy_tests.test_ivy import helpers
 
@@ -11,36 +11,68 @@ import ivy
 import ivy.functional.backends.numpy as ivy_np
 
 #GELU
-@given(
-    x=st.lists(st.floats()),
-    dtype=st.sampled_from(ivy.float_dtypes),
-    as_variable=st.booleans(),
-    with_out=st.booleans(),
-    num_positional_args=st.integers(0, 1),
-    native_array=st.booleans(),
-    container=st.booleans(),
-    class_method=st.booleans(),
+@pytest.mark.parametrize(
+    "bs_oc_target",
+    [
+        (
+            [1, 2],
+            10,
+            [
+                [
+                    [
+                        0.0,
+                        0.0604706,
+                        0.13065024,
+                        0.21018247,
+                        0.2984952,
+                        0.39483193,
+                        0.49829122,
+                        0.6078729,
+                        0.7225253,
+                        0.841192,
+                    ],
+                    [
+                        0.0,
+                        0.0604706,
+                        0.13065024,
+                        0.21018247,
+                        0.2984952,
+                        0.39483193,
+                        0.49829122,
+                        0.6078729,
+                        0.7225253,
+                        0.841192,
+                    ],
+                ]
+            ],
+        )
+    ],
 )
-def test_gelu( x, dtype, as_variable, with_out,class_method, num_positional_args, native_array, container, fw,):
-    if dtype in ivy.invalid_dtypes:
-        return  # invalid dtype
-    if dtype == "float16" and fw == "torch":
-        return  # torch does not support float16 for gelu
-    if dtype == "bfloat16":
-        return  # bfloat16 is not supported by numpy
-    #class_method = ivy.GELU
-    helpers.test_array_class(
-        dtype,
-        as_variable,
-        False,
-        num_positional_args,
-        native_array,
-        container,
-        False,
-        fw,
-        "GELU",
-        #x=np.asarray(x, dtype=dtype),
+@pytest.mark.parametrize("dtype", ["float32"])
+@pytest.mark.parametrize("tensor_fn", [ivy.array])
+def test_gelu(bs_oc_target, dtype, tensor_fn, device, compile_graph, call):
+    # smoke test
+    batch_shape, output_channels, target = bs_oc_target
+    x = ivy.asarray(
+        ivy.linspace(ivy.zeros(batch_shape), ivy.ones(batch_shape), output_channels),
+        "float32",
     )
+    gelu_layer = ivy.GELU()
+
+    # return
+    ret = gelu_layer(x)
+
+    # type test
+    assert ivy.is_ivy_array(ret)
+    # cardinality test
+    assert ret.shape == tuple(batch_shape + [output_channels])
+    # value test
+    assert np.allclose(call(gelu_layer, x), np.array(target))
+    # compilation test
+    if call is helpers.torch_call:
+        # pytest scripting does not **kwargs
+        return
+
 
 
 @given(
