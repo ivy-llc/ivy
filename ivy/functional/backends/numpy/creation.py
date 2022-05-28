ef0df536fd3751304198b82e4cf2baf126f0ad9f
Saeed Ashraf <saeedashrraf@gmail.com>
2022-05-28 18:47:34 +0200

    Small fix

--- ivy/functional/backends/numpy/creation.py
+++ ivy/functional/backends/numpy/creation.py
@@ -25,7 +25,7 @@ def asarray(object_in, dtype=None, device=None, copy=None):
         and len(object_in) != 0
         and dtype is None
     ):
-        dtype = dtype_from_str(default_dtype(item=object_in))        
+        dtype = default_dtype(item=object_in,as_native=True)        
         if copy is True:
             return _to_dev(np.copy(np.asarray(object_in,dtype=dtype)), device)
         else:
