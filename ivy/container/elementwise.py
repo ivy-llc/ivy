2e2c478ece19c57509a0b8b41f17d6a533fb286d
djl11 <daniel.lenton11@imperial.ac.uk>
2022-05-28 12:18:58 +0200

    added ivy.Container static method static_multiply, and refactored multiply instance method to use this.

--- ivy/container/elementwise.py
+++ ivy/container/elementwise.py
@@ -944,6 +944,28 @@ class ContainerWithElementwise(ContainerBase):
             out,
         )
 
+    @staticmethod
+    def static_multiply(
+        x1: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        x2: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "multiply",
+            x1,
+            x2,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
     def multiply(
         self: ivy.Container,
         x2: Union[ivy.Container, ivy.Array, ivy.NativeArray],
@@ -951,25 +973,11 @@ class ContainerWithElementwise(ContainerBase):
         to_apply: bool = True,
         prune_unapplied: bool = False,
         map_sequences: bool = False,
+        *,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        kw = {}
-        conts = {"x1": self}
-        if ivy.is_array(x2):
-            kw["x2"] = x2
-        else:
-            conts["x2"] = x2
-        return ContainerBase.handle_inplace(
-            ContainerBase.multi_map(
-                lambda xs, _: ivy.multiply(**dict(zip(conts.keys(), xs)), **kw)
-                if ivy.is_array(xs[0])
-                else xs,
-                list(conts.values()),
-                key_chains,
-                to_apply,
-                prune_unapplied,
-            ),
-            out,
+        return self.static_multiply(
+            self, x2, key_chains, to_apply, prune_unapplied, map_sequences, out=out
         )
 
     def negative(
