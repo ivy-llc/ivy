9edf71bec25ff9b3e5854c7d0c0e24f19f565c9c
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-05-31 12:57:23 +0530

    Hypothesis integration of test_compile (#1254)

--- ivy_tests/test_ivy/test_functional/test_core/test_compiler.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_compiler.py
@@ -1,13 +1,13 @@
 """Collection of tests for unified general functions."""
 
 # global
-import pytest
 import numpy as np
 from numbers import Number
+from hypothesis import given, strategies as st
 
 # local
 import ivy
-import ivy.functional.backends.numpy
+import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.helpers as helpers
 from ivy.func_wrapper import _wrap_functions, _unwrap_functions
 
@@ -34,42 +34,51 @@ def _fn_2(x, with_non_compiled: bool = False):
     return (x + 10) ** 0.5 - 5
 
 
-@pytest.mark.parametrize("x", [[1], [[0.0, 1.0], [2.0, 3.0]]])
-@pytest.mark.parametrize("dtype", ["float32"])
-@pytest.mark.parametrize("tensor_fn", [ivy.array, helpers.var_fn])
-@pytest.mark.parametrize("with_array_caching", [True, False])
-def test_compile(x, dtype, tensor_fn, with_array_caching, device, call):
+@given(
+    data=st.data(),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=st.booleans(),
+    with_array_caching=st.booleans()
+)
+def test_compile(data, dtype, as_variable, with_array_caching, device, call):
+    if dtype == 'float16':
+        return
+    x = data.draw(helpers.get_float_array(dtype, allow_negative=False))
     # smoke test
     if (
         (isinstance(x, Number) or len(x) == 0)
-        and tensor_fn == helpers.var_fn
+        and as_variable
         and call is helpers.mx_call
     ):
         # mxnet does not support 0-dimensional variables
-        pytest.skip()
-
+        return
     _unwrap_functions()
-
     # function 1
     comp_fn = ivy.compile(_fn_1)
     # type test
     assert callable(comp_fn)
     # value test
-    x = tensor_fn(x, dtype, device)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
     non_compiled_return = _fn_1(x)
-    x = tensor_fn(x, dtype, device)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
     compiled_return = comp_fn(x)
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
-
     # function 2
     comp_fn = ivy.compile(_fn_2)
     # type test
     assert callable(comp_fn)
     # value test
-    x = tensor_fn(x, dtype, device)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
     non_compiled_return = _fn_2(x)
-    x = tensor_fn(x, dtype, device)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
     compiled_return = comp_fn(x)
     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
-
     _wrap_functions()
