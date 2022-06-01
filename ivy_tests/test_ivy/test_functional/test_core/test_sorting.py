801956ee957a8694e715cee332bc52898336c546
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-06-01 18:57:22 +0100

    test_array_function refactor dtype to input_dtype (#1280)
    * test_array_function refactor dtype to input_dtype
    
    
    Former-commit-id: 30e06ba52fca45dfaa1df971eb57ec6e27ddb4e0

--- ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
@@ -14,7 +14,7 @@ import ivy.functional.backends.numpy as ivy_np
     array_shape=helpers.lists(
         st.integers(1, 5), min_size="num_dims", max_size="num_dims", size_bounds=[1, 5]
     ),
-    dtype=st.sampled_from(ivy_np.valid_dtypes),
+    input_dtype=st.sampled_from(ivy_np.valid_dtypes),
     data=st.data(),
     as_variable=st.booleans(),
     with_out=st.booleans(),
@@ -25,7 +25,7 @@ import ivy.functional.backends.numpy as ivy_np
 )
 def test_argsort(
     array_shape,
-    dtype,
+    input_dtype,
     data,
     as_variable,
     with_out,
@@ -36,12 +36,12 @@ def test_argsort(
     fw,
 ):
     # smoke for torch
-    if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
+    if fw == "torch" and input_dtype in ["uint16", "uint32", "uint64"]:
         return
 
     # we do not want any nans
     x = data.draw(
-        helpers.nph.arrays(shape=array_shape, dtype=dtype).filter(
+        helpers.nph.arrays(shape=array_shape, dtype=input_dtype).filter(
             lambda x: not np.any(np.isnan(x))
         )
     )
@@ -51,21 +51,8 @@ def test_argsort(
     descending = data.draw(st.booleans())
     stable = data.draw(st.booleans())
 
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        with_out,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "argsort",
-        x=x,
-        axis=axis,
-        descending=descending,
-        stable=stable,
-    )
+    helpers.test_array_function(input_dtype, as_variable, with_out, num_positional_args, native_array, container,
+                                instance_method, fw, "argsort", x=x, axis=axis, descending=descending, stable=stable)
 
 
 # sort
@@ -73,7 +60,7 @@ def test_argsort(
     array_shape=helpers.lists(
         st.integers(1, 5), min_size="num_dims", max_size="num_dims", size_bounds=[1, 5]
     ),
-    dtype=st.sampled_from(ivy_np.valid_dtypes),
+    input_dtype=st.sampled_from(ivy_np.valid_dtypes),
     data=st.data(),
     as_variable=st.booleans(),
     with_out=st.booleans(),
@@ -84,7 +71,7 @@ def test_argsort(
 )
 def test_sort(
     array_shape,
-    dtype,
+    input_dtype,
     data,
     as_variable,
     with_out,
@@ -95,12 +82,12 @@ def test_sort(
     fw,
 ):
     # smoke for torch
-    if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
+    if fw == "torch" and input_dtype in ["uint16", "uint32", "uint64"]:
         return
 
     # we do not want any nans
     x = data.draw(
-        helpers.nph.arrays(shape=array_shape, dtype=dtype).filter(
+        helpers.nph.arrays(shape=array_shape, dtype=input_dtype).filter(
             lambda x: not np.any(np.isnan(x))
         )
     )
@@ -110,18 +97,5 @@ def test_sort(
     descending = data.draw(st.booleans())
     stable = data.draw(st.booleans())
 
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        with_out,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "sort",
-        x=x,
-        axis=axis,
-        descending=descending,
-        stable=stable,
-    )
+    helpers.test_array_function(input_dtype, as_variable, with_out, num_positional_args, native_array, container,
+                                instance_method, fw, "sort", x=x, axis=axis, descending=descending, stable=stable)
