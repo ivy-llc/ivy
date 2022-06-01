a79fca9ce1e23710acda6839146da344f613e20c
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-06-01 08:50:25 +0100

    test_unique_inverse hypothesis integration.

--- ivy_tests/test_ivy/test_functional/test_core/test_set.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_set.py
@@ -61,14 +61,14 @@ def test_unique_values(array_shape,
     container=st.booleans(),
     instance_method=st.booleans())
 def test_unique_all(array_shape,
-                       dtype,
-                       as_variable,
-                       num_positional_args,
-                       native_array,
-                       container,
-                       instance_method,
-                       fw,
-                       device):
+                    dtype,
+                    as_variable,
+                    num_positional_args,
+                    native_array,
+                    container,
+                    instance_method,
+                    fw,
+                    device):
     if fw == "torch" and ("int" in dtype or "16" in dtype):
         return
 
@@ -128,7 +128,44 @@ def test_unique_counts(array_shape,
         "unique_counts",
         x=x)
 
-# Still to Add #
-# ---------------#
 
-# unique_inverse
+@given(array_shape=helpers.lists(
+    st.integers(2, 5),
+    min_size="num_dims",
+    max_size="num_dims",
+    size_bounds=[2, 5]),
+    dtype=st.sampled_from(ivy_np.valid_numeric_dtypes),
+    data=st.data(),
+    as_variable=st.booleans(),
+    num_positional_args=st.integers(0, 2),
+    native_array=st.booleans(),
+    container=st.booleans(),
+    instance_method=st.booleans())
+def test_unique_inverse(array_shape,
+                       dtype,
+                       data,
+                       as_variable,
+                       num_positional_args,
+                       native_array,
+                       container,
+                       instance_method,
+                       fw,
+                       device):
+
+    if fw == "torch" and ("int" in dtype or "16" in dtype):
+        return
+
+    x = data.draw(helpers.nph.arrays(shape=array_shape,
+                                     dtype=dtype))
+
+    helpers.test_array_function(
+        dtype,
+        as_variable,
+        False,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "unique_inverse",
+        x=x)
