30e06ba52fca45dfaa1df971eb57ec6e27ddb4e0
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-06-01 18:57:22 +0100

    test_array_function refactor dtype to input_dtype (#1280)
    * test_array_function refactor dtype to input_dtype

--- ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
@@ -25,24 +25,14 @@ def test_cross_entropy(
     instance_method,
     fw,
 ):
-    dtype, x = dtype_and_x
+    input_dtype, x = dtype_and_x
     if (v == [] for v in x):
         return
-    if fw == "torch" and dtype == "float16":
+    if fw == "torch" and input_dtype == "float16":
         return
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        False,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "cross_entropy",
-        true=np.asarray(x[0], dtype=dtype[0]),
-        pred=np.asarray(x[1], dtype=dtype[1]),
-    )
+    helpers.test_array_function(input_dtype, as_variable, False, num_positional_args, native_array, container,
+                                instance_method, fw, "cross_entropy", true=np.asarray(x[0], dtype=input_dtype[0]),
+                                pred=np.asarray(x[1], dtype=input_dtype[1]))
 
 
 # binary_cross_entropy
@@ -63,24 +53,21 @@ def test_binary_cross_entropy(
     instance_method,
     fw,
 ):
-    dtype, x = dtype_and_x
+    input_dtype, x = dtype_and_x
     if (v == [] for v in x):
         return
-    if fw == "torch" and dtype == "float16":
+    if fw == "torch" and input_dtype == "float16":
         return
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        False,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "binary_cross_entropy",
-        true=np.asarray(x[0], dtype=dtype[0]),
-        pred=np.asarray(x[1], dtype=dtype[1]),
-    )
+    helpers.test_array_function(input_dtype,
+                                as_variable,
+                                False,
+                                num_positional_args,
+                                native_array,
+                                container,
+                                instance_method,
+                                fw,
+                                "binary_cross_entropy", true=np.asarray(x[0], dtype=input_dtype[0]),
+                                pred=np.asarray(x[1], dtype=input_dtype[1]))
 
 
 # sparse_cross_entropy
@@ -101,21 +88,19 @@ def test_sparse_cross_entropy(
     instance_method,
     fw,
 ):
-    dtype, x = dtype_and_x
+    input_dtype, x = dtype_and_x
     if (v == [] for v in x):
         return
-    if fw == "torch" and dtype == "float16":
+    if fw == "torch" and input_dtype == "float16":
         return
-    helpers.test_array_function(
-        dtype,
-        as_variable,
-        False,
-        num_positional_args,
-        native_array,
-        container,
-        instance_method,
-        fw,
-        "sparse_cross_entropy",
-        true=np.asarray(x[0], dtype=dtype[0]),
-        pred=np.asarray(x[1], dtype=dtype[1]),
-    )
+    helpers.test_array_function(input_dtype,
+                                as_variable,
+                                False,
+                                num_positional_args,
+                                native_array,
+                                container,
+                                instance_method,
+                                fw,
+                                "sparse_cross_entropy",
+                                true=np.asarray(x[0], dtype=input_dtype[0]),
+                                pred=np.asarray(x[1], dtype=input_dtype[1]))
