a08c40d14934db922dde22e5fa7fd0033b73c276
djl11 <daniel.lenton11@imperial.ac.uk>
2022-06-04 10:45:16 +0200

    fixes to ivy.astype function, which should not be dtype wrapped, as the dtype argument is not keyword-only, and it cannot be as the Array API Standard mandates that it can be passed positionally.

--- ivy/functional/backends/torch/data_type.py
+++ ivy/functional/backends/torch/data_type.py
@@ -84,6 +84,7 @@ def broadcast_arrays(*arrays: torch.Tensor) -> List[torch.Tensor]:
 
 
 def astype(x: torch.Tensor, dtype: torch.dtype, copy: bool = True) -> torch.Tensor:
+    dtype = ivy.as_native_dtype(dtype)
     if isinstance(dtype, str):
         dtype = ivy.as_native_dtype(dtype)
     if copy:
