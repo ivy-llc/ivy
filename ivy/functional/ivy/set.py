e3b8cdaa48976c376a66f77b725de75b96d342ef
Ya2001 <srivats66@gmail.com>
2022-06-07 16:14:17 +0100

    Fixed issue on line 150 " multiple spaces after operator

--- ivy/functional/ivy/set.py
+++ ivy/functional/ivy/set.py
@@ -146,7 +146,7 @@ def unique_values(
 
 def unique_counts(
         x: Union[ivy.Array, ivy.NativeArray]
-) ->    Tuple[ivy.Array, ivy.Array]:
+) -> Tuple[ivy.Array, ivy.Array]:
     """Returns the unique elements of an input array ``x`` and the corresponding counts for
     each unique element in ``x``.
 
