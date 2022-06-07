77dda078d25e834d8b4b00fc5f874cc318a3264b
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-06-07 17:56:02 +0530

    more changes
    Former-commit-id: b0d83b31b1859bcba4842de1a122db9c1f46f0c6

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -1,7 +1,6 @@
 # global
 import numpy as np
 from typing import Optional
-import numpy.array_api as npa
 
 # local
 import ivy
@@ -16,9 +15,7 @@ def add(x1: np.ndarray, x2: np.ndarray) -> np.ndarray:
     if not isinstance(x2, np.ndarray):
         x2 = np.asarray(x2, dtype=x1.dtype)
     # numpy.array_api doesn't support float16
-    if x1.dtype == "float16" or x2.dtype == "float16":
-        return np.asarray(np.add(np.asarray(x1), np.asarray(x2)))
-    return np.asarray(npa.add(npa.asarray(x1), npa.asarray(x2)))
+    return np.add(np.asarray(x1), np.asarray(x2))
 
 
 def pow(
