0a72a22350c590a8e01b476b1edb7c47913965ee
sohamtiwari3120 <soham.tiwari800@gmail.com>
2022-06-07 14:45:29 +0530

    fix: added noqa for lin containing url

--- ivy/functional/ivy/manipulation.py
+++ ivy/functional/ivy/manipulation.py
@@ -334,7 +334,7 @@ def expand_dims(
 
     This method conforms to the `Array API Standard
     <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of the
-    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.elementwise_functions.tan.html>`_
+    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.manipulation_functions.expand_dims.html>`_ # noqa
     in the standard. The descriptions above assume an array input for simplicity, but
     the method also accepts :code:`ivy.Container` instances in place of
     :code:`ivy.Array` or :code:`ivy.NativeArray` instances, as shown in the type hints
