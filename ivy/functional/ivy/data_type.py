38544dc63d096c55ba2dd6a8f3fe727473d8f82c
Marouf Shaikh <maruf98@gmail.com>
2022-06-08 11:32:59 +0000

    added dtype keyword argument for func astype

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -202,11 +202,11 @@ def astype(
     --------
     >>> x = ivy.array([1, 2])
     >>> dtype = ivy.float64
-    >>> y = ivy.astype(x, dtype)
+    >>> y = ivy.astype(x, dtype = dtype)
     >>> print(y)
     ivy.array([1., 2.])
     """
-    return _cur_backend(x).astype(x, dtype, copy)
+    return _cur_backend(x).astype(x, dtype = dtype, copy = copy)
 
 
 # Extra #
