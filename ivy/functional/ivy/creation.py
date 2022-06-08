652bcd68d447294fc71ffb6ec32b780b857a8db1
CuriousNick <nickifeajika@gmail.com>
2022-06-08 08:29:13 +0100

    reformat layer norms (#1168)

--- ivy/functional/ivy/creation.py
+++ ivy/functional/ivy/creation.py
@@ -674,6 +674,7 @@ def native_array(
     # ToDo: Make this more efficient,
     # ideally without first converting to ivy.Array with ivy.asarray and then
     # converting back to native with ivy.to_native
+
     return ivy.to_native(ivy.asarray(x, dtype=dtype, device=device))
 
 
