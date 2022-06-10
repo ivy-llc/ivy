177bb55be75bccd11a63c1742dc657793c6b1920
Aarsh2001 <ac5973@srmist.edu.in>
2022-06-10 17:14:07 +0530

    hypothesis integration for test_norms

--- ivy_tests/test_ivy/test_stateful/test_norms.py
+++ ivy_tests/test_ivy/test_stateful/test_norms.py
@@ -1,36 +1,43 @@
 """Collection of tests for normalization layers."""
 
 # global
-import pytest
 import numpy as np
+from hypothesis import given, strategies as st
 
 # local
 import ivy
 from ivy.container import Container
+import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.helpers as helpers
 
 
 # layer norm
-@pytest.mark.parametrize(
-    "x_n_ns_n_target",
-    [
-        (
-            [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
-            [3],
-            [[-1.2247356, 0.0, 1.2247356], [-1.2247356, 0.0, 1.2247356]],
-        ),
-    ],
+@given(
+    x_n_ns_n_target=st.sampled_from(
+        [
+            (
+                [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                [3],
+                [[-1.2247356, 0.0, 1.2247356], [-1.2247356, 0.0, 1.2247356]],
+            ),
+        ]
+    ),
+    with_v=st.booleans(),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=st.booleans(),
 )
-@pytest.mark.parametrize("with_v", [True, False])
-@pytest.mark.parametrize("dtype", ["float32"])
-@pytest.mark.parametrize("tensor_fn", [ivy.array, helpers.var_fn])
 def test_layer_norm_layer(
-    x_n_ns_n_target, with_v, dtype, tensor_fn, device, compile_graph, call
+    x_n_ns_n_target, with_v, dtype, as_variable, device, compile_graph, call
 ):
     # smoke test
+    if dtype == "float16":
+        return
     x, normalized_shape, target = x_n_ns_n_target
-    x = tensor_fn(x, dtype, device)
-    target = tensor_fn(target, dtype, device)
+    x = ivy.array(x, dtype=dtype, device=device)
+    target = ivy.array(target, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
+        target = ivy.variable(target)
     if with_v:
         v = Container(
             {
