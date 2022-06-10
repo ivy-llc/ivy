630c705b71d78756459b97a8405fd03e8d1b34a5
Matt <matthew-barrett@hotmail.co.uk>
2022-06-10 22:13:39 +0100

    x

--- ivy_tests/test_ivy/test_functional/test_core/test_compiler.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_compiler.py
@@ -9,7 +9,6 @@ from hypothesis import given, strategies as st
 import ivy
 import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.helpers as helpers
-from ivy.func_wrapper import _wrap_functions, _unwrap_functions
 
 
 # Tests #
@@ -34,48 +33,48 @@ def _fn_2(x, with_non_compiled: bool = False):
     return (x + 10) ** 0.5 - 5
 
 
-# @given(
-#     data=st.data(),
-#     dtype=st.sampled_from(ivy_np.valid_float_dtypes),
-#     as_variable=st.booleans(),
-#     with_array_caching=st.booleans(),
-# )
-# def test_compile(data, dtype, as_variable, with_array_caching, device, call):
-#     shape = data.draw(helpers.get_shape())
-#     x = data.draw(helpers.array_values(dtype=dtype, shape=shape, allow_negative=False))
-#     # smoke test
-#     if (
-#         (isinstance(x, Number) or len(x) == 0)
-#         and as_variable
-#         and call is helpers.mx_call
-#     ):
-#         # mxnet does not support 0-dimensional variables
-#         return
-#     # function 1
-#     comp_fn = ivy.compile(_fn_1)
-#     # type test
-#     assert callable(comp_fn)
-#     # value test
-#     x = ivy.array(x, dtype=dtype, device=device)
-#     if as_variable:
-#         x = ivy.variable(x)
-#     non_compiled_return = _fn_1(x)
-#     x = ivy.array(x, dtype=dtype, device=device)
-#     if as_variable:
-#         x = ivy.variable(x)
-#     compiled_return = comp_fn(x)
-#     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
-#     # function 2
-#     comp_fn = ivy.compile(_fn_2)
-#     # type test
-#     assert callable(comp_fn)
-#     # value test
-#     x = ivy.array(x, dtype=dtype, device=device)
-#     if as_variable:
-#         x = ivy.variable(x)
-#     non_compiled_return = _fn_2(x)
-#     x = ivy.array(x, dtype=dtype, device=device)
-#     if as_variable:
-#         x = ivy.variable(x)
-#     compiled_return = comp_fn(x)
-#     assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
+@given(
+    data=st.data(),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=st.booleans(),
+    with_array_caching=st.booleans(),
+)
+def test_compile(data, dtype, as_variable, with_array_caching, device, call):
+    shape = data.draw(helpers.get_shape())
+    x = data.draw(helpers.array_values(dtype=dtype, shape=shape, allow_negative=False))
+    # smoke test
+    if (
+        (isinstance(x, Number) or len(x) == 0)
+        and as_variable
+        and call is helpers.mx_call
+    ):
+        # mxnet does not support 0-dimensional variables
+        return
+    # function 1
+    comp_fn = ivy.compile(_fn_1)
+    # type test
+    assert callable(comp_fn)
+    # value test
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
+    non_compiled_return = _fn_1(x)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
+    compiled_return = comp_fn(x)
+    assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
+    # function 2
+    comp_fn = ivy.compile(_fn_2)
+    # type test
+    assert callable(comp_fn)
+    # value test
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
+    non_compiled_return = _fn_2(x)
+    x = ivy.array(x, dtype=dtype, device=device)
+    if as_variable:
+        x = ivy.variable(x)
+    compiled_return = comp_fn(x)
+    assert np.allclose(ivy.to_numpy(non_compiled_return), ivy.to_numpy(compiled_return))
