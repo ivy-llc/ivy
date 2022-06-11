02cd90f58f1b98bf88e68035ba060fe665bc64f4
Matt Barrett <83289589+mattbarrett98@users.noreply.github.com>
2022-06-11 14:49:27 +0100

    old wrapping method replaced with simpler approach using decorators (#1375)

--- ivy/func_wrapper.py
+++ ivy/func_wrapper.py
@@ -1,13 +1,9 @@
 import ivy
 import inspect
-import importlib
 import functools
-import numpy as np
-from types import ModuleType
-from typing import Callable, Optional, List, Union
+from types import FunctionType
+from typing import Callable
 
-
-wrapped_modules_n_classes = []
 NON_WRAPPED_FUNCTIONS = [
     "copy_nest",
     "current_backend",
@@ -96,45 +92,6 @@ FUNCTIONS_W_CONT_SUPPORT = [
     "stable_divide",
     "stable_pow",
 ]
-ARRAYLESS_RET_FUNCTIONS = [
-    "to_numpy",
-    "to_list",
-    "to_scalar",
-    "is_native_array",
-    "is_ivy_array",
-    "is_variable",
-]
-NESTED_ARRAY_RET_FUNCTIONS = ["unstack", "split"]
-NON_DTYPE_WRAPPED_FUNCTIONS = [
-    "arange",
-    "asarray",
-    "array",
-    "full",
-    "prod",
-    "sum",
-    "astype",
-]
-NON_DEV_WRAPPED_FUNCTIONS = [
-    "get_all_ivy_arrays_on_dev",
-    "num_ivy_arrays_on_dev",
-    "print_all_ivy_arrays_on_dev",
-    "clear_mem_on_dev",
-    "total_mem_on_dev",
-    "to_dev",
-    "split_factor",
-    "set_split_factor",
-    "split_func_call",
-    "as_native_dev",
-    "as_ivy_dev",
-    "dev_unify_iter",
-    "dev_unify_nest",
-    "dev_unify",
-    "dev_unify_array",
-    "dev_util",
-    "percent_used_mem_on_dev",
-    "used_mem_on_dev",
-]
-
 FW_FN_KEYWORDS = {
     "numpy": [],
     "jax": [],
@@ -215,6 +172,7 @@ def inputs_to_native_arrays(fn: Callable) -> Callable:
         )
         return fn(*native_args, **native_kwargs)
 
+    new_fn.inputs_to_native_arrays = True
     return new_fn
 
 
@@ -244,6 +202,7 @@ def inputs_to_ivy_arrays(fn: Callable) -> Callable:
         )
         return fn(*ivy_args, **ivy_kwargs)
 
+    new_fn.inputs_to_ivy_arrays = True
     return new_fn
 
 
@@ -271,6 +230,7 @@ def outputs_to_ivy_arrays(fn: Callable) -> Callable:
         # convert all arrays in the return to `ivy.Array` instances
         return ivy.to_ivy(ret, nested=True, include_derived={tuple: True})
 
+    new_fn.outputs_to_ivy_arrays = True
     return new_fn
 
 
@@ -315,6 +275,7 @@ def infer_dtype(fn: Callable) -> Callable:
         # call the function with dtype provided explicitly
         return fn(*args, dtype=dtype, **kwargs)
 
+    new_fn.infer_dtype = True
     return new_fn
 
 
@@ -351,6 +312,7 @@ def infer_device(fn: Callable) -> Callable:
         # call the function with device provided explicitly
         return fn(*args, device=device, **kwargs)
 
+    new_fn.infer_device = True
     return new_fn
 
 
@@ -397,6 +359,7 @@ def handle_out_argument(fn: Callable) -> Callable:
         ret = fn(*args, **kwargs)
         return ivy.inplace_update(out, ret)
 
+    new_fn.handle_out_argument = True
     return new_fn
 
 
@@ -439,195 +402,55 @@ def handle_nestable(fn: Callable) -> Callable:
         # the passed arguments, returning an ivy or a native array.
         return fn(*args, **kwargs)
 
+    new_fn.handle_nestable = True
     return new_fn
 
 
 # Functions #
 
 
-def _wrap_function(fn: Callable) -> Callable:
-    """
-    Creates a wrapped ivy version of the function if it is not a private function and
-    not in the non wrapped functions list. This allows the new function to accept as
-    inputs an ivy array before performing the required operation and then returning
-    an ivy array.
+def _wrap_function(value: Callable, original: Callable) -> Callable:
+    """Apply wrapping to backend implementation `value` if the original implementation
+    `original` is also wrapped, and if `value` is not already wrapped. Attributes
+    `handle_nestable`, `infer_device` etc are set during wrapping, hence indicate to
+    us whether a certain function has been wrapped or not.
 
     Parameters
     ----------
-    fn
-        function to be wrapped
+    value
+        the new implementation to potentially wrap
+    original
+        the original implementation of `value` which tells us which wrappers we need.
 
     Returns
     -------
-        The wrapped version of the function with all the necessary attributes updated.
+    ret
+        `value` appropriately wrapped if `value` is a function, otherwise just the
+        input is returned.
     """
-    # do nothing if the function is private or in the non wrapped functions list
-    if hasattr(fn, "__name__") and (
-        fn.__name__[0] == "_" or fn.__name__ in NON_WRAPPED_FUNCTIONS
-    ):
-        return fn
-
-    # determine whether the function has an out argument
-    keys = inspect.signature(fn).parameters.keys()
-    handle_dtype = "dtype" in keys
-    handle_dev = "device" in keys
-
-    # get function name
-    fn_name = fn.__name__
-
-    # with outputs converted to ivy arrays
-    if fn_name not in ARRAYLESS_RET_FUNCTIONS + NESTED_ARRAY_RET_FUNCTIONS:
-        fn = outputs_to_ivy_arrays(fn)
-
-    # with input converted to native arrays
-    fn = inputs_to_native_arrays(fn)
-
-    # with inplace updates handled
-    fn = handle_out_argument(fn)
-
-    # with dtypes handled
-    if handle_dtype and fn_name not in NON_DTYPE_WRAPPED_FUNCTIONS:
-        fn = infer_dtype(fn)
-
-    # with device handled
-    if handle_dev and fn_name not in NON_DEV_WRAPPED_FUNCTIONS:
-        fn = infer_device(fn)
-
-    # with nestable property handled
-    if hasattr(ivy.Container, fn_name) and fn_name not in FUNCTIONS_W_CONT_SUPPORT:
-        fn = handle_nestable(fn)
-
-    # return the wrapped function
-    return fn
-
-
-def _unwrap_function(function_wrapped: Callable) -> Callable:
-    """
-    Unwraps the function `function_wrapped`.
-
-    Parameters
-    ----------
-    function_wrapped
-        The function to be unwrapped.
-
-    Returns
-    -------
-    The unwrapped version of the function which is the same as the passed function
-    for unwrapped functions and the inner_fn if the function is wrapped.
-    The newly unwrapped function accepts inputs and returns outputs as native arrays
-    instead of ivy arrays.
-    """
-    if not hasattr(function_wrapped, "wrapped") or not function_wrapped.wrapped:
-        return function_wrapped
-    return function_wrapped.inner_fn
-
-
-def _invalid_function(function: Callable, framework: Optional[str] = None) -> bool:
-    if framework is None:
-        framework = ivy.current_backend_str()
-    if isinstance(function, np.ufunc):
-        return False
-    if not hasattr(function, "__module__") or not function.__module__:
-        return True
-    fw_fn_keywords = ["ivy", framework] + FW_FN_KEYWORDS[framework]
-    for kw in fw_fn_keywords:
-        if kw in function.__module__:
-            return False
-    return True
-
-
-def _wrap_or_unwrap_functions(
-    wrap_or_unwrap_function: Callable,
-    val: Optional[Union[ModuleType, Callable]] = None,
-    framework: Optional[str] = None,
-    classes_to_wrap: Optional[List] = [],
-    native: Optional[bool] = False,
-    depth: Optional[int] = 0,
-) -> Union[Callable, ModuleType]:
-    if framework is None:
-        framework = ivy.current_backend_str()
-    if val is None:
-        val = importlib.import_module(ivy.current_backend_str()) if native else ivy
-    str_to_check = framework if native else "ivy"
-    is_class = inspect.isclass(val)
-    if isinstance(val, ModuleType) or (val in classes_to_wrap):
-        if val in wrapped_modules_n_classes or (
-            (
-                "__file__" not in val.__dict__
-                or (str_to_check not in val.__file__)
-                or "framework_handler" in val.__file__
-            )
-            and not is_class
-        ):
-            return val
-        wrapped_modules_n_classes.append(val)
-        # if `val` is a class we recursively call `_wrap_or_unwrap_functions`
-        # on every member of the class
-        if is_class:
-            for k in dir(val):
-                if native and (k in NATIVE_KEYS_TO_SKIP[framework]):
-                    continue
-                v = getattr(val, k)
-                if v is not None:
-                    # noinspection PyBroadException
-                    try:
-                        setattr(
-                            val,
-                            k,
-                            _wrap_or_unwrap_functions(
-                                wrap_or_unwrap_function,
-                                v,
-                                framework,
-                                classes_to_wrap,
-                                native,
-                                depth + 1,
-                            ),
-                        )
-                    except Exception:
-                        pass
-        # or if `val` is a module, we recursively call
-        # `_wrap_or_unwrap_functions` on each value of its dict
-        else:
-            for k, v in val.__dict__.items():
-                if native and (k in NATIVE_KEYS_TO_SKIP[framework] or k[0] == "_"):
-                    continue
-                if v is None:
-                    val.__dict__[k] = v
-                else:
-                    # noinspection PyBroadException
-                    try:
-                        val.__dict__[k] = _wrap_or_unwrap_functions(
-                            wrap_or_unwrap_function,
-                            v,
-                            framework,
-                            classes_to_wrap,
-                            native,
-                            depth + 1,
-                        )
-                    except Exception:
-                        pass
-        if depth == 0:
-            wrapped_modules_n_classes.clear()
-        return val
-    # if `val` is a function/method we wrap it and return it (unless
-    # there are issues with it being an invalid function)
-    elif callable(val) and not is_class:
-        if depth == 0:
-            wrapped_modules_n_classes.clear()
+    if isinstance(value, FunctionType):
         if (
-            hasattr(val, "inner_fn")
-            and (_invalid_function(val.inner_fn) and not native)
-        ) or (_invalid_function(val) and not native):
-            return val
-        return wrap_or_unwrap_function(val)
-    if depth == 0:
-        wrapped_modules_n_classes.clear()
-    return val
-
-
-def _wrap_functions():
-    return _wrap_or_unwrap_functions(_wrap_function)
-
-
-def _unwrap_functions():
-    return _wrap_or_unwrap_functions(_unwrap_function)
+            hasattr(original, "handle_nestable")
+            and not hasattr(value, "handle_nestable")
+        ) or (
+            hasattr(ivy.Container, value.__name__)
+            and value.__name__ not in FUNCTIONS_W_CONT_SUPPORT + NON_WRAPPED_FUNCTIONS
+        ):
+            value = handle_nestable(value)
+        if hasattr(original, "infer_device") and not hasattr(value, "infer_device"):
+            value = infer_device(value)
+        if hasattr(original, "infer_dtype") and not hasattr(value, "infer_dtype"):
+            value = infer_dtype(value)
+        if hasattr(original, "handle_out_argument") and not hasattr(
+            value, "handle_out_argument"
+        ):
+            value = handle_out_argument(value)
+        if hasattr(original, "inputs_to_native_arrays") and not hasattr(
+            value, "inputs_to_native_arrays"
+        ):
+            value = inputs_to_native_arrays(value)
+        if hasattr(original, "outputs_to_ivy_arrays") and not hasattr(
+            value, "outputs_to_ivy_arrays"
+        ):
+            value = outputs_to_ivy_arrays(value)
+    return value
