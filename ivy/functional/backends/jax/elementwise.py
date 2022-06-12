4f777a88f03b64c8d82907cb0df3e2cff1cf16bb
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2022-06-12 23:22:46 +0545

    reformat: floor - added relevant docstrings and docstring examples (#1328)
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
    Former-commit-id: 356d7d65293956ea4a7712c38deec1d914f2ccb9

--- ivy/functional/backends/jax/elementwise.py
+++ ivy/functional/backends/jax/elementwise.py
@@ -49,7 +49,8 @@ def ceil(x: JaxArray) -> JaxArray:
     return ret
 
 
-def floor(x: JaxArray) -> JaxArray:
+def floor(x: JaxArray
+         ) -> JaxArray:
     if "int" in str(x.dtype):
         ret = x
     else:
