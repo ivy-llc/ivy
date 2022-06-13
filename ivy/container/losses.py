5ae8936c32415471342e6045ff8a1d5bd68fa12e
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-13 21:11:34 +0800

    add ivy.Container instance method - binary_cross_entropy

--- ivy/container/losses.py
+++ ivy/container/losses.py
@@ -43,4 +43,36 @@ class ContainerWithLosses(ContainerBase):
             out,
         )
 
+    def binary_cross_entropy(
+        self: ivy.Container,
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"true": self}
+        if ivy.is_array(pred):
+            kw["pred"] = pred
+        else:
+            conts["pred"] = pred
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.binary_cross_entropy(
+                    **dict(zip(conts.keys(), xs)), **kw, epsilon=epsilon
+                )
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
     pass
