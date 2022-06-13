fix gelu and softmax

ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
@@ -1,11 +1,8 @@
 """Collection of tests for unified neural network activation functions."""
 
 # global
-import pytest
 import numpy as np
-import torch
 from hypothesis import given, strategies as st
-
 # local
 
 import ivy
@@ -20,12 +17,15 @@ import ivy.functional.backends.numpy as ivy_np
     as_variable=st.booleans(),
     with_out=st.booleans(),
     native_array=st.booleans(),
-    num_positional_args=st.integers(0, 1),
+    num_positional_args=st.integers(0, 2),
     container=st.booleans(),
     instance_method=st.booleans(),
 )
 def test_relu(dtype_and_x, as_variable, with_out, native_array,num_positional_args,container,instance_method,fw):
         dtype, x = dtype_and_x
+        x = np.asarray(x, dtype=dtype)
+        if x.shape == ():
+            return
         if fw == "torch" and dtype == "float16":
             return
         helpers.test_array_function(
@@ -38,17 +38,17 @@ def test_relu(dtype_and_x, as_variable, with_out, native_array,num_positional_ar
             container,
             instance_method,
             "relu",
-            x=np.asarray(x, dtype=dtype),
+            x=x,
         )
 
 
 # leaky_relu
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
-    as_variable=st.booleans(),
+    as_variable=helpers.list_of_length(st.booleans(), 2),
     native_array=st.booleans(),
     num_positional_args=st.integers(0, 2),
-    container=st.booleans(),
+    container=helpers.list_of_length(st.booleans(), 2),
     instance_method=st.booleans(),
     alpha = st.floats(),
 )
@@ -76,19 +76,19 @@ def test_leaky_relu(dtype_and_x, alpha,as_variable, num_positional_args,containe
 
 # gelu
 @given(
-    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes,allow_inf = False),
+    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
     as_variable=st.booleans(),
     native_array=st.booleans(),
-    num_positional_args=st.integers(0, 2),
+    num_positional_args=st.integers(0,2),
     container=st.booleans(),
     instance_method=st.booleans(),
-    approximate = st.booleans(),
-
+    approximate =st.booleans(),
 )
 def test_gelu(dtype_and_x,as_variable,approximate, num_positional_args,container,instance_method,native_array, fw):
     dtype,x = dtype_and_x
     if fw == "torch" and dtype == "float16":
         return
+    x = np.asarray(x, dtype=dtype)
     helpers.test_array_function(
         dtype,
         as_variable,
@@ -99,9 +99,8 @@ def test_gelu(dtype_and_x,as_variable,approximate, num_positional_args,container
         container,
         instance_method,
         "gelu",
-        x=np.asarray(x, dtype=dtype),
+        x=x,
         approximate = approximate,
-
     )
 
 # tanh
@@ -109,7 +108,7 @@ def test_gelu(dtype_and_x,as_variable,approximate, num_positional_args,container
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
     as_variable=st.booleans(),
     native_array=st.booleans(),
-    num_positional_args=st.integers(0, 1),
+    num_positional_args=st.integers(0, 2),
     container=st.booleans(),
     instance_method=st.booleans(),
 )
@@ -117,7 +116,6 @@ def test_tanh(dtype_and_x,as_variable, num_positional_args,container,instance_me
     dtype, x = dtype_and_x
     if fw == "torch" and dtype == "float16":
         return
-
     helpers.test_array_function(
         dtype,
         as_variable,
@@ -137,7 +135,7 @@ def test_tanh(dtype_and_x,as_variable, num_positional_args,container,instance_me
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
     as_variable=st.booleans(),
     native_array=st.booleans(),
-    num_positional_args=st.integers(0, 1),
+    num_positional_args=st.integers(0, 2),
     container=st.booleans(),
     instance_method=st.booleans(),
 )
@@ -157,8 +155,6 @@ def test_sigmoid(dtype_and_x,as_variable, num_positional_args,container,instance
         "sigmoid",
         x=np.asarray(x, dtype=dtype),
     )
-    
-    
 # softmax
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy.all_float_dtypes),
@@ -172,7 +168,7 @@ def test_sigmoid(dtype_and_x,as_variable, num_positional_args,container,instance
 )
 def test_softmax(dtype_and_x,as_variable, num_positional_args,container,instance_method,native_array, fw):
     dtype, x = dtype_and_x
-    axis = None
+    axis = -1
     if fw == "torch" and dtype == "float16":
         return
     x = np.asarray(x, dtype=dtype)
@@ -191,14 +187,12 @@ def test_softmax(dtype_and_x,as_variable, num_positional_args,container,instance
         x=x,
         axis = axis,
     )
-
-
 # softplus
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
     as_variable=st.booleans(),
     native_array=st.booleans(),
-    num_positional_args=st.integers(0, 1),
+    num_positional_args=st.integers(0, 2),
     container=st.booleans(),
     instance_method=st.booleans(),
 )