3959bb6a824943f48ede3f4ac390eb7e2154d136
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-13 21:09:38 +0800

    add ivy.Container instance method - cross_entropy

--- ivy/container/losses.py
+++ ivy/container/losses.py
@@ -1,4 +1,8 @@
+# global
+from typing import Optional, Union, List, Dict
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
@@ -6,4 +10,37 @@ from ivy.container.base import ContainerBase
 
 # noinspection PyMissingConstructor
 class ContainerWithLosses(ContainerBase):
+    def cross_entropy(
+        self: ivy.Container,
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        kw = {}
+        conts = {"true": self}
+        if ivy.is_array(pred):
+            kw["pred"] = pred
+        else:
+            conts["pred"] = pred
+        return ContainerBase.handle_inplace(
+            ContainerBase.multi_map(
+                lambda xs, _: ivy.cross_entropy(
+                    **dict(zip(conts.keys(), xs)), **kw, axis=axis, epsilon=epsilon
+                )
+                if ivy.is_array(xs[0])
+                else xs,
+                list(conts.values()),
+                key_chains,
+                to_apply,
+                prune_unapplied,
+            ),
+            out,
+        )
+
     pass
