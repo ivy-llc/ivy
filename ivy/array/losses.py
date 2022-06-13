213d5ab701386f5e0095d65ab3b4f19bc4f9eaab
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-13 20:25:03 +0800

    add ivy.Array instance method - cross_entropy

--- ivy/array/losses.py
+++ ivy/array/losses.py
@@ -1,8 +1,22 @@
 # global
 import abc
+from typing import Optional, Union
+
+# local
+import ivy
 
 # ToDo: implement all methods here as public instance methods
 
 
 class ArrayWithLosses(abc.ABC):
+    def cross_entropy(
+        self: ivy.Array,
+        pred: Union[ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        *,
+        out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.cross_entropy(self, pred, axis=axis, epsilon=epsilon, out=out)
+
     pass
