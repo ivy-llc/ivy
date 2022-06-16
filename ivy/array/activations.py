bf539325df6db8fe6af77209038d5c09572abd45
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-06-16 01:13:00 +0100

    implement ivy.array for activations

--- ivy/array/activations.py
+++ ivy/array/activations.py
@@ -1,8 +1,46 @@
 # global
 import abc
+from typing import Optional, Union
 
+# local
+import ivy
 # ToDo: implement all methods here as public instance methods
 
-
 class ArrayWithActivations(abc.ABC):
-    pass
+    def relu(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.relu(self, out=out)
+
+    def leaky_relu(
+            self: ivy.Array,
+            alpha: Optional[float] = 0.2,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.leaky_relu(self,alpha, out=out)
+
+    def gelu(
+            self: ivy.Array,
+            approximate: Optional[bool] = True,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.gelu(self, approximate, out=out)
+
+    def tanh(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.tanh(self, out=out)
+
+    def sigmoid(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.sigmoid(self, out=out)
+
+    def softmax(self: ivy.Array,
+                axis: Optional[int] = None,
+                out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.softmax(self,axis, out=out)
