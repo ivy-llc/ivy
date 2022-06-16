a6708a54dbe225342d86311244618e70cb6f9f9f
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-06-16 10:28:50 +0100

    fixing some nightly tests
    Former-commit-id: b24143bc944aafddd152fe0ec4633c4ec3ff7a29

--- ivy_tests/test_ivy/test_functional/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_creation.py
@@ -19,11 +19,6 @@ import ivy.functional.backends.numpy as ivy_np
 )
 def test_array(dtype_and_x, from_numpy, device, call, fw):
     dtype, object_in = dtype_and_x
-    if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
-        return
-    if call in [helpers.mx_call] and dtype == "int16":
-        # mxnet does not support int16
-        return
     # to numpy
     if from_numpy:
         object_in = np.array(object_in)
@@ -48,11 +43,6 @@ def test_array(dtype_and_x, from_numpy, device, call, fw):
 )
 def test_native_array(dtype_and_x, from_numpy, device, call, fw):
     dtype, object_in = dtype_and_x
-    if fw == "torch" and dtype in ["uint16", "uint32", "uint64"]:
-        return
-    if call in [helpers.mx_call] and dtype == "int16":
-        # mxnet does not support int16
-        return
     # to numpy
     if from_numpy:
         object_in = np.array(object_in)
@@ -101,10 +91,8 @@ def test_linspace(start_n_stop_n_num_n_axis, dtype, tensor_fn, device, call):
     target_shape.insert(axis + 1 if (axis and axis != -1) else len(target_shape), num)
     assert ret.shape == tuple(target_shape)
     # value test
-    ivy.set_backend('numpy')
-    np_ret = ivy.linspace(
-        ivy.to_numpy(start), ivy.to_numpy(stop), num, axis
-    )
+    ivy.set_backend("numpy")
+    np_ret = ivy.linspace(ivy.to_numpy(start), ivy.to_numpy(stop), num, axis)
     ivy.unset_backend()
     assert np.allclose(
         call(ivy.linspace, start, stop, num, axis, device=device),
@@ -146,7 +134,7 @@ def test_logspace(start_n_stop_n_num_n_base_n_axis, dtype, tensor_fn, device, ca
     assert np.allclose(
         call(ivy.logspace, start, stop, num, base, axis, device=device),
         ivy.functional.backends.numpy.logspace(
-            ivy.to_numpy(start), ivy.to_numpy(stop), num, base, axis
+            ivy.to_numpy(start), ivy.to_numpy(stop), num, base, axis, device=device
         ),
     )
 
