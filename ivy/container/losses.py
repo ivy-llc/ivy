d4543a59b3b332d3e47e51d1cf7475976a6661d7
JG <67888047+juliagsy@users.noreply.github.com>
2022-06-18 22:10:12 +0800

    functional_losses fix up (#1389)
    Co-authored-by: juliagsy<67888047+juliagsy@users.noreply.github.com>
    Former-commit-id: 24713f72e8d89dcc3862807a5d05289ac22373ae

--- ivy/container/losses.py
+++ ivy/container/losses.py
@@ -1,9 +1,150 @@
+# global
+from typing import Optional, Union, List, Dict
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
-# ToDo: implement all methods here as public instance methods
 
-
-# noinspection PyMissingConstructor
 class ContainerWithLosses(ContainerBase):
-    pass
+    @staticmethod
+    def static_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "cross_entropy",
+            true,
+            pred,
+            axis=axis,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    def cross_entropy(
+        self: ivy.Container,
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        return self.static_cross_entropy(
+            self,
+            pred,
+            axis,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
+
+    @staticmethod
+    def static_binary_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "binary_cross_entropy",
+            true,
+            pred,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    def binary_cross_entropy(
+        self: ivy.Container,
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.static_binary_cross_entropy(
+            self,
+            pred,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
+
+    @staticmethod
+    def static_sparse_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "sparse_cross_entropy",
+            true,
+            pred,
+            axis=axis,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    def sparse_cross_entropy(
+        self: ivy.Container,
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return self.static_sparse_cross_entropy(
+            self,
+            pred,
+            axis,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
