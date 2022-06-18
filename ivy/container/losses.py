782f19c42865b70e20aed98ffc3ff30b5caed736
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-18 17:14:40 +0800

    add static method to container/losses.py

--- ivy/container/losses.py
+++ ivy/container/losses.py
@@ -7,6 +7,32 @@ from ivy.container.base import ContainerBase
 
 
 class ContainerWithLosses(ContainerBase):
+    @staticmethod
+    def static_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "cross_entropy",
+            true,
+            pred,
+            axis=axis,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
     def cross_entropy(
         self: ivy.Container,
         pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
@@ -19,25 +45,39 @@ class ContainerWithLosses(ContainerBase):
         *,
         out: Optional[ivy.Container] = None
     ) -> ivy.Container:
-        kw = {}
-        conts = {"true": self}
-        if ivy.is_array(pred):
-            kw["pred"] = pred
-        else:
-            conts["pred"] = pred
-        return ContainerBase.handle_inplace(
-            ContainerBase.multi_map(
-                lambda xs, _: ivy.cross_entropy(
-                    **dict(zip(conts.keys(), xs)), **kw, axis=axis, epsilon=epsilon
-                )
-                if ivy.is_array(xs[0])
-                else xs,
-                list(conts.values()),
-                key_chains,
-                to_apply,
-                prune_unapplied,
-            ),
-            out,
+        return self.static_cross_entropy(
+            self,
+            pred,
+            axis,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
+
+    @staticmethod
+    def static_binary_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "binary_cross_entropy",
+            true,
+            pred,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
         )
 
     def binary_cross_entropy(
@@ -50,25 +90,40 @@ class ContainerWithLosses(ContainerBase):
         map_sequences: bool = False,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        kw = {}
-        conts = {"true": self}
-        if ivy.is_array(pred):
-            kw["pred"] = pred
-        else:
-            conts["pred"] = pred
-        return ContainerBase.handle_inplace(
-            ContainerBase.multi_map(
-                lambda xs, _: ivy.binary_cross_entropy(
-                    **dict(zip(conts.keys(), xs)), **kw, epsilon=epsilon
-                )
-                if ivy.is_array(xs[0])
-                else xs,
-                list(conts.values()),
-                key_chains,
-                to_apply,
-                prune_unapplied,
-            ),
-            out,
+        return self.static_binary_cross_entropy(
+            self,
+            pred,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+        )
+
+    @staticmethod
+    def static_sparse_cross_entropy(
+        true: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pred: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        axis: Optional[int] = -1,
+        epsilon: Optional[float] = 1e-7,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "sparse_cross_entropy",
+            true,
+            pred,
+            axis=axis,
+            epsilon=epsilon,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
         )
 
     def sparse_cross_entropy(
@@ -82,25 +137,14 @@ class ContainerWithLosses(ContainerBase):
         map_sequences: bool = False,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        kw = {}
-        conts = {"true": self}
-        if ivy.is_array(pred):
-            kw["pred"] = pred
-        else:
-            conts["pred"] = pred
-        return ContainerBase.handle_inplace(
-            ContainerBase.multi_map(
-                lambda xs, _: ivy.sparse_cross_entropy(
-                    **dict(zip(conts.keys(), xs)), **kw, axis=axis, epsilon=epsilon
-                )
-                if ivy.is_array(xs[0])
-                else xs,
-                list(conts.values()),
-                key_chains,
-                to_apply,
-                prune_unapplied,
-            ),
-            out,
+        return self.static_sparse_cross_entropy(
+            self,
+            pred,
+            axis,
+            epsilon,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
         )
-
-    pass
