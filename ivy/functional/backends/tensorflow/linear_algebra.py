df67b7c5ef4f0f2656129d2e526917c663ea48b8
whatcoloris <brian.mcdorkle@gmail.com>
2022-06-19 18:37:11 -0400

    Reformat outer (#1154)
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -180,8 +180,11 @@ def svd(
 def outer(
     x1: Union[tf.Tensor, tf.Variable],
     x2: Union[tf.Tensor, tf.Variable],
+    out: Optional[tf.Tensor] = None
 ) -> Union[tf.Tensor, tf.Variable]:
-    ret = tf.experimental.numpy.outer(x1, x2)
+    ret = tf.experimental.numpy.outer(x1, x2, out=out)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
     return ret
 
 
