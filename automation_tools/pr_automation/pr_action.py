ae1c70da8460f3ec244a244bbe7e9a2e211b94f2
Aarsh2001 <ac5973@srmist.edu.in>
2022-06-20 14:02:28 +0530

    added nestable decorator to functions

--- automation_tools/pr_automation/pr_action.py
+++ automation_tools/pr_automation/pr_action.py
@@ -21,8 +21,8 @@ def main():
             sys.exit(0)
 
     # Get all PRs, count how many times each Ivy Team Member has been assigned
-    all_prs = pr.command(f'gh pr list --json assignees')
-    all_names = [i['assignees'][0]['login'] for i in all_prs[1:]]
+    all_prs = pr.command(f"gh pr list --json assignees")
+    all_names = [i["assignees"][0]["login"] for i in all_prs[1:]]
     unique_names = set(all_names)
     count = {}
     for i in unique_names:
