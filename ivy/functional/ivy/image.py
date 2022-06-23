8ec7931a77d68dffa93c0e55be75fbe2ee571a1e
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-06-24 00:01:14 +0530

    docstring fixes

--- ivy/functional/ivy/image.py
+++ ivy/functional/ivy/image.py
@@ -176,8 +176,7 @@ def uint8_img_to_float_img(
     >>> image = ivy.reshape(x,(batch_size, h, w, d))
     >>> y = ivy.uint8_img_to_float_img(image)
     >>> print(y)
-    ivy.array([[[3.820471434542632e-37, 1.0082513512365273e-34],
-                [2.658462758989161e-32, 7.003653270560797e-30]]])
+    ivy.array([[[3.82e-37,1.01e-34],[2.66e-32,7.00e-30]]])
 
     """
     x_np = ivy.to_numpy(x).astype("uint8")
