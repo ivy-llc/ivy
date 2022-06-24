b4aba77458598ff219e39277cf835cb91439b361
Jack Huang <jackhuang1205@gmail.com>
2022-06-24 12:25:20 +0800

    reformat bitwise_and (#1538)
    Co-authored-by: jackhong12
    Co-authored-by: chie2727 <72862115+chie2727@users.noreply.github.com>

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -709,6 +709,124 @@ def bitwise_and(
         an array containing the element-wise results. The returned array must have a
         data type determined by :ref:`type-promotion`.
 
+
+    This method conforms to the `Array API Standard
+    <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of the
+    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.elementwise_functions.bitwise_and.html>`_ # noqa
+    in the standard. The descriptions above assume an array input for simplicity, but
+    the method also accepts :code:`ivy.Container` instances in place of
+    :code:`ivy.Array` or :code:`ivy.NativeArray` instances, as shown in the type hints
+    and also the examples below.
+
+    Functional Examples
+    -------------------
+
+    With :code:`ivy.Array` inputs:
+
+    >>> x = ivy.array([2, 3, 7])
+    >>> y = ivy.array([7, 1, 15])
+    >>> z = ivy.bitwise_and(x, y)
+    >>> print(z)
+    ivy.array([2, 1, 7])
+
+    >>> x = ivy.array([[True], \
+                       [False]])
+    >>> y = ivy.array([[True], \
+                       [True]])
+    >>> ivy.bitwise_and(x, y, out=x)
+    >>> print(x)
+    ivy.array([[ True],
+               [False]])
+
+    >>> x = ivy.array([1])
+    >>> y = ivy.array([3])
+    >>> ivy.bitwise_and(x, y, out=y)
+    >>> print(y)
+    ivy.array([1])
+
+    With :code:`ivy.NativeArray` inputs:
+
+    >>> x = ivy.native_array([True, True, False, False])
+    >>> y = ivy.native_array([True, False, True, False])
+    >>> ivy.bitwise_and(x, y, out=y)
+    >>> print(y)
+    ivy.native_array([ True, False, False, False])
+
+    >>> x = ivy.native_array([[True, False]])
+    >>> y = ivy.native_array([[True], \
+                              [False]])
+    >>> z = ivy.bitwise_and(x, y)
+    >>> print(z)
+    ivy.array([[ True, False],
+               [False, False]])
+
+    With a mix of :code:`ivy.Array` and :code:`ivy.NativeArray` inputs:
+
+    >>> x = ivy.array([[6, 5], \
+                       [3, 7]])
+    >>> y = ivy.native_array([[2, 11], \
+                              [9, 13]])
+    >>> z = ivy.bitwise_and(x, y)
+    >>> print(z)
+    ivy.array([[2, 1],
+               [1, 5]])
+
+    With :code:`ivy.Container` input:
+
+    >>> x = ivy.Container(a=ivy.array([1, 2, 3]), b=ivy.array([4, 5, 6]))
+    >>> y = ivy.Container(a=ivy.array([7, 8, 9]), b=ivy.array([10, 11, 11]))
+    >>> z = ivy.bitwise_and(x, y)
+    >>> print(z)
+    {
+        a: ivy.array([1, 0, 1]),
+        b: ivy.array([0, 1, 2])
+    }
+
+    With a mix of :code:`ivy.Array` and :code:`ivy.Container` inputs: 
+
+    >>> x = ivy.array([True, True])
+    >>> y = ivy.Container(a=ivy.array([True, False]), b=ivy.array([False, True]))
+    >>> z = ivy.bitwise_and(x, y)
+    >>> print(z)
+    {
+        a: ivy.array([True, False]),
+        b: ivy.array([False, True])
+    }
+
+    Instance Method Examples
+    ------------------------
+
+    Using :code:`ivy.Array` instance method:
+
+    >>> x = ivy.array([True, False])
+    >>> y = ivy.array([True, True])
+    >>> x.bitwise_and(y, out=y)
+    >>> print(y)
+    ivy.array([ True, False])
+
+    >>> x = ivy.array([[7], \
+                       [8], \
+                       [9]])
+    >>> y = ivy.native_array([[10], \
+                              [11], \
+                              [12]])
+    >>> z = x.bitwise_and(y)
+    >>> print(z)
+    ivy.array([[2],
+               [8],
+               [8]])
+
+    Using :code:`ivy.Container` instance method:
+    
+    >>> x = ivy.Container(a=ivy.array([True, True]), b=ivy.array([False, True]))
+    >>> y = ivy.Container(a=ivy.array([False, True]), b=ivy.array([False, True]))
+    >>> x.bitwise_and(y, out=y)
+    >>> print(y)
+    {
+        a: ivy.array([False, True]),
+        b: ivy.array([False, True])
+    }
+
     """
     return current_backend(x1, x2).bitwise_and(x1, x2, out=out)
 
