8164756358f287d227545f90e0facb97e25c973b
chie2727 <72862115+chie2727@users.noreply.github.com>
2022-06-24 14:32:27 +0900

    Update random.py

--- ivy/functional/ivy/random.py
+++ ivy/functional/ivy/random.py
@@ -65,11 +65,11 @@ def random_uniform(
     >>> print(y)
     ivy.array(0.26431865)
 
-    >>> y = ivy.random_uniform(0, 2, device="cpu")
+    >>> y = ivy.random_uniform(0.0, 2.0, device="cpu")
     >>> print(y)
     ivy.array(1.89150229)
     
-    >>> y = ivy.random_uniform(0.7, 1, device="cpu", shape=(2, 2))
+    >>> y = ivy.random_uniform(0.7, 1.0, device="cpu", shape=(2, 2))
     >>> print(y)
     ivy.array([[0.89629126, 0.94198485],
                [0.91405606, 0.72848724]])
