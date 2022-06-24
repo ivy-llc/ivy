f6ac9824a066c58b43c39162f2155ede05e0fc3d
Avijit Biswas <71291040+avizyt@users.noreply.github.com>
2022-06-25 01:09:38 +0530

    Add swish activation function in numpy activation.py

--- ivy/functional/backends/numpy/activations.py
+++ ivy/functional/backends/numpy/activations.py
@@ -34,6 +34,10 @@ def sigmoid(x: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(-x))
 
 
+def swish(x: np.ndarray) -> np.ndarray:
+    return x * (1 / (1 + np.exp(-x)))
+
+
 def tanh(x: np.ndarray) -> np.ndarray:
     return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 
