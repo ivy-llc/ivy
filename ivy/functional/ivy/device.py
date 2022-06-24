a9f9db0012a7ca34cb754809715f40e4ef6c57e2
naailkhan28 <102599656+naailkhan28@users.noreply.github.com>
2022-06-24 05:20:11 +0100

    Reformatted unset_default_device #1498 (#1499)
    Co-authored-by: naailkhan28
    Co-authored-by: chie2727 <72862115+chie2727@users.noreply.github.com>
    Former-commit-id: 0e88dc52db4d91c6ad61c4643e1ca30446ce275c

--- ivy/functional/ivy/device.py
+++ ivy/functional/ivy/device.py
@@ -661,8 +661,19 @@ def set_default_device(device: Union[ivy.Device, ivy.NativeDevice]):
     default_device_stack.append(device)
 
 
-def unset_default_device():
-    """"""
+def unset_default_device() -> None:
+    """Resets the default device to "cpu".
+
+    Examples
+    --------
+    >>> ivy.set_default_device("gpu:0")
+    >>> ivy.default_device()
+    "gpu:0"
+    >>> ivy.unset_default_device()
+    >>> ivy.default_device()
+    "cpu"
+
+    """
     global default_device_stack
     if default_device_stack:
         default_device_stack.pop(-1)
