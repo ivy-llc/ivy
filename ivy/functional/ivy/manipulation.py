8d27c492543ede4e94c75b766bcc48c4b1721cd9
Daniel <daniel.lenton11@imperial.ac.uk>
2022-06-25 13:37:33 -0400

    small fix for roll docstring.

--- ivy/functional/ivy/manipulation.py
+++ ivy/functional/ivy/manipulation.py
@@ -449,18 +449,9 @@ def roll(
         an output array having the same data type as ``x`` and whose elements, relative
         to ``x``, are shifted.
 
-
     This function conforms to the `Array API Standard
     <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of the
-    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.manipulation_functions.roll.html>`_ # noqa
-    in the standard. The descriptions above assume an array input for simplicity, but
-    the method also accepts :code:`ivy.Container` instances in place of
-    :code:`ivy.Array` or :code:`ivy.NativeArray` instances, as shown in the type hints
-    and also the examples below.
-
-    This function conforms to the `Array API Standard
-    <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of the
-    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.elementwise_functions.add.html>`_ # noqa
+    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.elementwise_functions.roll.html>`_ # noqa
     in the standard.
 
     Both the description and the type hints above assumes an array input for simplicity,
