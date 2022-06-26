66d6fcc8353fd029634f244644d681f289b42357
Daniel <daniel.lenton11@imperial.ac.uk>
2022-06-26 19:16:37 -0400

    fixed test_iinfo.

--- ivy_tests/test_ivy/test_functional/test_core/test_dtype.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_dtype.py
@@ -175,6 +175,14 @@ def test_dtype_bits(
     assert num_bits == num_bits_np
 
 
+@st.composite
+def _array_or_type(draw):
+    return draw(st.sampled_from(
+        (draw(helpers.dtype_and_values(ivy_np.valid_int_dtypes, 1)),
+         draw(st.sampled_from(ivy_np.valid_int_dtypes)))
+    ))
+
+
 # finfo
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy.valid_float_dtypes, 2),
@@ -210,35 +218,39 @@ def test_finfo(
 
 # iinfo
 @given(
-    dtype_and_x=helpers.dtype_and_values(ivy.valid_int_dtypes, 2),
-    as_variable=helpers.list_of_length(st.booleans(), 2),
+    type=_array_or_type(),
     num_positional_args=helpers.num_positional_args(fn_name="iinfo"),
-    native_array=helpers.list_of_length(st.booleans(), 2),
-    container=helpers.list_of_length(st.booleans(), 2),
-    instance_method=st.booleans(),
 )
 def test_iinfo(
-    dtype_and_x,
-    as_variable,
+    type,
     num_positional_args,
-    native_array,
-    container,
-    instance_method,
     fw,
 ):
-    input_dtype, x = dtype_and_x
-    helpers.test_array_function(
+    if isinstance(type, str):
+        input_dtype = type
+    else:
+        input_dtype, x = type
+        type = np.array(x, dtype=input_dtype)
+    ret = helpers.test_array_function(
         input_dtype,
-        as_variable,
+        False,
         False,
         num_positional_args,
-        native_array,
-        container,
-        instance_method,
+        False,
+        False,
+        False,
         fw,
         "iinfo",
-        x=x,
+        type=type,
+        test_values=False
     )
+    if not ivy.exists(ret):
+        return
+    mach_lims, mach_lims_np = ret
+    assert mach_lims.min == mach_lims_np.min
+    assert mach_lims.max == mach_lims_np.max
+    assert mach_lims.dtype == mach_lims_np.dtype
+    assert mach_lims.bits == mach_lims_np.bits
 
 
 # is_float_dtype
