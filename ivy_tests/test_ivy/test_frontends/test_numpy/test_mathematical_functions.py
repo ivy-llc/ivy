98304ffd3c32df1cc1c9c5010aaa6e48def040ee
Daniel <daniel.lenton11@imperial.ac.uk>
2022-06-26 11:52:20 -0400

    added tan function to NumPy frontend.

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions.py
@@ -0,0 +1,44 @@
+# global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+
+
+# tan
+@given(
+    dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.tan"),
+    native_array=st.booleans(),
+)
+def test_numpy_tan(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtype,
+        as_variable,
+        with_out,
+        num_positional_args,
+        native_array,
+        fw,
+        "numpy",
+        "tan",
+        x=np.asarray(x, dtype=input_dtype),
+        out=None,
+        where=True,
+        casting='same_kind',
+        order='k',
+        dtype=None,
+        subok=True
+    )
