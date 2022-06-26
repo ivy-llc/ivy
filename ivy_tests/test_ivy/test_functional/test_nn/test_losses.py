5212d1382987b7a81a60733c58cbb4a642e7678c
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-26 23:30:20 +0800

    fix sparse_cross_entropy() tests

--- ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
@@ -1,6 +1,6 @@
 # global
 import numpy as np
-from hypothesis import given, strategies as st
+from hypothesis import assume, given, strategies as st
 
 # local
 import ivy.functional.backends.numpy as ivy_np
@@ -33,7 +33,7 @@ def test_cross_entropy(
 ):
     if fw == "torch" and pred_dtype == "float16":
         return
-    shape = data.draw(helpers.get_shape(min_num_dims=1, max_num_dims=1, min_dim_size=1))
+    shape = data.draw(helpers.get_shape(min_num_dims=1, max_num_dims=1, min_dim_size=2))
     true = data.draw(
         helpers.array_values(
             dtype=true_dtype,
@@ -50,6 +50,8 @@ def test_cross_entropy(
             shape=shape,
             min_value=0,
             max_value=1,
+            exclude_min=True,
+            exclude_max=True,
             allow_negative=False,
         )
     )
@@ -111,6 +113,8 @@ def test_binary_cross_entropy(
             shape=shape,
             min_value=0,
             max_value=1,
+            exclude_min=True,
+            exclude_max=True,
             allow_negative=False,
         )
     )
@@ -129,56 +133,66 @@ def test_binary_cross_entropy(
     )
 
 
-# # sparse_cross_entropy
-# @given(
-#     data=st.data(),
-#     true_dtype=st.sampled_from(ivy_np.valid_int_dtypes),
-#     pred_dtype=st.sampled_from(ivy_np.valid_float_dtypes),
-#     as_variable=helpers.list_of_length(st.booleans(), 2),
-#     with_out=st.booleans(),
-#     num_positional_args=helpers.num_positional_args(fn_name="sparse_cross
-# _entropy"),
-#     native_array=helpers.list_of_length(st.booleans(), 2),
-#     container=helpers.list_of_length(st.booleans(), 2),
-#     instance_method=st.booleans(),
-# )
-# def test_sparse_cross_entropy(
-#     data,
-#     true_dtype,
-#     pred_dtype,
-#     as_variable,
-#     with_out,
-#     num_positional_args,
-#     native_array,
-#     container,
-#     instance_method,
-#     fw,
-# ):
-#     shape = data.draw(helpers.get_shape(min_num_dims=1, max_num_dims=1, min
-# _dim_size=2))
-#     pred = data.draw(helpers.array_values(dtype=pred_dtype, shape=shape))
-#     true = data.draw(helpers.array_values(dtype=true_dtype, shape=(1,), min
-# _value=0, max_value=len(pred)-1, allow_negative=False))
-#     # if (v == [] for v in x):
-#     #     return
-#     if fw == "torch" and pred_dtype == "float16":
-#         return
-#     if fw == "tensorflow" and true_dtype not in ["uint8", "int32", "int64"]:
-#         return
-#     # if fw == "numpy":
-#     #     return
-#     # assume(true)
-#     # assume(pred)
-#     helpers.test_array_function(
-#         [true_dtype, pred_dtype],
-#         as_variable,
-#         with_out,
-#         num_positional_args,
-#         native_array,
-#         container,
-#         instance_method,
-#         fw,
-#         "sparse_cross_entropy",
-#         true=np.asarray(true, dtype=true_dtype),
-#         pred=np.asarray(pred, dtype=pred_dtype),
-#     )
+# sparse_cross_entropy
+@given(
+    data=st.data(),
+    true_dtype=st.sampled_from(ivy_np.valid_int_dtypes),
+    pred_dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=helpers.list_of_length(st.booleans(), 2),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(fn_name="sparse_cross_entropy"),
+    native_array=helpers.list_of_length(st.booleans(), 2),
+    container=helpers.list_of_length(st.booleans(), 2),
+    instance_method=st.booleans(),
+)
+def test_sparse_cross_entropy(
+    data,
+    true_dtype,
+    pred_dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    shape = data.draw(helpers.get_shape(min_num_dims=1, max_num_dims=1, min_dim_size=2))
+    pred = data.draw(
+        helpers.array_values(
+            dtype=pred_dtype,
+            shape=shape,
+            min_value=0,
+            max_value=1,
+            exclude_min=True,
+            exclude_max=True,
+            allow_negative=False,
+        )
+    )
+    true = data.draw(
+        helpers.array_values(
+            dtype=true_dtype,
+            shape=(1,),
+            min_value=0,
+            max_value=len(pred),
+            allow_negative=False,
+        )
+    )
+    if fw == "torch" and pred_dtype == "float16":
+        return
+    if fw == "tensorflow" and true_dtype not in ["uint8", "int32", "int64"]:
+        return
+    assume(all([v < len(pred) for v in true]))
+    helpers.test_array_function(
+        [true_dtype, pred_dtype],
+        as_variable,
+        False,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "sparse_cross_entropy",
+        true=np.asarray(true, dtype=true_dtype),
+        pred=np.asarray(pred, dtype=pred_dtype),
+    )
