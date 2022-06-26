e262e96d70c2b16be1b433fe9e3815cdd06b2a9a
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-26 23:00:53 +0800

    fix binary_cross_entropy() tests

--- ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_losses.py
@@ -69,46 +69,66 @@ def test_cross_entropy(
 
 
 # binary_cross_entropy
-# @given(
-#     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes, 2),
-#     as_variable=helpers.list_of_length(st.booleans(), 2),
-#     with_out=st.booleans(),
-#     num_positional_args=helpers.num_positional_args(fn_name="binary_cross
-# _entropy"),
-#     native_array=helpers.list_of_length(st.booleans(), 2),
-#     container=helpers.list_of_length(st.booleans(), 2),
-#     instance_method=st.booleans(),
-# )
-# def test_binary_cross_entropy(
-#     dtype_and_x,
-#     as_variable,
-#     with_out,
-#     num_positional_args,
-#     native_array,
-#     container,
-#     instance_method,
-#     fw,
-# ):
-#     input_dtype, x = dtype_and_x
-#     if (v == [] for v in x):
-#         return
-#     if fw == "torch" and input_dtype == "float16":
-#         return
-#     helpers.test_array_function(
-#         input_dtype,
-#         as_variable,
-#         with_out,
-#         num_positional_args,
-#         native_array,
-#         container,
-#         instance_method,
-#         fw,
-#         "binary_cross_entropy",
-#         true=np.asarray(x[0], dtype=input_dtype[0]),
-#         pred=np.asarray(x[1], dtype=input_dtype[1]),
-#     )
-#
-#
+@given(
+    data=st.data(),
+    true_dtype=st.sampled_from(ivy_np.valid_int_dtypes),
+    pred_dtype=st.sampled_from(ivy_np.valid_float_dtypes),
+    as_variable=helpers.list_of_length(st.booleans(), 2),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(fn_name="binary_cross_entropy"),
+    native_array=helpers.list_of_length(st.booleans(), 2),
+    container=helpers.list_of_length(st.booleans(), 2),
+    instance_method=st.booleans(),
+)
+def test_binary_cross_entropy(
+    data,
+    true_dtype,
+    pred_dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    if fw == "torch" and pred_dtype == "float16":
+        return
+    shape = data.draw(helpers.get_shape(min_num_dims=1, max_num_dims=1, min_dim_size=2))
+    true = data.draw(
+        helpers.array_values(
+            dtype=true_dtype,
+            shape=shape,
+            min_value=0,
+            max_value=1,
+            allow_negative=False,
+        )
+    )
+    shape = (len(true),)
+    pred = data.draw(
+        helpers.array_values(
+            dtype=pred_dtype,
+            shape=shape,
+            min_value=0,
+            max_value=1,
+            allow_negative=False,
+        )
+    )
+    helpers.test_array_function(
+        [true_dtype, pred_dtype],
+        as_variable,
+        False,
+        num_positional_args,
+        native_array,
+        container,
+        instance_method,
+        fw,
+        "binary_cross_entropy",
+        true=np.asarray(true, dtype=true_dtype),
+        pred=np.asarray(pred, dtype=pred_dtype),
+    )
+
+
 # # sparse_cross_entropy
 # @given(
 #     data=st.data(),
