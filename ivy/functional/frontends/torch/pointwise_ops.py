2624e99e7378fcd27cb14f47084d9a078add5c48
Daniel <daniel.lenton11@imperial.ac.uk>
2022-06-26 14:42:48 -0400

    added add function to PyTorch frontend, with corresponding unit test.

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -1,7 +1,16 @@
 # global
 import ivy
 
+
+def add(input, other, *, alpha=1, out=None):
+    return ivy.add(input, other*alpha, out=out)
+
+
+add.unsupported_dtypes = ('float16',)
+
+
 def tan(input, *, out=None):
     return ivy.tan(input, out=out)
+
 
 tan.unsupported_dtypes = ('float16',)
