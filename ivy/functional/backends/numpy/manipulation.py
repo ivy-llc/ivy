b0d7a7624dbf9182677a0c5e7581714092f46e52
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-26 23:56:08 +0800

    revert clip()

--- ivy/functional/backends/numpy/manipulation.py
+++ ivy/functional/backends/numpy/manipulation.py
@@ -163,7 +163,6 @@ def clip(
     x: np.ndarray,
     x_min: Union[Number, np.ndarray],
     x_max: Union[Number, np.ndarray],
-    out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
-    ret = np.asarray(np.clip(x, x_min, x_max, out=out))
+    ret = np.asarray(np.clip(x, x_min, x_max))
     return ret
