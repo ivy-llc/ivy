6595ea2ddca44588b362b2696ec033d508da4155
Lorenzo Breschi <58804597+rnwzd@users.noreply.github.com>
2022-06-26 11:50:07 +0200

    random_uniform (#1257)
    Co-authored-by: rnwzd
    Co-authored-by: chie2727 <72862115+chie2727@users.noreply.github.com>

--- ivy/functional/ivy/random.py
+++ ivy/functional/ivy/random.py
@@ -51,18 +51,46 @@ def random_uniform(
         If size is ``None`` (Default), a single value is returned.
     device
         device on which to create the array 'cuda:0', 'cuda:1', 'cpu' etc.
+        (Default value = None).
 
     Returns
     -------
     ret
         Drawn samples from the parameterized uniform distribution.
 
-    Examples
-    --------
-    >>> y = ivy.random_uniform(0.0, 2.0)
+    Functional Examples
+    -------------------
+    
+    >>> y = ivy.random_uniform()
+    >>> print(y)
+    ivy.array(0.26431865)
+
+    >>> y = ivy.random_uniform(shape=3)
+    >>> print(y)
+    ivy.array([0.475, 0.878, 0.861])
+
+    >>> y = ivy.random_uniform(0.0, 2.0, device="cpu")
     >>> print(y)
     ivy.array(1.89150229)
+    
+    >>> y = ivy.random_uniform(0.7, 1.0, device="cpu", shape=(2, 2))
+    >>> print(y)
+    ivy.array([[0.89629126, 0.94198485],
+               [0.91405606, 0.72848724]])
 
+    Instance Method Examples
+    ------------------------
+    
+    With :code:`ivy.Container` input:
+    
+    >>> y = ivy.Container(a=ivy.random_uniform(), \
+                          b=ivy.random_uniform(shape=2))
+    >>> print(y)
+    {
+    a: ivy.array(0.7550739),
+    b: ivy.array([0.624, 0.00109])
+    }
+    
     """
     return current_backend().random_uniform(
         low, high, shape, device=device, dtype=dtype
