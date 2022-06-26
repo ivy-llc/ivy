902346161c99ac8f42d49b01e940f749ede7c2ed
Daniel <daniel.lenton11@imperial.ac.uk>
2022-06-26 14:58:04 -0400

    added add function to NumPy frontend, with corresponding unit test.

--- ivy/functional/frontends/numpy/mathematical_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions.py
@@ -2,6 +2,20 @@
 import ivy
 
 
+def add(x1, x2, /, out=None, *, where=True, casting='same_kind', order='k', dtype=None,
+        subok=True):
+    if dtype:
+        x1 = ivy.astype(x1, ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(x2, ivy.as_ivy_dtype(dtype))
+    ret = ivy.add(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+add.unsupported_dtypes = {"torch": ("float16",)}
+
+
 def tan(x, /, out=None, *, where=True, casting='same_kind', order='k', dtype=None,
         subok=True):
     if dtype:
@@ -10,5 +24,6 @@ def tan(x, /, out=None, *, where=True, casting='same_kind', order='k', dtype=Non
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
 
 tan.unsupported_dtypes = {"torch": ("float16",)}
