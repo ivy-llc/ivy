3d93481c9391f2520e2b4f348dd942f984f88bec
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-06-26 19:05:24 +0430

    1. add tanh test function to test_pointwise_ops.py module - tanh branch

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -1,10 +1,13 @@
 # global
 import ivy
 
+
 def tan(input, *, out=None):
     return ivy.tan(input, out=out)
 
+
 tan.unsupported_dtypes = ('float16',)
+
 
 def tanh(input, *, out=None):
     return ivy.tanh(input, out=out)
