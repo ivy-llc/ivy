e9faa6b49a680ce13aed308804de313240e45ee0
Soran Ghaderi <soran.gdr.cs@gmail.com>
2022-06-28 03:05:31 +0430

    1. Add annotations to the tanh() function located in torch frontend - pointwise_ops.py

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -1,10 +1,15 @@
 # global
+import torch
+
 import ivy
+
 
 def tan(input, *, out=None):
     return ivy.tan(input, out=out)
 
+
 tan.unsupported_dtypes = ('float16',)
 
-def tanh(input, *, out=None):
+
+def tanh(input: torch.Tensor, *, out: torch.Tensor = None) -> torch.Tensor:
     return ivy.tanh(input, out=out)
