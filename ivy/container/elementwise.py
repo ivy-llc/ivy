bbb7db151e5d958c3a4a9482c840b288d2e3b72f
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-27 16:20:34 +0800

    add negative() container static method

--- ivy/container/elementwise.py
+++ ivy/container/elementwise.py
@@ -289,7 +289,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -320,7 +320,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -368,7 +368,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -399,7 +399,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -428,7 +428,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -550,7 +550,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -636,7 +636,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -665,7 +665,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -694,7 +694,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -780,7 +780,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -809,7 +809,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -914,7 +914,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -943,7 +943,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -991,7 +991,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -1020,7 +1020,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -1061,6 +1061,26 @@ class ContainerWithElementwise(ContainerBase):
             self, x2, key_chains, to_apply, prune_unapplied, map_sequences, out=out
         )
 
+    @staticmethod
+    def static_negative(
+        x: Union[float, ivy.Container, ivy.Array, ivy.NativeArray],
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "negative",
+            x,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
     def negative(
         self: ivy.Container,
         key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
@@ -1069,15 +1089,13 @@ class ContainerWithElementwise(ContainerBase):
         map_sequences: bool = False,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        return self.handle_inplace(
-            self.map(
-                lambda x_, _: ivy.negative(x_) if ivy.is_array(x_) else x_,
-                key_chains,
-                to_apply,
-                prune_unapplied,
-                map_sequences,
-            ),
-            out,
+        return self.static_negative(
+            self,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
         )
 
     def not_equal(
@@ -1104,7 +1122,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -1152,7 +1170,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
@@ -1181,7 +1199,7 @@ class ContainerWithElementwise(ContainerBase):
                 key_chains,
                 to_apply,
                 prune_unapplied,
-                map_nests=map_nests
+                map_nests=map_nests,
             ),
             out,
         )
