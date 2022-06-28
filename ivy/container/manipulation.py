3473c6be18cd36cb4fa001ae87fec620490846f8
zaeemansari70 <99063526+zaeemansari70@users.noreply.github.com>
2022-06-28 17:49:56 +0500

    Update manipulation.py

--- ivy/container/manipulation.py
+++ ivy/container/manipulation.py
@@ -411,7 +411,6 @@ class ContainerWithManipulation(ContainerBase):
 
         Examples
         --------
-
         With one :code:`ivy.Container` input:
 
         >>> x = ivy.Container(a=ivy.array([0., 1., 2.]), \
@@ -466,7 +465,6 @@ class ContainerWithManipulation(ContainerBase):
 
         Examples
         --------
-
         >>> x = ivy.Container(a=ivy.array([0., 1., 2.]), b=ivy.array([3., 4., 5.]))
         >>> y = x.clip(1,2)
         >>> print(y)
