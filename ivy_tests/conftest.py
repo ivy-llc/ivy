f4d8993e5622ebe7d2101ef4f016c7dbd0282b38
Simone G <74716948+simonetgordon@users.noreply.github.com>
2022-06-29 21:02:16 +0100

    disable hypothesis health check filter 2 (filter too much) on CI

--- ivy_tests/conftest.py
+++ ivy_tests/conftest.py
@@ -1,4 +1,4 @@
 from hypothesis import settings, HealthCheck
 
-settings.register_profile("ci", suppress_health_check=(HealthCheck(3),))
+settings.register_profile("ci", suppress_health_check=(HealthCheck(3), HealthCheck(2)))
 settings.load_profile("ci")
