87c48699cce361aabb78bdfa128f0ae713b760e8
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-06-30 23:09:33 +0530

    changes
    Former-commit-id: 01fa45f1201b427616ec44111de52de02ef3604b

--- ivy_tests/test_docstrings.py
+++ ivy_tests/test_docstrings.py
@@ -49,41 +49,37 @@ def test_docstrings(backend):
     to_skip+=skip_list_temp
 
 
-    # for k, v in ivy.__dict__.copy().items():
-    #     if k == "Array":
-    #         for method_name in dir(v):
-    #             method = getattr(ivy.Array, method_name)
-    #             if helpers.docstring_examples_run(method, from_array=True):
-    #                 continue
-    #             success = False
-    #             failures.append("Array." + method_name)
-    #
-    #     elif k == "Container":
-    #         for method_name in dir(v):
-    #             method = getattr(ivy.Container, method_name)
-    #             if helpers.docstring_examples_run(method, from_container=True):
-    #                 continue
-    #             success = False
-    #             failures.append("Container." + method_name)
-    #
-    #     else:
-    #         if k in to_skip or helpers.docstring_examples_run(v):
-    #             continue
-    #         success = False
-    #         failures.append(k)
-    #
-    # if not success:
-    #     ivy.warn(
-    #         "the following methods had failing docstrings:\n\n{}".format(
-    #             "\n".join(failures)
-    #         )
-    #     )
-    x = ivy.Container(a=ivy.array([1, 2, 3]), \
-                      b=ivy.array([2, 3, 4]))
-    y = ivy.Container(a=ivy.array([4, 5, 6]), \
-                           b=ivy.array([5, 6, 7]))
-    z = ivy.add(x, y)
-    print(type(z))
+    for k, v in ivy.__dict__.copy().items():
+        if k == "Array":
+            for method_name in dir(v):
+                method = getattr(ivy.Array, method_name)
+                if helpers.docstring_examples_run(method, from_array=True):
+                    continue
+                success = False
+                failures.append("Array." + method_name)
+
+        elif k == "Container":
+            for method_name in dir(v):
+                method = getattr(ivy.Container, method_name)
+                if helpers.docstring_examples_run(method, from_container=True):
+                    continue
+                success = False
+                failures.append("Container." + method_name)
+
+        else:
+            if k in to_skip or helpers.docstring_examples_run(v):
+                continue
+            success = False
+            failures.append(k)
+
+    if not success:
+        ivy.warn(
+            "the following methods had failing docstrings:\n\n{}".format(
+                "\n".join(failures)
+            )
+        )
+
+
 
 
     ivy.unset_backend()
