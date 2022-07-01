de9a6cfa0515179ae5ae72291baf9b2f6c022135
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-07-01 11:11:16 +0530

    summing up the changes
    Former-commit-id: b2b73d69df79e04d8886f8a0cbe622e8cdc2bebe

--- ivy_tests/test_ivy/helpers.py
+++ ivy_tests/test_ivy/helpers.py
@@ -303,8 +303,7 @@ def docstring_examples_run(fn, from_container=False, from_array=False):
             try:
                 exec(line)
             except Exception as e:
-
-                print(e,"  ",ivy.current_backend_str(), " ", line)
+                raise e
 
     output = f.getvalue()
     output = output.rstrip()
@@ -337,7 +336,7 @@ def docstring_examples_run(fn, from_container=False, from_array=False):
         print(
             "output for ",
             fn_name,
-            "on run: ",
+            " on run: ",
             output,
             "\noutput in docs :",
             parsed_output,
