d5af31ac3207a0bfceefe507816757ce13a0553b
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-01 15:16:00 +0800

    fix tf

--- ivy/functional/backends/tensorflow/utility.py
+++ ivy/functional/backends/tensorflow/utility.py
@@ -6,26 +6,24 @@ from typing import Union, Tuple, Optional, List
 def all(
     x: Union[tf.Tensor, tf.Variable],
     axis: Optional[Union[int, Tuple[int], List[int]]] = None,
-    keepdims: bool = False,
+    keepdims: Optional[bool] = False,
 ) -> Union[tf.Tensor, tf.Variable]:
     if axis is None:
         num_dims = len(x.shape)
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = tf.reduce_all(tf.cast(x, tf.bool), axis=axis, keepdims=keepdims)
-    return ret
+    return tf.reduce_all(tf.cast(x, tf.bool), axis=axis, keepdims=keepdims)
 
 
 def any(
     x: Union[tf.Tensor, tf.Variable],
     axis: Optional[Union[int, Tuple[int], List[int]]] = None,
-    keepdims: bool = False,
+    keepdims: Optional[bool] = False,
 ) -> Union[tf.Tensor, tf.Variable]:
     if axis is None:
         num_dims = len(x.shape)
         axis = tuple(range(num_dims))
     elif isinstance(axis, list):
         axis = tuple(axis)
-    ret = tf.reduce_any(tf.cast(x, tf.bool), axis=axis, keepdims=keepdims)
-    return ret
+    return tf.reduce_any(tf.cast(x, tf.bool), axis=axis, keepdims=keepdims)
