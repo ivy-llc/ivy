090e5667e499d4eff95efa21cd5d152ee8af5d0e
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-07-01 11:59:58 +0530

    fixed square
    Former-commit-id: aea76a8f997cd3c01d3b52eff4deeb78436729f8

--- ivy_tests/test_ivy/helpers.py
+++ ivy_tests/test_ivy/helpers.py
@@ -303,6 +303,7 @@ def docstring_examples_run(fn, from_container=False, from_array=False):
             try:
                 exec(line)
             except Exception as e:
+                #print(e, " ", ivy.current_backend_str()," ",line)
                 raise e
 
     output = f.getvalue()
