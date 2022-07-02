8d246af44b4d29e18c221eef1cd53510099a8000
Rushali Grandhe <rushaligrandhe@gmail.com>
2022-07-02 07:45:15 +0400

    Fix submodule sorting (#1710)
    * added array and container implementations
    
    * fixed unit tests

--- ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
@@ -90,10 +90,6 @@ def test_sort(
     instance_method,
     fw,
 ):
-    # smoke for torch
-    if fw == "torch" and input_dtype in ["uint16", "uint32", "uint64"]:
-        return
-
     # we do not want any nans
     x = data.draw(
         helpers.nph.arrays(shape=array_shape, dtype=input_dtype).filter(
