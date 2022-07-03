7a67516ed573fb5d09fdaa564d61b583fa5fd167
Garamon <ollietoh@gmail.com>
2022-07-03 01:32:30 -0400

    added softplus instance and static methods to array and container

--- ivy/container/activations.py
+++ ivy/container/activations.py
@@ -1,4 +1,8 @@
+# global
+from typing import Optional, Union, List, Dict
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
@@ -6,4 +10,29 @@ from ivy.container.base import ContainerBase
 
 # noinspection PyMissingConstructor
 class ContainerWithActivations(ContainerBase):
-    pass
+    def softplus(
+        self: ivy.Container,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.softplus. This method simply wraps the
+        function, and so the docstring for ivy.softplus also applies to this method
+        with minimal changes.
+        """
+        return ContainerWithActivations.static_softplus(self, out=out)
+    
+    @staticmethod
+    def static_softplus(
+        x: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.softplus. This method simply wraps the
+        function, and so the docstring for ivy.softplus also applies to this method
+        with minimal changes.
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "softplus",
+            x,
+            out=out
+        )
