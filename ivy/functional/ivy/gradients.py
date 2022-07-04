6931d7ef1efefa544e8754ee95d371069d0f4049
hirwa <hirwanshutiflx@gmail.com>
2022-07-04 16:19:12 +0530

    Updated example for Container inputs

--- ivy/functional/ivy/gradients.py
+++ ivy/functional/ivy/gradients.py
@@ -337,10 +337,9 @@ def adam_step(
     >>> epsilon = 1e-5
     >>> adam_step_delta = ivy.adam_step(dcdw, mw, vw, step, beta1, beta2, epsilon)
     >>> print(adam_step_delta)
-    {
-        a: (list[3], <class ivy.array.Array> shape=[3]),
-        b: (list[3], <class ivy.array.Array> shape=[3])
-    }
+            ({a:ivy.array([0.,0.626,0.626]),b:ivy.array([0.626,0.626,0.626])},
+            {a:ivy.array([0.,0.13,0.26]),b:ivy.array([0.39,0.52,0.65])},
+            {a:ivy.array([0.,0.024,0.096]),b:ivy.array([0.216,0.384,0.6])})
     """
     step = float(ivy.to_scalar(step))
     mw = beta1 * mw + (1 - beta1) * dcdw
