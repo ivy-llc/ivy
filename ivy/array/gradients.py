326f73b0ff76b7c2e428931ab708103fafac57b1
hirwa <hirwanshutiflx@gmail.com>
2022-07-04 19:14:38 +0530

    Added update for array instance of adam_step

--- ivy/array/gradients.py
+++ ivy/array/gradients.py
@@ -10,8 +10,8 @@ import ivy
 
 class ArrayWithGradients(abc.ABC):
     def adam_step(self: ivy.Array,
-                  mw: Union[ivy.Array, ivy.NativeArray],
-                  vw: Union[ivy.Array, ivy.NativeArray],
+                  mw: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+                  vw: Union[ivy.container, ivy.Array, ivy.NativeArray],
                   step: Union[int, float],
                   beta1=0.9,
                   beta2=0.999,
@@ -25,14 +25,14 @@ class ArrayWithGradients(abc.ABC):
         Examples
         --------
         With :code:`ivy.Array` inputs:
-        >>> dcdw = ivy.array([1, 2, 3])
-        >>> mw = ivy.zeros(3)
-        >>> vw = ivy.zeros(1)
+        >>> dcdw = ivy.array([[[1.1], [3.2], [-6.3]]])
+        >>> mw = ivy.array([[0.], [0.], [0.]])
+        >>> vw = ivy.array([[0.], [0.], [0.]])
         >>> step = ivy.array(3)
         >>> adam_step_delta = dcdw.adam_step(mw, vw, step)
         >>> print(adam_step_delta)
-            (ivy.array([0.639, 0.639, 0.639]),
-            ivy.array([0.1, 0.2, 0.3]),
-            ivy.array([0.001, 0.004, 0.009]))
+            (ivy.array([[[ 0.639], [ 0.639], [-0.639]]]),
+            ivy.array([[[ 0.11], [ 0.32], [-0.63]]]),
+            ivy.array([[[0.00121], [0.0102 ], [0.0397 ]]]))
         """
         return ivy.adam_step(self._data, mw, vw, step, beta1, beta2, epsilon)
