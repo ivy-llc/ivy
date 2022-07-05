4098cdbba87db4133db032965c1aac7330c093e2
Simone G <74716948+simonetgordon@users.noreply.github.com>
2022-07-05 13:42:32 +0100

    working array api skips

--- ivy_tests/conftest.py
+++ ivy_tests/conftest.py
@@ -1,35 +1,25 @@
 from hypothesis import settings, HealthCheck
 from pytest import mark
-from os import path
+from pathlib import Path
 
-# flake8: noqa
 settings.register_profile("ci", suppress_health_check=(HealthCheck(3),))
 settings.load_profile("ci")
 
-skip_ids = [
-    "ivy/ivy_tests/test_array_api/array_api_tests/test_array_object.py::test_getitem_masking",
-    "ivy/ivy_tests/test_array_api/array_api_tests/test_array_object.py::test_setitem",
-]
-
-# skips_path = r"C:\ivy_tests\skips.txt"
-if path.exists("skips.txt"):
-    with open("skips.txt") as f:
+skip_ids = []
+skips_path = Path(__file__).parent / "skips.txt"
+if skips_path.exists():
+    with open(skips_path) as f:
         for line in f:
-            print("skip:", line)
-            # if line.startswith("test_array_api"):
-            id_ = line.strip("\n")
-            skip_ids.append(id_)
+            if line.startswith("ivy_tests"):
+                id_ = line.strip("\n")
+                skip_ids.append(id_)
 
 
 def pytest_collection_modifyitems(items):
+    skip_ivy = mark.skip(reason="ivy skip - see ivy_tests/skips.txt for details")
     for item in items:
         # skip if specified in skips.txt
         for id_ in skip_ids:
             if item.nodeid.startswith(id_):
-                item.add_marker(
-                    mark.skip(
-                        reason="failed health check-too much data \
-                        filtered in hypothesis test"
-                    )
-                )
+                item.add_marker(skip_ivy)
                 break
