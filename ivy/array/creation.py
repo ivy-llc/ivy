8bfdf09781088dc428c417c5210330c2726cc34a
James Keane <81018496+jkeane508@users.noreply.github.com>
2022-07-05 15:13:06 +0000

    Formatting

--- ivy/array/creation.py
+++ ivy/array/creation.py
@@ -1,7 +1,6 @@
 # global
 import abc
-from typing import Optional, Union, Tuple, List
-from numbers import Number
+from typing import Optional, Union, List
 
 # local
 import ivy
@@ -31,7 +30,7 @@ class ArrayWithCreation(abc.ABC):
         return ivy.full_like(
             self._data, fill_value, dtype=dtype, device=device, out=out
         )
-    
+
     def ones_like(
         self: ivy.Array,
         *,
@@ -50,20 +49,12 @@ class ArrayWithCreation(abc.ABC):
     ) -> ivy.Array:
         return ivy.zeros_like(self._data, dtype=dtype, device=device, out=out)
 
-    def tril(
-        self: ivy.Array,
-        k: int = 0,
-        out: Optional[ivy.Array] = None
-    ) -> ivy.Array:
+    def tril(self: ivy.Array, k: int = 0, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.tril(self._data, k, out=out)
-    
-    def triu(
-        self: ivy.Array,
-        k: int = 0,
-        out: Optional[ivy.Array] = None
-    ) -> ivy.Array:
+
+    def triu(self: ivy.Array, k: int = 0, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.triu(self._data, k, out=out)
-    
+
     def empty_like(
         self: ivy.Array,
         *,
@@ -74,19 +65,18 @@ class ArrayWithCreation(abc.ABC):
         return ivy.empty_like(self._data, dtype=dtype, device=device, out=out)
 
     def meshgrid(
-        *arrays: Union[ivy.Array, ivy.NativeArray],
-        indexing: Optional[str] = "xy"
+        *arrays: Union[ivy.Array, ivy.NativeArray], indexing: Optional[str] = "xy"
     ) -> List[ivy.Array]:
         return ivy.meshgrid(*arrays, indexing=indexing)
-    
+
     def from_dlpack(
         self: ivy.Array,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.from_dlpack(self._data, out=out)
 
-# Extra #
-# ------#
+    # Extra #
+    # ------#
 
     def native_array(
         self: ivy.Array,
@@ -95,4 +85,3 @@ class ArrayWithCreation(abc.ABC):
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
     ) -> ivy.NativeArray:
         return ivy.to_native(ivy.asarray(self._data, dtype=dtype, device=device))
-
