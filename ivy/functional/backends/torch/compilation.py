086ca2caabd10de1b6b42213d1cb9f2e333b02c0
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-05 22:00:42 +0800

    add `native_array` conversion to `torch`

--- ivy/functional/backends/torch/compilation.py
+++ ivy/functional/backends/torch/compilation.py
@@ -3,10 +3,15 @@
 # global
 import torch
 
+# local
+import ivy
+
 
 def compile(
     fn, dynamic=True, example_inputs=None, static_argnums=None, static_argnames=None
 ):
     if dynamic:
         return torch.jit.script(fn)
+    if example_inputs is not None:
+        example_inputs = ivy.to_native(example_inputs, nested=True)
     return torch.jit.trace(fn, example_inputs)
