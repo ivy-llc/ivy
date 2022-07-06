a27d6d63eaa03430bb37dd213db8fe55fd868668
Darshan H E <53147940+Darshan-H-E@users.noreply.github.com>
2022-07-06 20:50:32 +0530

    Fixing out in instance methods (#1784)
    * Minor fixes to type signature in array instance methods
    
    * Minor fixes to out arg in container instance methods

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -10,7 +10,7 @@ import ivy
 
 # noinspection PyUnresolvedReferences
 class ArrayWithElementwise(abc.ABC):
-    def abs(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def abs(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         """
         ivy.Array instance method variant of ivy.abs. This method simply wraps the
         function, and so the docstring for ivy.abs also applies to this method
@@ -18,10 +18,10 @@ class ArrayWithElementwise(abc.ABC):
         """
         return ivy.abs(self._data, out=out)
 
-    def acosh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def acosh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.acosh(self._data, out=out)
 
-    def acos(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def acos(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.acos(self._data, out=out)
 
     def add(
@@ -45,28 +45,30 @@ class ArrayWithElementwise(abc.ABC):
         """
         return ivy.add(self._data, x2, out=out)
 
-    def asin(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def asin(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.asin(self._data, out=out)
 
-    def asinh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def asinh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.asinh(self._data, out=out)
 
-    def atan(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def atan(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.atan(self._data, out=out)
 
     def atan2(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.atan2(self._data, x2, out=out)
 
-    def atanh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def atanh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.atanh(self._data, out=out)
 
     def bitwise_and(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.bitwise_and(self._data, x2, out=out)
@@ -74,16 +76,20 @@ class ArrayWithElementwise(abc.ABC):
     def bitwise_left_shift(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.bitwise_left_shift(self._data, x2, out=out)
 
-    def bitwise_invert(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def bitwise_invert(
+        self: ivy.Array, *, out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
         return ivy.bitwise_invert(self._data, out=out)
 
     def bitwise_or(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.bitwise_or(self._data, x2, out=out)
@@ -91,6 +97,7 @@ class ArrayWithElementwise(abc.ABC):
     def bitwise_right_shift(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.bitwise_right_shift(self._data, x2, out=out)
@@ -98,11 +105,12 @@ class ArrayWithElementwise(abc.ABC):
     def bitwise_xor(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.bitwise_xor(self._data, x2, out=out)
 
-    def ceil(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def ceil(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         """
         ivy.Array instance method variant of ivy.ceil. This method simply wraps the
         function, and so the docstring for ivy.ceil also applies to this method
@@ -117,15 +125,16 @@ class ArrayWithElementwise(abc.ABC):
         """
         return ivy.ceil(self._data, out=out)
 
-    def cos(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def cos(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.cos(self._data, out=out)
 
-    def cosh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def cosh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.cosh(self._data, out=out)
 
     def divide(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.divide(self._data, x2, out=out)
@@ -133,17 +142,18 @@ class ArrayWithElementwise(abc.ABC):
     def equal(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.equal(self._data, x2, out=out)
 
-    def exp(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def exp(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.exp(self._data, out=out)
 
-    def expm1(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def expm1(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.expm1(self._data, out=out)
 
-    def floor(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def floor(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         """
         ivy.Array instance method variant of ivy.floor. This method simply wraps the
         function, and so the docstring for ivy.floor also applies to this method
@@ -161,6 +171,7 @@ class ArrayWithElementwise(abc.ABC):
     def floor_divide(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.floor_divide(self._data, x2, out=out)
@@ -168,6 +179,7 @@ class ArrayWithElementwise(abc.ABC):
     def greater(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.greater(self._data, x2, out=out)
@@ -175,22 +187,24 @@ class ArrayWithElementwise(abc.ABC):
     def greater_equal(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.greater_equal(self._data, x2, out=out)
 
-    def isfinite(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def isfinite(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.isfinite(self._data, out=out)
 
-    def isinf(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def isinf(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.isinf(self._data, out=out)
 
-    def isnan(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def isnan(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.isnan(self._data, out=out)
 
     def less(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.less(self._data, x2, out=out)
@@ -198,25 +212,27 @@ class ArrayWithElementwise(abc.ABC):
     def less_equal(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.less_equal(self._data, x2, out=out)
 
-    def log(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def log(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.log(self._data, out=out)
 
-    def log1p(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def log1p(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.log1p(self._data, out=out)
 
-    def log2(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def log2(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.log2(self._data, out=out)
 
-    def log10(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def log10(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.log10(self._data, out=out)
 
     def logaddexp(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.logaddexp(self._data, x2, out=out)
@@ -224,16 +240,18 @@ class ArrayWithElementwise(abc.ABC):
     def logical_and(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.logical_and(self._data, x2, out=out)
 
-    def logical_not(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def logical_not(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.logical_not(self._data, out=out)
 
     def logical_or(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.logical_or(self._data, x2, out=out)
@@ -241,6 +259,7 @@ class ArrayWithElementwise(abc.ABC):
     def logical_xor(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.logical_xor(self._data, x2, out=out)
@@ -248,26 +267,29 @@ class ArrayWithElementwise(abc.ABC):
     def multiply(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.multiply(self._data, x2, out=out)
 
-    def negative(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def negative(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.negative(self._data, out=out)
 
     def not_equal(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.not_equal(self._data, x2, out=out)
 
-    def positive(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def positive(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.positive(self._data, out=out)
 
     def pow(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.pow(self._data, x2, out=out)
@@ -275,31 +297,33 @@ class ArrayWithElementwise(abc.ABC):
     def remainder(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.remainder(self._data, x2, out=out)
 
-    def round(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def round(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.round(self._data, out=out)
 
-    def sign(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def sign(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.sign(self._data, out=out)
 
-    def sin(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def sin(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.sin(self._data, out=out)
 
-    def sinh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def sinh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.sinh(self._data, out=out)
 
-    def square(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def square(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.square(self._data, out=out)
 
-    def sqrt(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def sqrt(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.sqrt(self._data, out=out)
 
     def subtract(
         self: ivy.Array,
         x2: Union[ivy.Array, ivy.NativeArray],
+        *,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         return ivy.subtract(self._data, x2, out=out)
@@ -334,8 +358,8 @@ class ArrayWithElementwise(abc.ABC):
         """
         return ivy.tanh(self._data, out=out)
 
-    def trunc(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def trunc(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.trunc(self._data, out=out)
 
-    def erf(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
+    def erf(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         return ivy.erf(self._data, out=out)
