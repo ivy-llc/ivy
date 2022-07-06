0b78a0272bac93cd14afa8619c290d8da623b913
Cooper Sheets <coopersheets@gmail.com>
2022-07-05 20:09:17 -0500

    Updated docs and added instance implementations

--- ivy/array/set.py
+++ ivy/array/set.py
@@ -1,8 +1,31 @@
 # global
 import abc
+from typing import Optional, Tuple
+
+# local
+import ivy
 
 # ToDo: implement all methods here as public instance methods
 
 
 class ArrayWithSet(abc.ABC):
-    pass
+    def unique_counts(
+        self: ivy.Array,
+        *,
+        out: Optional[Tuple[ivy.Array, ivy.Array]] = None
+    ) -> Tuple[ivy.Array, ivy.Array]:
+        """
+        ivy.Array instance method variant of ivy.unique_counts. This method simply wraps the
+        function, and so the docstring for ivy.unique_counts also applies to this method
+        with minimal changes.
+
+        Examples
+        --------
+        With :code:`ivy.Array` instance method:
+
+        >>> x = ivy.array([0., 1., 3. , 2. , 1. , 0.])
+        >>> y = x.unique_counts()
+        >>> print(y)
+        Tuple([0., 1., 2., 3.],[2,2,1,1])
+        """
+        return ivy.unique_counts(self._data, out=out)
--- ivy/container/set.py
+++ ivy/container/set.py
@@ -1,4 +1,8 @@
+# global
+from typing import Dict, List, Optional, Union
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
 
 # ToDo: implement all methods here as public instance methods
@@ -6,4 +10,69 @@ from ivy.container.base import ContainerBase
 
 # noinspection PyMissingConstructor
 class ContainerWithSet(ContainerBase):
-    pass
+    @staticmethod
+    def static_unique_counts(
+        x: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.unique_counts. This method simply wraps the
+        function, and so the docstring for ivy.unique_counts also applies to this method
+        with minimal changes.
+
+        Examples
+        --------
+        With :code:`ivy.Container` input:
+
+        >>> x = ivy.Container(a=ivy.array([0., 1., 3. , 2. , 1. , 0.]), b=ivy.array([1,2,1,3,4,1,3]))
+        >>> y = x.static_unique_counts()
+        >>> print(y)
+        {
+            a: Tuple([0., 1., 2., 3.],[2,2,1,1]),
+            b: Tuple([1,2,3,4],[3,1,2,1])
+        }
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "unique_counts",
+            x,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    def unique_counts(
+        self: ivy.Container,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.unique_counts. This method simply wraps the
+        function, and so the docstring for ivy.unique_counts also applies to this method
+        with minimal changes.
+
+        Examples
+        --------
+        With :code:`ivy.Container` input:
+
+        >>> x = ivy.Container(a=ivy.array([0., 1., 3. , 2. , 1. , 0.]), b=ivy.array([1,2,1,3,4,1,3]))
+        >>> y = x.unique_counts()
+        >>> print(y)
+        {
+            a: Tuple([0., 1., 2., 3.],[2,2,1,1]),
+            b: Tuple([1,2,3,4],[3,1,2,1])
+        }
+        """
+        return self.static_unique_counts(
+            self, key_chains, to_apply, prune_unapplied, map_sequences, out=out
+        )
