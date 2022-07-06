6a6e1c7602df5dc112c99792681422a529d9a13e
Darshan H E <53147940+Darshan-H-E@users.noreply.github.com>
2022-07-06 20:42:55 +0530

    Fixing out arg (#1770)
    * Updated out arg in functional ivy activations
    
    * Updated out arg docstring in activations
    
    * Updated out arg in functional ivy creation
    
    * Updated out arg in functional ivy dtype
    
    * Updated out arg in functional ivy elementwise
    
    * Updated out arg in functional ivy general
    
    * Updated out arg in functional ivy gradients
    
    * Updated out arg in functional ivy layers
    
    * Updated out arg in functional ivy linalg
    
    * Updated out arg in functional ivy manipulation
    
    * Updated out arg in functional ivy random
    
    * Updated out arg in functional ivy searching
    
    * Updated out arg in functional ivy set
    
    * Updated out arg in functional ivy statistical
    
    * Updated out arg in functional ivy norms
    
    * Updated @handle-out-arg decorator in compositional functions
    
    * Updated out arg in functional ivy general
    
    * Updated out arg in ivy functional layers, searching and set
    
    * lint fixes
    
    * lint fixes in dtype and general

--- ivy/array/sorting.py
+++ ivy/array/sorting.py
@@ -7,25 +7,20 @@ import ivy
 
 
 class ArrayWithSorting(abc.ABC):
-
     def argsort(
-            self: ivy.Array,
-            axis: int = -1,
-            descending: bool = False,
-            stable: bool = True,
-            out: Optional[ivy.Array] = None
+        self: ivy.Array,
+        axis: int = -1,
+        descending: bool = False,
+        stable: bool = True,
+        out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.argsort(
-            self._data, axis, descending, stable, out=out
-        )
+        return ivy.argsort(self._data, axis, descending, stable, out=out)
 
     def sort(
-            self: ivy.Array,
-            axis: int = -1,
-            descending: bool = False,
-            stable: bool = True,
-            out: Optional[ivy.Array] = None
+        self: ivy.Array,
+        axis: int = -1,
+        descending: bool = False,
+        stable: bool = True,
+        out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.sort(
-            self._data, axis, descending, stable, out=out
-        )
+        return ivy.sort(self._data, axis, descending, stable, out=out)
