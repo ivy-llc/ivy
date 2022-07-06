df39887dfac695adf2d65f7b520321d8ee893261
Saeed Ashraf <saeedashrraf@gmail.com>
2022-07-06 23:43:00 +0200

    More format fixes

--- ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_activations.py
@@ -5,8 +5,6 @@ import numpy as np
 from hypothesis import given, strategies as st
 
 # local
-
-import ivy
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 
