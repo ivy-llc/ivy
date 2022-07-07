64b5963796fc4c0ff64394f713945aa7baa6fbb3
James Keane <81018496+jkeane508@users.noreply.github.com>
2022-07-07 14:24:39 +0000

    Moved return statement below docstrings

--- ivy/array/creation.py
+++ ivy/array/creation.py
@@ -17,13 +17,12 @@ class ArrayWithCreation(abc.ABC):
         dtype: Optional[Union[ivy.Dtype, ivy.NativeDtype]] = None,
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
     ) -> ivy.Array:
-        return ivy.asarray(self._data, copy=copy, dtype=dtype, device=device)
-
         """
         ivy.Array instance method variant of ivy.asarray. This method simply wraps the
         function, and so the docstring for ivy.asarray also applies to this method
         with minimal changes.
         """
+        return ivy.asarray(self._data, copy=copy, dtype=dtype, device=device)
 
     def full_like(
         self: ivy.Array,
@@ -33,14 +32,14 @@ class ArrayWithCreation(abc.ABC):
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.full_like(
-            self._data, fill_value, dtype=dtype, device=device, out=out
-        )
         """
         ivy.Array instance method variant of ivy.full_like. This method simply wraps the
         function, and so the docstring for ivy.full_like also applies to this method
         with minimal changes.
         """
+        return ivy.full_like(
+            self._data, fill_value, dtype=dtype, device=device, out=out
+        )
 
     def ones_like(
         self: ivy.Array,
@@ -49,12 +48,12 @@ class ArrayWithCreation(abc.ABC):
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.ones_like(self._data, dtype=dtype, device=device, out=out)
         """
         ivy.Array instance method variant of ivy.ones_like. This method simply wraps the
         function, and so the docstring for ivy.ones_like also applies to this method
         with minimal changes.
         """
+        return ivy.ones_like(self._data, dtype=dtype, device=device, out=out)
 
     def zeros_like(
         self: ivy.Array,
@@ -63,28 +62,28 @@ class ArrayWithCreation(abc.ABC):
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.zeros_like(self._data, dtype=dtype, device=device, out=out)
         """
-        ivy.Array instance method variant of ivy.zeros_like. This method simply wraps 
-        the function, and so the docstring for ivy.zeros_like also applies to this 
+        ivy.Array instance method variant of ivy.zeros_like. This method simply wraps
+        the function, and so the docstring for ivy.zeros_like also applies to this
         method with minimal changes.
         """
+        return ivy.zeros_like(self._data, dtype=dtype, device=device, out=out)
 
     def tril(self: ivy.Array, k: int = 0, out: Optional[ivy.Array] = None) -> ivy.Array:
-        return ivy.tril(self._data, k, out=out)
         """
         ivy.Array instance method variant of ivy.tril. This method simply wraps the
         function, and so the docstring for ivy.tril also applies to this method
         with minimal changes.
         """
+        return ivy.tril(self._data, k, out=out)
 
     def triu(self: ivy.Array, k: int = 0, out: Optional[ivy.Array] = None) -> ivy.Array:
-        return ivy.triu(self._data, k, out=out)
         """
         ivy.Array instance method variant of ivy.triu. This method simply wraps the
         function, and so the docstring for ivy.triu also applies to this method
         with minimal changes.
         """
+        return ivy.triu(self._data, k, out=out)
 
     def empty_like(
         self: ivy.Array,
@@ -93,12 +92,12 @@ class ArrayWithCreation(abc.ABC):
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.empty_like(self._data, dtype=dtype, device=device, out=out)
         """
-        ivy.Array instance method variant of ivy.empty_like. This method simply wraps 
-        the function, and so the docstring for ivy.empty_like also applies to this 
+        ivy.Array instance method variant of ivy.empty_like. This method simply wraps
+        the function, and so the docstring for ivy.empty_like also applies to this
         method with minimal changes.
         """
+        return ivy.empty_like(self._data, dtype=dtype, device=device, out=out)
 
     def meshgrid(
         self: ivy.Array,
@@ -106,23 +105,23 @@ class ArrayWithCreation(abc.ABC):
         indexing: Optional[str] = "xy",
     ) -> List[ivy.Array]:
         ret = [self._data] + list(arrays)
-        return ivy.meshgrid(*ret, indexing=indexing)
         """
         ivy.Array instance method variant of ivy.meshgrid. This method simply wraps the
         function, and so the docstring for ivy.meshgrid also applies to this method
         with minimal changes.
         """
+        return ivy.meshgrid(*ret, indexing=indexing)
 
     def from_dlpack(
         self: ivy.Array,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
-        return ivy.from_dlpack(self._data, out=out)
         """
-        ivy.Array instance method variant of ivy.from_dlpack. This method simply wraps 
-        the function, and so the docstring for ivy.from_dlpack also applies to this 
+        ivy.Array instance method variant of ivy.from_dlpack. This method simply wraps
+        the function, and so the docstring for ivy.from_dlpack also applies to this
         method with minimal changes.
         """
+        return ivy.from_dlpack(self._data, out=out)
 
     # Extra #
     # ------#
@@ -133,9 +132,9 @@ class ArrayWithCreation(abc.ABC):
         dtype: Optional[Union[ivy.Dtype, ivy.NativeDtype]] = None,
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
     ) -> ivy.NativeArray:
-        return ivy.native_array(self._data, dtype=dtype, device=device)
         """
-        ivy.Array instance method variant of ivy.native_array. This method simply wraps 
-        the function, and so the docstring for ivy.native_array also applies to this 
+        ivy.Array instance method variant of ivy.native_array. This method simply wraps
+        the function, and so the docstring for ivy.native_array also applies to this
         method with minimal changes.
         """
+        return ivy.native_array(self._data, dtype=dtype, device=device)
