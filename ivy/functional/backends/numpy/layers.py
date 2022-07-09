01994ddd625c46dd716f7a6cb128c0b51a9a5ede
sherry30 <sherrytst30@gmail.com>
2022-07-09 15:08:11 +0500

    numpy conv2d fix: padding, dilations and strides
    Former-commit-id: 58dca9697c12873ec1b16419d2098cea808b255e

--- ivy/functional/backends/numpy/layers.py
+++ ivy/functional/backends/numpy/layers.py
@@ -40,22 +40,64 @@ def conv2d(
     data_format: str = "NHWC",
     dilations: int = 1,
 ) -> np.ndarray:
+
+    # adding dilations
+    if dilations > 1:
+        filters = np.insert(
+            filters,
+            [i for i in range(1, filters.shape[1])] * (dilations - 1),
+            values=0,
+            axis=1
+        )
+        filters = np.insert(
+            filters,
+            [i for i in range(1, filters.shape[0])] * (dilations - 1),
+            values=0,
+            axis=0
+        )
+
     filter_shape = filters.shape[0:2]
     filter_shape = list(filter_shape)
+
     if data_format == "NCHW":
         x = np.transpose(x, (0, 2, 3, 1))
+
+    x_shape = list(x.shape[1:3])
     if padding == "SAME":
+        if x_shape[1] % strides == 0:
+            pad_w = max(filter_shape[1] - strides, 0)
+        else:
+            pad_w = max(filter_shape[1] - (x_shape[1] % strides), 0)
+
+        if x_shape[0] % strides == 0:
+            pad_h = max(filter_shape[0] - strides, 0)
+        else:
+            pad_h = max(filter_shape[0] - (x_shape[0] % strides), 0)
+
         x = np.pad(
-            x, [[0, 0], [filter_shape[0] // 2] * 2, [filter_shape[1] // 2] * 2, [0, 0]]
+            x,
+            [(0, 0),
+             (pad_h // 2, pad_h - pad_h // 2),
+             (pad_w // 2, pad_w - pad_w // 2),
+             (0, 0)],
+            'constant'
         )
+
     x_shape = x.shape
     input_dim = filters.shape[-2]
     output_dim = filters.shape[-1]
-    new_h = x_shape[1] - filter_shape[0] + 1
-    new_w = x_shape[2] - filter_shape[1] + 1
+    new_h = (x_shape[1] - filter_shape[0]) // strides + 1
+    new_w = (x_shape[2] - filter_shape[1]) // strides + 1
     new_shape = [x_shape[0], new_h, new_w] + filter_shape + [x_shape[-1]]
-    # ToDo: add non-unit stride support
-    new_strides = x.strides[0:1] + x.strides[1:3] + x.strides[1:3] + x.strides[-1:]
+    new_strides = (
+        x.strides[0],
+        x.strides[1] * strides,
+        x.strides[2] * strides,
+        x.strides[1],
+        x.strides[2],
+        x.strides[3]
+    )
+
     # B x OH x OW x KH x KW x I
     sub_matrices = np.lib.stride_tricks.as_strided(
         x, new_shape, new_strides, writeable=False
