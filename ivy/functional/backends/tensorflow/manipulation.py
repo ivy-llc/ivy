b5f31d1b778fdc0da49c0f51d51bb6f331c77b24
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-11 11:42:29 +0800

    tf typehints - manipulation

--- ivy/functional/backends/tensorflow/manipulation.py
+++ ivy/functional/backends/tensorflow/manipulation.py
@@ -2,7 +2,10 @@
 import math
 import tensorflow as tf
 from numbers import Number
-from typing import Union, Tuple, Optional, List
+from typing import Union, Tuple, Optional, List, Sequence
+
+# local
+import ivy
 
 
 # Array API Standard #
@@ -73,7 +76,7 @@ def permute_dims(
 
 def reshape(
     x: Union[tf.Tensor, tf.Variable],
-    shape: Tuple[int, ...],
+    shape: Union[ivy.NativeShape, Sequence[int]],
 ) -> Union[tf.Tensor, tf.Variable]:
     ret = tf.reshape(x, shape)
     return ret
