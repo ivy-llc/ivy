5df5401aa92f42406ee6538ae0b9ae00b083f788
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-12 15:29:17 +0100

    Update activations.py

--- ivy/array/activations.py
+++ ivy/array/activations.py
@@ -1,49 +1,48 @@
 # global
 import abc
-from typing import Optional
-
+from typing import Optional, Union
 
 # local
 import ivy
 
 
+# ToDo: implement all methods here as public instance methods
 
 class ArrayWithActivations(abc.ABC):
-
-    def relu(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
-        return ivy.relu(self, out=out)
+    def relu(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
+    ) -> ivy.Array:
+        return ivy.relu(self._data, out=out)
 
     def leaky_relu(
-        self: ivy.Array, alpha: Optional[float] = 0.2, out: Optional[ivy.Array] = None
+            self: ivy.Array,
+            alpha: Optional[float] = 0.2,
+            out: Optional[ivy.Array] = None
     ) -> ivy.Array:
-        return ivy.leaky_relu(self, alpha, out=out)
+        return ivy.leaky_relu(self._data, alpha, out=out)
 
     def gelu(
-        self: ivy.Array,
-        approximate: Optional[bool] = True,
-        out: Optional[ivy.Array] = None,
+            self: ivy.Array,
+            approximate: Optional[bool] = True,
+            out: Optional[ivy.Array] = None
     ) -> ivy.Array:
-        return ivy.gelu(self, approximate, out=out)
+        return ivy.gelu(self._data, approximate, out=out)
 
-    def tanh(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
-        return ivy.tanh(self, out=out)
-
-    def sigmoid(self: ivy.Array, out: Optional[ivy.Array] = None) -> ivy.Array:
-        return ivy.sigmoid(self, out=out)
-
-    def softmax(
-        self: ivy.Array, axis: Optional[int] = None, out: Optional[ivy.Array] = None
+    def tanh(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
     ) -> ivy.Array:
-        return ivy.softmax(self, axis, out=out)
+        return ivy.tanh(self._data, out=out)
 
-    def softplus(
-        self: ivy.Array,
-        out: Optional[ivy.Array] = None
+    def sigmoid(
+            self: ivy.Array,
+            out: Optional[ivy.Array] = None
     ) -> ivy.Array:
-        """
-        ivy.Array instance method variant of ivy.softplus. This method simply wraps the
-        function, and so the docstring for ivy.softplus also applies to this method
-        with minimal changes.
-        """
-        return ivy.softplus(self._data, out=out)
+        return ivy.sigmoid(self._data, out=out)
 
+    def softmax(self: ivy.Array,
+                axis: Optional[int] = None,
+                out: Optional[ivy.Array] = None
+                ) -> ivy.Array:
+        return ivy.softmax(self._data, axis, out=out)
