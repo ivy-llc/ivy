ff0b00dcdf4973e5cb160d78b5734b5504804101
djl11 <daniel.lenton11@imperial.ac.uk>
2022-07-13 00:31:50 +0200

    modified all clip backends, such that the assertion is less_equal rather than less, with regards to the min values being smaller than the max values.
    Former-commit-id: f479d6e5c1158cdd8b71396048a94787dbcc050f

--- ivy/functional/backends/numpy/manipulation.py
+++ ivy/functional/backends/numpy/manipulation.py
@@ -170,6 +170,6 @@ def swapaxes(
 def clip(
     x: np.ndarray, x_min: Union[Number, np.ndarray], x_max: Union[Number, np.ndarray]
 ) -> np.ndarray:
-    assert ivy.all(ivy.less(x_min, x_max))
+    assert ivy.all(ivy.less_equal(x_min, x_max))
     ret = np.asarray(np.clip(x, x_min, x_max))
     return ret
