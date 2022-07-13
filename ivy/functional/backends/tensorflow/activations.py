0be5b40d8cbb87db436b7b58fef8472bb57bf725
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-13 20:17:40 +0100

    add out argument

--- ivy/functional/backends/tensorflow/activations.py
+++ ivy/functional/backends/tensorflow/activations.py
@@ -9,31 +9,54 @@ import tensorflow as tf
 from tensorflow.python.types.core import Tensor
 
 # local
+import ivy
 
 
-def relu(x: Tensor) -> Tensor:
-    return tf.nn.relu(x)
+def relu(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.relu(x)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def leaky_relu(x: Tensor, alpha: Optional[float] = 0.2) -> Tensor:
-    return tf.nn.leaky_relu(x, alpha)
+def leaky_relu(x: Tensor, alpha: Optional[float] = 0.2, out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.leaky_relu(x, alpha)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def gelu(x: Tensor, approximate: Optional[bool] = True) -> Tensor:
-    return tf.nn.gelu(x, approximate)
+def gelu(x: Tensor, approximate: Optional[bool] = True, out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.gelu(x, approximate)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def sigmoid(x: Tensor) -> Tensor:
-    return tf.nn.sigmoid(x)
+def sigmoid(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.sigmoid(x)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def tanh(x: Tensor) -> Tensor:
-    return tf.nn.tanh(x)
+def tanh(x: Tensor,out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.tanh(x)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def softmax(x: Tensor, axis: Optional[int] = None) -> Tensor:
-    return tf.exp(x) / tf.reduce_sum(tf.exp(x), axis, keepdims=True)
+def softmax(x: Tensor, axis: Optional[int] = None,out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.exp(x) / tf.reduce_sum(tf.exp(x), axis, keepdims=True)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
 
 
-def softplus(x: Tensor) -> Tensor:
-    return tf.nn.softplus(x)
+def softplus(x: Tensor,out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.softplus(x)
+    if ivy.exists(out):
+        return ivy.inplace_update(out, ret)
+    return ret
+
