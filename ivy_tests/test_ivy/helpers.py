69c47f5e149c4f30d3bdde9238994c8d1e9dac89
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-13 10:12:01 +0800

    fix np docstr

--- ivy_tests/test_ivy/helpers.py
+++ ivy_tests/test_ivy/helpers.py
@@ -745,7 +745,6 @@ def test_function(
                              container_flags, instance_method,\
                               fw, fn_name, x1=x1, x2=x2)
     """
-
     # convert single values to length 1 lists
     input_dtypes, as_variable_flags, native_array_flags, container_flags = as_lists(
         input_dtypes, as_variable_flags, native_array_flags, container_flags
