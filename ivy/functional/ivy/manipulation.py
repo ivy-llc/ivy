10ebe0681cac4afab8f1f796acd8dec0be4606d6
JG <67888047+juliagsy@users.noreply.github.com>
2022-07-13 20:22:22 +0800

    updates of`ivy.Shape` and `ivy.NativeShape`  (#1823)

--- ivy/functional/ivy/manipulation.py
+++ ivy/functional/ivy/manipulation.py
@@ -341,7 +341,7 @@ def permute_dims(
 @handle_nestable
 def reshape(
     x: Union[ivy.Array, ivy.NativeArray],
-    shape: Tuple[int, ...],
+    shape: Union[ivy.Shape, ivy.NativeShape],
     copy: Optional[bool] = None,
     *,
     out: Optional[ivy.Array] = None,
