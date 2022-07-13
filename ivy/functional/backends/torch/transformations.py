0edfd9bc973e9fd3d85e6f275929323325ee1df9
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-07-13 19:05:59 +0100

    updating the test

--- ivy/functional/backends/torch/transformations.py
+++ ivy/functional/backends/torch/transformations.py
@@ -6,6 +6,8 @@ import ivy
 
 
 def vmap(func, in_axes=0, out_axes=0):
-    new_func = functorch.vmap(func, in_axes, out_axes)
-    new_func = ivy.to_native_arrays_and_back(new_func)
-    return new_func
+    @ivy.to_native_arrays_and_back
+    def _vmap(*args, **kwargs):
+        new_func = functorch.vmap(func, in_axes, out_axes)
+        return new_func(*args, **kwargs)
+    return _vmap
