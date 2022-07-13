ada4e32f45077051e20b0bd9296b6014a7a9f6d0
DankGunther <73615185+DankGunther@users.noreply.github.com>
2022-07-13 18:38:19 +0100

    Update layers.py
    Updated Jax function conv3d_transpose
    included code to handle data format

--- ivy/functional/backends/jax/layers.py
+++ ivy/functional/backends/jax/layers.py
@@ -1,11 +1,13 @@
 """Collection of Jax network layers, wrapped to fit Ivy syntax and signature."""
 
 # global
+import math
+
 import jax.lax as jlax
 
 # local
 from ivy.functional.backends.jax import JaxArray
-from typing import Union, Tuple
+from typing import Union, Tuple, Sequence, List
 
 
 def conv1d(
@@ -65,9 +67,23 @@ def conv3d_transpose(
     filters: JaxArray,
     strides: Union[int, Tuple[int], Tuple[int, int], Tuple[int, int, int]],
     padding: Union[str, Sequence[Tuple[int, int]]],
-    dilations: Optional[Sequence[int]],
+    dilations: Union[int, Tuple[int], Tuple[int, int], Tuple[int, int, int]] = 1,
     data_format: str = "NDHWC"
 ) -> JaxArray:
-    return JaxArray.nn.conv3d_transpose(
-        x, filters, strides, padding, None, dilations, (data_format, "WIO", data_format)
-    ) 
+    filter_shape = list(filters.shape[0:1])
+    filters = filters.permute(3, 4, 0, 1, 2)
+    if data_format == "NDHWC":
+        x = jlax.conv_transpose(0, 4, 1, 2, 3)
+    if padding == "VALID":
+        padding_list: List[int] = [0, 0, 0]
+    elif padding == "SAME":
+        padding_list: List[int] = [math.floor(item / 2) for item in filter_shape]
+    else:
+        raise Exception(
+            "Invalid padding arg {}\n"
+            'Must be one of: "VALID" or "SAME"'.format(padding)
+        )
+    res = jlax.conv_transpose(
+        x, filters, strides, padding_list, None, dilation= dilations
+    )
+    return res
