2864557f80b8def7b25aaf2ffccbe7258ed7175e
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-14 00:28:46 +0100

    format fix

--- ivy/functional/backends/torch/activations.py
+++ ivy/functional/backends/torch/activations.py
@@ -19,18 +19,22 @@ def relu(x: torch.Tensor, out: Optional[torch.Tensor] = None) -> torch.Tensor:
     return ret
 
 
-def leaky_relu(x: torch.Tensor, alpha: Optional[float] = 0.2, out: Optional[torch.Tensor] = None) -> torch.Tensor:
+def leaky_relu(x: torch.Tensor,
+    alpha: Optional[float] = 0.2,
+    out: Optional[torch.Tensor] = None
+) -> torch.Tensor:
     ret = torch.nn.functional.leaky_relu(x, alpha)
     if ivy.exists(out):
         return ivy.inplace_update(out, ret)
     return ret
 
 
-def gelu(x, approximate: bool = True, out: Optional[torch.Tensor] = None) -> torch.Tensor:
+def gelu(x : torch.Tensor,
+    approximate: bool = True,
+    out: Optional[torch.Tensor] = None
+) -> torch.Tensor:
     if approximate:
-        ret = (
-                0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
-        )
+        ret = (0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3))))
     else:
         ret = torch.nn.functional.gelu(x)
     if ivy.exists(out):
