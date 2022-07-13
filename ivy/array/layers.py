e2799df2f789d66676e22f84c6618ad55f67d0f7
vincent1bt <vincent14319@gmail.com>
2022-07-12 21:33:59 -0500

    Fixed docstring formatting

--- ivy/array/layers.py
+++ ivy/array/layers.py
@@ -20,16 +20,18 @@ class ArrayWithLayers(abc.ABC):
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         """
-        ivy.Array instance method variant of ivy.depthwise_conv2d. This method simply wraps the
-        function, and so the docstring for ivy.depthwise_conv2d also applies to this method
-        with minimal changes.
+        ivy.Array instance method variant of ivy.depthwise_conv2d. This method
+        simply wraps the function, and so the docstring for ivy.depthwise_conv2d
+        also applies to this method with minimal changes.
 
         Examples
         --------
-        >>> x = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0 #NHWC
-        >>> filters = ivy.random_normal(0, 1, [3, 3, 3]) #HWI
+        >>> x = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0
+        >>> filters = ivy.random_normal(0, 1, [3, 3, 3])
         >>> y = x.depthwise_conv2d(filters, strides=2, padding='SAME')
         >>> print(y.shape)
         (1, 64, 64, 3)
         """
-        return ivy.depthwise_conv2d(self._data, filters, strides, padding, data_format, dilations, out=out)
+        return ivy.depthwise_conv2d(
+            self._data, filters, strides, padding, data_format, dilations, out=out
+        )
--- ivy/container/layers.py
+++ ivy/container/layers.py
@@ -22,17 +22,21 @@ class ContainerWithLayers(ContainerBase):
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
         """
-        ivy.Container static method variant of ivy.depthwise_conv2d. This method simply wraps the
-        function, and so the docstring for ivy.depthwise_conv2d also applies to this method
-        with minimal changes.
+        ivy.Container static method variant of ivy.depthwise_conv2d. This method
+        simply wraps the function, and so the docstring for ivy.depthwise_conv2d
+        also applies to this method with minimal changes.
 
         Examples
         --------
-        >>> a = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0 #NHWC
-        >>> b = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0 #NHWC
+        >>> a = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0
+        >>> b = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0
         >>> inp = ivy.Container(a=a, b=b)
         >>> filters = ivy.random_normal(0, 1, [3, 3, 3])
-        >>> y = ivy.Container.static_depthwise_conv2d(inp, filters, strides=2, padding='SAME')
+        >>> y = ivy.Container.static_depthwise_conv2d( \
+                                                    inp, \ 
+                                                    filters, \ 
+                                                    strides=2, \ 
+                                                    padding='SAME')
         >>> print(y.shape)
         [1, 64, 64, 3]
         """
@@ -58,14 +62,14 @@ class ContainerWithLayers(ContainerBase):
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
         """
-        ivy.Container instance method variant of ivy.depthwise_conv2d. This method simply wraps the
-        function, and so the docstring for ivy.depthwise_conv2d also applies to this method
-        with minimal changes.
+        ivy.Container instance method variant of ivy.depthwise_conv2d. This method
+        simply wraps the function, and so the docstring for ivy.depthwise_conv2d
+        also applies to this method with minimal changes.
 
         Examples
         --------
-        >>> a = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0 #NHWC
-        >>> b = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0 #NHWC
+        >>> a = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0
+        >>> b = ivy.randint(0, 255, (1, 128, 128, 3)).astype(ivy.float32) / 255.0
         >>> inp = ivy.Container(a=a, b=b)
         >>> filters = ivy.random_normal(0, 1, [3, 3, 3])
         >>> y = inp.depthwise_conv2d(filters, strides=2, padding='SAME')
@@ -75,4 +79,3 @@ class ContainerWithLayers(ContainerBase):
         return self.static_depthwise_conv2d(
             self, filters, strides, padding, data_format, dilations, out=out
         )
-
