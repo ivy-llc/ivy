a8e49ff99694554665b3ba7d6c3f86123f0f4a9a
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-07-14 09:31:32 +0000

    Updated docstrings for special methods and made minor changes to other function for add, tan and roll

--- ivy/container/container.py
+++ ivy/container/container.py
@@ -108,6 +108,20 @@ class Container(
         ivy.Container special method for the add operator, calling :code:`operator.add`
         for each of the corresponding leaves of the two containers.
 
+        Parameters
+        ----------
+        self
+            first input container. Should have a numeric data type.
+        other
+            second input array or container. Must be compatible with ``self``
+            (see :ref:`broadcasting`). Should have a numeric data type.
+        
+        Returns
+        -------
+        ret
+            a container containing the element-wise sums. The returned array must have a
+            data type determined by :ref:`type-promotion`.
+
         Examples
         --------
         With :code:`Number` instances at the leaves:
@@ -158,6 +172,20 @@ class Container(
         """
         ivy.Container reverse special method for the add operator, calling
         :code:`operator.add` for each of the corresponding leaves of the two containers.
+
+        Parameters
+        ----------
+        self
+            first input container. Should have a numeric data type.
+        other
+            second input array or container. Must be compatible with ``self``
+            (see :ref:`broadcasting`). Should have a numeric data type.
+
+        Returns
+        -------
+        ret
+            a container containing the element-wise sums. The returned array must have a
+            data type determined by :ref:`type-promotion`.
 
         Examples
         --------
