559a7140d479c95422262c9085adeee3d5163de7
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-14 18:45:13 +0000

    Keep experimenting

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-cd ivy
+
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
 docker run --rm --env IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test*
