f9fcd07f98ab23c02db09b8839edeafa1839741a
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-07-14 15:07:23 +0000

    Fixed the issue with the lambda function in ivy/functional/data_type.py

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -882,9 +882,6 @@ def is_int_dtype(
     }
     """
 
-    def check_type(x):
-        return isinstance(x, (int, np.integer)) and not type(x) == bool
-
     if ivy.is_native_array(dtype_in):
         dtype_in = ivy.dtype(dtype_in)
     elif isinstance(dtype_in, np.ndarray):
@@ -901,7 +898,7 @@ def is_int_dtype(
             True
             if ivy.nested_indices_where(
                 dtype_in,
-                check_type(dtype_in),
+                lambda x: isinstance(x, (int, np.integer)) and not type(x) == bool,
             )
             else False
         )
