b6a5acf606530a98f09ce833ae019cac584fd031
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-14 20:19:59 +0000

    use for loop

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -1,7 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
 
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
-
+core_tests= $(ls -d ivy_tests/test_ivy/test_functional/test_core/test*) 
 echo $PWD
-docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/"$2"
+echo $core_tests
+# docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/$2
+
+for i in $core_tests
+do
+  pytest $i &
+done
+
+wait
+echo "All done"
