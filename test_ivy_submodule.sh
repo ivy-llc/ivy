d414b8a2ef8512afaa67abf5aa03c21acdd897ec
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-14 19:33:15 +0000

    echo pwd

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -2,5 +2,6 @@
 
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
-core_tests=( $(ls -d ivy/ivy_tests/test_ivy/test_functional/test_core/test*) )
+core_tests=( $(ls -d ivy_tests/test_ivy/test_functional/test_core/test*) )
+echo $PWD
 docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ${core_tests[@]}
