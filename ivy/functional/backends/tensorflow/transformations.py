a62f69fa6b39c2fa5c104581b5943521710d58b2
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-07-15 00:43:45 +0100

    fixing tensorflow vmap for none axis

--- ivy/functional/backends/tensorflow/transformations.py
+++ ivy/functional/backends/tensorflow/transformations.py
@@ -62,11 +62,7 @@ def vmap(func, in_axes=0, out_axes=0):
                 number of positional arguments to the function being vectorized
                 or it should be an integer.''')
 
-        # Making sure not all in_axes are None
-        if isinstance(in_axes, (list, tuple)):
-            assert not all(ax is None for ax in in_axes), "All in_axes should be non-None"
-        else:
-            assert not (in_axes is None), "in_axes should be non-None if integer"
+
 
         # checking axis_size consistency
         axis_size = set()
@@ -75,12 +71,19 @@ def vmap(func, in_axes=0, out_axes=0):
             for arg in args:
                 axis_size.add(arg.shape[in_axes])
         elif isinstance(in_axes, (list, tuple)):
-            for (arg, axis) in zip(args, in_axes):
-                axis_size.add(arg.shape[axis])
+            for arg, axis in zip(args, in_axes):
+                if axis is not None:
+                    axis_size.add(arg.shape[axis])
+
 
         if len(axis_size) > 1:
             raise ValueError('''Inconsistent sizes. All axes should have the same size''')
 
+        # Making sure not all in_axes are None
+        if isinstance(in_axes, (list, tuple)):
+            assert not all(ax is None for ax in in_axes), "All in_axes should be non-None"
+        else:
+            assert not (in_axes is None), "in_axes should be non-None if integer"
 
 
         # Handling None in in_axes by broadcasting the axis_size
