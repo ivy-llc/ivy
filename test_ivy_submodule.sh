da7b2c79d0a4e6f90d6a654b7c4040d6f89e7b5e
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-14 20:40:19 +0000

    experiment

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
-core_tests=( $(ls -d /home/runner/work/ivy/ivy/ivy_tests/test_ivy/test_functional/test_core/test*) )
+
 echo $PWD
-echo $core_tests
-# docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/$2
-
-for i in $core_tests
-do
-  docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest $i &
-done
-
-wait
-echo "All done"
+docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/"$2"
