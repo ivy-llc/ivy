840b896d23fe850b7e443d79dc12171077fcb35b
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-14 19:30:24 +0000

    use full path

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -2,5 +2,5 @@
 
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
-core_tests=( $(ls -d ivy_tests/test_ivy/test_functional/test_core/test*) )
-docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/${core_tests[@]}
+core_tests=( $(ls -d ivy/ivy_tests/test_ivy/test_functional/test_core/test*) )
+docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ${core_tests[@]}
