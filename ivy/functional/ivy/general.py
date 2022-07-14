c8541f8af0483509f8d339a11edd1f4758641ab8
brgopalakrishnan <winwin.endeavor@gmail.com>
2022-07-14 10:08:12 +0530

    Function Reformating

--- ivy/functional/ivy/general.py
+++ ivy/functional/ivy/general.py
@@ -1220,6 +1220,204 @@ def value_is_nan(
     ret
         Boolean as to whether the input value is a nan or not.
 
+    Functional Examples
+    --------
+
+    With :code:`ivy.Array` input:
+
+    >>> x = ivy.array([1])
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    False
+
+    >>> x = ivy.array([1])
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    >>> x = ivy.array([float('INF')])
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    True
+
+    >>> x = ivy.array([float('INF')])
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    With :code:`ivy.NativeArray` input:
+
+    >>> x = ivy.native_array([1])
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    False
+
+    >>> x = ivy.native_array([1])
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    >>> x = ivy.native_array([float('INF')])
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    True
+
+    >>> x = ivy.native_array([float('INF')])
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    With :code:`Number` inputs:
+
+    >>> x = 1
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    False
+
+    >>> x = 1
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    >>> x = float('INF')
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    True
+
+    >>> x = float('INF')
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    False
+
+    With :code:`ivy.Container` input:
+
+    >>> x = ivy.Container(a=ivy.array([3]))
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    {
+        a: false
+    }
+
+    >>> x = ivy.Container(a=ivy.array([3]))
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    {
+        a: false
+    }
+    
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.array([-3]), c=ivy.array(float('INF')))
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: true
+    }
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.array([-3]), c=ivy.array(float('INF')))
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: false
+    }
+
+    With a mix of :code:`ivy.Array` :code:`ivy.NativeArray` :code:`Number` and :code:`ivy.Container` inputs: 
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.native_array([-3]), c=float('INF'))
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: true
+    }
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.native_array([-3]), c=float('INF'))
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: false
+    }
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.native_array([float('INF')]), c=-3)
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    {
+        a: false,
+        b: true,
+        c: false
+    }
+
+    >>> x = ivy.Container(a=ivy.array([1]), b=ivy.native_array([float('INF')]), c=-3)
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: false
+    }
+
+    >>> x = ivy.Container(a=ivy.array([float('INF')]), b=ivy.native_array([3]), c=-3)
+    >>> y = ivy.value_is_nan(x)
+    >>> print(y)
+    {
+        a: true,
+        b: false,
+        c: false
+    }
+
+    >>> x = ivy.Container(a=ivy.array([float('INF')]), b=ivy.native_array([3]), c=-3)
+    >>> y = ivy.value_is_nan(x, False)
+    >>> print(y)
+    {
+        a: false,
+        b: false,
+        c: false
+    }
+
+    Instance Method Examples
+    ------------------------
+
+    Using :code:`ivy.Array` instance method:
+
+    >>> x = ivy.array([1])
+    >>> x.value_is_nan()
+    False
+
+    >>> x = ivy.array([1])
+    >>> x.value_is_nan(False)
+    False
+
+    >>> x = ivy.array([float('INF')])
+    >>> x.value_is_nan()
+    True
+
+    >>> x = ivy.array([float('INF')])
+    >>> x.value_is_nan(False)
+    False
+    
+    Using :code:`ivy.Container` instance method:
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.array([-3]), c=ivy.array(float('INF')))
+    >>> x.value_is_nan()
+    {
+        a: false,
+        b: false,
+        c: true
+    }
+
+    >>> x = ivy.Container(a=ivy.array([3]), b=ivy.array([-3]), c=ivy.array(float('INF')))
+    >>> x.value_is_nan(False)
+    {
+        a: false,
+        b: false,
+        c: false
+    }
+
     """
     x_scalar = ivy.to_scalar(x) if ivy.is_native_array(x) else x
     if not x_scalar == x_scalar:
