1b034add358513708e6dbb74ef4b7b27cccbb98f
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-14 10:27:27 +0800

    fix attribute rules

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -14,6 +14,25 @@ from ivy.func_wrapper import (
     handle_nestable,
 )
 
+
+def _is_valid_dtypes_attributes(fn: Callable) -> bool:
+    if hasattr(fn, "supported_dtypes") and hasattr(fn, "unsupported_dtypes"):
+        fn_supported_dtypes = fn.supported_dtypes
+        fn_unsupported_dtypes = fn.unsupported_dtypes
+        if isinstance(fn_supported_dtypes, dict):
+            if isinstance(fn_unsupported_dtypes, dict):
+                backend_str = ivy.current_backend_str()
+                if (
+                    backend_str in fn_supported_dtypes
+                    and backend_str in fn_unsupported_dtypes
+                ):
+                    return False
+        else:
+            if isinstance(fn_unsupported_dtypes, tuple):
+                return False
+    return True
+
+
 # Array API Standard #
 # -------------------#
 
@@ -666,13 +685,13 @@ def function_supported_dtypes(fn: Callable) -> Tuple:
     ['int8', 'int16', 'int32', 'int64', 'uint8', \
      'bfloat16', 'float32', 'float64', 'bool']
     """
+    if not _is_valid_dtypes_attributes(fn):
+        raise Exception(
+            "supported_dtypes and unsupported_dtypes attributes cannot both \
+             exist in a particular backend"
+        )
     supported_dtypes = tuple()
     if hasattr(fn, "supported_dtypes"):
-        if hasattr(fn, "unsupported_dtypes"):
-            raise Exception(
-                "supported_dtypes and unsupported_dtypes attributes cannot both \
-                 exist in a particular function"
-            )
         fn_supported_dtypes = fn.supported_dtypes
         if isinstance(fn_supported_dtypes, dict):
             backend_str = ivy.current_backend_str()
@@ -706,13 +725,13 @@ def function_unsupported_dtypes(fn: Callable) -> Tuple:
     ('float16','uint16','uint32','uint64')
 
     """
+    if not _is_valid_dtypes_attributes(fn):
+        raise Exception(
+            "supported_dtypes and unsupported_dtypes attributes cannot both \
+             exist in a particular backend"
+        )
     unsupported_dtypes = ivy.invalid_dtypes
     if hasattr(fn, "unsupported_dtypes"):
-        if hasattr(fn, "supported_dtypes"):
-            raise Exception(
-                "supported_dtypes and unsupported_dtypes attributes cannot both \
-                 exist in a particular function"
-            )
         fn_unsupported_dtypes = fn.unsupported_dtypes
         if isinstance(fn_unsupported_dtypes, dict):
             backend_str = ivy.current_backend_str()
