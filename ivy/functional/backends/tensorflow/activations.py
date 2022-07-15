25f66b162a0423e98a53f5d7b29ddb8261076023
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-15 03:42:14 +0100

    Update activations.py

--- ivy/functional/backends/tensorflow/activations.py
+++ ivy/functional/backends/tensorflow/activations.py
@@ -22,52 +22,35 @@ def relu(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
 def leaky_relu(
     x: Tensor,
     alpha: Optional[float] = 0.2,
-    out: Optional[Tensor] = None
 ) -> Tensor:
-    ret = tf.nn.leaky_relu(x, alpha)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return tf.nn.leaky_relu(x, alpha)
+
 
 
 def gelu(
     x: Tensor,
     approximate: Optional[bool] = True,
-    out: Optional[Tensor] = None
 ) -> Tensor:
-    ret = tf.nn.gelu(x, approximate)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return tf.nn.gelu(x, approximate)
 
 
-def sigmoid(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
-    ret = tf.nn.sigmoid(x)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+def sigmoid(x: Tensor) -> Tensor:
+    return tf.nn.sigmoid(x)
 
 
-def tanh(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
-    ret = tf.nn.tanh(x)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+
+def tanh(x: Tensor) -> Tensor:
+    return tf.nn.tanh(x)
+
 
 
 def softmax(
     x: Tensor,
     axis: Optional[int] = None,
-    out: Optional[Tensor] = None
 ) -> Tensor:
-    ret = tf.exp(x) / tf.reduce_sum(tf.exp(x), axis, keepdims=True)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return tf.exp(x) / tf.reduce_sum(tf.exp(x), axis, keepdims=True)
 
 
-def softplus(x: Tensor, out: Optional[Tensor] = None) -> Tensor:
-    ret = tf.nn.softplus(x)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+
+def softplus(x: Tensor) -> Tensor:
+    return tf.nn.softplus(x)
