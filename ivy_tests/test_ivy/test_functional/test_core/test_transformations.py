fb939a69784ad5edb94c57e8baca91d703598ab7
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-07-15 01:21:03 +0100

    cleaning up test

--- ivy_tests/test_ivy/test_functional/test_core/test_transformations.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_transformations.py
@@ -18,69 +18,6 @@ def _fn3(x, y):
     ivy.add(x, y)
 
 
-# @given(func=st.sampled_from([_fn1, _fn2, _fn3]),
-#        shape1=st.shared(hnp.array_shapes(min_dims=2, max_dims=5, min_side=2, max_side=5),key="shape1"),
-#        shape2=hnp.array_shapes(min_dims=2, max_dims=5, min_side=2, max_side=5),
-#        in_axes= st.shared(helpers.get_num_axis(shape1, num=2), key="shape1"),
-#        in_axes_as_cont=st.booleans())
-# def test_vmap(func, shape1, shape2, in_axes_as_cont, fw):
-#     num_pos_args = func.__code__.co_argcount
-#     array1 = np.random.randint(0, 10, shape1)
-#     array2 = np.random.randint(0, 10, shape2)
-#     arrays = [ivy.to_native(ivy.array(array1)), ivy.to_native(ivy.array(array2))]
-#     # in_axes = data.draw(helpers.valid_axes(len(array1.shape)))
-#
-#     if in_axes_as_cont:
-#         dim_list = list()
-#         for xi in arrays:
-#             dim_list.append(len(xi.shape))
-#         in_axes = np.random.randint(1, high=dim_list, size=num_pos_args).tolist()
-#         vmapped_func = ivy.vmap(func, in_axes=list(in_axes), out_axes=0)
-#     else:
-#         vmapped_func = ivy.vmap(func, in_axes=0, out_axes=0)
-#
-#     assert callable(vmapped_func)
-#
-#     try:
-#         fw_res = vmapped_func(*arrays)
-#     except Exception as error:
-#         print("fw Error:", error)
-#         fw_res = None
-#
-#     ivy.set_backend("jax")
-#     arrays = [ivy.array(array1), ivy.array(array2)]
-#     if in_axes_as_cont:
-#         dim_list = list()
-#         for xi in arrays:
-#             dim_list.append(len(xi.shape))
-#         in_axes = np.random.randint(1, high=dim_list, size=num_pos_args).tolist()
-#         jax_vmapped_func = ivy.vmap(func, in_axes=(in_axes), out_axes=0)
-#     else:
-#         jax_vmapped_func = ivy.vmap(func, in_axes=0, out_axes=0)
-#
-#     assert callable(jax_vmapped_func)
-#
-#     try:
-#         jax_res = jax_vmapped_func(*arrays)
-#     except Exception as error:
-#         print("jax Error:", error)
-#         jax_res = None
-#
-#     ivy.unset_backend()
-#
-#     if fw_res is not None and jax_res is not None:
-#         assert ivy.array_equal(fw_res, jax_res), f"Results are not equal. fw: {fw_res}, Jax: {jax_res}"
-#
-#     elif fw_res is None and jax_res is None:
-#         pass
-#     else:
-#         print("shape1:", shape1, "shape2:", shape2)
-#         print("fw_res:", fw_res)
-#         print("jax_res:", jax_res)
-#         assert False, "One of the results is None while other isn't"
-#     #del jax_res, fw_res
-
-
 @given(func=st.sampled_from([_fn1, _fn2, _fn3]),
        arrays_and_axes=helpers.arrays_and_axes(allow_none=False,
                                                min_num_dims=2,
@@ -89,7 +26,7 @@ def _fn3(x, y):
                                                max_dim_size=10,
                                                num=2),
        in_axes_as_cont=st.booleans())
-def test_vmap2(func, arrays_and_axes, in_axes_as_cont, fw):
+def test_vmap(func, arrays_and_axes, in_axes_as_cont, fw):
 
     generated_arrays, in_axes = arrays_and_axes
     arrays = [ivy.native_array(array) for array in generated_arrays]
@@ -119,7 +56,7 @@ def test_vmap2(func, arrays_and_axes, in_axes_as_cont, fw):
     try:
         jax_res = jax_vmapped_func(*arrays)
     except Exception as error:
-        #print("jax Error:", error)
+        print("jax Error:", error)
         jax_res = None
 
     ivy.unset_backend()
@@ -142,7 +79,4 @@ def test_vmap2(func, arrays_and_axes, in_axes_as_cont, fw):
         # print("jax_res:", jax_res)
         assert False, "One of the results is None while other isn't"
     # TODO: Tune the examples generated
-
-
-
 
