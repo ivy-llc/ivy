69f7db9866338adeb64180f34103b47d70952010
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-15 03:56:35 +0100

    format

--- ivy/functional/backends/torch/activations.py
+++ ivy/functional/backends/torch/activations.py
@@ -27,11 +27,9 @@ def gelu(
     approximate: bool = True,
 ) -> torch.Tensor:
     if approximate:
-        return (
+            return (
                 0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)
-                                          )
-                           )
-        )
+                                          )))
     return torch.nn.functional.gelu(x)
 
 
