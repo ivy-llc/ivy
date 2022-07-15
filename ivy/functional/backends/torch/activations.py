83213ee229149b2bacb2632f5fadf0494cb70e3e
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-15 03:42:34 +0100

    Update activations.py

--- ivy/functional/backends/torch/activations.py
+++ ivy/functional/backends/torch/activations.py
@@ -12,38 +12,26 @@ import torch.nn
 import ivy
 
 
-def relu(x: torch.Tensor, out: Optional[torch.Tensor] = None) -> torch.Tensor:
-    ret = torch.relu(x)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+def relu(x: torch.Tensor) -> torch.Tensor:
+    return torch.relu(x)
 
 
 def leaky_relu(
-    x: torch.Tensor,
-    alpha: Optional[float] = 0.2,
-    out: Optional[torch.Tensor] = None
+        x: torch.Tensor,
+        alpha: Optional[float] = 0.2,
 ) -> torch.Tensor:
-    ret = torch.nn.functional.leaky_relu(x, alpha)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return torch.nn.functional.leaky_relu(x, alpha)
 
 
 def gelu(
-    x : torch.Tensor,
-    approximate: bool = True,
-    out: Optional[torch.Tensor] = None
+        x: torch.Tensor,
+        approximate: bool = True,
 ) -> torch.Tensor:
     if approximate:
-        ret = (
-            0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x**3)))
+        return (
+                0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
         )
-    else:
-        ret = torch.nn.functional.gelu(x)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return torch.nn.functional.gelu(x)
 
 
 def tanh(x: torch.Tensor, out: Optional[torch.Tensor] = None) -> torch.Tensor:
@@ -55,13 +43,10 @@ def sigmoid(x: torch.Tensor, out: Optional[torch.Tensor] = None) -> torch.Tensor
 
 
 def softmax(
-        x: torch.Tensor, axis: Optional[int] = None, out: Optional[torch.Tensor] = None
+        x: torch.Tensor, axis: Optional[int] = None
 ) -> torch.Tensor:
     exp_x = torch.exp(x)
-    ret = exp_x / torch.sum(exp_x, axis, keepdims=True)
-    if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-    return ret
+    return exp_x / torch.sum(exp_x, axis, keepdims=True)
 
 
 def softplus(x: torch.Tensor, out: Optional[torch.Tensor] = None) -> torch.Tensor:
