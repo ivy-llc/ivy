4dbb4262b998b3a8c4b76c913e00fff301eb5fae
ZoeCD <42352932+ZoeCD@users.noreply.github.com>
2022-07-15 13:31:38 -0500

    Add dtype static method

--- ivy/container/data_type.py
+++ ivy/container/data_type.py
@@ -132,6 +132,27 @@ class ContainerWithDataTypes(ContainerBase):
             out=out,
         )
 
+    @staticmethod
+    def static_dtype(
+        x: ivy.Container,
+        as_native: Optional[bool] = False,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        return ContainerBase.multi_map_in_static_method(
+            "dtype",
+            x,
+            as_native,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
     def dtype(
         self: ivy.Container,
         as_native: Optional[bool] = False,
@@ -141,15 +162,14 @@ class ContainerWithDataTypes(ContainerBase):
         map_sequences: bool = False,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        return self.handle_inplace(
-            self.map(
-                lambda x_, _: ivy.dtype(x_, as_native) if ivy.is_array(x_) else x_,
-                key_chains,
-                to_apply,
-                prune_unapplied,
-                map_sequences,
-            ),
-            out,
+        return self.static_dtype(
+            self,
+            as_native,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
         )
 
     @staticmethod
