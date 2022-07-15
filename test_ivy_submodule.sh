21a0be9128b17bbfd69a1f9319e9cbffb08e775c
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-15 02:10:17 +0000

    delete pwd

--- test_ivy_submodule.sh
+++ test_ivy_submodule.sh
@@ -3,5 +3,4 @@
 mkdir -p .hypothesis
 # shellcheck disable=SC2046
 
-echo $PWD
 docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/"$2"
