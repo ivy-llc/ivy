948ef95ae7cf6a9f6991446d61e9064655c3b023
owaisorakzai <84202122+owaisorakzai@users.noreply.github.com>
2022-07-16 23:23:07 +0500

    Reformating and docstring examples of tanh() in activations.py

--- ivy/array/activations.py
+++ ivy/array/activations.py
@@ -30,3 +30,34 @@ class ArrayWithActivations(abc.ABC):
 
         """
         return ivy.softplus(self._data, out=out)
+
+    def tanh(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
+        """
+        ivy.Array instance method variant of ivy.tanh. This method simply wraps the
+        function, and so the docstring for ivy.tanh also applies to this method
+        with minimal changes.
+
+        Parameters
+        ----------
+        self
+            input array whose elements each represent a hyperbolic angle. Should have a
+            real-valued floating-point data type.
+        out
+            optional output, for writing the result to. It must have a shape that the
+            inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            an array containing the hyperbolic tangent of each element in ``self``. The
+            returned array must have a real-valued floating-point data type determined
+            by :ref:`type-promotion`.
+
+        Examples
+        --------
+        >>> x = ivy.array([0., 1., 2.])
+        >>> y = x.tanh()
+        >>> print(y)
+        ivy.array([0., 0.762, 0.964])
+        """
+        return ivy.tanh(self._data, out=out)
