0dfeed7e70cb3a4ad8d4fc2ff853af8f072c43b0
djl11 <daniel.lenton11@imperial.ac.uk>
2022-07-17 13:08:43 +0200

    bug fixes for the numpy frontend tan unit test.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_np_trigonometric_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_np_trigonometric_functions.py
@@ -7,24 +7,23 @@ import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 
 
+@st.composite
+def _where(draw):
+    _, values = draw(helpers.dtype_and_values(("bool",)))
+    return draw(st.just(values) | st.just(True))
+
+
 # tan
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes),
     dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
-    where=st.sampled_from(
-        (
-            helpers.dtype_and_values(
-                ("bool",), shape=st.shared(helpers.get_shape(), key="shape")
-            ),
-            True,
-        )
-    ),
-    as_variable=st.booleans(),
+    where=_where(),
+    as_variable=helpers.array_bools(),
     with_out=st.booleans(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.tan"
     ),
-    native_array=st.booleans(),
+    native_array=helpers.array_bools(),
 )
 def test_numpy_tan(
     dtype_and_x,
@@ -37,7 +36,14 @@ def test_numpy_tan(
     fw,
 ):
     input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
+    input_dtype = [input_dtype]
+    where_array = isinstance(where, list)
+    if where_array:
+        where = np.asarray(where, dtype=np.bool)
+        input_dtype += ["bool"]
+        as_variable += [False]
+        native_array += [False]
+    values = helpers.test_frontend_function(
         input_dtype,
         as_variable,
         with_out,
@@ -46,11 +52,21 @@ def test_numpy_tan(
         fw,
         "numpy",
         "tan",
-        x=np.asarray(x, dtype=input_dtype),
+        x=np.asarray(x, dtype=input_dtype[0]),
         out=None,
         where=where,
         casting="same_kind",
         order="k",
         dtype=dtype,
         subok=True,
+        test_values=False,
     )
+    # noinspection DuplicatedCode
+    if values is None:
+        return
+    ret, frontend_ret = values
+    ret_flat = [np.where(where, x, np.zeros_like(x)) for x in helpers.flatten(ret)]
+    frontend_ret_flat = [
+        np.where(where, x, np.zeros_like(x)) for x in helpers.flatten(frontend_ret)
+    ]
+    helpers.value_test(ret_flat, frontend_ret_flat)
