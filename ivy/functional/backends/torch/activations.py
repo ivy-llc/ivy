0e67a8fb70632a25bb8aadb40516fef4f73970a3
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-17 20:01:49 +0100

    fix

--- ivy/functional/backends/torch/activations.py
+++ ivy/functional/backends/torch/activations.py
@@ -15,11 +15,17 @@ def relu(x: torch.Tensor) -> torch.Tensor:
     return torch.relu(x)
 
 
+relu.unsupported_dtypes = ("float16",)
+
+
 def leaky_relu(
     x: torch.Tensor,
     alpha: Optional[float] = 0.2,
 ) -> torch.Tensor:
     return torch.nn.functional.leaky_relu(x, alpha)
+
+
+leaky_relu.unsupported_dtypes = ("float16",)
 
 
 def gelu(
@@ -29,7 +35,10 @@ def gelu(
     if approximate:
         return 0.5 * x * (1 + torch.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
     return torch.nn.functional.gelu(x)
-   
+ 
+ 
+gelu.unsupported_dtypes = ("float16",)
+
 
 def tanh(x: torch.Tensor, *, out: Optional[torch.Tensor] = None) -> torch.Tensor:
     return torch.tanh(x, out=out)
@@ -39,6 +48,9 @@ def sigmoid(x: torch.Tensor, *, out: Optional[torch.Tensor] = None) -> torch.Ten
     return torch.sigmoid(x, out=out)
 
 
+sigmoid.unsupported_dtypes = ("float16",)
+
+
 def softmax(
     x: torch.Tensor, axis: Optional[int] = None
 ) -> torch.Tensor:
@@ -46,5 +58,11 @@ def softmax(
     return exp_x / torch.sum(exp_x, axis, keepdims=True)
 
 
+softmax.unsupported_dtypes = ("float16",)
+
+
 def softplus(x: torch.Tensor) -> torch.Tensor:
     return torch.nn.functional.softplus(x)
+    
+    
+softplus.unsupported_dtypes = ("float16",)
