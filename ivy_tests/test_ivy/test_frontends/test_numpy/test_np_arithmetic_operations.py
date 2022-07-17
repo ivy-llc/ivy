72a00559595b937836b69b3aeaaec2d3d6d8b8dd
djl11 <daniel.lenton11@imperial.ac.uk>
2022-07-17 13:21:04 +0200

    created a helper file for numpy frontend tests, and refactored some duplicate code in the existing tests to this helpers file.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_np_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_np_arithmetic_operations.py
@@ -5,19 +5,14 @@ from hypothesis import given, strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
-
-
-@st.composite
-def _where(draw):
-    _, values = draw(helpers.dtype_and_values(("bool",)))
-    return draw(st.just(values) | st.just(True))
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 
 
 # add
 @given(
     dtype_and_x=helpers.dtype_and_values(ivy_np.valid_float_dtypes, 2),
     dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
-    where=_where(),
+    where=np_frontend_helpers.where(),
     as_variable=helpers.array_bools(),
     with_out=st.booleans(),
     num_positional_args=helpers.num_positional_args(
@@ -36,13 +31,13 @@ def test_numpy_add(
     fw,
 ):
     input_dtype, x = dtype_and_x
-    where_array = isinstance(where, list)
-    if where_array:
-        where = np.asarray(where, dtype=np.bool)
-        input_dtype += ["bool"]
-        as_variable += [False]
-        native_array += [False]
-    values = helpers.test_frontend_function(
+    where = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+    np_frontend_helpers.test_frontend_function(
         input_dtype,
         as_variable,
         with_out,
@@ -61,11 +56,3 @@ def test_numpy_add(
         subok=True,
         test_values=False,
     )
-    if values is None:
-        return
-    ret, frontend_ret = values
-    ret_flat = [np.where(where, x, np.zeros_like(x)) for x in helpers.flatten(ret)]
-    frontend_ret_flat = [
-        np.where(where, x, np.zeros_like(x)) for x in helpers.flatten(frontend_ret)
-    ]
-    helpers.value_test(ret_flat, frontend_ret_flat)
