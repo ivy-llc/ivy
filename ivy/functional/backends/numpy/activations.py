c554bcbbb5b65e15fc42aff3f799efd8dadc26dd
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-17 18:18:46 +0100

    Update activations.py

--- ivy/functional/backends/numpy/activations.py
+++ ivy/functional/backends/numpy/activations.py
@@ -12,8 +12,8 @@ except (ImportError, ModuleNotFoundError):
     erf = None
 
 
-def relu(x: np.ndarray) -> np.ndarray:
-    return np.asarray(np.maximum(x, 0), x.dtype)
+def relu(x: np.ndarray, , out: Optional[np.ndarray] = None) -> np.ndarray:
+    return np.maximum(x, 0, out = out, dtype = x.dtype)
 
 
 def leaky_relu(
