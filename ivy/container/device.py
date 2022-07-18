61557154ea401039e5d74ce4edfdf5a2d1f4168e
Jamie Line <jamietline@gmail.com>
2022-07-18 23:51:52 +0100

    Added Docstring and example to ivy/container/device.py

--- ivy/container/device.py
+++ ivy/container/device.py
@@ -29,6 +29,65 @@ class ContainerWithDevice(ContainerBase):
         axis: int = 0,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.dev_unify_array. This method
+        simply wraps the function, and so the docstring for ivy.dev_unify_array
+        also applies to this method with minimal changes.
+
+        Parameters
+        ----------
+        xs
+            The list of arrays to unify onto the specified device.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is None.
+        to_apply
+            If True, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is True.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is False.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples). Default is False.
+        device
+            The device to unify the arrays to.
+        mode
+            The mode by which to unify, must be one of [ concat | mean | sum ]
+        axis
+            The axis along which to concatenate the array, if concat mode is set.
+            Default is 0.
+        out
+            optional output container, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            a container containing the contents of each member of xs, joined as
+            specified, each stored on a singular device.
+
+        Examples
+        --------
+
+        >>> x_cpu = [0., 0., 0.,]
+        >>> x_gpu = [1., 1., 1.,]
+        >>> x = {"cpu": ivy.asarray(x_cpu),\
+                 "gpu": ivy.asarray(x_gpu, device="gpu:0")}
+        >>> y_cpu = [2., 2., 2.,]
+        >>> y_gpu = [3., 3., 3.,]
+        >>> y = {"cpu": ivy.asarray(y_cpu),\
+                 "gpu": ivy.asarray(y_gpu, device="gpu:0")}
+        >>> z = ivy.Container(x=ivy.DevDistItem(x), y=ivy.DevDistItem(y))
+        >>> z_unified = ivy.dev_unify_array(\
+                z, device="cpu", mode="concat", axis=0)\
+            )
+        >>> print(z_unified)
+        {
+            x: ivy.array([0., 0., 0., 1., 1., 1.,])
+            y: ivy.array([2., 2., 2., 3., 3., 3.,])
+        }
+
+        """
+
         return ContainerBase.multi_map_in_static_method(
             "dev_unify_array",
             xs,
