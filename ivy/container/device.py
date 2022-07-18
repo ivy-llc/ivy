4d7731997d2c6355081c40db648a8791e9d2e29f
Unevilicorn <unevilicorn@gmail.com>
2022-07-18 14:30:57 +0100

    added container instance and static method for dev_clone_array

--- ivy/container/device.py
+++ ivy/container/device.py
@@ -1,5 +1,5 @@
 # local
-from typing import Union, Optional, Any
+from typing import Union, Optional, Any, Iterable
 
 import ivy
 from ivy.container.base import ContainerBase
@@ -72,3 +72,34 @@ class ContainerWithDevice(ContainerBase):
 
         """
         return self.to_device_static(self, device, stream=stream, out=out)
+
+    @staticmethod
+    def dev_clone_array_static(
+        container: ivy.Container, devices: Iterable[ivy.Device]
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.device.dev_clone_array. This method
+        simply wraps the function, and so the docstring for ivy.device.dev_clone_array
+        also applies to this method with minimal changes.
+
+        Examples
+        --------
+
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "dev_clone_array", container, devices
+        )
+
+    def dev_clone_array(
+        self: ivy.Container, devices: Iterable[ivy.Device]
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.device.dev_clone_array. This method
+        simply wraps the function, and so the docstring for ivy.device.dev_clone_array
+        also applies to this method with minimal changes.
+
+        Examples
+        --------
+
+        """
+        return self.dev_clone_array_static(self, devices)
