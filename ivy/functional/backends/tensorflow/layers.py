0fee4a71bc14120fa66e7edb6f0a9b25d2d79a92
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2022-07-18 21:17:46 +0600

    Adding droput x in tensor and rate in float

--- ivy/functional/backends/tensorflow/layers.py
+++ ivy/functional/backends/tensorflow/layers.py
@@ -102,3 +102,5 @@ def conv3d_transpose(
     return tf.nn.conv3d_transpose(
         x, filters, output_shape, strides, padding, data_format, dilations
     )
+def dropout(x: Tensor, rate:float, noise_shape=None, seed=None, name=None):
+    return tf.nn.dropout(x, rate, noise_shape, seed, name)
