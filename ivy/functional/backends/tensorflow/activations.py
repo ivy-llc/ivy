aa64c65b632eeda2d337f142d560f0f4bc5de24d
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-20 00:03:50 +0100

    Fixing up activations submodule
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy/functional/backends/tensorflow/activations.py
+++ ivy/functional/backends/tensorflow/activations.py
@@ -13,58 +13,44 @@ from tensorflow.python.types.core import Tensor
 
 def relu(
     x: Tensor,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.relu(x)
-
+   
 
 def leaky_relu(
-    x: Tensor, 
+    x: Tensor,
     alpha: Optional[float] = 0.2,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.leaky_relu(x, alpha)
 
 
 def gelu(
-    x: Tensor, 
+    x: Tensor,
     approximate: Optional[bool] = True,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.gelu(x, approximate)
 
 
 def sigmoid(
     x: Tensor,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.sigmoid(x)
 
 
 def tanh(
     x: Tensor,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.tanh(x)
 
 
 def softmax(
-    x: Tensor, 
+    x: Tensor,
     axis: Optional[int] = None,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.exp(x) / tf.reduce_sum(tf.exp(x), axis, keepdims=True)
 
 
 def softplus(
     x: Tensor,
-    *,
-    out: Optional[Tensor] = None
 ) -> Tensor:
     return tf.nn.softplus(x)
