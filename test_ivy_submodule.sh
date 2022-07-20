45276818b6ae1a277d0cb6c52c0fdbea0dcd8a0f
Jiahan Xie <88367305+jiahanxie353@users.noreply.github.com>
2022-07-20 19:23:58 +0000

    delete test submodules shell script

--- test_ivy_submodule.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-mkdir -p .hypothesis
-# shellcheck disable=SC2046
-
-docker run --rm -v IVY_BACKEND="$1"  -v $(pwd):/ivy -v $(pwd)/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/test_ivy/test_functional/test_core/"$2"
