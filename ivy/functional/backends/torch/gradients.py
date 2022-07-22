73190389a99867b411186dc4fb220990d169a10d
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-07-22 13:06:59 +0000

    Torch error

--- ivy/functional/backends/torch/gradients.py
+++ ivy/functional/backends/torch/gradients.py
@@ -1,10 +1,13 @@
 """Collection of PyTorch gradient functions, wrapped to fit Ivy syntax and signature."""
 
 # global
-import ivy
 import torch
 import warnings
 from typing import Optional
+
+# local
+import ivy
+from ivy.functional.ivy.gradients import to_container_and_back
 
 
 def variable(x):
@@ -22,6 +25,7 @@ def variable_data(x):
 
 
 # noinspection PyShadowingNames
+@to_container_and_back
 def execute_with_gradients(func, xs, retain_grads=False):
     func_ret = func(xs)
     if isinstance(func_ret, tuple):
@@ -30,6 +34,7 @@ def execute_with_gradients(func, xs, retain_grads=False):
     else:
         y = func_ret
         rest = tuple()
+    print("func_ret", func_ret)
     y = ivy.to_native(y)
     x_grads_flat = list(
         torch.autograd.grad(
@@ -40,6 +45,7 @@ def execute_with_gradients(func, xs, retain_grads=False):
         )
     )
     grads = xs.from_flat_list(x_grads_flat)
+    # grads = grads.to_ivy()
     y = ivy.to_ivy(y)
     if not retain_grads:
         y = ivy.stop_gradient(y)
