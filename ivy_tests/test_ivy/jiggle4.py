223a0da37ab9c67875a216016357d9e0a034f11b
djl11 <daniel.lenton11@imperial.ac.uk>
2022-07-22 20:43:41 +0200

    update.

--- ivy_tests/test_ivy/jiggle4.py
+++ ivy_tests/test_ivy/jiggle4.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Union
 from hypothesis import strategies as st
 from hypothesis import given
@@ -8,8 +9,23 @@ def bool_val_flags(cl_arg: Union[bool, None]):
         return st.booleans().filter(lambda x: x == cl_arg)
     return st.booleans()
 
+def _handle_var(test_fn):
+    def new_fn(data, get_command_line_flags, *args, **kwargs):
+
+        # if test_fn has keyword argument as_variable then:
+
+        #---------  should all be indented in an if statement
+        as_variable = data.draw(bool_val_flags(get_command_line_flags["as-variable"]))
+        kwargs["as_variable"] = as_variable
+        #----------
+
+        # etc.
+
+        return test_fn(*args, **kwargs)
+    return new_fn
+
 
 @given(data=st.data())
-def test_lol(get_command_line_flags, data):
-    as_variable = data.draw(bool_val_flags(get_command_line_flags["as-variable"]))
+@_handle_var
+def test_lol(as_variable):
     assert as_variable == True
