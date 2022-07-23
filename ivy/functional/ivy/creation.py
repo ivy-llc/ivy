e733921df37a3afb0bc45ce736a103fcf5a47e15
JG <67888047+juliagsy@users.noreply.github.com>
2022-07-23 17:05:04 +0800

    final small fix

--- ivy/functional/ivy/creation.py
+++ ivy/functional/ivy/creation.py
@@ -303,43 +303,47 @@ def full_like(
     Functional Examples
     -------------------
     With int datatype:
-    >>> import ivy
+    
     >>> x = ivy.array([1, 2, 3, 4, 5, 6])
     >>> fill_value = 1
     >>> y = ivy.full_like(x, fill_value)
     >>> print(y)
     ivy.array([1, 1, 1, 1, 1, 1])
+    
     >>> fill_value = 0.000123
     >>> x = ivy.ones(5)
-    >>> y = ivy.full_like(x , fill_value)
+    >>> y = ivy.full_like(x, fill_value)
     >>> print(y)
     ivy.array([0.000123, 0.000123, 0.000123, 0.000123, 0.000123])
 
     With float datatype:
-    >>> import ivy
-    >>> x = ivy.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]) # for filling values with floating datatype the array is required to be of same dtype (float in this case)
+    
+    >>> x = ivy.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
     >>> fill_value = 0.000123
     >>> y = ivy.full_like(x, fill_value)
     >>> print(y)
     ivy.array([0.000123, 0.000123, 0.000123, 0.000123, 0.000123, 0.000123])
 
     With ivy.NativeArray input:
+    
     >>> x = ivy.native_array([3.0, 8.0])
     >>> fill_value = 0.000123
     >>> y = ivy.full_like(x,fill_value)
     >>> print(y)
     ivy.array([0.000123, 0.000123])
-    >>> x2 = ivy.native_array([[3., 8., 2.],
-                                [2., 8., 3.]])
-    >>> y = ivy.full_like(x2 , fill_value)
+    
+    >>> x = ivy.native_array([[3., 8., 2.], [2., 8., 3.]])
+    >>> y = ivy.full_like(x, fill_value)
     >>> print(y)
     ivy.array([[0.000123, 0.000123, 0.000123],
-    [0.000123, 0.000123, 0.000123]])
+           [0.000123, 0.000123, 0.000123]])
 
-    With ivy.Container input
-    >>> x = ivy.Container(a = ivy.array([1.2,2.2324,3.234]) ,b = ivy.array([4.123,5.23,6.23]))
+    With ivy.Container input:
+    
+    >>> x = ivy.Container(a=ivy.array([1.2,2.2324,3.234]), \
+                           b=ivy.array([4.123,5.23,6.23]))
     >>> fill_value = 15.0
-    >>> y = ivy.full_like(x ,fill_value)
+    >>> y = ivy.full_like(x, fill_value)
     >>> print(y)
     {
         a: ivy.array([15., 15., 15.]),
@@ -350,6 +354,7 @@ def full_like(
     ------------------------
 
     With ivy.Array input:
+    
     >>> x = ivy.array([1, 2, 3, 4, 5, 6])
     >>> fill_value = 1
     >>> y = x.full_like(fill_value)
@@ -357,9 +362,12 @@ def full_like(
     ivy.array([1, 1, 1, 1, 1, 1])
 
     With ivy.Container input:
-    >>> x = ivy.Container(a = ivy.array([1,2,3]) ,b = ivy.array([4,5,6]))
+    
+    >>> x = ivy.Container(a=ivy.array([1,2,3]), \
+                           b=ivy.array([4,5,6]))
     >>> fill_value = 10
     >>> y = x.full_like(fill_value)
+    >>> print(y)
     {
         a: ivy.array([10, 10, 10]),
         b: ivy.array([10, 10, 10])
