20905016e6e702f85d01e3b81887f5f52f905b65
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-23 18:51:16 +0100

    add container reshape docstring

--- ivy/container/manipulation.py
+++ ivy/container/manipulation.py
@@ -116,9 +116,11 @@ class ContainerWithManipulation(ContainerBase):
             out=out,
         )
 
-    def reshape(
-        self: ivy.Container,
-        shape: Union[ivy.Shape, ivy.NativeShape, ivy.Container],
+    @staticmethod
+    def static_reshape(
+        x: Union[ivy.Array, ivy.NativeArray, ivy.Container],
+        shape: Union[int, Tuple[int,int], List[int]],
+        copy: Optional[bool] = None,
         key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
         to_apply: bool = True,
         prune_unapplied: bool = False,
@@ -126,14 +128,75 @@ class ContainerWithManipulation(ContainerBase):
         *,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
-        return ContainerBase.handle_inplace(
-            self.map(
-                lambda x_, _: ivy.reshape(x_, shape=shape) if ivy.is_array(x_) else x_,
-                key_chains,
-                to_apply,
-                prune_unapplied,
-                map_sequences,
-            ),
+        """
+        ivy.Container static method variant of ivy.reshape. This method simply wraps the
+        function, and so the docstring for ivy.reshape also applies to this method
+        with minimal changes.
+
+        Parameters
+        ----------
+        x
+            input container.
+
+        shape
+            The new shape should be compatible with the original shape. One shape dimension
+            can be -1. In this case, the value is inferred from the length of the array and
+            remaining dimensions.
+        copy
+             boolean indicating whether or not to copy the input array.
+             If True, the function must always copy.
+             If False, the function must never copy and must
+             raise a ValueError in case a copy would be necessary.
+             If None, the function must reuse existing memory buffer if possible
+             and copy otherwise. Default: None.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is None.
+        to_apply
+            If True, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is True.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is False.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples). Default is False.
+        out
+            optional output container, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+        optional output container, for writing the result to. It must have a shape that the
+        inputs broadcast to.
+
+        Examples
+        --------
+        With one :code:`ivy.Container` input:
+
+        >>> x = ivy.Container(a=ivy.array([0, 1, 2, 3, 4, 5]), \
+                              b=ivy.array([0, 1, 2, 3, 4, 5]))
+        >>> y = ivy.Container.static_reshape(x, (3,2))
+        >>> print(y)
+        {
+            a: ivy.array([[0, 1],
+                          [2, 3],
+                          [4, 5]]),
+            b: ivy.array([[0, 1],
+                          [2, 3],
+                          [4, 5]])
+        }
+
+
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "reshape",
+            x,
+            shape,
+            copy,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
             out=out,
         )
 
