ed8665754d123206f52e6d6f31e857d3e32032fa
ABDELKARIM ELHALLOUMI <47593939+kirodev@users.noreply.github.com>
2022-07-23 19:48:17 +0100

    add less array docstring

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -941,6 +941,15 @@ class ArrayWithElementwise(abc.ABC):
         ret
             an array containing the element-wise results. The returned array
             must have a data type of ``bool``.
+            
+        Examples
+        --------
+        >>> x1 = ivy.array([2., 5., 15.])
+        >>> x2 = ivy.array([3., 2., 4.])
+        >>> y = x1.less(x2)
+        >>> print(y)
+        ivy.array([ True, False, False])
+
         """
         return ivy.less(self._data, x2, out=out)
 
