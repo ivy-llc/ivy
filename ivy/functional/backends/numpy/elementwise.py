fc10621d1e552c879af3712b6db2b20d434cb8b0
Muhamad Nicky Salim <muhamadnickysalim@gmail.com>
2022-07-24 01:42:41 +0700

    Update functional/backends/.../elementwise.py

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -422,7 +422,10 @@ logaddexp.support_native_out = True
 
 @_handle_0_dim_output
 def logical_and(
-    x1: np.ndarray, x2: np.ndarray, *, out: Optional[np.ndarray] = None
+    x1: np.ndarray,
+    x2: np.ndarray,
+    *,
+    out: Optional[np.ndarray] = None
 ) -> np.ndarray:
     return np.logical_and(x1, x2, out=out)
 
