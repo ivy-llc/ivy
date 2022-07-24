39bd7ea8f4b1ab7c1d80507f66b988b95561eb76
PrachetShah <prachetshah25@gmail.com>
2022-07-24 19:05:38 +0530

    invalid_dtype reformated

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -1055,8 +1055,9 @@ def function_unsupported_dtypes(fn: Callable) -> Tuple:
 
 
 def invalid_dtype(dtype_in: Union[ivy.Dtype, str, None]) -> bool:
-    """Determines whether the provided data type is not support by the current
-    framework.
+    """
+    Determines whether the provided data type is not support by
+    the current framework.
 
     Parameters
     ----------
@@ -1067,7 +1068,26 @@ def invalid_dtype(dtype_in: Union[ivy.Dtype, str, None]) -> bool:
     -------
     ret
         Boolean, whether the data-type string is un-supported.
+    
+    Examples
+    --------
+    >>> ivy.invalid_dtype(dtype_in = None)
+    False
 
+    >>> ivy.invalid_dtype(dtype_in = 'uint64')
+    True
+
+    >>> ivy.invalid_dtype(dtype_in = ivy.float64)
+    True
+
+    >>> ivy.invalid_dtype(dtype_in = 'float32')
+    True
+
+    >>> ivy.invalid_dtype(dtype_in = ivy.native_int16)
+    True
+
+    >>> ivy.invalid_dtype(dtype_in = 'native_int16')
+    True
     """
     if dtype_in is None:
         return False
