6466db7d0acb3cc872d1f28d4bf8684b20cca908
Alibek Kaliyev <abekek01@gmail.com>
2022-07-25 00:07:39 +0000

    formatting docstrings added to isnan()

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -2253,6 +2253,12 @@ def isnan(
     """Tests each element ``x_i`` of the input array ``x`` to determine whether the
     element is ``NaN``.
 
+    **Special cases**
+
+    - If ``x_i`` is ``NaN``, the result is ``True``
+    - If ``x_i`` is ``+infinity``, the result is ``False``
+    - If ``x_i`` is ``-infinity``, the result is ``False``
+
     Parameters
     ----------
     x
@@ -2268,6 +2274,13 @@ def isnan(
         ``NaN`` and ``False`` otherwise. The returned array should have a data type of
         ``bool``.
 
+    This function conforms to the `Array API Standard
+    <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of the
+    `docstring <https://data-apis.org/array-api/latest/API_specification/generated/signatures.elementwise_functions.isnan.html>`_ # noqa
+    in the standard. The descriptions above assume an array input for simplicity, but
+    the method also accepts :code:`ivy.Container` instances in place of
+    :code:`ivy.Array` or :code:`ivy.NativeArray` instances, as shown in the type hints
+    and also the examples below.
     """
     return ivy.current_backend(x).isnan(x, out=out)
 
