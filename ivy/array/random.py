d24849254171716b83f8e833ef23cf776f295275
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-25 19:25:30 +0800

    add array instance method - random_uniform

--- ivy/array/random.py
+++ ivy/array/random.py
@@ -1,8 +1,28 @@
 # global
 import abc
+from typing import Optional, Union
+
+# local
+import ivy
 
 # ToDo: implement all methods here as public instance methods
 
 
 class ArrayWithRandom(abc.ABC):
-    pass
+    def random_uniform(
+        self: ivy.Array,
+        high: Union[ivy.Array, ivy.NativeArray] = 1.0,
+        shape: Optional[Union[ivy.Shape, ivy.NativeShape]] = None,
+        *,
+        device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
+        dtype: Optional[Union[ivy.Dtype, ivy.NativeDtype]] = None,
+        out: Optional[ivy.Array] = None,
+    ) -> ivy.Array:
+        return self.static_random_uniform(
+            self,
+            high,
+            shape,
+            device=device,
+            dtype=dtype,
+            out=out,
+        )
