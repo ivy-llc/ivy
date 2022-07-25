e8d2c4ad4cf84aa9db9ebaadeed9886e20a8ae45
1Doomdie1 <horia.todoran@yahoo.com>
2022-07-25 17:00:28 +0300

    Some small code optimizations

--- automation_tools/pr_automation/pr_action.py
+++ automation_tools/pr_automation/pr_action.py
@@ -15,18 +15,14 @@ def main():
     intern_points_of_contact = import_file("assets/intern_poc.json")
     interns = import_file("assets/intern_assign_rate.json")
 
-    # If a volunteer has opened a PR then assign it's coresponding Ivy intern
-    for ivy_intern, assigned_volunteers in interns_assigned_volunteers.items():
+    for (ivy_intern, assigned_volunteers), (ivy_member, assigned_interns) in zip(
+        interns_assigned_volunteers.items(), intern_points_of_contact.items()
+    ):
         if pr.author() in assigned_volunteers:
             pr.assign_intern(ivy_intern)
-            print(f"[+] {ivy_intern} was assigned to PR {pr.pr_number()}")
             sys.exit(0)
-
-    # If an intern has opened a PR then assign it's coresponding Ivy Team Member
-    for ivy_member, assigned_intern in intern_points_of_contact.items():
-        if pr.author() in assigned_intern:
+        elif pr.author() in assigned_interns:
             pr.assign_intern(ivy_member)
-            print(f"[+] {ivy_member} was assigned to PR {pr.pr_number()}")
             sys.exit(0)
 
     pr.assign_random_intern(interns)
