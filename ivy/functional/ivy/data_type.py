3c51496e93c5e499a4de6b0e45c58d5e389dc54f
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-07-25 19:50:45 +0530

    Reformating broadcast_arrays method (#2261)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -105,14 +105,68 @@ def broadcast_arrays(*arrays: Union[ivy.Array, ivy.NativeArray]) -> List[ivy.Arr
     Parameters
     ----------
     arrays
-        an arbitrary number of to-be broadcasted arrays.
+        an arbitrary number of arrays to-be broadcasted.
+        Each array must have the same shape. Each array must have the same dtype as its
+        corresponding input array.
 
     Returns
     -------
     ret
-        Each array must have the same shape. Each array must have the same dtype as its
-        corresponding input array.
+        A list containing broadcasted arrays of type `ivy.Array`
 
+    Examples
+    --------
+    With :code:`ivy.Array` input:
+
+    >>> x1 = ivy.array([1, 2, 3])
+    >>> x2 = ivy.array([4, 5, 6])
+    >>> y = ivy.broadcast_arrays(x1, x2)
+    >>> print(y)
+    [ivy.array([1, 2, 3]), ivy.array([4, 5, 6])]
+
+    With :code:`ivy.NativeArray` inputs:
+
+    >>> x1 = ivy.native_array([0.3, 4.3])
+    >>> x2 = ivy.native_array([3.1, 5])
+    >>> x3 = ivy.native_array([2, 0])
+    >>> y = ivy.broadcast_arrays(x1, x2, x3)
+    [ivy.array([0.3, 4.3]), ivy.array([3.1, 5.]), ivy.array([2, 0])]
+
+    With mixed :code:`ivy.Array` and :code:`ivy.NativeArray` inputs:
+
+    >>> x1 = ivy.array([1, 2])
+    >>> x2 = ivy.native_array([0.3, 4.3])
+    >>> y = ivy.broadcast_arrays(x1, x2)
+    >>> print(y)
+    [ivy.array([1, 2]), ivy.array([0.3, 4.3])]
+
+    With :code:`ivy.Container` inputs:
+
+    >>> x1 = ivy.Container(a=ivy.array([3, 1]), b=ivy.zeros(2))
+    >>> x2 = ivy.Container(a=ivy.array([4, 5]), b=ivy.array([2, -1]))
+    >>> y = ivy.broadcast_arrays(x1, x2)
+    >>> print(y)
+    [{
+        a: ivy.array([3, 1]),
+        b: ivy.array([0., 0.])
+    }, {
+        a: ivy.array([4, 5]),
+        b: ivy.array([2, -1])
+    }]
+
+    With mixed :code:`ivy.Array` and :code:`ivy.Container` inputs:
+
+    >>> x1 = ivy.zeros(2)
+    >>> x2 = ivy.Container(a=ivy.array([4, 5]), b=ivy.array([2, -1]))
+    >>> y = ivy.broadcast_arrays(x1, x2)
+    >>> print(y)
+    [{
+        a: ivy.array([0., 0.]),
+        b: ivy.array([0., 0.])
+    }, {
+        a: ivy.array([4, 5]),
+        b: ivy.array([2, -1])
+    }]
     """
     return current_backend(arrays[0]).broadcast_arrays(*arrays)
 
