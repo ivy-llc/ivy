9d74e1e72353a42208f6461a6a3937231bd0f13f
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-07-25 22:20:20 +0530

    ArgMax  (Add RawOps Operations for TensorFlow)

---
+++ ArgMax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def argmax(vector):
+    index, value = 0, vector[0]
+    for i,v in enumerate(vector):
+        if v > value:
+            index, value = i,v
+    return index
+vector = list(input())
+result = argmax(vector)
+print('arg max of %s: %d' % (vector, result))
+
+
+# In[ ]:
+
+
+
+
