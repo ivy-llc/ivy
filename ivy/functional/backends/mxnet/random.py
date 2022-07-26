6fdfebfed52a21dcdd19973cee2264cd1cc57b1b
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-26 14:39:43 +0800

    update mxnet

--- ivy/functional/backends/mxnet/random.py
+++ ivy/functional/backends/mxnet/random.py
@@ -6,7 +6,10 @@ from typing import Optional, Union, Sequence
 
 # local
 import ivy
-from ivy.functional.ivy.random import _check_bounds_and_get_shape
+from ivy.functional.ivy.random import (
+    _check_bounds_and_get_shape,
+    _randint_check_dtype_and_bound,
+)
 from ivy.functional.backends.mxnet import _1_dim_array_to_flat_array
 
 
@@ -81,21 +84,23 @@ def multinomial(
 def randint(
     low: Union[float, mx.nd.NDArray],
     high: Union[float, mx.nd.NDArray],
-    shape: Union[ivy.NativeShape, Sequence[int]],
+    shape: Optional[Union[ivy.NativeShape, Sequence[int]]] = None,
     *,
     device: mx.context.Context,
     dtype: type,
     out: Optional[mx.nd.NDArray],
 ) -> mx.nd.NDArray:
+    _randint_check_dtype_and_bound(low, high, dtype)
+    shape = _check_bounds_and_get_shape(low, high, shape)
     if isinstance(low, mx.nd.NDArray):
         low = int(low.asscalar())
     if isinstance(high, mx.nd.NDArray):
         high = int(high.asscalar())
-    if len(shape) == 0:
+    if shape == ():
         return _1_dim_array_to_flat_array(
-            mx.nd.random.randint(low, high, (1,), ctx=device)
+            mx.nd.random.randint(low, high, (1,), ctx=device, dtype=dtype)
         )
-    return mx.nd.random.randint(low, high, shape, ctx=device)
+    return mx.nd.random.randint(low, high, shape, ctx=device, dtype=dtype)
 
 
 def seed(seed_value: int = 0) -> None:
