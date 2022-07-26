77d21555b1453281ffea7aa65acf2baf4a50cde9
Unevilicorn <unevilicorn@gmail.com>
2022-07-26 16:07:20 +0100

    array instance method for searching

--- ivy/array/searching.py
+++ ivy/array/searching.py
@@ -1,8 +1,26 @@
 # global
 import abc
+from typing import Optional
+
+# local
+import ivy
+
 
 # ToDo: implement all methods here as public instance methods
 
 
 class ArrayWithSearching(abc.ABC):
-    pass
+    def argmax(
+        self: ivy.Array,
+        axis: Optional[int] = None,
+        keepdims: Optional[bool] = False,
+        *,
+        out: Optional[ivy.Array] = None
+    ) -> int:
+        """
+        ivy.Array instance method variant of ivy.argmax. This method simply
+        wraps the function, and so the docstring for ivy.argmax also applies
+        to this method with minimal changes.
+
+        """
+        return ivy.argmax(self, axis=axis, keepdims=keepdims, out=out)
