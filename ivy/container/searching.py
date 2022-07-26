c80f55754f5b451a590a7e2aeb1ed1b06771bee3
Unevilicorn <unevilicorn@gmail.com>
2022-07-26 16:07:44 +0100

    container instance and static method for argmax

--- ivy/container/searching.py
+++ ivy/container/searching.py
@@ -1,9 +1,45 @@
+# global
+from typing import Optional
+
 # local
+import ivy
 from ivy.container.base import ContainerBase
+
 
 # ToDo: implement all methods here as public instance methods
 
 
 # noinspection PyMissingConstructor
 class ContainerWithSearching(ContainerBase):
-    pass
+    @staticmethod
+    def static_argmax(
+        x: ivy.Container,
+        axis: Optional[int] = None,
+        keepdims: Optional[bool] = False,
+        *,
+        out: Optional[ivy.Array] = None
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.argmax. This method simply
+        wraps the function, and so the docstring for ivy.argmax also applies
+        to this method with minimal changes.
+
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "argmax", x, axis=axis, keepdims=keepdims, out=out
+        )
+
+    def argmax(
+        self: ivy.Container,
+        axis: Optional[int] = None,
+        keepdims: Optional[bool] = False,
+        *,
+        out: Optional[ivy.Array] = None
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.argmax. This method simply
+        wraps the function, and so the docstring for ivy.argmax also applies
+        to this method with minimal changes.
+
+        """
+        return self.static_argmax(self, axis=axis, keepdims=keepdims, out=out)
