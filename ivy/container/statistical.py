2c2fd3ca6d56888cdb402d99c2424d61931157bd
Luiz Matheus <luizmatheusga@gmail.com>
2022-07-25 23:56:35 -0300

    feat: add docstring to min in container

--- ivy/container/statistical.py
+++ ivy/container/statistical.py
@@ -12,15 +12,57 @@ from ivy.container.base import ContainerBase
 class ContainerWithStatistical(ContainerBase):
     def min(
         self: ivy.Container,
-        axis: Union[int, Tuple[int]] = None,
-        keepdims: bool = False,
+        axis: Optional[Union[int, Tuple[int]]] = None,
+        keepdims: Optional[bool] = False,
         key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
-        to_apply: bool = True,
-        prune_unapplied: bool = False,
-        map_sequences: bool = False,
+        to_apply: Optional[bool] = True,
+        prune_unapplied: Optional[bool] = False,
+        map_sequences: Optional[bool] = False,
         *,
         out: Optional[ivy.Container] = None,
     ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.min. This method simply wraps the
+        function, and so the docstring for ivy.abs also applies to this method
+        with minimal changes.
+        Parameters
+        ----------
+        x
+            input container. Should have a numeric data type.
+        keepdims
+            optional boolean, if True, the reduced axes (dimensions) must be included in the
+            result as singleton dimensions, and, accordingly, the result must be compatible
+            with the input array (see Broadcasting). Otherwise, if False, the reduced axes
+            (dimensions) must not be included in the result. Default: False.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is None.
+        to_apply
+            If True, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is True.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is False.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples). Default is False.
+        out
+            optional output container, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+        Returns
+        -------
+        ret
+            a container containing the absolute value of each element in ``x``. The
+            returned container must have the same data type as ``x``.
+        Examples
+        --------
+        >>> x = ivy.Container(a=ivy.array([0., 2.6, -3.5]),\
+                            b=ivy.array([4.5, -5.3, -0, -2.3]))
+        >>> y = ivy.Container.min(x)
+        >>> print(y)
+        {
+            a: ivy.array([0.]),
+            b: ivy.array([-5.3])
+        }
+        """
         return self.handle_inplace(
             self.map(
                 lambda x_, _: ivy.min(x_, axis, keepdims) if ivy.is_array(x_) else x_,
