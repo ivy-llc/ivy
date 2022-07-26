eb652db09d85a312939140d265a0873a57f4b9e4
Ajay sai reddy  D <ajayreddy6601@gmail.com>
2022-07-26 18:23:18 +0530

    reformatted linear algebra cross function modifying inputs and output

--- ivy/functional/backends/mxnet/linear_algebra.py
+++ ivy/functional/backends/mxnet/linear_algebra.py
@@ -25,7 +25,11 @@ def cholesky(x: mx.nd.NDArray, upper: bool = False) -> mx.nd.NDArray:
         )
 
 
-def cross(x1: mx.nd.NDArray, x2: mx.nd.NDArray, axis: int = -1) -> mx.nd.NDArray:
+def cross(
+        x1: Union[float,mx.nd.NDArray],
+        x2: Union[float,mx.nd.NDArray],
+        axis: int = -1
+    ) -> mx.nd.NDArray:
     return mx.np.cross(a=x1, b=x2, axis=axis)
 
 
