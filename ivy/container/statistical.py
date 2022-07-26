ffa327d7ebc289e32ac24d00ebe9dace03c205e2
Luiz Matheus <luizmatheusga@gmail.com>
2022-07-26 00:16:08 -0300

    feat: updated docstring in statistical at container

--- ivy/container/statistical.py
+++ ivy/container/statistical.py
@@ -25,15 +25,17 @@ class ContainerWithStatistical(ContainerBase):
         ivy.Container static method variant of ivy.min. This method simply wraps the
         function, and so the docstring for ivy.abs also applies to this method
         with minimal changes.
+
         Parameters
         ----------
         x
             input container. Should have a numeric data type.
         keepdims
-            optional boolean, if True, the reduced axes (dimensions) must be included in the
-            result as singleton dimensions, and, accordingly, the result must be compatible
-            with the input array (see Broadcasting). Otherwise, if False, the reduced axes
-            (dimensions) must not be included in the result. Default: False.
+            optional boolean, if True, the reduced axes (dimensions) must be included
+            in the result as singleton dimensions, and accordingly,
+            the result must be compatible with the input array (see Broadcasting).
+            Otherwise, if False, the reduced axes (dimensions) must not be included
+            in the result. Default: False.
         key_chains
             The key-chains to apply or not apply the method to. Default is None.
         to_apply
@@ -47,20 +49,22 @@ class ContainerWithStatistical(ContainerBase):
         out
             optional output container, for writing the result to. It must have a shape
             that the inputs broadcast to.
+
         Returns
         -------
         ret
             a container containing the absolute value of each element in ``x``. The
             returned container must have the same data type as ``x``.
+
         Examples
         --------
-        >>> x = ivy.Container(a=ivy.array([0., 2.6, -3.5]),\
-                            b=ivy.array([4.5, -5.3, -0, -2.3]))
+        >>> x = ivy.Container(a=ivy.array([0., 2.6, 10]),\
+                            b=ivy.array([4.5, -7, 0, 9]))
         >>> y = ivy.Container.min(x)
         >>> print(y)
         {
-            a: ivy.array([0.]),
-            b: ivy.array([-5.3])
+            a: ivy.array(0.),
+            b: ivy.array(-7)
         }
         """
         return self.handle_inplace(
