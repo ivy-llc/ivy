122433f4cca1dd3b22f4bd09ecc2509b3c54e4be
pawasRituja <ritujapawas.djr@gmail.com>
2022-07-27 13:06:10 +0000

    added ground-truth to test_norms

--- ivy_tests/test_ivy/test_functional/test_nn/test_norms.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_norms.py
@@ -1,6 +1,7 @@
 """Collection of tests for unified neural network layers."""
 
 # global
+import pytest
 import numpy as np
 from hypothesis import given, strategies as st
 
@@ -61,3 +62,41 @@ def test_layer_norm(
         offset=offset,
         new_std=new_std,
     )
+
+
+@pytest.mark.parametrize(
+    "x_n_ni_n_s_n_o_n_res",
+    [
+        (
+            [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+            -1,
+            [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+            [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+            [[-0.22473562, 2.0, 6.6742067], [-0.8989425, 5.0, 13.348413]],
+        ),
+    ],
+)
+@pytest.mark.parametrize("dtype", ["float32"])
+@pytest.mark.parametrize("tensor_fn", [ivy.array])
+def test_layer_norm_ground_truth(x_n_ni_n_s_n_o_n_res, dtype, tensor_fn, device, call):
+    # smoke test
+    x, norm_idxs, scale, offset, true_res = x_n_ni_n_s_n_o_n_res
+    x = tensor_fn(x, dtype=dtype, device=device)
+    scale = tensor_fn(scale, dtype=dtype, device=device)
+    offset = tensor_fn(offset, dtype=dtype, device=device)
+    true_res = tensor_fn(true_res, dtype=dtype, device=device)
+    ret = ivy.layer_norm(x, norm_idxs, scale=scale, offset=offset)
+    # type test
+    assert ivy.is_array(ret)
+    # cardinality test
+    assert ret.shape == true_res.shape
+    # value test
+    assert np.allclose(
+        call(ivy.layer_norm, x, norm_idxs, scale=scale, offset=offset),
+        ivy.to_numpy(true_res),
+    )
+    # compilation test
+    if call in [helpers.torch_call]:
+        # this is not a backend implemented function
+        return
+    helpers.assert_compilable(ivy.layer_norm)
