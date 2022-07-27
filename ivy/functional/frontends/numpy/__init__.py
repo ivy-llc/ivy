ee0935262e825671e44cd8d12d0268a2e226d6d2
James Keane <81018496+jkeane508@users.noreply.github.com>
2022-07-27 14:47:34 +0100

    Docs frontend tests (#2172)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>
    Co-authored-by: rush2406 <rushaligrandhe@gmail.com>

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa
+from . import creation_routines
+from .creation_routines import *
 from . import manipulation_routines
 from .manipulation_routines import *
 from . import mathematical_functions
