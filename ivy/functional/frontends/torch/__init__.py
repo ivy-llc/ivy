df79dbfa006c6799d75c31549f1a32069bcad485
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-07-28 16:55:18 +0530

    experiments

--- ivy/functional/frontends/torch/__init__.py
+++ ivy/functional/frontends/torch/__init__.py
@@ -1,14 +1,16 @@
 # flake8: noqa
-version=['latest_version']
+version = ['latest_version']
+faulty_version = {'1.11.0+cpu': [ones_like]}
 try:
- import torch
- version.append(torch.__version__)
+    import torch
+version.append(torch.__version__)
 except:
-    print("Torch not installed, assuming latest version: ",version[-1])
+print("Torch not installed, assuming latest version: ", version[-1])
 
-
-
-
+if version[-1] in faulty_version:
+    unusual_imports(1)  # we import with faulty functions replaced with version specific ones
+else:
+    unusual_imports(0)
 
 import importlib
 from . import indexing_slicing_joining_mutating_ops
