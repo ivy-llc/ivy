70a95b575c59fee3c72209a4f984538275f01fd6
eyra <eyra.wiley@gmail.com>
2022-07-28 22:42:24 +0500

    unique_inverse #858 #2271  Docstrings with examples

--- ivy/array/set.py
+++ ivy/array/set.py
@@ -50,4 +50,54 @@ class ArrayWithSet(abc.ABC):
         return ivy.unique_all(self._data)
 
     def unique_inverse(self: ivy.Array) -> NamedTuple:
+        """
+          ivy.Array instance method variant of ivy.unique_inverse. This method simply
+          wraps the function, and so the docstring for ivy.unique_inverse also applies
+          to this method with minimal changes.
+
+          Parameters
+          ----------
+          self
+              input array. If ``x`` has more than one dimension, the function must flatten
+              ``x`` and return the unique elements of the flattened array.
+
+          Returns
+          -------
+          ret
+
+              a namedtuple ``(values, inverse_indices)`` whose
+
+              - first element must have the field name ``values`` and must be an array
+              containing the unique elements of ``x``. The array must have the same data
+              type as ``x``.
+              - second element must have the field name ``inverse_indices`` and must be an
+              array containing the indices of ``values`` that reconstruct ``x``. The array
+              must have the same shape as ``x`` and must have the default array index data
+              type.
+
+             .. note::
+                The order of unique elements is not specified and may vary between
+                implementations.
+
+          This method conforms to the `Array API Standard
+          <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of
+          the `docstring <https://data-apis.org/array-api/latest/API_specification/
+          generated/signatures..set_functions.unique_inverse.html>`_
+          in the standard.
+
+          Examples
+          --------
+
+        >>> x = ivy.array([3,4,7,4,2,8,5])
+        >>> y = ivy.unique_inverse(x)
+        >>> print(y)
+        unique_inverse(values=ivy.array([2, 3, 4, 5, 7, 8]), inverse_indices=ivy.array([1, 2, 4, 2, 0, 5, 3]))
+
+        >>> x = ivy.array([0.3,0.4,0.7,0.4,0.2,0.8,0.5])
+        >>> y = ivy.unique_inverse(x)
+        >>> print(y)
+        unique_inverse(values=ivy.array([0.2, 0.3, 0.4, 0.5, 0.7, 0.8]), inverse_indices=ivy.array([1, 2, 4, 2, 0, 5, 3]))
+
+        """
+
         return ivy.unique_inverse(self._data)
--- ivy/container/set.py
+++ ivy/container/set.py
@@ -221,6 +221,65 @@ class ContainerWithSet(ContainerBase):
         prune_unapplied: bool = False,
         map_sequences: bool = False,
     ) -> ivy.Container:
+        """
+         ivy.Container static method variant of ivy.unique_inverse. This method simply
+         wraps the function, and so the docstring for ivy.unique_inverse also applies
+         to this method with minimal changes.
+
+         Parameters
+         ----------
+         self
+              input container. If ``x`` has more than one dimension, the function must
+              flatten ``x`` and return the unique elements of the flattened array.
+         key_chains
+              The key-chains to apply or not apply the method to. Default is None.
+         to_apply
+              If True, the method will be applied to key_chains, otherwise key_chains
+              will be skipped. Default is True.
+         prune_unapplied
+              Whether to prune key_chains for which the function was not applied.
+              Default is False.
+         map_sequences
+              Whether to also map method to sequences (lists, tuples). Default is False.
+
+         Returns
+         -------
+         ret
+
+              a namedtuple ``(values, inverse_indices)`` whose
+
+              - first element must have the field name ``values`` and must be an array
+              containing the unique elements of ``x``. The array must have the same data
+              type as ``x``.
+              - second element must have the field name ``inverse_indices`` and must be an
+              array containing the indices of ``values`` that reconstruct ``x``. The array
+              must have the same shape as ``x`` and must have the default array index data
+              type.
+
+              .. note::
+                 The order of unique elements is not specified and may vary between
+                 implementations.
+
+         This method conforms to the `Array API Standard
+         <https://data-apis.org/array-api/latest/>`_. This docstring is an extension of
+         the `docstring <https://data-apis.org/array-api/latest/API_specification/
+         generated/signatures..set_functions.unique_inverse.html>`_
+         in the standard.
+
+              Examples
+              --------
+
+              >>> x = ivy.Container(a=ivy.array([4., 8., 3. , 5. , 9. , 4.]), \
+                                    b=ivy.array([7,6,4,5,6,3,2]))
+              >>> y = ivy.ivy.unique_inverse(x)
+              >>> print(y)
+              {
+                a: (list[2], <class ivy.array.array.Array> shape=[5]),
+                b: (list[2], <class ivy.array.array.Array> shape=[6])
+              }
+
+         """
+
         return self.static_unique_inverse(
             self,
             key_chains=key_chains,
