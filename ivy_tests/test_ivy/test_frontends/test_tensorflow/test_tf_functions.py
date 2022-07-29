e714cd4850806a640eb3ed73881a33c7e42fe702
Emanuel <107799963+14Emanuel@users.noreply.github.com>
2022-07-30 02:44:14 +0300

    removing old linalg function code

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -190,61 +190,33 @@ def test_tensorflow_full(
         rtol=1e-05,
     )
 
-#linalg
-
-@st.composite
-def _dtypes(draw):
-    return draw(
-        st.shared(
-            helpers.list_of_length(
-                x=st.sampled_from(ivy_tf.valid_numeric_dtypes), length=1
-            ),
-            key="dtype",
-        )
-    )
-
-
-@st.composite
-def _fill_value(draw):
-    dtype = draw(_dtypes())[0]
-    if ivy.is_uint_dtype(dtype):
-        return draw(st.integers(0, 5))
-    if ivy.is_int_dtype(dtype):
-        return draw(st.integers(-5, 5))
-    return draw(st.floats(-5, 5))
-
-
+#linalg.adjoint
 @given(
-    shape=helpers.get_shape(
-        allow_none=False,
-        min_num_dims=1,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=10,
-    ),
-    fill_value=_fill_value(),
-    dtypes=_dtypes(),
+    xs_n_input_dtypes_n_unique_idx=_arrays_idx_n_dtypes(),
+    as_variable=helpers.array_bools(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.linalg.adjoint"
+        fn_name="ivy.functional.frontends.tensorflow.concat.linalg.adjoint"
     ),
+    native_array=helpers.array_bools(),
 )
 def test_tensorflow_linalg(
-    shape,
-    fill_value,
-    dtypes,
+    xs_n_input_dtypes_n_unique_idx,
+    as_variable,
     num_positional_args,
+    native_array,
     fw,
 ):
+    xs, input_dtypes, unique_idx = xs_n_input_dtypes_n_unique_idx
+    xs = [np.asarray(x, dtype=dt) for x, dt in zip(xs, input_dtypes)]
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=False,
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
-        native_array_flags=False,
+        native_array_flags=native_array,
         fw=fw,
         frontend="tensorflow",
         fn_name="linalg.adjoint",
-        dims=shape,
-        value=fill_value,
-        rtol=1e-05,
+        values=xs,
+        axis=unique_idx,
     )
