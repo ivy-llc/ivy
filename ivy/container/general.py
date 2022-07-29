0865174667394d71843451cf3b27b0b223baf909
JG <67888047+juliagsy@users.noreply.github.com>
2022-07-29 13:19:55 +0800

    container legacy method `einops_repeat` to equivalent method in `ivy/container/general.py` (#2381)

--- ivy/container/general.py
+++ ivy/container/general.py
@@ -578,6 +578,118 @@ class ContainerWithGeneral(ContainerBase):
             self, indices, key_chains, to_apply, prune_unapplied, map_sequences, out=out
         )
 
+    @staticmethod
+    def static_einops_repeat(
+        x: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        pattern: str,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+        **axes_lengths: Dict[str, int],
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.einops_repeat. This method simply
+        wraps the function, and so the docstring for ivy.einops_repeat also applies
+        to this method with minimal changes.
+
+        Parameters
+        ----------
+        x
+            Input array or container to be repeated.
+        pattern
+            Rearrangement pattern.
+        axes_lengths
+            Any additional specifications for dimensions.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is None.
+        to_apply
+            If True, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is True.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is False.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples). Default is False.
+        out
+            optional output container, for writing the result to. It must have a
+            shape that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            New container with einops.repeat having been applied.
+
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "einops_repeat",
+            x,
+            pattern,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+            **axes_lengths,
+        )
+
+    def einops_repeat(
+        self: ivy.Container,
+        pattern: str,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        *,
+        out: Optional[ivy.Container] = None,
+        **axes_lengths: Dict[str, int],
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.einops_repeat. This method simply
+        wraps the function, and so the docstring for ivy.einops_repeat also applies
+        to this method with minimal changes.
+
+        Parameters
+        ----------
+        self
+            Input array or container to be repeated.
+        pattern
+            Rearrangement pattern.
+        axes_lengths
+            Any additional specifications for dimensions.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is None.
+        to_apply
+            If True, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is True.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is False.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples). Default is False.
+        out
+            optional output container, for writing the result to. It must have a
+            shape that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            New container with einops.repeat having been applied.
+
+        """
+        return self.static_einops_repeat(
+            self,
+            pattern,
+            key_chains,
+            to_apply,
+            prune_unapplied,
+            map_sequences,
+            out=out,
+            **axes_lengths,
+        )
+
     def to_numpy(
         self,
         key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
