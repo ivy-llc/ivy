e921a82a563740fda26af8be0a1a58b619276000
changyonglik <theeahlag@gmail.com>
2022-08-01 08:20:12 +0900

    Added unit test
    Signed-off-by: changyonglik <theeahlag@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -68,6 +68,32 @@ def test_tensorflow_tan(
     )
 
 
+# random_normal
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_tf.valid_float_dtypes),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.random_normal"
+    ),
+    native_array=st.booleans(),
+)
+def test_tensorflow_random_normal(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_name="random_normal",
+        x=np.asarray(x, dtype=input_dtype),
+    )
+
+
 # noinspection DuplicatedCode
 @st.composite
 def _arrays_idx_n_dtypes(draw):
