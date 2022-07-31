b81d32775a9519eab61aed73ad86b724170b696e
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-07-31 23:17:49 +0530

    Delete argmax.py

--- argmax.py
+++
@@ -1,9 +0,0 @@
-def argmax(vector):
-    index, value = 0, vector[0]
-    for i,v in enumerate(vector):
-        if v > value:
-            index, value = i,v
-    return index
-vector = list(input())
-result = argmax(vector)
-print('arg max of %s: %d' % (vector, result))
