fdc2e5010ad3e27bb07c2ca6dc9990032b2f15c6
Pushpahasa N S <43383022+pushpahasa1@users.noreply.github.com>
2022-07-31 20:46:39 +0530

    added multiplication function
    Added at frontend numpy.arthematic_operations

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -24,3 +24,26 @@ def add(
 
 
 add.unsupported_dtypes = {"torch": ("float16",)}
+
+def multiply(
+    x1,
+    x2,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+    ret = ivy.multiply(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+multiply.unsupported_dtypes = {"torch": ("float16",)}
