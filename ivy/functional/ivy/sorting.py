7b8bc52a043b9e5b34dd96695c94c2e7570bfcac
Hua Zhao <zhaohua819@gmail.com>
2022-08-01 21:48:11 +0100

    Update sorting.py

--- ivy/functional/ivy/sorting.py
+++ ivy/functional/ivy/sorting.py
@@ -279,7 +279,7 @@ def searchsorted(
 
     ivy.array([3,2,4])
     """ 
-    return ivy.current_backend(x1).searchsorted(x1, v, side=side, sorter=None, out=out)
+    return ivy.current_backend(x1, v).searchsorted(x1, v, side=side, sorter=None, out=out)
 
 # Extra #
 # ------#
