836fbae635f1b98092213a37bc28f707122837e3
Unevilicorn <unevilicorn@gmail.com>
2022-08-01 16:42:12 +0100

    fix log2 occasional fail

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1189,6 +1189,10 @@ def test_log2(
     fw,
 ):
     input_dtype, x = dtype_and_x
+
+    # avoid logging values too close to zero
+    assume(not np.any(np.isclose(x, 0)))
+
     helpers.test_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
