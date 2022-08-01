5a5ce5236b5012ef47dc7aa9b5bb5c76495f576c
Amrabdelhamed611 <49839750+Amrabdelhamed611@users.noreply.github.com>
2022-08-01 08:30:30 +0200

    Reformat element-wise function log1p updating docs (#2273)
    * Reformat element-wise function log1p updating docs
    
    * Reformat element-wise function log1p updating docs
    
    * Reformat in container functions log1p & static_log1p
    
    * Reformat in array elementwise.py method log1p.
    
    * adding changes according to change reqeust for ivy  elementwise.py method log1p.
    
    Former-commit-id: 678e22fb9082c1439ea787e3020ef245e37711aa

--- ivy/array/elementwise.py
+++ ivy/array/elementwise.py
@@ -1068,6 +1068,7 @@ class ArrayWithElementwise(abc.ABC):
         function, and so the docstring for ivy.log also applies to this method
         with minimal changes.
 
+
         Parameters
         ----------
         self
@@ -1087,10 +1088,9 @@ class ArrayWithElementwise(abc.ABC):
 
     def log1p(self: ivy.Array, *, out: Optional[ivy.Array] = None) -> ivy.Array:
         """
-        ivy.Array instance method variant of ivy.log1p.
+       ivy.Array instance method variant of ivy.log1p.
         This method simply wraps the function, and so the docstring
         for ivy.log1p also applies to this method with minimal changes.
-
         Parameters
         ----------
         self
@@ -1098,13 +1098,26 @@ class ArrayWithElementwise(abc.ABC):
         out
             optional output array, for writing the result to. It must have a shape that
             the inputs broadcast to.
-
         Returns
         -------
         ret
             an array containing the evaluated result for each element in ``self``.
             The returned array must have a real-valued floating-point data
             type determined by :ref:`type-promotion`.
+
+        Examples
+        --------
+
+        >>> x = ivy.array([1 , 2 ,3 ])
+        >>> y = x.log1p()
+        >>> print(y)
+        ivy.array([0.693, 1.1  , 1.39 ])
+
+        >>> x = ivy.array([0.1 , .001 ])
+        >>> x.log1p( out = x)
+        >>> print(x)
+        ivy.array([0.0953, 0.001 ])
+
         """
         return ivy.log1p(self._data, out=out)
 
