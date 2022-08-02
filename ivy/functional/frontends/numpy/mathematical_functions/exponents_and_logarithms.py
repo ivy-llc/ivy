4f930b00f967697fc541750b29ae58a04af2d5a7
Alejandro Pinel Martínez <55890199+alekpinel@users.noreply.github.com>
2022-08-02 23:46:58 +0200

    Numpy Frontend: Added exp

---
+++ ivy/functional/frontends/numpy/mathematical_functions/exponents_and_logarithms.py
@@ -0,0 +1,24 @@
+# global
+import ivy
+
+
+def exp(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.exp(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+exp.unsupported_dtypes = {"torch": ("float16",)}
