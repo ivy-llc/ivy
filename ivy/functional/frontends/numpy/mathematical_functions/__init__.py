84b4844a74f21fa3e1d8bb82811764528eb1ca11
Alejandro Pinel Martínez <55890199+alekpinel@users.noreply.github.com>
2022-08-02 23:46:58 +0200

    Numpy Frontend: Added exp
    Former-commit-id: 3431855a0d97681e1a5bcb105a4a051dcbb0353a

--- ivy/functional/frontends/numpy/mathematical_functions/__init__.py
+++ ivy/functional/frontends/numpy/mathematical_functions/__init__.py
@@ -3,3 +3,5 @@ from . import arithmetic_operations
 from .arithmetic_operations import *
 from . import trigonometric_functions
 from .trigonometric_functions import *
+from . import exponents_and_logarithms
+from .exponents_and_logarithms import *
