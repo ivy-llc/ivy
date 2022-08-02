cc93b423e007adbdac08ceafb93585be75d7f517
ukj02 <ukj02@proton.me>
2022-08-02 14:46:04 -0400

    Update sorting.py

--- ivy/array/sorting.py
+++ ivy/array/sorting.py
@@ -26,18 +26,20 @@ class ArrayWithSorting(abc.ABC):
         self 
             input array.
         axis
-            axis along which to sort. If set to ``-1``, the function must sort along the last axis.
-            Default: ``-1``.
+            axis along which to sort. If set to ``-1``, the function
+            must sort along the last axis. Default: ``-1``.
         descending
             sort order. If ``True``, the returned indices sort ``x`` in descending order 
             (by value). If ``False``, the returned indices sort ``x`` in ascending order 
             (by value). Default: ``False``.
         stable
-            sort stability. If ``True``, the returned indices must maintain the relative
-            order of ``x`` values which compare as equal. If ``False``, the returned indices
-            may or may not maintain the relative order of ``x`` values which compare as equal 
-            (i.e., the relative order of ``x`` values which compare as equal is 
-            implementation-dependent). Default: ``True``.
+            sort stability. If ``True``, the returned indices
+            must maintain the relative order of ``x`` values 
+            which compare as equal. If ``False``, the returned
+            indices may or may not maintain the relative order
+            of ``x`` values which compare as equal (i.e., the
+            relative order of ``x`` values which compare as 
+            equal is implementation-dependent). Default: ``True``.
         
         Returns
         -------
