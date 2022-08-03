0e2cf8f75d65d6c4bf4bb61d984dc564f1c4380d
FrankShi9 <yunfan-shi@hotmail.com>
2022-08-03 21:08:04 +0100

    frontend tf test check fix 1

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -211,6 +211,6 @@ def test_tensorflow_hard_sigmoid(
         native_array_flags=native_array,
         fw=fw,
         frontend="tensorflow",
-        fn_name="hard_sigmoid",
+        fn_name="activations.hard_sigmoid",
         x=np.asarray(x, dtype=input_dtype),
     )
