e3388383e0bee637584bcac6ef8379cfe072d287
Aman Agarwal <agarwalaman190202@gmail.com>
2022-08-04 00:43:36 +0530

    arcsin and arccos in #1525 addressed

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -44,3 +44,46 @@ def tan(
 
 
 tan.unsupported_dtypes = {"torch": ("float16",)}
+
+
+def arcsin(
+    x,
+    /,
+    out=None,
+    *, 
+    where=True, 
+    casting='same_kind', 
+    order='K', 
+    dtype=None, 
+    subok=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.arcsin(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+arcsin.unsupported_dtypes = {"torch": ("float16",)}
+
+def arccos(
+    x,
+    /,
+    out=None, 
+    *, 
+    where=True, 
+    casting='same_kind', 
+    order='K', 
+    dtype=None, 
+    subok=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.arccos(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+arcsin.unsupported_dtypes = {"torch": ("float16",)}
