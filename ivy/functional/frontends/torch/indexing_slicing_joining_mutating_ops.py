d70e913eef971528b4405fcf390eac2d5911b9b6
Roll20 <cycla4@gmail.com>
2022-08-04 13:49:34 +0800

    commit staged

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,6 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+def concat(tensors, dim=0, *, out=None):
+    return ivy.concat(tensors, dim, out=out)
