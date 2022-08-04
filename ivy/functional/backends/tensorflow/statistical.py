9f7503d6dc13f23c7f37fd99f34d01e7e5d9cbb6
Linjie Xu <lin.svc@outlook.com>
2022-08-04 09:24:15 +0100

    reformatted var function: add examples, add type hint for keepdims (#2014)
    Co-authored-by: pawasRituja <ritujapawas.djr@gmail.com>

--- ivy/functional/backends/tensorflow/statistical.py
+++ ivy/functional/backends/tensorflow/statistical.py
@@ -1,7 +1,7 @@
 # global
 _round = round
 import tensorflow as tf
-from typing import Tuple, Union, Optional
+from typing import Tuple, Union, Optional, Sequence
 
 # local
 import ivy
@@ -101,9 +101,9 @@ def sum(
 
 def var(
     x: Union[tf.Tensor, tf.Variable],
-    axis: Optional[Union[int, Tuple[int]]] = None,
+    axis: Optional[Union[int, Sequence[int]]] = None,
     correction: Union[int, float] = 0.0,
-    keepdims: bool = False,
+    keepdims: Optional[bool] = False,
     *,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None
 ) -> Union[tf.Tensor, tf.Variable]:
