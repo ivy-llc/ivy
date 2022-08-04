4999cf67d6a00b28323e1e269b99ae1737d0ab1a
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-04 22:09:12 +0800

    Add reshape
    to frontends.torch.indexing_slicing_joining_mutating_ops

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,21 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def concat(tensors, dim=0, *, out=None):
+    return ivy.concat(tensors, dim, out=out)
+
+
+def stack(tensors, dim=0, *, out=None):
+    return ivy.stack(tensors, dim, out=out)
+
+
+def reshape(input, shape):
+    return ivy.expand_dims(input, shape)
+
+reshape.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
