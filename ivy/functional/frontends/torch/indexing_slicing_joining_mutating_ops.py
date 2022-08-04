b9a04a26d8e80825090e8ca634038be234009a4b
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-04 20:09:11 +0800

    Add stack
    to frontends.torch.indexing_slicing_joining_mutating_ops

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,11 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def concat(tensors, dim=0, *, out=None):
+    return ivy.concat(tensors, dim, out=out)
+
+
+def stack(tensors, dim=0, *, out=None):
+    return ivy.stack(tensors, dim, out=out)
