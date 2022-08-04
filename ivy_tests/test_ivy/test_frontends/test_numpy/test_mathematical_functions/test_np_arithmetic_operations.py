44ed4e6f03cc6b104c74b90bac4e06ac44df83ef
Pushpahasa N S <43383022+pushpahasa1@users.noreply.github.com>
2022-08-04 16:59:52 +0530

    ata generation part for hypothesis added
    will look at lint issue

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_arithmetic_operations.py
@@ -113,6 +113,19 @@ def test_numpy_subtract(
     
     
 #multiply
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_float_dtypes, num_arrays=2
+    ),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
+    where=np_frontend_helpers.where(),
+    as_variable=helpers.array_bools(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.multiply"
+    ),
+    native_array=helpers.array_bools(),
+)
 def test_numpy_multiply(
     dtype_and_x,
     dtype,
