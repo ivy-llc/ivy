a41f96a392292228fd4ac650667e9814fd0d34b5
FrankShi9 <yunfan-shi@hotmail.com>
2022-08-04 21:03:37 +0100

    frontend tf test check fix 4

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -194,23 +194,23 @@ def test_tensorflow_full(
 @given(
     dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_tf.valid_float_dtypes),
     as_variable=st.booleans(),
-    # num_positional_args=helpers.num_positional_args(
-    #     fn_name="ivy.functional.frontends.tensorflow.activations.hard_sigmoid"
-    # ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.hard_sigmoid"
+    ),
     native_array=st.booleans(),
 )
 def test_tensorflow_hard_sigmoid(
-    dtype_and_x, as_variable, native_array, fw
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
-        # num_positional_args=num_positional_args,
+        num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
         frontend="tensorflow",
-        fn_name="activations.hard_sigmoid",
+        fn_name="keras.activations.hard_sigmoid",
         x=np.asarray(x, dtype=input_dtype),
     )
