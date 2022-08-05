32abd8954962683c4909a0af88da2496415de3b0
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-05 10:47:40 +0800

    Add torch flip, fliplr, flipud
    to frontends.torch.other_operations

--- ivy/functional/frontends/torch/other_operations.py
+++ ivy/functional/frontends/torch/other_operations.py
@@ -1,1 +1,31 @@
+import ivy
 
+
+def flip(input, dims):
+    return ivy.flip(input, dims)
+
+flip.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
+
+
+def fliplr(input):
+    return ivy.fliplr(input)
+
+fliplr.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
+
+
+def flipud(input):
+    return ivy.flipud(input)
+
+flipud.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
