ad6f35caea5c5219ff85cccd27aead1fdff39d01
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-06 10:12:02 +0800

    removing other tasks currently in PR

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -2,18 +2,6 @@
 import ivy
 
 
-def cat(tensors, dim=0, *, out=None):
-    return ivy.concat(tensors, dim, out=out)
-
-
-def concat(tensors, dim=0, *, out=None):
-    return ivy.concat(tensors, dim, out=out)
-
-
-def stack(tensors, dim=0, *, out=None):
-    return ivy.stack(tensors, dim, out=out)
-
-
 def reshape(input, shape):
     return ivy.expand_dims(input, shape)
 
