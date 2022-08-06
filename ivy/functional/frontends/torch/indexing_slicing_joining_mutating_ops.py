e65b145d9618e3022d157b8dfff05afb6e9ae3d3
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-06 23:07:14 +0800

    add transpose, swapaxes, swapdims
    to frontends.torch.indexing_slicing_joining_mutating_ops
    
    link to issue: #2691

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,36 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def transpose(input, dim0, dim1):
+    return ivy.swapaxes(input, dim0, dim1)
+
+
+transpose.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
+
+
+def swapaxes(input, axis0, axis1):
+    return ivy.swapaxes(input, axis0, axis1)
+
+
+swapaxes.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
+
+
+def swapdims(input, dim0, dim1):
+    return ivy.swapaxes(input, dim0, dim1)
+
+
+swapdims.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
