a66d831392c2874778c30d6476028d869141b1ec
Emperor-WS <76920103+Emperor-WS@users.noreply.github.com>
2022-08-06 19:42:05 +0200

    Update __init__.py
    Updating it to import statistics

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -5,3 +5,5 @@ from . import manipulation_routines
 from .manipulation_routines import *
 from . import mathematical_functions
 from .mathematical_functions import *
+from . import statistics
+from .statistics import *
