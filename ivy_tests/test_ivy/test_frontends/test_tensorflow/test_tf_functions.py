50d3e9ef90a900e38b2c55ff4333803de1068dbc
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-06 22:39:52 +0800

    lint

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -194,11 +194,11 @@ def test_tensorflow_full(
 # mutiply
 @given(
     dtype_and_x=helpers.dtype_and_values(
-    available_dtypes=tuple(
-        set(ivy_np.valid_float_dtypes).intersection(
-            set(ivy_tf.valid_float_dtypes))),
-    num_arrays=2,
-    shared_dtype=True,
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_tf.valid_float_dtypes))),
+        num_arrays=2,
+        shared_dtype=True,
     ), 
     as_variable=helpers.list_of_length(x=st.booleans(), length=2),
     num_positional_args=helpers.num_positional_args(
