2c4bd31c6738badc8a33c468ec0ab9956a21d160
Ashi <ashigautam00@gmail.com>
2022-08-06 11:12:59 +0530

    add 'multiply' to numpy arithmetic_operations.py

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -69,3 +69,24 @@ def divide(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
+
+def multiply(
+    x1,
+    x2,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+    ret = ivy.multiply(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
