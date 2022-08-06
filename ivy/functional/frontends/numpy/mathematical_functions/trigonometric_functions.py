048113f1d4aff82bcd7ff72f8697bb4764266368
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-08-06 22:31:24 +0530

    Update trigonometric_functions.py

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -44,3 +44,25 @@ def tan(
 
 
 tan.unsupported_dtypes = {"torch": ("float16",)}
+
+
+def cosh(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.cosh(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+cosh.unsupported_dtypes = {"torch": ("float16",)}
