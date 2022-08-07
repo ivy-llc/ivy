784bbbee70380b3984258797579c7140cb5c0340
Darshan Tank <61326242+Darshan2104@users.noreply.github.com>
2022-08-07 10:13:45 +0000

    frontend -sinh added!

--- ivy/functional/frontends/numpy/mathematical_functions/__init__.py
+++ ivy/functional/frontends/numpy/mathematical_functions/__init__.py
@@ -3,3 +3,5 @@ from . import arithmetic_operations
 from .arithmetic_operations import *
 from . import trigonometric_functions
 from .trigonometric_functions import *
+from . import hyperbolic_functions
+from .hyperbolic_functions import *
---
+++ ivy/functional/frontends/numpy/mathematical_functions/hyperbolic_functions.py
@@ -0,0 +1,24 @@
+# global
+import ivy
+
+
+def sinh(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.sinh(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+sinh.unsupported_dtypes = {"torch": ("float16",)}
