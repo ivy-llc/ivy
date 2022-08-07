e8452dbfd18d15b9a9269b7c5c0585e301e36673
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-07 12:52:05 +0800

    add swapaxes

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,14 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def swapaxes(input, axis0, axis1):
+    return ivy.swapaxes(input, axis0, axis1)
+
+
+swapaxes.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
