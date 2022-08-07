7f51081da157f1ebc189c41ae0d51f86fcfefe59
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-07 12:47:45 +0800

    add transpose

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,14 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def transpose(input, dim0, dim1):
+    return ivy.swapaxes(input, dim0, dim1)
+
+
+transpose.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
