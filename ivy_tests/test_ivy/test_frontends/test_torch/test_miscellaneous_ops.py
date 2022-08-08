e9e8743dab13aae7f19120d473a28f117c3a3475
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-08 19:17:11 +0800

    add test_flipud to test_frontends.torch

---
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -0,0 +1,46 @@
+# global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+import ivy.functional.backends.torch as ivy_torch
+
+
+# flipud
+@given(
+    dtype_value=
+        helpers.dtype_and_values(
+            available_dtypes=tuple(
+                set(ivy_np.valid_float_dtypes).intersection(
+                    set(ivy_torch.valid_float_dtypes)))),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.flipud"),
+    native_array=st.booleans(),
+    data=st.data(),
+)
+def test_torch_flipud(
+    *,
+    data,
+    dtype_value,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, value = dtype_value
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_name="flipud",
+        input=np.asarray(value, dtype=input_dtype),
+    )
