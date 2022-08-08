b3ee7e90c0b44ff2262c0bb549b6cfa9581bda74
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-08 12:59:19 +0800

    lint fix

--- ivy/functional/frontends/numpy/sorting_searching_counting/searching.py
+++ ivy/functional/frontends/numpy/sorting_searching_counting/searching.py
@@ -4,7 +4,8 @@ import ivy
 
 def where(cond, x1=None, x2=None, /):
     if x1 is None and x2 is None :
-        # numpy where behaves as np.asarray(condition).nonzero() when x and y not included
+        # numpy where behaves as np.asarray(condition).nonzero() when x and y
+        # not included
         return ivy.asarray(cond).nonzero()
     elif x1 is not None and x2 is not None :
         return ivy.where(cond, x1, x2)
