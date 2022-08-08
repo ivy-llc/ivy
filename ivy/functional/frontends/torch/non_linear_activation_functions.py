4702c28bb20b6750c4943499c44a10a8da38ba48
Aymen Sekhri <aymensekhri20@gmail.com>
2022-08-08 14:58:58 +0100

    update

--- ivy/functional/frontends/torch/non_linear_activation_functions.py
+++ ivy/functional/frontends/torch/non_linear_activation_functions.py
@@ -7,3 +7,10 @@ def sigmoid(input, out=None):
 
 
 sigmoid.unsupported_dtypes = ("float16",)
+
+
+def leaky_relu(input, alpha=0.01):
+    return ivy.leaky_relu(input, alpha)
+
+
+leaky_relu.unsupported_dtypes = ("float16",)
--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -43,3 +43,42 @@ def test_torch_sigmoid(
         input=np.asarray(x, dtype=input_dtype),
         out=None,
     )
+
+
+# leaky_relu
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    alpha=st.floats(min_value=0, max_value=1, allow_infinity=False),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="leaky_relu",
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_leaky_relu(
+    dtype_and_x,
+    alpha,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_name="leaky_relu",
+        input=np.asarray(x, dtype=input_dtype),
+        alpha=alpha,
+    )
