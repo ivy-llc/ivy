f4240b9e00bf85265a70a9d6241f35e298a2a24e
gurkankilicaslan <57681394+gurkankilicaslan@users.noreply.github.com>
2022-08-08 21:51:13 +0300

    added cos to NumPy frontend (#2509)
    Co-authored-by: ZoeCD <zoe.cd@outlook.com>

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -2,6 +2,28 @@
 import ivy
 
 
+def cos(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.cos(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+cos.unsupported_dtypes = {"torch": ("float16",)}
+
+
 def sin(
     x,
     /,
