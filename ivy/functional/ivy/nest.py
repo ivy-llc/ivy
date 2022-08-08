8aa7819cb1330986b4bb8850541d1154dc2d0a85
RickSanchezStoic <57310695+RickSanchezStoic@users.noreply.github.com>
2022-08-08 12:59:15 +0530

    Docstring (#2661)
    * isolating docstring failing functions
    
    * lint fixes
    
    * docstring fixes
    
    * docstring fixes and type promotion
    
    * lint fixes
    
    * more lint fixes
    
    * docstring fix
    
    
    
    Former-commit-id: 326dab44f71ad99339d99cecf6ab9600760db40d

--- ivy/functional/ivy/nest.py
+++ ivy/functional/ivy/nest.py
@@ -821,7 +821,7 @@ def copy_nest(
     >>> copied_nest = ivy.copy_nest(nest)
     >>> print(copied_nest)
     ivy.array([[1., 2., 3.],
-            [7., 8., 9.]])    
+            [7., 8., 9.]])
 
     With :code:`Iterable` input:
 
