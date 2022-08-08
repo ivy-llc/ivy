ba4d4d2cdc15b7e9eb334c1e252ea911bf29c98c
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-08 13:21:19 +0800

    indexing routines

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 from . import creation_routines
 from .creation_routines import *
+from . import indexing_routines
+from .indexing_routines import *
 from . import sorting_searching_counting
 from .sorting_searching_counting import *
 from . import manipulation_routines
