5351ce19c4453a303aee1864a8c7b55df90c1aa0
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-08 15:30:44 +0800

    arguments sequences

--- ivy/functional/ivy/losses.py
+++ ivy/functional/ivy/losses.py
@@ -279,7 +279,7 @@ def sparse_cross_entropy(
 
     """
     true = ivy.one_hot(true, pred.shape[axis])
-    return ivy.cross_entropy(true, pred, axis, epsilon, out=out)
+    return ivy.cross_entropy(true, pred, axis=axis, epsilon=epsilon, out=out)
 
 
 sparse_cross_entropy.unsupported_dtypes = {"torch": ("float16",)}
