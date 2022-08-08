0b7bcff9b98d4a864d448ec402246947d42a8f5f
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-08 13:28:43 +0800

    ufunc files

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -3,9 +3,11 @@ from . import creation_routines
 from .creation_routines import *
 from . import indexing_routines
 from .indexing_routines import *
-from . import sorting_searching_counting
-from .sorting_searching_counting import *
 from . import manipulation_routines
 from .manipulation_routines import *
 from . import mathematical_functions
 from .mathematical_functions import *
+from . import sorting_searching_counting
+from .sorting_searching_counting import *
+from . import ufunc
+from .ufunc import *
