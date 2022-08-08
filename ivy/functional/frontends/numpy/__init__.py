2a9d25eb4480f63e632a071b6708ad5a033f6850
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-08 14:27:04 +0800

    add numpy frontend skeleton files - statistics, logic

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -1,9 +1,13 @@
 # flake8: noqa
 from . import creation_routines
 from .creation_routines import *
+from . import logic
+from .logic import *
 from . import sorting_searching_counting
 from .sorting_searching_counting import *
 from . import manipulation_routines
 from .manipulation_routines import *
 from . import mathematical_functions
 from .mathematical_functions import *
+from . import statistics
+from .statistics import *
