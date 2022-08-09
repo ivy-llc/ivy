674ce52cf7492825aceaee079c9510f76ebe3fa4
kanishkaditya shukla <shuklakanishkaditya12@gmail.com>
2022-08-09 11:53:42 +0530

    fixed requested changes

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -191,6 +191,7 @@ def test_tensorflow_full(
     )
 
 
+
 # mutiply
 @given(
     dtype_and_x=helpers.dtype_and_values(
@@ -290,3 +291,36 @@ def test_tensorflow_logical_xor(
         x=np.asarray(x[0], dtype=input_dtype[0]),
         y=np.asarray(x[1], dtype=input_dtype[1]),
     )
+
+# divide
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_tf.valid_float_dtypes))
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.divide"
+    ),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
+)
+def test_tensorflow_divide(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_name="divide",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
+    )
+
