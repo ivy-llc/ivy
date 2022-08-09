f52654c02f046adf53ac056b6a52da60d9f28d4e
Emperor-WS <76920103+Emperor-WS@users.noreply.github.com>
2022-08-09 19:53:33 +0200

    Update test_averages_and_variances.py
    Resolving some errors

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
@@ -44,7 +44,7 @@ def test_numpy_mean(
         as_variable=as_variable,
         native_array=native_array,
     )
-    helpers.test_function(
+    np_frontend_helpers.test_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=with_out,
