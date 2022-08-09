d4c39e1003eb5cfb7288910d06ff3dfa94a0a4c1
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-09 16:45:40 +0800

    move to other branch

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -7,8 +7,6 @@ from . import manipulation_routines
 from .manipulation_routines import *
 from . import mathematical_functions
 from .mathematical_functions import *
-from . import random
-from .random import *
 from . import sorting_searching_counting
 from .sorting_searching_counting import *
 from . import statistics
