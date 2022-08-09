33780f0419c601e82dfb49a527f5616d23d17a4f
Kunwar M. Saaim <kunwarsaaim@gmail.com>
2022-08-09 10:33:39 +0530

    torch frontend `arccos` function (#2796)
    Co-authored: juliagsy <67888047+juliagsy@users.noreply.github.com>

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -37,6 +37,13 @@ def acos(input, *, out=None):
 acos.unsupported_dtypes = ("float16",)
 
 
+def arccos(input, *, out=None):
+    return ivy.acos(input, out=out)
+
+
+arccos.unsupported_dtypes = ("float16",)
+
+
 def abs(input, *, out=None):
     return ivy.abs(input, out=out)
 
