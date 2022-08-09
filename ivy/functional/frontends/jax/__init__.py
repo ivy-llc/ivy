4fc76102ffb77b00c184b7be9a335eb6cc9fa30c
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-08-09 11:45:22 +0530

    added jax frontend files

--- ivy/functional/frontends/jax/__init__.py
+++ ivy/functional/frontends/jax/__init__.py
@@ -9,3 +9,5 @@ from .lax import parallel_operators
 from ivy.functional.frontends.jax.lax.parallel_operators import *
 from .lax import linalg
 from ivy.functional.frontends.jax.lax.linalg import *
+from .nn import non_linear_activations
+from ivy.functional.frontends.jax.nn.non_linear_activations import *
