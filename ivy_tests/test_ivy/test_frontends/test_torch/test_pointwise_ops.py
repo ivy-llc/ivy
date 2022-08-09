64c600d41a2b918d93dcb0a9ea07d37381ced5a7
kunwarsaaim <kunwarsaaim@gmail.com>
2022-08-09 10:12:05 +0530

    added torch arccos function

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -251,6 +251,45 @@ def test_torch_acos(
     )
 
 
+# arccos
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.arccos"
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_arccos(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_name="arccos",
+        input=np.asarray(x, dtype=input_dtype),
+        out=None,
+    )
+
+
 # subtract
 @given(
     dtype_and_x=helpers.dtype_and_values(
