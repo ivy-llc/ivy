2e59d148a4100d3e194c5d7aa81b63c57b19649d
CatB1t <skytedits@gmail.com>
2022-08-09 15:20:42 +0200

    fix test for det frontend

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_linalg.py
@@ -9,13 +9,15 @@ import ivy.functional.backends.tensorflow as ivy_tf
 
 @st.composite
 def _det_arrays(draw):
-    arbitrary_dims = draw(st.lists(st.integers(min_value=1)))
-    m = draw(st.integers(min_value=1, max_value=100))
+    arbitrary_dims = draw(st.lists(st.integers(min_value=1, max_value=10), max_size=5))
+    m = draw(st.integers(min_value=1, max_value=20))
     shape = (*arbitrary_dims, m, m)
     return draw(
         helpers.dtype_and_values(
             available_dtypes=ivy_tf.valid_float_dtypes,
             shape=shape,
+            min_value=-10,
+            max_value=10,
         )
     )
 
@@ -42,6 +44,6 @@ def test_tensorflow_det(
         frontend="tensorflow",
         fn_name="linalg.det",
         rtol=1e-5,
-        test_values=False,
+        atol=1e-5,
         input=np.asarray(x, dtype=input_dtype),
     )
