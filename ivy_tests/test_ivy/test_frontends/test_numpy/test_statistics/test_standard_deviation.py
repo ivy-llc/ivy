dc55fe8e864f497b86f497d1648fb766c0d1272d
Doregon499 <81925037+Doregon499@users.noreply.github.com>
2022-08-09 11:34:01 -0500

    Update test_standard_deviation.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_standard_deviation.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_standard_deviation.py
@@ -9,17 +9,23 @@ import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpe
 
 # std
 @given(
-dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_np.valid_float_dtypes),
-dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
-where=np_frontend_helpers.where(),
-as_variable=helpers.array_bools(),
-with_out=st.booleans(),
-num_positional_args=helpers.num_positional_args(
-    fn_name="ivy.functional.frontends.numpy.standard_deviation"
-),
-native_array=helpers.array_bools(),
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_numeric_dtypes
+    ),
+    dtype=st.sampled_from(ivy_np.valid_numeric_dtypes + (None,)),
+    where=np_frontend_helpers.where(),
+    as_variable=helpers.array_bools(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.std"
+    ),
+    native_array=helpers.array_bools(),
+    container=st.booleans(),
+    instance_method=st.booleans(),
+    correction=st.floats(),
 )
 def test_numpy_standard_deviation(
+    *,
     dtype_and_x,
     dtype,
     where,
@@ -27,31 +33,34 @@ def test_numpy_standard_deviation(
     with_out,
     num_positional_args,
     native_array,
+    container,
+    instance_method,
+    correction,
     fw,
 ):
     input_dtype, x = dtype_and_x
-    input_dtype = [input_dtype]
     where = np_frontend_helpers.handle_where_and_array_bools(
         where=where,
         input_dtype=input_dtype,
         as_variable=as_variable,
         native_array=native_array,
     )
-    np_frontend_helpers.test_frontend_function(
+    helpers.test_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        correction=correction,
         fw=fw,
         frontend="numpy",
-        fn_name="standard_deviation",
-        x=np.asarray(x, dtype=input_dtype[0]),
-        out=None,
-        where=where,
-        casting="same_kind",
-        order="k",
+        fn_name="std",
+        x=np.asarray(x, dtype=input_dtype),
+        axis=None,
         dtype=dtype,
-        subok=True,
-        test_values=False,
+        out=None,
+        keepdims=False,
+        where=where,
     )
