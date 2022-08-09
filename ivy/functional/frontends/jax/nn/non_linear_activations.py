526c27151ccaac0d05db544f03baf8f9159689db
Vikramjeet Singh <72499426+VikramxD@users.noreply.github.com>
2022-08-09 17:56:57 +0000

    changes

--- ivy/functional/frontends/jax/nn/non_linear_activations.py
+++ ivy/functional/frontends/jax/nn/non_linear_activations.py
@@ -6,3 +6,10 @@ def relu(x):
 
 
 relu.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
+
+def leaky_relu(x):
+    return ivy.leaky_relu(x)
+
+leaky_relu.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
