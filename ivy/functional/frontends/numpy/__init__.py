56c35fa9e390ee911a15bf9b37addab120562e05
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-09 10:20:40 +0800

    move to another branch - indexing

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -1,8 +1,6 @@
 # flake8: noqa
 from . import creation_routines
 from .creation_routines import *
-from . import indexing_routines
-from .indexing_routines import *
 from . import logic
 from .logic import *
 from . import manipulation_routines
