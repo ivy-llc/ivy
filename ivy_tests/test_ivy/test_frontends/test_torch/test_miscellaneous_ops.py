e5828a6f3ac805e070f65a5ad8e8846babff9c90
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-10 22:33:20 +0800

    update test_flip according to instructions

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -10,57 +10,34 @@ import ivy.functional.backends.torch as ivy_torch
 
 # flip
 @given(
-    dtype_value=
-        helpers.dtype_and_values(
-            available_dtypes=tuple(
+    dtype_values_axis = helpers.dtype_values_axis(
+        available_dtypes=tuple(
                 set(ivy_np.valid_float_dtypes).intersection(
                     set(ivy_torch.valid_float_dtypes))),
-            shape=st.shared(
-                helpers.get_shape(
-                    min_num_dims=1
-                ),
-                key='shape'
-            ),
-        ),
-    dims=
-        helpers.get_axis(
-            shape=st.shared(
-                helpers.get_shape(
-                    min_num_dims=1
-                ),
-                key='shape'
-            ),
-            min_size=1,
-            max_size=1,
-        ),
+        shape=helpers.get_shape(min_num_dims=1)
+    ),
     as_variable=st.booleans(),
-    with_out=st.booleans(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="functional.frontends.torch.flip"),
+        fn_name="ivy.functional.frontends.torch.flip"),
     native_array=st.booleans(),
-    data=st.data(),
 )
 def test_torch_flip(
     *,
-    data,
-    dtype_value,
-    dims,
+    dtype_values_axis,
     as_variable,
-    with_out,
     num_positional_args,
     native_array,
     fw,
 ):
-    input_dtype, value = dtype_value
+    input_dtype, value, axis = dtype_values_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
         frontend="torch",
         fn_name="flip",
         input=np.asarray(value, dtype=input_dtype),
-        dims=dims,
+        dims=axis,
     )
