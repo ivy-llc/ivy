6c06f03c7f04bdda7e88f1899e8a7b6c18e68af7
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-10 20:34:10 +0800

    re-add after merging upstream

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -4,3 +4,14 @@ import ivy
 
 def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
+
+
+def reshape(input, shape):
+    return ivy.reshape(input, shape)
+
+
+reshape.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
