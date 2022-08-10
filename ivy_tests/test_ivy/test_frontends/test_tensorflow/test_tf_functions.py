6602d305f1743374630e570d3a77258fed5e7664
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-08-10 23:22:55 +0530

    Update test_tf_functions.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -189,3 +189,35 @@ def test_tensorflow_full(
         value=fill_value,
         rtol=1e-05,
     )
+
+#argmax    
+
+@given(
+    xs_n_input_dtypes_n_unique_idx=_arrays_idx_n_dtypes(),
+    as_variable=helpers.array_bools(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.argmax"
+    ),
+    native_array=helpers.array_bools(),
+)
+def test_tensorflow_argmax(
+    xs_n_input_dtypes_n_unique_idx,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    xs, input_dtypes, unique_idx = xs_n_input_dtypes_n_unique_idx
+    xs = [np.asarray(x, dtype=dt) for x, dt in zip(xs, input_dtypes)]
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_name="argmax",
+        values=xs,
+        axis=unique_idx,
+    )
