64221f75406f9013480e5a256251b2bf65221e99
Yunfan Shi <53264047+FrankShi9@users.noreply.github.com>
2022-08-10 17:07:07 +0100

    Update hard_sigmoid in test_tf_functions.py
    From old PR hard_sigmoid #2511 #2580

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -329,3 +329,29 @@ def test_tensorflow_divide(
         x=np.asarray(x[0], dtype=input_dtype[0]),
         y=np.asarray(x[1], dtype=input_dtype[1]),
     )
+    
+    
+ @given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_tf.valid_float_dtypes),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.hard_sigmoid"
+    ),
+    native_array=st.booleans(),
+    # fw='tensorflow.keras'
+)
+def test_tensorflow_hard_sigmoid(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_name="hard_sigmoid",
+        x=np.asarray(x, dtype=input_dtype),
+    )
