e8c20af91400575686352b1febccdcdd1951c14c
khadimrassoul.mbaye <khadimrassoul.mbaye@adie.sn>
2022-08-10 16:26:50 +0000

    tested successfully jax lax frontend square operator function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -262,33 +262,32 @@ def test_jax_lax_sqrt(
         x=np.asarray(x, dtype=input_dtype),
     )
 
-
-# square
-@given(
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_jax.valid_float_dtypes),
-    as_variable=st.booleans(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.jax.lax.square"
-    ),
-    native_array=st.booleans(),
-)
-def test_jax_lax_square(
-        dtype_and_x,
-        as_variable,
-        num_positional_args,
-        native_array,
-        fw,
-):
-    input_dtype, x = dtype_and_x
-
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw="torch",
-        frontend="jax",
-        fn_name="lax.square",
-        x=np.asarray(x, dtype=input_dtype),
+    # square
+    @given(
+        dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_jax.valid_float_dtypes),
+        as_variable=st.booleans(),
+        num_positional_args=helpers.num_positional_args(
+            fn_name="ivy.functional.frontends.jax.lax.square"
+        ),
+        native_array=st.booleans(),
     )
+    def test_jax_lax_square(
+            dtype_and_x,
+            as_variable,
+            num_positional_args,
+            native_array,
+            fw,
+    ):
+        input_dtype, x = dtype_and_x
+
+        helpers.test_frontend_function(
+            input_dtypes=input_dtype,
+            as_variable_flags=as_variable,
+            with_out=False,
+            num_positional_args=num_positional_args,
+            native_array_flags=native_array,
+            fw="torch",
+            frontend="jax",
+            fn_name="lax.square",
+            x=np.asarray(x, dtype=input_dtype),
+        )
