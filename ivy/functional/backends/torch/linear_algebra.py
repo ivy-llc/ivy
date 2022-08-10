26cf21776c2106f6ab0d1f494ff53113dab7e177
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2022-08-10 17:59:41 +0500

    Update linear_algebra.py

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -147,7 +147,15 @@ def matrix_rank(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     # ToDo: add support for default rtol value here, for the case where None is provided
-    ret = torch.linalg.matrix_rank(x, rtol=rtol, out=out)
+    if rtol is None and out is None:
+        ret = torch.linalg.matrix_rank(x)
+    elif rtol is None and out is not None:
+        ret = torch.linalg.matrix_rank(x, out=out)
+    elif rtol is not None and out is None:
+        ret = torch.linalg.matrix_rank(x,rtol)
+    else:
+        ret = torch.linalg.matrix_rank(x,rtol, out=out)
+
     ret = torch.tensor(ret, dtype=ivy.default_int_dtype(as_native=True))
     return ret
 
