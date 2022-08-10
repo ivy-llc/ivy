136a9c1c77290dafc00dfbffbb278366553483dc
zaeemansari70 <zaeemansari70@gmail.com>
2022-08-10 12:32:54 +0500

    Resolved conflicts and added more math functions

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -14,3 +14,31 @@ def tan(x, name=None):
 
 
 tan.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
+
+def multiply(x, y, name=None):
+    return ivy.multiply(x, y)
+
+
+multiply.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
+
+def subtract(x, y, name=None):
+    return ivy.subtract(x, y)
+
+
+subtract.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
+
+def logical_xor(x, y, name='LogicalXor'):
+    return ivy.logical_xor(x, y)
+
+
+logical_xor.supported_dtypes = {"torch": ("bool", "bool")}
+
+
+def divide(x, y, name=None):
+    return ivy.divide(x, y)
+
+
+divide.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
