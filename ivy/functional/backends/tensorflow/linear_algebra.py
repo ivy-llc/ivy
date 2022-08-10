aade9ff9719903b7332f2aacc1f4a3b085660445
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2022-08-10 17:55:00 +0500

    Update linear_algebra.py

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -248,8 +248,6 @@ def matrix_rank(
     elif tf.size(x) == 1:
         ret = tf.math.count_nonzero(x)
     else:
-        x = tf.reshape(x, [-1])
-        x = tf.expand_dims(x, 0)
         x, rtol = ivy.promote_types_of_inputs(x, rtol)
         ret = tf.linalg.matrix_rank(x, rtol)
     ret = tf.cast(ret, ivy.default_int_dtype(as_native=True))
