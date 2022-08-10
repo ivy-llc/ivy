7116e370834fb93e0ca5fede8724b7b94cb2ec76
Ana <17798041+lightspeedana@users.noreply.github.com>
2022-08-10 16:31:19 +0200

    Update pointwise_ops.py

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -60,3 +60,10 @@ def subtract(input, other, *, alpha=1, out=None):
 
 
 subtract.unsupported_dtypes = ("float16",)
+
+
+def asin(input, *, out=None):
+    return ivy.asin(input, out=out)
+
+
+asin.unsupported_dtypes = ("float16",)
