59c5e81465eb8e3efd10102a8abc0ebb00cc3812
djl11 <daniel.lenton11@imperial.ac.uk>
2022-08-11 18:53:28 +0100

    small fix for test_scatter_nd for numpy backend.
    Former-commit-id: 551a93b8649d0efcc4e5cc7cb0dd8d57e7d9804d

--- ivy_tests/test_ivy/test_functional/test_core/test_general.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_general.py
@@ -924,17 +924,13 @@ def test_scatter_nd(inds_n_upd_n_shape_tnsr_n_wdup, red, dtype, tensor_fn, call)
     if red == "replace" and with_duplicates:
         # replace with duplicates give non-deterministic outputs
         return
+    inds_np = ivy.to_numpy(inds)
+    upd_np = ivy.to_numpy(upd)
+    tensor_np = ivy.to_numpy(tensor) if ivy.exists(tensor) else tensor
+    with ivy.functional.backends.numpy.use:
+        true_np = ivy.scatter_nd(inds_np, upd_np, shape, tensor_np, red).data
     ret = call(ivy.scatter_nd, inds, upd, shape, tensor, red)
-    true = np.asarray(
-        ivy.functional.backends.numpy.scatter_nd(
-            ivy.to_numpy(inds),
-            ivy.to_numpy(upd),
-            shape,
-            ivy.to_numpy(tensor) if ivy.exists(tensor) else tensor,
-            red,
-        )
-    )
-    assert np.allclose(ret, true)
+    assert np.allclose(ret, true_np)
 
 
 # gather
