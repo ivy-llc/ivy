5622959977abc00a0787f1da11ddfb2f78814289
Saad Malik <malik.sid1980@gmail.com>
2022-08-11 06:52:06 +0500

    LogicalOr

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -331,22 +331,22 @@ def test_tensorflow_divide(
     )
 
     
-# logical_not
+# logical_or
 @given(
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=tuple(
             [ivy.bool]
         ),
-        num_arrays=1,
+        num_arrays=2,
         shared_dtype=True,
     ),
-    as_variable=helpers.list_of_length(x=st.booleans(), length=1),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.logical_not"
+        fn_name="ivy.functional.frontends.tensorflow.logical_or"
     ),
-    native_array=helpers.list_of_length(x=st.booleans(), length=1),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
 )
-def test_tensorflow_logical_not(
+def test_tensorflow_logical_or(
     dtype_and_x, as_variable, num_positional_args, native_array, fw
 ):
     input_dtype, x = dtype_and_x
@@ -358,6 +358,8 @@ def test_tensorflow_logical_not(
         native_array_flags=native_array,
         fw=fw,
         frontend="tensorflow",
-        fn_name="math.logical_not",
+        fn_name="math.logical_or",
         x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
     )
+    
