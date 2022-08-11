1e1f470cbaba1b375fa8f91fd10fb9a28a08e6d6
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2022-08-11 10:29:18 +0500

    Update test_tf_linalg_functions.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_linalg_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_linalg_functions.py
@@ -1,25 +1,25 @@
 # global
-import ivy
 import numpy as np
 from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
 
 @given(
     dtype_x=helpers.dtype_and_values(
-        available_dtypes= ivy_np.valid_float_dtypes[1:],
-        min_num_dims= 2,
-        min_value= -10000000000.0,
-        max_value= 10000000000.0
+        available_dtypes=ivy_np.valid_float_dtypes[1:],
+        min_num_dims=2,
+        min_value=-10000000000.0,
+        max_value=10000000000.0
     ),
-    as_variable= st.booleans(),
-    native_array= st.booleans(),
-    num_positional_args= helpers.num_positional_args(
-      fn_name= "ivy.functional.frontends.tensorflow.matrix_rank"
+    as_variable=st.booleans(),
+    native_array=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+    fn_name="ivy.functional.frontends.tensorflow.matrix_rank"
     ),
-    tolr= st.floats(allow_nan=False, allow_infinity=False) | st.just(None),
-    data= st.data(),
+    tolr=st.floats(allow_nan=False, allow_infinity=False) | st.just(None),
+    data=st.data()
 )
 def test_matrix_rank(
   *,
@@ -31,17 +31,17 @@ def test_matrix_rank(
   tolr,
   fw
 ):
-    input_dtype , x = dtype_x
+    input_dtype , x =dtype_x
     helpers.test_frontend_function(
-        input_dtypes= input_dtype,
-        as_variable_flags= as_variable,
-        with_out= False,
-        num_positional_args= num_positional_args,
-        native_array_flags= native_array,
-        fw= fw,
-        frontend= "tensorflow",
-        fn_name= "linalg.matrix_rank",
-        atol= 1.0,
-        a= np.asarray(x, dtype=input_dtype),
-        tol= tolr
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_name="linalg.matrix_rank",
+        atol=1.0,
+        a=np.asarray(x, dtype=input_dtype),
+        tol=tolr
     )
