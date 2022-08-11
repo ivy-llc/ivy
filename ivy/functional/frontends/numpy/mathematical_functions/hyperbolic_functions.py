79673ee670b1881d62ae3c25ac3391606be935d1
Darshan Tank <61326242+Darshan2104@users.noreply.github.com>
2022-08-11 17:47:25 +0530

    frontend sinh (#2722)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

---
+++ ivy/functional/frontends/numpy/mathematical_functions/hyperbolic_functions.py
@@ -0,0 +1,26 @@
+# global
+import ivy
+from ivy.func_wrapper import from_zero_dim_arrays_to_float
+
+
+@from_zero_dim_arrays_to_float
+def sinh(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.sinh(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+sinh.unsupported_dtypes = {"torch": ("float16",)}
