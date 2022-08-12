2c7a7ec967c8054ef582fa102425e2dad67cfe04
JannisEbling <65505435+JannisEbling@users.noreply.github.com>
2022-08-12 12:59:25 +0200

    Update arithmetic_operations.py

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -118,6 +118,29 @@ def square(
 square.unsupported_dtypes = {"torch": ("float16",)}
 
 
+# absolute
+def absolute(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.absolute(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+absolute.unsupported_dtypes = {"torch": ("float16",)}
+
+
 def vdot(
     a,
     b,
