26f20533203779531f2b9cbfe1f82cf5e8a68fdd
Emperor-WS <76920103+Emperor-WS@users.noreply.github.com>
2022-08-12 19:16:12 +0200

    Update averages_and_variances.py
    to resolve some errors

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -12,8 +12,11 @@ def mean(
     where=True
 ):
     if dtype:
-        x = [ivy.astype(ivy.array(i), ivy.as_ivy_dtype(dtype)) for i in x]
-    return ivy.mean(x, axis=axis, keepdims=keepdims, out=out)
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.mean(x, axis=axis, keepdims=keepdims, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
 
 
 mean.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
