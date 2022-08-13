1490b64009582c6eb0459ead1cb289f6d099ef56
Kanak Mittal <kanakmittal6@gmail.com>
2022-08-13 16:27:30 +0530

    Added tests for TensorFlow tanh

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_tf_functions.py
@@ -355,3 +355,29 @@ def test_tensorflow_negative(
         fn_name="negative",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+    
+# tanh
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_tf.valid_float_dtypes),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.tanh"
+    ),
+    native_array=st.booleans(),
+)
+def test_tensorflow_tanh(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="tanh",
+        x=np.asarray(x, dtype=input_dtype),
+    )
