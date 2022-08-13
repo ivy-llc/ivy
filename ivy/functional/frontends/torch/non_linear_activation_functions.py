3ced785b98896ecb45199c698b69d70a76f50bc3
Michael Schock <m@mjschock.com>
2022-08-13 15:57:01 -0700

    Add PyTorch Frontend for LayerNorm (WIP)

--- ivy/functional/frontends/torch/non_linear_activation_functions.py
+++ ivy/functional/frontends/torch/non_linear_activation_functions.py
@@ -1,5 +1,7 @@
 # global
+# from typing import List, Optional
 import ivy
+# import torch
 
 
 def sigmoid(input, out=None):
@@ -16,3 +18,26 @@ def softmax(input, dim=None, dtype=None):
 
 
 softmax.unsupported_dtypes = ("float16",)
+
+
+# def layer_norm(
+#     input: torch.Tensor,
+#     normalized_shape: List[int],
+#     weight: Optional[torch.Tensor] = None,
+#     bias: Optional[torch.Tensor] = None,
+#     eps: float = ivy._MIN_BASE,
+#     new_std: Optional[float] = 1.0,
+#     out: Optional[torch.Tensor] = None
+# ) -> torch.Tensor:
+#     return ivy.layer_norm(
+#         x=input,
+#         normalized_idxs=normalized_shape,
+#         epsilon=eps,
+#         scale=weight,
+#         offset=bias,
+#         new_std=new_std,
+#         out=out
+#     )
+
+
+# layer_norm.unsupported_dtypes = ("float16",)
