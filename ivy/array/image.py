c311be391e0a57a8715b8dbfab829dad913ad140
Aarsh2001 <ac5973@srmist.edu.in>
2022-08-13 12:20:37 +0530

    push dashboard tools to another branch

--- ivy/array/image.py
+++
@@ -1,8 +0,0 @@
-# global
-import abc
-
-# ToDo: implement all methods here as public instance methods
-
-
-class ArrayWithImage(abc.ABC):
-    pass
--- ivy/functional/frontends/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-from . import numpy
-from . import jax
-from . import torch
-from . import tensorflow
