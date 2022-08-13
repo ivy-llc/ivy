c03aaae0b21cd97cfaec0c3ba82e8c61647531dc
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-08-13 08:00:14 +0000

    Fix lint failures in test_dashboard.py and frontends/tensorflow/math.py

--- automation_tools/dashboard_automation/tests_dashboard.py
+++ automation_tools/dashboard_automation/tests_dashboard.py
@@ -4,10 +4,10 @@ import json
 import emoji
 import pandas as pd
 from github import Github
-from typing import Dict, Union, Tuple
+from typing import Dict, Union
 
 
-url = "https://api.github.com/repos/unifyai/ivy/actions/runs?branch=master&status=completed&per_page=100&page=2"
+url = "https://api.github.com/repos/unifyai/ivy/actions/runs?branch=master&status=completed&per_page=100&page=2"  # noqa
 
 headers = {
     "Accept": "application/vnd.github+json",
@@ -22,7 +22,7 @@ config: Dict[Union[str, int], Union[str, type(emoji)]] = {
     1: "functional_nn_dashboard",
     2: "stateful_dashboard",
     "success": emoji.emojize(":white_check_mark:", language="alias"),
-    "failure": emoji.emojize(":x:", language="alias")
+    "failure": emoji.emojize(":x:", language="alias"),
 }
 results = []
 
@@ -31,6 +31,7 @@ def get_api_results(url, token, headers):
     headers["Authorization"] = "Bearer " + token
     response = requests.request("GET", url, headers=headers)
     return json.loads(response.text)
+
 
 def get_DataFrame(result_dict: dict) -> pd.DataFrame:
     data = pd.DataFrame.from_dict(
@@ -41,8 +42,11 @@ def get_DataFrame(result_dict: dict) -> pd.DataFrame:
         data.at[index_label] = [row_series.values[i][1] for i in range(4)]
     return data
 
+
 def make_clickable(url, name):
-    return '<a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(url,name)
+    return '<a href="{}" rel="noopener noreferrer" target="_blank">{}</a>'.format(
+        url, name
+    )
 
 
 def workflow_results(token):
@@ -83,15 +87,30 @@ def get_matrix_job_data(token):
             if name == "test-core-ivy":
                 if submodule not in functional_core_dict:
                     functional_core_dict[submodule] = []
-                functional_core_dict[submodule].append((backend, make_clickable(info['html_url'], config[info['conclusion']])))
+                functional_core_dict[submodule].append(
+                    (
+                        backend,
+                        make_clickable(info["html_url"], config[info["conclusion"]]),
+                    )
+                )
             elif name == "test-nn-ivy":
                 if submodule not in functional_nn_dict:
                     functional_nn_dict[submodule] = []
-                functional_nn_dict[submodule].append((backend, make_clickable(info['html_url'], config[info['conclusion']])))
+                functional_nn_dict[submodule].append(
+                    (
+                        backend,
+                        make_clickable(info["html_url"], config[info["conclusion"]]),
+                    )
+                )
             elif name == "test-stateful-ivy":
                 if submodule not in stateful_dict:
                     stateful_dict[submodule] = []
-                stateful_dict[submodule].append((backend, make_clickable(info['html_url'], config[info['conclusion']])))
+                stateful_dict[submodule].append(
+                    (
+                        backend,
+                        make_clickable(info["html_url"], config[info["conclusion"]]),
+                    )
+                )
 
     return (functional_core_dict, functional_nn_dict, stateful_dict)
 
@@ -99,12 +118,18 @@ def get_matrix_job_data(token):
 def main():
     token = str(sys.argv[1])
     g = Github(token)
-    repo = g.get_repo('unifyai/ivy')
+    repo = g.get_repo("unifyai/ivy")
     ivy_modules = get_matrix_job_data(token)
     for i, module in enumerate(ivy_modules):
         module_df = get_DataFrame(module)
-        file = repo.get_contents(f'test_dashboards/{config[i]}.md', ref = "master")
-        repo.update_file(file.path, f"update {config[i]}", module_df.to_markdown(), file.sha, branch ="master")
+        file = repo.get_contents(f"test_dashboards/{config[i]}.md", ref="master")
+        repo.update_file(
+            file.path,
+            f"update {config[i]}",
+            module_df.to_markdown(),
+            file.sha,
+            branch="master",
+        )
 
 
 if __name__ == "__main__":
