ce6f70068425daa8b36d23f14d246f414a17f715
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-08-13 13:01:48 +0100

    Fix a small mistake

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -101,7 +101,7 @@ def test_permute(
     helpers.test_frontend_function(
         input_dtypes=dtype,
         as_variable_flags=as_variable,
-        with_out=False
+        with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
