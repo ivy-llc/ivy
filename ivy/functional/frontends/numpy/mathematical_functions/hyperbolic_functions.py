2a8b5d8029d0db249aa21f50b95476a3e1c81afa
aryanxk02 <aryan.kenchappagol@gmail.com>
2022-08-13 11:32:39 +0530

    Add Numpy Frontend cosh hyperbolic function 🚀

--- ivy/functional/frontends/numpy/mathematical_functions/hyperbolic_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/hyperbolic_functions.py
@@ -24,3 +24,25 @@ def sinh(
 
 
 sinh.unsupported_dtypes = {"torch": ("float16",)}
+
+@from_zero_dim_arrays_to_float
+def cosh(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting='same_kind',
+    order='k',
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.cosh(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+sinh.unsupported_dtypes = {"torch": ("float16",)}
