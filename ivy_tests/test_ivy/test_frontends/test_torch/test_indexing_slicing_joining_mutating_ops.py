0facf004cf53391ad2e192e106703893322a2b4f
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-13 18:00:58 +0800

    update test_permute
    1. removing with_out per instructions
    2. adding a ret_tuple to helpers.get_axis to return a tuple only; instead of int or tuple(int)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -80,28 +80,27 @@ def test_torch_cat(
         available_dtypes=tuple(
             set(ivy_np.valid_float_dtypes).intersection(
                 set(ivy_torch.valid_float_dtypes))),
-        shape=helpers.get_shape(min_num_dims=1),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    axis = helpers.get_axis(shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"), ret_tuple=True),
     ),
     as_variable=helpers.array_bools(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.permute"),
     native_array=helpers.array_bools(),
-    with_out=st.booleans(),
 )
 @handle_cmd_line_args
 def test_permute(
         dtype_values_axis,
+        axis,
         as_variable,
         num_positional_args,
         native_array,
-        with_out,
         fw,
 ):
     dtype, value, axis = dtype_values_axis
     helpers.test_frontend_function(
         input_dtypes=dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
@@ -109,5 +108,4 @@ def test_permute(
         fn_name="permute",
         input=np.asarray(value, dtype=dtype),
         dims=axis,
-        out=None
     )
