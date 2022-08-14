ef6f6cc73a1a9c89f9fc80575603fabb662ec02c
Aïmane Harrak <aimane.harrak@gmail.com>
2022-08-14 07:06:33 +0100

    updated any method and removed redundant lines

--- ivy/functional/frontends/numpy/utility_routines/matrix_any.py
+++ ivy/functional/frontends/numpy/utility_routines/matrix_any.py
@@ -10,8 +10,7 @@ def any(
     *,
     where=True
 ):
-    ret = ivy.array(x)
-    ret = ivy.where(ivy.array(where), x, ivy.zeros_like(x))
+    ret = ivy.where(ivy.array(where), ivy.array(x), ivy.zeros_like(x))
     ret = ivy.any(ret, axis=axis, keepdims=keepdims, out=out)
     if len(ret.shape) == 0:
         ret = bool(ret) 
