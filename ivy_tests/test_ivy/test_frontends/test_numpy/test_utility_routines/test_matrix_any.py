c946532446b13c737e21ab0146a868c071bf4680
Aïmane Harrak <aimane.harrak@gmail.com>
2022-08-14 06:37:59 +0100

    resolved some formatting issues and added num_positional_args to any method test func

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_utility_routines/test_matrix_any.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_utility_routines/test_matrix_any.py
@@ -3,79 +3,88 @@ import numpy as np
 from hypothesis import given, strategies as st
 
 # local
-import ivy
 import ivy_tests.test_ivy.helpers as helpers
-import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 
 
 @st.composite
 def _array_with_dtype_axis_keepdims_and_where(draw):
     dtypes = draw(
-                helpers.array_dtypes(
-                    num_arrays=1
+        helpers.array_dtypes(
+            num_arrays=1
         )
     )
     shape = draw(
-                helpers.get_shape(
-                    min_num_dims=1,
-                    max_num_dims=5,
+        helpers.get_shape(
+           min_num_dims=1,
+           max_num_dims=5
         )
     )
     axis = draw(
-               helpers.integers(min_value=-1, max_value=len(shape)-1)
+        helpers.integers(min_value=-1, max_value=len(shape) - 1)
     )
     if axis == -1:
         axis = None
     x = draw(
-            helpers.array_values(
-                shape=shape,
-                dtype=dtypes[0]
+        helpers.array_values(
+            shape=shape,
+            dtype=dtypes[0]
         )
     )
-    where_shape_length = draw(helpers.integers(min_value=0,max_value=len(shape)))
+    where_shape_length = draw(helpers.integers(min_value=0, max_value=len(shape)))
     if where_shape_length != 0:
-        where_nb_dims_to_change = draw(helpers.integers(min_value=0, max_value=where_shape_length-1))
-        where_dims_to_change = [
-                                   draw(
-                                       helpers.integers(min_value=0, max_value=where_shape_length-1)
-            ) for i in range(where_nb_dims_to_change)
+        where_nb_dims_to_change = draw(
+            helpers.integers(
+                min_value=0,
+                max_value=where_shape_length - 1
+            )
+        )
+        where_dims_to_change = [draw(
+            helpers.integers(
+                min_value=0,
+                max_value=where_shape_length - 1
+            )) for i in range(where_nb_dims_to_change)
         ]
-        where_dims_list = [1]*where_shape_length
+        where_dims_list = [1] * where_shape_length
         for dim in where_dims_to_change:
             where_dims_list[dim] = shape[::-1][dim]
         where_dims_list = where_dims_list[::-1]
         where = draw(
-                    helpers.array_values(
-                        shape=where_dims_list,
-                        dtype="bool"
-            )
+            helpers.array_values(
+                shape=where_dims_list,
+                dtype="bool"
+           )
         )
     else:
         where = draw(
-        st.booleans()
+            st.booleans()
         ) 
     keepdims = draw(
-                   st.booleans()
+        st.booleans()
     )
     return x, dtypes[0], axis, keepdims, where
+
 
 @given(
     x_dtype_axis_keepdims_where=_array_with_dtype_axis_keepdims_and_where(),
     as_variable=st.booleans(),
     with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.any"
+    ),
     native_array=st.booleans()
 )
 def test_numpy_any(
     x_dtype_axis_keepdims_where,
     as_variable,
     with_out,
+    num_positional_args,
     native_array,
     fw
 ):
     x, input_dtype, axis, keepdims, where = x_dtype_axis_keepdims_where
     x = np.asarray(x, dtype=input_dtype)
-    num_positional_args = 1
+    num_positional_args = num_positional_args
     input_dtypes = [input_dtype]
     as_variable = [as_variable]
     native_array = [native_array]
@@ -100,5 +109,3 @@ def test_numpy_any(
         keepdims=keepdims,
         where=where
     )
-
-
