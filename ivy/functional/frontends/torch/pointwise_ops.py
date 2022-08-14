cc31148f2be153922cd633648eb47bb0f8e8e282
Kunwar M. Saaim <kunwarsaaim@gmail.com>
2022-08-14 22:16:35 +0530

    added torch frontend arcsin function and test (#2994)
    Former-commit-id: d9b102e39441d6f460677d2a4b1d0ac28635f81d

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -90,6 +90,13 @@ def asin(input, *, out=None):
 asin.unsupported_dtypes = ("float16",)
 
 
+def arcsin(input, *, out=None):
+    return ivy.asin(input, out=out)
+
+
+arcsin.unsupported_dtypes = ("float16",)
+
+
 def asinh(input, *, out=None):
     return ivy.asinh(input, out=out)
 
