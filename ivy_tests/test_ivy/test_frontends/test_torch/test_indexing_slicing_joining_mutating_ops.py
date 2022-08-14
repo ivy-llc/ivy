0df19bac9e618a586cd5f4d359edf0ee41070873
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-14 11:20:49 +0800

    fix `fn_name` to `fn_tree` 2

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -87,12 +87,13 @@ def test_torch_cat(
     dtype_values_axis=helpers.dtype_values_axis(
         available_dtypes=tuple(
             set(ivy_np.valid_float_dtypes).intersection(
-                set(ivy_torch.valid_float_dtypes)),
+                set(ivy_torch.valid_float_dtypes)
+            ),
         ),
         shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
     ),
     axis=helpers.get_axis(
-        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"), 
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
         ret_tuple=True,
     ),
     as_variable=helpers.array_bools(),
@@ -119,7 +120,7 @@ def test_permute(
         native_array_flags=native_array,
         fw=fw,
         frontend="torch",
-        fn_name="permute",
+        fn_tree="permute",
         input=np.asarray(value, dtype=dtype),
         dims=axis,
     )
