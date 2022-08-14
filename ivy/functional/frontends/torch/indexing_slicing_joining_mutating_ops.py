634fd0a3ebd44a6498d70eae7af3a437407475d0
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-14 10:39:48 +0800

    add concat
    to frontends.torch

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -6,6 +6,10 @@ def cat(tensors, dim=0, *, out=None):
     return ivy.concat(tensors, dim, out=out)
 
 
+def concat(tensors, dim=0, *, out=None):
+    return ivy.concat(tensors, dim, out=out)
+
+
 def permute(input, dims):
     return ivy.permute_dims(input, dims)
 
