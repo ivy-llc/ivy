e80ebd00e382112df78d92e9e299227fc12a4263
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-14 11:46:13 +0800

    fix `fn_name` to `fn_tree` in `test_frontend_function` (#2991)

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -49,5 +49,6 @@ def negative(x, name=None):
 
 
 negative.unsupported_dtypes = {
-    "torch": ("float16", "bfloat16", "uint8", "uint16", "uint32", "uint64")
+    "torch": ("float16", "bfloat16", "uint8", "uint16", "uint32", "uint64"),
+    "tensorflow": ("float16", "bfloat16", "uint8", "uint16", "uint32", "uint64"),
 }
