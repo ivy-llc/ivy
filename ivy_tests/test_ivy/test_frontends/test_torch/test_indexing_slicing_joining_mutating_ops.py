cbda87ec6123761e2aa842967874cde6ad1598fc
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-14 10:23:47 +0800

    add test_swapdims
    to test_frontends.torch

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -123,3 +123,55 @@ def test_permute(
         input=np.asarray(value, dtype=dtype),
         dims=axis,
     )
+
+    
+# swapdims
+@given(
+    dtype_value=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes))),
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2),
+            key='shape'),
+    ),
+    dim0=helpers.get_axis(
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2), 
+            key='shape')
+    ).filter(lambda axis: isinstance(axis, int)),
+    dim1=helpers.get_axis(
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2), 
+            key='shape')
+    ).filter(lambda axis: isinstance(axis, int)),
+    as_variable=helpers.array_bools(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.swapdims"
+    ),
+    native_array=helpers.array_bools(),
+)
+@handle_cmd_line_args
+def test_torch_swapdims(
+    dtype_value,
+    dim0,
+    dim1,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, value = dtype_value
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_name="swapdims",
+        input=np.asarray(value, dtype=input_dtype),
+        dim0=dim0,
+        dim1=dim1,
+    )
