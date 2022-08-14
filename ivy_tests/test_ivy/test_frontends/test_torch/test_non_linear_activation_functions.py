00d088dbc59fb52a649455c52de89b9446755a02
rubythalib33 <rubyabdullah14@gmail.com>
2022-08-14 21:25:25 +0700

    update torch gelu frontend

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -104,3 +104,79 @@ def test_torch_softmax(
         dim=axis,
         dtype=dtypes[0],
     )
+
+
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        ),
+        min_num_dims=1,
+    ),
+    as_variable=st.booleans(),
+    axis=st.integers(-1, 0),
+    dtypes=_dtypes(),
+    num_positional_args=helpers.num_positional_args(fn_name="softmax"),
+    native_array=st.booleans(),
+)
+def test_torch_softmax(
+    dtype_and_x,
+    as_variable,
+    axis,
+    dtypes,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_name="softmax",
+        input=np.asarray(x, dtype=input_dtype),
+        dim=axis,
+        dtype=dtypes[0],
+    )
+
+
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="gelu"),
+    native_array=st.booleans(),
+    approximate = st.sampled_from(["none", "tanh"])
+)
+def test_torch_gelu(
+    dtype_and_x,
+    num_positional_args,
+    native_array,
+    approximate,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.gelu",
+        input=np.asarray(x, dtype=input_dtype),
+        approximate=approximate,
+    )
