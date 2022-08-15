e0e164c6677fa628b9ce8a599952a8bc8511d653
Dang Linh Anh <70094015+anhdang000@users.noreply.github.com>
2022-08-15 15:19:57 +0700

    numpy tanh frontend (#2758)
    * add numpy tanh function
    
    * pass num_arrays=1 in array_bools
    
    * pass num_arrays=1 in array_bools (L116)
    
    * use fn_tree instead of fn_name
    
    * add decorator from_zero_dim_arrays_to_float
    
    * resolve merge conflict

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -118,3 +118,26 @@ def arccos(
 
 
 arcsin.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
+def tanh(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.tanh(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+tanh.unsupported_dtypes = {"torch": ("float16",)}
