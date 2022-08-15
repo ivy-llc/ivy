b3b32b6ace758df164d2b5a6377d78b5ffd55405
gunesevitan <gunesevitan@gmail.com>
2022-08-15 12:49:18 +0300

    order arg

--- ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
@@ -2,5 +2,5 @@ import ivy
 from typing import Sequence, Any, Union
 
 
-def reshape(a: Sequence[Any], newshape: Union[Sequence[int], int]) -> Sequence[Any]:
+def reshape(a: Sequence[Any], newshape: Union[Sequence[int], int], order='C') -> Sequence[Any]:
     return ivy.reshape(a, newshape)
