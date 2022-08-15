f2835f8b040a90dfee2d9e6e162cf1055064342c
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-15 14:08:11 +0800

    Update test_miscellaneous_ops.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -33,6 +33,7 @@ def test_torch_flipud(
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
+        with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
