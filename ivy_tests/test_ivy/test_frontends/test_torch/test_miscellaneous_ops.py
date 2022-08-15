c2c0a85292f67426fc5817f10fb7dd270a518468
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-15 13:48:14 +0800

    formatting

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -10,25 +10,26 @@ import ivy.functional.backends.torch as ivy_torch
 
 # flipud
 @given(
-    dtype_value=
-        helpers.dtype_and_values(
-            available_dtypes=tuple(
-                set(ivy_np.valid_float_dtypes).intersection(
-                    set(ivy_torch.valid_float_dtypes)))),
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)),
+        ),
+    ),
     as_variable=st.booleans(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.torch.flipud"),
+        fn_name="ivy.functional.frontends.torch.flipud"
+    ),
     native_array=st.booleans(),
 )
 def test_torch_flipud(
-    *,
-    dtype_value,
+    dtype_and_values,
     as_variable,
     num_positional_args,
     native_array,
     fw,
 ):
-    input_dtype, value = dtype_value
+    input_dtype, value = dtype_and_values
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -38,4 +39,4 @@ def test_torch_flipud(
         frontend="torch",
         fn_name="flipud",
         input=np.asarray(value, dtype=input_dtype),
-    )    
+    )
