f906cc9d92dbaf1a17548be832af3d2f52f95e4c
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-15 15:32:19 +0800

    torch frontend - add` swapdims` (#2986)
    Co-authored-by: juliagsy <67888047+juliagsy@users.noreply.github.com>
    
    Former-commit-id: e75f22f77b613df0b8988b29735f152303bcba5c

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -122,3 +122,59 @@ def test_torch_permute(
         input=np.asarray(value, dtype=dtype),
         dims=axis,
     )
+
+    
+# swapdims
+@given(
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes))
+        ),
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2),
+            key='shape'
+        ),
+    ),
+    dim0=helpers.get_axis(
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2), 
+            key='shape'
+        ),
+    ).filter(lambda axis: isinstance(axis, int)),
+    dim1=helpers.get_axis(
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=2), 
+            key='shape'
+        ),
+    ).filter(lambda axis: isinstance(axis, int)),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.swapdims"
+    ),
+    native_array=st.booleans(),
+)
+@handle_cmd_line_args
+def test_torch_swapdims(
+    dtype_and_values,
+    dim0,
+    dim1,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, value = dtype_and_values
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="swapdims",
+        input=np.asarray(value, dtype=input_dtype),
+        dim0=dim0,
+        dim1=dim1,
+    )
