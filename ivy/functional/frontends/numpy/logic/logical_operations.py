fd805fe70a65b9354e3b276b897221170cba1c6a
teddywaweru <teddywaweru@gmail.com>
2022-08-16 10:20:38 +0300

    defined numpy equal frontend

---
+++ ivy/functional/frontends/numpy/logic/logical_operations.py
@@ -0,0 +1,26 @@
+#global
+import ivy
+
+#local
+from ivy.func_wrapper import from_zero_dim_arrays_to_float
+
+@from_zero_dim_arrays_to_float
+def equal(
+    x1,
+    x2,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="samekind",
+    order="K",
+    dtype=None,
+    subok=True
+):
+    if dtype:
+        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+    ret = ivy.equal(x1,x2,out=out)
+    if ivy.is_array(where):
+        ret  = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
