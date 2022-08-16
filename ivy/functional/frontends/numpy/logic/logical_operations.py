80fbaef23713dc9551ea6c97820796471c7e6235
teddywaweru <teddywaweru@gmail.com>
2022-08-16 11:46:55 +0300

    fix lint formatting error

--- ivy/functional/frontends/numpy/logic/logical_operations.py
+++ ivy/functional/frontends/numpy/logic/logical_operations.py
@@ -1,8 +1,9 @@
-#global
+# global
 import ivy
 
-#local
+# local
 from ivy.func_wrapper import from_zero_dim_arrays_to_float
+
 
 @from_zero_dim_arrays_to_float
 def equal(
@@ -20,7 +21,7 @@ def equal(
     if dtype:
         x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
         x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
-    ret = ivy.equal(x1,x2,out=out)
+    ret = ivy.equal(x1, x2, out=out)
     if ivy.is_array(where):
-        ret  = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_logical_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_logical_operations.py
@@ -1,8 +1,8 @@
-#global
+# global
 import numpy as np
 from hypothesis import given, strategies as st
 
-#local
+# local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
