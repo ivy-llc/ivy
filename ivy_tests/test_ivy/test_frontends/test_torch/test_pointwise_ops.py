bd350af81d699757bc80c8559e9ea2776258d523
Mohammad-Talaat7 <110403191+Mohammad-Talaat7@users.noreply.github.com>
2022-08-16 06:14:00 +0200

    Update test_pointwise_ops.py
    Add test_torch_arctanh function

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -687,3 +687,42 @@ def test_torch_asinh(
         input=np.asarray(x, dtype=input_dtype),
         out=None,
     )
+
+
+# arctanh
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.arctanh"
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_arctanh(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="arctanh",
+        input=np.asarray(x, dtype=input_dtype),
+        out=None,
+    )
