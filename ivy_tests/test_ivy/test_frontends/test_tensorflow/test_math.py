9a7d1a13a964ed40f3905f49735eea6457d5518c
ream-kh <73292275+ream-kh@users.noreply.github.com>
2022-08-16 19:43:03 +0300

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -230,3 +230,34 @@ def test_tensorflow_negative(
         fn_tree="negative",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+    
+   # divide_no_nan
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_tf.valid_float_dtypes[:2] + ivy_tf.valid_uint_dtypes,
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.divide_no_nan"
+    ),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
+)
+def test_tensorflow_divide_no_nan(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="divide_no_nan",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
+    )
