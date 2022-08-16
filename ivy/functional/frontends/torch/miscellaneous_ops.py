b5d74ae9413f295780c9f335712255bd5f0cc14b
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-16 14:23:23 +0800

    add fliplr
    to frontends.torch via ivy.flip

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -10,3 +10,15 @@ flip.unsupported_dtypes = (
     "uint32",
     "uint64",
 )
+
+
+def fliplr(input):
+    assert len(input.shape) >= 2, "Requires the tensor to be at least 2-D"
+    return ivy.flip(input, (-1,))
+
+
+fliplr.unsupported_dtypes = (
+    "uint16",
+    "uint32",
+    "uint64",
+)
