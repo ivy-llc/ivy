4f7e11d14129ce974ead49057f09e97e2a03a61f
Tosin Akingbemisilu <tosinakingbemisilu@Tosins-MacBook-Air.local>
2022-08-16 21:33:05 +0100

    added mae to tensorflow frontend.

---
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -0,0 +1,5 @@
+import ivy
+
+
+def mae(y_true, y_pred):
+    return ivy.mae(y_true, y_pred)
---
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_metrics.py
@@ -0,0 +1,34 @@
+# global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.tensorflow as ivy_tf
+
+
+# mae
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_tf.valid_float_dtypes),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.mae"
+    ),
+    native_array=st.booleans(),
+)
+def test_tensorflow_mae(
+        dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="mae",
+        y_true=np.asarray(x, dtype=input_dtype),
+        y_pred=np.asarray(x, dtype=input_dtype),
+    )
