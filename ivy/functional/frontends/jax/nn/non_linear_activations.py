b8c312d87602fb54d5bbaa86e036163bf745e379
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-08-16 10:26:15 +0100

    test-frontend-jax passing (keyword argument fixes)

--- ivy/functional/frontends/jax/nn/non_linear_activations.py
+++ ivy/functional/frontends/jax/nn/non_linear_activations.py
@@ -9,14 +9,14 @@ relu.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
 
 
 def leaky_relu(x, negative_slope=0.01):
-    return ivy.leaky_relu(x, negative_slope)
+    return ivy.leaky_relu(x, alpha=negative_slope)
 
 
 leaky_relu.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
 
 
 def gelu(x, approximate=True):
-    return ivy.gelu(x, approximate)
+    return ivy.gelu(x, approximate=approximate)
 
 
 gelu.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
