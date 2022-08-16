2ab1b8a88f78e1153d380e8c75626745ba35a065
teddywaweru <teddywaweru@gmail.com>
2022-08-16 11:42:09 +0300

    fix lint formatting error

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_logical_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_logical_operations.py
@@ -1,13 +1,11 @@
 #global
 import numpy as np
-
 from hypothesis import given, strategies as st
 
 #local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
-
 
 
 # equal
