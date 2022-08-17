a83bee309bce96ebaa661110bde15a16bb52e106
CatB1t <skytedits@gmail.com>
2022-08-17 14:07:03 +0200

    add eq to jax frontend.
    Former-commit-id: 390c0790e078b0a56b05bf6ec3b9bd1126fc9e38

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -580,3 +580,39 @@ def test_jax_lax_mul(
         x=xs[0],
         y=xs[1],
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtypes_and_xs=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(ivy.valid_numeric_dtypes)
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.eq"
+    ),
+)
+def test_jax_lax_eq(
+    dtypes_and_xs,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtypes, xs = dtypes_and_xs
+    xs = [np.asarray(x, dtype=dt) for x, dt in zip(xs, input_dtypes)]
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.eq",
+        x=xs[0],
+        y=xs[1],
+    )
