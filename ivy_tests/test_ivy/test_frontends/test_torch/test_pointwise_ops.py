11b1989bf2ee0dd282cf7add1890253a823a8530
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-08-17 17:34:11 +0530

    Adding `torch.square` to the torch frontend (#3103)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -775,3 +775,42 @@ def test_torch_arctanh(
         input=np.asarray(x, dtype=input_dtype),
         out=None,
     )
+
+
+# square
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            )
+        )
+    ),
+    as_variable=st.booleans(),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.square"
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_square(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="square",
+        input=np.asarray(input, dtype=input_dtype),
+        out=None,
+    )
