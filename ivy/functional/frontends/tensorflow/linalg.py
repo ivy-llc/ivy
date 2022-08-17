ce085afc93dfe1b7d53e97e6bd6a15794e5b195f
Rahul Kadam <kadamrahulj0909@gmail.com>
2022-08-17 21:34:44 +0530

    added trace linear algebra function for tensorflow frontend

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -6,3 +6,10 @@ def det(input, name=None):
 
 
 det.unsupported_dtypes = ("float16", "bfloat16")
+
+
+def trace(input, offset):
+    return ivy.trace(input, offset)
+
+
+det.unsupported_dtypes = ("float16", "bfloat16")
