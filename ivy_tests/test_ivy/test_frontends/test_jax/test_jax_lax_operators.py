cfb425485be1d14d174ad65f9d6b1555f296e78b
Pengfei Yan <yanllearnn@gmail.com>
2022-08-17 15:22:58 +0800

    add atan2 to jax (#3102)
    Co-authored-by: Rushali <rushaligrandhe@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -370,7 +370,7 @@ def test_jax_lax_sin(
         x=np.asarray(x, dtype=input_dtype),
     )
 
-    
+
 # sign
 @given(
     dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_jax.valid_float_dtypes),
@@ -399,8 +399,8 @@ def test_jax_lax_sign(
         frontend="jax",
         fn_tree="lax.sign",
         x=np.asarray(x, dtype=input_dtype),
-    )    
-    
+    )
+
 
 # asin
 @given(
@@ -461,4 +461,42 @@ def test_jax_lax_sinh(
         frontend="jax",
         fn_tree="lax.sinh",
         x=np.asarray(x, dtype=input_dtype),
+    )
+
+
+# atan2
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_jax.valid_float_dtypes))
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.atan2"
+    ),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
+)
+def test_jax_lax_atan2(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.atan2",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
     )
