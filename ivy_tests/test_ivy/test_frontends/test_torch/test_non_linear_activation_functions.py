376919a589cae79acae3c98530a25e3327ab6580
Aymen Sekhri <39885103+AymenSe@users.noreply.github.com>
2022-08-17 21:58:40 +0100

    fixing

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -6,6 +6,7 @@ from hypothesis import given, strategies as st
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 import ivy.functional.backends.torch as ivy_torch
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 @st.composite
@@ -25,6 +26,118 @@ def _dtypes(draw):
             ),
             key="dtype",
         )
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="sigmoid"),
+)
+def test_torch_sigmoid(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="sigmoid",
+        input=np.asarray(x, dtype=input_dtype),
+        out=None,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        ),
+        min_num_dims=1,
+    ),
+    axis=st.integers(-1, 0),
+    dtypes=_dtypes(),
+    num_positional_args=helpers.num_positional_args(fn_name="softmax"),
+)
+def test_torch_softmax(
+    dtype_and_x,
+    as_variable,
+    axis,
+    dtypes,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="softmax",
+        input=np.asarray(x, dtype=input_dtype),
+        dim=axis,
+        dtype=dtypes[0],
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        )
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.gelu"
+    ),
+    approximate=st.sampled_from(["none", "tanh"]),
+)
+def test_torch_gelu(
+    dtype_and_x,
+    num_positional_args,
+    native_array,
+    approximate,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.gelu",
+        input=np.asarray(x, dtype=input_dtype),
+        approximate=approximate,
     )
 
 
@@ -61,82 +174,4 @@ def test_torch_relu(
         frontend="torch",
         fn_name="relu",
         input=np.asarray(x, dtype=input_dtype),
-    )
-
-
-@given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=tuple(
-            set(ivy_np.valid_float_dtypes).intersection(
-                set(ivy_torch.valid_float_dtypes)
-            )
-        )
-    ),
-    as_variable=st.booleans(),
-    with_out=st.booleans(),
-    num_positional_args=helpers.num_positional_args(fn_name="sigmoid"),
-    native_array=st.booleans(),
-)
-def test_torch_sigmoid(
-    dtype_and_x,
-    as_variable,
-    with_out,
-    num_positional_args,
-    native_array,
-    fw,
-):
-    input_dtype, x = dtype_and_x
-
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="torch",
-        fn_name="sigmoid",
-        input=np.asarray(x, dtype=input_dtype),
-        out=None,
-    )
-
-
-@given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=tuple(
-            set(ivy_np.valid_float_dtypes).intersection(
-                set(ivy_torch.valid_float_dtypes)
-            )
-        ),
-        min_num_dims=1,
-    ),
-    as_variable=st.booleans(),
-    axis=st.integers(-1, 0),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(fn_name="softmax"),
-    native_array=st.booleans(),
-)
-def test_torch_softmax(
-    dtype_and_x,
-    as_variable,
-    axis,
-    dtypes,
-    num_positional_args,
-    native_array,
-    fw,
-):
-    input_dtype, x = dtype_and_x
-
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="torch",
-        fn_name="softmax",
-        input=np.asarray(x, dtype=input_dtype),
-        dim=axis,
-        dtype=dtypes[0],
     )
