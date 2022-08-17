96f367ebd8be1ad6a192684386eefa1af8e68404
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-08-17 11:42:19 +0000

    Removing unsupported dtypes

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -141,6 +141,3 @@ arctanh.unsupported_dtypes = ("float16",)
 
 def square(input, *, out=None):
     return ivy.square(input, out=out)
-
-
-square.unsupported_dtypes = ("uint16", "uint64", "uint32")
