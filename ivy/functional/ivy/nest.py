3006670cc5e4f3622a26856ebd82f8cac28e0bf3
git <ar.matoorian@gmail.com>
2022-08-17 14:17:54 +0100

    attempted to fix the docstring

--- ivy/functional/ivy/nest.py
+++ ivy/functional/ivy/nest.py
@@ -265,39 +265,39 @@ def map_nest_at_indices(nest: Iterable, indices: Tuple, fn: Callable, /):
     --------
     With :code:`List` inputs:
 
-    >>> nest = [[1, 2, 3, 4, 25, 6], [9, 24, 79, 82, 35, 64]]
+    >>> nest = [['a', 'c', 'e', 'd', 'u', 'k'], ['m', 'n', 'f', 'p', 'q', 't']]
     >>> indices = [[0, 4], [1, 5]]
-    >>> fn = math.sqrt(x)
-    >>> ivy.set_nest_at_indices(nest, indices, values)
+    >>> fn = lambda x : x + 'b'
+    >>> ivy.set_nest_at_indices(nest, indices, fn)
     >>> print(nest)
-    [[1, 2, 3, 4, 5, 6], [9, 24, 79, 82, 35, 8]]
+    [['a', 'c', 'e', 'd', 'ub', 'k'], ['m', 'n', 'f', 'p', 'q', 'tb']]
 
     With :code:`Tuple` inputs:
 
     >>> nest = ([-9, 8, -27],[9, -4, -5, 7])
     >>> indices = ((0, 2),(1, 0),(1, 2))
-    >>> fn = math.abs(x)
-    >>> ivy.set_nest_at_indices(nest, indices, values)
+    >>> fn = abs
+    >>> ivy.set_nest_at_indices(nest, indices, fn)
     >>> print(nest)
     ([-9, 8, 27], [9, -4, 5, 7])
 
     With :code:`Dict` input:
 
-    >>> nest = {'a': [8., 16., 22.], 'b': [10., 44., 81.], 'c': [9.]}
-    >>> indices = (('a', 1), ('b', 2), ('c', 0))
-    >>> fn = math.sqrt(x)
+    >>> nest = {'a': [8., 16., 22.], 'b': [10., 44., 81.], 'c': [9., 75., 37.]}
+    >>> indices = (('a', 2), ('b', 0), ('c', 1))
+    >>> fn = lambda x : x + 1
     >>> ivy.set_nest_at_indices(nest, indices, values)
     >>> print(nest)
-    {'a': [8.0, 4.0, 22.0], 'b': [10.0, 44.0, 9.0], 'c': [3.0]}
+    {'a': [8.0, 16.0, 23.0], 'b': [11.0, 44.0, 81.0], 'c': [9.0, 76.0, 37.0]}
 
     With :code:`ivy.Array` inputs:
 
-    >>> nest = ivy.array([[-9., 8., -17.],[22., -57., -37.]])
+    >>> nest = ivy.array([[-9., 8., -17.],[11., -3., 5.]])
     >>> indices = ((0, 1),(1, 1),(1, 2))
-    >>> values = math.abs(x)
+    >>> values = lambda x : x ** 2
     >>> ivy.set_nest_at_indices(nest, indices, values)
     >>> print(nest)
-    ivy.array([[-9., 8., -17.], [22., 57., 37.]])
+    ivy.array([[-9., 64., -17.], [11., 9., 25.]])
     """
     [map_nest_at_index(nest, index, fn) for index in indices]
 
