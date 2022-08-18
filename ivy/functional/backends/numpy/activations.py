fixed softmax overlfow

ivy/functional/backends/numpy/activations.py
@@ -51,7 +51,7 @@ def softmax(
     *,
     axis: Optional[int] = None,
 ) -> np.ndarray:
-    exp_x = np.exp(x)
+    exp_x = np.exp(x - np.max(x))
     return exp_x / np.sum(exp_x, axis, keepdims=True)
 
 