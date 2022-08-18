88506888772f32b8cce3d5c6c1fa09098b1e1b0e
ream-kh <73292275+ream-kh@users.noreply.github.com>
2022-08-18 14:20:23 +0300

    Add atan2 function and tests to pytorch frontend  (#3139)
    Co-authored-by: ankitdsi2010 <ankitbiswas1101@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -814,3 +814,46 @@ def test_torch_square(
         input=np.asarray(input, dtype=input_dtype),
         out=None,
     )
+    
+    
+# atan2
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
+    with_out=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.atan2"
+    ),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
+)
+def test_torch_atan2(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="atan2",
+        input=np.asarray(x[0], dtype=input_dtype[0]),
+        other=np.asarray(x[1], dtype=input_dtype[1]),
+        out=None,
+    )
