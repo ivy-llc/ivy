e951c3f97232bda569adbc175374bdb7c4f2391b
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-18 22:03:16 +0800

    add test_squeeze

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -294,3 +294,48 @@ def test_torch_stack(
         dim=dim,
         out=None,
     )
+
+    
+# squeeze
+@given(
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes))
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    ),
+    dim=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        max_size=1,
+    ).filter(lambda axis: isinstance(axis, int)),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.squeeze"
+    ),
+    native_array=st.booleans(),
+    with_out=st.booleans(),
+)
+def test_torch_squeeze(
+    dtype_and_values,
+    dim,
+    as_variable,
+    num_positional_args,
+    native_array,
+    with_out,
+    fw,
+):
+    input_dtype, value = dtype_and_values
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="squeeze",
+        input=np.asarray(value, dtype=input_dtype),
+        dim=dim,
+        out=None,
+    )
