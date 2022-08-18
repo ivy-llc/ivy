e031e68c1f13c121addee9cdf2a3a49c7bc85b2e
Mustafa Hani <skytedits@gmail.com>
2022-08-18 16:14:40 +0200

    added neg to JAX frontend.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -744,3 +744,35 @@ def test_jax_lax_bitwise_or(
         x=np.asarray(x[0], dtype=input_dtype[0]),
         y=np.asarray(x[1], dtype=input_dtype[1]),
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy.valid_numeric_dtypes).difference(ivy.all_uint_dtypes)
+        ),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.neg"
+    ),
+)
+def test_jax_lax_neg(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.neg",
+        x=np.asarray(x, dtype=input_dtype),
+    )
