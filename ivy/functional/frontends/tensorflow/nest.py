d24a67dc44e3a675f419f78f30554bd0eb0b0761
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-18 15:43:25 +0800

    50% ragged

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -1,7 +1,26 @@
 # global
+import ivy
+import tensorflow as tf
+
+
+RAGGED_TYPES = (tf.RaggedTensor,)
+# SPARSE_TYPES = (tf.)
 
 
 def flatten(structure, expand_composites=False):
+    if expand_composites and isinstance(structure, RAGGED_TYPES):
+        new_struc = []
+        for child in structure:
+            result = flatten(child, True)
+            new_struc.append(result[0])
+        structure = new_struc
+        new_struc = []
+        for child in structure:
+            new_struc += ivy.to_list(child)
+        return [ivy.native_array(new_struc)]
+    # if expand_composites and isinstance(structure, tf.RaggedTensor):
+    #     print('yes!')
+    #     return structure
     if isinstance(structure, (tuple, list)):
         new_struc = []
         for child in structure:
