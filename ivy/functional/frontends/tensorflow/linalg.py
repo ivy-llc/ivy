0a9c82402ad70874bc8e372a732b6af682caa24b
Yusha Arif <101613943+YushaArif99@users.noreply.github.com>
2022-08-19 00:26:57 +0500

    Update linalg.py

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -5,11 +5,8 @@ import ivy
 def matrix_rank(a, tol=None, valiate_args=False, name=None):
     return ivy.matrix_rank(a, tol)
 
-
-
 def det(input, name=None):
     return ivy.det(input)
 
 
 det.unsupported_dtypes = ("float16", "bfloat16")
-
