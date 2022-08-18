768027c26433dd31d287be21f80f9c1eab5465c4
LNemiro <ludwignemiro@yahoo.com>
2022-08-18 14:53:41 +0100

    exponential frontend function

--- ivy/functional/frontends/tensorflow/activations.py
+++ ivy/functional/frontends/tensorflow/activations.py
@@ -1,16 +1,13 @@
 import ivy
 
 
-def exponential(x, /):
-    if isinstance(x.shape, tuple) and len(x.shape) > 0:
-        return [exponential(value) for value in x]
-    else:
-        return ivy.exp(x)
+def exponential(x):
+    return ivy.exp(x)
 
 
-exponential.unsupported_dtypes = {
-    "numpy": ("bfloat16", "int8", "uint8", "uint16"),
-    "jax": ("int8", "uint8"),
-    "tensorflow": ("int8", "uint8"),
-    "torch": ("int8", "uint8"),
+exponential.supported_dtypes = {
+    "numpy": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
+    "jax": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
+    "tensorflow": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
+    "torch": ("float8", "float32", "float64", "float128"),
 }
