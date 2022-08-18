df9e2f2930e79bcb3f52967f561fa3ff3a63fa60
kunwarsaaim <kunwarsaaim@gmail.com>
2022-08-18 21:08:09 +0530

    added torch frontend `bitwise_and` function

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -142,3 +142,10 @@ def atan2(input, other, *, out=None):
 
 
 atan2.unsupported_dtypes = ("float16",)
+
+
+def bitwise_and(input, other, *, out=None):
+    return ivy.bitwise_and(input, other, out=out)
+
+
+bitwise_and.unsupported_dtypes = ("float16",)
