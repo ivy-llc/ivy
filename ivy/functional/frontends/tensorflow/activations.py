e14276a9d39d95028fb7cd59842d7800eb78767f
LNemiro <ludwignemiro@yahoo.com>
2022-08-18 15:00:33 +0100

    Adding Todo regarding supported dtypes in exponential

--- ivy/functional/frontends/tensorflow/activations.py
+++ ivy/functional/frontends/tensorflow/activations.py
@@ -5,6 +5,8 @@ def exponential(x):
     return ivy.exp(x)
 
 
+# TODO: This doesn't seem right. Exponential should work with all Number types
+
 exponential.supported_dtypes = {
     "numpy": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
     "jax": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
