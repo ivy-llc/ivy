a948d50c46ecd676d61956c63296f194227d3270
Roll20 <102127487+PatternFinder@users.noreply.github.com>
2022-08-18 20:12:20 +0800

    add roll frontends.torch (#2898)
    Co-authored by: @Oxbeefbabe ludwignemiro@yahoo.com

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -48,3 +48,56 @@ def test_torch_flip(
         input=np.asarray(value, dtype=input_dtype),
         dims=axis,
     )
+    
+    
+# roll
+@given(
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes))
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    ),
+    shift=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    ),
+    axis=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    ),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.roll"
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_roll(
+    dtype_and_values,
+    shift,
+    axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, value = dtype_and_values
+    if isinstance(shift, int) and isinstance(axis, tuple):
+        axis = axis[0]
+    if isinstance(shift, tuple) and isinstance(axis, tuple):
+        if len(shift) != len(axis):
+            mn = min(len(shift), len(axis))
+            shift = shift[:mn]
+            axis = axis[:mn]
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="roll",
+        input=np.asarray(value, dtype=input_dtype),
+        shifts=shift,
+        dims=axis,
+    )
