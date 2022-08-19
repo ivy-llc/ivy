c2168b79e8e411929b1cb00e19d804854f3132c5
ZoeCD <42352932+ZoeCD@users.noreply.github.com>
2022-08-19 10:35:46 -0500

    Add acosh tensorflow frontend

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -6,6 +6,10 @@ def acos(x, name="acos"):
     return ivy.acos(x)
 
 
+def acosh(x, name="acosh"):
+    return ivy.acosh(x)
+
+
 def concat(values, axis, name="concat"):
     return ivy.concat(values, axis)
 
