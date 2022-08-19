fef1db460148ca9aa0666c7200a446d6e7e9124d
wammy19 <andrea.spiteri19@gmail.com>
2022-08-19 13:28:53 +0100

    Commit to rebase.

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -8,8 +8,9 @@ def sparse_categorical_crossentropy(
     axis=-1
 ):
     if from_logits:
-        y_pred = ivy.softmax(y_pred)
-    return ivy.sparse_cross_entropy(y_true, y_pred, axis=axis, epsilon=1e-7)
+        ivy.softmax(y_pred, out=y_pred)
+    return ivy.sparse_cross_entropy(y_true, y_pred, axis=axis)
 
 
 sparse_categorical_crossentropy.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
