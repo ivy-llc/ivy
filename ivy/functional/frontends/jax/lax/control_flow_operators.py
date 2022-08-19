1353ba00a348f84dd91c8b3e17cabb25c7303d9e
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-19 15:29:35 +0800

    function

--- ivy/functional/frontends/jax/lax/control_flow_operators.py
+++ ivy/functional/frontends/jax/lax/control_flow_operators.py
@@ -4,3 +4,14 @@ import ivy
 
 def map(f, xs):
     return ivy.stack([f(x) for x in xs])
+
+
+def while_loop(cond_fun, body_fun, init_val):
+    # compile
+    cond = ivy.compile(cond_fun, example_inputs=[init_val])
+    body = ivy.compile(body_fun, example_inputs=[init_val])
+    # function body
+    val = init_val
+    while cond(val):
+        val = body(val)
+    return val
