88f33bf3cd41539b5b63015ef2467a4001f98e53
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-08-19 16:14:45 +0530

    Added `torch.sqrt` function (#3226)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -939,3 +939,40 @@ def test_torch_log10(
         input=np.asarray(x, dtype=input_dtype),
         out=None,
     )
+
+
+# sqrt
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            )
+        )
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.sqrt"
+    ),
+)
+def test_torch_sqrt(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="sqrt",
+        input=np.asarray(input, dtype=input_dtype),
+        out=None,
+    )
