f23809ccf0dbe64e0e4b8c5fb9cc6a52342db146
wammy19 <andrea.spiteri19@gmail.com>
2022-08-19 13:28:53 +0100

    Commit to rebase.

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_metrics.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_metrics.py
@@ -1,46 +1,53 @@
 # global
-import numpy as np
 from hypothesis import given, strategies as st
+import ivy
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.tensorflow as ivy_tf
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
+@handle_cmd_line_args
 @given(
     y_true=st.lists(st.integers(min_value=0, max_value=5), min_size=1, max_size=1),
-    y_pred=helpers.dtype_and_values(
+    dtype_y_pred=helpers.dtype_and_values(
         available_dtypes=ivy_tf.valid_float_dtypes,
         shape=(5,),
         min_value=-10,
         max_value=10,
     ),
     from_logits=st.booleans(),
+    as_variable=st.booleans(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.sparse_categorical_crossentropy"
-    )
+    ),
+    with_out=st.booleans(),
+    native_array=st.booleans()
 )
 def test_sparse_categorical_crossentropy(
-        y_true,
-        y_pred,
-        from_logits,
-        num_positional_args,
-        fw
+    y_true,
+    dtype_y_pred,
+    from_logits,
+    as_variable,
+    num_positional_args,
+    native_array,
+    with_out,
+    fw
 ):
-    y_true = np.array(y_true, dtype='int32')
-    dtype = y_pred[0]
-    y_pred = np.array(y_pred[1], dtype=dtype)
+    y_true = ivy.array(y_true, dtype=ivy.int32)
+    dtype, y_pred = dtype_y_pred
 
     # Perform softmax on prediction if it's not a probability distribution.
     if not from_logits:
-        y_pred = np.exp(y_pred) / np.sum(np.exp(y_pred))
+        y_pred = ivy.exp(y_pred) / ivy.sum(ivy.exp(y_pred))
 
     helpers.test_frontend_function(
-        input_dtypes=['int32', dtype],
-        as_variable_flags=False,
-        with_out=False,
+        input_dtypes=[ivy.int32, dtype],
+        as_variable_flags=as_variable,
+        with_out=with_out,
         num_positional_args=num_positional_args,
-        native_array_flags=True,
+        native_array_flags=native_array,
         fw=fw,
         frontend="tensorflow",
         fn_tree="metrics.sparse_categorical_crossentropy",
@@ -48,3 +55,4 @@ def test_sparse_categorical_crossentropy(
         y_pred=y_pred,
         from_logits=from_logits
     )
+
