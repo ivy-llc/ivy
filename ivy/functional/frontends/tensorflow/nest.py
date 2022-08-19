d11336eabddae05e0bf00349f65cf46b78ecd833
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-19 10:40:14 +0800

    simplified and add torch sparse

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -1,15 +1,43 @@
 # global
+import ivy
 import tensorflow as tf
+import torch
+
+
+def _is_sparse_array(x):
+    if isinstance(x, tf.SparseTensor):
+        return True
+    if isinstance(x, torch.Tensor):
+        if x.layout in [torch.sparse_coo, torch.sparse_csr]:
+            return True
+    return False
+
+
+def _flatten_sparse_array(x):
+    if isinstance(x, tf.SparseTensor):
+        return [x.indices, x.values, x.dense_shape]
+    if isinstance(x, torch.Tensor):
+        if x.layout == torch.sparse_coo:
+            x = x.coalesce()
+            return [x.indices(), x.values(), ivy.native_array(x.size(), dtype="int64")]
+        if x.layout == torch.sparse_csr:
+            return [
+                x.crow_indices(),
+                x.col_indices(),
+                x.values(),
+                ivy.native_array(x.size(), dtype="int64"),
+            ]
 
 
 def flatten(structure, expand_composites=False):
-    if expand_composites and isinstance(structure, tf.RaggedTensor):
-        new_struc = [structure.flat_values]
-        for row_split in structure.nested_row_splits:
-            new_struc.append(row_split)
-        return new_struc
-    if expand_composites and isinstance(structure, tf.SparseTensor):
-        return [structure.indices, structure.values, structure.dense_shape]
+    if expand_composites:
+        if isinstance(structure, tf.RaggedTensor):
+            new_struc = [structure.flat_values]
+            for row_split in structure.nested_row_splits:
+                new_struc.append(row_split)
+            return new_struc
+        if _is_sparse_array(structure):
+            return _flatten_sparse_array(structure)
     if isinstance(structure, (tuple, list)):
         new_struc = []
         for child in structure:
