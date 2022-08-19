8c06c75b2b6e4892c7fe75402f6cfad0093df8d0
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-19 16:59:18 +0800

    first changes

--- ivy/functional/frontends/jax/lax/control_flow_operators.py
+++ ivy/functional/frontends/jax/lax/control_flow_operators.py
@@ -2,5 +2,11 @@
 import ivy
 
 
+def cond(pred, true_fun, false_fun, *operands, operand=None):
+    if pred:
+        return true_fun(*operands)
+    return false_fun(*operands)
+
+
 def map(f, xs):
     return ivy.stack([f(x) for x in xs])
