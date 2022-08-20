0e1786018b48d6d129ba68b8d0ebf8f66091a0f3
Cybersheralt <john.noble.463@gmail.com>
2022-08-20 14:34:29 +0200

    add prod function for numpy frontend

---
+++ ivy/functional/frontends/numpy/mathematical_functions/sums_products_differences.py
@@ -0,0 +1,25 @@
+# global
+import ivy
+
+# local
+from ivy.func_wrapper import from_zero_dim_arrays_to_float
+
+
+@from_zero_dim_arrays_to_float
+def prod(
+        x,
+        axis=None,
+        dtype=None,
+        out=None,
+        keepdims=False,
+        where=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.prod(x, axis=axis, keepdims=keepdims, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+prod.unsupported_dtypes = {"torch": ("float16",)}
