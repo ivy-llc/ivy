43b391448185af7791226cc8baa28cda208582b7
vishwaksenan <vishwaksenang@gmail.com>
2022-08-20 02:00:24 +0100

    adding cumsum function to torch frontend

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -18,7 +18,7 @@ import ivy.functional.backends.torch as ivy_torch
         shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
     ),
     axis=helpers.get_axis(
-        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"), 
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
         ret_tuple=True,
     ),
     as_variable=st.booleans(),
@@ -28,12 +28,12 @@ import ivy.functional.backends.torch as ivy_torch
     native_array=st.booleans(),
 )
 def test_torch_flip(
-    dtype_and_values,
-    axis,
-    as_variable,
-    num_positional_args,
-    native_array,
-    fw,
+        dtype_and_values,
+        axis,
+        as_variable,
+        num_positional_args,
+        native_array,
+        fw,
 ):
     input_dtype, value = dtype_and_values
     helpers.test_frontend_function(
@@ -47,4 +47,45 @@ def test_torch_flip(
         fn_tree="flip",
         input=np.asarray(value, dtype=input_dtype),
         dims=axis,
+    )
+
+
+# cumsum
+@given(
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)),
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+    ),
+    axis=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=2), key="shape"),
+    ).filter(lambda axis: isinstance(axis, int)),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.cumsum"
+    ),
+    native_array=st.booleans(),
+)
+def test_torch_cumsum(
+        dtype_and_values,
+        axis,
+        as_variable,
+        num_positional_args,
+        native_array,
+        fw,
+):
+    input_dtype, value = dtype_and_values
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="cumsum",
+        input=np.asarray(value, dtype=input_dtype),
+        dim=axis,
     )
