e35183cc9b97f8855d74be371efd902996ae19f7
Zifeng Tang <tangzifeng37@yeah.net>
2022-08-21 00:47:25 +0100

    Update: implement binary_accuracy

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -2,5 +2,9 @@ import ivy
 
 
 def binary_accuracy(y_true, y_pred, threshold=0.5):
-    ivy.equal(y_true, y_pred > threshold)
-    return ivy.mean(ivy.equal(y_true, y_pred > threshold))
+    return ivy.mean(
+        ivy.equal(y_true, ivy.astype(y_pred > threshold, y_pred.dtype)).astype(
+            y_pred.dtype
+        ),
+        axis=-1,
+    )
