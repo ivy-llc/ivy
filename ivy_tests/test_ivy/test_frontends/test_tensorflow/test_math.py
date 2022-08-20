875a5a5b02acb06ec89634c643dfd20015955eea
kimbring2 <kimbring2@gmail.com>
2022-08-20 13:32:12 +0900

    update argmax function for tensorflow frontend

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -432,3 +432,38 @@ def test_tensorflow_reduce_logsumexp(
         fn_tree="reduce_logsumexp",
         input_tensor=np.asarray(x, dtype=input_dtype),
     )
+
+
+# argmax
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.statistical_dtype_values(function="argmax"),
+    as_variable=helpers.array_bools(num_arrays=1),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.argmax"
+    ),
+    native_array=helpers.array_bools(num_arrays=1),
+)
+def test_tensorflow_argmax(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x, axis = dtype_and_x
+    if isinstance(axis, tuple):
+        axis = axis[0]
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="math.argmax",
+        input=np.asarray(x, dtype=input_dtype),
+        axis=axis,
+        output_type="int64"
+    )
