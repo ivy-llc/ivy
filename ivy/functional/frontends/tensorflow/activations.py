158206be6c1ddd1f7979c1e79724e460d9671a4e
Aml Hassan <66205928+Aml-Hassan-Abd-El-hamid@users.noreply.github.com>
2022-08-21 22:27:09 +0200

    Update activations.py

--- ivy/functional/frontends/tensorflow/activations.py
+++ ivy/functional/frontends/tensorflow/activations.py
@@ -6,10 +6,10 @@ def exponential(x):
 
 
 # TODO: This doesn't seem right. Exponential should work with all Number types
-
+#I added the int16 and int32, hope that solve it :|
 exponential.supported_dtypes = {
-    "numpy": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
-    "jax": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
-    "tensorflow": ("bfloat16", "float8", "float16", "float32", "float64", "float128"),
-    "torch": ("float8", "float32", "float64", "float128"),
+    "numpy": ("bfloat16", "float8", "float16", "float32", "float64", "float128","int16","int32"),
+    "jax": ("bfloat16", "float8", "float16", "float32", "float64", "float128","int16","int32"),
+    "tensorflow": ("bfloat16", "float8", "float16", "float32", "float64", "float128","int16","int32"),
+    "torch": ("float8", "float32", "float64", "float128","int16","int32"),
 }
