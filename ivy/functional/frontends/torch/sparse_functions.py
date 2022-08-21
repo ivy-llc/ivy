23c0a2dfcaaf03904d018bf5b66139ef8d048dbd
ukj02 <63927985+ukj02@users.noreply.github.com>
2022-08-21 10:21:11 -0400

    Added embedding in sparse function module for torch backend

---
+++ ivy/functional/frontends/torch/sparse_functions.py
@@ -0,0 +1,7 @@
+# local
+import ivy
+
+
+def embedding(input, weight, padding_idx=None, max_norm=None, norm_type=2.0, scale_grad_by_freq=False, sparse=False):
+    return ivy.embedding(input, weight, padding_idx=None, max_norm=None, norm_type=2.0, scale_grad_by_freq=False,
+                         sparse=False)
