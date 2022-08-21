b26c08b7cfe3f7ea7df05e67a25f670cbe074106
Saad Malik <malik.sid1980@gmail.com>
2022-08-21 07:44:06 +0500

    fix

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_arithmetic_operations.py
@@ -6,11 +6,9 @@ from hypothesis import given, strategies as st
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 # add
-@handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=ivy_np.valid_float_dtypes, num_arrays=2
@@ -49,7 +47,7 @@ def test_numpy_add(
         native_array_flags=native_array,
         fw=fw,
         frontend="numpy",
-        fn_tree="add",
+        fn_name="add",
         x1=np.asarray(x[0], dtype=input_dtype[0]),
         x2=np.asarray(x[1], dtype=input_dtype[1]),
         out=None,
@@ -101,7 +99,7 @@ def test_numpy_subtract(
         native_array_flags=native_array,
         fw=fw,
         frontend="numpy",
-        fn_tree="subtract",
+        fn_name="subtract",
         x1=np.asarray(x[0], dtype=input_dtype[0]),
         x2=np.asarray(x[1], dtype=input_dtype[1]),
         out=None,
@@ -153,7 +151,7 @@ def test_numpy_divide(
         native_array_flags=native_array,
         fw=fw,
         frontend="numpy",
-        fn_tree="divide",
+        fn_name="divide",
         x1=np.asarray(x[0], dtype=input_dtype[0]),
         x2=np.asarray(x[1], dtype=input_dtype[1]),
         out=None,
@@ -205,7 +203,7 @@ def test_numpy_multiply(
         native_array_flags=native_array,
         fw=fw,
         frontend="numpy",
-        fn_tree="multiply",
+        fn_name="multiply",
         x1=np.asarray(x[0], dtype=input_dtype[0]),
         x2=np.asarray(x[1], dtype=input_dtype[1]),
         out=None,
