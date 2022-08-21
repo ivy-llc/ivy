041bf0b726374c8b6a78f8dd5cf6ae4f002ed504
dgoel200113 <dgoel_be19@thapar.edu>
2022-08-22 00:30:50 +0530

    added average function

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -26,3 +26,57 @@ def mean(
 
 
 mean.unsupported_dtypes = {"torch": ("float16",)}
+
+novalue=object()
+def average(a, axis=None, weights=None, returned=False, *,
+            keepdims=novalue):
+
+    a = ivy.Array(a)
+
+    if keepdims is novalue:
+        keepdims_kw = {}
+    else:
+        keepdims_kw = {'keepdims': keepdims}
+
+    if weights is None:
+        avg = a.mean(axis, **keepdims_kw)
+        scl = avg.dtype.type(a.size/avg.size)
+    else:
+        wgt = ivy.Array(weights)
+
+        if issubclass(a.dtype.type, (int, bool)):
+            result_dtype = ivy.result_type(a.dtype, wgt.dtype, 'f8')  #doubt
+        else:
+            result_dtype = ivy.result_type(a.dtype, wgt.dtype)
+
+
+        if a.shape != wgt.shape:
+            if axis is None:
+                raise TypeError(
+                    "Axis must be specified when shapes of a and weights "
+                    "differ.")
+            if wgt.ndim != 1:
+                raise TypeError(
+                    "1D weights expected when shapes of a and weights differ.")
+            if wgt.shape[0] != a.shape[axis]:
+                raise ValueError(
+                    "Length of weights not compatible with specified axis.")
+
+            # setup wgt to broadcast along axis
+            wgt = ivy.broadcast_to(wgt, (a.ndim-1)*(1,) + wgt.shape)
+            wgt = ivy.swapaxes(wgt,-1, axis)
+
+        scl = ivy.sum(wgt,axis=axis, dtype=result_dtype)
+        if ivy.any(scl == 0.0):
+            raise ZeroDivisionError(
+                "Weights sum to zero, can't be normalized")
+
+        avg = ivy.multiply(a, wgt, dtype=result_dtype).sum(axis, **keepdims_kw) / scl
+
+    if returned:
+        if scl.shape != avg.shape:
+            scl = ivy.broadcast_to(scl, avg.shape).copy()
+        return avg, scl
+    else:
+        return avg
+
