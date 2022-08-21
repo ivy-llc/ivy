9069e3788789491fa8619c39165f44d7da7ee4ed
ukj02 <63927985+ukj02@users.noreply.github.com>
2022-08-21 17:51:02 -0400

    Update changes

--- ivy_tests/test_ivy/test_frontends/test_torch/test_sparse_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_sparse_functions.py
@@ -9,31 +9,64 @@ import ivy.functional.backends.torch as ivy_torch
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
+# noinspection DuplicatedCode
+@st.composite
+def _arrays_idx_n_dtypes(draw):
+    num_dims = draw(st.shared(helpers.ints(min_value=1, max_value=4), key="num_dims"))
+    num_arrays = draw(
+        st.shared(helpers.ints(min_value=2, max_value=4), key="num_arrays")
+    )
+    common_shape = draw(
+        helpers.lists(
+            arg=helpers.ints(min_value=2, max_value=3),
+            min_size=num_dims - 1,
+            max_size=num_dims - 1,
+        )
+    )
+    unique_idx = draw(helpers.ints(min_value=0, max_value=num_dims - 1))
+    unique_dims = draw(
+        helpers.lists(
+            arg=helpers.ints(min_value=2, max_value=3),
+            min_size=num_arrays,
+            max_size=num_arrays,
+        )
+    )
+    xs = list()
+    available_dtypes = set(ivy_torch.valid_float_dtypes).intersection(
+        ivy_torch.valid_float_dtypes
+    )
+    input_dtypes = draw(helpers.array_dtypes(available_dtypes=available_dtypes))
+    for ud, dt in zip(unique_dims, input_dtypes):
+        x = draw(
+            helpers.array_values(
+                shape=common_shape[:unique_idx] + [ud] + common_shape[unique_idx:],
+                dtype=dt,
+            )
+        )
+        xs.append(x)
+    return xs, input_dtypes, unique_idx
+
+
 # embedding
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=tuple(
-            set(ivy_np.valid_float_dtypes).intersection(
-                set(ivy_torch.valid_float_dtypes)
-            )
-        )
-    ),
+    xs_n_input_dtypes_n_unique_idx=_arrays_idx_n_dtypes(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="functional.frontends.torch.embedding"
+        fn_name="ivy.functional.frontends.torch.embedding"
     ),
 )
 def test_torch_embedding(
-    dtype_and_x,
+    xs_n_input_dtypes_n_unique_idx,
     as_variable,
-    with_out,
     num_positional_args,
     native_array,
+    with_out,
     fw,
 ):
-    input_dtype, x = dtype_and_x
+    xs, input_dtypes, unique_idx = xs_n_input_dtypes_n_unique_idx
+    xs = [np.asarray(x, dtype=dt) for x, dt in zip(xs, input_dtypes)]
     helpers.test_frontend_function(
-        input_dtypes=input_dtype,
+        input_dtypes=input_dtypes,
         as_variable_flags=as_variable,
         with_out=with_out,
         num_positional_args=num_positional_args,
@@ -41,6 +74,7 @@ def test_torch_embedding(
         fw=fw,
         frontend="torch",
         fn_tree="embedding",
-        input=np.asarray(x, dtype=input_dtype),
+        tensors=xs,
+        dim=unique_idx,
         out=None,
     )
