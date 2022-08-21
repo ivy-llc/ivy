3ab527f03a6d6bbf50903e59d221aad6decefad5
Mustafa Hani <skytedits@gmail.com>
2022-08-21 07:25:27 +0200

    add exp to JAX frontend.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -970,3 +970,33 @@ def test_jax_lax_full_like(
         dtype=dtype,
         shape=shape,
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy.valid_float_dtypes,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.exp"
+    ),
+)
+def test_jax_lax_exp(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.exp",
+        x=np.asarray(x, dtype=input_dtype),
+    )
