2366cbb5cecce091e07e4ccbeb4f071908278a00
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-22 14:36:14 +0800

    jax frontend - implement `cond` function and test (#3239)

--- ivy/functional/frontends/jax/lax/control_flow_operators.py
+++ ivy/functional/frontends/jax/lax/control_flow_operators.py
@@ -2,5 +2,17 @@
 import ivy
 
 
+def cond(pred, true_fun, false_fun, *operands, operand=None, linear=None):
+    if operand is not None and operands:
+        raise TypeError(
+            "if `operand` is passed, positional `operands` should not be passed"
+        )
+    operands = (operand,)
+
+    if pred:
+        return true_fun(*operands)
+    return false_fun(*operands)
+
+
 def map(f, xs):
     return ivy.stack([f(x) for x in xs])
