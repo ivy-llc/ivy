247a36bcb0c973ac72c66d42cb3b53d8bc8dfbac
DanielWit <daniel.wit@me.com>
2022-08-22 19:06:32 +0100

    tensorflow frontend shuffle

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -1,7 +1,28 @@
 # global
 
 import ivy
+from typing import Union
 
-def shuffle(value, seed = None, name = None):
+def shuffle(value, seed: int = 0, name = None) -> ivy.Array:
+
     ivy.seed(seed_value = seed)
-    return ivy.shuffle(value, out = name)
+
+    return ivy.shuffle(value, out = ivy.array(name))
+
+
+def normal(mean: Union[float, ivy.NativeArray, ivy.Array] = 0.0,
+           stddev: Union[float, ivy.NativeArray, ivy.Array] = 1.0,
+           shape: Union[float, ivy.NativeArray, ivy.Array] = None,
+           dtype = None,
+           seed = None,
+           name='None') -> ivy.Array:
+
+    if seed is not None:
+        ivy.seed(seed_value = seed)
+
+    return ivy.random_normal(mean = mean,
+                             std = stddev,
+                             shape = shape,
+                             dtype = dtype,
+                             device = None,
+                             out = None)
