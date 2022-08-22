006d2d000f993a9f92048401ab9fa7bab84ad90e
DanielWit <daniel.wit@me.com>
2022-08-22 19:29:45 +0100

    tensorflow frontend shuffle

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -1,28 +1,28 @@
 # global
 
 import ivy
-from typing import Union
-
-def shuffle(value, seed: int = 0, name = None) -> ivy.Array:
-
-    ivy.seed(seed_value = seed)
-
-    return ivy.shuffle(value, out = ivy.array(name))
 
 
-def normal(mean: Union[float, ivy.NativeArray, ivy.Array] = 0.0,
-           stddev: Union[float, ivy.NativeArray, ivy.Array] = 1.0,
-           shape: Union[float, ivy.NativeArray, ivy.Array] = None,
-           dtype = None,
-           seed = None,
-           name='None') -> ivy.Array:
+def shuffle(value, seed: int=None, name=None):
+
+
+    ivy.seed(seed_value=seed)
+    return ivy.shuffle(value, out=name)
+
+
+def normal(mean=0.0,
+           stddev=1.0,
+           shape=None,
+           dtype=None,
+           seed=None,
+           name='None'):
 
     if seed is not None:
-        ivy.seed(seed_value = seed)
+        ivy.seed(seed_value=seed)
 
-    return ivy.random_normal(mean = mean,
-                             std = stddev,
-                             shape = shape,
-                             dtype = dtype,
-                             device = None,
-                             out = None)
+    return ivy.random_normal(mean=mean,
+                             std=stddev,
+                             shape=shape,
+                             dtype=dtype,
+                             device=None,
+                             out=None)
