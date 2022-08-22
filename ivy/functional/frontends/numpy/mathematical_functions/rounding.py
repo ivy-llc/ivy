00bebf1a6407ecca4da0c741524a622ddddf373f
khalidalotaibi <khalid.al-otaibi@outlook.com>
2022-08-22 15:05:56 +0300

    Added floor function to rounding.py and a test to test_rounding.py

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -17,3 +17,21 @@ def ceil(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
+def floor(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.floor(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
