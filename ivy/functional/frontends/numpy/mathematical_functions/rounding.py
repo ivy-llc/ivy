4e1f064df8acc5eab9468701cbb64cf6437e31bb
khalidalotaibi <khalid.al-otaibi@outlook.com>
2022-08-22 14:21:01 +0300

    Added ceil function to rounding.py and test for ceil function to test_rounding.py

---
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -0,0 +1,19 @@
+import ivy
+
+def ceil(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.ceil(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
