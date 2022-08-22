1f9ff6cbde72bcbccc8b0a152982b5ac34c28899
DanielWit <daniel.wit@me.com>
2022-08-22 19:06:32 +0100

    tensorflow frontend shuffle

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -2,6 +2,27 @@
 
 import ivy
 
-def shuffle(value, seed = None, name = None):
-    ivy.seed(seed_value = seed)
-    return ivy.shuffle(value, out = name)
+def shuffle(value, seed: None, name=None):
+
+
+    ivy.seed(seed_value=seed)
+    return ivy.shuffle(value, out=name)
+
+
+def normal(mean=0.0,
+           stddev=1.0,
+           shape=None,
+           dtype=None,
+           seed=None,
+           name='None'):
+
+
+    if seed is not None:
+        ivy.seed(seed_value=seed)
+
+    return ivy.random_normal(mean=mean,
+                             std=stddev,
+                             shape=shape,
+                             dtype=dtype,
+                             device=None,
+                             out=None)
