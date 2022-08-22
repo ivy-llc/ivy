eaf3dee4123ad1dbf95697f081499fb62b094d3a
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-08-22 17:17:34 +0530

    torch allclose frontend implementation (#3349)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>
    Former-commit-id: 4ae5f80144076726c37250c1686d13309206c24c

---
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_comparison_ops.py
@@ -0,0 +1,52 @@
+# global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+import ivy.functional.backends.torch as ivy_torch
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+
+
+# allclose
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            ),
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.allclose"
+    ),
+    equal_nan=st.booleans(),
+)
+def test_torch_allclose(
+    dtype_and_input,
+    equal_nan,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="allclose",
+        input=np.asarray(input[0], dtype=input_dtype[0]),
+        other=np.asarray(input[1], dtype=input_dtype[1]),
+        rtol=1e-05,
+        atol=1e-08,
+        equal_nan=equal_nan,
+    )
