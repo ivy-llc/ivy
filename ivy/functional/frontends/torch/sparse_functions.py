9f2e7675b4cda2fb9de3611ba43f621ae4f4d983
ukj02 <ukj02@proton.me>
2022-08-22 17:02:55 -0400

    Update sparse_functions.py

--- ivy/functional/frontends/torch/sparse_functions.py
+++ ivy/functional/frontends/torch/sparse_functions.py
@@ -8,5 +8,3 @@ def embedding(num_embeddings, embedding_dim, padding_idx=None,
     return ivy.embedding(num_embeddings, embedding_dim, padding_idx=None,
                          max_norm=None, norm_type=2.0, scale_grad_by_freq=False,
                          sparse=False, _weight=None, device=None, dtype=None)
-
-
