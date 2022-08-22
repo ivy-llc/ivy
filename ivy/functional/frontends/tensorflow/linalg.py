97b919352662d509610c3d512e9580e5c4756eed
ukj02 <ukj02@proton.me>
2022-08-22 10:54:39 -0400

    Update linalg.py

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -6,15 +6,3 @@ def det(input, name=None):
 
 
 det.unsupported_dtypes = ("float16", "bfloat16")
-
-<<<<<<< HEAD
-
-
-
-def eigvalsh(tensor, name=None):
-    return ivy.eigvalsh(tensor)
-
-
-eigvalsh.unsupported_dtypes = ("float16", "bfloat16")
-=======
->>>>>>> 9f4596f592aa614b5a5d528a3418203660fb9146
