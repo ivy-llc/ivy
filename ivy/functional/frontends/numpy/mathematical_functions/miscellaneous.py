5ea0fdebe943e471ae19833cd6ad27215aff52de
Alex Weiss <alex.m.weiss2@gmail.com>
2022-08-23 16:04:48 -0400

    clip function

---
+++ ivy/functional/frontends/numpy/mathematical_functions/miscellaneous.py
@@ -0,0 +1,20 @@
+import ivy
+
+def clip(a, a_min, a_max, /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,):
+    if dtype:
+        a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+    if a_min > a_max:
+        ret = a.full_like(a_max)
+    else:
+        ret = ivy.clip(a, a_min, a_max, out=out)
+        
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
