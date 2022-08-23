9c5b7825ae542beacff93521a7f9a7316c356f6e
ojask12 <ojas.stud@gmail.com>
2022-08-23 23:09:19 +0530

    added numpy reshape and its test function

---
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
@@ -0,0 +1,10 @@
+# local
+import ivy
+
+def reshape(x,
+            /,
+            shape,
+            *,
+            out=None,
+            copy=None):
+    return ivy.reshape(x, shape,copy=copy, out=out)
