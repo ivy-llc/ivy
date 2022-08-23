6151284ea92554de6c01ed1012748f0d317c4ff2
ZoeCD <42352932+ZoeCD@users.noreply.github.com>
2022-08-23 09:23:37 -0500

    Add zeros_like raw ops tensorflow

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -919,3 +919,32 @@ def test_tensorflow_Reshape(
         tensor=np.asarray(x, dtype=dtype),
         shape=shape,
     )
+
+
+# Zeros_like
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_tf.valid_float_dtypes))
+        ),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.zeros_like"
+    ),
+)
+def test_tensorflow_zeros_like(
+        dtype_and_x, as_variable, num_positional_args, fw, native_array
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=1,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="zeros_like",
+        x=np.asarray(x, dtype=dtype),
+)
