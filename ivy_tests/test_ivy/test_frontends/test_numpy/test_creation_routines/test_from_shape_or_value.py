ed8d4846f7285bad29f2af18cd1ebd7cdd67d924
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-23 15:23:06 +0800

    numpy frontend - `creation: from shape or values` functions and tests (#3454)
    Former-commit-id: cad71f9c3133b05281f4933b7d532171d5603004

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_shape_or_value.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_shape_or_value.py
@@ -166,6 +166,164 @@ def test_numpy_identity(
     )
 
 
+# ones
+@handle_cmd_line_args
+@given(
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtypes=_dtypes(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.ones"
+    ),
+)
+def test_numpy_ones(
+    shape,
+    dtypes,
+    num_positional_args,
+    fw,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtypes,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=False,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="ones",
+        shape=shape,
+        dtype=dtypes[0],
+    )
+
+
+# ones_like
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_numeric_dtypes,
+        allow_inf=False,
+    ),
+    shape=helpers.get_shape(
+        allow_none=True,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.ones_like"
+    ),
+)
+def test_numpy_ones_like(
+    dtype_and_x,
+    shape,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=[input_dtype],
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="ones_like",
+        a=np.asarray(x, dtype=input_dtype),
+        dtype=input_dtype,
+        order="K",
+        subok=True,
+        shape=shape,
+    )
+
+
+# zeros
+@handle_cmd_line_args
+@given(
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtypes=_dtypes(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.zeros"
+    ),
+)
+def test_numpy_zeros(
+    shape,
+    dtypes,
+    num_positional_args,
+    fw,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtypes,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=False,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="zeros",
+        shape=shape,
+        dtype=dtypes[0],
+    )
+
+
+# zeros_like
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_numeric_dtypes,
+        allow_inf=False,
+    ),
+    shape=helpers.get_shape(
+        allow_none=True,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.zeros_like"
+    ),
+)
+def test_numpy_zeros_like(
+    dtype_and_x,
+    shape,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=[input_dtype],
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="zeros_like",
+        a=np.asarray(x, dtype=input_dtype),
+        dtype=input_dtype,
+        order="K",
+        subok=True,
+        shape=shape,
+    )
+
+
 # full
 @st.composite
 def _fill_value(draw):
@@ -211,4 +369,53 @@ def test_numpy_full(
         shape=shape,
         fill_value=fill_value,
         dtype=dtypes[0],
+    )
+
+
+# full_like
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_numeric_dtypes,
+        allow_inf=False,
+    ),
+    fill_value=_fill_value(),
+    dtypes=_dtypes(),
+    shape=helpers.get_shape(
+        allow_none=True,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.full_like"
+    ),
+)
+def test_numpy_full_like(
+    dtype_and_x,
+    fill_value,
+    dtypes,
+    shape,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=[input_dtype],
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="full_like",
+        a=np.asarray(x, dtype=input_dtype),
+        fill_value=fill_value,
+        dtype=dtypes[0],
+        order="K",
+        subok=True,
+        shape=shape,
     )
