b4714b69f144b93b237662af6af13d19bdec1dd0
Cybersheralt <john.noble.463@gmail.com>
2022-08-23 12:44:38 +0200

    modify test for prod function for numpy frontend

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
@@ -12,7 +12,9 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 # prod
 @handle_cmd_line_args
 @given(
-    dtype_and_x=np_frontend_helpers.dtype_x_bounded_axis(available_dtypes=ivy.current_backend().valid_dtypes),
+    dtype_and_x=np_frontend_helpers.dtype_x_bounded_axis(
+        available_dtypes=ivy.current_backend().valid_dtypes
+    ),
     dtype=st.sampled_from(ivy.current_backend().valid_dtypes + (None,)),
     where=np_frontend_helpers.where(),
     num_positional_args=helpers.num_positional_args(
