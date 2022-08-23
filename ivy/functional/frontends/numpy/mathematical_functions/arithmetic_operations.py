823915e178beb9530f65482918488da250b64847
aryanxk02 <aryan.kenchappagol@gmail.com>
2022-08-24 03:31:30 +0530

    Add Numpy Frontend trunc (#3503)

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -150,3 +150,22 @@ def vdot(
 ):
     ret = ivy.multiply(a, b).sum()
     return ret
+
+
+def trunc(
+    x,
+    /,
+    out=None,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.cbrt(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+    
