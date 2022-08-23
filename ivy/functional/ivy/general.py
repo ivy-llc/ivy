acbbf4db429c85558441c4a92149ce75b42bab58
Aryan Khatana <51334629+aryankhatana01@users.noreply.github.com>
2022-08-24 00:41:57 +0530

    Update general.py

--- ivy/functional/ivy/general.py
+++ ivy/functional/ivy/general.py
@@ -2333,23 +2333,7 @@ def num_arrays_in_memory():
 
 
 def print_all_arrays_in_memory():
-    """
-    Gets all the native Ivy arrays which are currently alive(in the garbage collector)
-    from get_all_arrays_in_memory() function and prints them to the console.
-    
-    Parameters
-    ----------
-    None
-
-    Output
-    ------
-    All the Ivy native arrays which are currently alive.
-
-    Returns
-    -------
-    None
-
-    """
+    """Prints all arrays which are currently alive."""
     for arr in get_all_arrays_in_memory():
         print(type(arr), arr.shape)
 
