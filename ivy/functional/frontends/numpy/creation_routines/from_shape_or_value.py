ac11bbfa8813078141088366cd0c5aa57097711b
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-23 12:22:17 +0800

    empty like

--- ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
+++ ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
@@ -6,5 +6,11 @@ def empty(shape, dtype="float64", order="C", *, like=None):
     return ivy.empty(shape, dtype=dtype)
 
 
+def empty_like(prototype, dtype=None, order="K", subok=True, shape=None):
+    if shape:
+        return ivy.empty(shape, dtype=dtype)
+    return ivy.empty_like(prototype, dtype=dtype)
+
+
 def full(shape, fill_value, dtype=None, order="C", *, like=None):
     return ivy.full(shape, fill_value, dtype=dtype)
