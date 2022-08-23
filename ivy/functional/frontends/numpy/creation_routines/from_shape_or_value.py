164412948f374804397fe6704f39d84c37f4d029
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-23 14:51:00 +0800

    numpy frontend - creation: `from shape and values` functions and tests (#3448)
    Former-commit-id: 97aec979a79a64bacc06cf61e75c53b7e5d2aa8a

--- ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
+++ ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
@@ -2,5 +2,23 @@
 import ivy
 
 
+def empty(shape, dtype="float64", order="C", *, like=None):
+    return ivy.empty(shape, dtype=dtype)
+
+
+def empty_like(prototype, dtype=None, order="K", subok=True, shape=None):
+    if shape:
+        return ivy.empty(shape, dtype=dtype)
+    return ivy.empty_like(prototype, dtype=dtype)
+
+
+def eye(N, M=None, k=0, dtype="float64", order="C", *, like=None):
+    return ivy.eye(N, M, k=k, dtype=dtype)
+
+
+def identity(n, dtype=None, *, like=None):
+    return ivy.eye(n, dtype=dtype)
+
+
 def full(shape, fill_value, dtype=None, order="C", *, like=None):
     return ivy.full(shape, fill_value, dtype=dtype)
