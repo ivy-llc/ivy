005a3fbe77f649cb64fd5428562f822f0343c6b6
Adithya Hegde <adithyah07@gmail.com>
2022-08-24 00:35:56 +0530

    Implement reciprocal in ivy core and add to jax.lax frontend (#3347)
    * add reciprocal to ivy core
    
    * add jax.lax.reciprocal frontend
    
    * remove redundant \ in elementwise tf backend
    
    * resolve merge conflict
    
    * fix formatting
    
    * update available dtypes to be more specific
    
    * fix indentation

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1653,6 +1653,40 @@ def test_pow(
     )
 
 
+# reciprocal
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_numeric_dtypes, num_arrays=1, allow_inf=True
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="reciprocal"),
+)
+def test_reciprocal(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="reciprocal",
+        x=np.asarray(x, dtype=input_dtype),
+    )
+
+
 # remainder
 @handle_cmd_line_args
 @given(
