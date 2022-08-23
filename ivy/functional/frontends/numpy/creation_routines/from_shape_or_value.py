57434c038b1d2109a25c023fcbd3fa924002028c
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-23 12:16:51 +0800

    empty func

--- ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
+++ ivy/functional/frontends/numpy/creation_routines/from_shape_or_value.py
@@ -2,5 +2,9 @@
 import ivy
 
 
+def empty(shape, dtype="float64", order="C", *, like=None):
+    return ivy.empty(shape, dtype=dtype)
+
+
 def full(shape, fill_value, dtype=None, order="C", *, like=None):
     return ivy.full(shape, fill_value, dtype=dtype)
