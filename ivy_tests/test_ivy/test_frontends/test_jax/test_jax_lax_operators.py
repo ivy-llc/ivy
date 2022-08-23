8d9c76e55f985ba4f17d05f7a99a14befbdc6472
Adithya Hegde <adithyah07@gmail.com>
2022-08-24 00:35:56 +0530

    Implement reciprocal in ivy core and add to jax.lax frontend (#3347)
    * add reciprocal to ivy core
    
    * add jax.lax.reciprocal frontend
    
    * remove redundant \ in elementwise tf backend
    
    * resolve merge conflict
    
    * fix formatting
    
    * update available dtypes to be more specific
    
    * fix indentation

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1211,6 +1211,38 @@ def test_jax_lax_broadcast(
 
 @handle_cmd_line_args
 @given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=list(
+            set(ivy_jax.valid_float_dtypes).intersection(set(ivy_np.valid_float_dtypes))
+        )
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.reciprocal"
+    ),
+)
+def test_jax_lax_reciprocal(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.reciprocal",
+        x=np.asarray(x, dtype=input_dtype),
+    )
+    
+
+@handle_cmd_line_args
+@given(
     dtype_x_bounded_axis=_dtype_x_bounded_axis(
         available_dtypes=ivy.valid_numeric_dtypes,
         min_num_dims=1,
