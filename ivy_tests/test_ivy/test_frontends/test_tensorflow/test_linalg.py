f91f3d02620651b222414d729fedb1160e1a1b70
Mohamed Haggag <haggag@datawallet.com>
2022-08-24 21:09:49 +0200

    lint_mod

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -158,7 +158,7 @@ def test_tensorflow_solve(
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.slogdet"
     ),
-     native_array=st.booleans(),
+    native_array=st.booleans(),
 )
 def test_tensorflow_slogdet(
     *,
