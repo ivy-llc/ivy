9d7964a215d31448b7f8e3ca0190fc534a6011c0
Tosin Harold Akingbemisilu <69729533+tosmartak@users.noreply.github.com>
2022-08-24 14:27:01 +0100

    Adding reduce_variance on Math function for Tensorflow Frontend (#3382)
    Co-authored-by: unevilicorn <unevilicorn@gmail.com>

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -149,10 +149,7 @@ def reduce_variance(input_tensor, axis=None, keepdims=False, name="reduce_varian
 
 
 reduce_variance.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64", "float16", 
-                   "float32", "float64"),
-    "torch": ("float16", "bfloat16"),
-    "numpy": ("float16", "bfloat16", "float32", "float64")
+    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
 }
 
 
