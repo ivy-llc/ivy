9b727e4edefcc1e8e6245e0b890cf90c3105618f
ukj02 <63927985+ukj02@users.noreply.github.com>
2022-08-24 14:26:14 -0400

    Added custom linear solve function to jax frontend

--- ivy/functional/frontends/jax/lax/custom_gradient_operators.py
+++ ivy/functional/frontends/jax/lax/custom_gradient_operators.py
@@ -3,3 +3,9 @@ import ivy
 
 def stop_gradient(x):
     return ivy.stop_gradient(x)
+
+
+def custom_linear_solve(matvec, b, solve, transpose_solve=None,
+                        symmetric=False, has_aux=False):
+    return ivy.custom_linear_solve(matvec, b, solve, transpose_solve=None,
+                                   symmetric=False, has_aux=False)
