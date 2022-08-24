301d7ffddf49ea8d05d2708d24eece25ec121474
Zifeng Tang <tangzifeng37@yeah.net>
2022-08-24 01:00:27 +0100

    Update binary_matches

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -1,10 +1,14 @@
 import ivy
 
 
-def binary_accuracy(y_true, y_pred, threshold=0.5):
-    return ivy.mean(
-        ivy.equal(y_true, ivy.astype(y_pred > threshold, y_pred.dtype)).astype(
-            y_pred.dtype
-        ),
-        axis=-1,
+def binary_matches(y_true, y_pred, threshold=0.5):
+    y_pred = ivy.array(y_pred)
+    threshold = ivy.astype(ivy.array(threshold), y_pred.dtype)
+    y_pred = ivy.astype(ivy.greater(y_pred, threshold), y_pred.dtype)
+    return ivy.astype(
+        ivy.equal(y_true, y_pred), ivy.default_float_dtype(input=y_pred, as_native=True)
     )
+
+
+def binary_accuracy(y_true, y_pred, threshold=0.5):
+    return ivy.mean(binary_matches(y_true, y_pred, threshold), axis=-1)
