ab60416c97ebbf9285ff697a1285345a7f228c80
Pengfei Yan <yanllearnn@gmail.com>
2022-08-24 19:49:06 +0800

    add cosh to jax (#3245)
    Co-authored-by: Auxia <sbhathal12@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1342,3 +1342,33 @@ def test_jax_lax_ne(
         x=xs[0],
         y=xs[1],
     )
+
+
+# cosh
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_jax.valid_float_dtypes),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.cosh"
+    ),
+)
+def test_jax_lax_cosh(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.cosh",
+        x=np.asarray(x, dtype=input_dtype),
+    )
