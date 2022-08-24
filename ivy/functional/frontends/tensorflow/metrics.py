c5f325a65b8ea0dd71843003f676236b7a4c11c9
AhmedMousa-ag <59775002+AhmedMousa-ag@users.noreply.github.com>
2022-08-24 17:37:27 +0200

    Added mean_absolute_error to tensorflow frontend

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -2,10 +2,10 @@ import ivy
 
 
 def sparse_categorical_crossentropy(
-    y_true,
-    y_pred,
-    from_logits=False,
-    axis=-1
+        y_true,
+        y_pred,
+        from_logits=False,
+        axis=-1
 ):
     if from_logits:
         y_pred = ivy.softmax(y_pred)
@@ -13,3 +13,9 @@ def sparse_categorical_crossentropy(
 
 
 sparse_categorical_crossentropy.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
+
+
+def mean_absolute_error(y_true, y_pred):
+    return ivy.mean(ivy.abs(y_true-y_pred))
+
+mean_absolute_error.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
