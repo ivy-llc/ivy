32c4e5ae49735bde109e34c4bb44e2c2226abd95
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-24 14:06:03 +0800

    funcs

--- ivy/functional/frontends/numpy/creation_routines/numerical_ranges.py
+++ ivy/functional/frontends/numpy/creation_routines/numerical_ranges.py
@@ -1,14 +1,21 @@
 # global
-# import ivy
+import ivy
 
 
-def arange():
-    pass
+def arange(start, stop=None, step=1, dtype=None, *, like=None):
+    return ivy.arange(start, stop, step, dtype=dtype)
 
 
-def linspace():
-    pass
+def linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None, axis=0):
+    ret = ivy.linspace(start, stop, num, axis=axis, endpoint=endpoint, dtype=dtype)
+    if retstep:
+        step = ret[1] - ret[0]
+        return ret, step
+    return ret
 
 
-def logspace():
-    pass
+def logspace(start, stop, num=50, endpoint=True, base=10.0, dtype=None, axis=0):
+    if not endpoint:
+        interval = (stop - start) / num
+        stop -= interval
+    return ivy.logspace(start, stop, num, base=base, axis=axis, dtype=dtype)
