5f4ce46d54abc64c1783464fff996cfc3e3579ed
Shivraj Singh Bhathal <67852952+Auxia@users.noreply.github.com>
2022-08-24 17:26:51 +0530

    Fixed Lint error

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -4,7 +4,6 @@ from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-import ivy.functional.backends.numpy as ivy_np
 import ivy.functional.backends.tensorflow as ivy_tf
 
 
