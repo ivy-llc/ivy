184006f6105c664bb8f7676b5ed5659e1151305a
V-G-spec <V-G-spec@users.noreply.github.com>
2022-08-24 15:35:53 +0530

    Solved a merge conflict

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy/functional/frontends/numpy/ndarray/other_attributes.py::max
-
+docker run --rm -it -v "$(pwd)":/ivy unifyai/ivy:latest python3 -m pytest ivy/ivy_tests/
