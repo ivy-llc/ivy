0e94a1c4cc521b017b6fbc5dc5cd355b2298fa0d
aryanxk02 <aryan.kenchappagol@gmail.com>
2022-08-24 11:20:02 +0530

    Add Numpy Frontend maximum

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -143,6 +143,26 @@ def cbrt(
     return ret
 
 
+def maximum(
+    x1,
+    x2,
+    /,
+    out=None,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    if dtype:
+        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+    ret = ivy.divide(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
 def vdot(
     a,
     b,
