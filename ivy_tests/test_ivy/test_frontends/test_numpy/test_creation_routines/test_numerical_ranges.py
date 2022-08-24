de68d7a26cde7e1bd17836ee3446d3d44a1ccf75
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-24 14:16:39 +0800

    arange

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_numerical_ranges.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_numerical_ranges.py
@@ -6,42 +6,40 @@ from hypothesis import given, strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 
-# import ivy.functional.backends.numpy as ivy_np
+import ivy.functional.backends.numpy as ivy_np
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 # arange
 @handle_cmd_line_args
 @given(
-    shape=helpers.get_shape(
-        allow_none=False,
-        min_num_dims=1,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=10,
-    ),
-    dtypes=st.booleans(),
-    temp=st.booleans(),
+    start=helpers.ints(min_value=-50, max_value=0),
+    stop=helpers.ints(min_value=1, max_value=50),
+    step=helpers.ints(min_value=1, max_value=5),
+    dtype=st.sampled_from(ivy_np.valid_numeric_dtypes),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.empty"
+        fn_name="ivy.functional.frontends.numpy.arange"
     ),
 )
-def test_numpy_empty(
-    shape,
-    dtypes,
+def test_numpy_arange(
+    start,
+    stop,
+    step,
+    dtype,
     num_positional_args,
     fw,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
+        input_dtypes=[dtype],
         as_variable_flags=False,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=False,
         fw=fw,
         frontend="numpy",
-        fn_tree="empty",
-        test_values=False,
-        shape=shape,
-        dtype=dtypes[0],
+        fn_tree="arange",
+        start=start,
+        stop=stop,
+        step=step,
+        dtype=dtype,
     )
