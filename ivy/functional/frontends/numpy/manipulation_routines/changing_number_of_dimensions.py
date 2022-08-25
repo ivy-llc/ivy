100006cd0542ce223c50689ce39bdb05873cf295
Nuh Muhammed Pişkin <44634165+nuhpiskin@users.noreply.github.com>
2022-08-25 11:47:24 +0300

    squeeze arguments and type hints is changed.

--- ivy/functional/frontends/numpy/manipulation_routines/changing_number_of_dimensions.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_number_of_dimensions.py
@@ -1,30 +1,14 @@
 # global
-from typing import Any, Sequence
-
 import ivy
 
 
 # squeeze
 def squeeze(
-    x: Sequence[Any],
-    axis: int,
+    x,
+    axis,
     /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="k",
-    dtype=None,
-    subok=True,
-    signature=None,
-    extobj=None,
-) -> Any:
-
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
-    ret = ivy.squeeze(x, axis, out=out)
-    if ivy.is_array(where):
-        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+):
+    ret = ivy.squeeze(x, axis)
     return ret
 
 
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_number_of_dimensions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_number_of_dimensions.py
@@ -29,8 +29,6 @@ def _squeeze_helper(draw):
         shape=st.shared(helpers.get_shape(), key="value_shape"),
     ),
     axis=_squeeze_helper(),
-    dtype=st.sampled_from(ivy_np.valid_numeric_dtypes + (None,)),
-    where=np_frontend_helpers.where(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.squeeze"
     ),
@@ -38,8 +36,6 @@ def _squeeze_helper(draw):
 def test_numpy_squeeze(
     dtype_and_x,
     axis,
-    dtype,
-    where,
     as_variable,
     with_out,
     num_positional_args,
@@ -48,12 +44,6 @@ def test_numpy_squeeze(
 ):
     input_dtype, x = dtype_and_x
     input_dtype = [input_dtype]
-    where = np_frontend_helpers.handle_where_and_array_bools(
-        where=where,
-        input_dtype=input_dtype,
-        as_variable=as_variable,
-        native_array=native_array,
-    )
     np_frontend_helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -65,11 +55,4 @@ def test_numpy_squeeze(
         fn_tree="squeeze",
         x=np.asarray(x, dtype=input_dtype[0]),
         axis=axis,
-        out=None,
-        where=where,
-        casting="same_kind",
-        order="k",
-        dtype=dtype,
-        subok=True,
-        test_values=False,
     )
