8d6ba63b50120ea46e7cc3d746d9aa764ba8f832
sreegandh804 <56138447+sreegandh804@users.noreply.github.com>
2022-08-25 15:06:45 +0530

    fixed pr assignment issues

--- automation_tools/pr_automation/pr_action.py
+++ automation_tools/pr_automation/pr_action.py
@@ -13,19 +13,24 @@ def main():
     pr = Process_pr(int(sys.argv[1]), sys.argv[2])
     interns_assigned_volunteers = import_file("assets/volunteer_go_to_intern.json")
     intern_points_of_contact = import_file("assets/intern_poc.json")
-    interns = import_file("assets/intern_assign_rate.json")
+    intern_assign_rates = import_file("assets/intern_assign_rate.json")
+    
+    interns_pocs = intern_points_of_contact.keys()
+    interns = intern_points_of_contact.values()
 
-    for (ivy_intern, assigned_volunteers), (ivy_member, assigned_interns) in zip(
-        interns_assigned_volunteers.items(), intern_points_of_contact.items()
-    ):
-        if pr.author() in assigned_volunteers:
-            pr.assign_intern(ivy_intern)
+    volunteer_pocs = interns_assigned_volunteers.keys()
+    volunteers = interns_assigned_volunteers.values()
+    
+    for volunteer, volunteer_poc in zip(volunteers, volunteer_pocs):
+        if pr.author() in volunteer:
+            pr.assign_intern(volunteer_poc)
             sys.exit(0)
-        elif pr.author() in assigned_interns:
-            pr.assign_intern(ivy_member)
+    for intern, interns_poc in zip(interns, interns_pocs):
+        if pr.author() in intern:
+            pr.assign_intern(interns_poc)
             sys.exit(0)
 
-    pr.assign_random_intern(interns)
+    pr.assign_random_intern(intern_assign_rates)
 
 
 if __name__ == "__main__":
