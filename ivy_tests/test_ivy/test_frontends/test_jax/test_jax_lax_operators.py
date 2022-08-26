5b6af9b6b2e71c05ae4a8c26e951b6112a51a203
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2022-08-26 09:38:20 +0800

    Updated the svd tests for JAX frontend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -24,6 +24,27 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     ),
     native_array = helpers.list_of_length(x=st.booleans(), length=2),
 )
+def test_jax_lax_svd(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.svd",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
+    )
 # add
 @handle_cmd_line_args
 @given(
