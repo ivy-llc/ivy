c524664eed2d66127e4762d6aa68a97aacb8d638
Emeka Okoli <mekkyokoli@gmail.com>
2022-08-26 03:43:33 +0100

    added cos function in jax frontend (#3488)
    * added cos function in jax frontend
    
    * Update ivy.iml
    
    * Update misc.xml
    
    * Delete ivy.iml
    
    * Create ivy.iml
    
    * Delete ivy.iml
    
    * Create ivy.iml
    
    * Added .idea to gitignore
    
    * removing changes in idea
    
    * Update ivy.iml
    
    * Update .gitignore
    
    * Update misc.xml
    
    * more changes
    
    * last fix

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1360,7 +1360,7 @@ def test_jax_lax_cosh(
     fw,
 ):
     input_dtype, x = dtype_and_x
-
+    
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -1502,4 +1502,35 @@ def test_jax_lax_gt(
         fn_tree="lax.gt",
         x=xs[0],
         y=xs[1],
+    )
+    
+    
+# cos
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=ivy_jax.valid_float_dtypes),
+    as_variable=st.booleans(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.cos"
+    ),
+    native_array=st.booleans(),
+)
+def test_jax_lax_cos(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.cos",
+        x=np.asarray(x, dtype=input_dtype),
     )
