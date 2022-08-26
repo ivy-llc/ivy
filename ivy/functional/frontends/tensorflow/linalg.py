143e06eb4a345a855e21a0b64f67ecacc13cffd7
Joel Pendleton <joelkieronpendleton@gmail.com>
2022-08-26 17:44:08 +0200

    Added pinv to tensorflow frontend.

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -27,3 +27,8 @@ def slogdet(input, name=None):
 
 
 slogdet.unsupported_dtypes = ("float16", "bfloat16")
+
+def pinv(a, rcond=None, validate_args=False, name=None):
+    return ivy.pinv(a, rcond)
+
+pinv.unsupported_dtypes = ("float16", "bfloat16")
