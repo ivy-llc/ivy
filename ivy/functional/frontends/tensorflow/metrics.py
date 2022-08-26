02363bca9a8ec498108150f9474d560b54dbbb6d
Mustafa <mustafa1adel@gmail.com>
2022-08-26 15:46:37 +0200

    add binary_crossentropy

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -20,4 +20,32 @@ def sparse_categorical_crossentropy(y_true, y_pred, from_logits=False, axis=-1):
     return ivy.sparse_cross_entropy(y_true, y_pred, axis=axis)
 
 
+def binary_crossentropy(y_true, y_pred,
+                        from_logits: bool =False,
+                        label_smoothing: float =0.) -> ivy.Array:
+    """Computes the binary crossentropy loss.
+    Parameters
+    ----------
+        y_true:
+            Ground truth values.
+        y_pred:
+            The predicted values.
+        from_logits: bool
+            Whether `y_pred` is expected to be a logits array/tensor. By default,
+            we assume that `y_pred` encodes a probability distribution.
+        label_smoothing: Float
+            in [0, 1]. If > `0` then smooth the labels by
+            squeezing them towards 0.5 That is, using `1. - 0.5 * label_smoothing`
+            for the target class and `0.5 * label_smoothing` for the non-target class.
+    Returns
+    -------
+        Binary crossentropy loss value.
+    """
+
+    if from_logits:
+        y_pred = ivy.softmax(y_pred)
+    return ivy.mean(ivy.binary_cross_entropy(y_true,
+                                             y_pred, label_smoothing))
+
+
 sparse_categorical_crossentropy.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
