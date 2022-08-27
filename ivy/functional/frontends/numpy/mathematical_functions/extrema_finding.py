c33f584032c30cf53f4d84872bc10314090c4647
Ameen Ur Rehman <ameenreh2003@gmail.com>
2022-08-28 00:13:25 +0530

    add max in mathematical function

---
+++ ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
@@ -0,0 +1,26 @@
+import ivy
+
+# local
+from ivy.func_wrapper import from_zero_dim_arrays_to_float
+
+#minimum
+def minimum(
+    x1,
+    x2,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True
+):
+    if dtype:
+        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
+        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+    ret = ivy.minimum(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
