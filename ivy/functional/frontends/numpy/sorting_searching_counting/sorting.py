4a619751258b853cd647067d8e56e9b9a37d9e91
PatricYan <yanllearnn@gmail.com>
2022-08-28 21:38:46 +0800

    add argsort to numpy

---
+++ ivy/functional/frontends/numpy/sorting_searching_counting/sorting.py
@@ -0,0 +1,13 @@
+import ivy
+
+
+def argsort(
+    x,
+    /,
+    *,
+    axis=-1,
+    descending=False,
+    stable=True,
+    out=None,
+):
+    return ivy.argsort(x, axis=axis, descending=descending, stable=stable, out=out)
