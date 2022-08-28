bee22068fcc226391044c8719aa12dc610221ec2
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-08-28 16:08:33 +0000

    Added isclose frontend for pytorch

--- ivy/functional/frontends/torch/comparison_ops.py
+++ ivy/functional/frontends/torch/comparison_ops.py
@@ -11,6 +11,13 @@ def _compute_allclose_with_tol(input, other, rtol, atol):
     return ivy.all(ret)
 
 
+def _compute_isclose_with_tol(input, other, rtol, atol):
+    return ivy.less_equal(
+        ivy.abs(ivy.subtract(input, other)),
+        ivy.add(atol, ivy.multiply(rtol, ivy.abs(other))),
+    )
+
+
 def allclose(input, other, rtol=1e-05, atol=1e-08, equal_nan=False):
     finite_input = ivy.isfinite(input)
     finite_other = ivy.isfinite(other)
@@ -64,3 +71,29 @@ def greater(input, other, *, out=None):
 
 
 gt = greater
+
+
+def isclose(input, other, rtol=1e-05, atol=1e-08, equal_nan=False):
+    finite_input = ivy.isfinite(input)
+    finite_other = ivy.isfinite(other)
+    if ivy.all(finite_input) and ivy.all(finite_other):
+        return _compute_isclose_with_tol(input, other, rtol, atol)
+
+    else:
+        finites = ivy.bitwise_and(finite_input, finite_other)
+        ret = ivy.zeros_like(finites)
+        ret_ = ret.astype(int)
+        input = input * ivy.ones_like(ret_)
+        other = other * ivy.ones_like(ret_)
+        ret[finites] = _compute_isclose_with_tol(
+            input[finites], other[finites], rtol, atol
+        )
+        nans = ivy.bitwise_invert(finites)
+        ret[nans] = ivy.equal(input[nans], other[nans])
+        if equal_nan:
+            both_nan = ivy.bitwise_and(ivy.isnan(input), ivy.isnan(other))
+            ret[both_nan] = both_nan[both_nan]
+        return ret
+
+
+isclose.unsupported_dtypes = ("float16",)
