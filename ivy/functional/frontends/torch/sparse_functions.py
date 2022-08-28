528346dd30150f6109410a35df42bf1cad9e50d5
qAp <jackchungchiehyu@gmail.com>
2022-08-28 14:16:09 +0000

    Functional frontend Pytorch one_hot

---
+++ ivy/functional/frontends/torch/sparse_functions.py
@@ -0,0 +1,13 @@
+
+
+
+import ivy
+
+
+def one_hot(tensor, num_classes=-1):
+    if num_classes==-1:
+        depth = int(tensor.max() + 1)
+    else:
+        depth = num_classes
+        
+    return ivy.one_hot(tensor, depth)
