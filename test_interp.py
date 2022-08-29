1689247087f2f11b66d53b7133641989eda5d14e
Alex Weiss <roboFiddle@users.noreply.github.com>
2022-08-29 23:18:17 +0000

    clean up test files

--- test_interp.py
+++
@@ -1,8 +0,0 @@
-import ivy
-ivy.set_backend('numpy')
-xp = ivy.array([0, 1, 2])
-yp = ivy.array([2, 3, 4])
-i = -5
-while i < 5:
-    print(i, ivy.functional.frontends.numpy.interp(i, xp, yp, period=3))
-    i += 0.5
