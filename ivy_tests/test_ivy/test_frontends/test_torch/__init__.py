e6b16474fe3f17926b8b9d0f35388c30d13a50aa
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-08-29 18:53:01 +0100

    move convtorch

--- ivy_tests/test_ivy/test_frontends/test_torch/__init__.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/__init__.py
@@ -2,3 +2,10 @@ import torch
 
 
 torch_classes_to_ivy_classes = {}
+
+
+def convtorch(argument):
+    """Convert NativeClass in argument to ivy frontend counter part for torch"""
+    if isinstance(argument, NativeClass):
+        return torch_classes_to_ivy_classes.get(argument._native_class)
+    return argument
