96fd532133574770f0df6c2a5a34dcb3c4ebf4ac
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-29 20:40:34 +0800

    slogdet tests

--- ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
@@ -195,6 +195,37 @@ def test_torch_det(
     )
 
 
+# slogdet
+@handle_cmd_line_args
+@given(
+    dtype_and_x=_get_dtype_and_square_matrix(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.slogdet"
+    ),
+)
+def test_torch_slogdet(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=[dtype],
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="slogdet",
+        input=np.asarray(x, dtype=dtype),
+    )
+
+
 # matmul
 @handle_cmd_line_args
 @given(
