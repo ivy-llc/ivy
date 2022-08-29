934c090562365a648a515faa009ea705413fef44
Nikolas Sokratous <nsokra02@gmail.com>
2022-08-29 12:41:17 +0300

    Update non_linear_activations.py

--- ivy/functional/frontends/jax/nn/non_linear_activations.py
+++ ivy/functional/frontends/jax/nn/non_linear_activations.py
@@ -337,8 +337,7 @@ swish.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
 
 
 def hard_swish(x):
-    res = (x * ivy.minimum(ivy.maximum(x+3, 0.0), 6.0)) / 6
-    
+    res = (x * ivy.minimum(ivy.maximum(x + 3, 0.0), 6.0)) / 6
     return ivy.asarray(res, dtype=x.dtype)
 
 
