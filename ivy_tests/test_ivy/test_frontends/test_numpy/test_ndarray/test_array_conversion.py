8e35fa22dff170f3225b8df53725cfefd518c3c0
Sowrab Chowdhury <sowrab.chowdhury@kcl.ac.uk>
2022-08-29 23:15:53 +0100

    Add base test to ndarray.tolist

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_array_conversion.py
@@ -0,0 +1,42 @@
+# global
+import ivy
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+
+@given(
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtypes=_dtypes(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.ndarray.tolist"
+    ),
+)
+
+
+def test_numpy_ndarray_tolist(
+    shape,
+    dtypes,
+    num_positional_args,
+    fw,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtypes,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=False,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="ndarray.tolist",
+        test_values=False,
+        shape=shape,
+        dtype=dtypes[0],
+    )
