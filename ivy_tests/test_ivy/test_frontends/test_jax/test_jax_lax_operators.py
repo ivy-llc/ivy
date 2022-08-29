1c96c2aec84bc28c12de2e8c3f8c6e16df306ad0
CatB1t <skytedits@gmail.com>
2022-08-29 16:45:21 +0200

    add log to JAX frontend.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1563,3 +1563,34 @@ def test_jax_lax_clamp(
         x=xs[0],
         max=xs[2],
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float", full=True),
+        min_value=1,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.log"
+    ),
+)
+def test_jax_lax_log(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.log",
+        x=np.asarray(x, dtype=input_dtype),
+    )
