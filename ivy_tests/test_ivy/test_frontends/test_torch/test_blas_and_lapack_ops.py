85a2318ffdcc9a400ea4e5b6e76d4d2a3ae790e3
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-08-29 20:34:59 +0800

    qr test fix

--- ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
@@ -309,8 +309,8 @@ def test_torch_pinverse(
 @given(
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float", index=1, full=True),
-        min_num_dims=3,
-        max_num_dims=5,
+        min_num_dims=2,
+        max_num_dims=2,
         min_dim_size=2,
         max_dim_size=5,
         min_value=2,
