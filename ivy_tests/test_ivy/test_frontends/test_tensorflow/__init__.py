3732d915f2a81cd7cde9086f346a109cd8c99dfc
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-08-29 18:52:26 +0100

    move convtensor

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/__init__.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/__init__.py
@@ -2,3 +2,8 @@ import tensorflow
 
 
 tensorflow_classes_to_ivy_classes = {}
+def convtensor(argument):
+    """Convert NativeClass in argument to ivy frontend counter part for tensorflow"""
+    if isinstance(argument, NativeClass):
+        return tensorflow_classes_to_ivy_classes.get(argument._native_class)
+    return argument
