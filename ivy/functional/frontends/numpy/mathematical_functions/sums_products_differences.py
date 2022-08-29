bf053c2fa0c12c5c5b08155f653f5f10aa1aca76
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-08-29 17:20:04 +0100

    update prod and sum frontend function

--- ivy/functional/frontends/numpy/mathematical_functions/sums_products_differences.py
+++ ivy/functional/frontends/numpy/mathematical_functions/sums_products_differences.py
@@ -6,6 +6,33 @@ from ivy.func_wrapper import from_zero_dim_arrays_to_float
 
 
 @from_zero_dim_arrays_to_float
+def sum(
+    x,
+    /,
+    *,
+    axis=None,
+    dtype=None,
+    keepdims=False,
+    out=None,
+    initial=None,
+    where=True,
+):
+    if not where:
+        return 0
+    if initial:
+        s = ivy.shape(x, as_array=True)
+        s[axis] = 1
+        header = ivy.full(ivy.Shape(s), initial)
+        if where:
+            x = ivy.where(where, x, ivy.default(out, ivy.zeros_like(x)))
+        x = ivy.concat([x, header], axis=axis)
+    return ivy.sum(x, axis=axis, dtype=dtype, keepdims=keepdims, out=out)
+
+
+sum.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
 def prod(
         x,
         /,
@@ -17,17 +44,16 @@ def prod(
         initial=None,
         where=True
 ):
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    if not where:
+        return 0
     if initial:
         s = ivy.shape(x, as_array=True)
         s[axis] = 1
         header = ivy.full(ivy.Shape(s), initial)
         if where:
-            x = ivy.where(where, x, ivy.default(out, ivy.zeros_like(x)))
+            x = ivy.where(where, x, ivy.default(out, ivy.ones_like(x)))
         x = ivy.concat([x, header], axis=axis)
-    ret = ivy.prod(x, axis=axis, keepdims=keepdims, out=out)
-    return ret
+    return ivy.prod(x, axis=axis, dtype=dtype, keepdims=keepdims, out=out)
 
 
 prod.unsupported_dtypes = {"torch": ("float16",)}
