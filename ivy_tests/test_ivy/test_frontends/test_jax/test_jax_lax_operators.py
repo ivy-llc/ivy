be4699c14aa7f3d86570aa782a3f9b616c7036f8
CatB1t <skytedits@gmail.com>
2022-08-29 17:25:10 +0200

    add rev to JAX frontend.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1594,3 +1594,36 @@ def test_jax_lax_log(
         fn_tree="lax.log",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_x_axis=_dtype_x_bounded_axis(
+        available_dtypes=helpers.get_dtypes("numeric", full=True),
+        min_num_dims=1,
+        min_dim_size=2,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.rev"
+    ),
+)
+def test_jax_lax_rev(
+    dtype_x_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x, axis = dtype_x_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.rev",
+        operand=np.asarray(x, dtype=input_dtype),
+        dimensions=(axis,),
+    )
