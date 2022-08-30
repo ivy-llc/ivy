2418f4416cb78765e3a12faa5ab401d63bba5612
Mustafa <mustafa1adel@gmail.com>
2022-08-30 21:15:47 +0200

    add binary_crossentropy #3551 (#3658)
    * add binary_crossentropy
    
    * add white spaces
    
    * add test_binary_crossentropy
    
    * Update test_metrics.py
    
    * Update test_metrics.py
    
    * edit binary_crossentropy
    
    * edit test_binary_crossentropy
    
    * reformate binary_crossentropy
    
    * reformate binary_crossentropy
    
    * Update metrics.py
    
    * Update metrics.py
    
    * Update test_metrics.py
    
    * Update test_metrics.py
    
    * Update test_metrics.py
    
    * Update test_metrics.py

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -33,3 +33,31 @@ mean_absolute_error.unsupported_dtypes = {
     "numpy": ("int8", "float64"),
     "torch": ("int8", "float64"),
 }
+
+
+def binary_crossentropy(y_true, y_pred,
+                        from_logits: bool = False,
+                        label_smoothing: float = 0.):
+    """Computes the binary crossentropy loss.
+    Parameters
+    ----------
+        y_true:
+            Ground truth values.
+        y_pred:
+            The predicted values.
+        from_logits: bool
+            Whether `y_pred` is expected to be a logits array/tensor. By default,
+            we assume that `y_pred` encodes a probability distribution.
+        label_smoothing: Float
+            in [0, 1]. If > `0` then smooth the labels by
+            squeezing them towards 0.5 That is, using `1. - 0.5 * label_smoothing`
+            for the target class and `0.5 * label_smoothing` for the non-target class.
+    Returns
+    -------
+        Binary crossentropy loss value.
+    """
+
+    if from_logits:
+        y_pred = ivy.softmax(y_pred)
+    return ivy.mean(ivy.binary_cross_entropy(y_true,
+                                             y_pred, label_smoothing))
