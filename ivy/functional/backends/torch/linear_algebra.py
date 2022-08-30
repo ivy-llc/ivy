2df8ecee6411bc3a9740af47887f9ce044643f80
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-08-30 14:11:54 +0100

    fix unsupported_dtypes in torch linear_algebra

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -135,7 +135,6 @@ def matrix_power(
     return torch.linalg.matrix_power(x, n, out=out)
 
 
-matrix_power.unsupported_dtypes = ("float16",)
 matrix_power.support_native_out = True
 
 
@@ -151,6 +150,7 @@ def matrix_rank(
     return ret
 
 
+matrix_rank.unsupported_dtypes = ("float16",)
 matrix_rank.support_native_out = True
 
 
@@ -307,9 +307,6 @@ def trace(
     return ret
 
 
-trace.unsupported_dtypes = ("float16",)
-
-
 def vecdot(
     x1: torch.Tensor,
     x2: torch.Tensor,
@@ -319,17 +316,6 @@ def vecdot(
 ) -> torch.Tensor:
     x1, x2 = x1.to(torch.float32), x2.to(torch.float32)
     return torch.tensordot(x1, x2, dims=([axis], [axis]), out=out)
-
-
-vecdot.unsupported_dtypes = (
-    "int8",
-    "int16",
-    "int32",
-    "int64",
-    "uint8",
-    "float16",
-    "float64",
-)
 
 
 vecdot.support_native_out = True
@@ -344,7 +330,6 @@ def vector_norm(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     py_normalized_vector = torch.linalg.vector_norm(x, ord, axis, keepdims, out=out)
-
     if py_normalized_vector.shape == ():
         ret = torch.unsqueeze(py_normalized_vector, 0)
     else:
@@ -380,4 +365,3 @@ def vector_to_skew_symmetric_matrix(
 
 
 vector_to_skew_symmetric_matrix.support_native_out = True
-vector_to_skew_symmetric_matrix.unsupported_dtypes = ("float16", "int8")
