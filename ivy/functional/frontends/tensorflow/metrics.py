f6a7870f8cace8f5bdbea2ec42a0f0f89fdacd50
Mustafa <mustafa1adel@gmail.com>
2022-08-30 20:45:36 +0200

    Update metrics.py

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -37,7 +37,7 @@ mean_absolute_error.unsupported_dtypes = {
 
 def binary_crossentropy(y_true, y_pred,
                         from_logits: bool = False,
-                        label_smoothing: float = 0.) -> ivy.Array:
+                        label_smoothing: float = 0.):
     """Computes the binary crossentropy loss.
     Parameters
     ----------
