bb003f65cbb20c9041f05b3651f16a4cc68964f9
Jamie Line <jamie@lets-unify.ai>
2022-08-31 17:29:00 +0100

    Added scrappy version of Torch triu_indices

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -41,21 +41,43 @@ cumsum.unsupported_dtypes = (
 
 
 def triu_indices(row, col, offset=0, dtype="int64", device="cpu", layout=None):
+    if row == 0 or col == 0:
+        return ivy.array([], dtype=dtype)
+
     lowest_included_diagonal = [
-        [i, i - offset] for i in range(-abs(offset), max(row, col) + abs(offset))
+        [i - offset, i] for i in range(-abs(offset), max(row, col) + abs(offset))
     ]
 
     # all_indices = []
     all_indices = [
-        [i, index[1]]
+        (i, index[1])
         for index in lowest_included_diagonal
         for i in range(0, index[0] + 1)
+        # if 0 <= i < row and 0 <= index[1] < col
     ]
-    # for index in lowest_included_diagonal:
-    # [X, increasing_number] until intersects lowest_included_diagonal
-    # all_indices.append([index[0], i] for i in range(0, index[1]+1))
-    #    all_indices += [[i, index[1]] for i in range(0, index[0] + 1)]
 
-    data = ivy.asarray(all_indices, copy=False)
+    all_indices = [
+        (i, index[1])
+        for i in range(
+            0, lowest_included_diagonal[-1][1] + 1
+        )  # The largest possible value is in the last item, and we wish to include it.
+        for index in lowest_included_diagonal
+        if 0 <= i < row
+        and 0 <= index[1] < col
+        and i <= index[0]  # THIS LAST AND IS NEW AND SKETCH
+    ]
+
+    # all_indices = list(dict.fromkeys(all_indices)) # Remove duplicate entries.
+    # all_indices.sort()
+    # all_indices = list(k for k, _ in itertools.groupby(all_indices))
+
+    # all_indices_filtered = list(filter(lambda i: 0 <= i[0] < row and 0 <= i[1] < col,
+    #   all_indices))
+
+    if len(all_indices) == 0:
+        return ivy.array([], dtype=dtype)
+
+    print(f"Row: {row}, Col: {col}, Offset: {offset}")
+
+    data = ivy.asarray(all_indices, copy=False, dtype=dtype)
     return data.matrix_transpose()
-    # return ivy.array(all_indices).matrix_transpose()
