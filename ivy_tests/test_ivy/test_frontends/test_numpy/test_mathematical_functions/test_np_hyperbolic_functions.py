49e949b525f5055f37db325270d86a1d1946b95c
Nicolas <n.904@hotmail.com>
2022-08-31 19:58:52 +0200

    arccosh test

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_hyperbolic_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_hyperbolic_functions.py
@@ -111,46 +111,6 @@ def test_numpy_tanh(
     )
 
 
-# arccosh
-@handle_cmd_line_args
-@given(
-    dtype_and_x=_dtype_x(available_dtypes=ivy_np.valid_float_dtypes),
-    dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.arccosh"
-    ),
-)
-def test_numpy_arccosh(
-    dtype_and_x,
-    dtype,
-    as_variable,
-    with_out,
-    num_positional_args,
-    native_array,
-    fw,
-):
-    (input_dtype, x), where = dtype_and_x
-
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="numpy",
-        fn_tree="arccosh",
-        x=np.asarray(x, dtype=input_dtype[0]),
-        out=None,
-        where=where,
-        casting="same_kind",
-        order="k",
-        dtype=dtype,
-        subok=True,
-        test_values=False,
-    )
-
-
 @st.composite
 def _dtype_x(draw, **kwargs):
     dtype, x, shape = draw(helpers.dtype_and_values(**kwargs, ret_shape=True))
@@ -194,6 +154,46 @@ def test_numpy_arcsinh(
         fw=fw,
         frontend="numpy",
         fn_tree="arcsinh",
+        x=np.asarray(x, dtype=input_dtype[0]),
+        out=None,
+        where=where,
+        casting="same_kind",
+        order="k",
+        dtype=dtype,
+        subok=True,
+        test_values=False,
+    )
+
+
+# arccosh
+@handle_cmd_line_args
+@given(
+    dtype_and_x=_dtype_x(available_dtypes=ivy_np.valid_float_dtypes),
+    dtype=st.sampled_from(ivy_np.valid_float_dtypes + (None,)),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.arccosh"
+    ),
+)
+def test_numpy_arccosh(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    (input_dtype, x), where = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="arccosh",
         x=np.asarray(x, dtype=input_dtype[0]),
         out=None,
         where=where,
