529950cd3a69fdb467851f7507d62ebffbff402a
CatB1t <skytedits@gmail.com>
2022-08-31 10:59:56 +0200

    remove redundant dtype checking in test.

--- ivy_tests/test_ivy/test_functional/test_core/test_general.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_general.py
@@ -249,10 +249,6 @@ def test_arrays_equal(*, x0_n_x1_n_res, device, fw):
 )
 def test_to_numpy(*, x0_n_x1_n_res, device, fw):
     dtype, object_in = x0_n_x1_n_res
-    assume(not (fw == "torch" and (dtype in ["uint16", "uint32", "uint64"])))
-    # torch does not support those dtypes
-    assume(not (fw == "mxnet" and dtype == "int16"))
-    # mxnet does not support int16
     # smoke test
     ret = ivy.to_numpy(ivy.array(object_in, dtype=dtype, device=device))
     # type test
