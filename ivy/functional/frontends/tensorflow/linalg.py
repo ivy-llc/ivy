92eb2ae5bdd93ef3b932cfdf3d374490d3fff337
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2022-08-31 22:50:12 +0800

    Resolved conflict

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -27,8 +27,3 @@ def slogdet(input, name=None):
 
 
 slogdet.unsupported_dtypes = ("float16", "bfloat16")
-
-def norm(x, axis=None, keepdims=False, ord=2):
-    return ivy.matrix_norm(x, axis, keepdims, ord)
-
-norm.unsupported_dtypes("float16", "bfloat16")
