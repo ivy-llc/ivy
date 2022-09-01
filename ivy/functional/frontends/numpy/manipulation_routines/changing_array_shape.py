5fe475728d321270b37f704493169a01a0020c16
Ojas Kulkarni <40053959+ojask12@users.noreply.github.com>
2022-09-01 11:22:45 +0530

    flake8 lint changes

--- ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
@@ -1,10 +1,11 @@
 # local
 import ivy
 
+
 def reshape(x,
             /,
             shape,
             *,
             out=None,
             copy=None):
-    return ivy.reshape(x, shape,copy=copy, out=out)
+    return ivy.reshape(x, shape, copy=copy, out=out)
