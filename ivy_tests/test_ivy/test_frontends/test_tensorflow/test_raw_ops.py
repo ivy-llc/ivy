f0b8a9cfb9d0c865a7549d2bab1d5e87e5790b11
Sakib Ahammed <11179214+sakibkuet10@users.noreply.github.com>
2022-09-01 09:07:25 +0100

    Add RawOps: Sub for TensorFlow frontend (#3668)
    Co-authored-by: unevilicorn <unevilicorn@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -616,6 +616,40 @@ def test_tensorflow_Minimum(
     )
 
 
+# Sub
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy.valid_numeric_dtypes,
+        num_arrays=2,
+        shared_dtype=True
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.Sub"
+    )
+)
+def test_tensorflow_Sub(
+        dtype_and_x,
+        as_variable,
+        num_positional_args,
+        native_array,
+        fw
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="raw_ops.Sub",
+        x=np.asarray(x[0], dtype=dtype[0]),
+        y=np.asarray(x[1], dtype=dtype[1]),
+    )
+
+
 # Less
 @handle_cmd_line_args
 @given(
