49978980a9da6ba2c1f848af8ed7e030ecc05103
LeoZZZ <leozz1999@163.com>
2022-09-01 13:29:23 +0800

    Reformatting

--- ivy/functional/frontends/torch/convolution_functions.py
+++ ivy/functional/frontends/torch/convolution_functions.py
@@ -4,17 +4,17 @@ import ivy
 def conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
     """
 
-    Parameters (Same as PyTorch Implementation)
-    ----------
-    input: input tensor of shape (batch_size, d_in, w)
-    weight: filters of shape (d_out, d_in/groups, fw)
-    bias:  optional bias of shape (d_out). Default: None
-    stride: the stride of the convolving kernel. Can be a single number or a one-element tuple (sW,). Default: 1
-    padding: implicit paddings on both sides of the input. Can be a string {‘valid’, ‘same’}, single number or a
-             one-element tuple (padW,). Default: 0 padding='valid' is the same as no padding.
-             padding='same' pads the input so the output has the same shape as the input.
-    dilation: the spacing between kernel elements. Can be a single number or a one-element tuple (dW,). Default: 1
-    groups: split input into groups, in_channels and out_channels should be divisible by the number of groups. Default: 1
+    Parameters (Same as PyTorch Implementation) ---------- input: input tensor of
+    shape (batch_size, d_in, w) weight: filters of shape (d_out, d_in/groups,
+    fw) bias:  optional bias of shape (d_out). Default: None stride: the stride of
+    the convolving kernel. Can be a single number or a one-element tuple (sW,
+    ). Default: 1 padding: implicit paddings on both sides of the input. Can be a
+    string {‘valid’, ‘same’}, single number or a one-element tuple (padW,). Default:
+    0 padding='valid' is the same as no padding. padding='same' pads the input so the
+    output has the same shape as the input. dilation: the spacing between kernel
+    elements. Can be a single number or a one-element tuple (dW,). Default: 1 groups:
+    split input into groups, in_channels and out_channels should be divisible by the
+    number of groups. Default: 1
 
     Returns
     -------
@@ -37,10 +37,17 @@ def conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
     _in_chunk = in_channel // groups
     _out_chunk = out_channel // groups
     for i in range(groups):
-        _input = ivy.permute_dims(input[:, int(i * _in_chunk):int((i + 1) * _in_chunk), :], axes=(0, 2, 1))
+        _input = ivy.permute_dims(
+            input[:, int(i * _in_chunk):int((i + 1) * _in_chunk), :],
+            axes=(0, 2, 1)
+        )
         out.append(
-            ivy.conv1d(_input, _weight, stride, padding,
-                       dilations=dilation)[:, :, int(i * _out_chunk):int((i + 1) * _out_chunk)]
+            ivy.conv1d(_input,
+                       _weight,
+                       stride,
+                       padding,
+                       dilations=dilation
+                       )[:, :, int(i * _out_chunk):int((i + 1) * _out_chunk)]
         )
     out = ivy.concat(out, axis=-1)
     if bias is not None:
@@ -51,17 +58,17 @@ def conv1d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
 def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
     """
 
-    Parameters (Same as PyTorch Implementation)
-    ----------
-    input: input tensor of shape (batch_size, d_in, h, w)
-    weight: filters of shape (d_out, d_in/groups, fh, fw)
-    bias:  optional bias of shape (d_out). Default: None
-    stride: the stride of the convolving kernel. Can be a single number or a one-element tuple (sH, sW). Default: 1
-    padding: implicit paddings on both sides of the input. Can be a string {‘valid’, ‘same’}, single number or a
-             one-element tuple (padH, padW). Default: 0 padding='valid' is the same as no padding.
-             padding='same' pads the input so the output has the same shape as the input.
-    dilation: the spacing between kernel elements. Can be a single number or a one-element tuple (dH, dW). Default: 1
-    groups: split input into groups, in_channels and out_channels should be divisible by the number of groups. Default: 1
+    Parameters (Same as PyTorch Implementation) ---------- input: input tensor of
+    shape (batch_size, d_in, h, w) weight: filters of shape (d_out, d_in/groups, fh,
+    fw) bias:  optional bias of shape (d_out). Default: None stride: the stride of
+    the convolving kernel. Can be a single number or a one-element tuple (sH,
+    sW). Default: 1 padding: implicit paddings on both sides of the input. Can be a
+    string {‘valid’, ‘same’}, single number or a one-element tuple (padH,
+    padW). Default: 0 padding='valid' is the same as no padding. padding='same' pads
+    the input so the output has the same shape as the input. dilation: the spacing
+    between kernel elements. Can be a single number or a one-element tuple (dH,
+    dW). Default: 1 groups: split input into groups, in_channels and out_channels
+    should be divisible by the number of groups. Default: 1
 
     Returns
     -------
@@ -78,20 +85,35 @@ def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
         padding = padding.upper()
     else:
         if type(padding) == int:
-            input = ivy.zero_pad(input, pad_width=((0, 0), (0, 0), (padding, padding), (padding, padding)))
+            input = ivy.zero_pad(input,
+                                 pad_width=(
+                                     (0, 0),
+                                     (0, 0),
+                                     (padding, padding),
+                                     (padding, padding))
+                                 )
         else:
             w_pad, h_pad = padding
-            input = ivy.zero_pad(input, pad_width=((0, 0), (0, 0), (w_pad, w_pad), (h_pad, h_pad)))
+            input = ivy.zero_pad(input, pad_width=(
+                (0, 0), (0, 0), (w_pad, w_pad), (h_pad, h_pad)
+            ))
 
     out = []
     _weight = ivy.permute_dims(weight, axes=(2, 3, 1, 0))
     _in_chunk = in_channel // groups
     _out_chunk = out_channel // groups
     for i in range(groups):
-        _input = ivy.permute_dims(input[:, int(i * _in_chunk):int((i + 1) * _in_chunk), :], axes=(0, 2, 3, 1))
+        _input = ivy.permute_dims(
+            input[:, int(i * _in_chunk):int((i + 1) * _in_chunk), :],
+            axes=(0, 2, 3, 1)
+        )
         out.append(
-            ivy.conv2d(_input, _weight, stride, padding,
-                       dilations=dilation)[:, :, :, int(i * _out_chunk):int((i + 1) * _out_chunk)]
+            ivy.conv2d(_input,
+                       _weight,
+                       stride,
+                       padding,
+                       dilations=dilation
+                       )[:, :, :, int(i * _out_chunk):int((i + 1) * _out_chunk)]
         )
     out = ivy.concat(out, axis=-1)
     if bias is not None:
