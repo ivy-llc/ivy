607525eb6736c17e5f9b5294f52ecd488c33bcfa
Akinwande Gbenga Vincent <akinwandegbengavincent@gmail.com>
2022-09-01 23:37:54 +0100

    add absolute function and tests to pytorch frontend

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -177,3 +177,9 @@ def sqrt(input, *, out=None):
 
 
 sqrt.unsupported_dtypes = ("float16",)
+
+def absolute(input, *, out=None):
+    return ivy.abs(input, out=out)
+
+
+absolute.unsupported_dtypes = ("float16",)
