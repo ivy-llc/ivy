019d0417127a85487cbfbe6e02638f1b2236722c
shunleili <lesliebjut@gmail.com>
2022-09-01 14:11:40 +0200

    Hi. I noticed ivy doesnt have ivy.dot. Should I write a dot function or use np.dot?

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -7,11 +7,12 @@ def sparse_categorical_accuracy(
 ):
     y_true = ivy.array(y_true)
     y_pred = ivy.array(y_pred)
+    sample_weight = ivy.array(sample_weight)
 
-    if sample_weight == None:
-        sample_weight: ndarray = ivy.ones(y_true.shape)
+    if sample_weight is None:
+        sample_weight = ivy.ones(y_true.shape)
     count = ivy.sum(sample_weight)
-    total = ivy.dot(sample_weight, ivy.equal(y_true, ivy.argmax(y_pred, axis=1)))
+    total = ivy.vecdot(sample_weight, ivy.equal(y_true, ivy.argmax(y_pred, axis=1)))
 
     return ivy.divide(total, count)
 
