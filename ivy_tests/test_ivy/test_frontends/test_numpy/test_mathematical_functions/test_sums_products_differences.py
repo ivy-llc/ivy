66ffffb8c56e4eaca206030b00f0b7dba36635f9
CatB1t <skytedits@gmail.com>
2022-09-01 17:37:49 +0200

    add cumsum to NumPy frontend.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
@@ -61,3 +61,40 @@ def test_numpy_prod(
         where=where,
         test_values=True,
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=np_frontend_helpers.dtype_x_bounded_axis(
+        available_dtypes=helpers.get_dtypes("valid", full=True),
+        min_num_dims=1,
+    ),
+    dtype=helpers.get_dtypes("numeric"),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.cumsum"
+    ),
+)
+def test_numpy_cumsum(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x, axis = dtype_and_x
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="cumsum",
+        x=np.asarray(x, dtype=input_dtype),
+        axis=axis,
+        dtype=dtype,
+        out=None,
+    )
