a000f90fe7ce93c29a78f36e23f7d28b3eeb0cb2
Khairul Nizam <khairuknizam@gmail.com>
2022-09-02 15:33:03 +0800

    Did up Degrees #3957

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -7,15 +7,15 @@ from ivy.func_wrapper import from_zero_dim_arrays_to_float
 
 @from_zero_dim_arrays_to_float
 def cos(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="k",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="k",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -30,15 +30,15 @@ cos.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def sin(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="k",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="k",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -53,15 +53,15 @@ sin.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def tan(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="K",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="K",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -76,15 +76,15 @@ tan.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def arcsin(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="K",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="K",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -99,15 +99,15 @@ arcsin.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def arccos(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="K",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="K",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -122,15 +122,15 @@ arcsin.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def arctan(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="K",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="K",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -145,15 +145,15 @@ arctan.unsupported_dtypes = {"torch": ("float16",)}
 
 @from_zero_dim_arrays_to_float
 def cosh(
-    x,
-    /,
-    out=None,
-    *,
-    where=True,
-    casting="same_kind",
-    order="k",
-    dtype=None,
-    subok=True,
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting="same_kind",
+        order="k",
+        dtype=None,
+        subok=True,
 ):
     if dtype:
         x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
@@ -164,3 +164,23 @@ def cosh(
 
 
 cosh.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
+def degrees(
+        x,
+        /,
+        out=None,
+        *,
+        where=True,
+        casting='same_kind',
+        order='K',
+        dtype=None,
+        subok=True,
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.degrees(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
