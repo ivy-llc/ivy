168bffecfcc3185769bbbd4d2ecbf20c345b6bb5
shunleili <lesliebjut@gmail.com>
2022-09-02 15:35:50 +0200

    It works with ivy.vecdot.

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -1,5 +1,6 @@
 import ivy
 
+
 def sparse_categorical_accuracy(
         y_true,
         y_pred,
@@ -7,10 +8,11 @@ def sparse_categorical_accuracy(
 ):
     y_true = ivy.array(y_true)
     y_pred = ivy.array(y_pred)
-    sample_weight = ivy.array(sample_weight)
 
     if sample_weight is None:
         sample_weight = ivy.ones(y_true.shape)
+
+    sample_weight = ivy.array(sample_weight)
     count = ivy.sum(sample_weight)
     total = ivy.vecdot(sample_weight, ivy.equal(y_true, ivy.argmax(y_pred, axis=1)))
 
@@ -18,10 +20,10 @@ def sparse_categorical_accuracy(
 
 
 def sparse_categorical_crossentropy(
-    y_true,
-    y_pred,
-    from_logits=False,
-    axis=-1
+        y_true,
+        y_pred,
+        from_logits=False,
+        axis=-1
 ):
     if from_logits:
         y_pred = ivy.softmax(y_pred)
