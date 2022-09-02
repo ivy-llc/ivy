ef89b147a8e61cb581e1f2205a0026e1ee4475bd
Ojas Kulkarni <40053959+ojask12@users.noreply.github.com>
2022-09-02 20:13:43 +0530

    test_frontend_numpy passing with some manipulation

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_array_shape.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_array_shape.py
@@ -1,4 +1,5 @@
 # global
+import numpy as np
 from hypothesis import given, strategies as st
 
 # local
@@ -6,18 +7,27 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
+@st.composite
+def dtypes_x_reshape(draw):
+    dtypes, x = draw(
+        helpers.dtype_and_values(
+            shape=helpers.get_shape(
+                allow_none=False,
+                min_num_dims=1,
+                max_num_dims=5,
+                min_dim_size=1,
+                max_dim_size=10,
+            )
+        )
+    )
+    shape = draw(helpers.reshape_shapes(shape=np.array(x).shape))
+    return dtypes, x, shape
+
+
 # reshape
 @handle_cmd_line_args
 @given(
-    x_dtypes=helpers.dtype_and_values(
-        shape=helpers.get_shape(
-            allow_none=False,
-            min_num_dims=1,
-            max_num_dims=5,
-            min_dim_size=1,
-            max_dim_size=10,
-        )
-    ),
+    dtypes_x_shape=dtypes_x_reshape(),
     copy=st.booleans(),
     with_out=st.booleans(),
     as_variable=helpers.array_bools(),
@@ -27,7 +37,7 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     native_array=helpers.array_bools(),
 )
 def test_numpy_reshape(
-    x_dtypes,
+    dtypes_x_shape,
     copy,
     with_out,
     as_variable,
@@ -35,18 +45,16 @@ def test_numpy_reshape(
     native_array,
     fw,
 ):
-    dtypes, x = x_dtypes
-    shape = helpers.reshape_shapes(shape=x.shape)
+    dtypes, x, shape = dtypes_x_shape
     helpers.test_frontend_function(
         input_dtypes=dtypes,
         as_variable_flags=as_variable,
         with_out=with_out,
-        num_positional_args=num_positional_args,
+        num_positional_args=2,
         native_array_flags=native_array,
         fw=fw,
         frontend="numpy",
         fn_tree="reshape",
         x=x,
-        shape=shape,
-        copy=copy,
+        shape=shape
     )
