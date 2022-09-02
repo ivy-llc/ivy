a58a87ab8950c8fcc779480d23afabd979592ade
Sharique Shamim <shariqueshamim001@gmail.com>
2022-09-02 13:24:56 +0530

    resolved as_variable and native_array flags

---
+++ ivy/functional/frontends/numpy/linear_algebra/linalg/norms_and_other_numbers.py
@@ -0,0 +1,8 @@
+# global
+import ivy
+
+# slogdet
+def slogdet(a):
+    return ivy.slogdet(a)
+
+slogdet.unsupported_dtypes = ("float16",)
---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_linalg/test_norms_and_other_numbers.py
@@ -0,0 +1,37 @@
+# global
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+from ivy_tests.test_ivy.test_functional.test_core.test_linalg import _get_dtype_and_matrix
+
+# slogdet
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_float_dtypes,
+        min_num_dims=2,
+        max_num_dims=2,
+        shape=helpers.ints(min_value=2, max_value=10).map(lambda x: tuple([x, x])),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.linalg.slogdet"
+    )
+)
+def test_numpy_slogdet(dtype_and_x, as_variable, native_array, num_positional_args, fw):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="linalg.slogdet",
+        a=np.array(x, dtype=dtype),
+        test_values=False
+    )
