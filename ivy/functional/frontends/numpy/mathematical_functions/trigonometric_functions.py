9f25dcc20486bfbce3264dc0a4337ad7300ef7e7
Nouran Gamal <g_nouran@yahoo.com>
2022-09-03 08:15:26 +0200

    Update trigonometric_functions.py

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -182,12 +182,10 @@ def arctan2(
     if dtype:
         x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
         x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
-    ret = ivy.atan2(x1,x2, out=out)
+    ret = ivy.atan2(x1, x2, out=out)
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
 
 
 arctan2.unsupported_dtypes = {"torch": ("float16",)}
-
-
