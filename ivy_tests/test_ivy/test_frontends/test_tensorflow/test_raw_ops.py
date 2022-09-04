f7a37fa70464e6ffa36fd0c0e459f4e680b4c166
Tariq Jamal <15tariqj@gmail.com>
2022-09-04 01:04:59 +0100

    add atan2 tensorflow frontend test

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -318,6 +318,38 @@ def test_tensorflow_Atan(
     )
 
 
+# Atan2
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_tf.valid_float_dtypes))
+        ),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.Atan2"
+    ),
+)
+def test_tensorflow_Atan2(
+    dtype_and_x, as_variable, num_positional_args, fw, native_array
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="raw_ops.Atan2",
+        x=np.asarray(x[1], dtype=dtype[1]),
+        y=np.asarray(x[0], dtype=dtype[0]),
+    )
+
+
 # BitwiseAnd
 @handle_cmd_line_args
 @given(
