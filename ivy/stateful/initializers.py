b330b39a20527c50a8a23b09a983bd94d6c968d7
Nouran Gamal <g_nouran@yahoo.com>
2022-09-04 08:25:11 +0200

    Update initializers.py

--- ivy/stateful/initializers.py
+++ ivy/stateful/initializers.py
@@ -10,7 +10,6 @@ class Constant:
     def __init__(self, constant):
         self._constant = constant
         
-
     def create_variables(
         self, var_shape, device, fan_out=None, fan_in=None, dtype=None
     ):
