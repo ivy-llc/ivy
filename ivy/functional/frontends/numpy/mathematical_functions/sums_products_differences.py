92044c165a8e5fd8e382fff59fd3d39c3b9f0701
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-09-04 15:14:56 +0100

    Individual task: Framework specific classes in the frontend (#3698)
    * Add NativeClass as a representation of a framework_specific class
    
    * create native_classes_to_ivy_classes dictionary for torch
    
    * fix format
    
    * add native_classes_to_ivy_classes dictionary for tensorflow
    
    * add native_classes_to_ivy_classes dictionary for numpy
    
    * add native_classes_to_ivy_classes dictionary for jax
    
    * small update to dic name
    
    * small update to dic name
    
    * small update to dic name
    
    * small update to dic name
    
    * add the conversion progress from NativeClass to represented classes in test_frontend_function
    
    * fix some formatting issues
    
    * fix some other docstring problems
    
    * fix some docstring problems
    
    * fix docstring error
    
    * update implementation of prod to make initial argument effective.
    
    * update default value of where argument
    
    * update data generation for sum test
    
    * removed the unused initial and where argument from backend np.sum
    
    * moving numpy.sum implementation to mathematical_functions/sums_products_differences
    
    * update prod and sum frontend function
    
    * moving test of sum to test_numpy/test_mathematical_functions/test_sums_products_differences.py
    
    * update the test for sum and prod
    
    * small fix
    
    * fix default value of keepdims for prod
    
    * fix formatting
    
    * fix formatting
    
    * update data generation of test
    
    * move convjax
    
    * move convnumpy
    
    * move convtensor
    
    * move convtorch
    
    * small fix
    
    * small fix
    
    * small fix
    
    * add import of native class
    
    * add import of native class
    
    * add import of native class
    
    * add import of native class
    
    * optimize conv process
    
    * small fix
    
    * fix formatting
    
    * fix formatting
    
    * fix ivy_np usage
    
    * remove empty file
    
    * remove empty file
    
    * fix formatting
    
    * Create correlating.py
    
    * Create test_correlating.py
    
    * Update test_sums_products_differences.py
    
    * Update test_sums_products_differences.py
    
    * resolve wrong auto merge
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * try replace ivy,current_backend() with ivy_np
    
    * update implementatino return ivy array than 0
    
    * feed tuple into ivy.shape

--- ivy/functional/frontends/numpy/mathematical_functions/sums_products_differences.py
+++ ivy/functional/frontends/numpy/mathematical_functions/sums_products_differences.py
@@ -6,31 +6,72 @@ from ivy.func_wrapper import from_zero_dim_arrays_to_float
 
 
 @from_zero_dim_arrays_to_float
-def prod(
-    x, /, *, axis=None, dtype=None, out=None, keepdims=None, initial=None, where=None
+def sum(
+    x,
+    /,
+    *,
+    axis=None,
+    dtype=None,
+    keepdims=False,
+    out=None,
+    initial=None,
+    where=True,
 ):
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
-    if ivy.is_array(where):
-        x = ivy.where(where, x, ivy.default(out, ivy.zeros_like(x)), out=out)
-    ret = ivy.prod(x, axis=axis, keepdims=keepdims, out=out)
-    return ret
+    if not where:
+        if dtype:
+            return ivy.astype(ivy.array(0), ivy.as_ivy_dtype(dtype))
+        return ivy.array(0)
+    if initial:
+        s = ivy.shape(x, as_array=True)
+        s[axis] = 1
+        header = ivy.full(ivy.Shape(tuple(s)), initial)
+        if where:
+            x = ivy.where(where, x, ivy.default(out, ivy.zeros_like(x)))
+        x = ivy.concat([x, header], axis=axis)
+    return ivy.sum(x, axis=axis, dtype=dtype, keepdims=keepdims, out=out)
+
+
+sum.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
+def prod(
+        x,
+        /,
+        *,
+        axis=None,
+        dtype=None,
+        out=None,
+        keepdims=False,
+        initial=None,
+        where=True
+):
+    if not where:
+        if dtype:
+            return ivy.astype(ivy.array(0), ivy.as_ivy_dtype(dtype))
+        return ivy.array(0)
+    if initial:
+        s = ivy.shape(x, as_array=True)
+        s[axis] = 1
+        header = ivy.full(ivy.Shape(tuple(s)), initial)
+        if where:
+            x = ivy.where(where, x, ivy.default(out, ivy.ones_like(x)))
+        x = ivy.concat([x, header], axis=axis)
+    return ivy.prod(x, axis=axis, dtype=dtype, keepdims=keepdims, out=out)
 
 
 prod.unsupported_dtypes = {"torch": ("float16",)}
-
-
-def sum(x, /, *, axis=None, dtype=None, out=None, keepdims=None, where=None):
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
-    if ivy.is_array(where):
-        x = ivy.where(where, x, ivy.default(out, ivy.zeros_like(x)), out=out)
-    return ivy.sum(x, axis=axis, keepdims=keepdims, out=out)
 
 
 def cumsum(a, /, axis=None, dtype=None, out=None):
     return ivy.cumsum(a, axis=axis, dtype=dtype, out=out)
 
 
+cumsum.unsupported_dtypes = {"torch": ("float16",)}
+
+
 def cumprod(a, /, axis=None, dtype=None, out=None):
     return ivy.cumprod(a, axis=axis, dtype=dtype, out=out)
+    
+    
+cumprod.unsupported_dtypes = {"torch": ("float16",)}
