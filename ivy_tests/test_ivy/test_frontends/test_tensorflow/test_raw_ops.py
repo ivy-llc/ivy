12a56925c8f4a1f2fd1d1083f03690616159a516
Ahmed Hossam <61050404+ahmshalan@users.noreply.github.com>
2022-09-04 18:00:50 +0200

    Added log to tensorflow frontend. (unifyai#3811) (#3817)
    Co-authored-by: sreegandh804 sreegandh804@gmail.com

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -794,3 +794,32 @@ def test_tensorflow_Expm1(
         fn_tree="raw_ops.Expm1",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+
+# Log
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_tf.valid_float_dtypes))
+        ),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.Log"
+    ),
+)
+def test_tensorflow_Log(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="raw_ops.Log",
+        x=np.asarray(x, dtype=input_dtype)
+    )
