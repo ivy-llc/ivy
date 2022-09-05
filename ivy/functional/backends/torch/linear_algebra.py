a7e235755b260ca8f8a58a7ff84dc93c2dd119b6
Zaeem Ansari <99063526+zaeemansari70@users.noreply.github.com>
2022-09-05 15:45:00 +0500

    `vecdot()` fixes for linalg.py (#3894)
    Co-authored-by: zaeemansari70, Sherry30, ZoeCD

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -318,9 +318,8 @@ def vecdot(
     *,
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
-    # dtype = torch.promote_types(x1.dtype, x2.dtype)
-    # x1, x2 = x1.type(torch.float32), x2.type(torch.float32)
-    return torch.tensordot(x1, x2, dims=([axis], [axis]), out=out)
+    dtype = torch.promote_types(x1.dtype, x2.dtype)
+    return torch.tensordot(x1, x2, dims=([axis], [axis]), out=out).type(dtype)
 
 
 vecdot.support_native_out = True
