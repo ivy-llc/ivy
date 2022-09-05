20a6c0d5d2fb4edbda708658685cec8bdb263d57
Ahmed Omar <40790298+ahmedo42@users.noreply.github.com>
2022-09-05 11:42:45 +0000

    added more functions, tests

--- ivy/functional/frontends/torch/reduction_ops.py
+++ ivy/functional/frontends/torch/reduction_ops.py
@@ -1,4 +1,5 @@
 import ivy
+from collections import namedtuple
 
 
 def argmax(input, dim=None, keepdim=False):
@@ -15,3 +16,85 @@ def amax(input, dim, keepdim=False, *, out=None):
 
 def amin(input, dim, keepdim=False, *, out=None):
     return ivy.min(input, axis=dim, keepdims=keepdim, out=out)
+
+
+def aminmax(input, *, dim=None, keepdim=False, out=None):
+    out_arrays = out is not None
+    if out_arrays:
+        if not isinstance(out_arrays, tuple):
+            raise TypeError("out must be a tuple")
+        if len(out) != 2:
+            raise ValueError("out must be a tuple of length 2")
+
+    min_array = out[0] if out_arrays else None
+    max_array = out[1] if out_arrays else None
+    tuple_key = "aminmax_out" if out_arrays else "aminmax"
+
+    min_out = ivy.min(input, axis=dim, keepdims=keepdim, out=min_array)
+
+    max_out = ivy.max(input, axis=dim, keepdims=keepdim, out=max_array)
+
+    ret = namedtuple(tuple_key, ["min", "max"])(min_out, max_out)
+
+    return ret
+
+
+aminmax.unsupported_dtypes = ("float16",)
+
+
+def all(input, dim=None, keepdim=False, *, out=None):
+    input_dtype = ivy.as_ivy_dtype(input.dtype)
+    ret = ivy.all(input, axis=dim, keepdims=keepdim, out=out)
+    if input_dtype == ivy.uint8:
+        ret = ivy.astype(ret, ivy.uint8, out=out)
+    return ret
+
+
+def any(input, dim=None, keepdim=False, *, out=None):
+    input_dtype = ivy.as_ivy_dtype(input.dtype)
+    ret = ivy.any(input, axis=dim, keepdims=keepdim, out=out)
+    if input_dtype == ivy.uint8:
+        ret = ivy.astype(ret, ivy.uint8, out=out)
+    return ret
+
+
+def max(input, dim=None, keepdim=False, *, out=None):
+    out_arrays = out is not None
+    if out_arrays:
+        if not isinstance(out_arrays, tuple):
+            raise TypeError("out must be a tuple")
+        if len(out) != 2:
+            raise ValueError("out must be a tuple of length 2")
+
+    values_array = out[0] if out_arrays else None
+    indices_array = out[1] if out_arrays else None
+    tuple_key = "max_out" if out is not None else "max"
+
+    values = ivy.max(input, axis=dim, keepdims=keepdim, out=values_array)
+    indices = ivy.argmax(input, axis=dim, keepdims=keepdim, out=indices_array)
+
+    ret = namedtuple(tuple_key, ["values", "indices"])(values, indices)
+
+    return ret
+
+
+def min(input, dim=None, keepdim=False, *, out=None):
+    out_arrays = out is not None
+    if out_arrays:
+        if not isinstance(out_arrays, tuple):
+            raise TypeError("out must be a tuple")
+        if len(out) != 2:
+            raise ValueError("out must be a tuple of length 2")
+
+    values_array = out[0] if out_arrays else None
+    indices_array = out[1] if out_arrays else None
+    tuple_key = "min_out" if out is not None else "min"
+
+    values = ivy.min(input, axis=dim, keepdims=keepdim, out=values_array)
+    indices = ivy.argmin(input, axis=dim, keepdims=keepdim, out=indices_array)
+
+    tuple_key = "min_out" if out is not None else "min"
+
+    ret = namedtuple(tuple_key, ["values", "indices"])(values, indices)
+
+    return ret
--- ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
@@ -1,33 +1,39 @@
 # global
-from typing import Sequence
 import numpy as np
 from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+import ivy.functional.backends.numpy as ivy_np
+import ivy.functional.backends.torch as ivy_torch
 
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.statistical_dtype_values(function="argmax"),
-    as_variable=helpers.array_bools(num_arrays=1),
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.argmax"
     ),
-    native_array=helpers.array_bools(num_arrays=1),
     keepdims=st.booleans(),
 )
 def test_torch_argmax(
-    dtype_and_x,
+    dtype_input_axis,
     as_variable,
     num_positional_args,
     native_array,
     keepdims,
     fw,
 ):
-    input_dtype, x, dim = dtype_and_x
-    dim = dim[0] if isinstance(dim, Sequence) else dim
+    input_dtype, x, axis = dtype_input_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -38,31 +44,36 @@ def test_torch_argmax(
         frontend="torch",
         fn_tree="argmax",
         input=np.asarray(x, dtype=input_dtype),
-        dim=dim,
+        dim=axis,
         keepdim=keepdims,
     )
 
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.statistical_dtype_values(function="argmin"),
-    as_variable=helpers.array_bools(num_arrays=1),
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.argmin"
     ),
-    native_array=helpers.array_bools(num_arrays=1),
     keepdims=st.booleans(),
 )
 def test_torch_argmin(
-    dtype_and_x,
+    dtype_input_axis,
     as_variable,
     num_positional_args,
     native_array,
     keepdims,
     fw,
 ):
-    input_dtype, x, dim = dtype_and_x
-    dim = dim[0] if isinstance(dim, Sequence) else dim
+    input_dtype, x, axis = dtype_input_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -73,24 +84,30 @@ def test_torch_argmin(
         frontend="torch",
         fn_tree="argmin",
         input=np.asarray(x, dtype=input_dtype),
-        dim=dim,
+        dim=axis,
         keepdim=keepdims,
     )
 
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.statistical_dtype_values(function="amax"),
-    as_variable=helpers.array_bools(num_arrays=1),
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.amax"
     ),
-    native_array=helpers.array_bools(num_arrays=1),
     keepdims=st.booleans(),
     with_out=st.booleans(),
 )
 def test_torch_amax(
-    dtype_and_x,
+    dtype_input_axis,
     as_variable,
     num_positional_args,
     native_array,
@@ -98,7 +115,7 @@ def test_torch_amax(
     with_out,
     fw,
 ):
-    input_dtype, x, dim = dtype_and_x
+    input_dtype, x, axis = dtype_input_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -109,7 +126,7 @@ def test_torch_amax(
         frontend="torch",
         fn_tree="amax",
         input=np.asarray(x, dtype=input_dtype),
-        dim=dim,
+        dim=axis,
         keepdim=keepdims,
         out=None,
     )
@@ -117,17 +134,23 @@ def test_torch_amax(
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.statistical_dtype_values(function="amin"),
-    as_variable=helpers.array_bools(num_arrays=1),
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.amin"
     ),
-    native_array=helpers.array_bools(num_arrays=1),
     keepdims=st.booleans(),
     with_out=st.booleans(),
 )
 def test_torch_amin(
-    dtype_and_x,
+    dtype_input_axis,
     as_variable,
     num_positional_args,
     native_array,
@@ -135,7 +158,7 @@ def test_torch_amin(
     with_out,
     fw,
 ):
-    input_dtype, x, dim = dtype_and_x
+    input_dtype, x, axis = dtype_input_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -146,7 +169,304 @@ def test_torch_amin(
         frontend="torch",
         fn_tree="amin",
         input=np.asarray(x, dtype=input_dtype),
-        dim=dim,
+        dim=axis,
         keepdim=keepdims,
         out=None,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtypes_arrays=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        num_arrays=3,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.aminmax"
+    ),
+    keepdims=st.booleans(),
+    with_out=st.booleans(),
+    axis=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        min_size=1,
+        max_size=1,
+        force_int=True,
+    ),
+)
+def test_torch_aminmax(
+    dtypes_arrays,
+    as_variable,
+    num_positional_args,
+    native_array,
+    keepdims,
+    with_out,
+    axis,
+    fw,
+):
+    input_dtype, x = dtypes_arrays
+    out = None
+    if with_out:
+        if fw == "numpy" and len(x[0]) == 1:
+            out = tuple(
+                [
+                    np.asarray(x[1][0], dtype=input_dtype[0]),
+                    np.asarray(x[2][0], dtype=input_dtype[0]),
+                ]
+            )
+        else:
+            out = tuple(
+                [
+                    np.asarray(x[1], dtype=input_dtype[0]),
+                    np.asarray(x[2], dtype=input_dtype[0]),
+                ]
+            )
+
+    input_dtypes = input_dtype[0] if not with_out else input_dtype
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="aminmax",
+        input=np.asarray(x[0], dtype=input_dtype[0]),
+        dim=axis,
+        keepdim=keepdims,
+        out=out,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        allow_inf=False,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.all"
+    ),
+    keepdims=st.booleans(),
+    with_out=st.booleans(),
+)
+def test_torch_all(
+    dtype_input_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    keepdims,
+    with_out,
+    fw,
+):
+    input_dtype, x, axis = dtype_input_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="all",
+        input=np.asarray(x, dtype=input_dtype),
+        dim=axis,
+        keepdim=keepdims,
+        out=None,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        allow_inf=False,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.any"
+    ),
+    keepdims=st.booleans(),
+    with_out=st.booleans(),
+)
+def test_torch_any(
+    dtype_input_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    keepdims,
+    with_out,
+    fw,
+):
+    input_dtype, x, axis = dtype_input_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="any",
+        input=np.asarray(x, dtype=input_dtype),
+        dim=axis,
+        keepdim=keepdims,
+        out=None,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtypes_arrays=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        num_arrays=3,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.max"
+    ),
+    keepdims=st.booleans(),
+    with_out=st.booleans(),
+    axis=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        min_size=1,
+        max_size=1,
+        force_int=True,
+    ),
+)
+def test_torch_max(
+    dtypes_arrays,
+    as_variable,
+    num_positional_args,
+    native_array,
+    keepdims,
+    with_out,
+    axis,
+    fw,
+):
+    input_dtype, x = dtypes_arrays
+    out = None
+    if with_out:
+        if fw == "numpy" and len(x[0]) == 1:
+            out = tuple(
+                [
+                    np.asarray(x[1][0], dtype=input_dtype[0]),
+                    np.asarray(x[2][0], dtype=input_dtype[0]),
+                ]
+            )
+        else:
+            out = tuple(
+                [
+                    np.asarray(x[1], dtype=input_dtype[0]),
+                    np.asarray(x[2], dtype=input_dtype[0]),
+                ]
+            )
+
+    input_dtypes = input_dtype[0] if not with_out else input_dtype
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="max",
+        input=np.asarray(x[0], dtype=input_dtype[0]),
+        dim=axis,
+        keepdim=keepdims,
+        out=out,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtypes_arrays=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_numeric_dtypes).intersection(
+                set(ivy_torch.valid_numeric_dtypes)
+            ),
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        num_arrays=3,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.min"
+    ),
+    keepdims=st.booleans(),
+    with_out=st.booleans(),
+    axis=helpers.get_axis(
+        shape=st.shared(helpers.get_shape(min_num_dims=1), key="shape"),
+        min_size=1,
+        max_size=1,
+        force_int=True,
+    ),
+)
+def test_torch_min(
+    dtypes_arrays,
+    as_variable,
+    num_positional_args,
+    native_array,
+    keepdims,
+    with_out,
+    axis,
+    fw,
+):
+    input_dtype, x = dtypes_arrays
+    out = None
+    if with_out:
+        if fw == "numpy" and len(x[0]) == 1:
+            out = tuple(
+                [
+                    np.asarray(x[1][0], dtype=input_dtype[0]),
+                    np.asarray(x[2][0], dtype=input_dtype[0]),
+                ]
+            )
+        else:
+            out = tuple(
+                [
+                    np.asarray(x[1], dtype=input_dtype[0]),
+                    np.asarray(x[2], dtype=input_dtype[0]),
+                ]
+            )
+
+    input_dtypes = input_dtype[0] if not with_out else input_dtype
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="min",
+        input=np.asarray(x[0], dtype=input_dtype[0]),
+        dim=axis,
+        keepdim=keepdims,
+        out=out,
     )
