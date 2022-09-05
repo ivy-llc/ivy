40e50dcdbb5b3f6e47415f18c884a367d964a6d1
ddzhulaii <daryna.dzhulay@gmail.com>
2022-09-05 16:40:15 +0000

    add missing argument

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -2,4 +2,5 @@ import ivy
 
 
 def accumulate(method, array, axis=0, dtype=None, out=None):
-    return array.__array_ufunc__(method, array=array, axis=axis, dtype=dtype, out=out)
+    return array.__array_ufunc__(method, "accummulate", array=array,
+        axis=axis, dtype=dtype, out=out)
