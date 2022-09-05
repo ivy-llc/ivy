b690ea0314ea884fc0b3f8c22fc889c823c5d649
Jamie Line <jamie@lets-unify.ai>
2022-09-06 00:15:25 +0100

    Added trace to Torch frontend
    Former-commit-id: 1705cfad47e7197c7339019668b469b41d0272be

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -2,6 +2,8 @@
 import numpy as np
 from hypothesis import given, strategies as st
 
+import ivy
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.numpy as ivy_np
@@ -186,4 +188,41 @@ def test_torch_cumsum(
         dim=axis,
         dtype=input_dtype,
         out=None,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_values=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)  # Not supported by Torch
+            ),
+        ),
+        shape=st.shared(helpers.get_shape(min_num_dims=2, max_num_dims=2), key="shape"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.trace"
+    ),
+)
+def test_torch_trace(
+    dtype_and_values,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, value = dtype_and_values
+    if "float16" in input_dtype:
+        input_dtype = ivy.FloatDtype("float32")  # Float16 is unsupported for trace.
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="trace",
+        input=np.asarray(value, dtype=input_dtype),
     )
