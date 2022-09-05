5b051312f27eb561a42f200977dfd78b73921a98
Joshua Yeats <69589979+yeatsy@users.noreply.github.com>
2022-09-05 17:34:26 +0100

    Adding `zeros()` to Pytorch Frontend (#4061)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
@@ -156,3 +156,43 @@ def test_torch_ones(
         device=device,
         requires_grad=requires_grad,
     )
+
+
+# zeros
+@handle_cmd_line_args
+@given(
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtypes=_dtypes(),
+    requires_grad=_requires_grad(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.zeros"
+    ),
+)
+def test_torch_zeros(
+        shape,
+        dtypes,
+        requires_grad,
+        device,
+        num_positional_args,
+        fw,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtypes,
+        as_variable_flags=False,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=False,
+        fw=fw,
+        frontend="torch",
+        fn_tree="zeros",
+        size=shape,
+        dtype=dtypes[0],
+        device=device,
+        requires_grad=requires_grad,
+    )
