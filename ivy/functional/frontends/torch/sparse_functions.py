be043202783e1c7d157d327de3fd78c99521bdfb
Jack Yu <jackchungchiehyu@gmail.com>
2022-09-05 09:42:24 +0000

    Clean up code

--- ivy/functional/frontends/torch/sparse_functions.py
+++ ivy/functional/frontends/torch/sparse_functions.py
@@ -1,15 +1,12 @@
-
-
-
 import ivy
 
 
 def one_hot(tensor, num_classes=-1):
-    if num_classes==-1:
+    if num_classes == -1:
         depth = int(tensor.max() + 1)
     else:
         depth = num_classes
-        
     return ivy.one_hot(tensor, depth)
 
-one_hot.supported_dtypes = {'torch': ('int64',)}
+
+one_hot.supported_dtypes = {"torch": ("int64",)}
