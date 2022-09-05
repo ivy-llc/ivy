db7a3bf1360c697b0e516bcefb2edc06198b5e25
Deepanshu Gupta <41968942+deepanshug4@users.noreply.github.com>
2022-09-05 22:53:39 +0530

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1006,4 +1006,4 @@ def test_tensorflow_intersection(
         fn_name="intersection",
         x=np.asarray(x[0], dtype=input_dtype[0]),
         y=np.asarray(x[1], dtype=input_dtype[1]),
-)
+    )
