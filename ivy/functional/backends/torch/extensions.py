96b6372c15ccb52fd39d1c64421249dc423fd540
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-06 16:37:00 +0800

    add csr todo comments

--- ivy/functional/backends/torch/extensions.py
+++ ivy/functional/backends/torch/extensions.py
@@ -2,6 +2,7 @@ import torch
 
 
 def is_native_sparse_array(x):
+    # to add csr
     return x.layout == torch.sparse_coo
 
 
@@ -9,4 +10,5 @@ def get_sparse_components(x):
     if x.layout == torch.sparse_coo:
         x = x.coalesce()
         return x.indices(), x.values(), x.size()
+    # to add csr
     raise Exception("not a Tensor with sparse COO layout")
