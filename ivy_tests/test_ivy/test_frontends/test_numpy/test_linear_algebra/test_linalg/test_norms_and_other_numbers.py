9fb13235cd68d4b1c2cbb46519f1331d5f82d9e5
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-09-06 10:59:12 +0530

    numpy frontend linalg - norms and numbers (#3959)
    * added numpy frontend linalg.inv and linalg.pinv with test
    
    * resolved as_variable and native_array flags
    
    * resolved as_variable and native_array flags
    
    * slogdet
    
    * slogdet
    
    * lint formatting
    
    * lint formatting
    
    * lint formatting
    
    * slogdet
    
    * lint formatting
    
    * passing -1 as tuple in reshape

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_linalg/test_norms_and_other_numbers.py
@@ -0,0 +1,37 @@
+# global
+import numpy as np
+from hypothesis import given
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy.functional.backends.numpy as ivy_np
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+
+# slogdet
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=ivy_np.valid_float_dtypes,
+        min_value=0,
+        max_value=50,
+        shape=helpers.ints(min_value=2, max_value=20).map(lambda x: tuple([x, x])),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.linalg.slogdet"
+    )
+)
+def test_numpy_slogdet(dtype_and_x, as_variable, native_array, num_positional_args, fw):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="linalg.slogdet",
+        a=np.array(x, dtype=dtype)
+    )
