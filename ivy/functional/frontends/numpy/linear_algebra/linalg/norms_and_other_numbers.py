824df378e8a6225c57f552166eb16328ee4242b2
Sharique Shamim <shariqueshamim001@gmail.com>
2022-09-06 11:25:26 +0530

    linalg matrix rank and det

--- ivy/functional/frontends/numpy/linear_algebra/linalg/norms_and_other_numbers.py
+++ ivy/functional/frontends/numpy/linear_algebra/linalg/norms_and_other_numbers.py
@@ -1,6 +1,25 @@
 # global
 import ivy
 
+# matrix_rank
+
+
+def matrix_rank(A, tol=None, hermitian=False):
+    ret = ivy.matrix_rank(x=A, rtol=tol)
+    return ivy.array(ret, dtype=ivy.int64)
+
+
+matrix_rank.unsupported_dtypes = ("float16",)
+
+# det
+
+
+def det(a):
+    return ivy.det(a)
+
+
+det.unsupported_dtypes = ("float16",)
+
 # slogdet
 
 
