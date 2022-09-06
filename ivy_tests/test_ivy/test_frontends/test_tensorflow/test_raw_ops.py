8d29cb50d5103b922bdcd41dbbde4b4b22ae4bac
Tariq Jamal <15tariqj@gmail.com>
2022-09-06 16:02:12 +0100

    Revert "add argument for "with_out" in tf atan2 test"
    This reverts commit 7c618e29d7b0ea42e5c804f43aeb8103446c3fde.

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -333,13 +333,13 @@ def test_tensorflow_Atan(
     ),
 )
 def test_tensorflow_Atan2(
-    dtype_and_x, as_variable, with_out, num_positional_args, fw, native_array
+    dtype_and_x, as_variable, num_positional_args, fw, native_array
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
+        with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
