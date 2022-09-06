20afe055d72fd6d8730b2ddc8bb92eb46327cd28
Matt <matthew-barrett@hotmail.co.uk>
2022-09-06 15:37:05 +0100

    small fix to `cholesky`

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -20,8 +20,7 @@ def cholesky(
     else:
         ret = torch.transpose(
             torch.linalg.cholesky(
-                torch.transpose(x, dim0=len(x.shape) - 1, dim1=len(x.shape) - 2),
-                out=out,
+                torch.transpose(x, dim0=len(x.shape) - 1, dim1=len(x.shape) - 2)
             ),
             dim0=len(x.shape) - 1,
             dim1=len(x.shape) - 2,
