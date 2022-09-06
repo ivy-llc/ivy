f4fb0a1a95ce4554b35d47847af1d16e8b4cb4a4
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-06 17:32:31 +0530

    Fixed failing tests for svd (#4102)

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -225,7 +225,10 @@ def svd(
     return results(U, D, VT)
 
 
-svd.unsupported_dtypes = ("float16",)
+svd.unsupported_dtypes = (
+    "float16",
+    "bfloat16",
+)
 
 
 def svdvals(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
