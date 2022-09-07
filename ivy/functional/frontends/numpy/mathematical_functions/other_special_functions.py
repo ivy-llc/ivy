b94fd04a79639ef79f35b550a07b94efe8777a5c
Hemanth Kumar Mudedla <hemanthkumarmudedlagmail.com>
2022-09-07 15:56:01 +0530

    code refactored

--- ivy/functional/frontends/numpy/mathematical_functions/other_special_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/other_special_functions.py
@@ -14,9 +14,7 @@ def lcm(
     dtype=None,
     subok=True,
 ):
-    if dtype:
-        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
-        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+
     ret = ivy.lcm(x1, x2, out=out)
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
@@ -35,14 +33,12 @@ def gcd(
     dtype=None,
     subok=True,
 ):
-    if dtype:
-        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
-        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
+
     ret = ivy.gcd(x1, x2, out=out)
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
-    
+
 
 @from_zero_dim_arrays_to_float
 def deg2rad(
