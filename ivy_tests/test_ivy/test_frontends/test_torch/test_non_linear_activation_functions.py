09f583b929b7a05015f55306986f26a62f33a5c9
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-07 16:51:49 +0530

    Adding `celu` to torch torch frontend (#4160)
    * Added celu function to torch frontend
    
    * Added celu test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -506,3 +506,44 @@ def test_torch_elu_(
         input=np.asarray(input, dtype=input_dtype),
         alpha=alpha,
     )
+
+
+# celu
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            ),
+        ),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.celu"
+    ),
+    inplace=st.booleans(),
+    alpha=helpers.floats(min_value=0, max_value=1, exclude_min=True),
+)
+def test_torch_celu(
+    dtype_and_input,
+    inplace,
+    alpha,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.celu",
+        input=np.asarray(input, dtype=input_dtype),
+        alpha=alpha,
+        inplace=inplace,
+    )
