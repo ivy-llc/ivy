eb87883dea6fe39a082966c602c3ec755703328d
Prabhu-V <108232437+Prabhu-V@users.noreply.github.com>
2022-09-06 23:02:02 -0400

    Formatting changes

--- ivy/functional/frontends/torch/vision_functions.py
+++ ivy/functional/frontends/torch/vision_functions.py
@@ -65,7 +65,9 @@ def pixel_unshuffle(input, downscale_factor):
     oh = h / downscale_factor
     ow = w / downscale_factor
 
-    input_reshaped = ivy.reshape(input, (b, c, oh, downscale_factor, ow, downscale_factor))
+    input_reshaped = ivy.reshape(input,
+     (b, c, oh, downscale_factor, ow, downscale_factor))
     return ivy.reshape(
         ivy.permute_dims(input_reshaped, (0, 1, 3, 5, 2, 4)), (b, oc, oh, ow)
     )
+    
