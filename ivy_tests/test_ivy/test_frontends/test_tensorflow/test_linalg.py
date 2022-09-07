0053f6718dbfcb3dd4e44be8123b664c602531bd
CatB1t <skytedits@gmail.com>
2022-09-07 18:13:08 +0200

    lint fix.

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -7,8 +7,9 @@ import sys
 import ivy_tests.test_ivy.helpers as helpers
 import ivy.functional.backends.tensorflow as ivy_tf
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
-from ivy_tests.test_ivy.test_functional.test_core.test_linalg \
-    import _get_dtype_value1_value2_axis_for_tensordot
+from ivy_tests.test_ivy.test_functional.test_core.test_linalg import (
+    _get_dtype_value1_value2_axis_for_tensordot,
+)
 
 
 @st.composite
