bf901fca7a7a42e0f3f7f0541830babe0302ca9d
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-09-07 13:25:34 +0100

    fix small formatting errors of test_indexing_slicing_joining_mutating_ops for torch

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -51,10 +51,10 @@ def _arrays_idx_n_dtypes(draw):
 @st.composite
 def _array_idxes_n_dtype(draw, **kwargs):
     num_dims = draw(helpers.ints(min_value=1, max_value=4))
-    dtype, x= draw(helpers.dtype_and_values(**kwargs, 
+    dtype, x = draw(helpers.dtype_and_values(**kwargs, 
                                             min_num_dims=num_dims, 
                                             max_num_dims=num_dims, 
-                                           shared_dtype=True))
+                                            shared_dtype=True))
     idxes = draw(tuple(helpers.ints(min_value=0, max_value=num_dims - 1)))
     return x, idxes, dtype
 
