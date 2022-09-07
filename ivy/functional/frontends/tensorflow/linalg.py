803e6e1efd253538c94bea24607290f4bf7af72c
Unevilicorn <unevilicorn@gmail.com>
2022-09-07 16:25:45 +0100

    remove tensorflow unsupported_dtypes

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -10,35 +10,20 @@ def det(input, name=None):
     return ivy.det(input)
 
 
-det.unsupported_dtypes = ("float16", "bfloat16")
-
-
 def eigvalsh(tensor, name=None):
     return ivy.eigvalsh(tensor)
-
-
-eigvalsh.unsupported_dtypes = ("float16", "bfloat16")
 
 
 def solve(x, y):
     return ivy.solve(x, y)
 
 
-solve.unsupported_dtypes = ("float16", "bfloat16")
-
-
 def slogdet(input, name=None):
     return ivy.slogdet(input)
 
 
-slogdet.unsupported_dtypes = ("float16", "bfloat16")
-
-
 def pinv(a, rcond=None, validate_args=False, name=None):
     return ivy.pinv(a, rcond)
-
-
-pinv.unsupported_dtypes = ("float16", "bfloat16")
 
 
 def tensordot(x, y, axes, name=None):
--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -10,9 +10,6 @@ def tan(x, name=None):
     return ivy.tan(x)
 
 
-tan.unsupported_dtypes = {"torch": ("float16",)}
-
-
 def multiply(x, y, name=None):
     return ivy.multiply(x, y)
 
@@ -25,20 +22,12 @@ def logical_xor(x, y, name="LogicalXor"):
     return ivy.logical_xor(x, y)
 
 
-logical_xor.supported_dtypes = {"torch": ("bool",)}
-
-
 def divide(x, y, name=None):
     return ivy.divide(x, y)
 
 
 def negative(x, name=None):
     return ivy.negative(x)
-
-
-negative.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
 
 
 def reciprocal_no_nan(input_tensor, name="reciprocal_no_nan"):
@@ -65,24 +54,8 @@ def reduce_euclidean_norm(
     )  # ord = '2' is the euclidean norm
 
 
-reduce_euclidean_norm.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
-
-
 def reduce_logsumexp(input_tensor, axis=None, keepdims=False, name="reduce_logsumexp"):
     return ivy.exp(input_tensor).sum(axis=axis, keepdims=keepdims).log()
-
-
-reduce_logsumexp.unsupported_dtypes = {
-    "tensorflow": (
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-    ),
-    "torch": ("float16",),
-}
 
 
 def logical_and(x, y, name="LogicalAnd"):
@@ -100,92 +73,36 @@ def reduce_max(input_tensor, axis=None, keepdims=False, name="reduce_max"):
     return ivy.max(input_tensor, axis=axis, keepdims=keepdims)
 
 
-reduce_max.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
-
-
 def reduce_min(input_tensor, axis=None, keepdims=False, name="reduce_min"):
     return ivy.min(input_tensor, axis=axis, keepdims=keepdims)
-
-
-reduce_min.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
 
 
 def reduce_prod(input_tensor, axis=None, keepdims=False, name="reduce_prod"):
     return ivy.prod(input_tensor, axis=axis, keepdims=keepdims)
 
 
-reduce_prod.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
-
-
 def reduce_std(input_tensor, axis=None, keepdims=False, name="reduce_std"):
     return ivy.std(input_tensor, axis=axis, keepdims=keepdims)
-
-
-reduce_std.unsupported_dtypes = {
-    "tensorflow": (
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-    ),
-}
 
 
 def asinh(x, name="asinh"):
     return ivy.asinh(x)
 
 
-asinh.unsupported_dtypes = {"torch": ("float16",)}
-
-
 def reduce_sum(input_tensor, axis=None, keepdims=False, name="reduce_sum"):
     return ivy.sum(input_tensor, axis=axis, keepdims=keepdims)
-
-
-reduce_sum.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
 
 
 def reduce_variance(input_tensor, axis=None, keepdims=False, name="reduce_variance"):
     return ivy.var(input_tensor, axis=axis, keepdims=keepdims)
 
 
-reduce_variance.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "uint64"),
-}
-
-
 def scalar_mul(scalar, x, name="scalar_mul"):
     return ivy.multiply(x, ivy.array([scalar]))
 
 
-scalar_mul.unsupported_dtypes = {
-    "tensorflow": (
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-        "float16",
-        "float32",
-        "float64",
-    ),
-    "torch": ("float16", "bfloat16"),
-    "numpy": ("float16", "bfloat16", "float32", "float64"),
-}
-
-
 def log_sigmoid(x, name=None):
     return -ivy.softplus(-x)
-
-
-log_sigmoid.unsupported_dtypes = {"torch": ("float16", "bfloat16")}
 
 
 def cumprod(x, axis=0, exclusive=False, reverse=False, name=None):
