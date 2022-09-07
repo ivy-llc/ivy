567b3dcede85cee78efb16242cac92b5054a734c
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-09-07 18:30:31 +0100

    Update correlating.py

--- ivy/functional/frontends/numpy/statistics/correlating.py
+++ ivy/functional/frontends/numpy/statistics/correlating.py
@@ -2,41 +2,41 @@
 import ivy
 
 def correlate(
-    a,    
-    v,
+    x,    
+    y,
     *,
     mode = None,
     out = None
 ):
 
-    n = min(len(a),len(v))
-    m = max(len(a),len(v))
+    n = min(len(x),len(y))
+    m = max(len(x),len(y))
     
-    if len(a)>len(v):
+    if len(x)>len(y):
         if mode == "full":
             r = n+m-1
             for j in range(0,n-1):
-                a = ivy.concat((ivy.array(0),a),axis = None)
+                x = ivy.concat((ivy.array(0),x),axis = None)
         elif mode == "same":
             r = m
             for j in range(0,(n-1)//2):
-                a = ivy.concat((ivy.array(0),a),axis = None)
+                x = ivy.concat((ivy.array(0),x),axis = None)
         else:
             r = m-n+1
             
-        ret = ivy.array([(v[:n]*ivy.roll(a,-t)[:n]).sum() for t in range(0,r)])
+        ret = ivy.array([(y[:n]*ivy.roll(x,-t)[:n]).sum() for t in range(0,r)])
 
     else:
         if mode == "full":
             r = n+m-1
             for j in range(0,n-1):
-                v = ivy.concat((ivy.array(0),v),axis = None)
+                y = ivy.concat((ivy.array(0),y),axis = None)
         elif mode == "same":
             r = m
             for j in range(0,(n-1)//2):
-                v = ivy.concat((ivy.array(0),v),axis = None)
+                y = ivy.concat((ivy.array(0),y),axis = None)
         else:
             r = m-n+1
         
-        ret = ivy.flip([(a[:n]*ivy.roll(v,-t)[:n]).sum() for t in range(0,r)])  
+        ret = ivy.flip([(x[:n]*ivy.roll(y,-t)[:n]).sum() for t in range(0,r)])  
     return ret
