72387340c49dd9e1a22798514a39d8f71b6a54b1
khaiprograms <65672847+khaiprograms@users.noreply.github.com>
2022-09-09 01:44:26 +0800

    Did up Degrees #3957

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -233,8 +233,3 @@ def degrees(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
-
-
-degrees.unsupported_dtypes = {"torch": ("float16",)}
-
-
