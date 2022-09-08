f9a1589817d997f8de1e5e298d09e70eb1d7ec4c
JonaOmara <omarajonathan@gmail.com>
2022-09-08 19:51:32 +0300

    Add radians to numpy frontend functions

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -211,3 +211,26 @@ def arctan2(
 
 
 arctan2.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
+def radians(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting='same_kind',
+    order='K',
+    dtype=None,
+    subok=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.multiply(ivy.divide(x, 180), ivy.pi, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+radians.unsupported_dtypes = {"torch": ("float16",)}
