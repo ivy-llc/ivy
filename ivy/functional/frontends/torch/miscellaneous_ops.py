5e7dc47307f288aaca43e3b8ee1d74428581aa0a
Jamie Line <jamie@lets-unify.ai>
2022-09-08 02:59:08 +0100

    Added block_diag to Torch frontend

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -54,3 +54,42 @@ cumprod.unsupported_dtypes = (
 
 def diagonal(input, offset=0, dim1=0, dim2=1):
     return ivy.diagonal(input, offset=offset, axis1=dim1, axis2=dim2)
+
+
+def block_diag(*tensors):
+    total_size_x = sum(
+        [tensor.shape[-1] if len(tensor.shape) > 0 else 1 for tensor in tensors]
+    )
+    total_size_y = sum(
+        [tensor.shape[-2] if len(tensor.shape) > 1 else 1 for tensor in tensors]
+    )
+
+    data = ivy.zeros((total_size_y, total_size_x), dtype=ivy.result_type(*tensors))
+
+    current_x_start = 0
+    current_y_start = 0
+
+    for tensor in tensors:
+        x_size = tensor.shape[-1] if len(tensor.shape) > 0 else 1
+        y_size = tensor.shape[-2] if len(tensor.shape) > 1 else 1
+
+        for x in range(x_size):
+            for y in range(y_size):
+                # It feels like there should be a much faster way of doing this
+                # but that might be my C programming instinct kicking in
+                # because the first thing that comes to mind to fix that is
+                # to do a short string of memcpy operations and some
+                # sketchy pointer arithmetic.
+
+                # Equally the performance may well be fine anyway I haven't checked.
+                if len(tensor.shape) == 2:
+                    data[current_y_start + y][current_x_start + x] = tensor[y][x]
+                elif len(tensor.shape) == 1:
+                    data[current_y_start + y][current_x_start + x] = tensor[x]
+                elif len(tensor.shape) == 0:
+                    data[current_y_start + y][current_x_start + x] = tensor
+
+        current_y_start += y_size
+        current_x_start += x_size
+
+    return data
