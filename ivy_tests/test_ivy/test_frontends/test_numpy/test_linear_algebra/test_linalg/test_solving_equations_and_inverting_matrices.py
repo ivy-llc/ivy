508e13f7241698170de2df48201ddd40e08ad5f6
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-09-09 19:46:54 +0530

    numpy frontend solve and matmul (#4251)
    * numpy frontend solve and matmul
    
    * lint formatting

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_linalg/test_solving_equations_and_inverting_matrices.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_linalg/test_solving_equations_and_inverting_matrices.py
@@ -5,6 +5,35 @@ from hypothesis import given
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+from ivy_tests.test_ivy.test_functional.test_core.test_linalg import \
+    _get_first_matrix, _get_second_matrix
+
+# solve
+
+
+@handle_cmd_line_args
+@given(
+    x=_get_first_matrix(),
+    y=_get_second_matrix(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.solve"
+    )
+)
+def test_numpy_solve(x, y, as_variable, native_array, num_positional_args, fw):
+    dtype1, x1 = x
+    dtype2, x2 = y
+    helpers.test_frontend_function(
+        input_dtypes=[dtype1, dtype2],
+        as_variable_flags=as_variable,
+        with_out=False,
+        native_array_flags=native_array,
+        num_positional_args=num_positional_args,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="linalg.solve",
+        a=np.array(x1, dtype=dtype1),
+        b=np.array(x2, dtype=dtype2)
+    )
 
 # inv
 
