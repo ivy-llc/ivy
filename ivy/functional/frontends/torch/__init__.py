0635555992c736195775bd2b2c70bc576e321fd8
James Keane <81018496+jkeane508@users.noreply.github.com>
2022-09-09 13:55:48 +0100

    Ivy Frontend Instance Method Setup (#3663)
    * init
    
    * function implementations
    
    * test implementations
    
    * formatting
    
    * fix
    
    * euclidean fix
    
    * erfcinv
    
    * function implementations
    
    * fixes
    
    * fixes
    
    * fixes
    
    * Halfway
    
    * Changes to frontend array
    
    * Changes to implementations
    
    * remove frontend_array parent class
    
    * Reset Helpers
    
    * Fixing Requests
    
    * fixes
    
    * Test frontend instance method v1.0
    
    * Added docstrings to helper, test implementations
    
    * instance test implementations
    
    * Update helper
    
    * update ndarray instance methods
    
    * frontend docs
    
    * Drafting docs for instance tests
    
    * Update to docs, unified instance tests
    
    * Fixes to classes
    
    * debugging
    
    Former-commit-id: 0e4789beb5c6c8f4f1f6f95e8066146583628924

--- ivy/functional/frontends/torch/__init__.py
+++ ivy/functional/frontends/torch/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa
+from . import tensor
+from .tensor import Tensor
 from . import indexing_slicing_joining_mutating_ops
 from .indexing_slicing_joining_mutating_ops import *
 from . import pointwise_ops
