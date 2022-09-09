ba1fdade08dacb1a9064902829bd3dc734c8a2ad
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-09 09:55:57 +0800

    fix docstr and warning logging

--- ivy/functional/backends/numpy/extensions.py
+++ ivy/functional/backends/numpy/extensions.py
@@ -2,15 +2,15 @@ import logging
 
 
 def is_native_sparse_array(x):
-    """Numpy does not support sparse arrays."""
+    """Numpy does not support sparse arrays natively."""
     return False
 
 
 def native_sparse_array(data=None, *, indices=None, values=None, dense_shape=None):
-    logging.warning("Numpy does not support sparse arrays, None is returned.")
+    logging.warning("Numpy does not support sparse arrays natively, None is returned.")
     return None
 
 
 def native_sparse_array_to_indices_values_and_shape(x):
-    """Numpy does not support sparse arrays."""
+    logging.warning("Numpy does not support sparse arrays natively, None is returned.")
     return None, None, None
