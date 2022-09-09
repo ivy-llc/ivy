29dfdf51841c27006bf6370e4e5ff79440d952ae
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-09-09 18:15:47 +0530

    minor fix

--- ivy/functional/backends/__init__.py
+++ ivy/functional/backends/__init__.py
@@ -1,11 +1,11 @@
 def dtype_from_version(dic,version):
     for i in dic.keys():
-        if i.strip(' ')[0]==str(version):
+        if i.strip(' ').split(" ")[0]==str(version):
             return dic[i]
-        if "above" in i and int(str(version).strip('.'))>=int(i.strip(' ')[0].strip('.')):
+        if "above" in i and int(str(version).strip('.'))>=int(i.strip(' ').split(" ")[0].strip('.')):
             return dic[i]
-        if "below" in i and int(str(version).strip('.'))<=int(i.strip(' ')[0].strip('.')):
+        if "below" in i and int(str(version).strip('.'))<=int(i.strip(' ').split(" ")[0].strip('.')):
             return dic[i]
-        if "to" in i and (int(str(version).strip('.'))>=int(i.strip(' ')[0].strip('.')) and int(str(version).strip('.'))<=int(i.strip(' ')[2].strip('.'))):
+        if "to" in i and (int(str(version).strip('.'))>=int(i.strip(' ').split(" ")[0].strip('.')) and int(str(version).strip('.'))<=int(i.strip(' ').split(" ")[2].strip('.'))):
             return dic[i]
 
