18c495422d7894661b006789b0b591877bf663f4
Unevilicorn <Unevilicorn@users.noreply.github.com>
2022-09-09 10:17:44 +0100

    Auto dtype tuple (#4197)

--- ivy/functional/frontends/numpy/manipulation_routines/tiling_arrays.py
+++ ivy/functional/frontends/numpy/manipulation_routines/tiling_arrays.py
@@ -1,14 +1,9 @@
-# global
+# local
 import ivy
 
 
 def tile(A, reps):
     return ivy.tile(A, reps)
-
-
-tile.unsupported_dtypes = {
-    "tensorflow": ("uint8", "uint16", "uint32", "int8", "int16"),
-}
 
 
 def repeat(a, repeats, axis=None):
