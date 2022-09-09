afc553e804d2c90ec116d3ceb27ef9fc9d4705f6
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-09-09 15:43:36 +0100

    sort general functions

--- ivy/functional/backends/jax/general.py
+++ ivy/functional/backends/jax/general.py
@@ -19,108 +19,6 @@ from ivy.functional.backends.jax.device import _to_device, _to_array
 from ivy.functional.backends.jax import JaxArray
 
 
-# noinspection PyUnresolvedReferences,PyProtectedMember
-def is_native_array(x, exclusive=False):
-    if exclusive:
-        return isinstance(
-            x,
-            (
-                jax.interpreters.xla._DeviceArray,
-                jaxlib.xla_extension.DeviceArray,
-                Buffer,
-            ),
-        )
-    return isinstance(
-        x,
-        (
-            jax.interpreters.xla._DeviceArray,
-            jaxlib.xla_extension.DeviceArray,
-            Buffer,
-            jax.interpreters.ad.JVPTracer,
-            jax.core.ShapedArray,
-            jax.interpreters.partial_eval.DynamicJaxprTracer,
-        ),
-    )
-
-
-def copy_array(x: JaxArray, *, out: Optional[JaxArray] = None) -> JaxArray:
-    return jnp.array(x)
-
-
-def array_equal(x0: JaxArray, x1: JaxArray) -> bool:
-    return bool(jnp.array_equal(x0, x1))
-
-
-def to_numpy(x: JaxArray, copy: bool = True) -> np.ndarray:
-    if copy:
-        return np.array(_to_array(x))
-    else:
-        return np.asarray(_to_array(x))
-
-
-def to_scalar(x: JaxArray) -> Number:
-    if isinstance(x, Number):
-        return x
-    else:
-        return _to_array(x).item()
-
-
-def to_list(x: JaxArray) -> list:
-    return _to_array(x).tolist()
-
-
-def shape(x: JaxArray, as_array: bool = False) -> Union[ivy.Shape, ivy.Array]:
-    if as_array:
-        return ivy.array(jnp.shape(x), dtype=ivy.default_int_dtype())
-    else:
-        return ivy.Shape(x.shape)
-
-
-def get_num_dims(x, as_tensor=False):
-    return jnp.asarray(len(jnp.shape(x))) if as_tensor else len(x.shape)
-
-
-def container_types():
-    return [FlatMapping]
-
-
-def unstack(x: JaxArray, axis: int, keepdims: bool = False) -> List[JaxArray]:
-    if x.shape == ():
-        return [x]
-    dim_size = x.shape[axis]
-    # ToDo: make this faster somehow, jnp.split is VERY slow for large dim_size
-    x_split = jnp.split(x, dim_size, axis)
-    if keepdims:
-        return x_split
-    return [jnp.squeeze(item, axis) for item in x_split]
-
-
-def inplace_update(
-    x: Union[ivy.Array, JaxArray],
-    val: Union[ivy.Array, JaxArray],
-    ensure_in_backend: bool = False,
-) -> ivy.Array:
-    if ivy.is_array(x) and ivy.is_array(val):
-        if ensure_in_backend:
-            raise Exception("JAX does not natively support inplace updates")
-        (x_native, val_native), _ = ivy.args_to_native(x, val)
-        if ivy.is_ivy_array(x):
-            x.data = val_native
-        else:
-            raise Exception("JAX does not natively support inplace updates")
-        return x
-    else:
-        return val
-
-
-def inplace_arrays_supported():
-    return False
-
-
-def inplace_variables_supported():
-    return False
-
-
 def _infer_dtype(dtype: jnp.dtype, x_dtype: jnp.dtype):
     default_dtype = ivy.infer_default_dtype(x_dtype)
     if ivy.dtype_bits(x_dtype) < ivy.dtype_bits(default_dtype):
@@ -128,6 +26,39 @@ def _infer_dtype(dtype: jnp.dtype, x_dtype: jnp.dtype):
     else:
         dtype = x_dtype
     return dtype
+
+
+def array_equal(x0: JaxArray, x1: JaxArray) -> bool:
+    return bool(jnp.array_equal(x0, x1))
+
+
+def container_types():
+    return [FlatMapping]
+
+
+def copy_array(x: JaxArray, *, out: Optional[JaxArray] = None) -> JaxArray:
+    return jnp.array(x)
+
+
+def cumprod(
+    x: JaxArray,
+    axis: int = 0,
+    exclusive: Optional[bool] = False,
+    *,
+    dtype: Optional[jnp.dtype] = None,
+    out: Optional[JaxArray] = None,
+) -> JaxArray:
+    dtype = ivy.as_native_dtype(dtype)
+    if dtype is None:
+        dtype = _infer_dtype(dtype, x.dtype)
+    if dtype != x.dtype:
+        x = x.astype(dtype)
+    if exclusive:
+        x = jnp.swapaxes(x, axis, -1)
+        x = jnp.concatenate((jnp.ones_like(x[..., -1:]), x[..., :-1]), -1)
+        res = jnp.cumprod(x, -1)
+        return jnp.swapaxes(res, axis, -1)
+    return jnp.cumprod(x, axis)
 
 
 def cumsum(
@@ -164,25 +95,143 @@ def cumsum(
     return jnp.cumsum(x, axis, dtype=dtype)
 
 
-def cumprod(
-    x: JaxArray,
-    axis: int = 0,
-    exclusive: Optional[bool] = False,
+def current_backend_str():
+    return "jax"
+
+
+def gather(
+    params: JaxArray,
+    indices: JaxArray,
+    axis: int = -1,
     *,
-    dtype: Optional[jnp.dtype] = None,
     out: Optional[JaxArray] = None,
 ) -> JaxArray:
-    dtype = ivy.as_native_dtype(dtype)
-    if dtype is None:
-        dtype = _infer_dtype(dtype, x.dtype)
-    if dtype != x.dtype:
-        x = x.astype(dtype)
+    return _to_device(jnp.take_along_axis(params, indices, axis))
+
+
+def gather_nd(
+    params: JaxArray, indices: JaxArray, *, out: Optional[JaxArray] = None
+) -> JaxArray:
+    indices_shape = indices.shape
+    params_shape = params.shape
+    num_index_dims = indices_shape[-1]
+    res_dim_sizes_list = [
+        reduce(mul, params_shape[i + 1 :], 1) for i in range(len(params_shape) - 1)
+    ] + [1]
+    result_dim_sizes = jnp.array(res_dim_sizes_list)
+    implicit_indices_factor = int(result_dim_sizes[num_index_dims - 1].item())
+    flat_params = jnp.reshape(params, (-1,))
+    new_shape = [1] * (len(indices_shape) - 1) + [num_index_dims]
+    indices_scales = jnp.reshape(result_dim_sizes[0:num_index_dims], new_shape)
+    indices_for_flat_tiled = jnp.tile(
+        jnp.reshape(jnp.sum(indices * indices_scales, -1, keepdims=True), (-1, 1)),
+        (1, implicit_indices_factor),
+    )
+    implicit_indices = jnp.tile(
+        jnp.expand_dims(jnp.arange(implicit_indices_factor), 0),
+        (indices_for_flat_tiled.shape[0], 1),
+    )
+    indices_for_flat = indices_for_flat_tiled + implicit_indices
+    flat_indices_for_flat = jnp.reshape(indices_for_flat, (-1,)).astype(jnp.int32)
+    flat_gather = jnp.take(flat_params, flat_indices_for_flat, 0)
+    new_shape = list(indices_shape[:-1]) + list(params_shape[num_index_dims:])
+    ret = jnp.reshape(flat_gather, new_shape)
+    return _to_device(ret)
+
+
+def get_num_dims(x, as_tensor=False):
+    return jnp.asarray(len(jnp.shape(x))) if as_tensor else len(x.shape)
+
+
+def indices_where(x: JaxArray) -> JaxArray:
+    where_x = jnp.where(x)
+    return jnp.concatenate([jnp.expand_dims(item, -1) for item in where_x], -1)
+
+
+def inplace_arrays_supported():
+    return False
+
+
+def inplace_decrement(
+    x: Union[ivy.Array, JaxArray], val: Union[ivy.Array, JaxArray]
+) -> ivy.Array:
+    (x_native, val_native), _ = ivy.args_to_native(x, val)
+    if ivy.is_ivy_array(x):
+        x.data -= val_native
+    else:
+        x = ivy.Array(x_native - val_native)
+    return x
+
+
+def inplace_increment(
+    x: Union[ivy.Array, JaxArray], val: Union[ivy.Array, JaxArray]
+) -> ivy.Array:
+    (x_native, val_native), _ = ivy.args_to_native(x, val)
+    if ivy.is_ivy_array(x):
+        x.data += val_native
+    else:
+        x = ivy.Array(x_native + val_native)
+    return x
+
+
+def inplace_update(
+    x: Union[ivy.Array, JaxArray],
+    val: Union[ivy.Array, JaxArray],
+    ensure_in_backend: bool = False,
+) -> ivy.Array:
+    if ivy.is_array(x) and ivy.is_array(val):
+        if ensure_in_backend:
+            raise Exception("JAX does not natively support inplace updates")
+        (x_native, val_native), _ = ivy.args_to_native(x, val)
+        if ivy.is_ivy_array(x):
+            x.data = val_native
+        else:
+            raise Exception("JAX does not natively support inplace updates")
+        return x
+    else:
+        return val
+
+
+def inplace_variables_supported():
+    return False
+
+
+def is_native_array(x, exclusive=False):
     if exclusive:
-        x = jnp.swapaxes(x, axis, -1)
-        x = jnp.concatenate((jnp.ones_like(x[..., -1:]), x[..., :-1]), -1)
-        res = jnp.cumprod(x, -1)
-        return jnp.swapaxes(res, axis, -1)
-    return jnp.cumprod(x, axis)
+        return isinstance(
+            x,
+            (
+                jax.interpreters.xla._DeviceArray,
+                jaxlib.xla_extension.DeviceArray,
+                Buffer,
+            ),
+        )
+    return isinstance(
+        x,
+        (
+            jax.interpreters.xla._DeviceArray,
+            jaxlib.xla_extension.DeviceArray,
+            Buffer,
+            jax.interpreters.ad.JVPTracer,
+            jax.core.ShapedArray,
+            jax.interpreters.partial_eval.DynamicJaxprTracer,
+        ),
+    )
+
+
+def multiprocessing(context=None):
+    return (
+        _multiprocessing if context is None else _multiprocessing.get_context(context)
+    )
+
+
+def one_hot(
+    indices: JaxArray, depth: int, *, device, out: Optional[JaxArray] = None
+) -> JaxArray:
+    res = jnp.eye(depth, dtype=indices.dtype)[
+        jnp.array(indices, dtype="int64").reshape(-1)
+    ]
+    return _to_device(res.reshape(list(indices.shape) + [depth]), device)
 
 
 def scatter_flat(
@@ -226,7 +275,6 @@ def scatter_flat(
     return _to_device(target)
 
 
-# noinspection PyShadowingNames
 def scatter_nd(
     indices: JaxArray,
     updates: JaxArray,
@@ -235,7 +283,6 @@ def scatter_nd(
     *,
     out: Optional[JaxArray] = None,
 ) -> JaxArray:
-
     # parse numeric inputs
     if indices not in [Ellipsis, ()] and not (
         isinstance(indices, Iterable) and Ellipsis in indices
@@ -302,86 +349,40 @@ def scatter_nd(
 scatter_nd.support_native_out = True
 
 
-def gather(
-    params: JaxArray,
-    indices: JaxArray,
-    axis: int = -1,
-    *,
-    out: Optional[JaxArray] = None,
-) -> JaxArray:
-    return _to_device(jnp.take_along_axis(params, indices, axis))
-
-
-def gather_nd(
-    params: JaxArray, indices: JaxArray, *, out: Optional[JaxArray] = None
-) -> JaxArray:
-    indices_shape = indices.shape
-    params_shape = params.shape
-    num_index_dims = indices_shape[-1]
-    res_dim_sizes_list = [
-        reduce(mul, params_shape[i + 1 :], 1) for i in range(len(params_shape) - 1)
-    ] + [1]
-    result_dim_sizes = jnp.array(res_dim_sizes_list)
-    implicit_indices_factor = int(result_dim_sizes[num_index_dims - 1].item())
-    flat_params = jnp.reshape(params, (-1,))
-    new_shape = [1] * (len(indices_shape) - 1) + [num_index_dims]
-    indices_scales = jnp.reshape(result_dim_sizes[0:num_index_dims], new_shape)
-    indices_for_flat_tiled = jnp.tile(
-        jnp.reshape(jnp.sum(indices * indices_scales, -1, keepdims=True), (-1, 1)),
-        (1, implicit_indices_factor),
-    )
-    implicit_indices = jnp.tile(
-        jnp.expand_dims(jnp.arange(implicit_indices_factor), 0),
-        (indices_for_flat_tiled.shape[0], 1),
-    )
-    indices_for_flat = indices_for_flat_tiled + implicit_indices
-    flat_indices_for_flat = jnp.reshape(indices_for_flat, (-1,)).astype(jnp.int32)
-    flat_gather = jnp.take(flat_params, flat_indices_for_flat, 0)
-    new_shape = list(indices_shape[:-1]) + list(params_shape[num_index_dims:])
-    ret = jnp.reshape(flat_gather, new_shape)
-    return _to_device(ret)
-
-
-def multiprocessing(context=None):
-    return (
-        _multiprocessing if context is None else _multiprocessing.get_context(context)
-    )
-
-
-def one_hot(
-    indices: JaxArray, depth: int, *, device, out: Optional[JaxArray] = None
-) -> JaxArray:
-    res = jnp.eye(depth, dtype=indices.dtype)[
-        jnp.array(indices, dtype="int64").reshape(-1)
-    ]
-    return _to_device(res.reshape(list(indices.shape) + [depth]), device)
-
-
-def indices_where(x: JaxArray) -> JaxArray:
-    where_x = jnp.where(x)
-    return jnp.concatenate([jnp.expand_dims(item, -1) for item in where_x], -1)
-
-
-def inplace_decrement(
-    x: Union[ivy.Array, JaxArray], val: Union[ivy.Array, JaxArray]
-) -> ivy.Array:
-    (x_native, val_native), _ = ivy.args_to_native(x, val)
-    if ivy.is_ivy_array(x):
-        x.data -= val_native
+def shape(x: JaxArray, as_array: bool = False) -> Union[ivy.Shape, ivy.Array]:
+    if as_array:
+        return ivy.array(jnp.shape(x), dtype=ivy.default_int_dtype())
     else:
-        x = ivy.Array(x_native - val_native)
-    return x
+        return ivy.Shape(x.shape)
 
 
-def inplace_increment(
-    x: Union[ivy.Array, JaxArray], val: Union[ivy.Array, JaxArray]
-) -> ivy.Array:
-    (x_native, val_native), _ = ivy.args_to_native(x, val)
-    if ivy.is_ivy_array(x):
-        x.data += val_native
+def to_list(x: JaxArray) -> list:
+    return _to_array(x).tolist()
+
+
+def to_numpy(x: JaxArray, copy: bool = True) -> np.ndarray:
+    if copy:
+        return np.array(_to_array(x))
     else:
-        x = ivy.Array(x_native + val_native)
-    return x
+        return np.asarray(_to_array(x))
+
+
+def to_scalar(x: JaxArray) -> Number:
+    if isinstance(x, Number):
+        return x
+    else:
+        return _to_array(x).item()
+
+
+def unstack(x: JaxArray, axis: int, keepdims: bool = False) -> List[JaxArray]:
+    if x.shape == ():
+        return [x]
+    dim_size = x.shape[axis]
+    # ToDo: make this faster somehow, jnp.split is VERY slow for large dim_size
+    x_split = jnp.split(x, dim_size, axis)
+    if keepdims:
+        return x_split
+    return [jnp.squeeze(item, axis) for item in x_split]
 
 
 def vmap(
@@ -392,7 +393,3 @@ def vmap(
     return ivy.to_native_arrays_and_back(
         jax.vmap(func, in_axes=in_axes, out_axes=out_axes)
     )
-
-
-def current_backend_str():
-    return "jax"
