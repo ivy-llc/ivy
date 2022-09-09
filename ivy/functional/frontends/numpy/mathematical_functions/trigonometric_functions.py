cda7878559b8a0d24c59ba289bfa1a892862be89
Jonathan Omara <omarajonathan@gmail.com>
2022-09-09 10:08:44 +0300

    numpy frontend - `radians` (#4209)
    Co-authored-by: juliagsy <67888047+juliagsy@users.noreply.github.com>

--- ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
+++ ivy/functional/frontends/numpy/mathematical_functions/trigonometric_functions.py
@@ -211,3 +211,23 @@ def arctan2(
 
 
 arctan2.unsupported_dtypes = {"torch": ("float16",)}
+
+
+@from_zero_dim_arrays_to_float
+def radians(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting='same_kind',
+    order='K',
+    dtype=None,
+    subok=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.divide(ivy.multiply(x, ivy.pi, out=out), 180)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
