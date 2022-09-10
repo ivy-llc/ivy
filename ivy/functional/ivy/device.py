3ba6d175dbdf8ce2eab0775b1780c722426bef7e
William Oskar Hirst <bod_holthe@outlook.com>
2022-09-10 22:25:25 +0200

    Changed doc string in print_all_arrays_on_dev
    Arrays in example are now of equal shape.

--- ivy/functional/ivy/device.py
+++ ivy/functional/ivy/device.py
@@ -317,21 +317,21 @@ def print_all_ivy_arrays_on_dev(
 
     With attr_only = True
 
-    >>> x = ivy.array([1,0,2], [3,2])
+    >>> x = ivy.array([1,0,2], [3,2,1])
     >>> y = ivy.dev(x)
     >>> ivy.print_all_ivy_arrays_on_dev(y)
     ((3,), 'int32')
-    ((2,), 'int32')
+    ((3,), 'int32')
     
     
 
     With attr_only = False:
     
-    >>> x = ivy.array([1,0,2], [3,2])
+    >>> x = ivy.array([1,0,2], [3,2,1])
     >>> y = ivy.dev(x)
     >>> ivy.print_all_ivy_arrays_on_dev(y, attr_only = False)
     [1,0,2]
-    [3,2]
+    [3,2,1]
 
     """
     arrs = ivy.get_all_ivy_arrays_on_dev(device).values()
