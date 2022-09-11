fa000039bfd748db59ab5889c942c9916e30f914
Sayantan Sadhu <sayantan.sayanaldo@gmail.com>
2022-09-12 01:11:16 +0530

    Handled errors and warnings

--- ivy/functional/frontends/torch/reduction_ops.py
+++ ivy/functional/frontends/torch/reduction_ops.py
@@ -1,20 +1,20 @@
 import ivy
 
-def dist(input,other,p=2):
+
+def dist(input, other, p=2):
     try:
         abs_val = ivy.abs(input - other)
-    except:
-        return f'The size of tensor a ({input.size}) must match the size of tensor b ({other.size}) at non-singleton dimension'
-        
-    if p == 0:
-        # removing zeroes from the abs_val array as ivy.pow([0.0],0) if coming to be 1
-        # but should be zero for pnorm calculation.
-        abs_val_without_zero = abs_val[abs_val != 0]
-        if abs_val_without_zero.size == 0:
-            return abs_val
-        return ivy.sum(ivy.pow(abs_val_without_zero,p))
+        if p == 0:
+            # removing zeroes from the abs_val array as ivy.pow([0.0],0) if coming to be 1
+            # but should be zero for pnorm calculation.
+            abs_val_without_zero = abs_val[abs_val != 0]
+            if abs_val_without_zero.size == 0:
+                return abs_val
+            return ivy.sum(ivy.pow(abs_val_without_zero, p))
     
-    pnorm = ivy.pow(ivy.sum(ivy.pow(abs_val,p)),1/p)
-    return pnorm
+        pnorm = ivy.pow(ivy.sum(ivy.pow(abs_val, p)), 1/p)
+        return pnorm
+    except ValueError as err:
+        print(err.args)
 
 dist.unsupported_dtypes = ("float16",)
--- ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
@@ -1,11 +1,20 @@
 import numpy as np
 from hypothesis import given, strategies as st
-
+import pytest
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
+def is_broadcastable(shp1, shp2):
+    for a, b in zip(shp1[::-1], shp2[::-1]):
+        if a == 1 or b == 1 or a == b:
+            pass
+        else:
+            return False
+    return True
+
+@pytest.mark.filterwarnings("ignore: dist")
 @handle_cmd_line_args
 @given(
     dtype_and_x=helpers.statistical_dtype_values(function="dist"),
@@ -28,16 +37,17 @@ def test_torch_dist(
 ):
     input_x_dtype, x, dim_x = dtype_and_x
     input_y_dtype, y, dim_y = dtype_and_y
-    helpers.test_frontend_function(
-        input_dtypes=[input_x_dtype],
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="torch",
-        fn_tree="dist",
-        input=np.asarray(x, dtype=input_x_dtype),
-        other=np.asarray(y, dtype=input_y_dtype),
-        p=p,
-    )
+    if is_broadcastable(np.array(x).shape,np.array(y).shape):
+        helpers.test_frontend_function(
+            input_dtypes=[input_x_dtype],
+            as_variable_flags=as_variable,
+            with_out=False,
+            num_positional_args=num_positional_args,
+            native_array_flags=native_array,
+            fw=fw,
+            frontend="torch",
+            fn_tree="dist",
+            input=np.asarray(x, dtype=input_x_dtype),
+            other=np.asarray(y, dtype=input_y_dtype),
+            p=p,
+        )
