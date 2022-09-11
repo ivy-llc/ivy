b3c7e25f01e4f3c7032bcde6c7bd038939fa8c4d
mattbarrett98 <matthew-barrett@hotmail.co.uk>
2022-09-11 15:24:44 +0100

    remove incorrect uses of `Optional`

--- ivy/functional/backends/numpy/activations.py
+++ ivy/functional/backends/numpy/activations.py
@@ -22,13 +22,13 @@ relu.support_native_out = True
 
 
 def leaky_relu(
-    x: np.ndarray, /, *, alpha: Optional[float] = 0.2, out: Optional[np.ndarray] = None
+    x: np.ndarray, /, *, alpha: float = 0.2, out: Optional[np.ndarray] = None
 ) -> np.ndarray:
     return np.asarray(np.where(x > 0, x, x * alpha), x.dtype)
 
 
 def gelu(
-    x, /, *, approximate: Optional[bool] = True, out: Optional[np.ndarray] = None
+    x, /, *, approximate: bool = True, out: Optional[np.ndarray] = None
 ) -> np.ndarray:
     if erf is None:
         raise Exception(
