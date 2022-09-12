001380fbc485bc6eb64c6c9a7b85d3cef8fe2b72
Rahul Sankar <rahulsnkr@gmail.com>
2022-09-11 20:25:29 -0400

    add mean_squared_logarithmic_error (#3816)
    * add mean_squared_logarithmic_error
    
    * fix lint issues
    
    * fix lint issues

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_metrics.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_metrics.py
@@ -501,3 +501,43 @@ def test_squared_hinge(
         y_true=y_true,
         y_pred=y_pred,
     )
+
+
+# mean_squared_logarithmic_error
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(set(ivy_tf.valid_float_dtypes))
+        ),
+        num_arrays=2,
+        min_num_dims=1,
+        shared_dtype=True,
+    ),
+    as_variable=helpers.list_of_length(x=st.booleans(), length=2),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.mean_squared_logarithmic_error"  # noqa: E501
+    ),
+    native_array=helpers.list_of_length(x=st.booleans(), length=2),
+)
+def test_tensorflow_metrics_mean_squared_logarithmic_error(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="keras.metrics.mean_squared_logarithmic_error",
+        y_true=np.asarray(x[0], dtype=input_dtype[0]),
+        y_pred=np.asarray(x[1], dtype=input_dtype[1]),
+    )
