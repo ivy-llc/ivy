bf191b150a4250823c8b4212808f385eaf4f3197
ShiftLove <100001509+ShiftLove@users.noreply.github.com>
2022-09-12 04:38:38 -0700

    Update matrix_and_vector_products.py

--- ivy/functional/frontends/numpy/linear_algebra/matrix_and_vector_products.py
+++ ivy/functional/frontends/numpy/linear_algebra/matrix_and_vector_products.py
@@ -4,3 +4,13 @@ import ivy
 
 def outer(a, b, out=None):
     return ivy.outer(a, b, out=out)
+
+
+#inner
+
+# global
+import ivy
+
+
+def inner(a, b, out=None):
+    return ivy.inner(a, b, out=out)
