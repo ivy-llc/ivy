ea37fa0404e6d8b19f0ad96bb76d6da43bb3591e
karalleyna <36455180+karalleyna@users.noreply.github.com>
2022-09-12 20:40:05 +0300

    Add the eig function for the PyTorch frontend

--- ivy/functional/frontends/torch/__init__.py
+++ ivy/functional/frontends/torch/__init__.py
@@ -43,3 +43,5 @@ from . import utilities
 from .utilities import *
 from . import vision_functions
 from .vision_functions import *
+from . import linalg
+from .linalg import *
---
+++ ivy/functional/frontends/torch/linalg.py
@@ -0,0 +1,5 @@
+import ivy
+
+
+def eig(input, *, out=None):
+    return ivy.eig(input, out=out)
