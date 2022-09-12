469df73aaad001960adccb6d2a73264cadc8eae4
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-09-12 14:10:20 +0100

    Update test_correlating.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
@@ -5,7 +5,7 @@ from hypothesis import given, strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy_tests.test_ivy.test_frontends.\
-        test_numpy.helpers as np_frontend_helpers
+       test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
