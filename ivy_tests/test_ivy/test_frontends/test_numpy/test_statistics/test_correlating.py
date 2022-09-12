972ab66f71426a707e7e6c67be08d5db7c061337
Alex Weiss <alex.m.weiss2@gmail.com>
2022-09-12 15:39:33 -0400

    fixed flake8 error

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
@@ -4,8 +4,7 @@ from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-import ivy_tests.test_ivy.test_frontends.\
-       test_numpy.helpers as np_frontend_helpers
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
