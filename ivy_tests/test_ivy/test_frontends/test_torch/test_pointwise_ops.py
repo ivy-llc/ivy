0365e8f0f7dc04e2026f7c43e5a699926fb3a99c
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-12 04:55:04 +0000

    Test fix for reshape and sqrt

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -934,9 +934,7 @@ def test_torch_trunc(
 # sqrt
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
-    ),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.sqrt"
     ),
