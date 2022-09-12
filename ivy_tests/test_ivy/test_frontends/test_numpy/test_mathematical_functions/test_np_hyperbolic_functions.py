649047262899f201aca8481adb74b953cac6e509
Poojan Vachharajani <pjmathematician@gmail.com>
2022-09-13 00:15:37 +0530

    Added arccosh

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_hyperbolic_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_np_hyperbolic_functions.py
@@ -162,3 +162,43 @@ def test_numpy_arcsinh(
         subok=True,
         test_values=False,
     )
+
+
+# arccosh
+@handle_cmd_line_args
+@given(
+    dtype_and_x=_dtype_x(available_dtypes=helpers.get_dtypes("float")),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.arccosh"
+    ),
+)
+def test_numpy_arccosh(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    (input_dtype, x), where = dtype_and_x
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="arccosh",
+        x=np.asarray(x, dtype=input_dtype[0]),
+        out=None,
+        where=where,
+        casting="same_kind",
+        order="k",
+        dtype=dtype,
+        subok=True,
+        test_values=False,
+    )
