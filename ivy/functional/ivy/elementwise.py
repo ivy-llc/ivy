8114df1be0079f3f97b046bdac52091e6f9bbd00
Matt <matthew-barrett@hotmail.co.uk>
2022-09-13 13:56:47 +0100

    move reciprocal from array standard to extra

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -4455,34 +4455,6 @@ def pow(
 @to_native_arrays_and_back
 @handle_out_argument
 @handle_nestable
-def reciprocal(
-    x: Union[float, ivy.Array, ivy.NativeArray],
-    /,
-    *,
-    out: Optional[ivy.Array] = None,
-) -> ivy.Array:
-    """Returns a new array with the reciprocal of each element in ``x``.
-
-    Parameters
-    ----------
-    x
-        Input array.
-    out
-        optional output array, for writing the result to. It must have a shape that the
-        inputs broadcast to.
-
-    Returns
-    -------
-    ret
-        A new array with the positive value of each element in ``x``.
-
-    """
-    return ivy.current_backend(x).reciprocal(x, out=out)
-
-
-@to_native_arrays_and_back
-@handle_out_argument
-@handle_nestable
 def remainder(
     x1: Union[float, ivy.Array, ivy.NativeArray],
     x2: Union[float, ivy.Array, ivy.NativeArray],
@@ -5652,3 +5624,31 @@ def minimum(
     }
     """
     return ivy.current_backend(x1).minimum(x1, x2, out=out)
+
+
+@to_native_arrays_and_back
+@handle_out_argument
+@handle_nestable
+def reciprocal(
+    x: Union[float, ivy.Array, ivy.NativeArray],
+    /,
+    *,
+    out: Optional[ivy.Array] = None,
+) -> ivy.Array:
+    """Returns a new array with the reciprocal of each element in ``x``.
+
+    Parameters
+    ----------
+    x
+        Input array.
+    out
+        optional output array, for writing the result to. It must have a shape that the
+        inputs broadcast to.
+
+    Returns
+    -------
+    ret
+        A new array with the positive value of each element in ``x``.
+
+    """
+    return ivy.current_backend(x).reciprocal(x, out=out)
