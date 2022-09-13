80c7c863bb3e5b20b46d0b2bd2f8a3ef167c7e2d
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-13 16:51:41 +0530

    Fixed failing test for linalg tensordot (#4407)

--- ivy_tests/test_ivy/helpers.py
+++ ivy_tests/test_ivy/helpers.py
@@ -2898,6 +2898,13 @@ def array_values(
                 ),
             )
         values = draw(list_of_length(x=st.integers(min_value, max_value), length=size))
+        for i, v in enumerate(values):
+            if max_op == "sqrt" and v != 0:
+                v = v / abs(v) * math.sqrt(abs(v))
+            elif max_op == "log" and v != 0:
+                v = (v / abs(v)) * (math.log(abs(v)) / math.log(2))
+            values[i] = int(v / large_value_safety_factor)
+
     elif "int" in dtype:
 
         if min_value is not None and max_value is not None:
@@ -2966,6 +2973,12 @@ def array_values(
                     length=size,
                 )
             )
+            for i, v in enumerate(values):
+                if max_op == "sqrt" and v != 0:
+                    v = v / abs(v) * math.sqrt(abs(v))
+                elif max_op == "log" and v != 0:
+                    v = (v / abs(v)) * (math.log(abs(v)) / math.log(2))
+                values[i] = int(v / large_value_safety_factor)
     elif "float" in dtype:
         dtype_info = {
             "float16": {"cast_type": "float16", "round_places": 3, "width": 16},
@@ -3029,9 +3042,9 @@ def array_values(
             )
         )
         for i, v in enumerate(values):
-            if max_op == "sqrt":
+            if max_op == "sqrt" and v != 0:
                 v = v / abs(v) * math.sqrt(abs(v))
-            elif max_op == "log":
+            elif max_op == "log" and v != 0:
                 v = (v / abs(v)) * (math.log(abs(v)) / math.log(2))
             values[i] = v / large_value_safety_factor
     elif dtype == "bool":
