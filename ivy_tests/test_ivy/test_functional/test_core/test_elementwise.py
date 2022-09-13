781ece7387932e00f9b35394fcb253f50f71046b
Matt <matthew-barrett@hotmail.co.uk>
2022-09-13 13:56:47 +0100

    move reciprocal from array standard to extra

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1727,42 +1727,6 @@ def test_pow(
     )
 
 
-# reciprocal
-@handle_cmd_line_args
-@given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=1,
-        allow_inf=True,
-    ),
-    num_positional_args=helpers.num_positional_args(fn_name="reciprocal"),
-)
-def test_reciprocal(
-    *,
-    dtype_and_x,
-    as_variable,
-    with_out,
-    num_positional_args,
-    native_array,
-    container,
-    instance_method,
-    fw,
-):
-    input_dtype, x = dtype_and_x
-    helpers.test_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        container_flags=container,
-        instance_method=instance_method,
-        fw=fw,
-        fn_name="reciprocal",
-        x=np.asarray(x, dtype=input_dtype),
-    )
-
-
 # remainder
 @handle_cmd_line_args
 @given(
@@ -2254,4 +2218,40 @@ def test_maximum(
         fn_name="maximum",
         x1=np.asarray(x[0], dtype=input_dtype[0]),
         x2=np.asarray(x[1], dtype=input_dtype[1]),
+    )
+
+
+# reciprocal
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=1,
+        allow_inf=True,
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="reciprocal"),
+)
+def test_reciprocal(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="reciprocal",
+        x=np.asarray(x, dtype=input_dtype),
     )
