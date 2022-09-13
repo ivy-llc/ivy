c44299c95fed63d9f4060b7644a1c0c38c97225d
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-13 10:08:10 +0800

    refactor helpers to assertions

--- ivy/assertions.py
+++ ivy/assertions.py
@@ -1,6 +1,30 @@
 import ivy
 
 
+# General #
+# ------- #
+
+
 def check_elem_in_list(elem, list):
     if elem not in list:
         raise ivy.exceptions.IvyException("{} is not one of {}".format(elem, list))
+
+
+# Creation #
+# -------- #
+
+
+def check_fill_value_and_dtype_are_compatible(fill_value, dtype):
+    if not (
+        (ivy.is_int_dtype(dtype) or ivy.is_uint_dtype(dtype))
+        and isinstance(fill_value, int)
+    ) and not (
+        ivy.is_float_dtype(dtype)
+        and isinstance(fill_value, float)
+        or isinstance(fill_value, bool)
+    ):
+        raise ivy.exceptions.IvyException(
+            "the fill_value: {} and data type: {} are not compatible".format(
+                fill_value, dtype
+            )
+        )
