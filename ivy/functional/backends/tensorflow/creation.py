1823bf914bb59df16f45b5bd4ca12cd484fae784
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-13 10:07:45 +0800

    tf

--- ivy/functional/backends/tensorflow/creation.py
+++ ivy/functional/backends/tensorflow/creation.py
@@ -10,9 +10,6 @@ from ivy import (
     as_ivy_dtype,
 )
 
-# noinspection PyProtectedMember
-from ivy.functional.ivy.creation import _assert_fill_value_and_dtype_are_compatible
-
 
 # Array API Standard #
 # -------------------#
@@ -216,7 +213,7 @@ def full(
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
     dtype = ivy.default_dtype(dtype=dtype, item=fill_value, as_native=True)
-    _assert_fill_value_and_dtype_are_compatible(dtype, fill_value)
+    ivy.assertions.check_fill_value_and_dtype_are_compatible(fill_value, dtype)
     with tf.device(device):
         return tf.fill(
             shape,
@@ -233,7 +230,7 @@ def full_like(
     device: str,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
-    _assert_fill_value_and_dtype_are_compatible(dtype, fill_value)
+    ivy.assertions.check_fill_value_and_dtype_are_compatible(fill_value, dtype)
     with tf.device(device):
         return tf.experimental.numpy.full_like(x, fill_value, dtype=dtype)
 
