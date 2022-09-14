0882f40cac472019db591abc4d1a5b6bff429daa
Ahmed Omar <40790298+ahmedo42@users.noreply.github.com>
2022-09-14 10:34:05 +0200

    deg2rad,rad2deg extensions  (#4239)

--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -682,3 +682,17 @@ def reciprocal(
 
 reciprocal.support_native_out = True
 reciprocal.unsupported_dtypes = ("float16",)
+
+
+def deg2rad(x: torch.Tensor, /, *, out: Optional[torch.Tensor] = None) -> torch.Tensor:
+    return torch.deg2rad(x, out=out)
+
+
+deg2rad.support_native_out = True
+
+
+def rad2deg(x: torch.Tensor, /, *, out: Optional[torch.Tensor] = None) -> torch.Tensor:
+    return torch.rad2deg(x, out=out)
+
+
+rad2deg.support_native_out = True
