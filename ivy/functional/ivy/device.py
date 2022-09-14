cb8dc0775f47f011c77f75f01349a485e4331c9f
JG <67888047+juliagsy@users.noreply.github.com>
2022-09-14 10:24:04 +0800

    refactor `raise Exception...` to IvyException or assertion helper function (#4384)

--- ivy/functional/ivy/device.py
+++ ivy/functional/ivy/device.py
@@ -485,7 +485,7 @@ def total_mem_on_dev(device: Union[ivy.Device, ivy.NativeDevice], /) -> float:
     elif device == "cpu":
         return psutil.virtual_memory().total / 1e9
     else:
-        raise Exception(
+        raise ivy.exceptions.IvyException(
             'Invalid device string input, must be on the form "gpu:idx" or "cpu", '
             "but found {}".format(device)
         )
@@ -530,8 +530,10 @@ def used_mem_on_dev(
     """
     ivy.clear_mem_on_dev(device)
     if "gpu" in device:
-        if process_specific:
-            raise Exception("process-specific GPU queries are currently not supported")
+        ivy.assertions.check_false(
+            process_specific,
+            "process-specific GPU queries are currently not supported",
+        )
         handle = _get_nvml_gpu_handle(device)
         info = pynvml.nvmlDeviceGetMemoryInfo(handle)
         return info.used / 1e9
@@ -541,7 +543,7 @@ def used_mem_on_dev(
         vm = psutil.virtual_memory()
         return (vm.total - vm.available) / 1e9
     else:
-        raise Exception(
+        raise ivy.exceptions.IvyException(
             'Invalid device string input, must be on the form "gpu:idx" or "cpu", '
             "but found {}".format(device)
         )
@@ -587,8 +589,10 @@ def percent_used_mem_on_dev(
     """
     ivy.clear_mem_on_dev(device)
     if "gpu" in device:
-        if process_specific:
-            raise Exception("process-specific GPU queries are currently not supported")
+        ivy.assertions.check_false(
+            process_specific,
+            "process-specific GPU queries are currently not supported",
+        )
         handle = _get_nvml_gpu_handle(device)
         info = pynvml.nvmlDeviceGetMemoryInfo(handle)
         return (info.used / info.total) * 100
@@ -598,7 +602,7 @@ def percent_used_mem_on_dev(
             return (psutil.Process(os.getpid()).memory_info().rss / vm.total) * 100
         return (1 - (vm.available / vm.total)) * 100
     else:
-        raise Exception(
+        raise ivy.exceptions.IvyException(
             'Invalid device string input, must be on the form "gpu:idx" or "cpu", '
             "but found {}".format(device)
         )
@@ -641,7 +645,7 @@ def dev_util(device: Union[ivy.Device, ivy.NativeDevice], /) -> float:
         handle = _get_nvml_gpu_handle(device)
         return pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
     else:
-        raise Exception(
+        raise ivy.exceptions.IvyException(
             'Invalid device string input, must be on the form "gpu:idx" or "cpu", '
             "but found {}".format(device)
         )
@@ -1172,12 +1176,11 @@ def function_supported_devices(fn: Callable, recurse=True) -> Tuple:
     ret
         The supported devices of the function
     """
-    if not _is_valid_devices_attributes(fn):
-        raise Exception(
-            "supported_devices and unsupported_devices attributes cannot both \
-             exist in a particular backend"
-        )
-
+    ivy.assertions.check_true(
+        _is_valid_devices_attributes(fn),
+        "supported_devices and unsupported_devices attributes cannot both \
+        exist in a particular backend",
+    )
     supported_devices = set(_get_devices(fn, complement=False))
 
     if recurse:
@@ -1204,12 +1207,11 @@ def function_unsupported_devices(fn: Callable, recurse=True) -> Tuple:
     ret
         The unsupported devices of the function
     """
-    if not _is_valid_devices_attributes(fn):
-        raise Exception(
-            "supported_devices and unsupported_devices attributes cannot both \
-             exist in a particular backend"
-        )
-
+    ivy.assertions.check_true(
+        _is_valid_devices_attributes(fn),
+        "supported_devices and unsupported_devices attributes cannot both \
+        exist in a particular backend",
+    )
     unsupported_devices = set(_get_devices(fn, complement=True))
 
     if recurse:
