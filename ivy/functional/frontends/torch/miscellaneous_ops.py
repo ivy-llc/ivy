317f75ddb09872529a597bd1346c7c6a359349da
Hellenn07 <59020915+Hellenn07@users.noreply.github.com>
2022-09-14 10:32:51 +0300

    Update miscellaneous_ops.py

---
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -0,0 +1,9 @@
+# global
+import ivy
+
+
+def searchsorted(sorted_sequence, values, *, out_int32=False, right=False, side='left', out=None, sorter=None):
+    return ivy.searchsorted(input, other * alpha, out=out)
+
+
+add.unsupported_dtypes = ("float16",)
