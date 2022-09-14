b84867de5cada442acadf1affc18c3d7be3b2214
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-14 18:09:24 +0530

    Fixed the matrix_rank function for numpy, jax and tensorflow (#4452)

--- ivy/functional/backends/numpy/linear_algebra.py
+++ ivy/functional/backends/numpy/linear_algebra.py
@@ -137,11 +137,19 @@ def matrix_rank(
     rtol: Optional[Union[float, Tuple[float]]] = None,
     out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
-    if rtol is None:
-        ret = np.linalg.matrix_rank(x)
+    singular_values = np.linalg.svd(x, compute_uv=False)
+    max_value = np.max(singular_values)
+    if rtol:
+        num = np.sum(singular_values > max_value * rtol)
     else:
-        ret = np.linalg.matrix_rank(x, rtol)
-    return np.asarray(ret, dtype=ivy.default_int_dtype(as_native=True))
+        num = singular_values.size
+    return np.asarray(num, dtype=ivy.default_int_dtype(as_native=True))
+
+
+matrix_rank.unsupported_dtypes = (
+    "float16",
+    "bfloat16",
+)
 
 
 def matrix_transpose(x: np.ndarray, *, out: Optional[np.ndarray] = None) -> np.ndarray:
