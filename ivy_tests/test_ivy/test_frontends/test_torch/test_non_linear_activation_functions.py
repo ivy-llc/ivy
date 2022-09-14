802afed551b06e5fe7e7ca384910bed457efb288
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-09-14 14:58:01 +0000

    add with_inplace flag for torch.nn.functional.threshold_

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -336,6 +336,7 @@ def test_torch_threshold_(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
+        with_inplace=True,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
