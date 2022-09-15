f01745fc3e03b46adcac63da8946b426caadf8eb
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-15 08:06:11 +0000

    lint fix

--- ivy/functional/backends/torch/general.py
+++ ivy/functional/backends/torch/general.py
@@ -46,6 +46,7 @@ def array_equal(x0: torch.Tensor, x1: torch.Tensor, /) -> bool:
     x1 = x1.type(dtype=dtype)
     return torch.equal(x0, x1)
 
+
 array_equal.unsupported_dtypes = ("bfloat16",)
 
 
@@ -340,12 +341,12 @@ def scatter_nd(
         initial_val = torch.tensor(0).type(dtype)
     elif reduction == "min":
         if dtype.is_floating_point:
-            initial_val = min(torch.finfo(dtype).max, 1e12) 
+            initial_val = min(torch.finfo(dtype).max, 1e12)
         else:
             initial_val = min(torch.iinfo(dtype).max, 1e12)
     elif reduction == "max":
         if dtype.is_floating_point:
-            initial_val = max(torch.finfo(dtype).min, 1e-12) 
+            initial_val = max(torch.finfo(dtype).min, 1e-12)
         else:
             initial_val = max(torch.iinfo(dtype).min, 1e-12)
     else:
