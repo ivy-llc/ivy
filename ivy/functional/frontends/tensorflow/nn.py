aea40d097be24242fd2aea4a79b2ea1158f3e547
Aditya Komaravolu <64011471+Aditya-Komaravolu@users.noreply.github.com>
2022-09-15 12:03:58 +0530

    lint change to max_pool1d func

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -123,5 +123,13 @@ def batch_normalization(x, mean, variance, offset, scale, variance_epsilon, name
     return x * ivy.astype(inv, x.dtype, copy=False) + ivy.astype(
         offset - mean * inv if offset is not None else -mean * inv, x.dtype
     )
-def max_pool1d( input, ksize, strides, padding, data_format='NWC', name=None ):
-    return ivy.max_pool1d(input, ksize= ksize,strides, padding, data_format=data_format)
+
+
+def max_pool1d( input, ksize, strides, padding, data_format='NWC', name=None):
+    return ivy.max_pool1d(
+        input, 
+        ksize= ksize,
+        strides, 
+        padding, 
+        data_format=data_format
+    )
