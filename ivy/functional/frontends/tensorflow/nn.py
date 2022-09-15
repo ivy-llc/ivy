a619c14ff4957bcf553853da379c1cf7e712c5fc
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-09-15 11:38:51 +0530

    Add ctc_unique_labels for tensorflow frontend

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -123,3 +123,7 @@ def batch_normalization(x, mean, variance, offset, scale, variance_epsilon, name
     return x * ivy.astype(inv, x.dtype, copy=False) + ivy.astype(
         offset - mean * inv if offset is not None else -mean * inv, x.dtype
     )
+
+
+def ctc_unique_labels(labels, name=None):
+    return ivy.ctc_unique_labels(labels)
