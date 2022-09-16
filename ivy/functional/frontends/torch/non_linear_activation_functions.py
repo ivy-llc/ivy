8283dc3438790b8b8ab5dcbf898ef61727d81e08
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-16 16:50:41 +0530

    Added log_softmax to torch frontend (#4525)

--- ivy/functional/frontends/torch/non_linear_activation_functions.py
+++ ivy/functional/frontends/torch/non_linear_activation_functions.py
@@ -185,3 +185,13 @@ def silu(input, inplace=False):
 def glu(input, dim=-1):
     a, b = ivy.split(input, num_or_size_splits=2, axis=dim)
     return ivy.multiply(a, ivy.sigmoid(b))
+
+
+# ToDo Implement log_softmax in ivy functional API
+# for it to be faster than ivy.log(ivy.softmax) and more mathematical stable
+def log_softmax(input, dim=None, dtype=None):
+    if dtype:
+        input = ivy.astype(ivy.array(input), ivy.as_ivy_dtype(dtype))
+    if dim is None:
+        dim = -1
+    return ivy.log(ivy.softmax(input, axis=dim))
