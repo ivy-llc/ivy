b4f4afcfe3c326f1dd19d0aa8e8931d06c24e882
duspic <72471213+duspic@users.noreply.github.com>
2022-09-16 10:26:33 +0200

    Update test_functions.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
@@ -1,39 +1,33 @@
 # global
-import numpy as np
 from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args, given
 
 
 #random
-#  size=helpers.get_shape(allow_none=True),
 @handle_cmd_line_args
 @given(
-    input_dtypes=helpers.get_dtypes("integer"),
-    as_variable=st.booleans(),
+    input_dtypes=helpers.get_dtypes('integer', full=False),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.absolute"
-    ),
-    native_array=st.booleans(),
-    with_out=st.booleans(),
+        fn_name="ivy.functional.frontends.numpy.random"
+        ),
+    size=helpers.get_shape(allow_none=True),
 )
-
 def test_numpy_random(
     input_dtypes,
-    as_variable,
     num_positional_args,
-    native_array,
-    with_out,
+    size,
+    fw,
 ):
     helpers.test_frontend_function(
         input_dtypes=input_dtypes,
-        as_variable_flags=as_variable,
-        with_out=with_out,
+        as_variable_flags=False,
+        with_out=False,
         num_positional_args=num_positional_args,
-        native_array_flags=native_array,
+        native_array_flags=False,
+        test_values=False,
         fw=fw,
         frontend="numpy",
         fn_tree="random.random",
