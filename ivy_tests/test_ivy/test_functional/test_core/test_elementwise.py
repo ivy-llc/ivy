eaa431c9f737f9ffaf1b53b7d67e97ebf828e29c
CatB1t <skytedits@gmail.com>
2022-09-16 17:25:46 +0200

    small fix for `acos` test.
    Former-commit-id: c54dc5f227d469e8f49fa983ebbbb7741ad36ff7

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -90,8 +90,9 @@ def test_acosh(
 @given(
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
-        large_value_safety_factor=20,
-        small_value_safety_factor=2.5,
+        large_abs_safety_factor=8,
+        small_abs_safety_factor=4,
+        safety_factor_scale="log",
     ),
     num_positional_args=helpers.num_positional_args(fn_name="acos"),
 )
