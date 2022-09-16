766184c7992acd66c114aacd868c578623ccf6e3
Emanuel <107799963+14Emanuel@users.noreply.github.com>
2022-09-16 23:24:15 +0300

    implementing frontend test for Cholesky function

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -394,3 +394,27 @@ def test_tensorflow_eye(
         batch_shape=batch_shape,
         dtype=dtype,
     )
+
+    
+@handle_cmd_line_args
+@given(
+    dtype_and_input=_get_dtype_and_matrix(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.cholesky"
+    ),
+)
+def test_tensorflow_cholesky(
+    dtype_and_input, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="linalg.cholesky",
+        input=np.asarray(x, dtype=input_dtype),
+    )
