7b64ed3f90b9780f3f8388e589f481aaac18d10e
karalleyna <36455180+karalleyna@users.noreply.github.com>
2022-09-17 18:02:33 +0300

    Add test for eigh

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
@@ -50,3 +50,48 @@ def test_jax_lax_cholesky(
         x=x,
         symmetrize_input=symmetrize_input,
     )
+
+
+# eigh
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        max_value=10,
+        shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: np.linalg.cond(x[1]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1])) != 0
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.lax.linalg.eigh"
+    ),
+    lower=st.booleans(),
+)
+def test_jax_lax_eigh(
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
+    fw,
+    lower,
+):
+    dtype, x = dtype_and_x
+    x = np.array(x, dtype=dtype)
+    # make symmetric positive-definite beforehand
+    x = np.matmul(x.T, x) + np.identity(x.shape[0]) * 1e-3
+    helpers.test_frontend_function(
+        input_dtypes=[dtype],
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.linalg.eigh",
+        rtol=1e-02,
+        x=x,
+        lower=lower,
+        symmetrize_input=False,
+    )
