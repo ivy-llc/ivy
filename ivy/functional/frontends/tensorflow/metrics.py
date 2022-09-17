f3d73b12df92cc67b0fe57528064421bcb84a710
Aditya Komaravolu <64011471+Aditya-Komaravolu@users.noreply.github.com>
2022-09-17 12:55:45 +0530

    fixed linting in metrics.py

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -206,12 +206,13 @@ def cosine_similarity(y_true, y_pred):
     y_pred = ivy.array(y_pred)
     y_true = ivy.array(y_true)
      
-    if len(y_pred.shape)==len(y_pred.shape) and len(y_true.shape)==2:
-        numerator = ivy.sum(y_true * y_pred, axis= 1)
-        denominator= (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
+    if len(y_pred.shape) == len(y_pred.shape) and len(y_true.shape) == 2:
+        numerator = ivy.sum(y_true * y_pred, axis=1)
+        denominator = (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
         cosine = numerator / denominator
 
     else:
         cosine = (y_true @ y_pred) / (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
     
     return cosine
+    
