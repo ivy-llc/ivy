e02628523147e78cec01b790f96554c53dbb8ada
Daryna Dzhulai <daryna.dzhulay@gmail.com>
2022-09-18 10:10:49 +0200

    use matrix_transpose

--- ivy/functional/frontends/numpy/manipulation_routines/transpose_like_operations.py
+++ ivy/functional/frontends/numpy/manipulation_routines/transpose_like_operations.py
@@ -2,13 +2,18 @@ import ivy
 
 
 def transpose(array, /, *, axes=None):
+    if array.ndim <= 2:
+        return ivy.matrix_transpose(array)
     origin_axes = list(range(len(array.shape)))
     if axes is None:
         axes = origin_axes[::-1]
     try:
         assert len(axes) > 1
-    except ValueError:
-        raise
+    except AssertionError:
+        raise ValueError("`axes` should have the same size the input array.ndim")
+    # use matrix_transpose for inner-most transposal
+    if (origin_axes[:-2] == axes[:-2]) & (origin_axes[-1] == axes[-2]):
+        return ivy.matrix_transpose(array)
     mapping = dict(zip(origin_axes, [axes.index(ax) for ax in origin_axes]))
     ia, ai = dict(zip(origin_axes, origin_axes)), dict(zip(origin_axes, origin_axes))
 
