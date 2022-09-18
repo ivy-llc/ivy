a4e5d3d116fab06c9a0a0a89102be7e7a3060a73
Prabhu-V <108232437+Prabhu-V@users.noreply.github.com>
2022-09-18 15:03:39 -0400

    Added unshuffle function and tests

--- ivy/functional/frontends/torch/vision_functions.py
+++ ivy/functional/frontends/torch/vision_functions.py
@@ -37,3 +37,41 @@ def pixel_shuffle(input, upscale_factor):
     return ivy.reshape(
         ivy.permute_dims(input_reshaped, (0, 1, 4, 2, 5, 3)), (b, oc, oh, ow)
     )
+
+
+def pixel_unshuffle(input, downscale_factor):
+
+    input_shape = ivy.shape(input)
+
+    assert (
+        ivy.get_num_dims(input) == 4
+    ), "pixel_shuffle expects 4D input, but got input with sizes " + str(input_shape)
+
+    b = input_shape[0]
+    c = input_shape[1]
+    h = input_shape[2]
+    w = input_shape[3]
+    downscale_factor_squared = downscale_factor * downscale_factor
+    assert ((h % downscale_factor == 0) & (w % downscale_factor == 0)), (
+        "pixel_unshuffle expects input height and width to be divisible by "
+        + "downscale_factor, but got input with sizes "
+        + str(input_shape)
+        + ", downscale_factor="
+        + str(downscale_factor)
+        + ", and either self.size(2)="
+        + str(h)
+        + " or self.size(3)="
+        + str(w)
+        + " is not divisible by "
+        + str(downscale_factor)
+    )
+    oc = c * downscale_factor_squared
+    oh = int(h / downscale_factor)
+    ow = int(w / downscale_factor)
+
+    input_reshaped = ivy.reshape(input,
+                                 (b, c, oh, downscale_factor, ow, downscale_factor))
+    return ivy.reshape(
+        ivy.permute_dims(input_reshaped, (0, 1, 3, 5, 2, 4)), (b, oc, oh, ow)
+    )
+    
