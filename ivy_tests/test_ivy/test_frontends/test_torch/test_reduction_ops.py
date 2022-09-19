0bcaf402e121f4c2edc811f4fad9b514ce4d01b5
Deeptendu <deeptendu.santra@protonmail.com>
2022-09-19 04:13:20 -0400

    Nans are problematic

--- ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
@@ -286,10 +286,10 @@ def test_torch_any(
     dtype_input_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("numeric"),
         force_int_axis=True,
-        allow_nan=True,
-        min_num_dims=1,
-        min_axis=-1,
-        max_axis=0,
+        valid_axis=True,
+        # allow_nan=True,
+        large_abs_safety_factor=2,
+        safety_factor_scale="log",
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.nansum"
