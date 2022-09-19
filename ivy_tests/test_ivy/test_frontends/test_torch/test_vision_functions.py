8a0d9cb229ad10d56acec05cca744438b854e2ff
Prabhu-V <108232437+Prabhu-V@users.noreply.github.com>
2022-09-19 16:06:34 -0400

    Changed assert to ivy.assertions.check_equal

--- ivy_tests/test_ivy/test_frontends/test_torch/test_vision_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_vision_functions.py
@@ -65,6 +65,7 @@ def test_torch_pixel_unshuffle(
     dtype_and_x,
     factor,
     as_variable,
+    with_out,
     num_positional_args,
     native_array,
     fw,
@@ -75,7 +76,7 @@ def test_torch_pixel_unshuffle(
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=False,
+        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
