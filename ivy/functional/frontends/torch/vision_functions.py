1844f2ccdd7030497a6317f5dae3e85672164c64
Prabhu-V <108232437+Prabhu-V@users.noreply.github.com>
2022-09-19 16:06:34 -0400

    Changed assert to ivy.assertions.check_equal

--- ivy/functional/frontends/torch/vision_functions.py
+++ ivy/functional/frontends/torch/vision_functions.py
@@ -43,27 +43,32 @@ def pixel_unshuffle(input, downscale_factor):
 
     input_shape = ivy.shape(input)
 
-    assert (
-        ivy.get_num_dims(input) == 4
-    ), "pixel_shuffle expects 4D input, but got input with sizes " + str(input_shape)
+    ivy.assertions.check_equal(
+        ivy.get_num_dims(input),
+        4,
+        message = (
+            f"pixel_unshuffle expects 4D input, "
+            f"but got input with sizes {input_shape}"
+            ),
+    ),
 
     b = input_shape[0]
     c = input_shape[1]
     h = input_shape[2]
     w = input_shape[3]
     downscale_factor_squared = downscale_factor * downscale_factor
-    assert ((h % downscale_factor == 0) & (w % downscale_factor == 0)), (
-        "pixel_unshuffle expects input height and width to be divisible by "
-        + "downscale_factor, but got input with sizes "
-        + str(input_shape)
-        + ", downscale_factor="
-        + str(downscale_factor)
-        + ", and either self.size(2)="
-        + str(h)
-        + " or self.size(3)="
-        + str(w)
-        + " is not divisible by "
-        + str(downscale_factor)
+
+    ivy.assertions.check_equal(
+        [h % downscale_factor, w % downscale_factor],
+        [0, 0], # Assert h % downscale_factor == 0 and w % downscale_factor == 0
+        message = (
+            f"pixel_unshuffle expects input height and width to be divisible by "
+            f"downscale_factor, but got input with sizes {input_shape}"
+            f", downscale_factor= {downscale_factor}"
+            f", and either self.size(2)= {h}"
+            f" or self.size(3)= {w}"
+            f" is not divisible by {downscale_factor}"
+            ),
     )
     oc = c * downscale_factor_squared
     oh = int(h / downscale_factor)
