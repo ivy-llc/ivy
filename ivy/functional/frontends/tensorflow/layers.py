da651c1531e14cc7db75ee637f8a3afe72484ecb
Himanshu Tripathi <himanshut3344@gmail.com>
2022-09-19 17:49:44 +0530

    Update layers.py
    Again when I run it the problem jaxlib appears on my windows system.

---
+++ ivy/functional/frontends/tensorflow/layers.py
@@ -0,0 +1,6 @@
+#global
+import ivy
+import ivy.functional.frontends.tensorflow as tf_frontend
+
+def tf_frontend.keras.layers.concatenate(value, axis, name="ivy.functional.frontends.tensorflow.keras.layers.concatenate"):
+    return tf_frontend.keras.layers.concatenate(value, axis)
