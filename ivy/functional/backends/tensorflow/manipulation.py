4ee9907333adc2f467429affbadc2a10551d101b
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-19 11:43:06 +0530

    Fixed failing test for clip by equating longlong and int64 (#4619)

--- ivy/functional/backends/tensorflow/manipulation.py
+++ ivy/functional/backends/tensorflow/manipulation.py
@@ -291,8 +291,10 @@ def clip(
 ) -> Union[tf.Tensor, tf.Variable]:
     ivy.assertions.check_less(x_min, x_max, message="min values must be less than max")
     if hasattr(x_min, "dtype") and hasattr(x_max, "dtype"):
-        promoted_type = tf.experimental.numpy.promote_types(x.dtype, x_min.dtype)
-        promoted_type = tf.experimental.numpy.promote_types(promoted_type, x_max.dtype)
+        promoted_type = ivy.as_native_dtype(ivy.promote_types(x.dtype, x_min.dtype))
+        promoted_type = ivy.as_native_dtype(
+            ivy.promote_types(promoted_type, x_max.dtype)
+        )
         x = tf.cast(x, promoted_type)
         x_min = tf.cast(x_min, promoted_type)
         x_max = tf.cast(x_max, promoted_type)
