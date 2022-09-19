5a82212025abdd6dc241168525c322795111f9d5
Sharique Shamim <shariqueshamim001@gmail.com>
2022-09-19 11:11:21 +0530

    numpy frontend logic isnan

--- ivy/functional/frontends/numpy/logic/array_type_testing.py
+++ ivy/functional/frontends/numpy/logic/array_type_testing.py
@@ -38,3 +38,22 @@ def isinf(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
+
+def isnan(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting='same_kind',
+    order='K',
+    dtype=None,
+    subok=True
+):
+    if dtype:
+        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
+    ret = ivy.isnan(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
