191a52a3e011088d0d720aaa45db03f59124b4be
Aditya Komaravolu <64011471+Aditya-Komaravolu@users.noreply.github.com>
2022-09-19 21:10:04 +0530

    added test func to abs

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -958,3 +958,32 @@ def test_tensorflow_zero_fraction(
         fn_tree="math.zero_fraction",
         value=np.asarray(x, dtype=input_dtype),
     )
+
+
+#abs
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=-5,
+        max_value=5
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.abs"
+    ),
+)
+def test_tensorflow_asinh(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="abs",
+        x=np.asarray(x, dtype=input_dtype),
+    )
