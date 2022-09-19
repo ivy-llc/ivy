5140f8e9c90542d67167dac57a2cb94581a98ff7
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-19 13:01:08 +0000

    Added hardswish

--- ivy/functional/frontends/torch/non_linear_activation_functions.py
+++ ivy/functional/frontends/torch/non_linear_activation_functions.py
@@ -205,3 +205,12 @@ def leaky_relu_(input, negative_slope=0.01):
     ret = ivy.leaky_relu(input, alpha=negative_slope)
     ivy.inplace_update(input, ret)
     return input
+
+
+def hardswish(input, inplace=False):
+    relu6_val = ivy.minimum(ivy.maximum(ivy.add(input, 3), 0), 6)
+    ret = ivy.multiply(input, ivy.divide(relu6_val, 6))
+    if inplace:
+        ivy.inplace_update(input, ret)
+        return input
+    return ret
--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -918,3 +918,36 @@ def test_torch_leaky_relu_(
         negative_slope=alpha,
         test_values=False,
     )
+
+
+# hardswish
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.hardswish"
+    ),
+)
+def test_torch_hardswish(
+    dtype_and_input,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_input
+    assume("float16" not in input_dtype)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.hardswish",
+        input=np.asarray(input, dtype=input_dtype),
+        inplace=False,
+    )
