dfd11cdb8ba199d3b4161bd291677dd48057eeda
atlasrule <51425254+atlasrule@users.noreply.github.com>
2022-09-20 20:59:58 +0300

    Add eigh to JAX frontend
    I've added eigh to JAX LAX frontend, and migrated function "symmetrize" as a helper, since it is commonly used.

--- ivy/functional/frontends/jax/lax/linalg.py
+++ ivy/functional/frontends/jax/lax/linalg.py
@@ -1,12 +1,26 @@
 import ivy
 
 
-def cholesky(x, /, *, symmetrize_input=True):
-    def symmetrize(x):
-        # TODO : Take Hermitian transpose after complex numbers added
-        return (x + ivy.swapaxes(x, -1, -2)) / 2
+def _symmetrize(x):
+    # TODO : Take Hermitian transpose after complex numbers added
+    return (x + ivy.swapaxes(x, -1, -2)) / 2
 
+
+def cholesky(x, /, *, symmetrize_input=True):
     if symmetrize_input:
-        x = symmetrize(x)
+        x = _symmetrize(x)
 
     return ivy.cholesky(x)
+
+
+def eigh(x, /, *, lower=True, symmetrize_input=True, sort_eigenvalues=True):
+    upper = not lower
+    
+    if symmetrize_input:
+        x = _symmetrize(x)
+
+    if sort_eigenvalues:
+        ret = ivy.eigh(x, upper)
+        return ret._replace(eigenvalues = ivy.sort(ret.eigenvalues)
+
+    return ivy.eigh(x, upper)
