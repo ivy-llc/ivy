43b2c9c30cdc0081766dbbb26d824c4c4c483315
CatB1t <skytedits@gmail.com>
2022-09-21 01:29:45 +0200

    fix `inner` test in `NumPy` frontend.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_matrix_and_vector_products.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_matrix_and_vector_products.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 from hypothesis import given, strategies as st
-import ivy.functional.frontends.numpy as ivy_np
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -82,47 +81,16 @@ def test_numpy_outer(
     )
 
 
-@st.composite
-def _inner_get_dtype_and_data(draw):
-    input_dtype = draw(
-        st.shared(st.sampled_from(ivy_np.valid_numeric_dtypes), key="shared_dtype")
-    )
-
-    shape = (
-        draw(st.integers(min_value=1, max_value=5)),
-        draw(st.integers(min_value=1, max_value=5)),
-    )
-    x = draw(
-        helpers.array_values(
-            dtype=input_dtype,
-            shape=shape,
-            min_value=-10,
-            max_value=10,
-        )
-    )
-
-    data1 = (input_dtype, x)
-
-    shape = (
-        draw(st.integers(min_value=1, max_value=5)),
-        draw(st.integers(min_value=1, max_value=5)),
-    )
-    data2 = draw(
-        helpers.dtype_and_values(
-            available_dtypes=ivy_np.valid_numeric_dtypes,
-            shape=shape,
-            min_value=-10,
-            max_value=10,
-        )
-    )
-
-    return data1, data2
-
-
 # inner
 @handle_cmd_line_args
 @given(
-    dtype_and_x=_inner_get_dtype_and_data(),
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_value=-10,
+        max_value=10,
+        num_arrays=2,
+        shared_dtype=True,
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.inner"
     ),
@@ -134,12 +102,9 @@ def test_numpy_inner(
     num_positional_args,
     fw,
 ):
-    data1, data2 = dtype_and_x
-    input_dtype1, x = data1
-    input_dtype2, y = data2
-
+    input_dtypes, xs = dtype_and_x
     helpers.test_frontend_function(
-        input_dtypes=[input_dtype1, input_dtype2],
+        input_dtypes=input_dtypes,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
@@ -147,9 +112,8 @@ def test_numpy_inner(
         fw=fw,
         frontend="numpy",
         fn_tree="inner",
-        a=np.array(x, dtype=input_dtype1),
-        b=np.array(y, dtype=input_dtype2),
-        out=None,
+        a=xs[0],
+        b=xs[1],
     )
 
 
