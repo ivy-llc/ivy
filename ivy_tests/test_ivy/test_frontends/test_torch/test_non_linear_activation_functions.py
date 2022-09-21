a753dca0d02df5775cc87eda606983de0d164bf3
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-21 17:50:56 +0000

    set num_pos_args to its value

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -895,7 +895,6 @@ def test_torch_leaky_relu_(
     )
 
 
-
 # hardswish
 @handle_cmd_line_args
 @given(
@@ -903,14 +902,10 @@ def test_torch_leaky_relu_(
         available_dtypes=helpers.get_dtypes("float"),
         safety_factor_scale="log",
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.torch.hardswish"
-    ),
 )
 def test_torch_hardswish(
     dtype_and_input,
     as_variable,
-    num_positional_args,
     native_array,
     fw,
 ):
@@ -920,7 +915,7 @@ def test_torch_hardswish(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
-        num_positional_args=num_positional_args,
+        num_positional_args=1,
         native_array_flags=native_array,
         fw=fw,
         frontend="torch",
