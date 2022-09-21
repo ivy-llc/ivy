b33fba99877ab1705adc4c1da83c5905c9648829
Kenneth Ambet <100001509+ShiftLove@users.noreply.github.com>
2022-09-21 15:33:09 -0700

    Update test_solving_equations_and_inverting_matrices.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
@@ -97,3 +97,33 @@ def test_numpy_pinv(dtype_and_x, as_variable, native_array, num_positional_args,
         a=np.array(x, dtype=dtype),
         rtol=1e-15,
     )
+    
+    
+    # tensorinv
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=2,
+        max_num_dims=2,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.linalg.tensorinv"
+    ),
+)
+def test_numpy_tensorinv(dtype_and_x, as_variable, native_array, num_positional_args, fw):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        native_array_flags=native_array,
+        num_positional_args=num_positional_args,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="linalg.tensorinv",
+        a=np.array(x, dtype=dtype)
+     
+    )
