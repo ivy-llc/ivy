4a7a7c1cae2696f5775150726082443b40dec2a6
ndurumo254 <erickndurumo@gmail.com>
2022-09-21 07:01:20 -0700

    added matrix_power function

--- ivy/functional/frontends/jax/lax/linalg.py
+++ ivy/functional/frontends/jax/lax/linalg.py
@@ -1,4 +1,8 @@
 import ivy
+from jax.numpy import linalg
+
+from brainpy.math.jaxarray import JaxArray
+from brainpy.math.numpy_ops import _remove_jaxarray
 
 
 def cholesky(x, /, *, symmetrize_input=True):
@@ -10,3 +14,16 @@ def cholesky(x, /, *, symmetrize_input=True):
         x = symmetrize(x)
 
     return ivy.cholesky(x)
+
+
+ #adding a max_power function
+
+def matrix_power(m,n):
+    #m is the input marix that is created by a numpy
+    #n is the output exponential power
+    # return will be n*m which is a ndarray or a matrix object
+    m= _remove_jaxarray(m)
+    return JaxArray(linalg.matrix_power(m,n))
+
+
+
