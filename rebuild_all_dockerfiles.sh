1cca532c0b08eb8ac11d189198d4f4b3a10e86b8
Simone G <74716948+simonetgordon@users.noreply.github.com>
2022-09-21 10:23:20 +0100

    move rebuild .sh to docker folder + fixes
    Former-commit-id: 40eaf97a0f8af1fcf4c064dcfaed7e4cab03e9b8

--- rebuild_all_dockerfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t unifyai/ivy:latest --no-cache -f docker/Dockerfile .
-docker build -t unifyai/ivy:latest-gpu --no-cache docker/DockerfileGPU .
-docker build -t unifyai/ivy:latest-copsim --no-cache docker/DockerfileCopsim .
