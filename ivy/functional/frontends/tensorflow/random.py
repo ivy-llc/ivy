4ccc04a98b43526c5ba96cbc4635eb0ff2c5ee21
Auxia <sbhathal12@gmail.com>
2022-09-21 15:55:10 +0000

    Removed Uniform

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -7,10 +7,3 @@ def shuffle(value, seed, name=None):
 
 
 shuffle.unsupported_dtypes = ("float16", "bfloat16")
-
-
-def uniform(shape, minval=0, maxval=None, dtype=None, seed=None, name=None):
-    return ivy.random_uniform(low=minval, high=maxval, shape=shape, dtype=dtype)
-
-
-uniform.unsupported_dtypes = ("float16", "bfloat16")
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
@@ -4,16 +4,13 @@ from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-
-# import ivy.functional.backends.numpy as ivy_np
-import ivy.functional.backends.tensorflow as ivy_tf
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 @handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=ivy_tf.valid_float_dtypes,
+        available_dtypes=helpers.get_dtypes("numeric"),
         allow_inf=False,
         min_num_dims=1,
         min_dim_size=2,
@@ -39,57 +36,4 @@ def test_tensorflow_shuffle(
         fn_tree="random.shuffle",
         value=np.asarray(x, dtype=input_dtype),
         seed=seed,
-    )
-
-
-@handle_cmd_line_args
-@given(
-    dtype_and_low=helpers.dtype_and_values(
-        available_dtypes=ivy_tf.valid_float_dtypes,
-        min_value=0,
-        max_value=1,
-        allow_inf=False,
-    ),
-    dtype_and_high=helpers.dtype_and_values(
-        available_dtypes=ivy_tf.valid_float_dtypes,
-        min_value=0,
-        max_value=1,
-        allow_inf=False,
-    ),
-    shape=helpers.get_shape(),
-    dtype=st.sampled_from(ivy_tf.valid_float_dtypes),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.uniform"
-    ),
-)
-def test_tensorflow_uniform(
-    dtype_and_low,
-    dtype_and_high,
-    shape,
-    dtype,
-    as_variable,
-    num_positional_args,
-    native_array,
-    fw,
-):
-    low_dtype, low = dtype_and_low
-    high_dtype, high = dtype_and_high
-    # If low and high are numerics and not arrays, shape=shape else shape=None
-    if isinstance(low, (int, float)) and isinstance(high, (int, float)):
-        shape = shape
-    else:
-        shape = None
-    helpers.test_frontend_function(
-        input_dtypes=[low_dtype, high_dtype],
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="tensorflow",
-        fn_tree="random.uniform",
-        shape=shape,
-        minval=np.asarray(low, dtype=low_dtype),
-        maxval=np.asarray(high, dtype=high_dtype),
-        dtype=dtype,
     )
