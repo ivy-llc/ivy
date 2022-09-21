b91dd1827e2cceb31c0bc06f09b9408c871451a7
Aarsh2001 <ac5973@srmist.edu.in>
2022-09-21 22:05:58 +0530

    checks

--- run_tests_CLI/run_ivy_core_test.py
+++ run_tests_CLI/run_ivy_core_test.py
@@ -35,6 +35,8 @@ backend = backends[0]
 submodule = submodules[0]
 
 # print(backend, submodule)
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
 os.environ["BACKEND"] = backend
 os.environ["SUBMODULE"] = submodule
 # os.system(f"./run_tests_CLI/test_ivy_core.sh {backend} test_{submodule}")
