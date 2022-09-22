0eddd433e28b3cc1dd3549515651d9c19217ef69
dovaaa <60916972+dovaaa@users.noreply.github.com>
2022-09-22 15:35:11 +0200

    Added LogicalOr to Raw_Ops tensorflow frontend  (#4783)

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -1060,3 +1060,33 @@ def test_tensorflow_zeros_like(
         fn_tree="raw_ops.ZerosLike",
         x=np.asarray(x, dtype=dtype),
     )
+
+
+# LogicalOr
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        dtype=["bool", "bool"],
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.LogicalOr"
+    ),
+)
+def test_tensorflow_LogicalOr(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="raw_ops.LogicalOr",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1]),
+    )
