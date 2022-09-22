7e7bdf9fda961906d6c3b49140be695ada695493
Kenneth Ambet <100001509+ShiftLove@users.noreply.github.com>
2022-09-22 03:04:04 -0700

    Update test_solving_equations_and_inverting_matrices.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
@@ -111,8 +111,7 @@ def test_numpy_pinv(dtype_and_x, as_variable, native_array, num_positional_args,
         fn_name="ivy.functional.frontends.numpy.linalg.tensorinv"
     ),
 )
-def test_numpy_tensorinv(dtype_and_x, as_variable, native_array, num_positional_args,
-    fw):
+def test_numpy_tensorinv(dtype_and_x,as_variable,native_array,num_positional_args,fw):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
