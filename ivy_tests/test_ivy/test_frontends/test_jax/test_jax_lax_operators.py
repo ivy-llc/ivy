fcd33697bf2aeeafdc145669680838a5f5593a59
kimbring2 <kimbring2@gmail.com>
2022-09-22 20:42:03 +0900

    shift_left function for jax frontend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -2112,3 +2112,40 @@ def test_jax_lax_erf(
         fn_tree="lax.erf",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.jax.lax.shift_left"
+    ),
+)
+def test_jax_lax_shift_left(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    # negative shifts will throw an exception
+    # shifts >= dtype witdth produce backend-defined behavior
+    x[1] = np.clip(x[1], 0, np.iinfo(input_dtype[1]).bits - 1)
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="lax.shift_left",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+        y=np.asarray(x[1], dtype=input_dtype[1])
+    )
