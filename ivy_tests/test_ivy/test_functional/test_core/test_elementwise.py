e35408295cb0bb41d67536e790dc00cc1d24a344
Nirzu97 <nirzu97@gmail.com>
2022-09-22 20:58:54 +0530

    Add test for isreal

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -2400,3 +2400,37 @@ def test_trunc_divide(
         x1=x1,
         x2=x2,
     )
+
+
+# isreal
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="isreal"),
+)
+def test_isreal(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="isreal",
+        x=np.asarray(x, dtype=input_dtype),
+    )
