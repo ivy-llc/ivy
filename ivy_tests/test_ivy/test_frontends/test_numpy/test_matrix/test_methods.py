8f4106149d009fdec28b66c95b1056c5789779bd
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-22 19:21:40 +0800

    update argmax

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_matrix/test_methods.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_matrix/test_methods.py
@@ -65,19 +65,16 @@ def _array_with_dtype_axis_keepdims_and_where(draw):
 @given(
     dtype_x_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("numeric"),
-        min_num_dims=1,
-        min_dim_size=1,
+        min_num_dims=2,
+        max_num_dims=2,
+        min_dim_size=2,
         valid_axis=True,
         force_int_axis=True,
         allow_neg_axes=False,
     ),
-    as_variable=helpers.array_bools(num_arrays=1),
-    with_out=st.booleans(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.argmax"
     ),
-    native_array=helpers.array_bools(num_arrays=1),
-    keep_dims=st.booleans(),
 )
 def test_numpy_argmax(
     dtype_x_axis,
@@ -86,13 +83,12 @@ def test_numpy_argmax(
     num_positional_args,
     native_array,
     fw,
-    keep_dims,
 ):
     input_dtype, x, axis = dtype_x_axis
     if isinstance(axis, tuple):
         axis = axis[0]
     input_dtype = [input_dtype]
-    np_frontend_helpers.test_frontend_function(
+    helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=with_out,
@@ -101,11 +97,9 @@ def test_numpy_argmax(
         fw=fw,
         frontend="numpy",
         fn_tree="argmax",
-        a=np.asarray(x, dtype=input_dtype[0]),
+        self=np.asarray(x, dtype=input_dtype[0]),
         axis=axis,
         out=None,
-        keepdims=keep_dims,
-        test_values=False,
     )
 
 
