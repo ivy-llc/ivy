1fd0e30e7c5926e913be1fb675c59fdd3bf7318f
Umar Burhanudin <44232856+umarfb@users.noreply.github.com>
2022-09-22 17:09:30 +0100

    Added linear activation (#4506)
    * Added linear activation
    
    * Added test for tensorflow linear activation

--- ivy/functional/frontends/tensorflow/activations.py
+++ ivy/functional/frontends/tensorflow/activations.py
@@ -1,10 +1,12 @@
 import ivy
 
-
 def hard_sigmoid(x):
     point_two = ivy.full(x.shape, 0.2)
     point_five = ivy.full(x.shape, 0.5)
     x = ivy.multiply(x, point_two)
     x = ivy.add(x, point_five)
     x = ivy.clip(x, 0.0, 1.0)
+    return x
+
+def linear(x):
     return x
