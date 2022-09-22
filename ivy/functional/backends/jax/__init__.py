003a181e80968fb3062bc6874b79ef9dd2fcde52
Nirzu97 <nirzu97@gmail.com>
2022-09-22 20:14:40 +0530

    Add complex dtype to JAX backend

--- ivy/functional/backends/jax/__init__.py
+++ ivy/functional/backends/jax/__init__.py
@@ -66,6 +66,8 @@ native_bfloat16 = jnp.dtype("bfloat16")
 native_float16 = jnp.dtype("float16")
 native_float32 = jnp.dtype("float32")
 native_float64 = jnp.dtype("float64")
+native_complex64 = jnp.dtype("complex64")
+native_complex128 = jnp.dtype("complex128")
 # noinspection PyShadowingBuiltins
 native_bool = jnp.dtype("bool")
 
@@ -83,6 +85,8 @@ valid_dtypes = (
     ivy.float16,
     ivy.float32,
     ivy.float64,
+    ivy.complex64,
+    ivy.complex128,
     ivy.bool,
 )
 valid_numeric_dtypes = (
@@ -98,6 +102,8 @@ valid_numeric_dtypes = (
     ivy.float16,
     ivy.float32,
     ivy.float64,
+    ivy.complex64,
+    ivy.complex128,
 )
 valid_int_dtypes = (
     ivy.int8,
@@ -111,6 +117,7 @@ valid_int_dtypes = (
 )
 valid_float_dtypes = (ivy.bfloat16, ivy.float16, ivy.float32, ivy.float64)
 valid_uint_dtypes = (ivy.uint8, ivy.uint16, ivy.uint32, ivy.uint64)
+valid_complex_dtypes = (ivy.complex64, ivy.complex128)
 
 # invalid data types
 invalid_dtypes = ()
@@ -118,6 +125,7 @@ invalid_numeric_dtypes = ()
 invalid_int_dtypes = ()
 invalid_float_dtypes = ()
 invalid_uint_dtypes = ()
+invalid_complex_dtypes = ivy.complex256
 
 native_inplace_support = False
 
