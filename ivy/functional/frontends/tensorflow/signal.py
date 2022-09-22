46174fad7badf967007bf2db696dbc0bb1d6fa89
teckno <mwapebt@gmail.com>
2022-09-22 16:46:11 +0200

    Adding tendorflow function  for hann_window

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -1,37 +1,6 @@
-
-import ivy
-import ivy.functional.frontends.tensorflow as tf_frontend
+import tensorflow as tf
 
 
-class Tensor:
-    def __init__(self, data):
-        if ivy.is_native_array(data):
-            data = ivy.Array(data)
-        self.data = data
-
-    # Instance Methoods #
-    # -------------------#
-
-    def Reshape(self, shape, name="Reshape"):
-        return tf_frontend.Reshape(self.data, shape, name)
-
-    def add(self, y, name="add"):
-        return tf_frontend.add(self.data, y, name)
-
-    def hann_window(self, name="hann_window"):
-        return ivy.hann_window(self.data, name)
-
-    def hamming_window(self, name="hamming_window"):
-        return ivy.hamming_window(self.data, name)
-
-    def blackman_window(self, name="blackman_window"):
-        return ivy.blackman_window(self.data, name)
-
-    def bartlett_window(self, name="bartlett_window"):
-        return ivy.bartlett_window(self.data, name)
-
-    def kaiser_window(self, name="kaiser_window"):
-        return ivy.kaiser_window(self.data, name)
-
-    def stft(self, name="stft"):
-        return ivy.stft(self.data, name)
+# hann_window 
+def hann_window(window_length, dtype=None):
+    return tf.signal.hann_window(window_length, dtype=dtype)
