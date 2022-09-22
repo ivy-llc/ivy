963e17f5de7b438039619c11eb323f46e8a15509
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-22 18:03:28 +0530

    Added `hardswish` to the torch frontend (#4659)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -895,6 +895,36 @@ def test_torch_leaky_relu_(
     )
 
 
+# hardswish
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        safety_factor_scale="log",
+    ),
+)
+def test_torch_hardswish(
+    dtype_and_input,
+    as_variable,
+    native_array,
+    fw,
+):
+    input_dtype, input = dtype_and_input
+    assume("float16" not in input_dtype)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=1,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.hardswish",
+        input=np.asarray(input, dtype=input_dtype),
+        inplace=False,
+    )
+    
+
 # hardsigmoid
 # ToDo Test inplace once inplace testing implemented
 # It was validated to work outside of the testing framework,
