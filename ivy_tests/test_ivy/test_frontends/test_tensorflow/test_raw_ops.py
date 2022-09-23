f2fa3bc3a2bd9a24fa83a7072c02dae81f8437e7
zuruoke <zuruokeokafor2016@gmail.com>
2022-09-23 14:48:56 +0100

    fix code formatting

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -746,6 +746,33 @@ def test_tensorflow_Minimum(
     )
 
 
+# Softmax
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.Softmax"
+    )
+)
+def test_tensorflow_Softmax(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="raw_ops.Softmax",
+        x=np.asarray(x, dtype=input_dtype),
+    )
+
+
 # Sub
 @handle_cmd_line_args
 @given(
@@ -1117,33 +1144,5 @@ def test_tensorflow_LogicalNot(
         fw=fw,
         frontend="tensorflow",
         fn_tree="raw_ops.LogicalNot",
-        x=np.asarray(x, dtype=input_dtype),
-    )
-
-# Softmax
-
-
-@handle_cmd_line_args
-@given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-    ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.Softmax"
-    )
-)
-def test_tensorflow_Softmax(
-    dtype_and_x, as_variable, num_positional_args, native_array, fw
-):
-    input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        fw=fw,
-        frontend="tensorflow",
-        fn_tree="raw_ops.Softmax",
         x=np.asarray(x, dtype=input_dtype),
     )
