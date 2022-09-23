1e9a4d855fe1ed71c3dc7d525f6308efee3118af
AnnaTz <anna.tzatzopoulou@gmail.com>
2022-09-23 13:53:05 +0100

    add test function for unfold

--- ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
@@ -1,6 +1,6 @@
 # global
 import random
-from hypothesis import given, strategies as st
+from hypothesis import given, assume, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -77,4 +77,61 @@ def test_torch_conv2d_2(input, weight, bias, stride, dilation, padding, fw):
         padding=padding,
         dilation=dilation,
         groups=groups,
+    )
+
+
+@st.composite
+def int_or_tuple(draw):
+    key_st = st.sampled_from(
+        [
+            st.integers(min_value=1),
+            st.tuples(st.integers(min_value=1)),
+            st.tuples(st.integers(min_value=1), st.integers(min_value=1)),
+        ]
+    )
+    return draw(key_st)
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=3,
+        max_num_dims=4,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.unfold"
+    ),
+)
+def test_torch_unfold(
+    dtype_and_input,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    x_dtype, x = dtype_and_input
+    kernel_size = int_or_tuple()
+    dilation = int_or_tuple()
+    padding = int_or_tuple()
+    stride = int_or_tuple()
+    arg_dtypes = x_dtype + [int, int, int, int]
+    if x.ndim() == 3:
+        assume(x.shape()[0] and x.shape()[1] and x.shape()[2])
+    elif x.ndim() == 4:
+        assume(x.shape()[1] and x.shape()[2] and x.shape()[3])
+    helpers.test_frontend_function(
+        input_dtypes=arg_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.unfold",
+        input=x,
+        kernel_size=kernel_size,
+        dilation=dilation,
+        padding=padding,
+        stride=stride,
     )
