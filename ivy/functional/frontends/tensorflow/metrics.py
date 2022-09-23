9ca2c6047a21036755e1f4485d6b6cae529232f9
Aditya-Komaravolu <komaravoluaditya@gmail.com>
2022-09-23 20:31:33 +0530

    made linting changes

--- ivy/functional/frontends/tensorflow/metrics.py
+++ ivy/functional/frontends/tensorflow/metrics.py
@@ -209,10 +209,9 @@ def cosine_similarity(y_true, y_pred):
     if len(y_pred.shape) == len(y_pred.shape) and len(y_true.shape) == 2:
         numerator = ivy.sum(y_true * y_pred, axis=1)
         denominator = (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
-        cosine = numerator / denominator
-
     else:
-        cosine = ivy.vecdot(y_true, y_pred) / \
-                 (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
-    
+        numerator = ivy.vecdot(y_true, y_pred)
+        denominator = (ivy.matrix_norm(y_true) * ivy.matrix_norm(y_pred))
+
+    cosine = numerator / denominator
     return cosine
