372779117f5257dddb73f5fa96f665451f594e45
Nielsencu <50238554+Nielsencu@users.noreply.github.com>
2022-09-24 23:37:59 +0200

    changed to ivy zeros

--- ivy/functional/frontends/torch/linear_functions.py
+++ ivy/functional/frontends/torch/linear_functions.py
@@ -30,8 +30,7 @@ def bilinear(input1, input2, weight, bias=None):
     weight_flattened = ivy.reshape(weight, (-1, weight_shape[-2], weight_shape[-1]))
 
     # Create empty output array with shape (*, Hout)
-    output = ivy.zeros_like([[0.0 for _ in range(ivy.shape(weight_flattened)[0])] 
-                            for _ in range(ivy.shape(input1_flattened)[0])])
+    output = ivy.zeros((ivy.shape(weight_flattened)[0]  ,ivy.shape(input1_flattened)[0]))
 
     for i in range(ivy.shape(weight_flattened)[0]):
         buff_1 = ivy.matmul(input1_flattened, weight_flattened[i])
