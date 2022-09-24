96d5640f8bd9526f930c040e42444f534374cc6e
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-09-24 22:35:55 +0530

    docs not rendering properly for general and updated make_docs_without_docker.sh (#4842)

--- ivy/array/general.py
+++ ivy/array/general.py
@@ -233,6 +233,7 @@ class ArrayWithGeneral(abc.ABC):
         Examples
         --------
         scatter values into an array
+
         >>> arr = ivy.array([1,2,3,4,5,6,7,8, 9, 10])
         >>> indices = ivy.array([[4], [3], [1], [7]])
         >>> updates = ivy.array([9, 10, 11, 12])
@@ -241,6 +242,7 @@ class ArrayWithGeneral(abc.ABC):
         ivy.array([ 1, 11,  3, 10,  9,  6,  7, 12,  9, 10])
 
         scatter values into an empty array
+
         >>> shape = ivy.array([2, 5])
         >>> indices = ivy.array([[1,4], [0,3], [1,1], [0,2]])
         >>> updates = ivy.array([25, 40, 21, 22])
@@ -365,8 +367,8 @@ class ArrayWithGeneral(abc.ABC):
                        [11, 2]],
                       [[3, 5],
                        [9, 7]]])
-        >>> reduced = x.einops_reduce('a b c -> b c', 'max')
-        >>> print(reduced)
+        >>> y = x.einops_reduce('a b c -> b c', 'max')
+        >>> print(y)
         ivy.array([[ 5,  5],
                    [11,  7]])
 
@@ -374,8 +376,8 @@ class ArrayWithGeneral(abc.ABC):
                         [11, 2, 9]],
                        [[3, 5, 7],
                         [9, 7, 1]]])
-        >>> reduced = x.einops_reduce('a b c -> a () c', 'min')
-        >>> print(reduced)
+        >>> y = x.einops_reduce('a b c -> a () c', 'min')
+        >>> print(y)
         ivy.array([[[5, 2, 3]],
                    [[3, 5, 1]]])
 
@@ -417,16 +419,16 @@ class ArrayWithGeneral(abc.ABC):
         Examples
         --------
         >>> x = ivy.array([5,4])
-        >>> repeated = x.einops_repeat('a -> a c', c=3)
-        >>> print(repeated)
+        >>> y = x.einops_repeat('a -> a c', c=3)
+        >>> print(y)
         ivy.array([[5, 4],
                    [5, 4],
                   [5, 4]])
 
         >>> x = ivy.array([[5,4],
                     [2, 3]])
-        >>> repeated = x.einops_repeat('a b ->  a b c', c=3)
-        >>> print(repeated)
+        >>> y = x.einops_repeat('a b ->  a b c', c=3)
+        >>> print(y)
         ivy.array([[[5, 5, 5],
                     [4, 4, 4]],
                    [[2, 2, 2],
