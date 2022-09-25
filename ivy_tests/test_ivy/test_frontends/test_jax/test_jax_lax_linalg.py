2bf3a28c92cd4440776fd98f3b0bdb4a59fb6c0d
ndurumo254 <erickndurumo@gmail.com>
2022-09-25 05:15:35 -0700

    tests added

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
@@ -50,3 +50,34 @@ def test_jax_lax_cholesky(
         x=x,
         symmetrize_input=symmetrize_input,
     )
+
+    # matrix_power
+    @handle_cmd_line_args
+    @given(
+        dtype_and_x=helpers.dtype_and_values(
+            available_dtypes=helpers.get_dtypes("float"),
+            min_value=0,
+            max_value=50,
+            shape=helpers.ints(min_value=2, max_value=8).map(lambda x: tuple([x, x])),
+        ),
+        n=helpers.ints(min_value=1, max_value=8),
+        num_positional_args=helpers.num_positional_args(
+            fn_name="ivy.functional.frontends.numpy.linalg.matrix_power"
+        ),
+    )
+    def test_jax_lax_matrix_power(
+        dtype_and_x, n, as_variable, native_array, num_positional_args, fw
+    ):
+        dtype, x = dtype_and_x
+        helpers.test_frontend_function(
+            input_dtypes=dtype,
+            as_variable_flags=as_variable,
+            with_out=False,
+            num_positional_args=num_positional_args,
+            native_array_flags=native_array,
+            fw=fw,
+            frontend="jax",
+            fn_tree="lax.linalg.matrix_power",
+            a=np.array(x, dtype=dtype),
+            n=n,
+        )
