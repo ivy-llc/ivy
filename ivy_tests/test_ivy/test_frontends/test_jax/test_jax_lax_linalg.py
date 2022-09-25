e5f6ffca8d7f504b2fed89fe90bf5a409a2f5c4d
karalleyna <aleynakara98@gmail.com>
2022-09-25 11:05:19 +0000

    Update test_jax_lax_linalg.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
@@ -1,12 +1,14 @@
 # global
 import sys
-import numpy as np
-from hypothesis import given, strategies as st
 
 # local
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args, assert_all_close
+import numpy as np
+from hypothesis import given
+from hypothesis import strategies as st
+from ivy_tests.test_ivy.helpers import assert_all_close
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 # svd
@@ -14,10 +16,13 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args, assert_all_close
 @given(
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=3,
-        max_num_dims=5,
-        min_dim_size=2,
-        max_dim_size=5,
+        min_value=0,
+        max_value=10,
+        shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: np.linalg.cond(x[1]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1])) != 0
+        and x[0] != "float16"
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.lax.linalg.svd"
@@ -35,19 +40,47 @@ def test_jax_lax_svd(
     fw,
 ):
     dtype, x = dtype_and_x
-    helpers.test_frontend_function(
+    x = np.array(x, dtype=dtype)
+    # make symmetric positive-definite beforehand
+    x = np.matmul(x.T, x) + np.identity(x.shape[0]) * 1e-3
+
+    frontend = "jax"
+
+    ret, frontend_ret = helpers.test_frontend_function(
         input_dtypes=[dtype],
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
-        frontend="jax",
+        frontend=frontend,
         fn_tree="lax.linalg.svd",
-        x=np.array(x, dtype=dtype),
+        x=x,
         full_matrices=full_matrices,
+        test_values=False,
         compute_uv=compute_uv,
     )
+
+    if compute_uv:
+        ret = [ivy.to_numpy(x) for x in ret]
+        frontend_ret = [np.asarray(x) for x in frontend_ret]
+
+        u, s, vh = ret
+        frontend_u, frontend_s, frontend_vh = frontend_ret
+
+        assert_all_close(
+            ret_np=u @ np.diag(s) @ vh,
+            ret_from_gt_np=frontend_u @ np.diag(frontend_s) @ frontend_vh,
+            rtol=1e-2,
+            ground_truth_backend=frontend,
+        )
+    else:
+        assert_all_close(
+            ret_np=ivy.to_numpy(ret),
+            ret_from_gt_np=np.asarray(frontend_ret[0]),
+            rtol=1e-2,
+            ground_truth_backend=frontend,
+        )
 
 
 # cholesky
@@ -79,8 +112,8 @@ def test_jax_lax_cholesky(
     x = np.array(x, dtype=dtype)
     # make symmetric positive-definite beforehand
     x = np.matmul(x.T, x) + np.identity(x.shape[0]) * 1e-3
-    ret, frontend_ret = helpers.test_frontend_function(
-        input_dtypes=[dtype],
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
@@ -88,17 +121,7 @@ def test_jax_lax_cholesky(
         fw=fw,
         frontend="jax",
         fn_tree="lax.linalg.cholesky",
-        test_values=False,
+        rtol=1e-02,
         x=x,
         symmetrize_input=symmetrize_input,
-    )
-    ret = [ivy.to_numpy(x) for x in ret]
-    frontend_ret = [np.asarray(x) for x in frontend_ret]
-
-    u, s, v = ret
-    frontend_u, frontend_s, frontend_v = frontend_ret
-
-    assert_all_close(
-        ret_np=u @ np.diag(s) @ v.T,
-        ret_from_gt_np=frontend_u @ np.diag(frontend_s) @ frontend_v.T,
     )
