72dae5225f1f29281f7775d7fc272c28ac1d85c3
Aarsh2001 <ac5973@srmist.edu.in>
2022-09-26 17:16:44 +0530

    refactor print to stdout

--- run_tests_CLI/run_ivy_stateful_test.py
+++ run_tests_CLI/run_ivy_stateful_test.py
@@ -27,9 +27,11 @@ j = run % M
 backend = backends[i]
 submodule = submodules[j]
 
+"""
 with open("~/.bashrc", "a") as outfile:
     # 'a' stands for "append"
     outfile.write(f"export BACKEND={backend}\n")
     outfile.write(f"export SUBMODULE={submodule}\n")
+"""
 
 os.system(f"./run_tests_CLI/test_ivy_stateful.sh {backend} test_{submodule}")
