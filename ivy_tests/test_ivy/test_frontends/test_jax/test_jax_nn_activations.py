4110dc25344f349708f8bcb47d66bf1663f70736
kundakinte <69470630+kundakinte@users.noreply.github.com>
2022-09-26 16:59:15 +0300

    added selu test to jax frontend nn tests

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_nn_activations.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_nn_activations.py
@@ -739,3 +739,36 @@ def test_jax_nn_hard_swish(
         fn_tree="nn.hard_swish",
         x=np.asarray(x, dtype=input_dtype),
     )
+
+
+@handle_cmd_line_args
+@given(
+     dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        large_abs_safety_factor=2,
+        small_abs_safety_factor=2,
+        safety_factor_scale="linear",
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.nn.selu"
+    ),
+)
+def test_jax_nn_selu(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="nn.selu",
+        x=np.asarray(x, dtype=input_dtype),
+    )
