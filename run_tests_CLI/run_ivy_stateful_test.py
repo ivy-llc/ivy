94551f9e05cd33ffdb949768f3e8623e4dd6e10c
Aarsh2001 <ac5973@srmist.edu.in>
2022-09-26 12:57:01 +0530

    refactor print to stdout

--- run_tests_CLI/run_ivy_stateful_test.py
+++ run_tests_CLI/run_ivy_stateful_test.py
@@ -14,21 +14,16 @@ submodules = [
 ]
 
 
-def cron_job():
-    run = int(sys.argv[1])
-    N = len(backends)
-    M = len(submodules)
+run = int(sys.argv[1])
+N = len(backends)
+M = len(submodules)
 
-    num_tests = N * M
-    run = run % num_tests
+num_tests = N * M
+run = run % num_tests
 
-    i = run // M
-    j = run % M
+i = run // M
+j = run % M
 
-    backend = backends[i]
-    submodule = submodules[j]
-    os.system(f". ./run_tests_CLI/test_ivy_stateful.sh {backend} test_{submodule}")
-
-
-if __name__ == "__main__":
-    cron_job()
+backend = backends[i]
+submodule = submodules[j]
+os.system(f". ./run_tests_CLI/test_ivy_stateful.sh {backend} test_{submodule}")
