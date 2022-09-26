2e8e94568d8aa399ba018a825efd53d099172ad6
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-09-26 08:53:35 +0000

    Added sub pytorch instance method and instance test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -57,6 +57,53 @@ def test_torch_instance_add(
     )
 
 
+# sub
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=tuple(
+            set(ivy_np.valid_float_dtypes).intersection(
+                set(ivy_torch.valid_float_dtypes)
+            )
+        ),
+        num_arrays=2,
+        min_value=-1e04,
+        max_value=1e04,
+        allow_inf=False,
+    ),
+    alpha=st.floats(min_value=-1e06, max_value=1e06, allow_infinity=False),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.Tensor.sub",
+    ),
+)
+def test_torch_instance_sub(
+    dtype_and_x,
+    alpha,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_array_instance_method(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        frontend_class=Tensor,
+        fn_tree="Tensor.sub",
+        rtol=1e-04,
+        self=np.asarray(x[0], dtype=input_dtype[0]),
+        other=np.asarray(x[1], dtype=input_dtype[1]),
+        alpha=alpha,
+        out=None,
+    )
+
+
 # reshape
 @st.composite
 def dtypes_x_reshape(draw):
