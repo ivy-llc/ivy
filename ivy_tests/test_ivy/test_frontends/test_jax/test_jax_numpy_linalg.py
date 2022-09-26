4e35a7a9125d24fc455b387d2369cd70de19899a
Aleyna Kara <36455180+karalleyna@users.noreply.github.com>
2022-09-27 02:37:21 +0300

    jax.numpy.linalg.inv (#4892)
    * Add numpy.linalg.inv to JAX frontend
    
    * Add test_jax_numpy_inv
    
    * Update test_jax_numpy_inv
    
    * Widened bounds on test for inv in Jax frontend
    
    * Removed redundant import statement
    
    Co-authored-by: Jamie Line <60134692+JamieLine@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -1,6 +1,7 @@
 # global
 import sys
 import numpy as np
+
 from hypothesis import given, strategies as st
 
 # local
@@ -93,4 +94,37 @@ def test_jax_numpy_eigh(
         ret_np=Q @ np.diag(L) @ Q.T,
         ret_from_gt_np=frontend_Q @ np.diag(frontend_L) @ frontend_Q.T,
         atol=1e-02,
+    )
+
+
+# inv
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=-100,
+        max_value=100,
+        shape=helpers.ints(min_value=1, max_value=10).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: "float16" not in x[0]
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.linalg.inv"
+    ),
+)
+def test_jax_numpy_inv(dtype_and_x, as_variable, native_array, num_positional_args, fw):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        native_array_flags=native_array,
+        num_positional_args=num_positional_args,
+        fw=fw,
+        frontend="jax",
+        fn_tree="numpy.linalg.inv",
+        a=np.asarray(x[0], dtype=dtype[0]),
     )
