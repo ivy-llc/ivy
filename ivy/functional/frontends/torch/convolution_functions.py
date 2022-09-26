c7f47bf9cb23e8bf6d326cc511aeed97387d594c
AnnaTz <anna.tzatzopoulou@gmail.com>
2022-09-26 15:32:06 +0100

    test fold and corrections

--- ivy/functional/frontends/torch/convolution_functions.py
+++ ivy/functional/frontends/torch/convolution_functions.py
@@ -45,18 +45,6 @@ def conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
     return ivy.permute_dims(out, axes=(0, 3, 1, 2))
 
 
-def _expanding_to_2(x):
-    if type(x) == int:
-        x = x * 2
-    elif type(x) == tuple:
-        if len(x) == 1:
-            x = [x] * 2
-        elif len(x) > 2:
-            x = x[:2]
-    x = [int(i) for i in x]
-    return x
-
-
 def _div_rtn(x, y):
     q = x / y
     r = x % y
@@ -67,33 +55,43 @@ def _div_rtn(x, y):
 
 def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
 
-    kernel_size = _expanding_to_2(kernel_size)
-    dilation = _expanding_to_2(dilation)
-    padding = _expanding_to_2(padding)
-    stride = _expanding_to_2(stride)
+    kernel_size = ivy.repeat(ivy.asarray(kernel_size), 2)[:2]
+    dilation = ivy.repeat(ivy.asarray(dilation), 2)[:2]
+    padding = ivy.repeat(ivy.asarray(padding), 2)[:2]
+    stride = ivy.repeat(ivy.asarray(stride), 2)[:2]
 
     kernel_height, kernel_width = kernel_size
     dilation_height, dilation_width = dilation
     pad_height, pad_width = padding
     stride_height, stride_width = stride
 
-    ivy.assertions.check_true(kernel_width > 0 and kernel_height > 0,
-                              message="kernel size should be greater than zero")
-    ivy.assertions.check_true(dilation_width > 0 and dilation_height > 0,
-                              message="dilation should be greater than zero")
-    ivy.assertions.check_true(pad_width > 0 and pad_height > 0,
-                              message="padding should be greater than zero")
-    ivy.assertions.check_true(stride_width > 0 and stride_height > 0,
-                              message="stride should be greater than zero")
+    ivy.assertions.check_true(
+        kernel_width > 0 and kernel_height > 0,
+        message="kernel size should be greater than zero",
+    )
+    ivy.assertions.check_true(
+        dilation_width > 0 and dilation_height > 0,
+        message="dilation should be greater than zero",
+    )
+    ivy.assertions.check_true(
+        pad_width >= 0 and pad_height >= 0, message="padding should be non-negative"
+    )
+    ivy.assertions.check_true(
+        stride_width > 0 and stride_height > 0,
+        message="stride should be greater than zero",
+    )
 
-    ndim = input.dim()
+    input = ivy.asarray(input)
+    ndim = input.ndim
 
     valid_dims = input.shape[1] != 0 and input.shape[2] != 0
-    ivy.assertions.check_true((ndim == 3 and input.shape[0] != 0 and valid_dims)
-                              or (ndim == 4 and valid_dims and input.shape[3] != 0),
-                              message="expected 3D or 4D (batch mode) tensor "
-                              "with possibly 0 batch size "
-                              "and other non-zero dimensions for input")
+    ivy.assertions.check_true(
+        (ndim == 3 and input.shape[0] != 0 and valid_dims)
+        or (ndim == 4 and valid_dims and input.shape[3] != 0),
+        message="expected 3D or 4D (batch mode) tensor "
+        "with possibly 0 batch size "
+        "and other non-zero dimensions for input",
+    )
 
     dim_batch = 0
     if ndim == 3:
@@ -103,23 +101,28 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
     input_width = input.shape[dim_batch + 3]
     output_height = int(
         _div_rtn(
-            input_height + 2 * pad_height
-            - (dilation_height * (kernel_height - 1) + 1),
-            stride_height) + 1)
+            input_height + 2 * pad_height - (dilation_height * (kernel_height - 1) + 1),
+            stride_height,
+        )
+        + 1
+    )
     output_width = int(
         _div_rtn(
-            input_width + 2 * pad_width
-            - (dilation_width * (kernel_width - 1) + 1),
-            stride_width) + 1)
+            input_width + 2 * pad_width - (dilation_width * (kernel_width - 1) + 1),
+            stride_width,
+        )
+        + 1
+    )
 
-    ivy.assertions.check_true(output_width < 1 and output_height < 1,
-                              message="calculated shape of the array "
-                                      "of sliding blocks is non-positive")
+    ivy.assertions.check_true(
+        output_width >= 1 and output_height >= 1,
+        message="calculated shape of the array " "of sliding blocks is non-positive",
+    )
 
     batched_input = True
-    if input.dim() == 3:
+    if input.ndim == 3:
         batched_input = False
-        input = ivy.reshape(input, {1, input.shape[0], input.shape[1], input.shape[2]})
+        input = ivy.reshape(input, (1, input.shape[0], input.shape[1], input.shape[2]))
 
     batch_size = input.shape[0]
     n_input_channels = input.shape[1]
@@ -127,14 +130,18 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
     input_width = input.shape[3]
     output_height = int(
         (input_height + 2 * pad_height - (dilation_height * (kernel_height - 1) + 1))
-        / stride_height + 1)
+        / stride_height
+        + 1
+    )
     output_width = int(
         (input_width + 2 * pad_width - (dilation_width * (kernel_width - 1) + 1))
-        / stride_width + 1)
+        / stride_width
+        + 1
+    )
     n_output_channels = n_input_channels * kernel_width * kernel_height
     output_length = output_height * output_width
 
-    output = ivy.zeros({batch_size, n_output_channels, output_length})
+    output = ivy.zeros((batch_size, n_output_channels, output_length))
 
     height_col = output_height
     width_col = output_width
@@ -146,7 +153,7 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
 
         for c_col in range(channels_col):
             w_offset = c_col % kernel_width
-            h_offset = (c_col / kernel_width) % kernel_height
+            h_offset = int((c_col / kernel_width) % kernel_height)
             c_im = int(c_col / kernel_height / kernel_width)
 
             for h_col in range(height_col):
@@ -155,10 +162,10 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
                 for w_col in range(width_col):
                     w_im = w_col * stride_width - pad_width + w_offset * dilation_width
 
-                    if (h_im >= 0 and h_im < input_height) \
-                            and (w_im >= 0 and w_im < input_width):
-                        data_col[(c_col * height_col + h_col) * width_col + w_col] = \
-                            data_im[(c_im * input_height + h_im) * input_width + w_im]
+                    if (h_im >= 0 and h_im < input_height) and (
+                        w_im >= 0 and w_im < input_width
+                    ):
+                        data_col[h_col, c_col + w_col] = data_im[c_im, h_im, w_im]
 
     if not batched_input:
         output = ivy.squeeze(output, axis=0)
@@ -168,11 +175,11 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
 
 def fold(input, output_size, kernel_size, dilation=1, padding=0, stride=1):
 
-    output_size = _expanding_to_2(output_size)
-    kernel_size = _expanding_to_2(kernel_size)
-    dilation = _expanding_to_2(dilation)
-    padding = _expanding_to_2(padding)
-    stride = _expanding_to_2(stride)
+    output_size = ivy.repeat(ivy.asarray(output_size), 2)[:2]
+    kernel_size = ivy.repeat(ivy.asarray(kernel_size), 2)[:2]
+    dilation = ivy.repeat(ivy.asarray(dilation), 2)[:2]
+    padding = ivy.repeat(ivy.asarray(padding), 2)[:2]
+    stride = ivy.repeat(ivy.asarray(stride), 2)[:2]
 
     output_height, output_width = output_size
     kernel_height, kernel_width = kernel_size
@@ -180,66 +187,91 @@ def fold(input, output_size, kernel_size, dilation=1, padding=0, stride=1):
     pad_height, pad_width = padding
     stride_height, stride_width = stride
 
-    ivy.assertions.check_true(output_width < 1 or output_height < 1,
-                              message="expected output spatial size to be positive")
-    ivy.assertions.check_true(kernel_width > 0 and kernel_height > 0,
-                              message="kernel size should be greater than zero")
-    ivy.assertions.check_true(stride_width > 0 and stride_height > 0,
-                              message="stride should be greater than zero")
-    ivy.assertions.check_true(dilation_width > 0 and dilation_height > 0,
-                              message="dilation should be greater than zero")
+    ivy.assertions.check_true(
+        output_width >= 1 or output_height >= 1,
+        message="expected output spatial size to be positive",
+    )
+    ivy.assertions.check_true(
+        kernel_width > 0 and kernel_height > 0,
+        message="kernel size should be greater than zero",
+    )
+    ivy.assertions.check_true(
+        stride_width > 0 and stride_height > 0,
+        message="stride should be greater than zero",
+    )
+    ivy.assertions.check_true(
+        dilation_width > 0 and dilation_height > 0,
+        message="dilation should be greater than zero",
+    )
 
-    ndim = input.dim()
+    input = ivy.asarray(input)
+    ndim = input.ndim
 
-    ivy.assertions.check_true((ndim == 2 and input.shape[0] and input.shape[1])
-                              or (ndim == 3 and input.shape[1] and input.shape[2]),
-                              message="expected 2D or 3D (batch mode) tensor "
-                                      "with possibly 0 batch size and "
-                                      "non-zero dimensions for input")
+    ivy.assertions.check_true(
+        (ndim == 2 and input.shape[0] and input.shape[1])
+        or (ndim == 3 and input.shape[1] and input.shape[2]),
+        message="expected 2D or 3D (batch mode) tensor "
+        "with possibly 0 batch size and "
+        "non-zero dimensions for input",
+    )
 
     dim_batch = 0
     if ndim == 3:
         dim_batch = -1
 
-    n_input_channels = input.shape(dim_batch + 1)
+    n_input_channels = input.shape[dim_batch + 1]
 
-    ivy.assertions.check_true(n_input_channels % (kernel_width * kernel_height) != 0,
-                              message="expected size of input's dimension 1 to be "
-                                      "divisible by the product of kernel_size")
+    ivy.assertions.check_true(
+        n_input_channels % (kernel_width * kernel_height) != 0,
+        message="expected size of input's dimension 1 to be "
+        "divisible by the product of kernel_size",
+    )
 
     input_length = input.shape[dim_batch + 2]
 
     blocks_height = int(
         _div_rtn(
-            output_height + 2 * pad_height
+            output_height
+            + 2 * pad_height
             - (dilation_height * (kernel_height - 1) - 1),
-            stride_height) + 1)
+            stride_height,
+        )
+        + 1
+    )
     blocks_width = int(
         _div_rtn(
-            output_width + 2 * pad_width
-            - (dilation_width * (kernel_width - 1) - 1),
-            stride_width) + 1)
+            output_width + 2 * pad_width - (dilation_width * (kernel_width - 1) - 1),
+            stride_width,
+        )
+        + 1
+    )
 
-    ivy.assertions.check_true(input_length != (blocks_height * blocks_width),
-                              message="expected size of input's dimension 2 to "
-                                      "match the calculated number of sliding blocks")
+    ivy.assertions.check_true(
+        input_length != (blocks_height * blocks_width),
+        message="expected size of input's dimension 2 to "
+        "match the calculated number of sliding blocks",
+    )
 
     batched_input = True
-    if input.dim() == 2:
+    if input.ndim == 2:
         batched_input = False
-        input = ivy.reshape(input, {1, input.shape[0], input.shape[1]})
+        input = ivy.reshape(input, (1, input.shape[0], input.shape[1]))
 
     batch_size = input.shape[0]
     n_output_channels = int(n_input_channels / (kernel_width * kernel_height))
 
-    output = ivy.zeros({batch_size, n_output_channels, output_height, output_width})
+    output = ivy.zeros((batch_size, n_output_channels, output_height, output_width))
 
     height_col = int(
         (output_height + 2 * pad_height - (dilation_height * (kernel_height - 1) + 1))
-        / stride_height + 1)
+        / stride_height
+        + 1
+    )
     width_col = int(
         (output_width + 2 * pad_width - (dilation_width * (kernel_width - 1) + 1))
-        / stride_width + 1)
+        / stride_width
+        + 1
+    )
     channels_col = n_output_channels * kernel_height * kernel_width
 
     for elt in range(batch_size):
@@ -248,7 +280,7 @@ def fold(input, output_size, kernel_size, dilation=1, padding=0, stride=1):
 
         for c_col in range(channels_col):
             w_offset = c_col % kernel_width
-            h_offset = (c_col / kernel_width) % kernel_height
+            h_offset = int((c_col / kernel_width) % kernel_height)
             c_im = int(c_col / kernel_height / kernel_width)
 
             for h_col in range(height_col):
@@ -257,11 +289,10 @@ def fold(input, output_size, kernel_size, dilation=1, padding=0, stride=1):
                 for w_col in range(width_col):
                     w_im = w_col * stride_width - pad_width + w_offset * dilation_width
 
-                    if (h_im >= 0 and h_im < output_height) \
-                            and (w_im >= 0 and w_im < output_width):
-                        data_im[(c_im * output_height + h_im) * output_width + w_im] \
-                            += \
-                            data_col[(c_col * height_col + h_col) * width_col + w_col]
+                    if (h_im >= 0 and h_im < output_height) and (
+                        w_im >= 0 and w_im < output_width
+                    ):
+                        data_im[c_im, h_im, w_im] += data_col[h_col, c_col + w_col]
 
     if not batched_input:
         output = ivy.squeeze(output, axis=0)
--- ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
@@ -1,6 +1,6 @@
 # global
 import random
-from hypothesis import given, assume, strategies as st
+from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -81,15 +81,15 @@ def test_torch_conv2d_2(input, weight, bias, stride, dilation, padding, fw):
 
 
 @st.composite
-def _int_or_tuple(draw):
+def _int_or_tuple(draw, min_val, max_val):
     val = draw(
         random.choice(
             [
-                st.integers(min_value=0, max_value=255),
-                st.tuples(st.integers(min_value=0, max_value=255)),
+                st.integers(min_val, max_val),
+                st.tuples(st.integers(min_val, max_val)),
                 st.tuples(
-                    st.integers(min_value=0, max_value=255),
-                    st.integers(min_value=0, max_value=255),
+                    st.integers(min_val, max_val),
+                    st.integers(min_val, max_val),
                 ),
             ]
         )
@@ -99,23 +99,20 @@ def _int_or_tuple(draw):
 
 @handle_cmd_line_args
 @given(
-    dtype_and_input_shape=helpers.dtype_and_values(
+    dtype_and_input_and_shape=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=3,
-        max_num_dims=4,
-        min_dim_size=0,
-        ret_shape=True,
+        shape=(1, 3, 6, 6),
     ),
-    kernel_size=_int_or_tuple(),
-    dilation=_int_or_tuple(),
-    padding=_int_or_tuple(),
-    stride=_int_or_tuple(),
+    kernel_size=_int_or_tuple(2, 5),
+    dilation=_int_or_tuple(1, 3),
+    padding=_int_or_tuple(0, 2),
+    stride=_int_or_tuple(1, 3),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.torch.unfold"
     ),
 )
 def test_torch_unfold(
-    dtype_and_input_shape,
+    dtype_and_input_and_shape,
     kernel_size,
     dilation,
     padding,
@@ -125,13 +122,7 @@ def test_torch_unfold(
     native_array,
     fw,
 ):
-    args_dtypes = list([dtype_and_input_shape[0]] + ["uint8"] * 4)
-    input_shape = dtype_and_input_shape[2]
-    input_ndims = len(input_shape)
-    if input_ndims == 3:
-        assume(input_shape[0] and input_shape[1] and input_shape[2])
-    elif input_ndims == 4:
-        assume(input_shape[1] and input_shape[2] and input_shape[3])
+    args_dtypes = list([dtype_and_input_and_shape[0]] + ["uint8"] * 4)
     helpers.test_frontend_function(
         input_dtypes=args_dtypes,
         as_variable_flags=as_variable,
@@ -141,7 +132,53 @@ def test_torch_unfold(
         fw=fw,
         frontend="torch",
         fn_tree="nn.functional.unfold",
-        input=dtype_and_input_shape[1],
+        input=dtype_and_input_and_shape[1],
+        kernel_size=kernel_size,
+        dilation=dilation,
+        padding=padding,
+        stride=stride,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_input_and_shape=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=(1, 12, 12),
+    ),
+    output_size=_int_or_tuple(3, 5),
+    kernel_size=_int_or_tuple(2, 5),
+    dilation=_int_or_tuple(1, 3),
+    padding=_int_or_tuple(0, 2),
+    stride=_int_or_tuple(1, 3),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.fold"
+    ),
+)
+def test_torch_fold(
+    dtype_and_input_and_shape,
+    output_size,
+    kernel_size,
+    dilation,
+    padding,
+    stride,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    args_dtypes = list([dtype_and_input_and_shape[0]] + ["uint8"] * 5)
+    helpers.test_frontend_function(
+        input_dtypes=args_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.fold",
+        input=dtype_and_input_and_shape[1],
+        output_size=output_size,
         kernel_size=kernel_size,
         dilation=dilation,
         padding=padding,
