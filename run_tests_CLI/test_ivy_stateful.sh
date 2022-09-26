b80a86d046f7be3dea8c265eaf03513853ae2855
Aarsh2001 <ac5973@srmist.edu.in>
2022-09-26 12:54:10 +0530

    refactor print to stdout

--- run_tests_CLI/test_ivy_stateful.sh
+++ run_tests_CLI/test_ivy_stateful.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --backend "$1" ivy_tests/test_ivy/test_stateful/"$2".py
-export BACKEND = $1 SUBMODULE = $2
+BACKEND=$1
+SUBMODULE=$2
+export BACKEND SUBMODULE
