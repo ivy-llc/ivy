1965e73090e3acffbe33a746d0df73e5c2e560cf
Matt <matthew-barrett@hotmail.co.uk>
2022-09-26 17:10:59 +0100

    fix inconsistent docstrings

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -120,7 +120,8 @@ def inv(
         else:
             cofactor = jnp.transpose(jnp.linalg.inv(x)) * jnp.linalg.det(x)
             inverse = jnp.multiply(
-                jnp.divide(1, jnp.linalg.det(x)), jnp.transpose(cofactor))
+                jnp.divide(1, jnp.linalg.det(x)), jnp.transpose(cofactor)
+            )
             ret = inverse
             return ret
 
@@ -138,7 +139,7 @@ def matmul(
     *,
     transpose_a: bool = False,
     transpose_b: bool = False,
-    out: Optional[JaxArray] = None
+    out: Optional[JaxArray] = None,
 ) -> JaxArray:
     if transpose_a is True:
         x1 = jnp.transpose(x1)
