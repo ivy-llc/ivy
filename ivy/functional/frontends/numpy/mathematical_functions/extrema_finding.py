6a8f80b7222345dbfa2a96cd9b3d69d11a31fd2b
Deeptendu <deeptendu.santra@protonmail.com>
2022-09-27 18:20:20 -0400

    Remove wrong out usage

--- ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
@@ -63,11 +63,11 @@ def nanmin(
     bound_max = ivy.finfo(a).max if initial is None else initial
     nan_mask = ivy.isnan(a)
     a = ivy.where(
-        ivy.logical_not(nan_mask), a, ivy.default(out, a.full_like(bound_max)), out=out
+        ivy.logical_not(nan_mask), a, ivy.default(out, a.full_like(bound_max))
     )
     if initial is not None:
         if ivy.is_array(where):
-            a = ivy.where(where, a, ivy.default(out, a.full_like(initial)), out=out)
+            a = ivy.where(where, a, ivy.default(out, a.full_like(initial)))
             nan_mask = ivy.where(where, nan_mask, nan_mask.full_like(True))
         s = ivy.shape(a, as_array=True)
         if axis is not None:
