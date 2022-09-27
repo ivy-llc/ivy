bffb46c447dd34bd1c878298239c1caab3ad14f5
faseehahmed <54709992+faseehahmed26@users.noreply.github.com>
2022-09-27 11:19:57 +0530

    Made Changes to Utilities.py file

---
+++ ivy/functional/frontends/torch/utilities.py
@@ -0,0 +1,17 @@
+# global
+import torch
+
+# local
+import ivy
+
+class Utilities:
+
+    def result_type(arg1,arg2):
+        return torch.result_type(arg1,arg2)
+        
+    def  can_cast(dtype1,dtype2):
+        return torch.can_cast(dtype1,dtype2)
+
+
+    def  _assert(condition,message):
+        return torch._assert(condition,message)
