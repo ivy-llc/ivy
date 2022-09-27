5e3a47081332c0fd36dc30605d4cd3c1436b3ed6
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-27 13:30:56 +0800

    fix min_value in data generation

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -1401,7 +1401,7 @@ def _get_clip_inputs(draw):
         min = None
         max = draw(
             helpers.array_values(
-                dtype=x_dtype[0], shape=shape, min_value=0, max_value=50
+                dtype=x_dtype[0], shape=shape, min_value=5, max_value=50
             )
         )
     return x_dtype, x, min, max
