bb89af8e8334f2ec570c61fbb3dc5469957f0a0a
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-09-27 16:46:23 +0530

    merge conflict

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_comparison.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_comparison.py
@@ -1,6 +1,7 @@
 # global
+from hypothesis import given
+from hypothesis import strategies as st
 import numpy as np
-from hypothesis import given, strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
