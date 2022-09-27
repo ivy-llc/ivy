840fb8d362435258c4c7544f2676d502462725b9
Martin Oywa <martinoywa2@gmail.com>
2022-09-27 10:07:20 +0000

    Automatic linting

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_matrix/test_methods.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_matrix/test_methods.py
@@ -94,3 +94,43 @@ def test_numpy_matrix_any(
         class_name="matrix",
         method_name="any",
     )
+
+
+# transpose
+@handle_cmd_line_args
+@given(
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=2,
+        max_num_dims=2,
+        min_dim_size=2,
+    ),
+)
+def test_numpy_transpose(
+    dtype_x_axis,
+    as_variable,
+    native_array,
+    fw,
+):
+    input_dtype, x, axis = dtype_x_axis
+    helpers.test_frontend_method(
+        input_dtypes_init=input_dtype,
+        as_variable_flags_init=as_variable,
+        num_positional_args_init=0,
+        native_array_flags_init=native_array,
+        all_as_kwargs_np_init={
+            "data": x[0],
+            "dtype": input_dtype[0],
+        },
+        input_dtypes_method=[],
+        as_variable_flags_method=as_variable,
+        all_as_kwargs_np_method={
+            "axis": axis,
+        },
+        num_positional_args_method=0,
+        native_array_flags_method=native_array,
+        fw=fw,
+        frontend="numpy",
+        class_name="matrix",
+        method_name="transpose",
+    )
