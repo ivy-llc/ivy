bd316b06f0100426aebc98140c1aac271e4efca2
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-09-27 17:41:34 +0100

    Update test_miscellaneous_ops.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -730,3 +730,42 @@ def test_torch_repeat_interleave(
         dim=dim,
         output_size=output_size,
     )
+
+    
+# cross
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True
+    ),
+    dim=st.integers(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.cross"
+    ),
+)
+def test_torch_cross(
+    dtype_and_x,
+    dim,
+    as_variable,
+    num_positional_args,
+    native_array,
+    with_out,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=True,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="cross",
+        input=np.asarray(x[0], dtype=input_dtype[0]),
+        other=np.asarray(x[1], dtype=input_dtype[1]),
+        dim=dim,
+        out=None,
+    )    
