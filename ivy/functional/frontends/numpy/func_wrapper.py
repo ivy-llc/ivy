20153c3671f1e9b9aa105d10351f97955bc88afd
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-27 12:07:27 +0800

    second fix

--- ivy/functional/frontends/numpy/func_wrapper.py
+++ ivy/functional/frontends/numpy/func_wrapper.py
@@ -54,7 +54,7 @@ def handle_numpy_casting(fn: Callable) -> Callable:
                     message="casting is 'no', dtype must be the same as input types",
                 )
         elif casting == "safe":
-            # TODO: check!
+            # TODO: check and fix!
             promoted_type = ivy.promote_types(ivy.dtype(args[0]), ivy.dtype(args[1]))
             if ivy.exists(dtype):
                 ivy.assertions.check_true(
@@ -64,12 +64,19 @@ def handle_numpy_casting(fn: Callable) -> Callable:
                     ),
                 )
                 promoted_type = dtype
-            args[0] = ivy.array(args[0], dtype=promoted_type)
-            args[1] = ivy.array(args[1], dtype=promoted_type)
+            args[0] = ivy.astype(args[0], promoted_type)
+            args[1] = ivy.astype(args[1], promoted_type)
         elif casting == "same_kind":
             # TODO: implement!
-            if _is_same_kind(ivy.dtype(args[0]), ivy.dtype(args[1])):
-                ivy.assertions.check_true(_is_same_kind())
+            if ivy.exists(dtype):
+                pass
+                # if _is_same_kind(ivy.dtype(args[0]), dtype)
+            else:
+                promoted_type = ivy.promote_types(
+                    ivy.dtype(args[0]), ivy.dtype(args[1])
+                )
+            args[0] = ivy.astype(args[0], promoted_type)
+            args[1] = ivy.astype(args[1], promoted_type)
         else:
             pass  # unsafe
         return fn(*args, **kwargs)
