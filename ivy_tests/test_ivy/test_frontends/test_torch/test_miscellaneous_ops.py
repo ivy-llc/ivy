91058df4e30802aee8779ed2ea66a5e0ba7f00e7
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-09-28 20:17:28 +0100

    Update test_miscellaneous_ops.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -767,3 +767,35 @@ def test_torch_repeat_interleave(
         dim=axis,
         output_size=output_size,
     )
+
+
+# ravel
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=1,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.ravel"
+    ),
+)
+def test_torch_ravel(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="ravel",
+        input=np.asarray(x, dtype=input_dtype),
+    )
