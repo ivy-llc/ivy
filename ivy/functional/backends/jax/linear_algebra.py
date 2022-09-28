69bc6270bd120f4eac68a52a56daab81600b732b
Matt <matthew-barrett@hotmail.co.uk>
2022-09-28 18:45:46 +0100

    fix formatting

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -136,7 +136,7 @@ def matmul(
     *,
     transpose_a: bool = False,
     transpose_b: bool = False,
-    out: Optional[JaxArray] = None
+    out: Optional[JaxArray] = None,
 ) -> JaxArray:
     if transpose_a is True:
         x1 = jnp.transpose(x1)
