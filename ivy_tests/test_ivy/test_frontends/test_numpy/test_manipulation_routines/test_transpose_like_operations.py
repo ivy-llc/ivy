594722926a96aa6811136d6874c74eb454504357
ddzhulaii <daryna.dzhulay@gmail.com>
2022-09-28 10:41:20 +0200

    add numpy.ndarray.transpose frontend (#4322)
    Co-authored-by: unevilicorn <unevilicorn@gmail.com>
    Co-authored-by: Oxbeefbabe <ludwignemiro@yahoo.com>

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_transpose_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_transpose_like_operations.py
@@ -5,8 +5,43 @@ from hypothesis import given, strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 
 
+# transpose
+@handle_cmd_line_args
+@given(
+    array_and_axes=np_frontend_helpers._array_and_axes_permute_helper(
+        min_num_dims=2, max_num_dims=5,
+        min_dim_size=2, max_dim_size=10,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.transpose"
+    ),
+)
+def test_numpy_transpose(
+    array_and_axes,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    array, dtype, axes = array_and_axes
+    helpers.test_frontend_function(
+        input_dtypes=[dtype],
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="transpose",
+        array=np.array(array, dtype=dtype),
+        axes=axes,
+    )
+
+
+# swapaxes
 @st.composite
 def st_dtype_arr_and_axes(draw):
     dtypes, xs, x_shape = draw(
