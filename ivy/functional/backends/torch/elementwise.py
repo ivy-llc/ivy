01facb98c4ca4524f181cb4390ccef657b4516d6
ZoeCD <42352932+ZoeCD@users.noreply.github.com>
2022-09-29 16:52:34 -0500

    fix bitwise right shift

--- ivy/functional/backends/torch/elementwise.py
+++ ivy/functional/backends/torch/elementwise.py
@@ -619,7 +619,6 @@ def bitwise_right_shift(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     x1, x2 = ivy.promote_types_of_inputs(x1, x2, array_api_promotion=True)
-    ivy.assertions.check_all(x2 >= 0, message="shifts must be non-negative")
     x2 = torch.clamp(x2, min=0, max=torch.iinfo(x2.dtype).bits - 1)
     return torch.bitwise_right_shift(x1, x2, out=out)
 
