be741c4eba0323cf99db5b1201200349d8c696bd
Deeptendu Santra <55111154+Dsantra92@users.noreply.github.com>
2022-09-29 05:53:38 -0400

    Add nanmin to numpy frontend (#4896)
    Co-authored-by: guillesanbri <guillesanbri@hotmail.com>

--- ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
@@ -33,7 +33,6 @@ def amin(
     initial=None,
     where=True,
 ):
-
     if initial is not None:
         s = ivy.shape(a, as_array=True)
         ax = axis
@@ -51,3 +50,41 @@ def amin(
         a = ivy.concat([a, header], axis=axis)
 
     return ivy.min(a, axis=axis, keepdims=keepdims, out=out)
+
+
+def nanmin(
+    a,
+    axis=None,
+    out=None,
+    keepdims=False,
+    initial=None,
+    where=True,
+):
+    nan_mask = ivy.isnan(a)
+    a = ivy.where(ivy.logical_not(nan_mask), a, a.full_like(+ivy.inf))
+    where_mask = None
+    if initial is not None:
+        if ivy.is_array(where):
+            a = ivy.where(where, a, a.full_like(initial))
+            where_mask = ivy.all(ivy.logical_not(where), axis=axis, keepdims=keepdims)
+        s = ivy.shape(a, as_array=True)
+        if axis is not None:
+            if isinstance(axis, (tuple, list)) or ivy.is_array(axis):
+                # introducing the initial in one dimension is enough
+                ax = axis[0] % len(s)
+                s[ax] = 1
+            else:
+                ax = axis % len(s)
+                s[ax] = 1
+        header = ivy.full(ivy.Shape(s.to_list()), initial, dtype=ivy.dtype(a))
+        if axis:
+            if isinstance(axis, (tuple, list)) or ivy.is_array(axis):
+                a = ivy.concat([a, header], axis=axis[0])
+            else:
+                a = ivy.concat([a, header], axis=axis)
+        else:
+            a = ivy.concat([a, header], axis=0)
+    res = ivy.min(a, axis=axis, keepdims=keepdims, out=out)
+    if where_mask is not None and ivy.any(where_mask):
+        res = ivy.where(ivy.logical_not(where_mask), res, ivy.nan, out=out)
+    return res
