0348be6baaf060e81078a2ff0d2fbc085f85842e
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-09-29 12:56:17 +0100

    Update test_jax_numpy_linalg.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -128,3 +128,44 @@ def test_jax_numpy_inv(dtype_and_x, as_variable, native_array, num_positional_ar
         fn_tree="numpy.linalg.inv",
         a=np.asarray(x[0], dtype=dtype[0]),
     )
+
+
+# slogdet
+@st.composite
+def square_shape(draw, dims=st.integers(min_value=2, max_value=20)):
+    shape = draw(dims)
+    return (shape, shape)
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        max_value=1e4,
+        min_value=-1e4,
+        shape=square_shape(),
+        num_arrays=1,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.linalg.slogdet"
+    ),
+)
+def test_jax_slogdet(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="jax",
+        fn_tree="numpy.linalg.slogdet",
+        a=np.asarray(x, dtype=input_dtype),
+    )
