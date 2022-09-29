a54f09c3bf822f80226f8ec13f9891a8727fa6b1
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-29 14:22:53 +0000

    Added gradient testing to acosh

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -78,12 +78,12 @@ def test_acosh(
     input_dtype, x = dtype_and_x
     helpers.test_function(
         input_dtypes=input_dtype,
-        as_variable_flags=[True],
+        as_variable_flags=as_variable,
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        container_flags=[False],
-        instance_method=False,
+        container_flags=container,
+        instance_method=instance_method,
         fw=fw,
         fn_name="acosh",
         rtol_=1e-2,
