25f5a41db91a6466e8b315085e6570d0a84bac61
amine_omairi <elomairiamine@gmail.com>
2022-09-29 13:08:54 +0000

    correct example

--- ivy/container/layers.py
+++ ivy/container/layers.py
@@ -60,16 +60,16 @@ class ContainerWithLayers(ContainerBase):
         Examples
         --------
         >>> x = ivy.Container(a=ivy.array([[1.1, 2.2, 3.3], [11, 22, 33]]), \
-            b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
+        b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
         >>> w = ivy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         >>> b = ivy.array([1, 0, -1])
         >>> y = ivy.Container.static_linear(x, w, bias=b)
         >>> print(y)
         {
-            a: ivy.array([[16.4, 35.2, 54.], 
-                      [155., 352., 549.]]),
-            b: ivy.array([[15.1, 31., 46.9], 
-                      [85., 195., 305.]])
+        a: ivy.array([[16.4, 35.2, 54.], \
+        [155., 352., 549.]]), \
+        b: ivy.array([[15.1, 31., 46.9], \
+        [85., 195., 305.]])
         }
 
         """
@@ -134,16 +134,16 @@ class ContainerWithLayers(ContainerBase):
         Examples
         --------
         >>> x = ivy.Container(a=ivy.array([[1.1, 2.2, 3.3], [11, 22, 33]]), \
-            b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
+        b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
         >>> w = ivy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         >>> b = ivy.array([1, 0, -1])
         >>> y = x.linear(w, bias=b)
         >>> print(y)
         {
-            a: ivy.array([[16.4, 35.2, 54.], 
-                      [155., 352., 549.]]),
-            b: ivy.array([[15.1, 31., 46.9], 
-                      [85., 195., 305.]])
+        a: ivy.array([[16.4, 35.2, 54.], \
+        [155., 352., 549.]]), \
+        b: ivy.array([[15.1, 31., 46.9], \
+        [85., 195., 305.]])
         }
         
         """
--- ivy/functional/ivy/layers.py
+++ ivy/functional/ivy/layers.py
@@ -70,49 +70,50 @@ def linear(
     >>> x = ivy.array([[0.666, -0.4269, 1.911]])
     >>> w = ivy.array([[1, 0, 0], [0, 0, 1]])
     >>> y = ivy.zeros(2)
+    >>> ivy.linear(x, w, out=y)
     >>> print(y)
     ivy.array([[0.666, 1.91 ]])
 
     >>> x = ivy.array([[1.546, 5.234, 6.487], \
-        [0.157, 5.753, 4.52], \
-        [5.165, 3.159, 7.101]])
+    [0.157, 5.753, 4.52], \
+    [5.165, 3.159, 7.101]])
     >>> w = ivy.array([[1.545, 2.547, 3.124], \
-        [5.852, 8.753, 6.963]])   
+    [5.852, 8.753, 6.963]])   
     >>> b = ivy.array([-1, 1])
     >>> ivy.linear(x, w, bias=b, out=x)
     >>> print(x)
-    ivy.array([[ 35. , 101. ],
-       [ 28. ,  83.7],
-       [ 37.2, 108. ]])
+    ivy.array([[ 35. , 101. ], \
+    [ 28. ,  83.7], \
+    [ 37.2, 108. ]])
         
     With :class:`ivy.Container` input:
 
     >>> x = ivy.Container(a=ivy.array([[1, 2, 3], [4, 5, 6]]), \
-        b=ivy.array([1.1, 2.2, 3.3]))
+    b=ivy.array([1.1, 2.2, 3.3]))
     >>> w = ivy.Container(a=ivy.array([[1, 2, 3], [-1, 1, 2]]), \
-        b=ivy.array([[0, -1, 1], [0, 1, 1]]))
+    b=ivy.array([[0, -1, 1], [0, 1, 1]]))
     >>> b = ivy.Container(a=ivy.array([1, -1]), b=ivy.array([1, 1]))
     >>> y = ivy.linear(x, w, bias=b)
     >>> print(y)
     {
-        a: ivy.array([[15, 6], 
-                      [33, 12]]),
-        b: ivy.array([2.1, 6.5])
+    a: ivy.array([[15, 6], \
+    [33, 12]]), \
+    b: ivy.array([2.1, 6.5])
     }
 
     With a mix of :class:`ivy.Array` and :class:`ivy.Container` inputs:
 
     >>> x = ivy.Container(a=ivy.array([[1.1, 2.2, 3.3], [11, 22, 33]]), \
-        b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
+    b=ivy.array([[1.245, 0.278, 4.105], [7, 13, 17]]))
     >>> w = ivy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     >>> b = ivy.Container(a=ivy.array([1, 0, -1]), b=ivy.array([1, 1, 0]))
     >>> ivy.linear(x, w, bias=b, out=x)
     >>> print(x)
     {
-        a: ivy.array([[16.4, 35.2, 54.], 
-                      [155., 352., 549.]]),
-        b: ivy.array([[15.1, 32., 47.9], 
-                      [85., 196., 306.]])
+    a: ivy.array([[16.4, 35.2, 54.], \
+    [155., 352., 549.]]), \
+    b: ivy.array([[15.1, 32., 47.9], \
+    [85., 196., 306.]])
     }
     
     """
