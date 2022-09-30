5c2127343e394f9f2e0f17578684df29a15ff608
Jamie Line <60134692+JamieLine@users.noreply.github.com>
2022-09-30 03:14:29 +0000

    Added ivy diag and Tensorflow implementation

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -63,6 +63,33 @@ det.unsupported_dtypes = (
     "bfloat16",
 )
 
+def diag(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    offset: Optional[int] = 0,
+    padding_value: Optional[float] = 0,
+    align: Optional[str] = "RIGHT_LEFT",
+    num_rows: Optional[int] = None,
+    num_cols: Optional[int] = None,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+):
+    if num_rows is None:
+        num_rows = -1
+    if num_cols is None:
+        num_cols = -1
+
+    return tf.linalg.diag(
+        x,
+        name='diag',
+        k=offset,
+        num_rows=num_rows,
+        num_cols=num_rows,
+        padding_value=padding_value,
+        align=align,
+    )
+
+
 
 def diagonal(
     x: Union[tf.Tensor, tf.Variable],
