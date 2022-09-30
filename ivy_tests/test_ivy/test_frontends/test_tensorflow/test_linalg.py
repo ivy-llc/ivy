1bbfe302830d725432d657b07c436b550e26c71f
atlasrule <beko757@gmail.com>
2022-09-30 10:35:07 +0300

    Add test for logdet

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -196,6 +196,36 @@ def test_tensorflow_solve(
     )
 
 
+# logdet
+@handle_cmd_line_args
+@given(
+    dtype_and_x=_get_dtype_and_matrix(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.logdet"
+    ),
+)
+def test_tensorflow_logdet(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="linalg.logdet",
+        input=x,
+    )
+
+
 # slogdet
 @handle_cmd_line_args
 @given(
