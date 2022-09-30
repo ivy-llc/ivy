f20ac93ab2ea1bd61e5933848d7475a1d77427b0
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-09-30 19:26:42 +0530

    Add dropout for tensorflow frontend (#4476)
    Co-authored-by: VedPatwardhan <vedpat3@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -617,3 +617,42 @@ def test_tensorflow_batch_normalization(
         scale=scale[0],
         variance_epsilon=1e-7,
     )
+
+    
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        shape=(3, 5),
+    ),
+    prob=helpers.array_values(dtype=ivy.float16, shape=(3, 5), min_value=0),
+    scale=helpers.array_values(dtype=ivy.float16, shape=(3, 5), min_value=0),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.dropout"
+    ),
+)
+def test_tensorflow_dropout(
+    dtype_and_x,
+    prob, 
+    scale, 
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=[input_dtype] * 2,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="nn.dropout",
+        x=x[0],
+        prob=prob,
+        scale=scale,
+    )
+    
