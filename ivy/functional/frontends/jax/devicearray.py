5b1780e6263913cf80a429092ad682cde8d52c9d
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-30 20:44:03 +0100

    fix pos test

--- ivy/functional/frontends/jax/devicearray.py
+++ ivy/functional/frontends/jax/devicearray.py
@@ -1,13 +1,34 @@
+# global
+import functools
+
+
 # local
 import ivy
 import ivy.functional.frontends.jax as jax_frontend
 
 
+def _frontend_wrapper(f):
+    @functools.wraps(f)
+    def decor(self, *args, **kwargs):
+        args = list(args)
+        if isinstance(self, jax_frontend.DeviceArray):
+            i = 0
+            for arg in args:
+                if isinstance(arg, jax_frontend.DeviceArray):
+                    args[i] = arg.data
+                i += 1
+            for u, v in kwargs:
+                if isinstance(v, jax_frontend.DeviceArray):
+                    kwargs[u] = v.data
+            return f(self, *args, **kwargs)
+        return getattr(self, f.__name__)(*args, **kwargs)
+
+    return decor
+
+
 class DeviceArray:
     def __init__(self, data):
-        if ivy.is_native_array(data):
-            data = ivy.Array(data)
-        self.data = data
+        self.data = ivy.array(data)
 
     # Instance Methods #
     # ---------------- #
@@ -21,29 +42,38 @@ class DeviceArray:
     # Special Methods #
     # --------------- #
 
+    @_frontend_wrapper
     def __pos__(self):
         return ivy.positive(self.data)
 
+    @_frontend_wrapper
     def __neg__(self):
         return jax_frontend.neg(self.data)
 
+    @_frontend_wrapper
     def __eq__(self, other):
         return jax_frontend.eq(self.data, other)
 
+    @_frontend_wrapper
     def __ne__(self, other):
         return jax_frontend.ne(self.data, other)
 
+    @_frontend_wrapper
     def __lt__(self, other):
         return jax_frontend.lt(self.data, other)
 
+    @_frontend_wrapper
     def __le__(self, other):
         return jax_frontend.le(self.data, other)
 
+    @_frontend_wrapper
     def __gt__(self, other):
         return jax_frontend.gt(self.data, other)
 
+    @_frontend_wrapper
     def __ge__(self, other):
         return jax_frontend.ge(self.data, other)
 
+    @_frontend_wrapper
     def __abs__(self):
         return jax_frontend.abs(self.data)
--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
@@ -2,6 +2,7 @@
 from hypothesis import given, strategies as st
 
 # local
+import jax.numpy as jnp
 from ivy.functional.frontends.jax.devicearray import DeviceArray
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
@@ -95,29 +96,19 @@ def test_jax_instance_add(
 
 # __pos__
 @handle_cmd_line_args
-@given(dtype_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("numeric")))
+@given(dtype_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")))
 def test_jax_special_pos(
     dtype_x,
-    as_variable,
-    native_array,
     fw,
 ):
     input_dtype, x = dtype_x
-    helpers.test_frontend_method(
-        input_dtypes_init=input_dtype,
-        as_variable_flags_init=as_variable,
-        num_positional_args_init=1,
-        native_array_flags_init=native_array,
-        all_as_kwargs_np_init={
-            "data": x[0],
-        },
-        input_dtypes_method=[],
-        as_variable_flags_method=[],
-        num_positional_args_method=0,
-        native_array_flags_method=[],
-        all_as_kwargs_np_method={},
-        fw=fw,
-        frontend="jax",
-        class_name="DeviceArray",
-        method_name="__pos__",
-    )
+    ret = +DeviceArray(x[0])
+    ret_gt = +jnp.array(x[0], dtype=input_dtype[0])
+    ret = helpers.flatten_and_to_np(ret=ret)
+    ret_gt = helpers.flatten_and_to_np(ret=ret_gt)
+    for (u, v) in zip(ret, ret_gt):
+        helpers.value_test(
+            ret=ret,
+            ret_from_gt=ret_gt,
+            ground_truth_backend="jax",
+        )
