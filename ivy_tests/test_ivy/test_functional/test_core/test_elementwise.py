58764d8d38c680f923abb5e6e5d587b7e3d8ecef
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-30 02:29:14 +0000

    Added gradient testing to atan2

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -317,14 +317,15 @@ def test_atan2(
     assume(not (np.any(np.isclose(x[0], 0)) or np.any(np.isclose(x[1], 0))))
     helpers.test_function(
         input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
+        as_variable_flags=[True],
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        container_flags=container,
-        instance_method=instance_method,
+        container_flags=[False],
+        instance_method=False,
         fw=fw,
         fn_name="atan2",
+        test_gradients=True,
         x1=x[0],
         x2=x[1],
     )
