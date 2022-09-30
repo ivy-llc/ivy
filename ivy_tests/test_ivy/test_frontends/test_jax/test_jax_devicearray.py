58919bf184f2aed346e9257f91cbe25eb1936c43
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-09-30 20:46:29 +0100

    neg test

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
@@ -112,3 +112,23 @@ def test_jax_special_pos(
             ret_from_gt=ret_gt,
             ground_truth_backend="jax",
         )
+
+
+# __neg__
+@handle_cmd_line_args
+@given(dtype_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("numeric")))
+def test_jax_special_neg(
+    dtype_x,
+    fw,
+):
+    input_dtype, x = dtype_x
+    ret = -DeviceArray(x[0])
+    ret_gt = -jnp.array(x[0], dtype=input_dtype[0])
+    ret = helpers.flatten_and_to_np(ret=ret)
+    ret_gt = helpers.flatten_and_to_np(ret=ret_gt)
+    for (u, v) in zip(ret, ret_gt):
+        helpers.value_test(
+            ret=ret,
+            ret_from_gt=ret_gt,
+            ground_truth_backend="jax",
+        )
