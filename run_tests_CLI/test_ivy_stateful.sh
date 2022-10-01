61a391c19b9015bf85b16778ec34827e4a62b3a6
Aarsh2001 <ac5973@srmist.edu.in>
2022-10-01 10:52:49 +0530

    added env variable for redis to shell script

--- run_tests_CLI/test_ivy_stateful.sh
+++ run_tests_CLI/test_ivy_stateful.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -e
+export REDIS_URL=$3 REDIS_PASSWD=$4
 docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --backend "$1" ivy_tests/test_ivy/test_stateful/"$2".py
