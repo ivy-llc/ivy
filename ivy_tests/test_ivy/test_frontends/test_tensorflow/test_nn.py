3d9e22eb554be7151e26c4d642fb331ded65eb2e
hmahmood24 <hmahmood.bee16@gmail.com>
2022-10-02 02:24:13 +0500

    frontends.tensorflow.nn: add support for sigmoid_cross_entropy_with_logits

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -635,3 +635,44 @@ def test_tensorflow_dropout(
         prob=prob,
         scale=scale,
     )
+
+
+# sigmoid_cross_entropy_with_logits
+@handle_cmd_line_args
+@given(
+    dtype_labels_logits = helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        min_value=0,
+        max_value=1,
+        min_num_dims=1,
+        shared_dtype=True
+    ),
+    as_variable=helpers.array_bools(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.nn.sigmoid_cross_entropy_with_logits"
+    ),
+    native_array=helpers.array_bools(),
+)
+def test_tensorflow_sigmoid_cross_entropy_with_logits(
+    dtype_labels_logits,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input_values = dtype_labels_logits
+    labels, logits = input_values
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        with_inplace=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="nn.sigmoid_cross_entropy_with_logits",
+        labels=labels,
+        logits=logits
+    )
