3bdbda8a0b8c1a56f0fe3be8ddadabf1e70acd71
Aarsh Chaube <65212523+Aarsh2001@users.noreply.github.com>
2022-10-01 11:15:43 +0530

    Update test_ivy_stateful.sh

--- run_tests_CLI/test_ivy_stateful.sh
+++ run_tests_CLI/test_ivy_stateful.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 export REDIS_URL="$3"
 export REDIS_PASSWD="$4"
+echo $REDIS_URL
 docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --backend "$1" ivy_tests/test_ivy/test_stateful/"$2".py
