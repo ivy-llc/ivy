ec2abf2eefaed0824b6791ec4e218eab43516617
hmahmood24 <hmahmood.bee16@gmail.com>
2022-10-03 00:37:05 +0500

    frontends.tensorflow.nn: add supported for weighted ce with logits

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -127,3 +127,31 @@ def batch_normalization(x, mean, variance, offset, scale, variance_epsilon, name
 
 def dropout(x, prob, scale, dtype, name=None):
     return ivy.dropout(x, prob, scale, dtype)
+
+
+def weighted_cross_entropy_with_logits(
+    labels=None, logits=None, pos_weight=1, name=None
+):
+    ivy.assertions.check_shape(labels, logits)
+    ones = ivy.ones_like(labels)
+    zeros = ivy.zeros_like(logits)
+    log_weight = ivy.add(ones, ivy.multiply(pos_weight - 1, labels))
+    ones_minus_labels = ivy.subtract(ones, labels)
+    first_term = ivy.multiply(ones_minus_labels, logits)
+
+    max_neg_logits = ivy.where(
+        ivy.negative(logits) >= zeros, ivy.negative(logits), zeros
+    )
+    neg_abs_logits = ivy.negative(ivy.abs(logits))
+    log_neg_abs_logits = ivy.log1p(ivy.exp(neg_abs_logits))
+    second_term = ivy.multiply(log_weight, ivy.add(log_neg_abs_logits, max_neg_logits))
+    return ivy.add(first_term, second_term)
+
+
+weighted_cross_entropy_with_logits.unsupported_dtypes = (
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "bool",
+)
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -644,3 +644,52 @@ def test_tensorflow_dropout(
         prob=prob,
         scale=scale,
     )
+
+
+# weighted_cross_entropy_with_logits
+@handle_cmd_line_args
+@given(
+    dtype_labels_logits=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        min_value=0,
+        max_value=1,
+        min_num_dims=1,
+        shared_dtype=True,
+    ),
+    pos_weight=st.one_of(
+        helpers.floats(
+            min_value=0,
+            max_value=10,
+        )
+    ),
+    name=st.sampled_from(["weighted_cross_entropy_with_logits"]),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.nn.weighted_cross_entropy_with_logits",  # noqa
+    ),
+)
+def test_tensorflow_weighted_cross_entropy_with_logits(
+    dtype_labels_logits,
+    pos_weight,
+    name,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, input_values = dtype_labels_logits
+    labels, logits = input_values
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        with_inplace=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="nn.weighted_cross_entropy_with_logits",
+        labels=labels,
+        logits=logits,
+        pos_weight=pos_weight,
+        name=name,
+    )
