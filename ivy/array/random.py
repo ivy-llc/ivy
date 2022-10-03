fc176854c6f0ab71fdbfb9e94b1cfdd6d1b1d203
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-03 13:12:27 +0100

    implemented superset behavior for randint (#5108)
    Coauthored by: @simonetgordon

--- ivy/array/random.py
+++ ivy/array/random.py
@@ -259,6 +259,7 @@ class ArrayWithRandom(abc.ABC):
         shape: Optional[Union[ivy.Shape, ivy.NativeShape]] = None,
         device: Optional[Union[ivy.Device, ivy.NativeDevice]] = None,
         dtype: Optional[Union[ivy.Dtype, ivy.NativeDtype]] = None,
+        seed: Optional[int] = None,
         out: Optional[ivy.Array] = None,
     ) -> ivy.Array:
         """ivy.Array instance method variant of ivy.randint. This method simply
@@ -282,6 +283,8 @@ class ArrayWithRandom(abc.ABC):
         dtype
              output array data type. If ``dtype`` is ``None``, the output array data
              type will be the default integer data type. Default ``None``
+        seed
+            A python integer. Used to create a random seed distribution
         out
             optional output array, for writing the result to. It must have a shape
             that the inputs broadcast to.
@@ -340,6 +343,7 @@ class ArrayWithRandom(abc.ABC):
             shape=shape,
             device=device,
             dtype=dtype,
+            seed=seed,
             out=out,
         )
 
