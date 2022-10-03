be26024911901057ed75e3ee9719526c64568173
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-10-03 13:47:58 +0000

    Gradient testing for elementwise subtract

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -2034,14 +2034,15 @@ def test_subtract(
     input_dtype, x = dtype_and_x
     helpers.test_function(
         input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
+        as_variable_flags=[True, True],
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        container_flags=container,
-        instance_method=instance_method,
+        container_flags=[False, False],
+        instance_method=False,
         fw=fw,
         fn_name="subtract",
+        test_gradients=True,
         rtol_=1e-2,
         atol_=1e-2,
         x1=x[0],
