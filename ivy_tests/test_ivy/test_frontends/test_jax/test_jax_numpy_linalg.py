c9efdfb83c04f5fbddb89beb15e69a91fca6471b
kimbring2 <kimbring2@gmail.com>
2022-10-04 17:22:01 +0900

    eigvals function for jax numpy frontend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -209,3 +209,46 @@ def test_jax_numpy_qr(
         a=np.asarray(x[0], dtype[0]),
         mode=mode,
     )
+
+
+# eigvals
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        max_value=10,
+        shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: "float16" not in x[0]
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.linalg.eigvals"
+    ),
+)
+def test_jax_numpy_eigvals(
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
+    fw,
+):
+    dtype, x = dtype_and_x
+    x = np.array(x[0], dtype=dtype[0])
+    # make symmetric positive-definite beforehand
+    x = np.matmul(x.T, x) + np.identity(x.shape[0]) * 1e-3
+
+    ret, frontend_ret = helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.linalg.eigvals",
+        test_values=False,
+        a=x,
+    )
