c4af3243820f25dfe8bcb15e60184003aeba6792
Marouf Shaikh <maruf98@gmail.com>
2022-10-04 10:54:11 +0100

    Added superset behaviour to svd (#5075)
    * added compute_uv arg
    
    * updated test_svd to support compute_uv arg
    
    * updated docstring to include compute_uv

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -306,11 +306,17 @@ solve.unsupported_dtypes = (
 
 
 def svd(
-    x: JaxArray, /, *, full_matrices: bool = True
+    x: JaxArray, /, *, compute_uv: bool = True, full_matrices: bool = True
 ) -> Union[JaxArray, Tuple[JaxArray, ...]]:
-    results = namedtuple("svd", "U S Vh")
-    U, D, VT = jnp.linalg.svd(x, full_matrices=full_matrices)
-    return results(U, D, VT)
+
+    if compute_uv:
+        results = namedtuple("svd", "U S Vh")
+        U, D, VT = jnp.linalg.svd(x, full_matrices=full_matrices, compute_uv=compute_uv)
+        return results(U, D, VT)
+    else:
+        results = namedtuple("svd", "S")
+        D = jnp.linalg.svd(x, full_matrices=full_matrices, compute_uv=compute_uv)
+        return results(D)
 
 
 svd.unsupported_dtypes = (
