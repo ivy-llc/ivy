4d2b324517edbfe742f56d8cf9af037bf9cb0bae
atlasrule <51425254+atlasrule@users.noreply.github.com>
2022-10-04 19:57:25 +0300

    Remove fw from test

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -219,7 +219,6 @@ def test_tensorflow_logdet(
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="tensorflow",
         fn_tree="linalg.logdet",
         matrix=x,
