a4110b80763bd6c6b365a67d39ab2765506f71d0
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-10-04 15:36:55 +0000

    Gradient testing for elementwise rad2deg

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -2391,12 +2391,12 @@ def test_rad2deg(
     input_dtype, x = dtype_and_x
     helpers.test_function(
         input_dtypes=input_dtype,
-        as_variable_flags=[True],
+        as_variable_flags=as_variable,
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        container_flags=[False],
-        instance_method=False,
+        container_flags=container,
+        instance_method=instance_method,
         fw=fw,
         fn_name="rad2deg",
         test_gradients=True,
