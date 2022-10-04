eecebab7c84a74e896a923ea2f62f64854df2314
CatB1t <skytedits@gmail.com>
2022-10-04 17:26:51 +0200

    temporary testing.

--- ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_sorting.py
@@ -2,9 +2,9 @@
 
 # global
 from hypothesis import given, strategies as st
+import numpy as np
 
 # local
-import ivy
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
@@ -101,22 +101,26 @@ def test_sort(
 
 @st.composite
 def _get_v_and_x(draw):
+    arb_leading_dims = draw(
+        helpers.get_shape(
+            min_num_dims=1,
+        )
+    )
+    nx = draw(st.integers(min_value=1, max_value=5))
+    nv = draw(st.integers(min_value=1, max_value=5))
     dtype_x, x = draw(
         helpers.dtype_and_values(
-            available_dtypes=helpers.get_dtypes("numeric"),
-            shape=draw(
-                helpers.get_shape(
-                    min_num_dims=1,
-                )
-            ),
+            dtype=["int32"],
+            shape=arb_leading_dims + (nx,),
         )
     )
     dtype_v, v = draw(
         helpers.dtype_and_values(
-            available_dtypes=helpers.get_dtypes("numeric"),
+            dtype=["int32"],
+            shape=arb_leading_dims + (nv,),
         )
     )
-    return [dtype_x, dtype_v], [x, v]
+    return dtype_x + dtype_v, x + v
 
 
 @handle_cmd_line_args
@@ -142,7 +146,6 @@ def test_searchsorted(
     ret_dtype,
 ):
     dtypes, xs = dtypes_and_xs
-    sorter = ivy.argsort(ivy.asarray(xs[0])) if sorter and fw != "tensorflow" else None
     helpers.test_function(
         input_dtypes=dtypes,
         as_variable_flags=as_variable,
@@ -153,9 +156,9 @@ def test_searchsorted(
         instance_method=instance_method,
         fw=fw,
         fn_name="searchsorted",
-        x=xs[0],
+        x=np.sort(xs[0]),
         v=xs[1],
         side=side,
-        sorter=sorter,
+        sorter=None,
         ret_dtype=ret_dtype,
     )
