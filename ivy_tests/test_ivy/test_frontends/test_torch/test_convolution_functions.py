ccc1b6dac1ce0ec8dd9787bfff2545e2a73499bc
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-05 09:43:43 +0100

    adding fold unfold to torch frontend convolution (#4799)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_convolution_functions.py
@@ -1,5 +1,7 @@
 # global
+import random
 from hypothesis import given, strategies as st
+
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -68,4 +70,110 @@ def test_torch_conv2d_2(input, weight, bias, stride, dilation, padding):
         padding=padding,
         dilation=dilation,
         groups=1,
+    )
+
+
+@st.composite
+def _int_or_tuple(draw, min_val, max_val):
+    val = draw(
+        random.choice(
+            [
+                st.integers(min_val, max_val),
+                st.tuples(st.integers(min_val, max_val)),
+                st.tuples(
+                    st.integers(min_val, max_val),
+                    st.integers(min_val, max_val),
+                ),
+            ]
+        )
+    )
+    return val
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_input_and_shape=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=(1, 3, 6, 6),
+    ),
+    kernel_size=_int_or_tuple(2, 5),
+    dilation=_int_or_tuple(1, 3),
+    padding=_int_or_tuple(0, 2),
+    stride=_int_or_tuple(1, 3),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.unfold"
+    ),
+)
+def test_torch_unfold(
+    dtype_and_input_and_shape,
+    kernel_size,
+    dilation,
+    padding,
+    stride,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    args_dtypes = list([dtype_and_input_and_shape[0]] + ["uint8"] * 4)
+    helpers.test_frontend_function(
+        input_dtypes=args_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.unfold",
+        input=dtype_and_input_and_shape[1],
+        kernel_size=kernel_size,
+        dilation=dilation,
+        padding=padding,
+        stride=stride,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_input_and_shape=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=(1, 12, 12),
+    ),
+    output_size=_int_or_tuple(3, 5),
+    kernel_size=_int_or_tuple(2, 5),
+    dilation=_int_or_tuple(1, 3),
+    padding=_int_or_tuple(0, 2),
+    stride=_int_or_tuple(1, 3),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.fold"
+    ),
+)
+def test_torch_fold(
+    dtype_and_input_and_shape,
+    output_size,
+    kernel_size,
+    dilation,
+    padding,
+    stride,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    args_dtypes = list([dtype_and_input_and_shape[0]] + ["uint8"] * 5)
+    helpers.test_frontend_function(
+        input_dtypes=args_dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="torch",
+        fn_tree="nn.functional.fold",
+        input=dtype_and_input_and_shape[1],
+        output_size=output_size,
+        kernel_size=kernel_size,
+        dilation=dilation,
+        padding=padding,
+        stride=stride,
     )
