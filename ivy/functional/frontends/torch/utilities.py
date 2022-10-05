e147503939551d33c0873dcf43e7eff5f0690128
faseehahmed <54709992+faseehahmed26@users.noreply.github.com>
2022-10-05 11:49:05 +0530

    Updated can_cast funtion

--- ivy/functional/frontends/torch/utilities.py
+++ ivy/functional/frontends/torch/utilities.py
@@ -1,8 +1,31 @@
 # global
+import torch
 
 # local
 import ivy
 
 
 def can_cast(from_, to):
-    return ivy.can_cast(from_, to)
+    res = 0
+    native_dtype_dict = {
+        'int8': torch.int8,
+        'int16': torch.int16,
+        'int32': torch.int32,
+        'int64': torch.int64,
+        'uint8': torch.uint8,
+        'bfloat16': torch.bfloat16,
+        'float16': torch.float16,
+        'float32': torch.float32,
+        'float64': torch.float64,
+        'bool': torch.bool,
+    }
+    if isinstance(from_, str) and isinstance(to, str):
+        key_list = list(native_dtype_dict.keys())
+        val_list = list(native_dtype_dict.values())
+        d1 = key_list[val_list.index(from_)]
+        d2 = key_list[val_list.index(to)]
+        res = ivy.can_cast(d1, d2)
+
+    else:
+        res = ivy.can_cast(from_, to)
+    return res
