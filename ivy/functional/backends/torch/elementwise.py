Fixed failing ivy tests for elementwise remainder and added gradient testing (#5271)

ivy/functional/backends/torch/elementwise.py
@@ -593,9 +593,7 @@ def remainder(
         diff = res - res_floored
         diff, x2 = ivy.promote_types_of_inputs(diff, x2)
         return torch.round(torch.mul(diff, x2, out=out), out=out).to(x1.dtype)
-    floor_div = torch.floor(torch.div(x1, x2)).to(x1.dtype)
-    res = x1 - floor_div * x2
-    return res.to(x1.dtype)
+    return torch.remainder(x1, x2, out=out).to(x1.dtype)
 
 
 remainder.support_native_out = True