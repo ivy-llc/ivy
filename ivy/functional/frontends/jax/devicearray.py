ef108d714110d71c80b085409399ca38c0f97cf1
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-10-06 21:50:47 +0100

    refactor decorator

--- ivy/functional/frontends/jax/devicearray.py
+++ ivy/functional/frontends/jax/devicearray.py
@@ -1,34 +1,15 @@
-# global
-import functools
-
-
 # local
 import ivy
 import ivy.functional.frontends.jax as jax_frontend
-
-
-def _frontend_wrapper(f):
-    @functools.wraps(f)
-    def decor(self, *args, **kwargs):
-        args = list(args)
-        if isinstance(self, jax_frontend.DeviceArray):
-            i = 0
-            for arg in args:
-                if isinstance(arg, jax_frontend.DeviceArray):
-                    args[i] = arg.data
-                i += 1
-            for u, v in kwargs.items():
-                if isinstance(v, jax_frontend.DeviceArray):
-                    kwargs[u] = v.data
-            return f(self, *args, **kwargs)
-        return getattr(self, f.__name__)(*args, **kwargs)
-
-    return decor
+from ivy.functional.frontends.jax.func_wrapper import to_ivy_arrays_and_back
 
 
 class DeviceArray:
     def __init__(self, data):
         self.data = ivy.array(data)
+
+    def __repr__(self):
+        return "ivy.functional.frontends.jax.DeviceArray(" + str(self.data) + ")"
 
     # Instance Methods #
     # ---------------- #
@@ -42,38 +23,38 @@ class DeviceArray:
     # Special Methods #
     # --------------- #
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __pos__(self):
         return ivy.positive(self.data)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __neg__(self):
         return jax_frontend.neg(self.data)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __eq__(self, other):
         return jax_frontend.eq(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __ne__(self, other):
         return jax_frontend.ne(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __lt__(self, other):
         return jax_frontend.lt(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __le__(self, other):
         return jax_frontend.le(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __gt__(self, other):
         return jax_frontend.gt(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __ge__(self, other):
         return jax_frontend.ge(self.data, other)
 
-    @_frontend_wrapper
+    @to_ivy_arrays_and_back
     def __abs__(self):
         return jax_frontend.abs(self.data)
