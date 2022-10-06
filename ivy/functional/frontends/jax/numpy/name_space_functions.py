2ddae947be8c65cf2c7de36cbbd04ebb27c90eba
trihernandez <43832443+trihernandez@users.noreply.github.com>
2022-10-06 04:20:55 -0700

    Added Newline

--- ivy/functional/frontends/jax/numpy/name_space_functions.py
+++ ivy/functional/frontends/jax/numpy/name_space_functions.py
@@ -118,3 +118,4 @@ def var(a, axis=None, dtype=None, out=None, ddof=0, keepdims=False, *, where=Non
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret.astype(dtype)
+    
