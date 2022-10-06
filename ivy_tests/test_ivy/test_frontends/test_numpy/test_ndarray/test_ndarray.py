9784e384494a1778a079baa9d4c12666e094a655
morleythomas <48353535+morleythomas@users.noreply.github.com>
2022-10-06 15:43:54 +0100

    Resolve format errors (#5072)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -287,3 +287,39 @@ def test_numpy_ndarray_all(
         class_name="ndarray",
         method_name="all",
     )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        force_int_axis=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.ndarray.argsort"
+    ),
+)
+def test_numpy_instance_argsort(
+    dtype_x_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x, axis = dtype_x_axis
+    helpers.test_frontend_instance_method(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        frontend_class=np.ndarray,
+        fn_tree="ndarray.argsort",
+        x=x[0],
+        axis=axis,
+    )
