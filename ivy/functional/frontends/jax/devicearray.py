341075de64f1fe02186b7ea90573f83adca68475
JG <67888047+juliagsy@users.noreply.github.com>
2022-10-06 22:13:37 +0100

    jax frontend special methods - pos, neg, abs, eq, ne, lt, le, gt, ge (#5120)

--- ivy/functional/frontends/jax/devicearray.py
+++ ivy/functional/frontends/jax/devicearray.py
@@ -1,18 +1,64 @@
 # local
 import ivy
 import ivy.functional.frontends.jax as jax_frontend
+from ivy.functional.frontends.jax.func_wrapper import to_ivy_arrays_and_back
 
 
 class DeviceArray:
     def __init__(self, data):
-        if ivy.is_native_array(data):
-            data = ivy.Array(data)
-        self.data = data
+        self.data = ivy.array(data)
 
-    # Instance Methoods #
-    # -------------------#
+    def __repr__(self):
+        return (
+            "ivy.functional.frontends.jax.DeviceArray("
+            + str(ivy.to_list(self.data))
+            + ")"
+        )
+
+    # Instance Methods #
+    # ---------------- #
+
     def reshape(self, new_sizes, dimensions=None):
         return jax_frontend.reshape(self.data, new_sizes, dimensions)
 
     def add(self, other):
         return jax_frontend.add(self.data, other)
+
+    # Special Methods #
+    # --------------- #
+
+    @to_ivy_arrays_and_back
+    def __pos__(self):
+        return ivy.positive(self.data)
+
+    @to_ivy_arrays_and_back
+    def __neg__(self):
+        return jax_frontend.neg(self.data)
+
+    @to_ivy_arrays_and_back
+    def __eq__(self, other):
+        return jax_frontend.eq(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __ne__(self, other):
+        return jax_frontend.ne(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __lt__(self, other):
+        return jax_frontend.lt(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __le__(self, other):
+        return jax_frontend.le(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __gt__(self, other):
+        return jax_frontend.gt(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __ge__(self, other):
+        return jax_frontend.ge(self.data, other)
+
+    @to_ivy_arrays_and_back
+    def __abs__(self):
+        return jax_frontend.abs(self.data)
