e678c67b46a78e7467c84962cfe14fd219a62a96
Kareem Morsy <kreem.morsy@hotmail.com>
2022-10-06 17:57:33 +0200

    Added Max to raw_ops

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -136,6 +136,10 @@ def MatMul(*, a, b, transpose_a=False, transpose_b=False, name="MatMul"):
     return ivy.matmul(a, b, transpose_a=transpose_a, transpose_b=transpose_b)
 
 
+def Max(*, input, axis, keep_dims=False, name="Max"):
+    return ivy.astype(ivy.max(input, axis=axis, keepdims=keep_dims), input.dtype)
+
+
 def Maximum(*, x, y, name="Maximum"):
     return tf_frontend.maximum(x, y)
 
