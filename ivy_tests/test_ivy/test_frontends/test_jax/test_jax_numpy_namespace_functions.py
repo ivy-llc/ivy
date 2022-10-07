fdade5db1a790b1e4974f151a49a1edd8b592843
kimbring2 <kimbring2@gmail.com>
2022-10-07 12:08:06 +0900

    arctan2 function for jax numpy frontend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -639,4 +639,36 @@ def test_jax_numpy_arctan(
         fn_tree="numpy.arctan",
         x=x[0],
     )
-    
+
+
+# arctan2
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.jax.numpy.arctan2"
+    ),
+)
+def test_jax_numpy_arctan2(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.arctan2",
+        x1=x[0],
+        x2=x[1],
+    )
