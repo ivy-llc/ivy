f6923ec9ac86fd5ded836ae1d1350dc3200b447d
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-07 21:21:51 +0530

    torch frontend `sin` and `sin_` instance methods (#5368)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy/functional/frontends/torch/Tensor.py
+++ ivy/functional/frontends/torch/Tensor.py
@@ -19,3 +19,14 @@ class Tensor:
 
     def add(self, other, *, alpha=1):
         return torch_frontend.add(self.data, other, alpha=alpha)
+
+    def sin(self, *, out=None):
+        return torch_frontend.sin(self.data, out=out)
+
+    def sin_(self):
+        self.data = self.sin()
+        return self.data
+
+
+# Tensor (alias)
+tensor = Tensor
