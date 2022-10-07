ffcd3f7006c8fcd3d43518fa2c8eb5c2d2b01e40
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-07 04:38:37 +0000

    refactored files for Tensor

--- ivy/functional/frontends/torch/__init__.py
+++ ivy/functional/frontends/torch/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
-from . import tensor
-from .tensor import tensor
+from . import Tensor
+from .Tensor import tensor
 from . import indexing_slicing_joining_mutating_ops
 from .indexing_slicing_joining_mutating_ops import *
 from . import pointwise_ops
