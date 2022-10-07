d58eb3f6f4b1d14f83d306d3f2505bfb7d6d1901
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-07 08:06:22 +0000

    lint fix

--- determine_test_coverage.py
+++ determine_test_coverage.py
@@ -8,8 +8,8 @@ tests = {}
 
 
 os.system(
-    "pytest --disable-pytest-warnings ivy_tests/test_ivy/ --my_test_dump true > test_names"
-)  # noqa
+    "pytest --disable-pytest-warnings ivy_tests/test_ivy/ --my_test_dump true > test_names"  # noqa
+)
 test_names = []
 with open("test_names") as f:
     i = 0
