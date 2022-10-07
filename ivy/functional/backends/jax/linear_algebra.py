f48c166cf4ad2cfa3731d4bf669e4214dd219874
RickSanchezStoic <57310695+RickSanchezStoic@users.noreply.github.com>
2022-10-07 17:40:44 +0530

    changes and fixes (#4643)
    * changes and fixes
    
    * changes:
    
    * merge conflicts
    
    * lint
    
    * lint fixes
    
    * lint fixes

--- ivy/functional/backends/jax/linear_algebra.py
+++ ivy/functional/backends/jax/linear_algebra.py
@@ -7,7 +7,7 @@ from collections import namedtuple
 import ivy
 from ivy import inf
 from ivy.functional.backends.jax import JaxArray
-
+from ivy import promote_types_of_inputs
 
 # Array API Standard #
 # -------------------#
@@ -41,6 +41,7 @@ def cross(
     axis: int = None,
     out: Optional[JaxArray] = None,
 ) -> JaxArray:
+    x1, x2 = promote_types_of_inputs(x1, x2)
     return jnp.cross(a=x1, b=x2, axisa=axisa, axisb=axisb, axisc=axisc, axis=axis)
 
 
@@ -370,6 +371,7 @@ def tensordot(
     axes: Union[int, Tuple[Sequence[int], Sequence[int]]] = 2,
     out: Optional[JaxArray] = None,
 ) -> JaxArray:
+    x1, x2 = promote_types_of_inputs(x1, x2)
     return jnp.tensordot(x1, x2, axes)
 
 
@@ -391,6 +393,7 @@ trace.unsupported_dtypes = ("float16", "bfloat16")
 def vecdot(
     x1: JaxArray, x2: JaxArray, /, *, axis: int = -1, out: Optional[JaxArray] = None
 ) -> JaxArray:
+    x1, x2 = promote_types_of_inputs(x1, x2)
     return jnp.tensordot(x1, x2, axes=(axis, axis))
 
 
