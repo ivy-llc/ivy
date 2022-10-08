061f139f6fb0e584a3243eedc1bbd2810367e5b7
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-10-08 19:04:34 +0100

    remove dtype - ceil

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -19,8 +19,6 @@ def ceil(
     dtype=None,
     subok=True,
 ):
-    if dtype:
-        x = ivy.astype(ivy.array(x), ivy.as_ivy_dtype(dtype))
     ret = ivy.ceil(x, out=out)
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
