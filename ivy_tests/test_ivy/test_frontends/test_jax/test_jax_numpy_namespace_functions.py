7e010cdbc4fd4bbb2477ca7df5dfbef51044c3cb
root <shrivastavavaatsalya@gmail.com>
2022-10-08 21:03:04 +0400

    Added jax.numpy.cos namespace function to jax frontend and tested.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -665,3 +665,32 @@ def test_jax_numpy_arctan2(
         x1=x[0],
         x2=x[1],
     )
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.cos"
+    ),
+)
+def test_jax_numpy_cos(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.cos",
+        x=x[0],
+    )
