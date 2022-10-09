15f4a0da88406d1d4479d7e66f8025119192935f
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-09 20:50:04 +0100

    Lcm extension (#5439)
    authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_functional/test_extensions.py
+++ ivy_tests/test_ivy/test_functional/test_extensions.py
@@ -233,3 +233,45 @@ def test_flatten(
         start_dim=start_dim,
         end_dim=end_dim,
     )
+
+
+# lcm
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        num_arrays=2,
+        shared_dtype=True,
+        min_num_dims=1,
+        max_num_dims=3,
+        min_value=-100,
+        max_value=100,
+        allow_nan=False
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="lcm"),
+)
+def test_lcm(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="lcm",
+        test_gradients=True,
+        x1=np.asarray(x[0], dtype=input_dtype[0]),
+        x2=np.asarray(x[1], dtype=input_dtype[1]),
+    )
