47fd92a163d24238766b7931fd6ab3f2756f05d1
nassimberrada <Nassim>
2022-10-10 21:43:38 +0100

    merges

--- ivy_tests/test_ivy/test_functional/test_extensions.py
+++ ivy_tests/test_ivy/test_functional/test_extensions.py
@@ -163,12 +163,10 @@ def test_sinc(
 @handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=1,
-        max_num_dims=1
+        available_dtypes=helpers.get_dtypes("float"), min_num_dims=1, max_num_dims=1
     ),
     dtype=helpers.get_dtypes("float", full=False),
-    num_positional_args=helpers.num_positional_args(fn_name="vorbis_window")
+    num_positional_args=helpers.num_positional_args(fn_name="vorbis_window"),
 )
 def test_vorbis_window(
     dtype_and_x,
@@ -193,6 +191,126 @@ def test_vorbis_window(
         fw=fw,
         fn_name="vorbis_window",
         x=x[0],
+        dtype=dtype,
+    )
+    
+
+# flatten
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=helpers.get_shape(min_num_dims=5, max_num_dims=5),
+    ),
+    start_dim=st.integers(1, 3),
+    end_dim=st.integers(3, 4),
+    num_positional_args=helpers.num_positional_args(fn_name="flatten"),
+)
+def test_flatten(
+    dtype_and_x,
+    start_dim,
+    end_dim,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtypes, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=True,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="flatten",
+        x=np.asarray(x[0], dtype=input_dtypes[0]),
+        start_dim=start_dim,
+        end_dim=end_dim,
+    )
+
+
+# lcm
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        num_arrays=2,
+        shared_dtype=True,
+        min_num_dims=1,
+        max_num_dims=3,
+        min_value=-100,
+        max_value=100,
+        allow_nan=False
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="lcm"),
+)
+def test_lcm(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="lcm",
+        test_gradients=True,
+        x1=np.asarray(x[0], dtype=input_dtype[0]),
+        x2=np.asarray(x[1], dtype=input_dtype[1]),
+    )
+
+
+# hann_window
+@handle_cmd_line_args
+@given(
+    window_length=helpers.ints(min_value=1, max_value=10),
+    input_dtype=helpers.get_dtypes("integer"),
+    periodic=st.booleans(),
+    num_positional_args=helpers.num_positional_args(fn_name="hann_window"),
+    dtype=helpers.get_dtypes("float"),
+)
+def test_hann_window(
+    window_length,
+    input_dtype,
+    periodic,
+    dtype,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="hann_window",
+        window_length=window_length,
+        periodic=periodic,
         dtype=dtype,
     )
 
