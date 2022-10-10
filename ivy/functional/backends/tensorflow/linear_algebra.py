633c481ce6338d3aeeef6b24a204d8e0bb88f3c7
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-10 19:13:18 +0100

    Vander extension (#5449)
    * vander_extension
    
    * test fixes
    
    * fixes
    
    * add container and array methods
    
    * corrections
    
    * changes
    
    * dtype corrections
    
    * corrections
    
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -642,3 +642,30 @@ def vector_to_skew_symmetric_matrix(
     # BS x 3 x 3
     ret = tf.concat((row1, row2, row3), -2)
     return ret
+
+
+vector_to_skew_symmetric_matrix.unsupported_dtypes = (
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "float16",
+    "float64",
+)
+
+
+def vander(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    N: Optional[int] = None,
+    increasing: Optional[bool] = False,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    return tf.experimental.numpy.vander(
+        x, N=N, increasing=increasing
+    )
