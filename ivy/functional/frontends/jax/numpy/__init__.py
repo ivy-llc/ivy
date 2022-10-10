4d51afc8df38040a56400dd91696145edc866f29
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-10 14:34:19 +0000

    order imports alphabetically

--- ivy/functional/frontends/jax/numpy/__init__.py
+++ ivy/functional/frontends/jax/numpy/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from . import name_space_functions
-from .name_space_functions import *
 from . import fft
 from . import linalg
+from . import name_space_functions
+from .name_space_functions import *
