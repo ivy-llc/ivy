070c1da4469c3d14ee7e420992ed1cca9817a5c3
sherry30 <sherrytst30@gmail.com>
2022-10-10 19:08:17 +0500

    fixing dropout test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_dropout_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_dropout_functions.py
@@ -1,66 +1,56 @@
 # global
 import numpy as np
 from hypothesis import given, strategies as st
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-import ivy.functional.backends.numpy as ivy_np
-import ivy.functional.backends.torch as ivy_torch
 
 
-@st.composite
-def _dtypes(draw):
-    return draw(
-        st.shared(
-            helpers.list_of_length(
-                x=st.sampled_from(
-                    tuple(
-                        set(ivy_np.valid_float_dtypes).intersection(
-                            set(ivy_torch.valid_float_dtypes)
-                        )
-                    )
-                    + (None,)
-                ),
-                length=1,
-            ),
-            key="dtype",
-        )
-    )
-
-
+@handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=tuple(
-            set(ivy_np.valid_float_dtypes).intersection(
-                set(ivy_torch.valid_float_dtypes)
-            )
-        )
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        max_value=50,
+        allow_inf=False,
+        min_num_dims=1,
+        max_num_dims=1,
+        min_dim_size=2,
     ),
-    as_variable=st.booleans(),
-    with_out=st.booleans(),
-    num_positional_args=helpers.num_positional_args(fn_name="functional.frontends.torch.dropout"),
-    native_array=st.booleans(),
+    prob=helpers.floats(min_value=0, max_value=0.9, width=64),
+    training=st.booleans(),
+    num_positional_args=helpers.num_positional_args(fn_name="ivy.functional.frontends.torch.dropout"),
+    with_inplace=st.booleans(),
 )
 def test_torch_dropout(
     dtype_and_x,
+    prob,
+    training,
+    with_inplace,
     as_variable,
-    with_out,
     num_positional_args,
     native_array,
-    fw,
 ):
     input_dtype, x = dtype_and_x
 
-    helpers.test_frontend_function(
+    ret = helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
+        with_out=False,
+        with_inplace=with_inplace,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="torch",
-        fn_name="dropout",
-        input=np.asarray(x, dtype=input_dtype),
-        out=None,
+        fn_tree="nn.functional.dropout",
+        input=x[0],
+        p=prob,
+        training=training,
+        test_values=False
     )
+    ret = helpers.flatten_and_to_np(ret=ret)
+    x = np.asarray(x[0], input_dtype[0])
+    for u in ret:
+        # cardinality test
+        assert u.shape == x.shape
 
