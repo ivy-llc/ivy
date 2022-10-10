00a5073746154f8dfb1e3fee083854618ebf86aa
Anagha Zachariah <anaghazachariah@gmail.com>
2022-10-10 13:57:48 +0530

    Update test_activations.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
@@ -111,21 +111,15 @@ def test_tensorflow_softmax(
 # relu
 @handle_cmd_line_args
 @given(
-    dtype_x_and_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=2,
-        max_axes_size=1,
-        force_int_axis=True,
-        valid_axis=True,
-    ),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.relu"
     ),
 )
 def test_tensorflow_relu(
-    dtype_x_and_axis, as_variable, num_positional_args, native_array
+    dtype_and_x, as_variable, num_positional_args, native_array
 ):
-    input_dtype, x, axis = dtype_x_and_axis
+    input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -134,6 +128,7 @@ def test_tensorflow_relu(
         native_array_flags=native_array,
         frontend="tensorflow",
         fn_tree="keras.activations.relu",
+        rtol=1e-2,
+        atol=1e-2,
         x=x[0],
-        axis=axis,
     )
