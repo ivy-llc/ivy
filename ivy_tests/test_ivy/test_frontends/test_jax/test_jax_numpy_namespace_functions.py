2a826a6d1f976520f7d952e7a968bacedb179f4f
trihernandez <43832443+trihernandez@users.noreply.github.com>
2022-10-10 01:07:18 -0700

    #5130 Uint16 (#5133)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -449,6 +449,35 @@ def test_jax_numpy_mean(
     )
 
 
+# uint16
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.uint16"
+    ),
+)
+def test_jax_numpy_uint16(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.uint16",
+        x=x[0],
+    )
+
+
 # var
 @handle_cmd_line_args
 @given(
