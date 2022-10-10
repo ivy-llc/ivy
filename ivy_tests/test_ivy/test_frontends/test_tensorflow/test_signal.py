f619da9d9c11b983271b27c20c58aca1e353a441
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-10-10 21:18:47 +0530

    Add  test vorbis_window

---
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -0,0 +1,41 @@
+# global
+import ivy
+import numpy as np
+from hypothesis import given, strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float")),
+        min_num_dims=1,
+        max_num_dims=1
+    ),
+    dtype=helpers.get_dtypes("float", full=False),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.vorbis_window")
+)
+def test_tensorflow_vorbis_window(
+        dtype_and_x,
+        dtype,
+        as_variable,
+        num_positional_args,
+        native_array,
+        fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="vorbis_window",
+        x=x[0],
+        dtype=dtype,
+    )
