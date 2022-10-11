4445d7ce1dff8f8876e264bfbca7ea6bfa2f65d7
Kareem Morsy <kreem.morsy@hotmail.com>
2022-10-11 13:15:16 +0200

    Added Invert to raw_ops (#5365)
    Co-authored-by: Aditya Pola <adityapola@gmail.com>

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -134,6 +134,10 @@ def Inv(*, x, name="Inv"):
     return ivy.astype(ivy.reciprocal(x), x.dtype)
 
 
+def Invert(*, x, name="Invert"):
+    return ivy.bitwise_invert(x)
+
+  
 def InvGrad(*, y, dy, name="InvGrad"):
     return ivy.multiply(ivy.negative(dy), ivy.multiply(y, y))
 
