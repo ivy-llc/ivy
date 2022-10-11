e98d15a418921ff0e590fc086bfac19b67f07476
Suvaditya Mukherjee <70141886+suvadityamuk@users.noreply.github.com>
2022-10-11 22:31:19 +0530

    Fixes task #5245 to add ReLU activation frontend tensorflow and merged latest commit histories (#5338)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
@@ -7,7 +7,9 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    dtype_and_x=helpers.dtype_and_values(
+    available_dtypes=helpers.get_dtypes("float")
+    ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.hard_sigmoid"
     ),
@@ -105,4 +107,27 @@ def test_tensorflow_softmax(
         fn_tree="keras.activations.softmax",
         x=x[0],
         axis=axis,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("numeric")),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.relu"
+    ),
+)
+def test_tensorflow_relu(
+    dtype_and_x, as_variable, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="keras.activations.relu",
+        x=x[0],
     )
