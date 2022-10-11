3f4847ebbe1263bca8b60789fffba10de5887ba9
Vidit Agarwal <vkagrawal07042003@gmail.com>
2022-10-11 17:33:15 +0530

    added rfft

--- ivy_tests/test_ivy/test_functional/test_extensions.py
+++ ivy_tests/test_ivy/test_functional/test_extensions.py
@@ -313,3 +313,36 @@ def test_hann_window(
         periodic=periodic,
         dtype=dtype,
     )
+
+
+#rfft
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="rfft"),
+)
+def test_rfft(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="rfft",
+        x=np.asarray(x[0], dtype=input_dtype[0])
+    )
