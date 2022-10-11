54152d3ec2172d61fe78cb754036fbcce2720ef0
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-10-11 21:49:16 +0100

    a templete for you to double check

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -6,26 +6,31 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 
 # hann_window
-
-
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-    ),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    target_dtype=helpers.get_dtypes("float"),
+    periodic=st.booleans(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.hann_window",
+        fn_name="ivy.functional.frontends.tensorflow.signal.hann_window"
     ),
 )
-# helpers.test_frontend_function to test hann_window
-def test_hann_window(dtype_and_x, num_positional_args):
-    # unpack
-    dtype, x = dtype_and_x
-
-    # test
+def test_tensorflow_hann_window(
+    dtype_and_x, target_dtype, as_variable, 
+    periodic, num_positional_args, native_array, fw
+):
+    input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
-        ivy.hann_window,
-        x,
-        dtype=dtype,
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
         num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="signal.hann_window",
+        window_length=x[0],
+        periodic=periodic,
+        dtype=target_dtype,
     )
+
