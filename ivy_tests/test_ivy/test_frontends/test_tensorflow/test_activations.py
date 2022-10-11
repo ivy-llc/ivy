41ff1fd63ffa94780e769eb881d1f7669269f92d
Mina-88 <minawagih8801@gmail.com>
2022-10-11 22:13:53 +0200

    removing extra arg

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
@@ -11,7 +11,6 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float")
     ),
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.hard_sigmoid"
     ),
