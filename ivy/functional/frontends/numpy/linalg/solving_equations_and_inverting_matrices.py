0eb2c9e83cb191eeb0b87e67d0a27c48237e4442
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-11 11:33:23 +0100

    Numpy frontend array decorators (#5376)

--- ivy/functional/frontends/numpy/linalg/solving_equations_and_inverting_matrices.py
+++ ivy/functional/frontends/numpy/linalg/solving_equations_and_inverting_matrices.py
@@ -1,5 +1,6 @@
 # local
 import ivy
+from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 from ... import versions
 from ivy.func_wrapper import with_unsupported_dtypes
 
@@ -8,15 +9,18 @@ version = versions["numpy"]
 
 # solve
 @with_unsupported_dtypes({"1.23.0 and below": ("float16",)}, version)
+@to_ivy_arrays_and_back
 def solve(a, b):
     return ivy.solve(a, b)
 
 
 # inv
+@to_ivy_arrays_and_back
 def inv(a):
     return ivy.inv(a)
 
 
 # pinv
+@to_ivy_arrays_and_back
 def pinv(a, rtol=1e-15, hermitian=False):
     return ivy.pinv(a, rtol)
