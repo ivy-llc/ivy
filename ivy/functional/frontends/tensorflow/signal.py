9d91a6cbb07fb9e440879c1fe2152b7afda2c5b6
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-10-11 21:56:47 +0100

    some guiding changes

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -1,7 +1,8 @@
-import tensorflow as tf
+import ivy
+import ivy.functional.frontends.tensorflow as ivy_tf
 
 # hann_window
-
-
-def hann_window(length, dtype=tf.float32):
-    return tf.signal.hann_window(length=length, dtype=dtype)
+def hann_window(window_length, periodic=True, dtype=ivy_tf.dtypes.float32, name=None): # ivy dtype is a better choice here
+    # you need to either implement it using compositions, or wait for ivy's own version of this function to be implemented
+    # more details in the deep dive document https://lets-unify.ai/ivy/deep_dive/16_ivy_frontends.html#ivy-frontends
+    pass
