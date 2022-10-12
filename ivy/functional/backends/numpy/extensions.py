d035e437e0d12a41cdd080c5ddb97763a5a650f4
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-12 14:29:15 +0100

    Kaiser_window extension (#5239)
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/backends/numpy/extensions.py
+++ ivy/functional/backends/numpy/extensions.py
@@ -184,3 +184,24 @@ def max_pool2d(
     if data_format == "NCHW":
         return np.transpose(res, (0, 3, 1, 2))
     return res
+
+
+def kaiser_window(
+    window_length: int,
+    periodic: bool = True,
+    beta: float = 12.0,
+    *,
+    dtype: Optional[np.dtype] = None,
+    out: Optional[np.ndarray] = None
+) -> np.ndarray:
+    if periodic is False:
+        return np.array(
+            np.kaiser(M=window_length, beta=beta),
+            dtype=dtype) 
+    else: 
+        return np.array(
+            np.kaiser(M=window_length + 1, beta=beta)[:-1],
+            dtype=dtype)
+
+
+kaiser_window.support_native_out = False
