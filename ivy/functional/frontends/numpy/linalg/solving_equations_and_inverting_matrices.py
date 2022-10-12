535dfaac2b873758b90efbf6e089e400d77a23b1
RickSanchezStoic <57310695+RickSanchezStoic@users.noreply.github.com>
2022-10-12 11:09:29 +0530

    numpy promotion table added (#5552)
    * numpy promotion table added
    
    * lint
    
    * changes and fixes
    
    * lint fixes
    
    * test
    
    * changes
    
    * changes
    
    * changes

--- ivy/functional/frontends/numpy/linalg/solving_equations_and_inverting_matrices.py
+++ ivy/functional/frontends/numpy/linalg/solving_equations_and_inverting_matrices.py
@@ -3,6 +3,7 @@ import ivy
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 from ... import versions
 from ivy.func_wrapper import with_unsupported_dtypes
+from ivy.functional.frontends.numpy import promote_types_of_numpy_inputs
 
 version = versions["numpy"]
 
@@ -11,6 +12,7 @@ version = versions["numpy"]
 @with_unsupported_dtypes({"1.23.0 and below": ("float16",)}, version)
 @to_ivy_arrays_and_back
 def solve(a, b):
+    a, b = promote_types_of_numpy_inputs(a, b)
     return ivy.solve(a, b)
 
 
