089fa5df576c2835cef27d64156ed61a8909b2e6
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-12 14:29:15 +0100

    Kaiser_window extension (#5239)
    Co-authored-by: nassimberrada <Nassim>

--- ivy/container/extensions.py
+++ ivy/container/extensions.py
@@ -563,3 +563,119 @@ class ContainerWithExtensions(ContainerBase):
             map_sequences=map_sequences,
             out=out,
         )
+
+    @staticmethod
+    def static_kaiser_window(
+        window_length: Union[int, ivy.Container],
+        periodic: bool = True,
+        beta: float = 12.0,
+        *,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        dtype: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.kaiser_window. This method 
+        simply wraps the function, and so the docstring for ivy.kaiser_window 
+        also applies to this method with minimal changes.
+
+        Parameters
+        ----------
+        window_length
+            input container including window lenghts.
+        periodic
+            If True, returns a periodic window suitable for use in spectral analysis.
+            If False, returns a symmetric window suitable for use in filter design.
+        beta
+            a float used as shape parameter for the window.
+        dtype
+            data type of the returned array.        
+        out
+            optional output container, for writing the result to.
+
+        Returns
+        -------
+        ret
+            The container that includes the Kaiser windows.
+
+        Examples
+        --------
+        >>> x = ivy.Container(a=3, b=5)
+        >>> ivy.Container.static_kaiser_window(x, True, 5)
+        {
+            a: ivy.array([0.2049, 0.8712, 0.8712]),
+            a: ivy.array([0.0367, 0.7753, 0.7753]),
+        }
+        """
+        return ContainerBase.multi_map_in_static_method(
+            "kaiser_window",
+            window_length,
+            periodic,
+            beta,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            dtype=dtype,
+            out=out,
+        )
+
+    def kaiser_window(
+        self: ivy.Container,
+        periodic: bool = True,
+        beta: float = 12.0,
+        *,
+        key_chains: Optional[Union[List[str], Dict[str, str]]] = None,
+        to_apply: bool = True,
+        prune_unapplied: bool = False,
+        map_sequences: bool = False,
+        dtype: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.kaiser_window. This method 
+        simply wraps the function, and so the docstring for ivy.kaiser_window 
+        also applies to this method with minimal changes.
+
+        Parameters
+        ----------
+        self
+            input container including window lenghts.
+        periodic
+            If True, returns a periodic window suitable for use in spectral analysis.
+            If False, returns a symmetric window suitable for use in filter design.
+        beta
+            a float used as shape parameter for the window.
+        dtype
+            data type of the returned array.        
+        out
+            optional output container, for writing the result to.
+
+        Returns
+        -------
+        ret
+            The container that includes the Kaiser windows.
+
+        Examples
+        --------
+        >>> x = ivy.Container(a=3, b=5)
+        >>> ivy.Container.static_kaiser_window(x, True, 5)
+        {
+            a: ivy.array([0.2049, 0.8712, 0.8712]),
+            a: ivy.array([0.0367, 0.7753, 0.7753]),
+        }
+        """
+        return self.static_kaiser_window(
+            self,
+            periodic,
+            beta,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            dtype=dtype,
+            out=out,
+        )
