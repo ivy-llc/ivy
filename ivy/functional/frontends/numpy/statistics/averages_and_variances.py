a983abb6e656262c1350c02a3613312300eac784
Ahmed-Khaled22 <113449925+Ahmed-Khaled22@users.noreply.github.com>
2022-10-12 17:20:10 +0200

    adding nanmean function to numpy frontend (#5511)
    Co-authored-by: Jackie Chen <cheezballz3.14@gmail.com>

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -25,3 +25,39 @@ def mean(
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
 
     return ret
+
+
+@from_zero_dim_arrays_to_float
+def nanmean(
+    a,
+    /,
+    *,
+    axis=None,
+    keepdims=False,
+    out=None,
+    dtype=None,
+    where=True,
+):
+    is_nan = ivy.isnan(a)
+    axis = tuple(axis) if isinstance(axis, list) else axis
+
+    if not any(is_nan):
+        if dtype:
+            a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+        ret = ivy.mean(a, axis=axis, keepdims=keepdims, out=out)
+
+        if ivy.is_array(where):
+            ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+
+
+    else:
+        a = [i for i in a if ivy.isnan(i) == False]
+
+        if dtype:
+            a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+        ret = ivy.mean(a, axis=axis, keepdims=keepdims, out=out)
+
+        if ivy.is_array(where):
+            ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+
+    return ret
