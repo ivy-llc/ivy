424bbe8e272656fe328ff2085c8c4b2000b785b7
Matt <matthew-barrett@hotmail.co.uk>
2022-10-13 19:27:18 +0100

    move instance method examples to their respective method docstrings

--- ivy_tests/test_ivy/test_functional/test_extensions.py
+++ ivy_tests/test_ivy/test_functional/test_extensions.py
@@ -608,7 +608,7 @@ def test_pad(
         min_dim_size=1,
         max_dim_size=3,
         num_arrays=2,
-        shared_dtype=True, 
+        shared_dtype=True,
     ),
     num_positional_args=helpers.num_positional_args(fn_name="heaviside"),
 )
