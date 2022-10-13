c6efc4b5499d4dbfb5f704c793299c0f3b4ffa20
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-10-13 15:15:08 +0530

    numpy frontend sorting

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
@@ -1,5 +1,6 @@
 # global
 from hypothesis import given
+import numpy as np
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -36,4 +37,72 @@ def test_numpy_argsort(
         fn_tree="argsort",
         x=x[0],
         axis=axis,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        force_int_axis=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.sort"
+    ),
+)
+def test_numpy_sort(
+    dtype_x_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x, axis = dtype_x_axis
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="numpy",
+        fn_tree="sort",
+        a=x[0],
+        axis=axis,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=1,
+        min_dim_size=1,
+        min_axis=-1,
+        max_axis=0,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.msort"
+    ),
+)
+def test_numpy_msort(
+    dtype_x_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    with_out,
+):
+    input_dtype, x, axis = dtype_x_axis
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="numpy",
+        fn_tree="msort",
+        a=x[0],
     )
