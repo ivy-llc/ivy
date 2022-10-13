77cde2bf8342c6040c03cbe5ce620d0a863ed846
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-10-13 12:33:49 +0200

    Update signal.py
    I read through and watched the video about front-end API

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -2,7 +2,6 @@ import ivy
 import ivy.functional.frontends.tensorflow as ivy_tf
 
 # hann_window
-def hann_window(window_length, periodic=True, dtype=ivy_tf.dtypes.float32, name=None): # ivy dtype is a better choice here
-    # you need to either implement it using compositions, or wait for ivy's own version of this function to be implemented
-    # more details in the deep dive document https://lets-unify.ai/ivy/deep_dive/16_ivy_frontends.html#ivy-frontends
-    pass
+
+def hann_window(window_length, periodic=True, dtype=ivy_tf.dtypes.float32, name=None):
+    return ivy.hann_window(window_length, periodic, dtype)
