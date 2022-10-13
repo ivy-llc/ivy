0ea34718715cab745b9fea9c248c611190c7144b
omerf <omerfarukballi@gmail.com>
2022-10-13 15:42:34 +0300

    isreal function update

--- ivy/functional/frontends/numpy/logic/array_type_testing.py
+++ ivy/functional/frontends/numpy/logic/array_type_testing.py
@@ -61,3 +61,21 @@ def isnan(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
+@handle_numpy_casting_special
+@to_ivy_arrays_and_back
+def isreal(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.isreal(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
