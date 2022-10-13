674c0fa9a5c330064431a0e43a2dddba43e13739
Sagnik Roy <58679327+sagnik1511@users.noreply.github.com>
2022-10-13 14:29:08 +0000

    added rint for numpy frontend[2]

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -3,15 +3,11 @@ import ivy
 
 # local
 from ivy.func_wrapper import from_zero_dim_arrays_to_float
-from ivy.functional.frontends.numpy.func_wrapper import (
-    to_ivy_arrays_and_back,
-    handle_numpy_casting,
-)
+from ivy.functional.frontends.numpy.func_wrapper import handle_numpy_casting
 
 
 @from_zero_dim_arrays_to_float
 @handle_numpy_casting
-@to_ivy_arrays_and_back
 def ceil(
     x,
     /,
@@ -29,7 +25,6 @@ def ceil(
     return ret
 
 
-@to_ivy_arrays_and_back
 def fix(
     x,
     /,
@@ -37,3 +32,22 @@ def fix(
 ):
     where = ivy.greater_equal(x, 0)
     return ivy.where(where, ivy.floor(x, out=out), ivy.ceil(x, out=out), out=out)
+
+
+@from_zero_dim_arrays_to_float
+@handle_numpy_casting
+def rint(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.rint(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
