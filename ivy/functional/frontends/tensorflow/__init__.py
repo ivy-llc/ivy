de8a0cd64bc0d578925fc2b690f8ac7a9cf347e9
JerryGCDing <13386637338@qq.com>
2022-10-13 13:38:12 -0400

    frontend tensorflow type promotion fix

--- ivy/functional/frontends/tensorflow/__init__.py
+++ ivy/functional/frontends/tensorflow/__init__.py
@@ -247,7 +247,7 @@ def promote_types_of_tensorflow_inputs(
     else:
         x1 = ivy.asarray(x1, dtype=x2.dtype)
         x2 = ivy.asarray(x2)
-    return ivy.to_native(x1), ivy.to_native(x2)
+    return x1, x2
 
 
 from . import tensor
