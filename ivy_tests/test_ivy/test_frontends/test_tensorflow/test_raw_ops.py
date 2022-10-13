a2a26997347b2ebd4110de9f07c16cc9c5c01b92
Ahmed Omar <40790298+ahmedo42@users.noreply.github.com>
2022-10-13 07:40:00 +0000

    tensorflow raw ops fixes

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -66,9 +66,7 @@ def test_tensorflow_Acosh(dtype_and_x, as_variable, num_positional_args, native_
         fn_name="ivy.functional.frontends.tensorflow.Add"
     ),
 )
-def test_tensorflow_Add(
-    dtype_and_x, as_variable, num_positional_args, native_array, fw
-):
+def test_tensorflow_Add(dtype_and_x, as_variable, num_positional_args, native_array):
     dtype, xs = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
@@ -254,9 +252,7 @@ def _dtypes(draw):
         fn_name="ivy.functional.frontends.tensorflow.Div"
     ),
 )
-def test_tensorflow_Div(
-    dtype_and_x, as_variable, num_positional_args, native_array, fw
-):
+def test_tensorflow_Div(dtype_and_x, as_variable, num_positional_args, native_array):
     dtype, xs = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
@@ -1011,7 +1007,7 @@ def test_tensorflow_Reshape(
         native_array_flags=native_array,
         frontend="tensorflow",
         fn_tree="raw_ops.Reshape",
-        tensor=x[0],
+        tensor=x,
         shape=shape,
     )
 
@@ -1264,7 +1260,7 @@ def test_tensorflow_Cumsum(
     helpers.test_frontend_function(
         input_dtypes=dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
+        with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend="tensorflow",
@@ -1900,7 +1896,6 @@ def test_tensorflow_Ceil(
     as_variable,
     num_positional_args,
     native_array,
-    fw,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -1909,7 +1904,6 @@ def test_tensorflow_Ceil(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="tensorflow",
         fn_tree="raw_ops.Ceil",
         x=x[0],
