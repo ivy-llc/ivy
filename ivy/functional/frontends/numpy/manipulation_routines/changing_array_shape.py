159cf1ef437e69f7641513ccbd393aba06698897
Sharique Shamim <64735973+sharique1006@users.noreply.github.com>
2022-10-13 18:18:46 +0530

    numpy frontend changing_array_shape, joining_arrays (#5657)
    * numpy frontend sorting
    
    * lint formatting
    
    * numpy frontend changing_array_shape, joining_arrays
    
    * lint formatting
    
    * lint formatting
    
    * lint formatting

--- ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_array_shape.py
@@ -6,3 +6,11 @@ from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 @to_ivy_arrays_and_back
 def reshape(x, /, newshape, order="C"):
     return ivy.reshape(x, shape=newshape)
+
+
+def broadcast_to(array, shape, subok=False):
+    return ivy.broadcast_to(array, shape)
+
+
+def ravel(a, order='C'):
+    return ivy.reshape(a, (-1,))
