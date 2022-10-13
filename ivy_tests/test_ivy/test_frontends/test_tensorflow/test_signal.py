337364f4701376487416e2097e18798ba542bb61
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-10-13 12:36:03 +0200

    Update test_signal.py
    Made change to test the function for hann_window

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -1,36 +1,44 @@
+# global
 import ivy
-from hypothesis import given, strategies as st
+import numpy as np
+from hypothesis import given, assume, strategies as st
+from ivy.functional.backends.numpy.data_type import dtype
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
-
 # hann_window
+
+
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
-    target_dtype=helpers.get_dtypes("float"),
-    periodic=st.booleans(),
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.signal.hann_window"
+        fn_name="ivy.functional.frontends.tensorflow.hann_window",
     ),
 )
-def test_tensorflow_hann_window(
-    dtype_and_x, target_dtype, as_variable, 
-    periodic, num_positional_args, native_array, fw
+def test_hann_window(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x , y = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="tensorflow",
-        fn_tree="signal.hann_window",
-        window_length=x[0],
-        periodic=periodic,
-        dtype=target_dtype,
-    )
+        fn_tree="hann_window",
+        window_length=x,
+        periodic=True,
+        dtype=y
 
+    )
