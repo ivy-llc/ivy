4a21f8715fb9557c6e6ff2b727bdddc7fe8f16df
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-13 23:43:38 +0530

    Adding some tensorflow frontend activations (#5551)

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_activations.py
@@ -115,7 +115,7 @@ def test_tensorflow_softmax(
     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
     approximate=st.booleans(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.gelu"
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.gelu"
     ),
 )
 def test_tensorflow_gelu(
@@ -128,8 +128,6 @@ def test_tensorflow_gelu(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        atol_=1e-2,
-        rtol_=1e-2,
         frontend="tensorflow",
         fn_tree="keras.activations.gelu",
         x=x[0],
@@ -143,7 +141,7 @@ def test_tensorflow_gelu(
         available_dtypes=helpers.get_dtypes("numeric")
     ),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.relu"
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.relu"
     ),
 )
 def test_tensorflow_relu(
@@ -158,5 +156,86 @@ def test_tensorflow_relu(
         native_array_flags=native_array,
         frontend="tensorflow",
         fn_tree="keras.activations.relu",
+        x=x[0],
+    )
+
+
+# softplus
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.softplus"
+    ),
+)
+def test_tensorflow_softplus(
+    dtype_and_x, as_variable, num_positional_args, native_array
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="keras.activations.softplus",
+        rtol=1e-2,
+        atol=1e-2,
+        x=x[0],
+    )
+
+
+# softsign
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.softsign"
+    ),
+)
+def test_tensorflow_softsign(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="keras.activations.softsign",
+        rtol=1e-2,
+        atol=1e-2,
+        x=x[0],
+    )
+
+
+# swish
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.keras.activations.swish"
+    ),
+)
+def test_tensorflow_swish(dtype_and_x, as_variable, num_positional_args, native_array):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="keras.activations.swish",
+        rtol=1e-2,
+        atol=1e-2,
         x=x[0],
     )
