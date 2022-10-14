385531e80a828084528fcc1ab36cf4f18543f0b0
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-10-14 18:13:45 +0530

    Updated execute_with_gradients to support multiple variables and structures with gradient indexing (#5559)

--- ivy/functional/backends/torch/gradients.py
+++ ivy/functional/backends/torch/gradients.py
@@ -2,12 +2,18 @@
 
 # global
 import torch
+import functorch
 import warnings
 from typing import Optional, Callable
 
 # local
 import ivy
-from ivy.functional.ivy.gradients import _unused_variables_to_zero_gradients
+from ivy.functional.ivy.gradients import (
+    _get_native_arrays_and_indices,
+    _forward_fn,
+    _zero_gradients_to_none_and_to_ivy,
+    _stop_grad_and_index,
+)
 
 
 def variable(x):
@@ -16,7 +22,7 @@ def variable(x):
     return x.clone().requires_grad_()
 
 
-def is_variable(x, exclusive: bool = False):
+def is_variable(x, /, *, exclusive: bool = False):
     return isinstance(x, torch.Tensor) and x.requires_grad
 
 
@@ -25,41 +31,49 @@ def variable_data(x):
 
 
 # noinspection PyShadowingNames
-def execute_with_gradients(func, xs, retain_grads=False):
+def execute_with_gradients(func, xs, /, *, retain_grads=False, grad_idxs=None):
     func_ret = func(xs)
-    if isinstance(func_ret, tuple):
-        y = func_ret[0]
-        rest = func_ret[1:]
-    else:
-        y = func_ret
-        rest = tuple()
     xs = ivy.to_native(xs)
-    y = ivy.to_native(y)
-    if isinstance(xs, ivy.Container):
-        x_grads_flat = list(
-            torch.autograd.grad(
-                [y],
-                [v for k, v in xs.to_iterator()],
+    arr_idxs, arr_values = _get_native_arrays_and_indices(func_ret)
+
+    if len(arr_values) == 1:
+        y = arr_values[0]
+    else:
+        y = arr_values
+
+    if isinstance(y, ivy.NativeArray):
+        if isinstance(xs, ivy.Container):
+            grads = xs.from_flat_list(
+                list(
+                    torch.autograd.grad(
+                        [y],
+                        [v for k, v in xs.to_iterator()],
+                        retain_graph=retain_grads,
+                        create_graph=retain_grads,
+                        allow_unused=True,
+                    )
+                )
+            )
+        else:
+            grads = torch.autograd.grad(
+                y,
+                xs,
                 retain_graph=retain_grads,
                 create_graph=retain_grads,
                 allow_unused=True,
-            )
-        )
-        grads = xs.from_flat_list(x_grads_flat)
+            )[0]
     else:
-        grads = torch.autograd.grad(
-            y,
-            xs,
-            retain_graph=retain_grads,
-            create_graph=retain_grads,
-            allow_unused=True,
-        )[0]
-    y = ivy.to_ivy(y)
-    grads = ivy.to_ivy(grads)
-    grads = _unused_variables_to_zero_gradients(grads, xs)
-    if not retain_grads:
-        y = ivy.stop_gradient(y)
-    return (y, grads, *rest)
+        if isinstance(xs, ivy.Container):
+            xs = xs.to_dict()
+        grad_func = functorch.jacrev(lambda x: _forward_fn(x, func))
+        grads_ = grad_func(xs)
+        if isinstance(xs, dict):
+            xs = ivy.Container(**xs)
+        grads = {arr_idxs[i]: grad for i, grad in enumerate(grads_)}
+
+    grads = _zero_gradients_to_none_and_to_ivy(grads)
+    grads = _stop_grad_and_index(y, retain_grads, grads, grad_idxs)
+    return func_ret, grads
 
 
 def value_and_grad(func):
@@ -77,7 +91,7 @@ def value_and_grad(func):
                 else ivy.to_native(ivy.zeros_like(ivy.to_ivy(x)))
             )
             grad = ivy.to_ivy(grad)
-            grad = _unused_variables_to_zero_gradients(grad, xs)
+            grad = _zero_gradients_to_none_and_to_ivy(grad)
             return grad
 
         grads = ivy.nested_map(
@@ -95,7 +109,7 @@ def stop_gradient(
     x: Optional[torch.Tensor],
     preserve_type: bool = True,
     *,
-    out: Optional[torch.Tensor] = None
+    out: Optional[torch.Tensor] = None,
 ):
     if is_variable(x) and preserve_type:
         with warnings.catch_warnings():
