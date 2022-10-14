2a34a645f518b86a6d34d3f1ab4396ea94aaaba7
Ayman Lafaz <ayman@aymanlafaz.xyz>
2022-10-14 22:28:37 +0100

    added initial implementation for tensorflow backend -- still needs improvements

--- ivy/functional/backends/tensorflow/extensions.py
+++ ivy/functional/backends/tensorflow/extensions.py
@@ -206,6 +206,7 @@ def isin(
     assume_unique: Optional[bool] = False,
     invert: Optional[bool] = False,
 ) -> tf.Tensor:
+
     if invert is True:
         return tf.math.logical_not(tf.math.equal(elements, test_elements))
     else:
