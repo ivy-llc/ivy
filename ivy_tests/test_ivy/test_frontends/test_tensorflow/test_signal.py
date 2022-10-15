bad9d1004bb3732302afdace8af192e31538f6e8
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-10-15 16:43:47 +0200

    Update test_signal.py
    Mad change on the "ivy.functional.frontends.tensorflow.signal.hann_window"  but could not figure out things on the what exactly is needed to the periodic and datatype.

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -1,4 +1,3 @@
-# global
 import ivy
 import numpy as np
 from hypothesis import given, assume, strategies as st
@@ -10,35 +9,45 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 # hann_window
 
-
-@handle_cmd_line_args
-@given(
+handle_cmd_line_args
+given(
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=2,
-        shared_dtype=True,
+        available_dtypes=helpers.get_dtypes("numeric")
     ),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.hann_window",
+        fn_name="ivy.functional.frontends.tensorflow.signal.hann_window"
     ),
 )
+
+
 def test_hann_window(
     dtype_and_x,
+    dtype,
+    where,
     as_variable,
+    with_out,
     num_positional_args,
     native_array,
+    fw,
 ):
-    input_dtype, x , y = dtype_and_x
+    input_dtype, x = dtype_and_x
+    where, as_variable, native_array = helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=False,
+        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
+        fw=fw,
         frontend="tensorflow",
         fn_tree="hann_window",
-        window_length=x,
-        periodic=True,
-        dtype=y
-
+        window_length=x[0],
+        periodic=x[1],
+        dtype=dtype[0],
     )
