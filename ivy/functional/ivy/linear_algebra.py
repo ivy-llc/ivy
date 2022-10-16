fd81247e720867e9f28b391f78128654b80b8064
amine_omairi <elomairiamine@gmail.com>
2022-10-16 21:07:44 +0100

    Reformat matrix_norm (#5173)
    Co-authored-by: juliagsy <67888047+juliagsy@users.noreply.github.com>

--- ivy/functional/ivy/linear_algebra.py
+++ ivy/functional/ivy/linear_algebra.py
@@ -898,9 +898,49 @@ def matrix_norm(
     Parameters
     ----------
     x
-        Input array.
+        Input array having shape (..., M, N) and whose innermost two dimensions
+        form MxN matrices. Should have a floating-point data type.
     ord
-        Order of the norm. Default is "fro".
+        order of the norm. The following mathematical norms must be supported:
+
+        +------------------+---------------------------------+
+        | ord              | description                     |
+        +==================+=================================+
+        | 'fro'            | Frobenius norm                  |
+        +------------------+---------------------------------+
+        | 'nuc'            | nuclear norm                    |
+        +------------------+---------------------------------+
+        | 1                | max(sum(abs(x), axis=0))        |
+        +------------------+---------------------------------+
+        | 2                | largest singular value          |
+        +------------------+---------------------------------+
+        | inf              | max(sum(abs(x), axis=1))        |
+        +------------------+---------------------------------+
+
+        The following non-mathematical "norms" must be supported:
+
+        +------------------+---------------------------------+
+        | ord              | description                     |
+        +==================+=================================+
+        | -1               | min(sum(abs(x), axis=0))        |
+        +------------------+---------------------------------+
+        | -2               | smallest singular value         |
+        +------------------+---------------------------------+
+        | -inf             | min(sum(abs(x), axis=1))        |
+        +------------------+---------------------------------+
+
+        If ``ord=1``, the norm corresponds to the induced matrix norm where 
+        ``p=1`` (i.e., the maximum absolute value column sum).
+
+        If ``ord=2``, the norm corresponds to the induced matrix norm where 
+        ``p=inf`` (i.e., the maximum absolute value row sum).
+
+        If ``ord=inf``, the norm corresponds to the induced matrix norm where 
+        ``p=2`` (i.e., the largest singular value).
+
+        Default: "fro".
+    axis
+        specifies the axes that hold 2-D matrices. Default: (-2, -1).
     keepdims
         If this is set to True, the axes which are normed over are left in the result as
         dimensions with size one. With this option the result will broadcast correctly
@@ -923,6 +963,62 @@ def matrix_norm(
     Both the description and the type hints above assumes an array input for simplicity,
     but this function is *nestable*, and therefore also accepts :class:`ivy.Container`
     instances in place of any of the arguments.
+
+    Examples
+    --------
+    With :class:'ivy.Array' inputs:
+    >>> x = ivy.array([[1., 2.], [3., 4.]])
+    >>> y = ivy.matrix_norm(x)
+    >>> print(y)
+    ivy.array(5.47722558)
+
+    >>> x = ivy.arange(8, dtype=float).reshape((2, 2, 2))
+    >>> y = ivy.zeros(2)
+    >>> ivy.matrix_norm(x, ord=1, out=y)
+    >>> print(y)
+    ivy.array([ 4., 12.])
+
+    >>> x = ivy.arange(12, dtype=float).reshape((3, 2, 2))
+    >>> ivy.matrix_norm(x, ord=ivy.inf, axis=(2, 1), out=x)
+    >>> print(x)
+    ivy.array([ 4., 12., 20.])
+
+    >>> x = ivy.array([[1.1, 2.2], [3.3, 4.4], [5.5, 6.6]])
+    >>> y = ivy.matrix_norm(x, ord='nuc', keepdims=True)
+    >>> print(y)
+    ivy.array([[11.]])
+
+    >>> x = ivy.array([[[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], \
+                        [[1., 0., 1.1], [1., 1., 0.]]])   
+    >>> ivy.matrix_norm(x, ord='fro', out=x)
+    >>> print(x)
+    ivy.array([10.5 ,  2.05])
+
+    With :class:'ivy.Container' input:
+    >>> x = ivy.Container(a=ivy.array([[0.666, 9.11], \
+                                       [42.69, 9.23]]), \
+                          b=ivy.array([[1.1, 2.2, 3.3], \
+                                       [4.4, 5.5, 6.6]]))   
+    >>> y = ivy.matrix_norm(x, ord=-ivy.inf)
+    >>> print(y)
+    {
+        a: ivy.array(9.776),
+        b: ivy.array(6.6000004)
+    }
+
+    With multiple :class:'ivy:Container' inputs:
+    >>> x = ivy.Container(a=ivy.arange(12, dtype=float).reshape((3, 2, 2)), \
+                          b=ivy.arange(8, dtype=float).reshape((2, 2, 2))) 
+    >>> ord = ivy.Container(a=1, b=float('inf'))
+    >>> axis = ivy.Container(a=(1, 2), b=(2, 1))
+    >>> k = ivy.Container(a=False, b=True)
+    >>> y = ivy.matrix_norm(x, ord=ord, axis=axis, keepdims=k)
+    >>> print(y)
+    {
+        a: ivy.array([4., 12., 20.]),
+        b: ivy.array([[[4.]], 
+                      [[12.]]])
+    }
 
     """
     return current_backend(x).matrix_norm(
