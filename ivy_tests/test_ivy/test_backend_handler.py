023dc83e7e947d15a5b9b5c66e7b7f38ffa62f85
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-10-17 14:13:37 +0100

    tests for current_backend and clear_backend_stack

--- ivy_tests/test_ivy/test_backend_handler.py
+++ ivy_tests/test_ivy/test_backend_handler.py
@@ -1,6 +1,11 @@
 # global
 import pytest
 import importlib
+import tensorflow as tf
+import torch
+import jax.numpy as jnp
+import numpy as np
+
 # local
 import ivy
 from ivy.backend_handler import _backend_dict
@@ -61,3 +66,32 @@ def test_unset_backend(backend):
     ivy.set_backend('torch')
     ivy.unset_backend()
     ivy.assertions.check_equal(ivy.current_backend_str(), backend)
+
+
+def test_clear_backend_stack():
+    for _ in range(3):
+        ivy.set_backend('numpy')
+        ivy.set_backend('tensorflow')
+        ivy.set_backend('torch')
+        ivy.set_backend('jax')
+
+    ivy.clear_backend_stack()
+    ivy.assertions.check_equal(ivy.backend_stack, [])
+
+
+@pytest.mark.parametrize(("backend", "array_type"),
+                         [("numpy", np.array(3.)),
+                          ("tensorflow",
+                           tf.constant([3.])),
+                          ("torch", torch.tensor([3.])),
+                          ("jax", jnp.array(3.))])
+def test_current_backend(backend, array_type):
+    # test backend inference from arguments when stack clear
+    ivy.clear_backend_stack()
+    ivy.assertions.check_equal(ivy.current_backend(array_type),
+                               importlib.import_module(_backend_dict[backend]))
+
+    # global_backend > argument's backend.
+    ivy.set_backend('torch')
+    ivy.assertions.check_equal(ivy.current_backend(array_type),
+                               importlib.import_module(_backend_dict['torch']))
