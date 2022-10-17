22a5c989fc48df375207db8b4523bcf7e9c26299
sherry30 <sherrytst30@gmail.com>
2022-10-17 14:30:54 +0500

    `contiguous` for torch frontend

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -3,6 +3,7 @@ import torch
 from hypothesis import assume, given, strategies as st
 
 # local
+from ivy.functional.frontends.torch.Tensor import Tensor
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
@@ -434,6 +435,41 @@ def test_torch_instance_tan(
         frontend="torch",
         class_name="tensor",
         method_name="tan",
+    )
+
+
+# contiguous
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        allow_inf=False,
+    ),
+)
+def test_torch_instance_contiguous(
+    dtype_and_x,
+    as_variable,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        input_dtypes_init=input_dtype,
+        as_variable_flags_init=as_variable,
+        num_positional_args_init=1,
+        native_array_flags_init=native_array,
+        all_as_kwargs_np_init={
+            "data": x[0],
+        },
+        input_dtypes_method=input_dtype,
+        as_variable_flags_method=as_variable,
+        num_positional_args_method=0,
+        native_array_flags_method=native_array,
+        all_as_kwargs_np_method={
+            "memory_format": torch.contiguous_format,
+        },
+        frontend="torch",
+        class_name="tensor",
+        method_name="contiguous",
     )
 
 
