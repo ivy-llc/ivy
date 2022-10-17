f1444b77083bcc49ed2077fafc71fd918f0f63a4
Kareem Morsy <kreem.morsy@hotmail.com>
2022-10-18 00:20:04 +0200

    Fix tests

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -2025,17 +2025,14 @@ def test_tensorflow_Relu6(dtype_and_x, as_variable, native_array):
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.Round"
-    ),
 )
-def test_tensorflow_Round(dtype_and_x, as_variable, num_positional_args, native_array):
+def test_tensorflow_Round(dtype_and_x, as_variable, native_array):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
-        num_positional_args=num_positional_args,
+        num_positional_args=0,
         native_array_flags=native_array,
         frontend="tensorflow",
         fn_tree="raw_ops.Round",
