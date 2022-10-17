e1c5c48f254e2300ee942c33a67724a851eca182
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-10-17 18:41:07 +0100

    using decorator for unsupported dtypes

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -232,9 +232,9 @@ def vander(x, N=None, increasing=False):
     else:
         return ivy.vander(x, N=N, increasing=increasing, out=None)
 
-
+@with_unsupported_dtypes(
+    {"1.11.0 and below": ("int8",)}, versions["torch"]
+)
 def lcm(input, other, *, out=None):
     return ivy.lcm(input, other, out=out)
 
-
-lcm.unsupported_dtypes = ("int8",)
