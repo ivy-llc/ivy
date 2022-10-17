98f775a492af21363594413cb2edd32ec1e55340
nassimberrada <Nassim>
2022-10-17 16:58:24 +0100

    kaiser_bessel

--- ivy/functional/backends/tensorflow/extensions.py
+++ ivy/functional/backends/tensorflow/extensions.py
@@ -215,3 +215,21 @@ def median(
         interpolation="midpoint",
         keepdims=keepdims,
     )
+
+
+def kaiser_bessel_window(
+    window_length: int,
+    periodic: bool = True,
+    beta: float = 12.0,
+    *,
+    dtype: Optional[tf.DType] = None,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    if periodic is True:
+        return tf.signal.kaiser_bessel_derived_window(
+            window_length + 1, beta, dtype, name=None
+        )[:-1]
+    else:
+        return tf.signal.kaiser_bessel_derived_window(
+            window_length, beta, dtype, name=None
+        )
