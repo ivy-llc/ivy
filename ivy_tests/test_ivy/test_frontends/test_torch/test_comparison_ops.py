93e53a9cc51e3c6bb9f01cbe4ee88867eb52682a
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-18 16:26:51 +0000

    added alias testing

--- ivy_tests/test_ivy/test_frontends/test_torch/test_comparison_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_comparison_ops.py
@@ -211,6 +211,8 @@ def test_torch_greater(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=with_out,
+        has_aliases=True,
+        all_aliases=["gt"],
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend="torch",
