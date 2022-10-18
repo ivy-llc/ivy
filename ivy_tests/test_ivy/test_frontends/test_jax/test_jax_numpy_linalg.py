91810575077ce816b66e70aaec6b689c67e90c55
modanesh <mohamad4danesh@gmail.com>
2022-10-18 11:28:50 +0330

    spacing and indentation correction

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -48,9 +48,9 @@ def test_jax_numpy_det(dtype_and_x, as_variable, native_array, num_positional_ar
         shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
     ).filter(
         lambda x: "float16" not in x[0]
-                  and "bfloat16" not in x[0]
-                  and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
-                  and np.linalg.det(np.asarray(x[1][0])) != 0
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.linalg.eigh"
@@ -59,12 +59,12 @@ def test_jax_numpy_det(dtype_and_x, as_variable, native_array, num_positional_ar
     symmetrize_input=st.booleans(),
 )
 def test_jax_numpy_eigh(
-        dtype_and_x,
-        as_variable,
-        native_array,
-        num_positional_args,
-        UPLO,
-        symmetrize_input,
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
+    UPLO,
+    symmetrize_input,
 ):
     dtype, x = dtype_and_x
     x = np.array(x[0], dtype=dtype[0])
@@ -107,9 +107,9 @@ def test_jax_numpy_eigh(
         shape=helpers.ints(min_value=1, max_value=10).map(lambda x: tuple([x, x])),
     ).filter(
         lambda x: "float16" not in x[0]
-                  and "bfloat16" not in x[0]
-                  and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
-                  and np.linalg.det(np.asarray(x[1][0])) != 0
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.linalg.inv"
@@ -141,9 +141,9 @@ def test_jax_numpy_inv(dtype_and_x, as_variable, native_array, num_positional_ar
         shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
     ).filter(
         lambda x: "float16" not in x[0]
-                  and "bfloat16" not in x[0]
-                  and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
-                  and np.linalg.det(np.asarray(x[1][0])) != 0
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.linalg.eigvalsh"
@@ -151,11 +151,11 @@ def test_jax_numpy_inv(dtype_and_x, as_variable, native_array, num_positional_ar
     UPLO=st.sampled_from(("L", "U")),
 )
 def test_jax_numpy_eigvalsh(
-        dtype_and_x,
-        as_variable,
-        native_array,
-        num_positional_args,
-        UPLO,
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
+    UPLO,
 ):
     dtype, x = dtype_and_x
     x = np.asarray(x[0], dtype=dtype[0])
@@ -194,11 +194,11 @@ def test_jax_numpy_eigvalsh(
     mode=st.sampled_from(("reduced", "complete")),
 )
 def test_jax_numpy_qr(
-        dtype_and_x,
-        mode,
-        as_variable,
-        native_array,
-        num_positional_args,
+    dtype_and_x,
+    mode,
+    as_variable,
+    native_array,
+    num_positional_args,
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -226,19 +226,19 @@ def test_jax_numpy_qr(
         shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
     ).filter(
         lambda x: "float16" not in x[0]
-                  and "bfloat16" not in x[0]
-                  and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
-                  and np.linalg.det(np.asarray(x[1][0])) != 0
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.linalg.eigvals"
     ),
 )
 def test_jax_numpy_eigvals(
-        dtype_and_x,
-        as_variable,
-        native_array,
-        num_positional_args,
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
 ):
     dtype, x = dtype_and_x
     x = np.array(x[0], dtype=dtype[0])
@@ -268,20 +268,20 @@ def test_jax_numpy_eigvals(
         shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
     ).filter(
         lambda x: "float16" not in x[0]
-                  and "bfloat16" not in x[0]
-                  and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
-                  and np.linalg.det(x[1][0]) != 0
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(x[1][0]) != 0
     ),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.linalg.cholesky"
     ),
 )
 def test_jax_numpy_cholesky(
-        dtype_and_x,
-        as_variable,
-        native_array,
-        num_positional_args,
-        fw,
+    dtype_and_x,
+    as_variable,
+    native_array,
+    num_positional_args,
+    fw,
 ):
     dtype, x = dtype_and_x
     x = np.asarray(x[0], dtype=dtype[0])
@@ -319,10 +319,10 @@ def test_jax_numpy_cholesky(
     ),
 )
 def test_jax_slogdet(
-        dtype_and_x,
-        as_variable,
-        num_positional_args,
-        native_array,
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -346,10 +346,7 @@ def test_jax_slogdet(
     ),
 )
 def test_jax_numpy_matrix_rank(
-    dtype_and_x,
-    as_variable,
-    native_array,
-    num_positional_args
+    dtype_and_x, as_variable, native_array, num_positional_args
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
