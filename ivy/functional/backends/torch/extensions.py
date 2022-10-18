d3d4d1a873fb7b9e16f7175fd1d67b70b8891a3f
danielmunioz <juandaniel.munoz@fhnw.ch>
2022-10-17 21:31:15 -0400

    Adding signal padding

--- ivy/functional/backends/torch/extensions.py
+++ ivy/functional/backends/torch/extensions.py
@@ -308,7 +308,14 @@ def flipud(
 flipud.support_native_out = False
 
 
-def dct(x: torch.Tensor, type: int = 2, norm: str = "ortho"):
+def dct(x: torch.Tensor, type: int = 2, n: int = None, norm: str = "ortho"):
+    if n is not None:
+        signal_len = x.shape[-1]
+        if n <= signal_len:
+            x = x[..., :n]
+        else:
+            # padding at dim=-1
+            x = torch.nn.functional.pad(x, (0, n - signal_len))
     real_zero = torch.tensor(0.0, dtype=x.dtype)
     axis_dim = x.shape[-1]
     axis_dim_float = torch.tensor(axis_dim, dtype=x.dtype)
