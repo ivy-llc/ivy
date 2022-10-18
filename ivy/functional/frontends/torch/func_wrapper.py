3b3251e49fd66ad4e13b0ee58ffe22f7718cd7c8
Ankit Biswas <ankitbiswas1101@gmail.com>
2022-10-18 21:22:27 +0000

    fixed linting errors

--- ivy/functional/frontends/torch/func_wrapper.py
+++ ivy/functional/frontends/torch/func_wrapper.py
@@ -18,7 +18,11 @@ def _from_torch_frontend_tensor_to_ivy_array(x):
 
 def _from_ivy_array_to_torch_frontend_tensor(x, nested=False, include_derived=None):
     if nested:
-        return ivy.nested_map(x, _from_ivy_array_to_torch_frontend_tensor, include_derived)
+        return ivy.nested_map(
+            x, 
+            _from_ivy_array_to_torch_frontend_tensor, 
+            include_derived
+        )
     elif isinstance(x, ivy.Array) or ivy.is_native_array(x):
         return torch_frontend.Tensor(x)
     return x
@@ -41,10 +45,14 @@ def inputs_to_ivy_arrays(fn: Callable) -> Callable:
             has_out = True
         # convert all input arrays to ivy.Array instances
         new_args = ivy.nested_map(
-            args, _from_torch_frontend_tensor_to_ivy_array, include_derived={tuple: True}
+            args, 
+            _from_torch_frontend_tensor_to_ivy_array, 
+            include_derived={tuple: True}
         )
         new_kwargs = ivy.nested_map(
-            kwargs, _from_torch_frontend_tensor_to_ivy_array, include_derived={tuple: True}
+            kwargs, 
+            _from_torch_frontend_tensor_to_ivy_array, 
+            include_derived={tuple: True}
         )
         # add the original out argument back to the keyword arguments
         if has_out:
