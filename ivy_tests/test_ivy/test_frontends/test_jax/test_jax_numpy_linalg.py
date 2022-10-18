780cb9e697740f59bf955cf5d6293609042db7c3
Omair Jadoon <ukj02@proton.me>
2022-10-18 04:09:34 -0400

    Added matrix_rank function to jax.numpy.linalg  (#5775)
    Co-authored-by: modanesh <mohamad4danesh@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -10,6 +10,7 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import assert_all_close, handle_cmd_line_args
 from ivy_tests.test_ivy.test_functional.test_core.test_linalg import (
     _get_dtype_and_matrix,
+    _matrix_rank_helper,
 )
 
 
@@ -333,4 +334,28 @@ def test_jax_slogdet(
         frontend="jax",
         fn_tree="numpy.linalg.slogdet",
         a=np.asarray(x[0], dtype=input_dtype[0]),
+    )
+
+
+# matrix_rank
+@handle_cmd_line_args
+@given(
+    dtype_and_x=_matrix_rank_helper(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.linalg.matrix_rank"
+    ),
+)
+def test_jax_numpy_matrix_rank(
+    dtype_and_x, as_variable, native_array, num_positional_args
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.linalg.matrix_rank",
+        M=x[0],
     )
