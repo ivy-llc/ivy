bd33c9436ce915e31ece4815fdcbb36909925e05
Ankit Biswas <ankitbiswas1101@gmail.com>
2022-10-19 04:21:10 +0530

    Added frontend array decorator functions to pytorch (#5879)
    added frontend array decorators for pytorch

--- ivy/functional/frontends/torch/random_sampling.py
+++ ivy/functional/frontends/torch/random_sampling.py
@@ -1,5 +1,7 @@
 import ivy
+from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
+@to_ivy_arrays_and_back
 def multinomial(input, num_samples, replacement=False, *, generator=None, out=None):
     return ivy.multinomial(input, num_samples, replace=replacement, out=out)
