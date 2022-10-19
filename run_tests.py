1f0ae49261fdb9725b6e97a3ad4909458dda789f
RashulChutani <rashul.chutani@gmail.com>
2022-10-19 22:40:17 +0530

    Bug Fixes Test Coverage [skip ci]

---
+++ run_tests.py
@@ -0,0 +1,18 @@
+# Run Tests
+import os
+
+if __name__ == "__main__":
+    failed = False
+    with open("tests_to_run", "r") as f:
+        for line in f:
+            test = line[:-1]
+            print(test)
+            ret = os.system(
+                f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest {test}'
+                # noqa
+            )
+            if ret != 0:
+                failed = True
+
+    if failed:
+        exit(1)
