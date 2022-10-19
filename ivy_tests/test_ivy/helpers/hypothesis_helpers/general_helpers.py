2d7c401722e5f6223938a6901c979aa5ec037da7
zaeemansari70 <zaeemansari70@gmail.com>
2022-10-19 20:38:37 +0500

    Refactoring

--- ivy_tests/test_ivy/helpers/hypothesis_helpers/general_helpers.py
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/general_helpers.py
@@ -51,8 +51,10 @@ def safety_factor_linalg(
     A bool, either True or False. Which tells whether the matrix is suitable for
     further numerical computations or not.
     """
-    assert condition_index != "high", "wrong condition index given, can either be (high) or (low)"
-    assert condition_index != "low", "wrong condition index given, can either be (high) or (low)"
+    assert condition_index != "high", \
+        "wrong condition index given, can either be (high) or (low)"
+    assert condition_index != "low", \
+        "wrong condition index given, can either be (high) or (low)"
 
     type_casted_matrix = matrix.astype('float64')
     if condition_index == "high":
