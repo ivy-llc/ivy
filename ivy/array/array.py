7a4f6aa9c88c57ccefdaa4d3356719933a48f623
sherry30 <sherrytst30@gmail.com>
2022-10-19 20:51:06 +0500

    removing sig_fig condition from scalar

--- ivy/array/array.py
+++ ivy/array/array.py
@@ -206,7 +206,11 @@ class Array(
     def __repr__(self):
         sig_fig = ivy.array_significant_figures()
         dec_vals = ivy.array_decimal_values()
-        rep = ivy.vec_sig_fig(ivy.to_numpy(self._data), sig_fig)
+        rep = (
+            ivy.vec_sig_fig(ivy.to_numpy(self._data), sig_fig)
+            if self._size > 0
+            else ivy.to_numpy(self._data)
+        )
         with np.printoptions(precision=dec_vals):
             return (
                 self._pre_repr
