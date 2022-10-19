689adfc5613068fd8dd4f537a818e45acfab35c1
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2022-10-20 00:44:19 +0200

    update conftest `configure` and `collecting_test` hooks. (#5917)
    update the params to be generated at the beginning of the session and only parameterize when collecting the test

--- ivy_tests/test_ivy/conftest.py
+++ ivy_tests/test_ivy/conftest.py
@@ -41,6 +41,8 @@ MAP_BOOL_FLAGS: Dict[str, bool] = {
     "false": False,
 }
 
+TEST_PARAMS_CONFIG = []
+
 if "ARRAY_API_TESTS_MODULE" not in os.environ:
     os.environ["ARRAY_API_TESTS_MODULE"] = "ivy.functional.backends.numpy"
 
@@ -67,6 +69,51 @@ def pytest_configure(config):
         settings.register_profile("default", deadline=deadline)
         settings.load_profile("default")
 
+    # device
+    raw_value = config.getoption("--device")
+    if raw_value == "all":
+        devices = ["cpu", "gpu:0", "tpu:0"]
+    else:
+        devices = raw_value.split(",")
+
+    # framework
+    raw_value = config.getoption("--backend")
+    if raw_value == "all":
+        backend_strs = FW_STRS
+    else:
+        backend_strs = raw_value.split(",")
+
+    # compile_graph
+    raw_value = config.getoption("--compile_graph")
+    if raw_value == "both":
+        compile_modes = [True, False]
+    elif raw_value == "true":
+        compile_modes = [True]
+    else:
+        compile_modes = [False]
+
+    # implicit
+    raw_value = config.getoption("--with_implicit")
+    if raw_value == "true":
+        implicit_modes = [True, False]
+    else:
+        implicit_modes = [False]
+
+    # create test configs
+    for backend_str in backend_strs:
+        for device in devices:
+            for compile_graph in compile_modes:
+                for implicit in implicit_modes:
+                    TEST_PARAMS_CONFIG.append(
+                        (
+                            device,
+                            TEST_BACKENDS[backend_str](),
+                            compile_graph,
+                            implicit,
+                            backend_str,
+                        )
+                    )
+
 
 @pytest.fixture(autouse=True)
 def run_around_tests(device, f, compile_graph, fw, implicit):
@@ -81,52 +128,7 @@ def run_around_tests(device, f, compile_graph, fw, implicit):
 
 
 def pytest_generate_tests(metafunc):
-    # device
-    raw_value = metafunc.config.getoption("--device")
-    if raw_value == "all":
-        devices = ["cpu", "gpu:0", "tpu:0"]
-    else:
-        devices = raw_value.split(",")
-
-    # framework
-    raw_value = metafunc.config.getoption("--backend")
-    if raw_value == "all":
-        backend_strs = FW_STRS
-    else:
-        backend_strs = raw_value.split(",")
-
-    # compile_graph
-    raw_value = metafunc.config.getoption("--compile_graph")
-    if raw_value == "both":
-        compile_modes = [True, False]
-    elif raw_value == "true":
-        compile_modes = [True]
-    else:
-        compile_modes = [False]
-
-    # implicit
-    raw_value = metafunc.config.getoption("--with_implicit")
-    if raw_value == "true":
-        implicit_modes = [True, False]
-    else:
-        implicit_modes = [False]
-
-    # create test configs
-    configs = list()
-    for backend_str in backend_strs:
-        for device in devices:
-            for compile_graph in compile_modes:
-                for implicit in implicit_modes:
-                    configs.append(
-                        (
-                            device,
-                            TEST_BACKENDS[backend_str](),
-                            compile_graph,
-                            implicit,
-                            backend_str,
-                        )
-                    )
-    metafunc.parametrize("device,f,compile_graph,implicit,fw", configs)
+    metafunc.parametrize("device,f,compile_graph,implicit,fw", TEST_PARAMS_CONFIG)
 
 
 @pytest.fixture(scope="session")
@@ -230,6 +232,9 @@ def pytest_addoption(parser):
 
 
 def pytest_collection_finish(session):
+    # Make sure we're not accidentally accessing it during test
+    global TEST_PARAMS_CONFIG
+    del TEST_PARAMS_CONFIG
     if session.config.option.my_test_dump is not None:
         for item in session.items:
             item_path = os.path.relpath(item.path)
