cb9591158022d8b47b3de09de09c9864807dcf0a
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-10-20 12:05:20 +0200

    Update signal.py
    Changed the dytpe

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -3,5 +3,7 @@ import ivy.functional.frontends.tensorflow as ivy_tf
 
 # hann_window
 
-def hann_window(window_length, periodic=True, dtype=ivy_tf.dtypes.float32, name=None):
+
+def hann_window(window_length, periodic=True, dtype=ivy.int32, name=None):
     return ivy.hann_window(window_length, periodic, dtype)
+
