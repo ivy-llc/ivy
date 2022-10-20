a900b31cfa5f0c854082b915f884be0c17a62598
lumi232 <110121029@nitt.edu>
2022-10-20 21:05:40 +0530

    added test for remainder numpy

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
@@ -434,3 +434,40 @@ def test_numpy_floor_divide(
         subok=True,
         test_values=False,
     )
+
+def test_remainder(
+    dtype_and_x,
+    where,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+):
+    input_dtypes, xs = dtype_and_x
+    dtype, input_dtypes, casting = np_frontend_helpers.handle_dtype_and_casting(
+        dtypes=input_dtypes,
+        get_dtypes_kind="numeric",
+    )
+    where, as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtypes,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="numpy",
+        fn_tree="remainder",
+        x1=xs[0],
+        x2=xs[1],
+        out=None,
+        where=where,
+        casting=casting,
+        order="K",
+        dtype=dtype,
+        subok=True,
+    )
