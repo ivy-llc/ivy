6beb2bbce4240553107354bd3fa76dce0a076d4a
Mwape  Bunda Timothy <mwapebt@gmail.com>
2022-10-21 18:30:48 +0200

    Update signal.py

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -1,7 +1,8 @@
+# global
 import ivy
 
-# hann_ window
 
+@hann_window
 def hann_window(window_length, periodic=True, dtype=ivy.int32, name=None):
     return ivy.hann_window(window_length, periodic, dtype, name)
 
