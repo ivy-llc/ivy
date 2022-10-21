Added additional tests. Fixed failing tests that were silently passing. Reimplemented function from scratch for torch backend.
fix:
    -torch\layers.py:
       -reimplemented dropout1d from scratch for compatibility with earlier torch versions.
       Similar to other backends, a Bernoulli r.v. is generated with the appropriate noise_shape using torch.rand > prob and then passed to torch.where as a mask.

    -ivy\array.py, test_nn\test_layers.py:
       -removed unused dtype argument

   -tensorflow\layers.py:
       - list(x.shape) instead of x.shape.as_list().

test:
    -test_nn\test_layers.py:
        -added a test for data_format
        -fixed failed tests

docs: fixed typo in ivy/layers.py

-Todo: switch to native dropout1d for pytorch backend once torch version is updated.

ivy/functional/backends/tensorflow/layers.py
@@ -452,7 +452,7 @@ def dropout1d(
         if data_format == "NCW":
             perm = (0, 2, 1) if len(x.shape) == 3 else (1, 0)
             x = tf.transpose(x, perm)
-        noise_shape = x.shape.as_list()
+        noise_shape = list(x.shape)
         noise_shape[-2] = 1
         res = tf.nn.dropout(x, prob, noise_shape=noise_shape)
         if data_format == "NCW":

ivy/functional/backends/torch/layers.py
@@ -688,10 +688,17 @@ def dropout1d(
     data_format: str = "NWC",
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
-    if data_format == "NWC":
-        perm = (0, 2, 1) if len(x.shape) == 3 else (1, 0)
-        x = torch.permute(x, perm)
-    res = torch.nn.functional.dropout1d(x, p=prob, training=training)
-    if data_format == "NWC":
-        res = torch.permute(res, perm)
-    return res
+    if training:
+        if data_format == "NWC":
+            perm = (0, 2, 1) if len(x.shape) == 3 else (1, 0)
+            x = torch.permute(x, perm)
+        # ToDo: switch to native dropout1d once torch version is updated.
+        noise_shape = list(x.shape)
+        noise_shape[-1] = 1
+        mask = torch.rand(noise_shape) > prob
+        res = torch.where(mask, x / (1 - prob), 0)
+        if data_format == "NWC":
+            res = torch.permute(res, perm)
+        return res
+    else:
+      return x