7e2a7c64e4782fa7300aa49d535b5ef72f5e8b28
Khadija Mahmoud <37675645+khadija267@users.noreply.github.com>
2022-10-21 13:18:08 +0200

    add var

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -26,3 +26,16 @@ def mean(
 
 
 mean.unsupported_dtypes = {"torch": ("float16",)}
+
+def var(a,/, axis=None, dtype=None, out=None, ddof=0, keepdims=False, *, where=True):
+
+    axis = tuple(axis) if isinstance(axis, list) else axis
+    if dtype:
+        a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+
+    ret = ivy.var(a, axis=axis, keepdims=keepdims, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+var.unsupported_dtypes = {"torch": ("float16",)}
+
