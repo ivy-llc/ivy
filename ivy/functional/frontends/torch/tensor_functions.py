b261eca3152a4e9c44d2208a87b4c0620b0e533a
TahaTasleem <mtaha306954@gmail.com>
2022-10-21 20:08:14 +0500

    Developed storage function

--- ivy/functional/frontends/torch/tensor_functions.py
+++ ivy/functional/frontends/torch/tensor_functions.py
@@ -1,15 +1,10 @@
 # local
 import ivy
 from ivy.functional.frontends.torch.Tensor import Tensor
-
-
 # TODO: Once the PyTorch Frontend Array Decorators are added,
 #  casting to Tensor before returning should be removed as redundant.
-
-
 def is_tensor(obj):
     return Tensor(ivy.is_array(obj))
-
 
 def is_storage(obj):
 	return Tensor(ivy.is_storage(obj))
@@ -20,7 +15,6 @@ def is_storage(obj):
 # def is_conj(obj):
 # 	return ivy.is_conj(obj)
 
-
 def numel(input):
     ivy.assertions.check_true(
         is_tensor(input),
@@ -28,14 +22,12 @@ def numel(input):
     )
     return Tensor(input.size)
 
-
 def is_floating_point(input):
     ivy.assertions.check_true(
         is_tensor(input),
         message="input must be a tensor",
     )
     return Tensor(ivy.is_float_dtype(input))
-
 
 def is_nonzero(input):
     ivy.assertions.check_equal(
