b0f185bf04ad52a51bac47953edff96e4cfba275
MuokaPWambua <muokapwambua@gmail.com>
2022-10-22 21:52:02 +0300

    add dct frontend api

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -1,6 +1,8 @@
 import ivy
 import ivy.functional.frontends.tensorflow as tf_frontend
+from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
 
 
+@to_ivy_arrays_and_back
 def dct(input, type=2, n=None, axis=-1, norm=None, name=None ):
         return ivy.dct(input, type, n, axis, norm, name)
