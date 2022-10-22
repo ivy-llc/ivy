34b7f4da5ed08bfaab39e3c43a4a5f4f684d5808
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2022-10-22 20:04:18 +0000

    fixing tests

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_solving_equations_and_inverting_matrices.py
@@ -97,30 +97,28 @@ def test_numpy_pinv(dtype_and_x, as_variable, native_array, num_positional_args)
 
 
 # tensorsolve
-
-
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
+    dtype_and_vals=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True,
         min_num_dims=2,
-        shape=helpers.ints(min_value=2).map(lambda x: tuple([x, x])),
-    ),
-    dtype_and_y=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=1,
-    ),
+        min_value=-1e05,
+        max_value=1e05,
+    ).filter(lambda x: (x[1][0].shape[-2] == x[1][0].shape[-1])
+             and (np.prod(x[1][0].shape[x[1][1].ndim:]) == np.prod(x[1][0].shape[:x[1][1].ndim]))),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.linalg.tensorsolve"
     ),
 )
 def test_numpy_tensorsolve(
-    dtype_and_x, dtype_and_y, as_variable, native_array, num_positional_args
+    dtype_and_vals, as_variable, native_array, num_positional_args
 ):
-    dtype1, x1 = dtype_and_x
-    dtype2, x2 = dtype_and_y
+    dtypes, x = dtype_and_vals
+    x1, x2 = x
     helpers.test_frontend_function(
-        input_dtypes=[dtype1, dtype2],
+        input_dtypes=dtypes,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
