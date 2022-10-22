a881c80adc42688694d317835ab31fc7ba4c48ab
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-22 16:50:49 +0100

    ivy pad backend consistency (#5710)

--- ivy/functional/backends/jax/extensions/layers.py
+++ ivy/functional/backends/jax/extensions/layers.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, Tuple, Callable, Literal, Sequence
+from typing import Optional, Union, Tuple, Callable, Literal, Sequence, Any
 from numbers import Number
 import ivy
 from ivy.functional.backends.jax import JaxArray
@@ -98,14 +98,28 @@ def max_pool2d(
     return res
 
 
+def kaiser_window(
+    window_length: int,
+    periodic: bool = True,
+    beta: float = 12.0,
+    *,
+    dtype: Optional[jnp.dtype] = None,
+    out: Optional[JaxArray] = None,
+) -> JaxArray:
+    if periodic is False:
+        return jnp.array(jnp.kaiser(M=window_length, beta=beta), dtype=dtype)
+    else:
+        return jnp.array(jnp.kaiser(M=window_length + 1, beta=beta)[:-1], dtype=dtype)
+
+
 def _flat_array_to_1_dim_array(x):
     return x.reshape((1,)) if x.shape == () else x
 
 
 def pad(
-    x: JaxArray,
-    /,
+    input: JaxArray,
     pad_width: Union[Sequence[Sequence[int]], JaxArray, int],
+    /,
     *,
     mode: Optional[
         Union[
@@ -130,52 +144,46 @@ def pad(
     end_values: Optional[Union[Sequence[Sequence[Number]], Number]] = 0,
     reflect_type: Optional[Literal["even", "odd"]] = "even",
     out: Optional[JaxArray] = None,
+    **kwargs: Optional[Any],
 ) -> JaxArray:
+    if callable(mode):
+        return jnp.pad(
+            _flat_array_to_1_dim_array(input),
+            pad_width,
+            mode=mode,
+            **kwargs,
+        )
     if mode in ["maximum", "mean", "median", "minimum"]:
         return jnp.pad(
-            _flat_array_to_1_dim_array(x),
+            _flat_array_to_1_dim_array(input),
             pad_width,
             mode=mode,
             stat_length=stat_length,
         )
     elif mode == "constant":
         return jnp.pad(
-            _flat_array_to_1_dim_array(x),
+            _flat_array_to_1_dim_array(input),
             pad_width,
             mode=mode,
             constant_values=constant_values,
         )
     elif mode == "linear_ramp":
         return jnp.pad(
-            _flat_array_to_1_dim_array(x),
+            _flat_array_to_1_dim_array(input),
             pad_width,
             mode=mode,
             end_values=end_values,
         )
     elif mode in ["reflect", "symmetric"]:
         return jnp.pad(
-            _flat_array_to_1_dim_array(x),
+            _flat_array_to_1_dim_array(input),
             pad_width,
             mode=mode,
             reflect_type=reflect_type,
         )
     else:
         return jnp.pad(
-            _flat_array_to_1_dim_array(x),
+            _flat_array_to_1_dim_array(input),
             pad_width,
             mode=mode,
         )
-
-
-def kaiser_window(
-    window_length: int,
-    periodic: bool = True,
-    beta: float = 12.0,
-    *,
-    dtype: Optional[jnp.dtype] = None,
-    out: Optional[JaxArray] = None,
-) -> JaxArray:
-    if periodic is False:
-        return jnp.array(jnp.kaiser(M=window_length, beta=beta), dtype=dtype)
-    else:
-        return jnp.array(jnp.kaiser(M=window_length + 1, beta=beta)[:-1], dtype=dtype)
