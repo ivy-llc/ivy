73ed6345c8fa15b22a7b339727229b70cee0bf84
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-22 16:47:09 +0100

    jax_flipud (#5908)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -1377,3 +1377,38 @@ def test_jax_numpy_moveaxis(
         source=source,
         destination=destination,
     )
+
+
+# flipud
+@handle_cmd_line_args
+@given(
+    dtype_and_m=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=-100,
+        max_value=100,
+        min_num_dims=1,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_dim_size=3,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.flipud"
+    ),
+)
+def test_jax_numpy_flipud(
+    dtype_and_m,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, m = dtype_and_m
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.flipud",
+        m=np.asarray(m[0], dtype=input_dtype[0]),
+    )
