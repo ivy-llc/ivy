f9a76aafcfe65ef27eb55e4e9a14aa8ee28b8743
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-10-24 17:07:00 +0530

    Update splitting_arrays.py

--- ivy/functional/frontends/numpy/manipulation_routines/splitting_arrays.py
+++ ivy/functional/frontends/numpy/manipulation_routines/splitting_arrays.py
@@ -1,5 +1,6 @@
 import ivy
 
 
+@to_ivy_arrays_and_back
 def split(a, splits, axis=0):
-    return ivy.split(a, /, *, splits=None, axis=0, with_remainder=False)
+    return ivy.split(a, splits=None, axis=0)
