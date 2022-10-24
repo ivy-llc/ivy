0d7450f488a23b9ad0b43e9b11e3a1a30a92cee4
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-10-25 01:19:53 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -27,7 +27,7 @@ def test_numpy_split(
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
-        input_dtypes="float32",
+        input_dtypes="int8",
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
