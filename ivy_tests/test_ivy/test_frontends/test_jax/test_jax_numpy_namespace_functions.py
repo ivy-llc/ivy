e9038e6f590cf060c81a83fafe84adf5c2473b3e
kimbring2 <kimbring2@gmail.com>
2022-10-24 13:04:32 +0900

    power function for jax numpy frontend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -1475,3 +1475,35 @@ def test_jax_numpy_flipud(
         fn_tree="numpy.flipud",
         m=np.asarray(m[0], dtype=input_dtype[0]),
     )
+
+
+# power
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.power"
+    ),
+)
+def test_jax_numpy_power(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.power",
+        x1=x[0],
+        x2=x[1],
+    )
