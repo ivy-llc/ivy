bfb678e1247d95651ee2d11d0d920bc0a4bfe7bf
TahaTasleem <mtaha306954@gmail.com>
2022-10-24 18:57:46 +0500

    Added Test for is_storage function

--- ivy/functional/frontends/torch/tensor_functions.py
+++ ivy/functional/frontends/torch/tensor_functions.py
@@ -8,11 +8,11 @@ from ivy.functional.frontends.torch.Tensor import Tensor
 
 
 def is_tensor(obj):
-    return Tensor(ivy.is_array(obj))
+    return Tensor(ivy.is_storage(obj))
 
 
 def is_storage(obj):
-    return Tensor(ivy.is_storage(obj))
+    return is_tensor(obj)
 
 
 # def is_complex(obj):
--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor_functions.py
@@ -32,6 +32,33 @@ def test_torch_is_tensor(
     )
 
 
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="functional.frontends.torch.is_storage"
+    ),
+)
+def test_torch_is_storage(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="torch",
+        fn_tree="is_storage",
+        obj=x[0],
+    )
+
+
 @handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
