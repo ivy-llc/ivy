e9569b0987c7a757b6e06fb583391b5e6cac99df
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-10-24 17:13:47 +0530

    Update splitting_arrays.py

--- ivy/functional/frontends/numpy/manipulation_routines/splitting_arrays.py
+++ ivy/functional/frontends/numpy/manipulation_routines/splitting_arrays.py
@@ -1,10 +1,12 @@
-#global
+# global
 import ivy
 from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
     handle_numpy_casting_special,
 )
 
 
+@handle_numpy_casting_special
 @to_ivy_arrays_and_back
 def split(a, splits, axis=0):
     return ivy.split(a, splits=None, axis=0)
