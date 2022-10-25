da6a59540affc950e2546ae11f9818eb3402e071
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-10-25 11:13:06 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -1,40 +1,36 @@
-from hypothesis import given
+# local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
-
 
 # split
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
+    dtype_x_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("numeric"),
-        min_num_dims=2,
-        min_dim_size=2,
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        force_int_axis=True,
     ),
-    shift=helpers.ints(min_value=1, max_value=10),
-    axis=helpers.ints(min_value=-1, max_value=1),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.split"
     ),
 )
 def test_numpy_split(
-    dtype_and_x,
-    shift,
-    axis,
+    dtype_x_axis,
     as_variable,
     num_positional_args,
     native_array,
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x, axis = dtype_x_axis
     helpers.test_frontend_function(
-        input_dtypes="int8",
+        input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend="numpy",
         fn_tree="split",
-        a=x[0],
-        shift=shift,
+        x=x[0],
         axis=axis,
     )
