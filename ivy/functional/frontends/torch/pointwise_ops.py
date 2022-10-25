4ad3cc548e52521759a6903de71904d60e16d535
vidit <vkagrawal07042003@gmail.com>
2022-10-26 00:13:35 +0530

    added arctan to frontend of torch point wise

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -275,3 +275,8 @@ def div(input, other, *, rounding_mode=None, out=None):
 @to_ivy_arrays_and_back
 def flipud(input):
     return ivy.flipud(input)
+
+
+@to_ivy_arrays_and_back
+def arctan(input, *, out=None):
+    return ivy.atan(input, out=out)
