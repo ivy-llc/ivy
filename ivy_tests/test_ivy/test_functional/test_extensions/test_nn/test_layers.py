18a5f703039c02b6c928bd13bb0e8d4d1318c4bb
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-10-25 15:57:30 +0100

    max_pool1d (#6144)

--- ivy_tests/test_ivy/test_functional/test_extensions/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_nn/test_layers.py
@@ -186,6 +186,43 @@ def test_max_pool2d(
     )
 
 
+@handle_cmd_line_args
+@given(
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=3, max_dims=3, min_side=1, max_side=4),
+    num_positional_args=helpers.num_positional_args(fn_name="max_pool1d"),
+)
+def test_max_pool1d(
+    *,
+    x_k_s_p,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    dtype, x, kernel, stride, pad = x_k_s_p
+    helpers.test_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="max_pool1d",
+        rtol_=1e-2,
+        atol_=1e-2,
+        ground_truth_backend="jax",
+        x=x[0],
+        kernel=kernel,
+        strides=stride,
+        padding=pad,
+    )
+
+
 # kaiser_window
 @handle_cmd_line_args
 @given(
