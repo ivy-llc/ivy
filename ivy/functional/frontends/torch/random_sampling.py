31e165c3fd1f42402a17d709029f22688dfd4992
ryparmar <rypar.martin@gmail.com>
2022-10-25 21:57:21 +0200

    seed pytorch frontend (#6118)

--- ivy/functional/frontends/torch/random_sampling.py
+++ ivy/functional/frontends/torch/random_sampling.py
@@ -2,6 +2,15 @@ import ivy
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
+def seed() -> int:
+    """Returns a 64 bit number used to seed the RNG"""
+    return int(ivy.randint(-(2**63), 2**63 - 1))
+
+
+def manual_seed(seed: int) -> None:
+    return ivy.seed(seed_value=seed)
+
+
 @to_ivy_arrays_and_back
 def multinomial(input, num_samples, replacement=False, *, generator=None, out=None):
     return ivy.multinomial(input, num_samples, replace=replacement, out=out)
