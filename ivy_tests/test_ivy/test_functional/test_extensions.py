c93f7b61c7a1989f7cb8a0855a32985ff5b93855
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2022-10-25 22:54:48 +0000

    Adding dct testing module

--- ivy_tests/test_ivy/test_functional/test_extensions.py
+++ ivy_tests/test_ivy/test_functional/test_extensions.py
@@ -783,3 +783,39 @@ def valid_dct(draw):
         norm = None
     
     return type, n, norm
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+        shared_dtype=True),
+    num_positional_args=helpers.num_positional_args(fn_name="dct"),
+)
+def test_dct(
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="dct",
+        x=x[0],
+    )
