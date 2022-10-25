55bb7db1860849aa134b3cd3ec0721ee4d3744dd
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-10-25 20:00:45 +0100

    Numpy docstring style fixes for diag, diagflat, pooling function(linalg and layer functions)

--- ivy/container/linear_algebra.py
+++ ivy/container/linear_algebra.py
@@ -581,7 +581,6 @@ class ContainerWithLinearAlgebra(ContainerBase):
 
         Examples
         --------
-
         >>> x = ivy.Container(a=[[0, 1, 2],
         >>>                      [3, 4, 5],
         >>>                      [6, 7, 8]])
