91a201dc6c48c67d2567199940249952fe21ed23
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-10-26 17:31:30 +0530

    Another fix to execute_with_gradients (#6187)

--- ivy/functional/backends/jax/gradients.py
+++ ivy/functional/backends/jax/gradients.py
@@ -97,7 +97,9 @@ def execute_with_gradients(
             grads = {ret_idxs[i]: grad for i, grad in enumerate(grads_)}
     if isinstance(xs, ivy.Container):
         grads = _set_duplicates(grads, duplicate_key_chains)
-    grads = ivy.nested_map(grads, lambda x: ivy.where(ivy.isnan(x), 0, x))
+    grads = ivy.nested_map(
+        grads, lambda x: ivy.where(ivy.isnan(x), 0, x) if ivy.is_ivy_array(x) else x
+    )
     grads = _remove_zeros_and_nones(grads, grads)
     func_ret, grads = _stop_grad_and_index(func_ret, retain_grads, grads, ret_grad_idxs)
     grads = ivy.to_ivy(grads)
