a7abdd13447e44cbbe285a4b2968bf42e2e36181
steelhitman <steelhitman.sm@gmail.com>
2022-10-26 18:25:10 +0100

    fft #5794

---
+++ ivy/functional/frontends/numpy/fft/fast_fourier_transform.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def FFT(x):
+    N = len(x)
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    e = np.exp(-2j * np.pi * k * n / N)
+    X = np.dot(e, x)
+    
+    return X
