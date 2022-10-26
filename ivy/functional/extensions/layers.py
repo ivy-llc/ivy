081f486b90c06870e1c17274744cd369d2ffce37
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-10-26 18:05:49 +0100

    hamming window extension (#6119)
    * hamming window
    
    * dealing with window_length=1 case
    
    * window = 0 and window = 1 cases
    
    * moved conditionals out of loop
    
    * changed test input type to int
    
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/extensions/layers.py
+++ ivy/functional/extensions/layers.py
@@ -16,7 +16,7 @@ from ivy.func_wrapper import (
     outputs_to_ivy_arrays,
 )
 from ivy.exceptions import handle_exceptions
-from math import sqrt
+from math import sqrt, pi, cos
 
 
 @to_native_arrays_and_back
@@ -760,3 +760,71 @@ def kaiser_bessel_derived_window(
     ]
 
     return ivy.array(dn_low + dn_mid, dtype=dtype, out=out)
+
+
+@to_native_arrays_and_back
+@handle_out_argument
+@handle_nestable
+@handle_exceptions
+def hamming_window(
+    window_length: int,
+    /,
+    *,
+    periodic: Optional[bool] = True,
+    alpha: Optional[float] = 0.54,
+    beta: Optional[float] = 0.46,
+    dtype: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
+    out: Optional[ivy.Array] = None,
+) -> ivy.Array:
+    """Computes the Hamming window with window length window_length
+
+    Parameters
+    ----------
+    window_length
+        an int defining the length of the window.
+    periodic
+         If True, returns a window to be used as periodic function.
+         If False, return a symmetric window.
+    alpha
+        The coefficient alpha in the hamming window equation 
+    beta
+        The coefficient beta in the hamming window equation 
+    dtype
+        data type of the returned array.
+    out
+        optional output array, for writing the result to.
+
+    Returns
+    -------
+    ret
+        The array containing the window.
+
+    Examples
+    --------
+    >>> ivy.hamming_window(5)
+    ivy.array([0.0800, 0.3979, 0.9121, 0.9121, 0.3979])
+    >>> ivy.hamming_window(5, periodic=False)
+    ivy.array([0.0800, 0.5400, 1.0000, 0.5400, 0.0800])
+    >>> ivy.hamming_window(5, periodic=False, alpha=0.2, beta=2)
+    ivy.array([-1.8000,  0.2000,  2.2000,  0.2000, -1.8000])
+    """
+    if window_length == 0:
+        return ivy.array([])
+    elif window_length == 1:
+        return ivy.array([1])
+    else:
+        if periodic is True:
+            window_length = window_length + 1
+            return ivy.array(
+                [alpha - beta * cos((2 * n * pi) / (window_length - 1))
+                    for n in range(0, window_length)][:-1],
+                dtype=dtype,
+                out=out
+            )
+        else:
+            return ivy.array(
+                [alpha - beta * cos((2 * n * pi) / (window_length - 1))
+                    for n in range(0, window_length)],
+                dtype=dtype,
+                out=out
+            )
