7a055f9b70fe8977b216e2c6f47132a003738eb7
yvonne <yjgeyer@gmail.com>
2022-10-27 13:14:56 +0700

    frontend jax.numpy.pinv and tests

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -465,3 +465,36 @@ def test_jax_norm(
         axis=axis,
         keepdims=keepdims,
     )
+
+
+# pinv
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=2,
+        max_num_dims=5,
+        min_dim_size=2,
+        max_dim_size=5,
+        large_abs_safety_factor=32,
+        small_abs_safety_factor=32,
+        safety_factor_scale="log",
+    ),
+    rtol=st.floats(1e-5, 1e-3),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.linalg.pinv"
+    ),
+)
+def test_jax_numpy_pinv(dtype_and_x, as_variable, native_array, num_positional_args, rtol):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.linalg.pinv",
+        rcond=rtol,
+        a=x[0],
+    )
