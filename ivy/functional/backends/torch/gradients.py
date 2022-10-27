Gradient testing for sparse_cross_entropy and more fixes to execute_with_gradients

ivy/functional/backends/torch/gradients.py
@@ -66,26 +66,41 @@ def execute_with_gradients(
         y = ret_values
 
     def grad_func(y):
-        if isinstance(xs, ivy.Container):
-            grads = xs.from_flat_list(
-                list(
-                    torch.autograd.grad(
-                        [y],
-                        [v for k, v in xs.to_iterator()],
-                        retain_graph=True,
-                        create_graph=retain_grads,
-                        allow_unused=True,
+        grads_ = ivy.nested_map(
+            xs, lambda x: ivy.to_native(ivy.zeros_like(x)), include_derived=True
+        )
+        try:
+            if isinstance(xs, ivy.Container):
+                grads = xs.from_flat_list(
+                    list(
+                        torch.autograd.grad(
+                            [y],
+                            [v for k, v in xs.to_iterator()],
+                            retain_graph=True,
+                            create_graph=retain_grads,
+                            allow_unused=True,
+                        )
                     )
                 )
-            )
-        else:
-            grads = torch.autograd.grad(
-                y,
-                xs,
-                retain_graph=True,
-                create_graph=retain_grads,
-                allow_unused=True,
-            )[0]
+                if isinstance(grads, ivy.Container):
+                    grads = grads.from_flat_list(
+                        ivy.nested_multi_map(
+                            lambda x, y: x[0] if x[1] is None else x[1], [grads_, grads]
+                        )
+                    )
+                else:
+                    grads = grads_ if grads is None else grads
+            else:
+                grads = torch.autograd.grad(
+                    y,
+                    xs,
+                    retain_graph=True,
+                    create_graph=retain_grads,
+                    allow_unused=True,
+                )[0]
+                grads = grads_ if grads is None else grads
+        except RuntimeError:
+            grads = grads_
         return grads
 
     if isinstance(y, ivy.NativeArray):