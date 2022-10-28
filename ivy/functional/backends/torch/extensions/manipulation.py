15b6adfdd67e54dcfc788e9b3c30b6a55246572f
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-10-28 19:08:27 +0530

    `ivy.top_k` extension (#6166)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy/functional/backends/torch/extensions/manipulation.py
+++ ivy/functional/backends/torch/extensions/manipulation.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, Sequence, Tuple
+from typing import Optional, Union, Sequence, Tuple, NamedTuple
 import torch
 
 
@@ -72,3 +72,19 @@ def rot90(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     return torch.rot90(m, k, axes)
+
+
+def top_k(
+    x: torch.Tensor,
+    k: int,
+    /,
+    *,
+    axis: Optional[int] = -1,
+    largest: Optional[bool] = True,
+    out: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    topk_res = NamedTuple(
+        "top_k", [("values", torch.Tensor), ("indices", torch.Tensor)]
+    )
+    indices, vals = torch.topk(x, k, dim=axis, largest=largest)
+    return topk_res(indices, vals)
