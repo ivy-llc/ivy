864e4190e8e32907673bfe3ccf51dad67ff37b13
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-28 10:46:54 +0100

    refactored test_creation extensions

--- ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_creation.py
@@ -1,38 +1,46 @@
-from hypothesis import given, strategies as st
+from hypothesis import strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+from ivy_tests.test_ivy.helpers import handle_test
 
 
-@handle_cmd_line_args
-@given(
+@handle_test(
+    fn_tree="functional.extensions.triu_indices",
     n_rows=helpers.ints(min_value=0, max_value=10),
     n_cols=st.none() | helpers.ints(min_value=0, max_value=10),
+    input_dtype=st.one_of(helpers.get_dtypes("integer")),
     k=helpers.ints(min_value=-10, max_value=10),
-    num_positional_args=helpers.num_positional_args(fn_name="triu_indices"),
 )
 def test_triu_indices(
     *,
     n_rows,
     n_cols,
+    input_dtype,
     k,
-    device,
     num_positional_args,
-    fw,
+    as_variable,
+    with_out,
+    native_array,
+    container_flags,
+    instance_method,
+    backend_fw,
+    fn_name,
+    on_device,
 ):
     helpers.test_function(
-        input_dtypes=["int32"],
-        as_variable_flags=[False],
-        with_out=None,
+        input_dtypes=input_dtype,
         num_positional_args=num_positional_args,
-        native_array_flags=[False],
-        container_flags=[False],
-        instance_method=False,
-        fw=fw,
-        fn_name="triu_indices",
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        native_array_flags=native_array,
+        container_flags=container_flags,
+        instance_method=instance_method,
+        on_device=on_device,
+        fw=backend_fw,
+        fn_name=fn_name,
         n_rows=n_rows,
         n_cols=n_cols,
         k=k,
-        device=device,
+        device=on_device,
     )
