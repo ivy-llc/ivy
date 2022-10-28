9e20c36af2c1f221db0168cc2ce52a3966187e1b
ahmed <ahmed42795@gmail.com>
2022-10-28 09:15:46 +0200

    remove local test file

--- test_function_docstring.py
+++
@@ -1,9 +0,0 @@
-import ivy
-from ivy_tests.test_docstrings import check_docstring_examples_run
-
-backend = "numpy"
-ivy.set_backend(backend)
-d = ivy.__dict__
-fn = "trace"
-method = d[fn]
-print(check_docstring_examples_run(fn=method))
