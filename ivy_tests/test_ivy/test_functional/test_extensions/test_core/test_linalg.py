dc39cff45cccfcea662a8abdb9c7e86c099b13ca
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-10-28 10:10:15 +0100

    refactored test_linalg extensions

--- ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
@@ -1,14 +1,11 @@
-# global
-from hypothesis import given
-
 # local
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+from ivy_tests.test_ivy.helpers import handle_test
 
 
 # diagflat
-@handle_cmd_line_args
-@given(
+@handle_test(
+    fn_tree="functional.extensions.diagflat",
     dtype_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
         min_num_dims=1,
@@ -17,31 +14,33 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
         max_dim_size=50,
     ),
     k=helpers.ints(min_value=-49, max_value=49),
-    num_positional_args=helpers.num_positional_args(fn_name="diagflat"),
 )
 def test_diagflat(
     *,
     dtype_x,
+    num_positional_args,
     as_variable,
     with_out,
-    num_positional_args,
     native_array,
-    container,
+    container_flags,
     instance_method,
-    fw,
+    backend_fw,
+    fn_name,
+    on_device,
     k,
 ):
     dtype, x = dtype_x
     helpers.test_function(
         input_dtypes=dtype,
+        num_positional_args=num_positional_args,
         as_variable_flags=as_variable,
         with_out=with_out,
-        num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        container_flags=container,
+        container_flags=container_flags,
         instance_method=instance_method,
-        fw=fw,
-        fn_name="diagflat",
+        on_device=on_device,
+        fw=backend_fw,
+        fn_name=fn_name,
         x=x[0],
         k=k,
     )
