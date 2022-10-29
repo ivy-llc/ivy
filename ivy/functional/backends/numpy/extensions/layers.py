90871caa86ab6dc201ff5caaa7d1646a044663bd
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2022-10-29 08:26:53 +0000

    Adding axis parameter to numpy backend

--- ivy/functional/backends/numpy/extensions/layers.py
+++ ivy/functional/backends/numpy/extensions/layers.py
@@ -335,7 +335,8 @@ def dct(
     *,
     type: Optional[Literal[1, 2, 3, 4]] = 2,
     n: Optional[int] = None,
+    axis: Optional[int]= -1,
     norm: Optional[Literal["ortho"]] = None,
     out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
-    return fft.dct(x, type=type, n=n, norm=norm)
+    return fft.dct(x, type=type, n=n, axis=axis, norm=norm)
