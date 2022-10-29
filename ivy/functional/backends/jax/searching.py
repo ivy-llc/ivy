8c56e6ee898c278d1564fcd8cceac7c61be10d31
kurshakuz <abilkasov@gmail.com>
2022-10-29 21:59:16 +0200

    implement gather torch frontend (#6325)
    * implement gather torch frontend
    
    * use only valid dtypes for array generation
    
    * fix flake warnings
    
    * fix flake warning

--- ivy/functional/backends/jax/searching.py
+++ ivy/functional/backends/jax/searching.py
@@ -73,6 +73,8 @@ def where(
     *,
     out: Optional[JaxArray] = None,
 ) -> JaxArray:
+    if x1 is None and x2 is None:
+        return jnp.where(condition)
     x1, x2 = ivy.promote_types_of_inputs(x1, x2)
     return jnp.where(condition, x1, x2).astype(x1.dtype)
 
