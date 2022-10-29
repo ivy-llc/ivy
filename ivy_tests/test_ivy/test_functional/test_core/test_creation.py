ec371e2909f120611ad8ab43d5b6bc313a5d9ff4
Aditya Pola <adityapola@gmail.com>
2022-10-29 06:22:11 +0000

    Refactoring. assert_allclose fails when bfloat16 values passed. Test works when bfloat16 is removed from data generation.

--- ivy_tests/test_ivy/test_functional/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_creation.py
@@ -1,10 +1,13 @@
 """Collection of tests for creation functions."""
 
+from importlib import import_module
+
 # global
 from hypothesis import given, strategies as st
 
 # local
 import ivy
+from ivy.backend_handler import _backend_dict
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 from ivy_tests.test_ivy.test_functional.test_core.test_dtype import astype_helper
@@ -381,81 +384,59 @@ def test_eye(
 
 
 @st.composite
-def dtype_x_frameworks(draw, min_num_dims, max_num_dims, min_dim_size, max_dim_size):
-    initial_framework = draw(st.sampled_from(["numpy", "jax", "tensorflow", "torch"]))
-    target_framework = draw(st.sampled_from(["numpy", "jax", "tensorflow", "torch"]))
+def dtype_x_frameworks(draw, available_dtypes=None):
+    initial_framework_str = draw(
+        st.sampled_from(["numpy", "jax", "tensorflow", "torch"])
+    )
+    target_framework_str = draw(
+        st.sampled_from(["numpy", "jax", "tensorflow", "torch"])
+    )
 
-    if initial_framework == "torch" or target_framework == "torch":
-        dtype_and_x = draw(
-            helpers.dtype_and_values(
-                available_dtypes=[
-                    "int8",
-                    "int16",
-                    "int32",
-                    "int64",
-                    "uint8",
-                    "float16",
-                    "float32",
-                    "float64",
-                ],
-                min_num_dims=min_num_dims,
-                max_num_dims=max_num_dims,
-                min_dim_size=min_dim_size,
-                max_dim_size=max_dim_size,
+    initial_framework = import_module(_backend_dict[initial_framework_str])
+    target_framework = import_module(_backend_dict[target_framework_str])
+    if available_dtypes is None:
+        available_dtypes = list(
+            set(initial_framework.valid_numeric_dtypes).intersection(
+                set(target_framework.valid_numeric_dtypes)
             )
         )
-    elif initial_framework == "numpy" or target_framework == "numpy":
-        dtype_and_x = draw(
-            helpers.dtype_and_values(
-                available_dtypes=[
-                    "int8",
-                    "int16",
-                    "int32",
-                    "int64",
-                    "uint8",
-                    "uint16",
-                    "uint32",
-                    "uint64",
-                    "float16",
-                    "float32",
-                    "float64",
-                ],
-                min_num_dims=min_num_dims,
-                max_num_dims=max_num_dims,
-                min_dim_size=min_dim_size,
-                max_dim_size=max_dim_size,
-            )
+        if "bfloat16" in available_dtypes:
+            available_dtypes.remove("bfloat16")
+
+    dtype_and_x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=available_dtypes,
+            min_num_dims=1,
+            max_num_dims=5,
+            min_dim_size=1,
+            max_dim_size=5,
         )
-    else:
-        dtype_and_x = draw(
-            helpers.dtype_and_values(
-                available_dtypes=helpers.get_dtypes("numeric"),
-                min_num_dims=min_num_dims,
-                max_num_dims=max_dim_size,
-                min_dim_size=min_dim_size,
-                max_dim_size=max_dim_size,
-            )
-        )
+    )
     dtype, x = dtype_and_x
-    return dtype, x, initial_framework, target_framework
+    return dtype, x, initial_framework_str, target_framework_str
 
 
 # __dlpack__ and to_dlpack
-@handle_cmd_line_args
-@given(
-    dtype_x_frameworks=dtype_x_frameworks(
-        min_num_dims=1, max_num_dims=5, min_dim_size=1, max_dim_size=5
-    )
-)
+# @handle_cmd_line_args
+@given(dtype_x_frameworks=dtype_x_frameworks())
 def test_to_dlpack(*, dtype_x_frameworks):
-    _, x, initial_framework, target_framework = dtype_x_frameworks
-    ivy.set_backend(initial_framework)
-    ivy_array = ivy.array(x[0])
-    ivy_np_array = ivy.to_numpy(ivy_array)
-    native_array = ivy_array.to_native()
-    ivy.set_backend(target_framework)
+    dtype, x, initial_framework_str, target_framework_str = dtype_x_frameworks
+    ivy.set_backend(initial_framework_str)
+    initial_array = ivy.array(x[0])
+    # if dtype[0] == 'bfloat16':
+    #     initial_np_array = initial_array.astype(ivy.float64).to_numpy()
+    # else:
+    initial_np_array = initial_array.to_numpy()
+    native_array = initial_array.to_native()
+    ivy.set_backend(target_framework_str)
     target_array = ivy.from_dlpack(native_array)
-    helpers.assert_all_close(ivy.to_numpy(target_array), ivy_np_array)
+    # if dtype[0] == 'bfloat16':
+    #     target_np_array = target_array.astype(ivy.float64).to_numpy()
+    #     # target_np_array = ivy.to_numpy(target_array.astype(ivy.float64))
+    # else:
+    target_np_array = target_array.to_numpy()
+    helpers.assert_all_close(target_np_array, initial_np_array)
+    # helpers.assert_all_close(target_array, initial_np_array)
     ivy.unset_backend()
     ivy.unset_backend()
 
