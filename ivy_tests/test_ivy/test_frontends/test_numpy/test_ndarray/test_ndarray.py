3ac0c14a178ab1e8e9faca5046a8368d39127058
Aidan Eicholz <aidaneicholz@gmail.com>
2022-10-29 00:03:03 -0400

    Update test_ndarray.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -100,49 +100,36 @@ def test_numpy_ndarray_reshape(
 
     
 # resize
-@st.composite
-def dtypes_x_resize(draw):
-    dtypes, x = draw(
-        helpers.dtype_and_values(
-            available_dtypes=helpers.get_dtypes("valid"),
-            shape=helpers.get_shape(
-                allow_none=False,
-                min_num_dims=1,
-                max_num_dims=5,
-                min_dim_size=1,
-                max_dim_size=10,
-            ),
-        )
-    )
-    shape = draw(helpers.resize_shapes(shape=np.array(x).shape))
-    return dtypes, x, shape
-
-
 @handle_cmd_line_args
 @given(
-    dtypes_x_shape=dtypes_x_resize(),
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+    ),
 )
 def test_numpy_ndarray_resize(
-    dtypes_x_shape,
+    dtype_and_x,
     as_variable,
     native_array,
+    fw,
 ):
-    input_dtype, x, shape = dtypes_x_shape
+    input_dtype, x = dtype_and_x
     helpers.test_frontend_method(
-        input_dtypes_init=input_dtype,
+        input_dtype_init=input_dtype,
         as_variable_flags_init=as_variable,
         num_positional_args_init=0,
         native_array_flags_init=native_array,
         all_as_kwargs_np_init={
             "data": x[0],
         },
-        input_dtypes_method=[],
+        input_dtypes_method=[input_dtype[1]],
         as_variable_flags_method=as_variable,
         num_positional_args_method=0,
         native_array_flags_method=native_array,
         all_as_kwargs_np_method={
-            "shape": shape,
+            "value": x[1],
         },
+        fw=fw,
         frontend="numpy",
         class_name="ndarray",
         method_name="resize",
