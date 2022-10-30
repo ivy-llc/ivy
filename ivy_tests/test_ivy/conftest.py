a21967d0c7c1d0f076ebe8a447adb9d9179f0eb1
CatB1t <skytedits@gmail.com>
2022-10-30 20:52:17 +0200

    fix for tests without `test_data`.

--- ivy_tests/test_ivy/conftest.py
+++ ivy_tests/test_ivy/conftest.py
@@ -68,15 +68,20 @@ def pytest_configure(config):
 
 @pytest.fixture(autouse=True)
 def run_around_tests(request, on_device, backend_fw, compile_graph, implicit):
-    try:
-        test_globals.setup_api_test(request.function.test_data, backend_fw.backend)
-    except Exception as e:
+    if hasattr(request.function, "test_data"):
+        try:
+            test_globals.setup_api_test(request.function.test_data, backend_fw.backend)
+        except Exception as e:
+            test_globals.teardown_api_test()
+            raise RuntimeError(f"Setting up test for {request.function} failed.") from e
+        with backend_fw.use:
+            with DefaultDevice(on_device):
+                yield
         test_globals.teardown_api_test()
-        raise RuntimeError(f"Setting up test for {request.function} failed.") from e
-    with backend_fw.use:
-        with DefaultDevice(on_device):
-            yield
-    test_globals.teardown_api_test()
+    else:
+        with backend_fw.use:
+            with DefaultDevice(on_device):
+                yield
 
 
 def pytest_generate_tests(metafunc):
