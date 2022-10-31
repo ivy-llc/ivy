25322fc35a546ac425776c36a9ee3cfe35646d60
hslau44 <hslau44@gmail.com>
2022-10-31 10:12:59 +0000

    remove min and max value in test

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -1511,7 +1511,7 @@ def test_jax_numpy_power(
 @handle_cmd_line_args
 @given(
     dtype_and_values=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"), num_arrays=3, shape=(1,), shared_dtype=True, min_value=0, max_value=100,
+        available_dtypes=helpers.get_dtypes("numeric"), num_arrays=3, shape=(1,), shared_dtype=True,
     ), 
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.jax.numpy.arange"
