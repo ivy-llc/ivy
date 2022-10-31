cb9d75ba8a46f58d9db096425aa49e24c01b3196
MuhammadSaeedBatikh <mohammud.saeed.batekh@gmail.com>
2022-10-31 13:28:12 +0200

    jax RNG key failed test fixed. (2)

--- ivy/functional/backends/jax/layers.py
+++ ivy/functional/backends/jax/layers.py
@@ -413,9 +413,8 @@ def dropout1d(
             x = jnp.transpose(x, perm)
         noise_shape = list(x.shape)
         noise_shape[-1] = 1
-        global RNG
-        RNG, rng_input = jax.random.split(RNG)
-        mask = jax.random.bernoulli(RNG, 1 - prob, noise_shape)
+        _, rng_input = jax.random.split(RNG)
+        mask = jax.random.bernoulli(rng_input, 1 - prob, noise_shape)
         res = jnp.where(mask, x / (1 - prob), 0)
         if data_format == "NWC":
             res = jnp.transpose(res, perm)
