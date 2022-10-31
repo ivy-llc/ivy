9b9e11959d92b8fb4239c0f06b2dfd553787a925
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2022-10-31 18:24:36 +0000

    Optimizing axis arg for torch backend

--- ivy/functional/backends/torch/extensions/layers.py
+++ ivy/functional/backends/torch/extensions/layers.py
@@ -199,62 +199,81 @@ def dct(
         raise ValueError("Norm must be either None or 'ortho'")
     if x.dtype not in [torch.float32, torch.float64]:
         x = x.type(torch.float32)
-    if axis != -1:
-        x = torch.transpose(x, axis, -1)
+    if axis < 0:
+        axis = axis + len(x.shape)
     if n is not None:
-        signal_len = x.shape[-1]
+        signal_len = x.shape[axis]
         if n <= signal_len:
-            x = x[..., :n]
+            local_idx = [slice(None)] * len(x.shape)
+            local_idx[axis] = slice(None, n)
+            x = x[local_idx]
         else:
-            x = torch.nn.functional.pad(x, (0, n - signal_len))
+            pad_idx = [0] * 2 * len(x.shape)
+            pad_idx[(len(pad_idx)-1)-(2 * axis)] = n - signal_len
+            x = torch.nn.functional.pad(x, pad_idx)
     real_zero = torch.tensor(0.0, dtype=x.dtype)
-    axis_dim = x.shape[-1]
+    axis_dim = x.shape[axis]
     axis_dim_float = torch.tensor(axis_dim, dtype=x.dtype)
 
     if type == 1:
         if norm:
             raise ValueError("Normalization not supported for type-I DCT")
-        x = torch.concat([x, x.flip(-1)[..., 1:-1]], dim=-1)
-        dct_out = torch.real(torch.fft.rfft(x, dim=-1))
+        axis_idx = [slice(None)] * len(x.shape)
+        axis_idx[axis] = slice(1, -1)
+        x = torch.concat([x, x.flip(axis)[axis_idx]], dim=axis)
+        dct_out = torch.real(torch.fft.rfft(x, dim=axis))
+        return dct_out
 
     elif type == 2:
+        scale_dims = [1] * len(x.shape)
+        scale_dims[axis] = axis_dim
         scale = 2.0 * torch.exp(
             torch.complex(
                 real_zero,
                 -torch.arange(axis_dim_float) * math.pi * 0.5 / axis_dim_float,
             )
-        )
-        dct_out = torch.real(torch.fft.rfft(x, n=2 * axis_dim)[..., :axis_dim] * scale)
+        ).view(scale_dims)
+
+        axis_idx = [slice(None)] * len(x.shape)
+        axis_idx[axis] = slice(None, axis_dim)
+        dct_out = torch.real(torch.fft.rfft(x, n=2 * axis_dim, axis=axis)[axis_idx] * scale)
         if norm == "ortho":
             n1 = 0.5 * torch.rsqrt(axis_dim_float)
             n2 = n1 * math.sqrt(2.0)
-            # vectorising scaling factors
             sf = torch.nn.functional.pad(n1.unsqueeze(0), (0, axis_dim - 1), value=n2)
-            dct_out = sf * dct_out
+            dct_out = sf.view(scale_dims) * dct_out
+        return dct_out
 
     elif type == 3:
-        if norm == "ortho":
-            n1 = torch.sqrt(axis_dim_float)
-            n2 = n1 * math.sqrt(0.5)
-            sf = torch.nn.functional.pad(n1.unsqueeze(0), (0, axis_dim - 1), value=n2)
-            x = x * sf
-        else:
-            x = x * axis_dim_float
+        scale_dims = [1] * len(x.shape)
+        scale_dims[axis] = axis_dim
         scale = 2.0 * torch.exp(
             torch.complex(
                 real_zero, torch.arange(axis_dim_float) * math.pi * 0.5 / axis_dim_float
             )
-        )
+        ).view(scale_dims)
+        if norm == "ortho":
+            n1 = torch.sqrt(axis_dim_float)
+            n2 = n1 * math.sqrt(0.5)
+            scale_dims = [1] * len(x.shape)
+            scale_dims[axis] = axis_dim
+            sf = torch.nn.functional.pad(n1.unsqueeze(0), (0, axis_dim - 1), value=n2)
+            x = x * sf.view(scale_dims)
+        else:
+            x = x * axis_dim_float
+            
+        axis_idx = [slice(None)] * len(x.shape)
+        axis_idx[axis] = slice(None, axis_dim)
         dct_out = torch.real(
-            torch.fft.irfft(scale * torch.complex(x, real_zero), n=2 * axis_dim)
-        )[..., :axis_dim]
+            torch.fft.irfft(scale * torch.complex(x, real_zero), n=2 * axis_dim, axis=axis)
+        )[axis_idx]
+        return dct_out
 
     elif type == 4:
-        dct_2 = dct(x, type=2, n=2 * axis_dim, norm=None)
-        dct_out = dct_2[..., 1::2]
+        dct_2 = dct(x, type=2, n=2 * axis_dim, axis=axis, norm=None)
+        axis_idx = [slice(None)] * len(x.shape)
+        axis_idx[axis] = slice(1, None, 2)
+        dct_out = dct_2[axis_idx]
         if norm == "ortho":
             dct_out *= math.sqrt(0.5) * torch.rsqrt(axis_dim_float)
-
-    if axis != -1:
-        dct_out = torch.transpose(dct_out, axis, -1)
-    return dct_out
+        return dct_out
