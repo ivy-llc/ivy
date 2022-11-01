8164a35be469558691bd74bc2263952f566a6401
Jamie Line <60134692+JamieLine@users.noreply.github.com>
2022-11-01 23:49:23 +0000

    Removed duplicate diag function

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -69,34 +69,6 @@ def det(
     return tf.linalg.det(x)
 
 
-def diag(
-    x: Union[tf.Tensor, tf.Variable],
-    /,
-    *,
-    offset: Optional[int] = 0,
-    padding_value: Optional[float] = 0,
-    align: Optional[str] = "RIGHT_LEFT",
-    num_rows: Optional[int] = None,
-    num_cols: Optional[int] = None,
-    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
-):
-    if num_rows is None:
-        num_rows = -1
-    if num_cols is None:
-        num_cols = -1
-
-    ret = tf.linalg.diag(
-        x,
-        name="diag",
-        k=offset,
-        num_rows=num_rows,
-        num_cols=num_cols,
-        padding_value=padding_value,
-        align=align,
-    )
-    return ret
-
-
 def diagonal(
     x: Union[tf.Tensor, tf.Variable],
     /,
