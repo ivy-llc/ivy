faf0cf4f3708143452de00ba5ea57c843b84e13c
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-01 10:38:51 +0100

    kron_extension (#6229)
    * kron_extension
    
    * current_backend inputs
    
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
@@ -45,3 +45,43 @@ def test_diagflat(
         x=x[0],
         k=k,
     )
+
+
+# kron
+@handle_cmd_line_args
+@given(
+    dtype_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_dim_size=3,
+        num_arrays=2,
+        shared_dtype=True
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="kron"),
+)
+def test_kron(
+    dtype_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    dtype, x = dtype_x
+    helpers.test_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="kron",
+        a=x[0],
+        b=x[1],
+    )
