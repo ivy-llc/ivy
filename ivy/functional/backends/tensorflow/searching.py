9d74b1f5dde3d3a28a975b648f0231e7496188b9
kurshakuz <abilkasov@gmail.com>
2022-11-02 20:09:21 +0100

    use argwhere instead of where in gather (#6512)

--- ivy/functional/backends/tensorflow/searching.py
+++ ivy/functional/backends/tensorflow/searching.py
@@ -85,8 +85,6 @@ def where(
     *,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
-    if x1 is None and x2 is None:
-        return tf.experimental.numpy.where(condition)
     x1, x2 = ivy.promote_types_of_inputs(x1, x2)
     return tf.cast(tf.experimental.numpy.where(condition, x1, x2), x1.dtype)
 
