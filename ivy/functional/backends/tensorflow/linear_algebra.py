Fixed gradient issue with astype usage (#6498)

ivy/functional/backends/tensorflow/linear_algebra.py
@@ -233,7 +233,7 @@ def matmul(
             x2_padded = True
         ret = tf.matmul(x1, x2)
 
-    ret = ivy.astype(ret, dtype_from, copy=False)
+    ret = ivy.astype(ret, dtype_from, copy=False).to_native()
     if x1_padded_2:
         ret = ret[0]
     elif x1_padded: