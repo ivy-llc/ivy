414eb9726d9af31597cb1693c29ec3dbe2ca5809
robertsn808 <robertsn@hawaii.edu>
2022-11-02 11:21:47 -1000

    #6500

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -1,8 +1,9 @@
-#in ivy/ivy/functional/frontends/numpy/ufunc/methods.py
+# in ivy/ivy/functional/frontends/numpy/ufunc/methods.py
 import ivy
 import ivy.functional.frontends.numpy as np
 from ivy.functional.frontends.numpy import ufunc
 from numpy import array
+
 
 def accumulate(array, axis, dtype, out):
     return ivy.Array
