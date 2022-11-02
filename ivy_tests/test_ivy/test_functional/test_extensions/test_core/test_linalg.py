3a6b96ec0f308f5a1b39083827eba1112cd08354
Jamie Line <60134692+JamieLine@users.noreply.github.com>
2022-11-02 00:00:49 +0000

    Cleaned test

--- ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_linalg.py
@@ -1,7 +1,7 @@
 # global
 import math
 
-from hypothesis import given, strategies as st, assume, settings
+from hypothesis import given, strategies as st, assume
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -136,19 +136,8 @@ def _generate_diag_args(draw):
 
 
 @handle_cmd_line_args
-@settings(max_examples=100000)
 @given(
     args_packet=_generate_diag_args(),
-    # @handle_cmd_line_args
-    # @given(
-    #    dtype_x=helpers.dtype_and_values(
-    #        available_dtypes=helpers.get_dtypes("numeric"),
-    #        min_num_dims=1,
-    #        max_num_dims=2,
-    #        min_dim_size=1,
-    #        max_dim_size=50,
-    #    ),
-    #    k=helpers.ints(min_value=-49, max_value=49),
     num_positional_args=helpers.num_positional_args(fn_name="diagflat"),
 )
 def test_diagflat(
@@ -190,31 +179,3 @@ def test_diagflat(
         atol_=1e-01,
         rtol_=1 / 64,
     )
-
-
-# def test_diagflat(
-#    *,
-#    dtype_x,
-# as_variable,
-# with_out,
-# num_positional_args,
-# native_array,
-# container,
-# instance_method,
-# fw,
-# k,
-# ):
-# dtype, x = dtype_x
-# helpers.test_function(
-#    input_dtypes=dtype,
-#    as_variable_flags=as_variable,
-#    with_out=with_out,
-#    num_positional_args=num_positional_args,
-#    native_array_flags=native_array,
-#    container_flags=container,
-#    instance_method=instance_method,
-#    fw=fw,
-#    fn_name="diagflat",
-#    x=x[0],
-#    k=k,
-# )
