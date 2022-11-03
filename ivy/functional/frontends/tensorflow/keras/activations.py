7a8d790944435282614469b07e5eec8a964af12f
Oscar Marte <84993924+oscar1marte@users.noreply.github.com>
2022-11-03 09:08:11 -0400

    added deserialize in tensorflow frontends (#5782)
    * added deserialize function to ivy activation funtions
    
    * added deserialize function to tensorflow frontends activation functions
    
    * added simple_test_two_function and get_callable_functions helper functions
    
    Co-authored-by: Oscar Marte <omarte@gobetterfly.com>, Yusha Arif <101613943+YushaArif99@users.noreply.github.com>

--- ivy/functional/frontends/tensorflow/keras/activations.py
+++ ivy/functional/frontends/tensorflow/keras/activations.py
@@ -124,3 +124,30 @@ selu.supported_dtypes = {
         "float64",
     ),
 }
+
+
+def deserialize(name, custom_objects=None):
+    return ivy.deserialize(name, custom_objects=custom_objects)
+
+
+deserialize.supported_dtypes = {
+    "numpy": (
+        "float16",
+        "float32",
+        "float64",
+    ),
+    "tensorflow": (
+        "float16",
+        "float32",
+        "float64",
+    ),
+    "torch": (
+        "float32",
+        "float64",
+    ),
+    "jax": (
+        "float16",
+        "float32",
+        "float64",
+    ),
+}
