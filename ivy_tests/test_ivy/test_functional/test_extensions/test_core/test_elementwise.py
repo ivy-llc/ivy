c8ea75b547a600db96e3f3029d4e4cbd375b9296
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-03 13:36:15 +0100

    is_neg_inf (#6538)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_core/test_elementwise.py
@@ -459,3 +459,41 @@ def test_isposinf(
         fn_name="isposinf",
         x=np.asarray(x[0], dtype=input_dtype[0]),
     )
+
+
+# isneginf
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=1,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_dim_size=3,
+        allow_inf=True,
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="isneginf"),
+)
+def test_isneginf(
+    dtype_and_x,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="isneginf",
+        x=np.asarray(x[0], dtype=input_dtype[0]),
+    )
