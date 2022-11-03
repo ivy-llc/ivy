7544fa4d4c85cdebec4d1e4ec11a2fbf2e5ffb93
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-11-03 11:13:02 +0000

    refactored test_from_shape_or_value.py and fixed tests

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_shape_or_value.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_shape_or_value.py
@@ -1,27 +1,15 @@
 # global
 import ivy
-from hypothesis import given, strategies as st
+from hypothesis import strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
-
-
-@st.composite
-def _dtypes(draw):
-    return draw(
-        st.shared(
-            helpers.list_of_length(
-                x=st.sampled_from(draw(helpers.get_dtypes("valid"))), length=1
-            ),
-            key="dtype",
-        )
-    )
+from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
 # empty
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.empty",
     shape=helpers.get_shape(
         allow_none=False,
         min_num_dims=1,
@@ -29,34 +17,36 @@ def _dtypes(draw):
         min_dim_size=1,
         max_dim_size=10,
     ),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.empty"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_empty(
     shape,
-    dtypes,
+    dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=[False],
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="empty",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         test_values=False,
         shape=shape,
-        dtype=dtypes[0],
+        dtype=dtype[0],
     )
 
 
 # empty_like
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.empty_like",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("valid"),
     ),
@@ -67,16 +57,18 @@ def test_numpy_empty(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.empty_like"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_empty_like(
     dtype_and_x,
     shape,
+    dtype,
     as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -85,11 +77,12 @@ def test_numpy_empty_like(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="empty_like",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         test_values=False,
         prototype=x[0],
-        dtype=input_dtype,
+        dtype=dtype[0],
         order="K",
         subok=True,
         shape=shape,
@@ -97,70 +90,74 @@ def test_numpy_empty_like(
 
 
 # eye
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.eye",
     rows=helpers.ints(min_value=3, max_value=10),
     cols=helpers.ints(min_value=3, max_value=10),
     k=helpers.ints(min_value=0, max_value=2),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.eye"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_eye(
     rows,
     cols,
     k,
-    dtypes,
+    dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=[False],
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="eye",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         N=rows,
         M=cols,
         k=k,
-        dtype=dtypes[0],
+        dtype=dtype[0],
     )
 
 
 # identity
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.identity",
     n=helpers.ints(min_value=1, max_value=10),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.identity"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_identity(
     n,
-    dtypes,
+    dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=[False],
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="identity",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         n=n,
-        dtype=dtypes[0],
+        dtype=dtype[0],
     )
 
 
 # ones
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.ones",
     shape=helpers.get_shape(
         allow_none=False,
         min_num_dims=1,
@@ -168,33 +165,35 @@ def test_numpy_identity(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.ones"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_ones(
     shape,
-    dtypes,
+    dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=[False],
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="ones",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         shape=shape,
-        dtype=dtypes[0],
+        dtype=dtype[0],
     )
 
 
 # ones_like
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.ones_like",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("valid"),
     ),
@@ -205,16 +204,18 @@ def test_numpy_ones(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.ones_like"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_ones_like(
     dtype_and_x,
     shape,
+    dtype,
     as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -223,10 +224,11 @@ def test_numpy_ones_like(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="ones_like",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         a=x[0],
-        dtype=input_dtype,
+        dtype=dtype[0],
         order="K",
         subok=True,
         shape=shape,
@@ -234,8 +236,8 @@ def test_numpy_ones_like(
 
 
 # zeros
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.zeros",
     shape=helpers.get_shape(
         allow_none=False,
         min_num_dims=1,
@@ -243,33 +245,35 @@ def test_numpy_ones_like(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    dtypes=_dtypes(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.zeros"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_zeros(
     shape,
-    dtypes,
+    dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     helpers.test_frontend_function(
-        input_dtypes=dtypes,
-        as_variable_flags=[False],
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="zeros",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         shape=shape,
-        dtype=dtypes[0],
+        dtype=dtype[0],
     )
 
 
 # zeros_like
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.zeros_like",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("valid"),
     ),
@@ -280,16 +284,18 @@ def test_numpy_zeros(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.zeros_like"
-    ),
+    dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_zeros_like(
     dtype_and_x,
+    dtype,
     shape,
     as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -298,30 +304,35 @@ def test_numpy_zeros_like(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="zeros_like",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         a=x[0],
-        dtype=input_dtype,
+        dtype=dtype[0],
         order="K",
         subok=True,
         shape=shape,
     )
 
 
-# full and full_like helpers
+# full and full_like helper
 @st.composite
-def _dtype_and_fill_value(draw):
-    dtype = draw(helpers.get_dtypes("numeric", full=False))
-    if ivy.is_uint_dtype(dtype):
-        return dtype, draw(helpers.ints(min_value=0, max_value=5))
-    elif ivy.is_int_dtype(dtype):
-        return dtype, draw(helpers.ints(min_value=-5, max_value=5))
-    return dtype, draw(helpers.floats(min_value=-5, max_value=5))
+def _input_fill_and_dtype(draw):
+    dtype = draw(helpers.get_dtypes("float", full=False))
+    dtype_and_input = draw(helpers.dtype_and_values(dtype=dtype))
+    if ivy.is_uint_dtype(dtype[0]):
+        fill_values = draw(st.integers(min_value=0, max_value=5))
+    elif ivy.is_int_dtype(dtype[0]):
+        fill_values = draw(st.integers(min_value=-5, max_value=5))
+    else:
+        fill_values = draw(st.floats(min_value=-5, max_value=5))
+    dtype_to_cast = draw(helpers.get_dtypes("float", full=False))
+    return dtype, dtype_and_input[1], fill_values, dtype_to_cast[0]
 
 
 # full
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="numpy.full",
     shape=helpers.get_shape(
         allow_none=False,
         min_num_dims=1,
@@ -329,42 +340,38 @@ def _dtype_and_fill_value(draw):
         min_dim_size=1,
         max_dim_size=10,
     ),
-    dtype_and_fill_value=_dtype_and_fill_value(),
-    # dtypes=helpers.get_dtypes("numeric"),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.full"
-    ),
+    input_fill_dtype=_input_fill_and_dtype(),
 )
 def test_numpy_full(
     shape,
-    dtype_and_fill_value,
-    # dtypes,
+    input_fill_dtype,
+    as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
-    dtype, fill_value = dtype_and_fill_value
+    input_dtype, x, fill, dtype_to_cast = input_fill_dtype
     helpers.test_frontend_function(
-        input_dtypes=dtype,
-        as_variable_flags=[False],
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="full",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         shape=shape,
-        fill_value=fill_value,
-        dtype=dtype,
+        fill_value=fill,
+        dtype=dtype_to_cast,
     )
 
 
 # full_like
-@handle_cmd_line_args
-@given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
-    ),
-    dtype_and_fill_value=_dtype_and_fill_value(),
-    # dtypes=helpers.get_dtypes("numeric", full=False),
+@handle_frontend_test(
+    fn_tree="numpy.full_like",
+    input_fill_dtype=_input_fill_and_dtype(),
     shape=helpers.get_shape(
         allow_none=True,
         min_num_dims=1,
@@ -372,32 +379,30 @@ def test_numpy_full(
         min_dim_size=1,
         max_dim_size=10,
     ),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.full_like"
-    ),
 )
 def test_numpy_full_like(
-    dtype_and_x,
-    dtype_and_fill_value,
-    # dtypes,
+    input_fill_dtype,
     shape,
     as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
-    input_dtype, x = dtype_and_x
-    dtype, fill_value = dtype_and_fill_value
+    input_dtype, x, fill, dtype_to_cast = input_fill_dtype
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="full_like",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         a=x[0],
-        fill_value=fill_value,
-        dtype=dtype,
+        fill_value=fill,
+        dtype=dtype_to_cast,
         order="K",
         subok=True,
         shape=shape,
