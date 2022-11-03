bd2d4d3de42b6f164a0345df311e2c7d18c0ce2f
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-11-03 16:06:48 +0000

    refactored test_correlating.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_correlating.py
@@ -1,15 +1,15 @@
 # global
-from hypothesis import given, strategies as st
+from hypothesis import strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
+from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
-# Correlate
-@handle_cmd_line_args
-@given(
+# correlate
+@handle_frontend_test(
+    fn_tree="numpy.correlate",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float", full=True),
         min_num_dims=1,
@@ -18,9 +18,6 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
         shared_dtype=True,
     ),
     mode=st.sampled_from(["valid", "same", "full"]),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.numpy.correlate"
-    ),
 )
 def test_numpy_correlate(
     dtype_and_x,
@@ -28,6 +25,9 @@ def test_numpy_correlate(
     as_variable,
     num_positional_args,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
 ):
     input_dtypes, xs = dtype_and_x
     np_frontend_helpers.test_frontend_function(
@@ -36,8 +36,9 @@ def test_numpy_correlate(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="numpy",
-        fn_tree="correlate",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         a=xs[0],
         v=xs[1],
         mode=mode,
