2ed3d38529e2c64c7cb27516e88bcb937d06d0b1
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-11-03 10:00:13 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -10,23 +10,22 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.split"
     ),
-    xs_n_input_dtypes_n_unique_idx=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
-    ),
+    dtype=helpers.get_dtypes("numeric", full=False, none=True),
+    where=np_frontend_helpers.where(),
 )
 def test_numpy_split(
     as_variable,
+    where,
+    dtype,
     num_positional_args,
-    native_array,
-    xs_n_input_dtypes_n_unique_idx,
 ):
-    indices_or_sections, ary, axis, input_dtypes = xs_n_input_dtypes_n_unique_idx
+    indices_or_sections, ary, axis, input_dtypes = dtype
     helpers.test_frontend_function(
         as_variable_flags=as_variable,
         input_dtypes=input_dtypes,
+        where=where,
         with_out=False,
         num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend="numpy",
         fn_tree="split",
         ary=ary[0],
