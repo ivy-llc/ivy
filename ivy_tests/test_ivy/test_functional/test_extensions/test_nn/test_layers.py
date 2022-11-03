8783386e23c925187d750d956dcb42fb320d6848
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-11-03 11:01:24 +0000

    recover the 2d test

--- ivy_tests/test_ivy/test_functional/test_extensions/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_extensions/test_nn/test_layers.py
@@ -151,10 +151,10 @@ def test_hann_window(
 
 @handle_cmd_line_args
 @given(
-    x_k_s_p=helpers.arrays_for_pooling(min_dims=5, max_dims=5, min_side=1, max_side=4),
-    num_positional_args=helpers.num_positional_args(fn_name="max_pool3d"),
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=4, max_dims=4, min_side=1, max_side=4),
+    num_positional_args=helpers.num_positional_args(fn_name="max_pool2d"),
 )
-def test_max_pool3d(
+def test_max_pool2d(
     *,
     x_k_s_p,
     with_out,
@@ -175,7 +175,7 @@ def test_max_pool3d(
         container_flags=container,
         instance_method=instance_method,
         fw=fw,
-        fn_name="max_pool3d",
+        fn_name="max_pool2d",
         rtol_=1e-2,
         atol_=1e-2,
         ground_truth_backend="jax",
@@ -450,4 +450,41 @@ def test_hamming_window(
         alpha=alpha,
         beta=beta,
         dtype=dtype,
+    )
+
+
+@handle_cmd_line_args
+@given(
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=5, max_dims=5, min_side=1, max_side=4),
+    num_positional_args=helpers.num_positional_args(fn_name="max_pool3d"),
+)
+def test_max_pool3d(
+    *,
+    x_k_s_p,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    dtype, x, kernel, stride, pad = x_k_s_p
+    helpers.test_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="max_pool3d",
+        rtol_=1e-2,
+        atol_=1e-2,
+        ground_truth_backend="jax",
+        x=x[0],
+        kernel=kernel,
+        strides=stride,
+        padding=pad,
     )
