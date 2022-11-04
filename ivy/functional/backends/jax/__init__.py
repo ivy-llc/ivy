0364813da45e7f2ac7e13ebbb443f8f2dea94aac
kurshakuz <abilkasov@gmail.com>
2022-11-04 20:42:56 +0000

    fix additional imports

--- ivy/functional/backends/jax/__init__.py
+++ ivy/functional/backends/jax/__init__.py
@@ -166,8 +166,8 @@ from . import gradients
 from .gradients import *
 from . import layers
 from .layers import *
-from . import linalg
-from .linalg import *
+from . import linear_algebra
+from .linear_algebra import *
 from . import manipulation
 from .manipulation import *
 from . import random
