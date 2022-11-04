89e5135287aa0a25c06602dacfb49e2a666f9582
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-04 17:48:16 +0100

    jax.kron (#6581)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -2080,3 +2080,39 @@ def test_jax_numpy_isposinf(
         fn_tree="numpy.isposinf",
         x=x[0],
     )
+
+
+# kron
+@handle_cmd_line_args
+@given(
+    dtype_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_dim_size=3,
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.kron"
+    ),
+)
+def test_jax_numpy_kron(
+    dtype_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.kron",
+        a=x[0],
+        b=x[1],
+    )
