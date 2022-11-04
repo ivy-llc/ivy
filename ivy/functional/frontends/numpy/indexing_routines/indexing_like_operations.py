c62b528f5d5593866cc6a1108b2e95f80d714576
robertsn808 <robertsn@hawaii.edu>
2022-11-03 17:33:41 -1000

    Revert "Update indexing_like_operations.py"
    This reverts commit 57a8f0c1848d4e4aced9253ed6e81a8b1dd6de36.

--- ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
+++
@@ -1,7 +0,0 @@
-from numpy import array_function_dispatch
-from numpy.core.fromnumeric import _take_dispatcher, _wrapfunc
-
-
-@array_function_dispatch(_take_dispatcher)
-def take(a, indices, axis=None, mode='raise'):
-    return _wrapfunc(a, 'take', indices, axis=axis, mode=mode)
