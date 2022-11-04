e45b1e9955ee9b96f3e1bc86f48dbafeabf68431
m7mdmosa27 <mo7mdmosa27@gmail.com>
2022-11-04 20:54:59 +0200

    add diag function and test_numpy_diag

--- ivy/functional/frontends/numpy/creation_routines/building_matrices.py
+++ ivy/functional/frontends/numpy/creation_routines/building_matrices.py
@@ -5,3 +5,7 @@ from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 @to_ivy_arrays_and_back
 def tril(m, k=0):
     return ivy.tril(m, k=0)
+    
+@to_ivy_arrays_and_back
+def diag(v, k=0):
+    return ivy.diag(v, k=0)
