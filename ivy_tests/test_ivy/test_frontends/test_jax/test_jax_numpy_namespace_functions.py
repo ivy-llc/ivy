509755577e06447be06c9d39b8f46b77ff7e062e
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-05 12:57:15 +0100

    jax_logaddexp2 (#6601)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -2153,3 +2153,42 @@ def test_jax_numpy_lcm(
         x1=x[0],
         x2=x[1],
     )
+
+
+# logaddexp2
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True,
+        min_num_dims=1,
+        max_num_dims=3,
+        min_value=-100,
+        max_value=100,
+        allow_nan=False,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.logaddexp2"
+    ),
+)
+def test_jax_numpy_logaddexp2(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.logaddexp2",
+        rtol=1e-03,
+        atol=1e-03,
+        x1=x[0],
+        x2=x[1],
+    )
