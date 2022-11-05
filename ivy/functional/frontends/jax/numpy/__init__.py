1d6ed56aeab42d80f2ad4276b775e47988e4c6e1
robertsn808 <robertsn@hawaii.edu>
2022-11-04 22:09:47 -1000

    Changes to /ivy/functional/frontends/jax/numpy/fft.py Changes to /ivy/functional/frontends/jax/numpy/__init__.py

--- ivy/functional/frontends/jax/numpy/__init__.py
+++ ivy/functional/frontends/jax/numpy/__init__.py
@@ -1,5 +1,20 @@
 # flake8: noqa
+from typing import Optional
+
+import numpy as np
+from etils.array_types import ArrayLike, Array
+from jax._src.numpy.fft import _fft_core_1d
+from jax._src.numpy.util import _wraps
+from jaxlib import xla_client
+
 from . import fft
 from . import linalg
 from . import name_space_functions
 from .name_space_functions import *
+
+
+@_wraps(np.fft.fft)
+def fft(a: ArrayLike, n: Optional[int] = None,
+        axis: int = -1, norm: Optional[str] = None) -> Array:
+    return _fft_core_1d('fft', xla_client.FftType.FFT, a, n=n, axis=axis,
+                        norm=norm)
---
+++ ivy/functional/frontends/jax/numpy/fft.py
@@ -0,0 +1,6 @@
+# in /ivy/functional/frontends/jax/numpy/fft.py
+
+import jax
+from tensorflow.python.framework.test_ops import a
+
+jax.numpy.fft.fft(a, n=None, axis=- 1, norm=None)
