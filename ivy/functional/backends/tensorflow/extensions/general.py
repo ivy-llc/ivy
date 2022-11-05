9636b286f9ff89d273ff74b3b8bf1269e6c67920
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2022-11-05 19:59:25 +0000

    Fixed some errors with tf & torch implementations

--- ivy/functional/backends/tensorflow/extensions/general.py
+++ ivy/functional/backends/tensorflow/extensions/general.py
@@ -10,6 +10,14 @@ def isin(
     assume_unique: Optional[bool] = False,
     invert: Optional[bool] = False,
 ) -> tf.Tensor:
+    if (tf.rank(elements) == 0):
+        elements = tf.reshape(elements, [1]) 
+    if (tf.rank(test_elements) == 0):
+        test_elements = tf.reshape(test_elements, [1])
     if not assume_unique:
         test_elements = tf.unique(tf.reshape(test_elements, [-1]))[0]
-    return tf.reduce_any(tf.equal(elements[..., tf.newaxis], test_elements), axis=-1) ^ invert
+    if elements.shape != test_elements.shape:
+        elements = elements[..., tf.newaxis]
+    ret = tf.reduce_any(
+        tf.equal(elements, test_elements), axis=-1) ^ invert
+    return ret
--- ivy/functional/backends/torch/extensions/general.py
+++ ivy/functional/backends/torch/extensions/general.py
@@ -1,7 +1,10 @@
 from typing import Optional
 import torch
+from ivy.func_wrapper import with_unsupported_dtypes
+from . import backend_version
 
 
+@with_unsupported_dtypes({"1.11.0 and below": ("bfloat16", "float16")}, backend_version)
 def isin(
     elements: torch.tensor,
     test_elements: torch.tensor,
