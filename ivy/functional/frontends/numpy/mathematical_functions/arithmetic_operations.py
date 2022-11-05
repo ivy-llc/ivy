bbaad35b11cc7e6f2cc9f29f7c4f258a17c181ab
Salisu Ali <salisbuk7897@gmail.com>
2022-11-05 16:50:45 +0100

    Revert "Added mod to numpy frontend mathematical functions"
    This reverts commit 2bd99d9a3cfc997e11c20e7d8d34938c9fd560cf.

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -184,24 +184,3 @@ def floor_divide(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
-
-
-@to_ivy_arrays_and_back
-def mod(
-        x1,
-        x2,
-        /,
-        out=None,
-        *,
-        where=True,
-        casting='same_kind',
-        order='K',
-        dtype=None,
-        subok=True):
-    if dtype:
-        x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
-        x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
-    ret = ivy.remainder(x1, x2, out=out)
-    if ivy.is_array(where):
-        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
-    return ret
