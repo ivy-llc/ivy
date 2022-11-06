359d3b161eac4353bcc9936559850e81533d9f92
Ogban Ugot <ogbanugot@Ogbans-MBP.lan>
2022-11-06 20:51:42 +0100

    add configurable tracemodes

--- ivy/exceptions.py
+++ ivy/exceptions.py
@@ -7,10 +7,6 @@ import types
 import traceback
 
 
-_frontend_truncated_stack_traces = "ivy/functional/frontends"
-_ivy_truncated_stack_traces = "ivy/"
-
-
 class IvyException(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -64,39 +60,43 @@ def handle_exceptions(fn: Callable) -> Callable:
         try:
             return fn(*args, **kwargs)
         except (IndexError, ValueError, AttributeError) as e:
-            if ivy.get_exception_trace_mode():
-                msg = filter_traceback(e.__traceback__, "ivy/functional/frontends")
-                raise ivy.exceptions.IvyError(fn.__name__, str(msg))
+            if (
+                ivy.get_exception_trace_mode() == "frontend"
+                or ivy.get_exception_trace_mode() == "ivy"
+            ):
+                tb = _process_traceback_frames(
+                    e.__traceback__, ivy.trace_mode_dict[ivy.get_exception_trace_mode()]
+                )
+                exp = ivy.exceptions.IvyError(fn.__name__, str(e))
+                raise exp.with_traceback(tb) from None
             else:
-                raise ivy.exceptions.IvyError(fn.__name__, str(e)) from None
+                raise ivy.exceptions.IvyError(fn.__name__, str(e))
         except Exception as e:
-            if ivy.get_exception_trace_mode():
-                tb = filter_traceback(e.__traceback__, "ivy/functional/backends")
+            if (
+                ivy.get_exception_trace_mode() == "frontend"
+                or ivy.get_exception_trace_mode() == "ivy"
+            ):
+                tb = _process_traceback_frames(
+                    e.__traceback__, ivy.trace_mode_dict[ivy.get_exception_trace_mode()]
+                )
                 exp = ivy.exceptions.IvyBackendException(fn.__name__, str(e))
-                exp.__traceback__ = tb
-                raise exp
+                raise exp.with_traceback(tb) from None
             else:
-                tb = filter_traceback(e.__traceback__, "ivy/functional/backends")
-                exp = ivy.exceptions.IvyBackendException(fn.__name__, str(e))
-                exp.__traceback__ = tb
-                raise exp from None
+                raise ivy.exceptions.IvyBackendException(fn.__name__, str(e))
 
     new_fn.handle_exceptions = True
     return new_fn
 
 
-def filter_traceback(tb, with_filter):
-    out = None
-    # Scan the traceback and collect relevant frames.
-    frames = list(traceback.walk_tb(tb))
-    for f, lineno in reversed(frames):
-        if path_contains(f.f_code.co_filename, with_filter):
-            out = types.TracebackType(
-                out, f, f.f_lasti, lineno
-            )  # pytype: disable=wrong-arg-count
-    return out
+def _process_traceback_frames(tb, with_filter):
+    new_tb = None
+    tb_list = list(traceback.walk_tb(tb))
+    for f, line_no in reversed(tb_list):
+        if _path_contains(f.f_code.co_filename, with_filter):
+            new_tb = types.TracebackType(new_tb, f, f.f_lasti, line_no)
+    return new_tb
 
 
-def path_contains(path, string_to_match):
+def _path_contains(path, string_to_match):
     path = os.path.abspath(path)
     return string_to_match in path
