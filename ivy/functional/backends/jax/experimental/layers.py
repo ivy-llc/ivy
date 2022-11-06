f940e1eb00cd7d21b76a20064489fea97d4e33e8
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-11-06 17:08:47 +0000

    refactored functions in experimental to correct submodules (#6653)
    part 3/3 of refactoring to experimental.

--- ivy/functional/backends/jax/experimental/layers.py
+++ ivy/functional/backends/jax/experimental/layers.py
@@ -1,41 +1,8 @@
-from typing import Optional, Union, Tuple, Callable, Literal, Sequence, Any
-from numbers import Number
+from typing import Optional, Union, Tuple
 import ivy
 from ivy.functional.backends.jax import JaxArray
 import jax.lax as jlax
 import jax.numpy as jnp
-import math
-
-
-def vorbis_window(
-    window_length: JaxArray,
-    *,
-    dtype: Optional[jnp.dtype] = jnp.float32,
-    out: Optional[JaxArray] = None,
-) -> JaxArray:
-    return jnp.array(
-        [
-            round(
-                math.sin(
-                    (ivy.pi / 2) * (math.sin(ivy.pi * (i) / (window_length * 2)) ** 2)
-                ),
-                8,
-            )
-            for i in range(1, window_length * 2)[0::2]
-        ],
-        dtype=dtype,
-    )
-
-
-def hann_window(
-    window_length: int,
-    periodic: Optional[bool] = True,
-    dtype: Optional[jnp.dtype] = None,
-    *,
-    out: Optional[JaxArray] = None,
-) -> JaxArray:
-    window_length = window_length + 1 if periodic is True else window_length
-    return jnp.array(jnp.hanning(window_length), dtype=dtype)
 
 
 def _pool(inputs, init, reduce_fn, window_shape, strides, padding):
@@ -130,83 +97,6 @@ def kaiser_window(
         return jnp.array(jnp.kaiser(M=window_length, beta=beta), dtype=dtype)
     else:
         return jnp.array(jnp.kaiser(M=window_length + 1, beta=beta)[:-1], dtype=dtype)
-
-
-def _flat_array_to_1_dim_array(x):
-    return x.reshape((1,)) if x.shape == () else x
-
-
-def pad(
-    input: JaxArray,
-    pad_width: Union[Sequence[Sequence[int]], JaxArray, int],
-    /,
-    *,
-    mode: Optional[
-        Union[
-            Literal[
-                "constant",
-                "edge",
-                "linear_ramp",
-                "maximum",
-                "mean",
-                "median",
-                "minimum",
-                "reflect",
-                "symmetric",
-                "wrap",
-                "empty",
-            ],
-            Callable,
-        ]
-    ] = "constant",
-    stat_length: Optional[Union[Sequence[Sequence[int]], int]] = None,
-    constant_values: Optional[Union[Sequence[Sequence[Number]], Number]] = 0,
-    end_values: Optional[Union[Sequence[Sequence[Number]], Number]] = 0,
-    reflect_type: Optional[Literal["even", "odd"]] = "even",
-    out: Optional[JaxArray] = None,
-    **kwargs: Optional[Any],
-) -> JaxArray:
-    if callable(mode):
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-            **kwargs,
-        )
-    if mode in ["maximum", "mean", "median", "minimum"]:
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-            stat_length=stat_length,
-        )
-    elif mode == "constant":
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-            constant_values=constant_values,
-        )
-    elif mode == "linear_ramp":
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-            end_values=end_values,
-        )
-    elif mode in ["reflect", "symmetric"]:
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-            reflect_type=reflect_type,
-        )
-    else:
-        return jnp.pad(
-            _flat_array_to_1_dim_array(input),
-            pad_width,
-            mode=mode,
-        )
 
 
 def max_pool3d(
--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
@@ -6,7 +6,7 @@ import numpy as np
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
-
+import ivy_tests.test_array_api.array_api_tests.hypothesis_helpers as hypothesis_helpers
 
 # Helpers #
 # ------- #
@@ -486,4 +486,161 @@ def test_i0(
         fw=fw,
         fn_name="i0",
         x=x[0],
+    )
+
+
+# flatten
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=1, max_num_dims=5), key="flatten_shape"
+        ),
+        min_value=-100,
+        max_value=100,
+    ),
+    axes=helpers.get_axis(
+        shape=st.shared(
+            helpers.get_shape(min_num_dims=1, max_num_dims=5), key="flatten_shape"
+        ),
+        allow_neg=True,
+        sorted=True,
+        min_size=2,
+        max_size=2,
+        unique=False,
+        force_tuple=True,
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="flatten"),
+)
+def test_flatten(
+    dtype_and_x,
+    axes,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtypes, x = dtype_and_x
+    x = np.asarray(x[0], dtype=input_dtypes[0])
+
+    if axes[1] == 0:
+        start_dim, end_dim = axes[1], axes[0]
+    elif axes[0] * axes[1] < 0:
+        if x.ndim + min(axes) >= max(axes):
+            start_dim, end_dim = max(axes), min(axes)
+        else:
+            start_dim, end_dim = min(axes), max(axes)
+    else:
+        start_dim, end_dim = axes[0], axes[1]
+    helpers.test_function(
+        input_dtypes=input_dtypes,
+        as_variable_flags=as_variable,
+        with_out=True,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="flatten",
+        x=x,
+        start_dim=start_dim,
+        end_dim=end_dim,
+    )
+
+
+def _st_tuples_or_int(n_pairs):
+    return st.one_of(
+        hypothesis_helpers.tuples(
+            st.tuples(
+                st.integers(min_value=1, max_value=4),
+                st.integers(min_value=1, max_value=4),
+            ),
+            min_size=n_pairs,
+            max_size=n_pairs,
+        ),
+        helpers.ints(min_value=1, max_value=4),
+    )
+
+
+@st.composite
+def _pad_helper(draw):
+    dtype, value, shape = draw(
+        helpers.dtype_and_values(
+            available_dtypes=helpers.get_dtypes("numeric"),
+            ret_shape=True,
+            min_num_dims=1,
+        )
+    )
+    ndim = len(shape)
+    pad_width = draw(_st_tuples_or_int(ndim))
+    stat_length = draw(_st_tuples_or_int(ndim))
+    constant_values = draw(_st_tuples_or_int(ndim))
+    end_values = draw(_st_tuples_or_int(ndim))
+    return dtype, value, pad_width, stat_length, constant_values, end_values
+
+
+@handle_cmd_line_args
+@given(
+    dtype_and_input_and_other=_pad_helper(),
+    mode=st.sampled_from(
+        [
+            "constant",
+            "edge",
+            "linear_ramp",
+            "maximum",
+            "mean",
+            "median",
+            "minimum",
+            "reflect",
+            "symmetric",
+            "wrap",
+        ]
+    ),
+    reflect_type=st.sampled_from(["even", "odd"]),
+    num_positional_args=helpers.num_positional_args(fn_name="pad"),
+)
+def test_pad(
+    *,
+    dtype_and_input_and_other,
+    mode,
+    reflect_type,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    (
+        dtype,
+        value,
+        pad_width,
+        stat_length,
+        constant_values,
+        end_values,
+    ) = dtype_and_input_and_other
+    helpers.test_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="pad",
+        ground_truth_backend="numpy",
+        input=value[0],
+        pad_width=pad_width,
+        mode=mode,
+        stat_length=stat_length,
+        constant_values=constant_values,
+        end_values=end_values,
+        reflect_type=reflect_type,
+        out=None,
     )
