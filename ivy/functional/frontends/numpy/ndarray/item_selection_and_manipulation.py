fe9503d9f49be9fb3032877e01994507912ae516
Hellenn07 <59020915+Hellenn07@users.noreply.github.com>
2022-11-07 10:04:05 +0300

    Update item_selection_and_manipulation.py

---
+++ ivy/functional/frontends/numpy/ndarray/item_selection_and_manipulation.py
@@ -0,0 +1,13 @@
+# global
+import ivy
+
+
+def partition(
+    a, kth, axis=- 1, 
+    kind='introselect', 
+    order=None
+):
+    return ivy.partition(a, kth, axis=- 1, 
+    kind='introselect', 
+    order=None)
+
