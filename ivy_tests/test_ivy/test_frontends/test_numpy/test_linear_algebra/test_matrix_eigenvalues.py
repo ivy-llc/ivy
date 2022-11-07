542c965c996fdb3b166a72467f5f70b0e3650607
William Oskar Hirst <bod_holthe@outlook.com>
2022-11-07 17:32:48 +0100

    Changed given arg to get_matrix.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_matrix_eigenvalues.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_matrix_eigenvalues.py
@@ -5,16 +5,13 @@ from hypothesis import given
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_cmd_line_args
-
+from ivy_tests.test_ivy.test_functional.test_core.test_linalg import (
+    _get_dtype_and_matrix,
+)
 
 @handle_cmd_line_args
 @given(
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        min_value=0,
-        max_value=50,
-        shape=helpers.ints(min_value=2, max_value=8).map(lambda x: tuple([x, x])),
-    ),
+    dtype_and_x=_get_dtype_and_matrix(),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.linalg.eig"
     ),
