71c89a00b7c83f7f2f1e16b605ae7d3e146ed248
kurshakuz <abilkasov@gmail.com>
2022-11-07 18:17:08 +0000

    revert array and container linear_algebra classes

--- ivy/container/container.py
+++ ivy/container/container.py
@@ -15,7 +15,7 @@ from .general import ContainerWithGeneral
 from .gradients import ContainerWithGradients
 from .image import ContainerWithImage
 from .layers import ContainerWithLayers
-from .linalg import ContainerWithLinalg
+from .linear_algebra import ContainerWithLinearAlgebra
 from .losses import ContainerWithLosses
 from .manipulation import ContainerWithManipulation
 from .norms import ContainerWithNorms
@@ -60,7 +60,7 @@ class Container(
     ContainerWithGradients,
     ContainerWithImage,
     ContainerWithLayers,
-    ContainerWithLinalg,
+    ContainerWithLinearAlgebra,
     ContainerWithLosses,
     ContainerWithManipulation,
     ContainerWithNorms,
