6883872d16d51e7271f52b8c3b93f33d9a02bb3a
Rupesh <rupeshgelal9@gmail.com>
2022-11-07 17:54:56 +0545

    tensor file committed
    Signed-off-by: Rupesh <rupeshgelal9@gmail.com>

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -47,6 +47,13 @@ class Tensor:
         self.data = self.cos()
         return self.data
 
+    def cosh(self, *, out=None):
+        return torch_frontend.cosh(self.data, out=out)
+
+    def cosh_(self):
+        self.data = self.cosh()
+        return self.data    
+
     def arcsin(self, *, out=None):
         return torch_frontend.arcsin(self.data, out=out)
 
