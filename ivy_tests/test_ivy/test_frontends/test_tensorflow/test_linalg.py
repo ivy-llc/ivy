a8623e5a58d357d789b87d1ee4eada409601201b
fspyridakos <72918194+fspyridakos@users.noreply.github.com>
2022-11-08 14:06:11 +0000

    Added trace to tf.linalg frontend (#6729)

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -532,3 +532,30 @@ def test_tensorflow_l2_normalize(
         x=x[0],
         axis=axis,
     )
+
+
+# trace
+@handle_cmd_line_args
+@given(
+    dtype_and_input=_get_dtype_and_matrix(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.linalg.trace"
+    ),
+)
+def test_tensorflow_trace(
+    dtype_and_input,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="tensorflow",
+        fn_tree="linalg.trace",
+        x=x[0],
+    )
