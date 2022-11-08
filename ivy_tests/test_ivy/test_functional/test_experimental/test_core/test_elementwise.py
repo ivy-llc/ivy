e1c7ab57e82779633a019aaa47d20f9eb8e0382d
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-11-08 16:00:42 +0000

    unit test

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
@@ -682,3 +682,50 @@ def test_signbit(
         fn_name="signbit",
         x=np.asarray(x[0], dtype=input_dtype[0]),
     )
+
+
+# allclose
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True,
+        min_num_dims=1,
+        max_num_dims=3,
+    ),
+    rtol=st.floats(min_value=1e-5, max_value=1e-5),
+    atol=st.floats(min_value=1e-5, max_value=1e-5),
+    equal_nan=st.booleans(),
+    num_positional_args=helpers.num_positional_args(fn_name="allclose"),
+)
+def test_allclose(
+    dtype_and_x,
+    rtol,
+    atol,
+    equal_nan,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="allclose",
+        x1=x[0],
+        x2=x[1],
+        rtol=rtol,
+        atol=atol,
+        equal_nan=equal_nan,
+    )
