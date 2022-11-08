c85f921ea2086afef04711d48a12f09e03dc45fc
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2022-11-08 09:34:03 +0000

    fix fmax as tf.math.equal can not compare nans

--- ivy/functional/backends/tensorflow/experimental/elementwise.py
+++ ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -55,8 +55,8 @@ def fmax(
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
     temp = tf.constant(float("nan"))
-    x1 = tf.where(tf.math.equal(x1, temp), x2, x1)
-    x2 = tf.where(tf.math.equal(x2, temp), x1, x2)
+    x1 = tf.where(tf.math.is_nan(x1, temp), x2, x1)
+    x2 = tf.where(tf.math.is_nan(x2, temp), x1, x2)
     tf.experimental.numpy.experimental_enable_numpy_behavior()
     ret = tf.experimental.numpy.maximum(x1, x2)
     return ret
