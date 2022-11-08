3b5a54731c65136e79cfada50b4b09f919458ba0
ojumah20 <onyekachukwuojumah20@gmail.com>
2022-11-08 12:30:32 +0100

    doc string update

--- ivy/functional/ivy/data_type.py
+++ ivy/functional/ivy/data_type.py
@@ -1871,6 +1871,16 @@ def unset_default_float_dtype():
     >>> ivy.unset_default_float_dtype()
     >>> ivy.default_float_dtype()
     'float32'
+
+
+    >>> ivy.set_default_float_dtype(ivy.float32)
+    >>> ivy.set_default_float_dtype(ivy.float64)
+    >>> ivy.default_float_dtype_stack
+    ['float32','float64']
+
+    >>> ivy.unset_default_float_dtype()
+    >>> ivy.default_float_dtype_stack
+    ['float32']
     """
     global default_float_dtype_stack
     if default_float_dtype_stack:
