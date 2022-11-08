41c7147176de4934e2d2307271c3c85167a8af4c
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-11-08 22:36:31 +0530

    fix the error

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -10,13 +10,13 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 @handle_cmd_line_args
 @given(
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float")),
+        available_dtypes=helpers.get_dtypes("float"),
         min_num_dims=1,
         max_num_dims=1
     ),
     dtype=helpers.get_dtypes("float", full=False),
     num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.vorbis_window")
+        fn_name="ivy.functional.frontends.tensorflow.vorbis_window"),
 )
 def test_tensorflow_vorbis_window(
         dtype_and_x,
