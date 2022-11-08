56fcc96e8b982d40a8e3bf3e4032f96279b9b4eb
Ammar-Shah <35238131+Ammar-Shah@users.noreply.github.com>
2022-11-08 17:13:45 +0500

    Update __init__.py

--- ivy/functional/frontends/jax/__init__.py
+++ ivy/functional/frontends/jax/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa
 from . import devicearray
 from .devicearray import DeviceArray
-from . import lax
+# from . import lax
 from . import nn
-from . import numpy
+# from . import numpy
 from . import _src
 
 import ivy
