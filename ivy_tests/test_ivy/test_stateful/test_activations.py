1717644ddccd67617ba140c200b929d7d9534a5e
CatB1t <skytedits@gmail.com>
2022-11-09 15:22:51 +0200

    update `test_activations.py`.

--- ivy_tests/test_ivy/test_stateful/test_activations.py
+++ ivy_tests/test_ivy/test_stateful/test_activations.py
@@ -2,7 +2,6 @@
 
 # global
 from hypothesis import strategies as st, assume
-import pytest
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -14,8 +13,6 @@ from ivy_tests.test_ivy.helpers.function_testing import (
     NumPositionalArg,
 )
 
-pytestmark = pytest.mark.skip("Fixing method testing issues, related to new decorator.")
-
 
 # GELU
 @handle_method(
@@ -24,16 +21,16 @@ pytestmark = pytest.mark.skip("Fixing method testing issues, related to new deco
         available_dtypes=helpers.get_dtypes("numeric")
     ),
     approximate=st.booleans(),
+    method_num_positional_args=helpers.num_positional_args(fn_name="GELU._forward"),
 )
 def test_gelu(
     *,
     dtype_and_x,
     approximate,
     init_num_positional_args: NumPositionalArg,
-    method_num_positional_args: NumPositionalArg,
+    method_num_positional_args,
     init_as_variable: AsVariableFlags,
     init_native_array: NativeArrayFlags,
-    init_container: ContainerFlags,
     method_as_variable: AsVariableFlags,
     method_native_array: NativeArrayFlags,
     method_container: ContainerFlags,
@@ -43,9 +40,11 @@ def test_gelu(
     input_dtype, x = dtype_and_x
     helpers.test_method(
         input_dtypes_init=input_dtype,
-        num_positional_args_init=init_num_positional_args,
         all_as_kwargs_np_init={"approximate": approximate},
         input_dtypes_method=input_dtype,
+        as_variable_flags_init=init_as_variable,
+        num_positional_args_init=init_num_positional_args,
+        native_array_flags_init=init_native_array,
         as_variable_flags_method=method_as_variable,
         num_positional_args_method=method_num_positional_args,
         native_array_flags_method=method_native_array,
@@ -66,34 +65,32 @@ def test_gelu(
         small_abs_safety_factor=4,
         safety_factor_scale="log",
     ),
-    num_positional_args_init=helpers.num_positional_args(fn_name="GEGLU.__init__"),
     num_positional_args_method=helpers.num_positional_args(fn_name="GEGLU._forward"),
 )
 def test_geglu(
     *,
     dtype_and_x,
-    num_positional_args_init,
+    num_positional_args_init: NumPositionalArg,
     num_positional_args_method,
-    as_variable,
-    native_array,
-    container,
+    method_as_variable_flags: AsVariableFlags,
+    method_native_array_flags: NativeArrayFlags,
+    method_container_flags: ContainerFlags,
     class_name,
     method_name,
 ):
     input_dtype, x = dtype_and_x
-    # float16 is somehow generated
-    assume("float16" not in input_dtype)
     # last dim must be even, this could replaced with a private helper
     assume(x[0].shape[-1] % 2 == 0)
     helpers.test_method(
         input_dtypes_init=input_dtype,
         num_positional_args_init=num_positional_args_init,
         input_dtypes_method=input_dtype,
-        as_variable_flags_method=as_variable,
+        as_variable_flags_method=method_as_variable_flags,
         num_positional_args_method=num_positional_args_method,
-        native_array_flags_method=native_array,
-        container_flags_method=container,
+        native_array_flags_method=method_native_array_flags,
+        container_flags_method=method_container_flags,
         all_as_kwargs_np_method={"inputs": x[0]},
-        class_name="GEGLU",
+        class_name=class_name,
+        method_name=method_name,
         atol_=1e-3,
     )
