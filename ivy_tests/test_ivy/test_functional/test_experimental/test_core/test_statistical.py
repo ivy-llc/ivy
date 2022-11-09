479bd502b66d9cecd754e3ec1c524cd4defd1f17
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-09 18:34:45 +0100

    ivy_unravel_index (#6807)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
@@ -125,3 +125,56 @@ def test_nanmean(
         keepdims=keep_dims,
         dtype=dtype,
     )
+
+
+# unravel_index
+@st.composite
+def max_value_as_shape_prod(draw):
+    shape = draw(
+        helpers.get_shape(
+            min_num_dims=1,
+            max_num_dims=5,
+            min_dim_size=1,
+            max_dim_size=5,
+        )
+    )
+    dtype_and_x = draw(
+        helpers.dtype_values_axis(
+            available_dtypes=helpers.get_dtypes("integer"),
+            min_value=0,
+            max_value=np.prod(shape) - 1
+        )
+    )
+    return dtype_and_x, shape
+
+
+@handle_cmd_line_args
+@given(
+    dtype_x_shape=max_value_as_shape_prod(),
+    num_positional_args=helpers.num_positional_args(fn_name="unravel_index"),
+)
+def test_unravel_index(
+    dtype_x_shape,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    dtype_and_x, shape = dtype_x_shape
+    input_dtype, x = dtype_and_x[0], dtype_and_x[1]
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="unravel_index",
+        indices=np.asarray(x[0], dtype=input_dtype[0]),
+        shape=shape
+    )
