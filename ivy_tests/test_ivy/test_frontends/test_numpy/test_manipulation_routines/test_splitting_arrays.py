5873da1b9303fa81ab9626ce40dbc7b0eb7dc91a
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-11-09 21:47:45 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -10,16 +10,16 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.numpy.split"
     ),
-    dtype_and_x=helpers.dtype_and_values(
+    available_dtypes=helpers.get_dtypes(
         dtype=helpers.get_dtypes("numeric", full=False, none=True),
     ),
 )
 def test_numpy_split(
     as_variable,
-    dtype_and_x,
+    available_dtype,
     num_positional_args,
 ):
-    indices_or_sections, ary, axis, input_dtypes = dtype_and_x
+    indices_or_sections, ary, axis, input_dtypes = available_dtype
     helpers.test_frontend_function(
         as_variable_flags=as_variable,
         input_dtypes=input_dtypes,
