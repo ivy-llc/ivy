633d47c92b9dde74e4536c96b923d0c942b8ba0f
pillarxyz <ayman.lafaz@um5r.ac.ma>
2022-11-09 20:30:37 +0000

    fixing isin for tensorflow

--- ivy/functional/backends/tensorflow/experimental/general.py
+++ ivy/functional/backends/tensorflow/experimental/general.py
@@ -10,14 +10,17 @@ def isin(
     assume_unique: Optional[bool] = False,
     invert: Optional[bool] = False,
 ) -> tf.Tensor:
+    input_shape = elements.shape
+
     if (tf.rank(elements) == 0):
         elements = tf.reshape(elements, [1])
     if (tf.rank(test_elements) == 0):
         test_elements = tf.reshape(test_elements, [1])
-    if not assume_unique:
+    if (not assume_unique):
         test_elements = tf.unique(tf.reshape(test_elements, [-1]))[0]
-    if elements.shape != test_elements.shape:
-        elements = elements[..., tf.newaxis]
-    ret = tf.reduce_any(
-        tf.equal(elements, test_elements), axis=-1) ^ invert
-    return ret
+
+    elements = tf.reshape(elements, [-1])
+    test_elements = tf.reshape(test_elements, [-1])
+
+    output = tf.reduce_any(tf.equal(tf.expand_dims(elements, -1), test_elements), axis=-1)
+    return tf.reshape(output, input_shape) ^ invert
