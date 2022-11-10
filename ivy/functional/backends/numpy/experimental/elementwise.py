b2743719c097547fa2a242a40f7aa206b49c3c23
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-11-10 12:17:52 +0530

    add allclose to ivy experimental (#6742)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy/functional/backends/numpy/experimental/elementwise.py
+++ ivy/functional/backends/numpy/experimental/elementwise.py
@@ -216,3 +216,19 @@ def signbit(
 
 
 signbit.support_native_out = True
+
+
+def allclose(
+    x1: np.ndarray,
+    x2: np.ndarray,
+    /,
+    *,
+    rtol: Optional[float] = 1e-05,
+    atol: Optional[float] = 1e-08,
+    equal_nan: Optional[bool] = False,
+    out: Optional[np.ndarray] = None,
+) -> bool:
+    return np.allclose(x1, x2, rtol=rtol, atol=atol, equal_nan=equal_nan)
+
+
+isclose.support_native_out = False
