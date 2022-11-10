7c9cb4d3c3d7b9a8aaea17264f78275a3a7ae57b
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-10 12:31:19 +0100

    Ivy fix (#6838)
    * ivy_fix
    
    * fixes
    
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/backends/tensorflow/experimental/elementwise.py
+++ ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -225,3 +225,15 @@ def allclose(
     return tf.experimental.numpy.allclose(
         x1, x2, rtol=rtol, atol=atol, equal_nan=equal_nan
     )
+
+
+@with_unsupported_dtypes(
+    {"2.9.1 and below": ("bfloat16,")}, backend_version
+)
+def fix(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> Union[tf.Tensor, tf.Variable]:
+    return tf.experimental.numpy.fix(x)
