15995ce90a42e1a9f2f3106dcaf0a255f54b9296
nassimberrada <Nassim>
2022-11-10 17:57:51 +0100

    ivy_nextafter

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
@@ -834,3 +834,43 @@ def test_fix(
         fn_name="fix",
         x=np.asarray(x[0], dtype=input_dtype[0]),
     )
+
+
+# nextafter
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float", index=2),
+        num_arrays=2,
+        shared_dtype=True,
+        min_value=-10,
+        max_value=10,
+        min_num_dims=1,
+        max_num_dims=3,
+    ),
+    num_positional_args=helpers.num_positional_args(fn_name="nextafter"),
+)
+def test_nextafter(
+    dtype_and_x,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
+        fn_name="nextafter",
+        x1=x[0],
+        x2=x[1],
+    )
