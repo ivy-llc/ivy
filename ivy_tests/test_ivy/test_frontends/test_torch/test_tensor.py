59d48fadd445cd8b09a908b383aca4a83c82feb5
Mohamad H. Danesh <mohamad4danesh@gmail.com>
2022-11-11 12:18:40 +0330

    adding torch frontend (#6828)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -2488,3 +2488,40 @@ def test_torch_instance_arctanh_(dtype_and_x, as_variable, native_array):
         class_name="tensor",
         method_name="arctanh_",
     )
+    
+    
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        min_value=-1e04,
+        max_value=1e04,
+        allow_inf=False,
+    ),
+)
+def test_torch_instance_pow(
+    dtype_and_x,
+    as_variable,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        input_dtypes_init=input_dtype,
+        as_variable_flags_init=as_variable,
+        num_positional_args_init=1,
+        native_array_flags_init=native_array,
+        all_as_kwargs_np_init={
+            "data": x[0],
+        },
+        input_dtypes_method=input_dtype,
+        as_variable_flags_method=as_variable,
+        num_positional_args_method=1,
+        native_array_flags_method=native_array,
+        all_as_kwargs_np_method={
+            "other": x[1],
+        },
+        frontend="torch",
+        class_name="tensor",
+        method_name="pow",
+    )
