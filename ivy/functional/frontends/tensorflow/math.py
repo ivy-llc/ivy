17554f42ec572c716f234b17b56d40f24bd1d5ba
Abhishek Jha <ajha17@asu.edu>
2022-11-11 08:48:31 +0530

    Modified the implementation to deal with None values

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -201,9 +201,9 @@ def polyval(coeffs, x, name=None):
 
 @to_ivy_arrays_and_back
 def pow(x, y, name="pow"):
-    if not (isinstance(x, int) or isinstance(x, float)):
+    if not (isinstance(x, int) or isinstance(x, float) or (x is None)):
         x = x.data
-    if not (isinstance(y, int) or isinstance(y, float)):
+    if not (isinstance(y, int) or isinstance(y, float) or (y is None)):
         y = y.data
     x, y = promote_types_of_tensorflow_inputs(x, y)
     return ivy.pow(x, y)
