c903b51a84b18cd5f6d0e0d0b9d8a5c28c09f4e1
Uday <iamuday1618@gmail.com>
2022-11-11 22:43:20 +0530

    Fix random.uniform tests

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -1,6 +1,8 @@
 import ivy
+from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
 
 
+@to_ivy_arrays_and_back
 def uniform(shape, minval=0, maxval=None, dtype=ivy.float32, seed=None, name=None):
     return ivy.random_uniform(
         low=minval, high=maxval, shape=shape, dtype=dtype, seed=seed
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
@@ -7,48 +7,52 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 
 @handle_cmd_line_args
 @given(
-    dtype_minval_maxval=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"), num_arrays=2, shared_dtype=True
+    dtype_and_min=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_value=-1000,
+        max_value=100,
     ),
-    dtype_seed=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("integer")),
-    dtypes=helpers.get_dtypes("float", full=False),
+    dtype_and_max=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_value=101,
+        max_value=1000,
+    ),
+    dtype_and_seed=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        max_num_dims=1,
+    ),
+    dtypes=helpers.get_dtypes(kind="numeric", full=False, key="dtypes"),
     num_positional_args=helpers.num_positional_args(
         fn_name="ivy.functional.frontends.tensorflow.random.uniform"
     ),
-    shape=helpers.get_shape(
-        allow_none=False,
-        min_num_dims=1,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=10,
-    ),
+    shape=helpers.get_shape(),
 )
 def test_tensorflow_random_uniform(
-    dtype_minval_maxval,
-    dtype_seed,
+    dtype_and_min,
+    dtype_and_max,
+    dtype_and_seed,
     dtypes,
     num_positional_args,
     shape,
     as_variable,
     native_array,
-    fw,
     with_out,
 ):
-    input_minval_maxval_dtype, (minval, maxval) = dtype_minval_maxval
-    input_seed_dtype, seed = dtype_seed
+    min_dtype, minval = dtype_and_min
+    max_dtype, maxval = dtype_and_max
+    seed_dtype, seed = dtype_and_seed
 
     helpers.test_frontend_function(
-        input_dtypes=input_minval_maxval_dtype + input_seed_dtype,
+        input_dtypes=min_dtype + max_dtype + seed_dtype,
         as_variable_flags=as_variable,
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="tensorflow",
         fn_tree="random.uniform",
         shape=shape,
         minval=minval[0],
         maxval=maxval[0],
-        dtype=dtypes[0],
-        seed=seed,
+        seed=seed[0],
+        dtype="numeric",
     )
