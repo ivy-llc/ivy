56f0e5c97f78ca02c906cef6a9da5385ef90016b
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-11-11 23:05:14 +0530

    multiply to jax numpy frontend (#6928)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -2581,3 +2581,35 @@ def test_jax_numpy_msort(
         fn_tree="numpy.msort",
         a=x[0],
     )
+
+
+# multiply
+@handle_cmd_line_args
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.jax.numpy.multiply"
+    ),
+)
+def test_jax_numpy_multiply(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="jax",
+        fn_tree="numpy.multiply",
+        x1=x[0],
+        x2=x[1],
+    )
