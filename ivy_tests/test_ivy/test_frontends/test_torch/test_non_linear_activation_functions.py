2eb94b0b381e01eb21200198d8e9cb1359a01f90
aryankhatana01 <aryankhatana01@gmail.com>
2022-11-13 15:12:08 +0530

    added relu_

--- ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_non_linear_activation_functions.py
@@ -542,6 +542,36 @@ def test_torch_relu(
     )
 
 
+# relu_
+@handle_cmd_line_args
+@given(
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.torch.nn.functional.relu_"
+    ),
+)
+def test_torch_relu_(
+    dtype_and_input,
+    as_variable,
+    num_positional_args,
+    native_array,
+):
+    input_dtype, input = dtype_and_input
+    _filter_dtypes(input_dtype)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="torch",
+        fn_tree="nn.functional.relu_",
+        input=input[0],
+    )
+
+
 # selu
 @handle_cmd_line_args
 @given(
