a3e4fa225c21469161fc4a70e213f086c4133e9e
unknown <usmansada99@gmail.com>
2022-11-14 00:49:34 +0100

    added tensorflow ones function in the tensorflow frontend

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -21,3 +21,8 @@ def clip_by_value(t, clip_value_min, clip_value_max):
 @with_unsupported_dtypes({"2.9.0 and below": ("float16", "bfloat16")}, "tensorflow")
 def eye(num_rows, num_columns=None, batch_shape=None, dtype=ivy.float32, name=None):
     return ivy.eye(num_rows, num_columns, batch_shape=batch_shape, dtype=dtype)
+
+
+@with_unsupported_dtypes({"2.9.0 and below": ("float16", "bfloat16")}, "tensorflow")
+def ones(shape, dtype=ivy.float32, name=None):
+    return ivy.ones(shape, dtype=dtype)
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -92,3 +92,40 @@ def test_tensorflow_eye(
         batch_shape=batch_shape,
         dtype=dtype,
     )
+
+
+# ones
+@handle_cmd_line_args
+@given(
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.tensorflow.ones"
+    ),
+)
+def test_tensorflow_ones(
+    shape,
+    dtype,
+    as_variable,
+    native_array,
+    num_positional_args,
+    fw,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="tensorflow",
+        fn_tree="ones",
+        dims=shape,
+        dtype=dtype,
+    )
