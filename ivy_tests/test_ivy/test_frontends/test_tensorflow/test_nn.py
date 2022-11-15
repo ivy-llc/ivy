64212a2137e29f518b9dacb7308917c5e610268f
Mohamed Ehab <60079115+mohamedehab0999@users.noreply.github.com>
2022-11-15 08:36:32 +0200

    Update test_nn.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -5,7 +5,6 @@ from hypothesis import strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 
-from ivy_tests.test_ivy.helpers import handle_cmd_line_args
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
