5dd5c463e5ecf663af8cc4300c532ea1bac49be6
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-11-15 21:59:29 +0530

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1284,18 +1284,20 @@ def test_tensorflow_pow(
     
   
 # argmin
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="tensorflow.math.argmin",
     dtype_and_x=statistical_dtype_values(function="argmin"),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.math.argmin"
-    ),
+    output_type=st.sampled_from(["int16", "uint16", "int32", "int64"]),
 )
 def test_tensorflow_argmin(
+    *,
     dtype_and_x,
-    as_variable,
     num_positional_args,
+    as_variable,
     native_array,
+    frontend,
+    fn_tree,
+    on_device,
     output_type,
 ):
     input_dtype, x, axis = dtype_and_x
@@ -1307,8 +1309,9 @@ def test_tensorflow_argmin(
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="tensorflow",
-        fn_tree="math.argmin",
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
         input=x[0],
         axis=axis,
         output_type=output_type,
