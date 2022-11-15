a778390be2959ed9f6b8a7827c4408f82e17e4c1
arslan <artikov.arslan@gmail.com>
2022-11-15 19:59:44 +0300

    interpolation

--- ivy/functional/frontends/torch/nn/functional/vision_functions.py
+++ ivy/functional/frontends/torch/nn/functional/vision_functions.py
@@ -148,3 +148,25 @@ def pad(input, padding, mode="constant", value=0):
                 + "['constant', 'reflect', 'replicate', 'circular']"
             ).format(mode)
         )
+
+
+def interpolate(input, size=None, scale_factor=None, mode='nearest', align_corners=None,
+                recompute_scale_factor=None, antialias=False):
+    input_shape = ivy.shape(input)
+    ivy.assertions.check_equal(
+        ivy.get_num_dims(input),
+        3,
+        message="pixel_shuffle expects 3D, 4D or 5D input, but got input with sizes "
+                + str(input_shape),)
+
+    ivy.assertions.check_equal(
+        ivy.get_num_dims(input),
+        4,
+        message="pixel_shuffle expects 3D, 4D or 5D input, but got input with sizes "
+                + str(input_shape), )
+
+    ivy.assertions.check_equal(
+        ivy.get_num_dims(input),
+        5,
+        message="pixel_shuffle expects 3D, 4D or 5D input, but got input with sizes "
+                + str(input_shape), )
