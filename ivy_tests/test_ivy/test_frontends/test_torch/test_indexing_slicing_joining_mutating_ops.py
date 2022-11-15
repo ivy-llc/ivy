7fa6d1ce65d2278ad0f4f75e722404641aa3c1ed
Rahul Sankar <rahulsnkr@gmail.com>
2022-11-15 11:28:47 -0500

    add torch dstack (#7069)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -720,3 +720,35 @@ def test_torch_hstack(
         on_device=on_device,
         tensors=value,
     )
+
+
+# dstack
+@handle_frontend_test(
+    fn_tree="torch.dstack",
+    dtype_value_shape=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_torch_dstack(
+    *,
+    dtype_value_shape,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, value = dtype_value_shape
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        tensors=value,
+    )
