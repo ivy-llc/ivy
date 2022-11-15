f282ff5f157b89c31dd3ec3de2484d6d049195d9
Rahul Sankar <rahulsnkr@gmail.com>
2022-11-15 11:28:47 -0500

    add torch dstack (#7069)

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -133,3 +133,8 @@ def movedim(input, source, destination):
 @to_ivy_arrays_and_back
 def hstack(tensors, *, out=None):
     return ivy.hstack(tensors, out=out)
+
+
+@to_ivy_arrays_and_back
+def dstack(tensors, *, out=None):
+    return ivy.dstack(tensors, out=out)
