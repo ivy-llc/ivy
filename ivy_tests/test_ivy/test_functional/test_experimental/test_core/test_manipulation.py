b5cd52c37ab3ac967371f641c729d579c86b55fd
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-11-15 02:35:15 +0000

    test update

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
@@ -875,7 +875,7 @@ def test_atleast_2d(
     indices=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("int"),
         min_value=0,
-        max_value=10,
+        max_value=5,
         min_num_dims=2,
         max_num_dims=3,
         min_dim_size=2,
