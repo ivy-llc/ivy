1068beca90abbd997a36cec9feb1f61b75a76f32
Amir Saeed <97600945+FuhrerNovachrono@users.noreply.github.com>
2022-11-15 16:02:20 +0100

    Update __init__.py

--- ivy/array/__init__.py
+++ ivy/array/__init__.py
@@ -7,4 +7,6 @@ from operator import mul
 
 # local
 from .wrapping import add_ivy_array_instance_methods
+from .wrapping import add_ivy_array_special_methods
+from .wrapping import add_ivy_array_reverse_special_methods
 from .array import Array
