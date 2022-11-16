ab778edabb5f3e1c6d4ce5d77820c9019005b6c8
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-11-16 17:46:52 +0000

    fixing missing import

--- ivy/functional/ivy/sorting.py
+++ ivy/functional/ivy/sorting.py
@@ -7,6 +7,7 @@ from ivy.func_wrapper import (
     to_native_arrays_and_back,
     handle_out_argument,
     handle_nestable,
+    handle_array_like
 )
 from ivy.exceptions import handle_exceptions
 
