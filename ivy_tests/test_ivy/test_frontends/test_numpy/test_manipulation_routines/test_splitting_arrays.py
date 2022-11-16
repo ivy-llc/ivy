65a0dcd92be9d0d8fd968dff8c89ee15f94af487
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-11-16 14:01:15 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -6,7 +6,7 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 # split
 @handle_frontend_test(
-    fn_tree="numpy.split"
+    fn_tree="numpy.split",
     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
 )
 def test_numpy_split(
