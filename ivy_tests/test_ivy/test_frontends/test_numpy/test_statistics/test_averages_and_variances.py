2bee2a518f8cfc094864eb5fd60422ba7ab97ab1
Martins Okoye <67116080+Martins-Code@users.noreply.github.com>
2022-11-16 17:28:18 +0000

    Add numpy.std frontend (#6872)
    Co-authored by: @MarShaikh <maruf98@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
@@ -125,3 +125,60 @@ def test_numpy_nanmean(
         where=where,
         test_values=False,
     )
+
+
+# std
+@handle_cmd_line_args
+@given(
+    dtype_and_x=statistical_dtype_values(function="std"),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
+    where=np_frontend_helpers.where(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.std"
+    ),
+    keep_dims=st.booleans(),
+)
+def test_numpy_std(
+        dtype_and_x,
+        dtype,
+        where,
+        as_variable,
+        with_out,
+        num_positional_args,
+        native_array,
+        fw,
+        keep_dims,
+):
+    input_dtype, x, axis, axis_excess = dtype_and_x
+    x_array = ivy.array(x[0])
+
+    if len(x_array.shape) == 2:
+        where = ivy.ones((x_array.shape[0], 1), dtype=ivy.bool)
+    elif len(x_array.shape) == 1:
+        where = True
+
+    where, as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        fw=fw,
+        frontend="numpy",
+        fn_tree="std",
+        x=x[0],
+        axis=axis,
+        dtype=dtype,
+        out=None,
+        correction=0,
+        keepdims=keep_dims,
+        where=where,
+        test_values=False,
+    )
