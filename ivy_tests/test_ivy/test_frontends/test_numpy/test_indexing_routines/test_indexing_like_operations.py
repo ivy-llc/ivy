5145cb87696e0bc192d33064de5a85f1cbc05572
Muhammad <45216290+hrak99@users.noreply.github.com>
2022-11-17 23:04:38 +0500

    kindly assist.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
@@ -21,6 +21,7 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
         shared_dtype=True,
         min_num_dims=2,
     ),
+    where=np_frontend_helpers.where(),
     #dtype_and_axis1=helpers.dtype_values_axis(
      #   available_dtypes=helpers.get_dtypes("numeric"),
         #num_arrays=1,
@@ -38,12 +39,12 @@ from ivy_tests.test_ivy.helpers import handle_cmd_line_args
     #dtype=helpers.get_dtypes("float", full=False, none=True),
     #where=np_frontend_helpers.where(),
     num_positional_args=helpers.num_positional_args(
-        fn_name="diagonal" # ivy.functional.frontends.numpy.
+        fn_name="ivy.functional.frontends.numpy.diagonal"
     ),
 
 )
 def test_numpy_diagonal(
-        dtype_and_x,as_variable,num_positional_args,native_array,
+        dtype_and_x,as_variable,num_positional_args,native_array,where,
         fw,offset,axis1,axis2):
     input_dtype, x = dtype_and_x
     as_variable = as_variable
@@ -51,7 +52,8 @@ def test_numpy_diagonal(
     np_frontend_helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=False, # without
+        where=where,
+        with_out=without,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         fw=fw,
