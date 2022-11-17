ba5ef9d954629c1e91a8d219d86aa2531b966fb7
Mohamed Ehab <60079115+mohamedehab0999@users.noreply.github.com>
2022-11-17 19:39:21 +0200

    Update test_nn.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -830,10 +830,9 @@ def test_tensorflow_weighted_cross_entropy_with_logits(
 
 # local_response_normalization
 @handle_frontend_test(
-    fn_tree="tensorflow.nn.local_response_normalization",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
-        min_value= 0,
+        min_value= -20,
         max_value= 20,
         min_num_dims=4,
         max_num_dims=4,
@@ -868,7 +867,7 @@ def test_tensorflow_local_response_normalization(
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend="tensorflow",
-        fn_tree=fn_tree,
+        fn_tree="tensorflow.nn.local_response_normalization",
         rtol=1e-3,
         atol=1e-3,
         input=input,
