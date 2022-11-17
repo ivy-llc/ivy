483a147f5629a59504b0251a1ec5bb45327553b7
Zaeem Ansari <99063526+zaeemansari70@users.noreply.github.com>
2022-11-17 12:20:56 +0500

    lint fix

--- ivy/array/linear_algebra.py
+++ ivy/array/linear_algebra.py
@@ -30,7 +30,9 @@ class ArrayWithLinearAlgebra(abc.ABC):
         >>> print(z)
         ivy.array(11.)
         """
-        return ivy.matmul(self._data, x2, transpose_a=transpose_a, transpose_b=transpose_b, out=out)
+        return ivy.matmul(
+            self._data, x2, transpose_a=transpose_a, transpose_b=transpose_b, out=out
+        )
 
     def cholesky(
         self: ivy.Array,
