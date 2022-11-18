9ad38da365e2b3249a53579b54f77dc0f37c8edc
Anurag verma <78868769+anurag629@users.noreply.github.com>
2022-11-18 18:21:21 +0000

    fixed conflicts

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
@@ -924,6 +924,7 @@ def test_diff(
         x=np.asarray(x[0], dtype=input_dtype[0]),
     )
 
+
 # zeta
 @handle_test(
     fn_tree="functional.experimental.zeta",
@@ -938,6 +939,25 @@ def test_diff(
     ),
 )
 def test_zeta(
+    dtype_and_x,
+    with_out,
+    as_variable,
+    num_positional_args,
+    native_array,
+    container,
+    instance_method,
+    fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        container_flags=container,
+        instance_method=instance_method,
+        fw=fw,
         fn_name="zeta",
         rtol_=1e-03,
         atol_=1e-03,
