0cac270c06bacf161bea628f6541d2d957e27a05
Antonios Malak <57007944+AntoniosMalak@users.noreply.github.com>
2022-11-18 10:39:12 +0200

    Update sorting.py

--- ivy/functional/ivy/sorting.py
+++ ivy/functional/ivy/sorting.py
@@ -106,7 +106,9 @@ def argsort(
         a: ivy.array([2, 0, 1]),
         b: ivy.array([[1, 0], [1, 0]])
     }
-    >>> x = ivy.Container(a=ivy.array([[1.5, 3.2],[2.3, 4]]), b=ivy.array([[[1,3],[3,2],[2,0]]]))
+    >>> x = ivy.Container(
+                a=ivy.array([[1.5, 3.2],[2.3, 4]]), 
+                b=ivy.array([[[1,3],[3,2],[2,0]]]))
     >>> y = x.argsort(axis=-1, descending=True, stable=False)
     >>> print(y)
     {
