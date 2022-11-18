e045b2e3de81764ec8a2e3744dce847ec53a24fb
Hamed Mehrabi <62473531+hamehrabi@users.noreply.github.com>
2022-11-18 20:52:59 +0000

    Add new line at the end of file

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -27,3 +27,4 @@ def eye(num_rows, num_columns=None, batch_shape=None, dtype=ivy.float32, name=No
 @to_ivy_arrays_and_back
 def squeeze(input, axis=None, name=None):
     return ivy.squeeze(input, axis)
+
