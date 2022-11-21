b9e38455ab82fa07da67ca1cf370817abd624e65
Lamaran1 <obinnahtc@gmail.com>
2022-11-21 17:07:01 -0500

    fix test

--- ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_manipulation.py
@@ -912,3 +912,33 @@ def test_unstack(
         axis=axis,
         keepdims=keepdims,
     )
+
+
+# indices
+@handle_cmd_line_args
+@given(
+    xs_n_input_dtypes_n_unique_idx=_arrays_idx_n_dtypes(),
+    num_positional_args=helpers.num_positional_args(fn_name="indices"),
+)
+
+def test_numpy_indices(
+    dtype_and_x,
+    pred_cond,
+    num_positional_args,
+    as_variable,
+    native_array,
+):
+
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="numpy",
+        fn_tree="indices",
+        pred=pred_cond,
+        operand=x[0],
+        sparse=False,
+    )
