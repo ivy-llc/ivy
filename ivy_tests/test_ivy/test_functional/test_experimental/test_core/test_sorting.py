c91a07478fd296a1609ad611ef4656d14777efd7
Nadide Beyza Dokur <nadidebeyzadokur@gmail.com>
2022-11-22 01:01:53 +0300

    lexsort experimental API added

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_sorting.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_sorting.py
@@ -44,3 +44,46 @@ def test_msort(
         fn_name=fn_name,
         x=x[0],
     )
+
+
+# lexsort
+@handle_test(
+    fn_tree="functional.experimental.lexsort",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_dim_size=3,
+        min_value=-100,
+        max_value=100,
+    ),
+)
+def test_lexsort(
+    dtype_and_x,
+    num_positional_args,
+    as_variable,
+    with_out,
+    native_array,
+    container_flags,
+    instance_method,
+    backend_fw,
+    fn_name,
+    on_device,
+    ground_truth_backend,
+):
+    dtype, x = dtype_and_x
+    helpers.test_function(
+        ground_truth_backend=ground_truth_backend,
+        input_dtypes=dtype,
+        num_positional_args=num_positional_args,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        native_array_flags=native_array,
+        container_flags=container_flags,
+        instance_method=instance_method,
+        on_device=on_device,
+        fw=backend_fw,
+        fn_name=fn_name,
+        a=x[0],
+    )
