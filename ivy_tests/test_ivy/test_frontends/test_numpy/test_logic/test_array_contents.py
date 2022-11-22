70c6f88e5bbd19e15695c1534ebf3ffedb708073
ndurumo254 <erickndurumo@gmail.com>
2022-11-23 02:09:19 +0300

    removed fw

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_contents.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_contents.py
@@ -177,7 +177,6 @@ def test_numpy_isnat(
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
         frontend="numpy",
         fn_tree="isnat",
         x=np.asarray(x, dtype=input_dtype),
