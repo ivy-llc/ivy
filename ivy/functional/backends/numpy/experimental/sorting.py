a04e03440532c7d6ddaa3b08488f566d6d824af4
Nadide Beyza Dokur <nadidebeyzadokur@gmail.com>
2022-11-22 14:54:17 +0300

    Revert "lexsort experimental API added"
    This reverts commit c91a07478fd296a1609ad611ef4656d14777efd7.

--- ivy/functional/backends/numpy/experimental/sorting.py
+++ ivy/functional/backends/numpy/experimental/sorting.py
@@ -11,14 +11,3 @@ def msort(
 
 
 msort_support_native_out = False
-
-
-# lexsort
-def lexsort(
-    keys: Union[np.ndarray, list, tuple], /, *, out: Optional[np.ndarray] = None,
-    axis: int = -1
-) -> np.ndarray:
-    return np.lexsort(keys, axis)
-
-
-lexsort_support_native_out = False
