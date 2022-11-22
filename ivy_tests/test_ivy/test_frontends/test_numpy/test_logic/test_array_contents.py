11382bfcbf462dbf111b9e03281856185e8bb8ac
ndurumo254 <erickndurumo@gmail.com>
2022-11-22 11:06:52 +0300

    full

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_contents.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_contents.py
@@ -163,3 +163,31 @@ def test_numpy_isnat(
     fn_tree,
     frontend,
 ):
+    input_dtype, x = dtype_and_x
+    dtype, input_dtype, casting = np_frontend_helpers.handle_dtype_and_casting(
+        dtypes=input_dtype,
+        get_dtypes_kind="float",
+    )
+    where, as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        out=None,
+        where=where,
+        casting=casting,
+        order="K",
+        dtype=dtype,
+        subok=True,
+    )
