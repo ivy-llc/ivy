3c6e8d8fcb5e0069998c87719fe508d22da85e1c
mihayounas <mihaelayounas55@gmail.com>
2022-11-22 10:55:45 +0000

    slice function

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -1,8 +1,6 @@
 # local
 import ivy
 import tensorflow as tf
-import numpy as np
-
 from ivy.func_wrapper import with_unsupported_dtypes
 from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
 
@@ -26,6 +24,10 @@ def eye(num_rows, num_columns=None, batch_shape=None, dtype=ivy.float32, name=No
     return ivy.eye(num_rows, num_columns, batch_shape=batch_shape, dtype=dtype)
 
 
+@with_unsupported_dtypes({"2.9.0 and below": ("float16", "bfloat16")}, "tensorflow")
 @to_ivy_arrays_and_back
 def slice(input_, begin, size, name=None):
-    return ivy.slice(input_, begin, size)
+    for i in ivy.array:
+        input_ = tf.constant([ivy.array])
+        size[i] = input_.dim_size(i) - begin[i]
+        return ivy.slice(input_, begin[i], size[i])
