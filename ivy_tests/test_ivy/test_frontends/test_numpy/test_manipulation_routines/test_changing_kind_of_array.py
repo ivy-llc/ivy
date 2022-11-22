89182cff16dc8fc6a90f1ce22e91651dd4b10269
Uzoagulu Promise C <99205369+Taciturny@users.noreply.github.com>
2022-11-22 03:18:47 +0000

    unresolved reference

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
@@ -1,6 +1,6 @@
 # local
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.helpers import handle_frontend_test, handle_frontend_function
+from ivy_tests.test_ivy.helpers import handle_frontend_test, handle_frontend_frontend
 
 
 # asarraya
