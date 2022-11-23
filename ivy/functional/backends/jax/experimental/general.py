78131253c27139d23828658125f9b9f895d43038
Abdelrahman Diab <37490334+abdrahmandiab@users.noreply.github.com>
2022-11-23 08:02:21 +0000

    added jax & numpy handling for unsupported dtypes

--- ivy/functional/backends/jax/experimental/general.py
+++ ivy/functional/backends/jax/experimental/general.py
@@ -2,8 +2,11 @@
 from typing import Optional
 from ivy.functional.backends.jax import JaxArray
 import jax.numpy as jnp
+from ivy.func_wrapper import with_unsupported_dtypes
+from . import backend_version
 
 
+@with_unsupported_dtypes({"0.3.14 and below": ("float16", "bfloat16")}, backend_version)
 def isin(
     elements: JaxArray,
     test_elements: JaxArray,
--- ivy/functional/backends/numpy/experimental/general.py
+++ ivy/functional/backends/numpy/experimental/general.py
@@ -1,7 +1,10 @@
 from typing import Optional
 import numpy as np
+from ivy.func_wrapper import with_unsupported_dtypes
+from . import backend_version
 
 
+@with_unsupported_dtypes({"1.23.0 and below": ("bfloat16",)}, backend_version)
 def isin(
     elements: np.ndarray,
     test_elements: np.ndarray,
