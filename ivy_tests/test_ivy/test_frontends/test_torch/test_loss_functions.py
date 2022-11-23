68038ab5c5e487ab932d43b139e346e6f660472c
Gabcddrc <seedjeff@hotmail.com>
2022-11-23 13:41:25 +0000

    Remove with_out arg from nll test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
@@ -315,7 +315,6 @@ def test_torch_nll_loss(
     reduce,
     reduction,
     as_variable,
-    with_out,
     num_positional_args,
     native_array,
     on_device,
@@ -328,7 +327,6 @@ def test_torch_nll_loss(
     helpers.test_frontend_function(
         input_dtypes=inputs_dtype + target_dtype + weights_dtype,
         as_variable_flags=as_variable,
-        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend=frontend,
