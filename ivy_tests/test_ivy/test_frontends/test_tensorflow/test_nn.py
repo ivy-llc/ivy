d93e0ce8619b3b8ece3623c77f8d2915c1f1c582
Hamed Mehrabi <62473531+hamehrabi@users.noreply.github.com>
2022-11-24 18:21:42 +0000

    Update test_tensorflow_convolution "with_out" arg

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -624,6 +624,7 @@ def test_tensorflow_conv3d_transpose(
 def test_tensorflow_convolution(
     *,
     x_f_d_df,
+    with_out,
     as_variable,
     num_positional_args,
     native_array,
@@ -635,7 +636,7 @@ def test_tensorflow_convolution(
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        with_out=False,
+        with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
         frontend=frontend,
