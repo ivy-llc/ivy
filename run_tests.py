bbdfa20622525d4efcd7291e339b66b7cfb4b070
Aarsh2001 <ac5973@srmist.edu.in>
2022-11-24 09:26:23 +0530

    debug dashboard function

--- run_tests.py
+++ run_tests.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         mongo_key = sys.argv[3]
     failed = False
     cluster = MongoClient(
-        f"mongodb+srv://deep-ivy:{mongo_key}@cluster0.qdvf8q3.mongodb.net/?retryWrites=true&w=majority"  # noqa
+        f"mongodb+srv://deep-ivy:{key}@cluster0.qdvf8q3.mongodb.net/?retryWrites=true&w=majority"  # noqa
     )
     db = cluster["Ivy_tests"]
     with open("tests_to_run", "r") as f:
@@ -65,6 +65,7 @@ if __name__ == "__main__":
             test, backend = line.split(",")
             print(test, backend)
             coll, submod, test = get_submodule(test)
+            print(coll, submod, test)
             if len(sys.argv) > 2:
                 ret = os.system(
                     f'docker run --rm --env REDIS_URL={redis_url} --env REDIS_PASSWD={redis_pass} -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest {test} --backend {backend}'  # noqa
