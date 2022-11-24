d555c518c0eb279322276ca372c79844af7e8bc2
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-11-24 11:09:07 +0100

    torch_logaddexp (#7317)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -1951,3 +1951,38 @@ def test_torch_pow(
         input=x[0],
         exponent=x[1],
     )
+
+
+# logaddexp
+@handle_frontend_test(
+    fn_tree="torch.logaddexp",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        shared_dtype=True
+    ),
+)
+def test_torch_logaddexp(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x1=x[0],
+        x2=x[1]
+    )
