ce2fcf03a0c332b0d7dda57ba2455a27f3cc7e77
suhulavu <suhulavu@gmail.com>
2022-11-24 23:10:41 -0600

    implemented tf.expand_dims

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -69,12 +69,8 @@ def rank(input, **kwargs):
 @to_ivy_arrays_and_back
 def ones_like(input, dtype=None, name=None):
     return ivy.ones_like(input, dtype=dtype)
-<<<<<<< Updated upstream
-=======
 
 
 @to_ivy_arrays_and_back
 def expand_dims(input, axis, name=None):
     return ivy.expand_dims(input, axis=axis)
-    
->>>>>>> Stashed changes
