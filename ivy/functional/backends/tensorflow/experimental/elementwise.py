471c01342e82cc10e40afff3e110f6d1e53a8ef3
Anurag verma <78868769+anurag629@users.noreply.github.com>
2022-11-25 20:33:16 +0530

    adding diff to all ivy backends (#6569)
    Co-authored-by: @simonetgordon <simonegordon12@icloud.com>

--- ivy/functional/backends/tensorflow/experimental/elementwise.py
+++ ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -264,6 +264,18 @@ def nextafter(
     return tf.experimental.numpy.nextafter(x1, x2)
 
 
+@with_unsupported_dtypes(
+    {"2.9.1 and below": ("uint8", "uint16", "uint32", "uint64")}, backend_version
+)
+def diff(
+    x: Union[tf.Tensor, tf.Variable, int, float, list, tuple],
+    /,
+    *,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None
+) -> Union[tf.Tensor, tf.Variable]:
+    return tf.experimental.numpy.diff(x)
+
+ 
 @with_unsupported_dtypes({"2.9.1 and below": ("bfloat16, float16,")}, backend_version)
 def zeta(
     x: Union[tf.Tensor, tf.Variable],
