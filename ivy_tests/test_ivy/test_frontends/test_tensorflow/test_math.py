1e9c8533b65452f9335af83a759a8ac730208ba7
Tina Tran <114256776+TTrumpet@users.noreply.github.com>
2022-11-25 13:51:30 -0500

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -11,6 +11,8 @@ from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 # accumulate_n
+
+
 @handle_frontend_test(
     fn_tree="tensorflow.math.accumulate_n",
     dtype_and_x=helpers.dtype_and_values(
@@ -42,6 +44,7 @@ def test_tensorflow_accumulate_n(
         x=x,
     )
 
+    
 # add
 @handle_frontend_test(
     fn_tree="tensorflow.math.add",
