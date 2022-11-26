8881282583097869cbbc5bee2525c9f909e35653
Mohsen Fatehifar <Mohsen.Fatehifar@gmail.com>
2022-11-26 17:01:59 +0000

    change l to le variable to bypass flak8

--- ivy_tests/test_ivy/test_functional/test_core/test_general.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_general.py
@@ -651,7 +651,7 @@ def values_and_ndindices(
                 max_num_dims=1,
                 min_dim_size=n,
                 max_dim_size=n,
-            ).filter(lambda l: len(set(l[1][0])) == len(l[1][0])),
+            ).filter(lambda le: len(set(le[1][0])) == len(le[1][0])),
             st.integers(min_value=n, max_value=n),
         )
     ),
