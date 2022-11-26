dc2181cda43fbb366e20c081c86f5f99f4face54
Mohamed Mosa <61886289+m7mdmosa27@users.noreply.github.com>
2022-11-26 09:33:41 +0200

    add diag function and his test function (#7309)
    Co-authored-by: Vansh Gupta <vanshg2001@protonmail.com>

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_building_matrices.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_building_matrices.py
@@ -43,3 +43,41 @@ def test_numpy_tril(
         m=x[0],
         k=k,
     )
+
+
+# diag
+@handle_frontend_test(
+    fn_tree="numpy.diag",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=1,
+        min_num_dims=2,
+        max_num_dims=2,
+        min_dim_size=1,
+        max_dim_size=2,
+    ),
+    k=helpers.ints(min_value=-10, max_value=10),
+)
+def test_numpy_diag(
+    dtype_and_x,
+    k,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        v=x[0],
+        k=k,
+    )
