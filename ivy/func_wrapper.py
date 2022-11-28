7948a02cb4fcc3c710872c4da75be365e29234ba
Vaatsalya <96314403+vaatsalya123@users.noreply.github.com>
2022-11-28 20:38:15 +0400

    Update func_wrapper.py

--- ivy/func_wrapper.py
+++ ivy/func_wrapper.py
@@ -27,16 +27,21 @@ FN_DECORATORS = [
 # Helpers #
 # --------#
 
+
 def _get_first_array(*args, **kwargs):
     # ToDo: make this more efficient, with function ivy.nested_nth_index_where
     arr = None
     if args:
-        if arr_idxs := ivy.nested_argwhere(args, ivy.is_array, stop_after_n_found=1):
+        arr_idxs = ivy.nested_argwhere(args, ivy.is_array, stop_after_n_found=1)
+        if arr_idxs:
             arr = ivy.index_nest(args, arr_idxs[0])
-        elif arr_idxs := ivy.nested_argwhere(kwargs, ivy.is_array, stop_after_n_found=1):
-            arr = ivy.index_nest(kwargs, arr_idxs[0])
+        else:
+            arr_idxs = ivy.nested_argwhere(kwargs, ivy.is_array, stop_after_n_found=1)
+            if arr_idxs:
+                arr = ivy.index_nest(kwargs, arr_idxs[0])
     elif kwargs:
-        if arr_idxs := ivy.nested_argwhere(kwargs, ivy.is_array, stop_after_n_found=1):
+        arr_idxs = ivy.nested_argwhere(kwargs, ivy.is_array, stop_after_n_found=1)
+        if arr_idxs:
             arr = ivy.index_nest(kwargs, arr_idxs[0])
     return arr
 
