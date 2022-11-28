19e328cf1771d763ea74b352c27afcd7446f12c7
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2022-11-28 14:05:29 -0400

    Adding zeros_like to tf frontend (#7235)
    Co-authored-by: @simonetgordon <simonegordon12@icloud.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -284,6 +284,39 @@ def test_tensorflow_ones_like(
     )
 
 
+# zeros_like
+@handle_frontend_test(
+    fn_tree="tensorflow.zeros_like",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+    dtype=helpers.get_dtypes("numeric", full=False),
+)
+def test_tensorflow_zeros_like(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    native_array,
+    num_positional_args,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        dtype=dtype[0],
+    )
+        
+
 # expand_dims
 @handle_frontend_test(
     fn_tree="tensorflow.expand_dims",
