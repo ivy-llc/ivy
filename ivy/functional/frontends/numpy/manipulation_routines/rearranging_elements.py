f76ad183c0203a3a8ef5c07d3dffc5fcf7f4d00b
JortdeJong1399 <119424445+JortdeJong1399@users.noreply.github.com>
2022-11-29 14:52:54 +0100

    Added rot90

--- ivy/functional/frontends/numpy/manipulation_routines/rearranging_elements.py
+++ ivy/functional/frontends/numpy/manipulation_routines/rearranging_elements.py
@@ -21,3 +21,8 @@ def fliplr(m):
 @to_ivy_arrays_and_back
 def flipud(m):
     return ivy.flipud(m, out=None)
+
+
+@to_ivy_arrays_and_back
+def rot90(m, k=1, axes=(0, 1)):
+    return ivy.rot90(m, k=k, axes=axes)
