8dbb603d9a99411e99bdcf59bcc237a7c5bb97c7
Roberto Nacu <roberto.nacu@gmail.com>
2022-11-29 17:24:06 +0000

    added concat to tensorflow frontend (#7366)
    * added concat to tensorflow frontend and tested
    
    * removed blank lines from end of file
    
    * specified available dtypes
    
    * refactored tensorflow_frontend_concat

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -357,6 +357,43 @@ def test_tensorflow_expand_dims(
     )
 
 
+# concat
+@handle_frontend_test(
+    fn_tree="tensorflow.concat",
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=st.integers(min_value=1, max_value=4),
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
+        shared_dtype=True,
+    ),
+)
+def test_tensorflow_concat(
+    *,
+    dtype_input_axis,
+    as_variable,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x, axis = dtype_input_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        values=x,
+        axis=axis,
+    )
+
+
 @handle_frontend_test(
     fn_tree="tensorflow.zeros",
     input=helpers.get_shape(
