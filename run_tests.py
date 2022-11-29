59a4e5fd70f6ee2688a5f49bcaf06d10da312406
Aarsh2001 <ac5973@srmist.edu.in>
2022-11-29 19:01:38 +0530

    fix run tests.py

--- run_tests.py
+++ run_tests.py
@@ -71,11 +71,11 @@ if __name__ == "__main__":
             print(coll, submod, test_fn)
             if len(sys.argv) > 2:
                 ret = os.system(
-                    f'docker run --rm --env REDIS_URL={redis_url} --env REDIS_PASSWD={redis_pass} -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --backend {backend} {test}'  # noqa
+                    f'docker run --rm --env REDIS_URL={redis_url} --env REDIS_PASSWD={redis_pass} -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest {test} --backend {backend}'  # noqa
                 )
             else:
                 ret = os.system(
-                    f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest --backend {backend} {test}'  # noqa
+                    f'docker run --rm -v "$(pwd)":/ivy -v "$(pwd)"/.hypothesis:/.hypothesis unifyai/ivy:latest python3 -m pytest {test} --backend {backend}'  # noqa
                 )
             if ret != 0:
                 update_individual_test_results(
