601f9d2e183a2f1e84492552370e47ec19b296b7
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2022-11-29 18:38:17 +0000

    Implemented tensorflow shape frontend function (#7456)
    Implemened tensorflow shape frontend function

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -394,6 +394,7 @@ def test_tensorflow_concat(
     )
 
 
+# zeros
 @handle_frontend_test(
     fn_tree="tensorflow.zeros",
     input=helpers.get_shape(
@@ -424,7 +425,41 @@ def test_tensorflow_zeros(
         with_out=with_out,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        frontend="tensorflow",
+        frontend=frontend,
         fn_tree=fn_tree,
         on_device=on_device,
+    )
+
+
+# shape
+@handle_frontend_test(
+    fn_tree="tensorflow.shape",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+    output_dtype=st.sampled_from(["int32", "int64"]),
+
+)
+def test_tensorflow_shape(
+    *,
+    dtype_and_x,
+    output_dtype,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x, = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        out_type=output_dtype,
     )
