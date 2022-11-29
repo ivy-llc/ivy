0c76dd69773fa7178f9d28fbacc3e71c6bc90aee
Marouf Shaikh <maruf98@gmail.com>
2022-11-29 11:35:39 +0000

    Fixing some linear algebra tests (#7418)
    * minor fix for cholesky test for jax
    
    * eigh:UPLO update inst. + test fix(to-do bug fix)
    
    * minor fix to matrix_power test
    
    * added container and static methods for eigh
    
    * updated positional args + out arg for qr
    
    * updated test_qr
    
    * fixed svd test
    
    * fixed tensordot pos args issue

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -452,7 +452,13 @@ def pinv(
 
 
 @with_unsupported_dtypes({"2.9.1 and below": ("float16", "bfloat16")}, backend_version)
-def qr(x: Union[tf.Tensor, tf.Variable], /, *, mode: str = "reduced") -> NamedTuple:
+def qr(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    mode: str = "reduced",
+    out: Optional[tf.Tensor] = None,
+) -> NamedTuple:
     res = namedtuple("qr", ["Q", "R"])
     if mode == "reduced":
         q, r = tf.linalg.qr(x, full_matrices=False)
