ef9c5ce1a40cd15b20e6beba9a66c12c1a201742
Ahmed Karem Mousa <59775002+AhmedMousa-ag@users.noreply.github.com>
2022-11-29 07:22:42 +0200

    added tf.zeros front end (#7200)
    * added tf.zeros front end
    
    * reformatted
    
    * reformatted
    
    * Reformatted
    
    * reformatted
    
    * reformatted

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -87,6 +87,11 @@ def ones_like(input, dtype=None, name=None):
     return ivy.ones_like(input, dtype=dtype)
 
 
+@to_ivy_arrays_and_back 
+def zeros(shape, dtype=ivy.float32, name=None):
+    return ivy.zeros(shape=shape, dtype=dtype)
+    
+  
 @to_ivy_arrays_and_back
 def expand_dims(input, axis, name=None):
     return ivy.expand_dims(input, axis=axis)
