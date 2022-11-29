e28ce477cdc46947e97cacff81097fb7f27809c2
Tina Tran <114256776+TTrumpet@users.noreply.github.com>
2022-11-28 23:21:20 -0500

    Update math.py

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -6,10 +6,7 @@ from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_b
 
 @to_ivy_arrays_and_back
 def accumulate_n(inputs, input_type=None, shape=None, dtype=None, name=None):
-    if input_type in ["int8", "int16", "int32", "int64"]:
-        return ivy.astype(ivy.sum(ivy.array(inputs)), input_type)
-    else:
-        return ivy.astype(ivy.sum(ivy.array(inputs)), ivy.int64)
+    return ivy.astype(ivy.sum(ivy.array(inputs)), ivy.int64)
 
 
 @to_ivy_arrays_and_back
