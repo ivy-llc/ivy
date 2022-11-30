5569dc0d6627660c11c94ea63fb15021fc24f8a7
CatB1t <skytedits@gmail.com>
2022-11-30 03:25:52 +0200

    remove out of date code.

--- ivy_tests/test_ivy/helpers/test_parameter_flags.py
+++ ivy_tests/test_ivy/helpers/test_parameter_flags.py
@@ -1,14 +1,5 @@
-from enum import Enum
-
-
 class ContainerFlags:
     pass
-
-
-class TestObjectType(Enum):
-    FUNCTION = 1
-    METHOD = 2
-    INIT = 3
 
 
 class NumPositionalArg:  # TODO for backward compatibility only
