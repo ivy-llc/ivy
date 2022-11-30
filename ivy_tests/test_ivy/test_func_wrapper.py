6f9a98de9314f91ebbd631c84d3f065527aa82c5
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-11-30 18:52:10 +0000

    Added tests for wrapper from_zero_dim_arrays_to_float, to_native_arrays_and_back, integer_arrays_to_float

--- ivy_tests/test_ivy/test_func_wrapper.py
+++ ivy_tests/test_ivy/test_func_wrapper.py
@@ -34,14 +34,15 @@ def test_handle_array_like(fn, x, expected_type):
     assert isinstance(handle_array_like(fn)(x), expected_type)
 
 
-def test_output_to_ivy_arrays():
+def test_outputs_to_ivy_arrays():
     assert isinstance(
         ivy.outputs_to_ivy_arrays(_fn1)(ivy.to_native(ivy.array([2.]))),
         ivy.Array)
     assert ivy.outputs_to_ivy_arrays(_fn1)(ivy.array(1)) == ivy.array(1)
 
 
-def _fn5(x: ivy.NativeArray):
+def _fn5(x):
+    # Test input was converted to native array
     assert isinstance(x, ivy.NativeArray)
 
 
@@ -49,9 +50,41 @@ def test_inputs_to_native_arrays():
     ivy.inputs_to_native_arrays(_fn5)(ivy.array(1))
 
 
-def _fn6(x: ivy.Array):
+def _fn6(x):
+    # Assert input was converted to Ivy Array
     assert isinstance(x, ivy.Array)
 
 
 def test_inputs_to_ivy_arrays():
     ivy.inputs_to_ivy_arrays(_fn6)(ivy.native_array(1))
+
+
+def test_from_zero_dim_arrays_to_float():
+    assert ivy.func_wrapper.from_zero_dim_arrays_to_float(_fn1)(ivy.array(1.)) == 1.
+    assert ivy.func_wrapper.from_zero_dim_arrays_to_float(_fn1)(ivy.array(1)) == 1.
+
+
+def _fn7(x):
+    # Assert input was converted to native array
+    assert isinstance(x, ivy.NativeArray)
+    return x
+
+
+def test_to_native_arrays_and_back():
+    x = ivy.array(1.)
+    res = ivy.func_wrapper.to_native_arrays_and_back(_fn7)(x)
+    assert isinstance(res, ivy.Array)
+
+
+@pytest.mark.parametrize(
+    ("x", "expected"),
+    [
+        (ivy.array(1), ivy.array(1.)),
+        (ivy.array([1, 3], dtype="int16"), ivy.array([1., 3.])),
+        (ivy.array([[[1, 0], [-2, 1.]]], dtype="float32"),
+         ivy.array([[[1., 0.], [-2., 1.]]]))
+    ],
+)
+def test_integer_arrays_to_float(x, expected):
+    # Todo: Fix dtype issue
+    assert ivy.array_equal(ivy.func_wrapper.integer_arrays_to_float(_fn1)(x), expected)
