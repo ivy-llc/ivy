fa20190590ea1a0376edf43bbe7a4d721fc4adf2
ZiadAmerr <xeiad.amerr@yahoo.com>
2022-11-30 19:10:02 +0200

    Added ability to test delta values as floats

--- ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
@@ -280,11 +280,13 @@ def test_torch_l1_loss(
         allow_inf=False,
         shared_dtype=True,
     ),
+    delta=helpers.floats(min_value=0, max_value=0.9),
     reduction=st.sampled_from(["none", "mean", "sum"]),
 )
 def test_huber_loss(
     *,
     dtype_and_x,
+    delta,
     reduction,
     as_variable,
     num_positional_args,
@@ -308,4 +310,5 @@ def test_huber_loss(
         input=pred,
         target=true,
         reduction=reduction,
+        delta=delta
     )
