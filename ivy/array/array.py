29531553b0745ad019afbe8f2166c0f733a25219
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-12-01 15:12:28 +0000

    Placing the examples for greater_equal accurately. Operator examples under special method, and array and container in their repective methods

--- ivy/array/array.py
+++ ivy/array/array.py
@@ -556,6 +556,46 @@ class Array(
         return ivy.greater(self._data, other)
 
     def __ge__(self, other):
+        """
+        Greater than or equal to
+
+        Returns
+        -------
+        An array containing the element-wise results. The returned array must have a
+        data type of bool.
+
+        Examples
+        --------
+        With :class:`ivy.Array` instances:
+
+        >>> x = ivy.array([6, 2, 3])
+        >>> y = ivy.array([4, 5, 6])
+        >>> z = x >= y
+        >>> print(z)
+        ivy.array([True,False,False])
+
+        With :class:`ivy.Container` instances:
+
+        >>> x = ivy.Container(a=ivy.array([4, 5, 6]),b=ivy.array([2, 3, 4]))
+        >>> y = ivy.Container(a=ivy.array([1, 2, 3]),b=ivy.array([5, 6, 7]))
+        >>> z = x >= y
+        >>> print(z)
+        {
+            a:ivy.array([True,True,True]),
+            b:ivy.array([False,False,False])
+        }
+
+        With mix of :class:`ivy.Array` and :class:`ivy.Container` instances:
+
+        >>> x = ivy.array([[5.1, 2.3, -3.6]])
+        >>> y = ivy.Container(a=ivy.array([[4.], [5.], [6.]]),b=ivy.array([[5.], [6.], [7.]]))
+        >>> z = x >= y
+        >>> print(z)
+        {
+            a:ivy.array([[True,False,False],[True,False,False],[False,False,False]]),
+            b:ivy.array([[True,False,False],[False,False,False],[False,False,False]])
+        }
+        """
         return ivy.greater_equal(self._data, other)
 
     def __and__(self, other):
