850bbc36ed6e9345a9b9c4120342151b15244967
Muhammad <45216290+hrak99@users.noreply.github.com>
2022-12-02 03:31:44 +0500

    Still facing same axis issue. I also change the argument unique=True, but in vain.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
@@ -2,7 +2,6 @@
 # global
 from hypothesis import strategies as st
 
-import ivy
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -17,59 +16,46 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 # import ivy
 
 
+@st.composite
+def st_dtype_arr_and_axes(draw):
+    dtypes, xs, x_shape = draw(
+        helpers.dtype_and_values(
+            num_arrays=1,
+            available_dtypes=helpers.get_dtypes("numeric"),
+            shape=st.shared(
+                helpers.get_shape(
+                    allow_none=False,
+                    min_num_dims=2,
+                    max_num_dims=5,
+                    min_dim_size=2,
+                    max_dim_size=10,
+                )
+            ),
+            ret_shape=True,
+        )
+    )
+    axis1, axis2 = draw(
+        helpers.get_axis(
+            shape=x_shape,
+            sorted=False,
+            unique=True,
+            min_size=2,
+            max_size=2,
+            force_tuple=True,
+        )
+    )
+    return dtypes[0], xs[0], axis1, axis2
+
+
 @handle_frontend_test(
     fn_tree="numpy.diagonal",
-    # dtype_x_axis=helpers.dtype_values_axis(
-    #   available_dtypes=helpers.get_dtypes("numeric"),
-    #  num_arrays=1,
-    # shared_dtype = True,
-    # min_num_dims = 2
-    # ),
-    # dtype_and_x=helpers.dtype_and_values(
-    #   available_dtypes=helpers.get_dtypes("numeric"),
-    #  num_arrays=1,
-    # min_value=0,
-    # max_value = 1,
-    # shared_dtype=True,
-    # min_num_dims=2,
-    # ),
-    dtype_x_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=1,
-        shared_dtype=True,
-        min_num_dims=2,
-        min_axis=-2,
-        max_axis=1,
-    ),
-    # dtype_and_axis=helpers.get_axis(
-    # available_dtypes=helpers.get_dtypes("numeric"),
-    #   shape=(2, 3),
-    #  min_size=2,
-    # max_size = 2,
-    # ),
-    # dtype_and_axis2=helpers.get_axis(
-    # available_dtypes=helpers.get_dtypes("numeric"),
-    #   shape=(2, 3),
-    #   min_size=1,
-    # ),
-    # where=np_frontend_helpers.where(),
-    # dtype_and_axis1=helpers.dtype_values_axis(
-    #   available_dtypes=helpers.get_dtypes("numeric"),
-    # num_arrays=1,
-    # min_value=0,
-    # max_value = 1,
-    #  shared_dtype=True,
-    # min_num_dims=2,
-    # ),
+    dtype_arr_and_axes=st_dtype_arr_and_axes(),
     offset=st.integers(min_value=-1, max_value=1),
-    # axis1=st.integers(min_value=-2, max_value=2),
-    # axis2=st.integers(min_value=-2, max_value=2),
-    # axis1!=axis2,
-    # dtype=helpers.get_dtypes("float", full=False, none=True),
-    # where=np_frontend_helpers.where(),
 )
 def test_numpy_diagonal(
+    dtype_arr_and_axes,
     # dtype_and_x,
+    # dtype_and_axis,
     as_variable,
     num_positional_args,
     native_array,
@@ -77,26 +63,26 @@ def test_numpy_diagonal(
     with_out,
     offset,
     # axis1,
-    dtype_x_axis,
+    # dtype_x_axis,
     # axis2,
     # dtype_and_axis2,
     on_device,
     fn_tree,
     frontend,
 ):
-    input_dtype, x, axis = dtype_x_axis
-    # axis = dtype_and_axis
-    # print ('axis',axis)
-    # axis2 = dtype_and_axis[1]
-    new_axis = 0
-    if axis < 0:
-        new_axis = axis + 1
-    else:
-        new_axis = axis - 1
+    input_dtype, x, axis1, axis2 = dtype_arr_and_axes
+    print(
+        "axis1",
+        axis1,
+        "axis2",
+        axis2,
+    )
+    # axes = dtype_and_axis
+    # print(axes)
     as_variable = as_variable
 
     np_frontend_helpers.test_frontend_function(
-        input_dtypes=input_dtype,
+        input_dtypes=[input_dtype],
         as_variable_flags=as_variable,
         on_device=on_device,
         frontend=frontend,
@@ -108,8 +94,8 @@ def test_numpy_diagonal(
         # fw=fw,
         # frontend="numpy",
         # fn_tree="diagonal",
-        a=ivy.native_array(x, dtype=ivy.int32),
+        a=x,  # ivy.native_array(x, dtype=ivy.int32),
         offset=offset,
-        axis1=new_axis,
-        axis2=axis,
+        axis1=axis1,
+        axis2=axis2,
     )
