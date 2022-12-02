b0b3526dfad3df8e436fa1494ec5a14c38a9fd00
anurag629 <akvermaav629@gmail.com>
2022-12-02 23:29:28 +0530

    fixed lint checks

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -880,46 +880,51 @@ def test_tensorflow_local_response_normalization(
     )
 
 
-@handle_cmd_line_args
-@given(
+@handle_frontend_test(
+    fn_tree="tensorflow.nn.conv_transpose",
     x_f_d_df=_x_and_filters(
         dtypes=helpers.get_dtypes("float", full=False),
         padding=st.sampled_from(["VALID", "SAME"]),
+        stride_min=3,
+        stride_max=4,
+        dilation_max=1,
         transpose=True,
     ),
-    as_variable=st.booleans(),
-    num_positional_args=helpers.num_positional_args(
-        fn_name="ivy.functional.frontends.tensorflow.conv_transpose"
-    ),
-    native_array=st.booleans(),
 )
-def test_conv_transpose(x_f_d_df, as_variable, num_positional_args, native_array, fw):
+def test_tensorflow_conv_transpose(
+    *,
+    x_f_d_df,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
     (
         input_dtype,
         x,
         filters,
-        dilation,
+        dilations,
         data_format,
         stride,
-        padding,
+        pad,
         output_shape,
     ) = x_f_d_df
-    input_dtype = [input_dtype] * 2
-    as_variable = [as_variable] * 2
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        num_positional_args=num_positional_args,
         with_out=False,
+        num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        fw=fw,
-        frontend="tensorflow",
-        fn_tree="nn.conv_transpose",
-        input=np.asarray(x, dtype=input_dtype[0]),
-        filters=np.asarray(filters, dtype=input_dtype[1]),
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x,
+        filters=filters,
         output_shape=output_shape,
         strides=stride,
-        padding=padding,
+        padding=pad,
         data_format=data_format,
-        dilations=dilation,
+        dilations=dilations,
     )
