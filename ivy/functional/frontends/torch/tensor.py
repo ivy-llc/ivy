3418388530675cd166790bcec1bde524a669aa50
Mohamad H. Danesh <mohamad4danesh@gmail.com>
2022-12-02 10:19:25 +0330

    Update tensor.py

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -327,7 +327,14 @@ class Tensor:
         return torch_frontend.permute(self, dims)
 
     def mean(self, dim=None, keepdim=False):
-        return torch_frontend.mean(self._ivy_array, dim=dim, keepdim=False)
+    return torch_frontend.mean(self._ivy_array, dim=dim, keepdim=keepdim)
+
+    def transpose(self, dim0, dim1):
+        return torch_frontend.transpose(self._ivy_array, dim0=dim0, dim1=dim1)
+
+    def transpose_(self, dim0, dim1):
+        self._ivy_array = self.transpose(dim0, dim1).ivy_array
+        return self
 
     # Special Methods #
     # -------------------#
