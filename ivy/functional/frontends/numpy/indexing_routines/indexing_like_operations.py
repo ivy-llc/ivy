db5a0dfa04efa9a21cb9dc4905b0d5ba48cc0977
Muhammad <45216290+hrak99@users.noreply.github.com>
2022-12-02 22:43:47 +0500

    Check this one.

--- ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
+++ ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
@@ -1,20 +1,7 @@
 import ivy
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 
-# from ivy.func_wrapper import (
-#   to_native_arrays_and_back,
-#  handle_out_argument,
-# handle_nestable,
-# handle_array_like,
-# )
-# from ivy.exceptions import handle_exceptions
-
 
 @to_ivy_arrays_and_back
 def diagonal(a, offset, axis1, axis2):
-    # ivy.assertions.check_equal(
-    #   axis1 == axis2, message="Both axis values should not be the same"
-    # )
-    # ivy.assertions.check_equal(x.ndim, 2, message="x must be 2-dimensional")
-
     return ivy.diagonal(a, offset=offset, axis1=axis1, axis2=axis2)
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
@@ -2,48 +2,11 @@
 # global
 from hypothesis import strategies as st
 
-
 # local
 import ivy_tests.test_ivy.helpers as helpers
-
-
-# from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
-#   statistical_dtype_values,
-# )
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 import ivy
-
-
-@st.composite
-def st_dtype_arr_and_axes(draw):
-    dtypes, xs, x_shape = draw(
-        helpers.dtype_and_values(
-            num_arrays=1,
-            available_dtypes=helpers.get_dtypes("numeric"),
-            shape=st.shared(
-                helpers.get_shape(
-                    allow_none=False,
-                    min_num_dims=2,
-                    max_num_dims=5,
-                    min_dim_size=2,
-                    max_dim_size=10,
-                )
-            ),
-            ret_shape=True,
-        )
-    )
-    axis1, axis2 = draw(
-        helpers.get_axis(
-            shape=x_shape,
-            sorted=False,
-            unique=True,
-            min_size=2,
-            max_size=2,
-            force_tuple=True,
-        )
-    )
-    return dtypes[0], xs[0], axis1, axis2
 
 
 @handle_frontend_test(
@@ -51,12 +14,9 @@ def st_dtype_arr_and_axes(draw):
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
         num_arrays=1,
-        # min_value=0,
-        # max_value = 1,
         shared_dtype=True,
         min_num_dims=2,
     ),
-    # dtype_arr_and_axes=st_dtype_arr_and_axes(),
     dtype_and_axis=helpers.get_axis(
         shape=st.shared(
             helpers.get_shape(
@@ -68,39 +28,28 @@ def st_dtype_arr_and_axes(draw):
             )
         ),
         unique=True,
+        allow_neg=True,
         max_size=2,
         min_size=2,
         force_tuple=True,
+        force_int=False,
     ),
     offset=st.integers(min_value=-1, max_value=1),
 )
 def test_numpy_diagonal(
-    # dtype_arr_and_axes,
     dtype_and_x,
     dtype_and_axis,
     as_variable,
     num_positional_args,
     native_array,
-    # where,
     with_out,
     offset,
-    # axis1,
-    # dtype_x_axis,
-    # axis2,
-    # dtype_and_axis2,
     on_device,
     fn_tree,
     frontend,
 ):
-    (
-        input_dtype,
-        x,
-    ) = dtype_and_x
+    input_dtype, x = dtype_and_x
     axis = dtype_and_axis
-    print("axis", axis)
-
-    # axes = dtype_and_axis
-    # print(axes)
     as_variable = as_variable
 
     np_frontend_helpers.test_frontend_function(
@@ -109,13 +58,9 @@ def test_numpy_diagonal(
         on_device=on_device,
         frontend=frontend,
         fn_tree=fn_tree,
-        # where=where,
         with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
-        # fw=fw,
-        # frontend="numpy",
-        # fn_tree="diagonal",
         a=ivy.native_array(x, dtype=ivy.int32),
         offset=offset,
         axis1=axis[0],
