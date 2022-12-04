f08f38c8e6a830c11b2f958f12a0f5265b20af4b
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2022-12-04 21:11:21 +0530

    lint fixes

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -1,6 +1,7 @@
 # global
 import ivy.functional.frontends.tensorflow.ragged as ragged_tf
 import ivy
+
 # try:
 #     import tensorflow as tf
 # except ImportError:
@@ -12,7 +13,7 @@ import ivy
 
 
 def _is_composite_array(x):
-    if isinstance(x, ragged_tf.ragged.RaggedTensor ):
+    if isinstance(x, ragged_tf.ragged.RaggedTensor):
         return True
     if ivy.is_ivy_sparse_array(x) or ivy.is_native_sparse_array(x):
         return True
--- ivy/functional/frontends/tensorflow/ragged/ragged.py
+++ ivy/functional/frontends/tensorflow/ragged/ragged.py
@@ -2,49 +2,59 @@ import ivy
 import ivy.functional.frontends.tensorflow as tf_frontend
 from typing import Any
 
-#TODO: Align behavior with tensorflow, modify so that the elements of the raggedTensor object are of type EagerTensor
+# TODO: Align behavior with tensorflow, modify so that the elements of the raggedTensor object are of type EagerTensor
 # ensure that the values and row_splits are of type EagerTensor too
-
-
 
 
 class RaggedTensor:
     def __init__(self, values, row_partition, internal=False, data=None):
         if not internal:
-            raise ivy.exceptions.IvyException("RaggedTensor constructor is private; please use one of the factory methods instead (e.g., RaggedTensor.from_row_lengths())")
-        self._values=values
-        self.data=data
-        self._row_partition=row_partition
+            raise ivy.exceptions.IvyException(
+                "RaggedTensor constructor is private; please use one of the factory methods instead (e.g., RaggedTensor.from_row_lengths())"
+            )
+        self._values = values
+        self.data = data
+        self._row_partition = row_partition
 
     @classmethod
     def from_row_splits(cls, values, row_splits, name=None, validate=True):
-        #TODO : modify this, if necessary, to accept raggedTensor inputs too
+        # TODO : modify this, if necessary, to accept raggedTensor inputs too
 
-        if values.shape[0]!=row_splits[-1] or row_splits[0]!=0:
-            if values.shape[0]!=row_splits[-1]:
-                ivy.exceptions.IvyException("first dimension of shape of values should be equal to the last dimension of row_splits")
+        if values.shape[0] != row_splits[-1] or row_splits[0] != 0:
+            if values.shape[0] != row_splits[-1]:
+                ivy.exceptions.IvyException(
+                    "first dimension of shape of values should be equal to the last dimension of row_splits"
+                )
             else:
                 ivy.exceptions.IvyException(
-                    "first value of row_splits should be equal to zero.")
-        data=[values[row_splits[i]:row_splits[i + 1],:]
-          for i in range(len(row_splits) - 1)]
+                    "first value of row_splits should be equal to zero."
+                )
+        data = [
+            values[row_splits[i] : row_splits[i + 1], :]
+            for i in range(len(row_splits) - 1)
+        ]
 
-        return cls(values=values,row_partition=row_splits, internal=True, data =data)
+        return cls(values=values, row_partition=row_splits, internal=True, data=data)
 
     @classmethod
-    def from_row_lengths(cls,
-        values, row_lengths, name=None,
+    def from_row_lengths(
+        cls,
+        values,
+        row_lengths,
+        name=None,
     ):
         # TODO : modify this, if necessary, to accept raggedTensor inputs too
-        if sum(row_lengths)!=values.shape[0]:
-            ivy.exceptions.IvyException("first dimension of values should be equal to sum(row_lengths) ")
-        data=[]
-        z=0
+        if sum(row_lengths) != values.shape[0]:
+            ivy.exceptions.IvyException(
+                "first dimension of values should be equal to sum(row_lengths) "
+            )
+        data = []
+        z = 0
         for length in row_lengths:
-            temp=[]
+            temp = []
             for i in range(length):
-                temp.append(values[z,:])
-                z+=1
+                temp.append(values[z, :])
+                z += 1
 
             data.append(ivy.asarray(temp))
 
@@ -53,17 +63,21 @@ class RaggedTensor:
         return cls(values=values, row_partition=row_lengths, internal=True, data=data)
 
     @classmethod
-    def from_value_rowids(cls,
-        values, value_rowids, nrows=None, name=None,
+    def from_value_rowids(
+        cls,
+        values,
+        value_rowids,
+        nrows=None,
+        name=None,
     ):
         if not nrows:
-            nrows=value_rowids[-1]+1
-        data=[]
+            nrows = value_rowids[-1] + 1
+        data = []
         for row in range(nrows):
-            temp=[]
+            temp = []
             for i in range(len(values)):
-                if value_rowids[i]==row:
-                    temp.append(values[i,:])
+                if value_rowids[i] == row:
+                    temp.append(values[i, :])
             data.append(ivy.asarray(temp))
 
         # data= [[values[i,:] for i in range(len(values)) if value_rowids[i] == row]
@@ -71,19 +85,21 @@ class RaggedTensor:
         return cls(values=values, row_partition=value_rowids, internal=True, data=data)
 
     @classmethod
-    def from_row_starts(cls,
-        values, row_starts, name=None,
+    def from_row_starts(
+        cls,
+        values,
+        row_starts,
+        name=None,
     ):
-        #TODO since row_starts will be a tensor try appending using concat after ensuring row_starts
+        # TODO since row_starts will be a tensor try appending using concat after ensuring row_starts
         # is a tensor
         row_starts.append(len(values))
-        return cls.from_row_splits(values,row_starts)
-
+        return cls.from_row_splits(values, row_starts)
 
     def to_list(self):
-        vals=[]
+        vals = []
         for i in self:
-            if isinstance(i,RaggedTensor):
+            if isinstance(i, RaggedTensor):
                 vals.append(i.to_list())
             else:
                 vals.append(ivy.to_list(i))
@@ -95,9 +111,9 @@ class RaggedTensor:
 
     @property
     def flat_values(self):
-        values=self.values
-        while isinstance(values,RaggedTensor):
-            values=values.values
+        values = self.values
+        while isinstance(values, RaggedTensor):
+            values = values.values
         return values
 
     @property
@@ -112,4 +128,3 @@ class RaggedTensor:
             rt_nested_splits.append(rt_values.row_splits)
             rt_values = rt_values.values
         return tuple(rt_nested_splits)
-
