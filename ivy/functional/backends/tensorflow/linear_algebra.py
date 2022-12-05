cfdc3159ee495c0dec07ddf83cd33dcf3c5f9aef
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-12-05 13:22:09 +0000

    fixing failing tests of miscellaneous_ops

--- ivy/functional/backends/tensorflow/linear_algebra.py
+++ ivy/functional/backends/tensorflow/linear_algebra.py
@@ -78,7 +78,7 @@ def diagonal(
     axis2: int = -1,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
-    return tf.experimental.numpy.diagonal(x, offset, axis1=axis1, axis2=axis2)
+    return tf.experimental.numpy.diagonal(x, offset=offset, axis1=axis1, axis2=axis2)
 
 
 @with_unsupported_dtypes({"2.9.1 and below": ("float16", "bfloat16")}, backend_version)
