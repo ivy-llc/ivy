e51fbdc6596fa8446dd4af620b02a942da653086
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-12-05 12:09:32 +0530

    Fixed failing test for is_ivy_array, is_native_array (#7586)

--- ivy/functional/ivy/general.py
+++ ivy/functional/ivy/general.py
@@ -120,6 +120,7 @@ def get_referrers_recursive(
     return ret_cont
 
 
+@handle_nestable
 @handle_exceptions
 def is_native_array(
     x: Union[ivy.Array, ivy.NativeArray], /, *, exclusive: bool = False
@@ -156,6 +157,7 @@ def is_native_array(
         return False
 
 
+@handle_nestable
 @handle_exceptions
 def is_ivy_array(
     x: Union[ivy.Array, ivy.NativeArray], /, *, exclusive: Optional[bool] = False
