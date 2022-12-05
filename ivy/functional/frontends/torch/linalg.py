8ccec79bce76bf9b2da4aac0af30ed0989ee076b
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2022-12-05 13:22:09 +0000

    fixing failing tests of miscellaneous_ops

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -6,7 +6,7 @@ from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
 def diagonal(input, *, offset=0, dim1=-2, dim2=-1):
-    return torch_frontend.diagonal(input, offset=offset, axis1=dim1, axis2=dim2)
+    return torch_frontend.diagonal(input, offset=offset, dim1=dim1, dim2=dim2)
 
 
 @to_ivy_arrays_and_back
