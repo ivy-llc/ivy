1413a0bea97568a8ff96832208a7aa388d454da7
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-12-05 19:53:43 +0530

    formatting fixes

--- ivy/functional/backends/torch/experimental/linear_algebra.py
+++ ivy/functional/backends/torch/experimental/linear_algebra.py
@@ -17,7 +17,7 @@ def diagflat(
     *,
     offset: Optional[int] = 0,
     padding_value: Optional[float] = 0,
-        align: Optional[str] = "RIGHT_LEFT",
+    align: Optional[str] = "RIGHT_LEFT",
         num_rows: Optional[int] = -1,
         num_cols: Optional[int] = -1,
         out: Optional[torch.Tensor] = None,
