3d872981f06ecf9f9048b4bd2486452a9efc232b
fspyridakos <fotissp@yahoo.com>
2022-12-06 13:21:22 +0000

    added frontend dtype namespaces

--- ivy/functional/frontends/jax/numpy/__init__.py
+++ ivy/functional/frontends/jax/numpy/__init__.py
@@ -5,5 +5,7 @@ from . import creation
 from .creation import *
 from . import name_space_functions
 from .name_space_functions import *
+from . import dtype
+from .dtype import *
 
 from .._src.numpy.lax_numpy import _rewriting_take
