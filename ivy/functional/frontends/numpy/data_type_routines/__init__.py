6178531d708aa7fd2072284a2917efee78cc81e8
fspyridakos <fotissp@yahoo.com>
2022-12-06 13:21:22 +0000

    added frontend dtype namespaces

---
+++ ivy/functional/frontends/numpy/data_type_routines/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from . import creating_data_types
+from creating_data_types import *
+from . import data_type_testing
+from .data_type_testing import *
+from . import data_type_information
+from .data_type_information import *
+from . import general
+from .general import *
+from . import miscellaneous
+from .miscellaneous import *
