1777a12158c2af6992eaa0043af2afc1a087c1a6
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-12-06 12:53:52 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -6,8 +6,8 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 # split
 @handle_frontend_test(
-    class_tree="ivy.functional.frontends.numpy.split()"
-    init_tree="numpy.split",
+    class_tree="numpy.ndarray"
+    init_tree="numpy.array",
     method_name="__split__",
 )
 def test_numpy_split(
