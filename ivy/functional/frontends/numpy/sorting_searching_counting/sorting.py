ae18982233fc76ab5ac9b83a0f4a6331d2ea9ca6
Fredricka David <110636706+Fredricka23@users.noreply.github.com>
2022-12-09 01:11:39 +0200

    numpy_sorting.py

--- ivy/functional/frontends/numpy/sorting_searching_counting/sorting.py
+++ ivy/functional/frontends/numpy/sorting_searching_counting/sorting.py
@@ -1,15 +1,12 @@
+import ivy
+
 def argsort(
-        x,
-        /,
-        *,
-        axis=-1,
-        kind=None,
-        order=None,
     x,
     /,
     *,
     axis=-1,
-    kind=None,
-    order=None,
+    descending=False,
+    stable=True,
+    out=None,
 ):
-    return ivy.argsort(x, axis=axis)
+    return ivy.argsort(x, axis=axis, descending=descending, stable=stable, out=out)
