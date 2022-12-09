5a1be2026c5a5ce9328f2e1f47d241d1ec69fa93
dash96 <64426988+dash96@users.noreply.github.com>
2022-12-09 22:15:59 +0100

    Update sort_complex.py

--- ivy/functional/frontends/numpy/sorting_searching_counting/sort_complex.py
+++ ivy/functional/frontends/numpy/sorting_searching_counting/sort_complex.py
@@ -3,7 +3,6 @@ from ivy.functional.frontends.numpy.func_wrapper import (
     to_ivy_arrays_and_back,
     handle_numpy_casting,
 )
-
 @handle_numpy_casting
 @to_ivy_arrays_and_back
 def sort_complex(array):
