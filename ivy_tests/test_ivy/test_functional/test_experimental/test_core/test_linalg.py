2613289555b4beccf183fdeafb2592edf2ca458b
anurag629 <akvermaav629@gmail.com>
2022-12-10 19:26:25 +0530

    integer

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
@@ -223,7 +223,7 @@ def test_kron(
 @handle_test(
     fn_tree="functional.experimental.matrix_exp",
     dtype_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("int"),
+        available_dtypes=helpers.get_dtypes("integer"),
         min_num_dims=2,
         max_num_dims=2,
         min_dim_size=2,
