d82d1f6a1a5cc1f3836f683966e5a99749446b1e
anurag629 <akvermaav629@gmail.com>
2022-12-10 19:18:15 +0530

    fixed number of dimension in test cases

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
@@ -223,10 +223,10 @@ def test_kron(
 @handle_test(
     fn_tree="functional.experimental.matrix_exp",
     dtype_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("int"),
         min_num_dims=2,
-        max_num_dims=4,
-        min_dim_size=1,
+        max_num_dims=2,
+        min_dim_size=2,
         max_dim_size=50,
     ),
 )
