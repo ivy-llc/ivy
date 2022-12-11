0ded5fb59846ac5feb0ae83f61e43898f6fe10df
div5252 <divyamsingal01@gmail.com>
2022-12-11 07:33:52 +0000

    Add jax.numpy.exp frontend function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -3583,3 +3583,33 @@ def test_jax_numpy_divide(
         a=x[0],
         b=x[1],
     )
+
+
+# exp
+@handle_frontend_test(
+    fn_tree="jax.numpy.exp",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float")
+    ),
+)
+def test_jax_numpy_exp(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
