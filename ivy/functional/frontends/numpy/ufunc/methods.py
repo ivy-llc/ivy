21d9a92dcf4c58223da781da1aab55517d79dc45
Norman Mico <normanrmico@gmail.com>
2022-12-11 14:06:35 +0200

    fixing indentation issues

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -3,15 +3,10 @@ from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
 
 
 @inputs_to_ivy_arrays
-def at(
-    ufunc,
-    array,
-    indices,
-    b=None,
-    /):
+def at(ufunc, array, indices, b=None, /):
     if b:
         for index in indices:
-          array[index] = ufunc(array[index], b)
+            array[index] = ufunc(array[index], b)
     else:
-       for index in indices:
-          array[index] = ufunc(array[index])
+        for index in indices:
+            array[index] = ufunc(array[index])
