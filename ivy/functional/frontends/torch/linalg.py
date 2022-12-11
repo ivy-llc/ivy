b7edaf31f2be5f21c4ea71748127b8920184c74b
Abhimanyu <abhimanyu305@gmail.com>
2022-12-11 12:26:20 +0530

    impl svd in torch frontend, all tests passing (#7691)
    Co-authored-by: Aditya Pola <adityapola@gmail.com>

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -67,3 +67,8 @@ def matrix_power(input, n, *, out=None):
 @to_ivy_arrays_and_back
 def matrix_rank(input, *, atol=None, rtol=None, hermitian=False, out=None):
     return ivy.astype(ivy.matrix_rank(input, atol=atol, rtol=rtol, out=out), ivy.int64)
+
+
+@to_ivy_arrays_and_back
+def svd(input, /, *, full_matrices=True):
+    return ivy.svd(input, compute_uv=True, full_matrices=full_matrices)
