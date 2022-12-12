05f4daacd25d91d6b2c61dbf45aae52178c00193
benedetta castagna <benny.castagna@gmail.com>
2022-12-12 14:16:24 +0800

    fixing other formatting issues

--- ivy/ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_cov.py
+++ ivy/ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_cov.py
@@ -9,10 +9,11 @@ from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
+
 # cov
 @handle_frontend_test(
-    fn_tree = "numpy.cov",
-    dtype_and_x=statistical_dtype_values(function="mean"), # <-- to change?
+    fn_tree="numpy.cov",
+    dtype_and_x=statistical_dtype_values(function="cov"),
     dtype=helpers.get_dtypes("float", full=False, none=True),
     where=np_frontend_helpers.where(),
     keep_dims=st.booleans(),
@@ -32,21 +33,21 @@ def test_numpy_cov(
         axis = axis[0]
 
     as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
-        input_dtype = input_dtype,
-        as_variable = as_variable,
-        native_array = native_array,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
     )
 
     np_frontend_helpers.test_frontend_function(
-        input_dtypes = input_dtype,
-        as_variable_flags = as_variable,
-        num_positional_args = num_positional_args,
-        native_array_flags = native_array,
-        frontend = frontend,
-        fn_tree = fn_tree,
-        on_device = on_device,
-        x = x[0],
-        axis = axis,
-        dtype = dtype[0],
-        test_values = False,
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        axis=axis,
+        dtype=dtype[0],
+        test_values=False,
     )
