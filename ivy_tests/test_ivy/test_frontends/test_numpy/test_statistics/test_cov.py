14844bc7dde7883776ece6b77775dede853a20a4
Benedetta Castagna <83835162+picqushini@users.noreply.github.com>
2022-12-12 19:02:08 +0800

    Update test_cov.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_cov.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_cov.py
@@ -1,3 +1,6 @@
+# global
+from hypothesis import strategies as st, assume
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
