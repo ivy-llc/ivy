288e2bd38068c34f4c493e1a583784c82e32f367
CatB1t <skytedits@gmail.com>
2022-12-12 18:04:36 +0200

    add flags builder, update `conftest.py` to use it.

--- ivy_tests/test_ivy/helpers/test_parameter_flags.py
+++ ivy_tests/test_ivy/helpers/test_parameter_flags.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+from hypothesis import strategies as st  # NOQA
+
+
 class ContainerFlags:
     pass
 
@@ -20,3 +24,32 @@ class NativeArrayFlags:
 
 class AsVariableFlags:
     pass
+
+
+@dataclass(frozen=True)
+class _FlagBuilder:
+    strategy: st.SearchStrategy
+
+
+_BuiltNativeArray = _FlagBuilder(st.lists(st.booleans(), min_size=1, max_size=1))
+_BuiltAsVariable = _FlagBuilder(st.lists(st.booleans(), min_size=1, max_size=1))
+_BuiltContainer = _FlagBuilder(st.booleans())
+_BuiltInstance = _FlagBuilder(st.booleans())
+_BuiltWithOut = _FlagBuilder(st.booleans())
+_BuiltGradient = _FlagBuilder(st.booleans())
+
+
+flags_mapping = {
+    "as_variable": "_BuiltAsVariable",
+    "native_array": "_BuiltNativeArray",
+    "container": "_BuiltContainer",
+    "with_out": "_BuiltWithOut",
+    "instance_method": "_BuiltInstance",
+    "test_gradients": "_BuiltGradient",
+}
+
+
+def build_flag(key: str, value: bool):
+    if value is not None:
+        value = st.just(value)
+    globals()[flags_mapping[key]] = _FlagBuilder(strategy=value)
