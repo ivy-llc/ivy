f2cc98acc5a073db3726c6cdb048d7f37b52fa6b
Benedetta Castagna <83835162+picqushini@users.noreply.github.com>
2022-12-12 18:49:12 +0800

    Create test_cov.py

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_cov.py
@@ -0,0 +1,53 @@
+# global
+from hypothesis import strategies as st, assume
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
+    statistical_dtype_values,
+)
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# cov
+@handle_frontend_test(
+    fn_tree="numpy.cov",
+    dtype_and_x=statistical_dtype_values(function="cov"),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
+    where=np_frontend_helpers.where(),
+    keep_dims=st.booleans(),
+)
+def test_numpy_cov(
+    dtype_and_x,
+    dtype,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, axis = dtype_and_x
+    if isinstance(axis, tuple):
+        axis = axis[0]
+
+    as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        axis=axis,
+        dtype=dtype[0],
+        test_values=False,
+    )
