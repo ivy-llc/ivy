e165d93ff51763bc0bdff71456d6a7ae1daa3693
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-12-13 16:55:06 +0100

    torch_logaddexp2 (#7906)
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -392,3 +392,8 @@ def rsqrt(input, *, out=None):
 @to_ivy_arrays_and_back
 def expm1(input, out=None):
     return ivy.expm1(input, out=out)
+
+
+@with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+def logaddexp2(x1, x2, out=None):
+    return ivy.logaddexp2(x1, x2, out=out)
