9b1fce3e7354faff5ce6ed0ed0c6ea641d4a59d6
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-12-13 09:24:32 +0530

    Renamed diff_ back to diff (#7864)

--- ivy/functional/backends/jax/experimental/elementwise.py
+++ ivy/functional/backends/jax/experimental/elementwise.py
@@ -188,7 +188,7 @@ def allclose(
     return jnp.allclose(x1, x2, rtol=rtol, atol=atol, equal_nan=equal_nan)
 
 
-def diff_(
+def diff(
     x: Union[JaxArray, int, float, list, tuple],
     /,
     *,
