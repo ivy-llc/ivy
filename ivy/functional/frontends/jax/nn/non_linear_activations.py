2f7617519e09def7a6d638fc3a31554dabdb78fc
kundakinte <69470630+kundakinte@users.noreply.github.com>
2022-12-13 21:37:36 +0300

    Update non_linear_activations.py
    reverted back to an older commit

--- ivy/functional/frontends/jax/nn/non_linear_activations.py
+++ ivy/functional/frontends/jax/nn/non_linear_activations.py
@@ -306,17 +306,8 @@ def softplus(x):
 
 @to_ivy_arrays_and_back
 def selu(x):
-    alpha = -ivy.sqrt(2 / ivy.pi) / ((1 - ivy.erf(1 / ivy.sqrt(2))) 
-            * ivy.exp(1 / 2) - 1)
-    scale = ((1 - (1 - ivy.erf(1 / ivy.sqrt(2))) * ivy.sqrt(ivy.e)) 
-            * ivy.sqrt(2 * ivy.pi) 
-            * (
-                2 * (1 - ivy.erf(ivy.sqrt(2))) * ivy.e ** 2 
-                + ivy.pi * (1 - ivy.erf(1 / ivy.sqrt(2))) ** 2 * ivy.e 
-                - 2 * (2 + ivy.pi) * (1 - ivy.erf(1 / ivy.sqrt(2))) * ivy.sqrt(ivy.e) 
-                + ivy.pi 
-                + 2
-            ) ** (-1 / 2))
+    alpha = 1.6732632423543772848170429916717
+    scale = 1.0507009873554804934193349852946
     return scale * elu(x, alpha)
 
 
