5db26ef2786a5147df124de37484d3b2e4f27fb6
arslan <artikov.arslan@gmail.com>
2022-12-14 19:21:10 +0300

    class_tree, method_tree, and method_name were added

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -4203,6 +4203,41 @@ def test_torch_instance_tril(
     )
 
 
+# sqrt
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="sqrt",
+    dtype_x=helpers.dtype_and_values(
+                available_dtypes=helpers.get_dtypes("bfloat16", full=True),
+    ),
+)
+def test_torch_instance_sqrt(
+    dtype_x,
+    init_num_positional_args: pf.NumPositionalArgFn,
+    method_num_positional_args: pf.NumPositionalArgMethod,
+    as_variable: pf.AsVariableFlags,
+    native_array: pf.NativeArrayFlags,
+    frontend,
+    frontend_method_data
+):
+    input_dtype, x = dtype_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_as_variable_flags=as_variable,
+        init_num_positional_args=init_num_positional_args,
+        init_native_array_flags=native_array,
+        init_all_as_kwargs_np={"data": x[0]},
+        method_input_dtypes=input_dtype,
+        method_as_variable_flags=as_variable,
+        method_num_positional_args=method_num_positional_args,
+        method_native_array_flags=native_array,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        frontend=frontend,
+    )
+
+
 # index_select
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -4418,37 +4453,4 @@ def test_torch_special_or(
     )
 
 
-# sqrt
-@handle_frontend_method(
-    class_tree=CLASS_TREE,
-    init_tree="torch.tensor",
-    method_name="sqrt",
-    dtype_x=helpers.dtype_and_values(
-                available_dtypes=helpers.get_dtypes("bfloat16", full=True),
-    ),
-)
-def test_torch_instance_sqrt(
-    dtype_x,
-    init_num_positional_args: pf.NumPositionalArgFn,
-    method_num_positional_args: pf.NumPositionalArgMethod,
-    as_variable: pf.AsVariableFlags,
-    native_array: pf.NativeArrayFlags,
-    frontend,
-    frontend_method_data
-):
-    input_dtype, x = dtype_x
-    helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        init_as_variable_flags=as_variable,
-        init_num_positional_args=init_num_positional_args,
-        init_native_array_flags=native_array,
-        init_all_as_kwargs_np={"data": x[0]},
-        method_input_dtypes=input_dtype,
-        method_as_variable_flags=as_variable,
-        method_num_positional_args=method_num_positional_args,
-        method_native_array_flags=native_array,
-        method_all_as_kwargs_np={},
-        frontend_method_data=frontend_method_data,
-        frontend=frontend,
-    )
 
