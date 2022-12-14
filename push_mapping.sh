408acbc48c7b65a6b9259cf4358affed3cade291
RashulChutani <rashul.chutani@gmail.com>
2022-12-14 10:00:27 +0530

    Bug Fixes Test Coverage [skip ci]

--- push_mapping.sh
+++ push_mapping.sh
@@ -6,3 +6,4 @@ SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
 ssh-keyscan -H "github.com" > "$SSH_KNOWN_HOSTS_FILE"
 export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
 echo "Hello!"
+git clone -b master git@github.com:unifyai/Mapping.git --depth 1
