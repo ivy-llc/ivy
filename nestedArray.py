e37b0d0ba1a039c6a90d040f863f6cd7fc082018
sherry30 <sherrytst30@gmail.com>
2022-12-14 22:04:41 +0500

    adding multi-dimensional example for `from_row_lengths`

--- nestedArray.py
+++ nestedArray.py
@@ -8,19 +8,29 @@ x = ivy.random_uniform(shape=(2, 2))
 y = ivy.random_uniform(shape=(2, 3))
 z = ivy.random_uniform(shape=(2, 2))
 
-
+# using nested_array
 c = ivy.NestedArray.nested_array([x, y, z], dtype=np.float64)
 print(c[0])
 print(c.dtype)
 print(c.ndim)
 print(c.shape)
 
+# using from_row_lengths
 c = ivy.NestedArray.from_row_lengths([3, 1, 4, 1, 5, 9, 2, 6], [4, 0, 3, 1, 0])
 print(c)
 print(c.dtype)
 print(c.ndim)
 print(c.shape)
 
+# using from_row_lengths with multi-dimensional values
+c = ivy.NestedArray.from_row_lengths([[3, 1, 4], [1, 5, 9], [2, 6, 9]], [2, 1])
+print(c)
+print(c.dtype)
+print(c.ndim)
+print(c.shape)
+
+
+# using from_row_split
 c = ivy.NestedArray.from_row_split([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8, 8])
 print(c)
 print(c.dtype)
