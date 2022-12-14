4f57dc004769814c0307b8ccbb13edaf2c9ca52a
RashulChutani <rashul.chutani@gmail.com>
2022-12-14 10:04:13 +0530

    Bug Fixes Test Coverage [skip ci]

--- push_mapping.sh
+++ push_mapping.sh
@@ -1,9 +1,15 @@
+SSH_DEPLOY_KEY=${1}
+GITHUB_SERVER="github.com"
 mkdir --parents "$HOME/.ssh"
 DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
-echo "$1" > "$DEPLOY_KEY_FILE"
+echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
 chmod 600 "$DEPLOY_KEY_FILE"
+
 SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
-ssh-keyscan -H "github.com" > "$SSH_KNOWN_HOSTS_FILE"
+ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
+
 export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+
 echo "Hello!"
+
 git clone -b master git@github.com:unifyai/Mapping.git --depth 1
