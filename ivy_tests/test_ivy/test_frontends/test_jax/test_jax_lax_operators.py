b0dbeb821e7799143cd407bd7fe15047a88f6c64
Dominic Qu <dcq21@ic.ac.uk>
2022-12-14 16:12:06 +0000

    Added test for int16

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -1465,6 +1465,36 @@ def test_jax_lax_gt(
     )
 
 
+@handle_frontend_test(
+    fn_tree="jax.lax.int16",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_jax_lax_int16(
+    *,
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
 # cos
 @handle_frontend_test(
     fn_tree="jax.lax.cos",
