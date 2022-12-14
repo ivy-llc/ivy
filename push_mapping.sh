08d56e7c69d34c0c760e1ab3e60afe3facfb264d
RashulChutani <rashul.chutani@gmail.com>
2022-12-14 10:30:54 +0530

    Bug Fixes Test Coverage [skip ci]

--- push_mapping.sh
+++ push_mapping.sh
@@ -16,16 +16,8 @@ export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNO
 echo "Hello!"
 
 # Setup git
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+#git config --global user.email "$USER_EMAIL"
+#git config --global user.name "$USER_NAME"
 
-{
-	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" git@github.com:unifyai/Mapping.git "$CLONE_DIR"
-} || {
-	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
-	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
-	exit 1
-}
+git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" git@github.com:unifyai/Mapping.git
 
