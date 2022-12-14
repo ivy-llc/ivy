44e709d4aac0cc681b758011c1d322aa709facf3
Aniket2002 <38128258+Aniket2002@users.noreply.github.com>
2022-12-14 16:00:18 +0530

    Update test_splitting_arrays.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -7,9 +7,7 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 # split
 @handle_frontend_test(
     fn_tree="numpy.split",
-    indices_or_sections_ary_axis=array_indices_axis(
-       
-    )
+    indices_or_sections_ary_axis=array_indices_axis(array_dtypes(helpers.ints(min_value=1, max_value=5)))
 )
 def test_numpy_split(
     fn_tree,
