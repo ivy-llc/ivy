e389b395c76b4c2d81a22ca030e7083289a07862
StevenFoo <keinsheng@gmail.com>
2022-12-16 02:08:22 +0800

    formatting

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -1,10 +1,8 @@
 # global
 import ivy
 from hypothesis import strategies as st
-import hypothesis.extra.numpy as nph
 # local
 import ivy_tests.test_ivy.helpers as helpers
-
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
