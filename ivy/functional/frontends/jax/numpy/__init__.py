84b858640e3e202a4d7d7167be104642dce46647
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-12-15 21:04:56 +0530

    refactored `jax.numpy` namespace functions (#7976)

--- ivy/functional/frontends/jax/numpy/__init__.py
+++ ivy/functional/frontends/jax/numpy/__init__.py
@@ -402,9 +402,19 @@ from . import fft
 from . import linalg
 from . import creation
 from .creation import *
-from . import name_space_functions
-from .name_space_functions import *
 from . import dtype
 from .dtype import can_cast, promote_types
+from . import indexing
+from .indexing import *
+from . import logic
+from .logic import *
+from . import manipulations
+from .manipulations import *
+from . import mathematical_functions
+from .mathematical_functions import *
+from . import statistical
+from .statistical import *
+from . import searching_sorting
+from .searching_sorting import *
 
 from .._src.numpy.lax_numpy import _rewriting_take
