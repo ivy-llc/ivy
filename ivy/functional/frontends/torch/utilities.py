c8750a193578e5197f69de3650954d49bda95b4f
KevinUnadkat <109789435+KevinUnadkat@users.noreply.github.com>
2022-12-15 17:07:46 +0530

    Update utilities.py

--- ivy/functional/frontends/torch/utilities.py
+++ ivy/functional/frontends/torch/utilities.py
@@ -1,6 +1,6 @@
-# local
 import ivy
+from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
-
+@to_ivy_arrays_and_back
 def result_type(tensor1, tensor2):
     return ivy.result_type(tensor1, tensor2)
