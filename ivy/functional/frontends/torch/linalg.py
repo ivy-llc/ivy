d1853d6977461476339d33235907373a30cae89a
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2022-12-15 17:52:02 +0500

    frontends.torch.linalg: add support for cross (#7656)
    Co-authored-by: jkeane508 <jameskeane508@gmail.com>

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -2,6 +2,7 @@
 import ivy
 import ivy.functional.frontends.torch as torch_frontend
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
+from ivy.func_wrapper import with_unsupported_dtypes
 
 
 @to_ivy_arrays_and_back
@@ -62,6 +63,12 @@ def slogdet(input, *, out=None):
 @to_ivy_arrays_and_back
 def matrix_power(input, n, *, out=None):
     return ivy.matrix_power(input, n, out=out)
+
+
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+def cross(input, other, *, dim=-1, out=None):
+    return torch_frontend.cross(input, other, dim, out=out)
 
 
 @to_ivy_arrays_and_back
