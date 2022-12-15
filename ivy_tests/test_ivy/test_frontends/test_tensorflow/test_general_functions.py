1eed73f3e460febd76189521ef868527f94a03c1
Diego Plata <diegogplata@gmail.com>
2022-12-15 06:15:58 -0600

    Add identity function to tensorflow frontend (#7627)
    * move implementation of identity function in general_functions file and add alias in RawOps file
    
    * add test for identity function in test_general_functions file
    
    * add blank line to fix formatting
    
    * fix formatting breaking up long line
    
    * fix circular import error in test_trensorflow_identity function
    
    * fix lint errors
    
    Co-authored-by: Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -408,6 +408,36 @@ def test_tensorflow_ones_like(
     )
 
 
+# identity
+@handle_frontend_test(
+    fn_tree="tensorflow.identity",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric", full=True),
+    ),
+)
+def test_tensorflow_identity(
+    dtype_and_x,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+    )
+
+
 # zeros_like
 @handle_frontend_test(
     fn_tree="tensorflow.zeros_like",
