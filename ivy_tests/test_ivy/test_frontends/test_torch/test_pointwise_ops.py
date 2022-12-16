2e63a01d3c1858d8bfe66f4461c4498858a25d0d
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-12-16 16:18:08 +0100

    torch_positive (#8530)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2218,3 +2218,36 @@ def test_torch_rad2deg(
         on_device=on_device,
         input=x[0],
     )
+
+
+# negative
+@handle_frontend_test(
+    fn_tree="torch.positive",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+    ),
+)
+def test_torch_positive(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+    )
