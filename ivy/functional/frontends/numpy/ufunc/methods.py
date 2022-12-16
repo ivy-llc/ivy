d6fd855f3b3253e63dc6e1988fbe54c99d54c582
Norman Mico <normanrmico@gmail.com>
2022-12-16 18:18:12 +0200

    Correcting formatting issues

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -1,9 +1,11 @@
 # local
 from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
 
+
 class ufunc:
     def __init__(self) -> None:
         pass
+
     @inputs_to_ivy_arrays
     def at(ufunc, array, indices, b=None, /):
         if b:
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ufunc/test_methods.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ufunc/test_methods.py
@@ -1,7 +1,6 @@
 # global
 
 # local
-import ivy_tests.test_ivy.helpers as helpers
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_method
 
