388bbdbd00c3fbe7848f7a60bdd434fb498f711e
sherry30 <sherrytst30@gmail.com>
2022-12-16 17:44:16 +0500

    test_array change

--- ivy_tests/test_ivy/test_array.py
+++ ivy_tests/test_ivy/test_array.py
@@ -65,9 +65,7 @@ def _pow_helper(draw, available_dtypes=None):
     )
     dtype2 = dtype2[0]
     if "int" in dtype2:
-        x2 = ivy.nested_map(
-            x2[0], lambda x: abs(x), include_derived={list: True}, shallow=False
-        )
+        x2 = ivy.nested_map(x2[0], lambda x: abs(x), include_derived={list: True})
     return [dtype1, dtype2], [x1, x2]
 
 
