8cc8894e3de7bca32deaf3f19d957d74f472453c
KevinUnadkat <109789435+KevinUnadkat@users.noreply.github.com>
2022-12-17 16:00:19 +0530

    Update test_utilities.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_utilities.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_utilities.py
@@ -1,3 +1,6 @@
+# global
+from hypothesis import strategies as st
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
@@ -5,12 +8,16 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 @handle_frontend_test(
     fn_tree="torch.result_type",
-    dtype_and_x=helpers.dtype_and_values(
+    dtype_x_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("valid"),
+        valid_axis=True,
+        max_axes_size=1,
     ),
+    keepdims=st.booleans(),
 )
 def test_torch_result_type(
-    dtype_and_x,
+    dtype_and_axis,
+    keepdims,
     as_variable,
     with_out,
     num_positional_args,
@@ -19,7 +26,7 @@ def test_torch_result_type(
     fn_tree,
     frontend,
 ):
-    input_dtype, x = dtype_and_x,
+    input_dtype, x, axis= dtype_x_axis,
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
@@ -30,4 +37,6 @@ def test_torch_result_type(
         fn_tree=fn_tree,
         on_device=on_device,
         input=x[0],
+        axis=axis,
+        keepdims=keepdims,
     )
