dc885664813e8ae9f7f8c73be08269179f67b814
Rene Fabricius <fabricius.rene@gmail.com>
2022-12-17 13:15:18 +0000

    Add frac for Torch frontend.

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2186,13 +2186,13 @@ def test_torch_i0(
 @handle_frontend_test(
     fn_tree="torch.rad2deg",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"), 
-        num_arrays=2, 
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
         shared_dtype=True,
         max_dim_size=3,
         max_num_dims=3,
         min_dim_size=1,
-        min_num_dims=1
+        min_num_dims=1,
     ),
 )
 def test_torch_rad2deg(
@@ -2229,6 +2229,38 @@ def test_torch_rad2deg(
     ),
 )
 def test_torch_positive(
+    *,
+    dtype_and_x,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+    )
+
+
+# frac
+@handle_frontend_test(
+    fn_tree="torch.frac",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_torch_frac(
     *,
     dtype_and_x,
     as_variable,
