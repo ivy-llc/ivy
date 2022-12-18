a6d6417389b2fa80c3203dbfb8416f94b7d7a05c
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-12-18 22:46:18 +0530

    Update test_statistical.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
@@ -257,7 +257,7 @@ def test_nanquantile(
     fn_name,
     ground_truth_backend,
 ):
-    input_dtype, x, axis, interpolation, q = dtype_x_axis, interpolation, q
+    input_dtype, x, axis, interpolation, q = dtype_x_axis
     helpers.test_function(
         ground_truth_backend=ground_truth_backend,
         input_dtypes=input_dtype,
