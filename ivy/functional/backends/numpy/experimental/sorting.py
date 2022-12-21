3da533c8e2ea5c3dd47ee830947702ba4fdb23cb
Nadide Beyza Dokur <nadidebeyzadokur@gmail.com>
2022-12-21 19:34:30 +0300

    Removed experimental-api changes

--- ivy/functional/backends/numpy/experimental/sorting.py
+++ ivy/functional/backends/numpy/experimental/sorting.py
@@ -11,14 +11,3 @@ def msort(
 
 
 msort_support_native_out = False
-
-
-# lexsort
-def lexsort(
-    x: Union[np.ndarray, list, tuple], /, *, out: Optional[np.ndarray] = None,
-    axis: int = -1
-) -> np.ndarray:
-    return np.lexsort(x, axis)
-
-
-lexsort_support_native_out = False
