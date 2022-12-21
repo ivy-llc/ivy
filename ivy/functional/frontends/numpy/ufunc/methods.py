ddfb8c947f642451e527344263078801863bd067
Norman Mico <normanrmico@gmail.com>
2022-12-21 19:39:35 +0200

    applying requested changes

--- ivy/functional/frontends/numpy/ufunc/methods.py
+++ ivy/functional/frontends/numpy/ufunc/methods.py
@@ -3,14 +3,15 @@ from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
 
 
 class ufunc:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     @inputs_to_ivy_arrays
-    def at(ufunc, array, indices, b=None, /):
+    def at(self, indices, b=None, /):
+        array = self._ivy_array
         if b:
             for index in indices:
-                array[index] = ufunc(array[index], b)
+                array[index] = self(array[index], b)
         else:
             for index in indices:
-                array[index] = ufunc(array[index])
+                array[index] = self(array[index])
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ufunc/test_methods.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ufunc/test_methods.py
@@ -1,38 +1,51 @@
 # global
 
 # local
+import ivy_tests.test_ivy.helpers as helpers
+import ivy_tests.test_ivy.helpers.test_parameter_flags as pf
 import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_method
 
 
-# at
+CLASS_TREE = "ivy.functional.frontends.numpy.ufunc"
+
+
+#at
 @handle_frontend_method(
-    init_name="at",
+    method_name="at",
     init_tree="numpy.ufunc",
-    method_tree="numpy.ufunc.methods.ufunc.at"
+    class_tree=CLASS_TREE,
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=1,
+        min_num_dims=2,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+    )
 )
 def test_numpy_ufunc_at(
     dtype_and_x,
-    as_variable,
-    num_positional_args,
-    native_array,
+    as_variable: pf.AsVariableFlags,
+    native_array: pf.NativeArrayFlags,
+    init_num_positional_args: pf.NumPositionalArgFn,
+    method_num_positional_args: pf.NumPositionalArgMethod,
+    frontend_method_data,
     frontend,
-    fn_tree,
-    on_device,
 ):
     input_dtype, x = dtype_and_x
     dtype, input_dtype, casting = np_frontend_helpers.handle_dtype_and_casting(
         dtypes=input_dtype,
         get_dtypes_kind="numeric",
     )
-    np_frontend_helpers.test_frontend_function(
+    np_frontend_helpers.test_frontend_method(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
-        num_positional_args=num_positional_args,
+        init_num_positional_args=init_num_positional_args,
+        method_num_positional_args=method_num_positional_args,
         native_array_flags=native_array,
         frontend=frontend,
-        fn_tree=fn_tree,
-        on_device=on_device,
+        frontend_method_data=frontend_method_data,
         x=x[0],
         dtype=dtype,
     )
