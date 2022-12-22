9e41c4be6480dab2c7463152ea628683ff208c12
Ashi Gautam <74711030+Change-m@users.noreply.github.com>
2022-12-22 21:06:29 +0530

    Update test_statistical.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
@@ -13,8 +13,8 @@ from ivy_tests.test_ivy.helpers import handle_test
 
 @st.composite
 def statistical_dtype_values(draw, *, function):
-    large_abs_safety_factor = 20
-    small_abs_safety_factor = 20
+    large_abs_safety_factor = 42
+    small_abs_safety_factor = 42
     if function in ["mean", "median", "std", "var"]:
         large_abs_safety_factor = 24
         small_abs_safety_factor = 24
