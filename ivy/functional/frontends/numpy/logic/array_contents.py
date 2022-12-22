a227651d401c040e485a92bb00641e182ba1203c
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2022-12-22 07:37:22 +0000

    datetime not supported

--- ivy/functional/frontends/numpy/logic/array_contents.py
+++ ivy/functional/frontends/numpy/logic/array_contents.py
@@ -33,6 +33,7 @@ def isclose(a, b, rtol=1e-05, atol=1e-08, equal_nan=False):
     return ivy.isclose(a, b, rtol=rtol, atol=atol, equal_nan=equal_nan)
 
 
+# TODO: datetime dtypes not supported by ivy yet
 def isnat(
     x,
     /,
