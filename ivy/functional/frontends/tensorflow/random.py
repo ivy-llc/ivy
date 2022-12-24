d34ff923ef55c8d0cfab67ccfc50e3ebeba010e2
Naman Garg <namangarg110@gmail.com>
2022-12-24 19:45:55 +0000

    formatting

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -8,7 +8,8 @@ from ivy.functional.frontends.tensorflow.func_wrapper import (
 
 
 @with_supported_dtypes({"2.9.0 and below": ("float16", "bfloat16",
-                                            "float32", "float64", " int32", "int64",)}, "tensorflow")
+                                            "float32", "float64", 
+                                            " int32", "int64",)}, "tensorflow")
 @with_unsupported_dtypes({"2.9.0 and below": ("bfloat16",
                                               "float32",)}, "tensorflow")
 @to_ivy_arrays_and_back
@@ -19,7 +20,9 @@ def uniform(
         dtype=ivy.float32,
         seed=None,
 ):
-    if str(dtype) == "<dtype: 'float32'>":  # currently, to_ivy_dtype & with_unsupported_dtypes is unable to handed tf.float32.
+    # currently, to_ivy_dtype & with_unsupported_dtypes is unable to handed tf.float32.
+    if str(dtype) == "<dtype: 'float32'>":  
         dtype = ivy.float32
-    return ivy.random_uniform(low=minval, high=maxval, shape=shape, device=ivy.default_device(),
+    return ivy.random_uniform(low=minval, high=maxval, shape=shape, 
+                              device=ivy.default_device(),
                               dtype=to_ivy_dtype(dtype), seed=seed)
