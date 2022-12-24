cb3171c8778586cc80e4b664cd24ccd52ed7f372
Chaitanya134 <chaitanyagiri2@gmail.com>
2022-12-25 02:58:07 +0530

    Change function ordering

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -1324,46 +1324,6 @@ def test_numpy_instance_truediv__(
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="numpy.array",
-    method_name="__itruediv__",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=2,
-    ),
-)
-def test_numpy_instance_itruediv__(
-    dtype_and_x,
-    as_variable,
-    num_positional_args_method,
-    native_array,
-    frontend_method_data,
-    frontend,
-):
-    input_dtype, xs = dtype_and_x
-    assume(not np.any(np.isclose(xs[1], 0)))
-
-    helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        init_as_variable_flags=as_variable,
-        init_num_positional_args=1,
-        init_native_array_flags=native_array,
-        init_all_as_kwargs_np={
-            "data": xs[0],
-        },
-        method_input_dtypes=input_dtype,
-        method_as_variable_flags=as_variable,
-        method_num_positional_args=num_positional_args_method,
-        method_native_array_flags=native_array,
-        method_all_as_kwargs_np={
-            "value": xs[1],
-        },
-        frontend_method_data=frontend_method_data,
-        frontend=frontend,
-    )
-
-
-@handle_frontend_method(
-    class_tree=CLASS_TREE,
-    init_tree="numpy.array",
     method_name="__and__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("bool"),
@@ -2145,6 +2105,46 @@ def test_numpy_instance_imul__(
         },
         frontend=frontend,
         frontend_method_data=frontend_method_data,
+    )
+
+
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="numpy.array",
+    method_name="__itruediv__",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+    ),
+)
+def test_numpy_instance_itruediv__(
+    dtype_and_x,
+    as_variable,
+    num_positional_args_method,
+    native_array,
+    frontend_method_data,
+    frontend,
+):
+    input_dtype, xs = dtype_and_x
+    assume(not np.any(np.isclose(xs[1], 0)))
+
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_as_variable_flags=as_variable,
+        init_num_positional_args=1,
+        init_native_array_flags=native_array,
+        init_all_as_kwargs_np={
+            "data": xs[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_as_variable_flags=as_variable,
+        method_num_positional_args=num_positional_args_method,
+        method_native_array_flags=native_array,
+        method_all_as_kwargs_np={
+            "value": xs[1],
+        },
+        frontend_method_data=frontend_method_data,
+        frontend=frontend,
     )
 
 
