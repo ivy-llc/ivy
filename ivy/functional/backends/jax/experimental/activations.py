e15f7eb8f4dc6f6e177b5542e8f3db3ac7ba6b32
ivy-branch <ivy.branch@lets-unify.ai>
2022-12-28 08:00:50 +0000

    lintfixbot: Auto-commit fixed lint errors in codebase

--- ivy/functional/backends/jax/experimental/activations.py
+++ ivy/functional/backends/jax/experimental/activations.py
@@ -3,11 +3,7 @@ from typing import Optional
 from ivy.functional.backends.jax import JaxArray
 
 
-def logit(x: JaxArray,
-          /,
-          *,
-          eps: Optional[float] = None,
-          out=None):
+def logit(x: JaxArray, /, *, eps: Optional[float] = None, out=None):
     if eps is None:
         x = jnp.where(jnp.logical_or(x > 1, x < 0), jnp.nan, x)
     else:
