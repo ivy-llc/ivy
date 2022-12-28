2bece6df63313f5fabee809ced85dfbdc0f6130d
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-12-28 10:32:54 +0000

    Fixed test_sinc: tf experimental numpy function sinc call lead to .astype attribute error

--- ivy/functional/backends/tensorflow/experimental/elementwise.py
+++ ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -17,7 +17,8 @@ def sinc(
     *,
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
-    return tf.cast(tf.experimental.numpy.sinc(x), x.dtype)
+    return tf.cast(tf.where(x == 0, 1, tf.math.sin(ivy.pi * x) / (ivy.pi * x)),
+                   x.dtype)
 
 
 @with_unsupported_dtypes({"2.9.1 and below": ("unsigned",)}, backend_version)
