798777559c49049ed350ff58a84e6d4cd71a7353
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2022-12-29 05:13:13 +0000

    Quick lint fix

--- ivy/functional/backends/tensorflow/experimental/linear_algebra.py
+++ ivy/functional/backends/tensorflow/experimental/linear_algebra.py
@@ -73,7 +73,7 @@ def multi_dot(
     *, 
     out: Optional[Union[tf.Tensor, tf.Variable]] = None,
 ) -> tf.Tensor:
-    #This implementation simply chains tf.tensordot multiple times
+    # This implementation simply chains tf.tensordot multiple times
     # TODO: reimplement this function once tf adds multi_dot or inplace updates
     if len(x) == 1:
         return x[0]
