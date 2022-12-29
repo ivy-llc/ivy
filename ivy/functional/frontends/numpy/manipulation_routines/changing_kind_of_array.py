150c3cd1c25f1666ad61183e9edea9514f95aea9
Uzoagulu Promise C <99205369+Taciturny@users.noreply.github.com>
2022-12-29 13:42:08 +0000

    Changed

--- ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
@@ -6,6 +6,7 @@ def asanyarray(
     a,
     dtype=None,
     order=None,
+    *,
     like=None
 ):
     return ivy.asanyarray(a, dtype=dtype, order=order, like=like)
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
@@ -1,20 +1,21 @@
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
+from hypothesis import strategies as st
 
 
 # asanyarray
-
 @handle_frontend_test(
     fn_tree="numpy.asanyarray",
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
-    order=helpers.dtype_values_axis(order=None),
-    like=helpers.dtype_values_axis(like=None),
+    dtype=helpers.get_dtypes("valid", full=False),
+    order=st.sampled_from(["C", "F", "A", "K"]),
+    like=helpers.get_dtypes("valid", full=False),
 )
 def test_numpy_asanyarray(
-    *,
-    dtype_and_x,
+    a,
+    dtype,
     order,
+    *,
     like,
     as_variable,
     num_positional_args,
@@ -23,9 +24,8 @@ def test_numpy_asanyarray(
     fn_tree,
     frontend,
 ):
-    input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
-        input_dtypes=input_dtype,
+        input_dtypes=dtype,
         as_variable_flags=as_variable,
         with_out=False,
         num_positional_args=num_positional_args,
@@ -33,8 +33,8 @@ def test_numpy_asanyarray(
         frontend=frontend,
         fn_tree=fn_tree,
         on_device=on_device,
-        a=x,
-        dtype=input_dtype[0],
+        a=a,
+        dtype=dtype[0],
         order=order,
         like=like,
     )
