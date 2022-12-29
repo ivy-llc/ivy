c8fa784d2a3191ab9ff3a10b2ba9296999030199
hmahmood24 <hmahmood.bee16@gmail.com>
2022-12-29 07:50:04 +0000

    frontends.torch.avg_pool2d: second commit

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pooling_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pooling_functions.py
@@ -1,5 +1,5 @@
 # global
-from hypothesis import strategies as st
+import ivy
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -7,23 +7,18 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
-@st.composite
-def _avg_poolnd_pad_helper(draw, min_value, max_value, num_dims=2):
-    pad = draw(st.integers(min_value=min_value, max_value=max_value))
-    return tuple([pad for _ in range(0, num_dims)])
-
-
 # avg_pool2d
 @handle_frontend_test(
     fn_tree="torch.nn.functional.avg_pool2d",
     dtype_x_k_s=helpers.arrays_for_pooling(
-        min_dims=4, max_dims=4, min_side=1, max_side=4
+        min_dims=4,
+        max_dims=4,
+        min_side=1,
+        max_side=4,
     ),
-    padding=_avg_poolnd_pad_helper(min_value=1, max_value=3, num_dims=2),
 )
 def test_torch_avg_pool2d(
     dtype_x_k_s,
-    padding,
     *,
     as_variable,
     num_positional_args,
@@ -32,7 +27,22 @@ def test_torch_avg_pool2d(
     fn_tree,
     on_device,
 ):
-    input_dtype, x, kernel_size, stride, _ = dtype_x_k_s
+    input_dtype, x, kernel_size, stride, padding = dtype_x_k_s
+
+    # Torch ground truth func expects input to be consistent
+    # with a channels first format i.e. NCHW
+    x[0] = x[0].reshape((x[0].shape[0], x[0].shape[-1], *x[0].shape[1:-1]))
+    x_shape = list(x[0].shape[2:])
+
+    # Torch ground truth func also takes padding input as an integer
+    # or a tuple of integers, not a string
+    padding = tuple(
+        [
+            ivy.handle_padding(x_shape[i], stride[0], kernel_size[i], padding)
+            for i in range(len(x_shape))
+        ]
+    )
+
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
