20f32cfdef7c75b378c1290db01b78404980246d
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2022-12-29 17:20:22 +0100

    ivy_nextafter_fixes (#9212)
    Co-authored-by: nassimberrada <Nassim>

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
@@ -718,7 +718,7 @@ def test_fix(
 @handle_test(
     fn_tree="functional.ivy.experimental.nextafter",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float", index=2),
+        available_dtypes=["float32", "float64"],
         num_arrays=2,
         shared_dtype=True,
         min_value=-10,
@@ -729,9 +729,12 @@ def test_fix(
     test_gradients=st.just(False),
 )
 def test_nextafter(
+    *,
     dtype_and_x,
     test_flags,
     backend_fw,
+    fn_name,
+    on_device,
     ground_truth_backend,
 ):
     input_dtype, x = dtype_and_x
@@ -740,7 +743,8 @@ def test_nextafter(
         input_dtypes=input_dtype,
         test_flags=test_flags,
         fw=backend_fw,
-        fn_name="nextafter",
+        fn_name=fn_name,
+        on_device=on_device,
         x1=x[0],
         x2=x[1],
     )
