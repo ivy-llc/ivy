4d1878af5e3d1120f7cf0b839518d4ea2e8c9cfa
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-12-29 09:56:29 +0530

    Gradient testing for searching (#9188)

--- ivy_tests/test_ivy/test_functional/test_core/test_searching.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_searching.py
@@ -56,7 +56,6 @@ def _broadcastable_trio(draw):
     fn_tree="functional.ivy.argmax",
     dtype_x_axis=_dtype_x_limited_axis(allow_none=True),
     keepdims=st.booleans(),
-    test_gradients=st.just(False),
     select_last_index=st.booleans(),
 )
 def test_argmax(
@@ -90,7 +89,6 @@ def test_argmax(
     dtype_x_axis=_dtype_x_limited_axis(allow_none=True),
     keepdims=st.booleans(),
     output_dtype=st.sampled_from([ivy.int32, ivy.int64]),
-    test_gradients=st.just(False),
 )
 def test_argmin(
     *,
@@ -131,7 +129,6 @@ def test_argmin(
     size=st.integers(min_value=1, max_value=5),
     fill_value=st.one_of(st.integers(0, 5), helpers.floats()),
     test_with_out=st.just(False),
-    test_gradients=st.just(False),
 )
 def test_nonzero(
     *,
@@ -163,7 +160,6 @@ def test_nonzero(
 @handle_test(
     fn_tree="functional.ivy.where",
     broadcastables=_broadcastable_trio(),
-    test_gradients=st.just(False),
 )
 def test_where(
     *,
@@ -193,7 +189,6 @@ def test_where(
 @handle_test(
     fn_tree="functional.ivy.argwhere",
     x=helpers.dtype_and_values(available_dtypes=("bool",)),
-    test_gradients=st.just(False),
 )
 def test_argwhere(
     *,
