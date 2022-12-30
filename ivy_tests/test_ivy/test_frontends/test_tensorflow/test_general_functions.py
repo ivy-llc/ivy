927deab7969c369899f52ab09dc92a5251bfc172
Chaitanya Mittal <chtnnh@gmail.com>
2022-12-30 14:37:26 +0400

    feat: add is_tensor to tensorflow frontend general functions (#7799)
    Co-authored-by: WilliamHirst bod_holthe@outlook.com

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -900,6 +900,33 @@ def test_tensorflow_stack(
     )
 
 
+# is_tensor
+@handle_frontend_test(
+    fn_tree="tensorflow.is_tensor",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+)
+def test_tensorflow_is_tensor(
+    *,
+    dtype_and_x,
+    with_out,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=[False],
+        with_out=with_out,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        x=x[0],
+    )
+
+
 # gather
 @handle_frontend_test(
     fn_tree="tensorflow.gather",
