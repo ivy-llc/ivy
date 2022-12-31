fb2da81e21e8d4c1d70af6a3dceb29089605c9d3
EthanWangtl <97998419+EthanWangtl@users.noreply.github.com>
2022-12-30 19:30:19 -0600

    change fmod function so the unit tests can pass

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -298,7 +298,7 @@ def fmod(
     if dtype:
         x1 = ivy.astype(ivy.array(x1), ivy.as_ivy_dtype(dtype))
         x2 = ivy.astype(ivy.array(x2), ivy.as_ivy_dtype(dtype))
-    ret = ivy.fmod(x1, x2, out=out)
+    ret = ivy.remainder(x1, x2, out=out)
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
