250bdbd6919daeca7103804a83822feb46bf9ea5
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-12-31 19:31:34 +0000

    `diagonal` array/container missing docstrings and examples

--- ivy/functional/ivy/linear_algebra.py
+++ ivy/functional/ivy/linear_algebra.py
@@ -356,10 +356,6 @@ def diagonal(
         optional output array, for writing the result to. It must have a shape that the
         inputs broadcast to.
 
-    out
-        optional output array to write the result in. Must have the same number
-        of dimensions as the function output.
-
     Returns
     -------
     ret
@@ -447,32 +443,30 @@ def diagonal(
     ivy.array([[3.],
                [4.]])
 
-    With :class:`ivy.NativeArray` inputs:
-
-    >>> x = ivy.native_array([[1., 2.],
-    ...                       [3., 4.]])
+    >>> x = ivy.array([[1., 2.],
+    ...                [3., 4.]])
     >>> d = ivy.diagonal(x)
     >>> print(d)
     ivy.array([1., 4.])
 
-    >>> x = ivy.native_array([[[ 0,  1,  2],
-    ...                        [ 3,  4,  5],
-    ...                        [ 6,  7,  8]],
-    ...                       [[ 9, 10, 11],
-    ...                        [12, 13, 14],
-    ...                        [15, 16, 17]],
-    ...                       [[18, 19, 20],
-    ...                        [21, 22, 23],
-    ...                        [24, 25, 26]]])
+    >>> x = ivy.array([[[ 0,  1,  2],
+    ...                 [ 3,  4,  5],
+    ...                 [ 6,  7,  8]],
+    ...                [[ 9, 10, 11],
+    ...                 [12, 13, 14],
+    ...                 [15, 16, 17]],
+    ...                [[18, 19, 20],
+    ...                 [21, 22, 23],
+    ...                 [24, 25, 26]]])
     >>> d = ivy.diagonal(x, offset=1, axis1=1, axis2=-1)
     >>> print(d)
     ivy.array([[ 1,  5],
                [10, 14],
                [19, 23]])
 
-    >>> x = ivy.native_array([[0, 1, 2],
-    ...                       [3, 4, 5],
-    ...                       [6, 7, 8]])
+    >>> x = ivy.array([[0, 1, 2],
+    ...                [3, 4, 5],
+    ...                [6, 7, 8]])
     >>> d = ivy.diagonal(x)
     >>> print(d)
     ivy.array([0, 4, 8])
@@ -787,6 +781,7 @@ def inv(
     x
         input array having shape ``(..., M, M)`` and whose innermost two dimensions form
         square matrices. Should have a floating-point data type.
+
     out
         optional output array, for writing the result to. It must have a shape that the
         inputs broadcast to.
