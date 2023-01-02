cc6e86ab201d3aaae5e7edde8d357ff2d9a3caa0
Mayank57 <50748052+Mayank57@users.noreply.github.com>
2023-01-02 11:45:54 +0530

    Added empty function to jax.numpy frontend (#9233)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
@@ -414,3 +414,40 @@ def test_numpy_triu(
         m=x[0],
         k=k,
     )
+
+
+# empty
+@handle_frontend_test(
+    fn_tree="jax.numpy.empty",
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+)
+def test_jax_numpy_empty(
+    shape,
+    dtype,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=False,
+        shape=shape,
+        dtype=dtype[0],
+    )
