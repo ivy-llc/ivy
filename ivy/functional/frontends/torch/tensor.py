11dde074db69c771a6c4f1743976659f722716b0
Usama Baloch <83680012+usama-baloch@users.noreply.github.com>
2023-01-02 17:01:31 +0500

    Added acosh_ instance method and test method to pytorch frontend (#8959)
    * Added cosh instance method to pytorch frontend
    
    * Added cosh method to pytorch frontend
    
    * Update test_tensor.py
    
    * Update tensor.py
    
    * Update tensor.py
    
    * Update test_tensor.py
    
    * Update test_tensor.py

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -467,7 +467,7 @@ class Tensor:
 
     def clone(self, memory_format=None):
         return torch_frontend.tensor(ivy.array(self._ivy_array, copy=True))
-
+    
     @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
     def acosh(self):
         return torch_frontend.acosh(self._ivy_array)
@@ -481,6 +481,11 @@ class Tensor:
 
     def masked_fill_(self, mask, value):
         self._ivy_array = self.masked_fill(mask, value).ivy_array
+        return self
+    
+    @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+    def acosh_(self):
+        self._ivy_array = self.acosh().ivy_array
         return self
 
     # Special Methods #
