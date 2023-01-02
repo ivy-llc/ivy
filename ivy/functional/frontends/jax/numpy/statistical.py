55eed05be2c9db960ce69be1df6efc37a66d07ec
MohamedEhab <mohamedehab0999@gmail.com>
2023-01-02 18:01:11 +0200

    frontend(jax): add jax.numpy.nanmax method

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -145,3 +145,48 @@ def max(a, axis=None, out=None, keepdims=False, where=None):
 
 
 amax = max
+
+@to_ivy_arrays_and_back
+def nanmax(
+    a,
+    axis=None,
+    out=None,
+    keepdims=False,
+    initial=None,
+    where=True,
+):
+    nan_mask = ivy.isnan(a)
+    a = ivy.where(ivy.logical_not(nan_mask), a, a.full_like(-ivy.inf))
+    where_mask = None
+    if initial is not None:
+        if ivy.is_array(where):
+            a = ivy.where(where, a, a.full_like(initial))
+            where_mask = ivy.all(ivy.logical_not(where), axis=axis, keepdims=keepdims)
+        s = ivy.shape(a, as_array=True)
+        if axis is not None:
+            if isinstance(axis, (tuple, list)) or ivy.is_array(axis):
+                # introducing the initial in one dimension is enough
+                ax = axis[0] % len(s)
+                s[ax] = 1
+            else:
+                ax = axis % len(s)
+                s[ax] = 1
+        header = ivy.full(ivy.Shape(s.to_list()), initial, dtype=ivy.dtype(a))
+        if axis:
+            if isinstance(axis, (tuple, list)) or ivy.is_array(axis):
+                a = ivy.concat([a, header], axis=axis[0])
+            else:
+                a = ivy.concat([a, header], axis=axis)
+        else:
+            a = ivy.concat([a, header], axis=0)
+    res = ivy.max(a, axis=axis, keepdims=keepdims, out=out)
+    if nan_mask is not None:
+        nan_mask = ivy.all(nan_mask, axis=axis, keepdims=keepdims, out=out)
+        if ivy.any(nan_mask):
+            res = ivy.where(ivy.logical_not(nan_mask),
+                            res,
+                            initial if initial is not None else ivy.nan,
+                            out=out)
+    if where_mask is not None and ivy.any(where_mask):
+        res = ivy.where(ivy.logical_not(where_mask), res, ivy.nan, out=out)
+    return res
--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_statistical.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_statistical.py
@@ -1,5 +1,5 @@
 # global
-from hypothesis import strategies as st
+from hypothesis import strategies as st , assume
 import numpy as np
 
 # local
@@ -10,7 +10,7 @@ from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
     statistical_dtype_values,
     _get_castable_dtype,
 )
-
+from ivy import inf
 
 # einsum
 @handle_frontend_test(
@@ -484,5 +484,64 @@ def test_jax_numpy_max(
         axis=axis,
         out=None,
         keepdims=keepdims,
+        where=where,
+    )
+
+
+# nanmax
+@handle_frontend_test(
+    fn_tree="jax.numpy.nanmax",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
+        large_abs_safety_factor=2,
+        safety_factor_scale="log",
+        allow_nan=True,
+        allow_inf=True,
+    ),
+    initial=st.one_of(st.floats(min_value=-1000, max_value=1000), st.none()),
+    keepdims=st.booleans(),
+    where=np_helpers.where(),
+)
+def test_numpy_nanmax(
+    dtype_x_axis,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+    where,
+    initial,
+    keepdims,
+):
+    if initial is None and np.all(where) is not True:
+        assume(initial is -inf)
+
+    input_dtype, x, axis = dtype_x_axis
+    where, as_variable, native_array = np_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtype,
+        as_variable=as_variable,
+        native_array=native_array,
+    )
+    np_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=with_out,
+        all_aliases=["numpy.nanmax"],
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        a=x[0],
+        axis=axis,
+        out=None,
+        keepdims=keepdims,
+        initial=initial,
         where=where,
     )
