35e46e0a17a4f9e21da8233de1c66a61065d7d30
Nassim Berrada <112006029+nassimberrada@users.noreply.github.com>
2023-01-03 16:26:46 +0100

    jax_trace (#9346)
    Co-authored-by: nassimberrada <Nassim>

--- ivy/functional/frontends/jax/numpy/mathematical_functions.py
+++ ivy/functional/frontends/jax/numpy/mathematical_functions.py
@@ -303,3 +303,8 @@ def nextafter(x1, x2):
 @to_ivy_arrays_and_back
 def remainder(x1, x2):
     return ivy.remainder(x1, x2)
+
+
+@to_ivy_arrays_and_back
+def trace(a, offset=0, axis1=0, axis2=1, out=None):
+    return ivy.trace(a, offset=offset, axis1=axis1, axis2=axis2, out=out)
