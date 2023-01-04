71ba8597a7c04e919918b88ec2811d59c7f67cb1
hello-fri-end <shahanwaar.khalid8@gmail.com>
2023-01-04 11:55:58 +0530

    Even though we're making sure that the divisors are not 0 when they are generated but they can still become 0 after casting to a smaller dtype. Hence, added a try and except block to ignore examples causing ZeroDivisionError

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
@@ -649,29 +649,33 @@ def test_numpy_fmod(
     fn_tree,
     on_device,
 ):
-    input_dtypes, xs, casting, dtype = dtypes_values_casting
-    assume(not np.any(np.isclose(xs[1], 0.0)))
-    where, as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
-        where=where,
-        input_dtype=input_dtypes,
-        as_variable=as_variable,
-        native_array=native_array,
-    )
-    np_frontend_helpers.test_frontend_function(
-        input_dtypes=input_dtypes,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
-        frontend=frontend,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x1=xs[0],
-        x2=xs[1],
-        out=None,
-        where=where,
-        casting=casting,
-        order="K",
-        dtype=dtype,
-        subok=True,
-    )
+    try:
+        input_dtypes, xs, casting, dtype = dtypes_values_casting
+        assume(not np.any(np.isclose(xs[1], 0.0)))
+        where, as_variable, native_array = np_frontend_helpers.handle_where_and_array_bools(
+            where=where,
+            input_dtype=input_dtypes,
+            as_variable=as_variable,
+            native_array=native_array,
+        )
+        np_frontend_helpers.test_frontend_function(
+            input_dtypes=input_dtypes,
+            as_variable_flags=as_variable,
+            with_out=with_out,
+            num_positional_args=num_positional_args,
+            native_array_flags=native_array,
+            frontend=frontend,
+            fn_tree=fn_tree,
+            on_device=on_device,
+            x1=xs[0],
+            x2=xs[1],
+            out=None,
+            where=where,
+            casting=casting,
+            order="K",
+            dtype=dtype,
+            subok=True,
+        )
+
+    except ZeroDivisionError:
+        assume(False)
