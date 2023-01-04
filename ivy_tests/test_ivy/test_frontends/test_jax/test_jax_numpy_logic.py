4d46707f9ac705e685bdec32481412e9d99d1223
Saiteja Utpala <73220310+SaitejaUtpala@users.noreply.github.com>
2023-01-04 23:07:37 +0530

    add isinf and complete tests (#9386)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
@@ -662,3 +662,34 @@ def test_jax_numpy_logical_and(
         x1=x[0],
         x2=x[1],
     )
+
+
+# isinf
+@handle_frontend_test(
+    fn_tree="jax.numpy.isinf",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"), allow_inf=True
+    ),
+)
+def test_jax_numpy_isinf(
+    *,
+    dtype_and_x,
+    num_positional_args,
+    as_variable,
+    native_array,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
