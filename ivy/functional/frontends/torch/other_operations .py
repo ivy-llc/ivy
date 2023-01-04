ccb830b0e4525adc45d8840691982b2a694ffa6a
teckno <mwapebt@gmail.com>
2023-01-04 12:31:16 +0200

    Creating a compsotion function fot bincount

--- ivy/functional/frontends/torch/other_operations .py
+++ ivy/functional/frontends/torch/other_operations .py
@@ -1,4 +1,22 @@
 import ivy
 # function for bincount
+import ivy
+
 def bincount(input, weights=None, minlength=0):
-    return ivy.bincount(input, weights=weights, minlength=minlength)
+    # Convert the input array to a sequence of integer indices
+    indices = ivy.as_index(input)
+
+    # Create an array of weights, if necessary
+    if weights is None:
+        weights = ivy.ones_like(indices)
+    else:
+        weights = ivy.as_array(weights)
+
+    # Create an array of zeros with the desired minimum length
+    counts = ivy.zeros(minlength, dtype=weights.dtype)
+
+    # Use the add_at function to accumulate the weights into the counts array
+    ivy.add_at(counts, indices, weights)
+
+    return counts
+
