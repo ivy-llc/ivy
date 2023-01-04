0feba5dfed14d37ea23a84c4cbcdd21ca7dc6193
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2023-01-04 17:37:38 +0530

    Added complex dtype support back to ivy and also modified data generation and helpers for the same (#9377)

--- ivy/functional/backends/jax/__init__.py
+++ ivy/functional/backends/jax/__init__.py
@@ -88,6 +88,8 @@ valid_dtypes = (
     ivy.float16,
     ivy.float32,
     ivy.float64,
+    ivy.complex64,
+    ivy.complex128,
     ivy.bool,
 )
 valid_numeric_dtypes = (
@@ -119,7 +121,7 @@ valid_uint_dtypes = (ivy.uint8, ivy.uint16, ivy.uint32, ivy.uint64)
 valid_complex_dtypes = (ivy.complex64, ivy.complex128)
 
 # invalid data types
-invalid_dtypes = ()
+invalid_dtypes = (ivy.complex256,)
 invalid_numeric_dtypes = ()
 invalid_int_dtypes = ()
 invalid_float_dtypes = ()
