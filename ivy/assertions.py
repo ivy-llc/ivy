f9f8e1c4d9af8fab9efc606b26ac4b4ffc5d1f7b
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2023-01-04 17:37:38 +0530

    Added complex dtype support back to ivy and also modified data generation and helpers for the same (#9377)

--- ivy/assertions.py
+++ ivy/assertions.py
@@ -134,13 +134,19 @@ def check_shape(x1, x2, message=""):
 
 
 def check_fill_value_and_dtype_are_compatible(fill_value, dtype):
-    if not (
-        (ivy.is_int_dtype(dtype) or ivy.is_uint_dtype(dtype))
-        and isinstance(fill_value, int)
-    ) and not (
-        ivy.is_float_dtype(dtype)
-        and isinstance(fill_value, float)
-        or isinstance(fill_value, bool)
+    if (
+        not (
+            (ivy.is_int_dtype(dtype) or ivy.is_uint_dtype(dtype))
+            and isinstance(fill_value, int)
+        )
+        and not (
+            ivy.is_complex_dtype(dtype) and isinstance(fill_value, (float, complex))
+        )
+        and not (
+            ivy.is_float_dtype(dtype)
+            and isinstance(fill_value, float)
+            or isinstance(fill_value, bool)
+        )
     ):
         raise ivy.exceptions.IvyException(
             "the fill_value: {} and data type: {} are not compatible".format(
