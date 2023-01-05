8417617ef0a8bb02fe475e44d5003f8a632f17b7
Jigyasa-Kumari <62018816+Jigyasa-Kumari@users.noreply.github.com>
2023-01-05 04:05:40 -0800

    Update test_jax_numpy_linalg.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_linalg.py
@@ -873,8 +873,8 @@ def test_jax_numpy_tensorinv(
         available_dtypes=helpers.get_dtypes("float"),
         num_arrays=1,
         allow_nan=True
-    )
-    p=st.sampled_from([None, "fro", np.inf, -np.inf, 1, -1, 2, -2]),
+    ),
+    p=st.sampled_from([None, "fro", np.inf, -np.inf, 1, -1, 2, -2])
 )
 def test_jax_numpy_cond(
     *,
