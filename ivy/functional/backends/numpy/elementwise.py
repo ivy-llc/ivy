b3ef182d1dcf964923e62453cbc10a699aa4c44d
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-05 23:06:24 +0000

    Complex unsupported: `logaddexp` in numpy and torch, `log1p` in torch, `isinf`/`isnan` in tensorflow backend. Fixed wrong version for bitwise and less/equal functions.

--- ivy/functional/backends/numpy/elementwise.py
+++ ivy/functional/backends/numpy/elementwise.py
@@ -426,6 +426,7 @@ log2.support_native_out = True
 
 
 @_scalar_output_to_0d_array
+@with_unsupported_dtypes({"1.23.0 and below": ("complex",)}, backend_version)
 def logaddexp(
     x1: np.ndarray, x2: np.ndarray, /, *, out: Optional[np.ndarray] = None
 ) -> np.ndarray:
