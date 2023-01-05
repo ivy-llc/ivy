2eaf4723f79773fcd3457d05e422f87f2c5c7ddc
Tiago-B-C-Reis <tiago.b.c.reis@hotmail.com>
2023-01-05 20:47:19 +0000

    fix test_5

--- ivy/functional/frontends/numpy/statistics/order_statistics.py
+++ ivy/functional/frontends/numpy/statistics/order_statistics.py
@@ -1,35 +1,66 @@
 # global
+import numpy as np
+from numpy import function_base as fb
+
 import ivy.functional.frontends.numpy
+from ivy.func_wrapper import infer_dtype
 from ivy.functional.frontends.numpy import from_zero_dim_arrays_to_scalar
 from ivy.functional.frontends.numpy.func_wrapper import (
     to_ivy_arrays_and_back,
 )
 
 
+@infer_dtype
 @to_ivy_arrays_and_back
 @from_zero_dim_arrays_to_scalar
 def percentile(a,
                q,
-               /,
-               *,
                axis=None,
                out=None,
                overwrite_input=False,
                method="linear",
                keepdims=False,
+               *,
                interpolation=None):
-
-    # The only difference between the quantile and percentile
-    # formula it's that quantile has the 'q' in range 0 to 1
-    # and percentile has the 'q' from 0 to 100.
-    # Here it's used the ivy.quantile due to the fact that ivy.percentile
-    # doesn't exist, so in order to calculate the percentile,
-    # the line below converts ivy.quantile to percentile.
-    q /= 100
-
     axis = tuple(axis) if isinstance(axis, list) else axis
 
-    ret = ivy.quantile(a, q, axis=axis, keepdims=keepdims, out=out,
-                       interpolation=interpolation)
+    methods = ["inverted_cdf", "averaged_inverted_cdf",
+               "closest_observation", "interpolated_inverted_cdf",
+               "hazen", "weibull", "median_unbiased",
+               "normal_unbiased"]
+    interpolations = ["linear", "lower", "higher",
+                      "midpoint", "nearest"]
 
-    return ret
+    q = np.true_divide(q, 100)
+    # undo any decay that the ufunc performed (see gh-13105)
+    q = np.asanyarray(q)
+    if not fb._quantile_is_valid(q):
+        raise ValueError("Percentiles must be in the range [0, 100]")
+
+    if interpolation is None:
+        if method in methods:
+            arr = fb._quantile_unchecked(
+                a, q, axis, out, overwrite_input, method, keepdims)
+            ret = ivy.infer_dtype(arr)
+            return ret
+        elif method in interpolations:
+            arr = ivy.quantile(a, q, axis=axis, keepdims=keepdims,
+                               out=out, interpolation=interpolation)
+            ret = ivy.infer_dtype(arr)
+            return ret
+    elif interpolation is not None:
+        if interpolation in interpolations:
+            arr = ivy.quantile(a, q, axis=axis, keepdims=keepdims,
+                               out=out, interpolation=interpolation)
+            ret = ivy.infer_dtype(arr)
+            return ret
+        elif method in methods:
+            arr = fb._quantile_unchecked(
+                a, q, axis, out, overwrite_input, method, keepdims)
+            ret = ivy.infer_dtype(arr)
+            return ret
+        elif method in interpolations:
+            arr = ivy.quantile(a, q, axis=axis, keepdims=keepdims,
+                               out=out, interpolation=interpolation)
+            ret = ivy.infer_dtype(arr)
+            return ret
