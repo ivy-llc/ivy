3843dff46e34c4fd6788e00732f0b7d710e9f208
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-01-05 15:13:35 -0500

    Update math.py

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -158,8 +158,7 @@ def is_strictly_increasing(x, name="is_strictly_increasing"):
     if ivy.array(x).size == 2:
         return ivy.array(x[0] < x[1])
     for i in range((ivy.array(x).size) - 1):
-        y = ivy.greater_equal(ivy.array([x[i+1]]), ivy.array([x[i]])
-        if y == ivy.array([True]):
+        if ivy.greater_equal(ivy.array([x[i]]), ivy.array([x[i+1]]) == ivy.array([True]):
             return ivy.array(False)
     return ivy.array(True)
 
