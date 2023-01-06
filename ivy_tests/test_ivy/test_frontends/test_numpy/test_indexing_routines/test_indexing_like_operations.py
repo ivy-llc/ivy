05e5ad4fc8cadc6296db67c110174601abadc552
seancraven <seanpietercraven@gmail.com>
2023-01-06 12:39:57 +0000

    Passes Diag Test

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
@@ -52,6 +52,7 @@ def test_numpy_diagonal(
         available_dtypes=helpers.get_dtypes("numeric"),
         min_num_dims=1,
         max_num_dims=2,
+        min_dim_size=2
     ),
     k=st.integers(min_value=-1, max_value=1),
 )
@@ -66,13 +67,20 @@ def test_numpy_diag(
         on_device,
         ):
     input_dtype, x = dtype_and_x
+    dtype, input_dtype, casting = np_frontend_helpers.handle_dtype_and_casting(
+        dtypes=input_dtype,
+        get_dtypes_kind="numeric",
+    )
+
     np_frontend_helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
         on_device=on_device,
         frontend=frontend,
         fn_tree=fn_tree,
+        with_out=False,
         num_positional_args=num_positional_args,
         native_array_flags=native_array,
+        v=x[0],
         k=k,
     )
