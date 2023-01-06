33e20958018bad4dbfbe9236cb308c69c461907c
seancraven <seanpietercraven@gmail.com>
2023-01-06 13:41:13 +0000

    Implement diag_indices.

--- ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
+++ ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
@@ -1,5 +1,6 @@
 import ivy
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
+from ivy.functional.frontends.numpy.func_wrapper import outputs_to_numpy_arrays
 
 
 @to_ivy_arrays_and_back
@@ -10,3 +11,9 @@ def diagonal(a, offset, axis1, axis2):
 @to_ivy_arrays_and_back
 def diag(v, k=0):
     return ivy.diag(v, k=k)
+
+
+@outputs_to_numpy_arrays
+def diag_indices(n, ndim):
+    idx = ivy.arange(n)
+    return (idx,) * ndim
