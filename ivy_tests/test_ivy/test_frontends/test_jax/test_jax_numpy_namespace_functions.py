444e0d882db8383043b561ba1f86508257ff2608
ndurumo254 <erickndurumo@gmail.com>
2023-01-07 11:23:16 +0300

    created test for isfinite function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_namespace_functions.py
@@ -3484,3 +3484,27 @@ def test_jax_numpy_negative(
         on_device=on_device,
         x=x[0],
     )
+
+
+def test_jax_numpy_isfinite(
+        *,
+        dtype_and_x,
+        num_positional_args,
+        as_variable,
+        native_array,
+        on_device,
+        fn_tree,
+        frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
