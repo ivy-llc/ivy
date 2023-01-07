16aba3ea589983e1d5e190d5cd4a092d1f9d588c
Soumya Kundu <aymuos@soumyas-air.home>
2023-01-07 17:48:57 +0000

    cos function without idea file

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -2092,3 +2092,32 @@ def test_jax_numpy_fix(
         test_values=False,
         x=x[0],
     )
+
+
+#cos
+ @handle_frontend_test(
+     fn_tree="jax.numpy.cos",
+     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+ )
+ def test_jax_numpy_cos(
+     *,
+     dtype_and_x,
+     num_positional_args,
+     as_variable,
+     native_array,
+     on_device,
+     fn_tree,
+     frontend,
+ ):
+     input_dtype, x = dtype_and_x
+     helpers.test_frontend_function(
+         input_dtypes=input_dtype,
+         as_variable_flags=as_variable,
+         with_out=False,
+         num_positional_args=num_positional_args,
+         native_array_flags=native_array,
+         frontend=frontend,
+         fn_tree=fn_tree,
+         on_device=on_device,
+         x=x[0],
+     )
