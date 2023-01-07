61512f1fee7a5f530b08a40de69c529e31590b7c
hmahmood24 <hmahmood.bee16@gmail.com>
2023-01-07 19:41:55 +0000

    frontends.tf.raw_ops: add support for ConcatV2

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -506,3 +506,6 @@ def Xlogy(*, x, y, name="Xlogy"):
     if (x == 0).all():
         return 0.0
     return ivy.multiply(x, ivy.log(y))
+
+
+ConcatV2 = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.concat))
