c2806660e51073c2cc05a9ac72f49cd9553fc91e
mohamedbelja <mohamedbeljaa@gmail.com>
2023-01-08 18:16:40 +0100

    Update test_linalg.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
@@ -760,36 +760,36 @@ def test_torch_solve(
         other=other,
     )
 
-#eigvals
+
+# eigvals
 @handle_frontend_test(
-    fn_tree="torch.linalg.eigvals",
-    dtype_x=_get_dtype_and_square_matrix(),
-    
+fn_tree="torch.linalg.eigvals",
+dtype_and_x=helpers.dtype_and_values(
+    available_dtypes=helpers.get_dtypes("float"),
+    min_num_dims=0,
+    max_num_dims=2,
+        ),
 )
 def test_torch_eigvals(
-    *,
-    dtype_x,
-    num_positional_args,
-    as_variable,
-    native_array,
-    frontend,
-    fn_tree,
-    on_device,
-):
-    input_dtype, x = dtype_x
+        *,
+        dtype_and_x,
+        as_variable,
+        with_out,
+        num_positional_args,
+        native_array,
+        on_device,
+        fn_tree,
+        frontend,
+    ):
+    input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        native_array_flags=native_array,
         as_variable_flags=as_variable,
-        with_out=False,
+        with_out=with_out,
         num_positional_args=num_positional_args,
+        native_array_flags=native_array,
         frontend=frontend,
         fn_tree=fn_tree,
         on_device=on_device,
-        input=x,
+        input=x[0],
     )
-
-    
-
-
-    
