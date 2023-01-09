72f9b351993f6dce0dbe5cfae57f2fecd9c1fc1a
Fotios Spyridakos <72918194+fspyridakos@users.noreply.github.com>
2023-01-09 12:20:45 +0200

    Update statistical.py

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -4,9 +4,7 @@ from ivy.functional.frontends.jax.func_wrapper import (
     to_ivy_arrays_and_back,
     handle_jax_dtype
 )
-from ivy.functional.frontends.numpy.func_wrapper import handle_numpy_dtype
 from ivy.functional.frontends.jax.numpy import promote_types_of_jax_inputs
-
 
 
 @to_ivy_arrays_and_back
