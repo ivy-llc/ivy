59e838c073a9d579afa65662778882d4b7048f60
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2023-01-10 11:30:43 +0530

    Fix jax.lcm frontend test (#9475)
    jax.lcm: Added a condition to make sure that none of inputs are of type unint64 for numpy and jax. The reason for this is that uint64 combined with any other integer datatype gets type promoted to float which is an invalid datatype for this function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -1006,6 +1006,8 @@ def test_jax_numpy_lcm(
     # https://github.com/tensorflow/tensorflow/issues/58955
     if ivy.current_backend_str() == "tensorflow":
         value_test = False
+    if ivy.current_backend_str() in ("jax", "numpy"):
+        assume(input_dtype[0]!="uint64" and input_dtype[1]!="uint64")
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         as_variable_flags=as_variable,
