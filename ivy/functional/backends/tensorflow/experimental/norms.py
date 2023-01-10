9087f33c7c38061adda972442ec3c7e76c836e83
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-10 16:05:41 +0000

    making tf accord with pytorch's implementation

--- ivy/functional/backends/tensorflow/experimental/norms.py
+++ ivy/functional/backends/tensorflow/experimental/norms.py
@@ -6,5 +6,7 @@ def l2_normalize(x: Union[tf.Tensor, tf.Variable],
                  axis: int = None,
                  out=None
                  ) -> tf.Tensor:
-    return tf.nn.l2_normalize(x, axis=axis)
 
+    denorm = tf.norm(x, axis=axis, keepdims=True)
+    denorm = tf.math.maximum(denorm, 1e-12)
+    return tf.math.divide(x, denorm)
