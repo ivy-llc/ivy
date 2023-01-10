992209ac67efadac690378af3a1d00979a1a8022
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-10 17:48:53 +0000

    l2_normalize experimental function (#9555)
    Along with some modification to helper function arrays_and_axes without affecting its original functionality.
    
    tf implemented compositionally to math results with torch.. else tensorflow's tf.nn.l2_normalize gives different results for some examples.

--- ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
@@ -523,12 +523,15 @@ def array_indices_axis(
 @st.composite
 def arrays_and_axes(
     draw,
+    available_dtypes=get_dtypes("float"),
     allow_none=False,
-    min_num_dims=0,
+    min_num_dims=1,
     max_num_dims=5,
     min_dim_size=1,
     max_dim_size=10,
     num=2,
+    returndtype=False,
+    force_int_axis=False,
 ):
     shapes = list()
     for _ in range(num):
@@ -542,20 +545,37 @@ def arrays_and_axes(
             )
         )
         shapes.append(shape)
+    if isinstance(available_dtypes, st._internal.SearchStrategy):
+        available_dtypes = draw(available_dtypes)
+
+    dtype = draw(
+        dtype_helpers.array_dtypes(
+            num_arrays=num,
+            available_dtypes=available_dtypes
+        )
+    )
     arrays = list()
     for shape in shapes:
         arrays.append(
             draw(
-                array_values(dtype="int32", shape=shape, min_value=-200, max_value=200)
+                array_values(dtype=dtype[0], shape=shape, min_value=-20, max_value=20)
             )
         )
-    all_axes_ranges = list()
-    for shape in shapes:
-        if None in all_axes_ranges:
-            all_axes_ranges.append(st.integers(0, len(shape) - 1))
+    if force_int_axis:
+        if len(shape) <= 2:
+            axes = draw(st.one_of(st.integers(0, len(shape) - 1), st.none()))
         else:
-            all_axes_ranges.append(st.one_of(st.none(), st.integers(0, len(shape) - 1)))
-    axes = draw(st.tuples(*all_axes_ranges))
+            axes = draw(st.integers(0, len(shape) - 1))
+    else:
+        all_axes_ranges = list()
+        for shape in shapes:
+            if None in all_axes_ranges:
+                all_axes_ranges.append(st.integers(0, len(shape) - 1))
+            else:
+                all_axes_ranges.append(st.one_of(st.none(), st.integers(0, len(shape) - 1)))
+        axes = draw(st.tuples(*all_axes_ranges))
+    if returndtype:
+        return dtype, arrays, axes
     return arrays, axes
 
 
