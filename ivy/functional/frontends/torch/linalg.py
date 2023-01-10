05f85f95edf75a36ccfcb48c95438acf4d83affb
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-10 20:28:17 +0300

    remove redundancy

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -151,12 +151,8 @@ def tensorinv(input, ind=2, *, out=None):
     inverse_shape = shape_ind_start + shape_ind_end
     input = ivy.reshape(input, shape=(prod_ind_end, -1))
     inverse_shape_tuple = tuple([*inverse_shape])
-    if len(ivy.shape(input)) > 1:
-        assert inv_ex(input, check_errors=True), f'{not_invertible}.'
-        inverse_tensor = ivy.inv(input)
-    else:
-        ret = ivy.reshape(input, shape=inverse_shape_tuple, out=out)
-        return ret if ivy.inv(ret) else not_invertible
+    assert inv_ex(input, check_errors=True), f'{not_invertible}.'
+    inverse_tensor = ivy.inv(input)
     return ivy.reshape(inverse_tensor, shape=inverse_shape_tuple, out=out)
 
 
