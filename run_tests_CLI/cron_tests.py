2629ddd4fe30d97358bb75186998f950e0612897
RashulChutani <rashul.chutani@gmail.com>
2023-01-11 21:39:37 +0530

    Testing Cron Tests Workflow [skip ci]

--- run_tests_CLI/cron_tests.py
+++ run_tests_CLI/cron_tests.py
@@ -30,11 +30,13 @@ test_names.sort()
 
 # Run 150 tests in each iteration of the cron job
 num_tests = len(test_names)
-tests_per_run = 150
+tests_per_run = 10
 start = run_iter * tests_per_run
 end = (run_iter + 1) * tests_per_run
+print("Running Tests:")
 with open("tests_to_run", "w") as f:
     for i in range(start, end):
         i = i % num_tests
         test = test_names[i]
+        print(test)
         f.write(test + "\n")
