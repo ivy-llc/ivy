0033057a3b9cc6af408011e53ac7dd249e738915
seancraven <85280921+seancraven@users.noreply.github.com>
2023-01-11 16:59:14 +0000

    diag (#9453)
    Co-authored-by: Fotios Spyridakos <72918194+fspyridakos@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_indexing_routines/test_indexing_like_operations.py
@@ -45,3 +45,38 @@ def test_numpy_diagonal(
         axis1=axis[0],
         axis2=axis[1],
     )
+
+
+@handle_frontend_test(
+    fn_tree="numpy.diag",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=1,
+        max_num_dims=2,
+        min_dim_size=2,
+    ),
+    k=st.integers(min_value=-1, max_value=1),
+)
+def test_numpy_diag(
+    dtype_and_x,
+    k,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        on_device=on_device,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        v=x[0],
+        k=k,
+    )
