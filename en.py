3508000302a87785980ade2ac331f27f9ad331a6
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-12 12:00:21 +0000

    Delete en.py

--- en.py
+++
@@ -1,8 +0,0 @@
-import ivy
-for fw in ['tensorflow', 'torch', 'jax', 'numpy']:
-    ivy.set_backend(fw)
-    print(fw)
-    weights = ivy.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]])
-    indices = ivy.array([0, 2])
-    print(ivy.embedding(weights, indices, max_norm=2))
-    ivy.unset_backend()
