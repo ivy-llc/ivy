dc56c8ec4a3582a111eb813e223aac429ca34855
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-13 17:42:32 +0000

    Ensure exception is raised where the context managers suppresses it.

--- ivy/functional/ivy/gradients.py
+++ ivy/functional/ivy/gradients.py
@@ -310,6 +310,9 @@ class GradientTracking:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         unset_with_grads()
+        if self and (exc_type is not None):
+            print(exc_tb)
+            raise exc_val
         return self
 
 
