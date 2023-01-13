0be74a9074ed32ddc33e63ffdcfbf7dd65b0b8f7
degagawolde <degagawolde@gmail.com>
2023-01-13 21:39:18 +0300

    numpy shuffle added to the ivy frontend

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
@@ -152,3 +152,35 @@ def test_numpy_normal(
         scale=scale,
         size=size,
     )
+
+
+# shuffle
+@handle_frontend_test(
+    fn_tree="numpy.random.shuffle",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+)
+def test_numpy_shuffle(
+        dtype_and_x,
+        as_variable,
+        num_positional_args,
+        native_array,
+        frontend,
+        fn_tree,
+        on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=True,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        test_values=False,
+        on_device=on_device,
+        x=x,
+    )
+
