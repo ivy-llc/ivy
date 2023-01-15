280a6a54b6a1652c03c03a5de900f9732b0a9d76
Mustafa Hani <skytedits@gmail.com>
2023-01-15 07:07:07 +0200

    more numpy refacotr.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_norms_and_other_numbers.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_linear_algebra/test_norms_and_other_numbers.py
@@ -25,15 +25,14 @@ from ivy_tests.test_ivy.test_functional.test_core.test_linalg import (
     ),
     keepdims=st.booleans(),
     ord=st.sampled_from([None, "fro", "nuc", "inf", "-inf", 0, 1, -1, 2, -2]),
+    test_with_out=st.just(False),
 )
 def test_numpy_norm(
     dtype_values_axis,
     keepdims,
     ord,
-    as_variable,
-    num_positional_args,
-    native_array,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
@@ -42,11 +41,8 @@ def test_numpy_norm(
         axis = None
     helpers.test_frontend_function(
         input_dtypes=dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         x=x[0],
@@ -66,25 +62,21 @@ def test_numpy_norm(
         max_value=1e05,
     ),
     rtol=st.floats(allow_nan=False, allow_infinity=False) | st.just(None),
+    test_with_out=st.just(False),
 )
 def test_numpy_matrix_rank(
     dtype_and_x,
     rtol,
-    as_variable,
-    num_positional_args,
-    native_array,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         A=x[0],
@@ -96,24 +88,20 @@ def test_numpy_matrix_rank(
 @handle_frontend_test(
     fn_tree="numpy.linalg.det",
     dtype_and_x=_get_dtype_and_matrix(),
+    test_with_out=st.just(False),
 )
 def test_numpy_det(
     dtype_and_x,
-    as_variable,
-    num_positional_args,
-    native_array,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         rtol=1e-2,
@@ -136,13 +124,12 @@ def test_numpy_det(
         num_arrays=1,
         safety_factor_scale="log",
     ),
+    test_with_out=st.just(False),
 )
 def test_numpy_slogdet(
     dtype_and_x,
-    as_variable,
-    num_positional_args,
-    native_array,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
@@ -150,11 +137,8 @@ def test_numpy_slogdet(
     assume(matrix_is_stable(x[0]))
     ret, ret_gt = helpers.test_frontend_function(
         input_dtypes=dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         a=x[0],
@@ -177,24 +161,20 @@ def test_numpy_slogdet(
 @handle_frontend_test(
     fn_tree="numpy.trace",
     dtype_and_x=_get_dtype_and_matrix(),
+    test_with_out=st.just(False),
 )
 def test_numpy_trace(
     dtype_and_x,
-    as_variable,
-    num_positional_args,
-    native_array,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
     dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         a=x[0],
