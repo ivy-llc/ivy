138846c8efd1130d457faba8a11f6c08d1e31307
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-16 11:41:55 +0000

    [Frontend] Tensorflow floor function (#9709)
    * add the test first
    
    * add the function

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1393,3 +1393,38 @@ def test_tensorflow_equal(
         x=x[0],
         y=x[1],
     )
+
+
+# floor
+@handle_frontend_test(
+    fn_tree="tensorflow.math.floor",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=1,
+        min_value=-20,
+        max_value=20
+    ),
+)
+def test_tensorflow_floor(
+    *,
+    dtype_and_x,
+    num_positional_args,
+    as_variable,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0]
+    )
+
