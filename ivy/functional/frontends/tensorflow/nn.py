40d6d000918c6611c0489f8da75974793a3fc14b
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-01-17 17:09:54 +0000

    first attempt to add fused_batch_norm

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -150,6 +150,58 @@ def batch_normalization(x, mean, variance, offset, scale, variance_epsilon, name
 
 
 @to_ivy_arrays_and_back
+@with_supported_dtypes(
+    {"2.9.0 and below": ("float32", "float16", "bfloat16")}, "tensorflow"
+)
+def fused_batch_norm(
+    x,
+    scale,
+    offset,
+    mean=None,
+    variance=None,
+    epsilon=0.001,
+    data_format='NHWC',
+    is_training=True,
+    name=None,
+    exponential_avg_factor=1.0,
+):
+    dims = len(x.shape)
+    if data_format[1] == "C":
+        if dims == 4:
+            x = ivy.permute_dims(x, axes=(0, 2, 3, 1))
+        elif dims == 5:
+            x = ivy.permute_dims(x, axes=(0, 2, 3, 4, 1))
+        else:
+            raise ivy.exceptions.IvyException(
+                "input tensor must be of 4 or 5 dimensions, got {}".format(dims)
+            )
+    if is_training:
+        x_mean = ivy.mean(x, axis=-1)
+        x_var = ivy.var(x, axis=-1)
+        x_norm = scale * (x - x_mean) / ivy.sqrt(x_var + epsilon) + offset
+    else:
+        # dtype = ivy.promote_types(x, mean)
+        x_norm = scale * (x - mean) / ivy.sqrt(variance + epsilon) + offset
+    if data_format[1] == "C":
+        if dims == 4:
+            x_norm = ivy.permute_dims(x_norm, axes=(0, 3, 1, 2))
+        elif dims == 5:
+            x_norm = ivy.permute_dims(x_norm, axes=(0, 4, 1, 2, 3))
+    if is_training:
+        if exponential_avg_factor == 1.0:
+            moving_mean = x_mean
+            moving_var = x_var
+        else:
+            moving_mean = x_mean * exponential_avg_factor + mean * (
+                        1 - exponential_avg_factor)
+            moving_var = x_var * exponential_avg_factor + variance * (
+                        1 - exponential_avg_factor)
+        return x_norm, moving_mean, moving_var
+    else:
+        return x_norm, mean, variance
+
+
+@to_ivy_arrays_and_back
 def dropout(x, prob, scale, dtype, name=None):
     return ivy.dropout(x, prob, scale, dtype)
 
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -1,6 +1,6 @@
 # global
 import ivy
-from hypothesis import strategies as st
+from hypothesis import strategies as st, reproduce_failure
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
@@ -707,6 +707,72 @@ def test_tensorflow_batch_normalization(
         offset=offset[0],
         scale=scale[0],
         variance_epsilon=1e-7,
+    )
+
+
+@st.composite
+def _batch_norm_helper(draw):
+    num_dims = draw(st.integers(min_value=4, max_value=5))
+    dtype, x = draw(helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=num_dims,
+        max_num_dims=num_dims,
+        min_value=-1e04,
+        max_value=1e04,
+    ))
+    epsilon = draw(st.floats(min_value=1e-07, max_value=1e-02))
+    factor = draw(st.floats(min_value=0, max_value=1, exclude_min=True))
+    training = draw(st.booleans())
+    if num_dims == 4:
+        data_format = draw(st.sampled_from(["NHWC", "NCHW"]))
+    else:
+        data_format = draw(st.sampled_from(["NDHWC", "NCDHW"]))
+    num_channels = x[0].shape[data_format.rfind("C")]
+    dtypes, vectors = draw(helpers.dtype_and_values(
+        available_dtypes=["float32"],
+        shape=(num_channels,),
+        num_arrays=4,
+        min_value=-1e02,
+        max_value=1e02,
+    ))
+    vectors[3] = ivy.abs(vectors[3])  # non-negative variance
+    return dtype + dtypes, x, epsilon, factor, training, data_format, vectors
+
+
+@handle_frontend_test(
+    fn_tree="tensorflow.nn.fused_batch_norm",
+    dtypes_args=_batch_norm_helper(),
+)
+def test_tensorflow_fused_batch_norm(
+    *,
+    dtypes_args,
+    as_variable,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    dtypes, x, epsilon, factor, training, data_format, vectors = dtypes_args
+    helpers.test_frontend_function(
+        input_dtypes=dtypes,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        rtol=0.1,
+        x=x[0],
+        scale=vectors[0],
+        offset=vectors[1],
+        mean=vectors[2],
+        variance=vectors[3],
+        epsilon=epsilon,
+        data_format=data_format,
+        is_training=training,
+        exponential_avg_factor=factor,
     )
 
 
