fed565ee4b9c575411ce53d2dca86f627bbd786d
Ngo Viet Hoai Bao <xoiga123@outlook.com.vn>
2023-01-17 16:29:30 +0700

    add typehint for output_shape and dilations in conv2d_transpose (#9718)
    Co-authored-by: Aditya Hari <adityahari3791@gmail.com>

--- ivy/functional/backends/torch/layers.py
+++ ivy/functional/backends/torch/layers.py
@@ -191,7 +191,7 @@ def conv2d_transpose(
     padding: str,
     /,
     *,
-    output_shape=None,
+    output_shape: Tuple[int] = None,
     data_format: str = "NHWC",
     dilations: Union[int, Tuple[int, int]] = 1,
     out: Optional[torch.Tensor] = None,
