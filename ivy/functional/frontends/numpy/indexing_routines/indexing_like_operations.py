1bafc9f1b9671931ab00c39fce080771b6fb4e19
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-01-18 23:37:10 +0000

    Cleanup and lint fix

--- ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
+++ ivy/functional/frontends/numpy/indexing_routines/indexing_like_operations.py
@@ -9,10 +9,12 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 def diagonal(a, offset, axis1, axis2):
     return ivy.diagonal(a, offset=offset, axis1=axis1, axis2=axis2)
 
+
 @outputs_to_numpy_arrays
 def diag_indices(n, ndim=2):
     idx = ivy.arange(n, dtype=int)
     return (idx,) * ndim
+
 
 @to_ivy_arrays_and_back
 def diag(v, k=0):
