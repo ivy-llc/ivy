cf2c132ece74ade68307ec72b67386c3314c3729
karalleyna <aleynakara98@gmail.com>
2023-01-18 17:10:20 +0100

    Reformat searching_sorting.py

--- ivy/functional/frontends/jax/numpy/searching_sorting.py
+++ ivy/functional/frontends/jax/numpy/searching_sorting.py
@@ -8,6 +8,7 @@ from ivy.functional.frontends.jax.func_wrapper import (
 )
 from ivy.functional.frontends.numpy.func_wrapper import from_zero_dim_arrays_to_scalar
 
+
 @to_ivy_arrays_and_back
 def argmax(a, axis=None, out=None, keepdims=False):
     return ivy.argmax(a, axis=axis, keepdims=keepdims, out=out)
@@ -56,11 +57,13 @@ def nonzero(a, *, size=None, fill_value=None):
 @from_zero_dim_arrays_to_scalar
 def nanargmax(a, /, *, axis=None, out=None, keepdims=False):
     if out is not None:
-        raise NotImplementedError("The 'out' argument to jnp.nanargmax is not supported.")
+        raise NotImplementedError(
+            "The 'out' argument to jnp.nanargmax is not supported."
+        )
     nan_mask = ivy.isnan(a)
     if not ivy.any(nan_mask):
         return ivy.argmax(a, axis=axis, keepdims=keepdims)
-    
+
     a = ivy.where(nan_mask, -ivy.inf, a)
     res = ivy.argmax(a, axis=axis, keepdims=keepdims)
     return ivy.where(ivy.all(nan_mask, axis=axis, keepdims=keepdims), -1, res)
