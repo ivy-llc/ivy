2a13e0912deb61a129fe2903ad2194280bd2bdba
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-01-19 10:43:39 +0000

    dtype casting

--- ivy/functional/frontends/tensorflow/compat/v1/nn.py
+++ ivy/functional/frontends/tensorflow/compat/v1/nn.py
@@ -36,8 +36,8 @@ def fused_batch_norm(
         x_var = ivy.var(x, axis=dims_to_avg)
         x_norm = scale * (x - x_mean) / ivy.sqrt(x_var + epsilon) + offset
     else:
-        # dtype = ivy.promote_types(x, mean)
         x_norm = scale * (x - mean) / ivy.sqrt(variance + epsilon) + offset
+    x_norm = ivy.array(x_norm, dtype=x.dtype)
     if data_format[1] == "C":
         if dims == 4:
             x_norm = ivy.permute_dims(x_norm, axes=(0, 3, 1, 2))
@@ -52,7 +52,7 @@ def fused_batch_norm(
                         1 - exponential_avg_factor)
             moving_var = x_var * exponential_avg_factor + variance * (
                         1 - exponential_avg_factor)
-        return x_norm, moving_mean, moving_var
+        return x_norm, ivy.astype(moving_mean, 'float32'), ivy.astype(moving_var, 'float32')
     else:
         return x_norm, mean, variance
     
