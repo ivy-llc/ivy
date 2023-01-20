2f33208b5ac0420de5f03c1dc2667901aaa31972
Khushiyant <khushiyant2002@gmail.com>
2023-01-20 23:13:20 +0530

    modify: Raw Ops: Rsqrt : reciprocal method

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -245,7 +245,7 @@ def MatMul(*, a, b, transpose_a=False, transpose_b=False, name="MatMul"):
 
 @to_ivy_arrays_and_back
 def Rsqrt(*, x, name="Rsqrt"):
-    return 1 / ivy.sqrt(x)
+    return ivy.sqrt(ivy.reciprocal(x))
 
 
 @to_ivy_arrays_and_back
