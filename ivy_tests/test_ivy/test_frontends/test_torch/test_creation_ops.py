873facd295344f8362009c679910c494772201ee
ahmed el-reweidy <64925287+elreweidy@users.noreply.github.com>
2023-01-21 23:19:16 +0200

    modify test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
@@ -525,21 +525,15 @@ def _heaviside_helper(draw):
 def test_torch_heaviside(
     *,
     dtype_and_input,
-    as_variable,
-    num_positional_args,
-    native_array,
+    test_flags,
     fn_tree,
     on_device,
-    with_out,
     frontend,
 ):
     input_dtype, data, values = dtype_and_input
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
+        test_flags=test_flags,
         frontend=frontend,
         fn_tree=fn_tree,
         input=data[0],
