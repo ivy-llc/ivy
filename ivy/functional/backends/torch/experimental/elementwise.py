54c5fb7f4344dc36ee21cf62bcc95d27432af990
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2023-01-22 21:02:36 +0000

    update implementations of zeta for torch so it has same behavior as tensorflow version

--- ivy/functional/backends/torch/experimental/elementwise.py
+++ ivy/functional/backends/torch/experimental/elementwise.py
@@ -385,10 +385,15 @@ def zeta(
     *,
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
-    return torch.special.zeta(x, q, out=out)
+    temp = torch.logical_and(torch.ne(torch.remainder(x, 2), 0), torch.ge(x, 1))
+    temp = torch.logical_and(temp, torch.le(q, 0))
+    nan_indices = torch.logical_or(temp, torch.lt(x, 1))
+    result = torch.special.zeta(x, q)
+    result.masked_fill_(nan_indices, float('nan'))
+    return result
 
 
-zeta.support_native_out = True
+zeta.support_native_out = false
 
 
 def gradient(
