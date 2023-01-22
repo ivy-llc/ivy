e5a6448f9b3e43f223f2551cc06feb045ab83b83
Aarsh2001 <ac5973@srmist.edu.in>
2023-01-22 13:38:48 +0530

    added DFT to experimental API

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
@@ -421,3 +421,45 @@ def test_embedding(
         indices=indices,
         max_norm=max_norm,
     )
+
+
+@handle_test(
+    fn_tree="functional.ivy.experimental.dft",
+    d_xfft_axis_n_length=x_and_fft(helpers.get_dtypes("complex")),
+    d_xifft_axis_n_length=x_and_ifft(),
+    inverse=st.booleans(),
+    onesided=st.booleans(),
+)
+def test_dft(
+    *,
+    d_xfft_axis_n_length,
+    d_xifft_axis_n_length,
+    inverse,
+    onesided,
+    test_flags,
+    backend_fw,
+    fn_name,
+    ground_truth_backend,
+):
+    print(fn_name)
+    if inverse:
+        dtype, x, axis, norm, dft_length = d_xifft_axis_n_length
+    else:
+        dtype, x, axis, norm, dft_length = d_xfft_axis_n_length
+
+    helpers.test_function(
+        ground_truth_backend=ground_truth_backend,
+        input_dtypes=dtype,
+        test_flags=test_flags,
+        fw=backend_fw,
+        fn_name=fn_name,
+        rtol_=1e-2,
+        atol_=1e-2,
+        test_gradients=False,
+        x=x,
+        axis=axis,
+        inverse=inverse,
+        onesided=onesided,
+        dft_length=dft_length,
+        norm=norm,
+    )
