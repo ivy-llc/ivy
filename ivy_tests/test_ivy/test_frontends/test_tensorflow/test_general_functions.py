48dade2762717fd7388b22cc9cb6af71c112372f
Shreya <70858293+alt-shreya@users.noreply.github.com>
2023-01-23 18:48:40 +0530

    fix(test): fixed dimension error

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -1169,15 +1169,15 @@ def _multiple_shape_helper(draw):
                               "bfloat16", "bfloat32"],
             ret_shape=True
         ))
-    input_dims = len(input_shape)
+    input_dims = abs(len(input_shape))
 
     dt_n_multiples = draw(
         helpers.dtype_and_values(
             available_dtypes=["int32", "int64"],
             max_value=10,
             shape=draw(helpers.get_shape(min_num_dims=1, max_num_dims=1,
-                                         min_dim_size=abs(input_dims),
-                                         max_dim_size=abs(input_dims))),
+                                         min_dim_size=input_dims,
+                                         max_dim_size=input_dims)),
         )
     )
     return input_dtype, input_array, dt_n_multiples
