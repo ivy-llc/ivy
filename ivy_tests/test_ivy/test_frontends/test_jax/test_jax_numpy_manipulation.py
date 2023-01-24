347e29721f8bfc4f9ebbd244c00a1058a04fb4fe
Nunyapa <halo.cola.115@gmail.com>
2023-01-24 17:48:51 +0900

    changed test_flags in test function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_manipulation.py
@@ -657,7 +657,7 @@ def test_jax_numpy_atleast_3d(
         on_device=on_device,
         **arys,
     )
-    
+
 
 # atleast_2d
 @handle_frontend_test(
@@ -669,12 +669,12 @@ def test_jax_numpy_atleast_3d(
     test_with_out=st.just(False),
 )
 def test_jax_numpy_atleast_2d(
-        *,
-        dtype_and_x,
-        on_device,
-        fn_tree,
-        frontend,
-        test_flags,
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
 ):
     input_dtype, arrays = dtype_and_x
     arys = {}
@@ -698,28 +698,25 @@ def test_jax_numpy_atleast_2d(
         available_dtypes=helpers.get_dtypes("valid"),
         num_arrays=helpers.ints(min_value=1, max_value=10),
     ),
+    test_with_out=st.just(False),
 )
 def test_jax_numpy_atleast_1d(
     *,
     dtype_and_x,
-    as_variable,
-    native_array,
     on_device,
     fn_tree,
     frontend,
+    test_flags,
 ):
     input_dtype, arrays = dtype_and_x
     arys = {}
     for i, (array, idtype) in enumerate(zip(arrays, input_dtype)):
         arys["arrs{}".format(i)] = np.asarray(array, dtype=idtype)
-    num_positional_args = len(arys)
+    test_flags.num_positional_args = len(arys)
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         **arys,
