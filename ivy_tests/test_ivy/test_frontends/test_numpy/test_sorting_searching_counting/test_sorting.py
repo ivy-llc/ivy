fa8abdeee109ef804238ebae0d6bf6f6f15e0667
Nadide Beyza Dokur <nadidebeyzadokur@gmail.com>
2023-01-26 22:21:52 +0300

    lint fixes

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
@@ -130,7 +130,8 @@ def test_numpy_sort_complex(
         test_values=False,
     )
 
-#All backends need to be implemented for this function.
+
+# All backends need to be implemented for this function.
 @handle_frontend_test(
     fn_tree="numpy.lexsort",
     dtype_x_axis=helpers.dtype_values_axis(
