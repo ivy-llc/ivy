46685dd032e2b78080934b7d5d30ecf7268e1b2a
Norman Mico <normanrmico@gmail.com>
2023-01-26 13:07:42 +0200

    resetting...

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -338,9 +338,9 @@ def RealDiv(*, x, y, name="RealDiv"):
     return ivy.divide(x, y)
 
 
-Reshape = to_ivy_arrays_and_back(
-    map_raw_ops_alias(tf_frontend.general_functions.reshape)
-)
+@to_ivy_arrays_and_back
+def Reshape(*, tensor, shape, name="Reshape"):
+    return ivy.reshape(tensor, shape)
 
 
 @to_ivy_arrays_and_back
