63ed6e3d54ba5fae439d142cf06983c87b9929cf
Akoji Timothy <46170255+CodeMaestro1000@users.noreply.github.com>
2023-01-26 21:50:32 +0000

    Test file for ifft function in numpy frontend

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -0,0 +1,30 @@
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+@handle_frontend_test(
+    fn_tree="numpy.fft.ifft",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    )
+)
+def test_numpy_iftt(
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        a=x,
+        n=None,
+        axis=-1,
+        norm=None
+    )
