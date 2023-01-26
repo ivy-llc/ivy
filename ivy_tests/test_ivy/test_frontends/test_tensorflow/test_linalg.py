92feed25efa165941be07d3df1452f3147a6530a
MEHDI-Ibrahim <98242475+MEHDI-Ibrahim@users.noreply.github.com>
2023-01-26 01:17:28 +0100

    added tf.linalg.matmul test function

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -637,3 +637,40 @@ def test_tensorflow_linalg_cholesky(
         rtol=1e-02,
         input=x,
     )
+#matmul
+@handle_frontend_test(
+    fn_tree="tensorflow.linalg.matmul",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        min_value=0,
+        max_value=10,
+        shared_dtype=True,
+        min_num_dims=2,
+        max_num_dims=2,
+    ),
+)
+def test_tensorflow_matmul(
+    *,
+    dtype_and_x,
+    num_positional_args,
+    as_variable,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    x1, x2 = x[0], x[1].T
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=[True],
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        a=x1,
+        b=x2,
+    )
