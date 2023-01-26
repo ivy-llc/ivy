2e852e0b794b3a99169561facc26807b1ad6e1e9
Elvis Rari <46717578+rarisenpai@users.noreply.github.com>
2023-01-26 19:10:31 +0000

    run pre-commit to fix

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -1323,16 +1323,17 @@ def test_numpy_instance_mul__(
         frontend_method_data=frontend_method_data,
     )
 
+
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="numpy.array",
-    method_name="__floordiv__",
+    method_name="__truediv__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
         num_arrays=2,
     ),
 )
-def test_numpy_instance_floordiv__(
+def test_numpy_instance_truediv__(
     dtype_and_x,
     as_variable: pf.AsVariableFlags,
     native_array: pf.NativeArrayFlags,
@@ -1363,16 +1364,17 @@ def test_numpy_instance_floordiv__(
         frontend=frontend,
     )
 
+
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="numpy.array",
-    method_name="__truediv__",
+    method_name="__floordiv__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
         num_arrays=2,
     ),
 )
-def test_numpy_instance_truediv__(
+def test_numpy_instance_floordiv__(
     dtype_and_x,
     as_variable: pf.AsVariableFlags,
     native_array: pf.NativeArrayFlags,
