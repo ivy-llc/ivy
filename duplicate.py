cb8d9497a3db55bd8a31cffeba8b0e6fa38dee82
zaeemansari70 <zaeemansari70@gmail.com>
2023-01-26 23:44:31 +0500

    removing lint errors

--- duplicate.py
+++ duplicate.py
@@ -14,17 +14,23 @@ def get_all_functions_from_directory(root_dir, startswith='test'):
         filename = filename.replace('.py', '')
         filename = filename.replace('/', '.')
         module = importlib.import_module(filename)
-        module_functions_names = [obj for obj in dir(module) if obj.startswith(startswith)]
+        module_functions_names = \
+            [obj for obj in dir(module) if obj.startswith(startswith)]
         functions_names.extend(module_functions_names)
     return functions_names
 
+
 def check_duplicate():
-    fn_test_core = get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_core')
-    fn_test_nn = get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_nn')
-    fn_test_experimental = get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_experimental')
+    fn_test_core = \
+        get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_core')
+    fn_test_nn = \
+        get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_nn')
+    fn_test_experimental = \
+        get_all_functions_from_directory('ivy_tests/test_ivy/test_functional/test_experimental')
     fn_ivy_test = set(fn_test_core).union(set(fn_test_nn))
     common_list = fn_ivy_test.intersection(set(fn_test_experimental))
     return common_list
+
 
 if __name__ == "__main__":
     common_set = check_duplicate()
