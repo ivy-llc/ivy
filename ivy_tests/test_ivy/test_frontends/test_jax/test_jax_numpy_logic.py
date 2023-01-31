a1b08514b0fba0dbe3d9be8c08cbac103f0b4581
MustafaDahshory <94312226+MustafaDahshory@users.noreply.github.com>
2023-01-31 03:52:07 +0200

    Update test_jax_numpy_logic.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
@@ -664,7 +664,7 @@ def test_jax_numpy_isclose(
     )
 
 
-# logical_not
+#logical_not
 @handle_frontend_test(
     fn_tree="jax.numpy.logical_not",
     dtypes_values=helpers.dtype_and_values(
@@ -674,24 +674,17 @@ def test_jax_numpy_isclose(
 )
 def test_jax_numpy_logical_not(
     dtypes_values,
-    with_out,
-    as_variable,
-    num_positional_args,
-    native_array,
     on_device,
     fn_tree,
     frontend,
+    test_flags,
 ):
     x_dtypes, x = dtypes_values
     np_helpers.test_frontend_function(
         input_dtypes=x_dtypes,
-        as_variable_flags=as_variable,
-        with_out=with_out,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         x=x[0],
     )
-
