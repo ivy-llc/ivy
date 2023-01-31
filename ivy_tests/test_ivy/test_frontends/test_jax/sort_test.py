5e539b82adf8ad504711cc3264a43afc3b805658
yumnaAlshalak <62077473+yumnaAlshalak@users.noreply.github.com>
2023-01-31 21:14:33 +0300

    Update sort_test.py

--- ivy_tests/test_ivy/test_frontends/test_jax/sort_test.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/sort_test.py
@@ -20,15 +20,15 @@ def test_numpy_sort(
     on_device,
     test_flags,
 ):
-    input_dtype, x, axis= dtype_x_axis
+    input_dtype, x= dtype_x_axis
 
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        with_out=False,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         a=x[0],
-        axis=axis
+        rtol=1e-03,
+        atol=1e-02,
     )
