a391449460a270073dcd4501df67fce0a7975b45
glasalm <122786162+glasalm@users.noreply.github.com>
2023-02-02 00:44:16 +0600

    add fabs jax implementation (#9708)
    Co-authored-by: ZoeCD <zoe.cd@outlook.com>,

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -1489,6 +1489,32 @@ def test_jax_numpy_fmin(
     )
 
 
+# fabs
+@handle_frontend_test(
+    fn_tree="jax.numpy.fabs",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_jax_numpy_fabs(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
 # fmod
 @handle_frontend_test(
     fn_tree="jax.numpy.fmod",
