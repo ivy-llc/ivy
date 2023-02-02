7d0eda4f06fa4ab48d461d006116c2c40f1a24ea
Jin Wang <jin.wang-12@student.manchester.ac.uk>
2023-02-02 16:09:42 +0000

    try to achieve stable argsort on torch with older version(1.10)

--- ivy/functional/backends/torch/experimental/sorting.py
+++ ivy/functional/backends/torch/experimental/sorting.py
@@ -23,13 +23,15 @@ def lexsort(
     size = keys.size(dim=0)
     if size == 0:
         raise TypeError('need sequence of keys with len > 0 in lexsort')
-    result = torch.argsort(keys[0], dim=axis, stable=True)
+    _, result = torch.sort(keys[0], dim=axis, stable=True)
+    # result = torch.argsort(keys[0], dim=axis, stable=True)
     if size == 1:
         return result
     for i in range(1, size):
         key = keys[i]
         ind = key[result]
-        temp = torch.argsort(ind, dim=axis, stable=True)
+        _, temp = torch.sort(ind, dim=axis, stable=True)
+        # temp = torch.argsort(ind, dim=axis, stable=True)
         result = result[temp]
     return result
 
