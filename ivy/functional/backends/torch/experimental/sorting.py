cd317c2724f6dc4c94a99a35e5a823c9a19eb714
Jin Wang <jin.wang-12@student.manchester.ac.uk>
2023-02-03 16:22:19 +0000

    lint fix

--- ivy/functional/backends/torch/experimental/sorting.py
+++ ivy/functional/backends/torch/experimental/sorting.py
@@ -24,14 +24,16 @@ def lexsort(
     if size == 0:
         raise TypeError('need sequence of keys with len > 0 in lexsort')
     _, result = torch.sort(keys[0], dim=axis, stable=True)
-    # result = torch.argsort(keys[0], dim=axis, stable=True) # only valid for torch > 1.12.0
+    # result = torch.argsort(keys[0], dim=axis, stable=True)
+    # only valid for torch > 1.12.0
     if size == 1:
         return result
     for i in range(1, size):
         key = keys[i]
         ind = key[result]
         _, temp = torch.sort(ind, dim=axis, stable=True)
-        # temp = torch.argsort(ind, dim=axis, stable=True) # only valid for torch > 1.12.0
+        # temp = torch.argsort(ind, dim=axis, stable=True)
+        # only valid for torch > 1.12.0
         result = result[temp]
     return result
 
