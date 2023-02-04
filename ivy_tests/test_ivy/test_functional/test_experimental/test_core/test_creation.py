4eaeb6b2caf71b526002e869f2793a705d5517a7
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-02-04 11:45:47 -0500

    Update test_creation.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_creation.py
@@ -339,3 +339,11 @@ def test_sequence_empty(
         test_values=False,
         ground_truth_backend=ground_truth_backend
     )
+    if not ivy.exists(ret):
+        return
+    res, res_np = ret
+    ivy.set_backend("tensorflow")
+    assert res.shape == res_np.shape
+    assert res.dtype == res_np.dtype
+    ivy.unset_backend()
+    
