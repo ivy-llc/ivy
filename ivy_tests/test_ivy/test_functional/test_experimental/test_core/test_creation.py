95925c0aa6e9037f9365b9abe55f8c8f5d67b04c
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-02-04 16:29:34 -0500

    Update test_creation.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_creation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_creation.py
@@ -306,11 +306,7 @@ def test_eye_like(
 @handle_test(
     fn_tree="functional.ivy.sequence_empty",
     shape=helpers.get_shape(
-        allow_none=False,
-        min_num_dims=1,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=5
+        allow_none=False
     ),
     dtype=helpers.get_dtypes("float", full=False),
     container_flags=st.just([False]),
@@ -337,5 +333,4 @@ def test_sequence_empty(
         device=on_device,
         ground_truth_backend=ground_truth_backend
     )
-
     
