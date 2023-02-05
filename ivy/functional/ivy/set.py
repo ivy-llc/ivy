3051eef6e4c9524642e932d777e34ca1267b0d1d
Charis Koutsis <xariskoutsis@gmail.com>
2023-02-05 16:14:48 +0200

    Update set.py

--- ivy/functional/ivy/set.py
+++ ivy/functional/ivy/set.py
@@ -294,7 +294,7 @@ def unique_values(
     instances in place of any of the arguments.
 
     """
-    return ivy.current_backend(x).unique_values(x, out=out) 
+    return ivy.current_backend(x).unique_values(x) 
 
 
 @to_native_arrays_and_back
