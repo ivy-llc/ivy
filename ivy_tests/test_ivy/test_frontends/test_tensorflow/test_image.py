2dea0c6ca570f8916f191c426d88822a038ca362
mitanshudodia <80592354+mitanshudodia@users.noreply.github.com>
2023-02-06 20:07:25 +0530

    Add files via upload

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_image.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_image.py
@@ -1,12 +1,9 @@
 from hypothesis import strategies as st
-
-# local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
-
 @handle_frontend_test(
-    fn_tree="tensorflow.image.resize",
+    fn_tree="tensorflow.image.resize", 
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("valid"),
     ),
@@ -17,6 +14,7 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 def test_tensorflow_image_resize(
     *,
     dtype_and_x,
+    size,
     expand_composite,
     use_array,
     frontend,
