be88ad1039a1bf0f8fb1ed6603e71df2c543d4c0
Eddiebee <edbeeotudor@gmail.com>
2023-02-06 11:12:24 +0100

    Implement shuffle random function for Tensorflow frontend

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -19,3 +19,13 @@ def uniform(shape, minval=0, maxval=None, dtype=ivy.float32, seed=None, name=Non
 @to_ivy_arrays_and_back
 def normal(shape, mean=0.0, stddev=1.0, dtype=ivy.float32, seed=None, name=None):
     return ivy.random_normal(mean=mean, std=stddev, shape=shape, dtype=dtype, seed=seed)
+
+
+# implement random shuffle
+@with_unsupported_dtypes(
+    {"2.9.0 and below": ("int8", "int16", "in32", "int64", "unsigned")}, "tensorflow"
+)
+@to_ivy_arrays_and_back
+def normal(value, seed=None, name=None):
+    return ivy.shuffle(x=value, seed=seed, name=name)
+
