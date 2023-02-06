d3f8989d0b612d3f6f6f505aa1fc9a0fce722632
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2023-02-06 22:37:10 +0530

    lint

--- multiversion_frontend_test.py
+++ multiversion_frontend_test.py
@@ -5,6 +5,8 @@ import importlib
 import codecs
 import io
 import os
+
+
 def available_frameworks():
     available_frameworks_lis = ["numpy", "jax", "tensorflow", "torch"]
     try:
@@ -30,14 +32,11 @@ def available_frameworks():
     return available_frameworks_lis
 
 
-
 def convtrue(argument):
     """Convert NativeClass in argument to true framework counter part"""
     if isinstance(argument, NativeClass):
         return argument._native_class
     return argument
-
-
 
 
 class NativeClass:
@@ -62,40 +61,39 @@ class NativeClass:
         self._native_class = native_class
 
 
+if __name__ == "__main__":
 
-
-
-if __name__=='__main__':
-
-    arg_lis=sys.argv
-    fw_lis=[]
-    jaxlib_flag=0
+    arg_lis = sys.argv
+    fw_lis = []
+    jaxlib_flag = 0
     for i in arg_lis[1:]:
-        if i.split('/')[0]=='jax':
-            fw_lis.append(i.split('/')[0]+'/'+i.split('/')[1])
-            fw_lis.append(i.split('/')[2]+'/'+i.split('/')[3])
-            jaxlib_flag=1
+        if i.split("/")[0] == "jax":
+            fw_lis.append(i.split("/")[0] + "/" + i.split("/")[1])
+            fw_lis.append(i.split("/")[2] + "/" + i.split("/")[3])
+            jaxlib_flag = 1
         else:
             fw_lis.append(i)
     config.allow_global_framework_imports(fw=fw_lis)
     if jaxlib_flag:
         import jaxlib
-    j=1
+    j = 1
     import ivy
+
     # ivy.bfloat16
-    ivy.set_backend(arg_lis[2].split('/')[0])
+    ivy.set_backend(arg_lis[2].split("/")[0])
     import numpy
+
     while j:
         try:
-            z=input()
-            pickle_dict=jsonpickle.loads(z)
-            frontend_fw=input()
+            z = input()
+            pickle_dict = jsonpickle.loads(z)
+            frontend_fw = input()
 
             frontend_fw = importlib.import_module(frontend_fw)
 
-            func=input()
+            func = input()
 
-            args_np,kwargs_np=pickle_dict['a'],pickle_dict['b']
+            args_np, kwargs_np = pickle_dict["a"], pickle_dict["b"]
             args_frontend = ivy.nested_map(
                 args_np,
                 lambda x: ivy.native_array(x)
@@ -113,15 +111,11 @@ if __name__=='__main__':
 
             # change ivy dtypes to native dtypes
             if "dtype" in kwargs_frontend:
-                kwargs_frontend["dtype"] = ivy.as_native_dtype(
-                    kwargs_frontend["dtype"]
-                )
+                kwargs_frontend["dtype"] = ivy.as_native_dtype(kwargs_frontend["dtype"])
 
             # change ivy device to native devices
             if "device" in kwargs_frontend:
-                kwargs_frontend["device"] = ivy.as_native_dev(
-                    kwargs_frontend["device"]
-                )
+                kwargs_frontend["device"] = ivy.as_native_dev(kwargs_frontend["device"])
 
             # check and replace the NativeClass objects in arguments
             # with true counterparts
@@ -132,11 +126,9 @@ if __name__=='__main__':
                 kwargs_frontend, fn=convtrue, include_derived=True, max_depth=10
             )
 
-            frontend_ret = frontend_fw.__dict__[func](
-                *args_frontend , **kwargs_frontend
-            )
-            frontend_ret=ivy.to_ivy(frontend_ret)
-            frontend_ret=jsonpickle.dumps(frontend_ret)
+            frontend_ret = frontend_fw.__dict__[func](*args_frontend, **kwargs_frontend)
+            frontend_ret = ivy.to_ivy(frontend_ret)
+            frontend_ret = jsonpickle.dumps(frontend_ret)
             print(frontend_ret)
         except EOFError:
             continue
