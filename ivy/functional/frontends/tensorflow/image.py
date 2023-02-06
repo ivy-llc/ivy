2380dc7f19ac87abf792f7f284b361745c12dece
mitanshudodia <80592354+mitanshudodia@users.noreply.github.com>
2023-02-06 20:23:52 +0530

    Update image.py

--- ivy/functional/frontends/tensorflow/image.py
+++ ivy/functional/frontends/tensorflow/image.py
@@ -5,4 +5,3 @@ from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_b
 @to_ivy_arrays_and_back
 def resize(images, size, preserve_aspect_ratio=False, antialias=False, name=None):
     return ivy.resize(images, size)
-
