34202b3d77d8d2dae1a3e0ea939356d572c298bb
unknown <clsandoval@up.edu.ph>
2023-02-07 14:22:13 +0800

    added jax.numpy.creation.empty_like

--- ivy/functional/frontends/jax/numpy/creation.py
+++ ivy/functional/frontends/jax/numpy/creation.py
@@ -104,3 +104,11 @@ def full_like(a, fill_value, dtype=None, shape=None):
 @to_ivy_arrays_and_back
 def ndim(a):
     return ivy.astype(ivy.array(a.ndim), ivy.int64)
+
+
+@handle_jax_dtype
+@to_ivy_arrays_and_back
+def empty_like(prototype, dtype=None, shape=None):
+    if shape:
+        return DeviceArray(ivy.empty(shape, dtype=dtype))
+    return DeviceArray(ivy.empty_like(prototype, dtype=dtype))
