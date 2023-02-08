ea04eaf6d16c22177f3540b3c356fc5560ca2b95
Eddiebee <edbeeotudor@gmail.com>
2023-02-08 13:58:35 +0100

    Revert "Refactor test_tensorflow shuffle function"
    This reverts commit e373349b678b926bacbbeee395c4283bc48ce7bb.

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
@@ -98,6 +98,7 @@ def test_tensorflow_normal(
     value=helpers.tf_tensor(array=[[1, 2], [3, 4], [5, 6]]).ivy_array,
     dtype=helpers.get_dtypes("float", full=False),
     seed=helpers.ints(min_value=0, max_value=10),
+    test_with_out=st.just(False),
 )
 def test_tensorflow_shuffle(
     frontend,
