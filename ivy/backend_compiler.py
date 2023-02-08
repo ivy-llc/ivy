00a18bb982af205dfa9d8bc5949f234e4dfdbd53
CatB1t <skytedits@gmail.com>
2023-02-08 02:17:48 +0200

    reorganize functions.

--- ivy/backend_compiler.py
+++ ivy/backend_compiler.py
@@ -9,171 +9,7 @@ from importlib.abc import Loader, MetaPathFinder
 
 IMPORT_CACHE = {}
 
-
-def _retrive_local_modules():
-    ret = []
-    wd = sys.path[0]
-    for entry in os.scandir(wd):
-        if entry.is_file():
-            if entry.name.endswith(".py"):
-                ret.append(entry.name[:-3])
-                continue
-        if entry.is_dir():
-            if "__init__.py" in os.listdir(wd + "/" + entry.name):
-                ret.append(entry.name)
-    return ret
-
-
-class IvyPathFinder(MetaPathFinder):
-    def find_spec(self, fullname, path, target=None):
-        if fullname.partition(".")[0] not in _retrive_local_modules():
-            # print("Global", fullname, "falling back to sys import")
-            return None
-        # We're local
-        if path is None or path == "":
-            path = [sys.path[0]]  # top level import --
-        if "." in fullname:
-            *parents, name = fullname.split(".")
-        else:
-            name = fullname
-        for entry in path:
-            if os.path.isdir(os.path.join(entry, name)):
-                # this module has child modules
-                filename = os.path.join(entry, name, "__init__.py")
-                submodule_locations = [os.path.join(entry, name)]
-            else:
-                filename = os.path.join(entry, name + ".py")
-                submodule_locations = None
-            if not os.path.exists(filename):
-                continue
-            # print("Found Local", fullname)
-            return spec_from_file_location(
-                fullname,
-                filename,
-                loader=IvyLoader(filename),
-                submodule_search_locations=submodule_locations,
-            )
-        # print("Couldn't find Local", fullname)
-        return None
-
-
-class IvyLoader(Loader):
-    def __init__(self, filename):
-        self.filename = filename
-
-    def create_module(self, spec):
-        return None  # use default module creation semantics
-
-    def exec_module(self, module):
-        with open(self.filename) as f:
-            data = f.read()
-
-        # manipulate data some way...
-        # print("Calling custom loader on ", module)
-        ast_tree = parse(data)
-        transformer = ImportTransformer()
-        transformer.visit_ImportFrom(ast_tree)
-        transformer.visit_Import(ast_tree)
-        transformer.impersonate_import(ast_tree)
-        ast.fix_missing_locations(ast_tree)
-        try:
-            exec(
-                compile(ast_tree, filename=self.filename, mode="exec"), module.__dict__
-            )
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
-            raise e
-
-
-LOCAL_MODULES = _retrive_local_modules()
-FINDER = IvyPathFinder()
-
-
-def _clear_cache():
-    global IMPORT_CACHE
-    IMPORT_CACHE = {}
-
-
-def _ivy_fromimport(name: str, package=None, mod_globals=None, from_list=(), level=0):
-    """
-    Handles absolute and relative from_import statmement
-    :param name:
-    :param package:
-    :param mod_globals:
-    :param from_list:
-    :param level:
-    :return:
-    """
-    module_exist = name != ""
-    name = "." * level + name
-    module = _ivy_import_module(name, package)
-    for entry_name, entry_asname in from_list:
-        if entry_name == "*":
-            if "__all__" in module.__dict__.keys():
-                _all = module.__dict__["__all__"]
-            else:
-                _all = {
-                    k: v for (k, v) in module.__dict__.items() if not k.startswith("__")
-                }
-            for k, v in _all.items():
-                mod_globals[k] = v
-            continue
-        alias = entry_name if entry_asname is None else entry_asname
-        # Handles attributes inside module
-        try:
-            mod_globals[alias] = module.__dict__[entry_name]
-            # In the case this is a module from a package
-        except KeyError:
-            if module_exist:
-                in_name = f"{name}.{entry_name}"
-            else:
-                in_name = name + entry_name
-            mod_globals[alias] = _ivy_import_module(in_name, package)
-    return module
-
-
-def _ivy_absolute_import(name: str, asname=None, mod_globals=None):
-    """
-    Handles absolute import statement
-    :param name:
-    :return:
-    """
-    if asname is None:
-        _ivy_import_module(name)
-        true_name = name.partition(".")[0]
-        module = IMPORT_CACHE[true_name]
-    else:
-        true_name = asname
-        module = _ivy_import_module(name)
-    mod_globals[true_name] = module
-
-
-def _ivy_import_module(name, package=None):
-    global IMPORT_CACHE
-    absolute_name = resolve_name(name, package)
-    try:
-        return IMPORT_CACHE[absolute_name]
-    except KeyError:
-        pass
-
-    path = None
-    if "." in absolute_name:
-        parent_name, _, child_name = absolute_name.rpartition(".")
-        parent_module = _ivy_import_module(parent_name)
-        path = parent_module.__spec__.submodule_search_locations
-    spec = FINDER.find_spec(absolute_name, path)
-    if spec is None:
-        msg = f"No module named {absolute_name!r}"
-        raise ModuleNotFoundError(msg, name=absolute_name)
-    # print(spec, name)
-    module = module_from_spec(spec)
-    IMPORT_CACHE[absolute_name] = module
-    spec.loader.exec_module(module)
-    if path is not None:
-        # Set reference to self in parent, if exist
-        setattr(parent_module, child_name, module)
-    return module
+# AST helpers ##################
 
 
 def _parse_absolute_fromimport(node: ast.ImportFrom):
@@ -292,6 +128,9 @@ def _create_attrs_from_node(node, attrs=()):
     return last_node
 
 
+# End AST helpers ##############
+
+
 class ImportTransformer(ast.NodeTransformer):
     def __init__(self):
         self.insert_index = 0  # TODO hacky solution for __future__
@@ -339,6 +178,168 @@ class ImportTransformer(ast.NodeTransformer):
                 ),
             )
         return tree
+
+
+def _retrive_local_modules():
+    ret = []
+    wd = sys.path[0]
+    for entry in os.scandir(wd):
+        if entry.is_file():
+            if entry.name.endswith(".py"):
+                ret.append(entry.name[:-3])
+                continue
+        if entry.is_dir():
+            if "__init__.py" in os.listdir(wd + "/" + entry.name):
+                ret.append(entry.name)
+    return ret
+
+
+class IvyPathFinder(MetaPathFinder):
+    def find_spec(self, fullname, path, target=None):
+        if fullname.partition(".")[0] not in _retrive_local_modules():
+            # print("Global", fullname, "falling back to sys import")
+            return None
+        # We're local
+        if path is None or path == "":
+            path = [sys.path[0]]  # top level import --
+        if "." in fullname:
+            *parents, name = fullname.split(".")
+        else:
+            name = fullname
+        for entry in path:
+            if os.path.isdir(os.path.join(entry, name)):
+                # this module has child modules
+                filename = os.path.join(entry, name, "__init__.py")
+                submodule_locations = [os.path.join(entry, name)]
+            else:
+                filename = os.path.join(entry, name + ".py")
+                submodule_locations = None
+            if not os.path.exists(filename):
+                continue
+            # print("Found Local", fullname)
+            return spec_from_file_location(
+                fullname,
+                filename,
+                loader=IvyLoader(filename),
+                submodule_search_locations=submodule_locations,
+            )
+        # print("Couldn't find Local", fullname)
+        return None
+
+
+class IvyLoader(Loader):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def exec_module(self, module):
+        with open(self.filename) as f:
+            data = f.read()
+
+        # print("Calling custom loader on ", module)
+        ast_tree = parse(data)
+        transformer = ImportTransformer()
+        transformer.visit_ImportFrom(ast_tree)
+        transformer.visit_Import(ast_tree)
+        transformer.impersonate_import(ast_tree)
+        ast.fix_missing_locations(ast_tree)
+        try:
+            exec(
+                compile(ast_tree, filename=self.filename, mode="exec"), module.__dict__
+            )
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+            raise e
+
+
+LOCAL_MODULES = _retrive_local_modules()
+FINDER = IvyPathFinder()
+
+
+def _clear_cache():
+    global IMPORT_CACHE
+    IMPORT_CACHE = {}
+
+
+def _ivy_fromimport(name: str, package=None, mod_globals=None, from_list=(), level=0):
+    """
+    Handles absolute and relative from_import statmement
+    :param name:
+    :param package:
+    :param mod_globals:
+    :param from_list:
+    :param level:
+    :return:
+    """
+    module_exist = name != ""
+    name = "." * level + name
+    module = _ivy_import_module(name, package)
+    for entry_name, entry_asname in from_list:
+        if entry_name == "*":
+            if "__all__" in module.__dict__.keys():
+                _all = module.__dict__["__all__"]
+            else:
+                _all = {
+                    k: v for (k, v) in module.__dict__.items() if not k.startswith("__")
+                }
+            for k, v in _all.items():
+                mod_globals[k] = v
+            continue
+        alias = entry_name if entry_asname is None else entry_asname
+        # Handles attributes inside module
+        try:
+            mod_globals[alias] = module.__dict__[entry_name]
+            # In the case this is a module from a package
+        except KeyError:
+            if module_exist:
+                in_name = f"{name}.{entry_name}"
+            else:
+                in_name = name + entry_name
+            mod_globals[alias] = _ivy_import_module(in_name, package)
+    return module
+
+
+def _ivy_absolute_import(name: str, asname=None, mod_globals=None):
+    """
+    Handles absolute import statement
+    :param name:
+    :return:
+    """
+    if asname is None:
+        _ivy_import_module(name)
+        true_name = name.partition(".")[0]
+        module = IMPORT_CACHE[true_name]
+    else:
+        true_name = asname
+        module = _ivy_import_module(name)
+    mod_globals[true_name] = module
+
+
+def _ivy_import_module(name, package=None):
+    global IMPORT_CACHE
+    absolute_name = resolve_name(name, package)
+    try:
+        return IMPORT_CACHE[absolute_name]
+    except KeyError:
+        pass
+
+    path = None
+    if "." in absolute_name:
+        parent_name, _, child_name = absolute_name.rpartition(".")
+        parent_module = _ivy_import_module(parent_name)
+        path = parent_module.__spec__.submodule_search_locations
+    spec = FINDER.find_spec(absolute_name, path)
+    if spec is None:
+        msg = f"No module named {absolute_name!r}"
+        raise ModuleNotFoundError(msg, name=absolute_name)
+    # print(spec, name)
+    module = module_from_spec(spec)
+    IMPORT_CACHE[absolute_name] = module
+    spec.loader.exec_module(module)
+    if path is not None:
+        # Set reference to self in parent, if exist
+        setattr(parent_module, child_name, module)
+    return module
 
 
 def with_backend(backend: str):
