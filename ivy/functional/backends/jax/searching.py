499e22c6a6abc3a1111a9298e5b2fc68d147344f
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-02-08 19:25:42 +0300

    Modified `argwhere`

--- ivy/functional/backends/jax/searching.py
+++ ivy/functional/backends/jax/searching.py
@@ -96,5 +96,10 @@ def where(
 # ----- #
 
 
-def argwhere(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
+def argwhere(
+        x: JaxArray,
+        /,
+        *,
+        out: Optional[JaxArray] = None
+) -> JaxArray:
     return jnp.argwhere(x)
