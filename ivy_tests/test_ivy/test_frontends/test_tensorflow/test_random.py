e72be63fe1a594bdc4fc17e16a55809d6ef01a2f
Eddiebee <edbeeotudor@gmail.com>
2023-02-08 01:11:08 +0100

    Refactor test_tensorflow_shuffle function

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_random.py
@@ -95,13 +95,7 @@ def test_tensorflow_normal(
 # random_shuffle
 @handle_frontend_test(
     fn_tree="tensorflow.random.shuffle",
-    value=helpers.get_shape(
-        allow_none=False,
-        min_num_dims=1,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=10,
-    ),
+    value=st.floats(allow_nan=False, allow_infinity=False, width=32),
     dtype=helpers.get_dtypes("float", full=False),
     seed=helpers.ints(min_value=0, max_value=10),
     test_with_out=st.just(False),
