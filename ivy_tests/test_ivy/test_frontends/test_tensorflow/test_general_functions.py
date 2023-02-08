240bb0c1b4b9764e5adf5fff00e478a037674048
unknown <snjenga631@gmail.com>
2023-02-08 17:15:43 +0300

    'datatypefix'

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -1347,7 +1347,7 @@ def test_tensorflow_where_with_xy(
     dtype_and_axis=helpers.dtype_and_values(
         min_num_dims=1,
         max_num_dims=1,
-        dtype="int32",
+        dtype=["int32"],
     )
 
 )
