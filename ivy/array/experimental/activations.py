c2aa00dd3e91a9372edd4217933564ef5ca7b372
Aarsh2001 <ac5973@srmist.edu.in>
2023-02-09 17:58:42 +0530

    added PRELU to experimental(all tests passing)

--- ivy/array/experimental/activations.py
+++ ivy/array/experimental/activations.py
@@ -83,5 +83,29 @@ class ArrayWithActivationsExperimental(abc.ABC):
 
     def prelu(
         self,
+        slope: Union[float, ivy.NativeArray, ivy.Array],
+        /,
+        *,
+        out: Optional["ivy.Array"] = None,
     ) -> ivy.Array:
-        pass
+        """
+        Prelu takes input data (Array) and slope array as input,
+        and produces one output data (array) where the function
+        f(x) = slope * x for x < 0, f(x) = x for x >= 0., is applied
+        to the data array elementwise. This operator supports unidirectional
+        broadcasting (array slope should be unidirectional broadcastable to
+        input tensor X);
+
+        Parameters
+        ----------
+        self
+            input array.
+        slope
+            Slope Array. The shape of slope can be smaller then first input X;
+            if so, its shape must be unidirectional broadcastable to X.
+        out
+            Optional output array.
+        Returns
+        -------
+        """
+        return ivy.prelu(self._data, slope, out=out)
