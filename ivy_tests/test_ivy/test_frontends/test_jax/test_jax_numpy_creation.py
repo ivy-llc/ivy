4b6a18a6487bfa168ff21a65e26dbb79350045d5
Carlos Sandoval <71140233+clsandoval@users.noreply.github.com>
2023-02-10 10:02:25 +0800

    Jax.numpy.creation.empty like (#10355)
    Co-authored-by: nathzi1505 <41519676+nathzi1505@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
@@ -485,6 +485,44 @@ def test_jax_numpy_ndim(
     )
 
 
+# empty_like
+@handle_frontend_test(
+    fn_tree="jax.numpy.empty_like",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+    shape=helpers.get_shape(
+        allow_none=True,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=10,
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+    test_with_out=st.just(False),
+)
+def test_jax_numpy_empty_like(
+    dtype_and_x,
+    shape,
+    dtype,
+    test_flags,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        a=x[0],
+        dtype=dtype[0],
+        shape=shape,
+    )
+
+
 # full
 @handle_frontend_test(
     fn_tree="jax.numpy.full",
