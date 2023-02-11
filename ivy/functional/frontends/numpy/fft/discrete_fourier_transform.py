d9091cfc71fde00637c3f77f8f00aec0c3ff2fb6
Akoji Timothy <46170255+CodeMaestro1000@users.noreply.github.com>
2023-02-11 18:04:16 +0000

    Fixed requested lint changes

--- ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
+++ ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
@@ -1,5 +1,6 @@
 import ivy
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
+
    
 @to_ivy_arrays_and_back
 def ifft(a, n=None, axis=-1, norm=None):
@@ -7,4 +8,3 @@ def ifft(a, n=None, axis=-1, norm=None):
     if norm is None:
         norm = "backward"
     return ivy.ifft(a, axis, norm=norm, n=n)
-
