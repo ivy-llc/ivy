db3e02b789e788523d3ad98449ca5b29fd894439
ivy-branch <ivy.branch@lets-unify.ai>
2023-02-11 08:00:47 +0000

    lintfixbot: Auto-commit fixed lint errors in codebase

--- ivy/functional/backends/torch/experimental/sorting.py
+++ ivy/functional/backends/torch/experimental/sorting.py
@@ -15,18 +15,14 @@ msort_support_native_out = True
 
 # lexsort
 def lexsort(
-    keys: torch.Tensor,
-    /,
-    *,
-    axis: int = -1,
-    out: Optional[torch.Tensor] = None
+    keys: torch.Tensor, /, *, axis: int = -1, out: Optional[torch.Tensor] = None
 ) -> torch.Tensor:
     shape = keys.size()
     if len(shape) == 1:
         _, result = torch.sort(keys, dim=axis, stable=True)
         return result
     if shape[0] == 0:
-        raise TypeError('need sequence of keys with len > 0 in lexsort')
+        raise TypeError("need sequence of keys with len > 0 in lexsort")
     if len(shape) == 2 and shape[1] == 1:
         return torch.tensor([0])
     _, result = torch.sort(keys[0], dim=axis, stable=True)
