c25cea26c3eb015a736cb3faa8f6a63517384e01
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-02-12 21:07:45 +0300

    Added examples to `array_indices_axis`

--- ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
@@ -30,7 +30,7 @@ def array_bools(
         special function that draws data randomly (but is reproducible) from a given
         data-set (ex. list).
     size
-        size of the list. If not specified, default is a random number between 1 and 4 inclusive.
+        size of the list.
 
     Returns
     -------
@@ -190,7 +190,10 @@ def lists(
     if not isinstance(max_size, int):
         max_size = draw(st.shared(integers, key=max_size))
     min_size, max_size = abs(min_size), abs(max_size)
-    min_size, max_size = (min_size, max_size) if min_size <= max_size else (max_size, min_size)
+    if min_size <= max_size:
+        min_size, max_size = (min_size, max_size)
+    else:
+        min_size, max_size = (max_size, min_size)
     return draw(st.lists(other, min_size=min_size, max_size=max_size))
 
 
@@ -242,7 +245,7 @@ def dtype_and_values(
     large_abs_safety_factor
         A safety factor of 1 means that all values are included without limitation,
 
-        when a "linear" safety factor scaler is used,  a safety factor of 2 means
+        when a "linear" safety factor scaler is used, a safety factor of 2 means
         that only 50% of the range is included, a safety factor of 3 means that
         only 33% of the range is included etc.
 
@@ -293,16 +296,22 @@ def dtype_and_values(
     Returns
     -------
     ret
-        A strategy that draws a list of dtype and arrays (as lists).
+        A strategy that draws a tuple of a list of dtypes and a list
+        of their respective arrays.
 
     Examples
     --------
     >>> dtype_and_values(
     ...     num_arrays=3,
     ... )
-    (['uint16', 'float16', 'uint16'], [array([37915,  6322, 26765, 12413, 26986, 34665], dtype=uint16), array([-5.000e-01, -5.000e-01, -2.000e+00, -6.711e-05, -1.100e+00, -5.955e+04], dtype=float16), array([40817, 56193, 29200,     0,  5851,  9746], dtype=uint16)])
-    (['bool', 'uint32', 'bool'], [array(False), array(0, dtype=uint32), array(False)])
-    (['int8', 'int8', 'int8'], [array(0, dtype=int8), array(0, dtype=int8), array(0, dtype=int8)])
+    (['uint16', 'float16', 'uint16'], [array([37915, 6322, 26765, 12413,
+        26986, 34665], dtype=uint16), array([-5.000e-01, -5.000e-01,
+        -2.000e+00, -6.711e-05, -1.100e+00, -5.955e+04], dtype=float16),
+        array([40817, 56193, 29200, 0, 5851, 9746], dtype=uint16)])
+    (['bool', 'uint32', 'bool'], [array(False), array(0, dtype=uint32),
+        array(False)])
+    (['int8', 'int8', 'int8'], [array(0, dtype=int8), array(0, dtype=int8),
+        array(0, dtype=int8)])
 
     >>> dtype_and_values(
     ...     available_dtypes=get_dtypes("numeric"),
@@ -311,18 +320,25 @@ def dtype_and_values(
     ...     num_arrays=2,
     ...     shared_dtype=True,
     ... ),
-    (['float32', 'float32'], [array([1.1, 1.5], dtype=float32), array([-5.9604645e-08,  5.9604645e-08], dtype=float32)])
+    (['float32', 'float32'], [array([1.1, 1.5], dtype=float32),
+        array([-5.9604645e-08, 5.9604645e-08], dtype=float32)])
     (['int32', 'int32'], [array(-5, dtype=int32), array(-1, dtype=int32)])
-    (['uint64', 'uint64'], [array([0], dtype=uint64), array([0], dtype=uint64)])
+    (['uint64', 'uint64'], [array([0], dtype=uint64), array([0],
+        dtype=uint64)])
 
     >>> dtype_and_values(
     ...     available_dtypes=get_dtypes("numeric"),
     ...     num_arrays=2,
     ...     ret_shape=True
     ... )
-    (['int8', 'int32'], [array([27], dtype=int8), array([192], dtype=int32)], (1,))
-    (['int32', 'int16'], [array(0, dtype=int32), array(0, dtype=int16)], ())
-    (['int32', 'int16'], [array([[-103, 12, -41795, 1170789994, 44251, 44209, 433075925]], dtype=int32), array([[24791, -24691, 24892, 16711, 7696, 972, 15357]], dtype=int16)], (1, 7))
+    (['int8', 'int32'], [array([27], dtype=int8), array([192],
+        dtype=int32)], (1,))
+    (['int32', 'int16'], [array(0, dtype=int32), array(0,
+        dtype=int16)], ())
+    (['int32', 'int16'], [array([[-103, 12, -41795, 1170789994,
+        44251, 44209, 433075925]], dtype=int32), array([[24791,
+        -24691, 24892, 16711, 7696, 972, 15357]], dtype=int16)],
+        (1, 7))
 
     """
     if isinstance(min_dim_size, st._internal.SearchStrategy):
@@ -437,7 +453,7 @@ def dtype_values_axis(
     large_abs_safety_factor
         A safety factor of 1 means that all values are included without limitation,
 
-        when a "linear" safety factor scaler is used,  a safety factor of 2 means
+        when a "linear" safety factor scaler is used, a safety factor of 2 means
         that only 50% of the range is included, a safety factor of 3 means that
         only 33% of the range is included etc.
 
@@ -499,7 +515,8 @@ def dtype_values_axis(
     Returns
     -------
     ret
-        A strategy that draws a dtype, an array (as list), and an axis.
+        A strategy that draws a tuple of a list of dtypes,
+        a list of arrays, and an axis.
 
     Examples
     -------
@@ -525,9 +542,12 @@ def dtype_values_axis(
     ...     min_axis=-2,
     ...     max_axis=1,
     ... )
-    (['float64'], [array([[1.90000000e+000, 1.63426649e+308], [-1.50000000e+000, -1.91931887e+234]])], -1)
-    (['bfloat16'], [array([[-1.29488e-38, -1.29488e-38], [-1.29488e-38, -1.29488e-38]], dtype=bfloat16)], 0)
-    (['float64'], [array([[-2.44758124e-308, -2.44758124e-308], [-2.44758124e-308, -2.44758124e-308]])], 0)
+    (['float64'], [array([[1.90000000e+000, 1.63426649e+308],
+        [-1.50000000e+000, -1.91931887e+234]])], -1)
+    (['bfloat16'], [array([[-1.29488e-38, -1.29488e-38],
+        [-1.29488e-38, -1.29488e-38]], dtype=bfloat16)], 0)
+    (['float64'], [array([[-2.44758124e-308, -2.44758124e-308],
+        [-2.44758124e-308, -2.44758124e-308]])], 0)
 
     >>> dtype_values_axis(
     ...     available_dtypes=get_dtypes("numeric"),
@@ -607,12 +627,18 @@ def array_indices_axis(
 
     Parameters
     ----------
+    draw
+        special function that draws data randomly (but is reproducible) from a given
+        data-set (ex. list).
     array_dtypes
         list of data type to draw the array dtype from.
     indices_dtypes
         list of data type to draw the indices dtype from.
     disable_random_axis
-        axis is set to -1 when True. Randomly generated with hypothesis if False.
+        Randomly generated with hypothesis if False. If True,
+        axis is set to 0 if axis_zero is True, -1 otherwise.
+    axis_zero
+        If True, axis is set to zero if disable_random_axis is True.
     allow_inf
         inf values are allowed to be generated in the values array when True.
     min_num_dims
@@ -628,8 +654,9 @@ def array_indices_axis(
 
     Returns
     -------
-    A strategy that can be used in the @given hypothesis decorator
-    which generates arrays of values and indices.
+    ret
+        A strategy that can be used in the @given hypothesis
+        decorator which generates arrays of values and indices.
 
     Examples
     --------
@@ -643,6 +670,48 @@ def array_indices_axis(
             max_dim_size=10
             )
     )
+
+    >>> array_indices_axis(
+    ...    array_dtypes=get_dtypes("valid"),
+    ...     indices_dtypes=["int64"],
+    ...     max_num_dims=1,
+    ...     indices_same_dims=True,
+    ...     disable_random_axis=True,
+    ...     axis_zero=True,
+    ... )
+    (['int64', 'int64'], array([-65536]), array([0]))
+    (['bool', 'int64'], array([False, False, False, True,
+        False, False, False, False]), array([0, 0, 2, 4,
+        0, 0, 0, 1]))
+    (['int64', 'int64'], array([0]), array([0]))
+
+    >>> array_indices_axis(
+    ...     array_dtypes=get_dtypes("valid"),
+    ...     indices_dtypes=get_dtypes("integer"),
+    ...     disable_random_axis=True,
+    ...     first_dimension_only=True,
+    ... )
+    (['float64', 'uint64'], array([-2.44758124e-308]),
+        array([0], dtype=uint64))
+    (['bool', 'uint64'], array([False]), array([0], dtype=uint64))
+    (['bool', 'int8'], array([False]), array([[0, 0, 0, 0],
+       [0, 0, 0, 0],
+       [0, 0, 0, 0],
+       [0, 0, 0, 0]], dtype=int8))
+
+    >>> array_indices_axis(
+    ...     array_dtypes=get_dtypes("valid"),
+    ...     indices_dtypes=["int64"],
+    ...     max_num_dims=1,
+    ...     indices_same_dims=True,
+    ... )
+    (['float16', 'int64'], array([-256.], dtype=float16),
+        array([0]), 0, 0)
+    (['uint8', 'int64'], array([1], dtype=uint8),
+        array([0]), -1, 0)
+    (['uint64', 'int64'], array([0], dtype=uint64),
+        array([0]), 0, 0)
+
     """
     x_dtype, x, x_shape = draw(
         dtype_and_values(
@@ -691,9 +760,10 @@ def array_indices_axis(
             )
         )
         indices_shape = batch_shape + shape_var
-    max_axis = max(x_shape[axis] - 1, 0)
     if first_dimension_only:
         max_axis = max(x_shape[0] - 1, 0)
+    else:
+        max_axis = max(x_shape[axis] - 1, 0)
     indices_dtype, indices = draw(
         dtype_and_values(
             available_dtypes=indices_dtypes,
@@ -824,7 +894,7 @@ def array_values(
     large_abs_safety_factor
         A safety factor of 1 means that all values are included without limitation,
 
-        when a "linear" safety factor scaler is used,  a safety factor of 2 means
+        when a "linear" safety factor scaler is used, a safety factor of 2 means
         that only 50% of the range is included, a safety factor of 3 means that
         only 33% of the range is included etc.
 
