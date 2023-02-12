f846e0baccfb22d5934ec5df0bd8486d20992cdd
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-02-12 03:14:56 +0300

    Minor reformatting

--- ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/array_helpers.py
@@ -81,25 +81,25 @@ def list_of_size(
     Examples
     --------
     >>> list_of_size(
-    >>>     other=st.sampled_from([-1, 5, 9]),
-    >>>     size=4,
-    >>> )
+    ...     other=st.sampled_from([-1, 5, 9]),
+    ...     size=4,
+    ... )
     [-1, 5, -1, -1]
     [9, -1, -1, -1]
     [9, 9, -1, 9]
 
     >>> list_of_size(
-    >>>     other=st.integers(min_value=0, max_value=4),
-    >>>     size=10,
-    >>> )
+    ...     other=st.integers(min_value=0, max_value=4),
+    ...     size=10,
+    ... )
     [3, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     [3, 3, 2, 4, 1, 0, 4, 2, 1, 2]
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     >>> list_of_size(
-    >>>     other=st.booleans(),
-    >>>     size=3,
-    >>> )
+    ...     other=st.booleans(),
+    ...     size=3,
+    ... )
     [False, False, False]
     [True, True, False]
     [False, True, False]
@@ -142,39 +142,39 @@ def lists(
     Examples
     --------
     >>> lists(
-    >>>     other=st.sampled_from([-1, 5, 9]),
-    >>>     min_size=4,
-    >>>     max_size=5,
-    >>> )
+    ...     other=st.sampled_from([-1, 5, 9]),
+    ...     min_size=4,
+    ...     max_size=5,
+    ... )
     [5, 5, 5, 9, 9]
     [5, 9, -1, -1]
     [5, 9, 5, 9]
 
     >>> lists(
-    >>>     other=st.integers(min_value=0, max_value=4),
-    >>>     size_bounds=(9, 10),
-    >>> )
+    ...     other=st.integers(min_value=0, max_value=4),
+    ...     size_bounds=(9, 10),
+    ... )
     [0, 2, 4, 3, 3, 3, 3, 2, 1, 4]
     [1, 0, 1, 2, 1, 4, 1, 3, 1]
     [1, 0, 1, 2, 1, 4, 1, 3, 1]
 
     >>> lists(
-    >>>     other=st.integers(min_value=0, max_value=4),
-    >>>     size_bounds=[9, 10],
-    >>> )
+    ...     other=st.integers(min_value=0, max_value=4),
+    ...     size_bounds=[9, 10],
+    ... )
     [1, 3, 0, 2, 0, 0, 1, 4, 2, 3]
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
     [1, 2, 4, 1, 1, 1, 4, 3, 2]
 
     >>> lists(
-    >>>     other=st.floats(
-    >>>         min_value=1,
-    >>>         max_value=3,
-    >>>         exclude_max=True,
-    >>>     ),
-    >>>     min_size=5,
-    >>>     max_size=5,
-    >>> )
+    ...     other=st.floats(
+    ...         min_value=1,
+    ...         max_value=3,
+    ...         exclude_max=True,
+    ...     ),
+    ...     min_size=5,
+    ...     max_size=5,
+    ... )
     [1.1, 1.0, 1.0, 1.0, 1.0]
     [2.00001, 2.00001, 1.0, 2.999999999999999, 1.9394938006792373]
     [1.0, 2.00001, 1.0, 2.999999999999999, 1.9394938006792373]
@@ -298,28 +298,28 @@ def dtype_and_values(
     Examples
     --------
     >>> dtype_and_values(
-    >>>     num_arrays=3,
-    >>> )
+    ...     num_arrays=3,
+    ... )
     (['uint16', 'float16', 'uint16'], [array([37915,  6322, 26765, 12413, 26986, 34665], dtype=uint16), array([-5.000e-01, -5.000e-01, -2.000e+00, -6.711e-05, -1.100e+00, -5.955e+04], dtype=float16), array([40817, 56193, 29200,     0,  5851,  9746], dtype=uint16)])
     (['bool', 'uint32', 'bool'], [array(False), array(0, dtype=uint32), array(False)])
     (['int8', 'int8', 'int8'], [array(0, dtype=int8), array(0, dtype=int8), array(0, dtype=int8)])
 
     >>> dtype_and_values(
-    >>>     available_dtypes=get_dtypes("numeric"),
-    >>>     min_value=-10,
-    >>>     max_value=10,
-    >>>     num_arrays=2,
-    >>>     shared_dtype=True,
-    >>> ),
+    ...     available_dtypes=get_dtypes("numeric"),
+    ...     min_value=-10,
+    ...     max_value=10,
+    ...     num_arrays=2,
+    ...     shared_dtype=True,
+    ... ),
     (['float32', 'float32'], [array([1.1, 1.5], dtype=float32), array([-5.9604645e-08,  5.9604645e-08], dtype=float32)])
     (['int32', 'int32'], [array(-5, dtype=int32), array(-1, dtype=int32)])
     (['uint64', 'uint64'], [array([0], dtype=uint64), array([0], dtype=uint64)])
 
     >>> dtype_and_values(
-    >>>     available_dtypes=get_dtypes("numeric"),
-    >>>     num_arrays=2,
-    >>>     ret_shape=True
-    >>> )
+    ...     available_dtypes=get_dtypes("numeric"),
+    ...     num_arrays=2,
+    ...     ret_shape=True
+    ... )
     (['int8', 'int32'], [array([27], dtype=int8), array([192], dtype=int32)], (1,))
     (['int32', 'int16'], [array(0, dtype=int32), array(0, dtype=int16)], ())
     (['int32', 'int16'], [array([[-103, 12, -41795, 1170789994, 44251, 44209, 433075925]], dtype=int32), array([[24791, -24691, 24892, 16711, 7696, 972, 15357]], dtype=int16)], (1, 7))
@@ -509,32 +509,32 @@ def dtype_values_axis(
     (['float64'], [array(-1.40129846e-45)])
 
     >>> dtype_values_axis(
-    >>>     available_dtypes=get_dtypes("numeric"),
-    >>>     num_arrays=2,
-    >>> )
+    ...     available_dtypes=get_dtypes("numeric"),
+    ...     num_arrays=2,
+    ... )
     (['int8', 'int16'], [array([[0]], dtype=int8), array([[1]], dtype=int16)], 0)
     (['uint16', 'uint16'], [array(0, dtype=uint16), array(0, dtype=uint16)], 0)
     (['float64', 'int16'], [array(-2.44758124e-308), array(0, dtype=int16)], 0)
 
     >>> dtype_values_axis(
-    >>>     available_dtypes=get_dtypes("float"),
-    >>>     min_num_dims=2,
-    >>>     max_num_dims=3,
-    >>>     min_dim_size=2,
-    >>>     max_dim_size=5,
-    >>>     min_axis=-2,
-    >>>     max_axis=1,
-    >>> )
+    ...     available_dtypes=get_dtypes("float"),
+    ...     min_num_dims=2,
+    ...     max_num_dims=3,
+    ...     min_dim_size=2,
+    ...     max_dim_size=5,
+    ...     min_axis=-2,
+    ...     max_axis=1,
+    ... )
     (['float64'], [array([[1.90000000e+000, 1.63426649e+308], [-1.50000000e+000, -1.91931887e+234]])], -1)
     (['bfloat16'], [array([[-1.29488e-38, -1.29488e-38], [-1.29488e-38, -1.29488e-38]], dtype=bfloat16)], 0)
     (['float64'], [array([[-2.44758124e-308, -2.44758124e-308], [-2.44758124e-308, -2.44758124e-308]])], 0)
 
     >>> dtype_values_axis(
-    >>>     available_dtypes=get_dtypes("numeric"),
-    >>>     num_arrays=1,
-    >>>     allow_inf=True,
-    >>>     allow_nan=True,
-    >>> )
+    ...     available_dtypes=get_dtypes("numeric"),
+    ...     num_arrays=1,
+    ...     allow_inf=True,
+    ...     allow_nan=True,
+    ... )
     (['float64'], [array([inf, -5.14361019e+16, 5.96046448e-08, 1.50000000e+00])], -51)
     (['int16'], [array(12445, dtype=int16)], 171)
     (['uint32'], [array([0], dtype=uint32)], 0)
