24de68d67c8924db9370b00a573af393591f401c
juliagsy <67888047+juliagsy@users.noreply.github.com>
2023-02-12 18:29:27 +0000

    fix scalar param

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -428,13 +428,13 @@ def promote_types_of_numpy_inputs(
         and x1.shape == ()
         and not (hasattr(x2, "shape") and x2.shape == ())
     ):
-        x1 = ivy.to_scalar(x1[()])
+        x1 = ivy.to_scalar(x1)
     if (
         hasattr(x2, "shape")
         and x2.shape == ()
         and not (hasattr(x1, "shape") and x1.shape == ())
     ):
-        x2 = ivy.to_scalar(x2[()])
+        x2 = ivy.to_scalar(x2)
     type1 = ivy.default_dtype(item=x1).strip("u123456789")
     type2 = ivy.default_dtype(item=x2).strip("u123456789")
     if hasattr(x1, "dtype") and not hasattr(x2, "dtype") and type1 == type2:
