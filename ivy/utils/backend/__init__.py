20f248812aa12b5ad833c7902363188675250aa9
CatB1t <skytedits@gmail.com>
2023-02-13 22:47:51 +0200

    refactor ast functions out of backend compiler.

--- ivy/utils/backend/__init__.py
+++ ivy/utils/backend/__init__.py
@@ -2,3 +2,4 @@
 from . import handler
 from .handler import *
 from . import compiler
+from ._importlib import _ivy_import_module
---
+++ ivy/utils/dynamic_import.py
@@ -0,0 +1,4 @@
+# NOQA
+from importlib import import_module as imp_md
+
+import_module = imp_md
