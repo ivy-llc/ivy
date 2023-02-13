8a64de9ab671b65cb72b95f5f94e3bb3bb7601de
Ubuntu <ubuntu@ip-172-31-13-204.ap-south-1.compute.internal>
2023-02-13 11:13:00 +0000

    additional fixes

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -3061,12 +3061,12 @@ def test_tensorflow_Elu(
 
 
 @handle_frontend_test(
-    fn_tree="tensorflow.raw_ops.Conv3D",
+    fn_tree="tensorflow.raw_ops.Conv2D",
     x_f_d_df=_x_and_filters(
         dtypes=helpers.get_dtypes("float", full=False),
-        data_format=st.sampled_from(["NDHWC"]),
+        data_format=st.sampled_from(["NHWC"]),
         padding=st.sampled_from(["SAME", "VALID"]),
-        type="3d",
+        type="2d",
         # Tensorflow backprop doesn't support dilations more than 1 on CPU
         dilation_min=1,
         dilation_max=1,
@@ -3084,7 +3084,7 @@ def test_tensorflow_Conv2D(
 ):
     input_dtype, x, filters, dilation, data_format, stride, padding = x_f_d_df
 
-    # Broadcast stirdes and dilations to correct dims for the ground truth
+    # Broadcast strides and dilation to correct dims for the ground truth
     # backend func to run correctly
     stride = _convolution_broadcast_helper(
         stride, num_spatial_dims=2, channel_index=3, name="strides"
