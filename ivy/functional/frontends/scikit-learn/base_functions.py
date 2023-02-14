11da5c65eda60f33e34750f955ee042c002d7cec
ndurumo254 <erickndurumo@gmail.com>
2023-02-14 18:04:15 +0300

    implimentinting get_config

--- ivy/functional/frontends/scikit-learn/base_functions.py
+++ ivy/functional/frontends/scikit-learn/base_functions.py
@@ -6,4 +6,12 @@ def is_classifier(estimator):
 
 
 def is_regressor(estimator):
-    return ivy.is_regressor
+    return ivy.is_regressor(estimator, "_estimator_type", None)
+
+
+def clone(estimator, *, safe=True):
+    return ivy.clone(estimator, "_estimator_type", None)
+
+
+def get_config():
+    return ivy.get_config()
--- ivy_tests/test_ivy/test_frontends/test_scikit_learn/test_base_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_scikit_learn/test_base_functions.py
@@ -1,4 +1,3 @@
-
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import assert_all_close
@@ -6,10 +5,94 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
 @handle_frontend_test(
+    fn_tree="scikit_learn.is_classifier",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+)
+def test_scikit_learn_is_classifier(
+        *,
+        dtype_and_x,
+        num_positional_args,
+        as_variable,
+        native_array,
+        frontend,
+        fn_tree,
+        on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
+@handle_frontend_test(
+    fn_tree="scikit-learn.is_regressor",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+)
+def test_scikit_learn_is_regressor(
+        *,
+        dtype_and_x,
+        num_positional_args,
+        as_variable,
+        native_array,
+        frontend,
+        fn_tree,
+        on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
+@handle_frontend_test(
     fn_tree="scikit-learn.is_classifier",
     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
 )
 def scikit_learn_is_classifier(
+        *,
+        dtype_and_x,
+        as_variable,
+        num_positional_args,
+        native_array,
+        on_device,
+        fn_tree,
+        frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
+@handle_frontend_test(
+    fn_tree="scikit-learn.get_config",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+)
+def scikit_learn_get_config(
         *,
         dtype_and_x,
         as_variable,
