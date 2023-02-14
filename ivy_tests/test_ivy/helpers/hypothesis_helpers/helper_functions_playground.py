557c550622275da1ab537a0745a7a60c3a89b449
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-02-15 02:52:04 +0300

    Added `helper_functions_playground(...)`

---
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/helper_functions_playground.py
@@ -0,0 +1,19 @@
+from hypothesis import strategies as st
+
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_test
+
+
+@handle_test(
+    fn_tree="functional.ivy.one_hot",
+
+    ### YOU SHOULD ONLY MODIFY THIS ###
+    helper_function_output=helpers.dtype_and_values(
+        num_arrays=2,
+        # ...
+    ),
+)
+def get_helper_function_output(
+        helper_function_output,
+):
+    print(helper_function_output)
