a73ce51d3fa287c6888e9943b26b751aa5f5c7b3
ali-aboelezz <countali22@gmail.com>
2023-02-14 17:26:17 +0200

    dropout first commit

---
+++ ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
@@ -0,0 +1,24 @@
+
+import ivy
+from ivy.func_wrapper import with_unsupported_dtypes
+
+from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
+
+
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+
+def dropout(input_, p=0.5, training=True, inplace=False):
+    if p<0 or p>1:
+        raise ValueError("dropout prob has to be between 0 and 1")
+    if training:
+       val = ivy.dropout(input_, p)
+    else :
+       val = input_
+    if inplace:
+       return input_
+
+    return val
+
+
+
