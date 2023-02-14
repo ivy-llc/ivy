f2c32a30ad0c23fd8dddf47691ba346ed2f1e710
Shehryar Tariq <sherrytst30@gmail.com>
2023-02-14 22:02:00 +0500

    making `size` argument chanel_last in `interpolate` jax backend

--- ivy/functional/backends/jax/experimental/layers.py
+++ ivy/functional/backends/jax/experimental/layers.py
@@ -442,7 +442,7 @@ def interpolate(
     # keeping the batch and channel dimension same
     dims = len(x.shape) - 2
     size = (size,) * dims if isinstance(size, int) else size
-    size = [*x.shape[0:2], *size]
+    size = [x.shape[0], *size, x.shape[1]]
 
     if align_corners or mode == "area":
         return ivy.interpolate(
