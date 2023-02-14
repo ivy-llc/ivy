bd3700e5b0751878caeaddf742a46797af9adec5
kaushal <kaushalc64@gmail.com>
2023-02-14 08:05:52 +0000

    raw_ops_and_tes_raw_ops_only

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -571,6 +571,38 @@ def Conv3D(
 
 
 @to_ivy_arrays_and_back
+def Conv2D(
+    *,
+    input,
+    filter,
+    strides,
+    padding,
+    data_format="NHWC",
+    dilations=[1, 1, 1, 1],
+    name="Conv2D",
+):
+    # ivy.backends.tensorflow expects strides and dilations to be
+    # a single integer value or a list of 3 values whereas the raw op
+    # expects a list of 4 values
+    if data_format == "NHWC":
+        strides = strides[1:-1]
+        dilations = dilations[1:-1]
+    elif data_format == "NCHW":
+        strides = strides[2:]
+        dilations = dilations[2:]
+
+    return tf_frontend.nn.conv2d(
+        input,
+        filter,
+        strides,
+        padding,
+        data_format=data_format,
+        dilations=dilations,
+        name=name,
+    )
+
+
+@to_ivy_arrays_and_back
 def Elu(features, name=None):
     zeros = ivy.zeros_like(features, dtype=ivy.dtype(features))
     ones = ivy.ones_like(features, dtype=ivy.dtype(features))
