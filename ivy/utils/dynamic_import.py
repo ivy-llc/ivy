627d6e63738619350821dd0841562ba93b6ea8af
CatB1t <skytedits@gmail.com>
2023-02-15 14:02:07 +0200

    fix invalid reference.

--- ivy/utils/dynamic_import.py
+++ ivy/utils/dynamic_import.py
@@ -5,5 +5,5 @@ from importlib import import_module as builtin_import
 
 def import_module(name, package=None):
     if ivy.is_local():
-        return ivy.utils.backend._ivy_import_module(name=name, package=package)
+        return ivy.utils._importlib._import_module(name=name, package=package)
     return builtin_import(name=name, package=package)
