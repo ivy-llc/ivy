fb28453fdf72a7a47d83ec98b9418e8bea7c8947
Suraj R <surajsrivaastav@gmail.com>
2023-02-15 12:20:07 +0530

    alpha_dropout #10549

--- ivy/functional/frontends/torch/nn/functional/dropout_functions.py
+++ ivy/functional/frontends/torch/nn/functional/dropout_functions.py
@@ -1,7 +1,6 @@
 # local
 import ivy
 from ivy.func_wrapper import with_unsupported_dtypes
-
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
@@ -12,6 +11,21 @@ def dropout(input, p=0.5, training=True, inplace=False):
         ret = input
     else:
         ret = ivy.dropout(input, p)
+    if inplace:
+        ivy.inplace_update(input, ret)
+        return input
+    return ret
+
+
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+def alpha_dropout(input, p=0.5, training=True, inplace=False):
+    if not training:
+        ret = input
+    if p < 0.0 or p > 1.0:
+        raise ValueError("dropout probability must be within 0 and 1")
+    else:
+        ret = ivy.alpha_dropout(input, p)
     if inplace:
         ivy.inplace_update(input, ret)
         return input
