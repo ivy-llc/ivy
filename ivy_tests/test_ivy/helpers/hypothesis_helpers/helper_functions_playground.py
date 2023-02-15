ff859e220c3412b0daaec0180114f144eb86567e
ZiadAmerr <xeiad.amerr@yahoo.com>
2023-02-15 12:49:07 +0300

    Added `helper_functions_playground(...)`

--- ivy_tests/test_ivy/helpers/hypothesis_helpers/helper_functions_playground.py
+++ ivy_tests/test_ivy/helpers/hypothesis_helpers/helper_functions_playground.py
@@ -7,7 +7,7 @@ from ivy_tests.test_ivy.helpers import handle_test
 @handle_test(
     fn_tree="functional.ivy.one_hot",
 
-    ### YOU SHOULD ONLY MODIFY THIS ###
+    # YOU SHOULD ONLY MODIFY THIS #
     helper_function_output=helpers.dtype_and_values(
         num_arrays=2,
         # ...
