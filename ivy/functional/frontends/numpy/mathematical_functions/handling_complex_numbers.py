19302bf7abc252b13d5f8e6e08078a9f49945477
pawel <pawel.magnu@gmail.com>
2023-02-15 17:20:18 +0100

    numpy.angle function

--- ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
+++ ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
@@ -8,3 +8,8 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 @to_ivy_arrays_and_back
 def _imag(val):
     return ivy.imag(val)
+
+
+@to_ivy_arrays_and_back
+def _angle(val,deg=False,) -> ivy.Array:
+    return ivy.angle(val, deg=deg)
