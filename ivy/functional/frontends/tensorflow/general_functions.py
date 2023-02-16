36349c5e267c3aa1dddbad6e1bcfacd2c49966ca
Stanley Njenga <snjenga631@gmail.com>
2023-02-16 11:49:03 +0300

    lint fix

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -344,6 +344,7 @@ def where(condition: ivy.array, x=None, y=None, name=None):
 @to_ivy_arrays_and_back
 def reverse(tensor, axis, name=None):
     return ivy.flip(tensor, axis=axis)
+    
 
 def roll(input, shift, axis, name=None):
     return ivy.roll(input, shift, axis=axis)
