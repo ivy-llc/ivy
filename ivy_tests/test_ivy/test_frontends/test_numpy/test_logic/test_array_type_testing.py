3c8bb082faeb00f154c427d88941b64ff2d515d3
zuruoke <zuruokeokafor2016@gmail.com>
2023-02-20 20:17:40 +0000

    add test for iscomplex  #10626

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_type_testing.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_logic/test_array_type_testing.py
@@ -147,3 +147,51 @@ def test_numpy_isnan(
         dtype=dtype,
         subok=True,
     )
+
+
+@handle_frontend_test(
+    fn_tree="numpy.iscomplex",
+    dtypes_values_casting=np_frontend_helpers.dtypes_values_casting_dtype(
+        arr_func=[
+            lambda: helpers.dtype_and_values(
+                available_dtypes=helpers.get_dtypes("float"),
+                min_value=-np.inf,
+                max_value=np.inf,
+            )
+        ],
+        special=True,
+    ),
+    where=np_frontend_helpers.where(),
+    number_positional_args=np_frontend_helpers.get_num_positional_args_ufunc(
+        fn_name="iscomplex"
+    ),
+)
+def test_numpy_iscomplex(
+    dtypes_values_casting,
+    where,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+
+    input_dtypes, x, casting, dtype = dtypes_values_casting
+    where, input_dtypes, test_flags = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtypes,
+        test_flags=test_flags,
+    )
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        out=None,
+        where=where,
+        casting=casting,
+        order="K",
+        dtype=dtype,
+        subok=True,
+    )
