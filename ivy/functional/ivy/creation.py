8a32358748c5d02fcb2fd154b8e5f9d2cc9d4573
infrared1029 <yasserfawzi1029@outlook.com>
2023-02-20 15:50:45 +0200

    fixes a bug where ivy.array creates arrays on default device despite the provided device if the input is a list, removed an unused import

--- ivy/functional/ivy/creation.py
+++ ivy/functional/ivy/creation.py
@@ -9,7 +9,6 @@ import numpy as np
 import ivy
 from ivy import to_ivy
 from ivy.utils.backend import current_backend
-from ivy.exceptions import handle_exceptions
 from ivy.func_wrapper import (
     handle_array_function,
     infer_device,
@@ -121,7 +120,10 @@ def asarray_infer_device(fn: Callable) -> Callable:
             The return of the function, with `device` passed explicitly.
         """
         if isinstance(args[0], list):
-            return fn(*args, device=ivy.default_device(as_native=True), **kwargs)
+            device = (
+                device if ivy.exists(device) else ivy.default_device(as_native=True)
+            )
+            return fn(*args, device=device, **kwargs)
 
         # find the first array argument, if required
         arr = None if ivy.exists(device) else args[0]
