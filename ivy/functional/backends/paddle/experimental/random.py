d91068a64376914003841ba622ccc2ddbf4935d4
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-21 17:56:45 +0200

    Update random.py

--- ivy/functional/backends/paddle/experimental/random.py
+++ ivy/functional/backends/paddle/experimental/random.py
@@ -1,7 +1,7 @@
 # global
 from typing import Optional, Union, Sequence
 import paddle
-from ivy.exceptions import IvyNotImplementedException
+from ivy.utils.exceptions import IvyNotImplementedException
 
 # local
 import ivy
