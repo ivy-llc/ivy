683ab0253948de7e53a6ff7f6ab492f698e3efcb
KevinUnadkat <kevin098unadkat@gmail.com>
2023-02-21 21:00:21 +0530

    made the required changes in utilities

--- ivy/functional/backends/paddle/utility.py
+++ ivy/functional/backends/paddle/utility.py
@@ -12,7 +12,21 @@ def all(
     keepdims: bool = False,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+    x = paddle.as_tensor(x).type(paddle.bool)
+    if axis is None:
+        num_dims = len(x.shape)
+        axis = list(range(num_dims))
+    if isinstance(axis, int):
+        return paddle.all(x, dim=axis, keepdim=keepdims, out=out)
+    dims = len(x.shape)
+    axis = [i % dims for i in axis]
+    axis.sort()
+    for i, a in enumerate(axis):
+        x = paddle.all(x, dim=a if keepdims else a - i, keepdim=keepdims, out=out)
+    return x
+
+
+all.support_native_out = True
 
 
 def any(
