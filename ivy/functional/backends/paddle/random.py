cbe226ef4337b7d84a8d47539e26eb7a40b0b577
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-21 17:53:59 +0200

    Update random.py

--- ivy/functional/backends/paddle/random.py
+++ ivy/functional/backends/paddle/random.py
@@ -6,7 +6,7 @@ from typing import Optional, Union, Sequence
 
 # local
 import ivy
-from ivy.exceptions import IvyNotImplementedException
+from ivy.utils.exceptions import IvyNotImplementedException
 from . import backend_version
 from paddle.fluid.libpaddle import Place
 # Extra #
