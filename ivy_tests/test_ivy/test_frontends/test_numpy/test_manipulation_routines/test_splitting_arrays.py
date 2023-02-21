49720ce6192380772e9523bfb7e0ca5ebc9b6a4e
Bao <xoiga123@outlook.com.vn>
2023-02-21 10:49:25 +0700

    add frontends.numpy.dsplit and test

---
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_splitting_arrays.py
@@ -0,0 +1,40 @@
+# global
+from hypothesis import strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+from ivy_tests.test_ivy.test_functional.test_experimental.test_core.test_manipulation import (  # noqa
+    _get_split_locations,
+)
+
+
+# dsplit
+@handle_frontend_test(
+    fn_tree="numpy.dsplit",
+    dtype_value=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        shape=st.shared(helpers.get_shape(min_num_dims=3), key="value_shape"),
+    ),
+    indices_or_sections=_get_split_locations(min_num_dims=3, axis=2),
+    test_with_out=st.just(False),
+)
+def test_numpy_dsplit(
+    *,
+    dtype_value,
+    indices_or_sections,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, value = dtype_value
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        ary=value[0],
+        indices_or_sections=indices_or_sections,
+    )
