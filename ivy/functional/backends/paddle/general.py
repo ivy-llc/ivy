9a473eb7b71984b759b9a8730c5a71798c02d03f
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-21 17:53:17 +0200

    Update general.py

--- ivy/functional/backends/paddle/general.py
+++ ivy/functional/backends/paddle/general.py
@@ -8,7 +8,7 @@ import paddle
 import numpy as np
 # local
 import ivy
-from ivy.exceptions import IvyNotImplementedException
+from ivy.utils.exceptions import IvyNotImplementedException
 
 from . import backend_version
 
