8bbb0644eb564d30291198a09ca79db33ba0afbd
Saurabh K Singh <72030498+singh1203@users.noreply.github.com>
2023-02-21 18:06:39 +0530

    left_shift #10568 (#10572)
    * Update logic.py
    
    Added left_shift for frontend.
    
    * Update test_jax_numpy_logic.py
    
    * Update test_jax_numpy_logic.py
    
    * Update test_jax_numpy_logic.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_logic.py
@@ -767,3 +767,30 @@ def test_jax_numpy_isscalar(
         on_device=on_device,
         x=x[0],
     )
+
+    
+# left_shift
+@handle_frontend_test(
+    fn_tree="jax.numpy.left_shift",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+)
+def test_jax_numpy_left_shift(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+    
