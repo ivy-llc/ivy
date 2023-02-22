9c460246b68b73628f5aed831412e080709c740c
nthemist <neofytos.themistokleous@cern.ch>
2023-02-22 20:40:11 +0000

    implemented squared_difference function for Tensorflow frontend

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -185,6 +185,36 @@ def test_tensorflow_subtract(
     )
 
 
+# squared_difference
+@handle_frontend_test(
+    fn_tree="tensorflow.math.squared_difference",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_squared_difference(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
+
+
 # logical_xor
 @handle_frontend_test(
     fn_tree="tensorflow.math.logical_xor",
