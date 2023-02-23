555eae8f3d7d395311a1cace1bcebadd1458e24f
KevinUnadkat <kevin098unadkat@gmail.com>
2023-02-23 16:30:36 +0530

    Adding angle from Pointwise Ops to pytorch frontend

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2115,3 +2115,29 @@ def test_torch_lerp(
         end=end,
         weight=weight,
     )
+
+
+# angle
+@handle_frontend_test(
+    fn_tree="torch.angle",
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_torch_angle(
+    *,
+    dtype_and_input,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+    )
