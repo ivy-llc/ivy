1be863b957e67ccd519da92a6157ee78d9240a7d
ndurumo254 <erickndurumo@gmail.com>
2023-02-23 23:41:08 +0300

    implimenting the atleast_2d function

--- ivy/functional/frontends/jax/numpy/manipulations.py
+++ ivy/functional/frontends/jax/numpy/manipulations.py
@@ -88,14 +88,14 @@ def stack(arrays, axis=0, out=None, dtype=None):
 
 @to_ivy_arrays_and_back
 def take(
-    a,
-    indices,
-    axis=None,
-    out=None,
-    mode=None,
-    unique_indices=False,
-    indices_are_sorted=False,
-    fill_value=None,
+        a,
+        indices,
+        axis=None,
+        out=None,
+        mode=None,
+        unique_indices=False,
+        indices_are_sorted=False,
+        fill_value=None,
 ):
     return ivy.take_along_axis(a, indices, axis, out=out)
 
@@ -136,3 +136,8 @@ def atleast_2d(*arys):
 @to_ivy_arrays_and_back
 def squeeze(a, axis=None):
     return ivy.squeeze(a, axis)
+
+
+@to_ivy_arrays_and_back
+def atleast_2d(*arys):
+    return ivy.atleast_2d(*arys)
