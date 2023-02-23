77cd844b43ab0e5d67bf6e4dc1e5d1b4128e3e73
ali-aboelezz <countali22@gmail.com>
2023-02-23 18:22:20 +0200

    function change

--- ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
+++ ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
@@ -8,17 +8,14 @@ from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
 
-def dropout(input_, p=0.5, training=True, inplace=False):
+def dropout(input, p=0.5, training=True, inplace=False):
     if p<0 or p>1:
         raise ValueError("dropout prob has to be between 0 and 1")
     if training:
-       val = ivy.dropout(input_, p)
+       val = ivy.dropout(input, p)
     else :
-       val = input_
+       val = input
     if inplace:
-       return input_
+       return input
 
     return val
-
-
-
