1ac2e18aa27d8e655362f5b2cdeeb8d875047d10
aparajith21 <aparajithr21@gmail.com>
2023-02-23 19:55:54 +0530

    sqrt

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -440,3 +440,8 @@ def abs(x, name=None):
 @to_ivy_arrays_and_back
 def log_softmax(logits, axis=None):
     return ivy.log_softmax(logits, axis=axis)
+
+
+@to_ivy_arrays_and_back
+def sqrt(x, name=None):
+    return ivy.sqrt(x)
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1497,3 +1497,30 @@ def test_tensorflow_abs(
         rtol=1e-02,
         x=x[0],
     )
+
+# sqrt
+@handle_frontend_test(
+    fn_tree="tensorflow.math.sqrt",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_sqrt(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        rtol=1e-02,
+        x=x[0],
+    )
