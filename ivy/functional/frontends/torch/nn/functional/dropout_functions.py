193ec2d0997cc6ef1648a9d1bf676ea669396481
Nasser <nmohammed02@yahoo.com>
2023-02-24 11:27:01 -0600

    dropout1d

--- ivy/functional/frontends/torch/nn/functional/dropout_functions.py
+++ ivy/functional/frontends/torch/nn/functional/dropout_functions.py
@@ -3,6 +3,8 @@ import ivy
 from ivy.func_wrapper import with_unsupported_dtypes
 
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
+
+
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
 def dropout(input, p=0.5, training=True, inplace=False):
