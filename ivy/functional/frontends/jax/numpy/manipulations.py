2535ebae9103b85e96a7dd30150c26c3d17c82b5
ndurumo254 <erickndurumo@gmail.com>
2023-02-24 19:40:48 +0300

    removing unused function 'atleast_2d' from line 132

--- ivy/functional/frontends/jax/numpy/manipulations.py
+++ ivy/functional/frontends/jax/numpy/manipulations.py
@@ -129,11 +129,6 @@ def atleast_1d(*arys):
 
 
 @to_ivy_arrays_and_back
-def atleast_2d(*arys):
-    return ivy.atleast_2d(*arys)
-
-
-@to_ivy_arrays_and_back
 def squeeze(a, axis=None):
     return ivy.squeeze(a, axis)
 
