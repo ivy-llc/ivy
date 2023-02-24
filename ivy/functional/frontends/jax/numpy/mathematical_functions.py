05de2f5bab69d0a77e118b306fa0269e62263ee0
Ario Zareinia <ariozareinia@gmail.com>
2023-02-24 17:50:08 +0000

    Type restrictions

--- ivy/functional/frontends/jax/numpy/mathematical_functions.py
+++ ivy/functional/frontends/jax/numpy/mathematical_functions.py
@@ -58,7 +58,16 @@ def dot(a, b, *, precision=None):
 def floor(x):
     return ivy.floor(x)
 
-
+@with_unsupported_dtypes(
+    {
+        "0.3.14 and below": (
+            "bfloat16",
+            "float16",
+            "float32",
+        )
+    },
+    "jax",
+)
 @to_ivy_arrays_and_back
 def gradient(f, *varargs, axis=None, edge_order=None):
     return ivy.gradient(f, varargs, axis=axis, edge_order=edge_order)
