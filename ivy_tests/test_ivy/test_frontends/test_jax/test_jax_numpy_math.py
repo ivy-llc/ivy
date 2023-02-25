e5183c678981151e6bb58f7ed75fb31d0ae3cd44
usama baloch <usamabaloch152001@gmail.com>
2023-02-25 14:22:34 +0500

    added reciprocal function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -2300,3 +2300,33 @@ def test_jax_numpy_inner(
         a=xs[0],
         b=xs[1],
     )
+
+
+# reciprocal
+@handle_frontend_test(
+    fn_tree="jax.numpy.reciprocal",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        small_abs_safety_factor=4,
+        large_abs_safety_factor=4,
+        safety_factor_scale="log",
+        num_arrays=1,
+    ),
+)
+def test_jax_numpy_reciprocal(
+    *,
+    dtype_and_x,
+    test_flags,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
