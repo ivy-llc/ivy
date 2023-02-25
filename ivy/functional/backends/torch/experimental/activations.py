fdac0355de7c61d58539601e97caf8e83ef8940b
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-25 21:25:37 +0200

    Reimplement ReLu6 in the backends and frontends (#10587)
    Co-authored-by: CatB1t <skytedits@gmail.com>

--- ivy/functional/backends/torch/experimental/activations.py
+++ ivy/functional/backends/torch/experimental/activations.py
@@ -23,3 +23,10 @@ def thresholded_relu(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     return torch.threshold(x, threshold=threshold, value=0)
+
+
+@with_unsupported_dtypes({"1.11.0 and below": ("bfloat16", "float16")}, backend_version)
+def relu6(x: torch.Tensor, /, *, out: Optional[torch.Tensor] = None) -> torch.Tensor:
+    return torch.nn.functional.relu6(x)
+
+relu6.unsupported_dtypes = ("float16", "bfloat16",)
