b85d782ee135b9eb1ab0c9bf7a3b23c42e88f99b
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-25 21:25:37 +0200

    Reimplement ReLu6 in the backends and frontends (#10587)
    Co-authored-by: CatB1t <skytedits@gmail.com>

--- ivy/functional/backends/jax/experimental/activations.py
+++ ivy/functional/backends/jax/experimental/activations.py
@@ -1,8 +1,11 @@
 from typing import Optional, Union
 
 # global
+import jax
 import jax.numpy as jnp
 from ivy.functional.backends.jax import JaxArray
+from jax import lax
+import ivy
 
 
 def logit(x: JaxArray, /, *, eps: Optional[float] = None, out=None):
@@ -11,6 +14,20 @@ def logit(x: JaxArray, /, *, eps: Optional[float] = None, out=None):
     else:
         x = jnp.clip(x, eps, 1 - eps)
     return jnp.log(x / (1 - x))
+
+
+def relu6(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
+    relu6_func = jax.nn.relu6
+
+    # sets gradient at 0 and 6 to 0 instead of 0.5
+    # can refactor to jax.nn.relu6 when this PR is merged
+    # https://github.com/google/jax/pull/14682 
+    def custom_grad_func(x_and_grad, one): return lax.select(
+        (6 > x_and_grad[0]) & (x_and_grad[0] > 0), one, lax.full_like(one, 0))
+
+    new_func = ivy.bind_custom_gradient_function(relu6_func, custom_grad_func)
+
+    return new_func(x).astype(x.dtype)
 
 
 def thresholded_relu(
