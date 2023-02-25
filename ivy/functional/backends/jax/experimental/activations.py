339d8cb504e6595b81fe8c16ad4f4997fbbb232f
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-02-25 21:20:17 +0200

    update jax.nn.relu6 to fix gradients at boundary conditions

--- ivy/functional/backends/jax/experimental/activations.py
+++ ivy/functional/backends/jax/experimental/activations.py
@@ -4,6 +4,8 @@ from typing import Optional, Union
 import jax
 import jax.numpy as jnp
 from ivy.functional.backends.jax import JaxArray
+from jax import lax
+import ivy
 
 
 def logit(x: JaxArray, /, *, eps: Optional[float] = None, out=None):
@@ -15,8 +17,17 @@ def logit(x: JaxArray, /, *, eps: Optional[float] = None, out=None):
 
 
 def relu6(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
-    x_dtype = x.dtype
-    return jax.nn.relu6(x).astype(x_dtype)
+    relu6_func = jax.nn.relu6
+
+    # sets gradient at 0 and 6 to 0 instead of 0.5
+    # can refactor to jax.nn.relu6 when this PR is merged
+    # https://github.com/google/jax/pull/14682 
+    def custom_grad_func(x_and_grad, one): return lax.select(
+        (6 > x_and_grad[0]) & (x_and_grad[0] > 0), one, lax.full_like(one, 0))
+
+    new_func = ivy.bind_custom_gradient_function(relu6_func, custom_grad_func)
+
+    return new_func(x).astype(x.dtype)
 
 
 def thresholded_relu(
