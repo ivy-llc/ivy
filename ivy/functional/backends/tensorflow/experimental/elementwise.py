03e13d13244edf957ca1f2f848fbc15c9bd70b24
pillarxyz <ayman.lafaz@um5r.ac.ma>
2023-02-25 18:32:39 +0100

    small workaround for tf producing int dtypes

--- ivy/functional/backends/tensorflow/experimental/elementwise.py
+++ ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -636,6 +636,8 @@ def ldexp(
     if tf.math.reduce_any(tf.math.less(x2, 0)):
         pos_exp = tf.cast(tf.math.greater_equal(x2, 0), x2.dtype) * x2
         neg_exp = tf.cast(tf.math.less(x2, 0), x2.dtype) * x2
-        return tf.math.multiply(x1, tf.math.pow(2, pos_exp)) / tf.math.pow(2, -neg_exp)
+        return tf.math.multiply(x1, tf.math.pow(2.0, pos_exp)) / tf.math.pow(
+            2, -neg_exp
+        )
     else:
-        return tf.math.multiply(x1, tf.math.pow(2, x2))
+        return tf.math.multiply(x1, tf.math.pow(2.0, x2))
