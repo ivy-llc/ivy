1dc384f728364ba286e0d05377cdf152985cd418
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2023-02-26 13:33:03 +0530

    Adding log_ (#10489)
    Added log instance method to the PyTorch frontend
    
    Co-authored by: Yusha Arif <101613943+YushaArif99@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -1320,6 +1320,38 @@ def test_torch_instance_log(
     )
 
 
+# log_
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="log_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        allow_inf=False,
+    ),
+)
+def test_torch_instance_log_(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+    )
+
+
 # __add__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
