73d827e0fe91340390e9ade5b55bf7528131f1ed
somaia02 <somaia.0246@gmail.com>
2023-02-26 10:14:03 +0000

    added frontend numpy copyto

--- ivy/functional/frontends/numpy/manipulation_routines/basic_operations.py
+++ ivy/functional/frontends/numpy/manipulation_routines/basic_operations.py
@@ -1,6 +1,16 @@
 # local
 import ivy
-from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
+from ivy.functional.frontends.numpy.func_wrapper import (
+    inputs_to_ivy_arrays,
+    handle_numpy_casting,
+)
+
+
+@inputs_to_ivy_arrays
+@handle_numpy_casting
+def copyto(dst, src, /, *, casting="same_kind", where=True):
+    if ivy.is_array(where):
+        ivy.where(where, src, dst, out=dst)
 
 
 @inputs_to_ivy_arrays
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_basic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_basic_operations.py
@@ -3,7 +3,58 @@ from hypothesis import strategies as st
 
 # local
 import ivy_tests.test_ivy.helpers as helpers
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# copyto
+@handle_frontend_test(
+    fn_tree="numpy.copyto",
+    dtypes_values_casting=np_frontend_helpers.dtypes_values_casting_dtype(
+        arr_func=[
+            lambda: helpers.dtype_and_values(
+                available_dtypes=helpers.get_dtypes("valid"),
+                num_arrays=2,
+                shared_dtype=True,
+                min_num_dims=1,
+            )
+        ],
+    ),
+    test_with_out=st.just(False),
+    where=np_frontend_helpers.where(),
+)
+def test_numpy_copyto(
+    dtypes_values_casting,
+    where,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtypes, xs, casting, _ = dtypes_values_casting
+    src = xs[0]
+    dst = xs[1]
+    where, input_dtypes, test_flags = np_frontend_helpers.handle_where_and_array_bools(
+        where=where, input_dtype=input_dtypes, test_flags=test_flags
+    )
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        src=src,
+        dst=dst,
+        where=where,
+        casting=casting,
+        test_values=False,
+    )
+    w = helpers.flatten_and_to_np(ret=where)
+    dst_f = helpers.flatten_and_to_np(ret=dst)
+    src_f = helpers.flatten_and_to_np(ret=src)
+    for i, j, c in zip(src_f, dst_f, w):
+        if c:
+            assert i == j
 
 
 # shape
