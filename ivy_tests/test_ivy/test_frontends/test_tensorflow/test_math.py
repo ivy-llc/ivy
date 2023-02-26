c975fd81951a75fbb6d53618435ee60f6ef6d5a4
Satyam Bansal <sbansal1999@gmail.com>
2023-02-26 11:03:53 +0530

    asin (#10857)
    Added tf.asin  to the TF frontend
    
    Co-authored-by: Yusha Arif <101613943+YushaArif99@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1552,3 +1552,32 @@ def test_tensorflow_abs(
         rtol=1e-02,
         x=x[0],
     )
+    
+    
+# asin
+@handle_frontend_test(
+    fn_tree="tensorflow.math.asin",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=-1,
+        max_value=1,
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_asin(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
