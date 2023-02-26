ba38a767d7f193b14022e3e99bec9b01094ad095
Sankalp <sankalp123427@gmail.com>
2023-02-26 07:34:23 +0530

    feat: Added signbit function for torch frontend (#10713)
    Co-authored-by: nathzi1505 <41519676+nathzi1505@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2115,3 +2115,29 @@ def test_torch_lerp(
         end=end,
         weight=weight,
     )
+
+
+# signbit
+@handle_frontend_test(
+    fn_tree="torch.signbit",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+    ),
+)
+def test_torch_signbit(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+    )
