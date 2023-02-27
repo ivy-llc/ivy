38f114781b42ad2435f7c36fcb4fb06d77f8f02d
Ayman Lafaz <mail@aymanlafaz.com>
2023-02-27 18:38:31 +0100

    bincount experimental implementation (#11116)
    Co-authored-by: Fotios Spyridakos <72918194+fspyridakos@users.noreply.github.com>

--- ivy/functional/backends/torch/experimental/statistical.py
+++ ivy/functional/backends/torch/experimental/statistical.py
@@ -127,3 +127,23 @@ def unravel_index(
 
 
 unravel_index.support_native_out = False
+
+
+def bincount(
+    x: torch.Tensor,
+    /,
+    *,
+    weights: Optional[torch.Tensor] = None,
+    minlength: Optional[int] = 0,
+    out: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+    if weights is None:
+        ret = torch.bincount(x, minlength=minlength)
+        ret = ret.to(x.dtype)
+    else:
+        ret = torch.bincount(x, weights=weights, minlength=minlength)
+        ret = ret.to(weights.dtype)
+    return ret
+
+
+bincount.support_native_out = False
