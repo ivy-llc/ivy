8383c4cb03d6a1369face71985385e857b9e7a68
Sravanth Kurmala <77894804+Sravanthgithub@users.noreply.github.com>
2023-02-28 04:55:22 +0530

    tf math sqrt, negative (#11118)
    Co-authored-by: zaeemansari70 <zaeemansari70@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -118,6 +118,56 @@ def test_tensorflow_exp(
         on_device=on_device,
         x=x[0],
     )
+    
+    
+# sqrt
+@handle_frontend_test(
+    fn_tree="tensorflow.math.sqrt",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_sqrt(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+    
+    
+# negative
+@handle_frontend_test(
+    fn_tree="tensorflow.math.negative",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_negative(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
 
 
 # multiply
