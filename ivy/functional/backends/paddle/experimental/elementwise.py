422eb1fce348c3c0b0db1281cbb77511860218af
vandit98 <grindandeat786@gmail.com>
2023-02-28 22:46:25 +0530

    added all necessary changes

--- ivy/functional/backends/paddle/experimental/elementwise.py
+++ ivy/functional/backends/paddle/experimental/elementwise.py
@@ -122,12 +122,14 @@ def nansum(
     keepdims: Optional[bool] = False,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    return paddle.nansum(x)
+    if out is not None:
+        raise ValueError("The 'out' argument is not supported in this implementation.")
+    return paddle.nansum(x, axis=axis, dtype=dtype)
 
 
 def gcd(
-    x1: Union[paddle.Tensor, int, list, tuple]=paddle.to_tensor(),
-    x2: Union[paddle.Tensor,int, float, list, tuple]=paddle.to_tensor(),
+    x1: Union[paddle.Tensor, int, list, tuple],
+    x2: Union[paddle.Tensor, float, list, tuple],
     /,
     *,
     out: Optional[paddle.Tensor] = None,
@@ -145,17 +147,25 @@ def isclose(
     equal_nan: Optional[bool] = False,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
+    if out is not None:
+        raise ValueError("The 'out' argument is not supported in this implementation.")
     return paddle.isclose(a, b, rtol=rtol, atol=atol, equal_nan=equal_nan)
 
 
 def angle(
-    input: paddle.Tensor,
+    input_tensor: paddle.Tensor,
     /,
     *,
     deg: Optional[bool] = None,
     out: Optional[paddle.Tensor] = None,
+    name: Optional[str] = None,
 ) -> paddle.Tensor:
-    return paddle.angle(input)
+    if deg is not None:
+        raise ValueError("The 'deg' argument is not supported in this implementation.")
+    if out is not None:
+        return paddle.angle(input_tensor, out=out, name=name)
+    else:
+        return paddle.angle(input_tensor, name=name)
 
 
 def imag(
@@ -177,7 +187,20 @@ def nan_to_num(
     neginf: Optional[Union[float, int]] = None,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    return paddle.nan_to_num(x, nan=nan, posinf=posinf, neginf=neginf)
+    if copy:
+        x = x.clone()
+    nan_mask = paddle.isnan(x)
+    if nan_mask.any():
+        x[nan_mask] = nan
+    if posinf is not None:
+        posinf_mask = paddle.isinf(x) & (x > 0)
+        if posinf_mask.any():
+            x[posinf_mask] = posinf
+    if neginf is not None:
+        neginf_mask = paddle.isinf(x) & (x < 0)
+        if neginf_mask.any():
+            x[neginf_mask] = neginf
+    return x
 
 
 
@@ -192,16 +215,20 @@ def logaddexp2(
 
 
 def diff(
-     x: Union[paddle.Tensor, list, tuple]=paddle.to_tensor(),
+    x: Union[paddle.Tensor, list, tuple],
     /,
     *,
     n: int = 1,
     axis: int = -1,
-    prepend: Optional[Union[paddle.Tensor, int, float, list, tuple]] = paddle.to_tensor(),
-    append: Optional[Union[paddle.Tensor, int, float, list, tuple]] = paddle.to_tensor(),
+    prepend: Optional[Union[paddle.Tensor, int, float, list, tuple]] = None,
+    append: Optional[Union[paddle.Tensor, int, float, list, tuple]] = None,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
+    x=paddle.to_tensor(x)
+    if out is not None:
+        raise ValueError("The 'out' argument is not supported in this implementation.")
     return paddle.diff(x, n=n, axis=axis, prepend=prepend, append=append)
+
 
 def signbit(
     x: Union[paddle.Tensor, float, int, list, tuple],
