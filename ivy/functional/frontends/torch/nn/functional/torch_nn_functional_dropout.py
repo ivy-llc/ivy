4adb3022e3142150ff44b15c5a5f17a7b4f0bdfc
ali-aboelezz <countali22@gmail.com>
2023-03-01 21:48:57 +0200

    lints

--- ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
+++ ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
@@ -1,19 +1,12 @@
 # local
 import ivy
 from ivy.func_wrapper import with_unsupported_dtypes
-
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
-@with_unsupported_dtypes({'1.11.0 and below': ('float16', )}, 'torch')
-def dropout(
-    input,
-    p=0.5,
-    training=True,
-    inplace=False,
-    ):
-
+@with_unsupported_dtypes({'1.11.0 and below': ('float16',)}, 'torch')
+def dropout(input, p=0.5, training=True, inplace=False):
     if training:
         return ivy.dropout(input, p, inplace=inplace)
     else:
