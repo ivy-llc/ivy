4c4b66c39a61a3473972ae01d8743a26660aec63
ali-aboelezz <countali22@gmail.com>
2023-03-01 21:33:48 +0200

    lint error fixing

--- ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
+++ ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
@@ -6,11 +6,15 @@ from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
-@with_unsupported_dtypes( { "1.11.0 and below" : ( "float16" , ) } , "torch" )
-
-def dropout( input , p = 0.5 , training = True , inplace = False ) :
+@with_unsupported_dtypes({'1.11.0 and below': ('float16', )}, 'torch')
+def dropout(
+    input,
+    p=0.5,
+    training=True,
+    inplace=False,
+    ):
 
     if training:
-        return ivy.dropout( input, p, inplace = inplace )
-    else :
+        return ivy.dropout(input, p, inplace=inplace)
+    else:
         return input
