16440744a07db40980f33f8ccf8703c421d5ccb9
Nouran Khaled Soliman <nouran.youssef99@eng-st.cu.edu.eg>
2023-03-01 09:39:33 +0200

    np frontend tri, triu (#11151)

--- ivy/functional/frontends/numpy/creation_routines/building_matrices.py
+++ ivy/functional/frontends/numpy/creation_routines/building_matrices.py
@@ -1,10 +1,27 @@
 import ivy
-from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
+from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
+    handle_numpy_dtype,
+)
 
 
 @to_ivy_arrays_and_back
 def tril(m, k=0):
     return ivy.tril(m, k=k)
+
+
+@to_ivy_arrays_and_back
+def triu(m, k=0):
+    return ivy.triu(m, k=k)
+
+
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+def tri(N, M=None, k=0, dtype="float64", *, like=None):
+    if M is None:
+        M = N
+    ones = ivy.ones((N, M), dtype=dtype)
+    return ivy.tril(ones, k=k)
 
 
 @to_ivy_arrays_and_back
