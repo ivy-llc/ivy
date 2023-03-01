af13c30239dfe0f575b1c02e037f65f556cadb5b
ali-aboelezz <countali22@gmail.com>
2023-03-01 22:10:42 +0200

    error fixing

--- ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
+++ ivy/functional/frontends/torch/nn/functional/torch_nn_functional_dropout.py
@@ -7,6 +7,7 @@ from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes({'1.11.0 and below': ('float16',)}, 'torch')
 def dropout(input, p=0.5, training=True, inplace=False):
+    input = ivy.cast(input, 'float32')
     if training:
         return ivy.dropout(input, p, inplace=inplace)
     else:
