486931226f7aebc0d364047fae6b8bbdf0e1a6f0
= <=>
2023-03-02 16:18:48 +0530

    added take_along_axis in jax

--- ivy/functional/frontends/jax/numpy/indexing.py
+++ ivy/functional/frontends/jax/numpy/indexing.py
@@ -13,3 +13,8 @@ def diagonal(a, offset=0, axis1=0, axis2=1):
 @to_ivy_arrays_and_back
 def diag(v, k=0):
     return ivy.diag(v, k=k)
+
+
+@to_ivy_arrays_and_back
+def take_along_axis(a, indices, axis):
+    return ivy.take_along_axis(a, indices, axis)
