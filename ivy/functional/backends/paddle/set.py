44810db1bad4472f9ea3cfa6f66f80a5a0fa1b4a
Maria <mariafiroz9@gmail.com>
2023-03-02 20:54:33 +0800

    unique_all implementation

--- ivy/functional/backends/paddle/set.py
+++ ivy/functional/backends/paddle/set.py
@@ -13,7 +13,19 @@ def unique_all(
     x: paddle.Tensor,
     /,
 ) -> Tuple[paddle.Tensor, paddle.Tensor, paddle.Tensor, paddle.Tensor]:
-    raise IvyNotImplementedException()
+        # Flatten the tensor to 1D
+        flat_x = paddle.flatten(x)
+        sorted_x = paddle.sort(flat_x)[0]
+        indices = paddle.where(sorted_x[1:] != sorted_x[:-1])[0] + 1
+        indices = paddle.concat([paddle.to_tensor([0]), indices, paddle.to_tensor([len(flat_x)])])
+        if len(indices) == 2:
+            unique_x = sorted_x[indices[:-1]]
+            counts = paddle.to_tensor([len(flat_x)])
+        else:
+            unique_x = sorted_x[indices[:-1]]
+            counts = indices[1:] - indices[:-1]
+
+        return unique_x
 
 
 def unique_counts(x: paddle.Tensor, /) -> Tuple[paddle.Tensor, paddle.Tensor]:
