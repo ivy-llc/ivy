e43bf5d4b155971633a16470dcfb2849eeaf22fe
AdvancedUno <ehlee8276@gmail.com>
2023-03-02 14:50:20 -0500

    feat: implement paddle unique_counts function

--- ivy/functional/backends/paddle/set.py
+++ ivy/functional/backends/paddle/set.py
@@ -2,7 +2,7 @@
 import paddle
 from typing import Tuple, Optional
 from collections import namedtuple
-
+import paddle.fluid as fluid
 # local
 
 from . import backend_version
@@ -17,7 +17,17 @@ def unique_all(
 
 
 def unique_counts(x: paddle.Tensor, /) -> Tuple[paddle.Tensor, paddle.Tensor]:
-    raise IvyNotImplementedException()
+    unique, counts = paddle.unique(x, return_counts=True)
+    nan_count = paddle.count_nonzero(paddle.where(paddle.isnan(x) > 0)).numpy()[0]
+
+    if nan_count > 0:
+        unique_nan = paddle.full(shape=[1, nan_count], fill_value=float('nan'), dtype=x.dtype)
+        counts_nan = paddle.full(shape=[1, nan_count], fill_value=1, dtype=x.dtype)
+        unique = fluid.layers.concat(input=[unique.astype(x.dtype), paddle.reshape(unique_nan, [nan_count])], axis=0)
+        counts = fluid.layers.concat(input=[counts.astype(x.dtype), paddle.reshape(counts_nan, [nan_count])], axis=0)
+
+    Results = namedtuple("Results", ["values", "counts"])
+    return Results(unique, counts)
 
 
 def unique_inverse(x: paddle.Tensor, /) -> Tuple[paddle.Tensor, paddle.Tensor]:
