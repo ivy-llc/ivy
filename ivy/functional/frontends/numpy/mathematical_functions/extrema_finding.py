69a92a5a1ab0dd2504e4e0453af7186a1f5e1f2d
Aryan Sharma <sharmaisgreat46@gmail.com>
2023-03-02 18:53:59 +0530

    Add fmin to numpy frontend mathematical function

--- ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/extrema_finding.py
@@ -113,6 +113,29 @@ def amax(
         res = ivy.where(ivy.logical_not(where_mask), res, ivy.nan, out=out)
     return res
 
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
+def fmin(
+    x1,
+    x2,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    ret = _minimum(x1, x2, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
 
 @handle_numpy_out
 @to_ivy_arrays_and_back
