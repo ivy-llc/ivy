1f33d257ff10c6245495422814afb8a6a4588197
RakshitKumar-04 <kumarrakshit0402@gmail.com>
2023-03-04 00:02:12 +0530

    #3345 : Added floor function in numpy frontend

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -16,6 +16,28 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 @to_ivy_arrays_and_back
 @handle_numpy_casting
 @from_zero_dim_arrays_to_scalar
+def floor(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.floor(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
 def _ceil(
     x,
     /,
