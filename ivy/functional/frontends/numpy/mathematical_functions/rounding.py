e4c18f1831f455cca0e7b5954bf3b66e6b0642d6
Ario Zareinia <ariozareinia@gmail.com>
2023-03-07 15:44:52 +0000

    First commit

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -32,6 +32,27 @@ def _ceil(
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
 
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
+def _floor(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="k",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.floor(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
 
 @handle_numpy_out
 @to_ivy_arrays_and_back
