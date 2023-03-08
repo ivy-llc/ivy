775564e16a958b3fed84ae37c650dc47517fff1d
danielmunioz <Dan.munioz.el@gmail.com>
2023-03-08 16:38:42 +0000

    Adding ndarray and creation imports to mxnet.numpy frontend

--- ivy/functional/frontends/mxnet/numpy/__init__.py
+++ ivy/functional/frontends/mxnet/numpy/__init__.py
@@ -1,6 +1,9 @@
 # flake8: noqa
 from . import random
 from . import linalg
+from . import ndarray
 from .linalg import *
 from . import mathematical_functions 
 from .mathematical_functions import *
+from . import creation
+from .creation import *
