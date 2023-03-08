a44086fc16041f6701dcaa82b7a00f4f15548a6b
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-03-07 22:56:37 -0500

    Update general.py

--- ivy/functional/backends/numpy/general.py
+++ ivy/functional/backends/numpy/general.py
@@ -346,8 +346,6 @@ def scatter_nd(
 scatter_nd.support_native_out = True
 
 
-@with_unsupported_dtypes({"1.23.0 and below": ("complex",)}, backend_version)
-@_scalar_output_to_0d_array
 def shape(
     x: np.ndarray,
     /,
