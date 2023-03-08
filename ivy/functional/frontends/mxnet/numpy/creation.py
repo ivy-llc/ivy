35061d1fcda29f96d1ae4e357961c1bf8de6a1bc
danielmunioz <Dan.munioz.el@gmail.com>
2023-03-08 16:40:42 +0000

    Modifying mxnex.array too create ndarrays directly

--- ivy/functional/frontends/mxnet/numpy/creation.py
+++ ivy/functional/frontends/mxnet/numpy/creation.py
@@ -1,4 +1,5 @@
 import ivy
+from ivy.functional.frontends.mxnet.numpy.ndarray import ndarray
 
 from ivy.functional.frontends.mxnet.func_wrapper import (
     to_ivy_arrays_and_back,
@@ -7,4 +8,4 @@ from ivy.functional.frontends.mxnet.func_wrapper import (
 
 @to_ivy_arrays_and_back
 def array(object, dtype=None, ctx=None):
-    return ivy.array(object, dtype=dtype)
+    return ndarray(object)
