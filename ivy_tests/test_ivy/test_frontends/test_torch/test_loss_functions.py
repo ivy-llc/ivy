390e2f3f0aaf8f036fc16e81b1c76c3886301c07
Harshanand B A <harsha@Harshanands-MacBook-Pro.local>
2023-03-08 14:07:16 -0500

    implemented the test for gaussian_nll_loss

--- ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_loss_functions.py
@@ -563,6 +563,75 @@ def test_torch_nll_loss(
     )
 
 
+# gaussian_nll_loss
+@handle_frontend_test(
+    fn_tree="torch.nn.functional.gaussian_nll_loss",
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_value=0.01,
+        max_value=1.0,
+        allow_inf=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+    ),
+    dtype_and_target=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_value=0.0,
+        max_value=1.0,
+        allow_inf=False,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+    ),
+    dtype_and_var=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        allow_inf=False,
+        min_value=0.0,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+    ),
+    eps=st.floats(
+        min_value=0.0,
+        max_value=1.0,
+        allow_nan=False,
+        allow_infinity=False,
+    ),
+    reduction=st.sampled_from(["mean", "sum"]),
+)
+def test_torch_gaussian_nll_loss(
+    *,
+    dtype_and_input,
+    dtype_and_target,
+    dtype_and_var,
+    eps,
+    reduction,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    inputs_dtype, input = dtype_and_input
+    target_dtype, target = dtype_and_target
+    var_dtype, var = dtype_and_var
+    helpers.test_frontend_function(
+        input_dtypes=inputs_dtype + target_dtype + var_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=input[0],
+        target=target[0],
+        var=var[0],
+        eps=eps,
+        reduction=reduction,
+    )
+
+
 # soft margin loss
 @handle_frontend_test(
     fn_tree="torch.nn.functional.soft_margin_loss",
