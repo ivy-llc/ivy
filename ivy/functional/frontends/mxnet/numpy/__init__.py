6b143262bbc552aef2be4fae96b4536b38024611
danielmunioz <Dan.munioz.el@gmail.com>
2023-03-09 06:14:40 +0000

    Adding symbol to __init__.py

--- ivy/functional/frontends/mxnet/numpy/__init__.py
+++ ivy/functional/frontends/mxnet/numpy/__init__.py
@@ -1,9 +1,11 @@
 # flake8: noqa
 from . import random
-from . import linalg
 from . import ndarray
+from . import linalg
 from .linalg import *
 from . import mathematical_functions 
 from .mathematical_functions import *
 from . import creation
 from .creation import *
+from . import symbol
+from .symbol import *
