34b4164a2a03342f297f2ff949d1199d022deb1f
shireenchand <shireen.chand@yahoo.com>
2023-03-09 21:24:09 +0530

    Added numpy trim_zeros with frontend and tests

--- ivy/functional/frontends/numpy/manipulation_routines/adding_and_removing_elements.py
+++ ivy/functional/frontends/numpy/manipulation_routines/adding_and_removing_elements.py
@@ -35,3 +35,21 @@ def unique(
         values.append(results.counts)
 
     return Results(*values)
+@to_ivy_arrays_and_back
+def trim_zeros(filt,trim='fb'):
+    first = 0
+    trim = trim.upper()
+    if 'F' in trim:
+        for i in filt:
+            if i != 0.:
+                break
+            else:
+                first = first + 1
+    last = len(filt)
+    if 'B' in trim:
+        for i in filt[::-1]:
+            if i != 0.:
+                break
+            else:
+                last = last - 1
+    return filt[first:last]
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_adding_and_removing_elements.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_adding_and_removing_elements.py
@@ -1,6 +1,7 @@
 # global
 
 # local
+from hypothesis import strategies as st
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
@@ -31,4 +32,33 @@ def test_numpy_unique(
         return_index=True,
         return_inverse=True,
         return_counts=True,
+    )
+
+@handle_frontend_test(
+    fn_tree="numpy.trim_zeros",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=1,
+        max_num_dims=1
+    ),
+    trim=st.sampled_from(['f','b','fb'])
+)
+def test_numpy_trim_zeros(
+    frontend,
+    on_device,
+    *,
+    dtype_and_x,
+    trim,
+    fn_tree,
+    test_flags,
+):
+    input_dtypes, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        filt=x[0],
+        trim=trim
     )
