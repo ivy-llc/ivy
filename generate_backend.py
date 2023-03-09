18eda7b7b2c104698fdbdd61c9d086faba0a0a11
CatB1t <skytedits@gmail.com>
2023-03-09 19:59:11 +0200

    add valid data types selection and flag toggle.

--- generate_backend.py
+++ generate_backend.py
@@ -1,3 +1,4 @@
+import ivy
 import sys
 import subprocess
 import pprint
@@ -12,7 +13,7 @@ _imported_backend = None
 _backend_is_installed = False
 
 backend = {"name": None, "alias": None}
-config = {
+config_natives = {
     "NativeArray": None,
     "NativeVariable": None,
     "NativeDevice": None,
@@ -20,8 +21,11 @@ config = {
     "NativeShape": None,
     "NativeSparseArray": None,
 }
-config_lists = {
-    "valid_devices": ["cpu"],
+config_valids = {
+    "valid_devices": list(ivy.all_devices),
+    "valid_int_dtypes": list(ivy.all_int_dtypes),
+    "valid_float_dtypes": list(ivy.all_float_dtypes),
+    "valid_complex_dtypes": list(ivy.all_complex_dtypes),
 }
 config_flags = {
     "native_inplace_support": False,
@@ -45,7 +49,7 @@ def _update_native_config_value(key):
         "\nPress ENTER to skip\n"
         f"Enter a value for {Style.BRIGHT + key + Style.NORMAL} "
         "(case sensistive) "
-        f"default: '{Style.BRIGHT}{config[key]}{Style.NORMAL}': "
+        f"default: '{Style.BRIGHT}{config_natives[key]}{Style.NORMAL}': "
     )
     ret = ret.strip(" ")
     if ret != "" and _imported_backend is not None:
@@ -55,7 +59,7 @@ def _update_native_config_value(key):
                 print(Fore.RED + f"{obj} is not a class.")
                 return False
             print(Fore.GREEN + f"Found class: {obj}")
-            config[key] = obj
+            config_natives[key] = obj
             return True
         except KeyError:
             print(Fore.RED + f"Couldn't find {backend['name']}.{ret}")
@@ -131,19 +135,56 @@ def _add_alias_for_backend():
     return True
 
 
+def _update_flag_config_value(key):
+    ret = input(
+        f"\nToggle flag {Style.BRIGHT}{key}{Style.NORMAL} [Y/n]? "
+        f"default: {Fore.RED}'{config_flags[key]}'"
+        f"{Style.RESET_ALL}: "
+    )
+    ret = ret.strip(" ").lower()
+    if ret == "y":
+        config_flags[key] = not config_flags[key]
+        return True
+    elif ret == "n" or ret == "":
+        return True
+    print(Fore.RED + f"{ret} not understood.")
+    return False
+
+
+def _update_valid_config_value(key):
+    print(f"Select items to remove from list {Style.BRIGHT}{key}:\n")
+    for i, item in enumerate(config_valids[key]):
+        print(f"{i}. {item}")
+    ret = input("\nPress ENTER to skip. Enter numbers (space seperated): ")
+    ret = ret.strip("")
+    if ret == "":
+        return True
+    indicies = ret.split(" ")
+    indicies = [int(item.strip(" ")) for item in indicies]
+    for i in sorted(indicies, reverse=True):
+        del config_valids[key][i]
+    return True
+
+
 if __name__ == "__main__":
     init(autoreset=True)
 
     _get_user_input(_get_backend)
 
-    for key in config:
+    for key in config_natives:
         _get_user_input(_update_native_config_value, key)
+
+    for key in config_flags:
+        _get_user_input(_update_flag_config_value, key)
+
+    for key in config_valids:
+        _get_user_input(_update_valid_config_value, key)
 
     print("\n:: Backend\n")
     pprint.pprint(backend, sort_dicts=False)
     print("\n:: Config\n")
-    pprint.pprint(config, sort_dicts=False)
-    pprint.pprint(config_lists, sort_dicts=False)
+    pprint.pprint(config_natives, sort_dicts=False)
+    pprint.pprint(config_valids, sort_dicts=False)
     pprint.pprint(config_flags, sort_dicts=False)
 
     print("\n:: Procced with generation? [Y/n]\n")
