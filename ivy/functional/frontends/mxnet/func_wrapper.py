ae46868b35e06f062e84f915816c14c2a83294ad
fspyridakos <fotissp@yahoo.com>
2023-03-10 14:53:36 +0000

    Added promotion and extra wrappers to mxnet frontend

--- ivy/functional/frontends/mxnet/func_wrapper.py
+++ ivy/functional/frontends/mxnet/func_wrapper.py
@@ -1,13 +1,14 @@
 import functools
+import inspect
 from typing import Callable
 
 import ivy
-import ivy.functional.frontends.mxnet as mxnet_frontend
+from ivy.functional.frontends.mxnet.numpy.ndarray import ndarray
 
 
 def _ivy_array_to_mxnet(x):
     if isinstance(x, ivy.Array) or ivy.is_native_array(x):
-        return mxnet_frontend.numpy.array(x)
+        return ndarray(x)
     return x
 
 
@@ -18,9 +19,6 @@ def _mxnet_frontend_array_to_ivy(x):
 
 
 def _native_to_ivy_array(x):
-    # TODO: replace `ndarray.NDArray` with `ivy.NativeArray``
-    # and `x.asnumpy()` with `x` once the mxnet tensor class 
-    # is implemented
     if isinstance(x, ivy.NativeArray):
         return ivy.array(x)
     return x
@@ -38,23 +36,14 @@ def inputs_to_ivy_arrays(fn: Callable) -> Callable:
         arguments into `ivy.Array` instances, and then calls the function with the
         updated arguments.
         """
-        has_out = False
-        out = None
-        if "out" in kwargs:
-            out = kwargs["out"]
-            del kwargs["out"]
-            has_out = True
-
         # convert all arrays in the inputs to ivy.Array instances
-        ivy_args = ivy.nested_map(
-            args, _to_ivy_array, include_derived=True, shallow=False
+        new_args = ivy.nested_map(
+            args, _to_ivy_array, include_derived={tuple: True}, shallow=False
         )
-        ivy_kwargs = ivy.nested_map(
-            kwargs, _to_ivy_array, include_derived=True, shallow=False
+        new_kwargs = ivy.nested_map(
+            kwargs, _to_ivy_array, include_derived={tuple: True}, shallow=False
         )
-        if has_out:
-            ivy_kwargs["out"] = out
-        return fn(*ivy_args, **ivy_kwargs)
+        return fn(*new_args, **new_kwargs)
 
     new_fn.inputs_to_ivy_arrays = True
     return new_fn
@@ -71,9 +60,7 @@ def outputs_to_frontend_arrays(fn: Callable) -> Callable:
         ret = fn(*args, **kwargs)
 
         # convert all arrays in the return to `frontend.Tensorflow.tensor` instances
-        return ivy.nested_map(
-            ret, _ivy_array_to_mxnet, include_derived={tuple: True}
-        )
+        return ivy.nested_map(ret, _ivy_array_to_mxnet, include_derived={tuple: True})
 
     new_fn.outputs_to_frontend_arrays = True
     return new_fn
@@ -85,3 +72,36 @@ def to_ivy_arrays_and_back(fn: Callable) -> Callable:
     and return arrays are all converted to `ndarray.NDArray` instances.
     """
     return outputs_to_frontend_arrays(inputs_to_ivy_arrays(fn))
+
+
+def handle_mxnet_out(fn: Callable) -> Callable:
+    @functools.wraps(fn)
+    def new_fn(*args, out=None, **kwargs):
+        if len(args) > (out_pos + 1):
+            out = args[out_pos]
+            kwargs = {
+                **dict(
+                    zip(
+                        list(inspect.signature(fn).parameters.keys())[
+                            out_pos + 1 : len(args)
+                        ],
+                        args[out_pos + 1 :],
+                    )
+                ),
+                **kwargs,
+            }
+            args = args[:out_pos]
+        elif len(args) == (out_pos + 1):
+            out = args[out_pos]
+            args = args[:-1]
+        if ivy.exists(out):
+            if not isinstance(out, ndarray):
+                raise ivy.utils.exceptions.IvyException(
+                    "Out argument must be an ivy.frontends.mxnet.numpy.ndarray object"
+                )
+            return fn(*args, out=out.ivy_array, **kwargs)
+        return fn(*args, **kwargs)
+
+    out_pos = list(inspect.signature(fn).parameters).index("out")
+    new_fn.handle_numpy_out = True
+    return new_fn
--- ivy/functional/frontends/mxnet/numpy/creation.py
+++ ivy/functional/frontends/mxnet/numpy/creation.py
@@ -1,10 +1,13 @@
-from ivy.functional.frontends.mxnet.numpy.ndarray import ndarray
-
+import ivy
 from ivy.functional.frontends.mxnet.func_wrapper import (
     to_ivy_arrays_and_back,
 )
+from ivy.functional.frontends.numpy.func_wrapper import handle_numpy_dtype
 
 
+@handle_numpy_dtype
 @to_ivy_arrays_and_back
 def array(object, dtype=None, ctx=None):
-    return ndarray(object)
+    if not ivy.is_array(object) and not dtype:
+        return ivy.array(object, dtype="float32", device=ctx)
+    return ivy.array(object, dtype=dtype, device=ctx)
