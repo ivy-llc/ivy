d5456c98925267919d72894938e9843ef08ad8f4
omar ragi <54076116+omarragi9@users.noreply.github.com>
2023-03-11 17:29:00 +0200

    Add fill test method
    Add fill test method (unifyai#11867)

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -1130,6 +1130,39 @@ def test_numpy_instance_std(
         on_device=on_device,
     )
 
+ #Fill
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="numpy.array",
+    method_name="fill",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+)
+def test_numpy_ndarray_fill(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtypes, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtypes,
+        init_all_as_kwargs_np={
+            "object": x[0],
+        },
+        method_input_dtypes=[],
+        method_all_as_kwargs_np={
+            "num": num,
+        },
+        frontend=frontend,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        on_device=on_device,
+    )
 
 @handle_frontend_method(
     class_tree=CLASS_TREE,
