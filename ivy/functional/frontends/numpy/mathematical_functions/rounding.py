10813727fc1ffa26ad577f75bb2b0049d9dfcab2
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2023-03-12 21:27:18 +0530

    floor added

--- ivy/functional/frontends/numpy/mathematical_functions/rounding.py
+++ ivy/functional/frontends/numpy/mathematical_functions/rounding.py
@@ -86,3 +86,24 @@ def rint(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, x), out=out)
     return ret
+
+
+@handle_numpy_out
+@handle_numpy_dtype
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
+def floor(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.floor(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
