c63913e1ca91909a37f90a685ee39c0c19a27162
CatB1t <skytedits@gmail.com>
2023-03-14 06:53:18 +0200

    add comments for tree generation.

--- scripts/backend_generation/tree_generation.py
+++ scripts/backend_generation/tree_generation.py
@@ -16,6 +16,7 @@ def _copy_tree(backend_reference_path: str, backend_generation_path: str):
             continue
 
         relative_path = os.path.relpath(root, backend_reference_path)
+        # Make backend dirs
         os.makedirs(os.path.join(backend_generation_path, relative_path))
 
         for name in files:
@@ -24,8 +25,10 @@ def _copy_tree(backend_reference_path: str, backend_generation_path: str):
                 continue
 
             with open(os.path.join(root, name)) as ref_file:
+                # Read source file from reference backend
                 ref_str = ref_file.read()
 
+            # Create target backend
             with open(
                 os.path.join(backend_generation_path, relative_path, name), "w"
             ) as generated_file:
@@ -33,6 +36,7 @@ def _copy_tree(backend_reference_path: str, backend_generation_path: str):
 
 
 def generate(backend_reference: str, target_backend: str):
+    # Set backend variables
     global _target_backend, _reference_backend
     _target_backend = target_backend
     _reference_backend = backend_reference
@@ -40,8 +44,11 @@ def generate(backend_reference: str, target_backend: str):
     backends_root = "../../ivy/functional/backends/"
     backend_reference_path = backends_root + backend_reference
     backend_generation_path = backends_root + target_backend
+
+    # Copy and generate backend tree
     _copy_tree(backend_reference_path, backend_generation_path)
 
 
 if __name__ == "__main__":
+    # TODO remove
     generate(sys.argv[1], sys.argv[2])
