83d283ac984647c96dd04a4fa46374f5f9a6477f
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-03-14 17:50:39 +0000

    fixed minor bug in ivy.pad jax backend: input dtype needs to be saved before the first casting

--- ivy/functional/backends/jax/experimental/manipulation.py
+++ ivy/functional/backends/jax/experimental/manipulation.py
@@ -167,10 +167,9 @@ def pad(
     stat_length = _to_nested_tuple(stat_length)
     constant_values = _to_nested_tuple(constant_values)
     end_values = _to_nested_tuple(end_values)
-    float_cast = False
-    if jnp.issubdtype(input.dtype, jnp.integer) and mode in ["mean", "median"]:
+    input_dtype = input.dtype
+    if jnp.issubdtype(input_dtype, jnp.integer) and mode in ["mean", "median"]:
         input = input.astype(jnp.float64)
-        float_cast = True
     if callable(mode):
         ret = jnp.pad(
             _flat_array_to_1_dim_array(input),
@@ -212,8 +211,8 @@ def pad(
             pad_width,
             mode=mode,
         )
-    if float_cast:
-        ret = jnp.round(ret).astype(input.dtype)
+    if jnp.issubdtype(input_dtype, jnp.integer) and mode in ["mean", "median"]:
+        ret = jnp.round(ret).astype(input_dtype)
     return ret
 
 
