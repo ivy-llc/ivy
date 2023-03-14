996dafcdb123726a20da028eb51bba67518d256a
Nooh <43852910+x0axz@users.noreply.github.com>
2023-03-14 14:07:15 +0500

    added jax.numpy.dstack function (#12322)
    Co-authored-by: WilliamHirst bod_holthe@outlook.com

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_manipulation.py
@@ -1073,6 +1073,38 @@ def test_jax_numpy_dsplit(
     )
 
 
+# dstack
+@handle_frontend_test(
+    fn_tree="jax.numpy.dstack",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        shared_dtype=True,
+        num_arrays=helpers.ints(min_value=1, max_value=10),
+        shape=helpers.get_shape(
+            min_num_dims=1,
+        ),
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_numpy_dstack(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        tup=x,
+    )
+
+
 # vsplit
 @handle_frontend_test(
     fn_tree="jax.numpy.vsplit",
