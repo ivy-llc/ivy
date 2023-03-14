ab7635c73d53cac3e689ee10dec9954e74cb7c91
CatB1t <skytedits@gmail.com>
2023-03-14 19:09:39 +0200

    add AST transformers to modify the generated module, update the tree generator to modify the AST tree and unparse it.

--- scripts/backend_generation/tree_generation.py
+++ scripts/backend_generation/tree_generation.py
@@ -1,3 +1,5 @@
+import astunparse
+import ast
 import sys
 import os
 
@@ -5,8 +7,131 @@ _reference_backend = ""
 _target_backend = ""
 
 
-def _parse_module(module_path: str):
-    pass
+_backend_ref_name = "tf"
+_target_backend_name = "torch"
+
+# This should be imported in the module
+_not_implemented_decorator_name = "not_implemented"
+_decorator_black_list = [
+    "with_unsupported_dtypes",
+    "with_supported_dtypes",
+    "with_unsupported_devices",
+    "with_supported_devices",
+    "with_unsupported_device_and_dtypes",
+    "with_supported_device_and_dtypes",
+]
+
+type_mapping = {
+    "int": "float",
+    "Tensor": "Array",
+    "Variable": "VarArray",
+    "Scaler": "NotAscaler",
+}
+
+
+class ImportTransformer(ast.NodeTransformer):
+    # TODO add backend specific imports for the type hints
+    def visit_Import(self, node: ast.Import):
+        if isinstance(node, ast.Import):
+            # Only keep "import typing" statements
+            entries_list = []
+            for entry in node.names:
+                if entry.name.startswith("typing"):
+                    entries_list.append(entry)
+            if len(entries_list) > 0:
+                node.names = entries_list
+            else:
+                self.generic_visit(node)
+                return None
+        self.generic_visit(node)
+        return node
+
+    def visit_ImportFrom(self, node: ast.ImportFrom):
+        # Only keep imports from "typing"
+        if isinstance(node, ast.ImportFrom):
+            if not node.module == "typing":
+                self.generic_visit(node)
+                return None
+        self.generic_visit(node)
+        return node
+
+
+class FunctionBodyTransformer(ast.NodeTransformer):
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        if isinstance(node, ast.FunctionDef):
+            # Remove private functions
+            if node.name.startswith("_"):
+                self.generic_visit(node)
+                return None
+            else:
+                # Replace function body with Pass
+                node.body = [ast.Pass()]
+                # Update decorators not to include ones in the blacklist
+                # Add Not Implemented decorator
+                new_list = [ast.Name(_not_implemented_decorator_name, ctx=ast.Load())]
+                for entry in node.decorator_list:
+                    if entry.id in _decorator_black_list:
+                        continue
+                    new_list.append(entry)
+                node.decorator_list = new_list
+        self.generic_visit(node)
+        return node
+
+
+class TypeHintTransformer(ast.NodeTransformer):
+    def __init__(self, type_map):
+        self.type_map = type_map
+        self.registered_type_hints = set()
+
+    def visit_Name(self, node: ast.Name):
+        if isinstance(node, ast.Name):
+            if node.id == _backend_ref_name:
+                node.id = _target_backend_name
+            else:
+                try:
+                    node.id = self.type_map[node.id]
+                except KeyError:
+                    pass
+        self.generic_visit(node)
+        return node
+
+    def visit_Attribute(self, node: ast.Attribute):
+        if isinstance(node, ast.Attribute):
+            try:
+                node.attr = self.type_map[node.attr]
+            except KeyError:
+                pass
+            else:
+                # Function Scope-Level
+                self.registered_type_hints.add(node.attr)
+        self.generic_visit(node)
+        return node
+
+
+# Modify the AST tree
+def _parse_module(tree: ast.Module) -> ast.Module:
+    _module_registered_type_hints = set()
+    # Walk the AST tree and update type hints
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef):
+            # Update Return Type hints
+            if node.returns is not None:
+                transformer = TypeHintTransformer(type_mapping)
+                transformer.visit_Name(node.returns)
+                _module_registered_type_hints.update(transformer.registered_type_hints)
+        # Update Arguments Type hints
+        if isinstance(node, ast.arguments):
+            transformer = TypeHintTransformer(type_mapping)
+            transformer.visit_Name(node)
+            _module_registered_type_hints.update(transformer.registered_type_hints)
+
+    # Update decorators, update function body, remove private functions
+    FunctionBodyTransformer().visit_FunctionDef(tree)
+    # Update imports, remove backend specific imports
+    ImportTransformer().visit_Import(tree)
+    # Add target backend import, add type hints classes imports
+    ast.fix_missing_locations(tree)
+    return tree
 
 
 def _copy_tree(backend_reference_path: str, backend_generation_path: str):
@@ -28,11 +153,13 @@ def _copy_tree(backend_reference_path: str, backend_generation_path: str):
                 # Read source file from reference backend
                 ref_str = ref_file.read()
 
+            tree_to_write = _parse_module(ast.parse(ref_str))
+
             # Create target backend
             with open(
                 os.path.join(backend_generation_path, relative_path, name), "w"
             ) as generated_file:
-                generated_file.write(ref_str)
+                generated_file.write(astunparse.unparse(tree_to_write))
 
 
 def generate(backend_reference: str, target_backend: str):
