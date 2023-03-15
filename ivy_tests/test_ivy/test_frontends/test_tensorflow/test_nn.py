6ea387e798a56dd591e19fb1d5cc73fe6a26d1b1
Ajith Pious <ajithpiousolive@gmail.com>
2023-03-15 12:41:26 +0530

    function test paramater changes

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -1246,37 +1246,31 @@ def test_tensorflow_embedding_lookup(
 
 
 @handle_frontend_test(
-    fn_name="tensorflow.nn.avg_pool1d",
-    x_f_d_df=_x_and_filters(
-        dtypes=helpers.get_dtypes("float", full=False),
-        data_format=st.sampled_from(["NWC", "NCW"]),
-        padding=st.sampled_from(["VALID", "SAME"]),
-        type="1d",
-    ),
+    fn_tree="tensorflow.nn.avg_pool1d",
+    data_format=df(data_format=st.sampled_from(["NWC"])),
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=3, max_dims=3, min_side=1, max_side=4),
+    test_with_out=st.just(False),
 )
 def test_tensorflow_avg_pool1d(
     *,
-    x_f_d_df,
-    as_variable,
-    num_positional_args,
-    native_array,
+    x_k_s_p,
+    data_format,
     frontend,
+    test_flags,
     fn_tree,
     on_device,
 ):
-    input_dtype, x, ksize, stride, data_format, padding = x_f_d_df
+    input_dtype, x, ksize, strides, padding = x_k_s_p
+    data_format = data_format
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        as_variable_flags=as_variable,
-        with_out=False,
-        num_positional_args=num_positional_args,
-        native_array_flags=native_array,
         frontend=frontend,
+        test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        input=x,
+        input=x[0],
         ksize=ksize,
-        strides=stride,
+        strides=strides,
         padding=padding,
         data_format=data_format,
     )
