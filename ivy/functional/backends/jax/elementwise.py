a70d7c2177a612f67a579b1b1dc92eac9705b1d5
Mahmoud Ashraf <32404268+MahmoudAshraf97@users.noreply.github.com>
2023-03-15 16:47:35 +0200

    fix complex behaviour in ivy.divide (#12486)
    Co-authored by MahmoudAshraf97 <hassouna97.ma@gmail.com>

--- ivy/functional/backends/jax/elementwise.py
+++ ivy/functional/backends/jax/elementwise.py
@@ -154,7 +154,7 @@ def divide(
 ) -> JaxArray:
     x1, x2 = ivy.promote_types_of_inputs(x1, x2)
     ret = jax.numpy.divide(x1, x2)
-    if ivy.is_float_dtype(x1.dtype):
+    if ivy.is_float_dtype(x1.dtype) or ivy.is_complex_dtype(x1.dtype):
         ret = jnp.asarray(ret, dtype=x1.dtype)
     else:
         ret = jnp.asarray(ret, dtype=ivy.default_float_dtype(as_native=True))
