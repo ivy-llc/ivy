c63c04ca805d539eb86ca88d75f9511037ed7afc
Shehryar Tariq <sherrytst30@gmail.com>
2023-03-15 07:38:35 +0500

    remove duplicate definition of `AddN` in tf frontend raw_ops

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -11,11 +11,6 @@ from ivy.functional.frontends.tensorflow.func_wrapper import (
 from ivy.func_wrapper import with_unsupported_dtypes
 
 
-@to_ivy_arrays_and_back
-def AddN(*, inputs, name="AddN"):
-    return ivy.sum(inputs, dtype=inputs.dtype, axis=0)
-
-
 AddN = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.math.add_n))
 
 
