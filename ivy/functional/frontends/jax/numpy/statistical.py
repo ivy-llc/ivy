1ebb3fdb568b8982af328724938c546a5749772d
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-03-16 18:19:21 +0530

    Update statistical.py

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -391,8 +391,7 @@ def std(a, axis=None, dtype=None, out=None, ddof=0, keepdims=False, *, where=Non
     axis = tuple(axis) if isinstance(axis, list) else axis
     if dtype is None:
         dtype = "float32" if ivy.is_int_dtype(a) else a.dtype
-    var_a = ivy.var(a, axis=axis, correction=ddof, keepdims=keepdims, out=out)
-    std_a = ivy.sqrt(var_a)
+    std_a = ivy.std(a, axis=axis, correction=ddof, keepdims=keepdims, out=out)
     if ivy.is_array(where):
         where = ivy.array(where, dtype=ivy.bool)
         std_a = ivy.where(where, std_a, ivy.default(out, ivy.zeros_like(std_a)), out=out)
