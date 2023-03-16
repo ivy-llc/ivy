175d80d2f7d15334530c063c50cdbe493a8604c2
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-03-16 18:11:22 +0000

    updating the dtypes to be valid as jax.numpy.copy can take booleans inputs

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
@@ -665,7 +665,7 @@ def test_jax_numpy_linspace(
 @handle_frontend_test(
     fn_tree="jax.numpy.copy",
     dtype_and_a=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("valid"),
         num_arrays=1,
         min_num_dims=0,
         max_num_dims=5,
