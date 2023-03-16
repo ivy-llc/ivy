eb83ba713b47521c136d5f5bc05b7ca4798bb81a
ndurumo254 <erickndurumo254>
2023-03-16 17:38:17 +0300

    shuffle branch

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -76,10 +76,3 @@ def permutation(x, /):
 def beta(a, b, size=None):
     return ivy.beta(a, b, shape=size)
 
-
-@to_ivy_arrays_and_back
-@from_zero_dim_arrays_to_scalar
-def shuffle(x, /):
-    if isinstance(x, int):
-        x = ivy.arange(x)
-    return ivy.shuffle(x)
