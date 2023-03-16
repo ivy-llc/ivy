2d70ddeda4e31b933cf09da14a7402501088f773
root <mattcrewe2308@hotmail.co.uk>
2023-03-16 15:39:27 +0000

    Add angle function to NumPy Frontend

--- ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
+++ ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
@@ -5,6 +5,19 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 )
 
 
+# TODO working for complex input only, could be expanded to handle whole
+# range of input which numpy is capable of taking
+@to_ivy_arrays_and_back
+def angle(z, deg=False):
+    angle = ivy.angle(z, deg=deg)
+
+    # numpy implementation multiplies by float64 when converting to degrees
+    # so implicit casting occures, but only to single value inputs (not arrays)
+    if deg and len(z.shape) == 0:
+        angle = ivy.astype(angle, ivy.float64)
+    return angle
+
+
 @to_ivy_arrays_and_back
 def _imag(val):
     return ivy.imag(val)
