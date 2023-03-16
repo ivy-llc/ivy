f3ee5000d4736bc2267bb430452af0082160705b
Xingyu Long <halolong@hotmail.com>
2023-03-15 22:14:49 -0600

    Add frexp for jax frontend and its test

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -2517,3 +2517,31 @@ def test_jax_numpy_around(
         on_device=on_device,
         a=x[0],
     )
+
+
+# frexp
+@handle_frontend_test(
+    fn_tree="jax.numpy.frexp",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=1,
+        max_value=100,
+    ),
+)
+def test_jax_numpy_frexp(
+    *,
+    dtype_and_x,
+    test_flags,
+    on_device,
+    fn_tree,
+    frontend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
