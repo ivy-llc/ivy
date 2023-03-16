30e3a1ed0a47e7bdfa11ac3055354e4a4092e8c0
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-03-16 17:39:19 -0400

    Add copy arguments to manipulation.py (#11842)
    Co-authored-by: ZoeCD <42352932+ZoeCD@users.noreply.github.com>

--- ivy/functional/backends/numpy/manipulation.py
+++ ivy/functional/backends/numpy/manipulation.py
@@ -48,9 +48,13 @@ def expand_dims(
     x: np.ndarray,
     /,
     *,
+    copy: Optional[bool] = None,
     axis: Union[int, Sequence[int]] = 0,
     out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
+    if copy:
+        newarr = x.copy()
+        return np.expand_dims(newarr, axis)
     return np.expand_dims(x, axis)
 
 
@@ -58,23 +62,38 @@ def flip(
     x: np.ndarray,
     /,
     *,
+    copy: Optional[bool] = None,
     axis: Optional[Union[int, Sequence[int]]] = None,
     out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
     num_dims = len(x.shape)
     if not num_dims:
+        if copy:
+            newarr = x.copy()
+            return newarr
         return x
     if axis is None:
         axis = list(range(num_dims))
     if type(axis) is int:
         axis = [axis]
     axis = [item + num_dims if item < 0 else item for item in axis]
+    if copy:
+        newarr = x.copy()
+        return np.flip(newarr, axis)
     return np.flip(x, axis)
 
 
 def permute_dims(
-    x: np.ndarray, /, axes: Tuple[int, ...], *, out: Optional[np.ndarray] = None
+    x: np.ndarray, 
+    /, 
+    axes: Tuple[int, ...], 
+    *, 
+    copy: Optional[bool] = None,
+    out: Optional[np.ndarray] = None
 ) -> np.ndarray:
+    if copy:
+        newarr = x.copy()
+        return np.transpose(newarr, axes)
     return np.transpose(x, axes)
 
 
@@ -116,6 +135,7 @@ def squeeze(
     /,
     axis: Union[int, Sequence[int]],
     *,
+    copy: Optional[bool] = None,
     out: Optional[np.ndarray] = None,
 ) -> np.ndarray:
     if isinstance(axis, list):
@@ -126,6 +146,9 @@ def squeeze(
         raise ivy.utils.exceptions.IvyException(
             "tried to squeeze a zero-dimensional input by axis {}".format(axis)
         )
+    if copy:
+        newarr = x.copy()
+        return np.squeeze(newarr, axis=axis)
     return np.squeeze(x, axis=axis)
 
 
@@ -150,6 +173,7 @@ def split(
     x: np.ndarray,
     /,
     *,
+    copy: Optional[bool] = None,
     num_or_size_splits: Optional[Union[int, Sequence[int]]] = None,
     axis: int = 0,
     with_remainder: bool = False,
@@ -161,6 +185,9 @@ def split(
                     num_or_size_splits
                 )
             )
+        if copy:
+            newarr = x.copy()
+            return [newarr]
         return [x]
     if num_or_size_splits is None:
         num_or_size_splits = x.shape[axis]
@@ -174,6 +201,9 @@ def split(
             ]
     if isinstance(num_or_size_splits, (list, tuple)):
         num_or_size_splits = np.cumsum(num_or_size_splits[:-1])
+    if copy:
+        newarr = x.copy()
+        return np.split(newarr, num_or_size_splits, axis)
     return np.split(x, num_or_size_splits, axis)
 
 
@@ -190,7 +220,11 @@ def repeat(
 
 
 def tile(
-    x: np.ndarray, /, repeats: Sequence[int], *, out: Optional[np.ndarray] = None
+    x: np.ndarray, 
+    /, 
+    repeats: Sequence[int], 
+    *, 
+    out: Optional[np.ndarray] = None
 ) -> np.ndarray:
     return np.tile(x, repeats)
 
@@ -213,17 +247,39 @@ def zero_pad(
 
 
 def swapaxes(
-    x: np.ndarray, axis0: int, axis1: int, /, *, out: Optional[np.ndarray] = None
+    x: np.ndarray, 
+    axis0: int, 
+    axis1: int, 
+    /, 
+    *, 
+    copy: Optional[bool] = None,
+    out: Optional[np.ndarray] = None
 ) -> np.ndarray:
+    if copy:
+        newarr = x.copy()
+        return np.swapaxes(newarr, axis0, axis1)
     return np.swapaxes(x, axis0, axis1)
 
 
 def unstack(
-    x: np.ndarray, /, *, axis: int = 0, keepdims: bool = False
+    x: np.ndarray, 
+    /, 
+    *, 
+    copy: Optional[bool] = None,
+    axis: int = 0, 
+    keepdims: bool = False
 ) -> List[np.ndarray]:
     if x.shape == ():
+        if copy:
+            newarr = x.copy()
+            return [newarr]
         return [x]
-    x_split = np.split(x, x.shape[axis], axis)
+    x_split = None
+    if copy:
+        newarr = x.copy()
+        x_split = np.split(newarr, newarr.shape[axis], axis)
+    else:
+        x_split = np.split(x, x.shape[axis], axis)
     if keepdims:
         return x_split
     return [np.squeeze(item, axis) for item in x_split]
