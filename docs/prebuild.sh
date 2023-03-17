0858b69698edd5fc08abfd079e7790da4e025387
Kareem Morsy <kreem.morsy@hotmail.com>
2023-03-17 12:26:17 +0000

    Installing torch-scatter using PyPI

--- docs/prebuild.sh
+++ docs/prebuild.sh
@@ -3,7 +3,4 @@
 # For some reason torch needed to be installed sequentially before installing from 
 # requirements.txt
 pip install torch || exit 1
-
-# torch-scatter supporting torch 2.0 is only available on github
-apt-get install -y git
-pip install git+https://github.com/rusty1s/pytorch_scatter.git || exit 1
+pip install torch-scatter || exit 1
