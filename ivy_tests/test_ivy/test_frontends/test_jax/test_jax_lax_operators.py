6c7ce71c3cdf381462d4e01a63c1f1e63752088a
kundakinte <69470630+kundakinte@users.noreply.github.com>
2023-03-18 01:21:03 +0300

    Update test_jax_lax_operators.py
    added test for erf

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_operators.py
@@ -2307,3 +2307,32 @@ def test_jax_lax_top_k(
         k=k,
         # test_values=False,
     )
+
+
+# erf
+@handle_frontend_test(
+    fn_tree="jax.lax.erf",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_jax_lax_erf(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
+
+
