514f97c0714c7d58ef7dbf23dd7840b6cf818631
trantuantdt <44942645+trantuantdt@users.noreply.github.com>
2023-03-18 18:54:47 +0700

    Update statistical.py

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -405,4 +405,3 @@ def std(a, axis=None, dtype=None, out=None, ddof=0, keepdims=False, *, where=Non
 @to_ivy_arrays_and_back
 def median(a, axis=None, out=None, overwrite_input=False, keepdims=False):
     return ivy.median(a, axis=axis, out=out, keepdims=keepdims)
-    
