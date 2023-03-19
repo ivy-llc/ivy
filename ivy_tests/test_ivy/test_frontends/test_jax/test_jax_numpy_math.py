8ed69687e3523654bd132c88df4b565ab5893054
Moamen <moamenelsaid423@yahoo.com>
2023-03-19 16:25:42 +0200

    add test function for jax.numpy.prod function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -548,6 +548,37 @@ def test_jax_numpy_dot(
     )
 
 
+#prod
+@handle_frontend_test(
+    fn_tree="jax.numpy.prod",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numirc"),
+        shape=helpers.get_shape(
+            min_num_dims=0,
+            max_num_dims=5,
+        ),
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_numpy_prod(
+    *,
+    dtype_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x,
+    )
+
+
 # mod
 @handle_frontend_test(
     fn_tree="jax.numpy.mod",
