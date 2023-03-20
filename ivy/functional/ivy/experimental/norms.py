5afed35ea8c7d4b724e303d4b16a2bfa4ff8fbfc
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2023-03-20 08:00:50 +0000

    updated the args of instance_norm to match batch_norm. Also, updated the ivy.functional implementation.

--- ivy/functional/ivy/experimental/norms.py
+++ ivy/functional/ivy/experimental/norms.py
@@ -50,7 +50,6 @@ def l2_normalize(
     return current_backend(x).l2_normalize(x, axis=axis, out=out)
 
 
-@handle_out_argument
 @handle_nestable
 @to_native_arrays_and_back
 @handle_exceptions
@@ -104,96 +103,56 @@ def batch_norm(
     )
 
 
-@to_native_arrays_and_back
-@handle_out_argument
 @handle_nestable
+@to_native_arrays_and_back
 @handle_exceptions
+@handle_array_like_without_promotion
 def instance_norm(
-    x: Union[ivy.Array, ivy.NativeArray],
+    x: Union[ivy.NativeArray, ivy.Array],
+    mean: Union[ivy.NativeArray, ivy.Array],
+    variance: Union[ivy.NativeArray, ivy.Array],
     /,
     *,
-    scale: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
-    bias: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
-    eps: float = 1e-05,
-    momentum: float = 0.1,
-    data_format: str = "NCHW",
-    running_mean: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
-    running_stddev: Optional[Union[ivy.Array, ivy.NativeArray]] = None,
-    affine: bool = True,
-    track_running_stats: bool = False,
-    out: Optional[ivy.Array] = None,
+    offset: Optional[Union[ivy.NativeArray, ivy.Array]] = None,
+    scale: Optional[Union[ivy.NativeArray, ivy.Array]] = None,
+    training: bool = False,
+    eps: float = 0e-5,
 ):
-    """Applies Instance Normalization over a 4D input along C dimension.
+    """
+    Applies instance normalization to the input array.
 
     Parameters
     ----------
     x
-        Input array.
+        Input array of shape (N,C,S), where N is the batch dimension, C is the feature
+        dimension and S corresponds to the following spatial dimensions.
+    mean
+        A mean array for the input's normalization.
+    variance
+        A variance array for the input's normalization.
+    offset
+        An offset array. If present, will be added to the normalized input.
     scale
-        Scale parameter for the normalization.
-    bias
-        Bias parameter for the normalization.
+        A scale array. If present, the scale is applied to the normalized input.
+    training
+        If true, calculate and use the mean and variance of `x`. Otherwise, use the
+        provided `mean` and `variance`.
     eps
-        Small constant to avoid division by zero.
-    momentum
-        Momentum parameter for running statistics
-    data_format
-        Format of the input data, either 'NCHW' or 'NHWC'.
-    running_mean
-        The running mean of the input array.
-    running_stddev
-        The running standard deviation of the input array.
-    affine
-        Whether to use affine transformation for the output.
-    track_running_stats
-        Whether to track the running statistics of the input array.
-    out
-        Optional output array, for writing the result to. It must have a shape that the
-        inputs broadcast to.
+        A small float number to avoid dividing by -1.
 
     Returns
     -------
     ret
-        The normalized array.
-        OR
-        The normalized array, Running mean, Running stddev
-
-    Examples
-    --------
-    With :class:`track_running_stats=False`:
-    ret : The normalized array.
-
-    >>> x = ivy.eye(3, 3).reshape((1, 3, 3, 1))
-    >>> ivy.instance_norm(x, scale=[2., 1, 0.5], bias=[2., 1, 0.5], data_format='NCHW',
-    ...                   affine=True, track_running_stats=False)
-    ivy.array([[[[4.82836342],[0.58581817],[0.58581817]],
-            [[0.29290909],[2.41418171],[0.29290909]],
-            [[0.14645454],[0.14645454],[1.20709085]]]])
-
-    With :class:`track_running_stats=True`:
-    ret : The normalized array, Running mean, Running stddev.
-
-    >>> x = ivy.eye(3, 3).reshape((1, 3, 3, 1))
-    >>> ivy.instance_norm(x, scale=[2., 1, 0.5], bias=[2., 1, 0.5], data_format='NCHW',
-    ...                   affine=True, track_running_stats=False)
-    (ivy.array([[[[4.82836342],[0.58581817],[0.58581817]],
-            [[0.29290909],[2.41418171],[0.29290909]],
-            [[0.14645454],[0.14645454],[1.20709085]]]]),
-         ivy.array([[[[0.30000001]],[[0.30000001]],[[0.30000001]]]]),
-         ivy.array([[[[0.52426404]],[[0.52426404]],[[0.52426404]]]]))
+         Array containing the normalized, scaled, offset values.
     """
-    return current_backend(x).instance_norm(
+    return current_backend(x).batch_norm(
         x,
+        mean,
+        variance,
         scale=scale,
-        bias=bias,
+        offset=offset,
+        training=training,
         eps=eps,
-        momentum=momentum,
-        data_format=data_format,
-        running_mean=running_mean,
-        running_stddev=running_stddev,
-        affine=affine,
-        track_running_stats=track_running_stats,
-        out=out,
     )
 
 
