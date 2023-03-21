9e78669950d1109c6e084bae9aa83f5ccc60bae4
RakshitKumar04 <kumarrakshit0402@gmail.com>
2023-03-22 01:31:09 +0530

    Resolved conflicts

--- ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
+++ ivy/functional/frontends/numpy/mathematical_functions/arithmetic_operations.py
@@ -295,6 +295,28 @@ def _mod(
 @to_ivy_arrays_and_back
 @handle_numpy_casting
 @from_zero_dim_arrays_to_scalar
+def _modf(
+    x, y,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.modf(x, y, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
+
+
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
 def _fmod(
     x1,
     x2,
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_arithmetic_operations.py
@@ -535,6 +535,55 @@ def test_numpy_mod(
     )
 
 
+# modf
+@handle_frontend_test(
+    fn_tree="numpy.modf",
+    dtypes_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float", "int"),
+    ),
+    where=np_frontend_helpers.where(),
+)
+def test_numpy_modf(
+    dtype_and_x,
+    where,
+    as_variable,
+    with_out,
+    num_positional_args,
+    native_array,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtypes, x, y = dtype_and_x
+    dtype, input_dtype, casting = np_frontend_helpers.handle_dtype_and_casting(
+        dtypes = input_dtype,
+        get_dtypes_kind = "float",
+    )
+    where, input_dtypes, test_flags = np_frontend_helpers.handle_where_and_array_bools(
+        where = where,
+        input_dtype = input_dtypes,
+        test_flags = test_flags,
+    )
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes = input_dtypes,
+        as_variable_flags = as_variable,
+        with_out = with_out,
+        num_positional_args = num_positional_args,
+        native_array_flags = native_array,
+        frontend = frontend,
+        fn_tree = fn_tree,
+        on_device = on_device,
+        x = x[0],
+        y = y[0],
+        out = None,
+        where = where,
+        casting = casting,
+        order = "K",
+        dtype = dtype,
+        subok = True,
+    )
+
+    
 # reciprocal
 @handle_frontend_test(
     fn_tree="numpy.reciprocal",
