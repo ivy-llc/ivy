52a1ae75ecdad1048c056c2eeb35d9710aad0e98
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2023-03-21 10:59:48 +0000

    added out argument and momentum to batch_norm

--- ivy/functional/backends/tensorflow/experimental/norms.py
+++ ivy/functional/backends/tensorflow/experimental/norms.py
@@ -25,15 +25,22 @@ def batch_norm(
     offset: Optional[Union[tf.Tensor, tf.Variable]] = None,
     training: bool = False,
     eps: float = 1e-5,
+    momentum: float = 1e-1,
+    out: Optional[tf.Tensor] = None,
 ) -> Union[tf.Tensor, tf.Variable]:
     ndims = len(x.shape)
+    runningmean = mean
+    runningvariance = variance
     if training:
         dims = (0, *range(2, ndims))
         mean = tf.math.reduce_mean(x, axis=dims)
         variance = tf.math.reduce_variance(x, axis=dims)
+        runningmean = (1 - momentum) * mean + momentum * runningmean
+        runningvariance = (1 - momentum) * variance + momentum * runningvariance
     x = tf.transpose(x, perm=(0, *range(2, ndims), 1))
     ret = tf.nn.batch_normalization(x, mean, variance, offset, scale, eps)
-    return tf.transpose(ret, perm=(0, ndims - 1, *range(1, ndims - 1)))
+    result = tf.transpose(ret, perm=(0, ndims - 1, *range(1, ndims - 1)))
+    return result, runningmean, runningvariance
 
 
 def instance_norm(
@@ -52,9 +59,9 @@ def instance_norm(
         dims = (*range(2, ndims),)
         mean = tf.math.reduce_mean(x, axis=dims)
         variance = tf.math.reduce_variance(x, axis=dims)
-    x = tf.transpose(x, perm=(*range(2, ndims), 0, 1))
+    x = tf.transpose(x, perm=(*range(2, ndims), 1, 0))
     ret = tf.nn.batch_normalization(x, mean, variance, offset, scale, eps)
-    return tf.transpose(ret, perm=(ndims - 2, ndims - 1, *range(0, ndims - 2)))
+    return tf.transpose(ret, perm=(ndims - 1, ndims - 2, *range(0, ndims - 2)))
 
 
 def lp_normalize(
