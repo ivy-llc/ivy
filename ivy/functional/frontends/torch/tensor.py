e44d94a83781703dbfb5fdd9c926d6101ccbf38f
Akshay Bhat <bhat7362@gmail.com>
2023-03-22 22:37:03 +0530

    added instance method for angle

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -269,6 +269,9 @@ class Tensor:
     def amin(self, dim=None, keepdim=False):
         return torch_frontend.amin(self._ivy_array, dim=dim, keepdim=keepdim)
 
+    def angle(self):
+        return torch_frontend.angle(self._ivy_array)
+
     @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
     def aminmax(self, dim=None, keepdim=False):
         return torch_frontend.aminmax(self._ivy_array, dim=dim, keepdim=keepdim)
