889cb1b78418240dc14a43b33e4fce4263882b64
Ayman Lafaz <ayman.lafaz@um5r.ac.ma>
2023-03-22 17:36:30 +0000

    ldexp numpy frontend (#12869)
    Co-authored by: nassimberrada

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -581,6 +581,7 @@ from ivy.functional.frontends.numpy.mathematical_functions.exponents_and_logarit
     _log2,
     _logaddexp,
     _logaddexp2,
+    _ldexp,
 )
 
 from ivy.functional.frontends.numpy.logic.array_type_testing import (
@@ -673,3 +674,5 @@ minimum = ufunc("_minimum")
 real = ufunc("_real")
 divmod = ufunc("_divmod")
 fmax = ufunc("_fmax")
+ldexp = ufunc("_ldexp")
+
