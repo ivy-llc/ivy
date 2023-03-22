a42d3984b43263ca9b32cca1ddd2f56bc1d8be70
hello-fri-end <shahanwaar.khalid8@gmail.com>
2023-03-22 16:05:08 +0530

    move batch_norm container and arrays methods from experimental/activations to experimental/norms

--- ivy/array/experimental/norms.py
+++ ivy/array/experimental/norms.py
@@ -38,7 +38,63 @@ class _ArrayWithNormsExperimental(abc.ABC):
                    [0.6, 0.8]])
         """
         return ivy.l2_normalize(self, axis=axis, out=out)
+    def batch_norm(
+            self,
+            mean: Union[ivy.NativeArray, ivy.Array],
+            variance: Union[ivy.NativeArray, ivy.Array],
+            /,
+            *,
+            offset: Optional[Union[ivy.NativeArray, ivy.Array]] = None,
+            scale: Optional[Union[ivy.NativeArray, ivy.Array]] = None,
+            training: bool = False,
+            eps: float = 1e-5,
+            momentum: float = 1e-1,
+            out: Optional[ivy.Array] = None,
+    ) -> ivy.Array:
+        """
+        ivy.Array instance method variant of ivy.batch_norm. This method
+        simply wraps the function, and so the docstring for ivy.batch_norm
+        also applies to this method with minimal changes.
 
+        Parameters
+        ----------
+        self
+            Input array of shape (N,C,S), where N is the batch dimension, C is the
+            feature dimension and S corresponds to the following spatial dimensions.
+        mean
+            A mean array for the input's normalization.
+        variance
+            A variance array for the input's normalization.
+        offset
+            An offset array. If present, will be added to the normalized input.
+        scale
+            A scale array. If present, the scale is applied to the normalized input.
+        training
+            If true, calculate and use the mean and variance of `x`. Otherwise, use the
+            provided `mean` and `variance`.
+        eps
+            A small float number to avoid dividing by 0.
+        momentum
+             The value used for the running_mean and running_var computation. Default value is 0.1.
+        out
+            Optional output array, for writing the result to.
+
+        Returns
+        -------
+        ret
+             Array containing the normalized, scaled, offset values.
+        """
+        return ivy.batch_norm(
+            self._data,
+            mean,
+            variance,
+            scale=scale,
+            offset=offset,
+            training=training,
+            eps=eps,
+            momentum=momentum,
+            out=out,
+        )
     def instance_norm(
         self: ivy.Array,
         /,
