79db4a7f24354bbe1c4ea12d1952594e169481b9
cmvcordova <cmvcordova@pm.me>
2023-03-22 00:43:35 -0400

    Added first 3 pytorch frontend gradient managing functions

---
+++ ivy/functional/frontends/torch/locally_disabling_gradient_computation.py
@@ -0,0 +1,24 @@
+import ivy
+import ivy.functional.frontends.torch as torch_frontend
+
+def no_grad():
+    return ivy.with_grads(with_grads=False)
+
+
+def enable_grad():
+    return ivy.with_grads(with_grads=True)
+
+
+def set_grad_enabled(mode=True):
+    return ivy.with_grads(mode)
+
+"""
+def is_grad_enabled():
+    return 
+
+def inference_mode(mode=True):
+    return 
+
+def is_inference_mode_enabled():
+    return
+"""
