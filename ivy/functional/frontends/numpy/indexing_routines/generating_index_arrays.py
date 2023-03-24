11c9dc71f257f6b8122e1c6e8727870a3015bb6a
billumillu <37789529+billumillu@users.noreply.github.com>
2023-03-24 05:03:33 +0000

    Update generating_index_arrays.py
    Added diag_indices( ), tril_indices( ) and tril_indices_from( ). Also, modified tril_indices( )

---
+++ ivy/functional/frontends/numpy/indexing_routines/generating_index_arrays.py
@@ -0,0 +1,24 @@
+import ivy
+from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
+    outputs_to_numpy_arrays,
+)
+
+
+
+@outputs_to_numpy_arrays
+def diag_indices(n, ndim=2):
+    idx = ivy.arange(n, dtype=int)
+    return (idx,) * ndim
+
+
+@to_ivy_arrays_and_back
+def tril_indices(n, k=0, m=None):
+    return ivy.tril_indices(n, k, m).    # changed (n, k, m) to (n, m, k)
+    
+  
+@to_ivy_arrays_and_back
+def tril_indices_from(arr, k=0):
+       if arr.ndim != 2:
+           raise ValueError("input array must be 2-d")
+       return ivy.tril_indices(arr.shape[-2], k=k, m=arr.shape[-1])
