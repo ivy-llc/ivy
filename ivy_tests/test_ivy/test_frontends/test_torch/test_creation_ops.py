4f842151ed98d5c22894e7e78d7dbba05109911b
Aditya Gandhamal <61016383+adityagandhamal@users.noreply.github.com>
2023-03-25 10:16:22 +0530

    change dtype to numeric
    To fix a failing test case on the Jax backend as the results did not match with the ground truth Torch framework. Changed input data type from `valid` to `numeric`

--- ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_creation_ops.py
@@ -607,8 +607,8 @@ def test_torch_heaviside(
 # asarray
 @handle_frontend_test(
     fn_tree="torch.asarray",
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
-    dtype=helpers.get_dtypes("valid", full=False),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("numeric")),
+    dtype=helpers.get_dtypes("numeric", full=False),
 )
 def test_torch_asarray(
     *,
