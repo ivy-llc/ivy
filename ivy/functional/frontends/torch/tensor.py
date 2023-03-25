2ad880730357d298eca8254318c470183e55d355
Debaditya Pal <debaditya.pal6@gmail.com>
2023-03-25 23:42:19 +0530

    Added fmin instance function to torch frontend

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -754,6 +754,10 @@ class Tensor:
     def atan2_(self, other):
         self._ivy_array = self.atan2(other).ivy_array
         return self
+    
+    @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+    def fmin(self, other, out=None):
+        return torch_frontend.fmin(self._ivy_array, other, out=out)
 
     # Special Methods #
     # -------------------#
