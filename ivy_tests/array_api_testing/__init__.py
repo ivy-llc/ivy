5fd7ac04b610590ab28f2dba5f19711fb27885d4
Aarsh2001 <ac5973@srmist.edu.in>
2023-03-25 14:35:53 +0530

    resolve import errrors in init

--- ivy_tests/array_api_testing/__init__.py
+++ ivy_tests/array_api_testing/__init__.py
@@ -1,6 +1,9 @@
+from .test_array_api import array_api_tests
+from .test_array_api.array_api_tests import *
+from .test_array_api import *
 from . import array_api_methods_to_test
 from . import test_array_api
-from .test_array_api import *
+
 
 try:
     from jax.config import config
