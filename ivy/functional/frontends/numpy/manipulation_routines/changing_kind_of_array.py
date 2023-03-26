5b20f33e11b5c6ff76b2734dbd614791c717709a
CodenificienT <61930093+codenificient@users.noreply.github.com>
2023-03-26 18:47:06 -0400

    update test_numpy_asanyarray

--- ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
@@ -1,1 +1,35 @@
 # local
+import ivy
+from typing import Optional, Union
+
+
+def asanyarray(
+    a: Union[ivy.Array],
+    dtype: Optional[ivy.dtype],
+    order: Optional = None,
+    *,
+    keepdims: bool = False,
+    like=None
+) -> ivy.Array:
+    """
+    Convert the input to an ndarray, but pass ndarray subclasses through.
+    Parameters
+    ----------
+    a
+        array_like
+        Input data, in any form that can be converted to an array.
+    dtype
+        data-type, optional
+        By default, the data-type is inferred from the input data.
+    order
+        {‘C’, ‘F’, ‘A’, ‘K’}, optional
+        Memory layout. Defaults to ‘C’.
+    like
+        array_like, optional
+        Reference object to allow the creation of arrays which are not NumPy arrays.
+        If an array-like passed in as like supports the __array_function__ protocol,
+    Returns
+    -------
+        out -- ndarray or an ndarray subclass
+    """
+    return ivy.any(a, axis=order, keepdims=keepdims, out=like)
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
@@ -1,47 +1,33 @@
-import ivy
-import numpy as np
-
-
 # local
-from ivy.functional.frontends.numpy import asanyarray
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
-def test_asanyarray():
-    # Test case 1: input is already an ndarray
-    arr = np.array([1, 2, 3])
-    assert np.array_equal(asanyarray(arr), arr)
-
-    # Test case 2: input is not an ndarray
-    ivy_arr = ivy.array([1, 2, 3])
-    np_arr = np.array([1, 2, 3])
-    assert np.array_equal(asanyarray(ivy_arr), np_arr)
-
-    # Test case 3: input is not an ndarray, and dtype is specified
-    ivy_arr = ivy.array([1, 2, 3], dtype=ivy.float32)
-    np_arr = np.array([1, 2, 3], dtype=np.float32)
-    assert np.array_equal(asanyarray(ivy_arr, dtype=np.float32), np_arr)
-
-    # Test case 4: input is not an ndarray, and order is specified
-    ivy_arr = ivy.array([[1, 2], [3, 4]])
-    np_arr = np.array([[1, 2], [3, 4]])
-    assert np.array_equal(asanyarray(ivy_arr, order="F"), np_arr.T)
-
-    # Test case 5: input is not an ndarray, and keepdims is True
-    ivy_arr = ivy.array([[1, 2], [3, 4]])
-    np_arr = np.array([[1, 2], [3, 4]])
-    assert np.array_equal(asanyarray(ivy_arr, keepdims=True), np_arr.reshape((2, 2, 1)))
-
-    # Test case 6: input is not an ndarray, and like is specified
-    ivy_arr = ivy.array([[1, 2], [3, 4]])
-    np_arr = np.array([[1, 2], [3, 4]])
-    like = np.zeros((2, 2))
-    assert np.array_equal(asanyarray(ivy_arr, like=like), np_arr)
-
-    # Test case 7: input is not an ndarray, and all optional parameters are specified
-    ivy_arr = ivy.array([[1, 2], [3, 4]], dtype=ivy.float32)
-    np_arr = np.array([[1, 2], [3, 4]], dtype=np.float32)
-    like = np.zeros((2, 2))
-    assert np.array_equal(
-        asanyarray(ivy_arr, dtype=np.float32, order="F", keepdims=True, like=like),
-        np_arr.T.reshape((2, 2, 1)),
+# asanyarray
+@handle_frontend_test(
+    fn_tree="numpy.asanyarray",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric")
+    ),
+    dtype=helpers.get_dtypes("numeric", full=False),
+)
+def test_numpy_asanyarray(
+    *,
+    dtype_and_x,
+    dtype,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        obj=x[0],
+        dtype=dtype[0],
+        device=on_device,
     )
