3741d903b210f22e12dbeee3751b732e65fcc221
youssef893 <youssefsayed893@gmail.com>
2023-03-27 21:07:39 +0200

    solve conflicts

--- ivy/functional/backends/tensorflow/experimental/activations.py
+++ ivy/functional/backends/tensorflow/experimental/activations.py
@@ -71,4 +71,5 @@ def selu(x: Tensor, /, *, out: Optional[Tensor] = None) -> Tensor:
     ret = tf.nn.selu(x)
     if ivy.exists(out):
         return ivy.inplace_update(out, ret).astype(x.dtype)
+    
     return ivy.astype(ret, x.dtype)
