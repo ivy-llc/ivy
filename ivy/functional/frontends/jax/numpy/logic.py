59e6f8fd57978501724dcb6425709171e1368584
Sohob <69919411+Sohob@users.noreply.github.com>
2023-03-27 19:25:56 +0200

    updated Frontend Jax Numpy iscomplexobj (#13216)
    Co-authored-by: nassimberrada <112006029+nassimberrada@users.noreply.github.com>

--- ivy/functional/frontends/jax/numpy/logic.py
+++ ivy/functional/frontends/jax/numpy/logic.py
@@ -209,3 +209,14 @@ def isrealobj(x: any):
 @to_ivy_arrays_and_back
 def iscomplex(x: any):
     return ivy.bitwise_invert(ivy.isreal(x))
+
+
+@to_ivy_arrays_and_back
+def iscomplexobj(x):
+    if x.ndim == 0:
+        return ivy.is_complex_dtype(ivy.dtype(x))
+    for ele in x:
+        if ivy.is_complex_dtype(ivy.dtype(ele)):
+            return True
+        else:
+            return False
