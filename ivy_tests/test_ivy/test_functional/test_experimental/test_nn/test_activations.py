cd32985794022b11dc12bb4254df4a5d26e97262
youssef893 <youssefsayed893@gmail.com>
2023-03-27 16:45:10 +0200

    added selu test function

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_activations.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_activations.py
@@ -238,3 +238,35 @@ def test_logsigmoid(
         on_device=on_device,
         input=x[0],
     )
+
+
+# selu
+@handle_test(
+    fn_tree="functional.ivy.experimental.selu",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        safety_factor_scale="log",
+        large_abs_safety_factor=120,
+    ),
+    test_with_out=st.just(False),
+)
+def test_selu(
+    *,
+    dtype_and_x,
+    test_flags,
+    backend_fw,
+    fn_name,
+    on_device,
+    ground_truth_backend,
+):
+    input_dtype, x = dtype_and_x
+    test_flags.num_positional_args = len(x)
+    helpers.test_function(
+        ground_truth_backend=ground_truth_backend,
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        fw=backend_fw,
+        fn_name=fn_name,
+        on_device=on_device,
+        input=x[0],
+    )
