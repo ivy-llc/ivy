6f2a9ba73e886c7665a27982f0ee7e4ef8db8db9
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-03-27 15:18:50 +0200

    add Slice RawOps operation to Tensorflow fronted (#12791)

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -714,3 +714,6 @@ def DebugGradientIdentity(input, name="DebugGradientIdentity"):
 @to_ivy_arrays_and_back
 def Real(input, Tout=ivy.float32, name="Real"):
     return ivy.Real(input, Tout=Tout)
+
+
+Slice = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.slice))
