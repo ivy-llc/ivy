0fa3597f924869316f9cd594959a35f9b6893651
shadymohammed7 <shadymohammed0@gmail.com>
2023-03-28 04:26:52 +0200

    created ifft2 function and relative test issue #13274

--- ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
+++ ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
@@ -8,3 +8,16 @@ def ifft(a, n=None, axis=-1, norm=None):
     if norm is None:
         norm = "backward"
     return ivy.ifft(a, axis, norm=norm, n=n)
+
+@to_ivy_arrays_and_back
+def ifft2(a,
+          n=None,
+          axis=(-1,-2),
+          norm=None
+          ):
+    a= ivy.array(a, dtype=ivy.complex128)
+    if norm is None:
+        norm = "backward"
+    return ivy.ifft2(a,axis, norm=norm, n=n)
+
+
