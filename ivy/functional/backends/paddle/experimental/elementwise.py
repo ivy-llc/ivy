9da597cfc79cb3a9c37443be8c14fce256ec5572
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-03-29 18:49:17 +0530

    Update elementwise.py

--- ivy/functional/backends/paddle/experimental/elementwise.py
+++ ivy/functional/backends/paddle/experimental/elementwise.py
@@ -84,17 +84,16 @@ def float_power(
 
 
 @with_unsupported_device_and_dtypes(
-    {"2.4.2 and below": {"cpu": ("uint16", "bfloat16","float16")}}, backend_version
-)  
+    {"2.4.2 and below": {"cpu": ("uint16", "bfloat16","float16","bool","complex128","complex64","int8","int16","uint8")}}, backend_version
+) 
 def exp2(
     x: Union[paddle.Tensor, float, list, tuple],
     /,
     *,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    y = paddle.full(shape=x.shape, fill_value=2, dtype=x.dtype)
-    res = paddle.pow(y,x)
-    return res
+    with ivy.ArrayMode(False):
+        return ivy.pow(2,x)
 
 
 def copysign(
