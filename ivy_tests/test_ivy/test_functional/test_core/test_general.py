4e89ff39ed47b9aaa8f0d21e2bdeabad7ad9d9f0
Harsh <90236987+Lancelot03@users.noreply.github.com>
2023-03-30 23:28:59 +0530

    Update test_general.py

--- ivy_tests/test_ivy/test_functional/test_core/test_general.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_general.py
@@ -1746,7 +1746,7 @@ _composition_1.test_unsupported_devices_and_dtypes = {
             "complex64",
             "complex128",
         ),
-        "paddle": ("uint16","uint32","uint64","bfloat16","complex64", "complex128"),
+        "paddle": ("uint16", "uint32", "uint64", "bfloat16", "complex64", "complex128"),
     },
     "gpu": {
         "numpy": ivy.all_dtypes,
@@ -1775,7 +1775,7 @@ _composition_2.test_unsupported_devices_and_dtypes = {
         "jax": ("complex64", "complex128"),
         "tensorflow": ("complex64", "complex128"),
         "torch": ("uint16", "uint32", "uint64", "float16", "complex64", "complex128"),
-        "paddle": ("uint16","uint32","uint64","bfloat16",),
+        "paddle": ("uint16", "uint32", "uint64", "bfloat16", ),
     },
     "gpu": {
         "numpy": ivy.all_dtypes,
