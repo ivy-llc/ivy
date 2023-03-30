b3dc779afe173f3857936d4d6b761f1f0a0d6ef3
vayuusaee <129206196+vayuusaee@users.noreply.github.com>
2023-03-30 23:53:30 +0530

    Added an if statement to check whether there are any changes to stash before doing so.
    This version checks whether there are any changes to stash before doing so, and uses git stash save instead of git stash to provide a description of the stash. It also checks whether there is a stash to pop before doing so, to avoid errors if there are no stashes to apply.

--- stash_pull.sh
+++ stash_pull.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# Set git pull.rebase mode to true
 git config pull.rebase true
-git stash
+
+# Stash any changes
+if git diff-index --quiet HEAD --; then
+    echo "No changes to stash"
+else
+    git stash save
+fi
+
+# Pull the latest changes from the remote repository
 git pull
-git stash apply
+
+# Apply any stashed changes
+if git stash list | grep -q "stash"; then
+    git stash pop
+fi
