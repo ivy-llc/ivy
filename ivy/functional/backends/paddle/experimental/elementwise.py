ef5e27d6b4d9e1264019571ff8703e0a5790031e
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-03-30 14:08:26 +0530

    added copysign in paddle elementwise.py (#13410)
    Co-authored-by: Mahmoud Ashraf <hassouna97.ma@gmail.com>

--- ivy/functional/backends/paddle/experimental/elementwise.py
+++ ivy/functional/backends/paddle/experimental/elementwise.py
@@ -96,6 +96,9 @@ def exp2(
         return ivy.pow(2,x)
 
 
+@with_unsupported_device_and_dtypes(
+    {"2.4.2 and below": {"cpu": ("uint16", "bfloat16")}}, backend_version
+)
 def copysign(
     x1: Union[paddle.Tensor, Number],
     x2: Union[paddle.Tensor, Number],
@@ -103,7 +106,10 @@ def copysign(
     *,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+    with ivy.ArrayMode(False):
+        x2 = ivy.where(ivy.equal(x2, 0),ivy.divide(1, x2),x2)
+        signs = ivy.sign(x2)
+        return ivy.multiply(ivy.abs(x1),signs)
 
 
 def count_nonzero(
