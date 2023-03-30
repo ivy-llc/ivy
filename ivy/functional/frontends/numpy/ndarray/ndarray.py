90c638a3cd95af0d78f144ce0a3d567c16e98e18
Deasy Indrawati <indrawatideasy@gmail.com>
2023-03-30 20:06:51 +0700

    Update ndarray.py

--- ivy/functional/frontends/numpy/ndarray/ndarray.py
+++ ivy/functional/frontends/numpy/ndarray/ndarray.py
@@ -469,13 +469,5 @@ class ndarray:
     def __mod__(self, value, /):
         return np_frontend.mod(self._ivy_array, value, out=self)
     
-    def byteswap(
-        self,
-        /,
-        *,
-        inplace=False,
-    ):
-        return np_frontend.byteswap(
-            self._ivy_array,
-            inplace=inplace,
-        )
+    def byteswap(self, *, inplace=False):
+    return np_frontend.byteswap(self._ivy_array, inplace=inplace)
