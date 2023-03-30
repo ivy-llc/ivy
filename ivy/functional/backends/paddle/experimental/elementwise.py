c1aa60cb849ccc05a9c572bdf7b6ca094bc0d7c5
VANDIT TYAGI <91458535+vandit98@users.noreply.github.com>
2023-03-30 13:41:17 +0530

    added nan_to_num and exp2 in paddle backend (#13350)
    Co-authored-by: Mahmoud Ashraf <hassouna97.ma@gmail.com>

--- ivy/functional/backends/paddle/experimental/elementwise.py
+++ ivy/functional/backends/paddle/experimental/elementwise.py
@@ -83,13 +83,17 @@ def float_power(
     return paddle.cast(paddle.pow(x1, x2), dtype=paddle.float64)
 
 
+@with_unsupported_device_and_dtypes(
+    {"2.4.2 and below": {"cpu": ("uint16", "bfloat16")}}, backend_version
+) 
 def exp2(
     x: Union[paddle.Tensor, float, list, tuple],
     /,
     *,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+    with ivy.ArrayMode(False):
+        return ivy.pow(2,x)
 
 
 def copysign(
@@ -190,6 +194,10 @@ def imag(
     return paddle.imag(val)
 
 
+@with_unsupported_dtypes(
+    {"2.4.2 and below": ("uint16", "bfloat16")},
+    backend_version,
+)
 def nan_to_num(
     x: paddle.Tensor,
     /,
@@ -200,7 +208,25 @@ def nan_to_num(
     neginf: Optional[Union[float, int]] = None,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+     with ivy.ArrayMode(False):
+        if ivy.is_int_dtype(x):
+            if posinf==None:
+                posinf = ivy.iinfo(x).max
+            if neginf==None:
+                neginf= ivy.iinfo(x).min
+        elif ivy.is_float_dtype(x) or ivy.is_complex_dtype(x):
+            if posinf==None:
+                posinf = ivy.finfo(x).max
+            if neginf==None:
+                neginf=ivy.finfo(x).min     
+        ret = ivy.where(ivy.isnan(x), paddle.to_tensor(nan, dtype=x.dtype), x)
+        ret = ivy.where(ivy.logical_and(ivy.isinf(ret), ret > 0), paddle.to_tensor(posinf, dtype=x.dtype), ret)
+        ret = ivy.where(ivy.logical_and(ivy.isinf(ret), ret < 0), paddle.to_tensor(neginf, dtype=x.dtype), ret)
+        if copy:
+            return ret.clone()
+        else:
+            x= ret
+            return x
 
 
 def logaddexp2(
