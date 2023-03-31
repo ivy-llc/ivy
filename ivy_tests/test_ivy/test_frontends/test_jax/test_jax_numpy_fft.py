d2132bad84af64804596896b54dc022e0cdc63aa
Gurumoorty Suresh <54660832+Guru-mp3@users.noreply.github.com>
2023-04-01 00:19:52 +0530

    Update test_jax_numpy_fft.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_fft.py
@@ -8,10 +8,11 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 # fft
 @handle_frontend_test(
     fn_tree="jax.numpy.fft.fft",
-    dtype_and_x = helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float"),
-    array_api_dtypes=True,
-    shape = helpers.ints(min_value=2).map(lambda x: tuple([x, ])))
-.filter(
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float"),
+                                         array_api_dtypes=True,
+                                         shape=helpers.ints(min_value=2).
+                                         map(lambda x: tuple([x, ])))
+    .filter(
     lambda x: "bfloat16" not in x[0]
     and "float16" not in x[0]
 )
@@ -23,7 +24,7 @@ def test_jax_numpy_fft(
     test_flags,
     on_device
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x=dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
