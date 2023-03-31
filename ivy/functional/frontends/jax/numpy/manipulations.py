096de15190b2cb5172ba1ecf63d4e9d17169ac6c
MohamedEhab <mohamedehab0999@gmail.com>
2023-03-31 04:43:23 +0200

    Fix lint

--- ivy/functional/frontends/jax/numpy/manipulations.py
+++ ivy/functional/frontends/jax/numpy/manipulations.py
@@ -252,8 +252,8 @@ def row_stack(tup):
 
 @to_ivy_arrays_and_back
 def hamming(M):
-    if M<=1:
-        return ivy.ones([M],dtype=ivy.float64)
+    if M <= 1:
+        return ivy.ones([M], dtype=ivy.float64)
     n = ivy.arange(M)
     ret = 0.54 - 0.46 * ivy.cos(2.0 * ivy.pi * n / (M - 1))
     return ret
