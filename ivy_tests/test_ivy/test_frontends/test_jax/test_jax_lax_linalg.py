5463c5a7ad1c7e19780b52350a2e887423258092
unknown <djalliltkd@gmail.com>
2023-03-31 14:27:10 +0100

    add test for eig function

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_linalg.py
@@ -121,6 +121,61 @@ def test_jax_lax_cholesky(
     )
 
 
+# eig
+@handle_frontend_test(
+    fn_tree="jax.numpy.linalg.eig",  # replace by "jax.lax.linalg.eigh"
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        max_value=10,
+        shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: "float16" not in x[0]
+        and "bfloat16" not in x[0]
+        and np.linalg.cond(x[1][0]) < 1 / sys.float_info.epsilon
+        and np.linalg.det(np.asarray(x[1][0])) != 0
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_numpy_eig(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    dtype, x = dtype_and_x
+    x = np.array(x[0], dtype=dtype[0])
+    """
+    make symmetric positive-definite since ivy does not support complex
+    data dtypes currently.
+    """
+    x = np.matmul(x.T, x) + np.identity(x.shape[0]) * 1e-3
+
+    ret, frontend_ret = helpers.test_frontend_function(
+        input_dtypes=dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=False,
+        a=x,
+    )
+
+    ret = [ivy.to_numpy(x) for x in ret]
+    frontend_ret = [np.asarray(x) for x in frontend_ret[0]]
+
+    L, Q = ret
+    frontend_L, frontend_Q = frontend_ret
+
+    assert_all_close(
+        ret_np=Q @ np.diag(L) @ Q.T,
+        ret_from_gt_np=frontend_Q @ np.diag(frontend_L) @ frontend_Q.T,
+        atol=1e-02,
+    )
+
+
 # eigh
 @handle_frontend_test(
     fn_tree="jax.lax.linalg.eigh",
