00932865870c56abf4d499cd79d9654fbafa7d22
hello-fri-end <shahanwaar.khalid8@gmail.com>
2023-03-31 14:36:41 +0530

    remove backend implementation of instance norm from jax and numpy backend since it's now a mixed function

--- ivy/functional/backends/jax/experimental/norms.py
+++ ivy/functional/backends/jax/experimental/norms.py
@@ -21,48 +21,6 @@ def l2_normalize(
     return x / denorm
 
 
-# @with_unsupported_dtypes
-# ({"0.3.14 and below": ("float16", "bfloat16")}, backend_version)
-# def instance_norm(
-#    x: JaxArray,
-#    mean: JaxArray,
-#    variance: JaxArray,
-#    /,
-#    *,
-#    scale: Optional[JaxArray] = None,
-#    offset: Optional[JaxArray] = None,
-#    training: bool = False,
-#    eps: float = 1e-5,
-#    momentum: float = 1e-1,
-#    out: Optional[JaxArray] = None,
-# ) -> Tuple[JaxArray, JaxArray, JaxArray]:
-#    # Instance Norm with (N,C,H,W) is the same as BatchNorm with (1, N * C, H, W)
-#    N = x.shape[0]
-#    C = x.shape[1]
-#    S = x.shape[2:]
-#    x = x.reshape((1, N * C, *S))
-#    mean = jnp.tile(mean, N)
-#    variance = jnp.tile(variance, N)
-#    scale = jnp.tile(scale, N)
-#    offset = jnp.tile(offset, N)
-#    xnormalized, runningmean, runningvariance = batch_norm(
-#        x,
-#        mean,
-#        variance,
-#        scale=scale,
-#        offset=offset,
-#        training=training,
-#        eps=eps,
-#        momentum=momentum,
-#        out=out,
-#    )
-#    return (
-#        xnormalized.reshape((N, C, *S)),
-#        runningmean.reshape(N, C).mean(0),
-#        runningvariance.reshape(N, C).mean(0),
-#    )
-
-
 @with_unsupported_dtypes({"0.3.14 and below": ("float16",)}, backend_version)
 def lp_normalize(
     x: JaxArray,
