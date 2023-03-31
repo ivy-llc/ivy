56cd656301994e56bd2510c9567c457acfa16ef9
hello-fri-end <shahanwaar.khalid8@gmail.com>
2023-03-31 14:36:41 +0530

    remove backend implementation of instance norm from jax and numpy backend since it's now a mixed function

--- ivy/functional/backends/numpy/experimental/norms.py
+++ ivy/functional/backends/numpy/experimental/norms.py
@@ -20,46 +20,6 @@ def l2_normalize(
     return x / denorm
 
 
-# def instance_norm(
-#    x: np.ndarray,
-#    mean: np.ndarray,
-#    variance: np.ndarray,
-#    /,
-#    *,
-#    scale: Optional[np.ndarray] = None,
-#    offset: Optional[np.ndarray] = None,
-#    training: bool = False,
-#    eps: float = 1e-5,
-#    momentum: float = 1e-1,
-#    out: Optional[np.ndarray] = None,
-# ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-#    # Instance Norm with (N,C,H,W) is the same as BatchNorm with (1, N * C, H, W)
-#    N = x.shape[0]
-#    C = x.shape[1]
-#    S = x.shape[2:]
-#    x = x.reshape((1, N * C, *S))
-#    mean = np.tile(mean, N)
-#    variance = np.tile(variance, N)
-#    scale = np.tile(scale, N)
-#    offset = np.tile(offset, N)
-#    xnormalized, runningmean, runningvariance = batch_norm(
-#        x,
-#        mean,
-#        variance,
-#        scale=scale,
-#        offset=offset,
-#        training=training,
-#        eps=eps,
-#        momentum=momentum,
-#        out=out,
-#    )
-#    return (
-#        xnormalized.reshape((N, C, *S)),
-#        runningmean.reshape(N, C).mean(0),
-#        runningvariance.reshape(N, C).mean(0),
-#    )
-
-
 def lp_normalize(
     x: np.ndarray,
     /,
