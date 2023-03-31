ccb7f78a5d182c9cf3f0199eede00a9027738235
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2023-03-31 07:17:47 +0000

    Used jax.interpreters instead of jax._src.interpreters in _to_array as jax._src.interpreters doesn't work in older versions of jax and removed unused import of config in jax which is a remnant of x64 usage

--- ivy/functional/backends/jax/device.py
+++ ivy/functional/backends/jax/device.py
@@ -21,7 +21,7 @@ def _to_array(x):
         return _to_array(x.primal)
     elif isinstance(x, jax.interpreters.partial_eval.DynamicJaxprTracer):
         return _to_array(x.aval)
-    elif isinstance(x, jax._src.interpreters.batching.BatchTracer):
+    if isinstance(x, jax.interpreters.batching.BatchTracer):
         return _to_array(x.val)
     return x
 
