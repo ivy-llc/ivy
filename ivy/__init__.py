232fd7946852c4244a7c3a16ee01ee1fa5c609e2
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2023-04-01 17:53:19 +0530

    data mode casting

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -1188,3 +1188,23 @@ for backend_framework in _not_imported_backends:
             f"{backend_framework} module has been imported while ivy doesn't "
             "import it without setting a backend, ignore if that's intended"
         )
+
+downcast_dtypes = False
+upcast_dtypes = False
+crosscast_dtypes = False
+cast_data_types = lambda: downcast_dtypes and upcast_dtypes and crosscast_dtypes
+
+
+def downcast_data_types(val=True):
+    global downcast_dtypes
+    downcast_dtypes = val
+
+
+def upcast_data_types(val=True):
+    global upcast_dtypes
+    upcast_dtypes = val
+
+
+def crosscast_data_types(val=True):
+    global crosscast_dtypes
+    crosscast_dtypes = val
