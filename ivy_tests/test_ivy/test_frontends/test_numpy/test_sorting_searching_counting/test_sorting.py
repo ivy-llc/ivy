db31e166df26b6d2eae56d6974a3653f5ce3264a
Deasy Indrawati <indrawatideasy@gmail.com>
2023-04-01 14:37:58 +0700

    Update test_sorting.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_sorting_searching_counting/test_sorting.py
@@ -160,31 +160,3 @@ def test_numpy_lexsort(
         keys=x[0],
         axis=axis,
     )
-
-
-@handle_frontend_test(
-    fn_tree="numpy.byteswap",
-    dtype_x_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        inplace=False,
-    ),
-    test_with_out=st.just(False),
-)
-def test_numpy_byteswap(
-    *,
-    dtype_x_axis,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device,
-):
-    input_dtype, x, axis = dtype_x_axis
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x=x[0],
-        inplace=inplace,
-    )
