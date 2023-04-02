7e719c2b07b24ecb098f39cbec15a31aa079dd73
Theophilus Onyejiaku <onyejiakut@gmail.com>
2023-04-02 07:24:36 +0100

    Update requirement_handler.py

--- requirement_handler.py
+++ requirement_handler.py
@@ -1,81 +1,13 @@
-# flake8: noqa
 from typing import List
 import operator
 import requests
 import sys
 
-url = "https://pypi.python.org/pypi/"
+PYPI_URL = "https://pypi.python.org/pypi/"
 
-
-def tilde(x, y):
-    if len(x) != len(y):
-        return None
-    for i in range(len(x) - 1):
-        if x[i] != y[i]:
-            return None
-    return True
-
-
-def lower_of_two(x, y):
-    return x if x < y else y
-
-
-def higher_of_two(x, y):
-    return x if x > y else y
-
-
-def make_len_equal(x, y):
-    if len(x) < len(y):
-        x = list(x)
-        while len(x) != len(y):
-            x.append("0")
-        return (tuple(x), y)
-    else:
-        y = list(y)
-        while len(y) != len(x):
-            y.append("0")
-        return (x, tuple(y))
-
-
-def inti(a):
-    try:
-        return int(a)
-    except:
-        return a
-
-
-def higher_or_lower(x, y, operator, operator1):
-    acceptable_version = None
-    if len(x) != len(y):
-        x, y = make_len_equal(x, y)
-    x = tuple(map(inti, x))
-    y = tuple(map(inti, y))
-    if string_dict[operator](x, y):
-        if operator in lower:
-            acceptable_version = lower_of_two(x, y)
-        else:
-            acceptable_version = higher_of_two(x, y)
-
-    if string_dict[operator1](y, x):
-        if operator1 in lower:
-            if acceptable_version:
-                acceptable_version = higher_of_two(
-                    acceptable_version, lower_of_two(y, x)
-                )
-            else:
-                acceptable_version = lower_of_two(y, x)
-        else:
-            if acceptable_version:
-                acceptable_version = higher_of_two(
-                    acceptable_version, higher_of_two(y, x)
-                )
-            else:
-                acceptable_version = higher_of_two(y, x)
-    return acceptable_version
-
-
-string_dict = {
-    "~=": tilde,
+# Define comparison operators
+COMPARISON_OPS = {
+    "~=": lambda x, y: x[: len(y)] == y,
     ">=": operator.ge,
     "<=": operator.le,
     ">": operator.gt,
@@ -84,117 +16,99 @@ string_dict = {
     "!=": operator.ne,
 }
 
-lower = ["<=", "<"]
+# Define a list of comparison operators that represent "lower than"
+LOWER_OPS = ["<=", "<"]
 
 
-def get_version_from_string(s):
-    s = s.replace("(", "").replace(")", "").split(",")
-    # s is now a list with version orderings for eg: [<2.0,>=1.37.0]
-    lis = []
-    for i in s:
-        string = ""
-        for j in range(len(i)):
-            while not str.isdigit(i[j]):
-                string = string + i[j]
-                j += 1
-            if string not in string_dict:
-                raise ValueError(f"string_dict doesn't have {string}")
-            lis.append({string: tuple(i[j:].split("."))})
-            break
-    return lis
+def compare_versions(x, y, op, op1):
+    """
+    Compares two version numbers using the given comparison operators.
+    Returns the higher or lower version number, depending on which operator was used.
+    """
+    if len(x) != len(y):
+        x, y = make_versions_equal_length(x, y)
 
+    x = tuple(map(convert_to_int_if_possible, x))
+    y = tuple(map(convert_to_int_if_possible, y))
 
-def get_package_versions(lis: List):
-    if not lis:
-        return dict()
-    dic = dict()
-    for i in lis:
-        if ";" in i:
-            i = i[: i.index(";")]
-        if " " in i:
-            dic[i.split(" ")[0]] = get_version_from_string(i.split(" ")[1])
+    if COMPARISON_OPS[op](x, y):
+        if op in LOWER_OPS:
+            return min(x, y)
         else:
-            dic[i] = ()
+            return max(x, y)
 
-    return dic
+    if COMPARISON_OPS[op1](y, x):
+        if op1 in LOWER_OPS:
+            return min(y, x)
+        else:
+            return max(y, x)
+
+    return None
 
 
-def resolution(conflict_keys, dic1, dic2):
-    resolution = {}
-    for i in conflict_keys:
-        if dic1[i] == dic2[i]:
+def make_versions_equal_length(x, y):
+    """
+    Make the given version numbers equal in length by padding with zeroes.
+    """
+    if len(x) < len(y):
+        x = list(x) + ["0"] * (len(y) - len(x))
+    else:
+        y = list(y) + ["0"] * (len(x) - len(y))
+    return x, y
+
+
+def convert_to_int_if_possible(value):
+    """
+    Convert a string to an int if possible, otherwise return the original string.
+    """
+    try:
+        return int(value)
+    except ValueError:
+        return value
+
+
+def parse_version_constraints(version_constraints):
+    """
+    Parse the version constraints string for a package into a dictionary of operator-version tuples.
+    """
+    version_constraints = (
+        version_constraints.replace("(", "")
+        .replace(")", "")
+        .replace(" ", "")
+        .split(",")
+    )
+    constraints_dict = {}
+    for constraint in version_constraints:
+        op, version = split_constraint(constraint)
+        constraints_dict[op] = tuple(version.split("."))
+    return constraints_dict
+
+
+def split_constraint(constraint):
+    """
+    Split a version constraint string into the comparison operator and version number.
+    """
+    for i in range(len(constraint)):
+        if constraint[i] not in COMPARISON_OPS:
             continue
-        for j in dic1[i]:
-            for k in dic2[i]:
-                resolution[i] = higher_or_lower(
-                    k[list(k.keys())[0]],
-                    j[list(j.keys())[0]],
-                    list(j.keys())[0],
-                    list(k.keys())[0],
-                )
-
-    return resolution
+        return constraint[:i], constraint[i + 1 :]
+    raise ValueError(f"Invalid version constraint: {constraint}")
 
 
-def package_conflicts(pkg1, pkg2=None):
-    data_1 = get_package_requirements(pkg1)
-    data_2 = get_package_requirements(pkg2)
-    if not data_1 or not data_2:
-        return None
-    data_1_dic = (
-        [] if not data_1["info"]["requires_dist"] else data_1["info"]["requires_dist"]
-    )
-    data_2_dic = (
-        [] if not data_2["info"]["requires_dist"] else data_2["info"]["requires_dist"]
-    )
-    data_1_dic = get_package_versions(
-        ["python " + str(data_1["info"]["requires_python"])] + data_1_dic
-    )
-    data_2_dic = get_package_versions(
-        ["python " + str(data_2["info"]["requires_python"])] + data_2_dic
-    )
-    return resolution(data_1_dic.keys() & data_2_dic.keys(), data_1_dic, data_2_dic)
+def get_package_version_constraints(package_name):
+    """
+    Retrieve the version constraints for the given package from PyPI.
+    """
+    package_info = requests.get(PYPI_URL + package_name + "/json").json()
+    requires_python = ["python " + str(package_info["info"]["requires_python"])]
+    requires_dist = package_info["info"]["requires_dist"] or []
+    return parse_version_constraints(requires_python + requires_dist)
 
 
-def get_package_requirements(pkg):
-    return requests.get(url + str(pkg) + "/json").json()
-
-
-def tuple_to_version(tup):
-    s = ""
-    for i in tup:
-        s += str(i) + "."
-    return s[:-1]
-
-
-# example usage
-# if __name__ == "__main__":
-#     fw1=sys.argv[1]
-#     fw2=sys.argv[2]
-#     python=sys.argv[3]
-#     conf=package_conflicts(fw1,fw2)
-#     if not conf:
-#         sys.exit("-1")
-#     if 'python' in conf:
-#         if tuple_to_version(conf['python'])>python:
-#             conf['python']=[conf['python'],-1]
-#         elif tuple_to_version(conf['python'])<python:
-#             conf['python']=[conf['python'],-2]
-#
-#         else: del conf['python']
-#     f=open("additional_requirements.txt","w")
-#     for key,val in conf.items():
-#         if key=='python':
-#             if val[-1]==-1:
-#                 print(f"The environment has python {python} which is lower than the required version {tuple_to_version(val[0])} do anticipate unexpected results")
-#             else:
-#                 print(
-#                     f"The environment has python {python} which is greater than the required version {tuple_to_version(val[0])} do anticipate unexpected results")
-#
-#         f.write(f"{key}=={val}")
-
-
-print(package_conflicts("h5py/3.7.0", "h5py/3.7.0"))
-# #
-# # print(get_package_requirements('tensorflow/2.2.0')["info"]["requires_dist"])
-# print(get_package_requirements('torch/1.5.0')["info"]["requires_python"])
+def resolve_version_conflicts(pkg1, pkg2):
+    """
+    Given two package names, resolve any version conflicts between them.
+    Returns a dictionary of package name-version tuples representing the resolved versions.
+    """
+    pkg1_constraints = get_package_version_constraints(pkg1)
+    pkg2_constraints = get_package_version_constraints
