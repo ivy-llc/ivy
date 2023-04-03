f4abfc9ac6bfbe115269a189c2bd57f01b800499
Aneesh Tickoo <72318258+Aneesh02@users.noreply.github.com>
2023-04-03 19:07:26 +0530

    Update test_sums_products_differences.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_sums_products_differences.py
@@ -334,23 +334,21 @@ def test_numpy_diff(
 # ediff1d
 @handle_frontend_test(
     fn_tree="numpy.ediff1d",
-    dtype_x_axis=helpers.dtype_values_axis(
+    dtype_and_values=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("numeric"),
         num_arrays=3,
         min_num_dims=1,
         min_dim_size=1,
-        valid_axis=True,
-        force_int_axis=True,
     ),
 )
 def test_numpy_ediff1d(
-        dtype_x_axis,
+        dtype_and_values,
         frontend,
         test_flags,
         fn_tree,
         on_device,
 ):
-    input_dtype, x, axis = dtype_x_axis
+    input_dtype, x = dtype_and_values
     np_frontend_helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
