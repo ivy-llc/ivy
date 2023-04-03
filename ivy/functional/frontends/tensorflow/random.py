bd4bbdee1fb79e1398b3219f837465b4fa9d0562
jieunboy0516 <jieunboy0516@gmail.com>
2023-04-03 06:57:56 +0000

    after lint check

--- ivy/functional/frontends/tensorflow/random.py
+++ ivy/functional/frontends/tensorflow/random.py
@@ -20,6 +20,7 @@ def uniform(shape, minval=0, maxval=None, dtype=ivy.float32, seed=None, name=Non
 def normal(shape, mean=0.0, stddev=1.0, dtype=ivy.float32, seed=None, name=None):
     return ivy.random_normal(mean=mean, std=stddev, shape=shape, dtype=dtype, seed=seed)
 
+
 # implement random shuffle
 @with_unsupported_dtypes(
     {"2.9.0 and below": ("int8", "int16", "in32", "int64", "unsigned")}, "tensorflow"
@@ -28,8 +29,11 @@ def normal(shape, mean=0.0, stddev=1.0, dtype=ivy.float32, seed=None, name=None)
 def shuffle(value, seed=None, name=None):
     return ivy.shuffle(value, seed=seed)
 
+
 @to_ivy_arrays_and_back
-def stateless_uniform(shape, seed, minval=0, maxval=None, dtype=ivy.float32, name=None, alg='auto_select'):
+def stateless_uniform(
+    shape, seed, minval=0, maxval=None, dtype=ivy.float32, name=None, alg="auto_select"
+):
     return ivy.random_uniform(
-        shape=shape, seed=seed[0]+seed[1], low=minval, high=maxval, dtype=dtype
+        shape=shape, seed=seed[0] + seed[1], low=minval, high=maxval, dtype=dtype
     )
