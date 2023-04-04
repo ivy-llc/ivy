d729f628b4cfb09b24681b8e50d592767da35452
younggraham-t <younggraham.t@gmail.com>
2023-04-04 10:32:10 -0400

    update asmatrix test and remove unused import from asmatrix

--- ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
@@ -3,12 +3,11 @@
 import ivy
 
 from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
-from ivy.functional.frontends.numpy import matrix
 
 @inputs_to_ivy_arrays
 def asmatrix(data, dtype=None):
     return ivy.matrix(data, dtype=dtype, copy=False)
-    
+
 
 
 
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
@@ -1,35 +1,40 @@
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-    
+from ivy_test.test_frontends.test_numpy import helpers as np_frontend_helpers
 
 
 @handle_frontend_test(
 fn_tree="numpy.asmatrix",
-dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-    get_dtypes_kind="numeric",
-),
+dtypes_values_casting=np_frontend_helpers.dtypes_values_casting_dtype(
+        arr_func=[
+            lambda: helpers.dtype_and_values(
+                available_dtypes=helpers.get_dtypes("float"),
+            )
+        ],
+        get_dtypes_kind="float",
+    ),
 number_positional_args=np_frontend_helpers.get_num_positional_args_ufunc(
     fn_name="asmatrix"
 ),
 )
 def test_numpy_asmatrix(
-    dtypes_and_x,
+    dtypes_values_casting,
     frontend,
     test_flags,
     fn_tree,
     on_device,
 ):
-    input_dtypes, x = dtypes_and_x
-    
+    input_dtypes, x, casting, dtype = dtypes_values_casting
+
     np_frontend_helpers.test_frontend_function(
         input_dtypes=input_dtypes,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        
+        x=x[0]
         out=None,
         dtype=dtype,
-        
+        casting=casting,
+
         )
