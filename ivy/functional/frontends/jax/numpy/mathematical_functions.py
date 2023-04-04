17fa267d0f494c843244f5de6c2312f0099e8392
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-04-04 10:46:22 +0000

    fixed tests of around in jax frontend

--- ivy/functional/frontends/jax/numpy/mathematical_functions.py
+++ ivy/functional/frontends/jax/numpy/mathematical_functions.py
@@ -515,9 +515,7 @@ def around(a, decimals=0, out=None):
     if ivy.shape(a) == ():
         a = ivy.expand_dims(a, axis=0)
     ret_dtype = a.dtype
-    factor = ivy.pow(10, decimals)
-    a = ivy.multiply(a, factor)
-    return ivy.divide(ivy.round(a), factor).astype(ret_dtype)
+    return ivy.round(a, decimals=decimals, out=out).astype(ret_dtype, copy=False)
 
 
 @to_ivy_arrays_and_back
