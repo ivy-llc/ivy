a91c5d2c050f8f010ea0af34b36b30cffec90316
Anurag verma <78868769+anurag629@users.noreply.github.com>
2023-04-05 13:24:42 +0000

    changed data_format value as suggested by Aarsh the 'channel_list' is not there

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -1314,7 +1314,7 @@ def test_tensorflow_crelu(
     fn_tree="tensorflow.nn.conv_transpose",
         x_f_d_df=_x_and_filters(
         dtypes=helpers.get_dtypes("float", full=False),
-        data_format=st.sampled_from(["channels_last"]),
+        data_format=st.sampled_from(["NWC", "NHWC"]),
         padding=st.sampled_from(["SAME", "VALID"]),
         dilation_max=1,
         type=None,
