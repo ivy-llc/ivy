235624cc6c1a25d00db4a5a305287c91a35338df
harshxtanwar <harshwardhan.t12345@gmail.com>
2023-04-06 07:19:45 +0000

    replaced unsupported dtypes with supported dtypes

--- ivy/functional/frontends/torch/utilities.py
+++ ivy/functional/frontends/torch/utilities.py
@@ -1,6 +1,6 @@
 import ivy
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
-from ivy.func_wrapper import with_unsupported_dtypes
+from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
 
 
 @to_ivy_arrays_and_back
@@ -15,13 +15,9 @@ def _assert(condition, message):
     else:
         return True
 
-@with_unsupported_dtypes({"1.11.0 and below": (
-        "int8", "int16", "int32",
-        'bool',
-        'float16', 'float32', 'float64', 
-        "complex64", 'complex38',
-        'bfloat16',
-        'uint8')}, "torch")
+@with_supported_dtypes({"1.11.0 and above": (
+        "int64",
+)}, "torch")
 @to_ivy_arrays_and_back
 def bincount(x, weights=None, minlength=0):
     return ivy.bincount(x, weights=weights, minlength=minlength)
