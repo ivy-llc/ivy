c2d8b6bb1059cd2c3d606cf0f5ac524cac2b5e48
Andrew Samy <andrew.samy.hanna1998@gmail.com>
2023-04-06 14:23:48 +0200

    Update averages_and_variances.py

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -395,34 +395,34 @@ def nanpercentile(
 @to_ivy_arrays_and_back
 @from_zero_dim_arrays_to_scalar
 def nanmedian(
-a,
-/,
-*,
-axis=None,
-keepdims=False,
-out=None,
-dtype=None,
-where=True,
+    a,
+    /,
+    *,
+    axis=None,
+    keepdims=False,
+    out=None,
+    dtype=None,
+    where=True,
 ):
-is_nan = ivy.isnan(a)
-axis = tuple(axis) if isinstance(axis, list) else axis
+    is_nan = ivy.isnan(a)
+    axis = tuple(axis) if isinstance(axis, list) else axis
 
-if not ivy.any(is_nan):
-if dtype:
-a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
-ret = ivy.median(a, axis=axis, keepdims=keepdims, out=out)
+    if not ivy.any(is_nan):
+        if dtype:
+            a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+        ret = ivy.median(a, axis=axis, keepdims=keepdims, out=out)
 
-if ivy.is_array(where):
-ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+        if ivy.is_array(where):
+            ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
 
-else:
-a = [i for i in a if ivy.isnan(i) is False]
+    else:
+        a = [i for i in a if ivy.isnan(i) is False]
 
-if dtype:
-a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
-ret = ivy.median(a, axis=axis, keepdims=keepdims, out=out)
+        if dtype:
+            a = ivy.astype(ivy.array(a), ivy.as_ivy_dtype(dtype))
+        ret = ivy.median(a, axis=axis, keepdims=keepdims, out=out)
 
-if ivy.is_array(where):
-ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+        if ivy.is_array(where):
+            ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
 
-return ret
+    return 
