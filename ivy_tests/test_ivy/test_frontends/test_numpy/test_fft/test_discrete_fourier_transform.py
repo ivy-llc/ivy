2d3a5d7c979478ba4ed97c122070c40ff3c93159
shadymohammed7 <shadymohammed0@gmail.com>
2023-04-07 02:56:38 +0200

    test improved

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -1,3 +1,5 @@
+import ivy
+import ivy.numpy as np
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
@@ -35,26 +37,27 @@ def test_numpy_iftt(
     fn_tree="numpy.fft.ifft2",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
-        shape=(4,4), array_api_dtypes=True
+        available_shapes=[(4, 4), (3, 5), (2, 2, 2)]
     )
 )
 def test_numpy_ifft2(
-    dtype_and_x,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device
+        dtype_and_x,
+        frontend,
+        test_flags,
+        fn_tree,
+        on_device
 ):
     input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        test_values=True,
-        a=x,
-        n=None,
-        axis=(-2, -1),
-        norm=None
-    )
+    a = ivy.array(x, dtype=ivy.complex128)
+
+    # Test `ifft2` function with different parameter values
+    for n in [None, 4]:
+        for axis in [(-1, -2), (-2, -1)]:
+            for norm in [None, "forward", "backward"]:
+
+                np_output = np.fft.ifft2(x, n=n, axes=axis, norm=norm)
+
+                ivy_output = ivy.to_numpy(ivy.ifft2(a, n=n, axis=axis, norm=norm))
+
+                helpers.assert_allclose(np_output, ivy_output, rtol=1e-4, atol=1e-4, backend=frontend)
+
