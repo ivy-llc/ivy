c08d6e6e56061e8e12c3146e71aa5e6626601c1c
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-04-07 23:58:57 +0100

    Fix test_tril_indices for paddle backend by returning a tuple like other backends and in accordance with the return sign

--- ivy/functional/backends/paddle/experimental/creation.py
+++ ivy/functional/backends/paddle/experimental/creation.py
@@ -200,6 +200,6 @@ def tril_indices(
     *,
     device: Place,
 ) -> Tuple[paddle.Tensor, ...]:
-    return to_device(
+    return tuple(to_device(
         paddle.tril_indices(n_rows, col=n_cols, offset=k, dtype="int64"), device
-    )
+    ))
