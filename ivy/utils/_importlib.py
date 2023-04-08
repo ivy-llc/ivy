6e828b7ca78707a88878de3133d3d10c66d6d7a4
CatB1t <skytedits@gmail.com>
2023-04-08 15:41:28 +0200

    add partial update for modules skipping.

--- ivy/utils/_importlib.py
+++ ivy/utils/_importlib.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from pathlib import Path
 from importlib.util import resolve_name, module_from_spec
 from ivy.utils.backend import ast_helpers
 
@@ -76,6 +78,54 @@ def _absolute_import(name: str, asname=None, mod_globals=None):
     mod_globals[true_name] = module
 
 
+def _get_all_modules(absolute_name: str):
+    ivy_path = sys.modules["ivy"].__file__
+    name_to_path = os.path.join(ivy_path, absolute_name.replace(".", os.path.sep))
+    for root, dirs, files in os.walk(module_path):
+        if root.endswith("__"):
+            continue
+        common = os.path.commonpath([root, module_path])
+        package_full = root[len(common) :].replace(os.path.sep, ".")
+        print(absolute_name + package_full)
+        for name in files:
+            if name.startswith("__") or not name.endswith(".py"):
+                continue
+            print(f"{absolute_name+package_full}.{name[:-3]}")
+        for name in dirs:
+            if name.startswith("__"):
+                continue
+            # print(name)
+
+
+modules_to_skip = ["ivy.data_classes", "ivy.compiler"]
+
+modules_to_exclude = ["ivy.data_classes.array.conversion"]
+
+_full_modules_to_skip = _get_modules(modules_to_skip[0])
+ivy_path_abs = Path(sys.modules["ivy"].__file__).parents[1]
+_modules_to_skip = []
+
+
+def _get_modules(absolute_name):
+    name_to_path = absolute_name.replace(".", os.path.sep)
+    module_path = os.path.join(ivy_path_abs, name_to_path)
+    print(module_path)
+    for root, dirs, files in os.walk(module_path):
+        if root.endswith("__"):
+            continue
+        common = os.path.commonpath([root, module_path])
+        package_full = root[len(common) :].replace(os.path.sep, ".")
+        print(absolute_name + package_full)
+        for name in files:
+            if name.startswith("__") or not name.endswith(".py"):
+                continue
+            print(f"{absolute_name+package_full}.{name[:-3]}")
+        for name in dirs:
+            if name.startswith("__"):
+                continue
+            # print(name)
+
+
 def _import_module(name, package=None):
     global import_cache
     absolute_name = resolve_name(name, package)
@@ -89,6 +139,13 @@ def _import_module(name, package=None):
         parent_name, _, child_name = absolute_name.rpartition(".")
         parent_module = _import_module(parent_name)
         path = parent_module.__spec__.submodule_search_locations
+    for _module in modules_to_skip:
+        if absolute_name.startswith(_module):
+            print("Skipping", absolute_name)
+            if path is not None:
+                # Set reference to self in parent, if exist
+                setattr(parent_module, child_name, sys.modules[absolute_name])
+            return sys.modules[absolute_name]
     for finder in path_hooks:
         spec = finder.find_spec(absolute_name, path)
         if spec is not None:
