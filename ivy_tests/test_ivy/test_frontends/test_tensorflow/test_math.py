1cf5c1c7eca6e60c7907b42ab9ed0e5a074269d5
Piyush Aaryan <piyushaaryan011@gmail.com>
2023-04-08 23:45:21 +0530

    added_cosh

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -2023,3 +2023,30 @@ def test_tensorflow_softmax(
         on_device=on_device,
         logits=x[0],
     )
+
+
+# cosh
+@handle_frontend_test(
+    fn_tree="tensorflow.math.cosh",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float_and_complex"),
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_cosh(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
