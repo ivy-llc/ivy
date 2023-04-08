df57a653c21d408193c02f68e729416993d04478
Ömer Faruk Ballı <omerfarukballi@gmail.com>
2023-04-08 17:32:48 +0000

    LogSoftmax #13877

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -463,8 +463,7 @@ def abs(x, name=None):
 
 @to_ivy_arrays_and_back
 def log_softmax(logits, axis=None):
-    return ivy.log_softmax(logits, axis=axis)
-
+    return ivy.log(ivy.softmax(ivy.array(logits), axis=None))
 
 @to_ivy_arrays_and_back
 def asin(x, name=None):
