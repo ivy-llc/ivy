576b82bfa653663d5bec8ebcf06e85c45962756e
Gurumoorty Suresh <54660832+Guru-mp3@users.noreply.github.com>
2023-04-10 00:16:28 +0530

    Update fft.py

--- ivy/functional/frontends/jax/numpy/fft.py
+++ ivy/functional/frontends/jax/numpy/fft.py
@@ -1,8 +1,10 @@
 # local
 import ivy
+from ivy.func_wrapper import with_unsupported_dtypes
 from ivy.functional.frontends.jax.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
-def fft(a, n=None, axis=-1, norm=None):
-    return ivy.fft(a, axis)
+@with_unsupported_dtypes({"0.4.8 and below": ("bool",)}, "jax")
+def fft(a, n=None, dim=-1, norm=None):
+    return ivy.fft(a, dim, n=n, norm=norm)
