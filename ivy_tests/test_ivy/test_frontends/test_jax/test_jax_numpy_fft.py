e0fdbfef0260678c9c3310f4e65ec3b9b05adf7b
Gurumoorty Suresh <54660832+Guru-mp3@users.noreply.github.com>
2023-04-10 00:17:43 +0530

    Update test_jax_numpy_fft.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_fft.py
@@ -1,3 +1,6 @@
+# global
+from hypothesis import strategies as st
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
@@ -6,21 +9,22 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 # fft
 @handle_frontend_test(
     fn_tree="jax.numpy.fft.fft",
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float"),
-                                         array_api_dtypes=True,
-                                         shape=helpers.ints(min_value=2).
-                                         map(lambda x: tuple([x, ])))
-    .filter(
-        lambda x: "bfloat16" not in x[0]
-        and "float16" not in x[0]
-    )
+    dtype_and_x = helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid"),
+                                           shape=helpers.ints(min_value=0).
+                                           map(lambda x: tuple([x, ]))),
+    dim = helpers.ints(min_value=-1, max_value=0),
+    n = helpers.ints(min_value=2),
+    norm = st.sampled_from(["backward","ortho","forward"])
 )
 def test_jax_numpy_fft(
     dtype_and_x,
     frontend,
     fn_tree,
     test_flags,
-    on_device
+    on_device,
+    dim,
+    n,
+    norm
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
@@ -30,5 +34,8 @@ def test_jax_numpy_fft(
         fn_tree=fn_tree,
         on_device=on_device,
         test_values=True,
-        a=x[0]
+        a=x[0],
+        dim=dim,
+        n=n,
+        norm=norm
     )
