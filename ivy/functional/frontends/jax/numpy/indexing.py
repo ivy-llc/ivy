f19817be855b5c32acfc75720b6e472e95055fcd
felixreynoso <engfelixreynoso@gmail.com>
2023-04-08 17:19:08 -0700

    Added line at the end of file, to pass formatting test.

--- ivy/functional/frontends/jax/numpy/indexing.py
+++ ivy/functional/frontends/jax/numpy/indexing.py
@@ -61,3 +61,4 @@ def apply_along_axis(func1d, axis, arr, *args, **kwargs):
         for kk in ivy.ndindex(Nk):
             out[ii + out[...,] + kk] = func1d(arr[ii + out[:, ] + kk])
     return out
+
