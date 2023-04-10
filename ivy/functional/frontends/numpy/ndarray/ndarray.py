520c235d46c05335b94fb2ac87b853237c522972
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-04-10 18:48:30 +0530

    temporary remove choose as working for its composition (#13942)
    This function isn't implemented and it is complex looking into it, removed it temporarily while implementing its composition

--- ivy/functional/frontends/numpy/ndarray/ndarray.py
+++ ivy/functional/frontends/numpy/ndarray/ndarray.py
@@ -317,9 +317,6 @@ class ndarray:
     def view(self):
         return np_frontend.reshape(self._ivy_array, tuple(self.shape))
 
-    def choose(self, choices, /, *, out=None, mode="raise"):
-        return np_frontend.choose(self._ivy_array, choices, out=out, mode=mode)
-
     def __add__(self, value, /):
         return np_frontend.add(self._ivy_array, value)
 
