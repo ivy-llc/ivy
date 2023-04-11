528a4b76c7a6b2657cede670b965377d724dc6bb
RickSanchezStoic <57310695+RickSanchezStoic@users.noreply.github.com>
2023-04-11 11:14:57 +0530

    Multicuda (#13963)
    * docker multicuda
    
    * multicuda
    
    * minor fix
    
    ---------

--- docker/build_DockerfileGPUMultiCuda.sh
+++ docker/build_DockerfileGPUMultiCuda.sh
@@ -1,1 +1,1 @@
-docker build --progress=plain --no-cache -t unifyai/multicuda:base  -f DockerfileGPUMultiCuda-base ..
+docker build --progress=plain --no-cache -t unifyai/multicuda:base_and_requirements  -f DockerfileGPUMultiCuda ..
