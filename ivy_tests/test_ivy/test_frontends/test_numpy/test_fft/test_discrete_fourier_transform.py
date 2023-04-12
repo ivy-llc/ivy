eb8dc8d61bfbb88ce63507f504bf80b25d4eb983
LDTR <108385721+ldtr-dev@users.noreply.github.com>
2023-04-12 09:17:39 -0500

    Added ifftshift function and its respective test function

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -1,6 +1,6 @@
+import pytest
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-
 
 @handle_frontend_test(
     fn_tree="numpy.fft.ifft",
@@ -21,4 +21,24 @@ def test_numpy_iftt(dtype_and_x, frontend, test_flags, fn_tree, on_device):
         n=None,
         axis=-1,
         norm=None,
+    )
+
+
+@pytest.mark.parametrize("array_shape", [(4,), (4, 4), (4, 4, 4)])
+@pytest.mark.parametrize("dtype_category", ["float", "complex"])
+@helpers.handle_frontend_test(fn_tree="numpy.fft.ifftshift")
+def test_numpy_ifftshift(dtype_category, array_shape, frontend, test_flags, fn_tree, on_device):
+    dtype_and_x = helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes(dtype_category), shape=array_shape, array_api_dtypes=True
+    )
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        a=x,
+        axes=None,
     )
