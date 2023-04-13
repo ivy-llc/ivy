825ab6bbccbb1a96bc57ef817bd0f321fa12e3ef
ElsharkawyZz <sharkawynoo271@gmail.com>
2023-04-13 16:20:09 +0200

    Add bincount numpy frontend

---
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -0,0 +1,22 @@
+import ivy
+from ivy.functional.frontends.numpy import handle_numpy_out, to_ivy_arrays_and_back, from_zero_dim_arrays_to_scalar,handle_numpy_dtype,handle_numpy_casting
+import ivy.functional.frontends.numpy as np_frontend
+
+@handle_numpy_out
+@to_ivy_arrays_and_back
+@from_zero_dim_arrays_to_scalar
+@handle_numpy_dtype
+@handle_numpy_casting
+
+def numpy_bincount(x, /, weights=None, minlength=None):
+    x_list = []
+    for i in range(x.shape[0]):
+        x_list.append(int(x[i]))
+    max_val = int(ivy.max(ivy.array(x_list)))
+    ret = [x_list.count(i) for i in range(0, max_val + 1)]
+    ret = ivy.array(ret)
+    ret = ivy.astype(ret, ivy.as_ivy_dtype(ivy.int64))
+    return ret
+
+
+
