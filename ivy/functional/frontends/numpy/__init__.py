721e2678fa733c4e2f7ea44099b97d4f70d16af8
RakshitKumar-04 <kumarrakshit0402@gmail.com>
2023-04-14 07:05:38 +0530

    #12400 Added conj to ivy frontend (#13248)

--- ivy/functional/frontends/numpy/__init__.py
+++ ivy/functional/frontends/numpy/__init__.py
@@ -554,6 +554,7 @@ from ivy.functional.frontends.numpy.mathematical_functions.trigonometric_functio
 from ivy.functional.frontends.numpy.mathematical_functions.handling_complex_numbers import (
     _imag,
     _real,
+    _conj,
 )
 
 from ivy.functional.frontends.numpy.mathematical_functions.hyperbolic_functions import (
@@ -692,3 +693,4 @@ fmin = ufunc("_fmin")
 ldexp = ufunc("_ldexp")
 floor = ufunc("_floor")
 frexp = ufunc("_frexp")
+conj = ufunc("_conj")
