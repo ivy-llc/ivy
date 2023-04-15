43a01d34891735ab575b499b7a01d397af239bc2
Tejas Jogi <tdjogi010@gmail.com>
2023-04-15 13:52:19 +0530

    Added __complex__ instance method and fixed other instance methods and tests

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ndarray/test_ndarray.py
@@ -2094,7 +2094,7 @@ def test_numpy_instance_lt__(
     init_tree="numpy.array",
     method_name="__int__",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("integer"),
         min_dim_size=1,
         max_dim_size=1,
     ),
@@ -2129,12 +2129,47 @@ def test_numpy_instance_int__(
     init_tree="numpy.array",
     method_name="__float__",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("float"),
         min_dim_size=1,
         max_dim_size=1,
     ),
 )
 def test_numpy_instance_float__(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtypes, xs = dtype_and_x
+
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtypes,
+        method_input_dtypes=input_dtypes,
+        init_all_as_kwargs_np={
+            "object": xs[0],
+        },
+        method_all_as_kwargs_np={},
+        frontend=frontend,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        on_device=on_device,
+    )
+
+
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="numpy.array",
+    method_name="__complex__",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_dim_size=1,
+        max_dim_size=1,
+    ),
+)
+def test_numpy_instance_complex__(
     dtype_and_x,
     frontend_method_data,
     init_flags,
