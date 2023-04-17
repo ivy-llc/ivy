89d3c03b32de05f3e6b60b4ba0101318592d478e
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-04-17 16:06:12 -0400

    Update test_norms.py

--- ivy_tests/test_ivy/test_functional/test_nn/test_norms.py
+++ ivy_tests/test_ivy/test_functional/test_nn/test_norms.py
@@ -87,9 +87,9 @@ def _generate_data_layer_norm(
 @handle_test(
     fn_tree="functional.ivy.layer_norm",
     values_tuple=_generate_data_layer_norm(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("float"),
     ),
-    new_std=st.floats(min_value=0.01, max_value=0.1),
+    new_std=st.floats(min_value=0.01),
     eps=st.floats(min_value=0.01, max_value=0.1),
 )
 def test_layer_norm(
