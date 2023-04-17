65973f833edcda28fbea0dd40f295a3ba1148fa7
Amitesh Vatsa <amitesh.vatsa.cd.mec20@itbhu.ac.in>
2023-04-17 13:47:31 +0530

    Adding arcsinh (#14206)
    Co-authored-by: @AnnaTz

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -1549,6 +1549,41 @@ def test_torch_special_add(
     )
 
 
+# arcsinh
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="arcsinh",
+    dtype_and_x=helpers.dtype_and_values(
+        min_value=-1.0,
+        max_value=1.0,
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_torch_instance_arcsinh(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=[],
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
 # __long__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
