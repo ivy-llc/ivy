d91a5a18f0aa88b59bf7ebc3a8540c6d3588c125
LDTR <108385721+ldtr-dev@users.noreply.github.com>
2023-04-17 23:00:21 -0500

    Updated ifftshift() function

--- ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
+++ ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
@@ -1,4 +1,6 @@
 import ivy
+import ivy.functional.frontends.numpy as np_frontend
+import numpy as np
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 
 
@@ -30,23 +32,30 @@ def ifftshift(x, axes=None):
     fftshift : Shift zero-frequency component to the center of the spectrum.
     Examples
     --------
-    >>> freqs = ivy.functional.frontends.numpy.fft.fftfreq(9, d=1./9).reshape(3, 3)
-    >>> freqs
-    array([[ 0.,  1.,  2.],
-           [ 3.,  4., -4.],
-           [-3., -2., -1.]])
-    >>> ivy.functional.frontends.numpy.fft.ifftshift(np.fft.fftshift(freqs))
-    array([[ 0.,  1.,  2.],
-           [ 3.,  4., -4.],
-           [-3., -2., -1.]])
+    >>> import ivy.functional.frontends.numpy as np_frontend
+    >>> arr = np_frontend.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    >>> arr
+    array([[ 1,  2,  3],
+           [ 4,  5,  6],
+           [ 7,  8,  9]])
+    >>> np_frontend.ifftshift(arr, axes=(0, 1))
+    array([[ 5,  6,  4],
+           [ 9,  7,  8],
+           [ 3,  1,  2]])
     """
-    x = ivy.asarray(x)
+    x = np_frontend.asarray(x)
     if axes is None:
         axes = tuple(range(x.ndim))
         shift = [-(dim // 2) for dim in x.shape]
-    elif isinstance(axes, (int, ivy.uint8, ivy.uint16, ivy.uint32, ivy.uint64)):
+    elif isinstance(axes, (int,
+                           type(ivy.uint8),
+                           type(ivy.uint16),
+                           type(ivy.uint32),
+                           type(ivy.uint64))):
         shift = -(x.shape[axes] // 2)
     else:
         shift = [-(x.shape[ax] // 2) for ax in axes]
 
-    return ivy.roll(x, shift, axes)
+    return np.roll(x, shift, axes)
+    # ^ Change to ivy `numpy.core.roll()` equivalent when available
+    
