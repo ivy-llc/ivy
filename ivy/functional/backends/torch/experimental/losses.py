80252274ba6475c52ad84d27731252b7e9637b32
Cemilcelik11 <cemilcelik1199@gmail.com>
2023-04-18 15:35:24 +0300

    Update losses.py

--- ivy/functional/backends/torch/experimental/losses.py
+++ ivy/functional/backends/torch/experimental/losses.py
@@ -11,6 +11,8 @@ from .. import backend_version
 
 from ivy.functional.ivy.experimental.linear_algebra import _check_valid_dimension_size
 
+import numpy as np
+
 @with_unsupported_dtypes({"1.13.0 and below": ("float16",)}, backend_version)
 def ctc_loss(
     log_probs: torch.Tensor,
@@ -28,10 +30,10 @@ def ctc_loss(
     #Compute the CTC loss using the Pytorch implementation
     ctc_loss = F.ctc_loss(
         log_probs=log_probs,
-        targets=targets,
-        input_lengths=input_lengths,
-        target_lengths=target_lengths,
-        blank=blank,
+        targets=targets.to(torch.int32),
+        input_lengths=input_lengths.to(torch.int64),
+        target_lengths=target_lengths.to(torch.int64),
+        blank=np.int32(blank),
         reduction=reduction,
         zero_infinity=zero_infinity,
 
