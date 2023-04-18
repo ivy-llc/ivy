dd920f2c83e1e59633ed521921175591b3b01f88
Vik <victorch9@gmail.com>
2023-04-18 14:18:38 +0000

    removed duplicate test for row_stack

--- ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_indexing_slicing_joining_mutating_ops.py
@@ -1128,33 +1128,6 @@ def test_torch_vsplit(
     )
 
 
-# row_stack
-@handle_frontend_test(
-    fn_tree="torch.row_stack",
-    dtype_value_shape=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        num_arrays=st.integers(1, 5),
-    ),
-)
-def test_torch_row_stack(
-    *,
-    dtype_value_shape,
-    on_device,
-    fn_tree,
-    frontend,
-    test_flags,
-):
-    input_dtype, value = dtype_value_shape
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        tensors=value,
-    )
-
-
 @handle_frontend_test(
     fn_tree="torch.where",
     broadcastables=_broadcastable_trio(),
