7bfb538572d2549da34edeb9a9b32a5ab9ed329d
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2023-04-19 12:24:17 +0100

    remove an unnecessary line, also to trigger CI test

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -16,7 +16,6 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
     fn_tree="tensorflow.math.imag",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float_and_complex"),
-        num_arrays=1,
         min_value=-20,
         max_value=20,
     ),
