081120d84c3dfa4847e765a75142dc748caa1ea2
ElsharkawyZz <sharkawynoo271@gmail.com>
2023-04-19 02:41:23 +0200

    update test funcation frontend

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
@@ -1,40 +1,32 @@
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.test_functional.test_core.test_statistical import (
-    statistical_dtype_values,
-)
-
-
+import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-
-
-#bincount
 
 @handle_frontend_test(
     fn_tree="numpy.bincount",
-
-    dtype_x_axis = statistical_dtype_values(function="bincount"),
-    dtype = helpers.get_dtypes("integer", full=False, none=True),
-
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+    ),
+    shape=helpers.get_shape(),
 )
 def test_numpy_bincount(
     *,
     dtype_and_x,
-    minlength,
-    weights,
     on_device,
     fn_tree,
     frontend,
     test_flags,
 ):
-    input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
+    input_dtypes, x = dtype_and_x
+    np_frontend_helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         x=x[0],
-        weights=weights,
-        minlength=minlength,
+        # weights=None,
+        # minlength=0,
+
 
     )
