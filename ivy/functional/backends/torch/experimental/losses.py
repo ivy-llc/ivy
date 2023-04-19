44be929a58890f77bc652ddf8ed6f1c73e6d8564
Cemilcelik11 <cemilcelik1199@gmail.com>
2023-04-19 13:43:45 +0300

    Update losses.py

--- ivy/functional/backends/torch/experimental/losses.py
+++ ivy/functional/backends/torch/experimental/losses.py
@@ -8,10 +8,8 @@ from typing import Optional, Tuple, Sequence, Union
 import ivy
 from ivy.func_wrapper import with_unsupported_dtypes
 from .. import backend_version
-
-from ivy.functional.ivy.experimental.linear_algebra import _check_valid_dimension_size
-
 import numpy as np
+from ivy.functional.ivy.experimental.linear_algebra import _check_valid_dimension_size
 
 @with_unsupported_dtypes({"1.13.0 and below": ("float16",)}, backend_version)
 def ctc_loss(
@@ -20,8 +18,8 @@ def ctc_loss(
     input_lengths: torch.Tensor,
     target_lengths: torch.Tensor,
     blank: int = 0,
-    reduction: str = "mean",
     zero_infinity: bool = True,
+    reduction: str = "mean",
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
 
@@ -36,9 +34,10 @@ def ctc_loss(
         blank=np.int32(blank),
         reduction=reduction,
         zero_infinity=zero_infinity,
-
-    )
+)
 
     return ctc_loss
+
+
 
 
