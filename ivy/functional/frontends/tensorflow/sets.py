2ea0bd21df4f47e854a858ac04d7d2855f08091f
rubada <ruba@al-dabbas.com>
2023-04-21 01:05:10 +0300

    Add Set functions for TensorFlow Frontend  sets.py

--- ivy/functional/frontends/tensorflow/sets.py
+++ ivy/functional/frontends/tensorflow/sets.py
@@ -6,16 +6,16 @@ from ivy.functional.frontends.tensorflow.func_wrapper import (to_ivy_arrays_and_
 @to_ivy_arrays_and_back
 def difference(a, b, aminusb=True, validate_indices=True):
     a, b = check_tensorflow_casting(a, b)
-    return ivy.difference(a, b, aminusb=True, validate_indices=True)
+    return ivy.difference(a, b, aminusb=aminusb, validate_indices=validate_indices)
 
 
 @to_ivy_arrays_and_back
 def intersection(a, b, validate_indices=True):
     a, b = check_tensorflow_casting(a, b)
-    return ivy.intersection(a, b, validate_indices=True)
+    return ivy.intersection(a, b, validate_indices=validate_indices)
 
 
 @to_ivy_arrays_and_back
 def union(a, b, validate_indices=True):
     a, b = check_tensorflow_casting(a, b)
-    return ivy.union(a, b, validate_indices=True)
+    return ivy.union(a, b, validate_indices=validate_indices)
