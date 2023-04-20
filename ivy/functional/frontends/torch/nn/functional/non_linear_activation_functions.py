93d20ae3ece41c829b5e3de90b1bd76a7de57a10
Szeryf19 <85789182+Szeryf19@users.noreply.github.com>
2023-04-20 11:10:08 +0100

    Add files via upload

--- ivy/functional/frontends/torch/nn/functional/non_linear_activation_functions.py
+++ ivy/functional/frontends/torch/nn/functional/non_linear_activation_functions.py
@@ -23,6 +23,22 @@ def _compute_elu(input, alpha=1.0, inplace=False):
         return input
     return ret
 
+def gumbel_softmax(logits: Tensor, tau: float = 1, hard: bool = False, eps: float = 1e-10, dim: int = -1):
+    if has_torch_function_unary(logits):
+        return handle_torch_function(gumbel_softmax, (logits,), logits, tau=tau, hard=hard, eps=eps, dim=dim)
+    if eps != 1e-10:
+        warnings.warn("`eps` parameter is deprecated and has no effect.")
+    gumbels = (-torch.empty_like(logits, memory_format=torch.legacy_contiguous_format).exponential_().log())  # ~Gumbel(0,1)
+    gumbels = (logits + gumbels) / tau  # ~Gumbel(logits,tau)
+    y_soft = gumbels.softmax(dim)
+    if hard:
+        index = y_soft.max(dim, keepdim=True)[1]
+        y_hard = torch.zeros_like(logits, memory_format=torch.legacy_contiguous_format).scatter_(dim, index, 1.0)
+        ret = y_hard - y_soft.detach() + y_soft
+    else:
+        ret = y_soft
+    return ret
+
 
 def _selu_with_inplace(input, inplace=False):
     alpha = 1.6732632423543772848170429916717
