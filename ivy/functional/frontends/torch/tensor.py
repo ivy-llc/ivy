84fc26907f210a8ccaf63d512c8d94299877fabf
hslau44 <hslau44@gmail.com>
2023-04-20 21:09:08 +0100

    add frontend torch instance ceil

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -938,3 +938,8 @@ class Tensor:
 
     def mul(self, other):
         return torch_frontend.mul(self, other)
+    
+    @with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
+    def ceil_(self):
+        self.ivy_array = torch_frontend.ceil(self).ivy_array
+        return self
