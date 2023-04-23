848f78f508b7552daedea8ff04448e0709a5f891
MuhammedAshraf2020 <muhammedashraf2661@gmail.com>
2023-04-23 08:21:34 +0200

    add jax.random.rademacher frontend

--- ivy/functional/frontends/jax/random.py
+++ ivy/functional/frontends/jax/random.py
@@ -73,3 +73,16 @@ def dirichlet(key, alpha, shape=None, dtype="float32"):
 def poisson(key, lam, shape=None, dtype=None):
     seed = _get_seed(key)
     return ivy.poisson(lam, shape=shape, dtype=dtype, seed=seed)
+
+
+@handle_jax_dtype
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes(
+    {"0.3.14 and below": ("unsigned", "int8", "int16")},
+    "jax",
+)
+def rademacher(key, shape, dtype="int64"):
+    seed = _get_seed(key)
+    b = ivy.bernoulli(ivy.array([0.5]), shape=shape, dtype="float32", seed=seed)
+    b = ivy.astype(b, dtype)
+    return 2 * b - 1
