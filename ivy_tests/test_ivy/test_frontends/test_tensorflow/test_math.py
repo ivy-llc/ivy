bfd84bf6a255035facd8017b1824c352527be593
Paula Ehab Alfy <42894502+paulaehab@users.noreply.github.com>
2023-04-24 05:00:50 +0200

    fix Acosh tensorflow tests (#14512)
    * fix Acosh tensorflow tests
    
    * add more paramters to test

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1863,7 +1863,9 @@ def test_tensorflow_acos(
 @handle_frontend_test(
     fn_tree="tensorflow.math.acosh",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
+        available_dtypes=helpers.get_dtypes("float_and_complex"),
+        small_abs_safety_factor=3,
+        safety_factor_scale="log",
     ),
     test_with_out=st.just(False),
 )
