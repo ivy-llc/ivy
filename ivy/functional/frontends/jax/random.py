11ac2b36ac156baf96bdee926a5d59b8194c4463
MuhammedAsharaf <53389411+MuhammedAshraf2020@users.noreply.github.com>
2023-04-24 22:09:26 +0200

    add jax.random.generalized_normal frontend (#14502)
    Co-authored-by: Kareem Morsy <kreem.morsy@hotmail.com>

--- ivy/functional/frontends/jax/random.py
+++ ivy/functional/frontends/jax/random.py
@@ -125,6 +125,14 @@ def gumbel(key, shape=(), dtype="float64"):
     },
     "jax",
 )
+def generalized_normal(key, p, shape=(), dtype="float64"):
+    seed = _get_seed(key)
+    g = ivy.gamma(1 / p, 1.0, shape=shape, dtype=dtype, seed=seed)
+    b = ivy.bernoulli(ivy.array([0.5]), shape=shape, dtype=dtype, seed=seed)
+    r = 2 * b - 1
+    return r * g ** (1 / p)
+
+
 def t(key, df, shape=(), dtype="float64"):
     seed = _get_seed(key)
     n = ivy.random_normal(shape=shape, dtype=dtype, seed=seed)
