dc5f5a6052aac54d2fa138068000e028511bfb3f
MuhammedAshraf2020 <muhammedashraf2661@gmail.com>
2023-04-24 13:36:59 +0200

    add BatchNorm2D Layer

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_random.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_random.py
@@ -267,12 +267,8 @@ def test_jax_dirichlet(
     lam=st.floats(min_value=0, max_value=5, exclude_min=True),
     shape=helpers.get_shape(
         min_num_dims=2, max_num_dims=2, min_dim_size=1, max_dim_size=5
-<<<<<<< Updated upstream
     ),
     dtype=helpers.get_dtypes("integer", full=False),
-=======
-    ),    dtype=helpers.get_dtypes("integer", full=False),
->>>>>>> Stashed changes
     test_with_out=st.just(False),
 )
 def test_jax_poisson(
@@ -623,8 +619,6 @@ def test_jax_rademacher(
     for (u, v) in zip(ret_np, ret_from_np):
         assert u.dtype == v.dtype
         assert u.shape == v.shape
-<<<<<<< Updated upstream
-=======
 
 
 @st.composite
@@ -706,55 +700,3 @@ def test_jax_gamma(
         assert u.dtype == v.dtype
         assert u.shape == v.shape
 
-
-@handle_frontend_test(
-    fn_tree="jax.random.gumbel",
-    dtype_key=helpers.dtype_and_values(
-        available_dtypes=["uint32"],
-        min_value=0,
-        max_value=2000,
-        min_num_dims=1,
-        max_num_dims=1,
-        min_dim_size=2,
-        max_dim_size=2,
-    ),
-    shape=helpers.get_shape(),
-    dtype=helpers.get_dtypes("float", full=False),
-)
-def test_jax_gumbel(
-    *,
-    dtype_key,
-    shape,
-    dtype,
-    on_device,
-    fn_tree,
-    frontend,
-    test_flags,
-):
-    input_dtype, key = dtype_key
-
-    def call():
-        return helpers.test_frontend_function(
-            input_dtypes=input_dtype,
-            frontend=frontend,
-            test_flags=test_flags,
-            fn_tree=fn_tree,
-            on_device=on_device,
-            test_values=False,
-            key=key[0],
-            shape=shape,
-            dtype=dtype[0],
-        )
-
-    ret = call()
-
-    if not ivy.exists(ret):
-        return
-
-    ret_np, ret_from_np = ret
-    ret_np = helpers.flatten_and_to_np(ret=ret_np)
-    ret_from_np = helpers.flatten_and_to_np(ret=ret_from_np)
-    for (u, v) in zip(ret_np, ret_from_np):
-        assert u.dtype == v.dtype
-        assert u.shape == v.shape
->>>>>>> Stashed changes
