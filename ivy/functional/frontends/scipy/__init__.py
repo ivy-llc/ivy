31130f4c1e7d817a17725b0ab2e68497dde9cf4d
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2023-04-25 11:40:16 +0000

    Initial commit to add directory skeleton and func wrapper for SciPy frontend

---
+++ ivy/functional/frontends/scipy/__init__.py
@@ -0,0 +1,21 @@
+from . import cluster
+from . import constants
+from . import datasets
+from . import fft
+from . import fftpack
+from . import integrate
+from . import interpolate
+from . import linalg
+from . import ndimage
+from . import odr
+from . import optimize
+from . import signal
+from . import sparse
+from . import sparse
+from . import spatial
+from . import special
+from . import stats
+
+import ivy.functional.frontends.numpy as np
+
+_frontend_array = np.array
---
+++ ivy/functional/frontends/scipy/linalg/__init__.py
@@ -0,0 +1,4 @@
+from .linalg import *
+from . import blas
+from . import lapack
+from . import interpolative
