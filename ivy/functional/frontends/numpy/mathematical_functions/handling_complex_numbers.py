0fb3ddcf9763c5b4546d132a920811600ab38251
RakshitKumar04 <kumarrakshit0402@gmail.com>
2023-04-25 22:51:49 +0530

    Added conjugate function to Numpy Frontend

--- ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
+++ ivy/functional/frontends/numpy/mathematical_functions/handling_complex_numbers.py
@@ -47,3 +47,25 @@ def _conj(
     if ivy.is_array(where):
         ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
     return ret
+
+
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
+def _conjugate(
+    x,
+    /,
+    out=None,
+    *,
+    where=True,
+    casting="same_kind",
+    order="K",
+    dtype=None,
+    subok=True,
+):
+    ret = ivy.conj(x, out=out)
+    if ivy.is_array(where):
+        ret = ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
+    return ret
