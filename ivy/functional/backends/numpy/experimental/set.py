e8ef3a74ba602b49f037e60cf8635cabe8ecf39a
Anurag verma <78868769+anurag629@users.noreply.github.com>
2023-04-26 17:30:05 +0000

    corrected the out argument in numpy

--- ivy/functional/backends/numpy/experimental/set.py
+++ ivy/functional/backends/numpy/experimental/set.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Optional
 
+
 def difference(
     x1: np.ndarray,
     x2: np.ndarray,
@@ -11,4 +12,4 @@ def difference(
     return np.setdiff1d(x1, x2)
 
 
-difference.support_native_out = False
+difference.support_native_out = True
