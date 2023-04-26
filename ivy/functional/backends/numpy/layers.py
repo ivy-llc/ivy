22087c4510589c2b2a0a4fddf24434c9560e92c7
zhumakhan <nazirzhumakhan@gmail.com>
2023-04-26 14:21:25 +0000

    fixed numpy conv2d

--- ivy/functional/backends/numpy/layers.py
+++ ivy/functional/backends/numpy/layers.py
@@ -40,14 +40,12 @@ def _dilate_pad_conv(x, filters, strides, padding, dims, dilations):
         pad_list = [(padding, padding)] * dims
     else:
         pad_list = padding
+
+    pad_width = [(0, 0), *[(_p, _p) for _p in pad_list], (0, 0)]
     x = np.pad(
         x,
-        [
-            (0, 0),
-            *pad_list,
-            (0, 0),
-        ],
-        "constant",
+        pad_width=pad_width,
+        mode="constant",
     )
     return x, filters
 
