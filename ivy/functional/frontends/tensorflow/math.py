068dfeb915d77dcdf5650a22e3c8879dd5b899f2
__Ayad__ <3bdullah3yad@gmail.com>
2023-04-26 23:35:53 +0200

    in_top_k added

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -658,6 +658,15 @@ def unsorted_segment_sum(
         x[segment_ids[i]] = x[segment_ids[i]] + data[i]
     return x
 
+
 def greater_equal(x, y, name=None):
     x, y = check_tensorflow_casting(x, y)
     return ivy.greater_equal(x, y)
+
+@to_ivy_arrays_and_back
+def in_top_k(targets, pred, k):
+    top_k = ivy.top_k(targets, k)
+    Ayad = []
+    for val in targets:
+        Ayad.extend([val in top_k.values])
+    return ivy.array(Ayad)
