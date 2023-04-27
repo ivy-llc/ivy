c441597ef8e88895bc31818666a95c77bfa948be
Jayanth941 <200260016@iitb.ac.in>
2023-04-27 19:57:37 +0530

    added new function

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_averages_and_variances.py
@@ -384,3 +384,45 @@ def test_numpy_nanmedian(
         out=None,
         keepdims=keep_dims,
     )
+
+
+@handle_frontend_test(
+    fn_tree="numpy.nanquantile",
+    dtype_values_axis=statistical_dtype_values(function="nanquantile"),
+    where=np_frontend_helpers.where(),
+    keep_dims=st.booleans(),
+)
+def test_numpy_nanquantile(
+    dtype_values_axis,
+    where,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    keep_dims,
+):
+    input_dtypes, values, axis = dtype_values_axis
+    if isinstance(axis, tuple):
+        axis = axis[0]
+
+    where, input_dtypes, test_flags = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtypes,
+        test_flags=test_flags,
+    )
+
+    np_frontend_helpers.test_frontend_function(
+        a=values[0][0],
+        q=values[0][1],
+        axis=axis,
+        out=None,
+        overwrite_input=None,
+        method=None,
+        keepdims=keep_dims,
+        interpolation=None,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        test_flags=test_flags,
+        input_dtypes=input_dtypes,
+    )
+
