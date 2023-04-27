0e2ca9e120d6d5c63a55fe84df6542c62b49e388
Omar Ahmed <60344203+omarahmed19992@users.noreply.github.com>
2023-04-27 05:28:40 +0200

    add idct function in signal file

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -1,3 +1,6 @@
+from typing import Union, Optional, Literal
+import tensorflow as tf
+
 import ivy
 from ivy.functional.frontends.tensorflow.func_wrapper import (
     to_ivy_arrays_and_back,
@@ -12,3 +15,25 @@ def kaiser_window(window_length, beta=12.0, dtype=ivy.float32, name=None):
 
 
 kaiser_window.supported_dtypes = ("float32", "float64", "float16", "bfloat16")
+
+def idct(
+    x: Union[tf.Tensor, tf.Variable],
+    /,
+    *,
+    type: Literal[1, 2, 3, 4] = 2,
+    n: Optional[int] = None,
+    axis: int = -1,
+    norm: Optional[Literal["ortho"]] = None,
+    out: Optional[Union[tf.Tensor, tf.Variable]] = None,
+) -> tf.Tensor:
+    if x.dtype not in (tf.float32, tf.float64):
+        x = tf.cast(x, tf.float32)
+    if axis != -1:
+        new_dims = list(range(len(x.shape)))
+        new_dims[axis], new_dims[-1] = new_dims[-1], axis
+        x = tf.transpose(x, new_dims)
+        idct_out = tf.signal.idct(x, type=type, n=n, axis=-1, norm=norm)
+        idct_out = tf.transpose(idct_out, new_dims)
+    else:
+        idct_out = tf.signal.idct(x, type=type, n=n, axis=-1, norm=norm)
+    return idct_out
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -40,3 +40,53 @@ def test_tensorflow_kaiser_window(
         beta=beta,
         dtype=dtype,
     )
+@st.composite
+def valid_idct(draw):
+    dtype, x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=helpers.get_dtypes("numeric"),
+            max_value=65280,
+            min_value=-65280,
+            min_num_dims=1,
+            max_num_dims=5,
+            min_dim_size=2,
+            max_dim_size=10,
+            shared_dtype=True,
+        )
+    )
+    dims_len = len(x[0].shape)
+    n = draw(st.sampled_from([None, "int"]))
+    axis = draw(helpers.ints(min_value=-dims_len, max_value=dims_len))
+    norm = draw(st.sampled_from([None, "ortho"]))
+    type = draw(helpers.ints(min_value=1, max_value=4))
+    if n == "int":
+        n = draw(helpers.ints(min_value=1, max_value=20))
+        if n <= 1 and type == 1:
+            n = 2
+
+    return dtype, x, type, n, axis, norm
+@handle_frontend_test(
+    fn_tree="tensorflow.signal.idct",
+    dtype_x_and_args=valid_idct(),
+    test_with_out=st.just(False),
+)
+def test_idct(
+    dtype_x_and_args,
+    test_flags,
+    frontend,
+    on_device,
+    fn_tree,
+):
+    input_dtype, x, type, n, axis, norm = dtype_x_and_args
+    helpers.test_frontend_function(
+        on_device=on_device,
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        x=x[0],
+        type=type,
+        n=n,
+        axis=axis,
+        norm=norm,
+    )
