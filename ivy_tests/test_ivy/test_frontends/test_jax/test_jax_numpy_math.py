46ff4069332a2ccee2266b72983d684bcf175189
Aditya Gandhamal <61016383+adityagandhamal@users.noreply.github.com>
2023-04-30 21:28:32 +0530

    Add signbit to Jax frontend (#14560)
    Co-authored-by: Vansh Gupta <vanshg2001@protonmail.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -2745,3 +2745,29 @@ def test_jax_numpy_polymul(
         atol=1e-05,
         rtol=1e-03,
     )
+
+
+# signbit
+@handle_frontend_test(
+    fn_tree="jax.numpy.signbit",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_jax_numpy_signbit(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
