cc67ad564f4a0a04c33ffd72def6049ed01674ad
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-05-01 04:44:12 +0300

    add composite test helper for polyint

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -2705,27 +2705,39 @@ def test_jax_numpy_polyder(
 
 
 # polyint
-@handle_frontend_test(
-    fn_tree="jax.numpy.polyint",
-    dtype_and_x=helpers.dtype_and_values(
+@st.composite
+def _get_array_values_m_and_k(draw):
+    dtype_and_x= draw(helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
         num_arrays=1,
         min_num_dims=1,
         max_num_dims=1,
         min_dim_size=1,
-    ),
-    m=st.integers(min_value=0, max_value=10),
+        )
+    )
+    dtype, x = dtype_and_x
+    m = draw(st.integers(min_value=0, max_value=10))
+    max_bound = m-1
+    if max_bound <= m:
+        k = None
+    else:
+        k = draw(st.integers(min_value=0, max_value=max_bound))
+    return dtype, x, m, k
+
+
+@handle_frontend_test(
+    fn_tree="jax.numpy.polyint",
+    dtype_and_x_and_k=_get_array_values_m_and_k(),
 )
 def test_jax_numpy_polyint(
     *,
-    dtype_and_x,
-    m,
+    dtype_and_x_and_k,
     test_flags,
     on_device,
     fn_tree,
     frontend,
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x, m, k = dtype_and_x_and_k
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         test_flags=test_flags,
@@ -2734,6 +2746,7 @@ def test_jax_numpy_polyint(
         on_device=on_device,
         p=x[0],
         m=m,
+        k=k,
     )
 
 
