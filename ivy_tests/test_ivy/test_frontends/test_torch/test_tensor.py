4dac9261d235bb1ffe57bf4c31db1131bb79a81a
profgandhi <72226961+profgandhi@users.noreply.github.com>
2023-05-01 19:44:55 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -7321,3 +7321,33 @@ def test_torch_instance_log10(
         frontend=frontend,
         on_device=on_device,
     )
+    
+# logdet
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="logdet",
+    dtype_and_x=_get_dtype_and_square_matrix(),
+)
+def test_torch_instance_logdet(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x,
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
