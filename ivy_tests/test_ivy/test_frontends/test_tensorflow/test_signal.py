33806f1f6f046a3bb119aa5ce9025171de9db3f0
Omar Ahmed <60344203+omarahmed19992@users.noreply.github.com>
2023-05-02 23:11:34 +0300

    edit

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -5,93 +5,93 @@ from hypothesis import strategies as st
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
-
-# kaiser_window
-@handle_frontend_test(
-    fn_tree="tensorflow.signal.kaiser_window",
-    dtype_and_window_length=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("integer")
-    ),
-    dtype_and_beta=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
-    ),
-    dtype=helpers.get_dtypes("numeric"),
-    test_with_out=st.just(False),
-)
-def test_tensorflow_kaiser_window(
-    *,
-    dtype_and_window_length,
-    dtype_and_beta,
-    dtype,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device,
-):
-    window_length_dtype, window_length = dtype_and_window_length
-    beta_dtype, beta = dtype_and_beta
-    helpers.test_frontend_function(
-        input_dtypes=[window_length_dtype[0], beta_dtype[0]],
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        window_length=window_length,
-        beta=beta,
-        dtype=dtype,
-    )
-
-
-# @st.composite
-# def valid_idct(draw):
-#     dtype, x = draw(
-#         helpers.dtype_and_values(
-#             available_dtypes=["float32", "float64"],
-#             max_value=65280,
-#             min_value=-65280,
-#             min_num_dims=1,
-#             max_num_dims=5,
-#             min_dim_size=2,
-#             max_dim_size=10,
-#             shared_dtype=True,
-#         )
-#     )
-#     n = draw(st.sampled_from([None]))
-#     axis = draw(st.sampled_from(["int"]))
-#     norm = draw(st.sampled_from([None, "ortho"]))
-#     type = draw(helpers.ints(min_value=1, max_value=4))
-#     if axis == "int":
-#         axis = -1
-#     if norm == "ortho" and type == 1:
-#         norm = None
-#     return dtype, x, type, n, axis, norm
 #
-#
-# # idct
+# # kaiser_window
 # @handle_frontend_test(
-#     fn_tree="tensorflow.signal.idct",
-#     dtype_x_and_args=valid_idct(),
+#     fn_tree="tensorflow.signal.kaiser_window",
+#     dtype_and_window_length=helpers.dtype_and_values(
+#         available_dtypes=helpers.get_dtypes("integer")
+#     ),
+#     dtype_and_beta=helpers.dtype_and_values(
+#         available_dtypes=helpers.get_dtypes("numeric")
+#     ),
+#     dtype=helpers.get_dtypes("numeric"),
 #     test_with_out=st.just(False),
 # )
-# def test_tensorflow_idct(
+# def test_tensorflow_kaiser_window(
 #     *,
-#     dtype_x_and_args,
+#     dtype_and_window_length,
+#     dtype_and_beta,
+#     dtype,
 #     frontend,
 #     test_flags,
 #     fn_tree,
 #     on_device,
 # ):
-#     input_dtype, x, type, n, axis, norm = dtype_x_and_args
+#     window_length_dtype, window_length = dtype_and_window_length
+#     beta_dtype, beta = dtype_and_beta
 #     helpers.test_frontend_function(
-#         input_dtypes=input_dtype,
+#         input_dtypes=[window_length_dtype[0], beta_dtype[0]],
 #         frontend=frontend,
 #         test_flags=test_flags,
 #         fn_tree=fn_tree,
 #         on_device=on_device,
-#         input=x[0],
-#         type=type,
-#         n=n,
-#         axis=axis,
-#         norm=norm,
-#         atol=1e-01,
+#         window_length=window_length,
+#         beta=beta,
+#         dtype=dtype,
 #     )
+
+
+@st.composite
+def valid_idct(draw):
+    dtype, x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=["float32", "float64"],
+            max_value=65280,
+            min_value=-65280,
+            min_num_dims=1,
+            max_num_dims=5,
+            min_dim_size=2,
+            max_dim_size=10,
+            shared_dtype=True,
+        )
+    )
+    n = draw(st.sampled_from([None]))
+    axis = draw(st.sampled_from(["int"]))
+    norm = draw(st.sampled_from([None, "ortho"]))
+    type = draw(helpers.ints(min_value=1, max_value=4))
+    if axis == "int":
+        axis = -1
+    if norm == "ortho" and type == 1:
+        norm = None
+    return dtype, x, type, n, axis, norm
+
+
+# idct
+@handle_frontend_test(
+    fn_tree="tensorflow.signal.idct",
+    dtype_x_and_args=valid_idct(),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_idct(
+    *,
+    dtype_x_and_args,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, type, n, axis, norm = dtype_x_and_args
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        type=type,
+        n=n,
+        axis=axis,
+        norm=norm,
+        atol=1e-01,
+    )
