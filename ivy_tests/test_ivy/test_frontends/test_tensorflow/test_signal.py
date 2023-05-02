fff067a08d1d94179c4a75c1ceee9c77ecf25036
Omar Ahmed <60344203+omarahmed19992@users.noreply.github.com>
2023-05-02 22:44:23 +0300

    edit

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -6,7 +6,7 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
-#kaiser_window
+# kaiser_window
 @handle_frontend_test(
     fn_tree="tensorflow.signal.kaiser_window",
     dtype_and_window_length=helpers.dtype_and_values(
@@ -40,11 +40,13 @@ def test_tensorflow_kaiser_window(
         beta=beta,
         dtype=dtype,
     )
+
+
 @st.composite
 def valid_idct(draw):
     dtype, x = draw(
         helpers.dtype_and_values(
-            available_dtypes=["float32","float64"],
+            available_dtypes=["float32", "float64"],
             max_value=65280,
             min_value=-65280,
             min_num_dims=1,
@@ -58,37 +60,38 @@ def valid_idct(draw):
     axis = draw(st.sampled_from(["int"]))
     norm = draw(st.sampled_from([None, "ortho"]))
     type = draw(helpers.ints(min_value=1, max_value=4))
-    if axis =="int":
+    if axis == "int":
         axis = -1
     if norm == "ortho" and type == 1:
         norm = None
     return dtype, x, type, n, axis, norm
 
-#idct
+
+# idct
 @handle_frontend_test(
     fn_tree="tensorflow.signal.idct",
     dtype_x_and_args=valid_idct(),
     test_with_out=st.just(False),
 )
 def test_tensorflow_idct(
-     *,
-     dtype_x_and_args,
-     frontend,
-     test_flags,
-     fn_tree,
-     on_device,
+    *,
+    dtype_x_and_args,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
 ):
-     input_dtype, x, type, n, axis, norm = dtype_x_and_args
-     helpers.test_frontend_function(
-         input_dtypes=input_dtype,
-         frontend=frontend,
-         test_flags=test_flags,
-         fn_tree=fn_tree,
-         on_device=on_device,
-         input=x[0],
-         type=type,
-         n=n,
-         axis=axis,
-         norm=norm,
-         atol=1e-01,
+    input_dtype, x, type, n, axis, norm = dtype_x_and_args
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        type=type,
+        n=n,
+        axis=axis,
+        norm=norm,
+        atol=1e-01,
     )
