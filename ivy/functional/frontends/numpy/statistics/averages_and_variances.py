1a01f9f2fbd8ce963a0b682126b10484e847cc07
luka <l.lortkifanidze@noxtton.com>
2023-05-03 22:20:35 +0400

    shorter nanpercentile

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -320,6 +320,7 @@ def cpercentile(N, percent, key=lambda x: x):
     return d0 + d1
 
 
+
 def nanpercentile(
     a,
     /,
@@ -335,57 +336,18 @@ def nanpercentile(
     a = ivy.array(a)
     q = ivy.divide(q, 100.0)
     q = ivy.array(q)
-    # print(q)
+
     if not _quantile_is_valid(q):
         raise ValueError("percentile s must be in the range [0, 100]")
-    if axis is None:
-        resultarray = []
-        nanlessarray = []
-        for x in a:
-            for i in x:
-                if not ivy.isnan(i):
-                    nanlessarray.append(i)
+    if axis == 1:
+        nanlessarrayofarrays = [[t for t in i if not ivy.isnan(t)] for i in a]
+        resultarray = [[cpercentile(ii, i) for ii in nanlessarrayofarrays] for i in q]
+    else:
+        a = ivy.swapaxes(a, 0, 1) if axis == 0 else a
+        nanlessarrayofarrays = [[t for t in i if not ivy.isnan(t)] for i in a]
+        resultarray = [[cpercentile(ii, i) for ii in nanlessarrayofarrays] for i in q]
+    return resultarray
 
-        for i in q:
-            resultarray.append(cpercentile(nanlessarray, i))
-        return resultarray
-    elif axis == 1:
-        resultarray = []
-        nanlessarrayofarrays = []
-        for i in a:
-            nanlessarray = []
-            for t in i:
-                if not ivy.isnan(t):
-                    nanlessarray.append(t)
-            nanlessarrayofarrays.append(nanlessarray)
-        for i in q:
-            arrayofpercentiles = []
-            for ii in nanlessarrayofarrays:
-                arrayofpercentiles.append(cpercentile(ii, i))
-            resultarray.append(arrayofpercentiles)
-        return resultarray
-    elif axis == 0:
-        resultarray = []
-
-        try:
-            a = ivy.swapaxes(a, 0, 1)
-        except ivy.utils.exceptions.IvyError:
-            ivy.logging.warning("axis is 0 but couldn't swap")
-
-        finally:
-            nanlessarrayofarrays = []
-            for i in a:
-                nanlessarray = []
-                for t in i:
-                    if not ivy.isnan(t):
-                        nanlessarray.append(t)
-                nanlessarrayofarrays.append(nanlessarray)
-            for i in q:
-                arrayofpercentiles = []
-                for ii in nanlessarrayofarrays:
-                    arrayofpercentiles.append(cpercentile(ii, i))
-                resultarray.append(arrayofpercentiles)
-        return resultarray
 
 
 # nanmedian
