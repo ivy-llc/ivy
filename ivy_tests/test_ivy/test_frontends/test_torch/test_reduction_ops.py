07dd8d5e7b837add4bbf31430014d9a020e4a9de
Haarsh2003 <115811177+Haarsh2003@users.noreply.github.com>
2023-05-03 19:31:19 +0530

    Update test_reduction_ops.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
@@ -838,3 +838,35 @@ def test_torch_unique(
         return_counts=return_counts,
         dim=axis,
     )
+
+    
+ @handle_frontend_test(
+    fn_tree="torch.mode",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+    keepdim=st.booleans(),
+)
+def test_torch_mode(
+    *,
+    dtype_x_axis,
+    keepdim,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x, dim = dtype_x_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        dim=dim,
+        keepdim=keepdim,
+    )
