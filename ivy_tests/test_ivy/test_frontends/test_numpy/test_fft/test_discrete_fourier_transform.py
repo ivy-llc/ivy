c159908765e19c67c75d6a5d6543d94e36ef2ebd
m-afa <129254852+m-afa@users.noreply.github.com>
2023-05-03 19:25:07 +0300

    handle test_numpy_ftt parameters

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -1,5 +1,6 @@
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
+from hypothesis import strategies as st
 
 
 @handle_frontend_test(
@@ -46,12 +47,16 @@ def test_numpy_ifttshift(dtype_and_x, frontend, test_flags, fn_tree, on_device):
 
 @handle_frontend_test(
     fn_tree="numpy.fft.fft",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"), shape=(4,), array_api_dtypes=True
+    dtype_input_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("float_and_complex"), shape=(2,),
+        min_axis=-1,
+        force_int_axis=True,
     ),
+    norm=st.sampled_from(["backward", "ortho", "forward"]),
+    n=st.integers(min_value=2, max_value=10),
 )
-def test_numpy_ftt(dtype_and_x, frontend, test_flags, fn_tree, on_device):
-    input_dtype, x = dtype_and_x
+def test_numpy_ftt(dtype_input_axis, norm, n, frontend, test_flags, fn_tree, on_device):
+    input_dtype, x , axis = dtype_input_axis
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
@@ -59,8 +64,8 @@ def test_numpy_ftt(dtype_and_x, frontend, test_flags, fn_tree, on_device):
         fn_tree=fn_tree,
         on_device=on_device,
         test_values=True,
-        a=x,
-        n=None,
-        axis=-1,
-        norm=None,
+        a=x[0],
+        n=n,
+        axis=axis,
+        norm=norm,
     )
