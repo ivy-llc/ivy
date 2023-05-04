1e089cc35dae56a7bd6d48423621434b2239e811
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2023-05-04 13:19:54 +0000

    Add dct and idct to scipy.fft and their tests

--- ivy_tests/test_ivy/test_frontends/test_scipy/test_fft/test_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_scipy/test_fft/test_fft.py
@@ -55,6 +55,57 @@
 #     return dtype, x, dim, norm, n
 
 
+# @st.composite
+# def valid_dct(draw):
+#     dtype, x = draw(
+#         helpers.dtype_and_values(
+#             available_dtypes=helpers.get_dtypes("numeric"),
+#             max_value=65280,
+#             min_value=-65280,
+#             min_num_dims=1,
+#             max_num_dims=5,
+#             min_dim_size=2,
+#             max_dim_size=10,
+#             shared_dtype=True,
+#         )
+#     )
+#     dims_len = len(x[0].shape)
+#     n = draw(st.sampled_from([None, "int"]))
+#     axis = draw(helpers.ints(min_value=-dims_len, max_value=dims_len))
+#     norm = draw(st.sampled_from([None, "ortho"]))
+#     type = draw(helpers.ints(min_value=1, max_value=4))
+#     if n == "int":
+#         n = draw(helpers.ints(min_value=1, max_value=20))
+#         if n <= 1 and type == 1:
+#             n = 2
+#     if norm == "ortho" and type == 1:
+#         norm = None
+#     return dtype, x, type, n, axis, norm
+
+
+# @st.composite
+# def valid_idct(draw):
+#     dtype, x = draw(
+#         helpers.dtype_and_values(
+#             available_dtypes=["float32", "float64"],
+#             max_value=65280,
+#             min_value=-65280,
+#             min_num_dims=1,
+#             max_num_dims=5,
+#             min_dim_size=2,
+#             max_dim_size=10,
+#             shared_dtype=True,
+#         )
+#     )
+#     n = None
+#     axis = -1
+#     norm = draw(st.sampled_from([None, "ortho"]))
+#     type = draw(helpers.ints(min_value=1, max_value=4))
+#     if norm == "ortho" and type == 1:
+#         norm = None
+#     return dtype, x, type, n, axis, norm
+
+
 # ### Tests ###
 
 # # fft
@@ -108,4 +159,64 @@
 #         dim=dim,
 #         norm=norm,
 #         n=n,
+#     )
+
+
+# # dct
+# @handle_frontend_test(
+#     fn_tree="scipy.fft.dct",
+#     dtype_x_and_args=valid_dct(),
+#     test_with_out=st.just(False),
+# )
+# def test_dct(
+#     dtype_x_and_args,
+#     frontend,
+#     test_flags,
+#     fn_tree,
+#     on_device,
+# ):
+#     input_dtype, x, _type, n, axis, norm = dtype_x_and_args
+#     helpers.test_frontend_function(
+#         input_dtypes=input_dtype,
+#         frontend=frontend,
+#         test_flags=test_flags,
+#         fn_tree=fn_tree,
+#         on_device=on_device,
+#         x=x[0],
+#         type=_type,
+#         n=n,
+#         axis=axis,
+#         norm=norm,
+#         rtol_=1e-3,
+#         atol_=1e-1,
+#     )
+
+
+# # idct
+# @handle_frontend_test(
+#     fn_tree="scipy.fft.idct",
+#     dtype_x_and_args=valid_idct(),
+#     test_with_out=st.just(False),
+# )
+# def test_idct(
+#     dtype_x_and_args,
+#     frontend,
+#     test_flags,
+#     fn_tree,
+#     on_device,
+# ):
+#     input_dtype, x, _type, n, axis, norm = dtype_x_and_args
+#     helpers.test_frontend_function(
+#         input_dtypes=input_dtype,
+#         frontend=frontend,
+#         test_flags=test_flags,
+#         fn_tree=fn_tree,
+#         on_device=on_device,
+#         x=x[0],
+#         type=_type,
+#         n=n,
+#         axis=axis,
+#         norm=norm,
+#         rtol_=1e-3,
+#         atol_=1e-1,
 #     )
