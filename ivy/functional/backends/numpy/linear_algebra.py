2ab654a85fc9f403979205ef2e133c3b3b710347
Zaeem Ansari <zaeemansari70@gmail.com>
2023-05-05 18:46:58 +0500

    numpy implementation for lu

--- ivy/functional/backends/numpy/linear_algebra.py
+++ ivy/functional/backends/numpy/linear_algebra.py
@@ -499,6 +499,20 @@ def lu(
     *,
     pivot: bool = True,
     permute_l: bool = 0,
-    out: Optional[Tuple[tf.Tensor, tf.Tensor]] = None,
+    out: Optional[Tuple[np.ndarray, np.ndarray, np.ndarray]] = None,
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    # still implementing
+    n = A.shape[0]
+    L = np.eye(n)
+    P = np.eye(n)
+    
+    for i in range(n):
+        for k in range(i, n):
+            if ~np.isclose(U[i, i], 0.0):
+                break
+            U[[k, k + 1]] = U[[k + 1, k]]
+            P[[k, k + 1]] = P[[k + 1, k]]
+        factor = U[i + 1:, i] / U[i, i]
+        L[i + 1:, i] = factor
+        U[i + 1:] -= factor[:, np.newaxis] * U[i]
+
+    return P, L, U
