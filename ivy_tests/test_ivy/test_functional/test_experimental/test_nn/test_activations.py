41388568199666d9a9b1afac2925254f3c0fc362
Suraj520 <suraj.suraj.4.4.1996@gmail.com>
2023-05-07 01:29:09 +0530

    Unit tests for elu activation in experimental

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_activations.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_activations.py
@@ -71,6 +71,41 @@ def test_thresholded_relu(
     )
 
 
+# elu
+@handle_test(
+    fn_tree="elu",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        shape=st.shared(helpers.get_shape(), key="elu"),
+        large_abs_safety_factor=8,
+        small_abs_safety_factor=8,
+        safety_factor_scale="log",
+    ),
+    alpha=st.floats(min_value=-0.1, max_value=10.0),
+)
+def test_elu(
+    *,
+    dtype_and_x,
+    alpha,
+    test_flags,
+    backend_fw,
+    fn_name,
+    on_device,
+    ground_truth_backend,
+):
+    dtype, x = dtype_and_x
+    helpers.test_function(
+        ground_truth_backend=ground_truth_backend,
+        input_dtypes=dtype,
+        fw=backend_fw,
+        test_flags=test_flags,
+        fn_name=fn_name,
+        on_device=on_device,
+        x=x[0],
+        alpha=alpha,
+    )
+
+
 # prelu
 @handle_test(
     fn_tree="prelu",
