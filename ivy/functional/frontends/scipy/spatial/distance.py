bf80b5fad39d4fe1d6cb8acd7223e2f2d699d9cf
raj kumar <79806602+raj921@users.noreply.github.com>
2023-05-06 10:44:42 +0530

    shortend minkowski funtion

--- ivy/functional/frontends/scipy/spatial/distance.py
+++ ivy/functional/frontends/scipy/spatial/distance.py
@@ -31,26 +31,14 @@ def _validate_weights(w, dtype="float64"):
 # minkowski
 @to_ivy_arrays_and_back
 def minkowski(u, v, p=2, /, *, w=None):
-    u = _validate_vector(u)
-    v = _validate_vector(v)
-    if p <= 0:
-        raise ValueError("p must be greater than 0")
+    u, v = _validate_vector(u), _validate_vector(v)
+    if p <= 0: raise ValueError("p must be greater than 0")
     u_v = u - v
     if w is not None:
         w = _validate_weights(w)
-        if p == 1:
-            root_w = w
-        elif p == 2:
-            # better precision and speed
-            root_w = ivy.sqrt(w)
-        elif p == ivy.inf:
-            root_w = w != 0
-        else:
-            root_w = ivy.pow(w, 1 / p)
+        root_w = w if p == 1 else ivy.sqrt(w) if p == 2 else w != 0 if p == ivy.inf else ivy.pow(w, 1/p)
         u_v = ivy.multiply(root_w, u_v)
-    dist = sc_frontend.linalg.norm(u_v, ord=p)
-    return dist
-
+    return sc_frontend.linalg.norm(u_v, ord=p)
 
 # euclidean
 @to_ivy_arrays_and_back
