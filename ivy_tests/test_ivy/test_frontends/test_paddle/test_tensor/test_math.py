5b70346cd99419438f812daf1cf31312c685695d
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2023-05-06 12:17:47 +0000

    Add test for paddle.cos and fix test for paddle.sin

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -27,5 +27,31 @@ def test_paddle_sin(
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        input=x[0],
+        x=x[0],
+    )
+
+
+# cos
+@handle_frontend_test(
+    fn_tree="paddle.cos",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_paddle_cos(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
     )
