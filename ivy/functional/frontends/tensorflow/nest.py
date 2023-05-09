acab35eb3eaa0ac898e524d0060d9ef3347a91b2
Jono Tassia <jonotassia@gmail.com>
2023-05-09 13:32:28 +0100

    Restructuring tf.flatten function

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -36,13 +36,8 @@ def flatten(structure, expand_composites=False):
     if expand_composites and _is_composite_array(structure):
         return _flatten_composite_array(structure)
     elif isinstance(structure, (tuple, list)):
-        new_struc = []
-        for child in structure:
-            new_struc += flatten(child)
-        return new_struc
+        return [flatten(child) for child in structure]
     elif isinstance(structure, dict):
-        new_struc = []
-        for key in sorted(structure):
-            new_struc += flatten(structure[key])
-        return new_struc
+        return [flatten(structure[key]) for key in sorted(structure)]
+
     return [structure]
