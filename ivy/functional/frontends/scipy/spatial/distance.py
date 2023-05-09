e0b4957edcbb35984294ca0d44622b14a1f5eafc
raj kumar <79806602+raj921@users.noreply.github.com>
2023-05-09 22:09:49 +0530

    updated minkowski funntion
    as you told I added shortened function  root_w is a variable used to store the root of the weight w. I used a dictionary to store the values of root_w for different values of p. If p is not 1, 2, or infinity.  use the ** operator to calculate the root of w.and I use list comprehension to calculate the weighted difference vector, applying the square root of w to each element in u_v if w is not None.

--- ivy/functional/frontends/scipy/spatial/distance.py
+++ ivy/functional/frontends/scipy/spatial/distance.py
@@ -30,15 +30,14 @@ def _validate_weights(w, dtype="float64"):
 
 # minkowski
 @to_ivy_arrays_and_back
-def minkowski(u, v, p=2, /, *, w=None):
-    u, v = _validate_vector(u), _validate_vector(v)
-    if p <= 0: raise ValueError("p must be greater than 0")
+def minkowski(u: List[float], /, *, v: List[float], p: float = 2, w: Optional[float] = None) -> float:
+    u,v = _validate_vector(u),_validate_vector(v)
+    if p <= 0:
+        raise ValueError("p must be greater than 0")
     u_v = u - v
-    if w is not None:
-        w = _validate_weights(w)
-        root_w = w if p == 1 else ivy.sqrt(w) if p == 2 else w != 0 if p == ivy.inf else ivy.pow(w, 1/p)
-        u_v = ivy.multiply(root_w, u_v)
-    return sc_frontend.linalg.norm(u_v, ord=p)
+    root_w = {1: w, 2: ivy.sqrt(w), ivy.inf: w != 0}.get(p, w ** (1/p) if w else None)
+    u_v = [ivy.multiply(root_w, elem) if root_w is not None else elem for elem in u_v]
+    return ivy_np.linalg.norm(u_v, ord=p)
 
 
 # euclidean
