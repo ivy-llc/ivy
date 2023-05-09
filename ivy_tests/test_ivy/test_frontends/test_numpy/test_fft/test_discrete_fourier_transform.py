654a0bf13b320e11cee96118bc8367fa05db5de8
Ruba Dabbas <ruba@al-dabbas.com>
2023-05-09 05:09:48 +0300

    Solve conflict after merging

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -26,13 +26,13 @@ def test_numpy_iftt(dtype_and_x, frontend, test_flags, fn_tree, on_device):
 
 
 @handle_frontend_test(
-    fn_tree="numpy.fft.rfft",
+    fn_tree="numpy.fft.ifftshift",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("real_and_complex"), array_api_dtypes=True
+        available_dtypes=helpers.get_dtypes("float"), shape=(4,), array_api_dtypes=True
     ),
 )
-def test_numpy_rftt(*, dtype_and_x, frontend, test_flags, fn_tree, on_device):
-    input_dtype, x = dtype_and_x
+def test_numpy_ifttshift(dtype_and_x, frontend, test_flags, fn_tree, on_device):
+    input_dtype, arr = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
@@ -40,10 +40,8 @@ def test_numpy_rftt(*, dtype_and_x, frontend, test_flags, fn_tree, on_device):
         fn_tree=fn_tree,
         on_device=on_device,
         test_values=True,
-        a=x,
-        n=None,
-        axis=-1,
-        norm="backward",
+        x=arr[0],
+        axes=None,
     )
 
 
@@ -71,4 +69,26 @@ def test_numpy_fft(dtype_input_axis, norm, n, frontend, test_flags, fn_tree, on_
         n=n,
         axis=axis,
         norm=norm,
+    )
+
+
+@handle_frontend_test(
+    fn_tree="numpy.fft.rfft",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("real_and_complex"), array_api_dtypes=True
+    ),
+)
+def test_numpy_rftt(*, dtype_and_x, frontend, test_flags, fn_tree, on_device):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        a=x,
+        n=None,
+        axis=-1,
+        norm="backward",
     )
