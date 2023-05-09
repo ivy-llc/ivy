e49e86c1da5269fce7b351a596ab14897a2b94f7
Paula Ehab Alfy <42894502+paulaehab@users.noreply.github.com>
2023-05-09 20:10:36 +0000

    fixing the tests

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -893,7 +893,7 @@ class Tensor:
     def __mul__(self, other):
         return torch_frontend.mul(self, other)
 
-    @with_unsupported_dtypes({"1.11.0 and below": ("bfloat16",)}, "torch")
+    @with_unsupported_dtypes({"1.11.0 and below": "bfloat16"}, "torch")
     def __matmul__(self, other):
         return torch_frontend.matmul(self, other)
 
--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -1892,44 +1892,44 @@ def test_torch_special_mul(
     )
 
 
+@st.composite
+def _get_dtype_and_multiplicative_matrices(draw):
+    return draw(
+        st.one_of(
+            _get_dtype_input_and_matrices(),
+            _get_dtype_and_3dbatch_matrices(),
+        )
+    )
+
+
 # __matmul__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="torch.tensor",
     method_name="__matmul__",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        min_num_dims=1,
-        num_arrays=2,
-        shared_dtype=True,
-        min_value=-1e04,
-    ),
+    dtype_tensor1_tensor2=_get_dtype_and_multiplicative_matrices(),
 )
 def test_torch_special_matmul(
-    dtype_and_x,
+    dtype_tensor1_tensor2,
     frontend_method_data,
     init_flags,
     method_flags,
     frontend,
     on_device,
 ):
-    input_dtype, x = dtype_and_x
+    dtype, tensor1, tensor2 = dtype_tensor1_tensor2
     helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
+        init_input_dtypes=dtype,
         init_all_as_kwargs_np={
-            "data": x[0],
+            "data": tensor1,
         },
-        method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={
-            "other": x[1],
-        },
+        method_input_dtypes=dtype,
+        method_all_as_kwargs_np={"other": tensor2},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
         frontend=frontend,
         on_device=on_device,
-        rtol_=1e-04,
-        atol_=1e-07,
     )
 
 
