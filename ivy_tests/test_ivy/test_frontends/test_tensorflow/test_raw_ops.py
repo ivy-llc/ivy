9cd43b8b812c3a47f13633f64891c2a276f8fda0
MuhammadAbuzar36 <60536266+MuhammadAbuzar36@users.noreply.github.com>
2023-05-10 01:18:35 +0500

    added test_tensorflow_LogicalAnd

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_raw_ops.py
@@ -1656,8 +1656,38 @@ def test_tensorflow_LogicalOr(  # NOQA
         x=x[0],
         y=x[1],
     )
+    
+    
+# LogicalAnd
+@handle_frontend_test(
+    fn_tree="tensorflow.raw_ops.LogicalAnd",
+    dtype_and_x=helpers.dtype_and_values(
+        dtype=["bool", "bool"],
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_LogicalAnd(  # NOQA
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
 
-
+    
 # LogicalNot
 @handle_frontend_test(
     fn_tree="tensorflow.raw_ops.LogicalNot",
