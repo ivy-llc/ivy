0db61bd8a935edd6f0edf0f1bf57ab2f46fc7a43
mtnvdsk <71755581+mtnvdsk@users.noreply.github.com>
2023-05-11 16:21:44 +0530

    Update test_layers.py

--- ivy_tests/test_ivy/test_stateful/test_layers.py
+++ ivy_tests/test_ivy/test_stateful/test_layers.py
@@ -1092,7 +1092,7 @@ def test_sequential_layer(
 #MaxPool1D
 @handle_method(
     method_tree="MaxPool1D.__call__",
-    x_k_s_p=helpers.arrays_for_pooling(min_dims=2, max_dims=3, min_side=1, max_side=4),
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=3, max_dims=3, min_side=2, max_side=4),
 )
 def test_maxpool1d_layer(
     *,
@@ -1106,23 +1106,20 @@ def test_maxpool1d_layer(
     method_flags,
 ):
     input_dtype, x, kernel_size, stride, padding = x_k_s_p
-
-    if len(x[0].shape) == 2:
-        x[0] = np.expand_dims(x[0], axis=2)
-
     helpers.test_method(
         ground_truth_backend=ground_truth_backend,
         init_flags=init_flags,
         method_flags=method_flags,
         init_all_as_kwargs_np={
-            "kernel_size": kernel_size[-1],
-            "stride": stride[-1],
-            "padding": padding[-1],
+            "kernel_size": kernel_size,
+            "stride": stride,
+            "padding": padding,
+            "data_format": "NCW",
             "device": on_device,
             "dtype": input_dtype[0],
         },
         method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={"inputs": x[0], "data_format": "NCW"},
+        method_all_as_kwargs_np={"inputs": x[0]},
         class_name=class_name,
         method_name=method_name,
         test_gradients=test_gradients,
