bef75c9286fd76bc702423a1078e742ae5ecaea9
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-05-13 19:53:18 -0400

    Update order_statistics.py

--- ivy/functional/frontends/numpy/statistics/order_statistics.py
+++ ivy/functional/frontends/numpy/statistics/order_statistics.py
@@ -54,8 +54,14 @@ def nanpercentile(
     q = ivy.divide(q, 100.0)
     q = ivy.array(q)
     # print(q)
-    if not _quantile_is_valid(q):
-        raise ValueError("percentile s must be in the range [0, 100]")
+    try:
+        if not _quantile_is_valid(q):
+            raise ivy.utils.exceptions.IvyError()
+    except ivy.utils.exceptions.IvyError:
+        ivy.logging.warning("percentile s must be in the range [0, 100]")
+    finally:
+        return []
+    
     if axis is None:
         resultarray = []
         nanlessarray = []
