63dacb9acc62d82c76ecc2e9ef45def347162142
raj kumar <79806602+raj921@users.noreply.github.com>
2023-05-13 11:24:25 +0530

    added acos paddle.tensor.math Mathematical Functions to PaddlePaddle Frontend

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -16,3 +16,8 @@ def sin(x, name=None):
 @to_ivy_arrays_and_back
 def cos(x, name=None):
     return ivy.cos(x)
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def acos(x, name=None):
+    return ivy.acos(x)
