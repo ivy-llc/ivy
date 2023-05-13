3260777528ff2306cf2afecc74026b207d2a6a20
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2023-05-13 09:27:20 +0000

    Add test for __getitem__ instance method in Paddle frontend and fix the reshape instance method test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -9,44 +9,50 @@ from ivy_tests.test_ivy.helpers import handle_frontend_method
 CLASS_TREE = "ivy.functional.frontends.paddle.Tensor"
 
 
+@st.composite
+def _reshape_helper(draw):
+    # generate a shape s.t len(shape) > 0
+    shape = draw(
+        helpers.get_shape(
+            allow_none=False,
+            min_num_dims=1,
+            max_num_dims=3,
+            min_dim_size=1,
+            max_dim_size=3,
+        )
+    )
+
+    reshape_shape = draw(helpers.reshape_shapes(shape=shape))
+
+    dtypes, x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=helpers.get_dtypes("valid"),
+            shape=shape,
+        )
+    )
+    return dtypes, x, reshape_shape
+
+
+# reshape
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="paddle.to_tensor",
     method_name="reshape",
-    dtype_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid", full=True),
-        shape=st.shared(
-            helpers.get_shape(min_num_dims=2, max_num_dims=5), key="value_shape"
-        ),
-    ),
-    shape=helpers.reshape_shapes(
-        shape=st.shared(
-            helpers.get_shape(min_num_dims=2, max_num_dims=5), key="value_shape"
-        ),
-    ),
-    unpack_shape=st.booleans(),
+    dtype_x_shape=_reshape_helper(),
 )
 def test_paddle_instance_reshape(
-    dtype_x,
-    shape,
-    unpack_shape,
+    dtype_x_shape,
     frontend_method_data,
     init_flags,
     method_flags,
     frontend,
     on_device,
 ):
-    input_dtype, x = dtype_x
+    input_dtype, x, shape = dtype_x_shape
     assume(len(shape) != 0)
     shape = {
         "shape": shape,
     }
-    if unpack_shape:
-        method_flags.num_positional_args = len(shape["shape"]) + 1
-        i = 0
-        for x_ in shape["shape"]:
-            shape["x{}".format(i)] = x_
-            i += 1
     helpers.test_frontend_method(
         init_input_dtypes=input_dtype,
         init_all_as_kwargs_np={
@@ -54,6 +60,38 @@ def test_paddle_instance_reshape(
         },
         method_input_dtypes=input_dtype,
         method_all_as_kwargs_np=shape,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
+# __getitem__
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="paddle.to_tensor",
+    method_name="__getitem__",
+    dtype_x_index=helpers.dtype_array_index(
+        available_dtypes=helpers.get_dtypes("valid"),
+        allow_neg_step=False,
+    ),
+)
+def test_paddle_instance_getitem(
+    dtype_x_index,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x, index = dtype_x_index
+    helpers.test_frontend_method(
+        init_input_dtypes=[input_dtype[0]],
+        init_all_as_kwargs_np={"data": x},
+        method_input_dtypes=[input_dtype[1]],
+        method_all_as_kwargs_np={"item": index},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
