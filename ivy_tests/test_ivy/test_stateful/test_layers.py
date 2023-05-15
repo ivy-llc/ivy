677a9e2f6ddbb347d0757a65c9af76fc24086160
mtnvdsk <71755581+mtnvdsk@users.noreply.github.com>
2023-05-15 14:22:02 +0530

    Update test_layers.py

--- ivy_tests/test_ivy/test_stateful/test_layers.py
+++ ivy_tests/test_ivy/test_stateful/test_layers.py
@@ -1125,8 +1125,7 @@ def test_maxpool1d_layer(
         test_gradients=test_gradients,
         on_device=on_device,
         atol=1e-5,
-        rtol=1e-5,
-        padding_type=padding_type,                                        
+        rtol=1e-5,                                       
     )
     
     
