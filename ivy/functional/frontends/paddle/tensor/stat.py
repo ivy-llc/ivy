6972adeb3a7cc70fecd9d28a72f96eaad81631a2
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-15 21:02:19 +0530

    Updated stat.py
    Added mean function

--- ivy/functional/frontends/paddle/tensor/stat.py
+++ ivy/functional/frontends/paddle/tensor/stat.py
@@ -1,1 +1,10 @@
 # global
+import ivy
+from ivy.functional.frontends.paddle.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+
+
+@to_ivy_arrays_and_back
+def mean(x, axis, keepdims, out):
+    return ivy.mea(x, axis, keepdims, out)
