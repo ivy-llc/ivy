107a295708856d44b1335cfa9e42ef00f8bf1a18
Carlos Sandoval <71140233+clsandoval@users.noreply.github.com>
2023-05-15 08:35:32 +0000

    add align corners=True mode, fix linting

--- ivy/functional/frontends/torch/nn/functional/vision_functions.py
+++ ivy/functional/frontends/torch/nn/functional/vision_functions.py
@@ -325,10 +325,16 @@ def upsample_bilinear(input, size=None, scale_factor=None):
 def affine_grid(theta, size, align_corners=False):
     if len(size) == 4:
         N, C, H, W = size
+        base_grid = ivy.empty((N, H, W, 3))
         if align_corners:
-            base_grid = ivy.empty((N, H, W, 3))
             base_grid[:, :, :, 0] = ivy.linspace(-1, 1, W)
             base_grid[:, :, :, 1] = ivy.expand_dims(ivy.linspace(-1, 1, H), axis=-1)
             base_grid[:, :, :, 2] = ivy.full((W, H), 1)
-            grid = ivy.matmul(base_grid.view((N, H * W, 3)), theta.swapaxes(1, 2))
-            return grid.view((N, H, W, 2))
+        else:
+            base_grid[:, :, :, 0] = ivy.linspace(-1, 1, W) * (W - 1) / W
+            base_grid[:, :, :, 1] = ivy.expand_dims(
+                ivy.linspace(-1, 1, H) * (H - 1) / H, axis=-1
+            )
+            base_grid[:, :, :, 2] = ivy.full((W, H), 1)
+        grid = ivy.matmul(base_grid.view((N, H * W, 3)), ivy.swapaxes(theta, 1, 2))
+        return grid.view((N, H, W, 2))
