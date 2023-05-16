98d9555b3d063337b9ab817b2c9b3139e7aa2e53
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-05-16 10:29:33 -0400

    Update test_jax_src_tree_util.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_tree_util.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_tree_util.py
@@ -1,7 +1,7 @@
 # local
 import ivy
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-from ivy.functional.frontends.jax._src.tree_util import tree_map
+from ivy.functional.frontends.jax._src.tree_util import tree_leaves, tree_map
 import hypothesis.strategies as st
 
 
