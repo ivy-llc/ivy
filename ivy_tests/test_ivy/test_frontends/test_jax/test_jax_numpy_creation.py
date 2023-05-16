e27e27ddc020246a28092ba69b93ecd9d6124eb4
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-05-16 15:53:15 +0300

    Min dim set to 2 to facilitate `axis` testing

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
@@ -866,7 +866,7 @@ def test_jax_numpy_cdouble(
     dtype_arr=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("integer"),
         min_num_dims=2,
-        max_num_dims=2,
+        max_num_dims=3,
         min_dim_size=10,
         max_dim_size=100,
     ),
