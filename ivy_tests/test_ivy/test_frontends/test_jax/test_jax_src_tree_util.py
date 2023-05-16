840fa3e88418cfbb8a3e9c182b6134592095341e
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-05-16 16:17:03 -0400

    Update test_jax_src_tree_util.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_tree_util.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_tree_util.py
@@ -58,13 +58,13 @@ def test_jax_tree_leaves(
     on_device,
 ):
     # Apply the tree_leaves function to obtain the leaves of the tree
-    result = jax.numpy.asarray(tree_leaves(tree, is_leaf=None))
+    result = tree_leaves(tree, is_leaf=None)
 
     # compute the expected result
-    expected = jax.numpy.asarray(jax.tree_util.tree_leaves(tree))
+    expected = jax.tree_util.tree_leaves(tree)
 
     # value test
-    assert ivy.equal(ivy.Array(result), ivy.Array(expected))
+    assert ivy.equal(result, expected)
 
 
 # tree_map
