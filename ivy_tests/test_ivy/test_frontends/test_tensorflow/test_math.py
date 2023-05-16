0672219be0db4dadb5fe64f2cd303749ac686ef1
Chandan S <chandans4131@gmail.com>
2023-05-16 22:49:01 +0530

    sign- -- tensorflow frontend function added (#3)

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -2527,3 +2527,30 @@ def test_tensorflow_in_top_k(
         pred=x[1],
         k=k,
     )
+
+
+# sign
+@handle_frontend_test(
+    fn_tree="tensorflow.math.sign",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_sign(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
