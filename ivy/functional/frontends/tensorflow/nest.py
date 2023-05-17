b98b8bc4346c662bbad62bfaa10ef6b895348029
Jono Tassia <jonotassia@gmail.com>
2023-05-17 16:39:58 +0100

    Reverting tf.nest.flatten back to original implementation since it runs much faster until we finalise plan to leverage ivy.flatten

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -3,7 +3,6 @@ import ivy.functional.frontends.tensorflow.ragged as ragged_tf
 import ivy
 
 # local
-from functools import reduce
 
 # try:
 #     import tensorflow as tf
@@ -39,9 +38,13 @@ def flatten(structure, expand_composites=False):
     if expand_composites and _is_composite_array(structure):
         return _flatten_composite_array(structure, expand_composites=expand_composites)
     elif isinstance(structure, (tuple, list)):
-        return list(reduce(lambda x, y: flatten(x) + flatten(y), structure))
+        new_struc = []
+        for child in structure:
+            new_struc += flatten(child)
+        return new_struc
     elif isinstance(structure, dict):
-        return list(
-            reduce(lambda x, y: flatten(x) + flatten(y), sorted(structure.values()))
-        )
+        new_struc = []
+        for key in sorted(structure):
+            new_struc += flatten(structure[key])
+        return new_struc
     return [structure]
