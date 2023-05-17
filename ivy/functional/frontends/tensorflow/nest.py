4e3416ce9fb3529df88ed804ab186860d2cadeb3
Jono Tassia <jonotassia@gmail.com>
2023-05-17 16:51:27 +0100

    Updating with list comp to shorten tf.nest.flatten

--- ivy/functional/frontends/tensorflow/nest.py
+++ ivy/functional/frontends/tensorflow/nest.py
@@ -38,13 +38,7 @@ def flatten(structure, expand_composites=False):
     if expand_composites and _is_composite_array(structure):
         return _flatten_composite_array(structure, expand_composites=expand_composites)
     elif isinstance(structure, (tuple, list)):
-        new_struc = []
-        for child in structure:
-            new_struc += flatten(child)
-        return new_struc
+        return [x for child in structure for x in flatten(child)]
     elif isinstance(structure, dict):
-        new_struc = []
-        for key in sorted(structure):
-            new_struc += flatten(structure[key])
-        return new_struc
+        return [x for key in sorted(structure) for x in flatten(structure[key])]
     return [structure]
