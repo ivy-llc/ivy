d85d0eeacfa6b79a168a8f78cd490204224197e0
Ali Nour <56680303+Ali-12122@users.noreply.github.com>
2023-05-18 14:19:01 +0300

    implemented torch.nn.functional.dropout2d

--- ivy/functional/frontends/torch/nn/functional/dropout_functions.py
+++ ivy/functional/frontends/torch/nn/functional/dropout_functions.py
@@ -16,3 +16,25 @@ def dropout(input, p=0.5, training=True, inplace=False):
         ivy.inplace_update(input, ret)
         return input
     return ret
+
+
+@to_ivy_arrays_and_back
+def dropout2d(tensor, p=0.5, training=True, inplace=False):
+    if tensor.ndim != 4:
+        raise ValueError("Input must have exactly 4 dimensions.")
+
+    if not training:
+        return tensor
+
+    shape = tensor.shape
+    mask = ivy.bernoulli(probs=p, shape=(shape[2], shape[3]))
+    mask = ivy.tile(mask, shape[1])
+    mask = ivy.reshape(mask, (shape[1], shape[2], shape[3]))
+    mask = ivy.tile(mask, shape[0])
+    mask = ivy.reshape(mask, shape)
+
+    if inplace:
+        tensor *= mask
+        return tensor
+    else:
+        return tensor * mask
--- ivy_tests/test_ivy/test_frontends/test_torch/test_dropout_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_dropout_functions.py
@@ -50,3 +50,48 @@ def test_torch_dropout(
     for u in ret:
         # cardinality test
         assert u.shape == x.shape
+
+
+@handle_frontend_test(
+    fn_tree="torch.nn.functional.dropout2d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=0,
+        max_value=50,
+        allow_inf=False,
+        min_num_dims=4,
+        max_num_dims=4,
+        min_dim_size=1,
+    ),
+    prob=helpers.floats(min_value=0, max_value=0.99),
+    training=st.booleans(),
+    test_with_out=st.just(False),
+    test_inplace=st.booleans(),
+)
+def test_torch_dropout2d(
+    *,
+    dtype_and_x,
+    prob,
+    training,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_datatype, x = dtype_and_x
+    ret = helpers.test_frontend_function(
+        input_dtypes=input_datatype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        p=prob,
+        training=training,
+        test_values=False,
+    )
+    ret = helpers.flatten_and_to_np(ret=ret)
+    x = np.asarray(x[0], input_datatype[0])
+    for u in ret:
+        # cardinality test
+        assert u.shape == x.shape
