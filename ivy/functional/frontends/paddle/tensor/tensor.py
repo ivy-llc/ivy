a2b4b2aa52c87c561b52a8b3fbf7449cbbc4f741
Oluwamayowa Olawumi <mayor.michael19@gmail.com>
2023-05-18 10:06:13 +0000

    made changes to method

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -94,18 +94,6 @@ class Tensor:
     def abs(self):
         return paddle_frontend.abs(self)
 
-
-"""
-    def abs(self):
-        if isinstance(self._ivy_array, list):
-            # If the input is a list, compute the absolute value.
-            abs_value = [abs(num) for num in self._ivy_array]
-            return abs_value
-        elif isinstance(self._ivy_array, (int, float)):
-            # If the input is an int or float, compute its absolute value.
-            abs_value = abs(self._ivy_array)
-            return abs_value
-        else:
-            # Raise an exception if the input type is not supported.
-            raise ValueError("Invalid input type. Must be a list, int, or float.")
-"""
+    def abs_(self):
+        self.ivy_array = self.abs().ivy_array
+        return self
