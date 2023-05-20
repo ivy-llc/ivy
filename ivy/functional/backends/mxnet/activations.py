37ec5059dec21189fae1dd06412d2695bc052863
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-05-20 20:31:40 +0100

    add gelu activation function for the mx backend

--- ivy/functional/backends/mxnet/activations.py
+++ ivy/functional/backends/mxnet/activations.py
@@ -4,13 +4,18 @@ MXNet activation functions.
 Collection of MXNet activation functions, wrapped to fit Ivy syntax and
 signature.
 """
+import mxnet as mx
+import numpy as np
+
 from ivy.utils.exceptions import IvyNotImplementedException
 from typing import Optional, Union
 import ivy
 
 
 def gelu(x: None, /, *, approximate: bool = False, out: Optional[None] = None) -> None:
-    raise IvyNotImplementedException()
+    if approximate:
+        return 0.5 * x * (1 + mx.nd.tanh(((2 / np.pi) ** 0.5) * (x + 0.044715 * x ** 3)))
+    return mx.nd.LeakyReLU(x, act_type='gelu')
 
 
 def leaky_relu(x: None, /, *, alpha: float = 0.2, out: Optional[None] = None) -> None:
