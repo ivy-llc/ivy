5d7058c08bd43954c1824f9c23edf174b95e4220
Shubham <99467100+TheGreatShubham@users.noreply.github.com>
2023-05-21 08:58:50 +0530

    Update math.py

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -35,6 +35,7 @@ def cosh(x, name=None):
 def tanh(x, name=None):
     return ivy.tanh(x)
 
+
 @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
 @to_ivy_arrays_and_back
 def acosh(x, name=None):
