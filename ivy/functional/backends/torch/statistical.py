3bdfabc85ab4805422a2cf416e70a4875472ae44
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-05-21 18:32:45 +0100

    update dtype spec version for cummax in torch backend to 2.0.1

--- ivy/functional/backends/torch/statistical.py
+++ ivy/functional/backends/torch/statistical.py
@@ -345,7 +345,7 @@ cumsum.support_native_out = True
 # Function does support uint8, but allowing support for unsigned will cause
 # the function to break the upcasting rule defined in the Array API Standard
 @with_unsupported_dtypes(
-    {"1.11.0 and below": ("uint8", "bfloat16", "float16")},
+    {"2.0.1 and below": ("uint8", "bfloat16", "float16")},
     backend_version,
 )
 def cummax(
