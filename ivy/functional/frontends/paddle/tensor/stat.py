bb3afaeee24bce7c0105acd9715d6dfe09291677
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-23 11:46:52 +0530

    Update stat.py

--- ivy/functional/frontends/paddle/tensor/stat.py
+++ ivy/functional/frontends/paddle/tensor/stat.py
@@ -17,5 +17,5 @@ def var(input, axis=None, correction=0, keepdims=False, out=None):
 
 @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
 @to_ivy_arrays_and_back
-def nanmedian(input, axis=None, keepdims=False, overwrite_input=Flase, out=None):
+def nanmedian(input, axis=None, keepdims=False, overwrite_input=False, out=None):
     return ivy.var(input, axis=axis, keepdims=keepdims, overwrite_input=overwrite_input out=None)
