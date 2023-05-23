ec094b2a31e8489c2ce42c3e206821f9bc0be0cb
Rounak Saraf <46487512+Rounak183@users.noreply.github.com>
2023-05-23 05:49:49 +0000

    added cholesky_inverse function to linear_algebra

--- ivy/functional/frontends/torch/blas_and_lapack_ops.py
+++ ivy/functional/frontends/torch/blas_and_lapack_ops.py
@@ -91,14 +91,8 @@ def cholesky(input, upper=False, *, out=None):
 
 @to_ivy_arrays_and_back
 def cholesky_inverse(input, upper=True, *, out=None):
-    cholesky_factor = ivy.cholesky(input, upper=upper, out=None)
-    cholesky_factor_transpose = ivy.matrix_transpose(cholesky_factor)
-    if not upper:
-        inv = ivy.inv(ivy.matmul(cholesky_factor, cholesky_factor_transpose, out=None))
-    else:
-        inv = ivy.inv(ivy.matmul(cholesky_factor_transpose, cholesky_factor, out=None))
-    return inv
-
+    return ivy.cholesky_inverse(input, upper=upper, out=out)
+    
 
 @to_ivy_arrays_and_back
 def det(input):
