6364365a34cad320fa2fe8f760ef08e92fd3a701
SANTHOSHKAKARLA <134357125+SANTHOSHKAKARLA@users.noreply.github.com>
2023-05-23 10:31:48 +0530

    Update stat.py

--- ivy/functional/frontends/paddle/tensor/stat.py
+++ ivy/functional/frontends/paddle/tensor/stat.py
@@ -9,3 +9,8 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 @to_ivy_arrays_and_back
 def mean(input, axis=None, keepdims=False, out=None):
     return ivy.std(input, axis=axis, keepdims=keepdims, out=None)
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def median(input, axis=None, keepdims=False, out=None):
+    return ivy.median(input, axis=axis, keepdims=keepdims, out=None)
