aa3ac8eeb80b383ab0691c23927b755d73918f3c
Matt <matthew-barrett@hotmail.co.uk>
2023-05-23 16:03:31 +0100

    remove unnecessary jax frontend module after compiler wrapping improvements

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_api.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_src_api.py
@@ -3,9 +3,8 @@ import ivy
 from ivy.functional.ivy.gradients import _variable
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-from ivy.functional.frontends.jax._src.api import device_put, device_get
+from ivy.functional.frontends.jax import device_put, device_get, vmap
 from hypothesis import strategies as st
-from ivy.functional.frontends.jax._src.api import vmap
 import jax
 
 
