188264a3010e5afa3ec564cc5c282f4cd8885791
VladZetta <rypsa2012@gmail.com>
2023-05-23 22:42:20 +0700

    Added asin function and test for frontend

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -40,3 +40,8 @@ def tanh(x, name=None):
 @to_ivy_arrays_and_back
 def acosh(x, name=None):
     return ivy.acosh(x)
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def asin(x, name=None):
+    return ivy.asin(x)
