727233dbdbd976a08f4ed43179c9451d311f5635
mustafaa7med <mustafaa7med@gmail.com>
2023-05-24 09:55:40 +0300

    updating tensor.py

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -374,7 +374,7 @@ class Tensor:
     def floor(self, *, out=None):
         return torch_frontend.floor(self)
 
-    @with_unsupported_dtypes({"2.0.1 and below": ("float16",)}, "torch")
+    @with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
     def not_equal(self, other, *, out=None):
         return torch_frontend.not_equal(self, other, out=out)
 
