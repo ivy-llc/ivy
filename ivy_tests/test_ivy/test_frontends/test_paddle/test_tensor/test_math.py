c96b077aae67d142f298dab32b0d9457c4632435
raj kumar <79806602+raj921@users.noreply.github.com>
2023-05-24 22:26:20 +0530

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -83,30 +83,7 @@ def test_paddle_acos(
         atol=1e-2,
         x=x[0],
     )
-# atan
-@handle_frontend_test(
-    fn_tree="paddle.atan",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-    ),
-)
-def test_paddle_atan(
-    *,
-    dtype_and_x,
-    on_device,
-    fn_tree,
-    frontend,
-    test_flags,
-):
-    input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x=x[0],
-    )
+
 
 
 # tanh
@@ -133,5 +110,30 @@ def test_paddle_tanh(
         fn_tree=fn_tree,
         on_device=on_device,
         atol=1e-2,
+        x=x[0],
+    )
+    
+ # atan
+@handle_frontend_test(
+    fn_tree="paddle.atan",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_paddle_atan(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
         x=x[0],
     )
