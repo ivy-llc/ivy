7229915579342ab4082c94e0ac72eb090d2f271c
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-24 16:51:01 +0530

    added atan tests

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -238,6 +238,30 @@ def test_paddle_atanh(
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        atol=1e-2,
+        x=x[0],
+    )
+
+# atan
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.atan",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_atan(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
         x=x[0],
     )
