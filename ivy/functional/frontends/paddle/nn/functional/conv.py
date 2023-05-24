5e85b5a44e17edd91d64c4f09504f8106e25c11d
shapeetha <129947783+shapeetha@users.noreply.github.com>
2023-05-24 17:09:11 +0100

    Update conv.py

--- ivy/functional/frontends/paddle/nn/functional/conv.py
+++ ivy/functional/frontends/paddle/nn/functional/conv.py
@@ -1,1 +1,5 @@
-# local
+import ivy
+import ivy.functional.frontends.paddle.nn.functional as pdf
+
+def conv2D(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
+  return pdf.conv2D(weight, bias, stride, padding, dilation, groups)
