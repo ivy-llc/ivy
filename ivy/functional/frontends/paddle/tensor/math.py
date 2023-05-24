1884b8f98662c2cdddc583c50f5d11fda282eab2
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-24 16:21:14 +0530

    Update math.py

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -46,3 +46,9 @@ def acosh(x, name=None):
 @to_ivy_arrays_and_back
 def asin(x, name=None):
     return ivy.asin(x)
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def sqrt(x, name=None):
+    return ivy.sqrt(x)
+
