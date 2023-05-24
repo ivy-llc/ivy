66b43960715d56f7d6848d93e789b9ae0284ed8d
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2023-05-24 19:50:35 +0000

    Added bool to creation.py and added test.

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_creation.py
@@ -779,6 +779,28 @@ def test_jax_numpy_double(
         x=x[0],
     )
 
+# bool_
+@handle_frontend_test(
+    fn_tree="jax.numpy.bool_",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("bool")),
+)
+def test_jax_numpy_bool_(
+    dtype_and_x,
+    test_flags,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
 
 @handle_frontend_test(
     fn_tree="jax.numpy.geomspace",
