fd066cc47bc086769672920960cc7415166bc865
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-24 17:01:11 +0530

    added round

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -61,3 +61,8 @@ def atanh(x, name=None):
 @to_ivy_arrays_and_back
 def atan(x, name=None):
     return ivy.atan(x)
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def round(x, name=None):
+    return ivy.round(x)
