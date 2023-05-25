1015624c8fb4779a805ddd9a6f32bf44128c7e9d
Paula Ehab Alfy <42894502+paulaehab@users.noreply.github.com>
2023-05-25 15:28:43 -0700

    fix torch.conj_pyhsical function and tests (#15871)

--- ivy/functional/frontends/torch/pointwise_ops.py
+++ ivy/functional/frontends/torch/pointwise_ops.py
@@ -467,7 +467,7 @@ def angle(input, *, out=None):
 
 @to_ivy_arrays_and_back
 def conj_physical(input, *, out=None):
-    return ivy.conj_physical(input, out=out)
+    return ivy.conj(input, out=out)
 
 
 @to_ivy_arrays_and_back
