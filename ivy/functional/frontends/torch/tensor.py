e571a4f09feb4e87b32493b1c58952ce59caa857
mustafaa7med <mustafaa7med@gmail.com>
2023-05-25 21:56:46 +0300

    removing a variable and adjusting parameters (*, out=None)

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -381,10 +381,8 @@ class Tensor:
     ne = not_equal
 
     @with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
-    def equal(self, other, *, out=None):
-        return torch_frontend.equal(self, other, out=out)
-
-    eq = equal
+    def equal(self, other):
+        return torch_frontend.equal(self, other)
 
     def new_zeros(self, size, *, dtype=None, device=None, requires_grad=False):
         return torch_frontend.zeros(
