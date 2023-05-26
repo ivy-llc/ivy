5372ea3ca0465231b48cabf80329e3afbc87facf
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-26 10:28:46 +0530

    Update math.py

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -46,18 +46,3 @@ def acosh(x, name=None):
 @to_ivy_arrays_and_back
 def asin(x, name=None):
     return ivy.asin(x)
-
-@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
-@to_ivy_arrays_and_back
-def relu(x, name=None):
-    return ivy.relu(x)
-
-@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
-@to_ivy_arrays_and_back
-def sigmoid(x, name=None):
-    return ivy.sigmoid(x)
-
-@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
-@to_ivy_arrays_and_back
-def leaky_relu(x, alpha=0.2, name=None):
-    return ivy.leaky_relu(x, alpha=alpha)
