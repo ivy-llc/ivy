a49411d8cc6c3140530cd7c16c74f4a96cd3382e
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-26 11:09:56 +0530

    Update test_paddle_activation.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
@@ -104,3 +104,28 @@ def test_paddle_log_sigmoid(
         on_device=on_device,
         x=x[0],
     )
+
+# silu
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.log_silu",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_log_silu(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
