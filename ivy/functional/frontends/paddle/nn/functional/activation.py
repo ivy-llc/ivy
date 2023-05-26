0b3a1dc86002f4c0c698b5fabd472e1042d0cd5e
Maher mohsen <73169639+maher-mohsen@users.noreply.github.com>
2023-05-26 03:19:46 +0300

    add softmax to activation in paddle

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,5 +1,6 @@
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
-
+from ivy.functional.frontends.paddle.tensor.math import softmax as paddle_softmax 
 
 tanh = paddle_tanh
+softmax = paddle_softmax
