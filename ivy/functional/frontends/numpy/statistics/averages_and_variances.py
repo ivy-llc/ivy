ad5750082f0bd00fab90ff0bedd095835365c262
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-05-26 12:26:46 -0400

    Update averages_and_variances.py

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -267,11 +267,11 @@ def _quantile_is_valid(q):
 def cpercentile(N, percent, key=lambda x: x):
     """
     Find the percentile   of a list of values.
-    
+
     @parameter N - is a list of values. Note N MUST BE already sorted.
     @parameter percent - a float value from 0.0 to 1.0.
     @parameter key - optional key function to compute value from each element of N.
-    
+
     @return - the percentile  of the values
     """
     N.sort()
