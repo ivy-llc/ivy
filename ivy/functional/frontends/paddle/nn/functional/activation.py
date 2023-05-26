260a46015abec72f27463ab04dc1c63d6a00cebf
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-26 10:23:10 +0530

    Update activation.py

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,5 +1,11 @@
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
+from ivy.functional.frontends.paddle.tensor.math import relu as paddle_relu
+from ivy.functional.frontends.paddle.tensor.math import sigmoid as paddle_sigmoid
+from ivy.functional.frontends.paddle.tensor.math import leaky_relu as paddle_leaky_relu
 
 
 tanh = paddle_tanh
+relu=paddle_relu
+sigmoid=paddle_sigmoid
+leaky_relu=paddle_leaky_relu
