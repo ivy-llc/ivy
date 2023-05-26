9ca89e52791767ccd9641737b5dd774c0fa7475e
Santhosh Kakarla <65003974+KSANTHOSH200@users.noreply.github.com>
2023-05-26 10:34:44 +0530

    Update activation.py

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,11 +1,25 @@
+# global
+import ivy
+from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
+from ivy.functional.frontends.paddle.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
-from ivy.functional.frontends.paddle.tensor.math import relu as paddle_relu
-from ivy.functional.frontends.paddle.tensor.math import sigmoid as paddle_sigmoid
-from ivy.functional.frontends.paddle.tensor.math import leaky_relu as paddle_leaky_relu
-
-
 tanh = paddle_tanh
-relu=paddle_relu
-sigmoid=paddle_sigmoid
-leaky_relu=paddle_leaky_relu
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def relu(x, name=None):
+    return ivy.relu(x)
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def sigmoid(x, name=None):
+    return ivy.sigmoid(x)
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def leaky_relu(x, alpha=0.2, name=None):
+    return ivy.leaky_relu(x, alpha=alpha)
