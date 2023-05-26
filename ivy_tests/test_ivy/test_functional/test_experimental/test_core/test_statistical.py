ace246a2b6b3469560fed8a87aaf5823b3c40474
Selvaraj Sembulingam <66372829+selvaraj-sembulingam@users.noreply.github.com>
2023-05-26 15:18:01 +0530

    `Igamma` added in experimental (#15717)
    Co-authored-by: Shehryar Tariq <sherrytst30@gmail.com>

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
@@ -435,3 +435,45 @@ def test_bincount(
         weights=x[1],
         minlength=min_length,
     )
+
+
+# igamma
+@handle_test(
+    fn_tree="functional.ivy.experimental.igamma",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=["float32"],
+        num_arrays=2,
+        shared_dtype=True,
+        abs_smallest_val=1e-5,
+        min_num_dims=2,
+        max_num_dims=2,
+        min_dim_size=3,
+        max_dim_size=3,
+        min_value=2,
+        max_value=100,
+        allow_nan=False,
+    ),
+    test_gradients=st.just(False),
+    test_with_out=st.just(False),
+)
+def test_igamma(
+    *,
+    dtype_and_x,
+    test_flags,
+    backend_fw,
+    fn_name,
+    on_device,
+    ground_truth_backend,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        ground_truth_backend=ground_truth_backend,
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        on_device=on_device,
+        fw=backend_fw,
+        fn_name=fn_name,
+        rtol_=1e-04,
+        a=x[0],
+        x=x[1],
+    )
