cdd70a888baf76ca1f7893ddf4bddb9e7fbb2776
Maher mohsen <73169639+maher-mohsen@users.noreply.github.com>
2023-05-26 14:05:54 +0300

    Adding testing function for log_softmax_.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -190,3 +190,28 @@ def test_paddle_asin(
         on_device=on_device,
         x=x[0],
     )
+    
+#log_softmax
+@handle_frontend_test(
+fn_tree="paddle.tensor.math.log_softmax",
+dtype_and_x=helpers.dtype_and_values(
+    available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_log_softmax(
+*,
+dtype_and_x,
+frontend,
+test_flags,
+fn_tree,
+on_device,
+):
+input_dtype, x = dtype_and_x
+helpers.test_frontend_function(
+    input_dtypes=input_dtype,
+    frontend=frontend,
+    test_flags=test_flags,
+    fn_tree=fn_tree,
+    on_device=on_device,
+    x=x[0],
+   )
