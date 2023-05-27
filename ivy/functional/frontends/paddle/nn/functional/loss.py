bccc5f89b9ff495d43a5d1e7b12da635c4d389cf
diksha-shrivastava13 <93143295+diksha-shrivastava13@users.noreply.github.com>
2023-05-27 20:13:25 +0530

    Updated loss.py

--- ivy/functional/frontends/paddle/nn/functional/loss.py
+++ ivy/functional/frontends/paddle/nn/functional/loss.py
@@ -1,6 +1,7 @@
 # local
 import ivy
 from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+from ivy.func_wrapper import with_unsupported_dtypes
 
 
 def _get_reduction_func(reduction):
