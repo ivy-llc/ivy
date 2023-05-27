dc99442558e661cd4c885737f513d2bdc1fe9b6a
Barichello <luizgubarichello@gmail.com>
2023-05-27 16:58:27 -0300

    softplus commit

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,5 +1,10 @@
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
+import ivy
 
 
 tanh = paddle_tanh
+
+
+def softplus(x, beta=1, threshold=20):
+    return ivy.where(x * beta > threshold, x, ivy.log(1 + ivy.exp(x * beta)) / beta)
