4334086b0cc067a5b4176786ff61af18527feab5
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-05-27 17:29:53 +0530

    added binary_cross_entropy loss

--- ivy/functional/frontends/paddle/nn/functional/loss.py
+++ ivy/functional/frontends/paddle/nn/functional/loss.py
@@ -1,1 +1,28 @@
 # local
+import ivy
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+
+
+def _get_reduction_func(reduction):
+    if reduction == "none":
+        ret = lambda x: x
+    elif reduction == "mean":
+        ret = ivy.mean
+    elif reduction == "sum":
+        ret = ivy.sum
+    else:
+        raise ivy.utils.exceptions.IvyException(
+            "{} is not a valid value for reduction".format(reduction)
+        )
+    return ret
+
+
+@to_ivy_arrays_and_back
+def binary_cross_entropy(input, label, weight=None, reduction="mean", name=None):
+    reduction = _get_reduction_func(reduction)
+    result = ivy.binary_cross_entropy(label, input, epsilon=0.0)
+
+    if weight is not None:
+        result = ivy.multiply(weight, result)
+    result = reduction(result)
+    return result
