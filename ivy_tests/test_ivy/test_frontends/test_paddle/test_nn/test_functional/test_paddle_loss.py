e7ecb34731437007db55815f3559463f096c39ea
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-05-27 17:32:16 +0530

    added binary_cross_entropy loss test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_loss.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_loss.py
@@ -1,3 +1,60 @@
 # global
+from hypothesis import strategies as st
 
 # local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# binary_cross_entropy
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.binary_cross_entropy",
+    dtype_and_vals=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float32", "float64"),
+        num_arrays=3,
+        shared_dtype=True,
+        min_value=1.0013580322265625e-05,
+        max_value=1.0,
+        large_abs_safety_factor=2,
+        small_abs_safety_factor=2,
+        safety_factor_scale="linear",
+        allow_inf=False,
+        exclude_min=True,
+        exclude_max=True,
+        min_num_dims=1,
+        max_num_dims=1,
+        min_dim_size=2,
+        shape=(5,),
+    ),
+    name=st.none(),
+    reduction=st.sampled_from(["mean", "none", "sum", None]),
+)
+def test_torch_binary_cross_entropy(
+    *,
+    dtype_and_vals,
+    reduction,
+    name,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_vals
+    pred_dtype, pred = input_dtype[0], x[0]
+    true_dtype, true = input_dtype[1], x[1]
+    weight_dtype, weight = input_dtype[2], x[2]
+
+    helpers.test_frontend_function(
+        input_dtypes=[pred_dtype, true_dtype, weight_dtype],
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=pred,
+        label=true,
+        weight=weight,
+        reduction=reduction,
+        name=name,
+        rtol=1e-02,
+        atol=1e-02,
+    )
