830ead7729697abe14bcf3064c41befc14229661
Rohan Dawkhar <86348193+sachelsout@users.noreply.github.com>
2023-05-27 10:52:44 +0000

    Added gelu activation function and test to PaddlePaddle frontend

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,5 +1,15 @@
+# global
+import ivy
+from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
+from ivy.functional.frontends.paddle.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
-
-
 tanh = paddle_tanh
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def gelu(x, approximate=False, name=None):
+    return ivy.gelu(x, approximate=approximate)
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
@@ -1,3 +1,33 @@
 # global
+from hypothesis import strategies as st
 
 # local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+# gelu
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.gelu",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_paddle_gelu(
+    *,
+    dtype_and_x,
+    approximate,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        approximate=approximate,
+    )
