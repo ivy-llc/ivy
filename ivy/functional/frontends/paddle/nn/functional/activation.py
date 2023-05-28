5f2409904f30ad37b50528cb361c978bfaa6f59d
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-05-28 15:17:32 +0300

    Closes #15977

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -8,3 +8,17 @@ from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 @to_ivy_arrays_and_back
 def tanh(x, name=None):
     return ivy.tanh(x)
+
+
+@with_unsupported_dtypes(
+    {"2.4.2 and below": ("bfloat16", "uint32", "uint16", "uint64")}, "paddle"
+)
+@to_ivy_arrays_and_back
+def selu(
+    x,
+    alpha=1.6732632423543772848170429916717,
+    scale=1.0507009873554804934193349852946,
+    name=None,
+):
+    mask = (x > 0).astype(float)
+    return scale * ((x * mask) + (alpha * (ivy.exp(x) - 1) * (1 - mask)))
