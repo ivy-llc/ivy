5d61f4b5daaf4490beeef6678ce41f06b07dd652
metric-space <functor.soup@gmail.com>
2023-05-27 23:23:47 -0400

    Add 'native_sparse_array' and 'native_sparse_array_to_indices_values_and_shape' for paddle backend

--- ivy/functional/backends/paddle/experimental/sparse_array.py
+++ ivy/functional/backends/paddle/experimental/sparse_array.py
@@ -1,4 +1,9 @@
-from ivy.utils.exceptions import IvyNotImplementedException
+import ivy
+from ivy.functional.ivy.experimental.sparse_array import (
+    _verify_coo_components,
+    _verify_csr_components,
+    _is_data_not_indices_values_and_shape,
+)
 import paddle
 
 
@@ -17,9 +22,70 @@ def native_sparse_array(
     values=None,
     dense_shape=None,
     format="coo",
-):
-    raise IvyNotImplementedException()
+    device=None,
+    requires_grad=False,
+    dtype=None,
+) -> paddle.Tensor:
+    format = format.lower()
+
+    if format not in ["coo", "csr"]:
+        raise ivy.utils.exceptions.IvyException(
+            "paddle only supports 'coo' and 'csr' sparse formats."
+        )
+
+    if _is_data_not_indices_values_and_shape(
+        data,
+        coo_indices,
+        crow_indices,
+        col_indices,
+        ccol_indices,
+        row_indices,
+        values,
+        dense_shape,
+    ):
+        ivy.utils.assertions.check_true(
+            ivy.is_native_sparse_array(data), message="not a sparse array"
+        )
+        return data
+
+    if format == "coo":
+        _verify_coo_components(
+            indices=coo_indices, values=values, dense_shape=dense_shape
+        )
+        return paddle.sparse.sparse_coo_tensor(
+            indices=coo_indices,
+            values=values,
+            shape=dense_shape,
+            dtype=dtype,
+            place=device,
+            stop_gradient=not requires_grad,
+        )
+    else:
+        _verify_csr_components(
+            crow_indices=crow_indices,
+            col_indices=col_indices,
+            values=values,
+            dense_shape=dense_shape,
+        )
+        return paddle.sparse.sparse_csr_tensor(
+            crows=crow_indices,
+            cols=col_indices,
+            values=values,
+            shape=dense_shape,
+            dtype=dtype,
+            place=device,
+            stop_gradient=not requires_grad,
+        )
 
 
 def native_sparse_array_to_indices_values_and_shape(x):
-    raise IvyNotImplementedException()
+    if not is_native_sparse_array(x):
+        raise ivy.utils.exceptions.IvyException("not a Paddle Sparse Array")
+    if x.is_sparse_coo():
+        return {"coo_indices": x.indices()}, x.values(), x.shape
+    else:
+        return (
+            {"crow_indices": x.crows(), "col_indices": x.cols()},
+            x.values(),
+            x.shape,
+        )
