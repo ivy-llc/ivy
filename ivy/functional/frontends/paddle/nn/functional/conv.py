2dc2a543984d77c81069d04fadcbc23feebaa3b5
shapeetha <129947783+shapeetha@users.noreply.github.com>
2023-05-28 17:37:36 +0100

    Update conv.py

--- ivy/functional/frontends/paddle/nn/functional/conv.py
+++ ivy/functional/frontends/paddle/nn/functional/conv.py
@@ -1,5 +1,6 @@
 import ivy
-# in ivy/functional/frontends/tensorflow/nn.py
+from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
+
 @to_ivy_arrays_and_back
 def conv2D(input, weight, bias, stride, padding, dilation, groups):
   return pdf.conv2D(weight, bias=bias, stride=stride, padding=padding, dilation=dilation, groups=groups)
