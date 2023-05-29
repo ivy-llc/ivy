d3cd70a0baec4aed44fce452ec5c0ce84f0f6b25
Maher mohsen <73169639+maher-mohsen@users.noreply.github.com>
2023-05-29 14:41:39 +0300

    Fixing formatting

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,6 +1,8 @@
 # local
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
-from ivy.functional.frontends.paddle.tensor.math import log_softmax as paddle_log_softmax
+from ivy.functional.frontends.paddle.tensor.math import (
+    log_softmax as paddle_log_softmax,
+ )
 
 
 tanh = paddle_tanh
