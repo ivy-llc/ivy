863a5ae5067591a3ed6981d6cb11eb4526904d2e
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-05-29 13:35:41 +0530

    updated loss

--- ivy/functional/frontends/paddle/nn/functional/loss.py
+++ ivy/functional/frontends/paddle/nn/functional/loss.py
@@ -21,11 +21,16 @@ def _get_reduction_func(reduction):
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
 def binary_cross_entropy(input, label, weight=None, reduction="mean", name=None):
-    reduction = _get_reduction_func(reduction)
-    output_array = ivy.zeros((1,))
-    result = ivy.binary_cross_entropy(label, input, epsilon=0.0, out=output_array)
+    reduction_fn = _get_reduction_func(reduction)
+    result = ivy.binary_cross_entropy(label, input, epsilon=0.0)
 
     if weight is not None:
         result = ivy.multiply(weight, result)
-    result = reduction(result)
+
+    if reduction in ["sum", "mean"]:
+        result = reduction_fn(result).reshape(
+            1,
+        )
+    else:
+        result = reduction_fn(result)
     return result
