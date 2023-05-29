fcf10f952f09a76eb3f946e08265fea65cc6a60e
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-05-29 18:42:27 +0300

    Changes to func + test

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,8 +1,34 @@
 # local
+import ivy
+from ivy.utils.exceptions import handle_exceptions
+from ivy.func_wrapper import with_unsupported_dtypes
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
 from ivy.functional.frontends.paddle.tensor.math import (
     log_softmax as paddle_log_softmax,
 )
+
+
+@with_unsupported_dtypes(
+    {"2.4.2 and below": ("bfloat16", "uint32", "uint16", "uint64")}, "paddle"
+)
+@to_ivy_arrays_and_back
+@handle_exceptions
+def selu(
+    x,
+    /,
+    *,
+    alpha=1.6732632423543772848170429916717,
+    scale=1.0507009873554804934193349852946,
+    name=None,
+):
+    if scale <= 1.0:
+        raise ValueError(f"The scale must be greater than 1.0. Received: {scale}.")
+
+    if alpha < 0:
+        raise ValueError(f"The alpha must be no less than zero. Received: {alpha}.")
+    mask = (x > 0).astype(float)
+    return scale * ((x * mask) + (alpha * (ivy.exp(x) - 1) * (1 - mask)))
 
 
 tanh = paddle_tanh
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
@@ -13,10 +13,14 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
         safety_factor_scale="log",
         small_abs_safety_factor=20,
     ),
+    scale=helpers.ints(min_value=1, max_value=10),
+    alpha=helpers.ints(min_value=1, max_value=10),
 )
 def test_paddle_selu(
     *,
     dtype_and_x,
+    scale,
+    alpha,
     on_device,
     fn_tree,
     frontend,
@@ -30,4 +34,6 @@ def test_paddle_selu(
         fn_tree=fn_tree,
         on_device=on_device,
         x=x[0],
+        alpha=alpha,
+        scale=scale,
     )
