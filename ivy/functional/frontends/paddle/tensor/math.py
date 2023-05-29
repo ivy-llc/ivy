7a6565dd5b62da3923d1274046eec5fc5f34cd2e
NripeshN <nn2012@hw.ac.uk>
2023-05-29 05:55:46 +0400

    Add function paddle frontend math

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -64,3 +64,9 @@ def divide(x, y, name=None):
 @to_ivy_arrays_and_back
 def multiply(x, y, name=None):
     return ivy.multiply(x, y)
+
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def add(x, y, name=None):
+    return ivy.add(x, y)
