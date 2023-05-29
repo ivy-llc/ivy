0ffbbc98f9910bec8ffce54d756ff2584f867163
a.ayad <3bdullah3yad@gmail.com>
2023-05-29 23:05:58 +0300

    v1

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -919,6 +919,10 @@ class Tensor:
 
     def addr(self, vec1, vec2, *, beta=1, alpha=1, out=None):
         return torch_frontend.addr(self, vec1, vec2, beta=beta, alpha=alpha, out=out)
+    
+    def addr_(self):
+        self.ivy_array = self.addr().ivy_array
+        return self
 
     # Special Methods #
     # -------------------#
