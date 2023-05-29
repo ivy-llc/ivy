4234dd4ec7c93c0779eca9e04c4747541e66aae0
AlexandraPosta <posta_alex222@yahoo.com>
2023-05-29 17:38:04 +0100

    Init commit on hardswish

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,9 +1,19 @@
 # local
+import ivy
+from ivy.func_wrapper import with_supported_dtypes
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
 from ivy.functional.frontends.paddle.tensor.math import (
     log_softmax as paddle_log_softmax,
 )
-
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 
 tanh = paddle_tanh
 log_softmax = paddle_log_softmax
+
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def hardswish(x, name=None):
+    relu6_val = ivy.relu6(ivy.add(input, 3))
+    ret = ivy.multiply(input, ivy.divide(relu6_val, 6))
+    return ret
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
@@ -1,3 +1,34 @@
 # global
 
+
 # local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# hardswish
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.hardswish",
+    dtype_and_input=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        safety_factor_scale="log",
+    ),
+    # test_with_out=st.just(False),
+)
+def test_paddle_hardswish(
+    *,
+    dtype_and_input,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_input
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
