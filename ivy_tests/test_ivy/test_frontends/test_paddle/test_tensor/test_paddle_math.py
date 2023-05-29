81310fc5b870841b0b0356d1f2e67988008723d1
Maher mohsen <73169639+maher-mohsen@users.noreply.github.com>
2023-05-29 14:35:22 +0300

    Fixing formatting

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -195,16 +195,14 @@ def test_paddle_asin(
 # log_softmax
 @handle_frontend_test(
     fn_tree="paddle.tensor.math.log_softmax",
+    aliases=["paddle.log_softmax", "paddle.nn.functional.log_softmax"],
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
+        available_dtypes=helpers.get_dtypes("float"),
     ),
 )
 def test_paddle_log_softmax(
     *,
     dtype_and_x,
-    frontend,
-    test_flags,
-    fn_tree,
     on_device,
     fn_tree,
     frontend,
@@ -217,6 +215,7 @@ def test_paddle_log_softmax(
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
+        atol=1e-2,
         x=x[0],
     )
 
