53653066cf5252c87eaaba0882de0d1ac61851a7
Rishabh Kumar <rishabhkkumarsc1@gmail.com>
2023-05-30 23:46:02 +0530

    experiments

--- docker/multiversion_framework_directory.py
+++ docker/multiversion_framework_directory.py
@@ -46,14 +46,13 @@ def install_pkg(path, pkg, base="fw/"):
 
 
 def install_deps(pkgs, path_to_json, base="/opt/fw/"):
-    print(pkgs)
-    print(path_to_json)
     for fw in pkgs:
         fw, ver = fw.split("/")
         path = base + fw + "/" + ver
         # check to see if this pkg has specific version dependencies
         with open(path_to_json, "r") as file:
             json_data = json.load(file)
+            print(json_data)
             for keys in json_data[fw]:
                 # check if key is dict
                 if isinstance(keys, dict):
