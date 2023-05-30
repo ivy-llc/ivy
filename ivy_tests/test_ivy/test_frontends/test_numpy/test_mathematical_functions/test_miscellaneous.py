5b3b959cc9fc07ba439c6c8fe05db0d9f0a33965
akshatvishu <akshatnayak197@gmail.com>
2023-05-30 13:19:31 +0000

    fixed disparity between native lcm and ivy's frontend implementations

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_miscellaneous.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_miscellaneous.py
@@ -682,7 +682,7 @@ def test_numpy_copysign(
 @handle_frontend_test(
     fn_tree="numpy.lcm",
     dtype_and_inputs=helpers.dtype_and_values(
-        available_dtypes=["int16", "int32", "int64"],
+        available_dtypes=helpers.get_dtypes("integer"),
         num_arrays=2,
         shared_dtype=False,
         min_num_dims=1,
