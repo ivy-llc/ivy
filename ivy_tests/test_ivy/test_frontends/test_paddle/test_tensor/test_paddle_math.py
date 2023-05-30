f54a72d1c4cfb8b461269e60f84be3c2bbe06074
AlexandraPosta <posta_alex222@yahoo.com>
2023-05-30 21:34:28 +0100

    create pow

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -506,3 +506,33 @@ def test_paddle_ceil(
         on_device=on_device,
         x=x[0],
     )
+
+
+# pow
+@handle_frontend_test(
+    fn_tree="paddle.pow",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        allow_inf=False,
+        shared_dtype=True
+    ),
+)
+def test_paddle_pow(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
