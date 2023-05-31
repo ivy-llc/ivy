5633c8818b368b17da18418b88b0991aa1a6ce3b
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-05-31 06:57:57 +0000

    Small fixes

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -1,6 +1,5 @@
 # local
 import ivy
-from ivy.utils.exceptions import handle_exceptions
 from ivy.func_wrapper import with_supported_dtypes
 from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 from ivy.functional.frontends.paddle.tensor.math import tanh as paddle_tanh
@@ -9,42 +8,8 @@ from ivy.functional.frontends.paddle.tensor.math import (
 )
 
 
-def _batch_promotion(*args, default_dtype="float64"):
-    # Promote all types
-    promote_types = set()
-
-    for arg in args:
-        if args is None:
-            continue
-        if isinstance(arg, (float, int)):
-            continue
-        promote_types.add(ivy.dtype(arg))
-
-    if "float64" in promote_types:
-        return "float64"
-
-    if "float32" in promote_types:
-        return "float32"
-
-    if "float16" in promote_types:
-        return "float32" if "bfloat16" in promote_types else "float16"
-
-    if "bfloat16" in promote_types:
-        return "bfloat16"
-
-    if "int64" in promote_types or "uint64" in promote_types:
-        return "float64"
-
-    ints = ["int8", "int16", "int32"]
-    if "uint32" in promote_types and any(d in promote_types for d in ints):
-        return "float64"
-
-    return default_dtype
-
-
 @with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
-@handle_exceptions
 def selu(
     x,
     /,
@@ -60,9 +25,9 @@ def selu(
         raise ValueError(f"The alpha must be no less than zero. Received: {alpha}.")
 
     ret = ivy.where(x > 0, x, alpha * ivy.expm1(x))
-    dtype = _batch_promotion(x, alpha, default_dtype="float64")
-    arr = ivy.asarray(ret, dtype=dtype)
-    return scale * arr
+    dtype = ivy.dtype(x)
+    arr = scale * ret
+    return ivy.asarray(arr, dtype=dtype)
 
 
 tanh = paddle_tanh
