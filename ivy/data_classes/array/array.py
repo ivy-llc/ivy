13dd44c9aea0d0772065e99572f4cd18158ca2f6
Matt <matthew-barrett@hotmail.co.uk>
2023-06-01 15:42:05 +0100

    avoiding having too much logic in ivy.Array.__init__ and leave computing of attributes to when they are actually called- speeds up to_ivy wrapping

--- ivy/data_classes/array/array.py
+++ ivy/data_classes/array/array.py
@@ -148,19 +148,14 @@ class Array(
             raise ivy.utils.exceptions.IvyException(
                 "data must be ivy array, native array or ndarray"
             )
-        self._size = (
-            functools.reduce(mul, self._data.shape) if len(self._data.shape) > 0 else 0
-        )
-        self._itemsize = ivy.itemsize(self._data)
-        self._strides = ivy.strides(self._data)
-        self._dtype = ivy.dtype(self._data)
-        self._device = ivy.dev(self._data)
-        self._dev_str = ivy.as_ivy_dev(self._device)
-        self._pre_repr = "ivy.array"
-        if "gpu" in self._dev_str:
-            self._post_repr = ", dev={})".format(self._dev_str)
-        else:
-            self._post_repr = ")"
+        self._size = None
+        self._strides = None
+        self._itemsize = None
+        self._dtype = None
+        self._device = None
+        self._dev_str = None
+        self._pre_repr = None
+        self._post_repr = None
         self.backend = ivy.current_backend_str()
         if dynamic_backend is not None:
             self._dynamic_backend = dynamic_backend
@@ -218,11 +213,15 @@ class Array(
     @property
     def dtype(self) -> ivy.Dtype:
         """Data type of the array elements."""
+        if self._dtype is None:
+            self._dtype = ivy.dtype(self._data)
         return self._dtype
 
     @property
     def device(self) -> ivy.Device:
         """Hardware device the array data resides on."""
+        if self._device is None:
+            self._device = ivy.dev(self._data)
         return self._device
 
     @property
@@ -254,16 +253,22 @@ class Array(
     @property
     def size(self) -> Optional[int]:
         """Number of elements in the array."""
+        if self._size is None:
+            self._size = functools.reduce(mul, self._data.shape) if len(self._data.shape) > 0 else 0
         return self._size
 
     @property
     def itemsize(self) -> Optional[int]:
         """Size of array elements in bytes."""
+        if self._itemsize is None:
+            self._itemsize = ivy.itemsize(self._data)
         return self._itemsize
 
     @property
     def strides(self) -> Optional[int]:
         """Get strides across each dimension."""
+        if self._strides is None:
+            self._strides = ivy.strides(self._data)
         return self._strides
 
     @property
@@ -344,6 +349,13 @@ class Array(
         return ivy
 
     def __repr__(self):
+        if self._dev_str is None:
+            self._dev_str = ivy.as_ivy_dev(self.device)
+            self._pre_repr = "ivy.array"
+            if "gpu" in self._dev_str:
+                self._post_repr = ", dev={})".format(self._dev_str)
+            else:
+                self._post_repr = ")"
         sig_fig = ivy.array_significant_figures()
         dec_vals = ivy.array_decimal_values()
         if self.backend == "" or ivy.is_local():
@@ -354,7 +366,7 @@ class Array(
             # from the currently set backend
             backend = ivy.with_backend(self.backend, cached=True)
         arr_np = backend.to_numpy(self._data)
-        rep = ivy.vec_sig_fig(arr_np, sig_fig) if self._size > 0 else np.array(arr_np)
+        rep = ivy.vec_sig_fig(arr_np, sig_fig) if self.size > 0 else np.array(arr_np)
         with np.printoptions(precision=dec_vals):
             repr = rep.__repr__()[:-1].partition(", dtype")[0].partition(", dev")[0]
             return (
