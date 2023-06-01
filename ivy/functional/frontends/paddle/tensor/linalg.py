8eb86ca08b4a69d408f89b9ef5513d00eabd48e6
Wachira Benard Kimani <82706283+benkimz@users.noreply.github.com>
2023-06-01 14:29:07 +0000

    dot function for paddle.tensor.linalg

--- ivy/functional/frontends/paddle/tensor/linalg.py
+++ ivy/functional/frontends/paddle/tensor/linalg.py
@@ -14,7 +14,12 @@ def matmul(x, y, transpose_x=False, transpose_y=False, name=None):
     x, y = promote_types_of_paddle_inputs(x, y)
     return ivy.matmul(x, y, transpose_a=transpose_x, transpose_b=transpose_y)
 
-
+# dot
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def dot(a, b):
+    return ivy.multiply(a, b)
+    
 # norm
 @with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
