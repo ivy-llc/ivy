ba32f26d0f9dd86292c4c88ae22bb651aeccf5ff
Kenfack Anafack Alex Bruno <kenfack_bruno@outlook.fr>
2023-06-02 09:53:49 +0100

    added exp instance method to paddle.tensor.tensor.Tensor. class

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -101,7 +101,11 @@ class Tensor:
     @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def asinh(self, name=None):
         return ivy.asinh(self._ivy_array)
-      
+
     @with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
     def asin(self, name=None):
         return ivy.asin(self._ivy_array)
+
+    @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+    def exp(self, name=None):
+        return ivy.exp(self._ivy_array)
