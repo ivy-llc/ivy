49bc3c46d1e9d7184ac91b78df897dbad24f9486
shapeetha <129947783+shapeetha@users.noreply.github.com>
2023-06-02 11:13:08 +0100

    Update conv.py

--- ivy/functional/frontends/paddle/nn/functional/conv.py
+++ ivy/functional/frontends/paddle/nn/functional/conv.py
@@ -3,7 +3,15 @@ import ivy
 from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
-def conv2D(input, filters, stride=1, padding=0, data_format=None, dilations=None, out=None):
+def conv2D(
+    input,
+    filters,
+    stride=1,
+    padding=0,
+    data_format=None,
+    dilations=None,
+    out=None,
+):
     return ivy.conv2D(
         input,
         filters,
@@ -11,5 +19,5 @@ def conv2D(input, filters, stride=1, padding=0, data_format=None, dilations=None
         padding=padding,
         data_format=data_format,
         dilations=dilations,
-        out=out
+        out=out,
     )
