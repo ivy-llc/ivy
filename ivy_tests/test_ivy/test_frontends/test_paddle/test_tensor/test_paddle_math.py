2847eae9cd6254de416c0949af8f91b310806332
a1701s <133611303+a1701s@users.noreply.github.com>
2023-06-02 00:35:21 -0400

    added less_than

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -506,3 +506,33 @@ def test_paddle_ceil(
         on_device=on_device,
         x=x[0],
     )
+
+    
+# less_than
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.less_than",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+)
+def test_paddle_less_than(
+    *,
+    dtype_and_x,
+    on_device,
+    frontend,
+    fn_tree,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
+
