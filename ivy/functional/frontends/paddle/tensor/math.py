4bf60ae0ef12f51575bdec86d14d242f689639ac
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-06-02 19:15:05 +0100

    add back the decorators that were applied to multiply func

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -72,6 +72,8 @@ def abs(x, name=None):
     return ivy.abs(x)
 
 
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
 def multiply(x, y, name=None):
     return ivy.multiply(x, y)
 
