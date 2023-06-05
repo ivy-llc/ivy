5ac7aa63e254047b206d333854a5e8f6da27178d
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-06-05 19:35:09 +0000

    Removed supported dtypes decorator

--- ivy/functional/frontends/paddle/tensor/attribute.py
+++ ivy/functional/frontends/paddle/tensor/attribute.py
@@ -3,20 +3,8 @@ import ivy
 from ivy.functional.frontends.paddle.func_wrapper import (
     to_ivy_arrays_and_back,
 )
-from ivy.func_wrapper import (
-    with_supported_dtypes,
-)
 
 
-@with_supported_dtypes(
-    {
-        "2.4.2 and below": (
-            "complex64",
-            "complex128",
-        )
-    },
-    "paddle",
-)
 @to_ivy_arrays_and_back
 def is_complex(x):
     return ivy.is_complex_dtype(x)
