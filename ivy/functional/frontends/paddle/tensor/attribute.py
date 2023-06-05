05c52b12f733c9700c8b86586096e63945a3b496
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-06-05 17:36:30 +0000

    Shortened the supported dtypes

--- ivy/functional/frontends/paddle/tensor/attribute.py
+++ ivy/functional/frontends/paddle/tensor/attribute.py
@@ -11,19 +11,6 @@ from ivy.func_wrapper import (
 @with_supported_dtypes(
     {
         "2.4.2 and below": (
-            "bool",
-            "int8",
-            "int16",
-            "int32",
-            "int64",
-            "uint8",
-            "uint16",
-            "uint32",
-            "uint64",
-            "bfloat16",
-            "float16",
-            "float32",
-            "float64",
             "complex64",
             "complex128",
         )
