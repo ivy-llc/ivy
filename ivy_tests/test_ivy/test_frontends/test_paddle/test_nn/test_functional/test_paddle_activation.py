ba6d35cedd326c95afabd4fc3c6f790ff345af64
Rohan Dawkhar <86348193+sachelsout@users.noreply.github.com>
2023-06-06 20:40:23 +0000

    added relu_ activation function and test for PaddlePaddle frontend

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_activation.py
@@ -246,3 +246,26 @@ def test_paddle_softsign(
         on_device=on_device,
         x=x[0],
     )
+
+
+# relu_
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.relu_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_relu_(
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        x=x[0],
+    )
