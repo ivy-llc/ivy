8cd58bee1255bf7ac22fa3dd814fac6b897bfb7e
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-06-06 18:05:34 +0000

    available dtypes to valid in the test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_attribute.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_attribute.py
@@ -8,7 +8,7 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 @handle_frontend_test(
     fn_tree="paddle.tensor.attribute.is_complex",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=helpers.get_dtypes("valid"),
     ),
 )
 def test_paddle_is_complex(
