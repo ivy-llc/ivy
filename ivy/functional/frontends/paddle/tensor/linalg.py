10be4e10b9cebf2e04a3e99969627cf372696c67
Wachira Benard Kimani <82706283+benkimz@users.noreply.github.com>
2023-06-06 06:02:26 +0000

    dot function for paddle.tensor.linalg

--- ivy/functional/frontends/paddle/tensor/linalg.py
+++ ivy/functional/frontends/paddle/tensor/linalg.py
@@ -123,7 +123,7 @@ def cholesky(x, /, *, upper=False, name=None):
 
 
 # dot
-@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64", "int32", "int64")}, "paddle")
 @to_ivy_arrays_and_back
 def dot(x, y, /, *, axis=-1, name=None):
     x, y = promote_types_of_paddle_inputs(x, y)
