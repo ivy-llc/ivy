b60c65fe9f98b611a3df46c70f6c31f1cb9b1782
Paula Ehab Alfy <42894502+paulaehab@users.noreply.github.com>
2023-06-06 22:14:29 +0000

    edit available_dtypes

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2229,7 +2229,7 @@ def test_torch_signbit(
 @handle_frontend_test(
     fn_tree="torch.angle",
     dtype_and_input=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
+        available_dtypes=["float64", "complex64", "complex128"],
     ),
 )
 def test_torch_angle(
