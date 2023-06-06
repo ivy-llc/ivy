93f174275b9a0717aec1be6cb45160f0701f61b5
Haris Mahmood <70361308+hmahmood24@users.noreply.github.com>
2023-06-06 08:36:11 +0000

    Modify tf.range frontend function such that it can take positional and keyword arguments just like the native function

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -211,7 +211,7 @@ def ensure_shape(x, shape, name=None):
 @with_unsupported_dtypes({"2.12.0 and below": ("float16", "bfloat16")}, "tensorflow")
 @handle_tf_dtype
 @to_ivy_arrays_and_back
-def range(start, limit=None, delta=1, /, *, dtype=None, name=None):
+def range(start, limit=None, delta=1, dtype=None, name=None):
     return ivy.arange(start, limit, delta, dtype=dtype)
 
 
