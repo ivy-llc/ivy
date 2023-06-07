56c521a9469c188969c67663157331164fb3da2e
Mohab Mohamed Ali <mohabsir00@gmail.com>
2023-06-07 21:11:27 +0300

    Added numel function to paddle frontend

--- ivy/functional/frontends/paddle/tensor/stat.py
+++ ivy/functional/frontends/paddle/tensor/stat.py
@@ -10,3 +10,9 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 @to_ivy_arrays_and_back
 def mean(input, axis=None, keepdims=False, out=None):
     return ivy.std(input, axis=axis, keepdims=keepdims, out=None)
+
+
+@with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def numel(input, name=None):
+    return ivy.prod(ivy.array(input.size), ivy.int64)
