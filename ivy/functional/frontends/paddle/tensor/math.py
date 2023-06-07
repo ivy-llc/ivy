6d5c13e2c5ade472c410a23cbf06dd2ed03bfa65
Muhammad Ishaque Nizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-06-07 14:02:15 +0500

    ENH: sinh added from todo list #15039 (#16166)
    Co-authored-by: @AnnaTz

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -116,6 +116,12 @@ def ceil(x, name=None):
     return ivy.ceil(x)
 
 
+@with_supported_dtypes({"2.4.2 and below": ("float16", "float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def sinh(x, name=None):
+    return ivy.sinh(x)
+
+
 @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
 @to_ivy_arrays_and_back
 def pow(x, y, name=None):
