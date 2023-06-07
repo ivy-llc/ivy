dc09eee31efdce9a672b0ffa1e405c90d8ccb92c
Meesum1799 <meesum.syed99@gmail.com>
2023-06-07 14:52:59 -0700

    Fixed liniting error

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1122,7 +1122,11 @@ class Tensor:
 
     def normal_(self, mean=0, std=1, *, generator=None):
         self.ivy_array = ivy.random_normal(
-            mean=mean, std=std, shape=self.shape, dtype=self.dtype, device=self.device
+            mean=mean,
+            std=std,
+            shape=self.shape,
+            dtype=self.dtype,
+            device=self.device
         )
         return self
 
