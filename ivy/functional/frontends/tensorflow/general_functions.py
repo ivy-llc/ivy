0dc5be8315a7992b3c20652447533ee9acfcdbb7
Shocker444 <Donaldesset04@outlook.com>
2023-06-07 10:38:09 +0100

    Update

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -551,7 +551,7 @@ def while_loop(
     return ivy.while_loop(test_fn=cond, body_fn=body, vars=loop_vars)
 
 
-@with_unsupported_dtypes({"2.12.0 and below": ("int8", "int16")}, "tensorflow")
+@with_unsupported_dtypes({"2.12.0 and below": ("float16", "bfloat16")}, "tensorflow")
 @to_ivy_arrays_and_back
 def truncatediv(x, y, name=None):
     return x.trunc_divide(y)
