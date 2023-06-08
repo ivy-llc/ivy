a3b229a5dbd69c4ddf88221098181b42eded4d64
danielmunioz <47380745+danielmunioz@users.noreply.github.com>
2023-06-08 00:14:33 +0000

    Updating 'randint' to reflect '_check_bounds_and_get_shape' always returning 'ivy.Shape' objects

--- ivy/functional/backends/tensorflow/random.py
+++ ivy/functional/backends/tensorflow/random.py
@@ -141,7 +141,7 @@ def randint(
         dtype = ivy.default_int_dtype()
     dtype = ivy.as_native_dtype(dtype)
     _randint_check_dtype_and_bound(low, high, dtype)
-    shape = _check_bounds_and_get_shape(low, high, shape)
+    shape = _check_bounds_and_get_shape(low, high, shape).shape
     low = tf.cast(low, "float32")
     high = tf.cast(high, "float32")
     with tf.device(device):
