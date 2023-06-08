cabf1fca3b50fc7d97e0074517c7c23582e44a8b
usama baloch <usamabaloch152001@gmail.com>
2023-06-08 21:03:43 +0500

    updated inputs_to_ivy_arrays decorator

--- ivy/func_wrapper.py
+++ ivy/func_wrapper.py
@@ -454,6 +454,14 @@ def inputs_to_ivy_arrays(fn: Callable) -> Callable:
         -------
             The return of the function, with ivy arrays passed in the arguments.
         """
+        if not ivy.get_array_mode():
+            x = ivy.nested_map(args, lambda x: ivy.is_native_array(x))
+            x += tuple(ivy.nested_map(kwargs, lambda x: ivy.is_native_array(x)).values())
+            if any(x):
+                warnings.warn("In this case operators might cause inconsistent "
+                              "behavior when array_mode is set to False")
+            return fn(*args, **kwargs)
+
         has_out = False
         if "out" in kwargs:
             out = kwargs["out"]
