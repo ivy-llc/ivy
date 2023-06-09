d2bbe7966ee5eee634c50583ffb4b57e2f483b0c
bipinkrishnan <bipinkrishna.p@gmail.com>
2023-06-09 06:58:18 +0000

    Remove separate installation of torch scatter

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,3 @@
-pip install torch
-ver=$(pip show torch | grep Version | cut -d ' ' -f2)
-pip install torch-scatter -f https://data.pyg.org/whl/torch-$ver+cpu.html
 pip install -r requirements/requirements.txt
 if [[ $(arch) == 'arm64' ]]; then
       pip install -r requirements/optional_m1_1.txt
