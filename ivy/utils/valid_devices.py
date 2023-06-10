c8a7cc67cb9fc97d545c263798e63100430adb17
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-06-10 11:40:40 -0400

    Update valid_devices.py

--- ivy/utils/valid_devices.py
+++ ivy/utils/valid_devices.py
@@ -1,1 +1,58 @@
+import ivy.functional.backends.jax.__init__ as jx
+import ivy.functional.backends.mxnet.__init__ as mxn
+import ivy.functional.backends.numpy.__init__ as np
+import ivy.functional.backends.paddle.__init__ as pdl
+import ivy.functional.backends.tensorflow.__init__ as tf
+import ivy.functional.backends.torch.__init__ as torch
 
+
+def jax_valid_device(device: str, /):
+    if device not in jx.valid_devices:
+        if device in jx.invalid_devices:
+            return False, "{} is an invalid device for jax".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
+
+
+
+def mxnet_valid_device(device: str, /):
+    if device not in mxn.valid_devices:
+        if device in mxn.invalid_devices:
+            return False, "{} is an invalid device for mxnet".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
+
+
+def numpy_valid_device(device: str, /):
+    if device not in np.valid_devices:
+        if device in np.invalid_devices:
+            return False, "{} is an invalid device for numpy".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
+
+
+
+def paddle_valid_device(device: str, /):
+    if device not in pdl.valid_devices:
+        if device in pdl.invalid_devices:
+            return False, "{} is an invalid device for paddle".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
+
+
+
+def tensorflow_valid_device(device: str, /):
+    if device not in tf.valid_devices:
+        if device in tf.invalid_devices:
+            return False, "{} is an invalid device for tensorflow".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
+
+
+
+def torch_valid_device(device: str, /):
+    if device not in torch.valid_devices:
+        if device in torch.invalid_devices:
+            return False, "{} is an invalid device for pytorch".format(device)
+        return False, "{} is not a device".format(device)
+    return True, ""
