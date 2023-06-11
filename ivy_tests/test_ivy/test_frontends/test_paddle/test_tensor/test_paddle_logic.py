7cdbb4fbcc5628ecac97870e90ff23a6d8fcb9cc
DubeySandeep <dubeysandeep.in@gmail.com>
2023-06-11 06:32:53 +0530

    Adds test for frontend of paddle.tensor.logic.bitwise_not

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_logic.py
@@ -350,3 +350,28 @@ def test_paddle_bitwise_and(
         x=x[0],
         y=x[1],
     )
+
+
+# bitwise_not
+@handle_frontend_test(
+    fn_tree="paddle.bitwise_not",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+    test_with_out=st.just(True),
+)
+def test_paddle_bitwise_not(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
