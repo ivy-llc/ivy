852204b9d20ca4340f27b5c610b97cd10218af4d
Jagjeet Singh Sunghera <jagjeetsunghera12@gmail.com>
2023-06-12 11:15:03 +0100

    Added argsort implementation (#16572)

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -137,3 +137,7 @@ class Tensor:
     @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def log10(self, name=None):
         return ivy.log10(self._ivy_array)
+
+    @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
+    def argsort(self, axis=- 1, descending=False, name=None):
+        return ivy.argsort(self._ivy_array, axis=axis, descending=descending)
