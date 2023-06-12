ab74b4265257fe5d702b632875203f40dac91ad2
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-06-12 13:02:02 -0400

    Update __init__.py

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -8,6 +8,7 @@ import sys
 
 
 import ivy.utils.backend.handler
+import ivy.utils.valid_devices
 from ivy._version import __version__ as __version__
 
 _not_imported_backends = list(ivy.utils.backend.handler._backend_dict.keys())
@@ -86,6 +87,9 @@ class Device(str):
                     dev_str[4:].isnumeric(),
                     message="{} must be numeric".format(dev_str[4:]),
                 )
+            valid, msg = valid_devices.is_valid_device(dev_str):
+            if not valid:
+                raise ivy.utils.exceptions.IvyException(msg)
         return str.__new__(cls, dev_str)
 
 
