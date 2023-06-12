74c32242e18b1d2f9c7fdc417e1355c9cbd64f3c
Arun <109756689+ArunRo45@users.noreply.github.com>
2023-06-12 05:08:40 +0000

    added paddle.tensor.randint

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -1,1 +1,11 @@
 # global
+
+
+# local
+import ivy
+from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
+
+
+@to_ivy_arrays_and_back
+def randint(low=0, high=None, shape=[1], dtype=None, name=None):
+    return ivy.randint(low, high, shape=shape, dtype=dtype)
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_random.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_random.py
@@ -1,3 +1,48 @@
 # global
 
 # local
+import ivy
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# randint
+@handle_frontend_test(
+    fn_tree="paddle.tensor.randint",
+    low=helpers.ints(min_value=0, max_value=10),
+    high=helpers.ints(min_value=11, max_value=20),
+    dtype=helpers.get_dtypes("integer"),
+    shape=helpers.get_shape(),
+)
+def test_paddle_randint(
+    low,
+    high,
+    dtype,
+    frontend,
+    test_flags,
+    shape,
+    fn_tree,
+):
+    def call():
+        helpers.test_frontend_function(
+            input_dtypes=dtype,
+            frontend=frontend,
+            test_values=False,
+            fn_tree=fn_tree,
+            test_flags=test_flags,
+            low=low,
+            high=high,
+            shape=shape,
+        )
+
+    ret = call()
+
+    if not ivy.exists(ret):
+        return
+
+    ret_np, ret_from_np = ret
+    ret_np = helpers.flatten_and_to_np(ret=ret_np)
+    ret_from_np = helpers.flatten_and_to_np(ret=ret_from_np)
+    for u, v in zip(ret_np, ret_from_np):
+        assert u.dtype == v.dtype
+        assert u.shape == v.shape
