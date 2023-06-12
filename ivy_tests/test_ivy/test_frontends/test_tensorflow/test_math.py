5d7555a99969c9122a7b21d2fb33eb97f032c7ba
Aditya <adityaraj2019.sahu@gmail.com>
2023-06-12 18:15:40 +0530

    atanh test function updated

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -2075,8 +2075,7 @@ def test_tensorflow_atan(
 # atanh
 @handle_frontend_test(
     fn_tree="tensorflow.math.atanh",
-    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
-    test_with_out=st.just(False),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float"))
 )
 def test_tensorflow_atanh(
     *,
