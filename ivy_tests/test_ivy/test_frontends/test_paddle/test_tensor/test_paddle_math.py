9b4cf2dfc423168f2a1753789b1e1191e425e2cd
henrik syvertsson <henrik.syvertsson@gmail.com>
2023-06-13 17:52:47 +0200

    add paddle.tensor.math.pow
    actually, someone else has already added this function.
    But I at least change to get_dtypes("valid").
    Tests fail for this, but they passed for the old get_dtypes("float").

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -510,7 +510,7 @@ def test_paddle_sinh(
 @handle_frontend_test(
     fn_tree="paddle.pow",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
+        available_dtypes=helpers.get_dtypes("valid"),
         num_arrays=2,
         allow_inf=False,
         shared_dtype=True,
