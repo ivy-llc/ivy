3a3389a04bcfd7e089481e9fd80922c7d9304dd1
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-06-14 11:19:46 +0200

    Add optional_2.txt to install_dependencies.sh script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,4 +4,5 @@ if [[ $(arch) == 'arm64' ]]; then
       pip install -r requirements/optional_m1_2.txt
 else
     pip install -r requirements/optional.txt
+    pip install -r requirements/optional_2.txt
 fi
