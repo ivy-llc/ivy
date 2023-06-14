de70413ae7924e665b79ea1f6ad15e1d61c27bb6
Rohan Dawkhar <86348193+sachelsout@users.noreply.github.com>
2023-06-14 11:21:25 +0000

    added tanhshrink function and test for PaddlePaddle frontend

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -131,3 +131,14 @@ def celu(
     prod = alpha * (ivy.exp(x / alpha) - 1)
     ret = ivy.maximum(0, x) + ivy.minimum(0, prod)
     return ret
+
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def tanhshrink(
+    x,
+    /,
+    *,
+    name=None,
+):
+    return ivy.subtract(x, ivy.tanh(x))
