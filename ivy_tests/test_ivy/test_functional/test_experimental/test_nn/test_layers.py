50510ded416337105febbd9d825066b1c113bf6e
Muhammad Abdullah <69182153+abdulasiraj@users.noreply.github.com>
2023-06-14 12:42:44 +0500

    Revert "adding max pool 3d in paddle layers"

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
@@ -106,7 +106,6 @@ def test_max_pool3d(
     test_flags,
     backend_fw,
     fn_name,
-    on_device,
 ):
     dtype, x, kernel, stride, pad = x_k_s_p
     helpers.test_function(
@@ -114,7 +113,6 @@ def test_max_pool3d(
         input_dtypes=dtype,
         test_flags=test_flags,
         fw=backend_fw,
-        on_device=on_device,
         fn_name=fn_name,
         rtol_=1e-2,
         atol_=1e-2,
