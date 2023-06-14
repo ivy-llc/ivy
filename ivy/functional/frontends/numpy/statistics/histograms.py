96a3d2865253e671e852d1cd49072a03cbf4be85
Rohit Singh <RohitSinghSays21@gmail.com>
2023-06-14 21:00:15 +0530

    After Checks Failed

--- ivy/functional/frontends/numpy/statistics/histograms.py
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -1,7 +1,24 @@
 import ivy
-from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
+from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
+    from_zero_dim_arrays_to_scalar,
+)
 
 
 @to_ivy_arrays_and_back
-def histogram(a, bins=10, range=None, density=None, weights=None):
-    return ivy.histogram(a, bins, range, density, weights)
+@from_zero_dim_arrays_to_scalar
+def histogram(
+    a,
+    *,
+    bins=10,
+    range=None,
+    density=None,
+    weights=None,
+):
+    return ivy.histogram(
+        a,
+        bins=bins,
+        range=range,
+        density=density,
+        weights=weights,
+    )
