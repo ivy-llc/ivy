b6dc749d42056d5ed26662f3e044a17296bae653
JG <67888047+juliagsy@users.noreply.github.com>
2023-06-16 13:40:51 +0800

    fix decorators

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -160,7 +160,6 @@ class Tensor:
     def add_(self, name=None):
         return ivy.add(self._ivy_array)
 
-    with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
-
+    @with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
     def square(self, name=None):
         return ivy.square(self._ivy_array)
