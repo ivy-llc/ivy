bab3d93180b582cd0d54c11a534fb4e5c6341c41
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-06-16 10:25:23 +0500

    EHN: adiing isinf in paddle.tenisr.tesnor.tesnor

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -159,3 +159,10 @@ class Tensor:
     @with_supported_dtypes({"2.4.3 and below": ("float32", "float64")}, "paddle")
     def add_(self, name=None):
         return ivy.add(self._ivy_array)
+
+    @with_supported_dtypes(
+        {"2.4.2 and below": ("float16", "float32", "float64", "int32", "int64")},
+        "paddle",
+    )
+    def isinf(self, name=None):
+        return ivy.isinf(self._ivy_array)
