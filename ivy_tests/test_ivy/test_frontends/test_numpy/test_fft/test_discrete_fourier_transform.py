0c5591ea160e9f635a0741c534f019476a4d96b9
rajveer43 <rajveer.rathod1301@gmail.com>
2023-06-16 14:53:52 +0530

    changesss

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_fft/test_discrete_fourier_transform.py
@@ -194,7 +194,7 @@ def test_numpy_rfftfreq(n, sample_rate, frontend, test_flags, fn_tree, on_device
 @handle_frontend_test(
     fn_tree="numpy.fft.rfft2",
     dtype_input_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("valid"),
+        available_dtypes=helpers.get_dtypes("float_and_complex"),
         min_num_dims=2,
         max_num_dims=2,
         force_tuple_axis=True,
