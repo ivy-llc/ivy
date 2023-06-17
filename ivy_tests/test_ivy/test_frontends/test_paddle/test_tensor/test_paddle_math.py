0d4e6dc8268f565ad3eacc2b1c5befda762f02a8
Rohan Dawkhar <86348193+sachelsout@users.noreply.github.com>
2023-06-17 10:43:32 +0530

    Added tan function to paddle.tensor.math in the paddlepaddle frontend (#15931)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -695,3 +695,30 @@ def test_paddle_rad2deg(
         on_device=on_device,
         x=x[0],
     )
+
+
+# tan
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.tan",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_paddle_tan(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        atol=1e-2,
+        x=x[0],
+    )
