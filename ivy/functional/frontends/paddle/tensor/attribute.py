933ee5f2f6b55c80b696a6042dd0805b412d7607
Selvaraj <selvaskingdom@gmail.com>
2023-06-18 20:15:37 +0530

    real-paddle-frontend

--- ivy/functional/frontends/paddle/tensor/attribute.py
+++ ivy/functional/frontends/paddle/tensor/attribute.py
@@ -8,3 +8,8 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 @to_ivy_arrays_and_back
 def is_complex(x):
     return ivy.is_complex_dtype(x)
+
+
+@to_ivy_arrays_and_back
+def real(x):
+    return ivy.real(x)
