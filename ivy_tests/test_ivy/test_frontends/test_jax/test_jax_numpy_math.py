78b1979267694024a55453a43aeb96fa6561c61d
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-06-18 21:41:40 +0100

    use log safety scale instead of the linear in test_jax_numpy_fmod to avoid overflows in paddle backend

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy_math.py
@@ -1799,7 +1799,8 @@ def test_jax_numpy_fabs(
     dtype_and_inputs=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
         num_arrays=2,
-        large_abs_safety_factor=2,
+        large_abs_safety_factor=1.5,
+        safety_factor_scale="log",
     ),
     test_with_out=st.just(False),
 )
