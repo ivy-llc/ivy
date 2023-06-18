147d11120717a214eece1fd55604e72586969289
MelatKebedeAbraham <melatkebedeabraham@gmail.com>
2023-06-18 21:15:06 +0300

    Commit

--- ivy/functional/frontends/numpy/sorting_searching_counting/sorting.py
+++ ivy/functional/frontends/numpy/sorting_searching_counting/sorting.py
@@ -33,7 +33,3 @@ def sort_complex(a):
 @to_ivy_arrays_and_back
 def lexsort(keys, /, *, axis=-1):
     return ivy.lexsort(keys, axis=axis)
-
-@to_ivy_arrays_and_back
-def argpartition(a, kth, axis=-1, kind='introselect', order=None):
-    return ivy.argpartition(a, kth, axis=axis, kind=kind)
