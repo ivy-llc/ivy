3e6cf6cf1fad1548709e93f9e6374c9ea0757056
Akshay Venkataraman <akshayv2k@gmail.com>
2023-06-19 20:00:00 +0530

    added rfft paddle

--- ivy/functional/frontends/paddle/fft.py
+++ ivy/functional/frontends/paddle/fft.py
@@ -14,3 +14,13 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 def fft(x, n=None, axis=-1.0, norm="backward", name=None):
     ret = ivy.fft(ivy.astype(x, "complex128"), axis, norm=norm, n=n)
     return ivy.astype(ret, x.dtype)
+
+
+@with_supported_dtypes(
+    {"2.4.2 and below": ("float32", "float64")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def rfft(x, n=None, axis=-1.0, norm="backward", name=None):
+    ret = ivy.rfft(ivy.astype(x, "float64"), axis, norm=norm, n=n)
+    return ret
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_paddle_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_paddle_fft.py
@@ -42,3 +42,41 @@ def test_paddle_fft(
         axis=axis,
         norm=norm,
     )
+
+
+@handle_frontend_test(
+    fn_tree="paddle.fft.rfft",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("valid_real"),
+        min_value=-10,
+        max_value=10,
+        min_num_dims=1,
+        min_dim_size=2,
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+    n=st.one_of(
+        st.integers(min_value=2, max_value=10),
+        st.just(None),
+    ),
+    norm=st.sampled_from(["backward", "ortho"]),
+)
+def test_paddle_rfft(
+    dtype_x_axis,
+    n,
+    norm,
+    frontend,
+    test_flags,
+    fn_tree,
+):
+    input_dtypes, x, axis = dtype_x_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        x=x[0],
+        n=n,
+        axis=axis,
+        norm=norm,
+    )
