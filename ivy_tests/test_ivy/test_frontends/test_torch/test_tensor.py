5d3ece5734892f3f4a85926cf1e0beb3ef0d5099
Jakub Bober <jakubbober.work@gmail.com>
2023-06-21 23:17:58 +0200

    Implement the bitwise_not_ function and the corresponding test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -6993,6 +6993,40 @@ def test_torch_instance_atan2_(
     )
 
 
+# bitwise_not_
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="bitwise_not_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        num_arrays=2,
+    ),
+)
+def test_torch_instance_bitwise_not_(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        method_all_as_kwargs_np={},
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
 # bitwise_and_
 @handle_frontend_method(
     class_tree=CLASS_TREE,
