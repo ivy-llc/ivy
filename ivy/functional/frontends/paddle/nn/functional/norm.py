48ac34baba78b1536983b29ada977be65fc8e9ee
Rohan Saxena <rohan2000saxena@gmail.com>
2023-06-21 19:00:22 +0530

    Descriptive commit message

--- ivy/functional/frontends/paddle/nn/functional/norm.py
+++ ivy/functional/frontends/paddle/nn/functional/norm.py
@@ -1,1 +1,9 @@
 # local
+import paddle.fluid as fluid
+
+
+bn = fluid.layers.batch_norm
+
+input_data = fluid.data(name='input', shape=[None, 10], dtype='float32')
+output = bn(input_data, is_test=True)
+print(output)
