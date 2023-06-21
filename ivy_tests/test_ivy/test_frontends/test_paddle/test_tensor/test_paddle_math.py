12cde54cb2708aa24c1dd75a09b2893ca3a35230
baberabb <92168766+baberabb@users.noreply.github.com>
2023-06-21 14:31:23 +0100

    add paddle.tensor.math.exp frontend and test function (#17088)
    * add paddle.tensor.math.exp frontend and test function
    
    * added newline to fix lint issues

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -748,3 +748,29 @@ def test_paddle_tan(
         atol=1e-2,
         x=x[0],
     )
+
+
+# exp
+@handle_frontend_test(
+    fn_tree="paddle.exp",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_exp(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
