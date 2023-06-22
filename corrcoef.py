d9bfac081ee907ebc0ad053814f82097bcc55d7f
Robs AG <104177566+UnwillingCrab85@users.noreply.github.com>
2023-06-21 19:16:35 -0600

    Delete corrcoef.py

--- corrcoef.py
+++
@@ -1,9 +0,0 @@
-def corrcoef(x, y):
-    """
-    Args:
-        x (paddle.Tensor): The first variable.
-        y (paddle.Tensor): The second variable.
-    """
-    correlation_matrix = paddle.fluid.layers.correlation(x, y)
-    correlation_coefficient = correlation_matrix.numpy()[0, 1]
-    return correlation_coefficient
