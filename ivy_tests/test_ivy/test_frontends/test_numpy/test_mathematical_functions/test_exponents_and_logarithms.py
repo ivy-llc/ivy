af84dd6ea3b9dfe44d7498641d65a8cad5bc9e9c
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-06-22 18:45:54 +0000

    tol for exp2

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_exponents_and_logarithms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_mathematical_functions/test_exponents_and_logarithms.py
@@ -139,7 +139,7 @@ def test_numpy_exp2(
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        atol=1e-02,
+        rtol=1e-02,
         x=x[0],
         out=None,
         where=where,
