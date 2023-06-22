4b984781f9494cd4678e24f53d6160f70b17d728
Muhammad Saeed <mohammud.saeed.batekh@gmail.com>
2023-06-22 14:12:29 +0300

    Update test_paddle_math.py
    delete test_paddle_amax

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -800,29 +800,3 @@ def test_paddle_exp(
         on_device=on_device,
         x=x[0],
     )
-
-
-# amax
-@handle_frontend_test(
-    fn_tree="paddle.tensor.math.amax",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-    ),
-)
-def test_paddle_amax(
-    *,
-    dtype_and_x,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device,
-):
-    input_dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x=x[0],
-    )
