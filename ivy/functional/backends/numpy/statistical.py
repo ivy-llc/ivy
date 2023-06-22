2e25331b6632107bbe2881fb32496815132cb068
Robs AG <104177566+UnwillingCrab85@users.noreply.github.com>
2023-06-21 19:14:14 -0600

    Update statistical.py

--- ivy/functional/backends/numpy/statistical.py
+++ ivy/functional/backends/numpy/statistical.py
@@ -394,3 +394,13 @@ def igamma(
 
     igamma_vec = np.vectorize(igamma_cal)
     return igamma_vec(a, x)
+
+def corrcoef(x, y):
+    """
+    Args:
+        x (paddle.Tensor): The first variable.
+        y (paddle.Tensor): The second variable.
+    """
+    correlation_matrix = paddle.fluid.layers.correlation(x, y)
+    correlation_coefficient = correlation_matrix.numpy()[0, 1]
+    return correlation_coefficient
