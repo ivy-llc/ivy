2cfdd5dd7922e70cb603949040738242260233fb
Vik <victorch9@gmail.com>
2023-06-23 12:39:09 +0000

    Added tests for jax_devicearray_prod

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_devicearray.py
@@ -445,6 +445,46 @@ def test_jax_devicearray_sort(
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="jax.numpy.array",
+    method_name="prod",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        force_int_axis=True,
+        min_axis=-1,
+        max_axis=-1,
+        min_dim_size=2,
+        max_dim_size=100,
+        min_num_dims=2,
+    ),
+)
+def test_jax_devicearray_prod(
+    dtype_x_axis,
+    on_device,
+    frontend,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+):
+    input_dtype, x, axis = dtype_x_axis
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "object": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "axis": axis,
+        },
+        frontend=frontend,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        on_device=on_device,
+    )
+
+
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="jax.numpy.array",
     method_name="__pos__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("valid"),
