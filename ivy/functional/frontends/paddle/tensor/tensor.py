a46a85d4c1755506bf0a94c6a4a877773634622d
Akshay Venkataraman <akshayv2k@gmail.com>
2023-06-23 12:48:06 +0530

    max function

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -208,14 +208,15 @@ class Tensor:
     @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def sort(self, axis=-1, descending=False, name=None):
         return ivy.sort(self._ivy_array, axis=axis, descending=descending)
-
     
     @with_supported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def any(self, axis=None, keepdim=False, name=None):
         return ivy.any(self._ivy_array, axis=axis, keepdims=keepdim)
-    
-    
+
     @with_supported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def astype(self, dtype):
         return ivy.astype(self._ivy_array, dtype=dtype)
-    
+
+    @with_supported_dtypes({"2.4.2 and below": ("float32", "float64", "int32", "int64")}, "paddle")
+    def max(self, axis=None, keepdim=False, name=None):
+        return ivy.max(self._ivy_array, axis=axis, keepdims=keepdim)
