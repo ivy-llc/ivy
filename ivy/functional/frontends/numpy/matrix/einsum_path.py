15439cb57dd79db12950f1880c6ec8b8b48058e0
Jesee Jackson Kuya <125478197+jaykuva@users.noreply.github.com>
2023-06-23 15:26:29 -0400

    Update einsum_path.py

--- ivy/functional/frontends/numpy/matrix/einsum_path.py
+++ ivy/functional/frontends/numpy/matrix/einsum_path.py
@@ -4,4 +4,3 @@ from numpy.core.einsumfunc import einsum_path
 def matrix_multiply(a, b):
     path = einsum_path('ij, jk -> ik', a, b)
     result = np.einsum('ij, jk -> ik', a, b, optimize=path)
-    return result
