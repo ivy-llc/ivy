9667b746ebf12d31687fda569eb84ae816ddcb93
kshitijgundale <kshitijgundale08@gmail.com>
2023-06-23 19:08:20 +0530

    formatting fix

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -2156,32 +2156,19 @@ def test_tensorflow_truncatemod(
 @handle_frontend_test(
     fn_tree="tensorflow.unravel_index",
     indices=helpers.array_values(
-        dtype=helpers.get_dtypes("integer"),
-        shape=(1,2),
-        min_value=0,
-        max_value=49    
+        dtype=helpers.get_dtypes("integer"), shape=(1, 2), min_value=0, max_value=49
     ),
     dims=helpers.array_values(
-        dtype=helpers.get_dtypes("integer"),
-        shape=(1,2),
-        min_value=50
-    )
+        dtype=helpers.get_dtypes("integer"), shape=(1, 2), min_value=50
+    ),
 )
-def test_unravel_index(
-    *,
-    indices,
-    dims,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device
-):
+def test_unravel_index(*, indices, dims, frontend, test_flags, fn_tree, on_device):
     helpers.test_frontend_function(
-        input_dtypes=['int32'],
+        input_dtypes=["int32"],
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
         indices=indices[0],
-        dims=dims[0]
+        dims=dims[0],
     )
