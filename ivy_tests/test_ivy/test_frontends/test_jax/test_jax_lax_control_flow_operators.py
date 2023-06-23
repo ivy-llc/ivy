318847dbba3b2b3395f3c10e88e382b39cd70fb0
TimothyElems <elemstimothy@protonmail.com>
2023-06-23 19:54:06 +0000

    add scan function tests

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_control_flow_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_control_flow_operators.py
@@ -335,56 +335,6 @@ def test_jax_lax_while_loop(
 
 # import ivy
 
-# def test_jax_lax_scan(
-#     *,
-#     dtype_and_x,
-#     test_flags,
-#     on_device,
-#     fn_tree,
-#     frontend,
-# ):
-#     def _test_f(carry, x):
-#         y = carry * x
-#         new_carry = x + y
-#         return new_carry, y
-
-#     input_dtype, x = dtype_and_x
-#     length = len(x)
-#     expected_carry = 0
-#     expected_ys = []
-#     carry = expected_carry
-#     for _ in range(unroll):
-#         for elem in x:
-#             carry, y = _test_f(carry, elem)
-#             expected_ys.append(y)
-
-#     expected_result = (expected_carry, ivy.stack(expected_ys))
-
-#     scan_result = scan(
-#         _test_f,
-#         expected_carry,
-#         x,
-#         length=length,
-#         reverse=False,
-#         unroll=unroll,
-#     )
-
-#     assert scan_result == expected_result
-
-#     helpers.test_frontend_function(
-#         input_dtypes=input_dtype,
-#         test_flags=test_flags,
-#         frontend=frontend,
-#         fn_tree=fn_tree,
-#         on_device=on_device,
-#         f=_test_f,
-#         init=expected_carry,
-#         xs=x,
-#         length=length,
-#         expected_result=expected_result,
-#     )
-
-
 def test_jax_lax_scan(
     *,
     dtype_and_x,
@@ -408,7 +358,7 @@ def test_jax_lax_scan(
             carry, y = _test_f(carry, elem)
             expected_ys.append(y)
 
-    expected_result = (expected_carry, expected_ys)
+    expected_result = (expected_carry, ivy.stack(expected_ys))
 
     scan_result = scan(
         _test_f,
@@ -433,3 +383,4 @@ def test_jax_lax_scan(
         length=length,
         expected_result=expected_result,
     )
+
