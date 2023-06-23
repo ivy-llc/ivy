0d20aaa0402b22aa972e1057affc1e6feae4904e
Mai Hassanin ElBaradei <77697746+MaiElBaradei@users.noreply.github.com>
2023-06-23 17:03:36 +0300

    Revert "resolve conflict"
    This reverts commit 0da12ec0ad02df36135a696cc6485107a3edb375.

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -423,7 +423,8 @@ class Tensor:
     def new_zeros(self, *size, dtype=None, device=None, requires_grad=False):
         if isinstance(size[0], tuple):
             return torch_frontend.zeros(
-                size[0], dtype=dtype, device=device, requires_grad=requires_grad
+                size[0], dtype=dtype, device=device,
+                requires_grad=requires_grad
             )
         return torch_frontend.zeros(
             size, dtype=dtype, device=device, requires_grad=requires_grad
