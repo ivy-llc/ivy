1f51432c6d7085787a4d79acd61db9796e25fa58
Rakshit Sharma <kumarrakshit0402@gmail.com>
2023-06-23 16:05:37 +0530

    Added Modf API test function

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_elementwise.py
@@ -845,3 +845,33 @@ def test_frexp(
         on_device=on_device,
         x=x[0],
     )
+
+
+# modf
+@handle_test(
+    fn_tree="functional.ivy.experimental.modf",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=1,
+        min_value=0,
+        exclude_min=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_modf(
+        dtype_and_x,
+        frontend,
+        test_flags,
+        fn_tree,
+        on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+    
