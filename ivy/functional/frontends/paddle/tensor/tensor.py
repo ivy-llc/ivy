91cc6cfba33b831ac067b682b3621d77c721edf7
samthakur587 <samunder268@gmail.com>
2023-06-23 19:18:51 +0530

    first commit

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -180,3 +180,14 @@ class Tensor:
     @with_unsupported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
     def sort(self, axis=-1, descending=False, name=None):
         return ivy.sort(self._ivy_array, axis=axis, descending=descending)
+    
+    @with_supported_dtypes({"2.4.2 and below": (
+            "bool",
+            "uint8",
+            "int8",
+            "int16",
+            "int32",
+            "int64",
+        )}, "paddle")
+    def bitwise_and(x, y, out=None, name=None):
+        return paddle_frontend.bitwise_and(x, y)
