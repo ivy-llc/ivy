94be8c1ca2f24235bfccde89d7d2b47046baa1e4
TimothyElems <elemstimothy@protonmail.com>
2023-06-24 22:03:34 +0000

    add scan function tests

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_control_flow_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_lax_control_flow_operators.py
@@ -4,7 +4,6 @@ from hypothesis import strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
-# from typing import Callable, List, Tuple
 
 @handle_frontend_test(
     fn_tree="jax.lax.cond",
