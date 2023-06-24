2d697ef3053be33d70a0fb03a6c167a6814c27bb
afarinizadi <57967322+afarinizadi@users.noreply.github.com>
2023-06-24 22:51:33 +0100

    Update test_paddle_math.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -912,3 +912,28 @@ def test_paddle_reciprocal(
         on_device=on_device,
         x=x[0],
     )
+# increment
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.increment",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+    ),
+)
+def test_paddle_increment(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
