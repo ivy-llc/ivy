d35d94f3fcb27dcd0c0de00151de26d45a7d67ff
Jesee Jackson Kuya <125478197+jaykuva@users.noreply.github.com>
2023-06-24 05:24:11 -0400

    Delete einsum_path.py

--- ivy/functional/frontends/numpy/matrix/einsum_path.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from numpy.core.einsumfunc import einsum_path
-
-def matrix_multiply(a, b):
-    path = einsum_path('ij, jk -> ik', a, b)
-    result = np.einsum('ij, jk -> ik', a, b, optimize=path)
