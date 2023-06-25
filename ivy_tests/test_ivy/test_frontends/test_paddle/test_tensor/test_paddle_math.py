19241b7dad3b9204d90ef3406d6b04f91e128b40
henrik syvertsson <henrik.syvertsson@gmail.com>
2023-06-25 13:21:08 +0200

    add paddle.tensor.math.lcm
    close #17554
    also fix typo in description for ivy.lcm()

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -911,3 +911,35 @@ def test_paddle_reciprocal(
         on_device=on_device,
         x=x[0],
     )
+
+
+# lcm
+@handle_frontend_test(
+    fn_tree="paddle.lcm",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        min_num_dims=1,
+        safety_factor_scale="log",
+        large_abs_safety_factor=2,
+        shared_dtype=True,
+    ),
+)
+def test_paddle_lcm(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        test_flags=test_flags,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
