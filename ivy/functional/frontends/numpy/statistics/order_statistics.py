310f8e6413c9c01470aa8e8198df35f68dc3e3b1
Dhruv Saxena <115158822+dhruvsaxena11@users.noreply.github.com>
2023-06-26 21:50:40 +0530

    Update order_statistics.py

--- ivy/functional/frontends/numpy/statistics/order_statistics.py
+++ ivy/functional/frontends/numpy/statistics/order_statistics.py
@@ -1,10 +1,5 @@
 # global
 import ivy
-from ivy.core.container import Container
-from ivy.core.operations import arithmetic_ops
-from ivy.core.tensor import Tensor
-from ivy.numpy.base import promote_types_of_numpy_inputs
-from ivy import numpy as ivy_np
 
 def _quantile_is_valid(q):
     # avoid expensive reductions, relevant for arrays with < O(1000) elements
@@ -109,48 +104,28 @@ def nanpercentile(
                 resultarray.append(arrayofpercentiles)
         return resultarray
 
-def quantile(arr, q, axis=None, out=None, overwrite_input=False, interpolation='linear'):
-    arr = promote_types_of_numpy_inputs(arr)
-    if axis is None:
-        arr = arr.flatten()
-        axis = 0
-    elif axis < 0:
-        axis += arr.ndim
-    if out is not None:
-        out = promote_types_of_numpy_inputs(out)
 
-    # Sort the array along the specified axis
-    sorted_arr = arithmetic_ops.sort(arr, axis=axis)
 
-    # Calculate the index based on the desired quantile
-    alpha = q / 100.0
-    if interpolation == 'linear':
-        index = alpha * (sorted_arr.shape[axis] - 1)
-        lower_idx = arithmetic_ops.floor(index)
-        upper_idx = arithmetic_ops.ceil(index)
-        weight = index - lower_idx
-    elif interpolation == 'lower':
-        index = alpha * sorted_arr.shape[axis]
-        lower_idx = arithmetic_ops.floor(index)
-        upper_idx = lower_idx
-        weight = 0
-    elif interpolation == 'higher':
-        index = alpha * sorted_arr.shape[axis]
-        lower_idx = arithmetic_ops.ceil(index)
-        upper_idx = lower_idx
-        weight = 0
-    else:
-        raise ValueError("Interpolation method '{}' not supported.".format(interpolation))
-
-    # Calculate the quantile value
-    if out is not None:
-        ret = out
-    else:
-        ret = Container(arr)
-    ret.val = (1.0 - weight) * sorted_arr.take(lower_idx, axis=axis) + weight * sorted_arr.take(upper_idx, axis=axis)
-
-    if overwrite_input:
-        arr.val = ret.val
-        return arr
-    else:
-        return ret
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@handle_numpy_casting
+@from_zero_dim_arrays_to_scalar
+def quantile(
+    x,
+    q,
+    /,
+    out=None,
+    *,
+    interpolation="linear",
+    kind="increasing",
+    order="k",
+    dtype=None,
+    keepdims=False,
+):
+  
+    x = ivy.as_array(x)
+    ret = ivy.quantile(x, q, interpolation=interpolation, kind=kind, order=order, out=out)
+    if keepdims:
+        ret = ivy.expand_dims(ret, -1)
+    return ret
