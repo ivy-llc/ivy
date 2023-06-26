8b7d8ff578a280a0ffea4d3156ccfe84133781f1
MalikHaroonKhokhar <113617367+MalikHaroonKhokhar@users.noreply.github.com>
2023-06-26 18:13:05 +0000

    Pending changes exported from your codespace

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -263,3 +263,15 @@ def solve_ex(A, B, *, left=True, check_errors=False, out=None):
             result = A * math.nan
             info = ivy.ones(A.shape[:-2], dtype=ivy.int32)
             return result, info
+def cholesky_ex(A, *, upper=False, check_errors=False, out=None):
+    try:
+        matrix = ivy.cholesky(A,upper=upper, out=out)
+        info = ivy.zeros(A.shape[:-2], dtype=ivy.int32)
+        return matrix, info
+    except RuntimeError as e:
+        if check_errors:
+            raise RuntimeError(e)
+        else:
+            matrix = A * math.nan
+            info = ivy.ones(A.shape[:-2], dtype=ivy.int32)
+            return matrix, info
