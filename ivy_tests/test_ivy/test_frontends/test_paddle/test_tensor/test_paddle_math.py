83a75b98efdc3863c8c2c487205ea63ebd898dc4
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-06-26 16:38:04 +0100

    added paddle_frontend_tensor_math_fmax

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -966,3 +966,29 @@ def test_paddle_gcd(
         x=x[0],
         y=x[1],
     )
+
+@handle_frontend_test(
+    fn_tree="paddle.fmax",
+    dtypes_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"), num_arrays=2, shared_dtype=True
+    ),
+)
+def test_paddle_fmax(
+    *,
+    dtypes_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtypes_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
+    
