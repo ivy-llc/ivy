79693180c319eb396fe08ff8225d289f96e2dc01
GOKULNATH S <99210041858@klu.ac.in>
2023-06-26 04:38:22 +0000

    added_mindspore_softmax

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -1,2 +1,17 @@
 """Includes Mindspore Frontend functions listed in the TODO list
 https://github.com/unifyai/ivy/issues/14951."""
+
+import ivy
+import ivy.functional.frontends.paddle as paddle_frontend
+from ivy.func_wrapper import with_supported_dtypes, with_unsupported_dtypes
+import ivy
+from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
+
+@with_supported_dtypes(
+    {"2.5.0 and below": ("float32", "float16")},
+    "Mindspore",
+)
+
+@to_ivy_arrays_and_back
+def softmax(x, axis=-1):
+    return ivy.softmax(x, axis=axis)
--- ivy_tests/test_ivy/test_frontends/test_mindspore/test_mindspore_numpy.py
+++ ivy_tests/test_ivy/test_frontends/test_mindspore/test_mindspore_numpy.py
@@ -1,42 +1,42 @@
 # global
+from hypothesis import strategies as st
 
 # local
+import ivy
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
-# TODO: uncomment after frontend is not required
-#  to be set as backend in test_frontend_function
-# @handle_frontend_test(
-#     fn_tree="mindspore.numpy.array",
-#     dtype_and_a=helpers.dtype_and_values(
-#         available_dtypes=helpers.get_dtypes("numeric"),
-#         num_arrays=1,
-#         min_num_dims=0,
-#         max_num_dims=5,
-#         min_dim_size=1,
-#         max_dim_size=5,
-#     ),
-#     ndmin=st.integers(min_value=0, max_value=5),
-#     copy=st.booleans(),
-#     test_with_out=st.just(False),
-# )
-# def test_mindspore_numpy_array(
-#     dtype_and_a,
-#     frontend,
-#     test_flags,
-#     fn_tree,
-#     on_device,
-#     copy,
-#     ndmin,
-# ):
-#     dtype, a = dtype_and_a
-#     helpers.test_frontend_function(
-#         input_dtypes=dtype,
-#         frontend=frontend,
-#         test_flags=test_flags,
-#         fn_tree=fn_tree,
-#         on_device=on_device,
-#         object=a,
-#         dtype=None,
-#         copy=copy,
-#         ndmin=ndmin,
-#     )
+
+@handle_frontend_test(
+    fn_tree="mindspore.softmax",
+    dtype_x_and_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=2,
+        max_axes_size=1,
+        force_int_axis=True,
+        valid_axis=True,
+    ),
+    test_with_out=st.just(False),
+)
+
+
+def test_mindspore_softmax(
+    *,
+    dtype_x_and_axis,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):   
+    input_dtype, x, axis = dtype_x_and_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        axis=axis,
+    )
+    
