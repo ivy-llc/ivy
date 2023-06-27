5dc6bc5042f150a75f2eec6fdad6e3325460a926
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-06-27 11:41:25 +0500

    EHN: adding function  ifft in paddle fft.py file

--- ivy/functional/frontends/paddle/fft.py
+++ ivy/functional/frontends/paddle/fft.py
@@ -14,3 +14,13 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 def fft(x, n=None, axis=-1.0, norm="backward", name=None):
     ret = ivy.fft(ivy.astype(x, "complex128"), axis, norm=norm, n=n)
     return ivy.astype(ret, x.dtype)
+
+
+@with_supported_dtypes(
+    {"2.5.0 and below": ("complex64", "complex128")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def ifft(x, n=None, axis=-1.0, norm="backward", name=None):
+    ret = ivy.ifft(ivy.astype(x, "complex128"), axis, norm=norm, n=n)
+    return ivy.astype(ret, x.dtype)
