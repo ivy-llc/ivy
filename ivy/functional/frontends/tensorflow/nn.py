eab0bb168f0d62bf59d1f2e12b639726ea035685
alrizkipascar <alrizkipasca99@gmail.com>
2023-06-27 06:51:48 +0000

    ctc_unique_labels

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -488,3 +488,13 @@ def pool(
 @to_ivy_arrays_and_back
 def log_poisson_loss(targets, log_input, compute_full_loss=False, name=None):
     return ivy.log_poisson_loss(targets, log_input, compute_full_loss, name)
+
+
+# ctc_unique_labels
+@to_ivy_arrays_and_back
+def ctc_unique_labels(labels, name=None):
+    ctc_labels = ivy.unique_all(labels, by_value=False)
+    unique_pad = ivy.pad(
+        ctc_labels[0], (0, labels.size - ctc_labels[0].size), mode="constant"
+    )
+    return unique_pad, ctc_labels[2]
--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_nn.py
@@ -1603,3 +1603,36 @@ def test_log_poisson_loss(
         compute_full_loss=compute_full_loss,
         atol=1e-2,
     )
+
+
+# ctc_unique_labels
+@handle_frontend_test(
+    fn_tree="tensorflow.nn.ctc_unique_labels",
+    dtype_x=helpers.dtype_and_values(
+        available_dtypes=["int64", "int32"],
+        min_value=1,
+        max_value=100,
+        min_dim_size=1,
+        max_dim_size=10,
+        min_num_dims=2,
+        max_num_dims=2,
+    ),
+    test_with_out=st.just([False]),
+)
+def test_tensorflow_ctc_unique_labels(
+    *,
+    dtype_x,
+    frontend,
+    fn_tree,
+    test_flags,
+    on_device,
+):
+    dtype, x = dtype_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        labels=x[0],
+    )
