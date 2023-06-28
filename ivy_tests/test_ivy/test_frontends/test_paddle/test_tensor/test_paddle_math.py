e9ac6c0acf76547a239980c99844b52b07250cc7
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-06-28 14:07:56 +0100

    added paddle frontend tensor math angle method

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -1055,3 +1055,28 @@ def test_paddle_logit(
         x=x[0],
         eps=1e-2,
     )
+
+
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.angle",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=["float64", "complex64", "complex128"],
+    ),
+)
+def test_paddle_angle(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
