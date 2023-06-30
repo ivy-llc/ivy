d6c7b52749f790f2a5a6120d9b3ca4f5065b4605
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2023-06-30 23:25:34 +0000

    fix data_format bug

--- ivy/functional/frontends/torch/nn/functional/dropout_functions.py
+++ ivy/functional/frontends/torch/nn/functional/dropout_functions.py
@@ -15,5 +15,5 @@ def dropout(input, p=0.5, training=True, inplace=False):
 @with_unsupported_dtypes({"2.0.1 and below": ("float16",)}, "torch")
 def dropout1d(input, p=0.5, training=True, inplace=False):
     if inplace:
-        return ivy.dropout1d(input, p, training=training, out=input)
-    return ivy.dropout1d(input, p, training=training)
+        return ivy.dropout1d(input, p, training=training, data_format="NCW", out=input)
+    return ivy.dropout1d(input, p, data_format="NCW", training=training)
