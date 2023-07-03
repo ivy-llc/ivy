96e7a77a19991bd44d0e58386ef22990761060a5
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-07-03 12:14:52 +0530

    added selu function + test

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -1,2 +1,15 @@
 """Includes Mindspore Frontend functions listed in the TODO list
 https://github.com/unifyai/ivy/issues/14951."""
+
+# local
+import ivy
+from ivy.func_wrapper import with_supported_dtypes
+
+
+@with_supported_dtypes({"2.5.0 and below": ("float16", "float32")}, "mindspore")
+def selu(input_x):
+    alpha = 1.67326324
+    scale = 1.05070098
+    ret = ivy.where(input_x > 0, input_x, alpha * ivy.expm1(input_x))
+    arr = scale * ret
+    return ivy.astype(arr, input_x.dtype)
