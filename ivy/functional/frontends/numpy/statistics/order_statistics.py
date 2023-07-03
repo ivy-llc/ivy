e8ba56b4ac8ae75759a7d470ee8753f2847cdaa5
Dhruv Saxena <115158822+dhruvsaxena11@users.noreply.github.com>
2023-07-03 17:32:27 +0530

    order_statistics.py

--- ivy/functional/frontends/numpy/statistics/order_statistics.py
+++ ivy/functional/frontends/numpy/statistics/order_statistics.py
@@ -1,6 +1,14 @@
 # global
 import ivy
-
+import numpy
+from ivy.func_wrapper import with_unsupported_dtypes
+from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
+    handle_numpy_dtype,
+    from_zero_dim_arrays_to_scalar,
+    handle_numpy_out,
+    
+)
 
 def _quantile_is_valid(q):
     # avoid expensive reductions, relevant for arrays with < O(1000) elements
@@ -34,6 +42,41 @@ def _cpercentile(N, percent, key=lambda x: x):
     d1 = key(N[int(c)]) * (k - f)
     return d0 + d1
 
+
+@handle_numpy_out
+@handle_numpy_dtype
+@to_ivy_arrays_and_back
+@from_zero_dim_arrays_to_scalar
+
+def percentile(
+    a,
+    q,
+    axis=None,
+    interpolation='linear',
+    keepdims=False
+):
+    a = ivy.array(a)
+    q = ivy.divide(q, 100.0)
+    q = ivy.array(q)
+
+    if not ivy.all(ivy.logical_and(q >= 0, q <= 1)):
+        ivy.logging.warning("Percentiles must be in the range [0, 100]")
+        return []
+
+    if axis is None:
+        nanlessarray = ivy.where(ivy.isnan(a), ivy.inf, a)
+        nanlessarray = ivy.reshape(nanlessarray, (-1,))
+        resultarray = ivy.percentile(nanlessarray, q)
+        return resultarray
+    elif axis == 1:
+        nanlessarrayofarrays = ivy.where(ivy.isnan(a), ivy.inf, a)
+        resultarray = ivy.percentile(nanlessarrayofarrays, q, axis=1, keepdims=keepdims)
+        return resultarray
+    elif axis == 0:
+        nanlessarrayofarrays = ivy.where(ivy.isnan(a), ivy.inf, a)
+        nanlessarrayofarrays = ivy.swapaxes(nanlessarrayofarrays, 0, 1)
+        resultarray = ivy.percentile(nanlessarrayofarrays, q, axis=0, keepdims=keepdims)
+        return resultarray
 
 def nanpercentile(
     a,
