73d82f3e69af533c586763d61bd5f6620bec6190
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-07-03 13:43:22 +0100

    add back initial code

--- ivy/data_classes/array/array.py
+++ ivy/data_classes/array/array.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 # global
+import copy
 import functools
 import numpy as np
 from operator import mul
@@ -1133,9 +1134,20 @@ class Array(
         return ivy.bitwise_right_shift(self._data, other)
 
     def __deepcopy__(self, memodict={}):
-        array_copy = ivy.copy_array(self._data)
-        memodict[id(self)] = array_copy
-        return to_ivy(array_copy)
+        try:
+            return to_ivy(self._data.__deepcopy__(memodict))
+        except AttributeError:
+            # ToDo: try and find more elegant solution to jax inability to
+            #  deepcopy device arrays
+            if ivy.current_backend_str() == "jax":
+                np_array = copy.deepcopy(self._data)
+                jax_array = ivy.array(np_array)
+                return to_ivy(jax_array)
+            return to_ivy(copy.deepcopy(self._data))
+        except:
+            array_copy = ivy.copy_array(self._data)
+            memodict[id(self)] = array_copy
+            return to_ivy(array_copy)
 
     def __len__(self):
         if not len(self._data.shape):
