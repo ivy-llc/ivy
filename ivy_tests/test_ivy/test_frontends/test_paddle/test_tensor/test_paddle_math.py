1160d22b596e5e76e7013ae8d7716bda21a32cd4
remenraj <remenraj@gmail.com>
2023-07-04 16:40:27 +0530

    Add test_paddle_digamma to paddle frontend tests

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -1298,3 +1298,29 @@ def test_paddle_erf(
         on_device=on_device,
         x=x[0],
     )
+
+
+# digamma
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.digamma",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_digamma(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
