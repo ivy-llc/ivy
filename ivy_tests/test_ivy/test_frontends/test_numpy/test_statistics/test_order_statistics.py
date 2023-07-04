59cd95a7ab35a260781288ef753e618244ac45f2
Arnav-Menon <arnavmenon@gmail.com>
2023-07-03 20:37:57 -0700

    added numpy frontend ptp test

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_order_statistics.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_order_statistics.py
@@ -10,6 +10,44 @@ import ivy_tests.test_ivy.test_frontends.test_numpy.helpers as np_frontend_helpe
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
+# ptp
+@handle_frontend_test(
+    fn_tree="numpy.ptp",
+    dtype_values_axis=_statistical_dtype_values(function="ptp"),
+    where=np_frontend_helpers.where(),
+    keep_dims=st.booleans(),
+)
+def test_numpy_ptp(
+    dtype_values_axis,
+    where,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    keep_dims,
+):
+    input_dtypes, values, axis = dtype_values_axis
+    if isinstance(axis, tuple):
+        axis = axis[0]
+
+    where, input_dtypes, test_flags = np_frontend_helpers.handle_where_and_array_bools(
+        where=where,
+        input_dtype=input_dtypes,
+        test_flags=test_flags,
+    )
+
+    np_frontend_helpers.test_frontend_function(
+        a=values,
+        axis=axis,
+        out=None,
+        keepdims=keep_dims,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        test_flags=test_flags,
+        input_dtypes=input_dtypes,
+    )
+
+
 # nanpercentile
 @handle_frontend_test(
     fn_tree="numpy.nanpercentile",
