19b723b354ff4e779dd678c4707215de2b9f9c62
Dhruv <59387969+druvdub@users.noreply.github.com>
2023-07-04 20:47:17 +0100

    Update supported dtypes for the bincount

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -442,10 +442,12 @@ class Tensor:
         y_ivy = y._ivy_array if isinstance(y, Tensor) else _to_ivy_array(y)
         return ivy.floor_divide(self._ivy_array, y_ivy)
 
-    @with_supported_dtypes({"2.5.0 and below": ("int32", "int64")}, "paddle")
-    def bincount(self, weights=None, minlength=0, out=None, name=None):
+    @with_supported_dtypes(
+        {"2.5.0 and below": ("int32", "int64", "float32", "float64")}, "paddle"
+    )
+    def bincount(self, weights=None, minlength=0, name=None):
         return ivy.bincount(
-            self._ivy_array, weights=weights, minlength=minlength, out=out
+            self._ivy_array, weights=weights, minlength=minlength, out=None
         )
 
     @with_unsupported_dtypes({"2.4.2 and below": ("int16", "float16")}, "paddle")
