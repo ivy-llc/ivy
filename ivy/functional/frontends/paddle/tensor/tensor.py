6c2807460f73d1a169009f8cccf070c2da98a9cc
tasneem <atasneem789@gmail.com>
2023-07-05 12:25:38 +0300

    modified:   ivy/functional/frontends/paddle/tensor/tensor.py 	modified:   ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -494,6 +494,7 @@ class Tensor:
     )
     def logical_not(self, out=None, name=None):
         return ivy.logical_not(self.ivy_array)
-   @with_unsupported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+
+    @with_unsupported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
     def std(self, axis=None, unbiased=True, keepdim=False, name=None):
         return ivy.std(self._ivy_array, axis=axis, keepdims=keepdim)
