c2726da0438a3bae0d85e0f330bbc17227a79364
Rishab Mallick <rishabmallick6@gmail.com>
2023-07-05 17:52:28 +0530

    chore: adds code accidentally removed

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
@@ -2282,6 +2282,22 @@ def test_paddle_neg(
     frontend_method_data,
     init_flags,
     method_flags,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
 
 
 # isnan
