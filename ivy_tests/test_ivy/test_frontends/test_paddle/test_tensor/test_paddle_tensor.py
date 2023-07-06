22740302102fd00bae3dc8964e927d58e7a7f599
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-07-06 16:55:54 +0100

    Implement set_item to avoid scatter_nd errors + Refactor get_item to support all cases (#17417)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
@@ -45,23 +45,6 @@ def _reshape_helper(draw):
 
 
 @st.composite
-def _setitem_helper(draw, available_dtypes, allow_neg_step=True):
-    input_dtype, x, index = draw(
-        helpers.dtype_array_index(
-            available_dtypes=available_dtypes,
-            allow_neg_step=allow_neg_step,
-        )
-    )
-    val_dtype, val = draw(
-        helpers.dtype_and_values(
-            available_dtypes=available_dtypes,
-            shape=x[index].shape,
-        )
-    )
-    return input_dtype + val_dtype, x, index, val[0]
-
-
-@st.composite
 def _get_dtype_and_square_matrix(draw):
     dim_size = draw(helpers.ints(min_value=2, max_value=5))
     dtype = draw(helpers.get_dtypes("float", index=1, full=False))
@@ -169,15 +152,23 @@ def test_paddle_instance_reshape(
     )
 
 
+def _filter_query(query):
+    return (
+        query.ndim > 1 if isinstance(query, np.ndarray) else
+        not any(isinstance(i, np.ndarray) and i.ndim <= 1 for i in query)
+        if isinstance(query, tuple) else True
+    )
+
+
 # __getitem__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
     init_tree="paddle.to_tensor",
     method_name="__getitem__",
-    dtype_x_index=helpers.dtype_array_index(
+    dtype_x_index=helpers.dtype_array_query(
         available_dtypes=helpers.get_dtypes("valid"),
         allow_neg_step=False,
-    ),
+    ).filter(lambda x: x[0][0] == x[0][-1] and _filter_query(x[-2])),
 )
 def test_paddle_instance_getitem(
     dtype_x_index,
@@ -191,7 +182,7 @@ def test_paddle_instance_getitem(
     helpers.test_frontend_method(
         init_input_dtypes=[input_dtype[0]],
         init_all_as_kwargs_np={"data": x},
-        method_input_dtypes=[input_dtype[1]],
+        method_input_dtypes=[*input_dtype[1:]],
         method_all_as_kwargs_np={"item": index},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
@@ -206,9 +197,9 @@ def test_paddle_instance_getitem(
     class_tree=CLASS_TREE,
     init_tree="paddle.to_tensor",
     method_name="__setitem__",
-    dtypes_x_index_val=_setitem_helper(
+    dtypes_x_index_val=helpers.dtype_array_query_val(
         available_dtypes=helpers.get_dtypes("valid"),
-    ),
+    ).filter(lambda x: x[0][0] == x[0][-1] and _filter_query(x[-2])),
 )
 def test_paddle_instance_setitem(
     dtypes_x_index_val,
@@ -219,7 +210,6 @@ def test_paddle_instance_setitem(
     on_device,
 ):
     input_dtype, x, index, val = dtypes_x_index_val
-    assume(len(index) != 0)
     helpers.test_frontend_method(
         init_input_dtypes=[input_dtype[0]],
         init_all_as_kwargs_np={"data": x},
