565f1ff057547e9968bce03fa3475d7575df6b54
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-07-07 09:01:40 +0100

    will close issue 18239

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
@@ -154,9 +154,13 @@ def test_paddle_instance_reshape(
 
 def _filter_query(query):
     return (
-        query.ndim > 1 if isinstance(query, np.ndarray) else
-        not any(isinstance(i, np.ndarray) and i.ndim <= 1 for i in query)
-        if isinstance(query, tuple) else True
+        query.ndim > 1
+        if isinstance(query, np.ndarray)
+        else (
+            not any(isinstance(i, np.ndarray) and i.ndim <= 1 for i in query)
+            if isinstance(query, tuple)
+            else True
+        )
     )
 
 
@@ -2252,6 +2256,8 @@ def test_paddle_instance_conj(
         frontend=frontend,
         on_device=on_device,
     )
+
+
 # floor_
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -2285,7 +2291,6 @@ def test_paddle_instance_floor_(
     )
 
 
-
 # log2
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -2317,6 +2322,7 @@ def test_paddle_instance_log2(
         frontend=frontend,
         on_device=on_device,
     )
+
 
 # neg
 @handle_frontend_method(
@@ -2535,6 +2541,45 @@ def test_paddle_cond(
         },
         method_input_dtypes=input_dtype,
         method_all_as_kwargs_np={"p": p},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="paddle.to_tensor",
+    method_name="sgn",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float_and_complex"),
+        min_num_dims=1,
+        max_num_dims=1,
+        min_dim_size=1,
+        max_dim_size=1,
+        abs_smallest_val=1e-10,
+        min_value=-10,
+        max_value=10,
+    ),
+)
+def test_paddle_sgn(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
