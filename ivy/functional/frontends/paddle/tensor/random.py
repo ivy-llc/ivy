06a84e059dbca2b7efbd9862f65a3b7137da1e3b
Khaled Atef <105244576+KhaledAtef00@users.noreply.github.com>
2023-07-08 17:13:28 +0300

    Update random.py

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -1,6 +1,7 @@
 # global
 import ivy
 from ivy.func_wrapper import with_supported_dtypes
+from ivy.func_wrapper import with_supported_device_and_dtypes
 from ivy.functional.frontends.paddle.func_wrapper import (
     to_ivy_arrays_and_back,
 )
