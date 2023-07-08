54536b0f10d289f3b24b08ddbda47eb3067e7a55
samunder singh <83540902+samthakur587@users.noreply.github.com>
2023-07-08 21:19:30 +0530

    Update tensor.py

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -523,12 +523,11 @@ class Tensor:
 
     @with_unsupported_dtypes({"2.5.0 and below": ("float16", "bfloat16")}, "paddle")
     def sign(self, name=None):
-        return ivy.sign(self._ivy_array)
+        return ivy.sign(self._ivy_array, np_variant=False)
 
     @with_unsupported_dtypes({"2.5.0 and below": ("float16", "bfloat16")}, "paddle")
     def var(self, axis=None, *, unbiased=True, keepdim=False):
         return paddle_frontend.var(self, axis=axis, unbiased=unbiased, keepdim=keepdim)
-        return ivy.sign(self._ivy_array, np_variant=False)
 
     @with_unsupported_dtypes({"2.5.0 and below": ("float16", "bfloat16")}, "paddle")
     def sgn(self, name=None):
