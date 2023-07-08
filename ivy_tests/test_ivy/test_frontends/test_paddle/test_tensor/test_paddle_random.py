a92ee4e4c2f6219825bda7814cc73e71804ff338
Khaled Atef <105244576+KhaledAtef00@users.noreply.github.com>
2023-07-08 15:46:45 +0300

    Update test_paddle_random.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_random.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_random.py
@@ -104,10 +104,8 @@ def test_paddle_poisson(dtype_and_x, frontend, test_flags, fn_tree):
     input_dtypes=st.sampled_from(["int32", "int64"]),
     shape=helpers.get_shape(
             allow_none=False,
-            min_num_dims=1,
-            max_num_dims=3,
+            min_num_dims=0,
             min_dim_size=1,
-            max_dim_size=3,
         )
     dtype=helpers.get_dtypes("valid"),
 )
