5876b86d64014fe75fc5cfea3b657e410d52d7d1
Lucifer60 <804523034@qq.com>
2023-07-08 12:58:20 +0800

    add silu

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -214,3 +214,9 @@ def mish(x, name=None):
 @to_ivy_arrays_and_back
 def leaky_relu(x, negative_slope=0.01, name=None):
     return ivy.leaky_relu(x)
+
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def silu(x,name=None): # silu : y = x * sigmoid(x)
+    return x * ivy.sigmoid(x)
