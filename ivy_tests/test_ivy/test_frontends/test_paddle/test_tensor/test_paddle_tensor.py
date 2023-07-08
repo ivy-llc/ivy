7c266f2da53bd8b6f8856d1f37f8b638ee91b937
Dhruv <59387969+druvdub@users.noreply.github.com>
2023-07-08 01:34:48 +0100

    Update test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_tensor.py
@@ -2160,13 +2160,12 @@ def test_paddle_instance_floor_(
     init_tree="paddle.to_tensor",
     method_name="bincount",
     dtypes_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("int"),
-        num_arrays=1,
-        shape=(-1,),
-        shared_dtype=True,
+        available_dtypes=["int32", "int64"],
+        num_arrays=2,
+        min_value=0,
     ),
 )
-def test_paddle_bincount(
+def test_paddle_instance_bincount(
     dtypes_and_x,
     frontend_method_data,
     init_flags,
@@ -2174,12 +2173,16 @@ def test_paddle_bincount(
     frontend,
     on_device,
 ):
-    input_dtype, (x,) = dtypes_and_x
+    input_dtype, x = dtypes_and_x
     helpers.test_frontend_method(
         init_input_dtypes=input_dtype,
-        init_all_as_kwargs_np={"data": x},
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
         method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={},
+        method_all_as_kwargs_np={
+            "weights": x[1],
+        },
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
