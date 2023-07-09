397aa9da255859f8e30ea7e3006cae6a47e2026b
ArsalanAli915 <i191885@nu.edu.pk>
2023-07-09 13:51:45 +0500

    new commit due to deletion of files

--- ivy/functional/frontends/paddle/fft2.py
+++ ivy/functional/frontends/paddle/fft2.py
@@ -7,12 +7,10 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 
 
 @with_supported_dtypes(
-    {"2.4.2 and below": ("float32", "float64", "complex64", "complex128")},
+    {"2.5.0 and below": ("complex64", "complex128")},
     "paddle",
 )
 @to_ivy_arrays_and_back
-def fft2(x, n=None, axes=(- 2, - 1), norm="backward", name=None):
-    return ivy.fft2(x, axes, norm=norm, n=n)
-
-
-
+def fft2(x, s=None, axes=(- 2, - 1), norm='backward', name=None):
+    ret = ivy.fft2(ivy.astype(x, "complex128"), axes, norm=norm, n=s)
+    return ivy.astype(ret, x.dtype)
