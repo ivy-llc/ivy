eaa284060978dc53f93c619925140408d330efbe
Mirzonabot <mirzonabot.mirzonabotov_2023@ucentralasia.org>
2023-07-10 00:47:51 +0600

    feat(jax): implement statistical function (percentile)

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -486,3 +486,9 @@ def cov(m, y=None, rowvar=True, bias=False, ddof=None, fweights=None, aweights=N
     return ivy.cov(
         m, y, rowVar=rowvar, bias=bias, ddof=ddof, fweights=fweights, aweights=aweights
     )
+
+# percentile
+
+@to_ivy_arrays_and_back
+def percentile(a, q, axis=None, out=None, overwrite_input=False, interpolation="linear", keepdims=False):
+    return ivy.percentile(a, q, axis=axis, out=out, interpolation=interpolation, keepdims=keepdims)
