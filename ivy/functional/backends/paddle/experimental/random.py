95d5050494ac72f73947d16a78cc9cd35bef0c6a
Mohammed <maymn0535@gmail.com.com>
2023-07-09 23:28:30 +0300

    add beta to paddle backend

--- ivy/functional/backends/paddle/experimental/random.py
+++ ivy/functional/backends/paddle/experimental/random.py
@@ -1,15 +1,15 @@
 # global
 from typing import Optional, Union, Sequence
 import paddle
-
+from ivy.functional.backends.paddle.device import to_device
 from ivy import with_unsupported_device_and_dtypes
 from ivy.functional.backends.paddle import backend_version
 from ivy.utils.exceptions import IvyNotImplementedException
-
+from ivy.functional.ivy.random import _check_bounds_and_get_shape
 # local
 import ivy
 from paddle.device import core
-
+from ivy.func_wrapper import with_supported_device_and_dtypes
 # dirichlet
 
 
@@ -49,6 +49,7 @@ def dirichlet(
     return res
 
 
+# beta
 def beta(
     alpha: Union[float, paddle.Tensor],
     beta: Union[float, paddle.Tensor],
@@ -60,7 +61,16 @@ def beta(
     seed: Optional[int] = None,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+    if seed is not None:
+        paddle.seed(seed)
+    shape = _check_bounds_and_get_shape(alpha, beta, shape)
+    dtype = paddle.float32 if dtype is None else dtype
+    beta = paddle.cast(beta, alpha.dtype)
+    dist = paddle.distribution.Beta(alpha, beta)
+    sample = dist.sample(shape)
+    sample = paddle.cast(sample, dtype)
+    sample = to_device(sample, device) if device is not None else sample
+    return sample
 
 
 def gamma(
