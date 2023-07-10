35450804c3d34812f0d84221907212ec0ace6d80
gigadev <gigadevteam666@gmail.com>
2023-07-10 21:07:27 +0300

    Added frontend paddle.nn.functional.pooling.max_pool1d

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -1,1 +1,45 @@
 # local
+import ivy
+from ivy.functional.frontends.paddle.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+
+
+def _broadcast_pooling_helper(x, pool_dims: str = "2d", name: str = "padding"):
+    dims = {"1d": 1, "2d": 2, "3d": 3}
+
+    if isinstance(x, int):
+        return tuple([x for _ in range(dims[pool_dims])])
+
+    if len(x) == 1:
+        return tuple([x[0] for _ in range(dims[pool_dims])])
+    elif len(x) == dims[pool_dims]:
+        return tuple(x)
+    elif len(x) != dims[pool_dims]:
+        raise ValueError(
+            f"`{name}` must either be a single int, "
+            f"or a tuple of {dims[pool_dims]} ints. "
+        )
+
+
+@to_ivy_arrays_and_back
+def max_pool1d(x, kernel_size, stride=None, padding=0):
+    kernel_size = _broadcast_pooling_helper(kernel_size, "1d", name="kernel_size")
+    stride = _broadcast_pooling_helper(stride, "1d", name="stride")
+    padding = _broadcast_pooling_helper(padding, "1d", name="padding")
+    kernel_pads = list(zip(kernel_size, padding))
+
+    data_format = "NCW"
+
+    if not all([pad <= kernel / 2 for kernel, pad in kernel_pads]):
+        raise ValueError(
+            "pad should be smaller than or equal to half of kernel size, "
+            f"but got padding={padding}, kernel_size={kernel_size}. "
+        )
+    # figure out whether to apply padding
+    if all([pad == ivy.ceil((kernel - 1) / 2) for kernel, pad in kernel_pads]):
+        padding_str = "SAME"
+    else:
+        padding_str = "VALID"
+
+    return ivy.max_pool1d(x, kernel_size, stride, padding_str, data_format=data_format)
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_pooling.py
@@ -1,3 +1,58 @@
 # global
+from hypothesis import strategies as st
 
 # local
+import ivy
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# max_pool1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.max_pool1d",
+    dtype_x_k_s=helpers.arrays_for_pooling(
+        min_dims=3,
+        max_dims=3,
+        min_side=1,
+        max_side=3,
+        explicit_or_str_padding=False,
+        only_explicit_padding=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_paddle_max_pool1d(
+    dtype_x_k_s,
+    *,
+    test_flags,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, kernel_size, stride, padding = dtype_x_k_s
+    # Paddle ground truth func expects input to be consistent
+    # with a channels first format i.e. NCW
+    x[0] = x[0].reshape((x[0].shape[0], x[0].shape[-1], x[0].shape[1]))
+    x_shape = [x[0].shape[2]]
+
+    # Paddle ground truth func also takes padding input as an integer
+    # or a tuple of integers, not a string
+    padding = tuple(
+        [
+            ivy.functional.layers._handle_padding(
+                x_shape[i], stride[0], kernel_size[i], padding
+            )
+            for i in range(len(x_shape))
+        ]
+    )
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+    )
