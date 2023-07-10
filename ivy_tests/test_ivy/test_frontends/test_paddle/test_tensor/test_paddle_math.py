19c89eda2a3bdd5dd129f516385af43b2e45a6f2
Arsh Pratap <62841337+arshPratap@users.noreply.github.com>
2023-07-10 20:44:56 +0530

    Added frac method to paddle frontend (#18225)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_math.py
@@ -1385,3 +1385,29 @@ def test_paddle_maximum(
         x=x[0],
         y=x[1],
     )
+
+
+# frac
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.frac",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"), num_arrays=1
+    ),
+)
+def test_paddle_frac(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
