6546e27736d680e4ea71d15bef95465eb36dc45f
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2023-07-10 13:10:14 +0000

    fix function signature

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -213,5 +213,5 @@ def mish(x, name=None):
 
 @with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
-def softplus(x, /, *, beta=1, threshold=20, name=None):
+def softplus(x, beta=1, threshold=20, name=None):
     return ivy.softplus(x, beta=beta, threshold=threshold)
