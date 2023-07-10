ba0b118e9c9afd79b76f41601f43160333266eb7
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2023-07-11 01:53:58 +0530

    lint

--- ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy/test_logic.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_jax_numpy/test_logic.py
@@ -975,9 +975,7 @@ def test_jax_iscomplexobj(
 @handle_frontend_test(
     fn_tree="jax.numpy.setxor1d",
     dtypes_values=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
-        num_arrays=2,
-        shared_dtype=True
+        available_dtypes=helpers.get_dtypes("valid"), num_arrays=2, shared_dtype=True
     ),
     assume_unique=st.booleans(),
     test_with_out=st.just(False),
