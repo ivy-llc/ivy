6d386932442109483307d61fb92953cc9275cca0
NripeshN <nn2012@hw.ac.uk>
2023-07-11 17:38:48 +0530

    Add Gather paddle manipulation frontend

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_paddle_manipulation.py
@@ -450,3 +450,34 @@ def test_paddle_broadcast_to(
         x=x[0],
         shape=shape,
     )
+
+
+# gather
+@handle_frontend_test(
+    fn_tree="paddle.gather",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=1,
+        max_num_dims=6,
+    ),
+    index=st.integers(min_value=0, max_value=10),
+)
+def test_paddle_gather(
+    *,
+    dtype_and_x,
+    index,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        index=index,
+    )
