1102e88fc907f897091fe1d4a582093d2e65c67a
Madjic-CH <a.chergui@esi-sba.dz>
2023-07-11 17:37:18 +0100

    implemented paddle.lgamma with its test in paddle frontend.

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -925,6 +925,34 @@ def test_paddle_neg(
     )
 
 
+# lgamma
+@handle_frontend_test(
+    fn_tree="paddle.lgamma",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_lgamma(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    backend_fw,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
 # exp
 @handle_frontend_test(
     fn_tree="paddle.exp",
