d73f2ff6a5fe5ffbeaaa113ca1c560e13cf9b266
Shubham Agrawal <shubham.agra1206@gmail.com>
2023-07-12 11:45:15 +0530

    Fix test

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
@@ -851,11 +851,9 @@ def test_adaptive_avg_pool3d(
     backend_fw,
     fn_name,
     on_device,
-    ground_truth_backend,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_function(
-        ground_truth_backend=ground_truth_backend,
         input_dtypes=input_dtype,
         test_flags=test_flags,
         fw=backend_fw,
