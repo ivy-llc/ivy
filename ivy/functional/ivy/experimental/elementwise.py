51b8f109e6f9d8ad2eedce2f187f7670854b340b
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-07-12 21:10:39 +0200

    Updated Elementwise Docstring to align with Array API newly added special cases (#19323)
    Added the support for complex dtypes along with the special cases in Elementwise functions

--- ivy/functional/ivy/experimental/elementwise.py
+++ ivy/functional/ivy/experimental/elementwise.py
@@ -912,7 +912,31 @@ def conj(
     out: Optional[ivy.Array] = None,
 ) -> ivy.Array:
     """
-    Compute the complex conjugate of complex values in x.
+    Return the complex conjugate for each element ``x_i`` of the input array ``x``.
+
+    For complex number of the form
+
+    .. math::
+        a + bj
+    
+    the complex conjugate is defined as
+    
+    .. math::
+        a - bj
+
+    Hence, the returned conjugates must be computed by negating
+    the imaginary component of each element ``x_i``
+
+    This method conforms to the
+    `Array API Standard <https://data-apis.org/array-api/latest/>`_.
+    This docstring is an extension of the
+    `docstring <https://data-apis.org/array-api/latest/
+    API_specification/generated/array_api.conj.html>`_
+    in the standard.
+
+    Both the description and the type hints above assumes an array input for simplicity,
+    but this function is *nestable*, and therefore also accepts :class:`ivy.Container`
+    instances in place of any of the arguments.
 
     Parameters
     ----------
