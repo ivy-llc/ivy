e45f69bf70a24719f11bfe9aa7fc74e3b0bf11f0
Kareem Morsy <kreem.morsy@hotmail.com>
2023-07-12 13:57:06 +0300

    Add a clarification

--- ivy/functional/ivy/activations.py
+++ ivy/functional/ivy/activations.py
@@ -442,6 +442,8 @@ def sigmoid(
     >>> ivy.sigmoid(x,out=y)
     >>> print(y)
     ivy.array([0.269, 0.731, 0.881])
+    
+    With :class:`ivy.Container` input:
 
     >>> x = ivy.Container(a=ivy.array([0.]),
                           b=ivy.Container(c=ivy.array([1.]),
