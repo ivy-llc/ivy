fc25f6b25910b9fbb6a2a588bee4feae7c1e8e90
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-07-12 10:38:12 +0000

    equal array API support

--- ivy/functional/ivy/elementwise.py
+++ ivy/functional/ivy/elementwise.py
@@ -2073,6 +2073,32 @@ def equal(
         an array containing the element-wise results. The returned array must have a
         data type of bool.
 
+    **Special cases**
+
+    For real-valued floating-point operands,
+
+    - If ``x1_i`` is ``NaN`` or ``x2_i`` is ``NaN``, the result is ``False``.
+    - If ``x1_i`` is ``+infinity`` and ``x2_i`` is ``+infinity``,
+    the result is ``True``.
+    - If ``x1_i`` is ``-infinity`` and ``x2_i`` is ``-infinity``,
+    the result is ``True``.
+    - If ``x1_i`` is ``-0`` and ``x2_i`` is either ``+0`` or ``-0``,
+    the result is ``True``.
+    - If ``x1_i`` is ``+0`` and ``x2_i`` is either ``+0`` or ``-0``,
+    the result is ``True``.
+    - If ``x1_i`` is a finite number, ``x2_i`` is a finite number,
+    and ``x1_i`` equals ``x2_i``, the result is ``True``.
+    - In the remaining cases, the result is ``False``.
+
+    For complex floating-point operands, let ``a = real(x1_i)``,
+    ``b = imag(x1_i)``, ``c = real(x2_i)``, ``d = imag(x2_i)``, and
+
+    - If ``a``, ``b``, ``c``, or ``d`` is ``NaN``, the result is ``False``.
+    - In the remaining cases, the result is the logical AND of the equality
+    comparison between the real values ``a`` and ``c`` (real components) and
+    between the real values ``b`` and ``d`` (imaginary components),
+    as described above for real-valued floating-point operands
+    (i.e., ``a == c AND b == d``).
 
     This method conforms to the
     `Array API Standard <https://data-apis.org/array-api/latest/>`_.
