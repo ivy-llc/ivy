fb717db97240620a309ae796686fa6f535329c7d
SARTHAK <shibugarg0303@gmail.com>
2023-07-14 02:45:51 +0530

    removed changes from backend files and added test_file for avg_pool1d

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -1,1 +1,59 @@
 # local
+import ivy
+from ivy.func_wrapper import with_supported_dtypes
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+
+
+def _broadcast_pooling_helper(x, pool_dims, name):
+    if isinstance(x, int):
+        return tuple([x for _ in range(pool_dims)])
+    if len(x) == 1:
+        return tuple([x[0] for _ in range(pool_dims)])
+    elif len(x) == pool_dims:
+        return tuple(x)
+    elif isinstance(x, str):
+        return x
+    elif len(x) != pool_dims:
+        raise ValueError(
+            f"`{name}` must either be a single int, or a tuple of {pool_dims} ints. "
+        )
+
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
+def avg_pool1d(
+    x,
+    kernel_size,
+    stride=1,
+    padding=0,
+    /,
+    *,
+    data_format: str = "NWC",
+    count_include_pad=False,
+    ceil_mode=False,
+    divisor_override=None,
+    name=None,
+):
+    kernel_size = _broadcast_pooling_helper(kernel_size, 1, name="kernel_size")
+    stride = _broadcast_pooling_helper(stride, 1, name="stride")
+    padding = _broadcast_pooling_helper(padding, 1, name="padding")
+    kernel_pads = list(zip(kernel_size, padding))
+
+    # Padding should be less than or equal to half of kernel size
+    if not isinstance(padding, str):
+        if not all([pad <= kernel // 2 for kernel, pad in kernel_pads]):
+            raise ValueError(
+                "pad should be smaller than or equal to half of kernel size, "
+                f"but got padding={padding}, kernel_size={kernel_size}. "
+            )
+
+    return ivy.avg_pool1d(
+        x,
+        kernel_size,
+        stride,
+        padding,
+        data_format=data_format,
+        count_include_pad=count_include_pad,
+        ceil_mode=ceil_mode,
+        # division_override=divisor_override,
+    )
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_paddle_pooling.py
@@ -1,3 +1,59 @@
 # global
+from hypothesis import strategies as st
 
-# local
+# local	# local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+from ivy.functional.ivy.layers import _handle_padding
+
+
+# avg_pool1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.pooling.avg_pool1d",
+    dtype_x_k_s=helpers.arrays_for_pooling(
+        min_dims=3,
+        max_dims=3,
+        min_side=1,
+        max_side=3,
+    ),
+    ceil_mode=st.booleans(),
+)
+def test_paddle_avg_pool1d(
+    dtype_x_k_s,
+    ceil_mode,
+    *,
+    test_flags,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, kernel, stride, padding = dtype_x_k_s
+
+    if len(stride) == 1:
+        stride = stride[0]
+
+    if isinstance(padding, str):
+        if padding == "SAME":
+            padding = [
+                _handle_padding(x[0].shape[i + 1], stride[i], kernel[i], padding)
+                for i in range(1)
+            ]
+        else:
+            padding = 0
+    elif len(padding) == 1:
+        pass
+    else:
+        padding = 0
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        input=x[0],
+        kernel_size=kernel,
+        stride=stride,
+        padding=padding,
+        ceil_mode=ceil_mode,
+    )
