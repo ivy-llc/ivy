84177cedfd4fbdb7dcf9e7b84d58956cef401491
Shreyansh Bardia <shrebardia13@gmail.com>
2023-07-14 02:14:26 +0530

    added input decorator

--- ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
+++ ivy/functional/frontends/numpy/manipulation_routines/changing_kind_of_array.py
@@ -1,6 +1,8 @@
 # local
+from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
 import ivy.functional.frontends.numpy as np_frontend
 
 
+@inputs_to_ivy_arrays
 def asmatrix(data, dtype=None):
     return np_frontend.matrix(data, dtype=dtype, copy=False)
