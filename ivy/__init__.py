cb7978be759f3fefe37d032ad79c980ed74bbbff
shephinphilip <shephinphilipj404@gmail.com>
2023-07-13 23:06:04 +0530

    Resolved conflict

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -1443,6 +1443,7 @@ class IvyWithGlobalProps(sys.modules[__name__].__class__):
 # Placeholder comment
 modified_class = IvyWithGlobalProps
 sys.modules[__name__].__class__ = modified_class
+
 if (
     "ivy" in sys.modules.keys()
     and sys.modules["ivy"].utils._importlib.IS_COMPILING_WITH_BACKEND
