51cacc77ab91a44d0dea8c4f9458354c60cf7ce9
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-07-13 22:25:26 +0100

    refactor self.data to a property that depends on the underlying array of the Series and returns the data in original format

--- ivy/functional/frontends/pandas/series.py
+++ ivy/functional/frontends/pandas/series.py
@@ -7,9 +7,9 @@ class Series:
         self, data, index=None, dtype=None, name=None, copy=False, fastpath=False
     ):
         self.name = name
+        self.orig_data = data
         if ivy.is_native_array(data):
-            data = ivy.Array(data)
-            self.array = data
+            self.array = ivy.array(data)
 
         # repeatedly used checks
         data_is_array = isinstance(data, (ivy.Array, np.ndarray))
@@ -31,21 +31,18 @@ class Series:
                 index = list(data.keys())
 
         if data_is_array_or_like:
-            self.data = data
             self.index = index
             self.array = ivy.array(data)
 
         elif isinstance(data, dict):
             self.index = index
-            self.data = list(data.values())
-            self.array = ivy.array(self.data)
+            self.array = ivy.array(list(data.values()))
 
         elif isinstance(data, (int, float)):
             if len(index) > 1:
                 data = [data] * len(index)
-            self.data = data
             self.index = index
-            self.array = ivy.array(self.data)
+            self.array = ivy.array(data)
         elif isinstance(data, str):
             pass # TODO: implement string series
         else:
@@ -54,3 +51,13 @@ class Series:
     def __repr__(self):
         series_name = f"{self.name} " if self.name is not None else ""
         return f"frontends.pandas.Series {series_name}({self.array.to_list()}, index={self.index})"
+
+    @property
+    def data(self):
+        # return underlying data in the original format
+        ret = self.array.to_list()
+        if isinstance(self.orig_data, tuple):
+            ret = tuple(ret)
+        elif isinstance(self.orig_data, dict):
+            ret = dict(zip(self.orig_data.keys(), ret))
+        return ret
