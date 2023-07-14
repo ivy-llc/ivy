c6749a41cf1329ead820d6389b1346b384dfde39
Madjic-CH <a.chergui@esi-sba.dz>
2023-07-14 13:21:42 +0100

    updated instance_norm function

--- ivy/functional/frontends/paddle/nn/functional/norm.py
+++ ivy/functional/frontends/paddle/nn/functional/norm.py
@@ -6,8 +6,8 @@ from ivy import with_supported_dtypes, to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def instance_norm(x,
-                  running_mean=None,
-                  running_var=None,
+                  running_mean,
+                  running_var,
                   weight=None,
                   bias=None,
                   use_input_stats=True,
@@ -15,6 +15,11 @@ def instance_norm(x,
                   eps=1e-05,
                   data_format='NCHW',
                   name=None):
-    return ivy.instance_norm(x, running_mean, running_var,
-                             eps=eps, momentum=momentum,
-                             data_format=data_format, training=use_input_stats)
+    return ivy.instance_norm(x,
+                             scale=None,
+                             bias=bias,
+                             eps=eps,
+                             momentum=momentum,
+                             data_format=data_format,
+                             running_mean=running_mean,
+                             running_stddev=running_var)
