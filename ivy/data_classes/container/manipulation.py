ca61c4efbabc9bff0709fe263c52febf4f2a6b27
Tugay Gül <sonder.tg@gmail.com>
2023-07-15 22:21:55 +0300

    fix container.squeeze doc test

--- ivy/data_classes/container/manipulation.py
+++ ivy/data_classes/container/manipulation.py
@@ -1249,7 +1249,7 @@ class _ContainerWithManipulation(ContainerBase):
         --------
         >>> x = ivy.Container(a=ivy.array([[[10.], [11.]]]),
         ...                   b=ivy.array([[[11.], [12.]]]))
-        >>> y = x.squeeze(2)
+        >>> y = x.squeeze(axis=2)
         >>> print(y)
         {
             a: ivy.array([[10., 11.]]),
@@ -1258,11 +1258,13 @@ class _ContainerWithManipulation(ContainerBase):
 
         >>> x = ivy.Container(a=ivy.array([[[10.], [11.]]]),
         ...                   b=ivy.array([[[11.], [12.]]]))
-        >>> y = x.squeeze(0)
+        >>> y = x.squeeze(axis=0)
         >>> print(y)
         {
-            a: ivy.array([[10.], [11.]]),
-            b: ivy.array([[11.], [12.]])
+            a: ivy.array([[10.],
+                          [11.]]),
+            b: ivy.array([[11.],
+                          [12.]])
         }
         """
         return self._static_squeeze(
