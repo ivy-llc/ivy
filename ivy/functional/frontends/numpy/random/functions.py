9ea293971145bcc7e57d62b4153dc0d2920861be
VedantPol <polvedant7@gmail.com>
2023-07-17 17:27:53 +0530

    changed valueError to IvyValueError

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -189,7 +189,9 @@ def gumbel(loc=0.0, scale=1.0, size=None):
 @from_zero_dim_arrays_to_scalar
 def triangular(left, mode, right, size=None):
     if left > mode or mode > right or left == right:
-        raise ValueError("left < mode < right is not being followed")
+        raise ivy.utils.exceptions.IvyValueError(
+            "left < mode < right is not being followed"
+        )
     u = ivy.random_uniform(low=0.0, high=1.0, shape=size, dtype="float64")
     condition = u <= (mode - left) / (right - left)
     values1 = left + (right - left) * (u * (mode - left) / (right - left)) ** 0.5
