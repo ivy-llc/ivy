b74968605f85425b327c94d65c224653842b4e14
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-07-17 21:37:29 +0100

    add test for to_numpy method and add tests for DataFrame too. commented out until frontend tests are independent of a backend

--- ivy_tests/test_ivy/test_frontends/test_pandas/test_series.py
+++ ivy_tests/test_ivy/test_frontends/test_pandas/test_series.py
@@ -1,7 +1,6 @@
 # tests for the pandas frontend Series class methods
 
-
-CLASS_TREE = "ivy.functional.frontends.pandas.series.Series"
+CLASS_TREE = "ivy.functional.frontends.pandas.Series"
 
 # ToDo: uncomment this when frontend tests are independent of backends
 
@@ -34,3 +33,35 @@ CLASS_TREE = "ivy.functional.frontends.pandas.series.Series"
 #         frontend=frontend,
 #         on_device=on_device,
 #     )
+
+
+# @handle_frontend_method(
+#     class_tree=CLASS_TREE,
+#     init_tree="pandas.Series",
+#     method_name="to_numpy",
+#     dtype_x=helpers.dtype_and_values(
+#         available_dtypes=helpers.get_dtypes("valid"))
+# )
+# def test_pandas_series_abs(
+#     dtype_x,
+#     frontend,
+#     frontend_method_data,
+#     init_flags,
+#     method_flags,
+#     on_device,
+# ):
+#     input_dtype, x = dtype_x
+#     helpers.test_frontend_method(
+#         init_input_dtypes=input_dtype,
+#         init_all_as_kwargs_np={
+#             "data": x[0],
+#         },
+#         method_input_dtypes=input_dtype,
+#         method_all_as_kwargs_np={},
+#         frontend_method_data=frontend_method_data,
+#         init_flags=init_flags,
+#         method_flags=method_flags,
+#         frontend=frontend,
+#         on_device=on_device,
+#     )
+
