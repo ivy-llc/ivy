6ca9208d944d99d0c797a3045cbef2929ae10ff5
Shreyansh Bardia <shrebardia13@gmail.com>
2023-07-18 15:32:29 +0530

    removed leading underscore

--- ivy/functional/frontends/numpy/broadcast/methods.py
+++ ivy/functional/frontends/numpy/broadcast/methods.py
@@ -7,50 +7,50 @@ class broadcast:
     @to_ivy_arrays_and_back
     def __init__(self, *args):
         data = ivy.broadcast_arrays(*map(ivy.array, args))
-        self.__shape = data[0].shape
-        self.__ndim = data[0].ndim
-        self.__index = 0
-        self.__numiter = len(data)
-        self.__size = data[0].size
-        self.__data = tuple((*zip(*(ivy.flatten(i) for i in data)),))
-        self.__iters = tuple((iter(ivy.flatten(i)) for i in data))
+        self._shape = data[0].shape
+        self._ndim = data[0].ndim
+        self._index = 0
+        self._numiter = len(data)
+        self._size = data[0].size
+        self._data = tuple((*zip(*(ivy.flatten(i) for i in data)),))
+        self._iters = tuple((iter(ivy.flatten(i)) for i in data))
 
     @property
     def shape(self):
-        return self.__shape
+        return self._shape
 
     @property
     def ndim(self):
-        return self.__ndim
+        return self._ndim
 
     @property
     def nd(self):
-        return self.__ndim
+        return self._ndim
 
     @property
     def numiter(self):
-        return self.__numiter
+        return self._numiter
 
     @property
     def size(self):
-        return self.__size
+        return self._size
 
     @property
     def iters(self):
-        return self.__iters
+        return self._iters
 
     @property
     def index(self):
-        return self.__index
+        return self._index
 
     def __next__(self):
         if self.index < self.size:
-            self.__index += 1
-            return self.__data[self.index - 1]
+            self._index += 1
+            return self._data[self.index - 1]
         raise StopIteration
 
     def __iter__(self):
         return self
 
     def reset(self):
-        self.__index = 0
+        self._index = 0
