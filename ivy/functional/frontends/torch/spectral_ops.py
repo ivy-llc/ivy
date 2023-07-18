03869adb016e28aa05f053f3a21a0e24f568e409
ManoBharathi93 <immanobharathi21@gmail.com>
2023-07-18 15:12:53 +0530

    hann_window_updated

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -2,18 +2,10 @@ import ivy
 from ivy.functional.frontends.torch.func_wrapper import (
     to_ivy_arrays_and_back,
 )
+from ivy.func_wrapper import with_supported_dtypes
 
 
+@with_supported_dtypes({"2.0.1 and below": "int"}, "torch")
 @to_ivy_arrays_and_back
-def hann_window(
-    window_length,
-    periodic=True,
-    *,
-    dtype=None,
-    device,
-    layout=ivy.strides,
-    required_grad=False
-) -> ivy.Array:
-    return ivy.hann_window(
-        window_length, periodic, dtype, layout, device, required_grad
-    )
+def hann_window(size, *, periodic=True, dtype=None, out=None) -> ivy.Array:
+    return ivy.hann_window(size, periodic, dtype, out)
--- ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
@@ -1,35 +1,22 @@
 import ivy_tests.test_ivy.helpers as helpers
-from ivy_tests.test_ivy.test_frontends.test_torch.test_tensor import _requires_grad
 from hypothesis import strategies as st
 from ivy_tests.test_ivy.helpers.testing_helpers import handle_frontend_test
 
 
 @handle_frontend_test(
     fn_tree="torch.hann_window",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric")
-    ),
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("int")),
     periodic=st.booleans(),
-    dtype=helpers.get_dtypes("numeric", full=False, none=True),
-    requires_grad=_requires_grad(),
+    dtype=helpers.get_dtypes("float", full=False, none=True),
 )
-def test_torch_hann_window(
-    periodic,
-    dtype_and_x,
-    dtype,
-    num_positional_args: 2,
-    on_device,
-    frontend,
-    requires_grad,
-):
+def test_torch_hann_window(periodic, dtype_and_x, dtype, fn_tree, frontend, test_flags):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
-        frontend=frontend,
-        window_length=x[0],
+        test_flags=test_flags,
+        fn_tree=fn_tree,
         periodic=periodic,
         dtype=dtype[0],
-        on_device=on_device,
-        requires_grad=requires_grad,
+        size=x[0],
     )
