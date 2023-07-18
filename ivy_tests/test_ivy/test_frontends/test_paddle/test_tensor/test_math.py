36d012c7385bde99d5e6a261199c0394fc5a328f
Ookamice <49585613+Ookamice@users.noreply.github.com>
2023-07-18 09:45:55 +0100

    Changed test dtype from float to valid

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -1508,7 +1508,7 @@ def test_paddle_lerp(
 @handle_frontend_test(
     fn_tree="paddle.tensor.math.outer",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
+        available_dtypes=helpers.get_dtypes("valid"),
         num_arrays=2,
         shared_dtype=True,
     ),
