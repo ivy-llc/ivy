d57634a526deaed914325785519c07224c2256ee
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-07-18 10:49:01 +0500

    setting forment

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -354,9 +354,7 @@ def max(x, axis=None, keepdim=False, name=None):
     return ivy.max(x, axis=axis, keepdims=keepdim)
 
 
-@with_supported_dtypes(
-    {"2.5.0 and below": ("float32", "float64")}, "paddle"
-)
+@with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def lerp(x, y, weight, name=None):
     return ivy.lerp(x, y, weight)
