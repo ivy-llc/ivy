4502ae3495d137ec391f633b0a48496333cc2381
Alrizki Pasca <87491167+alrizkipascar@users.noreply.github.com>
2023-07-19 04:48:41 +0700

    test_experimental/test_core/test_statistical.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_statistical.py
@@ -639,3 +639,72 @@ def test_nanmedian(
         keepdims=keep_dims,
         overwrite_input=overwriteinput,
     )
+
+
+@st.composite
+def _percentile_helper(draw):
+    dtype, values, axis = draw(
+        helpers.dtype_values_axis(
+            available_dtypes=helpers.get_dtypes("float"),
+            min_num_dims=1,
+            max_num_dims=5,
+            min_dim_size=2,
+            large_abs_safety_factor=2,
+            small_abs_safety_factor=2,
+            valid_axis=True,
+            allow_neg_axes=False,
+            min_axes_size=1,
+            force_int_axis=True,
+        )
+    )
+    q = draw(
+        helpers.array_values(
+            dtype=helpers.get_dtypes("float"),
+            shape=helpers.get_shape(min_dim_size=1, max_num_dims=1, min_num_dims=1),
+            min_value=0,
+            max_value=100,
+            exclude_max=False,
+            exclude_min=False,
+        )
+    )
+    interpolation = draw(
+        helpers.list_of_size(
+            x=st.sampled_from(["linear", "lower", "higher", "midpoint", "nearest"]),
+            size=1,
+        )
+    )
+    return dtype, values, axis, interpolation, q
+
+
+# percentile
+@handle_test(
+    fn_tree="functional.ivy.experimental.percentile",
+    dtype_and_x=_percentile_helper(),
+    keep_dims=st.booleans(),
+    test_gradients=st.just(False),
+    test_with_out=st.just(False),
+)
+def test_percentile(
+    *,
+    dtype_and_x,
+    keep_dims,
+    test_flags,
+    backend_fw,
+    fn_name,
+    on_device,
+    ground_truth_backend,
+):
+    input_dtype, values, axis, interpolation, q = dtype_and_x
+    helpers.test_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        ground_truth_backend=ground_truth_backend,
+        fw=backend_fw,
+        fn_name=fn_name,
+        on_device=on_device,
+        a=values[0],
+        q=q,
+        axis=axis,
+        interpolation=interpolation[0],
+        keepdims=keep_dims,
+    )
