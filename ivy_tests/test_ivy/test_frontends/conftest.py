5dd182ba4915715ad42b939ffc7d1d83fc8acc9a
CatB1t <skytedits@gmail.com>
2023-07-18 04:16:08 +0300

    Fix `run_around_test` to yield a valid value for frontends.

--- ivy_tests/test_ivy/test_frontends/conftest.py
+++ ivy_tests/test_ivy/test_frontends/conftest.py
@@ -5,15 +5,15 @@ from ivy_tests.test_ivy.helpers import globals as test_globals
 
 @pytest.fixture(autouse=True)
 def run_around_tests(request, on_device, backend_fw, frontend, compile_graph, implicit):
-    if not hasattr(request.function, "test_data"):
-        return
-
-    try:
-        test_globals.setup_frontend_test(
-            request.function.test_data, frontend, backend_fw, on_device
-        )
-    except Exception as e:
+    if hasattr(request.function, "test_data"):
+        try:
+            test_globals.setup_frontend_test(
+                request.function.test_data, frontend, backend_fw, on_device
+            )
+        except Exception as e:
+            test_globals.teardown_frontend_test()
+            raise RuntimeError(f"Setting up test for {request.function} failed.") from e
+        yield
         test_globals.teardown_frontend_test()
-        raise RuntimeError(f"Setting up test for {request.function} failed.") from e
-    yield
-    test_globals.teardown_frontend_test()
+    else:
+        yield
