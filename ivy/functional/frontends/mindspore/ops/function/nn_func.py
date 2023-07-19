dd9c0caf2116c4469b9cb4e45979691ab4e43e4f
diksha-shrivastava13 <E21CSEU0293@bennett.edu.in>
2023-07-19 15:59:47 +0530

    changed to ivy.selu

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -4,12 +4,10 @@ https://github.com/unifyai/ivy/issues/14951."""
 # local
 import ivy
 from ivy.func_wrapper import with_supported_dtypes
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 
 
 @with_supported_dtypes({"2.0.0 and below": ("float16", "float32")}, "mindspore")
+@to_ivy_arrays_and_back
 def selu(input_x):
-    alpha = 1.67326324
-    scale = 1.05070098
-    ret = ivy.where(input_x > 0, input_x, alpha * ivy.expm1(input_x))
-    arr = scale * ret
-    return ivy.astype(arr, input_x.dtype)
+    return ivy.selu(input_x)
