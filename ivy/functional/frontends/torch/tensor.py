bac8fbb675f3d208a77b44123a0b308181645c99
Amritds <amritds18@gmail.com>
2023-07-19 18:54:51 +0530

    added instance method for torch frontend logaddexp function, with corresponding test

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1566,6 +1566,10 @@ class Tensor:
             else:
                 next_function(_grad_list[idx])
 
+    @with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
+    def logaddexp(self, other):
+        return torch_frontend.logaddexp(self, other)
+
 
 class Size(tuple):
     def __new__(cls, iterable=()):
