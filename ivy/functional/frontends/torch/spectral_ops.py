d5b63814ad3e8fe7ceef2580368b9b0bac9dda3d
ManoBharathi93 <immanobharathi21@gmail.com>
2023-07-19 18:57:56 +0530

    remove_device_kwarg

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -8,12 +8,8 @@ from ivy.func_wrapper import with_supported_dtypes
 @with_supported_dtypes({"2.0.1 and below": "int"}, "torch")
 @to_ivy_arrays_and_back
 def hann_window(
-    window_length,
-    periodic=True,
-    *,
-    dtype=None,
-    device=None,
-    requires_grad=False,
-    out=None
+    window_length, periodic=True, *, dtype=None, requires_grad=False, out=None
 ):
-    return ivy.hann_window(window_length, periodic, dtype, out=out)
+    return ivy.hann_window(
+        window_length, periodic, dtype, requires_grad=requires_grad, out=out
+    )
--- ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
@@ -1,6 +1,7 @@
 import ivy_tests.test_ivy.helpers as helpers
 from hypothesis import strategies as st
 from ivy_tests.test_ivy.helpers.testing_helpers import handle_frontend_test
+from ivy_tests.test_ivy.test_frontends.test_torch.test_tensor import _requires_grad
 
 
 @handle_frontend_test(
@@ -8,17 +9,24 @@ from ivy_tests.test_ivy.helpers.testing_helpers import handle_frontend_test
     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("int")),
     periodic=st.booleans(),
     dtype=helpers.get_dtypes("float", full=False, none=True),
+    requires_grad=_requires_grad(),
 )
 def test_torch_hann_window(
-    periodic, dtype_and_x, dtype, fn_tree, frontend, on_device, test_flags
+    periodic,
+    dtype_and_x,
+    dtype,
+    fn_tree,
+    frontend,
+    test_flags,
+    requires_grad,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
-        on_device=on_device,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
+        requires_grad=requires_grad,
         periodic=periodic,
         dtype=dtype[0],
         window_length=x[0],
