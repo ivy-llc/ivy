e7a3314998d74e1f02548f14605aaa2640bc0500
Mano Bharathi M <88357044+ManoBharathi93@users.noreply.github.com>
2023-07-19 06:49:02 +0530

    Update test_spectral_ops.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
@@ -1,22 +1,24 @@
 import ivy_tests.test_ivy.helpers as helpers
 from hypothesis import strategies as st
 from ivy_tests.test_ivy.helpers.testing_helpers import handle_frontend_test
-
+from ivy_tests.test_ivy.test_frontends.test_torch.test_tensor import _requires_grad
 
 @handle_frontend_test(
     fn_tree="torch.hann_window",
     dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("int")),
     periodic=st.booleans(),
     dtype=helpers.get_dtypes("float", full=False, none=True),
+    requires_grad=_requires_grad(),
 )
-def test_torch_hann_window(periodic, dtype_and_x, dtype, fn_tree, frontend, test_flags):
+def test_torch_hann_window(periodic, dtype_and_x, dtype, fn_tree, frontend,requires_grad, test_flags):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         frontend=frontend,
+        requires_grad=requires_grad,
         test_flags=test_flags,
         fn_tree=fn_tree,
         periodic=periodic,
         dtype=dtype[0],
-        size=x[0],
+        window_length=x[0],
     )
