2c3aa2007120891765b04517ccd6b7ac79c6feff
Shreyansh Bardia <shrebardia13@gmail.com>
2023-07-19 22:45:14 +0530

    updated test

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
@@ -2,31 +2,82 @@
 from hypothesis import strategies as st
 
 # local
-import ivy_tests.test_ivy.test_functional.test_experimental.test_core.test_statistical as test_statistical
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
+import numpy as np
 
 
 @st.composite
-def _a_bins_weights_range(draw):
-    a, bin_size, *_, range, weights, _, dtype_input = draw(
-        test_statistical._histogram_helper()
+def _histogram_helper(draw):
+    dtype_input = draw(st.sampled_from(draw(helpers.get_dtypes("float"))))
+    bins = draw(
+        helpers.array_values(
+            dtype=dtype_input,
+            shape=(draw(helpers.ints(min_value=1, max_value=10)),),
+            abs_smallest_val=-10,
+            min_value=-10,
+            max_value=10,
+        )
     )
-    bin_selectors = [
-        "stone",
-        "auto",
-        "doane",
-        "fd",
-        "rice",
-        "scott",
-        "sqrt",
-        "sturges",
-        bin_size,
-    ]
+    bins = np.asarray(sorted(set(bins)), dtype=dtype_input)
+    if len(bins) == 1:
+        bins = int(abs(bins[0]))
+        if bins == 0:
+            bins = 1
+        if dtype_input in draw(helpers.get_dtypes("unsigned")):
+            range = (
+                draw(
+                    helpers.floats(
+                        min_value=0, max_value=10, exclude_min=False, exclude_max=False
+                    )
+                ),
+                draw(
+                    helpers.floats(
+                        min_value=11, max_value=20, exclude_min=False, exclude_max=False
+                    )
+                ),
+            )
+        else:
+            range = (
+                draw(helpers.floats(min_value=-10, max_value=0)),
+                draw(helpers.floats(min_value=1, max_value=10)),
+            )
+        range = draw(st.sampled_from([range, None]))
+    else:
+        range = None
+    shape = draw(
+        helpers.get_shape(
+            min_num_dims=1, max_num_dims=1, min_dim_size=5, max_dim_size=20
+        )
+    )
+    a = draw(
+        helpers.array_values(
+            dtype=dtype_input,
+            shape=shape,
+            min_value=-20,
+            max_value=20,
+        )
+    )
+    weights = draw(
+        helpers.array_values(
+            dtype=dtype_input,
+            shape=shape,
+            min_value=-20,
+            max_value=20,
+        )
+    )
+    # "stone",
+    bin_selectors = ["auto", "doane", "fd", "rice", "scott", "sqrt", "sturges", bins]
     bins = draw(st.sampled_from(bin_selectors))
     if isinstance(bins, str):
         weights = None
-    return a, bins, weights, range
+    weights = draw(st.sampled_from([weights, None]))
+    return (
+        a,
+        bins,
+        range,
+        weights,
+    )
 
 
 # bincount
@@ -67,7 +118,7 @@ def test_numpy_bincount(
     )
 
 
-@handle_frontend_test(fn_tree="numpy.histogram_bin_edges", data=_a_bins_weights_range())
+@handle_frontend_test(fn_tree="numpy.histogram_bin_edges", data=_histogram_helper())
 def test_numpy_histogram_bin_edges(
     *,
     data,
@@ -76,7 +127,7 @@ def test_numpy_histogram_bin_edges(
     frontend,
     test_flags,
 ):
-    a, bins, weights, range = data
+    a, bins, range, weights = data
     helpers.test_frontend_function(
         frontend=frontend,
         input_dtypes=[a.dtype],
