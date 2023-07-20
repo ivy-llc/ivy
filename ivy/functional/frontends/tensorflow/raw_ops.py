1fb230204825e1881ac0cb4b4e39f67bde2260ca
MuminAhmadKhan <muminkhan2000@gmail.com>
2023-07-20 10:21:01 +0530

    Removed commented function implementation

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -906,8 +906,4 @@ Imag.supported_dtypes = {
     ),
 }
 
-# @to_ivy_arrays_and_back
-# def ExpandDims(* , input, axis, name=None):
-#     return ivy.expand_dims(input, axis=axis)
-
 ExpandDims = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.expand_dims))
