35d79dc899d55bb1988596a953575b733cb363df
HaiderSultanArc <haidersultanarc@gmail.com>
2023-07-20 06:11:36 +0500

    paddle adaptive_avg_pool2d

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
@@ -140,3 +140,43 @@ def test_paddle_adaptive_avg_pool1d(
         x=x[0],
         output_size=output_size,
     )
+
+
+# adaptive_avg_pool2d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.adaptive_avg_pool2d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=3,
+        max_num_dims=4,
+        min_dim_size=1,
+        max_value=100,
+        min_value=-100,
+    ),
+    output_size=st.one_of(
+        st.tuples(
+            helpers.ints(min_value=1, max_value=5),
+            helpers.ints(min_value=1, max_value=5),
+        ),
+        helpers.ints(min_value=1, max_value=5),
+    ),
+)
+def test_paddle_adaptive_avg_pool2d(
+    *,
+    dtype_and_x,
+    output_size,
+    test_flags,
+    frontend,
+    on_device,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        output_size=output_size,
+    )
