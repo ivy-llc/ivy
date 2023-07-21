061df749b147380fd1b0a48eac074a746612afd9
CatB1t <skytedits@gmail.com>
2023-07-21 13:51:28 +0300

    Add frontends config in testing.

---
+++ ivy_tests/test_ivy/test_frontends/config/numpy.py
@@ -0,0 +1,70 @@
+valid_devices = "cpu"
+invalid_devices = ("gpu", "tpu")
+
+valid_dtypes = [
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "float16",
+    "float32",
+    "float64",
+    "complex64",
+    "complex128",
+    "bool",
+]
+invalid_dtypes = ["bfloat16"]
+
+valid_numeric_dtypes = [
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "float16",
+    "float32",
+    "float64",
+    "complex64",
+    "complex128",
+]
+invalid_numeric_dtypes = ["bfloat16"]
+
+valid_int_dtypes = [
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+]
+invalid_int_dtypes = []
+
+valid_uint_dtypes = [
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+]
+invalid_uint_dtypes = []
+
+valid_float_dtypes = [
+    "float16",
+    "float32",
+    "float64",
+]
+invalid_float_dtypes = ["bfloat16"]
+
+valid_complex_dtypes = [
+    "complex64",
+    "complex128",
+]
+invalid_complex_dtypes = []
