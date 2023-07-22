93ea12befe3cb7c84d88853b499f86d581c3a1ff
72santhi <83107730+72santhi@users.noreply.github.com>
2023-07-22 22:22:05 +0530

    Update functions.py

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -172,18 +172,7 @@ def standard_cauchy(size=None):
 @from_zero_dim_arrays_to_scalar
 def rayleigh(scale, size=None):
     u = ivy.random_uniform(low=0.0, high=1.0, shape=size, dtype="float64")
-    log_u = ivy.log(u)def triangular(left, mode, right, size=None):
-    if left > mode or mode > right or left == right:
-        raise ivy.utils.exceptions.IvyValueError(
-            "left < mode < right is not being followed"
-        )
-    u = ivy.random_uniform(low=0.0, high=1.0, shape=size, dtype="float64")
-    condition = u <= (mode - left) / (right - left)
-    values1 = left + (right - left) * (u * (mode - left) / (right - left)) ** 0.5
-    values2 = (
-        right - (right - mode) * ((1 - u) * (right - mode) / (right - left)) ** 0.5
-    )
-    return ivy.where(condition, values1, values2)
+    log_u = ivy.log(u)
     x = ivy.multiply(scale, ivy.sqrt(ivy.multiply(-2, log_u)))
     return x
 
