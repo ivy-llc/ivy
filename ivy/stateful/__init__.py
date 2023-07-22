471f3743d881ff5f59aaa7067a9bb2abc76a74fb
Haider Sultan  ⚡ <haidersultanarc@gmail.com>
2023-07-22 23:54:05 +0500

    Automated __all__ in __init__.py file (#19891)

--- ivy/stateful/__init__.py
+++ ivy/stateful/__init__.py
@@ -14,73 +14,17 @@ from . import optimizers
 from .optimizers import *
 from . import sequential
 from .sequential import *
-
+import types
 
 __all__ = [
-    "activations",
-    "GEGLU",
-    "GELU",
-    "Hardswish",
-    "LeakyReLU",
-    "Logit",
-    "LogSoftmax",
-    "Mish",
-    "PReLU",
-    "ReLU",
-    "ReLU6",
-    "Sigmoid",
-    "SiLU",
-    "Softmax",
-    "Softplus",
-    "Tanh",
-    "converters",
-    "ModuleConverters",
-    "to_ivy_module",
-    "initializers",
-    "Constant",
-    "FirstLayerSiren",
-    "GlorotUniform",
-    "Initializer",
-    "KaimingNormal",
-    "Ones",
-    "RandomNormal",
-    "Siren",
-    "Uniform",
-    "Zeros",
-    "layers",
-    "Linear",
-    "Dropout",
-    "MultiHeadAttention",
-    "Conv1D",
-    "Conv1DTranspose",
-    "Conv2D",
-    "Conv2DTranspose",
-    "DepthwiseConv2D",
-    "Conv3D",
-    "Conv3DTranspose",
-    "LSTM",
-    "MaxPool1D",
-    "MaxPool2D",
-    "MaxPool3D",
-    "AvgPool1D",
-    "AvgPool2D",
-    "AvgPool3D",
-    "AdaptiveAvgPool1d",
-    "AdaptiveAvgPool2d",
-    "FFT",
-    "Dct",
-    "Embedding",
-    "module",
-    "Module",
-    "norms",
-    "LayerNorm",
-    "BatchNorm2D",
-    "optimizers",
-    "Adam",
-    "LAMB",
-    "LARS",
-    "Optimizer",
-    "SGD",
-    "sequential",
-    "Sequential",
+    name
+    for name, thing in globals().items()
+    if not (
+        name.startswith("_")
+        or name == "ivy"
+        or name == "helpers"
+        or (callable(thing) and "ivy" not in thing.__module__)
+        or (isinstance(thing, types.ModuleType) and "ivy" not in thing.__name__)
+    )
 ]
+del types
