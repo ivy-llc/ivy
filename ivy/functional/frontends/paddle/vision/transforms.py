85d5548ab21ad4cdec3ff5d6af3efaee4e422ac6
alec-kr <52685467+alec-kr@users.noreply.github.com>
2023-07-22 23:37:38 -0400

    Add adjust_brightness to transforms.py

--- ivy/functional/frontends/paddle/vision/transforms.py
+++ ivy/functional/frontends/paddle/vision/transforms.py
@@ -1,1 +1,10 @@
 # global
+import ivy
+from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
+import ivy.functional.frontends.paddle as paddle
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+
+@with_unsupported_dtypes({"2.5.0 and below": ("float16", "bfloat16")}, "paddle")
+# Return the brightness-adjusted image
+def adjust_brightness(img, brightness_factor):
+  return paddle.vision.transforms.adjust_brightness(img, brightness_factor)
