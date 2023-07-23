82784ec08e044f6c1b834ef81b24b27c8f337d3e
Bhushan Srivastava <bhushansrivastava2001@gmail.com>
2023-07-23 23:06:33 +0530

    Reverting log1p test method

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -9794,31 +9794,28 @@ def test_torch_instance_stride(
     class_tree=CLASS_TREE,
     init_tree="torch.tensor",
     method_name="log1p",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        allow_inf=False,
+    dtype_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
     ),
 )
 def test_torch_instance_log1p(
-    dtype_and_x,
+    dtype_x,
+    frontend,
     frontend_method_data,
     init_flags,
     method_flags,
-    frontend,
     on_device,
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x = dtype_x
     helpers.test_frontend_method(
         init_input_dtypes=input_dtype,
-        init_all_as_kwargs_np={
-            "data": x[0],
-        },
+        init_all_as_kwargs_np={"data": x[0]},
         method_input_dtypes=input_dtype,
         method_all_as_kwargs_np={},
+        frontend=frontend,
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
-        frontend=frontend,
         on_device=on_device,
     )
 
