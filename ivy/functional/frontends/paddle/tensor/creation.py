87d8249093e029e358b196afb992320e8c6e855e
Iggy Galang <kalachuchis@wsl.com>
2023-07-23 13:58:13 +0800

    paddle tensor creation clone

--- ivy/functional/frontends/paddle/tensor/creation.py
+++ ivy/functional/frontends/paddle/tensor/creation.py
@@ -139,3 +139,9 @@ def meshgrid(*args, **kwargs):
 @to_ivy_arrays_and_back
 def logspace(start, stop, num, base=10.0, dtype=None, name=None):
     return ivy.logspace(start, stop, num=num, base=base, dtype=dtype)
+
+
+@with_unsupported_dtypes({"2.5.0 and below": ("bfloat16", "uint16")}, "paddle")
+@to_ivy_arrays_and_back
+def clone(x):
+    return ivy.copy_array(x)
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_creation.py
@@ -555,3 +555,27 @@ def test_paddle_logspace(
         base=base,
         dtype=dtype[0],
     )
+
+
+# clone
+@handle_frontend_test(
+    fn_tree="paddle.clone",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+)
+def test_paddle_clone(
+    *,
+    dtype_and_x,
+    test_flags,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
