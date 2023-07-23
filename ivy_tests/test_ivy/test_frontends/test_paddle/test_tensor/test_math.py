f028e050e1eeefa82a718f7704ed791e97cc11f2
NripeshN <nn2012@hw.ac.uk>
2023-07-23 13:03:08 +0530

    Add prod paddle frontend math

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -1597,3 +1597,37 @@ def test_paddle_rsqrt(
         on_device=on_device,
         x=x[0],
     )
+
+
+# prod
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.prod",
+    dtype_and_x=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_axis=-1,
+        max_axis=0,
+        min_num_dims=1,
+        force_int_axis=False,
+        allow_inf=False,
+        allow_nan=False,
+    ),
+)
+def test_paddle_prod(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x, axis = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        axis=axis,
+        keepdim=False,
+    )
