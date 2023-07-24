23d94c3a30f80857115110dce9a4d034c7909832
Okeoma Amaobi <92605808+OkeyAmy@users.noreply.github.com>
2023-07-24 08:57:50 +0100

    Update test_mathematical_functions.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_mathematical_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_mathematical_functions.py
@@ -3037,3 +3037,27 @@ def test_jax_conjugate(
         on_device=on_device,
         x=x[0],
     )
+
+#sin
+@handle_frontend_test(
+    fn_tree="jax.numpy.sin",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_jax_sin(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
