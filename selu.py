5c50acc1e2e052a4793e7744150e5eb16186e41a
SaimR036 <98948522+SaimR036@users.noreply.github.com>
2023-07-24 11:17:41 +0000

    Pending changes exported from your codespace

---
+++ selu.py
@@ -0,0 +1,28 @@
+import tensorflow
+import jax
+import numpy
+import ivy
+def SELU(x , lambdaa = 1.0507, alpha = 1.6732):
+    y = 1
+    if type(x) == type(y):
+        if x >= 0:
+            return ivy.array(int(lambdaa * x))
+        else:
+            return ivy.array(int(lambdaa * alpha * (ivy.exp(x) - 1)))
+    elif len(x) == 0:
+        if x >= 0:
+            return ivy.array(int(lambdaa * x))
+        else:
+            return ivy.array(int(lambdaa * alpha * (ivy.exp(x) - 1)))
+    shape = ivy.Shape((len(x),))
+    fill_value = ivy.Container(a = 1)
+    dtype = ivy.Container(a = ivy.int32)
+    arr = ivy.full(shape, fill_value, dtype = dtype)
+    arr = arr["a"]
+    print(arr)
+    for i in range(len(x)):
+        if x[i] >= 0:
+            arr[i] = int(lambdaa * x[i])
+        else:
+            arr[i] = int(lambdaa * alpha * (ivy.exp(x[i]) - 1))
+    return arr
