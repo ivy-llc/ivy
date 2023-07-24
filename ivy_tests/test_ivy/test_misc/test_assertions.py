78f63f0c1d2a0184c4a603b53d6d7a4c52b4d351
adityagandhamal <adityagandhamal1327@gmail.com>
2023-07-24 16:19:23 +0530

    test check_equal

--- ivy_tests/test_ivy/test_misc/test_assertions.py
+++ ivy_tests/test_ivy/test_misc/test_assertions.py
@@ -2,7 +2,7 @@ import sys
 import os
 import contextlib
 import pytest
-from ivy.utils.assertions import check_greater, check_less
+from ivy.utils.assertions import check_equal, check_greater, check_less
 
 
 @pytest.mark.parametrize(
@@ -76,6 +76,49 @@ def test_check_greater(x1, x2, allow_equal):
 
     if x1 > x2:
         assert not lines.strip()
+
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(filename)
+
+
+@pytest.mark.parametrize(
+    "x1, x2, inverse",
+    [
+        (5, 10, False),
+        (10, 10, False),
+        (5, 5, True),
+        (10, 5, True),
+    ],
+)
+def test_check_equal(x1, x2, inverse):
+    filename = "except_out.txt"
+    orig_stdout = sys.stdout
+    f = open(filename, "w")
+    sys.stdout = f
+    lines = ""
+    try:
+        check_equal(x1, x2, inverse)
+    except Exception as e:
+        print(e)
+    sys.stdout = orig_stdout
+    f.close()
+
+    with open(filename) as f:
+        lines += f.read()
+
+    if inverse:
+        if x1 == x2:
+            assert "must not be equal" in lines.strip()
+
+        if x1 != x2:
+            assert not lines.strip()
+
+    if not inverse:
+        if x1 != x2:
+            assert "must be equal" in lines.strip()
+
+        if x1 == x2:
+            assert not lines.strip()
 
     with contextlib.suppress(FileNotFoundError):
         os.remove(filename)
