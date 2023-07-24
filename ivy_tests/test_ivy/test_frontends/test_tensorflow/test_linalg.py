f449f8862fbef9defc3f02b31514d5f6891876c0
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-07-24 03:00:23 +0300

    filter unsupported data types

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -1143,6 +1143,8 @@ def test_tensorflow_tensor_diag(
         min_value=0,
         max_value=10,
         shape=helpers.ints(min_value=2, max_value=2).map(lambda x: tuple([x, x])),
+    ).filter(
+        lambda x: "float16" not in x[0]
     ),
     test_with_out=st.just(False),
 )
