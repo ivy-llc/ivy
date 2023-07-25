fe8cb392d8d063d150a4ef9bb0d83f88b86e9893
Aditya <adityaraj2019.sahu@gmail.com>
2023-07-25 06:11:23 +0530

    fix - return int32 instead of float32

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -434,7 +434,10 @@ def unsorted_segment_sum(
     ivy.utils.assertions.check_equal(
         list(segment_ids.shape), [list(data.shape)[0]], as_array=False
     )
-    sum_array = ivy.zeros(tuple([num_segments] + (list(data.shape))[1:]))
+    sum_array = ivy.zeros(
+        tuple([num_segments] + (list(data.shape))[1:]), 
+        dtype=ivy.int32
+    )
     for i in range((segment_ids).shape[0]):
         sum_array[segment_ids[i]] = sum_array[segment_ids[i]] + data[i]
     return sum_array
