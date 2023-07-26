4c49f5730d6f08c31f879c4f1b6e0f52ae0fc170
CatB1t <skytedits@gmail.com>
2023-07-27 01:39:40 +0300

    Add `FrontendTestData` struct.

--- ivy_tests/test_ivy/helpers/structs.py
+++ ivy_tests/test_ivy/helpers/structs.py
@@ -1,3 +1,4 @@
+from typing import Callable, List
 from dataclasses import dataclass
 
 
@@ -7,3 +8,13 @@ class FrontendMethodData:
     framework_init_module: str
     init_name: str
     method_name: str
+
+
+@dataclass(kw_only=True, frozen=True)
+class FrontendTestData:
+    fw_function: Callable
+    ivy_function: Callable
+    ivy_function_args: List[str]
+    fn_tree: str
+    fn_name: str
+    supported_device_dtypes: dict = None
