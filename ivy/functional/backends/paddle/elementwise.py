27252d9bde9a2ad4ab7b662c6435264147bd5ac5
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-07-27 11:38:53 +0200

    Add bfloat16 to paddle.sqrt's unsupported dtypes

--- ivy/functional/backends/paddle/elementwise.py
+++ ivy/functional/backends/paddle/elementwise.py
@@ -249,6 +249,10 @@ def sign(
     return paddle.sgn(x)
 
 
+@with_unsupported_device_and_dtypes(
+    {"2.5.0 and below": {"cpu": ("bfloat16",)}},
+    backend_version,
+)
 def sqrt(x: paddle.Tensor, /, *, out: Optional[paddle.Tensor] = None) -> paddle.Tensor:
     if x.dtype in [
         paddle.int8,
