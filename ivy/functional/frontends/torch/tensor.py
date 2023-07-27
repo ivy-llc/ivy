f95c3adab5213c4520e53ba265952efd378eb8c2
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-07-27 07:57:57 +0000

    torch.Tensor.diagonal + test

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1515,6 +1515,10 @@ class Tensor:
     def diag(self, diagonal=0):
         return torch_frontend.diag(self, diagonal=diagonal)
 
+    @with_unsupported_dtypes({"2.0.1 and below": ("bfloat16",)}, "torch")
+    def diagonal(self, offset=0, dim1=0, dim2=1):
+        return torch_frontend.diagonal(self, offset=offset, dim1=dim1, dim2=dim2)
+
     def gather(self, dim, index):
         return torch_frontend.gather(self, dim=dim, index=index)
 
