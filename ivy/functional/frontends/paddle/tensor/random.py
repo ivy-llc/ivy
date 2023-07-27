8e0db3e3bb801e3e94f5a0ec26e8fca96eba3fde
Fayad-Alman <108896305+Fayad-Alman@users.noreply.github.com>
2023-07-27 17:52:29 -0400

    Update random.py

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -114,6 +114,6 @@ def randint_like(x, low=0, high=None, dtype=None, name=None):
     "paddle",
 )
 @to_ivy_arrays_and_back
-def exponential(x, lam=1.0, name=None):
+def exponential_(x, lam=1.0, name=None):
     uniform = ivy.random_uniform(shape=x, dtype=x.dtype)
     return -lam * ivy.log(lam - uniform)
