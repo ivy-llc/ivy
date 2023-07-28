77f56ffd3582386998b483ef10229e39f9801226
Sarvesh Kesharwani <sarveshkumar5513@gmail.com>
2023-07-28 18:28:01 +0530

    assign (#17917)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_creation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_creation.py
@@ -630,3 +630,35 @@ def test_paddle_logspace(
         base=base,
         dtype=dtype[0],
     )
+    
+
+
+# assign
+@handle_frontend_test(
+    fn_tree="paddle.assign",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(True),
+)
+def test_paddle_assign(
+    dtype_and_x,
+    test_flags,
+    backend_fw,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        output=x[1],
+    )
