aec40e2768c730f77e71bd9b13216d5e3c7f46a5
Matteo Angelini <matteo.angelini@studenti.unipr.it>
2023-07-28 16:32:25 +0200

    Add test_torch_logit to test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -3632,6 +3632,44 @@ def test_torch_unfold(
     )
 
 
+# logit
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="logit",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=1,
+    ),
+    eps=st.floats(),
+)
+def test_torch_logit(
+    dtype_and_x,
+    eps,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "eps": eps,
+        },
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
 # __mod__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
