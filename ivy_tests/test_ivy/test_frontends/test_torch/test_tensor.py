0d68efdcc7d38999ed89612002a0615995174140
Mohammad Alameen <60472861+Al-ameen007@users.noreply.github.com>
2023-07-28 18:46:37 +0000

    added bitwise_xor_

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -7332,6 +7332,42 @@ def test_torch_bitwise_xor(
     )
 
 
+# bitwise_xor_
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="bitwise_xor_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=st.one_of(st.just(("bool",)), helpers.get_dtypes("integer")),
+        num_arrays=2,
+    ),
+)
+def test_torch_bitwise_xor_(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "other": x[1],
+        },
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
 # cumprod
 @handle_frontend_method(
     class_tree=CLASS_TREE,
