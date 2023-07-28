f022658e9827dd073b4f559076b3faab5b00630b
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-07-28 07:25:09 +0000

    Updated numpy and torch backends

--- ivy/functional/backends/torch/manipulation.py
+++ ivy/functional/backends/torch/manipulation.py
@@ -397,6 +397,10 @@ def put_along_axis(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     if mode:
+        if mode == "add":
+            mode = "sum"
+        elif mode == "mul":
+            mode = "prod"
         ret = torch.scatter_reduce(arr, axis, indices, values, reduce=mode, out=out)
     elif mode is None:
         ret = torch.scatter_add(arr, axis, indices, values, out=out)
