a56566046fb8685606e9bc6061da187db1cbf08d
Vishwas Singh <vishwassingh@Vishwass-MacBook-Air.local>
2023-07-28 21:43:20 +0530

    corref function in paddle experimental file

--- ivy/functional/backends/paddle/experimental/statistical.py
+++ ivy/functional/backends/paddle/experimental/statistical.py
@@ -6,7 +6,6 @@ import ivy
 
 # local
 from ivy.func_wrapper import with_unsupported_device_and_dtypes
-from ivy.utils.exceptions import IvyNotImplementedException
 from . import backend_version
 
 
@@ -253,13 +252,40 @@ def quantile(
 
 def corrcoef(
     x: paddle.Tensor,
+    y: paddle.Tensor = None,
     /,
     *,
-    y: Optional[paddle.Tensor] = None,
+    
     rowvar: Optional[bool] = True,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    raise IvyNotImplementedException()
+    
+    if len(x.shape) > 2 or len(x.shape) < 1:
+        raise ValueError("x has more than 2 or less than 1 dimensions")
+    
+    if x.dtype not in [paddle.float32, paddle.float64]:
+        raise TypeError("The data type of X must be float64 or float32")
+        
+    c = cov(x, y)
+    
+    dia = paddle.diag(c)
+    
+    if paddle.is_complex(dia):
+        dia = dia.real()
+        
+    stddev = paddle.sqrt(dia)
+    
+    c /= stddev[:, None]
+    c /= stddev[None, :]
+    
+    if paddle.is_complex(c):
+        return paddle.complex(
+            paddle.clip(c.real(), -1, 1), paddle.clip(c.imag(), -1, 1)
+        )
+    else:
+        c = paddle.clip(c, -1, 1)
+        
+    return c
 
 
 def histogram(
