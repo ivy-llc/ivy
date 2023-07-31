339a5f55ba1b89ccb772befcad356244508b3eb1
arham-kk <kmuhammadarham@gmail.com>
2023-07-31 19:52:57 +0500

    fix

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -22,7 +22,15 @@ def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
 
 @handle_tf_dtype
 @to_ivy_arrays_and_back
-def kaiser_bessel_derived_window(window_length, beta=12.0, dtype=ivy.float32, name=None):
-    return ivy.kaiser_bessel_derived_window(window_length, beta=beta, dtype=dtype)
+def kaiser_bessel_derived_window(
+    window_length, beta=12.0, dtype=ivy.float32, name=None
+):
 
-kaiser_bessel_derived_window.supported_dtypes = ("float32", "float64", "float16", "bfloat16")
+    return ivy.kaiser_bessel_derived_window(window_length, periodic=False, beta=beta, dtype=dtype)
+
+kaiser_bessel_derived_window.supported_dtypes = (
+    "float32",
+    "float64",
+    "float16",
+    "bfloat16",
+)
