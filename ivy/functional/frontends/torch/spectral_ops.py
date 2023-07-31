b73291f3e5fea4e9e4985020e5f8c0abae728779
Olawale Onabola <walexi4great@gmail.com>
2023-07-30 17:31:35 -0700

    Add Spectral Ops to PyTorch Frontend - hamming_window

---
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -0,0 +1,19 @@
+import ivy
+from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
+
+
+@to_ivy_arrays_and_back
+def hamming_window(
+    window_length,
+    periodic=True,
+    alpha=0.54,
+    beta=0.46,
+    *,
+    dtype=None,
+    layout=None,
+    device=None,
+    requires_grad=False
+):
+    return ivy.hamming_window(
+        window_length, periodic=periodic, alpha=alpha, beta=beta, dtype=dtype
+    ).requires_grad_(requires_grad)
