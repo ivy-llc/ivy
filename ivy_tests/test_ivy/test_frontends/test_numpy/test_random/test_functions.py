9a9ac4fbb9f3b872f21e5013d52e7dd8c26f26e5
VedantPol <polvedant7@gmail.com>
2023-07-31 13:07:59 +0530

    reformated test_functions

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
@@ -747,8 +747,8 @@ def test_numpy_gumbel(
         scale=scale,
         size=size,
     )
-    
-    
+
+
 def test_numpy_f(
     input_dtypes,
     size,
@@ -771,43 +771,6 @@ def test_numpy_f(
         on_device=on_device,
         dfn=dfn,
         dfd=dfd,
-        size=size,
-    )
-
-
-@handle_frontend_test(
-    fn_tree="numpy.random.gamma",
-    input_dtypes=helpers.get_dtypes("float", full=False),
-    shape=st.floats(
-        allow_nan=False, allow_infinity=False, width=32, min_value=0, exclude_min=True
-    ),
-    scale=st.floats(
-        allow_nan=False, allow_infinity=False, width=32, min_value=0, exclude_min=True
-    ),
-    size=st.tuples(
-        st.integers(min_value=2, max_value=5), st.integers(min_value=2, max_value=5)
-    ),
-    test_with_out=st.just(False),
-)
-def test_numpy_gamma(
-    input_dtypes,
-    shape,
-    scale,
-    size,
-    frontend,
-    test_flags,
-    fn_tree,
-    on_device,
-):
-    helpers.test_frontend_function(
-        input_dtypes=input_dtypes,
-        test_flags=test_flags,
-        frontend=frontend,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        test_values=False,
-        shape=shape,
-        scale=scale,
         size=size,
     )
 
