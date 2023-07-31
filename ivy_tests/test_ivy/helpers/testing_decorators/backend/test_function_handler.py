e453416852561803403dc72e3616061510e463bd
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-07-31 18:54:06 +0000

    Add test for decorator.

---
+++ ivy_tests/test_ivy/helpers/testing_decorators/backend/test_function_handler.py
@@ -0,0 +1,52 @@
+import pytest
+
+from ivy_tests.test_ivy.helpers.testing_decorators.backend.function_handler import (
+    BackendFunctionHandler,
+)
+from hypothesis import strategies as st
+
+
+@pytest.mark.backend_independent
+def test_returns_no_hypothesis_func():
+    def fake_test_fn():
+        pass
+
+    original_fn_id = id(fake_test_fn)
+    wrapped_fn_id = id(
+        BackendFunctionHandler(
+            fn_tree="ivy.functional.ivy.add",
+            ground_truth="tensorflow",
+        )(fake_test_fn)
+    )
+
+    assert original_fn_id == wrapped_fn_id
+
+
+@pytest.mark.backend_independent
+def test_returns_hypothesis_func():
+    def fake_test_fn():
+        pass
+
+    wrapped_fn = BackendFunctionHandler(
+        fn_tree="ivy.functional.ivy.add",
+        ground_truth="tensorflow",
+        x=st.integers(),
+    )(fake_test_fn)
+
+    assert id(fake_test_fn) != id(wrapped_fn)
+    assert hasattr(wrapped_fn, "hypothesis")
+    assert wrapped_fn.is_hypothesis_test
+
+
+@pytest.mark.backend_independent
+def test_has_test_attrs():
+    def fake_test_fn():
+        pass
+
+    wrapped_fn = BackendFunctionHandler(
+        fn_tree="ivy.functional.ivy.add",
+        ground_truth="tensorflow",
+        x=st.integers(),
+    )(fake_test_fn)
+
+    assert hasattr(wrapped_fn, "_is_ivy_backend_test")
