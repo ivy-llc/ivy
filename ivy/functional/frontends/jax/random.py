3c0184ee7b05f002644078a8721dc314370d5a5b
TemiOjo25 <110747346+TemiOjo25@users.noreply.github.com>
2023-07-31 18:31:48 -0400

    Update random.py

--- ivy/functional/frontends/jax/random.py
+++ ivy/functional/frontends/jax/random.py
@@ -350,13 +350,13 @@ def multivariate_normal(key, mean, cov, shape=None, dtype="float64", method="cho
     if shape is None:
         shape = ivy.broadcast_shapes(mean.shape[:-1], cov.shape[:-2])
     
-    if method == "cholesky":
+    if method == 'cholesky':
         cov_factor = ivy.cholesky(cov)
     elif method == 'eigh':
-        (w, v) = ivy.eigh(cov)
+        (w, v) = ivy.linalg.eigh(cov)
         cov_factor = w * ivy.sqrt(s[..., None, :])
-    elif method == "svd":
-        (u, s, __) = ivy.svd(cov)
+    elif method == 'svd':
+        (u, s, _) = ivy.linalg.svd(cov)
         cov_factor = u * ivy.sqrt(s[...,None, :])
     
     normal = ivy.random_normal(seed, shape=shape+mean.shape[-1:],dtype=dtype)
