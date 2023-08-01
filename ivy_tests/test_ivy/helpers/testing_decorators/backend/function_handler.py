17818b02d1fa267c18c86445b878ab267ee08974
CatB1t <skytedits@gmail.com>
2023-08-01 11:17:25 +0300

    Update `BackendFunctionHandler` to use methods from inherited class.

--- ivy_tests/test_ivy/helpers/testing_decorators/backend/function_handler.py
+++ ivy_tests/test_ivy/helpers/testing_decorators/backend/function_handler.py
@@ -1,9 +1,8 @@
 import inspect
-import functools
 
 from hypothesis import strategies as st
 from typing import List, Callable, Any
-from ..function_test import FunctionHandler
+from ..function_test import FunctionHandler, num_positional_args_from_dict
 from ...globals import TestData
 from ...test_parameter_flags import (
     build_function_flags_with_defaults,
@@ -25,14 +24,29 @@ class BackendFunctionHandler(FunctionHandler):
         else:
             self.test_flags = test_flags
 
+        self.possible_args = {
+            "ground_truth_backend": st.just(self.ground_truth),
+            "fn_name": st.just(self.test_data.fn_name),
+            "test_data": self.test_data,
+            "test_flags": self.test_flags,
+        }
+
     def _build_test_data(self):
-        module_tree, _, fn_name = self.fn_tree.rpartition(".")
-        return TestData(module_tree=module_tree, fn_name=fn_name)
+        module_tree, fn_name = self._partition_fn_tree(self.fn_tree)
+        supported_device_dtypes = self._get_supported_devices_dtypes(self.fn_tree)
+        return TestData(
+            module_tree=module_tree,
+            fn_name=fn_name,
+            supported_device_dtypes=supported_device_dtypes,
+        )
 
     def _build_test_flags_with_defaults(self):
+        dict_for_num_pos_strategy = self._build_parameters_info_dict(self.fn_tree)
         self.test_flags = build_function_flags_with_defaults(
             ground_truth_backend=st.just(self.ground_truth),
-            num_positional_args=st.integers(),
+            num_positional_args=num_positional_args_from_dict(
+                dict_for_num_pos_strategy
+            ),
         )
 
     @property
@@ -40,25 +54,29 @@ class BackendFunctionHandler(FunctionHandler):
         return len(self._given_kwargs.items()) > 0
 
     @property
-    def paremeters(self) -> List[str]:
+    def parameters(self) -> List[str]:
         return inspect.signature(self._callable_fn).parameters
 
     def _add_test_attrs_to_fn(self, fn: Callable[..., Any]):
         fn._is_ivy_backend_test = True
+        fn.ground_truth_backend = self.ground_truth
+        fn.test_data = self.test_data
         return fn
 
-    def _bind_test_data(self, fn: Callable[..., Any]):
-        if "test_data" in inspect.signature(fn).parameters:
-            ret = functools.partial(fn, test_data=self.test_data)
-        return ret
+    def _update_given_kwargs(self, fn):
+        param_names = inspect.signature(fn).parameters.keys()
+        # Check if these arguments are being asked for
+        filtered_args = set(param_names).intersection(self.possible_args.keys())
+        for key in filtered_args:
+            self._given_kwargs[key] = self.possible_args[key]
 
     def __call__(self, fn: Callable[..., Any]):
-        fn = self._bind_test_data(fn)
-
         if self.is_hypothesis_test:
+            self._update_given_kwargs(fn)
             wrapped_fn = self._wrap_with_hypothesis(fn)
         else:
             wrapped_fn = fn
 
         self._add_test_attrs_to_fn(wrapped_fn)
+        self._wrap_handle_not_implemented(wrapped_fn)
         return wrapped_fn
