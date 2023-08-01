3d60d7da8c2b2cf8ac6985ee1ecdaf9ee5355f6a
Nwaamaka Iduwe <110701338+Nwaamaka-Iduwe@users.noreply.github.com>
2023-08-01 03:31:19 +0100

    Update test_changing_array_shape.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_array_shape.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_array_shape.py
@@ -330,3 +330,35 @@ def test_numpy_require(
         requirements=requirements,
         like=like,
     )
+
+
+#flatten
+@handle_frontend_test(
+    fn_tree="numpy.ndarray.flatten",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+    order=st.sampled_from(["C", "F", "A", "K"]),
+    test_with_out=st.just(False),
+)
+def test_numpy_flatten(
+    *,
+    dtype_and_x,
+    order,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        order=order,
+    )
+    
