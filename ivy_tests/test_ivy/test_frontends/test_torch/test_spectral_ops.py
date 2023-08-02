510461f80606938bbd2784b47e3772e8b685e77c
Olawale Onabola <walexi4great@gmail.com>
2023-08-01 20:16:32 -0700

    fix failing tests

--- ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
@@ -9,19 +9,28 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 # hamming_window
 @handle_frontend_test(
     fn_tree="torch.hamming_window",
-    window_length=helpers.ints(min_value=1),
+    dtype_and_window_length=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        max_num_dims=0,
+        min_value=1,
+        max_value=20,
+    ),
     periodic=st.booleans(),
-    alpha=helpers.floats(min_value=0),
-    beta=helpers.floats(min_value=0),
+    dtype_and_coefficients=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        max_num_dims=0,
+        num_arrays=2,
+        min_value=0,
+        max_value=5,
+    ),
+    dtype=helpers.get_dtypes("float"),
     requires_grad=st.booleans(),
     test_with_out=st.just(False),
-    dtype=helpers.get_dtypes("integer"),
 )
 def test_torch_hamming_window(
-    window_length,
+    dtype_and_window_length,
     periodic,
-    alpha,
-    beta,
+    dtype_and_coefficients,
     *,
     dtype,
     requires_grad,
@@ -29,14 +38,17 @@ def test_torch_hamming_window(
     frontend,
     test_flags,
 ):
+    window_length_dtype, window_length = dtype_and_window_length
+    coefficients_dtypes, coefficients = dtype_and_coefficients
     helpers.test_frontend_function(
-        input_dtypes=dtype,
-        window_length=window_length,
+        input_dtypes=window_length_dtype + coefficients_dtypes,
+        test_flags=test_flags,
+        window_length=int(window_length[0]),
         periodic=periodic,
-        alpha=alpha,
-        beta=beta,
+        alpha=float(coefficients[0]),
+        beta=float(coefficients[1]),
+        dtype=dtype[0],
         requires_grad=requires_grad,
         fn_tree=fn_tree,
         frontend=frontend,
-        test_flags=test_flags,
     )
