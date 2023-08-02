dc9528ef4f215cf6a93b383ae86b434253f8a02c
mosesdaudu001 <mosesdaudu001@gmail.com>
2023-08-02 15:47:12 +0100

    resolved conflict with torch frontend test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -11139,44 +11139,6 @@ def test_torch_instance_conj(
     )
 
 
-# igamma
-@handle_frontend_method(
-    class_tree=CLASS_TREE,
-    init_tree="torch.tensor",
-    method_name="igamma",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
-        num_arrays=2,
-        shared_dtype=True,
-        min_num_dims=1,
-        min_value=-100,
-        max_value=100,
-    ),
-)
-def test_torch_igamma(
-    dtype_and_x,
-    frontend,
-    frontend_method_data,
-    init_flags,
-    method_flags,
-    on_device,
-    backend_fw,
-):
-    input_dtype, x = dtype_and_x
-    helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        backend_to_test=backend_fw,
-        init_all_as_kwargs_np={"data": x[0]},
-        method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={"other": x[1]},
-        frontend=frontend,
-        frontend_method_data=frontend_method_data,
-        init_flags=init_flags,
-        method_flags=method_flags,
-        on_device=on_device,
-    )
-
-
 @st.composite
 def _get_clip_min_inputs(draw):
     shape = draw(
