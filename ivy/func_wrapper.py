13ba570e94afedd3f20e0201c65dc037829775c2
Muhammad Abdullah <69182153+abdulasiraj@users.noreply.github.com>
2023-08-02 14:58:07 +0500

    Update func_wrapper.py

--- ivy/func_wrapper.py
+++ ivy/func_wrapper.py
@@ -821,8 +821,7 @@ def handle_device_shifting(fn: Callable) -> Callable:
         unique_devices = set(devices)
         # check if arrays are on the same device
         if len(unique_devices) == 1:
-            with ivy.DefaultDevice(next(iter(unique_devices))):
-                return ivy.handle_soft_device_variable(*args, fn=fn, **kwargs)
+            return fn(*args, **kwargs)
         # raise when arrays are on different devices
         elif len(unique_devices) > 1:
             raise ivy.utils.exceptions.IvyException(
