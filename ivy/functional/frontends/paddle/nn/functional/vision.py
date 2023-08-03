031e61536fe1cb8e9a907d27006bd970af87a0ab
Zawyar Ur Rehman <zawyarurrehman@outlook.com>
2023-08-03 18:58:21 +0400

    Added a compositional implementation of paddle.nn.functional.vision.channel_shuffle

--- ivy/functional/frontends/paddle/nn/functional/vision.py
+++ ivy/functional/frontends/paddle/nn/functional/vision.py
@@ -1,1 +1,50 @@
 # local
+import ivy
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+
+@to_ivy_arrays_and_back
+def channel_shuffle(x, groups, data_format="NCHW", name=None):
+    # ToDo ivy.channel_shuffle  # 21235
+    # Issue Link: https://github.com/unifyai/ivy/issues/21235
+    # This implementation will be simplified when ivy.channel_shuffle is implemented
+    if len(x.shape) != 4:
+        raise ValueError(
+            "Input x should be 4D tensor, but received x with the shape of {}".format(
+                x.shape
+            )
+        )
+
+    if not isinstance(groups, int):
+        raise TypeError("groups must be int type")
+
+    if groups <= 0:
+        raise ValueError("groups must be positive")
+        # assert (channels % groups == 0)
+
+    if data_format not in ["NCHW", "NHWC"]:
+        raise ValueError(
+            "Attr(data_format) should be 'NCHW' or 'NHWC'."
+            "But recevie Attr(data_format): {} ".format(data_format)
+        )
+
+    if data_format == "NCHW":
+        n, c, h, w = x.shape
+        if c % groups != 0:
+            raise ValueError('channels must be divisible by groups')
+        new_shape = (n, groups, c // groups, h, w)
+        result = ivy.reshape(x, ivy.Shape(new_shape))
+        result = ivy.permute_dims(result, (0, 2, 1, 3, 4))
+        oshape = [n, c, h, w]
+        result = ivy.reshape(result,ivy.Shape(oshape))
+        return result
+    else:
+        n, h, w, c = x.shape
+        if c % groups != 0:
+            raise ValueError('channels must be divisible by groups')
+        new_shape = (n, h, w, groups, c // groups)
+        result = ivy.reshape(x, ivy.Shape(new_shape))
+        result = ivy.permute_dims(result,(0, 1, 2, 4, 3))
+        oshape = [n, h, w, c]
+        result = ivy.reshape(result, ivy.Shape(oshape))
+        return result
+
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_vision.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_vision.py
@@ -1,3 +1,40 @@
 # global
-
+import ivy
+from hypothesis import assume, strategies as st
 # local
+import ivy.functional.frontends.paddle as paddle_frontend
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+import math
+
+
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.channel_shuffle",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=["float32","float64"],
+    min_num_dims=4,
+    max_num_dims=4,
+    ret_shape=True
+    ),
+    groups=helpers.number_helpers.ints(min_value=1),
+    test_with_out=st.just(False)
+)
+def test_paddle_channel_shuffle(
+    *,
+    dtype_and_x,
+    groups,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x,shape = dtype_and_x
+    groups=math.gcd(groups,shape[1])
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        groups=groups
+    )
