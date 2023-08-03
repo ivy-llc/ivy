87293d943f0e4f44de2c2e3270c26807380755fb
CatB1t <skytedits@gmail.com>
2023-08-04 00:28:09 +0300

    Add method frontend handler decorator.

---
+++ ivy_tests/test_ivy/helpers/decorators/frontend/method_decorator_frontend.py
@@ -0,0 +1,60 @@
+# from hypothesis import strategies as st
+from ivy_tests.test_ivy.helpers.test_parameter_flags import (
+    BuiltAsVariableStrategy,
+    BuiltNativeArrayStrategy,
+)
+from ivy_tests.test_ivy.helpers.test_parameter_flags import frontend_method_flags
+from ivy_tests.test_ivy.helpers.decorators.base.method_decorator_base import (
+    MethodHandlerBase,
+)
+
+
+class FrontendMethodHandlerBase(MethodHandlerBase):
+    def __init__(
+        self,
+        class_tree: str,
+        init_tree: str,
+        method_name: str,
+        init_num_positional_args=None,
+        init_native_arrays=BuiltNativeArrayStrategy,
+        init_as_variable_flags=BuiltAsVariableStrategy,
+        method_num_positional_args=None,
+        method_native_arrays=BuiltNativeArrayStrategy,
+        method_as_variable_flags=BuiltAsVariableStrategy,
+        **_given_kwargs,
+    ):
+        self._build_init_flags(
+            init_num_positional_args, init_native_arrays, init_as_variable_flags
+        )
+        self._build_method_flags(
+            method_num_positional_args,
+            method_native_arrays,
+            method_as_variable_flags,
+        )
+        self._given_kwargs = _given_kwargs
+
+    def _build_init_flags(
+        self, init_num_positional_args, init_as_variable_flags, init_native_arrays
+    ):
+        self.init_flags = frontend_method_flags(
+            num_positional_args=init_num_positional_args,
+            as_variable=init_as_variable_flags,
+            native_arrays=init_native_arrays,
+        )
+
+    def _build_method_flags(
+        self, method_num_positional_args, method_as_variable_flags, method_native_arrays
+    ):
+        self.method_flags = frontend_method_flags(
+            num_positional_args=method_num_positional_args,
+            as_variable=method_as_variable_flags,
+            native_arrays=method_native_arrays,
+        )
+
+    @property
+    def possible_args(self):
+        return {
+            "init_flags": self.init_flags,
+            "method_flags": self.method_flags,
+            # "frontend_method_data": st.just(frontend_helper_data),
+        }
