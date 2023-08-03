e3e3de3faed014edafb247686f59ffa6105b4dbe
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-04 03:49:13 +0530

    dtype == valid

--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_losses.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_losses.py
@@ -62,7 +62,7 @@ def test_log_poisson_loss(
 @handle_test(
     fn_tree="functional.ivy.experimental.l1_loss",
     dtype_true_and_pred=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
+        available_dtypes=helpers.get_dtypes("float"),
         min_value=1e-04,
         max_value=1,
         allow_inf=False,
