66f8625e6fee07b9b9431b8c0617d931d2a4919c
Vedant Pandey <vedantpandey459@gmail.com>
2023-08-03 23:37:13 +0530

    flatten added

---
+++ ivy/functional/frontends/tensorflow/keras/layers.py
@@ -0,0 +1,15 @@
+import ivy
+import ivy.functional.frontends.tensorflow as tf_frontend
+from ivy.functional.frontends.tensorflow.func_wrapper import to_ivy_arrays_and_back
+
+@to_ivy_arrays_and_back
+def flatten(x,copy=None,start_dim=0, end_dim=-1, order='C', out=None):
+    ivy.utils.assertions.check_elem_in_list(
+            order,
+            ["C", "F", "A", "K"],
+            message="order must be one of 'C', 'F', 'A', or 'K'",
+        )
+    if (order in ["K","A","F"]):
+        return ivy.flatten(x,copy=copy,start_dims=start_dim,end_dims=end_dim,order="F",out=out)
+    else:
+         return ivy.flatten(x,copy=copy,start_dims=start_dim,end_dims=end_dim,order="C",out=out)
---
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_keras/test_layers.py
@@ -0,0 +1,33 @@
+# global
+import numpy as np
+from hypothesis import strategies as st
+import ivy
+
+# local
+import ivy
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+@handle_frontend_test(
+    fn_tree="tensorflow.keras.image.layers",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_tensorfloe_layers_flatten(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],)
