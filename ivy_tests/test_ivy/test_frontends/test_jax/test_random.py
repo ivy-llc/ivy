6de1fabfdcd3fe2dd3c4fb26ff4ba51961d2302c
Vaibhav Deshpande <137694306+stalemate1@users.noreply.github.com>
2023-08-04 02:19:37 +0530

    Update test_random.py

--- ivy_tests/test_ivy/test_frontends/test_jax/test_random.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_random.py
@@ -1357,6 +1357,68 @@ def test_jax_maxwell(
 
 @pytest.mark.xfail
 @handle_frontend_test(
+    fn_tree="jax.random.double_sided_maxwell",
+    dtype_key=helpers.dtype_and_values(
+        available_dtypes=["uint32"],
+        min_value=0,
+        max_value=2000,
+        min_num_dims=1,
+        max_num_dims=1,
+        min_dim_size=2,
+        max_dim_size=2,
+    ),
+    shape=helpers.get_shape(),
+    dtype=helpers.get_dtypes("float", full=False),
+    loc=st.integers(min_value=10, max_value=100),
+    scale=st.floats(min_value=0, max_value=100, exclude_min=True),
+    test_with_out=st.just(False),
+)
+def test_jax_double_sided_maxwell(
+    *,
+    dtype_key,
+    loc,
+    scale,
+    shape,
+    dtype,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, key = dtype_key
+
+    def call():
+        return helpers.test_frontend_function(
+            input_dtypes=input_dtype,
+            frontend=frontend,
+            test_flags=test_flags,
+            fn_tree=fn_tree,
+            on_device=on_device,
+            test_values=False,
+            backend_to_test=backend_fw,
+            key=key[0],
+            loc=loc,
+            scale=scale,
+            shape=shape,
+            dtype=dtype[0],
+        )
+
+    ret = call()
+
+    if not ivy.exists(ret):
+        return
+
+    ret_np, ret_from_np = ret
+    ret_np = helpers.flatten_and_to_np(backend=backend_fw, ret=ret_np)
+    ret_from_np = helpers.flatten_and_to_np(backend=backend_fw, ret=ret_from_np)
+    for u, v in zip(ret_np, ret_from_np):
+        assert u.dtype == v.dtype
+        assert u.shape == v.shape
+
+
+@pytest.mark.xfail
+@handle_frontend_test(
     fn_tree="jax.random.ball",
     dtype_key=helpers.dtype_and_values(
         available_dtypes=["uint32"],
