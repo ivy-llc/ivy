bd2e965378746bf31d49be5bd6a39cc8c4d31911
Sanchay Thalnerkar <thalnerkarsanchay17@gmail.com>
2023-08-04 23:39:32 +0530

    done

--- ivy/functional/frontends/paddle/tensor/manipulation.py
+++ ivy/functional/frontends/paddle/tensor/manipulation.py
@@ -62,6 +62,15 @@ def split(x, num_or_sections, axis=0, name=None):
 
 
 @with_unsupported_dtypes(
+    {"2.5.1 and below": ("int16", "complex64", "complex128")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def tolist(x):
+    return ivy.tolist(x)
+
+
+@with_unsupported_dtypes(
     {"2.5.1 and below": ("float16", "bfloat16", "int8", "int16")},
     "paddle",
 )
@@ -132,3 +141,8 @@ def gather(params, indices, axis=-1, batch_dims=0, name=None):
 @to_ivy_arrays_and_back
 def roll(x, shifts, axis=None, name=None):
     return ivy.roll(x, shifts, axis=axis)
+
+
+@to_ivy_arrays_and_back
+def to_list(x):
+    return x.tolist()
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_manipulation.py
@@ -601,3 +601,33 @@ def test_paddle_roll(
         shifts=shift,
         axis=axis,
     )
+
+
+# tolist
+@handle_frontend_test(
+    fn_tree="paddle.tensor.manipulation.tolist",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=1,
+        max_num_dims=6,
+    ),
+)
+def test_tolist(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
