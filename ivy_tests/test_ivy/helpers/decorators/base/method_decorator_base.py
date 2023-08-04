ec750b3cb60734940d94fbba0d6d557632799810
CatB1t <skytedits@gmail.com>
2023-08-04 18:40:15 +0300

    Update method handler base.

--- ivy_tests/test_ivy/helpers/decorators/base/method_decorator_base.py
+++ ivy_tests/test_ivy/helpers/decorators/base/method_decorator_base.py
@@ -1,9 +1,15 @@
+import inspect
 import importlib
+from typing import Any, Callable
+from ivy_tests.test_ivy.helpers.structs import ParametersInfo
 
 from abc import abstractproperty
 from ivy_tests.test_ivy.helpers.decorators.base.decorator_base import HandlerBase
 from ivy_tests.test_ivy.helpers.pipeline_helper import update_backend
 from ivy_tests.test_ivy.helpers.available_frameworks import available_frameworks
+from ivy_tests.test_ivy.helpers.decorators.strategies import (
+    num_positional_args_from_dict,
+)
 
 
 class MethodHandlerBase(HandlerBase):
@@ -14,6 +20,40 @@ class MethodHandlerBase(HandlerBase):
     @abstractproperty
     def method_tree():
         pass
+
+    def _build_parameter_info(self, fn):
+        total = num_positional_only = num_keyword_only = 0
+        # TODO refactor out
+        for param in inspect.signature(fn).parameters.values():
+            if param.name == "self":
+                continue
+            total += 1
+            if param.kind == param.POSITIONAL_ONLY:
+                num_positional_only += 1
+            elif param.kind == param.KEYWORD_ONLY:
+                num_keyword_only += 1
+            elif param.kind == param.VAR_KEYWORD:
+                num_keyword_only += 1
+        return ParametersInfo(
+            total=total,
+            positional_only=num_positional_only,
+            keyword_only=num_keyword_only,
+        )
+
+    def _build_parameters_info_dict(self, method_tree):
+        ret = {}
+
+        for framework in available_frameworks:
+            with update_backend(framework) as ivy_backend:
+                method = self._import_method(method_tree)
+                parameter_info = self._build_parameter_info(method)
+                ret[framework] = parameter_info
+
+        return ret
+
+    def _build_num_positional_arguments_strategy(self, method_tree: str):
+        dict_for_num_pos_strategy = self._build_parameters_info_dict(method_tree)
+        return num_positional_args_from_dict(dict_for_num_pos_strategy)
 
     def _partition_method_tree(self, method_tree: str):
         class_module_and_name, _, method_name = method_tree.rpartition(".")
@@ -42,3 +82,14 @@ class MethodHandlerBase(HandlerBase):
                     )
                 supported_device_dtypes[backend_str] = organized_dtypes
         return supported_device_dtypes
+
+    def __call__(self, fn: Callable[..., Any]):
+        if self.is_hypothesis_test:
+            self._update_given_kwargs(fn)
+            wrapped_fn = self._wrap_with_hypothesis(fn)
+        else:
+            wrapped_fn = fn
+
+        self._add_test_attributes_to_test_function(wrapped_fn)
+        self._handle_not_implemented(wrapped_fn)
+        return wrapped_fn
