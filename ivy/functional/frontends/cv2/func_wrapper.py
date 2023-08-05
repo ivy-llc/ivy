b12d433fc6f7a495a483ce41ab8635fc618e53d9
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-08-05 20:22:52 +0200

    Replicate what's done in scipy's func_wrapper

--- ivy/functional/frontends/cv2/func_wrapper.py
+++ ivy/functional/frontends/cv2/func_wrapper.py
@@ -1,6 +1,101 @@
+import functools
 from typing import Callable
-from ..numpy.func_wrapper import to_ivy_arrays_and_back as to_ivy_arrays_and_back_numpy
+
+import ivy
+import ivy.functional.frontends.numpy as np_frontend
+
+
+def _native_to_ivy_array(x):
+    if isinstance(x, ivy.NativeArray):
+        return ivy.array(x)
+    return x
+
+
+def _from_cv2_frontend_array_to_ivy_array(x):
+    if (
+        # add other types of frontend arrays
+        isinstance(x, np_frontend.ndarray)
+    ):
+        return ivy.to_scalar(x.ivy_array)
+    if hasattr(x, "ivy_array"):
+        return x.ivy_array
+    return x
+
+
+def _to_ivy_array(x):
+    return _from_cv2_frontend_array_to_ivy_array(_native_to_ivy_array(x))
+
+
+def inputs_to_ivy_arrays(fn: Callable) -> Callable:
+    @functools.wraps(fn)
+    def _inputs_to_ivy_arrays_cv2(*args, **kwargs):
+        """
+        Convert `ndarray.NDArray` into `ivy.Array` instances.
+
+        Convert all `ndarray.NDArray` instances in both the positional
+        and keyword arguments into `ivy.Array` instances, and then calls
+        the function with the updated arguments.
+        """
+        # check if kwargs contains an out argument, and if so, remove it
+        has_out = False
+        out = None
+        if "out" in kwargs:
+            out = kwargs["out"]
+            del kwargs["out"]
+            has_out = True
+        # convert all arrays in the inputs to ivy.Array instances
+        new_args = ivy.nested_map(
+            args, _to_ivy_array, include_derived={tuple: True}, shallow=False
+        )
+        new_kwargs = ivy.nested_map(
+            kwargs, _to_ivy_array, include_derived={tuple: True}, shallow=False
+        )
+        # add the original out argument back to the keyword arguments
+        if has_out:
+            new_kwargs["out"] = out
+        return fn(*new_args, **new_kwargs)
+
+    _inputs_to_ivy_arrays_cv2.inputs_to_ivy_arrays = True
+    return _inputs_to_ivy_arrays_cv2
+
+
+def _from_ivy_array_to_cv2_frontend_array(x, nested=False, include_derived=None):
+    if nested:
+        return ivy.nested_map(
+            x, _from_ivy_array_to_cv2_frontend_array, include_derived, shallow=False
+        )
+    elif isinstance(x, ivy.Array):
+        return np_frontend.array(x)
+    return x
+
+
+def outputs_to_frontend_arrays(fn: Callable) -> Callable:
+    @functools.wraps(fn)
+    def _outputs_to_frontend_arrays_cv2(*args, **kwargs):
+        """
+        Convert `ivy.Array` into `ndarray.NDArray` instances.
+
+        Call the function, and then converts all `ivy.Array` instances
+        in the function return into `ndarray.NDArray` instances.
+        """
+        # call unmodified function
+        ret = fn(*args, **kwargs)
+
+        # convert all arrays in the return to `frontend.Tensorflow.tensor` instances
+        return _from_ivy_array_to_cv2_frontend_array(
+            ret, nested=True, include_derived={tuple: True}
+        )
+
+    _outputs_to_frontend_arrays_cv2.outputs_to_frontend_arrays = True
+    return _outputs_to_frontend_arrays_cv2
 
 
 def to_ivy_arrays_and_back(fn: Callable) -> Callable:
-    return to_ivy_arrays_and_back_numpy(fn)
+    """
+    Wrap `fn` so it receives and returns `ivy.Array` instances.
+
+    Wrap `fn` so that input arrays are all converted to `ivy.Array`
+    instances and return arrays are all converted to `ndarray.NDArray`
+    instances.
+    """
+    return outputs_to_frontend_arrays(inputs_to_ivy_arrays(fn))
