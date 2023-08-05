bddbd2fd403b9a4e72fbbe4463c802d5f65e6118
Harsh <90236987+Lancelot03@users.noreply.github.com>
2023-08-05 21:59:52 +0530

    Update raw_ops.py adding Nextafter

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -957,3 +957,7 @@ Einsum = to_ivy_arrays_and_back(
         "tensorflow",
     )(map_raw_ops_alias(tf_frontend.general_functions.einsum))
 )
+
+
+NextAfter = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.math.nextafter))
+
