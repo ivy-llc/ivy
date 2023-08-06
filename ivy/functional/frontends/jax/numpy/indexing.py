6b98ce37eba6e1e16135ef4e86f592d71f6f94d8
Jeffrey Ju <haoruju@usc.edu>
2023-08-06 14:39:46 -0700

    added indices function to jax frontend

--- ivy/functional/frontends/jax/numpy/indexing.py
+++ ivy/functional/frontends/jax/numpy/indexing.py
@@ -80,3 +80,23 @@ def diag_indices_from(arr):
         raise ValueError("All dimensions of input must be of equal length")
     idx = ivy.arange(n, dtype=int)
     return (idx,) * ndim
+
+
+
+
+@to_ivy_arrays_and_back
+def indices(dimensions, dtype=int, sparse=False):
+    dimensions = tuple(dimensions)
+    N = len(dimensions)
+    shape = (1,) * N
+    if sparse:
+        res = tuple()
+    else:
+        res = ivy.empty((N,) + dimensions, dtype=dtype)
+    for i, dim in enumerate(dimensions):
+        idx = ivy.arange(dim, dtype=dtype).reshape(shape[:i] + (dim,) + shape[i + 1 :])
+        if sparse:
+            res = res + (idx,)
+        else:
+            res[i] = idx
+    return res
