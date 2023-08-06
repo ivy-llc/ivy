e314faa654f0bd95eff7dadcb3fdbbcfdf16ef68
NouranYoussef <nyoussef@tmcmf.com>
2023-08-06 14:39:26 +0300

    conv test

--- ivy/functional/frontends/paddle/nn/functional/conv.py
+++ ivy/functional/frontends/paddle/nn/functional/conv.py
@@ -2,7 +2,30 @@
 import ivy
 from ivy.func_wrapper import with_supported_dtypes
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
-from ivy.functional.frontends.torch.nn.functional.convolution_functions import _conv
+from ivy.functional.frontends.torch.nn.functional.convolution_functions import _conv as conv
+from ivy.functional.frontends.torch.nn.functional.convolution_functions import _conv_transpose as conv_transpose
+
+
+def _conv(x, weight, data_format, bias=None, stride=1, padding=0, dilation=1, groups=1):
+    ndims = len(x.shape)
+    dims = ndims - 2
+    if 1 >= dims >= 3:
+        raise ivy.utils.exceptions.IvyError(
+            f"invalid for input with {dims} dims"
+        )
+    # channel first input
+    if data_format not in ['NCL', 'NCHW', 'NCDHW']:
+        if data_format in ['NLC', 'NHWC', 'NDHWC']:
+            x = ivy.permute_dims(x, axes=(0, ndims - 1, *range(1, ndims - 1)))
+        else:
+            raise ivy.utils.exceptions.IvyError(
+                "data_format should be " +
+                "'NCL' or 'NLC' " if dims == 1 else
+                "'NCHW' or 'NHWC' " if dims == 2 else
+                "'NCDHW' or 'NDHWC' " +
+                f"but got data_format '{data_format}'"
+            )
+    return conv(x, weight, bias, stride, padding, dilation, groups)
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -10,16 +33,7 @@ def conv1d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCL', name=None
 ):
-    if data_format != 'NCL':
-        if data_format == 'NLC':
-            ndims = len(x.shape)
-            x = ivy.permute_dims(x, axes=(0, ndims - 1, *range(1, ndims - 1)))
-        else:
-            raise ivy.utils.exceptions.IvyError(
-                "data_format should be 'NCL' or 'NLC' "
-                f"but got data_format '{data_format}'"
-            )
-    return _conv(x, weight, bias, stride, padding, dilation, groups)
+    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -27,16 +41,7 @@ def conv2d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCHW', name=None
 ):
-    if data_format != 'NCHW':
-        if data_format == 'NHWC':
-            ndims = len(x.shape)
-            x = ivy.permute_dims(x, axes=(0, ndims - 1, *range(1, ndims - 1)))
-        else:
-            raise ivy.utils.exceptions.IvyError(
-                "data_format should be 'NCHW' or 'NHWC' "
-                f"but got data_format '{data_format}'"
-            )
-    return _conv(x, weight, bias, stride, padding, dilation, groups)
+    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -44,15 +49,4 @@ def conv3d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCDHW', name=None
 ):
-    # channel first input
-    if data_format != 'NCDHW':
-        if data_format == 'NDHWC':
-            ndims = len(x.shape)
-            x = ivy.permute_dims(x, axes=(0, ndims - 1, *range(1, ndims - 1)))
-        else:
-            raise ivy.utils.exceptions.IvyError(
-                "data_format should be 'NCDHW' or 'NDHWC' "
-                f"but got data_format '{data_format}'"
-            )
-    return _conv(x, weight, bias, stride, padding, dilation, groups)
-
+    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_conv.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_conv.py
@@ -1,3 +1,101 @@
 # global
+from hypothesis import strategies as st
 
 # local
+import ivy
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+from ivy_tests.test_ivy.test_frontends.test_torch.test_nn.test_functional.test_convolution_functions import x_and_filters
+
+# conv1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv1d",
+    dtype_vals=x_and_filters(dim=1),
+)
+def test_paddle_conv1d(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, fc = dtype_vals
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        dilation=dilations,
+        groups=fc,
+    )
+
+# conv2d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv2d",
+    dtype_vals=x_and_filters(dim=2),
+)
+def test_paddle_conv2d(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, fc = dtype_vals
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        dilation=dilations,
+        groups=fc,
+    )
+
+# conv3d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv3d",
+    dtype_vals=x_and_filters(dim=3),
+)
+def test_paddle_conv3d(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, fc = dtype_vals
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        dilation=dilations,
+        groups=fc,
+    )
