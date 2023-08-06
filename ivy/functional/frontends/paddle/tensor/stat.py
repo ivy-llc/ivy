49143ab3cc5c638044e140f5d3b9623b228d408f
Aml Hassan Esmil <Amal180214@feng.bu.edu.eg>
2023-08-06 05:49:47 +0300

    Update stat.py to fix lint test

--- ivy/functional/frontends/paddle/tensor/stat.py
+++ ivy/functional/frontends/paddle/tensor/stat.py
@@ -46,6 +46,7 @@ def median(x, axis=None, keepdim=False, name=None):
     )
     return ivy.median(x, axis=axis, keepdims=keepdim)
 
+
 @with_supported_dtypes({"2.5.0 and below": ("float16", "float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def var(x, axis=None, unbiased=True, keepdim=False, name=None):
