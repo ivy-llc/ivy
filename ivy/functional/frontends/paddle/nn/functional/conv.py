3032ac3746a40815f6988f4d6f97f1a5165dea1b
NouranYoussef <nyoussef@tmcmf.com>
2023-08-06 17:53:03 +0300

    conv transpose , tests

--- ivy/functional/frontends/paddle/nn/functional/conv.py
+++ ivy/functional/frontends/paddle/nn/functional/conv.py
@@ -2,11 +2,10 @@
 import ivy
 from ivy.func_wrapper import with_supported_dtypes
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
-from ivy.functional.frontends.torch.nn.functional.convolution_functions import _conv as conv
-from ivy.functional.frontends.torch.nn.functional.convolution_functions import _conv_transpose as conv_transpose
+from ....torch.nn.functional import convolution_functions
 
 
-def _conv(x, weight, data_format, bias=None, stride=1, padding=0, dilation=1, groups=1):
+def _channel_first_input(x,data_format):
     ndims = len(x.shape)
     dims = ndims - 2
     if 1 >= dims >= 3:
@@ -23,9 +22,39 @@ def _conv(x, weight, data_format, bias=None, stride=1, padding=0, dilation=1, gr
                 "'NCL' or 'NLC' " if dims == 1 else
                 "'NCHW' or 'NHWC' " if dims == 2 else
                 "'NCDHW' or 'NDHWC' " +
-                f"but got data_format '{data_format}'"
-            )
-    return conv(x, weight, bias, stride, padding, dilation, groups)
+                f"but got data_format '{data_format}'")
+    return x
+
+def _conv(
+        x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
+        data_format='NLC'
+):
+    x = _channel_first_input(x,data_format)
+    return convolution_functions._conv(
+        x,
+        weight,
+        bias=bias,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups
+    )
+
+def _conv_transpose(
+        x, weight, bias=None, stride=1, padding=0, output_padding=0,
+        dilation=1, groups=1, data_format='NLC'
+):
+    x = _channel_first_input(x,data_format)
+    return convolution_functions._conv_transpose(
+        x,
+        weight,
+        bias=bias,
+        stride=stride,
+        padding=padding,
+        output_padding=output_padding,
+        groups=groups,
+        dilation=dilation
+    )
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -33,7 +62,7 @@ def conv1d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCL', name=None
 ):
-    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
+    return _conv(x, weight, bias, stride, padding, dilation, groups,data_format)
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -41,7 +70,7 @@ def conv2d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCHW', name=None
 ):
-    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
+    return _conv(x, weight, bias, stride, padding, dilation, groups,data_format)
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
@@ -49,4 +78,32 @@ def conv3d(
     x, weight, bias=None, stride=1, padding=0, dilation=1, groups=1,
     data_format='NCDHW', name=None
 ):
-    return _conv(x, weight, data_format, bias, stride, padding, dilation, groups)
+    return _conv(x, weight, bias, stride, padding, dilation, groups,data_format)
+
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def conv1d_transpose(
+        x, weight, bias=None, stride=1, padding=0, output_padding=0, groups=1,
+        dilation=1, output_size=None, data_format='NCL', name=None
+):
+    return _conv_transpose(x, weight, bias, stride, padding, output_padding,
+                           dilation, groups, data_format)
+
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def conv2d_transpose(
+        x, weight, bias=None, stride=1, padding=0, output_padding=0, dilation=1,
+        groups=1, output_size=None, data_format='NCHW', name=None
+):
+    return _conv_transpose(x, weight, bias, stride, padding, output_padding,
+                           dilation, groups, data_format)
+
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def conv3d_transpose(
+        x, weight, bias=None, stride=1, padding=0, output_padding=0, groups=1,
+        dilation=1, output_size=None, data_format='NCDHW', name=None
+):
+    return _conv_transpose(x, weight, bias, stride, padding, output_padding,
+                           dilation, groups, data_format)
+
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_conv.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_conv.py
@@ -1,11 +1,12 @@
 # global
-from hypothesis import strategies as st
+from hypothesis import strategies as st, assume
 
 # local
 import ivy
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 from ivy_tests.test_ivy.test_frontends.test_torch.test_nn.test_functional.test_convolution_functions import x_and_filters
+from ivy_tests.test_ivy.test_frontends.test_torch.test_nn.test_functional.test_convolution_functions import _output_shape
 
 # conv1d
 @handle_frontend_test(
@@ -98,4 +99,126 @@ def test_paddle_conv3d(
         padding=padding,
         dilation=dilations,
         groups=fc,
+    )
+
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv1d_transpose",
+    dtype_vals=x_and_filters(dim=1, transpose=True),
+)
+def test_paddle_conv1d_tranpose(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, output_pad, fc = dtype_vals
+    dilations = 1  # ToDo: remove this when support for dilation > 1 is added
+    assume(
+        all(
+            x > 0
+            for x in _output_shape(
+                1, dilations, strides, padding, output_pad, vals.shape, weight.shape
+            )
+        )
+    )
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        output_padding=output_pad,
+        groups=fc,
+        dilation=dilations,
+    )
+
+
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv2d_transpose",
+    dtype_vals=x_and_filters(dim=2, transpose=True),
+)
+def test_paddle_conv2d_tranpose(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, output_pad, fc = dtype_vals
+    dilations = 1  # ToDo: remove this when support for dilation > 1 is added
+    assume(
+        all(
+            x > 0
+            for x in _output_shape(
+                2, dilations, strides, padding, output_pad, vals.shape, weight.shape
+            )
+        )
+    )
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        output_padding=output_pad,
+        groups=fc,
+        dilation=dilations,
+    )
+
+
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.conv3d_transpose",
+    dtype_vals=x_and_filters(dim=3, transpose=True),
+)
+def test_paddle_conv3d_tranpose(
+    *,
+    dtype_vals,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    dtype, vals, weight, bias, dilations, strides, padding, output_pad, fc = dtype_vals
+    dilations = 1  # ToDo: remove this when support for dilation > 1 is added
+    assume(
+        all(
+            x > 0
+            for x in _output_shape(
+                3, dilations, strides, padding, output_pad, vals.shape, weight.shape
+            )
+        )
+    )
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=vals,
+        weight=weight,
+        bias=bias,
+        stride=strides,
+        padding=padding,
+        output_padding=output_pad,
+        groups=fc,
+        dilation=dilations,
     )
