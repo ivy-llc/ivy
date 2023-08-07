9ccae3e937f0ab6d5274aa3d9173d8adadfd3e05
Simranjeet Singh <simranjeet@adeptmind.ai>
2023-08-07 11:43:13 +0530

    Add tie_in to jax frontend api with test (#20992)
    Co-authored-by: Anwaar Khalid <shahanwaar.khalid8@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_jax/test_lax/test_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_lax/test_operators.py
@@ -2989,3 +2989,35 @@ def test_jax_cbrt(
         on_device=on_device,
         x=x[0],
     )
+
+
+# tie_in
+@handle_frontend_test(
+    fn_tree="jax.lax.tie_in",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_tie_in(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
