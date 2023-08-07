df4c24eec9c2194505ab89bbd720c1acc41fff44
alec-kr <52685467+alec-kr@users.noreply.github.com>
2023-08-07 13:04:45 -0400

    Update transforms.py
    Rewrote code to work without native paddle functions.

--- ivy/functional/frontends/paddle/vision/transforms.py
+++ ivy/functional/frontends/paddle/vision/transforms.py
@@ -1,10 +1,11 @@
 # global
 import ivy
 from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
-import ivy.functional.frontends.paddle as paddle
-from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+from PIL import Image, ImageEnhance, ImageOps
 
 @with_unsupported_dtypes({"2.5.0 and below": ("float16", "bfloat16")}, "paddle")
 # Return the brightness-adjusted image
 def adjust_brightness(img, brightness_factor):
-  return paddle.vision.transforms.adjust_brightness(img, brightness_factor)
+    enhancer = ImageEnhance.Brightness(img)
+    img = enhancer.enhance(brightness_factor)
+    return img
