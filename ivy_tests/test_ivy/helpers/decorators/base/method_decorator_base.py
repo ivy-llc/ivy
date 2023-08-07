e0b950ecc6fb5e87969e252be5074bbcad9be3d4
CatB1t <skytedits@gmail.com>
2023-08-07 07:43:52 +0300

    Remove dead code.

--- ivy_tests/test_ivy/helpers/decorators/base/method_decorator_base.py
+++ ivy_tests/test_ivy/helpers/decorators/base/method_decorator_base.py
@@ -1,13 +1,7 @@
-import inspect
-from ivy_tests.test_ivy.helpers.structs import ParametersInfo
-
 from abc import abstractproperty
 from ivy_tests.test_ivy.helpers.decorators.base.decorator_base import HandlerBase
 from ivy_tests.test_ivy.helpers.pipeline_helper import update_backend
 from ivy_tests.test_ivy.helpers.available_frameworks import available_frameworks
-from ivy_tests.test_ivy.helpers.decorators.strategies import (
-    num_positional_args_from_dict,
-)
 
 
 class MethodHandlerBase(HandlerBase):
@@ -18,57 +12,6 @@ class MethodHandlerBase(HandlerBase):
     @abstractproperty
     def method_tree():
         pass
-
-    def _build_parameter_info(self, fn):
-        total = num_positional_only = num_keyword_only = 0
-        # TODO refactor out
-        for param in inspect.signature(fn).parameters.values():
-            if param.name == "self":
-                continue
-            total += 1
-            if param.kind == param.POSITIONAL_ONLY:
-                num_positional_only += 1
-            elif param.kind == param.KEYWORD_ONLY:
-                num_keyword_only += 1
-            elif param.kind == param.VAR_KEYWORD:
-                num_keyword_only += 1
-        return ParametersInfo(
-            total=total,
-            positional_only=num_positional_only,
-            keyword_only=num_keyword_only,
-        )
-
-    def _build_parameters_info_dict_from_function(self, function_tree):
-        ret = {}
-
-        for framework in available_frameworks:
-            method = self._import_function(function_tree, framework)
-            parameter_info = self._build_parameter_info(method)
-            ret[framework] = parameter_info
-
-        return ret
-
-    def _build_parameters_info_dict_from_method(self, method_tree):
-        ret = {}
-
-        for framework in available_frameworks:
-            method = self._import_method(method_tree, framework)
-            parameter_info = self._build_parameter_info(method)
-            ret[framework] = parameter_info
-
-        return ret
-
-    def _build_num_positional_arguments_strategy_from_function(self, method_tree: str):
-        dict_for_num_pos_strategy = self._build_parameters_info_dict_from_function(
-            method_tree
-        )
-        return num_positional_args_from_dict(dict_for_num_pos_strategy)
-
-    def _build_num_positional_arguments_strategy_from_method(self, method_tree: str):
-        dict_for_num_pos_strategy = self._build_parameters_info_dict_from_method(
-            method_tree
-        )
-        return num_positional_args_from_dict(dict_for_num_pos_strategy)
 
     def _partition_method_tree(self, method_tree: str):
         class_module_and_name, _, method_name = method_tree.rpartition(".")
