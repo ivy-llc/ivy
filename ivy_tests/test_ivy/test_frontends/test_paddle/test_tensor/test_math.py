9dbc1bfe06dad329bfcc27848450a413244c7d4e
Muhammad Ishaque Nizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-08-07 22:57:20 +0500

    EHN: adding kron in paddle math..py (#18280)
    Co-authored-by: zaeemansari70 <zaeemansari70@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -1894,3 +1894,35 @@ def test_paddle_mm(
         input=x,
         mat2=y,
     )
+
+
+# kron
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.kron",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=2,
+        allow_inf=False,
+        shared_dtype=True,
+    ),
+)
+def test_paddle_kron(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    backend_fw,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        fn_tree=fn_tree,
+        test_flags=test_flags,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
