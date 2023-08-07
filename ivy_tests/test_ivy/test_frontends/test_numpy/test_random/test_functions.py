cc1233556a596abb5faf666f26a953c30f2c73e3
Ellenella <fitsumella90@gmail.com>
2023-08-07 12:30:33 +0300

    I've just updated Docker to the latest version, and it has resolved the 'dill not found' error.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_random/test_functions.py
@@ -832,3 +832,41 @@ def test_numpy_logistic(
         scale=scale,
         size=size,
     )
+
+
+# pareto
+@handle_frontend_test(
+    fn_tree="numpy.random.pareto",
+    input_dtypes=helpers.get_dtypes("float", index=2),
+    a=st.floats(
+        allow_nan=False,
+        allow_infinity=False,
+        width=32,
+        min_value=1,
+        max_value=1000,
+        exclude_min=True,
+    ),
+    size=helpers.get_shape(allow_none=True),
+    test_with_out=st.just(False),
+)
+def test_numpy_pareto(
+        input_dtypes,
+        frontend,
+        test_flags,
+        backend_fw,
+        fn_tree,
+        on_device,
+        a,
+        size,
+):
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=False,
+        a=a,
+        size=size,
+    )
