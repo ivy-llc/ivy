b4bdc55718ef7fa026ef4ee3b0e15c13140ee08f
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-08-07 21:56:25 +0100

    uncomment to_numpy test and leave marked xfail until testing pipeline fully in order

--- ivy_tests/test_ivy/test_frontends/test_pandas/test_series.py
+++ ivy_tests/test_ivy/test_frontends/test_pandas/test_series.py
@@ -1,12 +1,13 @@
 # global
-# from hypothesis import strategies as st
-# import numpy as np
+from hypothesis import strategies as st
+import numpy as np
+import pytest
 # local
-# import ivy_tests.test_ivy.helpers as helpers
-# from ivy_tests.test_ivy.helpers import handle_frontend_method
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_method
 
 
-# CLASS_TREE = "ivy.functional.frontends.pandas.Series"
+CLASS_TREE = "ivy.functional.frontends.pandas.Series"
 
 # ToDo: uncomment this when frontend tests are independent of backends
 
@@ -41,40 +42,43 @@
 #     )
 
 
-# @handle_frontend_method(
-#     class_tree=CLASS_TREE,
-#     init_tree="pandas.Series",
-#     method_name="to_numpy",
-#     dtype_x=helpers.dtype_and_values(
-#         available_dtypes=helpers.get_dtypes("valid")),
-#     na_value=st.sampled_from([None, np.nan, np.inf, -np.inf]),
-#     copy=st.booleans(),
-# )
-# def test_pandas_series_to_numpy(
-#     dtype_x,
-#     frontend,
-#     na_value,
-#     copy,
-#     frontend_method_data,
-#     init_flags,
-#     method_flags,
-#     on_device,
-# ):
-#     # todo add castable dtypes for output
-#     input_dtype, x = dtype_x
-#     helpers.test_frontend_method(
-#         init_input_dtypes=input_dtype,
-#         init_all_as_kwargs_np={
-#             "data": x[0],
-#         },
-#         method_input_dtypes=input_dtype,
-#         method_all_as_kwargs_np={
-#             "na_value": na_value,
-#             "copy": copy,
-#         },
-#         frontend_method_data=frontend_method_data,
-#         init_flags=init_flags,
-#         method_flags=method_flags,
-#         frontend=frontend,
-#         on_device=on_device,
-#     )
+@pytest.mark.xfail(reason="testing pipeline fixes")
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="pandas.Series",
+    method_name="to_numpy",
+    dtype_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid")),
+    na_value=st.sampled_from([None, np.nan, np.inf, -np.inf]),
+    copy=st.booleans(),
+)
+def test_pandas_series_to_numpy(
+    dtype_x,
+    na_value,
+    copy,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    on_device,
+    frontend,
+    backend_fw,
+):
+    # todo add castable dtypes for output
+    input_dtype, x = dtype_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "na_value": na_value,
+            "copy": copy,
+        },
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        on_device=on_device,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+    )
