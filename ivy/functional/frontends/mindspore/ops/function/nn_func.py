03369bafc4485eb47f5719d232e8099a9c981357
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-09 01:13:24 +0530

    added the softshrink support

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -54,14 +54,24 @@ def selu(input_x):
 def softsign(x):
     return ivy.divide(x, ivy.add(1, ivy.abs(x)))
 
+
 @with_supported_dtypes(
-    {"2.0.0 and below": ("int8", "int16", "int32", "int64", "float16", "float32", "float64")},
+    {
+        "2.0.0 and below": (
+            "int8",
+            "int16",
+            "int32",
+            "int64",
+            "float16",
+            "float32",
+            "float64",
+        )
+    },
     "mindspore",
 )
 @to_ivy_arrays_and_back
 def dropout3d(input, p=0.5, training=True):
     return ivy.dropout3d(input, p, training=training, data_format="NCDHW")
-
 
 
 @with_supported_dtypes(
@@ -169,3 +179,11 @@ def avg_pool2d(
         count_include_pad=count_include_pad,
         divisor_override=divisor_override,
     )
+
+
+@with_supported_dtypes({"2.0.0 and below": ("float32", "float64")}, "mindspore")
+@to_ivy_arrays_and_back
+def softshrink(x, lambd=0.5):
+    low = ivy.where(ivy.less(input, -lambd), ivy.add(input, lambd), 0)
+    up = ivy.where(ivy.greater(input, lambd), ivy.subtract(input, lambd), 0)
+    return ivy.add(low, up)
--- ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
+++ ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
@@ -1,5 +1,5 @@
 # global
-# from hypothesis import strategies as st
+# from hypothesis import assume, strategies as st
 
 # local
 # TODO: uncomment after frontend is not required
@@ -359,4 +359,38 @@
 #         pad_mode=pad_mode,
 #         count_include_pad=count_include_pad,
 #         divisor_override=None,
+#     )
+
+# def _filter_dtypes(input_dtype):
+#     assume(("bfloat16" not in input_dtype) and ("float16" not in input_dtype))
+
+# # softshrink
+# @handle_frontend_test(
+#     fn_tree="mindspore.ops.function.nn_func.softshrink",
+#     dtype_and_input=helpers.dtype_and_values(
+#         available_dtypes=helpers.get_dtypes("float"),
+#     ),
+#     lambd=helpers.floats(min_value=0, max_value=1, exclude_min=True),
+# )
+# def test_torch_softshrink(
+#     *,
+#     dtype_and_input,
+#     lambd,
+#     on_device,
+#     fn_tree,
+#     frontend,
+#     test_flags,
+#     backend_fw,
+# ):
+#     input_dtype, x = dtype_and_input
+#     _filter_dtypes(input_dtype)
+#     helpers.test_frontend_function(
+#         input_dtypes=input_dtype,
+#         backend_to_test=backend_fw,
+#         frontend=frontend,
+#         test_flags=test_flags,
+#         fn_tree=fn_tree,
+#         on_device=on_device,
+#         x=x[0],
+#         lambd=lambd,
 #     )
