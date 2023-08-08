dd272980efacdb62dd48db3667f13c0bca1a3cb8
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-08 22:58:04 +0530

    added the support

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -54,14 +54,24 @@ def selu(input_x):
 def softsign(x):
     return ivy.divide(x, ivy.add(1, ivy.abs(x)))
 
+
 @with_supported_dtypes(
-    {"2.0.0 and below": ("int8", "int16", "int32", "int64", "float16", "float32", "float64")},
+    {
+        "2.0.0 and below": (
+            "int8",
+            "int16",
+            "int32",
+            "int64",
+            "float16",
+            "float32",
+            "float64",
+        )
+    },
     "mindspore",
 )
 @to_ivy_arrays_and_back
 def dropout3d(input, p=0.5, training=True):
     return ivy.dropout3d(input, p, training=training, data_format="NCDHW")
-
 
 
 @with_supported_dtypes(
@@ -168,4 +178,15 @@ def avg_pool2d(
         pad_mode=pad_mode,
         count_include_pad=count_include_pad,
         divisor_override=divisor_override,
+    )
+
+
+@with_supported_dtypes(
+    {"2.0.0 and below": ("float16", "float32", "float64")},
+    "mindspore",
+)
+@to_ivy_arrays_and_back
+def fast_gelu(input_x):
+    return (input_x / (1 + ivy.exp(-1.702 * ivy.abs(input_x)))) * ivy.exp(
+        0.851 * (input_x - ivy.abs(input_x))
     )
