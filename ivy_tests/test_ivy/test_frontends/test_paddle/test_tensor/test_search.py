d825156246c773a32ad41b42d5cb49236b7bac72
Luc G. K. Atakpa <45469409+kgmann@users.noreply.github.com>
2023-08-08 15:37:24 +0100

    fix: fixed frontend test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_search.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_search.py
@@ -168,9 +168,12 @@ def test_paddle_searchsorted(
 @handle_frontend_test(
     fn_tree="paddle.topk",
     dtype_x_and_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("valid")
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
     ),
-    k=st.integers(min_value=0, max_value=5),
+    k = st.data(),
     sorted=st.booleans(),
     largest=st.booleans(),
 )
@@ -180,21 +183,25 @@ def test_paddle_topk(
     k,
     sorted,
     largest,
+    on_device,
     fn_tree,
     frontend,
-    test_flags,
-    on_device,
     backend_fw,
+    test_flags,
 ):
     input_dtypes, x, axis = dtype_x_and_axis
+    k = k.draw(st.integers(min_value=1, max_value=x[0].shape[axis]))
     helpers.test_frontend_function(
         input_dtypes=input_dtypes,
         frontend=frontend,
+        backend_to_test=backend_fw,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        fw=backend_fw,
+        x=x[0],
         k=k,
-        sorted=sorted,
+        axis=axis,
         largest=largest,
+        sorted=sorted,
+        test_values=False,
     )
