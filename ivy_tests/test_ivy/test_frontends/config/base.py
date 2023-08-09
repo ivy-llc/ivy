ecabec5299bb6e7ac968c71429c7267be21f3095
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-08-09 23:19:20 +0000

    Add base classes for frontend config.

---
+++ ivy_tests/test_ivy/test_frontends/config/base.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractproperty, abstractmethod
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class SupportedDeviecs:
+    valid_devices: List[str]
+    invalid_devices: List[str]
+
+
+# TODO can be refactored and be constructed dynamically
+@dataclass
+class SupportedDtypes:
+    valid_dtypes: List[str]
+    invalid_dtypes: List[str]
+
+    valid_numeric_dtypes: List[str]
+    invalid_numeric_dtypes: List[str]
+
+    valid_int_dtypes: List[str]
+    invalid_int_dtypes: List[str]
+
+    valid_uint_dtypes: List[str]
+    invalid_uint_dtypes: List[str]
+
+    valid_uint_dtypes: List[str]
+    invalid_uint_dtypes: List[str]
+
+    valid_float_dtypes: List[str]
+    invalid_float_dtypes: List[str]
+
+    valid_complex_dtypes: List[str]
+    invalid_complex_dtypes: List[str]
+
+
+class FrontendConfig(ABC):
+    @abstractproperty
+    def supported_dtypes() -> SupportedDtypes:
+        pass
+
+    @abstractproperty
+    def supported_devices() -> SupportedDeviecs:
+        pass
+
+    @abstractproperty
+    def Dtype():
+        pass
+
+    @abstractproperty
+    def Device():
+        pass
+
+    @abstractmethod
+    def native_array(self, x):
+        pass
+
+    @abstractmethod
+    def is_native_array(self, x):
+        pass
+
+    @abstractmethod
+    def to_numpy(self, x):
+        pass
+
+    @abstractmethod
+    def as_native_dtype(self, dtype: str):
+        pass
+
+    @abstractmethod
+    def as_native_device(self, device: str):
+        pass
+
+    @abstractmethod
+    def isscalar(self, x):
+        pass
