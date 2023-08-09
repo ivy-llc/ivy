f84126252a3a3b2d0c7221e6ea0ce39ee4a0b06d
Niteshk84 <niteshkesharwani726@gmail.com>
2023-08-09 23:44:37 +0530

    Added func and test

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1671,6 +1671,10 @@ class Tensor:
     def gcd(self, other, *, out=None):
         return torch_frontend.gcd(self, other, out=out)
 
+    @with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16", "uint16", "bool", "complex64", "complex128")}, "torch")
+    def isnan(self):
+        return torch_frontend.isnan(self)
+
 
 class Size(tuple):
     def __new__(cls, iterable=()):
