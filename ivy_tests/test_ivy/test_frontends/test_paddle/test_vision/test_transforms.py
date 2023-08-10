d1ec33635fc0a191108dd4f4f21173719a6bce1b
praneet9 <praneetbomma@gmail.com>
2023-08-10 12:52:17 +0100

    paddle crop test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_vision/test_transforms.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_vision/test_transforms.py
@@ -5,6 +5,36 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
+# crop
+@handle_frontend_test(
+    fn_tree="paddle.crop",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("valid")),
+)
+def test_paddle_crop(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, input = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        image=input[0],
+        top=input[1],
+        left=input[2],
+        height=input[3],
+        width=input[4]
+    )
+
+
 # to_tensor
 @handle_frontend_test(
     fn_tree="paddle.to_tensor",
