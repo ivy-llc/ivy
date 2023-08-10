a061ac8da17d6d879b7f92436f2eb926a8044511
Olawale Onabola <walexi4great@gmail.com>
2023-08-10 05:27:10 -0700

    fix failing test - unsupported dtypes

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -1,10 +1,8 @@
 import ivy
-from ivy.func_wrapper import with_unsupported_dtypes
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
-@with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
 def hamming_window(
     window_length,
     periodic=True,
@@ -19,3 +17,11 @@ def hamming_window(
     return ivy.hamming_window(
         window_length, periodic=periodic, alpha=alpha, beta=beta, dtype=dtype
     )
+
+
+hamming_window.unsupported_dtypes = {
+    "torch": ("float16", "bfloat16"),
+    "numpy": ("float16", "bfloat16"),
+    "jax": ("float16", "bfloat16"),
+    "tensorflow": ("float16", "bfloat16"),
+}
