9fd27ea0106bfc8d5b843e27254ba24e5a0a1031
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-08-10 00:12:34 +0000

    Update Numpy config.

--- ivy_tests/test_ivy/test_frontends/config/numpy.py
+++ ivy_tests/test_ivy/test_frontends/config/numpy.py
@@ -1,103 +1,117 @@
+from .base import FrontendConfig, SupportedDeviecs, SupportedDtypes
 import numpy as np
 
-valid_devices = "cpu"
-invalid_devices = ("gpu", "tpu")
 
-valid_dtypes = [
-    "int8",
-    "int16",
-    "int32",
-    "int64",
-    "uint8",
-    "uint16",
-    "uint32",
-    "uint64",
-    "float16",
-    "float32",
-    "float64",
-    "complex64",
-    "complex128",
-    "bool",
-]
-invalid_dtypes = ["bfloat16"]
+class NumpyFrontendConfig(FrontendConfig):
+    Dtype = np.dtype
+    Device = str
 
-valid_numeric_dtypes = [
-    "int8",
-    "int16",
-    "int32",
-    "int64",
-    "uint8",
-    "uint16",
-    "uint32",
-    "uint64",
-    "float16",
-    "float32",
-    "float64",
-    "complex64",
-    "complex128",
-]
-invalid_numeric_dtypes = ["bfloat16"]
+    valid_devices = "cpu"
+    invalid_devices = ("gpu", "tpu")
 
-valid_int_dtypes = [
-    "int8",
-    "int16",
-    "int32",
-    "int64",
-    "uint8",
-    "uint16",
-    "uint32",
-    "uint64",
-]
-invalid_int_dtypes = []
+    valid_dtypes = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+        "float16",
+        "float32",
+        "float64",
+        "complex64",
+        "complex128",
+        "bool",
+    ]
+    invalid_dtypes = ["bfloat16"]
 
-valid_uint_dtypes = [
-    "uint8",
-    "uint16",
-    "uint32",
-    "uint64",
-]
-invalid_uint_dtypes = []
+    valid_numeric_dtypes = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+        "float16",
+        "float32",
+        "float64",
+        "complex64",
+        "complex128",
+    ]
+    invalid_numeric_dtypes = ["bfloat16"]
 
-valid_float_dtypes = [
-    "float16",
-    "float32",
-    "float64",
-]
-invalid_float_dtypes = ["bfloat16"]
+    valid_int_dtypes = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+    ]
+    invalid_int_dtypes = []
 
-valid_complex_dtypes = [
-    "complex64",
-    "complex128",
-]
-invalid_complex_dtypes = []
+    valid_uint_dtypes = [
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+    ]
+    invalid_uint_dtypes = []
 
+    valid_float_dtypes = [
+        "float16",
+        "float32",
+        "float64",
+    ]
+    invalid_float_dtypes = ["bfloat16"]
 
-# Helpers for function testing
+    valid_complex_dtypes = [
+        "complex64",
+        "complex128",
+    ]
+    invalid_complex_dtypes = []
 
+    @property
+    def supported_devices(self):
+        return SupportedDeviecs(
+            valid_devices=self.valid_devices, invalid_devices=self.invalid_devices
+        )
 
-Dtype = np.dtype
-Device = str
+    @property
+    def supported_dtypes(self):
+        return SupportedDtypes(
+            valid_dtypes=self.valid_dtypes,
+            invalid_dtypes=self.invalid_dtypes,
+            valid_numeric_dtypes=self.valid_numeric_dtypes,
+            invalid_numeric_dtypes=self.invalid_numeric_dtypes,
+            valid_int_dtypes=self.valid_intdtypes,
+            invalid_int_dtypes=self.invalid_int_dtypes,
+            valid_uint_dtypes=self.valid_uint_dtypes,
+            invalid_uint_dtypes=self.invalid_uint_dtypes,
+            valid_float_dtypes=self.valid_float_dtypes,
+            invalid_float_dtypes=self.invalid_float_dtypes,
+        )
 
+    def native_array(self, x):
+        return np.array(x)
 
-def native_array(x):
-    return np.array(x)
+    def is_native_array(self, x):
+        return isinstance(x, np.ndarray)
 
+    def to_numpy(self, x):
+        return x
 
-def is_native_array(x):
-    return isinstance(x, np.ndarray)
+    def as_native_dtype(self, dtype: str):
+        return np.dtype(dtype)
 
+    def as_native_device(self, device: str):
+        return device
 
-def to_numpy(x):
-    return x
-
-
-def as_native_dtype(dtype: str):
-    return np.dtype(dtype)
-
-
-def as_native_dev(device: str):
-    return device
-
-
-def isscalar(x):
-    return np.isscalar(x)
+    def isscalar(self, x):
+        return np.isscalar(x)
