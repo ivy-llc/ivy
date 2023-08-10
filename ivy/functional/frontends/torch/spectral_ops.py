8a02bafdb2af365cde86ed195fd80523730b3f63
Olawale Onabola <walexi4great@gmail.com>
2023-08-10 05:06:17 -0700

    fix failing test - added unsupported dtypes decorator

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -1,8 +1,10 @@
 import ivy
+from ivy.func_wrapper import with_unsupported_dtypes
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
+@with_unsupported_dtypes({"2.0.1 and below": ("float16", "bfloat16")}, "torch")
 def hamming_window(
     window_length,
     periodic=True,
