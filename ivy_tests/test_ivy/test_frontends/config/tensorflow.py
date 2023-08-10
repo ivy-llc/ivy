fca8bf357b8ec39ab58656498f8edf3a65be6cfc
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-08-10 01:49:59 +0000

    Update tensorflow config to use backend.

--- ivy_tests/test_ivy/test_frontends/config/tensorflow.py
+++ ivy_tests/test_ivy/test_frontends/config/tensorflow.py
@@ -1,5 +1,5 @@
 from .base import FrontendConfig, SupportedDeviecs, SupportedDtypes
-import tensorflow as tf
+import ivy
 
 
 def get_config():
@@ -7,120 +7,55 @@ def get_config():
 
 
 class TensorflowFrontendConfig(FrontendConfig):
-    Dtype = tf.dtypes.DType
-    Device = tf.DeviceSpec
+    def __init__(self):
+        self.backend = ivy.with_backend("tensorflow", cached=True)
 
-    valid_devices = ("cpu", "gpu")
-    invalid_devices = ("tpu",)
+    @property
+    def Dtype(self):
+        return self.backend.Dtype
 
-    valid_dtypes = [
-        "int8",
-        "int16",
-        "int32",
-        "int64",
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-        "bfloat16",
-        "float16",
-        "float32",
-        "float64",
-        "complex64",
-        "complex128",
-        "bool",
-    ]
-    invalid_dtypes = []
-
-    valid_numeric_dtypes = [
-        "int8",
-        "int16",
-        "int32",
-        "int64",
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-        "bfloat16",
-        "float16",
-        "float32",
-        "float64",
-        "complex64",
-        "complex128",
-    ]
-    invalid_numeric_dtypes = []
-
-    valid_int_dtypes = [
-        "int8",
-        "int16",
-        "int32",
-        "int64",
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-    ]
-    invalid_int_dtypes = []
-
-    valid_uint_dtypes = [
-        "uint8",
-        "uint16",
-        "uint32",
-        "uint64",
-    ]
-    invalid_uint_dtypes = []
-
-    valid_float_dtypes = [
-        "bfloat16",
-        "float16",
-        "float32",
-        "float64",
-    ]
-    invalid_float_dtypes = []
-
-    valid_complex_dtypes = [
-        "complex64",
-        "complex128",
-    ]
-    invalid_complex_dtypes = []
+    @property
+    def Device(self):
+        return self.backend.Device
 
     @property
     def supported_devices(self):
         return SupportedDeviecs(
-            valid_devices=self.valid_devices, invalid_devices=self.invalid_devices
+            valid_devices=self.backend.valid_devices,
+            invalid_devices=self.backend.invalid_devices,
         )
 
     @property
     def supported_dtypes(self):
         return SupportedDtypes(
-            valid_dtypes=self.valid_dtypes,
-            invalid_dtypes=self.invalid_dtypes,
-            valid_numeric_dtypes=self.valid_numeric_dtypes,
-            invalid_numeric_dtypes=self.invalid_numeric_dtypes,
-            valid_int_dtypes=self.valid_int_dtypes,
-            invalid_int_dtypes=self.invalid_int_dtypes,
-            valid_uint_dtypes=self.valid_uint_dtypes,
-            invalid_uint_dtypes=self.invalid_uint_dtypes,
-            valid_float_dtypes=self.valid_float_dtypes,
-            invalid_float_dtypes=self.invalid_float_dtypes,
-            valid_complex_dtypes=self.valid_complex_dtypes,
-            invalid_complex_dtypes=self.invalid_complex_dtypes,
+            valid_dtypes=self.backend.valid_dtypes,
+            invalid_dtypes=self.backend.invalid_dtypes,
+            valid_numeric_dtypes=self.backend.valid_numeric_dtypes,
+            invalid_numeric_dtypes=self.backend.invalid_numeric_dtypes,
+            valid_int_dtypes=self.backend.valid_int_dtypes,
+            invalid_int_dtypes=self.backend.invalid_int_dtypes,
+            valid_uint_dtypes=self.backend.valid_uint_dtypes,
+            invalid_uint_dtypes=self.backend.invalid_uint_dtypes,
+            valid_float_dtypes=self.backend.valid_float_dtypes,
+            invalid_float_dtypes=self.backend.invalid_float_dtypes,
+            valid_complex_dtypes=self.backend.valid_complex_dtypes,
+            invalid_complex_dtypes=self.backend.invalid_complex_dtypes,
         )
 
     def native_array(self, x):
-        return tf.constant(x)
+        return self.backend.native_array(x)
 
     def is_native_array(self, x):
-        return isinstance(x, (tf.Tensor, tf.Variable))
+        return self.backend.is_native_array(x)
 
     def to_numpy(self, x):
-        return x.numpy()
+        return self.backend.to_numpy(x)
 
     def as_native_dtype(self, dtype: str):
-        return tf.as_dtype(dtype)
+        return self.backend.as_native_dtype(dtype)
 
     def as_native_device(self, device: str):
-        return tf.device(device)
+        return self.backend_as_native_dev(device)
 
     def isscalar(self, x):
-        return self.is_native_array(x) and tf.experimental.numpy.isscalar(x)
+        return self.backend.isscalar(x)
