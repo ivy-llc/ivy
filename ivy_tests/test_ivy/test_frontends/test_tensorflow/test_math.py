92c98f2f639e23eff57d61966cb6387b5a19af18
Nripesh Niketan <86844847+NripeshN@users.noreply.github.com>
2023-08-10 12:52:14 +0530

    Add rint tensorflow math (#19869)

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -2884,6 +2884,32 @@ def test_tensorflow_atanh(
     )
 
 
+@handle_frontend_test(
+    fn_tree="tensorflow.math.rint",
+    dtype_and_x=helpers.dtype_and_values(available_dtypes=helpers.get_dtypes("float")),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_rint(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        backend_to_test=backend_fw,
+        x=x[0],
+    )
+
+
 # bincount
 @handle_frontend_test(
     fn_tree="tensorflow.math.bincount",
