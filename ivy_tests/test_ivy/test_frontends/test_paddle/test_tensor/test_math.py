435cbab68ba6ddd8b2d19553943b96bbd51cd3b1
ArsalanAli915 <i191885@nu.edu.pk>
2023-08-10 16:55:36 +0500

    add rsqrt_ function

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -1746,6 +1746,33 @@ def test_paddle_rsqrt(
         x=x[0],
     )
 
+# rsqrt_
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.rsqrt_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_rsqrt(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
 
 # prod
 @handle_frontend_test(
