fc7c7154876d8f7b48ad0df8b024aba722c90d8c
Preeti-ing <139554427+Preeti-ing@users.noreply.github.com>
2023-08-11 22:59:22 +0530

    Delete functions.py

--- functional/frontends/numpy/random/functions.py
+++
@@ -1,4 +0,0 @@
-@to_ivy_arrays_and_back
-@from_zero_dim_arrays_to_scalar
-def standard_exponential(size = None):
-    return ivy.standard_exponential(scale = 1.0,shape = size,dtype = 'float64',method = 'zig')
