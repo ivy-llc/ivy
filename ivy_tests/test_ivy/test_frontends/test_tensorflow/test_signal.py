2e05d9d35ce1b9b0fa83ceaae76b330d202320c2
Enigmafiend <41777835+Enigmafiend@users.noreply.github.com>
2023-08-12 00:25:53 +0500

    Update test_signal.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -176,3 +176,33 @@ def test_tensorflow_vorbis_window(
         window_length=int(x[0]),
         # dtype=dtype[0],
     )
+
+# hann_window
+@handle_frontend_test(
+    fn_tree="tensorflow.signal.hann_window",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=["int32","int64"],
+        max_num_dims=0,
+        min_value=5,
+        max_value=10,
+    ),
+    periodic=st.booleans(),
+    # dtype=helpers.get_dtypes("float", full=False),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_hann_window(
+    *, dtype_and_x, test_flags, backend_fw, fn_tree, on_device, frontend,periodic  # ,dtype
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        atol=1e-02,
+        window_length=int(x[0]),
+        periodic=periodic,
+        # dtype=dtype[0],
+    )
