499b1acc599b50fac65d8192f5f0515c1661817d
FraBomba6 <francebomba6@gmail.com>
2023-08-11 12:03:36 +0200

    alpha_dropout and relative test

--- ivy/functional/frontends/torch/nn/functional/dropout_functions.py
+++ ivy/functional/frontends/torch/nn/functional/dropout_functions.py
@@ -11,6 +11,25 @@ def dropout(input, p=0.5, training=True, inplace=False):
     return ivy.dropout(input, p, training=training)
 
 
+# ToDo: this function will be simplified once ivy.alpha_dropout is implemented
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes({"2.0.1 and below": ("float16",)}, "torch")
+def alpha_dropout(input, p=0.5, training=False, inplace=False):
+    neg_saturation = ivy.log1p(ivy.exp(-ivy.square(input)))
+    mask = ivy.where(
+        ivy.random_uniform(shape=input.shape, device=ivy.dev(input)) < p,
+        0.0,
+        1.0,
+    )
+    if inplace:
+        ivy.inplace_update(input, mask * input + (1 - mask) * neg_saturation)
+        ivy.inplace_update(input, input / ivy.sqrt(1 - p / (1 - p + 1e-5)))
+        return input
+    else:
+        masked = mask * input + (1 - mask) * neg_saturation
+        return masked / ivy.sqrt(1 - p / (1 - p + 1e-5))
+
+
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes({"2.0.1 and below": ("float16",)}, "torch")
 def dropout1d(input, p=0.5, training=True, inplace=False):
