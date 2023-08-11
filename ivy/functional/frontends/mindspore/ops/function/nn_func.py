403d2140f76973fbe24a7faa5871dd5244986bf7
Donghu Guo <123098998+guodh321@users.noreply.github.com>
2023-08-11 01:43:04 +0100

    Added log_softmax for Mindspore frontend #21254 (#21324)

--- ivy/functional/frontends/mindspore/ops/function/nn_func.py
+++ ivy/functional/frontends/mindspore/ops/function/nn_func.py
@@ -55,6 +55,12 @@ def softsign(x):
     return ivy.divide(x, ivy.add(1, ivy.abs(x)))
 
 
+@with_supported_dtypes({"2.0.0 and below": ("float16", "float32")}, "mindspore")
+@to_ivy_arrays_and_back
+def log_softmax(input, axis=-1):
+    return ivy.log_softmax(input)
+
+
 @with_supported_dtypes({"2.0 and below": ("float16", "float32")}, "mindspore")
 @to_ivy_arrays_and_back
 def kl_div(logits, labels, reduction="mean"):
