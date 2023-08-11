57957ff3eb02304c6bce6b00beb390f6d99d65d6
Donghu Guo <123098998+guodh321@users.noreply.github.com>
2023-08-11 01:43:04 +0100

    Added log_softmax for Mindspore frontend #21254 (#21324)

--- ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
+++ ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
@@ -319,6 +319,27 @@
 #         x=x[0],
 #         output_size=output_size,
 #     )
+
+
+# log_softmax
+# @handle_frontend_test(
+#     fn_tree="mindspore.ops.function.nn_func.log_softmax",
+#     dtype_and_x=helpers.dtype_and_values(
+#         available_dtypes=helpers.get_dtypes("valid"),
+#         safety_factor_scale="log",
+#         small_abs_safety_factor=20,
+#     ),
+# )
+# def test_mindspore_log_softmax(
+#     *,
+#     dtype_and_x,
+#     on_device,
+#     fn_tree,
+#     frontend,
+#     test_flags,
+# ):
+#     input_dtype, x = dtype_and_x
+
 # def _is_same_padding(padding, stride, kernel_size, input_shape):
 #     output_shape = tuple(
 #         [
@@ -421,13 +442,18 @@
 # ):
 #     input_dtype, x = dtype_and_x
 
+
 #     helpers.test_frontend_function(
 #         input_dtypes=input_dtype,
 #         frontend=frontend,
 #         test_flags=test_flags,
 #         fn_tree=fn_tree,
 #         on_device=on_device,
+
+#         x=x[0],
+
 #         input=x[0],
+
 #     )
 
 
