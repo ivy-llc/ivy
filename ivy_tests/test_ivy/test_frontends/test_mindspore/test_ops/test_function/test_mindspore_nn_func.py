eb051803ae1402a0f9a01f668cd7d75d29b6b601
Yash Jindal <57638523+yashj8@users.noreply.github.com>
2023-08-11 11:06:31 +0530

    completed test

--- ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
+++ ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
@@ -89,6 +89,20 @@
 # def test_mindspore_hardswish(
 #     *,
 #     dtype_and_x,
+#     on_device,
+#     fn_tree,
+#     frontend,
+#     test_flags,
+# ):
+#     input_dtype, x = dtype_and_x
+#     helpers.test_frontend_function(
+#         input_dtypes=input_dtype,
+#         frontend=frontend,
+#         test_flags=test_flags,
+#         fn_tree=fn_tree,
+#         on_device=on_device,
+#         x=x[0],
+#     )
 
 
 # pad
