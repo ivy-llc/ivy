b3b7e5346e01951f1f7fa3c7565ae22f75380e73
Shreyansh Bardia <shrebardia13@gmail.com>
2023-08-12 18:43:47 +0530

    included dtype error in all function

--- ivy/functional/frontends/numpy/ndarray/ndarray.py
+++ ivy/functional/frontends/numpy/ndarray/ndarray.py
@@ -250,6 +250,11 @@ class ndarray:
         return np_frontend.swapaxes(self, axis1, axis2)
 
     def all(self, axis=None, dtype=None, out=None, keepdims=False, *, where=True):
+        if not (dtype is None or ivy.is_bool_dtype(dtype)):
+            raise TypeError(
+                "No loop matching the specified signature and "
+                "casting was found for ufunc logical_or"
+            )
         return np_frontend.all(self, axis, out, keepdims, where=where)
 
     def any(self, axis=None, dtype=None, out=None, keepdims=False, *, where=True):
