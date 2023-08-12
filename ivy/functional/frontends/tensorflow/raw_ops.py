33b4afb424a1557e9285681b297ed6bb97839f38
Jubii100 <90710349+Jubii100@users.noreply.github.com>
2023-08-13 02:39:10 +0400

    Add RowOps Tile to Tensorflow Frontend
    Close #21545

--- ivy/functional/frontends/tensorflow/raw_ops.py
+++ ivy/functional/frontends/tensorflow/raw_ops.py
@@ -569,6 +569,9 @@ def TanhGrad(*, y, dy, name="TanhGrad"):
     return ivy.multiply(dy, ivy.subtract(1, ivy.multiply(y, y)))
 
 
+Tile = to_ivy_arrays_and_back(map_raw_ops_alias(tf_frontend.general_functions.tile))
+
+
 @to_ivy_arrays_and_back
 def Transpose(*, x, perm, name="Transpose"):
     ret = ivy.permute_dims(x, axes=perm)
