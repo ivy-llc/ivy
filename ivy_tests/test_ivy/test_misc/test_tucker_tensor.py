1b0f18ceb0e48cd61977031fc8c00379b7ce59af
Anwaar Khalid <shahanwaar.khalid8@gmail.com>
2023-08-12 07:30:48 +0000

    Added test for tucker to tensor from TensorLy

--- ivy_tests/test_ivy/test_misc/test_tucker_tensor.py
+++ ivy_tests/test_ivy/test_misc/test_tucker_tensor.py
@@ -36,3 +36,40 @@ def test_validate_tucker_tensor(true_shape, true_rank):
     # Not enough factors
     with np.testing.assert_raises(ValueError):
         ivy.TuckerTensor.validate_tucker_tensor((core, factors[:1]))
+
+
+@pytest.mark.parametrize(
+    "X, ranks, true_res",
+    [
+        (
+            [
+                [[1.0, 13], [4, 16], [7, 19], [10, 22]],
+                [[2, 14], [5, 17], [8, 20], [11, 23]],
+                [[3, 15], [6, 18], [9, 21], [12, 24]],
+            ],
+            [2, 3, 4],
+            [
+                [
+                    [390.0, 1518, 2646, 3774],
+                    [1310, 4966, 8622, 12278],
+                    [2230, 8414, 14598, 20782],
+                ],
+                [
+                    [1524, 5892, 10260, 14628],
+                    [5108, 19204, 33300, 47396],
+                    [8692, 32516, 56340, 80164],
+                ],
+            ],
+        )
+    ],
+)
+def test_tucker_to_tensor(X, ranks, true_res):
+    """Test for tucker_to_tensor."""
+    X = ivy.array(X)
+    U = [
+        ivy.arange(R * s, dtype=ivy.float32).reshape((R, s))
+        for (R, s) in zip(ranks, X.shape)
+    ]
+    true_res = ivy.array(true_res)
+    res = ivy.TuckerTensor.tucker_to_tensor((X, U))
+    assert np.allclose(true_res, res)
