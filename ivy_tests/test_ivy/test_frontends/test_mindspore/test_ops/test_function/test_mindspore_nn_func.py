b2f76e89e4230cbbfba1d28d041fe35d3fe6be89
Yash Jindal <57638523+yashj8@users.noreply.github.com>
2023-08-13 14:26:44 +0530

    restored deleted lines from test

--- ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
+++ ivy_tests/test_ivy/test_frontends/test_mindspore/test_ops/test_function/test_mindspore_nn_func.py
@@ -93,6 +93,16 @@
 #     fn_tree,
 #     frontend,
 #     test_flags,
+# ):
+#     input_dtype, x = dtype_and_x
+#     helpers.test_frontend_function(
+#         input_dtypes=input_dtype,
+#         frontend=frontend,
+#         test_flags=test_flags,
+#         fn_tree=fn_tree,
+#         on_device=on_device,
+#         x=x[0],
+#     )
 
 
 # pad
