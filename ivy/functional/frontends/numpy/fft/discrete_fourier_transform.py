0687079aa9444b37820e637887e16cb3bf6baa30
MartinOcharo <73750165+MartinOcharo@users.noreply.github.com>
2023-08-13 22:44:22 +0300

    Update discrete_fourier_transform.py

--- ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
+++ ivy/functional/frontends/numpy/fft/discrete_fourier_transform.py
@@ -140,10 +140,4 @@ def rfftn(a, s=None, axes=None, norm=None):
     return ivy.rfftn(a, s=s, axes=axes, norm=norm)
 
 
-@with_unsupported_dtypes({"1.25.1 and below": ("float16",)}, "numpy")
-@to_ivy_arrays_and_back
-def ifft2(a, s=None, axis=(-2, -1), norm=None):
-    a = ivy.array(a, dtype=ivy.complex128)
-    if norm is None:
-        norm = "backward"
-    return ivy.ifft2(a, axis, norm=norm, s=s)
+
