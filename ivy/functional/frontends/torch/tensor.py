2e97de61a41afbed084a206e18db0eca90a5b9f0
Bhushan Srivastava <bhushansrivastava2001@gmail.com>
2023-08-13 22:05:36 +0530

    added log1p_

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1566,6 +1566,12 @@ class Tensor:
     def log1p(self):
         return torch_frontend.log1p(self)
 
+    @with_unsupported_dtypes({"2.0.1 and below": ("float16",)}, "torch")
+    def log1p_(self):
+        promoted_type = ivy.promote_types(self.dtype, "float32")
+        self.ivy_array = torch_frontend.log1p(self).to(promoted_type).ivy_array
+        return self
+
     def baddbmm(self, batch1, batch2, *, beta=1, alpha=1):
         return torch_frontend.baddbmm(
             self, batch1=batch1, batch2=batch2, beta=beta, alpha=alpha
