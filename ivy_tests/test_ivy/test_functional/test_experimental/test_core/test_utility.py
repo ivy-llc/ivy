d7ee996c0a8b3f974c2a49596033d288a4080045
yash goel <yashgoel236437@gmail.com>
2023-08-14 21:40:15 +0530

    dtype in test

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_utility.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_utility.py
@@ -6,6 +6,9 @@ from ivy_tests.test_ivy.helpers import handle_test
 @handle_test(
     fn_tree="functional.ivy.experimental.optional_get_element",
     dtype_and_x=helpers.dtype_and_values(
+        dtype=[
+            'int8', 'int16', 'int32', 'complex64', 'complex128', 'bool', 'float16', 'float32', 'float64', 'string', 'uint8'
+        ],
         min_value=-100,
         max_value=100,
         min_num_dims=0,
