f76738aae76a1520b91a59dfb7bb379db7e9e5c4
melch-inno <iba-melch@hotmail.com>
2023-08-15 08:43:54 -0400

    Remove  type cast

--- ivy/functional/frontends/paddle/nn/functional/norm.py
+++ ivy/functional/frontends/paddle/nn/functional/norm.py
@@ -1,12 +1,11 @@
 # local
 import ivy
-import paddle
 from ivy.func_wrapper import with_supported_dtypes
 from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
 
 
 @to_ivy_arrays_and_back
-@with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def batch_norm(
     x,
     running_mean,
@@ -18,13 +17,6 @@ def batch_norm(
     epsilon=1e-05,
     data_format="NCHW",
 ):
-    # convert to paddle tensors
-    x = paddle.to_tensor(ivy.to_list(x), dtype=x.dtype)
-    weight = paddle.to_tensor(ivy.to_list(weight), dtype=weight.dtype)
-    bias = paddle.to_tensor(ivy.to_list(bias), dtype=bias.dtype)
-    running_mean = paddle.to_tensor(ivy.to_list(running_mean), dtype=running_mean.dtype)
-    running_var = paddle.to_tensor(ivy.to_list(running_var), dtype=running_var.dtype)
-
     normalized, _, _ = ivy.batch_norm(
         x,
         running_mean,
