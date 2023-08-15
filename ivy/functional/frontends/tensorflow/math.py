cc02bea5339c0516be5172060cd02cd86b750fce
soumesh113 <58463935+soumesh113@users.noreply.github.com>
2023-08-15 17:45:09 +0530

    Update math.py

--- ivy/functional/frontends/tensorflow/math.py
+++ ivy/functional/frontends/tensorflow/math.py
@@ -549,6 +549,19 @@ def tanh(x, name=None):
 def rsqrt(x, name=None):
     return ivy.reciprocal(ivy.sqrt(x))
 
+@to_ivy_arrays_and_back
+def segment_sum(data, segment_ids, name= "segment_sum"):
+    data = ivy.array(data)
+    segment_ids = ivy.array(segment_ids)
+    ivy.utils.assertions.check_equal(
+        list(segment_ids.shape), [list(data.shape)[0]], as_array=False
+    )
+    sum_array = ivy.zeros(
+        tuple([segment_ids[-1]+1] + (list(data.shape))[1:]), dtype = ivy.int32
+    )
+    for i in range((segment_ids).shape[0]):
+         sum_array[segment_ids[i]] = sum_array[segment_ids[i]] + data[i]
+    return sum_array
 
 @to_ivy_arrays_and_back
 def nextafter(x1, x2, name=None):
