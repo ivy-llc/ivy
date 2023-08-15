b1b43385b5d7a89cae81ae84a249b7503b1c476f
VanshAgarwal12113097 <123140273+VanshAgarwal12113097@users.noreply.github.com>
2023-08-15 18:25:54 +0530

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1941,6 +1941,37 @@ def test_tensorflow_rsqrt(
     )
 
 
+# segment_mean
+@handle_frontend_test(
+    fn_tree="tensorflow.math.segment_mean",
+    data=helpers.array_values(dtype=ivy.int32, shape=(5, 6), min_value=1, max_value=9),
+    segment_ids=helpers.array_values(
+        dtype="int32", shape=(5,), min_value=0, max_value=4
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_segment_mean(
+    *,
+    data,
+    segment_ids,
+    frontend,
+    test_flags,
+    fn_tree,
+    backend_fw,
+    on_device,
+):
+    helpers.test_frontend_function(
+        input_dtypes=["int32", "int64"],
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        data=data,
+        segment_ids=segment_ids,
+    )
+
+
 # nextafter
 @handle_frontend_test(
     fn_tree="tensorflow.math.nextafter",
