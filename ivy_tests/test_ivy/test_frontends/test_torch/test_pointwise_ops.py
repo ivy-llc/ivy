5e5109f6e552364bf051db19c087d29dd08ddc2c
Simranjeet Singh <simranjeet@adeptmind.ai>
2023-08-15 06:20:37 +0530

    Add ldexp to torch frontend api with test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_pointwise_ops.py
@@ -2834,6 +2834,41 @@ def test_torch_igamma(
     )
 
 
+# ldexp
+@handle_frontend_test(
+    fn_tree="torch.ldexp",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=2,
+        min_value=-1e04,
+        max_value=1e04,
+        allow_inf=False,
+        min_dim_size=1,
+        min_num_dims=1,
+    ),
+)
+def test_torch_ldexp(
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        rtol=1e-03,
+        input=x[0],
+        other=x[1],
+    )
+
+
 @handle_frontend_test(
     fn_tree="torch.lgamma",
     dtype_and_input=helpers.dtype_and_values(
