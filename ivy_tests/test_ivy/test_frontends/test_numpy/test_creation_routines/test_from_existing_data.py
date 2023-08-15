f6de113e17aa9fb2b49ca15dae07dc4ee3e10d7d
Brian Gichungu <el20bwg@leeds.ac.uk>
2023-08-15 15:38:06 +0100

    Fixed numpy_frombuffer front end and test function

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_existing_data.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_existing_data.py
@@ -4,6 +4,9 @@ from hypothesis import strategies as st
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
+from ivy_tests.test_ivy.test_functional.test_core.test_creation import (
+    _get_dtype_buffer_count_offset,
+)
 
 
 @handle_frontend_test(
@@ -109,32 +112,28 @@ def test_numpy_copy(
 # frombuffer
 @handle_frontend_test(
     fn_tree="numpy.frombuffer",
-    dtype_and_a=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=1,
-        min_num_dims=0,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=5,
-    ),
+    dtype_buffer_count_offset=_get_dtype_buffer_count_offset(),
     test_with_out=st.just(False),
 )
 def test_numpy_frombuffer(
-    dtype_and_a,
-    frontend,
-    test_flags,
-    fn_tree,
+    *,
+    dtype_buffer_count_offset,
     on_device,
+    fn_tree,
+    frontend,
     backend_fw,
+    test_flags,
 ):
-    dtype, a = dtype_and_a
+    input_dtype, buffer, count, offset = dtype_buffer_count_offset
     helpers.test_frontend_function(
-        input_dtypes=dtype,
-        backend_to_test=backend_fw,
+        input_dtypes=input_dtype,
         frontend=frontend,
         test_flags=test_flags,
+        backend_to_test=backend_fw,
         fn_tree=fn_tree,
         on_device=on_device,
-        buffer=a,
-        dtype=dtype[0],
+        buffer=buffer,
+        dtype=input_dtype[0],
+        count=count,
+        offset=offset,
     )
