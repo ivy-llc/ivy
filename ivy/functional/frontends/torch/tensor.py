a254b1a9a784f57c53f4562aa68f9bad3ceb4fe6
Bhushan Srivastava <59949692+he11owthere@users.noreply.github.com>
2023-08-17 01:08:25 +0530

    Added remainder_ method to the PyTorch frontend (#22022)

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1059,6 +1059,13 @@ class Tensor:
     def remainder(self, other, *, out=None):
         return torch_frontend.remainder(self, other, out=out)
 
+    @with_supported_dtypes(
+        {"2.0.1 and below": ("float16", "float32", "float64", "bfloat16")}, "torch"
+    )
+    def remainder_(self, other, *, out=None):
+        self.ivy_array = torch_frontend.remainder(self, other, out=out).ivy_array
+        return self
+
     def bitwise_not_(self):
         self.ivy_array = self.bitwise_not().ivy_array
         return self
