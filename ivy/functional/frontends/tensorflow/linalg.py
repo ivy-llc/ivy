58a9e935bbc1c72e2a3e4e833ab60f78e2b3afee
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-08-16 03:36:52 +0000

    Added lu_solve function in linalg

--- ivy/functional/frontends/tensorflow/linalg.py
+++ ivy/functional/frontends/tensorflow/linalg.py
@@ -470,3 +470,8 @@ def tensor_diag_part(input, name=None):
     reshaped = ivy.reshape(input, (prod, prod))
     diagonal = ivy.diagonal(reshaped)
     return ivy.reshape(diagonal, tuple(half_shape))
+
+
+@to_ivy_arrays_and_back
+def lu_solve(lower_upper, perm, rhs, validate_args=False, name=None):
+    return ivy.solve(lower_upper, perm, rhs, validate_args=False, name=None)
