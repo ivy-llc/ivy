320cd6157bcfbee89f99638fd2ab54dbe8c328f8
illia-bab <illia@unify.ai>
2023-08-17 14:05:39 +0300

    modify builtins init file

--- ivy/functional/frontends/builtins/__init__.py
+++ ivy/functional/frontends/builtins/__init__.py
@@ -1,6 +1,5 @@
-from . import list
-from .list import *
 from . import func_wrapper
 from .func_wrapper import *
 from . import built_in_functions
 from .built_in_functions import *
+from . import math
