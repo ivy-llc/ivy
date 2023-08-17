5f7481bf5612fc081aa0d410245af184cd64ee32
Dharshannan Sugunan <dharshannan1607@gmail.com>
2023-08-17 22:43:20 +0100

    stft function implemented
    The stft function has been implemented using a combination of ivy framework function, however the float values produced are off by fractions from the actual tensorflow counterpart of the stft function. Will soon code up a test function for the stft function.

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -35,6 +35,51 @@ def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
     inverse_type = {1: 1, 2: 3, 3: 2, 4: 4}[type]
     return ivy.dct(input, type=inverse_type, n=n, axis=axis, norm=norm)
 
+# stft
 @to_ivy_arrays_and_back
-def stft(input_signal, frame_length, frame_step, fft_length):
-    pass
+def stft(signals, frame_length, frame_step, fft_length=None,
+         window_fn=ivy.hann_window, pad_end=False, name=None):
+
+    def stft_1D(signals, frame_length, frame_step, fft_length, pad_end):
+
+        num_samples = signals.shape[-1]
+
+        if pad_end:
+            num_samples = signals.shape[-1]
+            num_frames = -(-num_samples // frame_step)
+            pad_length = max(0, frame_length + frame_step * (num_frames - 1) - num_samples)
+
+            signals = ivy.pad(signals, [(0, pad_length)])
+        else:
+            num_frames = 1 + (num_samples - frame_length) // frame_step
+
+        stft_result = []
+
+        window = window_fn(frame_length, dtype=ivy.float32)
+
+        for i in range(num_frames):
+
+            start = i * frame_step
+            end = start + frame_length
+            frame = signals[..., start:end]
+            windowed_frame = frame * window
+            pad_length = fft_length - frame_length
+            windowed_frame = ivy.pad(windowed_frame, [(0, pad_length)])
+
+            fft_frame = ivy.fft(ivy.astype(windowed_frame, ivy.complex64), -1)
+            slit = int((fft_length // 2 + 1))
+            stft_result.append(fft_frame[..., 0:slit])
+
+        stft = ivy.stack(stft_result, axis=0)
+        return ivy.asarray(stft)
+
+    def stft_helper(nested_list, frame_length, frame_step, fft_length):
+
+        nested_list = ivy.asarray(nested_list)
+        if len(ivy.shape(nested_list)) > 1:
+            return [stft_helper(sublist, frame_length, frame_step, fft_length)
+                    for sublist in nested_list]
+        else:
+            return stft_1D(nested_list, frame_length, frame_step, fft_length, pad_end)
+
+    return ivy.asarray(stft_helper(signals, frame_length, frame_step, fft_length))
