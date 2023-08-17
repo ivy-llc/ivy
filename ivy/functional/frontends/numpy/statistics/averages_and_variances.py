05cdec2b3fa85079b3e8d7826ade6bc0bfebfae5
Shreyansh Bardia <shrebardia13@gmail.com>
2023-08-17 18:42:37 +0530

    updated var implementation and using var in std

--- ivy/functional/frontends/numpy/statistics/averages_and_variances.py
+++ ivy/functional/frontends/numpy/statistics/averages_and_variances.py
@@ -15,17 +15,42 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 @from_zero_dim_arrays_to_scalar
 def var(x, /, *, axis=None, ddof=0.0, keepdims=False, out=None, dtype=None, where=True):
     axis = tuple(axis) if isinstance(axis, list) else axis
-    dtype = (
-        dtype
-        if dtype is not None
-        else ivy.float64 if ivy.is_int_dtype(x.dtype) else x.dtype
-    )
-    ret = ivy.var(x, axis=axis, correction=ddof, keepdims=keepdims, out=out)
-    ret = (
-        ivy.where(where, ret, ivy.default(out, ivy.zeros_like(ret)), out=out)
-        if ivy.is_array(where)
-        else ret
-    )
+    if dtype is None:
+        if ivy.is_int_dtype(x.dtype):
+            dtype = ivy.float64
+        else:
+            dtype = x.dtype
+    if where is True or not ivy.any(~ivy.array(where)):
+        ret = ivy.var(x, axis=axis, correction=ddof, keepdims=keepdims, out=out)
+    elif ivy.any(where):
+        x = ivy.where(where, x, 0)
+        sum = ivy.sum(x, axis=axis, keepdims=True)
+        cnt = ivy.sum(where, axis=axis, keepdims=True, dtype=ivy.int64)
+        avg = ivy.divide(sum, cnt)
+        x = ivy.subtract(x, avg)
+        x = ivy.where(where, x, 0)
+        if ivy.is_complex_dtype(x.dtype):
+            sqr = ivy.multiply(x, x.conj()).real
+        else:
+            sqr = ivy.multiply(x, x)
+        var = ivy.sum(sqr, axis=axis, dtype=dtype, out=out, keepdims=keepdims)
+        cnt = ivy.sum(where, axis=axis, keepdims=keepdims, dtype=ivy.int64)
+        dof = cnt - ddof
+        ret = ivy.divide(var, dof).reshape(var.shape)
+    else:
+        if axis is not None:
+            axis = (axis,) if isinstance(axis, int) else axis
+            axis = tuple(map(lambda i: i if i >= 0 else x.ndim + i, axis))
+            if keepdims:
+                shape = tuple(1 if i in axis else x.shape[i] for i in range(x.ndim))
+            else:
+                shape = tuple(x.shape[i] for i in range(x.ndim) if i not in axis)
+        else:
+            if keepdims:
+                shape = tuple(1 for i in range(x.ndim))
+            else:
+                shape = ()
+        ret = ivy.full(shape, ivy.nan)
     return ret.astype(dtype, copy=False)
 
 
@@ -106,45 +131,12 @@ def std(
     dtype=None,
     where=True,
 ):
-    axis = tuple(axis) if isinstance(axis, list) else axis
-    if dtype is None:
-        if ivy.is_int_dtype(x.dtype):
-            dtype = ivy.float64
-        else:
-            dtype = x.dtype
-    if where is True or not ivy.any(~ivy.array(where)):
-        ret = ivy.std(x, axis=axis, correction=ddof, keepdims=keepdims, out=out)
-    elif ivy.any(where):
-        x = ivy.where(where, x, 0)
-        sum = ivy.sum(x, axis=axis, keepdims=True)
-        cnt = ivy.sum(where, axis=axis, keepdims=True, dtype=ivy.int64)
-        avg = ivy.divide(sum, cnt)
-        x = ivy.subtract(x, avg)
-        x = ivy.where(where, x, 0)
-        if ivy.is_complex_dtype(x.dtype):
-            sqr = ivy.multiply(x, x.conj()).real
-        else:
-            sqr = ivy.multiply(x, x)
-        var = ivy.sum(sqr, axis=axis, dtype=dtype, out=out, keepdims=keepdims)
-        cnt = ivy.sum(where, axis=axis, keepdims=keepdims, dtype=ivy.int64)
-        dof = cnt - ddof
-        var = ivy.divide(var, dof).reshape(var.shape)
-        ret = ivy.sqrt(var, out=out)
-    else:
-        if axis is not None:
-            axis = (axis,) if isinstance(axis, int) else axis
-            axis = tuple(map(lambda i: i if i >= 0 else x.ndim + i, axis))
-            if keepdims:
-                shape = tuple(1 if i in axis else x.shape[i] for i in range(x.ndim))
-            else:
-                shape = tuple(x.shape[i] for i in range(x.ndim) if i not in axis)
-        else:
-            if keepdims:
-                shape = tuple(1 for i in range(x.ndim))
-            else:
-                shape = ()
-        ret = ivy.full(shape, ivy.nan)
-    return ret.astype(dtype, copy=False)
+    ret = var(
+        x, axis=axis, ddof=ddof, keepdims=keepdims, out=out, dtype=dtype, where=where
+    ).ivy_array
+    if ivy.backend == "torch" and ret.dtype == "float16":
+        return ivy.sqrt(ret.astype(ivy.float64), out=out).astype(ret.dtype)
+    return ivy.sqrt(ret, out=out)
 
 
 @to_ivy_arrays_and_back
