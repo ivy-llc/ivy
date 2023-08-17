681cde947ad09b9d51d9fb548ca8b5ace24748c9
illia-bab <illia@unify.ai>
2023-08-17 13:55:04 +0300

    implemented all, any and round builtin functions

--- ivy/functional/frontends/builtins/built_in_functions.py
+++ ivy/functional/frontends/builtins/built_in_functions.py
@@ -16,3 +16,22 @@ def range(start, /, stop=None, step=1):
     if not stop:
         return ivy.arange(0, stop=start, step=step)
     return ivy.arange(start, stop=stop, step=step)
+
+
+@to_ivy_arrays_and_back
+@from_zero_dim_arrays_to_scalar
+def all(iterable):
+    return ivy.all(iterable)
+
+
+@to_ivy_arrays_and_back
+@from_zero_dim_arrays_to_scalar
+def any(iterable):
+    return ivy.any(iterable)
+
+
+@from_zero_dim_arrays_to_scalar
+def round(number, ndigits=None):
+    if not ndigits:
+        return ivy.round(number)
+    return ivy.round(number, decimals=ndigits)
