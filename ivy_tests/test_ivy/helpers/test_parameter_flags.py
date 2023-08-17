261176582b4bc4825cb898bf113f463c0950060d
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-08-17 12:52:38 +0000

    Update references to update_backend.

--- ivy_tests/test_ivy/helpers/test_parameter_flags.py
+++ ivy_tests/test_ivy/helpers/test_parameter_flags.py
@@ -1,7 +1,7 @@
 import abc
 from hypothesis import strategies as st
 from . import globals as test_globals
-from .pipeline_helper import update_backend
+from .pipeline_helper import BackendHandler
 
 
 @st.composite
@@ -100,7 +100,7 @@ class FunctionTestFlags(TestFlags):
 
     def apply_flags(self, args_to_iterate, input_dtypes, offset, *, backend, on_device):
         ret = []
-        with update_backend(backend) as backend:
+        with BackendHandler.update_backend(backend) as backend:
             for i, entry in enumerate(args_to_iterate, start=offset):
                 x = backend.array(entry, dtype=input_dtypes[i], device=on_device)
                 if self.as_variable[i]:
@@ -178,7 +178,7 @@ class FrontendFunctionTestFlags(TestFlags):
 
     def apply_flags(self, args_to_iterate, input_dtypes, offset, *, backend, on_device):
         ret = []
-        with update_backend(backend) as backend:
+        with BackendHandler.update_backend(backend) as backend:
             for i, entry in enumerate(args_to_iterate, start=offset):
                 x = backend.array(entry, dtype=input_dtypes[i], device=on_device)
                 if self.as_variable[i]:
@@ -239,7 +239,7 @@ class InitMethodTestFlags(TestFlags):
 
     def apply_flags(self, args_to_iterate, input_dtypes, offset, *, backend, on_device):
         ret = []
-        with update_backend(backend) as backend:
+        with BackendHandler.update_backend(backend) as backend:
             for i, entry in enumerate(args_to_iterate, start=offset):
                 x = backend.array(entry, dtype=input_dtypes[i], device=on_device)
                 if self.as_variable[i]:
@@ -293,7 +293,7 @@ class MethodTestFlags(TestFlags):
 
     def apply_flags(self, args_to_iterate, input_dtypes, offset, *, backend, on_device):
         ret = []
-        with update_backend(backend) as backend:
+        with BackendHandler.update_backend(backend) as backend:
             for i, entry in enumerate(args_to_iterate, start=offset):
                 x = backend.array(entry, dtype=input_dtypes[i], device=on_device)
                 if self.as_variable[i]:
@@ -350,7 +350,7 @@ class FrontendMethodTestFlags(TestFlags):
 
     def apply_flags(self, args_to_iterate, input_dtypes, offset, *, backend, on_device):
         ret = []
-        with update_backend(backend) as backend:
+        with BackendHandler.update_backend(backend) as backend:
             for i, entry in enumerate(args_to_iterate, start=offset):
                 x = backend.array(entry, dtype=input_dtypes[i], device=on_device)
                 if self.as_variable[i]:
