d08ca805b295f7b58298b9c8812d55093baf59fe
Enigmafiend <41777835+Enigmafiend@users.noreply.github.com>
2023-08-18 00:03:14 +0500

    Update fft.py

--- ivy/functional/frontends/torch/fft.py
+++ ivy/functional/frontends/torch/fft.py
@@ -4,8 +4,8 @@ from ivy import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
 def rfftn(input, s=None, dim=None, norm=None, *, out=None):
-    if dim == None:
+    if dim is None:
         dim = [-1]
-    if s == None:
+    if s is None:
         s = [input.size]
     return ivy.rfftn(input, s=s, axes=dim, norm=norm)
