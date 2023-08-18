85d253432173a46671abbb5972756bd67aea4447
illia-bab <illia@unify.ai>
2023-08-18 15:48:33 +0300

    implemented special functions and manually tested

--- ivy/functional/frontends/builtins/math/__init__.py
+++ ivy/functional/frontends/builtins/math/__init__.py
@@ -2,3 +2,5 @@ from . import power_and_logarithmic_functions
 from .power_and_logarithmic_functions import *
 from . import angular_conversion
 from .angular_conversion import *
+from . import special_functions
+from .special_functions import *
