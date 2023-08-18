84bd98d5b2a90538016e46e0da42e890b7b207ef
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-08-18 11:12:10 +0100

    removed duplicate import

--- ivy/functional/frontends/jax/numpy/logic.py
+++ ivy/functional/frontends/jax/numpy/logic.py
@@ -140,14 +140,13 @@ def any(a, axis=None, out=None, keepdims=False, *, where=None):
 alltrue = all
 
 sometrue = any
-from ivy.functional.frontends.jax.numpy import promote_types_of_jax_inputs
 
 
 @to_ivy_arrays_and_back
 # known issue in jnp's documentation of arguments
 # https://github.com/google/jax/issues/9119
 def logical_and(x1, x2, /):
-    x1, x2 = promote_types_of_jax_inputs(x1, x2)
+    x1, x2 = promote_jax_arrays(x1, x2)
     if x1.dtype == "complex128" or x2.dtype == "complex128":
         x1 = ivy.astype(x1, ivy.complex128)
         x2 = ivy.astype(x2, ivy.complex128)
