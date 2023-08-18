3ac7c9dce8855c9388a72e9af551c7bff641f75e
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-08-18 13:00:29 +0000

    Updated polynomial fit

---
+++ ivy/functional/frontends/numpy/ma/polynomial_fit.py
@@ -0,0 +1,26 @@
+import ivy
+
+from ivy.functional.frontends.numpy.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+
+
+@to_ivy_arrays_and_back
+def roots(p):
+    if isinstance(p, list):
+        p = ivy.asarray(p)
+
+    ftype = ivy.dtype(p)
+    if ftype in ["float64", "complex128"]:
+        ftype = "complex128"
+    else:
+        ftype = "complex64"
+
+    if p.size < 2:
+        return ivy.array([], dtype=ftype)
+    A = ivy.diag(
+        ivy.ones((p.size - 2), dtype=p.dtype),
+        k=-1,
+    )
+    A[0, :] = -p[1:] / p[0]
+    return ivy.eigvals(A)
