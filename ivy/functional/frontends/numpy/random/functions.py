df3fa66693634fd6c4091face03e4a0b9954a8fa
unknown <svetozar.milosevski@gmail.com>
2023-08-18 16:08:51 +0200

    Shape issue fix attempt 2

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -249,14 +249,14 @@ def hyper_helper(ngood, nbad, nsample):
             ngood -= 1
         elif nbad > 0:
             nbad -= 1
-    return sum(u)
+    return sum(u)  # ivy.array(sum(u))
 
 
 @to_ivy_arrays_and_back
 @from_zero_dim_arrays_to_scalar
 def hypergeometric(ngood, nbad, nsample, size=None):
     if size is None:
-        return hyper_helper(ngood, nbad, nsample)
+        return ivy.array(hyper_helper(ngood, nbad, nsample))
     u = ivy.empty(size, dtype=int)
     for index, s in ivy.ndenumerate(u):
         u[index] = hyper_helper(ngood, nbad, nsample)
