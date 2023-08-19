7238e077a031b165fadc0d4a2880cc4ee4b3ffb7
soumesh113 <58463935+soumesh113@users.noreply.github.com>
2023-08-19 16:26:47 +0530

    Update test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -1943,31 +1943,34 @@ def test_tensorflow_rsqrt(
 #segment_sum
 @handle_frontend_test(
     fn_tree="tensorflow.math.segment_sum",
-    data=helpers.array_values(dtype=ivy.int32, shape=(5, 6), min_value=1, max_value=9),
-    segment_ids=helpers.array_values(
-        dtype=ivy.int32, shape=(5,), min_value=0, max_value=4
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+        min_num_dims=1,
+        max_num_dims=3,
     ),
     test_with_out=st.just(False),
 )
 def test_tensorflow_segment_sum(
     *,
-    data,
-    segment_ids,
+    dtype_and_x
     frontend,
     test_flags,
     fn_tree,
     backend_fw,
     on_device,
 ):
+    input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
-        input_dtypes=["int32", "int64"],
+        input_dtypes=input_dtype,
         frontend=frontend,
         backend_to_test=backend_fw,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        data=data,
-        segment_ids=segment_ids,
+        data=x[0],
+        segment_ids=x[1],
     )
 
 # nextafter
