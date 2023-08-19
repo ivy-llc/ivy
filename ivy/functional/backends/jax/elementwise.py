e44646f44a76b60bf00ca9e515b053dabce4b35d
Mustafa Talha <103145399+mtilerisoy@users.noreply.github.com>
2023-08-19 19:48:25 +0300

    Update elementwise.py Jax

--- ivy/functional/backends/jax/elementwise.py
+++ ivy/functional/backends/jax/elementwise.py
@@ -462,9 +462,14 @@ def sinh(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
     return jnp.sinh(x)
 
 
-def sqrt(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
+def sqrt(
+    x: Union[float, JaxArray],
+    /,
+    *,
+    out: Optional[JaxArray] = None
+) -> JaxArray:
     return jnp.sqrt(x)
-
+    
 
 def square(x: JaxArray, /, *, out: Optional[JaxArray] = None) -> JaxArray:
     return jnp.square(x)
