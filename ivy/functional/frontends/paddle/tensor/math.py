0a336fc64b0a935bb0c68e0a82b39b018547ab9a
ArsalanAli915 <i191885@nu.edu.pk>
2023-08-20 22:36:17 +0500

    made the function sqrt function inplace

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -403,8 +403,7 @@ def rsqrt(x, name=None):
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def rsqrt_(x, name=None):
-    return ivy.reciprocal(ivy.sqrt(x))
-
+    return ivy.inplace_update(x, ivy.reciprocal(ivy.sqrt(x)))
 
 @with_supported_dtypes(
     {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle"
