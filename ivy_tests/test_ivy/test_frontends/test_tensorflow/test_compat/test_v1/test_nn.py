a2485be85e44b1fa45d5c5d985aad159f003b001
Bhavit Karnatak <106897686+Bhavit03@users.noreply.github.com>
2023-08-20 15:41:08 +0000

    lint

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_compat/test_v1/test_nn.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_compat/test_v1/test_nn.py
@@ -132,7 +132,7 @@ def test_tensorflow_separable_conv2d(
     backend_fw,
     on_device,
 ):
-    input_dtype, x, filters, dilation, data_format, stride, padding = x_f_d_df 
+    input_dtype, x, filters, dilation, data_format, stride, padding = x_f_d_df
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
@@ -188,7 +188,7 @@ def test_tensorflow_max_pool(
 @handle_frontend_test(
     fn_tree="tensorflow.compat.v1.nn.depthwise_conv2d_backprop_input",
     x_f_d_df=_x_and_filters(
-        input_dtypes=helpers.get_dtypes("float", "bfloat16", full=False),
+        input_dtypes=helpers.get_dtypes("float", full=False),
         dtypes=helpers.get_dtypes("integer", full=False),
         data_format=st.sampled_from(["NHWC", "NCHW"]),
         padding=st.sampled_from(["VALID", "SAME"]),
