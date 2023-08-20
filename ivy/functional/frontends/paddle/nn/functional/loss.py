177354845d2e8303c7c6e7e35389f2c4a1654269
Aditya Raj <109805994+zeus2x7@users.noreply.github.com>
2023-08-21 01:39:51 +0530

    Update loss.py

--- ivy/functional/frontends/paddle/nn/functional/loss.py
+++ ivy/functional/frontends/paddle/nn/functional/loss.py
@@ -316,6 +316,7 @@ def nll_loss(
         output = output - loss[ignore_index] / den
     return output
 
+
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def dice_loss(input, label, epsilon=0.00001, name=None):
@@ -326,15 +327,15 @@ def dice_loss(input, label, epsilon=0.00001, name=None):
     ivy.assertions.check_true(
         len(input.shape) == len(label.shape), 
         message=str(
-           "The rank of input and label should be equal, "
-           "but received input: %d, label: %d." % (len(input.shape), len(label.shape))
+            "The rank of input and label should be equal, "
+            "but received input: %d, label: %d." % (len(input.shape), len(label.shape))
        ),
     )
     ivy.assertions.check_true(
         label.shape[-1] == 1, 
         message=str(
-           "The last dimension of label should be 1, but received %d."
-           % label.shape[-1]
+            "The last dimension of label should be 1, but received %d."
+            % label.shape[-1]
        ),
     )
     ivy.assertions.check_true(
