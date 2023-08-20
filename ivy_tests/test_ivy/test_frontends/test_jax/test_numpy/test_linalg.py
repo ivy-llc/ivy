dc865f32b81911d7419a280fcb04f7422d705992
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-08-20 16:26:38 +0100

    fix multi_dot test implementation in jax front. match the parameters, create a sequence of arrays, remove manual value test, fix the fn_tree

--- ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_linalg.py
@@ -896,12 +896,14 @@ def test_jax_cond(
 
 # multi_dot
 @handle_frontend_test(
-    fn_tree="jax.lax.linalg.multi_dot",
+    fn_tree="jax.numpy.linalg.multi_dot",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
         min_value=0,
         max_value=10,
         shape=helpers.ints(min_value=2, max_value=5).map(lambda x: tuple([x, x])),
+        num_arrays=2,
+        shared_dtype=True,
     ).filter(
         lambda x: "float16" not in x[0]
         and "bfloat16" not in x[0]
@@ -910,34 +912,22 @@ def test_jax_cond(
     ),
     test_with_out=st.just(False),
 )
-def test_jax_lax_multi_dot(
+def test_jax_multi_dot(
     *,
     dtype_and_x,
     on_device,
     fn_tree,
     frontend,
     test_flags,
+    backend_fw
 ):
     dtype, x = dtype_and_x
-    x = np.asarray(x[0], dtype=dtype[0])
-
-    ret, frontend_ret = helpers.test_frontend_function(
+    helpers.test_frontend_function(
         input_dtypes=dtype,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        test_values=False,
-        args=(x,),
-    )
-
-    ret = ivy.to_numpy(ret)
-    frontend_ret = np.asarray(frontend_ret)
-
-    assert_all_close(
-        ret_np=ret,
-        ret_from_gt_np=frontend_ret,
-        rtol=1e-2,
-        atol=1e-2,
-        ground_truth_backend=frontend,
+        arrays=(x[0],x[1]),
+        backend_to_test=backend_fw,
     )
