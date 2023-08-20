0a5423c93a68e74dd652627b1bf0e0f11b4f5726
Olawale Onabola <walexi4great@gmail.com>
2023-08-19 21:37:40 -0700

    fix failing test

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -5,14 +5,14 @@ from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
 @with_unsupported_dtypes(
-    {"2.0.1 and below": ("float16", "bfloat16", "float32", "float64", "complex")},
+    {"2.0.1 and below": ("float16", "bfloat16")},
     "torch",
 )
 def hamming_window(
     window_length,
-    periodic=True,
     alpha=0.54,
     beta=0.46,
+    periodic=True,
     *,
     dtype=None,
     layout=None,
@@ -20,5 +20,5 @@ def hamming_window(
     requires_grad=False
 ):
     return ivy.hamming_window(
-        window_length, periodic=periodic, alpha=alpha, beta=beta, dtype=dtype
+        window_length, alpha=alpha, beta=beta, periodic=periodic, dtype=dtype
     )
--- ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_spectral_ops.py
@@ -15,7 +15,6 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
         min_value=1,
         max_value=20,
     ),
-    periodic=st.booleans(),
     dtype_and_coefficients=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float"),
         max_num_dims=0,
@@ -23,13 +22,14 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
         min_value=0,
         max_value=5,
     ),
+    periodic=st.booleans(),
     dtype=helpers.get_dtypes("float"),
     test_with_out=st.just(False),
 )
 def test_torch_hamming_window(
     dtype_and_window_length,
-    periodic,
     dtype_and_coefficients,
+    periodic,
     *,
     dtype,
     fn_tree,
@@ -39,15 +39,18 @@ def test_torch_hamming_window(
 ):
     window_length_dtype, window_length = dtype_and_window_length
     coefficients_dtypes, coefficients = dtype_and_coefficients
+
     helpers.test_frontend_function(
         input_dtypes=window_length_dtype + coefficients_dtypes,
-        test_flags=test_flags,
         window_length=int(window_length[0]),
-        periodic=periodic,
         alpha=float(coefficients[0]),
         beta=float(coefficients[1]),
+        periodic=periodic,
         dtype=dtype[0],
         fn_tree=fn_tree,
         frontend=frontend,
+        test_flags=test_flags,
         backend_to_test=backend_fw,
+        rtol=1e-1,
+        atol=1e-1,
     )
