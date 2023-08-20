f86a21d6b30a0cf58020fb76e6c222a4612b5d7d
Jay Choy <choyzhengjay@gmail.com>
2023-08-20 18:51:33 +0100

    Added msort

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1088,6 +1088,9 @@ class Tensor:
     def fmin(self, other):
         return torch_frontend.fmin(self, other)
 
+    def msort(self, out=None):
+        return torch_frontend.msort(self, out=out)
+
     @with_unsupported_dtypes(
         {"2.0.1 and below": ("float16", "bfloat16", "complex")}, "torch"
     )
