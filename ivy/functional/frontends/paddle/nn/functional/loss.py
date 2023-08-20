2da44515e069193601b1023263d14f03067cfad4
Aditya Raj <109805994+zeus2x7@users.noreply.github.com>
2023-08-21 01:30:24 +0530

    Update loss.py

--- ivy/functional/frontends/paddle/nn/functional/loss.py
+++ ivy/functional/frontends/paddle/nn/functional/loss.py
@@ -318,38 +318,38 @@ def nll_loss(
 
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
-def dice_loss(input,label,epsilon=0.00001,name=None):
+def dice_loss(input, label, epsilon=0.00001, name=None):
     ivy.assertions.check_true(
         len(input.shape) >= 2,
         message="The rank of input should be greater than or equal to 2.",
     )
     ivy.assertions.check_true(
         len(input.shape) == len(label.shape), 
-        message= str(
+        message=str(
            "The rank of input and label should be equal, "
            "but received input: %d, label: %d." % (len(input.shape), len(label.shape))
        ),
     )
     ivy.assertions.check_true(
         label.shape[-1] == 1, 
-        message= str(
+        message=str(
            "The last dimension of label should be 1, but received %d."
            % label.shape[-1]
        ),
     )
     ivy.assertions.check_true(
-         input.shape[:-1] == label.shape[:-1],
-         message="All dimensions should be equal except the last one.",
+        input.shape[:-1] == label.shape[:-1],
+        message="All dimensions should be equal except the last one.",
     )
     ivy.assertions.check_true(
         input.numel() > 0 and label.numel() > 0,
-        message= "Any dimension of input and label cannot be equal to 0.",
+        message="Any dimension of input and label cannot be equal to 0.",
     )
-    label = ivy.squeeze(label,axis=-1)
-    label=ivy.one_hot(label,input.shape[-1])
-    reduce_dim =list(range(1,len(input.shape)))
-    intersect = ivy.multiply(input,label)
-    inse = ivy.sum(intersect,axis=reduce_dim)
-    dice_denominator = ivy.sum(input,axis=reduce_dim) + ivy.sum(label,axis=reduce_dim)
-    dice_score =1-inse*2/(dice_denominator+epsilon)
+    label = ivy.squeeze(label, axis=-1)
+    label = ivy.one_hot(label, input.shape[-1])
+    reduce_dim = list(range(1, len(input.shape)))
+    intersect = ivy.multiply(input, label)
+    inse = ivy.sum(intersect, axis=reduce_dim)
+    dice_denominator = ivy.sum(input, axis=reduce_dim) + ivy.sum(label, axis=reduce_dim)
+    dice_score = 1 - inse * 2 / (dice_denominator + epsilon)
     return ivy.mean(dice_score)
