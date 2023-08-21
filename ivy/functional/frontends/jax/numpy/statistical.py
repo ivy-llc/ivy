1e58e32f5708fc1e779dfbb7e513c3fc07c6b09c
iAdiG <142774406+iAdiG@users.noreply.github.com>
2023-08-21 08:54:44 +0000

    percentile function and test

--- ivy/functional/frontends/jax/numpy/statistical.py
+++ ivy/functional/frontends/jax/numpy/statistical.py
@@ -514,3 +514,34 @@ def quantile(
     return ivy.quantile(
         a, q, axis=axis, keepdims=keepdims, interpolation=method, out=out
     )
+
+
+@to_ivy_arrays_and_back
+@with_unsupported_dtypes(
+    {"0.4.14 and below": ("complex64", "complex128", "bfloat16", "bool", "float16")},
+    "jax",
+)
+def percentile(
+    a,
+    q,
+    /,
+    *,
+    axis=None,
+    out=None,
+    overwrite_input=False,
+    method="linear",
+    keepdims=False,
+    interpolation=None,
+):
+    if method == "nearest":
+        return ivy.quantile(
+            a,
+            q / 100,
+            axis=axis,
+            keepdims=keepdims,
+            interpolation="nearest_jax",
+            out=out,
+        )
+    return ivy.quantile(
+        a, q / 100, axis=axis, keepdims=keepdims, interpolation=method, out=out
+    )
