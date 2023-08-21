c1c0f1fdb99d8fb81d86426192d973ae26468d14
conan-exploits <angayinoko@albany.edu>
2023-08-21 04:50:17 -0400

    Removing all the unrelated changesfrom this PR

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_norm.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_norm.py
@@ -45,39 +45,6 @@ def test_paddle_layer_norm(
     )
 
 
-# instance_norm
-@handle_frontend_test(
-    fn_tree="paddle.nn.functional.instance_norm",
-    values_tuple=_generate_data_layer_norm(
-        available_dtypes=helpers.get_dtypes("float"),
-    ),
-    eps=st.floats(min_value=0.01, max_value=0.1),
-)
-def test_paddle_instance_norm(
-    *,
-    values_tuple,
-    normalized_shape,
-    eps,
-    test_flags,
-    frontend,
-    on_device,
-    fn_tree,
-):
-    (dtype, x, normalized_shape, scale, offset) = values_tuple
-    helpers.test_frontend_function(
-        input_dtypes=dtype,
-        frontend=frontend,
-        test_flags=test_flags,
-        on_device=on_device,
-        fn_tree=fn_tree,
-        x=x[0],
-        normalized_shape=normalized_shape,
-        weight=scale[0],
-        bias=offset[0],
-        epsilon=eps,
-    )
-
-
 # bach_norm
 @handle_frontend_test(
     fn_tree="paddle.nn.functional.bach_norm",
