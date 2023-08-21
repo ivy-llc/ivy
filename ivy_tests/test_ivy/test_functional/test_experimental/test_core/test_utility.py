d511aecf6df2a8da2d792b9827c745418b4a3931
Kareem Morsy <kreem.morsy@hotmail.com>
2023-08-21 15:58:28 +0000

    Add support for lists in `optional_get_element`

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_utility.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_utility.py
@@ -1,3 +1,6 @@
+# global
+from hypothesis import strategies as st
+
 # local
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_test
@@ -6,16 +9,19 @@ from ivy_tests.test_ivy.helpers import handle_test
 @handle_test(
     fn_tree="functional.ivy.experimental.optional_get_element",
     dtype_and_x=helpers.dtype_and_values(),
+    input_tensor=st.booleans(),
 )
 def test_optional_get_element(
     *,
     dtype_and_x,
+    input_tensor,
     test_flags,
     backend_fw,
     fn_name,
     on_device,
 ):
     input_dtype, x = dtype_and_x
+    fn_input = x[0] if input_tensor else x
 
     helpers.test_function(
         input_dtypes=input_dtype,
@@ -23,5 +29,5 @@ def test_optional_get_element(
         on_device=on_device,
         backend_to_test=backend_fw,
         fn_name=fn_name,
-        x=x[0],
+        x=fn_input,
     )
