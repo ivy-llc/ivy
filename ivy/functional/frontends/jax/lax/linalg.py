6d161f42cdf5469a7b6a12f3bd9a7032d8029cc0
Riya Saxena <29riyasaxena@gmail.com>
2023-08-21 17:05:59 +0530

    fix black formatting

--- ivy/functional/frontends/jax/lax/linalg.py
+++ ivy/functional/frontends/jax/lax/linalg.py
@@ -39,6 +39,7 @@ def eigh(x, /, *, lower=True, symmetrize_input=True, sort_eigenvalues=True):
 
     return ivy.eigh(x, UPLO=UPLO)
 
+
 @to_ivy_arrays_and_back
 def lu(x):
     return ivy.lu(ivy.lu_factor(x))
