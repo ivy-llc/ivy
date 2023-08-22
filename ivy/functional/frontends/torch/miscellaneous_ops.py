9fd9ca59aa3d78616e94e7ee5e7cb9fd6e5cfcc6
Darshan Eshwarappa <darshanjhe@gmail.com>
2023-08-22 06:26:56 +0530

    Added torch bincount frontend function

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -509,3 +509,8 @@ def corrcoef(input):
 @to_ivy_arrays_and_back
 def kron(input, other, *, out=None):
     return ivy.kron(input, other, out=out)
+
+@with_supported_dtypes({"2.0.1 and above": ("int64",)}, "torch")
+@to_ivy_arrays_and_back
+def bincount(x, weights=None, minlength=0):
+    return ivy.bincount(x, weights=weights, minlength=minlength)
