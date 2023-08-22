3ed1e6cd587f6c8da69b7190c2cd853c4848fbb5
Joe Shepherd <sudo.jshepherd01@gmail.com>
2023-08-22 15:37:24 +0100

    fix(test): Apply safety factor to atan test

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -180,7 +180,10 @@ def test_asinh(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
 @handle_test(
     fn_tree="functional.ivy.atan",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float_and_complex")
+        available_dtypes=helpers.get_dtypes("float_and_complex"),
+        large_abs_safety_factor=2,
+        small_abs_safety_factor=2,
+        safety_factor_scale="log",
     ),
 )
 def test_atan(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
