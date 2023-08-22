a892bc61b79f51ead596482fd9080d50e7901c6c
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2023-08-22 13:05:56 +0530

    Some minor changes to `backends/torch/device.py`

--- ivy/functional/backends/torch/device.py
+++ ivy/functional/backends/torch/device.py
@@ -25,11 +25,9 @@ def dev(
     if as_native:
         if isinstance(dv, torch.device):
             dv = dv.type
-        if torch.cuda.is_available():
-            return torch.device(dv.replace("gpu", "cuda"))
-        elif hasattr(torch.backends, "mps"):
-            if torch.backends.mps.is_available():
-                return torch.device(dv.replace("gpu", "mps"))
+        elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+            return torch.device(dv.replace("gpu", "mps"))
+        return torch.device(dv.replace("gpu", "cuda"))
     return as_ivy_dev(dv)
 
 
@@ -55,18 +53,15 @@ def as_ivy_dev(device: torch.device, /):
     dev_type, dev_idx = (device.type, device.index)
     if dev_type == "cpu":
         return ivy.Device(dev_type)
-    elif dev_type == "cuda":
-        return ivy.Device(
-            dev_type.replace("cuda", "gpu")
-            + (":" + (str(dev_idx) if dev_idx is not None else "0"))
-        )
     elif dev_type == "mps":
         return ivy.Device(
             dev_type.replace("mps", "gpu")
             + (":" + (str(dev_idx) if dev_idx is not None else "0"))
         )
-    else:
-        raise Exception(f"Unknown device type {dev_type}")
+    return ivy.Device(
+        dev_type.replace("cuda", "gpu")
+        + (":" + (str(dev_idx) if dev_idx is not None else "0"))
+    )
 
 
 def as_native_dev(
@@ -75,10 +70,9 @@ def as_native_dev(
 ) -> Optional[torch.device]:
     if not isinstance(device, str):
         return device
-    if device == "cuda":
-        return torch.device(ivy.Device(device).replace("gpu", "cuda"))
-    elif device == "mps":
+    if device == "mps":
         return torch.device(ivy.Device(device).replace("gpu", "mps"))
+    return torch.device(ivy.Device(device).replace("gpu", "cuda"))
 
 
 def clear_cached_mem_on_dev(device: Union[ivy.Device, torch.device], /) -> None:
@@ -90,20 +84,15 @@ def clear_cached_mem_on_dev(device: Union[ivy.Device, torch.device], /) -> None:
 
 
 def num_gpus() -> int:
-    if torch.cuda.is_available():
-        return torch.cuda.device_count()
-    elif hasattr(torch.backends, "mps"):
-        if torch.backends.mps.is_available():
-            return 1
-    return 0
+    if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        return 1
+    return torch.cuda.device_count()
 
 
-def device_is_available() -> bool:
-    gpu_available = torch.cuda.is_available()
-    mps_available = False
+def gpu_is_available() -> bool:
     if hasattr(torch.backends, "mps"):
-        mps_available = torch.backends.mps.is_available()
-    return gpu_available or mps_available
+        return torch.backends.mps.is_available()
+    return torch.cuda.is_available()
 
 
 # noinspection PyUnresolvedReferences
