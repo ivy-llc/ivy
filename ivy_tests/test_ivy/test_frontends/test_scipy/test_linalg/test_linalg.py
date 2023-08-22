9ba973298c541abe9a45eda3f8a4524722c14b2e
umairjavaid <bscs13024@itu.edu.pk>
2023-08-22 21:55:25 +0000

    jax scipy functions shouldnt be called in the scipy tests. this causes test to fail

--- ivy_tests/test_ivy/test_frontends/test_scipy/test_linalg/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_scipy/test_linalg/test_linalg.py
@@ -153,71 +153,7 @@ def test_scipy_tril(
     )
 
 
-# triu
-@handle_frontend_test(
-    fn_tree="jax.scipy.triu",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
-        num_arrays=1,
-        min_num_dims=2,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=5,
-    ),
-    k=helpers.ints(min_value=-10, max_value=10),
-    test_with_out=st.just(False),
-)
-def test_scipy_triu(
-    dtype_and_x,
-    k,
-    test_flags,
-    frontend,
-    fn_tree,
-    on_device,
-    backend_fw,
-):
-    dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=dtype,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        m=x[0],
-        k=k,
-    )
 
-
-# inv
-@handle_frontend_test(
-    fn_tree="jax.scipy.inv",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        small_abs_safety_factor=2,
-        safety_factor_scale="log",
-        shape=helpers.ints(min_value=2, max_value=20).map(lambda x: tuple([x, x])),
-    ).filter(lambda x: np.linalg.cond(x[1][0].tolist()) < 1 / sys.float_info.epsilon),
-    test_with_out=st.just(False),
-)
-def test_scipy_inv(
-    dtype_and_x,
-    test_flags,
-    frontend,
-    fn_tree,
-    on_device,
-    backend_fw,
-):
-    dtype, x = dtype_and_x
-    helpers.test_frontend_function(
-        input_dtypes=dtype,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        a=x[0],
-    )
 
 
 # pinv
