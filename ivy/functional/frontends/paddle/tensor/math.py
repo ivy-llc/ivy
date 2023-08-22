fb05d88c1892ec53d9c3f8684eb360dca37b645a
Elvis888361 <elvisndegwa90@gmail.com>
2023-08-22 16:43:21 +0300

    second commit

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -64,6 +64,16 @@ def abs(x, name=None):
     return ivy.abs(x)
 
 
+def sigmoid(x, name=None):
+    return ivy.sigmoid(x)
+
+
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def sigmoid(x, name=None):
+    return ivy.sigmoid(x)
+
+
 @with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
 @to_ivy_arrays_and_back
 def sigmoid(x, name=None):
