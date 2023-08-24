e3682a85907ffe69c153ed7563c1f4f64873842f
Sheikhman_Kalderon <duah229@outlook.com>
2023-08-24 09:39:15 +0000

    test function for intersection in tensorflow frontend

---
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_intersection.py
@@ -0,0 +1,30 @@
+from ivy_tests.frontend_helpers import handle_frontend_test
+import ivy_tests.test_ivy.helpers as helpers
+import hypothesis.strategies as st
+
+@handle_frontend_test(
+    fn_tree="ivy.intersection",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float")
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_intersection(
+    *,
+    dtype_and_x,
+    frontend,
+    backend_fw,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
