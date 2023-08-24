fd4aaff158d98f77961191de21f4a5f680555acb
Sheikhman_Kalderon <duah229@outlook.com>
2023-08-24 10:04:48 +0000

    intersection function written in sets and test_intersection implemented with your correction

--- ivy/functional/frontends/tensorflow/sets.py
+++ ivy/functional/frontends/tensorflow/sets.py
@@ -1,17 +1,6 @@
-#global
 import ivy
 
-#intersection function
-def intersection(x, y):
-    
-    """ 
-    # Example usage
-    x = ivy.convert_to_constant([[1, 2, 3], [4, 5, 6]])
-    y = ivy.convert_to_constant([[2, 3, 4], [5, 6, 7]])
-    result = intersection(x, y)
-    print(result)
-
-    """
+def intersection(x, y, validate_indices=False):
     # Using Ivy's functional API to compute the intersection of sets
-    intersection_set = ivy.functional.frontends.tensorflow.sets.intersection(x, y, validate_indices=True)
+    intersection_set = ivy.sets.intersection(x, y, validate_indices=validate_indices)
     return intersection_set
