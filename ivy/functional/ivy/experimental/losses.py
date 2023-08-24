13a9a9bef5c33933c2ed0886721bf9782d89d57a
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2023-08-24 14:22:08 +0800

    fix: remove extra docstring explainations

--- ivy/functional/ivy/experimental/losses.py
+++ ivy/functional/ivy/experimental/losses.py
@@ -264,50 +264,6 @@ def smooth_l1_loss(
     {
         a: ivy.array(0.3467)
     }
-
-    Smooth L1 loss defined in the Fast R-CNN paper as:
-    ::
-                      | 0.5 * x ** 2 / beta   if abs(x) < beta
-        smoothl1(x) = |
-                      | abs(x) - 0.5 * beta   otherwise,
-
-    where x = input - target.
-
-    Smooth L1 loss is related to Huber loss, which is defined as:
-    ::
-                    | 0.5 * x ** 2                  if abs(x) < beta
-         huber(x) = |
-                    | beta * (abs(x) - 0.5 * beta)  otherwise
-
-    Smooth L1 loss is equal to huber(x) / beta. This leads to the following
-    differences:
-
-     - As beta -> 0, Smooth L1 loss converges to L1 loss, while Huber loss
-       converges to a constant 0 loss.
-     - As beta -> +inf, Smooth L1 converges to a constant 0 loss, while Huber loss
-       converges to L2 loss.
-     - For Smooth L1 loss, as beta varies, the L1 segment of the loss has a constant
-       slope of 1. For Huber loss, the slope of the L1 segment is beta.
-
-    Smooth L1 loss can be seen as exactly L1 loss, but with the abs(x) < beta
-    portion replaced with a quadratic function such that at abs(x) = beta, its
-    slope is 1. The quadratic segment smooths the L1 loss near x = 0.
-
-
-        input (Tensor): input tensor of any shape
-        target (Tensor): target value tensor with the same shape as input
-        beta (float): L1 to L2 change point.
-            For beta values < 1e-5, L1 loss is computed.
-        reduction: 'none' | 'mean' | 'sum'
-                 'none': No reduction will be applied to the output.
-                 'mean': The output will be averaged.
-                 'sum': The output will be summed.
-
-
-        The loss with the reduction option applied.
-        The tests will be failing for the paddle due to bfloat16 and these
-        tests are not only failing for smooth_l1_loss but also for other
-        implemented loss functions
     """
     if beta < 1e-5:
         # if beta == 0,  will result in nan gradients when
