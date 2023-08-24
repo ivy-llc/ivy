97e33aa234a719eb984d5b275195e32b88bb61f0
unknown <svetozar.milosevski@gmail.com>
2023-08-24 16:03:47 +0200

    ivy.sign issue

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -259,5 +259,5 @@ def triangular(left, mode, right, size=None):
 @from_zero_dim_arrays_to_scalar
 def laplace(loc=0.0, scale=1.0, size=None):
     u = ivy.random_uniform(low=0.0, high=0.0, shape=size, dtype="float64")
-    u = loc - scale * ivy.sign(u) * ivy.log(1 - 2 * ivy.abs(u))
+    u = loc - scale * ivy.log(1 - 2 * ivy.abs(u))
     return u
