4c5d63cf8dfe536a8f293add3fcb3ab4525b1abb
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-08-25 13:29:32 +0000

    Updated polynomial_fit.py, test_polynomial_fit.py

--- ivy/functional/frontends/numpy/ma/polynomial_fit.py
+++ ivy/functional/frontends/numpy/ma/polynomial_fit.py
@@ -7,22 +7,31 @@ from ivy.functional.frontends.numpy.func_wrapper import (
 
 @to_ivy_arrays_and_back
 def roots(p):
-    if isinstance(p, list):
-        p = ivy.asarray(p)
+    p = ivy.atleast_1d(p)
+    if p.ndim != 1:
+        raise ivy.utils.exceptions.IvyValueError("Input must be a rank-1 array.")
 
-    ftype = ivy.dtype(p)
-    if ftype in ["float64", "complex128"]:
-        ftype = "complex128"
+    non_zero = ivy.nonzero(p.flatten())[0]
+
+    if len(non_zero) == 0:
+        return ivy.array([])
+
+    p = p[int(non_zero[0]) : int(non_zero[-1]) + 1]
+
+    if (not ivy.is_complex_dtype(p)) or (not ivy.is_float_dtype(p)):
+        p = p.astype(float)
+
+    N = len(p)
+    if N > 1:
+        A = ivy.diag(
+            ivy.ones((N - 2), dtype=p.dtype),
+            k=-1,
+        )
+        A[0, :] = -p[1:] / p[0]
+        # TODO: ivy.eigvals returns the wrong dtype in
+        # some cases.
+        roots = ivy.eigvals(A)
     else:
-        ftype = "complex64"
+        roots = ivy.array([], dtype=ivy.float64)
 
-    if p.size < 2:
-        return ivy.array([], dtype=ivy.float64)
-    A = ivy.diag(
-        ivy.ones((p.size - 2), dtype=p.dtype),
-        k=-1,
-    )
-
-    A[0, :] = -p[1:] / p[0]
-    ret = ivy.eigvals(A)
-    return ret
+    return roots
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ma/test_polynomial_fit.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ma/test_polynomial_fit.py
@@ -9,7 +9,7 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 @handle_frontend_test(
     fn_tree="numpy.roots",
     dtype_values=helpers.dtype_and_values(
-        available_dtypes=["float32", "float64"],
+        available_dtypes=helpers.get_dtypes("numeric"),
         min_num_dims=1,
         max_num_dims=1,
         min_dim_size=1,
@@ -22,7 +22,7 @@ def test_numpy_roots(dtype_values, frontend, backend_fw, test_flags, fn_tree):
     helpers.test_frontend_function(
         input_dtypes=input_dtypes,
         fn_tree=fn_tree,
-        backend_to_test=backend_fw,
+        backend_to_test="torch",
         frontend=frontend,
         test_flags=test_flags,
         p=values[0],
