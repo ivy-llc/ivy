c77423b192edc43fa4a8aaf344fc2dd22df41be3
Waqar Ahmed <ahmedwaqar309@gmail.com>
2023-08-25 10:41:51 +0500

    Update signal.py<kaiser_bessel_derived_window>
    Added kaiser_bessel_derived_window function to tensorflow.frontend.signal.py

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -34,3 +34,12 @@ vorbis_window.supported_dtypes = ("float32", "float64", "float16", "bfloat16")
 def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
     inverse_type = {1: 1, 2: 3, 3: 2, 4: 4}[type]
     return ivy.dct(input, type=inverse_type, n=n, axis=axis, norm=norm)
+
+#kaiser_bessel_derived_window
+@handle_tf_dtype
+@to_ivy_arrays_and_back
+def kaiser_bessel_derived_window(window_length, beta=12.0, dtype=ivy.float32, name=None):
+    return ivy.kaiser_bessel_derived_window(window_length, beta=beta, dtype=dtype)
+
+
+kaiser_bessel_derived_window.supported_dtypes = ("float32", "float64", "float16", "bfloat16")
