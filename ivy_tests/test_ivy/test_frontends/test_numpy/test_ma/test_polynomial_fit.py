e109b0688165b003a05a1001633618d31df070cd
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-08-25 14:31:06 +0000

    Updated test_polynomial_fit.py

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_ma/test_polynomial_fit.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_ma/test_polynomial_fit.py
@@ -9,7 +9,12 @@ from ivy_tests.test_ivy.helpers import handle_frontend_test
 @handle_frontend_test(
     fn_tree="numpy.roots",
     dtype_values=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("numeric"),
+        available_dtypes=[
+            "float32",
+            "float64",
+            "int32",
+            "int64",
+        ],
         min_num_dims=1,
         max_num_dims=1,
         min_dim_size=1,
@@ -22,7 +27,7 @@ def test_numpy_roots(dtype_values, frontend, backend_fw, test_flags, fn_tree):
     helpers.test_frontend_function(
         input_dtypes=input_dtypes,
         fn_tree=fn_tree,
-        backend_to_test="torch",
+        backend_to_test=backend_fw,
         frontend=frontend,
         test_flags=test_flags,
         p=values[0],
