eb634eb90d22f7ee9189a239955610d28af0652b
notabombe <semigroove_newsmanmen@slmail.me>
2023-08-26 14:08:49 +0100

    Added moveaxis for Paddle frontend
    closes #19157

--- ivy/functional/frontends/paddle/tensor/manipulation.py
+++ ivy/functional/frontends/paddle/tensor/manipulation.py
@@ -95,6 +95,63 @@ def reshape_(x, shape):
 
 @with_supported_dtypes(
     {
+        "2.5.1 and below": (
+            "float32",
+            "float64",
+            "int32",
+            "int64",
+        )
+    },
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def moveaxis(
+    x: paddle.Tensor,
+    source: Union[int, List[int]],
+    destination: Union[int, List[int]],
+    /,
+    *,
+    out: Optional[paddle.Tensor] = None,
+) -> paddle.Tensor:
+    """
+    Move axes of an array to new positions.
+    Other axes remain in their original order.
+    Parameters
+    ----------
+    x : paddle.Tensor
+        The input tensor.
+    source : int
+        The original position of the axes to move. These must be unique.
+    destination : int
+        The destination position for each of the original axes. These must also be unique.
+    Returns
+    -------
+    result : paddle.Tensor
+        Array with moved axes. This array is a view of the input array.
+    """
+   if isinstance(source, list) and isinstance(destination, list):
+        if len(source) != len(destination):
+            raise ValueError("Source and destination lists must have the same length")
+        for src, dest in zip(source, destination):
+            x = moveaxis(x, src, dest)
+        return x
+     else:
+        if source == destination:
+            return x
+        if source < 0:
+            source = x.ndim + source
+        if destination < 0:
+            destination = x.ndim + destination
+        if source < 0 or source >= x.ndim or destination < 0 or destination >= x.ndim:
+            raise ValueError("Source or destination axis out of range")
+        order = list(range(x.ndim))
+        order.pop(source)
+        order.insert(destination, source)
+        return paddle.transpose(x, order)
+    
+
+@with_supported_dtypes(
+    {
         "2.5.0 and below": (
             "float32",
             "float64",
