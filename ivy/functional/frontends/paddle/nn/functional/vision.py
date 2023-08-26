bffadfceaece97426f388f325bd9508161956aae
JONAHKYAGABA <kyagabajonah@gmail.com>
2023-08-26 23:10:46 +0300

    all changes

--- ivy/functional/frontends/paddle/nn/functional/vision.py
+++ ivy/functional/frontends/paddle/nn/functional/vision.py
@@ -170,3 +170,105 @@ def pixel_shuffle(x, upscale_factor, data_format="NCHW"):
     return ivy.reshape(
         ivy.permute_dims(input_reshaped, (0, 1, 4, 2, 5, 3)), (b, oh, ow, oc)
     )
+@to_ivy_arrays_and_back
+def grid_sample(x, grid, mode='bilinear', padding_mode='zeros', align_corners=True):
+
+    _modes = ['bilinear', 'nearest']
+    _padding_modes = ['zeros', 'reflection', 'border']
+    if mode not in _modes:
+        raise ValueError(
+            "The mode of grid sample function should be in {}, but got: {}".format(
+                _modes, mode
+            )
+        )
+    if padding_mode not in _padding_modes:
+        raise ValueError(
+            "The padding mode of grid sample function should be in {}, but got: {}".format(
+                _padding_modes, padding_mode
+            )
+        )
+
+    if not isinstance(align_corners, bool):
+        raise ValueError(
+            "The align corners should be bool, but got: {}".format(
+                align_corners
+            )
+        )
+    N, C, H, W = x.shape
+    if len(grid.shape) == 4:
+        _, grid_H, grid_W, _ = grid.shape
+    elif len(grid.shape) == 5:
+        _, _, grid_D, grid_H, grid_W = grid.shape
+    else:
+        raise ValueError("Invalid grid shape")
+
+    grid_x = grid[:, :, :, 0]
+    grid_y = grid[:, :, :, 1]
+
+    if align_corners:
+        grid_x = 0.5 * (grid_x + 1) * (W - 1)
+        grid_y = 0.5 * (grid_y + 1) * (H - 1)
+    else:
+        grid_x = 0.5 * grid_x * (W - 1)
+        grid_y = 0.5 * grid_y * (H - 1)
+
+    y_t = ivy.zeros((N, C, grid_H, grid_W), dtype=x.dtype)
+
+    for n in range(N):
+        for c in range(C):
+            for i in range(grid_H):
+                for j in range(grid_W):
+                    x_float = grid_x[n, i, j]
+                    y_float = grid_y[n, i, j]
+
+                    x_w = int(ivy.floor(x_float))
+                    x_e = x_w + 1
+                    y_n = int(ivy.floor(y_float))
+                    y_s = y_n + 1
+
+                    d_w = x_float - x_w
+                    d_e = x_e - x_float
+                    d_n = y_float - y_n
+                    d_s = y_s - y_float
+
+                    if mode == 'bilinear':
+                        wn = x[n, c, y_n, x_w]
+                        en = x[n, c, y_n, x_e]
+                        ws = x[n, c, y_s, x_w]
+                        es = x[n, c, y_s, x_e]
+
+                        if padding_mode == 'border':
+                            wn = x[n, c, max(y_n, 0), max(x_w, 0)]
+                            en = x[n, c, max(y_n, 0), min(x_e, W - 1)]
+                            ws = x[n, c, min(y_s, H - 1), max(x_w, 0)]
+                            es = x[n, c, min(y_s, H - 1), min(x_e, W - 1)]
+                        elif padding_mode == 'reflection':
+                            wn = x[n, c, H - 1 - abs(y_n), W - 1 - abs(x_w)]
+                            en = x[n, c, H - 1 - abs(y_n), W - 1 - abs(x_e)]
+                            ws = x[n, c, H - 1 - abs(y_s), W - 1 - abs(x_w)]
+                            es = x[n, c, H - 1 - abs(y_s), W - 1 - abs(x_e)]
+                        elif padding_mode == 'zeros':
+                            wn = 0 if y_n < 0 or x_w < 0 else x[n, c, y_n, x_w]
+                            en = 0 if y_n < 0 or x_e >= W else x[n, c, y_n, x_e]
+                            ws = 0 if y_s >= H or x_w < 0 else x[n, c, y_s, x_w]
+                            es = 0 if y_s >= H or x_e >= W else x[n, c, y_s, x_e]
+
+                        value = wn * d_e * d_s + en * d_w * d_s + ws * d_e * d_n + es * d_w * d_n
+                    elif mode == 'nearest':
+                        x_int = int(ivy.round(x_float))
+                        y_int = int(ivy.round(y_float))
+
+                        if padding_mode == 'border':
+                            x_int = ivy.clip(x_int, 0, W - 1)
+                            y_int = ivy.clip(y_int, 0, H - 1)
+                        elif padding_mode == 'reflection':
+                            x_int = abs(x_int % (2 * W - 2))
+                            y_int = abs(y_int % (2 * H - 2))
+                        elif padding_mode == 'zeros':
+                            x_int = max(0, min(x_int, W - 1))
+                            y_int = max(0, min(y_int, H - 1))
+
+                        value = x[n, c, y_int, x_int]
+
+                    y_t[n, c, i, j] = value
+    return y_t
