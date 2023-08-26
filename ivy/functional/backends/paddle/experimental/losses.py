5b1ea32aa1d17f2d91874dae60d1e2eb8d7b322f
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-27 00:53:54 +0530

    reviwed changes

--- ivy/functional/backends/paddle/experimental/losses.py
+++ ivy/functional/backends/paddle/experimental/losses.py
@@ -70,7 +70,7 @@ def smooth_l1_loss(
     {
         "2.5.1 and below": {
             "cpu": (
-                "bfloat16float16",
+                "bfloat16",
                 "int8",
                 "int16",
                 "int32",
