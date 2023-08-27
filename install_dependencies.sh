3d1e53851c121b7a6354cd78bea7f9256b603ebc
Nripesh Niketan <86844847+NripeshN@users.noreply.github.com>
2023-08-27 09:14:14 +0530

    Fix(install): Fix apple silicon install (#22635)

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 pip install -r requirements/requirements.txt
 if [[ $(arch) == 'arm64' ]]; then
-      pip install -r requirements/optional_m1_1.txt
-      pip install -r requirements/optional_m1_2.txt
+      pip install -r requirements/optional_apple_silicon_1.txt
+      pip install -r requirements/optional_apple_silicon_2.txt
 else
     pip install -r requirements/optional.txt
 fi
