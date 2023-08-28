13ea70921ac1c9c771006046c8d66260bf489a2f
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-28 19:53:04 +0530

    added the numpy changes

--- ivy/functional/backends/numpy/experimental/losses.py
+++ ivy/functional/backends/numpy/experimental/losses.py
@@ -6,7 +6,7 @@ from ivy.func_wrapper import with_unsupported_dtypes
 from . import backend_version
 
 
-# Implementation of smooth_l1_loss in the given format
+
 @with_unsupported_dtypes({"1.25.2 and below": ("bool",)}, backend_version)
 @_scalar_output_to_0d_array
 def huber_loss(
@@ -17,10 +17,10 @@ def huber_loss(
     delta: Optional[float] = 1.0,
     reduction: Optional[str] = "mean",
 ) -> np.ndarray:
-    abs_diff = ivy.abs(input - target)
+    abs_diff = np.abs(input - target)
     quadratic_loss = 0.5 * (abs_diff**2)
     linear_loss = delta * (abs_diff - 0.5 * delta)
-    loss = ivy.where(abs_diff <= delta, quadratic_loss, linear_loss)
+    loss = np.where(abs_diff <= delta, quadratic_loss, linear_loss)
 
     if reduction == "sum":
         return ivy.sum(loss)
