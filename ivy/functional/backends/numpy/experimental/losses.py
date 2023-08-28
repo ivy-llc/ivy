5071dc9e5f5ea2056424ab8a45f7cff3ef1b1160
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-08-28 20:10:36 +0530

    removed ivy import

--- ivy/functional/backends/numpy/experimental/losses.py
+++ ivy/functional/backends/numpy/experimental/losses.py
@@ -1,6 +1,5 @@
 import numpy as np
 from typing import Optional
-import ivy
 from ivy.functional.backends.numpy.helpers import _scalar_output_to_0d_array
 from ivy.func_wrapper import with_unsupported_dtypes
 from . import backend_version
