40864fe9cd29db0de71b8b973e4b9c75528cb91a
Bhushan Srivastava <59949692+he11owthere@users.noreply.github.com>
2023-08-29 23:59:09 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -927,7 +927,7 @@ def test_paddle_tensor_ceil(
     init_tree="paddle.to_tensor",
     method_name="ceil_",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
+        available_dtypes=helpers.get_dtypes("valid"),
     ),
 )
 def test_paddle_tensor_ceil_(
