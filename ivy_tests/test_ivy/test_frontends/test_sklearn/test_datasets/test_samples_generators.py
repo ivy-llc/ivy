b14700b9d37a8ba0234505a20b96253932f2741a
Anubhav Goyal <91376215+Anubhav-Goyal01@users.noreply.github.com>
2023-08-31 02:34:47 +0530

    feat(frontend): Implemented make_moons dataset generating function along with test (#22800)
    for sklearn frontend

--- ivy_tests/test_ivy/test_frontends/test_sklearn/test_datasets/test_samples_generators.py
+++ ivy_tests/test_ivy/test_frontends/test_sklearn/test_datasets/test_samples_generators.py
@@ -24,3 +24,27 @@ def test_sklearn_make_circles(
         on_device=on_device,
         test_values=False,
     )
+
+
+@handle_frontend_test(
+    fn_tree="sklearn.datasets.make_moons",
+    n_samples=helpers.ints(min_value=1, max_value=5),
+)
+def test_sklearn_make_moons(
+    n_samples,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    helpers.test_frontend_function(
+        n_samples=n_samples,
+        input_dtypes=["int32"],
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        frontend=frontend,
+        on_device=on_device,
+        test_values=False,
+    )
