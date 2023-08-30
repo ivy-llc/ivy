be72b704f2ce9adb3d5a84fc37108d64ea359e0c
Aryan Sharma <aryansharma@Aryans-MacBook-Air.local>
2023-08-30 15:25:47 +0530

    nansum added to paddle.tensor.math

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -518,3 +518,9 @@ def tanh(x, name=None):
 @to_ivy_arrays_and_back
 def trunc(x, name=None):
     return ivy.trunc(x)
+
+
+@with_supported_dtypes({"2.4.2 and below": ("float32", "float64", "int32", "int64")}, "paddle")
+@to_ivy_arrays_and_back
+def nansum(x, dim=None, dtype=None, name=None):
+    return ivy.nansum(x, axis=dim, dtype=dtype)
