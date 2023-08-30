7da967b8be826ca374f73273573597908eab1410
samunder singh <83540902+samthakur587@users.noreply.github.com>
2023-08-31 00:05:18 +0530

    added the remainer to paddle tensor (#22625)
    Co-authored-by: paulaehab<eng.paulaehab@gmail.com>

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -660,3 +660,7 @@ class Tensor:
     )
     def trunc(self, name=None):
         return paddle_frontend.Tensor(ivy.trunc(self._ivy_array))
+
+    @with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
+    def remainder(self, y, name=None):
+        return ivy.remainder(self._ivy_array, y)
