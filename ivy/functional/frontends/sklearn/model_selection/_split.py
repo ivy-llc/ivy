82707d0bf91dc06fb770228dc991c1033fc31a5b
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-08-30 22:43:33 +0100

    feat(frontend): added an implementation for the essential test indices generator for KFOld class in sklearn front. The method is fundamental for KFold's split method and its validity should be tested through tests for split

--- ivy/functional/frontends/sklearn/model_selection/_split.py
+++ ivy/functional/frontends/sklearn/model_selection/_split.py
@@ -37,7 +37,19 @@ class KFold(BaseCrossValidator):
         raise NotImplementedError
 
     def _iter_test_indices(self, X=None, y=None, groups=None):
-        raise NotImplementedError
+        n_samples = X.shape[0]
+        indices = ivy.arange(n_samples)
+        if self.shuffle:
+            indices = ivy.shuffle(indices, seed=self.random_state)
+
+        n_splits = self.n_splits
+        fold_sizes = ivy.full(n_splits, n_samples // n_splits, dtype=ivy.default_int_dtype())
+        fold_sizes[: n_samples % n_splits] += 1
+        current = 0
+        for fold_size in fold_sizes:
+            start, stop = current, current + fold_size
+            yield indices[start:stop]
+            current = stop
 
 
 @to_ivy_arrays_and_back
