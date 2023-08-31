bbcd6646a874eb7817ed07e67b3d56ac4eea3e1f
JG <67888047+juliagsy@users.noreply.github.com>
2023-08-31 15:12:58 +0100

    Update mathematical_functions.py

--- ivy/functional/frontends/jax/numpy/mathematical_functions.py
+++ ivy/functional/frontends/jax/numpy/mathematical_functions.py
@@ -192,7 +192,8 @@ def arcsinh(x, /):
 
 
 @to_ivy_arrays_and_back
-def gradient(f, *varargs, axis=None, edge_order=1):
+def gradient(f, *varargs, axis=None, edge_order=None):
+    edge_order = edge_order if edge_order is not None else 1
     return ivy.gradient(f, spacing=varargs, axis=axis, edge_order=edge_order)
 
 
