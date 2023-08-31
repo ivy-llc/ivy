fb7f334c0eb9f2f8568192bd28d632dd2022214e
RashulChutani <rashul.chutani@gmail.com>
2023-08-31 20:35:49 +0530

    Resolve issues in synchronize_db.py script [skip ci]

--- run_tests_CLI/synchronize_db.py
+++ run_tests_CLI/synchronize_db.py
@@ -20,7 +20,9 @@ module_map = {
 
 
 def keys_to_delete_from_db(all_tests, module, data, current_key=""):
-    """Recursively navigate and identify keys not in the list."""
+    """
+    Recursively navigate and identify keys not in the list.
+    """
     keys_for_deletion = []
 
     for key, value in data.items():
@@ -56,7 +58,7 @@ submodules = (
     "test_mindspore",
     "test_onnx",
     "test_sklearn",
-    "test_xgboost",
+    "test_xgboost"
 )
 db_dict = {
     "test_functional/test_core": ["core", 10],
@@ -75,10 +77,8 @@ db_dict = {
     "test_mindspore": ["mindspore", 23],
     "test_onnx": ["onnx", 24],
     "test_sklearn": ["sklearn", 25],
-    "test_xgboost": ["xgboost", 26],
+    "test_xgboost": ["xgboost", 26]
 }
-
-
 def get_submodule(test_path):
     test_path = test_path.split("/")
     for name in submodules:
@@ -113,9 +113,8 @@ def main():
     for collection_name in db.list_collection_names():
         collection = db[collection_name]
         for document in collection.find({}):
-            undesired_keys = keys_to_delete_from_db(
-                all_tests, module_map[collection_name], document
-            )
+            print(document)
+            undesired_keys = keys_to_delete_from_db(all_tests, module_map[collection_name], document)
             for key in undesired_keys:
                 print(key)
                 # collection.update_one({"_id": document["_id"]}, {"$unset": {key: 1}})
