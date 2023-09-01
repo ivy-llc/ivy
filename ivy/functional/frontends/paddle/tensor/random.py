1daadd3710ae6fdf7301d82600bf88977c2731cd
Aditya Raj <109805994+zeus2x7@users.noreply.github.com>
2023-09-02 01:14:16 +0530

    Update random.py

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -6,6 +6,7 @@ from ivy.functional.frontends.paddle.func_wrapper import (
     to_ivy_arrays_and_back,
 )
 
+
 @with_supported_dtypes(
     {"2.5.1 and below": ("float32", "float64")},
     "paddle",
