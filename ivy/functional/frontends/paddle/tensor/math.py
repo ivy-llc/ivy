1664510ff2c05b2ade990b5de71b258c620725f1
Nitesh Kesharwani <141853536+NiteshK84@users.noreply.github.com>
2023-09-01 04:48:21 +0000

    added func and test

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -15,6 +15,74 @@ def ceil_(x, name=None):
     return ivy.ceil(x, out=x)
 
 
+@with_supported_dtypes(
+    {"2.4.2 and below": ("float32", "float64", "int32", "int64")}, "paddle"
+)
+@to_ivy_arrays_and_back
+def clip(x, min=None, max=None, name=None):
+    if min is None:
+        min = ivy.min(x)
+    if max is None:
+        max = ivy.max(x)
+    return ivy.clip(x, min, max)
+
+
+@with_unsupported_dtypes({"2.4.2 and below": ("int16", "float16")}, "paddle")
+@to_ivy_arrays_and_back
+def conj(x, name=None):
+    return ivy.conj(x)
+
+
+@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def cos(x, name=None):
+    return ivy.cos(x)
+
+
+@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def cosh(x, name=None):
+    return ivy.cosh(x)
+
+
+@with_supported_dtypes(
+    {
+        "2.5.1 and below": (
+            "int32",
+            "int64",
+            "float32",
+            "float64",
+            "complex64",
+            "complex128",
+        )
+    },
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def cumprod(x, dim=None, dtype=None, name=None):
+    return ivy.cumprod(x, axis=dim, dtype=dtype)
+
+
+@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def deg2rad(x, name=None):
+    return ivy.deg2rad(x)
+
+
+@with_supported_dtypes(
+    {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle"
+)
+@to_ivy_arrays_and_back
+def diff(x, n=1, axis=-1, prepend=None, append=None, name=None):
+    return ivy.diff(x, n=n, axis=axis, prepend=prepend, append=append)
+
+
+@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
+@to_ivy_arrays_and_back
+def divide(x, y, name=None):
+    return ivy.divide(x, y)
+
+
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def exp_(x, name=None):
