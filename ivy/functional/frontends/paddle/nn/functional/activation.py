c4b6e9507c6397da56570e2e8e74d5427b0de7f9
arshPratap <arshpratapofficial@gmail.com>
2023-09-01 13:07:03 +0530

    Added glu activation to Paddle frontend

--- ivy/functional/frontends/paddle/nn/functional/activation.py
+++ ivy/functional/frontends/paddle/nn/functional/activation.py
@@ -40,6 +40,17 @@ def gelu(x, approximate=False, name=None):
     return ivy.gelu(x, approximate=approximate)
 
 
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+@to_ivy_arrays_and_back
+def glu(x, axis=-1, name=None):
+    size = x.shape[axis]
+    ivy.utils.assertions.check_equal(
+        size % 2, 0, message="axis size must be divisible by 2", as_array=False
+    )
+    a, b = ivy.split(x, num_or_size_splits=2, axis=axis)
+    return ivy.multiply(a, ivy.sigmoid(b))
+
+
 @with_supported_dtypes({"2.4.2 and below": ("float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def gumbel_softmax(x, temperature=1.0, hard=False, axis=-1, name=None):
