c18354bd2fd3ed4f83c29bff0407b45f2cb081f0
ArsalanAli915 <i191885@nu.edu.pk>
2023-09-02 12:58:05 +0500

    fixed checks

--- ivy/functional/frontends/paddle/tensor/math.py
+++ ivy/functional/frontends/paddle/tensor/math.py
@@ -184,12 +184,6 @@ def exp(x, name=None):
     return ivy.exp(x)
 
 
-@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
-@to_ivy_arrays_and_back
-def subtract_(x, y, name=None):
-    return ivy.inplace_update(x, subtract(x, y))
-
-
 @with_supported_dtypes({"2.5.1 and below": ("float16", "float32", "float64")}, "paddle")
 @to_ivy_arrays_and_back
 def expm1(x, name=None):
