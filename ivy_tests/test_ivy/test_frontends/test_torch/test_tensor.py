5939f6dcebd8e25d9e395298716fabf6f0506abb
Kai Wu <46016066+wushidiguo@users.noreply.github.com>
2023-09-03 07:46:30 +0800

    add frontend method torch.tensor.negative (#21822)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -6729,6 +6729,44 @@ def test_torch_tensor_flip(
     )
 
 
+# negative
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="negative",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_value=-1e04,
+        max_value=1e04,
+        allow_inf=False,
+    ),
+)
+def test_torch_tensor_negative(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    on_device,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
 # fliplr
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -12539,7 +12577,7 @@ def test_torch_tensor_vsplit(
         on_device=on_device,
     )
 
-
+ 
 # where
 @handle_frontend_method(
     class_tree=CLASS_TREE,
