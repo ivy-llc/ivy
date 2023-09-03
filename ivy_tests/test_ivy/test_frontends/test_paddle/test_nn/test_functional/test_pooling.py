026efbf065b88a0b69779c03ce58dbacb1e19e0e
KaylaMLe <kaylale2014@gmail.com>
2023-09-03 11:05:30 -0700

    lint fix

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
@@ -9,171 +9,6 @@ from ivy_tests.test_ivy.test_frontends.test_torch.test_nn.test_functional import
 )
 
 
-# avg_pool3d
-@handle_frontend_test(
-    fn_tree="paddle.nn.functional.pooling.avg_pool3d",
-    df_x_k_s_p=helpers.arrays_for_pooling(
-        min_dims=4,
-        max_dims=4,
-        min_side=2,
-        max_side=4,
-    ),
-    exclusive=st.booleans(),
-    ceil_mode=st.booleans(),
-    data_format=st.sampled_from(["NDHWC", "NCDHW"]),
-)
-def test_paddle_avg_pool3d(
-    df_x_k_s_p,
-    exclusive,
-    ceil_mode,
-    data_format,
-    *,
-    test_flags,
-    backend_fw,
-    frontend,
-    fn_tree,
-    on_device,
-):
-    input_df, x, kernel, stride, padding = df_x_k_s_p
-
-    if data_format == "NCDHW":
-        x[0] = x[0].reshape(
-            (x[0].shape[0], x[0].shape[4], x[0].shape[1], x[0].shape[2], x[0].shape[3])
-        )
-
-    if len(stride) == 1:
-        stride = (stride[0], stride[0], stride[0])
-
-    if padding == "SAME":
-        padding = test_pooling_functions.calculate_same_padding(
-            kernel, stride, x[0].shape[2:]
-        )
-    else:
-        padding = (0, 0, 0)
-
-    helpers.test_frontend_function(
-        input_dtypes=input_df,
-        test_flags=test_flags,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x=x[0],
-        kernel_size=kernel,
-        stride=stride,
-        padding=padding,
-        ceil_mode=ceil_mode,
-        exclusive=exclusive,
-        divisor_override=None,
-        data_format=data_format,
-    )
-
-
-# avg_pool2d
-@handle_frontend_test(
-    fn_tree="paddle.nn.functional.pooling.avg_pool2d",
-    dtype_x_k_s=helpers.arrays_for_pooling(
-        min_dims=4,
-        max_dims=4,
-        min_side=2,
-        max_side=4,
-    ),
-    ceil_mode=st.booleans(),
-    exclusive=st.booleans(),
-    data_format=st.sampled_from(["NCHW", "NHWC"]),
-)
-def test_paddle_avg_pool2d(
-    dtype_x_k_s,
-    exclusive,
-    ceil_mode,
-    data_format,
-    *,
-    test_flags,
-    backend_fw,
-    frontend,
-    fn_tree,
-    on_device,
-):
-    input_dtype, x, kernel, stride, padding = dtype_x_k_s
-
-    if data_format == "NCHW":
-        x[0] = x[0].reshape(
-            (x[0].shape[0], x[0].shape[3], x[0].shape[1], x[0].shape[2])
-        )
-    if len(stride) == 1:
-        stride = (stride[0], stride[0])
-    if padding == "SAME":
-        padding = test_pooling_functions.calculate_same_padding(
-            kernel, stride, x[0].shape[2:]
-        )
-    else:
-        padding = (0, 0)
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        test_flags=test_flags,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        x=x[0],
-        kernel_size=kernel,
-        stride=stride,
-        padding=padding,
-        ceil_mode=ceil_mode,
-        exclusive=exclusive,
-        divisor_override=None,
-        data_format=data_format,
-    )
-
-
-# avg_pool1d
-@handle_frontend_test(
-    fn_tree="paddle.nn.functional.avg_pool1d",
-    x_k_s_p_df=helpers.arrays_for_pooling(
-        min_dims=3,
-        max_dims=3,
-        min_side=2,
-        max_side=4,
-        data_format="channel_first",
-    ),
-    exclusive=st.booleans(),
-    ceil_mode=st.just(False),
-    test_with_out=st.just(False),
-)
-def test_paddle_avg_pool1d(
-    *,
-    x_k_s_p_df,
-    frontend,
-    test_flags,
-    backend_fw,
-    on_device,
-    fn_tree,
-    exclusive,
-    ceil_mode,
-):
-    input_dtype, x, kernel_size, stride, padding = x_k_s_p_df
-    if padding == "SAME":
-        padding = test_pooling_functions.calculate_same_padding(
-            kernel_size, stride, [x[0].shape[2]]
-        )
-    else:
-        padding = (0,)
-    helpers.test_frontend_function(
-        input_dtypes=input_dtype,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        test_flags=test_flags,
-        on_device=on_device,
-        fn_tree=fn_tree,
-        x=x[0],
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=padding,
-        exclusive=exclusive,
-        ceil_mode=ceil_mode,
-    )
-
-    
 # adaptive_avg_pool1d
 @handle_frontend_test(
     fn_tree="paddle.nn.functional.adaptive_avg_pool1d",
@@ -425,6 +260,66 @@ def test_paddle_avg_pool2d(
         padding = (0, 0)
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
+        test_flags=test_flags,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        kernel_size=kernel,
+        stride=stride,
+        padding=padding,
+        ceil_mode=ceil_mode,
+        exclusive=exclusive,
+        divisor_override=None,
+        data_format=data_format,
+    )
+
+
+# avg_pool3d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.pooling.avg_pool3d",
+    df_x_k_s_p=helpers.arrays_for_pooling(
+        min_dims=4,
+        max_dims=4,
+        min_side=2,
+        max_side=4,
+    ),
+    exclusive=st.booleans(),
+    ceil_mode=st.booleans(),
+    data_format=st.sampled_from(["NDHWC", "NCDHW"]),
+)
+def test_paddle_avg_pool3d(
+    df_x_k_s_p,
+    exclusive,
+    ceil_mode,
+    data_format,
+    *,
+    test_flags,
+    backend_fw,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_df, x, kernel, stride, padding = df_x_k_s_p
+
+    if data_format == "NCDHW":
+        x[0] = x[0].reshape(
+            (x[0].shape[0], x[0].shape[4], x[0].shape[1], x[0].shape[2], x[0].shape[3])
+        )
+
+    if len(stride) == 1:
+        stride = (stride[0], stride[0], stride[0])
+
+    if padding == "SAME":
+        padding = test_pooling_functions.calculate_same_padding(
+            kernel, stride, x[0].shape[2:]
+        )
+    else:
+        padding = (0, 0, 0)
+
+    helpers.test_frontend_function(
+        input_dtypes=input_df,
         test_flags=test_flags,
         backend_to_test=backend_fw,
         frontend=frontend,
