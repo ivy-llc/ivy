3a10637d6adb45e5d1b01a5c62ba0d222b0262a9
Nitesh Kesharwani <141853536+NiteshK84@users.noreply.github.com>
2023-09-03 21:45:12 +0530

    xlogy_ (#22821)

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1910,6 +1910,14 @@ class Tensor:
     def cummax(self, dim):
         return torch_frontend.cummax(self, dim)
 
+    @with_unsupported_dtypes(
+        {"2.0.1 and below": ("bfloat16",)},
+        "torch",
+    )
+    def xlogy_(self, *, other, out=None):
+        self.ivy_array = torch_frontend.xlogy(self, other, out=out).ivy_array
+        return self
+
     def ne(self, other):
         return self.not_equal(other)
 
