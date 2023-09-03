4e60e57fb8d18c1b3901095f1890323109384c9d
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-09-03 02:18:23 +0100

    feat(frontend): implement fit method for LabelEncoder in sklearn frontend along with test (pass)

--- ivy/functional/frontends/sklearn/preprocessing/_label.py
+++ ivy/functional/frontends/sklearn/preprocessing/_label.py
@@ -1,9 +1,16 @@
 from ivy.functional.frontends.sklearn.base import BaseEstimator, TransformerMixin
+import ivy
 
 
 class LabelEncoder(TransformerMixin, BaseEstimator):
     def fit(self, y):
-        raise NotImplementedError
+        shape = y.shape
+        if len(shape) == 2 and shape[1] == 1:
+            y = y.reshape(-1)
+        elif len(shape) != 1:
+            raise ValueError("y should be a 1d array, or column")
+        self.classes_ = ivy.unique_values(y)
+        return self
 
     def fit_transform(self, y):
         raise NotImplementedError
