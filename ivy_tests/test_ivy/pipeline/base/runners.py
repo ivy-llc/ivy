fc825329a491559ac15d85ec4d7d750465b1cdbd
Mostafa Hani <71686115+CatB1t@users.noreply.github.com>
2023-09-04 03:14:07 +0000

    Add method in TestCaseSubRunner

--- ivy_tests/test_ivy/pipeline/base/runners.py
+++ ivy_tests/test_ivy/pipeline/base/runners.py
@@ -3,6 +3,15 @@ from dataclasses import dataclass
 import numpy as np  # for type hint only
 
 
+@dataclass
+class TestCaseSubRunnerResult:
+    flatten_elements_np: np.ndarray
+    shape: tuple
+    device: str
+    dtype: str
+    type: str
+
+
 class TestCaseRunner(ABC):
     @abstractmethod
     def _assert_dtype(self):
@@ -34,6 +43,19 @@ class TestCaseRunner(ABC):
 
 
 class TestCaseSubRunner(ABC):
+    def split_args_to_args_and_kwargs(num_positional_args, test_arguments):
+        """
+        Split the kwargs into args and kwargs.
+
+        The first num_positional_args ported to args.
+        """
+        args = [v for v in list(test_arguments.values())[:num_positional_args]]
+        kwargs = {
+            k: test_arguments[k]
+            for k in list(test_arguments.keys())[num_positional_args:]
+        }
+        return args, kwargs
+
     @abstractmethod
     def _search_args():
         pass
@@ -49,12 +71,3 @@ class TestCaseSubRunner(ABC):
     @abstractmethod
     def get_results():
         pass
-
-
-@dataclass
-class TestCaseSubRunnerResult:
-    flatten_elements_np: np.ndarray
-    shape: tuple
-    device: str
-    dtype: str
-    type: str
