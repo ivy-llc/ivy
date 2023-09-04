a2ed5674f61d2f4dc68ca9cc638a22252c50fa23
muzakkirhussain011 <138936198+muzakkirhussain011@users.noreply.github.com>
2023-09-04 21:23:26 +0530

    Create tokenization.py
    This tokenization.py file is a part of Ivy, a deep learning framework with a focus on flexibility and extensibility. It enhances Ivy's capabilities by integrating the Natural Language Toolkit (NLTK), a powerful library for natural language processing, into Ivy's NumPy backend.
    
    Functions:
    
    tokenize(text): This function tokenizes a given input text into individual words using NLTK's word tokenization. It returns a list of words extracted from the input text.
    
    sentence_tokenize(text): The sentence_tokenize function tokenizes the input text into sentences using NLTK's sentence tokenization. It returns a list of sentences, making it useful for tasks that involve processing text at the sentence level.
    
    Why NLTK Integration Matters:
    
    NLTK is widely used in the field of natural language processing for its comprehensive text processing capabilities. By integrating NLTK into Ivy, users can harness NLTK's tokenization functions within Ivy's framework, allowing for seamless text preprocessing and analysis alongside other deep learning tasks. This integration enhances Ivy's versatility and makes it a suitable choice for NLP projects.
    
    Usage:
    
    To tokenize text using NLTK within Ivy, users can simply import this module and call the tokenize or sentence_tokenize functions, passing their text as input. The functions return the tokenized output, which can be further utilized for downstream NLP tasks.
    
    By integrating this NLTK module, Ivy empowers users to efficiently handle and preprocess text data, bridging the gap between deep learning and natural language processing.

---
+++ ivy/functional/backends/nltk/tokenization.py
@@ -0,0 +1,27 @@
+import nltk
+
+def tokenize(text):
+    """
+    Tokenize a text using NLTK.
+
+    Args:
+        text (str): The input text to tokenize.
+
+    Returns:
+        list of str: A list of tokens extracted from the input text.
+    """
+    tokens = nltk.word_tokenize(text)
+    return tokens
+
+def sentence_tokenize(text):
+    """
+    Tokenize a text into sentences using NLTK.
+
+    Args:
+        text (str): The input text to tokenize.
+
+    Returns:
+        list of str: A list of sentences extracted from the input text.
+    """
+    sentences = nltk.sent_tokenize(text)
+    return sentences
