9149a0721239da5adac1a9bbc79f277433e1e5cb
Arsalan ali <i191885@nu.edu.pk>
2023-09-04 18:04:52 +0500

    Sqrt  (#22972)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_math.py
@@ -2187,6 +2187,34 @@ def test_paddle_sqrt(
     )
 
 
+# sqrt_
+@handle_frontend_test(
+    fn_tree="paddle.tensor.math.sqrt_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_sqrt_(
+    *,
+    dtype_and_x,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
 # square
 @handle_frontend_test(
     fn_tree="paddle.tensor.math.square",
