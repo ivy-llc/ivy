53003813047761f0a35a6ce3e9f5f61e9d7d7df8
Kareem Morsy <kreem.morsy@hotmail.com>
2023-09-05 18:48:52 +0000

    fix: cleanup

--- demos_conf.py
+++ demos_conf.py
@@ -1,8 +1,3 @@
-# Retrieve html_theme_options from docs/conf.py
-from docs.conf import html_theme_options
-
-html_theme_options["switcher"]["json_url"] = "https://unify.ai/docs/versions/ivy.json"
-
 youtube_map = {
     # TODO: Add video tutorials
     # "quickstart": "tmhFTFSEa6k",
