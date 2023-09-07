cd42cc1c002bb984acfc4986dd98456b5e4af38c
James Mbeti <jamesmbeti@gmail.com>
2023-09-07 17:54:04 +0300

    added histogram statistic function to numpy frontend

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
@@ -45,3 +45,49 @@ def test_numpy_bincount(
         weights=None,
         minlength=0,
     )
+
+# histogram
+@handle_frontend_test(
+    fn_tree="numpy.histogram",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("integer"),
+        min_value=1,
+        max_value=2,
+        shape=st.shared(
+            helpers.get_shape(
+                min_num_dims=1,
+                max_num_dims=1,
+            ),
+            key="a_s_d",
+        ),
+    ),
+    test_with_out=st.just(False),
+)
+def test_numpy_histogram(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    backend_fw,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    bins = 10
+    range = None
+    density = None
+    weights = None
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        a=x[0],
+        bins=bins,
+        range=range,
+        density=density,
+        weights=weights
+    )
