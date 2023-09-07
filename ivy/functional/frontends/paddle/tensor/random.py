ffeaeae79830d9b344d933526b4077b41ecf245b
DHANUSH N <coder.dhanush@gmail.com>
2023-09-07 15:09:11 +0530

    frontend paddle exponential_ (#23070)
    Hello @dhanush-2501, thank you for your contribution!

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -12,6 +12,15 @@ from ivy.functional.frontends.paddle.func_wrapper import (
     "paddle",
 )
 @to_ivy_arrays_and_back
+def exponential_(x, lam=1.0, name=None):
+    return ivy.multiply(lam, ivy.exp(ivy.multiply(-lam, x)))
+
+
+@with_supported_dtypes(
+    {"2.5.1 and below": ("float32", "float64")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
 def normal(mean=0.0, std=1.0, shape=None, name=None):
     return ivy.random_normal(mean=mean, std=std, shape=shape)
 
