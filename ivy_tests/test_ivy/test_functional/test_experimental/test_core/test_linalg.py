403b0a3a6ae13152e6d035da2de95f1fd06ccc96
Madjic-CH <a.chergui@esi-sba.dz>
2023-09-07 11:39:45 +0100

    fix(test): fix higher_order_moment input type.

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
@@ -405,7 +405,7 @@ def _higher_order_moment_data(draw):
     order = draw(helpers.ints(min_value=0, max_value=10))
     dtype, x = draw(
         helpers.dtype_and_values(
-            available_dtypes=helpers.get_dtypes("valid"),
+            available_dtypes=["float32", "float64"],
             shape=shape,
             large_abs_safety_factor=20,
             small_abs_safety_factor=20,
