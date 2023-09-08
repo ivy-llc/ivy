49ed04ca58b3c5ff5df10d0660edc8f44394979e
Vaatsalya <96314403+vaatsalya123@users.noreply.github.com>
2023-09-08 13:00:48 -0400

    Try without venv setup (Test push)

--- ivy/engines/setup_xla.sh
+++ ivy/engines/setup_xla.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pip install virtualenv
+#pip install virtualenv
 cd XLA/rust_api/
-mkdir xla_build && virtualenv xla_build
-source xla_build/bin/activate
+#mkdir xla_build && virtualenv xla_build
+#source xla_build/bin/activate
 wget https://github.com/elixir-nx/xla/releases/download/v0.4.4/xla_extension-x86_64-linux-gnu-cuda111.tar.gz
 tar -xzvf xla_extension-x86_64-linux-gnu-cuda111.tar.gz
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
