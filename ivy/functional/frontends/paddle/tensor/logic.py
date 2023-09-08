25fdf27b97cfee4d54b0a40bc3d908aa85330020
AnnaTz <111577222+AnnaTz@users.noreply.github.com>
2023-09-08 14:20:03 +0100

    fix(frontend-paddle): Adds note comment about correct function placement

--- ivy/functional/frontends/paddle/tensor/logic.py
+++ ivy/functional/frontends/paddle/tensor/logic.py
@@ -1,2 +1,6 @@
 # local
 from ..logic import *  # noqa: F401
+
+# NOTE:
+# Only inplace functions are to be added in this file.
+# Please add non-inplace counterparts to `/frontends/paddle/logic.py`.
