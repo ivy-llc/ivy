1aa2b7f3a2979057caa6b2ceb1d4b4d362377dce
Novend <nchidinma4@gmail.com>
2023-09-08 17:21:58 +0100

    Update random.py

--- ivy/functional/frontends/paddle/tensor/random.py
+++ ivy/functional/frontends/paddle/tensor/random.py
@@ -20,19 +20,6 @@ def exponential_(x, lam=1.0, name=None):
     return ivy.multiply(lam, ivy.exp(ivy.multiply(-lam, x)))
 
 
-@with_supported_dtypes(
-    {"2.5.1 and below": ("float32", "float64")},
-    "paddle",
-)
-@to_ivy_arrays_and_back
-def rnormal(mean=0.0, std=1.0, shape=None, name=None):
-    return ivy.random_normal(mean=mean, std=std, shape=shape)
-
-
-@with_supported_dtypes(
-    {"2.5.1 and below": ("float32", "float64")},
-    "paddle",
-)
 @to_ivy_arrays_and_back
 def uniform_(x, min=-1.0, max=1.0, seed=0, name=None):
     x = ivy.array(x)
