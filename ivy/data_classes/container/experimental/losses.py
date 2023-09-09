853356fd6e9a60ba0bd9c4b51d942cdc749d5b3b
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2023-09-09 13:26:49 +0800

    feat: add container version of log_poisson_loss

--- ivy/data_classes/container/experimental/losses.py
+++ ivy/data_classes/container/experimental/losses.py
@@ -161,6 +161,181 @@ class _ContainerWithLossesExperimental(ContainerBase):
         )
 
     @staticmethod
+    def _static_log_poisson_loss(
+        input: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        target: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        /,
+        *,
+        compute_full_loss: bool = False,
+        axis: int = -1,
+        reduction: Optional[Union[str, ivy.Container]] = "mean",
+        key_chains: Optional[Union[List[str], Dict[str, str], ivy.Container]] = None,
+        to_apply: Union[bool, ivy.Container] = True,
+        prune_unapplied: Union[bool, ivy.Container] = False,
+        map_sequences: Union[bool, ivy.Container] = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        """
+        ivy.Container static method variant of ivy.log_poisson_loss. This method simply wraps the
+        function, and so the docstring for ivy.log_poisson_loss also applies to this method with
+        minimal changes.
+
+        Parameters
+        ----------
+        input
+            input array or container.
+        target
+            input array or container containing the targeted values.
+        compute_full_loss
+            whether to compute the full loss. If false, a constant term is dropped
+            in favor of more efficient optimization. Default: ``False``.
+        axis
+            the axis along which to compute the log-likelihood loss. If axis is ``-1``,
+            the log-likelihood loss will be computed along the last dimension.
+            Default: ``-1``.
+        reduction
+            ``'mean'``: The output will be averaged.
+            ``'sum'``: The output will be summed.
+            ``'none'``: No reduction will be applied to the output. Default: ``'none'``.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is ``None``.
+        to_apply
+            If input, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is ``input``.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is ``False``.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples).
+            Default is ``False``.
+        out
+            optional output container, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            The L1 loss between the input array and the targeted values.
+
+        Examples
+        --------
+        With :class:`ivy.Container` inputs:
+
+        >>> x = ivy.Container(a=ivy.array([1, 2, 3]), b=ivy.array([4, 5, 6]))
+        >>> y = ivy.Container(a=ivy.array([2, 2, 2]), b=ivy.array([5, 5, 5]))
+        >>> z = ivy.Container.static_log_poisson_loss(x, y, reduction='mean')
+        >>> print(z)
+        {
+            a: ivy.array(1.),
+            b: ivy.array(0.)
+        }
+
+        With a mix of :class:`ivy.Array` and :class:`ivy.Container` inputs:
+
+        >>> x = ivy.array([1, 2, 3])
+        >>> y = ivy.Container(a=ivy.array([2, 2, 2]), b=ivy.array([5, 5, 5]))
+        >>> z = ivy.Container.static_log_poisson_loss(x, y, reduction='mean')
+        >>> print(z)
+        {
+            a: ivy.array(1.),
+            b: ivy.array(4.)
+        }
+        """
+        return ContainerBase.cont_multi_map_in_function(
+            "log_poisson_loss",
+            input,
+            target,
+            compute_full_loss=compute_full_loss,
+            axis=axis,
+            reduction=reduction,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    def log_poisson_loss(
+        self: ivy.Container,
+        target: Union[ivy.Container, ivy.Array, ivy.NativeArray],
+        /,
+        *,
+        compute_full_loss: bool = False,
+        axis: int = -1,
+        reduction: Optional[Union[str, ivy.Container]] = "mean",
+        key_chains: Optional[Union[List[str], Dict[str, str], ivy.Container]] = None,
+        to_apply: Union[bool, ivy.Container] = True,
+        prune_unapplied: Union[bool, ivy.Container] = False,
+        map_sequences: Union[bool, ivy.Container] = False,
+        out: Optional[ivy.Container] = None,
+    ) -> ivy.Container:
+        """
+        ivy.Container instance method variant of ivy.log_poisson_loss. This method simply wraps
+        the function, and so the docstring for ivy.log_poisson_loss also applies to this method
+        with minimal changes.
+
+        Parameters
+        ----------
+        self
+            input container.
+        target
+            input array or container containing the targeticted values.
+        compute_full_loss
+            whether to compute the full loss. If false, a constant term is dropped
+            in favor of more efficient optimization. Default: ``False``.
+        axis
+            the axis along which to compute the log-likelihood loss. If axis is ``-1``,
+            the log-likelihood loss will be computed along the last dimension.
+            Default: ``-1``.
+        reduction
+            ``'mean'``: The output will be averaged.
+            ``'sum'``: The output will be summed.
+            ``'none'``: No reduction will be applied to the output. Default: ``'none'``.
+        key_chains
+            The key-chains to apply or not apply the method to. Default is ``None``.
+        to_apply
+            If input, the method will be applied to key_chains, otherwise key_chains
+            will be skipped. Default is ``input``.
+        prune_unapplied
+            Whether to prune key_chains for which the function was not applied.
+            Default is ``False``.
+        map_sequences
+            Whether to also map method to sequences (lists, tuples).
+            Default is ``False``.
+        out
+            optional output container, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            The L1 loss between the input array and the targeticted values.
+
+        Examples
+        --------
+        >>> x = ivy.Container(a=ivy.array([1, 2, 3]), b=ivy.array([4, 5, 6]))
+        >>> y = ivy.Container(a=ivy.array([2, 2, 2]), b=ivy.array([5, 5, 5]))
+        >>> z = x.log_poisson_loss(y)
+        >>> print(z)
+        {
+            a: ivy.array(1.),
+            b: ivy.array(0.)
+        }
+        """
+        return self._static_log_poisson_loss(
+            self,
+            target,
+            compute_full_loss=compute_full_loss,
+            axis=axis,
+            reduction=reduction,
+            key_chains=key_chains,
+            to_apply=to_apply,
+            prune_unapplied=prune_unapplied,
+            map_sequences=map_sequences,
+            out=out,
+        )
+
+    @staticmethod
     def _static_smooth_l1_loss(
         input: Union[ivy.Container, ivy.Array, ivy.NativeArray],
         target: Union[ivy.Container, ivy.Array, ivy.NativeArray],
