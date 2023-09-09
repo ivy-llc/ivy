7e48d32c448c6eeacab897e09a2398021a0cff72
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2023-09-09 01:00:40 +0000

    added function max_pool3d looks like there is another error on ivy.adaptive average pooling 3d as it is not found in module ivy so i have commented it out

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -107,28 +107,13 @@ def max_pool3d(
     kernel_size,
     stride=None,
     padding=0,
-    dilation=(1, 1, 1),
+    dilation=1,
     ceil_mode=False,
     data_format="NCHW",
     name=None,
 ):
     if stride is None:
         stride = kernel_size
-    if len(x.shape) < 3:
-        raise ValueError("The input tensor must have at least three dimensions.")
-    if x.shape[2:] != kernel_size:
-        raise ValueError(
-            "The shape of the input tensor must be the same as the shape of the kernel tensor.")
-    if not isinstance(padding, (tuple, list)):
-        padding = (padding,) * 3
-    if len(padding) != 3:
-        raise ValueError(
-            "The padding argument must be a single number or a tuple of three numbers.")
-    if not isinstance(dilation, (tuple, list)):
-        dilation = (dilation,) * 3
-    if len(dilation) != 3:
-        raise ValueError(
-            "The dilation argument must be a single number or a tuple of three numbers.")
     kernel_size = _broadcast_pooling_helper(kernel_size, "3d", name="kernel_size")
     padding = _broadcast_pooling_helper(padding, "3d", name="padding")
     dilation = _broadcast_pooling_helper(dilation, "3d", name="dilation")
