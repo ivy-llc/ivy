1ba4f52e7f16a4573df17785f5c7f3544206e385
Enigmafiend <41777835+Enigmafiend@users.noreply.github.com>
2023-09-09 14:44:56 +0500

    Update signal.py

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -37,6 +37,7 @@ def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
 
 
 # hann_window
+@handle_tf_dtype
 @to_ivy_arrays_and_back
 def hann_window(window_length, periodic=True, dtype=ivy.float32, name=None):
     if window_length % 2 == 1 and periodic and window_length != 1:
