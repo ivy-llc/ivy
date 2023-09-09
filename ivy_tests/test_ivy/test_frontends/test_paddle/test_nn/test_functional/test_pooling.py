78d1e24bc96ef7e0977ff2df621be3f086c3687a
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2023-09-09 23:46:07 +0000

    first commit

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
@@ -276,6 +276,55 @@ def test_paddle_avg_pool2d(
     )
 
 
+# max_pool3d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.max_pool3d",
+    dtype_x_k_s=helpers.arrays_for_pooling(
+        min_dims=5,
+        max_dims=5,
+        min_side=2,
+        max_side=4,
+    ),
+    ceil_mode=st.booleans(),
+)
+def test_paddle_max_pool3d(
+    dtype_x_k_s,
+    ceil_mode,
+    *,
+    test_flags,
+    backend_fw,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, kernel, stride, padding = dtype_x_k_s
+
+    if len(stride) == 1:
+        stride = (stride[0], stride[0], stride[0])
+
+    if padding == "SAME":
+        padding = test_pooling_functions.calculate_same_padding(
+            kernel, stride, x[0].shape[2:]
+        )
+    else:
+        padding = (0, 0, 0)
+
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        kernel_size=kernel,
+        stride=stride,
+        padding=padding,
+        ceil_mode=ceil_mode,
+        data_format="NCDHW",  # You can adjust the data format as needed
+    )
+
+
 # max_unpool1d
 @handle_frontend_test(
     fn_tree="paddle.nn.functional.max_unpool1d",
