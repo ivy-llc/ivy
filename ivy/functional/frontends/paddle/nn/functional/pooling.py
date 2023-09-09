e517c994fc22cdfd89a7d2065d1acb2f85ad2e40
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2023-09-09 01:12:28 +0000

    added max_pool3d function

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -139,6 +139,7 @@ def max_pool3d(
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def max_unpool1d(
+    x,
     indices,
     kernel_size,
     stride=None,
