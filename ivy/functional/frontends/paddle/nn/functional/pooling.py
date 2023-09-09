908b342d48a281ac86adfb6ec277e3fb9722d76d
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2023-09-09 17:02:53 +0000

    testing

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -20,10 +20,10 @@ def adaptive_avg_pool2d(x, output_size, data_format="NCHW", name=None):
     return ivy.adaptive_avg_pool2d(x, output_size)
 
 
-# @to_ivy_arrays_and_back
-# @with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
-# def adaptive_avg_pool3d(x, output_size, data_format="NCHW", name=None):
-#     return ivy.adaptive_avg_pool3d(x, output_size)
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.0 and below": ("float32", "float64")}, "paddle")
+def adaptive_avg_pool3d(x, output_size, data_format="NCHW", name=None):
+    return ivy.adaptive_avg_pool3d(x, output_size)
 
 
 @to_ivy_arrays_and_back
@@ -102,6 +102,53 @@ def avg_pool2d(
 
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+def max_pool3d(
+    x,
+    kernel_size,
+    stride=None,
+    padding=0,
+    dilation=(1, 1, 1),
+    ceil_mode=False,
+    data_format="NCHW",
+    name=None,
+):
+    if stride is None:
+        stride = kernel_size
+    if not isinstance(padding, (tuple, list)):
+        padding = (padding,) * 3
+    if len(padding) != 3:
+        raise ValueError(
+            "The padding argument must be a single number or a tuple of three numbers."
+        )
+    if not isinstance(dilation, (tuple, list)):
+        dilation = (dilation,) * 3
+    if len(dilation) != 3:
+        raise ValueError(
+            "The dilation argument must be a single number or a tuple of three numbers."
+        )
+    kernel_size = _broadcast_pooling_helper(kernel_size, "3d", name="kernel_size")
+    padding = _broadcast_pooling_helper(padding, "3d", name="padding")
+    dilation = _broadcast_pooling_helper(dilation, "3d", name="dilation")
+    # Figure out padding string
+    if all(
+        [pad == ivy.ceil((kernel - 1) / 2) for kernel, pad in zip(kernel_size, padding)]
+    ):
+        padding = "SAME"
+    else:
+        padding = "VALID"
+    return ivy.max_pool3d(
+        x,
+        kernel_size,
+        stride,
+        padding,
+        dilation=dilation,
+        ceil_mode=ceil_mode,
+        data_format=data_format,
+    )
+
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def max_unpool1d(
     x,
     indices,
@@ -112,4 +159,4 @@ def max_unpool1d(
     output_size=None,
     name=None,
 ):
-    return ivy.max_unpool1d(x, indices, kernel_size, stride, padding)
+    return ivy.max_unpool1d(x, indices, kernel_size, stride, padding, data_format)
