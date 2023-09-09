1230e7225602021f0d769f28a3a92eaa956011ef
Muhammad Farjad Ali Raza <43019983+themohal@users.noreply.github.com>
2023-09-09 18:15:46 +0000

    testing

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_nn/test_functional/test_pooling.py
@@ -1,172 +1,371 @@
+# global
+from hypothesis import strategies as st
+
 # local
-
-import ivy
-from ivy.func_wrapper import with_unsupported_dtypes, with_supported_dtypes
-from ivy.functional.frontends.paddle.func_wrapper import (
-    to_ivy_arrays_and_back,
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+from ivy_tests.test_ivy.test_frontends.test_torch.test_nn.test_functional import (
+    test_pooling_functions,
 )
-from ivy.utils.assertions import check_equal
 
 
-@to_ivy_arrays_and_back
-@with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
-def affine_grid(theta, out_shape, align_corners=True):
-    if len(out_shape) == 4:
-        N, C, H, W = out_shape
-        base_grid = ivy.empty((N, H, W, 3))
-        if align_corners:
-            base_grid[:, :, :, 0] = ivy.linspace(-1, 1, W)
-            base_grid[:, :, :, 1] = ivy.expand_dims(ivy.linspace(-1, 1, H), axis=-1)
-            height_values = ivy.expand_dims(ivy.linspace(-1, 1, H), axis=-1)
-            base_grid[:, :, :, 1] = ivy.array(
-                [[[height_values[i]] * W for i in range(H)]]
-            )[:, :, :, 0]
-            base_grid[:, :, :, 2] = ivy.full((H, W), 1)
-            grid = ivy.matmul(base_grid.view((N, H * W, 3)), theta.swapaxes(1, 2))
-            return grid.view((N, H, W, 2))
-        else:
-            base_grid[:, :, :, 0] = ivy.linspace(-1, 1, W) * (W - 1) / W
-            base_grid[:, :, :, 1] = ivy.expand_dims(
-                ivy.linspace(-1, 1, H) * (H - 1) / H, axis=-1
-            )
-            height_values = ivy.expand_dims(
-                ivy.linspace(-1, 1, H) * (H - 1) / H, axis=-1
-            )
-            base_grid[:, :, :, 1] = ivy.array(
-                [[[height_values[i]] * W for i in range(H)]]
-            )[:, :, :, 0]
-            base_grid[:, :, :, 2] = ivy.full((H, W), 1)
-        grid = ivy.matmul(base_grid.view((N, H * W, 3)), ivy.swapaxes(theta, 1, 2))
-        return grid.view((N, H, W, 2))
-    else:
-        N, C, D, H, W = out_shape
-        base_grid = ivy.empty((N, D, H, W, 4))
-        if align_corners:
-            base_grid[:, :, :, :, 0] = ivy.linspace(-1, 1, W)
-            base_grid[:, :, :, :, 1] = ivy.expand_dims(ivy.linspace(-1, 1, H), axis=-1)
-            height_values = ivy.linspace(-1, 1, H)
-            base_grid[:, :, :, :, 1] = ivy.array(
-                [[[[height_values[i]] * W for i in range(H)]] * D]
-            )
-            base_grid[:, :, :, :, 2] = ivy.expand_dims(
-                ivy.expand_dims(ivy.linspace(-1, 1, D), axis=-1), axis=-1
-            )
-            width_values = ivy.linspace(-1, 1, D)
-            base_grid[:, :, :, :, 2] = ivy.array(
-                [[ivy.array([[width_values[i]] * W] * H) for i in range(D)]]
-            )
-            base_grid[:, :, :, :, 3] = ivy.full((D, H, W), 1)
-            grid = ivy.matmul(base_grid.view((N, D * H * W, 4)), theta.swapaxes(1, 2))
-            return grid.view((N, D, H, W, 3))
-        else:
-            base_grid[:, :, :, :, 0] = ivy.linspace(-1, 1, W) * (W - 1) / W
-            base_grid[:, :, :, :, 1] = ivy.expand_dims(
-                ivy.linspace(-1, 1, H) * (H - 1) / H, axis=-1
-            )
-            height_values = ivy.linspace(-1, 1, H) * (H - 1) / H
-            base_grid[:, :, :, :, 1] = ivy.array(
-                [[[[height_values[i]] * W for i in range(H)]] * D]
-            )
-            base_grid[:, :, :, :, 2] = ivy.expand_dims(
-                ivy.expand_dims(ivy.linspace(-1, 1, D) * (D - 1) / D, axis=-1), axis=-1
-            )
-            width_values = ivy.linspace(-1, 1, D) * (D - 1) / D
-            base_grid[:, :, :, :, 2] = ivy.array(
-                [[ivy.array([[width_values[i]] * W] * H) for i in range(D)]]
-            )
-            base_grid[:, :, :, :, 3] = ivy.full((D, H, W), 1)
-            grid = ivy.matmul(base_grid.view((N, D * H * W, 4)), theta.swapaxes(1, 2))
-            return grid.view((N, D, H, W, 3))
-
-
-@to_ivy_arrays_and_back
-@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
-def channel_shuffle(x, groups, data_format="NCHW", name=None):
-    if len(ivy.shape(x)) != 4:
-        raise ValueError(
-            "Input x should be 4D tensor, but received x with the shape of {}".format(
-                ivy.shape(x)
-            )
-        )
-
-    if not isinstance(groups, int):
-        raise TypeError("groups must be int type")
-
-    if groups <= 0:
-        raise ValueError("groups must be positive")
-
-    if data_format not in ["NCHW", "NHWC"]:
-        raise ValueError(
-            "Attr(data_format) should be 'NCHW' or 'NHWC'."
-            "But recevie Attr(data_format): {} ".format(data_format)
-        )
-
-    if data_format == "NCHW":
-        b, c, h, w = ivy.shape(x)
-        x = ivy.reshape(x, (b, groups, c // groups, h, w))
-        x = ivy.permute_dims(x, (0, 2, 1, 3, 4))
-        x = ivy.reshape(x, (b, c, h, w))
-    else:
-        b, h, w, c = ivy.shape(x)
-        x = ivy.reshape(x, (b, h, w, groups, c // groups))
-        x = ivy.permute_dims(x, (0, 1, 2, 4, 3))
-        x = ivy.reshape(x, (b, h, w, c))
-    return x
-
-
-@to_ivy_arrays_and_back
-def pixel_shuffle(x, upscale_factor, data_format="NCHW"):
-    input_shape = ivy.shape(x)
-    check_equal(
-        len(input_shape),
-        4,
-        message="pixel shuffle requires a 4D input, but got input size {}".format(
-            input_shape
-        ),
+# adaptive_avg_pool1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.adaptive_avg_pool1d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=3,
+        max_num_dims=3,
+        min_dim_size=1,
+        max_value=100,
+        min_value=-100,
+    ),
+    output_size=helpers.ints(min_value=1, max_value=5),
+)
+def test_paddle_adaptive_avg_pool1d(
+    *,
+    dtype_and_x,
+    output_size,
+    test_flags,
+    frontend,
+    on_device,
+    backend_fw,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        output_size=output_size,
     )
 
-    if not isinstance(upscale_factor, int):
-        raise ValueError("upscale factor must be int type")
 
-    if data_format not in ["NCHW", "NHWC"]:
-        raise ValueError(
-            "Attr(data_format) should be 'NCHW' or 'NHWC'."
-            "But recevie Attr(data_format): {} ".format(data_format)
-        )
-
-    b = input_shape[0]
-    c = input_shape[1] if data_format == "NCHW" else input_shape[3]
-    h = input_shape[2] if data_format == "NCHW" else input_shape[1]
-    w = input_shape[3] if data_format == "NCHW" else input_shape[2]
-
-    upscale_factor_squared = upscale_factor**2
-
-    check_equal(
-        c % upscale_factor_squared,
-        0,
-        message=(
-            "pixel shuffle expects input channel to be divisible by square of upscale"
-            " factor, but got input with sizes {}, upscale factor={}, and"
-            " self.size(1)={}, is not divisible by {}".format(
-                input_shape, upscale_factor, c, upscale_factor_squared
-            )
+# adaptive_avg_pool2d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.adaptive_avg_pool2d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=4,
+        max_num_dims=4,
+        min_dim_size=1,
+        max_value=100,
+        min_value=-100,
+    ),
+    output_size=st.one_of(
+        st.tuples(
+            helpers.ints(min_value=1, max_value=5),
+            helpers.ints(min_value=1, max_value=5),
         ),
-        as_array=False,
+        helpers.ints(min_value=1, max_value=5),
+    ),
+)
+def test_paddle_adaptive_avg_pool2d(
+    *,
+    dtype_and_x,
+    output_size,
+    test_flags,
+    frontend,
+    on_device,
+    backend_fw,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        output_size=output_size,
     )
 
-    oc = int(c / upscale_factor_squared)
-    oh = h * upscale_factor
-    ow = w * upscale_factor
 
-    if data_format == "NCHW":
-        input_reshaped = ivy.reshape(x, (b, oc, upscale_factor, upscale_factor, h, w))
-    else:
-        input_reshaped = ivy.reshape(x, (b, h, w, upscale_factor, upscale_factor, oc))
+# adaptive_avg_pool3d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.adaptive_avg_pool3d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=5,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_value=100,
+        min_value=-100,
+    ),
+    output_size=st.one_of(
+        st.tuples(
+            helpers.ints(min_value=1, max_value=5),
+            helpers.ints(min_value=1, max_value=5),
+        ),
+        helpers.ints(min_value=1, max_value=5),
+    ),
+)
+def test_paddle_adaptive_avg_pool3d(
+    *,
+    dtype_and_x,
+    output_size,
+    test_flags,
+    frontend,
+    backend_fw,
+    on_device,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        output_size=output_size,
+    )
 
-    if data_format == "NCHW":
-        return ivy.reshape(
-            ivy.permute_dims(input_reshaped, (0, 1, 4, 2, 5, 3)), (b, oc, oh, ow)
+
+# adaptive_max_pool2d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.adaptive_max_pool2d",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        min_num_dims=4,
+        max_num_dims=4,
+        min_dim_size=1,
+        # Setting max and min value because this operation in paddle is not
+        # numerically stable
+        max_value=100,
+        min_value=-100,
+    ),
+    output_size=st.one_of(
+        st.tuples(
+            helpers.ints(min_value=1, max_value=5),
+            helpers.ints(min_value=1, max_value=5),
+        ),
+        helpers.ints(min_value=1, max_value=5),
+    ),
+)
+def test_paddle_adaptive_max_pool2d(
+    *,
+    dtype_and_x,
+    output_size,
+    test_flags,
+    frontend,
+    on_device,
+    backend_fw,
+    fn_tree,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        output_size=output_size,
+    )
+
+
+# avg_pool1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.avg_pool1d",
+    x_k_s_p_df=helpers.arrays_for_pooling(
+        min_dims=3,
+        max_dims=3,
+        min_side=2,
+        max_side=4,
+        data_format="channel_first",
+    ),
+    exclusive=st.booleans(),
+    ceil_mode=st.just(False),
+    test_with_out=st.just(False),
+)
+def test_paddle_avg_pool1d(
+    *,
+    x_k_s_p_df,
+    frontend,
+    test_flags,
+    backend_fw,
+    on_device,
+    fn_tree,
+    exclusive,
+    ceil_mode,
+):
+    input_dtype, x, kernel_size, stride, padding = x_k_s_p_df
+    if padding == "SAME":
+        padding = test_pooling_functions.calculate_same_padding(
+            kernel_size, stride, [x[0].shape[2]]
         )
-    return ivy.reshape(
-        ivy.permute_dims(input_reshaped, (0, 1, 4, 2, 5, 3)), (b, oh, ow, oc)
+    else:
+        padding = (0,)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        exclusive=exclusive,
+        ceil_mode=ceil_mode,
+    )
+
+
+# avg_pool2d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.pooling.avg_pool2d",
+    dtype_x_k_s=helpers.arrays_for_pooling(
+        min_dims=4,
+        max_dims=4,
+        min_side=2,
+        max_side=4,
+    ),
+    ceil_mode=st.booleans(),
+    exclusive=st.booleans(),
+    data_format=st.sampled_from(["NCHW", "NHWC"]),
+)
+def test_paddle_avg_pool2d(
+    dtype_x_k_s,
+    exclusive,
+    ceil_mode,
+    data_format,
+    *,
+    test_flags,
+    backend_fw,
+    frontend,
+    fn_tree,
+    on_device,
+):
+    input_dtype, x, kernel, stride, padding = dtype_x_k_s
+
+    if data_format == "NCHW":
+        x[0] = x[0].reshape(
+            (x[0].shape[0], x[0].shape[3], x[0].shape[1], x[0].shape[2])
+        )
+    if len(stride) == 1:
+        stride = (stride[0], stride[0])
+    if padding == "SAME":
+        padding = test_pooling_functions.calculate_same_padding(
+            kernel, stride, x[0].shape[2:]
+        )
+    else:
+        padding = (0, 0)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        kernel_size=kernel,
+        stride=stride,
+        padding=padding,
+        ceil_mode=ceil_mode,
+        exclusive=exclusive,
+        divisor_override=None,
+        data_format=data_format,
+    )
+
+
+# max_pool3d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.max_pool3d",
+    x_k_s_p_df=helpers.arrays_for_pooling(
+        min_dims=5,
+        max_dims=5,
+        min_side=2,
+        max_side=4,
+        data_format="NCHW",
+    ),
+    stride=st.tuples(
+        helpers.ints(min_value=1, max_value=3),
+    ),
+    padding=st.tuples(
+        helpers.ints(min_value=0, max_value=2),
+    ),
+    dilation=st.tuples(
+        helpers.ints(min_value=1, max_value=2),
+    ),
+    ceil_mode=st.booleans(),
+    return_indices=st.booleans(),
+)
+def test_paddle_max_pool3d(
+    *,
+    x_k_s_p_df,
+    stride,
+    padding,
+    dilation,
+    ceil_mode,
+    return_indices,
+    frontend,
+    test_flags,
+    on_device,
+    backend_fw,
+    fn_tree,
+):
+    input_dtype, x, kernel_size, padding, data_format = x_k_s_p_df
+    if padding == "SAME":
+        padding = test_pooling_functions.calculate_same_padding(
+            kernel_size, stride, x[0].shape[2:]
+        )
+    else:
+        padding = (0, 0, 0)
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        ceil_mode=ceil_mode,
+        data_format=data_format,
+        return_indices=return_indices,
+    )
+
+
+# max_unpool1d
+@handle_frontend_test(
+    fn_tree="paddle.nn.functional.max_unpool1d",
+    x_k_s_p=helpers.arrays_for_pooling(min_dims=3, max_dims=3, min_side=1, max_side=4),
+    indices=st.lists(st.integers(0, 1), min_size=1, max_size=4),
+)
+def test_paddle_max_unpool1d(
+    *,
+    x_k_s_p,
+    indices,
+    test_flags,
+    frontend,
+    on_device,
+    backend_fw,
+    fn_tree,
+):
+    (input_dtype, x, kernel_size, stride, padding) = x_k_s_p
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        on_device=on_device,
+        fn_tree=fn_tree,
+        x=x[0],
+        indices=indices,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
     )
