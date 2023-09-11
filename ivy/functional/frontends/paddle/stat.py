a76b9000dbb804f106acc4f1f98b7930911c1c93
Ahmed Hisham <62694574+Mghrabi@users.noreply.github.com>
2023-09-11 14:35:24 +0300

    fixed wrong returned shape of paddle.mean function (#23389)

--- ivy/functional/frontends/paddle/stat.py
+++ ivy/functional/frontends/paddle/stat.py
@@ -10,7 +10,6 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 @to_ivy_arrays_and_back
 def mean(input, axis=None, keepdim=False, out=None):
     ret = ivy.mean(input, axis=axis, keepdims=keepdim, out=out)
-    ret = ivy.expand_dims(ret, axis=-1) if ret.ndim == 0 else ret
     return ret
 
 
