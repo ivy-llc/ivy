645ec04f942ba6e2854b9f4f539eb0ac945d7ef9
Boghdady9 <MohamedBoghdady@my.uopeople.edu>
2023-09-11 13:53:50 +0300

    added backward method to Paddle Frontend

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -747,3 +747,9 @@ class Tensor:
 
     def is_floating_point(self):
         return paddle_frontend.is_floating_point(self._ivy_array)
+
+    @with_supported_dtypes(
+        {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle")
+
+    def backward(self,x,grad_tensor=None,retain_graph=False,):
+        return paddle_frontend.Tensor.backward(x, grad_tensor=grad_tensor, retain_graph=retain_graph)
