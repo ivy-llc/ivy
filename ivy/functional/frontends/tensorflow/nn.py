bc3126150300f9220b4c9b8dc59b499bc5661833
adityadeshmukh369 <deshmukhaditya@iitgn.ac.in>
2023-09-11 13:40:29 +0530

    Issue #22783

--- ivy/functional/frontends/tensorflow/nn.py
+++ ivy/functional/frontends/tensorflow/nn.py
@@ -87,6 +87,9 @@ def avg_pool(input, ksize, strides, padding, data_format="NWC", name=None):
 def avg_pool1d(input, ksize, strides, padding, data_format="NWC", name=None):
     return ivy.avg_pool1d(input, ksize, strides, padding, data_format=data_format)
 
+@to_ivy_arrays_and_back
+def avg_pool2d(input, ksize, strides, padding, data_format="NHWC", name=None):
+    return ivy.avg_pool2d(input, ksize, strides, padding, data_format=data_format)
 
 # avg_pool3d
 @to_ivy_arrays_and_back
