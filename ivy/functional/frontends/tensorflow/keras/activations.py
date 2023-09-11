c84538034b925dca2cfbbacae1b048345e5ac779
Priyansh Kumar Singh <52667323+Nik-code@users.noreply.github.com>
2023-09-11 21:43:05 +0530

    Added serialize function to activations.py
    Added serialize function to file /ivy/functional/frontends/tensorflow/keras/activations.py

--- ivy/functional/frontends/tensorflow/keras/activations.py
+++ ivy/functional/frontends/tensorflow/keras/activations.py
@@ -47,6 +47,44 @@ def deserialize(name, custom_objects=None):
 
 
 @with_supported_dtypes(
+    {"2.13.0 and below": ("float16", "float32", "float64")},
+    "tensorflow",
+)
+def serialize(func, custom_objects=None):
+    # If the activation function is None, return None
+    if func is None:
+        return None
+
+    # If the activation function is already a string, return it
+    elif isinstance(func, str):
+        return func
+
+    # If the activation function is callable (a function), get its name
+    elif callable(func):
+        # Check if the function is in the custom_objects dictionary
+        if custom_objects:
+            for name, custom_func in custom_objects.items():
+                if custom_func == func:
+                    return name
+
+        # Check if the function is in the ACTIVATION_FUNCTIONS list
+        if func.__name__ in ACTIVATION_FUNCTIONS:
+            return func.__name__
+
+        # Check if the function is in the tensorflow frontend activations
+        elif func in tf_frontend.keras.activations.__dict__.values():
+            for name, tf_func in tf_frontend.keras.activations.__dict__.items():
+                if tf_func == func:
+                    return name
+
+        else:
+            raise ValueError(f"Unknown activation function: {func}.")
+
+    else:
+        raise ValueError(f"Could not interpret activation function: {func}")
+
+
+@with_supported_dtypes(
     {"2.13.0 and below": ("bfloat16", "float16", "float32", "float64")},
     "tensorflow",
 )
