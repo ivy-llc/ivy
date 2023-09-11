6154a19952ca7faf9aafc8350c5a9b7400f165ef
ReneFabricius <65137969+ReneFabricius@users.noreply.github.com>
2023-09-11 18:29:20 +0300

    Set ground truth backend for frontend tests

--- ivy_tests/test_ivy/helpers/globals.py
+++ ivy_tests/test_ivy/helpers/globals.py
@@ -78,6 +78,7 @@ def setup_frontend_test(frontend: str, backend: str, device: str, test_data: Tes
     _set_frontend(frontend)
     _set_backend(backend)
     _set_device(device)
+    _set_ground_truth_backend(frontend)
 
 
 def teardown_frontend_test():
@@ -85,6 +86,7 @@ def teardown_frontend_test():
     _unset_frontend()
     _unset_backend()
     _unset_device()
+    _unset_ground_truth_backend()
 
 
 def _set_test_data(test_data: TestData):
