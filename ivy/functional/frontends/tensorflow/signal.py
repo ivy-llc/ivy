8c7053dc22e8eca011e1e8de372b2a75c604201d
Dharshannan Sugunan <dharshannan1607@gmail.com>
2023-09-12 20:08:40 +0100

    Update to solve merge conflicts

--- ivy/functional/frontends/tensorflow/signal.py
+++ ivy/functional/frontends/tensorflow/signal.py
@@ -19,6 +19,25 @@ def idct(input, type=2, n=None, axis=-1, norm=None, name=None):
     return ivy.dct(input, type=inverse_type, n=n, axis=axis, norm=norm)
 
 
+@with_supported_dtypes(
+    {"2.13.0 and below": ("float32", "float64", "float16", "bfloat16")},
+    "tensorflow",
+)
+@handle_tf_dtype
+@to_ivy_arrays_and_back
+def kaiser_window(window_length, beta=12.0, dtype=ivy.float32, name=None):
+    return ivy.kaiser_window(window_length, periodic=False, beta=beta, dtype=dtype)
+
+
+@with_supported_dtypes(
+    {"2.13.0 and below": ("float16", "float32", "float64", "bfloat16")},
+    "tensorflow",
+)
+@to_ivy_arrays_and_back
+def vorbis_window(window_length, dtype=ivy.float32, name=None):
+    return ivy.vorbis_window(window_length, dtype=dtype, out=None)
+
+
 # stft
 @to_ivy_arrays_and_back
 def stft(signals, frame_length, frame_step, fft_length=None,
