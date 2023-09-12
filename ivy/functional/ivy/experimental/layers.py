51499835ff60bad8d13d97985db57065b78cc459
Dharshannan Sugunan <94248626+Dharshannan@users.noreply.github.com>
2023-09-12 17:51:55 +0100

    Implement_stft_functional_api (#22581)

--- ivy/functional/ivy/experimental/layers.py
+++ ivy/functional/ivy/experimental/layers.py
@@ -2803,3 +2803,71 @@ def rfftn(
         raise ValueError("s and axes must have the same length.")
 
     return ivy.current_backend(x).rfftn(x, s=s, axes=axes, norm=norm, out=out)
+
+
+# stft
+@handle_exceptions
+@handle_backend_invalid
+@handle_nestable
+@handle_array_like_without_promotion
+@handle_out_argument
+@to_native_arrays_and_back
+@handle_device_shifting
+def stft(
+    signals: Union[ivy.Array, ivy.NativeArray],
+    frame_length: int,
+    frame_step: int,
+    /,
+    *,
+    fft_length: Optional[int] = None,
+    window_fn: Optional = None,
+    pad_end: bool = False,
+    name: Optional[str] = None,
+    out: Optional[ivy.Array] = None,
+) -> ivy.Array:
+    """
+    ivy.Container static method variant of ivy.stft.
+
+    This method simply wraps the function, and so the docstring for
+    ivy.stft also applies to this method with minimal changes.
+
+    Parameters
+    ----------
+    signals
+        Input Arrays.
+    frame_length
+       An integer scalar Tensor. The window length in samples.
+    frame_step
+        An integer scalar Tensor. The number of samples to step.
+    fft_length, optional
+        An integer scalar Tensor. The size of the FFT to apply.
+        If not provided, uses the smallest power of 2 enclosing frame_length.
+    window_fn, optional
+        A callable that takes a window length and a dtype
+        keyword argument and returns a [window_length] Tensor of samples
+        in the provided datatype. If set to None, no windowing is used.
+    pad_end, optional
+        Whether to pad the end of signals with zeros when the provided frame length
+        and step produces a frame that lies partially past its end.
+    name, optional
+        An optional name for the operation.
+    out, optional
+        Optional output array for writing the result.
+
+    Returns
+    -------
+    ret
+        A [..., frames, fft_unique_bins] Tensor of
+        complex64/complex128 STFT values where fft_unique_bins is
+        fft_length // 2 + 1 (the unique components of the FFT).
+    """
+    return ivy.current_backend(signals).stft(
+        signals,
+        frame_length,
+        frame_step,
+        fft_length=fft_length,
+        window_fn=window_fn,
+        pad_end=pad_end,
+        name=name,
+        out=out,
+    )
--- ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_nn/test_layers.py
@@ -251,6 +251,24 @@ def _valid_dct(draw):
 
 
 @st.composite
+def _valid_stft(draw):
+    dtype, x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=["float32", "float64"],
+            max_value=65280,
+            min_value=-65280,
+            min_num_dims=1,
+            min_dim_size=2,
+            shared_dtype=True,
+        )
+    )
+    frame_length = draw(helpers.ints(min_value=16, max_value=100))
+    frame_step = draw(helpers.ints(min_value=1, max_value=50))
+
+    return dtype, x, frame_length, frame_step
+
+
+@st.composite
 def _x_and_fft(draw):
     min_fft_points = 2
     dtype = draw(helpers.get_dtypes("valid", full=False))
@@ -1242,4 +1260,37 @@ def test_rfftn(
         s=s,
         axes=axes,
         norm=norm,
+    )
+
+
+# test_stft
+@handle_test(
+    fn_tree="functional.ivy.experimental.stft",
+    dtype_x_and_args=_valid_stft(),
+    ground_truth_backend="tensorflow",
+    test_gradients=st.just(False),
+)
+def test_stft(
+    *,
+    dtype_x_and_args,
+    test_flags,
+    backend_fw,
+    fn_name,
+    on_device,
+):
+    dtype, x, frame_length, frame_step = dtype_x_and_args
+    helpers.test_function(
+        input_dtypes=dtype,
+        test_flags=test_flags,
+        backend_to_test=backend_fw,
+        on_device=on_device,
+        fn_name=fn_name,
+        rtol_=1e-2,
+        atol_=1e-2,
+        signals=x[0],
+        frame_length=frame_length,
+        frame_step=frame_step,
+        fft_length=None,
+        window_fn=None,
+        pad_end=True,
     )
