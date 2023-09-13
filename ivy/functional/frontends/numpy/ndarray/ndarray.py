f5915cdf108b528cfc1795d2d2eb6052563b5d5d
ShreyanshBardia <shrebardia13@gmail.com>
2023-09-14 00:10:09 +0530

    fixed tests for ndarray.mean

--- ivy/functional/frontends/numpy/ndarray/ndarray.py
+++ ivy/functional/frontends/numpy/ndarray/ndarray.py
@@ -165,9 +165,9 @@ class ndarray:
     def argsort(self, *, axis=-1, kind=None, order=None):
         return np_frontend.argsort(self, axis=axis, kind=kind, order=order)
 
-    def mean(self, *, axis=None, dtype=None, out=None, keepdims=False, where=True):
+    def mean(self, axis=None, dtype=None, out=None, keepdims=False, *, where=True):
         return np_frontend.mean(
-            self,
+            self.ivy_array,
             axis=axis,
             dtype=dtype,
             out=out,
