14c628dbb326acd6b427eae10af9ba676d5f6101
Moses Daudu <mosesdaudu001@gmail.com>
2023-09-13 12:40:39 +0300

    removed unsupported decorator

--- ivy/functional/backends/tensorflow/activations.py
+++ ivy/functional/backends/tensorflow/activations.py
@@ -46,7 +46,6 @@ def relu(x: Tensor, /, *, complex_mode="jax", out: Optional[Tensor] = None) -> T
     return tf.nn.relu(x)
 
 
-# @with_unsupported_dtypes({"2.13.0 and below": ("complex",)}, backend_version)
 def sigmoid(x: Tensor, /, *, omplex_mode="jax", out: Optional[Tensor] = None) -> Tensor:
     if not ivy.is_array(x):
         x = float(x)
