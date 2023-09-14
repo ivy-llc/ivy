83b04a08bd387b9df81876d15944d60c009cc587
Parthib <parthibdutta02@gmail.com>
2023-09-14 08:18:06 +0530

    Added tests

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_fft.py
@@ -108,6 +108,40 @@ def test_paddle_hfft(
 
 
 @handle_frontend_test(
+    fn_tree="paddle.fft.hfftn",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("complex"),
+        min_value=-10,
+        max_value=10,
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+)
+def test_paddle_hfftn(
+    dtype_x_axis,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    backend_fw,
+):
+    input_dtype, x, s, axes = dtype_x_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        x=x,
+        s=s,
+        axes=axes,
+    )
+
+
+@handle_frontend_test(
     fn_tree="paddle.fft.ifft",
     dtype_x_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("valid"),
