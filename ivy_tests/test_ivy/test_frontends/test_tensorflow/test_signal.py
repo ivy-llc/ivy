422a308ba183762dc68b7094b6c049713cec1cb9
Waqar Ahmed <ahmedwaqar309@gmail.com>
2023-09-14 05:14:18 +0500

    feat(frontend): Added kaiser_bessel_derived_window to tensorflow frontend  (#22517)
    Co-authored-by: Saeed Ashraf <saeedashrraf@gmail.com>

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_signal.py
@@ -119,6 +119,43 @@ def test_tensorflow_idct(
     )
 
 
+# kaiser_bessel_derived_window
+@handle_frontend_test(
+    fn_tree="tensorflow.signal.kaiser_bessel_derived_window",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        max_num_dims=0,
+        min_value=1,
+        max_value=10,
+    ),
+    beta=st.floats(min_value=1, max_value=5),
+    # dtype=helpers.get_dtypes("float", full=False),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_kaiser_bessel_derived_window(
+    *,
+    dtype_and_x,
+    beta,
+    test_flags,
+    backend_fw,
+    fn_tree,
+    on_device,
+    frontend,  # dtype
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        test_flags=test_flags,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        window_length=int(x[0]),
+        beta=beta,
+        # dtype=dtype[0],
+    )
+
+
 # kaiser_window
 @handle_frontend_test(
     fn_tree="tensorflow.signal.kaiser_window",
