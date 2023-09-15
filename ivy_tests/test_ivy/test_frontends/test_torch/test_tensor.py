91abf98e05d1d7e8b6aaaad753cbfb7f1bf9cd42
Jaskirat Singh <juskirat2000@gmail.com>
2023-09-16 01:02:20 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -4235,6 +4235,40 @@ def test_torch_tensor_bernoulli(
     )
 
 
+# bernoulli_
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="bernoulli_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+    test_with_out=st.just(True),
+)
+def test_torch_tensor_bernoulli_(
+    dtype_and_x,
+    frontend,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        init_all_as_kwargs_np={
+            "input": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={"generator": x[1], "out": x[2]},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+    )
+
+
 # bitwise_and
 @handle_frontend_method(
     class_tree=CLASS_TREE,
