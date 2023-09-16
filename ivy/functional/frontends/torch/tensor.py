8c3df473d10cb44b16f477f655ec4b62e9fd3ddb
Jaskirat Singh <juskirat2000@gmail.com>
2023-09-16 22:35:16 +0530

    Update tensor.py

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1110,7 +1110,7 @@ class Tensor:
         self.ivy_array = self.atan2(other).ivy_array
         return self
 
-    @with_unsupported_dtypes({"2.0.1 and below": ("float32", "float64")}, "torch")
+    @with_unsupported_dtypes({"2.0.1 and below": ("bfloat16",)}, "torch")
     def fmax(self, other):
         return torch_frontend.fmax(self, other)
 
