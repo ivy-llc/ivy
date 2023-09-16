44c89a77b65b1c121b778ec5f0696e8149a9cdc8
Khawaja Junaid <22100072@lums.edu.pk>
2023-09-16 21:22:40 +0000

    feat: nump loadtxt frontend function

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_existing_data.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_creation_routines/test_from_existing_data.py
@@ -138,3 +138,38 @@ def test_numpy_frombuffer(
         buffer=a,
         dtype=dtype[0],
     )
+
+
+# loadtxt
+@handle_frontend_test(
+    fn_tree="numpy.loadtxt",
+    fname=st.text(),
+    dtype_and_a=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=1,
+        min_num_dims=0,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+    ),
+    test_with_out=st.just(False),
+)
+def test_numpy_loadtxt(
+    fname,
+    dtype_and_a,
+    frontend,
+    test_flags,
+    fn_tree,
+    on_device,
+    backend_fw,
+):
+    dtype, a = dtype_and_a
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        fname=fname,
+    )
