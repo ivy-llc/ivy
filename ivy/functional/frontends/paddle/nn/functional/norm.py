888a719abe3ba8893e4a4a12ab55f03f421b6d09
conan-exploits <angayinoko@albany.edu>
2023-09-16 01:22:09 -0400

    Added normalization back

--- ivy/functional/frontends/paddle/nn/functional/norm.py
+++ ivy/functional/frontends/paddle/nn/functional/norm.py
@@ -21,7 +21,15 @@ def batch_norm(
         x, mean, variance, offset, scale, training, epsilon, momentum, data_format
     )
 
+#normalization
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+def normalize(x, p=2, axis=1, epsilon=1e-12, name=None):
+    if axis < 0:
+        axis = ivy.get_num_dims(x) + axis
+    return ivy.lp_normalize(x, p=p, axis=axis)
 
+#layer_norm
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def layer_norm(x, normalized_shape, weight=None, bias=None, epsilon=1e-05, name=None):
