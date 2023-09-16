20a4047d612a1daa80c59921443d8f1e8f7eec56
ZWRiddle <99762930+ZWRiddle@users.noreply.github.com>
2023-09-16 16:28:22 +0800

    Update linear_algebra.py

--- ivy/functional/ivy/linear_algebra.py
+++ ivy/functional/ivy/linear_algebra.py
@@ -2043,10 +2043,9 @@ def solve(
     Both the description and the type hints above assume an array input for simplicity,
     but this function is *nestable*, and therefore also accepts :class:`ivy.Container`
     instances in place of any of the arguments.
-    
+
     Examples
     --------
-    
     With class:`ivy.Array` input:
     >>> A = ivy.array([[1.1, 1.2, 1.3],
                        [2.1, 2.2, 2.3],
@@ -2061,7 +2060,7 @@ def solve(
                    [0]])
     >>> print(x.shape)
     (1,3)
-    
+
     For shape(A) = (2,3,3) and shape(B) = (2,3,1):
     >>> A = ivy.array([[[11.1, 11.2, 11.3],
                         [12.1, 12.2, 12.3],
@@ -2086,7 +2085,7 @@ def solve(
                 [0]]])
     >>> print(x.shape)
     (2,1,3)
-    
+
     For shape(A) = (3,3) and shape(B) = (3,2):
     >>> A = ivy.array([[1.1, 1.2, 1.3],
                        [2.1, 2.2, 2.3],
@@ -2104,7 +2103,7 @@ def solve(
                 [0]]])
     >>> print(x.shape)
     (2,1,3)
-    
+
     With class:`ivy.Container` input:
     >>> A = ivy.array([[1.1, 1.2, 1.3],
                        [2.1, 2.2, 2.3],
