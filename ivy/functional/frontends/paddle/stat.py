046cdc734104009e257a2d8134d73c7b611c57ac
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2023-09-15 19:54:43 -0700

    added quantile method

--- ivy/functional/frontends/paddle/stat.py
+++ ivy/functional/frontends/paddle/stat.py
@@ -52,6 +52,27 @@ def numel(x, name=None):
     return ivy.array(prod if prod > 0 else ivy.array(length, dtype=ivy.int64))
 
 
+@with_supported_dtypes({"float32", "float64"})
+@to_ivy_arrays_and_back
+def quantile(x, q, axis=None, keepdim=False, name=None):
+    x = (
+        ivy.cast(x, ivy.float64)
+        if ivy.dtype(x) == "float64"
+        else ivy.cast(x, ivy.float32)
+    )
+    sorted_x = ivy.sort(x, axis=axis)
+    rank = (ivy.shape(sorted_x, axis=axis) - 1) * q
+    rank_int = ivy.floor(rank)
+    rank_frac = rank - rank_int
+    lower_values = ivy.gather(sorted_x, ivy.cast(rank_int, ivy.int32), axis=axis)
+    upper_values = ivy.gather(sorted_x, ivy.cast(rank_int + 1, ivy.int32), axis=axis)
+    quantile_value = lower_values + rank_frac * (upper_values - lower_values)
+    if not keepdim:
+        quantile_value = ivy.squeeze(quantile_value, axis=axis)
+
+    return quantile_value
+
+
 @with_supported_dtypes(
     {"2.5.1 and below": ("float32", "float64", "uint16")},
     "paddle",
