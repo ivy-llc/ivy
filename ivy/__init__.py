5966caaeecddc0a95bedf4612863dee5ad244c21
Niraj Rajendra Kulkarni <nirajkulkarni2609@gmail.com>
2023-09-17 13:47:09 +0100

    Parafac2 class Implementation (#23274)
    Co-authored-by: Anwaar Khalid <shahanwaar.khalid8@gmail.com>

--- ivy/__init__.py
+++ ivy/__init__.py
@@ -83,6 +83,10 @@ class CPTensor:
     pass
 
 
+class Parafac2Tensor:
+    pass
+
+  
 class TTTensor:
     pass
 
@@ -762,6 +766,7 @@ from .data_classes.container import (
     add_ivy_container_instance_methods,
 )
 from .data_classes.nested_array import NestedArray
+from .data_classes.factorized_tensor import TuckerTensor, CPTensor, Parafac2Tensor
 from .data_classes.factorized_tensor import TuckerTensor, CPTensor, TTTensor
 from ivy.utils.backend import (
     current_backend,
