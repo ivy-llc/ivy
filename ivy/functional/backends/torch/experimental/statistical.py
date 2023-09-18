dc9bb04aee599da13979c99f6b639e925f54cb53
Madjid Chergui <100947451+Madjid-CH@users.noreply.github.com>
2023-09-18 10:14:14 +0100

    feat(torch backend): removed manual dtype casting. (#23659)

--- ivy/functional/backends/torch/experimental/statistical.py
+++ ivy/functional/backends/torch/experimental/statistical.py
@@ -567,7 +567,7 @@ cov.support_native_out = False
 
 
 @with_unsupported_dtypes(
-    {"2.0.1 and below": ("uint8", "bfloat16", "float16")},
+    {"2.0.1 and below": ("float16",)},
     backend_version,
 )
 def cummax(
@@ -580,11 +580,7 @@ def cummax(
     dtype: Optional[torch.dtype] = None,
     out: Optional[torch.Tensor] = None,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    if x.dtype in (torch.bool, torch.float16):
-        x = x.to(dtype=torch.float64)
-    elif x.dtype in (torch.int16, torch.int8, torch.uint8):
-        x = x.to(dtype=torch.int64)
-    elif x.dtype in (torch.complex64, torch.complex128):
+    if x.dtype in (torch.complex64, torch.complex128):
         x = x.real.to(dtype=torch.float64)
 
     if exclusive or reverse:
