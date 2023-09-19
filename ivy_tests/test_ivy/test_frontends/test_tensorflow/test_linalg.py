202d1d9365d2004a309316eaea5183efc1f8df9a
Abdullah Sabry <Abdullahsabry02@gmail.com>
2023-09-19 23:56:41 +0300

    Fixed tests

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_linalg.py
@@ -1316,7 +1316,7 @@ def tridiagonal_compact_filter(x):
             [dummy_idx] + [(i + 1, i) for i in range(dim - 1)],
         ]
     )
-    matrix = ivy.scatter_nd(indices, diagonals, shape=ivy.array([dim, dim]))
+    matrix = ivy.scatter_nd(indices, diagonals, ivy.array([dim, dim]), reduction="replace")
     return tridiagonal_matrix_filter(matrix)
 
 
