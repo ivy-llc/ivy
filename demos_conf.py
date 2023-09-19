9cb7adb11166214b9507e2234da025028035de1a
Kareem Morsy <kreem.morsy@hotmail.com>
2023-09-19 13:55:02 +0300

    feat: add youtube link

--- demos_conf.py
+++ demos_conf.py
@@ -3,6 +3,7 @@ youtube_map = {
     "demos/learn_the_basics/01_write_ivy_code": "lYW_V4ZkYEo",
     "demos/learn_the_basics/02_unify_code": "YKrxYB-1Xio",
     "demos/learn_the_basics/03_compile_code": "fqnhEi-qA4w",
+    "demos/learn_the_basics/04_transpile_code": "6WCYOF1vdLw",
 }
 
 nbsphinx_execute = 'never'
