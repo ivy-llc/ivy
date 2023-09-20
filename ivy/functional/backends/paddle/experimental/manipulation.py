58ae79ff5d62f8614439b3d51dae6c4cf593f874
ReneFabricius <65137969+ReneFabricius@users.noreply.github.com>
2023-09-20 12:08:55 +0000

    No reason to restrict dtypes

--- ivy/functional/backends/paddle/experimental/manipulation.py
+++ ivy/functional/backends/paddle/experimental/manipulation.py
@@ -198,10 +198,6 @@ def flipud(
     return paddle.flip(m, axis=0)
 
 
-@with_unsupported_device_and_dtypes(
-    {"2.5.1 and below": {"cpu": ("int16", "bfloat16")}},
-    backend_version,
-)
 def vstack(
     arrays: Sequence[paddle.Tensor],
     /,
