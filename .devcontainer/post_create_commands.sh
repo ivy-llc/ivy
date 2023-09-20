f4b0802351ea5010a1484dfa60cfea7c1a2e08d1
Nripesh Niketan <86844847+NripeshN@users.noreply.github.com>
2023-09-20 10:43:25 +0400

    Chore: Add Dockerfile for Apple Silicon Macs (#23650)

--- .devcontainer/post_create_commands.sh
+++ .devcontainer/post_create_commands.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+cd ivy
+
 git submodule update --init --recursive
 
 python3 -m pip install --user -e .
 
 python3 -m pip install pre-commit
 
-git config --global --add safe.directory /workspaces/ivy
+git config --global --add safe.directory .
 
-( cd /workspaces/ivy/ && pre-commit install)
+pre-commit install
+
+cd ..
