f24497b9d71ea21c242c82984607300b7237ef9d
Olaoluwa <olaoluwaidowu22@gmail.com>
2023-09-20 09:57:03 +0100

    fix: uninstall black

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -769,15 +769,29 @@ class Tensor:
     @with_supported_dtypes(
         {
             "2.5.1 and below": (
-                    "bool",
-                    "float16",
-                    "float32",
-                    "float64",
-                    "int32",
-                    "int64",
+                "bool",
+                "int8",
+                "int16",
+                "int32",
+                "int64",
+                "float32",
+                "float64",
             )
         },
         "paddle",
     )
-    def chunk(self, chunks, axis = 0, name=None):
+    @with_supported_dtypes(
+        {
+            "2.5.1 and below": (
+                "bool",
+                "float16",
+                "float32",
+                "float64",
+                "int32",
+                "int64",
+            )
+        },
+        "paddle",
+    )
+    def chunk(self, chunks, axis=0, name=None):
         return paddle_frontend.split(self._ivy_array, num_or_sections=chunks, axis=axis)
