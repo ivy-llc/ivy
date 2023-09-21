dda65ab6d76d4f7ed5c8906896bbcdb91cbef317
Said Gamal <said_gamal55@yahoo.com>
2023-09-21 16:02:35 +0300

    Added asanyarray to Numpy Frontend and tested.

--- ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_manipulation_routines/test_changing_kind_of_array.py
@@ -1,5 +1,6 @@
 # global
 import numpy as np
+from hypothesis import given
 
 # local
 import ivy.functional.frontends.numpy as np_frontend
@@ -28,3 +29,35 @@ def test_numpy_asscalar(arr: np.ndarray):
     ret_1 = arr.item()
     ret_2 = np_frontend.asscalar(arr)
     assert ret_1 == ret_2
+
+
+# asanyarray
+@handle_frontend_test
+@given(
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid")
+    ),
+    as_variable=helpers.array_bools(),
+    num_positional_args=helpers.num_positional_args(
+        fn_name="ivy.functional.frontends.numpy.asanyarray"
+    ),
+    native_array=helpers.array_bools(),
+)
+def test_numpy_asanyarray(
+        dtype_and_x,
+        as_variable,
+        num_positional_args,
+        native_array,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        as_variable_flags=as_variable,
+        with_out=False,
+        num_positional_args=num_positional_args,
+        native_array_flags=native_array,
+        frontend="numpy",
+        fn_tree="asanyarray",
+        a=x,
+        dtype=input_dtype,
+    )
