9c735b3f4c2ab8fe30bb27655ca67465200e8194
James Mbeti <jamesmbeti@gmail.com>
2023-09-21 12:32:17 +0300

    style(python): improving code formatting in histograms.py

--- ivy/functional/frontends/numpy/statistics/histograms.py
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -3,10 +3,12 @@ import numpy as np
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 from ivy.func_wrapper import with_supported_dtypes
 
+
 @with_supported_dtypes({"1.26.0 and below": ("int64",)}, "numpy")
 @to_ivy_arrays_and_back
 def bincount(x, /, weights=None, minlength=0):
     return ivy.bincount(x, weights=weights, minlength=minlength)
+
 
 @with_supported_dtypes({"1.26.0 and below": ("int64",)}, "numpy")
 @to_ivy_arrays_and_back
