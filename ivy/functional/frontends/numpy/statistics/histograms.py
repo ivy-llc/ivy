b1f2a3fe8d0c782d73b75b160d364baa4025cfd8
James Mbeti <jamesmbeti@gmail.com>
2023-09-21 11:35:21 +0300

    update hist function

--- ivy/functional/frontends/numpy/statistics/histograms.py
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -1,55 +1,14 @@
 import ivy
+import numpy as np
 from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 from ivy.func_wrapper import with_supported_dtypes
-
 
 @with_supported_dtypes({"1.25.2 and below": ("int64",)}, "numpy")
 @to_ivy_arrays_and_back
 def bincount(x, /, weights=None, minlength=0):
     return ivy.bincount(x, weights=weights, minlength=minlength)
 
-
-@with_supported_dtypes(
-    {
-        "1.25.2 and below": (
-            "int64",
-            "float64",
-        )
-    },
-    "numpy",
-)
+@with_supported_dtypes({"1.25.2 and below": ("int64",)}, "numpy")
 @to_ivy_arrays_and_back
 def histogram(a, bins=10, range=None, density=None, weights=None):
-    if range:
-        data_min, data_max = range
-    else:
-        data_min = min(a)
-        data_max = max(a)
-
-    bin_edges = ivy.linspace(data_min, data_max, bins + 1)
-    bin_counts = ivy.zeros(bins, dtype="float64")
-
-    if range:
-        valid_indices = (a >= data_min) & (a <= data_max)
-        a = a[valid_indices]
-        if weights is not None:
-            weights = weights[valid_indices]
-
-    bin_indices = ((ivy.array(a) - data_min) / (data_max - data_min) * bins).astype(int)
-    bin_indices = ivy.clip(bin_indices, 0, bins - 1)
-
-    if weights is None:
-        bin_counts += ivy.bincount(bin_indices, minlength=bins)
-    else:
-        bin_counts += ivy.bincount(bin_indices, weights=weights, minlength=bins)
-
-    if density:
-        total_weight = ivy.sum(bin_counts)
-        bin_width_normalized = (
-            (data_max - data_min) / bins
-            if density is None
-            else (data_max - data_min) / (total_weight * bins)
-        )
-        bin_counts /= total_weight * bin_width_normalized
-
-    return bin_counts.astype("int64"), bin_edges
+    return np.histogram(a, bins=bins, range=range, density=density, weights=weights)
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
@@ -50,21 +50,18 @@ def test_numpy_bincount(
 @handle_frontend_test(
     fn_tree="numpy.histogram",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("integer"),
-        min_value=1,
-        max_value=2,
-        shape=st.shared(
-            helpers.get_shape(
-                min_num_dims=1,
-                max_num_dims=1,
-            ),
-            key="a_s_d",
-        ),
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=1,
+        num_arrays=1,
+        shared_dtype=True,
+        large_abs_safety_factor=24,
+        small_abs_safety_factor=24,
+        safety_factor_scale="log",
     ),
     test_with_out=st.just(False),
 )
 def test_numpy_histogram(
-    *,
     dtype_and_x,
     on_device,
     fn_tree,
@@ -72,22 +69,18 @@ def test_numpy_histogram(
     backend_fw,
     test_flags,
 ):
-    input_dtype, x = dtype_and_x
-    bins = 10
-    range = None
-    density = None
-    weights = None
-
+    input_dtypes, x = dtype_and_x
     helpers.test_frontend_function(
-        input_dtypes=input_dtype,
+        input_dtypes=input_dtypes,
         frontend=frontend,
         backend_to_test=backend_fw,
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
+        rtol=1e-2,
         a=x[0],
-        bins=bins,
-        range=range,
-        density=density,
-        weights=weights,
+        bins=10,
+        range=None,
+        density=None,
+        weights=None,
     )
