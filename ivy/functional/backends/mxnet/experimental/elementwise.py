dac5962c6ba25d9f7a689f67124664210f82ab04
Arvind Thorat <127589052+portfolio-1@users.noreply.github.com>
2023-09-21 03:25:10 -0700

    Update elementwise.py

--- ivy/functional/backends/mxnet/experimental/elementwise.py
+++ ivy/functional/backends/mxnet/experimental/elementwise.py
@@ -28,6 +28,16 @@ def sinc(
 ) -> Union[(None, mx.ndarray.NDArray)]:
     raise IvyNotImplementedException()
 
+def amin(
+    x1: Union[(None, mx.ndarray.NDArray)],
+    x2: Union[(None, mx.ndarray.NDArray)],
+    /,
+    *,
+    use_where: bool = True,
+    out: Optional[Union[(None, mx.ndarray.NDArray)]] = None,
+) -> Union[(None, mx.ndarray.NDArray)]:
+    raise IvyNotImplementedException()
+
 
 def fmax(
     x1: Union[(None, mx.ndarray.NDArray)],
