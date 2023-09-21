0cc0f8eda69f24171751c08817807d484c988307
akshatvishu <akshatnayak197@gmail.com>
2023-09-21 17:31:13 +0000

    chore: remove del statement for module retention

--- ivy/functional/backends/paddle/experimental/__init__.py
+++ ivy/functional/backends/paddle/experimental/__init__.py
@@ -23,26 +23,3 @@ from .sorting import *
 from .sparse_array import *
 from .statistical import *
 from .utility import *
-
-del (
-    activations,
-    converters,
-    creation,
-    data_type,
-    device,
-    elementwise,
-    general,
-    gradients,
-    layers,
-    linear_algebra,
-    losses,
-    manipulation,
-    norms,
-    random,
-    searching,
-    set,
-    sorting,
-    sparse_array,
-    statistical,
-    utility,
-)
