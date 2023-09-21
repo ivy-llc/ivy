c233ba1a2bbf01767e53ce1037f759fe58a0ebc0
Arvind Thorat <127589052+portfolio-1@users.noreply.github.com>
2023-09-21 03:29:25 -0700

    Update elementwise.py

--- ivy/functional/backends/torch/experimental/elementwise.py
+++ ivy/functional/backends/torch/experimental/elementwise.py
@@ -393,3 +393,19 @@ def erfc(
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
     return torch.special.erfc(x)
+
+def amin(
+    x1: Union[float, torch.Tensor],
+    x2: Union[float, torch.Tensor],
+    /,
+    *,
+    use_where: bool = True,
+    out: Optional[torch.Tensor] = None,
+) -> torch.Tensor:
+    x1, x2 = ivy.promote_types_of_inputs(x1, x2)
+    
+       return torch.amin(x1, x2, out=out)
+    
+
+minimum.support_native_out = True
+
