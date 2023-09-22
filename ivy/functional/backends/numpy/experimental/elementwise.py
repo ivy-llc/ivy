3e1272461c9570c094b9f2e9f7e14fedc1656a4a
Arvind Thorat <127589052+portfolio-1@users.noreply.github.com>
2023-09-21 20:01:59 -0700

    Update elementwise.py

--- ivy/functional/backends/numpy/experimental/elementwise.py
+++ ivy/functional/backends/numpy/experimental/elementwise.py
@@ -592,8 +592,7 @@ def amin(
 ) -> np.ndarray:
     x1, x2 = ivy.promote_types_of_inputs(x1, x2)
     if ivy.exists(out):
-        return ivy.inplace_update(out, ret)
-        return ret
+        return ivy.inplace_update(out)
         return np.amin(x1, x2, out=out)
 
 
