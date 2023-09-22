4bcdeecf9829a432c5f2c890988e0994a4012c0b
Arvind Thorat <127589052+portfolio-1@users.noreply.github.com>
2023-09-22 08:50:55 -0700

    Update elementwise.py

--- ivy/functional/backends/jax/experimental/elementwise.py
+++ ivy/functional/backends/jax/experimental/elementwise.py
@@ -475,15 +475,14 @@ def erfc(
     return js.special.erfc(x)
 
 def amin(
-      x1: Union[float, JaxArray],
-      x2: Union[float, JaxArray],
-      /,
-      *,
-      out: Optional[JaxArray] = None,
-)    ->  JaxArray:
-      x1, x2 = ivy.promote_types_of_inputs(x1, x2)
-      return jnp.minimum(x1, x2)
-
+    x1: Union[float, JaxArray],
+    x2: Union[float, JaxArray],
+    /,
+    *,
+    out: Optional[JaxArray] = None,
+ ) -> JaxArray:
+    x1, x2 = ivy.promote_types_of_inputs(x1, x2)
+    return jnp.minimum(x1, x2)
 
 
 
