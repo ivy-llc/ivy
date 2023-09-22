173a97e74c07b305220ba4984ebe6a319c531d57
Shubham Tiwari <58452132+Shubham7-1@users.noreply.github.com>
2023-09-22 11:41:12 +0530

    Created test function for approx_max_k test_math.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -3230,3 +3230,36 @@ def test_tensorflow_zeta(
         x=x[0],
         q=x[1],
     )
+# approx_max_k
+@handle_frontend_test(
+    fn_tree="tensorflow.math.approx_max_k",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        large_abs_safety_factor=24,
+        small_abs_safety_factor=24,
+        safety_factor_scale="log",
+        min_num_dims=1,
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_approx_max_k(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    backend_fw,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    k = 5
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        value=x[0],
+        k=k
+    )
