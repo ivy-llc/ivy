475f3586a0cef79b282f6e8db9bb3e121534c5c5
Boghdady9 <MohamedBoghdady@my.uopeople.edu>
2023-09-24 13:03:06 +0300

    passed lint test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -424,42 +424,6 @@ def test_paddle_is_floating_point(
     )
 
 
-# mean
-@handle_frontend_method(
-    class_tree=CLASS_TREE,
-    init_tree="paddle.to_tensor",
-    method_name="mean",
-    dtype_and_x=_statistical_dtype_values(function="mean"),
-    keepdim=st.booleans(),
-)
-def test_paddle_tensor_mean(
-    dtype_and_x,
-    keepdim,
-    frontend,
-    backend_fw,
-    frontend_method_data,
-    init_flags,
-    method_flags,
-    on_device,
-):
-    input_dtype, x, axis = dtype_and_x
-    helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        init_all_as_kwargs_np={"data": x[0]},
-        method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={
-            "axis": axis,
-            "keepdim": keepdim,
-        },
-        frontend=frontend,
-        frontend_method_data=frontend_method_data,
-        init_flags=init_flags,
-        backend_to_test=backend_fw,
-        method_flags=method_flags,
-        on_device=on_device,
-    )
-
-
 # __add__
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -2709,6 +2673,42 @@ def test_paddle_tensor_max(
         frontend=frontend,
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
+        method_flags=method_flags,
+        on_device=on_device,
+    )
+
+
+# mean
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="paddle.to_tensor",
+    method_name="mean",
+    dtype_and_x=_statistical_dtype_values(function="mean"),
+    keepdim=st.booleans(),
+)
+def test_paddle_tensor_mean(
+    dtype_and_x,
+    keepdim,
+    frontend,
+    backend_fw,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    on_device,
+):
+    input_dtype, x, axis = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={"data": x[0]},
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "axis": axis,
+            "keepdim": keepdim,
+        },
+        frontend=frontend,
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        backend_to_test=backend_fw,
         method_flags=method_flags,
         on_device=on_device,
     )
