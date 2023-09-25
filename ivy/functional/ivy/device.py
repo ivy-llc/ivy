a004692cd58a61c176e03e5b572be9c38473e013
Aniket Mali <aniketmali002@gmail.com>
2023-09-25 21:13:44 +0530

    Reformat dev_util

--- ivy/functional/ivy/device.py
+++ ivy/functional/ivy/device.py
@@ -639,7 +639,9 @@ def percent_used_mem_on_dev(
 
 
 @handle_exceptions
-def dev_util(device: Union[ivy.Device, ivy.NativeDevice], /) -> float:
+def dev_util(device: Union[ivy.Device, ivy.NativeDevice],
+    /,
+) -> float:
     """
     Get the current utilization (%) for a given device.
 
