a25da35465723514fbec314211f1e4326751ec73
Daniel Bisina <danbis664@gmail.com>
2023-09-25 20:29:25 +0000

    feat: added histogram to numpy frontend

--- ivy/functional/frontends/numpy/statistics/histograms.py
+++ ivy/functional/frontends/numpy/statistics/histograms.py
@@ -7,3 +7,9 @@ from ivy.func_wrapper import with_supported_dtypes
 @to_ivy_arrays_and_back
 def bincount(x, /, weights=None, minlength=0):
     return ivy.bincount(x, weights=weights, minlength=minlength)
+
+
+@with_supported_dtypes({"1.26.0 and below": ("int64",)}, "numpy")
+@to_ivy_arrays_and_back
+def histogram(a, bins=10, range=None, density=None, weights=None):
+    return ivy.histogram(a, bins=bins, range=range, density=density, weights=weights)
--- ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
+++ ivy_tests/test_ivy/test_frontends/test_numpy/test_statistics/test_histograms.py
@@ -45,3 +45,43 @@ def test_numpy_bincount(
         weights=None,
         minlength=0,
     )
+
+
+# histogram
+@handle_frontend_test(
+    fn_tree="numpy.histogram",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        min_num_dims=1,
+        max_num_dims=1,
+        num_arrays=1,
+        shared_dtype=True,
+        large_abs_safety_factor=24,
+        small_abs_safety_factor=24,
+        safety_factor_scale="log",
+    ),
+    test_with_out=st.just(False),
+)
+def test_numpy_histogram(
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    backend_fw,
+    test_flags,
+):
+    input_dtypes, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtypes,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        rtol=1e-2,
+        a=x[0],
+        bins=10,
+        range=None,
+        density=None,
+        weights=None,
+    )
