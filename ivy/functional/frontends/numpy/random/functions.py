eb59215a71b4cd62b6b103e047478de28b20230c
Pradyumna_bidoo <Pradyumnabidoo99@gmail.com>
2023-09-25 15:31:08 +0100

    Required Changes

--- ivy/functional/frontends/numpy/random/functions.py
+++ ivy/functional/frontends/numpy/random/functions.py
@@ -74,9 +74,10 @@ def dirichlet(alpha, size=None):
 @from_zero_dim_arrays_to_scalar
 def exponential(scale=1.0, size=None, dtype="float64"):
     if scale > 0:
-        u = ivy.random_uniform(low=0.0, high=0.0, shape=size, dtype=dtype)
+        #Generate samples that are uniformly distributed based on given parameters
+        u = ivy.random_uniform(low=0.0, high=1.0, shape=size, dtype=dtype)
         return ivy.exp(scale, out=u)
-    return 0
+    return 0            #if scale parameter is less than or equal to 0 then return 0
 
 
 @to_ivy_arrays_and_back
@@ -320,7 +321,3 @@ def zipf(a, size=None):
     u = ivy.random_uniform(low=0.0, high=1.0, shape=size, dtype="float64")
     return ivy.floor(ivy.pow(1 / (1 - u), 1 / a))
 
-@to_ivy_arrays_and_back
-@from_zero_dim_arrays_to_scalar
-def exponential(scale=1.0, size=None):
-    return ivy.exponential(scale=scale, shape=size, dtype="float64")
