8a141d0e48213f6c417fa34577cdadaf2320e9d3
Muftawu <mohammedyiwere@gmail.com>
2023-09-25 10:31:44 +0000

    resolved errors

--- ivy/functional/ivy/nest.py
+++ ivy/functional/ivy/nest.py
@@ -530,7 +530,7 @@ def set_nest_at_indices(
         result = copy_nest(nest, include_derived=True)
     result = list(result) if is_tuple else result
 
-    assert type(indices) != None and type(values) != None, TypeError(values, indices)
+    assert type(indices) is not None and type(values) is not None, TypeError(values, indices)
     assert len(indices) == len(
         values
     ), " 'indices' and 'values' must be of the same length"
