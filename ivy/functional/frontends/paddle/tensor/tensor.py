b549a666fc196bd8ddddc72fcb908a64be4d77f1
Boghdady9 <100009172+Boghdady9@users.noreply.github.com>
2023-09-26 13:33:18 +0300

    feat(paddle): Add real method to Paddle Frontend (#26049)

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -778,3 +778,7 @@ class Tensor:
     )
     def less_equal(self, y, name=None):
         return paddle_frontend.less_equal(self._ivy_array, y)
+
+    @with_supported_dtypes({"2.5.1 and below": ("complex64", "complex128")}, "paddle")
+    def real(self, name=None):
+        return paddle_frontend.real(self._ivy_array)
