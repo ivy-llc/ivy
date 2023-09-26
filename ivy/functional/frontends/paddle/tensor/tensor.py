dde58a7532089b46515ad4117ef481d2af701c94
Boghdady9 <MohamedBoghdady@my.uopeople.edu>
2023-09-27 01:17:43 +0300

    feat(paddle): Add kron method to Paddle Frontend

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -793,3 +793,9 @@ class Tensor:
     )
     def cast(self, dtype):
         return paddle_frontend.cast(self, dtype)
+
+    @with_supported_dtypes(
+        {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle"
+    )
+    def kron(self, y, name=None):
+        return paddle_frontend.kron(self._ivy_array, y)
