9286a1f463f9d9a1770bf9b373fce8abca2c82ed
SherjeelAhmed <sherjeelchaudhary011@gmail.com>
2023-09-26 15:26:23 +0500

    Updated device.py

--- ivy/functional/ivy/device.py
+++ ivy/functional/ivy/device.py
@@ -212,9 +212,8 @@ def get_all_ivy_arrays_on_dev(
 
 
 @handle_exceptions
-
 def num_arrays_on_dev(device: Union[ivy.Device, ivy.NativeDevice], /) -> int:
-
+    """
     Return the number of arrays which are currently alive on the specified device.
 
     Parameters
