d6ade3933fba5e4279ded73045378a74ca39737a
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-09-26 17:25:57 +0000

    update the test_flags

--- ivy_tests/test_ivy/helpers/test_parameter_flags.py
+++ ivy_tests/test_ivy/helpers/test_parameter_flags.py
@@ -431,7 +431,6 @@ class FrontendMethodTestFlags(TestFlags):
         as_variable,
         native_arrays,
         precision_mode,
-        test_compile,
         inplace,
         test_trace,
         generate_frontend_arrays,
@@ -440,7 +439,6 @@ class FrontendMethodTestFlags(TestFlags):
         self.native_arrays = native_arrays
         self.as_variable = as_variable
         self.precision_mode = precision_mode
-        self.test_compile = test_compile
         self.inplace = inplace
         self.test_trace = test_trace
         self.generate_frontend_arrays = generate_frontend_arrays
@@ -463,7 +461,6 @@ class FrontendMethodTestFlags(TestFlags):
             f"native_arrays={self.native_arrays}. "
             f"as_variable={self.as_variable}. "
             f"precision_mode={self.precision_mode}. "
-            f"test_compile={self.test_compile}."
             f"inplace={self.inplace}. "
             f"test_trace={self.test_trace}."
             f"generate_frontend_arrays={self.generate_frontend_arrays}."
@@ -481,7 +478,6 @@ def frontend_method_flags(
     as_variable,
     native_arrays,
     precision_mode,
-    test_compile,
     inplace,
     test_trace,
     generate_frontend_arrays,
@@ -493,7 +489,6 @@ def frontend_method_flags(
             as_variable=as_variable,
             native_arrays=native_arrays,
             precision_mode=precision_mode,
-            test_compile=test_compile,
             inplace=inplace,
             test_trace=test_trace,
             generate_frontend_arrays=generate_frontend_arrays,
