cc863084a1211dd29bb28d5ccc8fb588a268e3c0
eniola-ajala <ajalaeniola454@gmail.com>
2023-09-27 03:18:50 +0800

    Update manipulation.py: index_add_

--- ivy/functional/frontends/paddle/tensor/manipulation.py
+++ ivy/functional/frontends/paddle/tensor/manipulation.py
@@ -20,3 +20,26 @@ def reshape_(x, shape):
     ret = ivy.reshape(x, shape)
     ivy.inplace_update(x, ret)
     return x
+
+# Add the index_add_ function
+@with_unsupported_dtypes(
+    {"2.5.1 and below": ("int8", "uint8", "int16", "uint16", "float16", "bfloat16")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def index_add_(x, index, value):
+    """
+    Index_add for PaddlePaddle Frontend.
+
+    Args:
+        x (ivy.Array): The input array.
+        index (ivy.Array): The indices where values should be added.
+        value (ivy.Array): The values to add to the specified indices.
+
+    Returns:
+        ivy.Array: The modified input array with values added at specified indices.
+    """
+    # Implement the index_add_ function
+    ret = index_add(x, index, value)
+    ivy.inplace_update(x, ret)
+    return x
