766246a4b745175723ce21d4ae847761810217ed
Muhammad Farjad Ali Raza <farjadmohal@gmail.com>
2023-09-27 02:13:00 +0500

    Fixed #23251

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -102,6 +102,41 @@ def avg_pool2d(
 
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+def max_pool3d(
+    x,
+    kernel_size,
+    stride=None,
+    padding=0,
+    ceil_mode=False,
+    data_format="NCDHW",
+    name=None,
+):
+    kernel_size = _broadcast_pooling_helper(kernel_size, "3d", name="kernel_size")
+    stride = _broadcast_pooling_helper(
+        stride if stride is not None else kernel_size, "3d", name="stride"
+    )
+    padding = _broadcast_pooling_helper(padding, "3d", name="padding")
+
+    # Figure out padding string
+    if all(
+        [pad == ivy.ceil((kernel - 1) / 2) for kernel, pad in zip(kernel_size, padding)]
+    ):
+        padding = "SAME"
+    else:
+        padding = "VALID"
+
+    return ivy.max_pool3d(
+        x,
+        kernel_size,
+        stride,
+        padding,
+        data_format=data_format,
+        ceil_mode=ceil_mode,
+    )
+
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def max_unpool1d(
     x,
     indices,
