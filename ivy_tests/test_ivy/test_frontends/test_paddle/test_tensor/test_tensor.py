709075e1d6b94529a2aa6823864b9f10779135f2
victory okechukwu <okeavictory@gmail.com>
2023-09-27 22:52:41 +0100

    Adding tensor exponential_ method

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -1956,6 +1956,11 @@ def test_paddle_tensor_exponential_(
     )
 
 
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="paddle.to_tensor",
+    method_name="exponential_",
+)
 # floor
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -4350,33 +4355,3 @@ def test_paddle_tensor_zero_(
         frontend=frontend,
         on_device=on_device,
     )
-
-    # Assuming you have custom decorators and testing conventions
-    @handle_frontend_method(
-        class_tree=CLASS_TREE,
-        init_tree="paddle.to_tensor",
-        method_name="exponential_",
-        # Add other parameters as needed for your testing
-    )
-    def test_paddle_tensor_exponential_(
-        dtype_x,  # Include any necessary parameters here
-        frontend,
-        frontend_method_data,
-        init_flags,
-        method_flags,
-        on_device,
-        backend_fw,
-    ):
-        input_dtype, x = dtype_x
-        helpers.test_frontend_method(
-            init_input_dtypes=input_dtype,
-            backend_to_test=backend_fw,
-            init_all_as_kwargs_np={"data": x[0]},
-            method_input_dtypes=input_dtype,
-            method_all_as_kwargs_np={},
-            frontend_method_data=frontend_method_data,
-            init_flags=init_flags,
-            method_flags=method_flags,
-            frontend=frontend,
-            on_device=on_device,
-        )
