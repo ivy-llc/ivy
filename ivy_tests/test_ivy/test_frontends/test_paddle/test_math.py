300ef86685fc1e73b388ed8612a12e67a2446312
Moksh Malik <98465137+mokshmalik5757@users.noreply.github.com>
2023-09-27 15:58:03 +0530

    feat: added log10 function in paddle frontend (#24461)

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_math.py
@@ -1433,6 +1433,32 @@ def test_paddle_log2(
         x=x[0],
     )
 
+@handle_frontend_test(
+    fn_tree="paddle.log10",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_log10(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    backend_fw,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
 
 # logit
 @handle_frontend_test(
