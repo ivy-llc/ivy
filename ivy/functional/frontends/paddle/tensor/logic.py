30d5746b4c487e7e0f988b980d8779f63a011672
AW786WA <wackali77@gmail.com>
2023-09-28 01:18:44 +0400

    Failing tests with several issues.Kindly, provide your valuable feedback at the initial implementation stage.

--- ivy/functional/frontends/paddle/tensor/logic.py
+++ ivy/functional/frontends/paddle/tensor/logic.py
@@ -1,2 +1,35 @@
 # local
-from ..logic import *  # noqa: F401
+import ivy
+import ivy.functional.frontends.paddle
+ivy.set_backends("tensorflow")
+from ivy.func_wrapper import (
+    with_supported_dtypes,
+    with_unsupported_dtypes,
+)
+from ivy.functional.frontends.paddle.func_wrapper import to_ivy_arrays_and_back
+
+# Defines the `greater_equal` function with dtype support and Ivy array conversion
+@with_supported_dtypes(
+    {
+        "2.5.1 and below": (
+            "bool",
+            "float16",
+            "float32",
+            "float64",
+            "int32",
+            "int64",
+        )
+    },
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def greater_equal(x, y):
+    return ivy.greater_equal(x, y)
+
+# Example usage
+# Sample inputs to demonstrate the expected behavior
+# Result returns the truth value of x>=y, an ivy.array
+x = ivy.array([1, 2, 3])
+y = ivy.array([1, 3, 2])
+result = ivy.greater_equal(x, y)
+print(result)
