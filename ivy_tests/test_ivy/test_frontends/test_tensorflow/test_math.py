09a6f338d0ab81fd9d5c13863376896fa524a71e
Emmanuel Katchy <katchyemma@gmail.com>
2023-09-28 02:17:12 +0000

    Add tests for bessel_i0e

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -449,6 +449,37 @@ def test_tensorflow_atanh(
     )
 
 
+# bessel_i0e
+@handle_frontend_test(
+    fn_tree="tensorflow.math.bessel_i0e",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_bessel_i0e(
+    *,
+    dtype_and_x,
+    frontend,
+    test_flags,
+    fn_tree,
+    backend_fw,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+    )
+
+
 # bincount
 @handle_frontend_test(
     fn_tree="tensorflow.math.bincount",
