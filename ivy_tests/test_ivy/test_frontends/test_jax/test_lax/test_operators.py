8fb9cb623ec7a4118de3af6fa3ca17697a7073cf
Oluyele Sunday Anthony <36515560+Tonycrux@users.noreply.github.com>
2023-09-29 16:55:00 +0100

    feat: implement complex operator in jax.lax (#26141)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_lax/test_operators.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_lax/test_operators.py
@@ -1114,6 +1114,38 @@ def test_jax_clamp(
     )
 
 
+# complex
+@handle_frontend_test(
+    fn_tree="jax.lax.complex",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("numeric"),
+        num_arrays=2,
+        shared_dtype=True,
+    ),
+    test_with_out=st.just(False),
+)
+def test_jax_complex(
+    *,
+    dtype_and_x,
+    on_device,
+    fn_tree,
+    frontend,
+    test_flags,
+    backend_fw,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        y=x[1],
+    )
+
+
 # concat
 @handle_frontend_test(
     fn_tree="jax.lax.concatenate",
