7d326126c8d6ab5a5991a4def0ac5e74fa81d68a
vedant2003 <97391064+jgyfutub@users.noreply.github.com>
2023-09-29 23:45:15 +0530

    Update test_layers.py

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_keras/test_layers.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_keras/test_layers.py
@@ -1,23 +1,21 @@
 # global
+import numpy as np
 from hypothesis import strategies as st
+import ivy
 
 # local
+import ivy
 from hypothesis import strategies as st
 import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
-
 @handle_frontend_test(
-    fn_tree="tensorflow.keras.layers.flatten",
+    fn_tree="tensorflow.keras.layers.Flatten",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"),
-        min_num_dims=3,
-        max_num_dims=5,
-        min_dim_size=1,
-        max_dim_size=4,
+    available_dtypes=helpers.get_dtypes("valid"),
     ),
-    test_with_out=st.just(False),
-)
+     test_with_out=st.just(False)
+     )
 def test_tensorflow_layers_flatten(
     *,
     dtype_and_x,
@@ -27,7 +25,8 @@ def test_tensorflow_layers_flatten(
     test_flags,
     backend_fw,
 ):
-    input_dtype, x = dtype_and_x
+    input_dtype, x  = dtype_and_x
+    print(x)
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
@@ -35,5 +34,5 @@ def test_tensorflow_layers_flatten(
         test_flags=test_flags,
         fn_tree=fn_tree,
         on_device=on_device,
-        x=x[0],
-    )
+        x=x,
+        )
