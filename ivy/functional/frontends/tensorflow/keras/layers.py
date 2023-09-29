9bcb69a27a7dbb78dc5c90c4ef3441985f45331b
vedant2003 <97391064+jgyfutub@users.noreply.github.com>
2023-09-29 23:42:18 +0530

    Update layers.py

--- ivy/functional/frontends/tensorflow/keras/layers.py
+++ ivy/functional/frontends/tensorflow/keras/layers.py
@@ -1,20 +1,7 @@
 import ivy
-from ivy.func_wrapper import to_ivy_arrays_and_back, handle_array_function
-
+from ivy.func_wrapper import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
-@handle_array_function
-def flatten(x, /, *, copy=None, start_dim=0, end_dim=-1, order="C", out=None):
-    ivy.utils.assertions.check_elem_in_list(
-        order,
-        ["C", "F", "A", "K"],
-        message="order must be one of 'C', 'F', 'A', or 'K'",
-    )
-    if order in ["K", "A", "F"]:
-        return ivy.flatten(
-            x, copy=copy, start_dim=start_dim, end_dim=end_dim, order="F", out=out
-        )
-    else:
-        return ivy.flatten(
-            x, copy=copy, start_dim=start_dim, end_dim=end_dim, order="C", out=out
-        )
+def Flatten(x):
+    flattened=ivy.flatten(x,copy=None,start_dim=0,end_dim=-1,order="C",out=None)
+    return flattened
