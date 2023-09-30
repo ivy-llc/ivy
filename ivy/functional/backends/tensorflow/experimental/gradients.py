86f7f6a0d9714f47aee531c78de319ce81d6bf8e
Yog Dharaskar <yogpradip@gmail.com>
2023-09-30 12:47:55 +0530

    Implement VJP and JVP for TensorFlow backend

--- ivy/functional/backends/tensorflow/experimental/gradients.py
+++ ivy/functional/backends/tensorflow/experimental/gradients.py
@@ -1,5 +1,6 @@
 # global
 import tensorflow as tf
+from typing import Callable
 
 # local
 import ivy
@@ -19,3 +20,43 @@ def bind_custom_gradient_function(func, custom_grad_fn):
         return ivy.to_native((ret, grad), nested=True, include_derived=True)
 
     return inputs_to_native_arrays(custom_module)
+
+
+def vjp(func: Callable, *primals):
+    def grad_fn(*x_in):
+        return ivy.to_native(
+            func(*ivy.to_ivy(x_in, nested=True)), nested=True, include_derived=True
+        )
+
+    with tf.GradientTape(persistent=True, watch_accessed_variables=False) as tape:
+        tape.watch(ivy.to_native(primals, nested=True))
+        primals_out = grad_fn(*ivy.to_native(primals, nested=True))
+
+    def vjpfun(x_in):
+        grads = tape.gradient(
+            primals_out,
+            ivy.to_native(primals, nested=True),
+            output_gradients=ivy.to_native(x_in, nested=True),
+        )
+        return ivy.to_ivy(grads, nested=True, include_derived=True)
+
+    return (ivy.to_ivy(primals_out, nested=True, include_derived=True), vjpfun)
+
+
+def jvp(func: Callable, primals, tangents):
+    def grad_fn(x_in):
+        return ivy.to_native(
+            func(ivy.to_ivy(x_in, nested=True)), nested=True, include_derived=True
+        )
+
+    primals = ivy.to_native(primals, nested=True)
+    tangents = ivy.to_native(tangents, nested=True)
+
+    with tf.autodiff.ForwardAccumulator(
+        primals,
+        tangents,
+    ) as acc:
+        primals_out = grad_fn(*primals)
+    tangents_out = acc.jvp(primals_out)
+
+    return ivy.to_ivy((primals_out, tangents_out), nested=True, include_derived=True)
