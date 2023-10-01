17f21798cb56e7ee4a01ac9ccdb8901a6982b528
Dheeraj <dheerajbal.18@gmail.com>
2023-10-01 13:35:09 +0530

    Added  ldl_factor fuction to torch frontend in linalg.py file.

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -403,3 +403,12 @@ def vector_norm(input, ord=2, dim=None, keepdim=False, *, dtype=None, out=None):
     return ivy.vector_norm(
         input, axis=dim, keepdims=keepdim, ord=ord, out=out, dtype=dtype
     )
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes(
+    {"2.0.1 and below": ("float32", "float64", "complex32", "complex64")}, "torch"
+)
+def ldl_factor(A, *, hermitian=False, out=None):
+    return ivy.ldl_factor(
+        A, hermitian=hermitian, out=out
+    )
