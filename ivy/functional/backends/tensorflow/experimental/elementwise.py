fix: simplify paddle backend implementation of `gradient`

ivy/functional/backends/tensorflow/experimental/elementwise.py
@@ -294,7 +294,7 @@ def gradient(
             if distances.dtype.is_integer:
                 # Convert numpy integer types to float64 to avoid modular
                 # arithmetic in np.diff(distances).
-                distances = distances.astype(tf.experimental.numpy.float64)
+                distances = tf.cast(distances, tf.float64)
             diffx = tf.experimental.numpy.diff(distances)
             # if distances are constant reduce to the scalar case
             # since it brings a consistent speedup
@@ -323,7 +323,7 @@ def gradient(
     slice4 = [slice(None)] * N
 
     if x.dtype.is_integer:
-        x = x.astype(tf.experimental.numpy.float64)
+        x = tf.cast(x, tf.float64)
 
     for axis, ax_dx in zip(axes, dx):
         if x.shape[axis] < edge_order + 1: