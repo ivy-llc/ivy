a33ba592df686b539734591bc65cedfc8df5aa30
imsoumya18 <soumyadeep184@gmail.com>
2023-10-02 12:11:46 +0530

    started writing test, will change it

--- ivy/functional/frontends/paddle/manipulation.py
+++ ivy/functional/frontends/paddle/manipulation.py
@@ -92,7 +92,29 @@ def gather_nd(x, index, name=None):
 )
 @to_ivy_arrays_and_back
 def index_add(x, index, axis, value, name=None):
-    return ivy.index_add(x, index, axis, value)
+    input = ivy.swapaxes(x, axis, 0)
+    source = ivy.swapaxes(value, axis, 0)
+    _to_adds = []
+    index = sorted(zip(ivy.to_list(index), range(len(index))), key=(lambda x: x[0]))
+    while index:
+        _curr_idx = index[0][0]
+        while len(_to_adds) < _curr_idx:
+            _to_adds.append(ivy.zeros_like(source[0]))
+        _to_add_cum = ivy.get_item(source, index[0][1])
+        while (1 < len(index)) and (index[0][0] == index[1][0]):
+            _to_add_cum = _to_add_cum + ivy.get_item(source, index.pop(1)[1])
+        index.pop(0)
+        _to_adds.append(_to_add_cum)
+    while len(_to_adds) < input.shape[0]:
+        _to_adds.append(ivy.zeros_like(source[0]))
+    _to_adds = ivy.stack(_to_adds)
+    if len(input.shape) < 2:
+        # Added this line due to the paddle backend treating scalars as 1-d arrays
+        _to_adds = ivy.flatten(_to_adds)
+
+    ret = ivy.add(input, _to_adds, alpha=1)
+    ret = ivy.swapaxes(ret, 0, axis, out=None)
+    return ret
 
 
 @to_ivy_arrays_and_back
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_manipulation.py
@@ -471,6 +471,36 @@ def test_paddle_gather_nd(
     )
 
 
+@handle_frontend_test(
+    fn_tree="paddle.index_add",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"), num_arrays=1
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+)
+def test_paddle_index_add(
+    *,
+    dtype_and_x,
+    dtype,
+    on_device,
+    backend_fw,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        dtype=dtype[0],
+    )
+
+
 # repeat_interleave
 @handle_frontend_test(
     fn_tree="paddle.repeat_interleave",
