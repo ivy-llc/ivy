c0188ca57ef493e2eaca2d75c5fc4ed4e3c552f2
Dheeraj <dheerajbal.18@gmail.com>
2023-10-03 12:56:14 +0530

    changes in frontend

--- ivy/functional/frontends/torch/linalg.py
+++ ivy/functional/frontends/torch/linalg.py
@@ -134,19 +134,6 @@ def inv_ex(A, *, check_errors=False, out=None):
 def ldl_factor(A, *, hermitian=False, out=None):
     return ivy.ldl_factor(A, hermitian=hermitian, out=out)
 
-
-@to_ivy_arrays_and_back
-@with_supported_dtypes({"2.0.1 and below": ("float", "complex")}, "torch")
-def ldl_factor(A, *, hermitian=False, out=None):
-    return ivy.ldl_factor(A, hermitian=hermitian, out=out)
-
-
-@to_ivy_arrays_and_back
-@with_supported_dtypes({"2.0.1 and below": ("numeic", "float", "complex")}, "torch")
-def ldl_factor(A, *, hermitian=False, out=None):
-    return ivy.ldl_factor(A, hermitian=hermitian, out=out)
-
-
 @to_ivy_arrays_and_back
 @with_supported_dtypes(
     {"2.0.1 and below": ("float32", "float64", "complex32", "complex64")}, "torch"
