1ac922e9deb8fdbec1f081e5ac9d45a4d122a8d9
Mutahir <mutahiralileghari@gmail.com>
2023-10-04 01:58:42 +0500

    feat: add ones initializer in general function tensorflow (#26539)

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -329,6 +329,15 @@ def ones(shape, dtype=ivy.float32, name=None):
     return ivy.ones(shape, dtype=dtype)
 
 
+@with_unsupported_dtypes({"2.14.0 and below": ("float16", "bfloat16")}, "tensorflow")
+@to_ivy_arrays_and_back
+def ones_initializer(shape, dtype=None, name=None):
+    # todo internal: fix behaviour
+    if dtype is None:
+        dtype = ivy.default_dtype()
+    return ivy.ones(shape, dtype=dtype)
+
+
 @handle_tf_dtype
 @to_ivy_arrays_and_back
 def ones_like(input, dtype=None, name=None):
