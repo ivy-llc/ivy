a2704324266512427c94324b44a2945b46e9b1a6
imsoumya18 <soumyadeep184@gmail.com>
2023-10-03 21:37:48 +0530

    test

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_manipulation.py
@@ -474,13 +474,30 @@ def test_paddle_gather_nd(
 @handle_frontend_test(
     fn_tree="paddle.index_add",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid"), num_arrays=1
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=3,
+        max_num_dims=3,
+        num_arrays=3,
+    ),
+    dtype_and_x2=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=2,
+        max_num_dims=2,
+        num_arrays=1,
+    ),
+    dtype_and_x3=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=3,
+        max_num_dims=3,
+        num_arrays=2,
     ),
     dtype=helpers.get_dtypes("valid", full=False),
 )
 def test_paddle_index_add(
     *,
     dtype_and_x,
+    dtype_and_x2,
+    dtype_and_x3,
     dtype,
     on_device,
     backend_fw,
@@ -489,6 +506,8 @@ def test_paddle_index_add(
     test_flags,
 ):
     input_dtype, x = dtype_and_x
+    index = dtype_and_x2[1]
+    value = dtype_and_x3[1]
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
@@ -497,6 +516,9 @@ def test_paddle_index_add(
         fn_tree=fn_tree,
         on_device=on_device,
         x=x[0],
+        index=index[0],
+        axis=0,
+        value=value[0],
         dtype=dtype[0],
     )
 
