d7c09e81bd864be4e8ebaf783d7e60273f16b4af
Bhargavoza1 <66210089+Bhargavoza1@users.noreply.github.com>
2023-10-04 16:19:19 +0530

    feat(frontend): add diagonal_scatter to PyTorch frontend  (#24377)

--- ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
+++ ivy/functional/frontends/torch/indexing_slicing_joining_mutating_ops.py
@@ -6,6 +6,7 @@ from ivy.functional.frontends.torch.func_wrapper import (
     numpy_to_torch_style_args,
     to_ivy_shape,
 )
+from ivy.func_wrapper import with_unsupported_dtypes
 
 
 @to_ivy_arrays_and_back
@@ -68,6 +69,35 @@ def concat(tensors, dim=0, *, out=None):
 @to_ivy_arrays_and_back
 def conj(input):
     return ivy.conj(input)
+
+
+# diagonal_scatter
+@with_unsupported_dtypes(
+    {
+        "2.0.1 and below": (
+            "bfloat16",
+            "float16",
+        )
+    },
+    "torch",
+)
+@to_ivy_arrays_and_back
+def diagonal_scatter(input, src, offset=0, dim1=0, dim2=1):
+    input = ivy.copy_array(input)
+    input_shape = input.shape
+    indices = ivy.arange(0, input.size)
+    diagonal_indices = ivy.diagonal(
+        indices.reshape(input.shape), offset=offset, axis1=dim1, axis2=dim2
+    )
+    if not (src.shape == diagonal_indices.shape):
+        raise ivy.utils.exceptions.IvyException(
+            "src must have shape equal to specified diagonal of input. src size ="
+            f" {src.shape}, diagonal size = {diagonal_indices.shape}"
+        )
+    input = input.reshape((-1,))
+    input[diagonal_indices.reshape((-1,))] = src.reshape((-1,))
+    input = input.reshape(input_shape)
+    return input
 
 
 @to_ivy_arrays_and_back
