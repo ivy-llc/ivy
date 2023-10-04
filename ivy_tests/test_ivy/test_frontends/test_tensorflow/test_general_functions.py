3733727d82d7b5d7a9d37519829ef06defe13a16
Mutahir <mutahiralileghari@gmail.com>
2023-10-04 23:52:48 +0500

    feat: add test case for ones initializer

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_general_functions.py
@@ -1363,6 +1363,40 @@ def test_tensorflow_ones_like(
     )
 
 
+# ones initializer
+@handle_frontend_test(
+    fn_tree="tensorflow.ones_initializer",
+    shape=helpers.get_shape(
+        allow_none=False,
+        min_num_dims=1,
+        max_num_dims=10,
+        min_dim_size=1,
+        max_dim_size=5,
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+    test_with_out=st.just(False),
+)
+def test_tensorflow_ones_initializer(
+    shape,
+    dtype,
+    frontend,
+    backend_fw,
+    test_flags,
+    fn_tree,
+    on_device,
+):
+    helpers.test_frontend_function(
+        shape=shape,
+        dtype=dtype[0],
+        input_dtypes=dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+    )
+
+
 # pad
 @handle_frontend_test(
     fn_tree="tensorflow.pad",
