50182744e5bf0795307712e821d8447153426e6d
Shreyansh Bardia <104841983+ShreyanshBardia@users.noreply.github.com>
2023-10-04 20:12:52 +0530

    fix: added missing backend_fw arg to test_torch_view_as_real (#26586)
    Co-authored-by: @AnnaTz

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -1814,10 +1814,12 @@ def test_torch_view_as_real(
     fn_tree,
     frontend,
     test_flags,
+    backend_fw,
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
         frontend=frontend,
         test_flags=test_flags,
         fn_tree=fn_tree,
