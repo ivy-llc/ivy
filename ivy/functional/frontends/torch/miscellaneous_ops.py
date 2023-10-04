3223d03c5dd2bf68a6f87ea1e49112a0de40c2e1
teckno <mwapebt@gmail.com>
2023-10-04 13:50:30 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/unifyai/ivy""

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -1086,7 +1086,6 @@ def view_as_real(input):
     return ivy.corrcoef(input, y=None, rowvar=True)
 
 
-
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.0.1 and below": ("float32", "float64")}, "torch")
 def cdist(x1, x2, p=2.0, compute_mode='use_mm_for_euclid_dist_if_necessary'):
