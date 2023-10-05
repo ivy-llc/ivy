bd421a4ad1d7c167a2515ca0bbb4f0a2734c7fea
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2023-10-05 16:11:04 +0530

    update(ivy): updated jax version to 0.4.17 (#26639)

--- ivy/functional/frontends/jax/numpy/mathematical_functions.py
+++ ivy/functional/frontends/jax/numpy/mathematical_functions.py
@@ -67,7 +67,7 @@ def around(a, decimals=0, out=None):
 
 
 @with_unsupported_dtypes(
-    {"0.4.16 and below": ("bfloat16",)},
+    {"0.4.17 and below": ("bfloat16",)},
     "jax",
 )
 @to_ivy_arrays_and_back
@@ -220,7 +220,7 @@ def expm1(
 
 
 @with_unsupported_dtypes(
-    {"0.4.16 and below": ("uint16",)},
+    {"0.4.17 and below": ("uint16",)},
     "jax",
 )
 @to_ivy_arrays_and_back
@@ -390,7 +390,7 @@ def minimum(x1, x2, /):
 
 
 @to_ivy_arrays_and_back
-@with_unsupported_dtypes({"0.4.16 and below": ("complex",)}, "jax")
+@with_unsupported_dtypes({"0.4.17 and below": ("complex",)}, "jax")
 def mod(x1, x2, /):
     x1, x2 = promote_types_of_jax_inputs(x1, x2)
     return ivy.remainder(x1, x2)
@@ -432,7 +432,7 @@ def negative(
 
 @with_unsupported_dtypes(
     {
-        "0.4.16 and below": (
+        "0.4.17 and below": (
             "bfloat16",
             "float16",
         )
@@ -477,7 +477,7 @@ def polyadd(a1, a2):
 
 
 @with_unsupported_dtypes(
-    {"0.4.16 and below": ("float16",)},
+    {"0.4.17 and below": ("float16",)},
     "jax",
 )
 @to_ivy_arrays_and_back
@@ -519,7 +519,7 @@ def polydiv(u, v, *, trim_leading_zeros=False):
 
 
 @with_unsupported_dtypes(
-    {"0.4.16 and below": ("float16",)},
+    {"0.4.17 and below": ("float16",)},
     "jax",
 )
 @to_ivy_arrays_and_back
