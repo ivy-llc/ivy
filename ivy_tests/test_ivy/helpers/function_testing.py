7afa26252f43e23b479eb9de6e75c86ada26ee0a
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-10-05 17:02:14 +0100

    fix(tests): simplify check for non frontend arrays found in a returned tuple

--- ivy_tests/test_ivy/helpers/function_testing.py
+++ ivy_tests/test_ivy/helpers/function_testing.py
@@ -643,7 +643,7 @@ def _assert_frontend_ret(ret, for_fn=True):
     if is_ret_tuple:
         non_frontend_idxs = ivy.nested_argwhere(
             ret,
-            lambda _x: _is_frontend_array(_x) if ivy.is_array(_x) else True,
+            lambda _x: not _is_frontend_array(_x),
         )
         assert not non_frontend_idxs, (
             f"Frontend {fn_or_method} return contains non-frontend arrays at positions"
