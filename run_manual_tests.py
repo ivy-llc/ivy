900687cf3ccc76cd1a24b41eff3e7752de04c851
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2023-10-05 17:20:09 +0530

    fix(ci): remove whitespace from backend and avoid generating output logs for debugging

--- run_manual_tests.py
+++ run_manual_tests.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
             backends = ["all"]
             test_arg = line.split(",")
             if len(test_arg) > 1:
-                backends = [test_arg[1]]
+                backends = [test_arg[1].strip()]
             if backends[0] == "all":
                 backends = ["numpy", "jax", "tensorflow", "torch", "paddle"]
 
@@ -162,7 +162,6 @@ if __name__ == "__main__":
                     }
                 test_info["results"] = {frontend_version: test_info["results"]}
 
-            json.dump({"$set": test_info}, open("output.json", "w"))
             id = test_info.pop("_id")
             print(collection.update_one({"_id": id}, {"$set": test_info}, upsert=True))
 
