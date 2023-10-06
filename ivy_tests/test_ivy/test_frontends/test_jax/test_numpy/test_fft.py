561fb948a20283db75d935e225be01860b7cea1b
zerodoxxx <nbhatnagar3010@gmail.com>
2023-10-06 10:41:29 +0530

    Fixed lint error for ifftshift test

--- ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_fft.py
@@ -6,6 +6,31 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_frontend_test
 
 
+@handle_frontend_test(
+    fn_tree="jax.numpy.fft.ifftshift",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_value=-10,
+        max_value=10,
+        min_num_dims=1,
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+)
+def test_jax_ifftshift(dtype_x_axis, frontend, test_flags, fn_tree, on_device):
+    input_dtype, x, axes = dtype_x_axis
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        x=x[0],
+        axes=axes,
+    )
+
+
 # fft
 @handle_frontend_test(
     fn_tree="jax.numpy.fft.fft",
