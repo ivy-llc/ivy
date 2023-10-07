279fb545a1adccf7ac6aa25f9a374ad5fa74f14c
samunder singh <83540902+samthakur587@users.noreply.github.com>
2023-10-07 15:02:26 +0000

    updated the sigmoid on tf backend

--- ivy/functional/backends/tensorflow/experimental/activations.py
+++ ivy/functional/backends/tensorflow/experimental/activations.py
@@ -73,18 +73,18 @@ def silu(
 
 
 @with_supported_dtypes({"2.14.0 and below": ("float",)}, backend_version)
-def elu(x: Tensor, /, *, alpha: float = 1.0, out: Optional[Tensor] = None) -> Tensor:
-    # alpha = tf.cast(alpha, x.dtype)
-    # ret = tf.cast(tf.where(x > 0, x, tf.multiply(alpha, tf.math.expm1(x))), x.dtype)
-    ret = tf.keras.layers.ELU(alpha)(x)
+def sigmoid(x: Tensor, /, *, out: Optional[Tensor] = None) -> Tensor:
+    ret = tf.nn.sigmoid(x)
     if ivy.exists(out):
         return ivy.inplace_update(out, ret).astype(x.dtype)
     return ivy.astype(ret, x.dtype)
 
 
 @with_supported_dtypes({"2.14.0 and below": ("float",)}, backend_version)
-def sigmoid(x: Tensor, /, *, out: Optional[Tensor] = None) -> Tensor:
-    ret = tf.nn.sigmoid(x)
+def elu(x: Tensor, /, *, alpha: float = 1.0, out: Optional[Tensor] = None) -> Tensor:
+    # alpha = tf.cast(alpha, x.dtype)
+    # ret = tf.cast(tf.where(x > 0, x, tf.multiply(alpha, tf.math.expm1(x))), x.dtype)
+    ret = tf.keras.layers.ELU(alpha)(x)
     if ivy.exists(out):
         return ivy.inplace_update(out, ret).astype(x.dtype)
     return ivy.astype(ret, x.dtype)
