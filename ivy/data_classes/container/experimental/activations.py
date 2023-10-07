e1afcbff065ee9cb78170b1ee5448016a346f7e6
Kareem Morsy <kreem.morsy@hotmail.com>
2023-10-07 13:43:10 +0300

    Update ivy/data_classes/container/experimental/activations.py

--- ivy/data_classes/container/experimental/activations.py
+++ ivy/data_classes/container/experimental/activations.py
@@ -1263,20 +1263,20 @@ class _ContainerWithActivationExperimental(ContainerBase):
 
         Parameters
         ----------
-        x :
+        x
             Input container.
-        lambd :
+        lambd
             Lambda value for soft shrinkage calculation.
-        key_chains :
+        key_chains
             The key-chains to apply or not apply the method to.
-        to_apply :
+        to_apply
             If True, the method will be applied to key_chains, otherwise key_chains
             will be skipped.
-        prune_unapplied :
+        prune_unapplied
             Whether to prune key_chains for which the function was not applied.
-        map_sequences :
+        map_sequences
             Whether to also map method to sequences (lists, tuples).
-        out :
+        out
             optional output container, for writing the result to. It must have a shape
             that the inputs broadcast to.
 
