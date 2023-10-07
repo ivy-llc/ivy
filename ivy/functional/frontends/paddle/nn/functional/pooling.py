d2996838c98a586d9b1f32be3eb6f3adea23fa3f
Neha <psxng4@nottingham.ac.uk>
2023-10-07 18:31:21 +0100

    Adding Average Pool 3D function to paddle

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -102,6 +102,63 @@ def avg_pool2d(
 
 @to_ivy_arrays_and_back
 @with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
+def avg_pool3d(
+    x,
+    kernel_size,
+    stride=None,
+    padding=0,
+    ceil_mode=False,
+    exclusive=True,
+    divisor_override=None,
+    data_format="NCDHW",
+    name=None,
+):
+    """
+    Applies 3D average pooling over the input tensor.
+
+    Parameters:
+    - x (Tensor): Input tensor.
+    - kernel_size (int|list|tuple): Size of the pooling kernel.
+    - stride (int|list|tuple, optional): Stride size for pooling.
+    - padding (int|str|list|tuple, optional): Padding size or type.
+    - ceil_mode (bool, optional): Whether to use ceil or floor for output
+    shape calculation.
+    - exclusive (bool, optional): Whether to exclude padding points in average
+    pooling.
+    - divisor_override (int|float, optional): If specified, it will be used as
+    divisor.
+    - data_format (str, optional): Format of the input data.
+    - name (str, optional): Name for the operation.
+
+    Returns:
+    - Tensor: A 5-D tensor after applying 3D average pooling.
+    """
+    if stride is None:
+        stride = kernel_size
+    kernel_size = _broadcast_pooling_helper(kernel_size, "3d", name="kernel_size")
+    padding = _broadcast_pooling_helper(padding, "3d", name="padding")
+    if all(
+        [pad == ivy.ceil((kernel - 1) / 2) for kernel, pad in zip(kernel_size, padding)]
+    ):
+        padding = "SAME"
+    else:
+        padding = "VALID"
+
+    count_include_pad = not exclusive
+    return ivy.avg_pool3d(
+        x,
+        kernel_size,
+        stride,
+        padding,
+        data_format=data_format,
+        count_include_pad=count_include_pad,
+        ceil_mode=ceil_mode,
+        divisor_override=divisor_override,
+    )
+
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.5.1 and below": ("float32", "float64")}, "paddle")
 def max_unpool1d(
     x,
     indices,
