ca1ef7da0be65761ae8713d5e5e410352ed6ce05
muhammadhammad-tech <75118771+muhammadhammad-tech@users.noreply.github.com>
2023-10-07 14:03:24 +0000

    heaviside Fixes #26492

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -885,3 +885,9 @@ class Tensor:
     def gather_(self, y, name=None):
         res = self.gather(self, y)
         return ivy.inplace_update(self, res)
+
+    @with_supported_dtypes(
+        {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle"
+    )
+    def heaviside(self, y, name=None):
+        return paddle_frontend.heaviside(self, y)
