da87dbae0b4cf88b607a29014a40b5d5570feeb1
teckno <mwapebt@gmail.com>
2023-10-08 00:44:22 +0200

    Cdist_Frontend

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -541,3 +541,16 @@ def view_as_real(input):
     re_part = ivy.real(input)
     im_part = ivy.imag(input)
     return ivy.stack((re_part, im_part), axis=-1)
+
+@to_ivy_arrays_and_back
+@with_supported_dtypes({"2.0.1 and below": ("float32", "float64")}, "torch")
+def cdist(x1, x2, p=2.0, compute_mode='use_mm_for_euclid_dist_if_necessary'):
+    pairwise_diff = x1[:, :, None] - x2[:, None, :]
+    if p == 2.0:
+        if compute_mode == 'use_mm_for_euclid_dist_if_necessary' and (pairwise_diff.shape[1] > 25 or pairwise_diff.shape[2] > 25):
+            distances = ivy.vector_norm(pairwise_diff, axis=-1)
+        else:
+            distances = ivy.vector_norm(pairwise_diff, axis=-1)
+    else:
+        distances = ivy.vector_norm(pairwise_diff, ord=p, axis=-1)
+    return distances
