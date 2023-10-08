1ba0eab9c6dcf21dab97ff7d417406e8951aeb47
imsoumya18 <soumyadeep184@gmail.com>
2023-10-08 10:59:51 +0530

    index_add_ issue #26393

--- ivy/functional/frontends/paddle/tensor/manipulation.py
+++ ivy/functional/frontends/paddle/tensor/manipulation.py
@@ -6,6 +6,39 @@ from ivy.functional.frontends.paddle.func_wrapper import (
 )
 from ivy.func_wrapper import with_unsupported_dtypes
 
+
+@with_supported_dtypes(
+    {"2.5.1 and below": ("int32", "int64", "float16", "float32", "float64")},
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def index_add_(x, index, axis, value, name=None):
+    input = ivy.swapaxes(x, axis, 0)
+    source = ivy.swapaxes(value, axis, 0)
+    _to_adds = []
+    index = sorted(zip(ivy.to_list(index), range(len(index))), key=(lambda x: x[0]))
+    while index:
+        _curr_idx = index[0][0]
+        while len(_to_adds) < _curr_idx:
+            _to_adds.append(ivy.zeros_like(source[0]))
+        _to_add_cum = ivy.get_item(source, index[0][1])
+        while (1 < len(index)) and (index[0][0] == index[1][0]):
+            _to_add_cum = _to_add_cum + ivy.get_item(source, index.pop(1)[1])
+        index.pop(0)
+        _to_adds.append(_to_add_cum)
+    while len(_to_adds) < input.shape[0]:
+        _to_adds.append(ivy.zeros_like(source[0]))
+    _to_adds = ivy.stack(_to_adds)
+    if len(input.shape) < 2:
+        # Added this line due to the paddle backend treating scalars as 1-d arrays
+        _to_adds = ivy.flatten(_to_adds)
+
+    ret = ivy.add(input, _to_adds, alpha=1)
+    ret = ivy.swapaxes(ret, 0, axis, out=None)
+    x = ret
+    return x
+
+
 # NOTE:
 # Only inplace functions are to be added in this file.
 # Please add non-inplace counterparts to `/frontends/paddle/manipulation.py`.
--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_manipulation.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_manipulation.py
@@ -25,6 +25,58 @@ def dtypes_x_reshape_(draw):
     return dtypes, x, shape
 
 
+@handle_frontend_test(
+    fn_tree="paddle.tensor.manipulation.index_add_",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=3,
+        max_num_dims=3,
+        num_arrays=3,
+    ),
+    dtype_and_x2=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=2,
+        max_num_dims=2,
+        num_arrays=1,
+    ),
+    dtype_and_x3=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        min_num_dims=3,
+        max_num_dims=3,
+        num_arrays=2,
+    ),
+    dtype=helpers.get_dtypes("valid", full=False),
+)
+def test_paddle_index_add(
+    *,
+    dtype_and_x,
+    dtype_and_x2,
+    dtype_and_x3,
+    dtype,
+    on_device,
+    backend_fw,
+    fn_tree,
+    frontend,
+    test_flags,
+):
+    input_dtype, x = dtype_and_x
+    index = dtype_and_x2[1]
+    value = dtype_and_x3[1]
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x[0],
+        index=index[0],
+        axis=0,
+        value=value[0],
+        dtype=dtype[0],
+    )
+
+
 # reshape_
 @handle_frontend_test(
     fn_tree="paddle.tensor.manipulation.reshape_",
