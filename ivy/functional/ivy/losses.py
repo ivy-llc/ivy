d67e60a73de9555b13fdb76e05eb65bde8fffa09
MarkCodering <mark.chen.sstm@gmail.com>
2023-10-08 20:55:59 +0800

    remove decorator

--- ivy/functional/ivy/losses.py
+++ ivy/functional/ivy/losses.py
@@ -380,7 +380,6 @@ def sparse_cross_entropy(
         true, pred, axis=axis, epsilon=epsilon, reduction=reduction, out=out
     )
 
-@handle_array_function
 def nll_loss(true, pred, epsilon=1e-7):
     """
     Calculate the Negative Log Likelihood (NLL) loss.
