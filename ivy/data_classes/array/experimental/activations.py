e79957bb10af5a4de91ac21eab1d0fa0e06aa08f
akshatvishu <33392262+akshatvishu@users.noreply.github.com>
2023-10-12 16:37:26 +0530

    feat(activations): add scaled_tanh(stanh) to ivy experimental API. (#26841)

--- ivy/data_classes/array/experimental/activations.py
+++ ivy/data_classes/array/experimental/activations.py
@@ -389,3 +389,53 @@ class _ArrayWithActivationsExperimental(abc.ABC):
         ivy.array([ 0.39, -0.57])
         """
         return ivy.celu(self._data, alpha=alpha, complex_mode=complex_mode, out=out)
+
+    def scaled_tanh(
+        self: ivy.Array,
+        /,
+        *,
+        alpha: float = 1.7159,
+        beta: float = 0.67,
+        out: Optional[ivy.Array] = None,
+    ) -> ivy.Array:
+        """
+        ivy.Array instance method variant of ivy.scaled_tanh. This method simply wraps
+        the function, and so the docstring for ivy.scaled_tanh also applies to this
+        method with minimal changes.
+
+        Parameters
+        ----------
+        self
+            input array.
+        alpha
+            The scaling parameter for the output.
+            Determines the amplitude of the tanh function.
+            Default: 1.7159
+        beta
+            The scaling parameter for the input.
+            Determines the slope of the tanh function.
+            Default: 0.67
+        out
+            optional output array, for writing the result to. It must have a shape
+            that the inputs broadcast to.
+
+        Returns
+        -------
+        ret
+            an array after applying the scaled_tanh activation.
+
+        Examples
+        --------
+        >>> x = ivy.array([-3., 2., 3.])
+        >>> x.scaled_tanh()
+        ivy.array([-1.65537548,  1.49570239,  1.65537548])
+
+        >>> x = ivy.array([2., 2., 2.])
+        >>> x.scaled_tanh(alpha=9, beta=0.1)
+        ivy.array([1.77637792, 1.77637792, 1.77637792])
+
+        >>> x = ivy.array([2., 2., 2.])
+        >>> x.scaled_tanh(alpha=0.1, beta=9)
+        ivy.array([0.1, 0.1, 0.1])
+        """
+        return ivy.scaled_tanh(self._data, alpha=alpha, beta=beta, out=out)
