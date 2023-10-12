0e5ab03479c0fc23b9a0d24250297d74a61fa6e1
mintesin <50635173+mintesin@users.noreply.github.com>
2023-10-12 14:40:35 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -357,16 +357,13 @@ def test_paddle_diff(
     helpers.test_frontend_method(
         init_input_dtypes=input_dtype,
         backend_to_test=backend_fw,
-        init_all_as_kwargs_np={
-            "data": x[0],
-        },
         method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={"y": x[1]},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
         method_flags=method_flags,
         frontend=frontend,
         on_device=on_device,
+        x=x[0]
     )
 
 
