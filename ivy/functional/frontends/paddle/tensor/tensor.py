9f93295bf685fdddfa5d375da4aa55023e5ddb90
mintesin <50635173+mintesin@users.noreply.github.com>
2023-10-12 12:42:12 +0530

    argument correction

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -551,8 +551,8 @@ class Tensor:
         },
         "paddle",
     )
-    def diff(self_, y, n=1, axis=-1, prepend=None, append=None, name=None):
-        self_ = paddle_frontend.diff(self_, y)
+    def diff(self_,n=1, axis=-1, prepend=None, append=None, name=None):
+        self_ = paddle_frontend.diff(self_)
         return self_
 
     @with_unsupported_dtypes(
