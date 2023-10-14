f967ccbd36ae2bb6bddbf77261b0c1cac771e775
Subhodip Ghosh <macy16661@gmail.com>
2023-10-14 20:57:42 +0530

    prev

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -900,27 +900,3 @@ class Tensor:
     )
     def to_dense(self):
         return paddle_frontend.Tensor(ivy.to_dense_array(self))
-
-    @with_unsupported_dtypes(
-        {"2.5.1 and below": ("int16", "complex64", "complex128")},
-        "paddle",
-    )
-    def split(self, num_or_sections, axis=0, name=None):
-        return paddle_frontend.split(self._ivy_array, num_or_sections, axis, name)
-
-    @with_supported_dtypes(
-        {"2.5.1 and below": ("float32", "float64", "int32", "int64")}, "paddle"
-    )
-    def frac(self, name=None):
-        return paddle_frontend.frac(self._ivy_array)
-
-    @with_unsupported_dtypes({"2.5.1 and below": ("float16", "bfloat16")}, "paddle")
-    def gather(self, y, name=None):
-        return paddle_frontend.gather(self, y)
-
-    @with_unsupported_dtypes(
-        {"2.5.1 and below": ("float16", "uint8", "int8", "bool")}, "paddle"
-    )
-    def gather_(self, y, name=None):
-        res = self.gather(self, y)
-        return ivy.inplace_update(self, res)
