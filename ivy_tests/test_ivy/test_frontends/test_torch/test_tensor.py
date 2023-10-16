589edc3f3f515ac85962b2a03c31a3140648b552
sharjeelnawaz8182 <chsharjeel8182@gmail.com>
2023-10-16 11:50:02 +0500

    added pytorch corrcoef instance method and test function

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -5601,11 +5601,11 @@ def test_torch_tensor_corrcoef(
 ):
     input_dtype, x = dtype_and_x
     helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
+        init_input_dtypes=["float64"],
         init_all_as_kwargs_np={
             "data": x[0],
         },
-        method_input_dtypes=[],
+        method_input_dtypes=input_dtype,
         method_all_as_kwargs_np={},
         frontend_method_data=frontend_method_data,
         init_flags=init_flags,
