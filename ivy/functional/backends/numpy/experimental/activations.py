91ff07954768244fb04f7122832f8036661d2141
samunder singh <83540902+samthakur587@users.noreply.github.com>
2023-10-16 20:00:56 +0530

    Update activations.py

--- ivy/functional/backends/numpy/experimental/activations.py
+++ ivy/functional/backends/numpy/experimental/activations.py
@@ -6,8 +6,6 @@ import numpy as np
 # local
 import ivy
 from ivy.functional.backends.numpy.helpers import _scalar_output_to_0d_array
-
-
 from ivy.func_wrapper import (
     with_unsupported_dtypes,
 )
