f8a04e1749e990738fbcd2b0e3f150ffd7243a2e
KevinUli <kevinuli11@gmail.com>
2023-10-17 22:39:44 -0700

    Added copy test flag for experimental API

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
@@ -505,6 +505,7 @@ def st_tuples(elements, *, min_size=0, max_size=None, unique_by=None, unique=Fal
     test_with_out=st.just(False),
     test_gradients=st.just(False),
     ground_truth_backend="numpy",
+    test_with_copy=st.just(True),
 )
 def test_as_strided(*, all_args, test_flags, backend_fw, fn_name, on_device):
     dtype, x, shape, strides = all_args
@@ -555,6 +556,7 @@ def test_associative_scan(
     ),
     test_with_out=st.just(False),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_atleast_1d(dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     input_dtypes, arrays = dtype_and_x
@@ -581,6 +583,7 @@ def test_atleast_1d(dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     ),
     test_with_out=st.just(False),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_atleast_2d(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     input_dtypes, arrays = dtype_and_x
@@ -607,6 +610,7 @@ def test_atleast_2d(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     ),
     test_with_out=st.just(False),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_atleast_3d(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     input_dtypes, arrays = dtype_and_x
@@ -694,6 +698,7 @@ def test_concat_from_sequence(
 # dsplit
 @handle_test(
     fn_tree="functional.ivy.experimental.dsplit",
+    test_with_copy=st.just(True),
 )
 def test_dsplit(
     dtype_and_x, indices_or_sections, test_flags, backend_fw, fn_name, on_device
@@ -763,6 +768,7 @@ def test_dstack(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     container_flags=st.just([False]),
     test_instance_method=st.just(False),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_expand(*, dtype_and_x, shape, test_flags, backend_fw, fn_name, on_device):
     dtype, x = dtype_and_x
@@ -824,6 +830,7 @@ def test_fill_diagonal(
 @handle_test(
     fn_tree="functional.ivy.experimental.flatten",
     data=_flatten_data_helper(),
+    test_with_copy=st.just(True),
 )
 def test_flatten(
     *,
@@ -855,6 +862,7 @@ def test_flatten(
         min_num_dims=2,
     ),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_fliplr(*, dtype_and_m, test_flags, backend_fw, fn_name, on_device):
     input_dtype, m = dtype_and_m
@@ -881,6 +889,7 @@ def test_fliplr(*, dtype_and_m, test_flags, backend_fw, fn_name, on_device):
         max_dim_size=3,
     ),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_flipud(*, dtype_and_m, test_flags, backend_fw, fn_name, on_device):
     input_dtype, m = dtype_and_m
@@ -955,6 +964,7 @@ def test_heaviside(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     indices_or_sections=_get_splits(allow_none=False, min_num_dims=2, axis=1),
     test_gradients=st.just(False),
     test_with_out=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_hsplit(
     dtype_and_x, indices_or_sections, test_flags, backend_fw, fn_name, on_device
@@ -1096,6 +1106,7 @@ def test_matricize(*, data, test_flags, backend_fw, fn_name, on_device):
         force_int=True,
     ),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_moveaxis(
     *, dtype_and_a, source, destination, test_flags, backend_fw, fn_name, on_device
@@ -1283,6 +1294,7 @@ def test_put_along_axis(
         max_dim_size=10,
     ),
     test_gradients=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_rot90(dtype_m_k_axes, test_flags, backend_fw, fn_name, on_device):
     input_dtype, m, k, axes = dtype_m_k_axes
@@ -1527,6 +1539,7 @@ def test_unique_consecutive(
     indices_or_sections=_get_splits(allow_none=False, min_num_dims=2, axis=0),
     test_gradients=st.just(False),
     test_with_out=st.just(False),
+    test_with_copy=st.just(True),
 )
 def test_vsplit(
     dtype_and_x, indices_or_sections, test_flags, backend_fw, fn_name, on_device
