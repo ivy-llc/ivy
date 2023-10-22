750567bb0153c2dfe4cfcccf1de165f7719cfc1f
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2023-10-22 18:16:00 +0530

    Fixed passing positional arguments in few functions

--- ivy/functional/frontends/paddle/nn/functional/pooling.py
+++ ivy/functional/frontends/paddle/nn/functional/pooling.py
@@ -112,4 +112,11 @@ def max_unpool1d(
     output_size=None,
     name=None,
 ):
-    return ivy.max_unpool1d(x, indices, kernel_size, stride, padding, data_format)
+    return ivy.max_unpool1d(
+        x,
+        indices,
+        kernel_size,
+        strides=stride,
+        padding=padding,
+        data_format=data_format,
+    )
