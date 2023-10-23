11fe7b80028f25f1ab6bef48e04f1440b3c4d1aa
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2023-10-23 10:03:17 +0000

    fix _to_tf_padding

--- ivy/functional/ivy/experimental/manipulation.py
+++ ivy/functional/ivy/experimental/manipulation.py
@@ -42,6 +42,8 @@ def _to_tf_padding(pad_width, ndim):
     if isinstance(pad_width, Number):
         pad_width = [[pad_width] * 2] * ndim
     elif len(pad_width) == 2 and isinstance(pad_width[0], Number):
+        pad_width = [pad_width] * ndim
+    elif isinstance(pad_width, (list, tuple)) and isinstance(pad_width[0], (list, tuple)):
         pad_width = pad_width * ndim
     return pad_width
 
