32c462662e6a58a247dd8b6eb074d14c515e9aa9
muhammadhammad-tech <75118771+muhammadhammad-tech@users.noreply.github.com>
2023-10-24 07:21:51 +0000

    stft Fixes #26989

---
+++ ivy/functional/frontends/paddle/signal.py
@@ -0,0 +1,43 @@
+import ivy
+from ivy.functional.frontends.tensorflow.func_wrapper import (
+    to_ivy_arrays_and_back,
+)
+from ivy.func_wrapper import with_supported_dtypes
+
+
+# stft
+@with_supported_dtypes(
+    {
+        "2.5.1 and below": (
+            "complex64",
+            "complex128",
+        )
+    },
+    "paddle",
+)
+@to_ivy_arrays_and_back
+def stft(
+    x,
+    n_fft,
+    hop_length=None,
+    win_length=None,
+    window=None,
+    center=True,
+    pad_mode="reflect",
+    normalized=False,
+    onesided=True,
+    name=None,
+):
+    x = ivy.asarray(x)
+    return ivy.stft(
+        x,
+        n_fft,
+        hop_length=hop_length,
+        win_length=win_length,
+        window=window,
+        center=center,
+        pad_mode=pad_mode,
+        normalized=normalized,
+        onesided=onesided,
+        name=name,
+    )
---
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_signal.py
@@ -0,0 +1,69 @@
+# global
+from hypothesis import strategies as st
+
+# local
+import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy.helpers import handle_frontend_test
+
+
+# --- Helpers --- #
+# --------------- #
+
+
+@st.composite
+def _valid_stft(draw):
+    dtype, x = draw(
+        helpers.dtype_and_values(
+            available_dtypes=["float32", "float64"],
+            max_value=65280,
+            min_value=-65280,
+            min_num_dims=1,
+            min_dim_size=2,
+            shared_dtype=True,
+        )
+    )
+    n_fft = draw(helpers.ints(min_value=16, max_value=100))
+    hop_length = draw(helpers.ints(min_value=1, max_value=50))
+
+    return dtype, x, n_fft, hop_length
+
+
+# --- Main --- #
+# ------------ #
+
+
+# Updated test function
+@handle_frontend_test(
+    fn_tree="paddle.signal.stft",
+    dtype_x_and_args=_valid_stft(),
+    test_with_out=st.just(False),
+)
+def test_paddle_stft(
+    *,
+    dtype_x_and_args,
+    frontend,
+    test_flags,
+    fn_tree,
+    backend_fw,
+    on_device,
+):
+    input_dtype, x, n_fft, hop_length = dtype_x_and_args
+    helpers.test_frontend_function(
+        input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        frontend=frontend,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        x=x,
+        n_fft=n_fft,
+        hop_length=hop_length,
+        win_length=None,
+        window=None,
+        center=True,
+        pad_mode="reflect",
+        normalized=False,
+        onesided=True,
+        atol=1e-02,
+        rtol=1e-02,
+    )
