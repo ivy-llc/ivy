e7afea0b22d3db695dc26738b3b7a884d83517a2
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-10-26 09:27:55 +0000

    Updated test_linalg.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_linalg.py
@@ -746,8 +746,8 @@ def _tensor_train_matrix_data(draw):
             available_dtypes=helpers.get_dtypes("float"),
             min_value=0.1,
             max_value=10,
-            min_num_dims=2,
-            max_num_dims=5,
+            min_num_dims=4,
+            max_num_dims=4,
             min_dim_size=2,
             max_dim_size=5,
             ret_shape=True,
@@ -755,7 +755,7 @@ def _tensor_train_matrix_data(draw):
     )
     dims = len(shape)
     rank = []
-    for i in range(dims):
+    for i in range(dims - 1):
         rank.append(draw(helpers.ints(min_value=1, max_value=shape[i])))
 
     rank[0] = rank[-1] = 1
