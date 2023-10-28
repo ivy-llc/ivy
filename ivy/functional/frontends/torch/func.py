ab04f981ba382853f0bf223d5a48444727e42599
Subhodip Ghosh <macy16661@gmail.com>
2023-10-28 23:17:23 +0530

    added device func

--- ivy/functional/frontends/torch/func.py
+++ ivy/functional/frontends/torch/func.py
@@ -6,12 +6,12 @@ from ivy.functional.frontends.torch.func_wrapper import (
 from ivy.func_wrapper import outputs_to_ivy_arrays
 
 
+def device(type, index=0):
+    return ivy.as_ivy_dev(type)
+
+
 def vmap(func, in_dims=0, out_dims=0, randomness="error", *, chunk_size=None):
     fun = outputs_to_native_arrays(func)
     return to_ivy_arrays_and_back(
         outputs_to_ivy_arrays(ivy.vmap(fun, in_axes=in_dims, out_axes=out_dims))
     )
-
-
-def device(type, index=0):
-    return ivy.as_ivy_dev(type)
