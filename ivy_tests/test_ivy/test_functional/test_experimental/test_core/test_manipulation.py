65eb5e984d0c4e3f78bc3c023fab2a801a2104fa
Oluwatobi Oyebade <64794915+tbeetech@users.noreply.github.com>
2023-10-28 16:49:33 +0100

    Update test_manipulation.py

--- ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
+++ ivy_tests/test_ivy/test_functional/test_experimental/test_core/test_manipulation.py
@@ -9,6 +9,9 @@ import ivy_tests.test_ivy.helpers as helpers
 from ivy_tests.test_ivy.helpers import handle_test, create_concatenable_arrays_dtypes
 from ivy.functional.ivy.experimental.manipulation import _check_bounds
 from ivy_tests.test_ivy.test_functional.test_core.test_manipulation import _get_splits
+from ivy_tests.test_ivy.test_functional.test_experimental.test_core.test_dtypes import _st_dtypes
+from ivy_tests.test_ivy.test_functional.test_experimental.test_core.test_sequences import _st_sequence_dtypes_and_values
+
 
 
 # --- Helpers --- #
@@ -1323,15 +1326,23 @@ def test_rot90(dtype_m_k_axes, test_flags, backend_fw, fn_name, on_device):
     test_with_out=st.just(False),
     test_with_copy=st.just(True),
 )
+
+
+@handle_test(
+    fn_tree="functional.ivy.experimental.sequence_insert",
+    dtype_and_x=_st_sequence_dtypes_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+        shape=st.shared(helpers.get_shape(), key="seq_shape"),
+    ),
+    indices_dtypes=_st_dtypes(dtypes=helpers.get_dtypes("valid"), shape=[1]),
+    insert_dtypes=helpers.get_dtypes("valid"),
+    indices_or_sections=_get_splits(allow_none=False, min_num_dims=1),
+    test_gradients=st.just(False),
+    test_with_out=st.just(False),
+    test_with_copy=st.just(True),
+)
 def test_sequence_insert(
-    dtype_and_x,
-    indices_dtypes,
-    insert_dtypes,
-    indices_or_sections,
-    test_flags,
-    backend_fw,
-    fn_name,
-    on_device,
+    dtype_and_x, indices_dtypes, insert_dtypes, indices_or_sections, test_flags, backend_fw, fn_name, on_device
 ):
     input_dtype, x = dtype_and_x
     indices_dtype = indices_dtypes[0]
@@ -1346,6 +1357,7 @@ def test_sequence_insert(
         indices=indices_or_sections,
         values=insert_dtype(x[1][0]),
     )
+
 
 
 @handle_test(
