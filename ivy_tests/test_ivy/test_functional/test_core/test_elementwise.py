9f2aab8346a1a6f3ec5383141067dc86d97a33ee
Aaryan562 <82304628+Aaryan562@users.noreply.github.com>
2023-11-02 09:22:28 +0000

    focused on paddle and its float16 dtype and made test_gradients for it to false

--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -1772,10 +1772,11 @@ def test_sign(*, dtype_and_x, np_variant, test_flags, backend_fw, fn_name, on_de
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("float_and_complex")
     ),
-    test_gradients=st.just(False),
 )
 def test_sin(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
     input_dtype, x = dtype_and_x
+    if "paddle" in backend_fw and input_dtype[0] == "float16":
+        assume(not test_flags.test_gradients)
     helpers.test_function(
         input_dtypes=input_dtype,
         test_flags=test_flags,
