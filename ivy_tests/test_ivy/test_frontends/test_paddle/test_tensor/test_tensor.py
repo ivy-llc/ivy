b0a744e8d5ca7ed57ede50d7be4b0a9bc8867480
Anudeep Sanapala <71971574+anudeeps0306@users.noreply.github.com>
2023-11-07 21:16:27 +0530

    feat: Implement is_complex tensor Instance Methods to paddle Frontend (#26762)
    Co-authored-by: ivy-branch <ivy.branch@lets-unify.ai>

--- ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_paddle/test_tensor/test_tensor.py
@@ -411,6 +411,39 @@ def test_paddle__reshape(
     )
 
 
+# is_complex
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="paddle.to_tensor",
+    method_name="is_complex",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("valid"),
+    ),
+)
+def test_paddle_is_complex(
+    dtype_and_x,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    frontend,
+    backend_fw,
+    on_device,
+):
+    input_dtype, x = dtype_and_x
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={"data": x[0]},
+        method_input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+    
 # abs
 @handle_frontend_method(
     class_tree=CLASS_TREE,
