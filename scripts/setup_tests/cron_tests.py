4b508b90e5305d7660c36ad8d86c2692677e14e8
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2023-11-14 21:54:42 +0530

    Updated the gpu cron tests to only run for the priority functions as it's infeasible to run all tests (#27283)

--- scripts/setup_tests/cron_tests.py
+++ scripts/setup_tests/cron_tests.py
@@ -1,11 +1,19 @@
 import sys
 from get_all_tests import get_all_tests
 
-run_iter = int(sys.argv[1])
-test_names = get_all_tests()
-# Run 150 tests in each iteration of the cron job
+
+run_iter, gpu = int(sys.argv[1]), sys.argv[2]
+if gpu == "true":
+    from setup_priority_tests import main
+
+    main()
+    with open("tests_to_run", "r") as f:
+        test_names = [line.strip() for line in f.readlines()]
+    tests_per_run = 10
+else:
+    test_names = get_all_tests()
+    tests_per_run = 150
 num_tests = len(test_names)
-tests_per_run = 150
 start = run_iter * tests_per_run
 end = (run_iter + 1) * tests_per_run
 print("Running Tests:")
