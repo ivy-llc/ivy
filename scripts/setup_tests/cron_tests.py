780af95caa3ce387690e0415908b914ed562287e
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2023-11-14 16:31:51 +0300

    initial commit

--- scripts/setup_tests/cron_tests.py
+++ scripts/setup_tests/cron_tests.py
@@ -1,8 +1,17 @@
 import sys
 from get_all_tests import get_all_tests
 
-run_iter = int(sys.argv[1])
-test_names = get_all_tests()
+
+run_iter, gpu = int(sys.argv[1]), sys.argv[2]
+if gpu == "true":
+    from setup_priority_tests import main
+
+    main()
+    with open("tests_to_run", "r") as f:
+        test_names = [line.strip().split(",")[0] for line in f.readlines()]
+    test_names = list(set(test_names))
+else:
+    test_names = get_all_tests()
 # Run 150 tests in each iteration of the cron job
 num_tests = len(test_names)
 tests_per_run = 150
