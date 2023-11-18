e8ca6eecf1550490831b567b465d1c58b7fd4462
aibenStunner <ebenezergadri99@gmail.com>
2023-11-18 04:04:06 +0000

    feat: added __ge__ tensor instance method

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -68,6 +68,22 @@ class Tensor:
     def __add__(self, y, /, name=None):
         return paddle_frontend.add(self, y)
 
+    @with_unsupported_dtypes(
+        {
+            "2.5.2 and below": (
+                "bool",
+                "uint8",
+                "int8",
+                "int16",
+                "complex64",
+                "complex128",
+            )
+        },
+        "paddle",
+    )
+    def __ge__(self, y, /, name=None):
+        return paddle_frontend.logic.greater_equal(self, y)
+
     def __getitem__(self, item):
         ivy_args = ivy.nested_map(_to_ivy_array, [self, item])
         ret = ivy.get_item(*ivy_args)
