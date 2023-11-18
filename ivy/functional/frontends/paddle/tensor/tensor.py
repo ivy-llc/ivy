5920365d49401b3c19352d7f73422cc9efb3c945
Gadri Ebenezer <ebenezergadri99@gmail.com>
2023-11-18 18:39:46 +0000

    feat: Paddle Frontend: tensor __sub__  (#27305)
    Co-authored-by: NripeshN <nripesh14@gmail.com>

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -69,6 +69,13 @@ class Tensor:
         return paddle_frontend.add(self, y)
 
     @with_unsupported_dtypes(
+        {"2.5.2 and below": ("bool", "unsigned", "int8", "float16", "bfloat16")},
+        "paddle",
+    )
+    def __sub__(self, y, /, name=None):
+        return paddle_frontend.subtract(self, y)
+
+    @with_unsupported_dtypes(
         {"2.5.2 and below": ("uint8", "int8", "int16", "float16", "bfloat16")},
         "paddle",
     )
