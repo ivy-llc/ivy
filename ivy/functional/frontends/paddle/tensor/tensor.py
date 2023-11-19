78d44a267d61299202b0ddf7f7e1a1483d856983
niixxaaa <arunimasanthosh2302@gmail.comm>
2023-11-19 20:22:08 +0400

    feat: Add __neg__ method to Tensor class

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -191,6 +191,9 @@ class Tensor:
     def __rmul__(self, y, /, name=None):
         return paddle_frontend.multiply(self, y)
 
+    def __neg__(self):
+        return paddle_frontend.neg(self)
+
     # Instance Methods #
     # ---------------- #
 
