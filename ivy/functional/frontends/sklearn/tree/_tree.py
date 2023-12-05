a4f8a48101318d6b4a347b391ccdd797c3fccaab
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-12-05 15:48:25 +0000

    feat: add base TreeBuilder class for sklearn frontend

---
+++ ivy/functional/frontends/sklearn/tree/_tree.py
@@ -0,0 +1,10 @@
+class TreeBuilder:
+    def build(
+        self,
+        tree,
+        X,
+        y,
+        sample_weight=None,
+        missing_values_in_feature_mask=None,
+    ):
+        pass
