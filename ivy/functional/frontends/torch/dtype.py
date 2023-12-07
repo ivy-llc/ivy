607bd81370016ee96126ad3bf3b62887c6782d20
Sujata R <135149476+sroycho4@users.noreply.github.com>
2023-12-07 18:50:35 +0530

    changed in dtype

--- ivy/functional/frontends/torch/dtype.py
+++ ivy/functional/frontends/torch/dtype.py
@@ -1,14 +1,18 @@
-# local
+# Importing necessary modules
+# 'ivy' is presumably a library for array operations,
+# and 'torch_frontend' is an Ivy frontend for PyTorch interoperability.
 import ivy
 import ivy.functional.frontends.torch as torch_frontend
 
-
+# Define the default data type for Ivy tensors as torch float32
 _default_dtype = torch_frontend.float32
 
-
+# Function to check if casting from one data type to another is possible
 def can_cast(from_, to):
     from_str = str(from_)
     to_str = str(to)
+
+    # Check specific cases where casting is not allowed
     if "float" in from_str and "bool" in to_str:
         return False
     if "float" in from_str and "int" in to_str:
@@ -20,18 +24,21 @@ def can_cast(from_, to):
         return False
     if "bool" in to_str:
         return from_str == to_str
+    
+    # Allow casting in all other cases
     return True
 
-
+# Function to get the default data type
 def get_default_dtype():
     return _default_dtype
 
-
+# Function to promote types for binary operations
 def promote_types(type1, type2, /):
     return torch_frontend.promote_types_torch(type1, type2)
 
-
+# Function to set the default data type
 def set_default_dtype(d):
+    # Check if the provided data type is a valid floating-point type
     ivy.utils.assertions.check_elem_in_list(
         d,
         [
@@ -42,6 +49,7 @@ def set_default_dtype(d):
         ],
         message="only floating-point types are supported as the default type",
     )
+    # Update the global default data type
     global _default_dtype
     _default_dtype = d
     return
