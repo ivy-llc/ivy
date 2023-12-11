84b6b3cb1c8eb7907ba1a5287b01f238f7d6162f
Kacper-W-Kozdon <k.kozdon@gmail.com>
2023-12-11 16:20:25 +0100

    feat: unflatten_frontend + backend + test

--- ivy/functional/frontends/torch/miscellaneous_ops.py
+++ ivy/functional/frontends/torch/miscellaneous_ops.py
@@ -503,8 +503,8 @@ def triu_indices(row, col, offset=0, dtype="int64", device="cpu", layout=None):
 
 
 @to_ivy_arrays_and_back
-def unflatten(x, /, *, dim, sizes):
-    return ivy.unflatten(x, axis=dim, shape=sizes)
+def unflatten(x, /, *, axis, shape):
+    return ivy.unflatten(x, axis, shape)
 
 
 @to_ivy_arrays_and_back
