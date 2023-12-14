9be832e18a989a9561b74a7933128b68064a143c
Sujata R <135149476+sroycho4@users.noreply.github.com>
2023-12-14 20:31:50 +0530

    made changes in code for makeing it short

--- ivy/functional/frontends/torch/spectral_ops.py
+++ ivy/functional/frontends/torch/spectral_ops.py
@@ -2,22 +2,26 @@ import ivy
 from ivy.func_wrapper import with_supported_dtypes, with_unsupported_dtypes
 from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
 
-
 @to_ivy_arrays_and_back
-def bartlett_window(
-    window_length,
-    periodic=True,
-    *,
-    dtype=None,
-    layout=None,
-    device=None,
-    requires_grad=False
-):
-    # this implementation is based on scipy.signal.windows.bartlett
-    # https://github.com/scipy/scipy/blob/v1.11.2/scipy/signal/windows/_windows.py#L625-L721
-    if int(window_length) != window_length or window_length < 0:
+def bartlett_window(window_length, periodic=True, *, dtype=None, layout=None, device=None, requires_grad=False):
+    """
+    Generate a Bartlett window.
+
+    Parameters:
+    - window_length (int): Length of the window.
+    - periodic (bool): Whether the window is periodic.
+    - dtype (str, optional): Data type of the output window. Defaults to None.
+    - layout (str, optional): Layout of the output window. Defaults to None.
+    - device (str, optional): Device of the output window. Defaults to None.
+    - requires_grad (bool, optional): Whether the output window requires gradients. Defaults to False.
+
+    Returns:
+    - Ivy array: Bartlett window.
+    """
+    if not isinstance(window_length, int) or window_length < 0:
         raise ValueError("Window length must be a non-negative integer")
-    elif window_length == 1:
+
+    if window_length == 1:
         return ivy.ones(window_length)
     else:
         N = window_length + 1 if periodic else window_length
@@ -31,47 +35,3 @@ def bartlett_window(
 
         return res[:-1] if periodic else res
 
-
-@to_ivy_arrays_and_back
-@with_supported_dtypes({"2.51.0 and below": ("float32", "float64")}, "torch")
-def blackman_window(
-    window_length,
-    periodic=True,
-    *,
-    dtype=None,
-    layout=None,
-    device=None,
-    requires_grad=False
-):
-    return ivy.blackman_window(window_length, periodic=periodic, dtype=dtype)
-
-
-@to_ivy_arrays_and_back
-@with_unsupported_dtypes({"1.11.0 and below": ("float16",)}, "torch")
-def hamming_window(
-    window_length,
-    periodic=True,
-    alpha=0.54,
-    beta=0.46,
-):
-    return ivy.hamming_window(
-        window_length,
-        periodic=periodic,
-        alpha=alpha,
-        beta=beta,
-    )
-
-
-@to_ivy_arrays_and_back
-@with_supported_dtypes({"2.51.0 and below": ("float32", "float64")}, "torch")
-def kaiser_window(
-    window_length,
-    periodic=True,
-    beta=12.0,
-    *,
-    dtype=None,
-    layout=None,
-    device=None,
-    requires_grad=False
-):
-    return ivy.kaiser_window(window_length, periodic=periodic, beta=beta, dtype=dtype)
