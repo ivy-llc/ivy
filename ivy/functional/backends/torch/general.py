3eed23b68f4ac019f4d215b43eb106c705ce2eed
Kacper-W-Kozdon <k.kozdon@gmail.com>
2023-12-14 19:20:40 +0100

    torch_gather_fix copied, not cleaned

--- ivy/functional/backends/torch/general.py
+++ ivy/functional/backends/torch/general.py
@@ -195,11 +195,21 @@ def gather(
         param_singleton_dims = torch.Size(param_singleton_dims_table)
 
         params_ex = (
-            params
-            if (indices.dim() - batch_dims <= 1)
+            params.reshape(
+                (
+                    params.shape[:axis]
+                    + max([
+                        ind_singleton_dims[batch_dims : batch_dims + 1],
+                        params.shape[axis : axis + 1],
+                    ])
+                    + ind_singleton_dims[batch_dims + 1 :]
+                    + params.shape[axis + 1 :]
+                )
+            )
+            if (indices.dim() - batch_dims <= 1 or params.dim() - axis <= 1)
             else params.reshape(
                 params.shape[:axis]
-                + ind_singleton_dims[batch_dims:]
+                + ind_singleton_dims[batch_dims + 1 :]
                 + params.shape[axis + 1 :]
             )
         )
