7ae51faa65e7c8506bc6b9c4e7029e346fcf8cdf
Yog Dharaskar <yogpradip@gmail.com>
2023-12-18 11:32:39 +0000

    Fix issue with update_backend

--- ivy_tests/test_ivy/helpers/pipeline_helper.py
+++ ivy_tests/test_ivy/helpers/pipeline_helper.py
@@ -5,11 +5,12 @@ from typing import Callable
 
 
 class WithBackendContext:
-    def __init__(self, backend) -> None:
+    def __init__(self, backend, cached=True) -> None:
         self.backend = backend
+        self.cached = cached
 
     def __enter__(self):
-        return ivy.with_backend(self.backend, cached=True)
+        return ivy.with_backend(self.backend, cached=self.cached)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         return
