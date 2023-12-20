95b1e5102489127ecabb6cdb34d823d9e3b1be99
Kacper-W-Kozdon <k.kozdon@gmail.com>
2023-12-20 19:12:13 +0100

    unflatten_frontend test updates

--- ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_miscellaneous_ops.py
@@ -1755,6 +1755,36 @@ def test_torch_unflatten(
     shape,
 ):
     dtype, x, axes = dtype_input_axes
+
+    def factorization(n):
+        factors = []
+
+        def get_factor(n):
+            x_fixed = 2
+            cycle_size = 2
+            x = 2
+            factor = 1
+
+            while factor == 1:
+                for count in range(cycle_size):
+                    if factor > 1:
+                        break
+                    x = (x * x + 1) % n
+                    factor = math.gcd(x - x_fixed, n)
+
+                cycle_size *= 2
+                x_fixed = x
+
+            return factor
+
+        while n > 1:
+            next = get_factor(n)
+            factors.append(next)
+            n //= next
+
+        return factors
+
+    shape = factorization(shape[axes])
     helpers.test_frontend_function(
         input_dtypes=dtype,
         frontend=frontend,
