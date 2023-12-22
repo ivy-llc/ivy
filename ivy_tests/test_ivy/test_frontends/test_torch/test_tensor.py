1e765d8fcf519b870e1d99e0b4a796fca25f30de
Prajyot Shashikant Bhusari <140729090+Prajyot70@users.noreply.github.com>
2023-12-22 15:45:20 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -53,7 +53,6 @@ from ivy_tests.test_ivy.test_functional.test_experimental.test_core.test_statist
 )
 
 
-# cpu
 from ivy_test.helpers import helpers
 from ivy_test.helpers.helpers import handle_frontend_method
 from ivy_test.helpers import helper
@@ -5407,6 +5406,43 @@ def test_torch_cov(
     )
 
 
+#cpu
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="ivy.to_device",
+    dtype_and_x=helpers.dtype_and_values(
+        available_dtypes=helpers.get_dtypes("float"),
+        num_arrays=1,
+        min_value=-1e04,
+        max_value=1e04,
+        allow_inf=False,
+    ),
+)
+def test_torch_instance_to_cpu(
+    dtype_and_x,
+    frontend,
+    backend_fw,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+):
+    input_dtype, x = dtype_and_x
+    helper.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        init_all_as_kwargs_np={
+            "data": x[0],
+        },
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={},
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+    )
+
+
 # cross
 @handle_frontend_method(
     class_tree=CLASS_TREE,
@@ -7835,42 +7871,6 @@ def test_torch_index_select(
         method_flags=method_flags,
         frontend=frontend,
         on_device=on_device,
-    )
-
-
-@handle_frontend_method(
-    class_tree=CLASS_TREE,
-    init_tree="torch.tensor",
-    method_name="ivy.to_device",
-    dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("float"),
-        num_arrays=1,
-        min_value=-1e04,
-        max_value=1e04,
-        allow_inf=False,
-    ),
-)
-def test_torch_instance_to_cpu(
-    dtype_and_x,
-    frontend,
-    backend_fw,
-    frontend_method_data,
-    init_flags,
-    method_flags,
-):
-    input_dtype, x = dtype_and_x
-    helper.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        init_all_as_kwargs_np={
-            "data": x[0],
-        },
-        method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={},
-        frontend_method_data=frontend_method_data,
-        init_flags=init_flags,
-        method_flags=method_flags,
-        frontend=frontend,
-        backend_to_test=backend_fw,
     )
 
 
@@ -14305,6 +14305,3 @@ def test_torch_zero_(
         on_device=on_device,
     )
 
-
-# run the test
-test_torch_instance_to_cpu
