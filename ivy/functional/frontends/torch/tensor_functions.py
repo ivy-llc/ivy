3a02fb475b810a4eab28762d749ca3ffc19dfc00
Sujata R <135149476+sroycho4@users.noreply.github.com>
2023-12-26 19:08:49 +0530

    The scatter, scatter_add, and scatter_reduce functions are now implemented

--- ivy/functional/frontends/torch/tensor_functions.py
+++ ivy/functional/frontends/torch/tensor_functions.py
@@ -1,61 +1,71 @@
 # local
 import ivy
 from ivy.func_wrapper import with_supported_dtypes
-from ivy.functional.frontends.torch.func_wrapper import to_ivy_arrays_and_back
-
+from ivy.functional.frontends.numpy.func_wrapper import to_ivy_arrays_and_back
 
 @to_ivy_arrays_and_back
 def is_complex(input):
     return ivy.is_complex_dtype(input)
 
-
 @to_ivy_arrays_and_back
 def is_floating_point(input):
     return ivy.is_float_dtype(input)
-
 
 @to_ivy_arrays_and_back
 def is_nonzero(input):
     return ivy.nonzero(input)[0].size != 0
 
-
 @to_ivy_arrays_and_back
 def is_tensor(obj):
     return ivy.is_array(obj)
-
 
 @to_ivy_arrays_and_back
 def numel(input):
     return ivy.astype(ivy.array(input.size), ivy.int64)
 
-
 @to_ivy_arrays_and_back
 @with_supported_dtypes(
-    {"2.1.2 and below": ("float32", "float64", "int32", "int64")}, "torch"
+    {"2.1.1 and below": ("float32", "float64", "int32", "int64")}, "numpy"
 )
 def scatter(input, dim, index, src):
-    return ivy.put_along_axis(input, index, src, dim, mode="replace")
-
+    index_shape = [1] * input.ndim
+    index_shape[dim] = -1
+    index = ivy.unstack(ivy.expand_dims(index, dim), axis=-1)
+    src = ivy.reshape(src, index_shape + list(src.shape[1:]))
+    return ivy.scatter_nd_add(input, index, src)
 
 @to_ivy_arrays_and_back
 @with_supported_dtypes(
-    {"2.1.2 and below": ("float32", "float64", "int32", "int64")}, "torch"
+    {"2.1.1 and below": ("float32", "float64", "int32", "int64")}, "numpy"
 )
 def scatter_add(input, dim, index, src):
-    return ivy.put_along_axis(input, index, src, dim, mode="sum")
-
+    index_shape = [1] * input.ndim
+    index_shape[dim] = -1
+    index = ivy.unstack(ivy.expand_dims(index, dim), axis=-1)
+    src = ivy.reshape(src, index_shape + list(src.shape[1:]))
+    return ivy.scatter_nd_add(input, index, src)
 
 @to_ivy_arrays_and_back
 @with_supported_dtypes(
-    {"2.1.2 and below": ("float32", "float64", "int32", "int64")}, "torch"
+    {"2.1.1 and below": ("float32", "float64", "int32", "int64")}, "numpy"
 )
 def scatter_reduce(input, dim, index, src, reduce, *, include_self=True):
     mode_mappings = {
-        "sum": "sum",
+        "sum": "add",
         "amin": "min",
         "amax": "max",
-        "prod": "mul",
-        "replace": "replace",
+        "prod": "multiply",
+        "replace": "copy",
     }
     reduce = mode_mappings.get(reduce, reduce)
-    return ivy.put_along_axis(input, index, src, dim, mode=reduce)
+
+    index_shape = [1] * input.ndim
+    index_shape[dim] = -1
+    index = ivy.unstack(ivy.expand_dims(index, dim), axis=-1)
+    src = ivy.reshape(src, index_shape + list(src.shape[1:]))
+    
+    scattered = ivy.scatter_nd_add(input, index, src)
+    
+    if include_self:
+        scattered = getattr(ivy, reduce)(scattered, src)
+    return scattered
