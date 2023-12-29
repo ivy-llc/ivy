d5e04d4c666034344202225d4ce172e0abfd3df9
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-12-29 10:07:02 +0000

    fix: generate only signed integer labels for testing sklearn decision tree predict method, simplify and refactor of the logic to avoid attribute error when calling upon sklearn tree submodule.

--- ivy_tests/test_ivy/test_frontends/test_sklearn/test_tree/test_tree.py
+++ ivy_tests/test_ivy/test_frontends/test_sklearn/test_tree/test_tree.py
@@ -1,4 +1,4 @@
-import ivy.functional.frontends.sklearn as sklearn_frontend
+from ivy.functional.frontends.sklearn.tree import DecisionTreeClassifier as ivy_DTC
 import numpy as np
 import ivy
 from hypothesis import given
@@ -10,8 +10,8 @@ import ivy_tests.test_ivy.helpers as helpers
 
 
 # helper functions
-def _get_sklearn_predict(X, y, max_depth, module=None):
-    clf = module.tree.DecisionTreeClassifier(max_depth=max_depth)
+def _get_sklearn_predict(X, y, max_depth, DecisionTreeClassifier):
+    clf = DecisionTreeClassifier(max_depth=max_depth)
     clf.fit(X, y)
     return clf.predict
 
@@ -22,20 +22,19 @@ def _get_sklearn_predict(X, y, max_depth, module=None):
 
 # todo: integrate with already existing strats and generalize
 @given(
-    X=helpers.array_values(shape=(5, 2), dtype=helpers.get_dtypes("float")),
-    y=helpers.array_values(shape=(5,), dtype=helpers.get_dtypes("integer")),
+    X=helpers.array_values(
+        shape=(5, 2), dtype=helpers.get_dtypes("float", prune_function=False)
+    ),
+    y=helpers.array_values(
+        shape=(5,), dtype=helpers.get_dtypes("signed_integer", prune_function=False)
+    ),
 )
 def test_sklearn_tree_predict(X, y):
     try:
-        import sklearn
+        from sklearn.tree import DecisionTreeClassifier as sklearn_DTC
     except ImportError:
         print("sklearn not installed, skipping test_sklearn_tree_predict")
         return
-    sklearn_pred = _get_sklearn_predict(X, y, max_depth=3, module=sklearn)(X)
-    for fw in helpers.available_frameworks:
-        ivy.set_backend(fw)
-        ivy_pred = _get_sklearn_predict(
-            ivy.array(X), ivy.array(y), max_depth=3, module=sklearn_frontend
-        )(X)
-        assert np.allclose(ivy_pred.to_numpy(), sklearn_pred)
-        ivy.unset_backend()
+    sklearn_pred = _get_sklearn_predict(X, y, 3, sklearn_DTC)(X)
+    ivy_pred = _get_sklearn_predict(ivy.array(X), ivy.array(y), 3, ivy_DTC)(X)
+    assert np.allclose(ivy_pred.to_numpy(), sklearn_pred)
