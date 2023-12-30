7673380b8fed8763fcbcd47ea07d4c2afd593c60
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2023-12-30 10:39:54 +0000

    fix: specify bool as unsupported type for isin in torch front

--- ivy/functional/frontends/torch/comparison_ops.py
+++ ivy/functional/frontends/torch/comparison_ops.py
@@ -143,7 +143,7 @@ def isfinite(input):
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")}, "torch"
+    {"2.1.2 and below": ("float16", "bfloat16", "complex", "bool")}, "torch"
 )
 @to_ivy_arrays_and_back
 def isin(elements, test_elements, *, assume_unique=False, invert=False):
