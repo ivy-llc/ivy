b9cb39bbe81d38f60d73b4102cfe7ca1e4168c61
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-01-09 16:37:38 +0530

    fix: Fixed passing of un-necessary argument `dtype` (#27880)

--- ivy/functional/frontends/paddle/tensor/tensor.py
+++ ivy/functional/frontends/paddle/tensor/tensor.py
@@ -560,9 +560,9 @@ class Tensor:
         return paddle_frontend.isfinite(self)
 
     @with_supported_dtypes({"2.4.2 and below": ("float16", "bfloat16")}, "paddle")
-    def all(self, axis=None, keepdim=False, dtype=None, name=None):
+    def all(self, axis=None, keepdim=False, name=None):
         return paddle_frontend.Tensor(
-            ivy.all(self.ivy_array, axis=axis, keepdims=keepdim, dtype=dtype)
+            ivy.all(self.ivy_array, axis=axis, keepdims=keepdim)
         )
 
     @with_supported_dtypes({"2.5.2 and below": ("float16", "bfloat16")}, "paddle")
