88cffff668bba45487653ea644874688df3d2ee0
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-01-17 14:39:31 +0530

    fix: Fixed the issue with dtype mismatches as the precision_mode argument wasn't being passed everywhere

--- ivy_tests/test_ivy/helpers/function_testing.py
+++ ivy_tests/test_ivy/helpers/function_testing.py
@@ -242,6 +242,7 @@ def test_function_backend_computation(
             target_fn,
             *copy_args,
             test_trace=test_flags.test_trace,
+            precision_mode=test_flags.precision_mode,
             **copy_kwargs,
         )
 
@@ -265,14 +266,14 @@ def test_function_backend_computation(
                     ret_from_target,
                     ret_np_flat_from_target,
                 ) = get_ret_and_flattened_np_array(
-                    fw, instance.__getattribute__(fn_name), *args, **kwargs, out=out
+                    fw, instance.__getattribute__(fn_name), *args, **kwargs, out=out, precision_mode=test_flags.precision_mode
                 )
             else:
                 (
                     ret_from_target,
                     ret_np_flat_from_target,
                 ) = get_ret_and_flattened_np_array(
-                    fw, ivy_backend.__dict__[fn_name], *args, **kwargs, out=out
+                    fw, ivy_backend.__dict__[fn_name], *args, **kwargs, out=out, precision_mode=test_flags.precision_mode
                 )
             test_ret = (
                 ret_from_target[getattr(ivy_backend.__dict__[fn_name], "out_index")]
