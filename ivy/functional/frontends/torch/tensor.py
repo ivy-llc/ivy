5c3aeae8e104e8ecaecc7d6fbc7f10072d4e60d7
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2024-01-17 05:20:49 +0000

    fix: fixed an issue with _nested_get that was failing the unsupported dtypes function when testing with a frontend tensor method that uses the .to call as we can't infer the type of the instance where .to is being called in arbitrary code, also updated the log1p frontend method implementations for the same

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1711,12 +1711,14 @@ class Tensor:
     )
     def log1p(self):
         promoted_type = ivy.promote_types(self.dtype, "float32")
-        return torch_frontend.log1p(self).to(promoted_type)
+        res = torch_frontend.log1p(self)
+        return res.to(promoted_type)
 
     @with_supported_dtypes({"2.1.2 and below": ("float32", "float64")}, "torch")
     def log1p_(self):
         promoted_type = ivy.promote_types(self.dtype, "float32")
-        self.ivy_array = torch_frontend.log1p(self).to(promoted_type).ivy_array
+        res = torch_frontend.log1p(self)
+        self.ivy_array = res.to(promoted_type).ivy_array
         return self
 
     def baddbmm(self, batch1, batch2, *, beta=1, alpha=1):
