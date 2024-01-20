9b02410f5819c5d35540fbe8d98e8146ad5f862f
samthakur587 <samunder268@gmail.com>
2024-01-20 23:53:49 +0530

    fix: fixed ivy.sum not passing the test case at paddle & torch backend

--- ivy/functional/backends/torch/statistical.py
+++ ivy/functional/backends/torch/statistical.py
@@ -181,7 +181,9 @@ def std(
 
 # Function does support uint8, but allowing support for unsigned will cause
 # the function to break the upcasting rule defined in the Array API Standard
-@with_unsupported_dtypes({"2.1.2 and below": ("uint8",)}, backend_version)
+@with_unsupported_dtypes(
+    {"2.1.2 and below": ("uint8", "float16", "bfloat16")}, backend_version
+)
 def sum(
     x: torch.Tensor,
     /,
