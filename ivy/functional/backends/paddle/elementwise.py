363f0b2ce270ec4cf7e5543868b3ba3d24e4e85d
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-01-27 06:22:44 +0530

    Fixed paddle_backend.divide by adding an if condition to separate dtypes.

--- ivy/functional/backends/paddle/elementwise.py
+++ ivy/functional/backends/paddle/elementwise.py
@@ -399,12 +399,13 @@ def divide(
     *,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    if paddle.is_complex(x1) or paddle.is_complex(x2):
-        angle_value = paddle.angle(x1) - paddle.angle(x2)
-        abs_value = paddle.abs(x1) / paddle.abs(x2)
-        return paddle.complex(
-            abs_value * paddle.cos(angle_value), abs_value * paddle.sin(angle_value)
-        )
+    if not (isinstance(x1, float) or isinstance(x2, float)):
+        if paddle.is_complex(x1) or paddle.is_complex(x2):
+            angle_value = paddle.angle(x1) - paddle.angle(x2)
+            abs_value = paddle.abs(x1) / paddle.abs(x2)
+            return paddle.complex(
+                abs_value * paddle.cos(angle_value), abs_value * paddle.sin(angle_value)
+            )
     x1, x2, ret_dtype = _elementwise_helper(x1, x2)
     return (x1 / x2).astype(ret_dtype)
 
