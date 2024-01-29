454ece6121ac8cb8930d9a2712dfb5b5b1df40b6
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-01-29 19:11:39 +0530

    Added float16 dtype as one of the supported dtypes.

--- ivy/functional/frontends/paddle/creation.py
+++ ivy/functional/frontends/paddle/creation.py
@@ -88,7 +88,7 @@ def empty_like(x, dtype=None, name=None):
 
 
 @with_supported_dtypes(
-    {"2.6.0 and below": ("float32", "float64", "int32", "int64")}, "paddle"
+    {"2.6.0 and below": ("float16", "float32", "float64", "int32", "int64")}, "paddle"
 )
 @to_ivy_arrays_and_back
 def eye(num_rows, num_columns=None, dtype=None, name=None):
