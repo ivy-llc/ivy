a706f8fdbc167c5650d45acef5562ff18633a3ec
samunder singh <83540902+samthakur587@users.noreply.github.com>
2024-02-01 02:29:32 +0530

    fix: fixed the failing test case at paddle backend (#27831)
    fixed tests at paddle backend

--- ivy/functional/backends/paddle/elementwise.py
+++ ivy/functional/backends/paddle/elementwise.py
@@ -120,8 +120,9 @@ def isinf(
     return paddle.zeros(shape=x.shape, dtype=bool)
 
 
-@with_supported_dtypes(
-    {"2.6.0 and below": ("float32", "float64", "int32", "int64")}, backend_version
+@with_unsupported_dtypes(
+    {"2.6.0 and below": ("bfloat16",)},
+    backend_version,
 )
 def equal(
     x1: Union[float, paddle.Tensor],
@@ -131,16 +132,11 @@ def equal(
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
     x1, x2, ret_dtype = _elementwise_helper(x1, x2)
-    diff = paddle_backend.subtract(x1, x2)
-    ret = paddle_backend.logical_and(
-        paddle_backend.less_equal(diff, 0), paddle_backend.greater_equal(diff, 0)
-    )
-    # ret result is sufficient for all cases except where the value is +/-INF of NaN
-    return paddle_backend.where(
-        paddle_backend.isnan(diff),
-        ~paddle_backend.logical_or(paddle_backend.isnan(x1), paddle_backend.isnan(x2)),
-        ret,
-    )
+    if paddle.is_complex(x1):
+        real = paddle.equal(x1.real(), x2.real())
+        imag = paddle.equal(x1.imag(), x2.imag())
+        return paddle_backend.logical_and(real, imag)
+    return paddle.equal(x1, x2)
 
 
 @with_supported_dtypes(
--- ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py
@@ -687,7 +687,7 @@ def test_divide(*, dtype_and_x, test_flags, backend_fw, fn_name, on_device):
 @handle_test(
     fn_tree="functional.ivy.equal",
     dtype_and_x=helpers.dtype_and_values(
-        available_dtypes=helpers.get_dtypes("valid", full=True), num_arrays=2
+        available_dtypes=helpers.get_dtypes("valid", full=False), num_arrays=2
     ),
     test_gradients=st.just(False),
 )
