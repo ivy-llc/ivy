b3029486b1b3adaa79125866d89b29988b6a1e8b
rohitsalla <sallarohit1011@gmail.com>
2024-02-02 00:08:32 +0530

    Update test_tensor.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_tensor.py
@@ -13856,6 +13856,53 @@ def test_torch_unique(
     )
 
 
+ # tensor.unique
+@handle_frontend_method(
+    class_tree=CLASS_TREE,
+    init_tree="torch.tensor",
+    method_name="tensor.unique",
+    dtype_x_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("valid"),
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+    sorted=st.booleans(),
+    return_inverse=st.booleans(),
+    return_counts=st.booleans(),
+)
+def test_torch_tensor_unique(
+    dtype_x_axis,
+    sorted,
+    return_inverse,
+    return_counts,
+    frontend,
+    frontend_method_data,
+    init_flags,
+    method_flags,
+    on_device,
+    backend_fw,
+):
+    input_dtype, x, axis = dtype_x_axis
+    helpers.test_frontend_method(
+        init_input_dtypes=input_dtype,
+        backend_to_test=backend_fw,
+        init_all_as_kwargs_np={"data": x[0]},
+        method_input_dtypes=input_dtype,
+        method_all_as_kwargs_np={
+            "sorted": sorted,
+            "return_inverse": return_inverse,
+            "return_counts": return_counts,
+            "dim": axis,
+        },
+        frontend_method_data=frontend_method_data,
+        init_flags=init_flags,
+        method_flags=method_flags,
+        frontend=frontend,
+        on_device=on_device,
+    )
+
+
+#
 # unique_consecutive
 @handle_frontend_method(
     class_tree=CLASS_TREE,
