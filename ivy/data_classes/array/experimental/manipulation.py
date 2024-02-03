01c9eb79090eb6765c99cdb25c34b6568b160294
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-03 16:23:44 +0100

    ivy_unflatten fixing errors with shape and dim for array

--- ivy/data_classes/array/experimental/manipulation.py
+++ ivy/data_classes/array/experimental/manipulation.py
@@ -1184,7 +1184,7 @@ class _ArrayWithManipulationExperimental(abc.ABC):
         ivy.array([[[1.2, 2.3], [3.4, 4.5]], [[5.6, 6.7], [7.8, 8.9]]])
         """
         return ivy.unflatten(
-            self,
+            self._data,
             dim=dim,
             shape=shape,
             out=out,
