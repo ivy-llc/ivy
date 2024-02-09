b57a1032a0ed72247cbc7a4206bac73b1c86e77f
Felix Hirwa Nshuti <hirwanshutiflx@gmail.com>
2024-02-09 15:37:18 +0530

    Revert "fix: `Shape.__bool__` method tests (#28218)"
    This reverts commit 9789ea5392d9585d65acfa308a9907fb50a120d5.

--- ivy_tests/test_ivy/test_misc/test_shape.py
+++ ivy_tests/test_ivy/test_misc/test_shape.py
@@ -5,8 +5,6 @@ import ivy_tests.test_ivy.helpers as helpers
 
 from ivy_tests.test_ivy.helpers import handle_method
 
-CLASS_TREE = "ivy.Shape"
-
 
 @handle_method(
     method_tree="Shape.__add__",
@@ -46,7 +44,6 @@ def test_shape__add__(
 
 
 @handle_method(
-    init_tree=CLASS_TREE,
     method_tree="Shape.__bool__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=st.one_of(st.just(("bool",)), helpers.get_dtypes("integer")),
@@ -72,7 +69,7 @@ def test_shape__bool__(
         backend_to_test=backend_fw,
         init_flags=init_flags,
         method_flags=method_flags,
-        init_all_as_kwargs_np={"shape_tup": (x[0],)},
+        init_all_as_kwargs_np={"data": x[0]},
         init_input_dtypes=dtype,
         method_input_dtypes=[],
         method_all_as_kwargs_np={},
