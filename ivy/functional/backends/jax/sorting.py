d38dd80cfa9549ca1ce9a668a3714b4f250ef7a9
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-02-10 17:34:31 +0530

    Updated jax version mapping from 0.4.23 to 0.4.24

--- ivy/functional/backends/jax/sorting.py
+++ ivy/functional/backends/jax/sorting.py
@@ -78,7 +78,7 @@ def searchsorted(
 
 
 # msort
-@with_unsupported_dtypes({"0.4.23 and below": ("complex",)}, backend_version)
+@with_unsupported_dtypes({"0.4.24 and below": ("complex",)}, backend_version)
 def msort(
     a: Union[JaxArray, list, tuple],
     /,
