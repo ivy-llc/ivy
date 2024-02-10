3277232de7c4a704f6cbd873d11799efb394811d
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-02-10 18:58:54 +0530

    feat: Updated `torch` version mapping form `2.1.2` to `2.2` (#28236)

--- ivy/functional/backends/torch/layers.py
+++ ivy/functional/backends/torch/layers.py
@@ -14,7 +14,7 @@ from ivy.functional.ivy.layers import _get_embed_dim, _handle_padding, _deconv_l
 
 
 @with_supported_dtypes(
-    {"2.1.2 and below": ("float32", "float64", "complex")},
+    {"2.2 and below": ("float32", "float64", "complex")},
     backend_version,
 )
 def multi_head_attention(
@@ -117,7 +117,7 @@ multi_head_attention.partial_mixed_handler = (
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def linear(
@@ -254,7 +254,7 @@ def _tranpose_padding(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 # noinspection PyUnresolvedReferences
@@ -287,7 +287,7 @@ def conv1d(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def conv1d_v_1p9p0_and_above(
@@ -325,7 +325,7 @@ def conv1d_v_1p9p0_and_above(
 
 @with_unsupported_dtypes(
     {
-        "2.1.2 and below": (
+        "2.2 and below": (
             "float16",
             "bfloat16",
             "complex",
@@ -379,7 +379,7 @@ def conv1d_transpose(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 # noinspection PyUnresolvedReferences
@@ -412,7 +412,7 @@ def conv2d(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def conv2d_v_1p9p0_and_above(
@@ -450,7 +450,7 @@ def conv2d_v_1p9p0_and_above(
 
 @with_unsupported_dtypes(
     {
-        "2.1.2 and below": (
+        "2.2 and below": (
             "float16",
             "bfloat16",
             "complex",
@@ -507,7 +507,7 @@ def conv2d_transpose(
 
 @with_unsupported_dtypes(
     {
-        "2.1.2 and below": (
+        "2.2 and below": (
             "float16",
             "bfloat16",
             "complex",
@@ -548,7 +548,7 @@ def depthwise_conv2d(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")}, backend_version
+    {"2.2 and below": ("float16", "bfloat16", "complex")}, backend_version
 )
 # noinspection PyUnresolvedReferences
 def conv3d(
@@ -580,7 +580,7 @@ def conv3d(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")}, backend_version
+    {"2.2 and below": ("float16", "bfloat16", "complex")}, backend_version
 )
 def conv3d_v_1p9p0_and_above(
     x: torch.Tensor,
@@ -616,7 +616,7 @@ def conv3d_v_1p9p0_and_above(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 # noinspection PyUnresolvedReferences
@@ -669,7 +669,7 @@ def conv3d_transpose(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def conv_general_dilated(
@@ -718,7 +718,7 @@ def conv_general_dilated(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def conv_general_dilated_v_1p9p0_and_above(
@@ -772,7 +772,7 @@ def conv_general_dilated_v_1p9p0_and_above(
 
 
 @with_unsupported_dtypes(
-    {"2.1.2 and below": ("float16", "bfloat16", "complex")},
+    {"2.2 and below": ("float16", "bfloat16", "complex")},
     backend_version,
 )
 def conv_general_transpose(
