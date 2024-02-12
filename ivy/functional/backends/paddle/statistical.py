4fc40a096a650e2d860eee2f15ce8b25fd92db34
Jackson McClintock <jacksondm33@gmail.com>
2024-02-12 18:00:10 -0500

    fix: check for empty tensor in `mean` for paddle backend

--- ivy/functional/backends/paddle/statistical.py
+++ ivy/functional/backends/paddle/statistical.py
@@ -160,6 +160,16 @@ def max(
     return ret.astype(ret_dtype)
 
 
+def _calculate_reduced_shape(x, axis, keepdims):
+    if axis is None:
+        axis = tuple(range(len(x.shape)))
+    elif type(axis) not in (tuple, list):
+        axis = (axis,)
+    if keepdims:
+        return [1 if i in axis else x.shape[i] for i in range(len(x.shape))]
+    return [x.shape[i] for i in range(len(x.shape)) if i not in axis]
+
+
 @with_supported_dtypes(
     {"2.6.0 and below": ("bool", "complex", "float32", "float64")}, backend_version
 )
@@ -174,6 +184,9 @@ def mean(
 ) -> paddle.Tensor:
     if dtype is not None:
         x = ivy.astype(x, dtype).to_native()
+    if 0 in x.shape:
+        shape = _calculate_reduced_shape(x, axis, keepdims)
+        ret = paddle.empty(shape)
     if paddle.is_complex(x):
         ret = paddle.complex(
             paddle.mean(x.real(), axis=axis, keepdim=keepdims),
