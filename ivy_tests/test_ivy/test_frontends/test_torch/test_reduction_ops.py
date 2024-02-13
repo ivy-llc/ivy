b91890ed4f913ada15acc4d22e7b19f50303e5e1
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-13 20:55:11 +0100

    reformatted tf backend for mean

--- ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_reduction_ops.py
@@ -466,7 +466,7 @@ def test_torch_max(
         max_value=1e04,
     ),
     keepdims=st.booleans(),
-    dtypes=helpers.get_dtypes("numeric", none=True, full=False, prune_function=True),
+    dtypes=helpers.get_dtypes("valid", none=True, full=False, prune_function=True),
 )
 def test_torch_mean(
     *,
