fac7d29bf7c2290ff3c1e90d15d678fac69faca6
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2024-02-13 11:36:45 +0300

    Updated jax.nms backend - native while loop

--- ivy/functional/backends/jax/layers.py
+++ ivy/functional/backends/jax/layers.py
@@ -501,7 +501,8 @@ def nms(
         keep = jnp.zeros((size,), dtype=jnp.int64)
         keep_idx = 0
 
-        while jnp.unique(order).size > 1:
+        def body_fn(loop_vars):
+            keep, keep_idx, boxes, areas, order = loop_vars
             max_iou_idx = order[0]
             keep = keep.at[keep_idx].set(max_iou_idx)
             keep_idx += 1
@@ -521,6 +522,16 @@ def nms(
             order = order.at[forward].set(order[forward[::-1]])
             boxes = boxes.at[forward].set(boxes[forward[::-1]])
             areas = areas.at[forward].set(areas[forward[::-1]])
+
+            return keep, keep_idx, boxes, areas, order
+
+        def cond_fn(loop_vars):
+            _, _, _, _, order = loop_vars
+            unique_size = len(order)
+            return jnp.unique(order, size=unique_size).size > 1
+
+        init_vars = (keep, keep_idx, boxes, areas, order)
+        keep, keep_idx, boxes, _, _ = jlax.while_loop(cond_fn, body_fn, init_vars)
 
         ret = jnp.array(keep[:keep_idx], dtype=jnp.int64)
 
