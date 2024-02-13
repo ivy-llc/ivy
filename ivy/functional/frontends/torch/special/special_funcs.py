3957146032aa4b075340228695bc92a5b1a71c46
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2024-02-13 21:45:08 +0000

    feat: add torch frontend special function erfcx and the corresponding test
    because of its independece on erfc it seems to sometimes fail with value mismatches similar to those in erfc. Needs fix for erfc in backend implementations.

--- ivy/functional/frontends/torch/special/special_funcs.py
+++ ivy/functional/frontends/torch/special/special_funcs.py
@@ -13,6 +13,13 @@ def erfc(input, *, out=None):
     return 1.0 - ivy.erf(input, out=out)
 
 
+@with_unsupported_dtypes({"2.2 and below": ("float16", "complex", "bfloat16")}, "torch")
+@to_ivy_arrays_and_back
+def erfcx(input, *, out=None):
+    ret = erfc(input) * ivy.exp(input**2)
+    return ret
+
+
 @to_ivy_arrays_and_back
 def erfinv(input, *, out=None):
     return ivy.erfinv(input, out=out)
