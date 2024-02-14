3a8f810d19a78c9f65738e9e29b1c211ff6279e5
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2024-02-14 11:01:22 +0000

    chore: remove additional set_backend call in test_torch_log_softmax

--- ivy_tests/test_ivy/test_frontends/test_torch/test_nn/test_functional/test_non_linear_activation_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_nn/test_functional/test_non_linear_activation_functions.py
@@ -649,7 +649,6 @@ def test_torch_log_softmax(
     backend_fw,
 ):
     input_dtype, x, axis = dtype_x_and_axis
-    ivy.set_backend(backend_fw)
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
         backend_to_test=backend_fw,
@@ -662,7 +661,6 @@ def test_torch_log_softmax(
         _stacklevel=3,
         dtype=dtypes[0],
     )
-    ivy.previous_backend()
 
 
 # logsigmoid
