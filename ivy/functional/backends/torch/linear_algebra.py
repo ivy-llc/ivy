06ff7e2d954fe728784123a5628c8e1acee2fba4
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-19 00:29:23 +0100

    backends->torch->linalg

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -206,7 +206,7 @@ def matrix_norm(
 ) -> torch.Tensor:
     if dtype is not None:
         dtype = ivy.as_native_dtype(dtype)
-    if "complex" not in ivy.as_ivy_dtype(x.dtype) and "complex" in dtype:
+    if "complex" not in ivy.as_ivy_dtype(x.dtype) and "complex" in str(dtype):
         x = ivy.astype(x, dtype)
     return torch.linalg.matrix_norm(
         x, ord=ord, dim=axis, keepdim=keepdims, dtype=dtype, out=out
