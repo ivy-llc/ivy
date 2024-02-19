00ba2b9a784a8bb4736e823acf674a2e76da0556
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-19 20:27:03 +0100

    fix: wrong output dtype; changes in the test

--- ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
@@ -837,6 +837,8 @@ def test_torch_matrix_norm(
     on_device,
 ):
     input_dtype, x, axis = dtype_values_axis
+    if int(dtype[0][-2:]) < int(input_dtype[0][-2:]) and "128" not in dtype[0]:
+        dtype[0] = dtype[0][:-1] + input_dtype[0][-2:]
 
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
