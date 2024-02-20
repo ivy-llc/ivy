aab68dafed4fdfe97d59a5ef0c2b284e324e9712
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-02-20 13:59:56 +0530

    Updated test

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -286,7 +286,7 @@ def test_tensorflow_argmin(
     on_device,
     output_type,
 ):
-    input_dtype, x, axis, dtype3, where = dtype_and_x
+    input_dtype, x, axis = dtype_and_x[:3]
     if isinstance(axis, tuple):
         axis = axis[0]
     helpers.test_frontend_function(
