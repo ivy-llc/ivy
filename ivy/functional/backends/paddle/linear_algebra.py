abead2cc8e0a8ece4c1390c89bed7e5068a8c252
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-22 23:40:39 +0100

    core linalg matrix_norm trying test with no dtype

--- ivy/functional/backends/paddle/linear_algebra.py
+++ ivy/functional/backends/paddle/linear_algebra.py
@@ -266,8 +266,7 @@ def matrix_norm(
     dtype: Optional[paddle.dtype] = None,
     out: Optional[paddle.Tensor] = None,
 ) -> paddle.Tensor:
-    if dtype is not None:
-        x = ivy.astype(x, dtype)
+
     axis_ = list(axis)  # paddle.moveaxis doesn't support tuple axes
     if ord == "nuc":
         x = paddle.moveaxis(x, axis_, [-2, -1])
