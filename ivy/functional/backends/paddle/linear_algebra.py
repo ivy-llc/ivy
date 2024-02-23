b2ec293598fd7f65e77c2a8e1f153e237d5a90f3
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-23 13:58:13 +0100

    fix paddle bf dtype argument

--- ivy/functional/backends/paddle/linear_algebra.py
+++ ivy/functional/backends/paddle/linear_algebra.py
@@ -268,7 +268,7 @@ def matrix_norm(
 ) -> paddle.Tensor:
     if dtype is not None:
         dtype = ivy.as_ivy_dtype(dtype) if dtype is not str else dtype
-        x = ivy.native_array(x, dtype)
+        x = ivy.native_array(x, dtype=dtype)
     axis_ = list(axis)  # paddle.moveaxis doesn't support tuple axes
     if ord == "nuc":
         x = paddle.moveaxis(x, axis_, [-2, -1])
