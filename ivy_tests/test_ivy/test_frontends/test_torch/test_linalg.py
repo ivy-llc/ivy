7fb666540ecb5e4d23ac7cead35481901fe9fd23
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-25 18:57:42 +0100

    grad test flag False

--- ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
@@ -930,6 +930,8 @@ def test_torch_matrix_norm(
         dtypes[0] = input_dtype[0]
     if dtypes[0] is not None:
         dtypes[0] = input_dtype[0][:-2] + max([input_dtype[0][-2:], dtypes[0][-2:]])
+    if dtypes[0] is not None and "28" in dtypes[0]:
+        dtypes[0] = dtypes[0][:-2] + "64"
 
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
--- ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
@@ -672,6 +672,8 @@ def test_matrix_norm(
         dtypes[0] = input_dtype[0]
     if dtypes[0] is not None:
         dtypes[0] = input_dtype[0][:-2] + max([input_dtype[0][-2:], dtypes[0][-2:]])
+    if dtypes[0] is not None and "28" in dtypes[0]:
+        dtypes[0] = dtypes[0][:-2] + "64"
 
     # test_flags.test_gradients = False
 
