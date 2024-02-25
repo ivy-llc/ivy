2d0270c879bf3c8903fad402a486eec2f4a45672
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-25 18:39:35 +0100

    grad test flag False

--- ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
@@ -644,7 +644,7 @@ def test_matmul(*, x, y, test_flags, backend_fw, fn_name, on_device):
 # matrix_norm
 @handle_test(
     fn_tree="functional.ivy.matrix_norm",
-    # ground_truth_backend="numpy",
+    ground_truth_backend="torch",
     dtype_value_axis=helpers.dtype_values_axis(
         available_dtypes=helpers.get_dtypes("float_and_complex"),
         min_num_dims=2,
@@ -674,7 +674,7 @@ def test_matrix_norm(
         dtypes[0] = input_dtype[0][:-2] + max([input_dtype[0][-2:], dtypes[0][-2:]])
 
     # test_flags.test_gradients = False
-    test_flags.ground_truth_backend = "numpy"
+
     assume(matrix_is_stable(x[0], cond_limit=10))
     helpers.test_function(
         input_dtypes=input_dtype,
