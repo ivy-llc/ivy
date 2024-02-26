d50180710a43a59ef0729d57fa88f5fe035dd019
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-02-26 02:12:22 +0100

    grad test flag False

--- ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
+++ ivy_tests/test_ivy/test_functional/test_core/test_linalg.py
@@ -674,6 +674,8 @@ def test_matrix_norm(
         dtypes[0] = input_dtype[0][:-2] + max([input_dtype[0][-2:], dtypes[0][-2:]])
     if "bfloat" in dtypes[0]:
         dtypes[0] = "bfloat16"
+    if dtypes[0] == "float16":
+        dtypes[0] = "float32"
     test_flags.test_gradients = False
 
     assume(matrix_is_stable(x[0], cond_limit=10))
