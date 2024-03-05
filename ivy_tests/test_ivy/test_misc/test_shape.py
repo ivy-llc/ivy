ac5cbf71189c57de34907f0cd18a97960db8d77d
ayush <52037893+ayush111111@users.noreply.github.com>
2024-03-05 09:36:40 +0000

    test fix for tf

--- ivy_tests/test_ivy/test_misc/test_shape.py
+++ ivy_tests/test_ivy/test_misc/test_shape.py
@@ -44,6 +44,7 @@ def test_shape__add__(
 
 
 @handle_method(
+    init_tree=CLASS_TREE,
     method_tree="Shape.__bool__",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=st.one_of(st.just(("bool",)), helpers.get_dtypes("integer")),
@@ -69,7 +70,7 @@ def test_shape__bool__(
         backend_to_test=backend_fw,
         init_flags=init_flags,
         method_flags=method_flags,
-        init_all_as_kwargs_np={"data": x[0]},
+        init_all_as_kwargs_np={"shape_tup": x},
         init_input_dtypes=dtype,
         method_input_dtypes=[],
         method_all_as_kwargs_np={},
