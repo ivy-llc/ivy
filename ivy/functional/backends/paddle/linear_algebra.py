0dfcf9d224b4adf0454ce158d0a24f88cd923b00
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-03-06 23:49:36 +0100

    changes to norms -2, 2, -inf, inf in paddle.linalg.norm

--- ivy/functional/backends/paddle/linear_algebra.py
+++ ivy/functional/backends/paddle/linear_algebra.py
@@ -269,6 +269,7 @@ def matrix_norm(
     if dtype is not None:
         x = ivy.astype(x, dtype).to_native()
     axis_ = list(axis)  # paddle.moveaxis doesn't support tuple axes
+    x.stop_gradient = False
     if ord == "nuc":
         x = paddle.moveaxis(x, axis_, [-2, -1])
         # backend implementation is used here instead of native implementation
