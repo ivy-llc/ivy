60e8b00583a2d4919bcb1712b529b3e5ce7dc179
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-03-08 18:30:19 +0100

    changes to out in torch backend

--- ivy/functional/backends/torch/linear_algebra.py
+++ ivy/functional/backends/torch/linear_algebra.py
@@ -206,7 +206,12 @@ def matrix_norm(
     dtype: Optional[torch.dtype] = None,
     out: Optional[torch.Tensor] = None,
 ) -> torch.Tensor:
-    if "complex" in ivy.as_native_dtype(dtype) and ivy.exists(out):
+    # ivy forces out to be the same type as the input
+    # but it has to be the same type as ret.
+    if (
+        "complex" in ivy.as_native_dtype(dtype)
+        or "complex" in ivy.as_native_dtype(x.dtype)
+    ) and ivy.exists(out):
         out = ivy.astype(out, "float64").to_native()
     ret = torch.linalg.matrix_norm(
         x, ord=ord, dim=axis, keepdim=keepdims, dtype=dtype, out=out
