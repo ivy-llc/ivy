0bea022cd69d236a984ce944423ed7e92ea468db
Jay Choy <91728831+ZJay07@users.noreply.github.com>
2024-03-11 11:09:19 +0000

    fixed tf.reduce_prod

--- ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
+++ ivy_tests/test_ivy/test_frontends/test_tensorflow/test_math.py
@@ -2432,6 +2432,8 @@ def test_tensorflow_reduce_min(
     fn_tree="tensorflow.math.reduce_prod",
     dtype_and_x=helpers.dtype_and_values(
         available_dtypes=helpers.get_dtypes("numeric"),
+        min_value=-5,
+        max_value=5,
     ),
     test_with_out=st.just(False),
 )
@@ -2453,6 +2455,8 @@ def test_tensorflow_reduce_prod(
         fn_tree=fn_tree,
         on_device=on_device,
         input_tensor=x[0],
+        rtol=1e-02,
+        atol=1e-02,
     )
 
 
