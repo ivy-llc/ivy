fix: fix test_matrix_norm and test_torch_matrix_norm by handling dtype, add type-casting, fix torch backend out type, expand tests, note gradient test issue in Paddle (#28500)

ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
@@ -899,7 +899,7 @@ def test_torch_matrix_exp(
 @handle_frontend_test(
     fn_tree="torch.linalg.matrix_norm",
     dtype_values_axis=helpers.dtype_values_axis(
-        available_dtypes=helpers.get_dtypes("float_and_complex"),
+        available_dtypes=helpers.get_dtypes("valid"),
         min_num_dims=2,
         min_axes_size=2,
         max_axes_size=2,
@@ -911,7 +911,7 @@ def test_torch_matrix_exp(
     ),
     ord=st.sampled_from(["fro", "nuc", np.inf, -np.inf, 1, -1, 2, -2]),
     keepdim=st.booleans(),
-    dtypes=helpers.get_dtypes("float_and_complex", none=True, full=False),
+    dtypes=st.sampled_from((None, "16", "32", "64")),
 )
 def test_torch_matrix_norm(
     *,
@@ -926,10 +926,12 @@ def test_torch_matrix_norm(
     on_device,
 ):
     input_dtype, x, axis = dtype_values_axis
-    if dtypes[0] is not None and "complex128" in input_dtype[0]:
-        dtypes[0] = input_dtype[0]
-    if dtypes[0] is not None:
-        dtypes[0] = input_dtype[0][:-2] + max([input_dtype[0][-2:], dtypes[0][-2:]])
+    if dtypes is not None:
+        # torch backend does not allow down-casting.
+        if input_dtype[0] == "complex128":
+            dtypes = input_dtype[0]
+        else:
+            dtypes = input_dtype[0][0:-2] + max([input_dtype[0][-2:], dtypes])
 
     helpers.test_frontend_function(
         input_dtypes=input_dtype,
@@ -944,7 +946,7 @@ def test_torch_matrix_norm(
         ord=ord,
         dim=axis,
         keepdim=keepdim,
-        dtype=dtypes[0],
+        dtype=dtypes,
     )
 
 