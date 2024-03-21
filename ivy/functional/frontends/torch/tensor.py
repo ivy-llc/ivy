28594e1a48d08a4847f6fe80c08fe8f4295c8d00
Jay Choy <91728831+ZJay07@users.noreply.github.com>
2024-03-21 19:26:40 +0000

    proper fix

--- ivy/functional/frontends/torch/tensor.py
+++ ivy/functional/frontends/torch/tensor.py
@@ -1186,9 +1186,7 @@ class Tensor:
     def fix(self):
         return torch_frontend.fix(self)
 
-    @with_supported_dtypes(
-        {"2.2 and below": ("int32", "int64", "float64", "float32")}, "torch"
-    )
+    @with_unsupported_dtypes({"2.2 and below": ("float16", "complex")}, "torch")
     def fix_(self):
         self.ivy_array = self.fix().ivy_array
         return self
