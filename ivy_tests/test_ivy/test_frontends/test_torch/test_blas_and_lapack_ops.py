14376c758c8de44750ec583c87c3947ce5800aee
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2024-03-21 17:18:52 +0000

    chore: remove duplicated and incorrect test for torch's matrix rank
    the correct version is [here](https://github.com/unifyai/ivy/blob/88581decd00a3a834031dcb9f59c63bc013ed72e/ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py#L990)

--- ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
@@ -691,37 +691,6 @@ def test_torch_matmul(
     )
 
 
-# matrix_rank
-@handle_frontend_test(
-    fn_tree="torch.linalg.matrix_rank",
-    # aliases=["torch.matrix_rank",], deprecated since 1.9. uncomment with multi-version
-    # testing pipeline
-    dtype_x_hermitian_atol_rtol=_matrix_rank_helper(),
-)
-def test_torch_matrix_rank(
-    dtype_x_hermitian_atol_rtol,
-    on_device,
-    fn_tree,
-    frontend,
-    test_flags,
-    backend_fw,
-):
-    dtype, x, hermitian, atol, rtol = dtype_x_hermitian_atol_rtol
-    assume(matrix_is_stable(x, cond_limit=10))
-    helpers.test_frontend_function(
-        input_dtypes=dtype,
-        backend_to_test=backend_fw,
-        frontend=frontend,
-        test_flags=test_flags,
-        fn_tree=fn_tree,
-        on_device=on_device,
-        A=x,
-        atol=atol,
-        rtol=rtol,
-        hermitian=hermitian,
-    )
-
-
 # mm
 @handle_frontend_test(
     fn_tree="torch.mm",
