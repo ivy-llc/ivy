12428b7f56958d5c9b2af5057e4fd473d91fa720
Jay Choy <91728831+ZJay07@users.noreply.github.com>
2024-03-23 16:18:51 +0000

    fixed tensorflow.clip_by_value

--- ivy/functional/frontends/tensorflow/general_functions.py
+++ ivy/functional/frontends/tensorflow/general_functions.py
@@ -94,7 +94,7 @@ def clip_by_norm(t, clip_norm, axes=None):
 
 
 @to_ivy_arrays_and_back
-@with_unsupported_dtypes({"2.15.0 and below": ("float16",)}, "tensorflow")
+@with_unsupported_dtypes({"2.15.0 and below": ("float16", "complex")}, "tensorflow")
 def clip_by_value(t, clip_value_min, clip_value_max):
     ivy.utils.assertions.check_all_or_any_fn(
         clip_value_min,
