4f05a09ff892de70d72e4facfc69496830d5237a
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2024-03-30 06:11:59 +0000

    added ifftshift and corresponding test

--- ivy/functional/frontends/jax/numpy/fft.py
+++ ivy/functional/frontends/jax/numpy/fft.py
@@ -79,6 +79,33 @@ def ifftn(a, s=None, axes=None, norm=None):
 
 
 @to_ivy_arrays_and_back
+def ifftshift(x, axes=None):
+    # Check if an array
+    if not ivy.is_array(x):
+        raise ValueError("Input 'x' must be an  array")
+
+    # Get the shape of x
+    shape = ivy.shape(x)
+
+    # If axes is None, shift all axes
+    if axes is None:
+        axes = list(range(len(shape)))
+
+    # Initialize a list to store the shift values
+    shift_values = []
+
+    # Calculate shift values for each axis
+    for axis in axes:
+        axis_size = shape[axis]
+        shift = -ivy.floor(axis_size / 2).astype(ivy.int32)
+        shift_values.append(shift)
+
+    # Perform the shift using Ivy's roll function
+    result = ivy.roll(x, shift_values, axes)
+    return result
+
+
+@to_ivy_arrays_and_back
 @with_unsupported_dtypes({"1.25.2 and below": ("float16", "bfloat16")}, "numpy")
 def rfft(a, n=None, axis=-1, norm=None):
     if n is None:
