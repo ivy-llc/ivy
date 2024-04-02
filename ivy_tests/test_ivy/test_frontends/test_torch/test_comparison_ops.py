9be170c9a795a8294d0a831b50a64209d9e3877c
Jay Choy <91728831+ZJay07@users.noreply.github.com>
2024-04-02 19:06:04 +0000

    used abs_smallest_val instead

--- ivy_tests/test_ivy/test_frontends/test_torch/test_comparison_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_comparison_ops.py
@@ -143,7 +143,7 @@ def test_torch_eq(
         num_arrays=2,
         allow_inf=False,
         shared_dtype=True,
-        min_value=1e-07,
+        abs_smallest_val=1e-07,
     ),
 )
 def test_torch_equal(
