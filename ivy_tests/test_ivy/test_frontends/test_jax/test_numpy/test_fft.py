3221d42bc695c599fa140b3375a7ebfe502aa797
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2024-04-09 15:09:03 +0530

    feat: implemented ifftshift and the corresponding test for jax frontend (#28707)

--- ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_fft.py
+++ ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_fft.py
@@ -271,6 +271,43 @@ def test_jax_numpy_ifftn(
     )
 
 
+# ifftshift
+@handle_frontend_test(
+    fn_tree="jax.numpy.fft.ifftshift",
+    dtype_values_axis=helpers.dtype_values_axis(
+        available_dtypes=helpers.get_dtypes("valid"),
+        num_arrays=1,
+        min_value=-1e5,
+        max_value=1e5,
+        min_num_dims=1,
+        max_num_dims=5,
+        min_dim_size=1,
+        max_dim_size=5,
+        allow_inf=False,
+        large_abs_safety_factor=2.5,
+        small_abs_safety_factor=2.5,
+        safety_factor_scale="log",
+        valid_axis=True,
+        force_int_axis=True,
+    ),
+)
+def test_jax_numpy_ifftshift(
+    dtype_values_axis, backend_fw, frontend, test_flags, fn_tree, on_device
+):
+    dtype, values, axis = dtype_values_axis
+    helpers.test_frontend_function(
+        input_dtypes=dtype,
+        frontend=frontend,
+        backend_to_test=backend_fw,
+        test_flags=test_flags,
+        fn_tree=fn_tree,
+        on_device=on_device,
+        test_values=True,
+        x=values[0],
+        axes=axis,
+    )
+
+
 # rfft
 @handle_frontend_test(
     fn_tree="jax.numpy.fft.rfft",
