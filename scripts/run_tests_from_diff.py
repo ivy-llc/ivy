6d25d958aac5a581d8f5bdb789c28efdcdfcf340
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2024-05-16 14:47:07 +0100

    fix: CI tests (#28749)

--- scripts/run_tests_from_diff.py
+++ scripts/run_tests_from_diff.py
@@ -69,7 +69,7 @@ print("Running tests:", test_paths)
 
 for test_path in set(test_paths):
     pytest_command = (
-        f"pytest {file_path} -p no:warnings --tb=short --backend jax,tensorflow,torch"
+        f"pytest {test_path} -p no:warnings --tb=short --backend jax,tensorflow,torch"
     )
     print(f"Running test command: {pytest_command}")
     result = subprocess.run(pytest_command, shell=True)
