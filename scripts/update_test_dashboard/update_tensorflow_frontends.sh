fba64d6ff60abddf9386bed4af5205dcc32988ab
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-19 22:13:56 +0100

    fix pytest exiting the script prematurely

--- scripts/update_test_dashboard/update_tensorflow_frontends.sh
+++ scripts/update_test_dashboard/update_tensorflow_frontends.sh
@@ -3,8 +3,11 @@
 submodule=$1
 backend=$2
 
+set +e
 pytest ivy_tests/test_ivy/test_frontends/test_tensorflow/test_$submodule.py --backend $backend -p no:warnings --tb=short
 pytest_exit_code=$?
+set -e
+
 if [ $pytest_exit_code -eq 0 ] || [ $pytest_exit_code -eq 1 ]; then
     exit 0
 else
