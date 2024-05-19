edf5c5ef469b4ab64794bd6de2dfb9179d9a143c
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-19 16:46:17 +0100

    fix

--- scripts/update_test_dashboard/update_tensorflow_image_frontends.sh
+++ scripts/update_test_dashboard/update_tensorflow_image_frontends.sh
@@ -4,3 +4,9 @@ submodule=$1
 backend=$2
 
 pytest ivy_tests/test_ivy/test_frontends/test_tensorflow/test_image/test_$submodule.py --backend $backend -p no:warnings --tb=short
+pytest_exit_code=$?
+if [ $pytest_exit_code -eq 0 ] || [ $pytest_exit_code -eq 1 ]; then
+    exit 0
+else
+    exit 1
+fi
