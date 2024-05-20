b44c3cc9c7ef44332eff833ef5d6d5d1e6c7470b
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-20 01:04:14 +0100

    populate the scripts

---
+++ scripts/update_test_dashboard/update_ivy_experimental_core.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+set +e
+pytest ivy_tests/test_ivy/test_functional/test_experimental/test_core/ -p no:warnings --tb=short
+pytest_exit_code=$?
+set -e
+
+if [ $pytest_exit_code -eq 0 ] || [ $pytest_exit_code -eq 1 ]; then
+    exit 0
+else
+    exit 1
+fi
