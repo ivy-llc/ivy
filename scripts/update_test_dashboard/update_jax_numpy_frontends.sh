3110e725b10af530329d1bf00236c608934275b5
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-20 02:27:06 +0100

    fix workflow error check bug

--- scripts/update_test_dashboard/update_jax_numpy_frontends.sh
+++ scripts/update_test_dashboard/update_jax_numpy_frontends.sh
@@ -2,8 +2,11 @@
 
 submodule=$1
 
+set +e
 pytest ivy_tests/test_ivy/test_frontends/test_jax/test_numpy/test_$submodule.py -p no:warnings --tb=short
 pytest_exit_code=$?
+set -e
+
 if [ $pytest_exit_code -eq 0 ] || [ $pytest_exit_code -eq 1 ]; then
     exit 0
 else
