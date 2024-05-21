75cd402ad376a9fe0265e984e2156d2037d8583f
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2024-05-21 13:33:59 +0100

    cleanup

--- scripts/update_test_dashboard/update_torch_frontends.sh
+++ scripts/update_test_dashboard/update_torch_frontends.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# pip install pytest-json-report 
-# pytest ivy_tests/test_ivy/test_functional/test_core/test_elementwise.py -p no:warnings --json-report --json-report-file=report.json
-
 submodule=$1
 backend=$2
 workflow_link=$3
