6807b0e1931e1c297908a89c576e614e301cc0ac
Abdullah Sabry <abdullahsabry02@gmail.com>
2024-07-12 20:45:58 +0300

    fix(torch-frontend): fix failing test for torch.nn.functional.pad frontend (#27206)
    Co-authored-by: Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>

--- ivy_tests/test_ivy/test_frontends/test_torch/test_nn/test_functional/test_vision_functions.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_nn/test_functional/test_vision_functions.py
@@ -68,7 +68,7 @@ def _pad_generator(draw, shape, mode):
             max_pad_value = shape[i] - 1
         pad = pad + draw(
             st.tuples(
-                st.integers(min_value=-3, max_value=max(0, max_pad_value)),
+                st.integers(min_value=0, max_value=max(0, max_pad_value)),
                 st.integers(min_value=-3, max_value=max(0, max_pad_value)),
             )
         )
