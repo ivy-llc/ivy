5f000a71de9ce151202ad05a9dc13843ddaa4a2f
muzakkirhussain011 <138936198+muzakkirhussain011@users.noreply.github.com>
2024-08-08 21:09:07 +0300

    Update ivy_tests/test_ivy/test_frontends/test_sklearn/test_preprocessing/test_label.py
    Co-authored-by: Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>

--- ivy_tests/test_ivy/test_frontends/test_sklearn/test_preprocessing/test_label.py
+++ ivy_tests/test_ivy/test_frontends/test_sklearn/test_preprocessing/test_label.py
@@ -98,22 +98,8 @@ def test_sklearn_label_encoder_fit_transform(
     on_device,
     backend_fw,
 ):
-    input_dtype, x = dtype_x
-
-    helpers.test_frontend_method(
-        init_input_dtypes=input_dtype,
-        init_all_as_kwargs_np={},
-        method_input_dtypes=input_dtype,
-        method_all_as_kwargs_np={
-            "y": x[0],
-        },
-        frontend_method_data=frontend_method_data,
-        init_flags=init_flags,
-        method_flags=method_flags,
-        frontend=frontend,
-        on_device=on_device,
-        backend_to_test=backend_fw,
-    )
+    # TODO: add test for this method with encoder already fitted with data
+    pass
 
 
 @handle_frontend_method(
