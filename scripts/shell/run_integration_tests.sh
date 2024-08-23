07759c5d296a3d063f722694f460ee02f3fb32f7
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2024-08-23 02:11:01 +0100

    run integration tests outside of docker to prevent 'no space left on device' (#28810)

--- scripts/shell/run_integration_tests.sh
+++ scripts/shell/run_integration_tests.sh
@@ -4,6 +4,27 @@ integration=$1
 target=$2
 
 export IVY_KEY=$3
+export VERSION=linux-nightly
 
+pip3 install -r requirements/requirements.txt --upgrade
+pip3 install jax
+pip3 install jaxlib
+pip3 install flax
+pip3 install opencv-python
+pip3 install pytest
+pip3 install tensorflow
+pip3 install torch
+pip3 install torchvision
 pip3 install kornia
+pip3 install accelerate
+pip3 install transformers
+pip3 install redis
+pip3 install hypothesis
+
+# get the nightly binaries
+python << 'EOF'
+import ivy
+ivy.utils.cleanup_and_fetch_binaries()
+EOF
+
 pytest ivy_tests/test_integrations/test_$integration.py -p no:warnings --target $target
