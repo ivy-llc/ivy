12217c740a5cf83aaa1ff6c8f2cc816524d030fd
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2024-09-28 20:42:14 +0100

    small fix of test_torch.test_blas_and_lapack_ops.test_torch_svd

--- ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_blas_and_lapack_ops.py
@@ -887,7 +887,7 @@ def test_torch_svd(
         ret = [x.detach() for x in ret]
     ret = [np.asarray(x) for x in ret]
     frontend_ret = [
-        np.asarray(x.resolve_conj()).astype(input_dtype[0]) for x in frontend_ret
+        np.asarray(x.resolve_conj()) for x in frontend_ret
     ]
     u, s, v = ret
     frontend_u, frontend_s, frontend_v = frontend_ret
