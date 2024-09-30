e87356699e6383fc489ec7068a7bdd02022868ed
Daniel4078 <45633544+Daniel4078@users.noreply.github.com>
2024-09-30 10:43:49 +0100

    Update test_linalg.py

--- ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
+++ ivy_tests/test_ivy/test_frontends/test_torch/test_linalg.py
@@ -1288,7 +1288,7 @@ def test_torch_svd(
                 frontend_u[..., : frontend_s.shape[0]]
                 @ np.diag(frontend_s)
                 @ frontend_vh
-            )
+            ),
             ret_from_gt_np=u[..., : s.shape[0]] @ np.diag(s) @ vh,
             atol=1e-04,
             backend=backend_fw,
@@ -1296,7 +1296,7 @@ def test_torch_svd(
         )
     else:
         helpers.assert_all_close(
-            ret_np=(frontend_u @ np.diag(frontend_s) @ frontend_vh).astype(d),
+            ret_np=(frontend_u @ np.diag(frontend_s) @ frontend_vh),
             ret_from_gt_np=u @ np.diag(s) @ vh,
             atol=1e-04,
             backend=backend_fw,
