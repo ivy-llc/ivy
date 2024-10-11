ae1e0023f08335e9d8d0c26b4263d6b80e3eed00
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2024-10-11 19:24:11 +0100

    Update compiler.py 7298511
    Co-authored-by: ivy-dev-bot <hello@transpile-ai.com>

--- ivy/compiler/compiler.py
+++ ivy/compiler/compiler.py
@@ -2,83 +2,18 @@ from typing import Callable, Optional, List, Union, Iterable, Sequence, Mapping
 
 
 def clear_graph_cache():
-    """Clears the graph cache which gets populated if `graph_caching` is set to
-    `True` in `ivy.trace_graph`, `ivy.transpile` or `ivy.unify`. Use this to
+    """Clears the graph cache which gets populated if `graph_caching` is set
+    to `True` in `ivy.trace_graph`, `ivy.transpile` or `ivy.unify`. Use this to
     reset or clear the graph cache if needed.
 
     Examples
     --------
     >>> import ivy
-    >>> ivy.clear_graph_cache()
-    """
+    >>> ivy.clear_graph_cache()"""
+
     from ._compiler import clear_graph_cache as _clear_graph_cache
 
     return _clear_graph_cache()
-
-
-def graph_transpile(
-    *objs: Callable,
-    source: Optional[str] = None,
-    to: Optional[str] = None,
-    with_numpy: bool = True,
-    backend_compile: bool = False,
-    static_argnums: Optional[Union[int, Iterable[int]]] = None,
-    static_argnames: Optional[Union[str, Iterable[str]]] = None,
-    compile_mode: Optional[str] = None,
-    graph_caching: bool = True,
-    graph_optimizations: bool = True,
-    modes_to_trace: str = "all",
-    stateful: Optional[List] = None,
-    arg_stateful_idxs: Optional[List] = None,
-    kwarg_stateful_idxs: Optional[List] = None,
-    args: Optional[Sequence] = None,
-    kwargs: Optional[Mapping] = None,
-    params_v=None,
-    v=None
-):
-    """Transpiles Callable objects passed as arguments. If args and kwargs are
-    specified, transpilation is performed eagerly, otherwise, transpilation
-    will happen lazily.
-
-    Parameters
-    ----------
-    objs
-        The native Callables to be transpiled
-    source
-        The framework that `obj` is from.
-    to
-        The target framework to transpile `obj` to.
-    args
-        If specified, arguments that will be used to transpile eagerly.
-    kwargs
-        If specified, keyword arguments that will be used to transpile eagerly.
-
-    Returns
-    -------
-    Either a transpiled Graph or a non-initialized LazyGraph.
-    """
-    from ._compiler import graph_transpile as _graph_transpile
-
-    return _graph_transpile(
-        *objs,
-        source=source,
-        to=to,
-        with_numpy=with_numpy,
-        backend_compile=backend_compile,
-        static_argnums=static_argnums,
-        static_argnames=static_argnames,
-        compile_mode=compile_mode,
-        graph_caching=graph_caching,
-        graph_optimizations=graph_optimizations,
-        modes_to_trace=modes_to_trace,
-        stateful=stateful,
-        arg_stateful_idxs=arg_stateful_idxs,
-        kwarg_stateful_idxs=kwarg_stateful_idxs,
-        args=args,
-        kwargs=kwargs,
-        params_v=params_v,
-        v=v,
-    )
 
 
 def source_to_source(
@@ -96,7 +31,6 @@ def source_to_source(
     e.g. (source="torch_frontend", target="ivy") or (source="torch_frontend", target="tensorflow") etc.
 
     Args:
-    ----
         object: The object (class/function) to be translated.
         source (str, optional): The source framework. Defaults to 'torch'.
         target (str, optional): The target framework. Defaults to 'tensorflow'.
@@ -107,9 +41,8 @@ def source_to_source(
                                          the old implementation. Defaults to 'True'.
 
     Returns:
-    -------
-    The translated object.
-    """
+        The translated object."""
+
     from ._compiler import source_to_source as _source_to_source
 
     return _source_to_source(
@@ -140,8 +73,7 @@ def trace_graph(
     params_v=None,
     v=None
 ):
-    """Takes `fn` and traces it into a more efficient composition of backend
-    operations.
+    """Takes `fn` and traces it into a more efficient composition of backend operations.
 
     Parameters
     ----------
@@ -211,8 +143,8 @@ def trace_graph(
     >>> start = time.time()
     >>> graph(x)
     >>> print(time.time() - start)
-    0.0001785755157470703
-    """
+    0.0001785755157470703"""
+
     from ._compiler import trace_graph as _trace_graph
 
     return _trace_graph(
@@ -252,7 +184,6 @@ def transpile(
     e.g. (source="torch_frontend", target="ivy") or (source="torch_frontend", target="tensorflow") etc.
 
     Args:
-    ----
         object: The object (class/function) to be translated.
         source (str, optional): The source framework. Defaults to 'torch'.
         target (str, optional): The target framework. Defaults to 'tensorflow'.
@@ -263,9 +194,8 @@ def transpile(
                                          the old implementation. Defaults to 'True'.
 
     Returns:
-    -------
-    The translated object.
-    """
+        The translated object."""
+
     from ._compiler import transpile as _transpile
 
     return _transpile(
@@ -273,31 +203,4 @@ def transpile(
         source=source,
         target=target,
         reuse_existing=reuse_existing,
-    )
-
-
-def unify(
-    *objs: Callable,
-    source: Optional[str] = None,
-    graph_caching: bool = True,
-    graph_optimizations: bool = True,
-    args: Optional[Sequence] = None,
-    kwargs: Optional[Mapping] = None,
-    with_numpy: bool = True,
-    modes_to_trace: str = "all",
-    **transpile_kwargs
-):
-
-    from ._compiler import unify as _unify
-
-    return _unify(
-        *objs,
-        source=source,
-        graph_caching=graph_caching,
-        graph_optimizations=graph_optimizations,
-        args=args,
-        kwargs=kwargs,
-        with_numpy=with_numpy,
-        modes_to_trace=modes_to_trace,
-        **transpile_kwargs,
     )
