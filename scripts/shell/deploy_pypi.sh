8d8384f0095aa1d0b1caf414cebcca73daad513c
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2024-10-22 09:28:06 +0100

    debug: building wheels

--- scripts/shell/deploy_pypi.sh
+++ scripts/shell/deploy_pypi.sh
@@ -4,4 +4,4 @@ jq -c '.compiler[]' available_configs.json | while read config; do
     python -m build
     python3 scripts/rename_wheels.py
 done
-python3 -m twine upload dist/* -u "__token__" -p "$PYPI_PASSWORD" --verbose
+# python3 -m twine upload dist/* -u "__token__" -p "$PYPI_PASSWORD" --verbose
