bb46a72f1ba79347bc939e95e115580650aeea0d
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2024-12-16 15:55:19 +0000

    add tracer tests

---
+++ ivy_tests/test_tracer/test_tracing/simple_math_in_ivy.py
@@ -0,0 +1,21 @@
+import ivy
+
+
+def add(x, y):
+    return ivy.add(x, y)
+
+
+def sub(x, y):
+    return ivy.subtract(x, y)
+
+
+def mul(x, y):
+    return ivy.multiply(x, y)
+
+
+def div(x, y):
+    return ivy.divide(x, y)
+
+
+def pow(x, y):
+    return ivy.pow(x, y)
