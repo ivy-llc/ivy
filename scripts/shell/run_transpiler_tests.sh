3bb56e151263db9466ecf13dde7f9d78754664cb
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2025-01-02 12:57:18 +0000

    add missing datasets and transformers installs to script

--- scripts/shell/run_transpiler_tests.sh
+++ scripts/shell/run_transpiler_tests.sh
@@ -3,6 +3,6 @@
 folder=$1
 export DEBUG=0
 
-python3 -m pip install timm kornia FrEIA
+python3 -m pip install datasets transformers timm kornia FrEIA
 
 python3 -m pytest ivy_tests/test_transpiler/$folder -p no:warnings
