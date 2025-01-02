7ae292d10bca111ac00ab747807c600fceca52d3
Sam-Armstrong <samuel_e_armstrong@yahoo.co.uk>
2025-01-02 13:17:06 +0000

    only run transpiler ci on push/pr to main

--- scripts/shell/run_transpiler_tests.sh
+++ scripts/shell/run_transpiler_tests.sh
@@ -3,6 +3,6 @@
 folder=$1
 export DEBUG=0
 
-python3 -m pip install datasets transformers onnx timm kornia FrEIA
+python3 -m pip install datasets transformers onnx timm kornia FrEIA tf-keras
 
 python3 -m pytest ivy_tests/test_transpiler/$folder -p no:warnings
