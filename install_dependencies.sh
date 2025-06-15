e40c87cfc5f6cdd2ef9f02f107cf71e4a4ea7323
Sam Armstrong <88863522+Sam-Armstrong@users.noreply.github.com>
2025-06-15 20:06:46 +0100

    remove non-generalised requirements files

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,8 +4,7 @@ pip install -e .
 pip install -r requirements/requirements.txt
 if [[ $(arch) == 'arm64' ]]; then
       brew install pandoc
-      pip install -r requirements/optional_apple_silicon_1.txt
-      pip install -r requirements/optional_apple_silicon_2.txt
+      pip install -r requirements/optional.txt
 else
     sudo apt-get update
     sudo apt-get install pandoc -y
